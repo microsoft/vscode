5e39bad6c830803d5b230d656dccad0dbf5175eb
Daniel Imms <tyriar@tyriar.com>
2015-11-26 19:37:07 -0800

    Add copyright to htmlTagSpecifications

--- src/vs/languages/html/common/htmlTagSpecifications.ts
+++ src/vs/languages/html/common/htmlTagSpecifications.ts
@@ -1,3 +1,8 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
 export class HTMLTagSpecification {
 	constructor(public label: string, public attributes: string[] = []) {}
 }
