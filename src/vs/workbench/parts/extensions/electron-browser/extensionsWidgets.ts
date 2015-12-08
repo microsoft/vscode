a9856260abc9f655e1df00018307f8195578aa4d
isidor <inikolic@microsoft.com>
2015-12-08 15:56:35 +0100

    show all extension commands on extension status bar click.

--- src/vs/workbench/parts/extensions/electron-browser/extensionsWidgets.ts
+++ src/vs/workbench/parts/extensions/electron-browser/extensionsWidgets.ts
@@ -5,49 +5,40 @@
 
 import nls = require('vs/nls');
 import Severity from 'vs/base/common/severity';
+import errors = require('vs/base/common/errors');
 import dom = require('vs/base/browser/dom');
 import lifecycle = require('vs/base/common/lifecycle');
 import statusbar = require('vs/workbench/browser/parts/statusbar/statusbar');
 import { IPluginService, IPluginStatus } from 'vs/platform/plugins/common/plugins';
-import { IMessageService } from 'vs/platform/message/common/message';
+import { IQuickOpenService } from 'vs/workbench/services/quickopen/browser/quickOpenService';
 
 var $ = dom.emmet;
 
 export class ExtensionsStatusbarItem implements statusbar.IStatusbarItem {
 
 	private toDispose: lifecycle.IDisposable[];
-	private severity: Severity;
 	private domNode: HTMLElement;
-	private status: { [id: string]: IPluginStatus };
 
 	constructor(
 		@IPluginService private pluginService: IPluginService,
-		@IMessageService private messageService: IMessageService
+		@IQuickOpenService private quickOpenService: IQuickOpenService
 	) {
 		this.toDispose = [];
-		this.severity = Severity.Ignore;
 
 		this.pluginService.onReady().then(() => {
-			this.status = this.pluginService.getPluginsStatus();
-			Object.keys(this.status).forEach(key => {
-				this.severity = this.status[key].messages.reduce((maxSeverity, message) => Math.max(maxSeverity, message.type), this.severity);
+			const pluginsStatus = this.pluginService.getPluginsStatus();
+			Object.keys(pluginsStatus).forEach(key => {
+				const severity = pluginsStatus[key].messages.reduce((maxSeverity, message) => Math.max(maxSeverity, message.type), Severity.Ignore);
+				this.domNode.classList.add(Severity[severity].toLowerCase());
 			});
-			this.domNode.hidden = this.severity === Severity.Ignore;
 		});
 	}
 
 	public render(container: HTMLElement): lifecycle.IDisposable {
 		this.domNode = dom.append(container, $('.extensions-statusbar octicon octicon-package'));
-		this.domNode.title = nls.localize('extensionsStatus', "Extensions Status"),
-		this.domNode.hidden = true;
+		this.domNode.title = nls.localize('extensions', "Extensions"),
 		this.toDispose.push(dom.addDisposableListener(this.domNode, 'click', () => {
-			Object.keys(this.status).forEach(key => {
-				this.status[key].messages.forEach(m => {
-					if (m.type === this.severity) {
-						this.messageService.show(m.type, m.message);
-					}
-				});
-			});
+			this.quickOpenService.show('>extensions: ').done(null, errors.onUnexpectedError);
 		}));
 
 		return {
