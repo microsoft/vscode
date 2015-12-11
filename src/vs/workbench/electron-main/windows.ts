18c4a65cb26517c25ce0f487feb6d169145bc647
Sofian Hnaide <sofianal@microsoft.com>
2015-12-11 12:57:00 -0800

    Revert "remove send-a-simle"
    This reverts commit 29d13705bcd44573357a12e57f5972bb76baac91.

--- src/vs/workbench/electron-main/windows.ts
+++ src/vs/workbench/electron-main/windows.ts
@@ -554,6 +554,7 @@ export class WindowsManager {
 		configuration.updateChannel = UpdateManager.channel;
 		configuration.recentPaths = this.getRecentlyOpenedPaths(workspacePath, filesToOpen);
 		configuration.aiConfig = env.product.aiConfig;
+		configuration.sendASmile = env.product.sendASmile;
 		configuration.enableTelemetry = env.product.enableTelemetry;
 		configuration.userEnv = userEnv;
 
