3b4f6aa38aff95c4da8d5d01e02530b45670babc
Joao Moreno <jomo@microsoft.com>
2015-12-15 14:28:30 +0100

    remove extension node modules

--- build/gulpfile.vscode.js
+++ build/gulpfile.vscode.js
@@ -166,6 +166,7 @@ function packageTask(platform, arch, opts) {
 			'extensions/**',
 			'!extensions/*/src/**',
 			'!extensions/*/out/**/test/**',
+			'!extensions/*/node_modules/**', // remove this asap, need to ignore dev dependencies
 			'!extensions/typescript/bin/**'
 		], { base: '.' });
 
