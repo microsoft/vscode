4b8db4f6bc7c0e3bfc156ca64c10a2df65c975f4
Martin Aeschlimann <martinae@microsoft.com>
2015-12-15 17:22:20 +0100

    Duplicate module definitions upon opening LESS or SASS files #1282

---
+++ src/vs/languages/sass/common/sassTokenTypes.ts
@@ -0,0 +1,13 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+'use strict';
+
+/* always keep in sync with cssTokenTypes */
+
+export const TOKEN_SELECTOR = 'entity.name.selector';
+export const TOKEN_SELECTOR_TAG = 'entity.name.tag';
+export const TOKEN_PROPERTY =  'support.type.property-name';
+export const TOKEN_VALUE =  'support.property-value';
+export const TOKEN_AT_KEYWORD = 'keyword.control.at-rule';
