6420d956d4de48c97de7ac6b3f4f0721a60c4dd6
Benjamin Pasero <benjpas@microsoft.com>
2016-01-04 09:47:53 +0100

    better hint for new auto save setting

--- src/vs/workbench/electron-browser/integration.ts
+++ src/vs/workbench/electron-browser/integration.ts
@@ -130,7 +130,7 @@ export class ElectronIntegration {
 		ipc.on('vscode:showAutoSaveInfo', () => {
 			this.messageService.show(
 				Severity.Info, {
-					message: nls.localize('autoSaveInfo', "The **File | Auto Save** option moved into settings. Please configure **files.autoSaveDelay** to enable auto save again."),
+					message: nls.localize('autoSaveInfo', "The **File | Auto Save** option moved into settings. Please configure **files.autoSaveDelay: 1** to restore the old behavior."),
 					actions: [
 						CloseAction,
 						this.instantiationService.createInstance(OpenGlobalSettingsAction, OpenGlobalSettingsAction.ID, OpenGlobalSettingsAction.LABEL)
