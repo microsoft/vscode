2bc11998d879bcf291ea8041ca734ec6173d28ff
isidor <inikolic@microsoft.com>
2016-01-12 17:42:14 +0100

    debug: improve simple wording

--- src/vs/workbench/parts/debug/node/debugAdapter.ts
+++ src/vs/workbench/parts/debug/node/debugAdapter.ts
@@ -114,7 +114,7 @@ export class Adapter {
 	private warnRelativePaths(attribute: any): void {
 		if (attribute) {
 			attribute.pattern = '^\\${.*}.*|^((\\/|[a-zA-Z]:\\\\)[^\\(\\)<>\\\'\\"\\[\\]]+)';
-			attribute.errorMessage = nls.localize('relativePathsNotConverted', "Relative paths will no longer be converted to absolute ones. Consider using ${workspaceRoot} as a prefix.");
+			attribute.errorMessage = nls.localize('relativePathsNotConverted', "Relative paths will no longer be automatically converted to absolute ones. Consider using ${workspaceRoot} as a prefix.");
 		}
 	}
 }
