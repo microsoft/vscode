9eebf6657f6e62f2118a1fcec3c1fd2edc22c9fd
Johannes Rieken <johannes.rieken@gmail.com>
2016-01-13 15:06:25 +0100

    move up octicon logic so that it can be reused in quick open

---
+++ src/vs/base/browser/ui/octiconLabel/octiconLabel.ts
@@ -0,0 +1,31 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+'use strict';
+
+
+import 'vs/css!./octicons/octicons';
+import {escape} from 'vs/base/common/strings';
+
+export function expand(text: string): string {
+	return text.replace(/\$\((\w+)\)/g, (match, g1) => {
+		return `<span class="octicon octicon-${g1}"></span>`;
+	});
+}
+
+export class OcticonLabel {
+
+	private _container: HTMLElement;
+
+	constructor(container: HTMLElement) {
+		this._container = container;
+	}
+
+	set text(text: string) {
+		let innerHTML = text || '';
+		innerHTML = escape(innerHTML);
+		innerHTML = expand(innerHTML)
+		this._container.innerHTML = innerHTML;
+	}
+}
