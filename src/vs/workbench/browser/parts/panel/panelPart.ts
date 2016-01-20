572d02d2bf1b02c688fbe14e4e1e61da41425186
isidor <inikolic@microsoft.com>
2016-01-20 11:16:29 +0100

    panel: toggle panel is cmd + J

--- src/vs/workbench/browser/parts/panel/panelPart.ts
+++ src/vs/workbench/browser/parts/panel/panelPart.ts
@@ -151,4 +151,4 @@ export class TogglePanelAction extends Action {
 }
 
 let actionRegistry = <IWorkbenchActionRegistry>Registry.as(WorkbenchExtensions.WorkbenchActions);
-actionRegistry.registerWorkbenchAction(new SyncActionDescriptor(TogglePanelAction, TogglePanelAction.ID, TogglePanelAction.LABEL, { primary: KeyMod.CtrlCmd | KeyCode.KEY_L }), nls.localize('view', "View"));
+actionRegistry.registerWorkbenchAction(new SyncActionDescriptor(TogglePanelAction, TogglePanelAction.ID, TogglePanelAction.LABEL, { primary: KeyMod.CtrlCmd | KeyCode.KEY_J }), nls.localize('view', "View"));
