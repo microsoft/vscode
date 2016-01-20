8a850be7fc1b59919e28935481b64cc55c7805a3
xzper <mail@xzper.com>
2016-01-20 13:27:52 +0800

    replace powershell with findstr

--- scripts/npm.bat
+++ scripts/npm.bat
@@ -2,7 +2,7 @@
 setlocal
 
 set npm_config_disturl="https://atom.io/download/atom-shell"
-for /f "delims=" %%A in ('powershell -Command "(Get-Content -Raw %~dp0..\package.json | ConvertFrom-Json).electronVersion"') do set "npm_config_target=%%A"
+for /f "tokens=2 delims=:, " %%a in ('findstr /R /C:"\"electronVersion\":.*" %~dp0..\package.json') do set npm_config_target=%%~a
 set npm_config_arch="ia32"
 set HOME=~\.electron-gyp
 
