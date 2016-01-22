3f74bd8369fbbffdd572eca87b1b5cadd3f85a66
Wade Anderson <waander@Wades-MacBook-Pro.local>
2016-01-22 14:10:22 -0800

    Changed the words prompting feedback.

--- src/vs/workbench/parts/feedback/browser/feedback.ts
+++ src/vs/workbench/parts/feedback/browser/feedback.ts
@@ -104,7 +104,7 @@ export class FeedbackDropdown extends Dropdown {
 
 		this.feedbackForm = <HTMLFormElement>$form.getHTMLElement();
 
-		$('h2.title').text(nls.localize("label.sendASmile", "Let us know how we're doing")).appendTo($form);
+		$('h2.title').text(nls.localize("label.sendASmile", "Tweet us your feedback")).appendTo($form);
 
 		this.invoke($('div.cancel').attr('tabindex', '0'), () => {
 			this.hide();
