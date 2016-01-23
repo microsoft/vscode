5ce70e051ff2c3b511b49672ac1cfc38c46a0c6f
Martin Aeschlimann <martinae@microsoft.com>
2016-01-23 21:13:03 +0100

    Add WorkbenchContentProvider

--- src/vs/workbench/electron-browser/shell.ts
+++ src/vs/workbench/electron-browser/shell.ts
@@ -94,6 +94,7 @@ import {IModeService} from 'vs/editor/common/services/modeService';
 import {IUntitledEditorService, UntitledEditorService} from 'vs/workbench/services/untitled/common/untitledEditorService';
 import {CrashReporter} from 'vs/workbench/electron-browser/crashReporter';
 import {IThemeService, ThemeService} from 'vs/workbench/services/themes/node/themeService';
+import {WorkbenchContentProvider} from 'vs/workbench/services/contentprovider/common/workbenchContentProvider';
 import { IServiceCtor, isServiceEvent } from 'vs/base/common/service';
 import { connect, Client } from 'vs/base/node/service.net';
 import { IExtensionsService } from 'vs/workbench/parts/extensions/common/extensions';
@@ -217,6 +218,9 @@ export class WorkbenchShell {
 				this.onWorkbenchStarted();
 			}
 		});
+
+		// content service
+		this.workbench.getInstantiationService().createInstance(WorkbenchContentProvider).start();
 
 		// Electron integration
 		this.workbench.getInstantiationService().createInstance(ElectronIntegration).integrate(this.container);
