241d480c933128225441a1b26607e77d4ceaaf47
isidor <inikolic@microsoft.com>
2016-01-27 17:06:19 +0100

    fixes #2462

--- src/vs/workbench/parts/debug/browser/debugHover.ts
+++ src/vs/workbench/parts/debug/browser/debugHover.ts
@@ -151,6 +151,7 @@ export class DebugHoverWidget implements editorbrowser.IContentWidget {
 			this.treeContainer.hidden = true;
 			this.valueContainer.hidden = false;
 			viewer.renderExpressionValue(expression, false, this.valueContainer, false);
+			this.valueContainer.title = '';
 		}
 
 		this.showAtPosition = position;
