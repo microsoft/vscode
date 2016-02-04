58f156a0ea4d5bd55eaff006e8db18eb237b9513
Johannes Rieken <johannes.rieken@gmail.com>
2016-02-04 11:20:16 +0100

    add extensions.enableSuggestions flag

--- src/vs/workbench/parts/extensions/electron-browser/extensionTipsService.ts
+++ src/vs/workbench/parts/extensions/electron-browser/extensionTipsService.ts
@@ -4,12 +4,13 @@
  *--------------------------------------------------------------------------------------------*/
 
 import URI from 'vs/base/common/uri';
+import {onUnexpectedError} from 'vs/base/common/errors';
 import {values, forEach} from 'vs/base/common/collections';
-import {isEmptyObject} from 'vs/base/common/types';
 import {IDisposable, disposeAll} from 'vs/base/common/lifecycle';
 import {TPromise as Promise} from 'vs/base/common/winjs.base';
 import {match} from 'vs/base/common/glob';
 import Event, {Emitter} from 'vs/base/common/event';
+import {IConfigurationService} from 'vs/platform/configuration/common/configuration';
 import {IExtensionsService, IGalleryService, IExtensionTipsService, IExtension} from 'vs/workbench/parts/extensions/common/extensions';
 import {IModelService} from 'vs/editor/common/services/modelService';
 import {EventType} from 'vs/editor/common/editorCommon';
@@ -70,15 +71,20 @@ export class ExtensionTipsService implements IExtensionTipsService {
 	private _onDidChangeTips: Emitter<void> = new Emitter<void>();
 	private _tips: { [id: string]: ExtensionTip } = Object.create(null);
 	private _toDispose: IDisposable[] = [];
+	private _availableExtensions: Promise<ExtensionMap>;
 
 	constructor(
 		@IExtensionsService private _extensionService: IExtensionsService,
 		@IGalleryService private _galleryService: IGalleryService,
-		@IModelService private _modelService: IModelService
+		@IModelService private _modelService: IModelService,
+		@IConfigurationService configurationService: IConfigurationService
 	) {
-		if (this._galleryService.isEnabled()) {
-			this._init();
-		}
+
+		configurationService.loadConfiguration('extensions').then(value => {
+			if (value && value.enableSuggestions) {
+				this._init();
+			}
+		}, onUnexpectedError);
 	}
 
 	dispose() {
@@ -97,7 +103,52 @@ export class ExtensionTipsService implements IExtensionTipsService {
 
 	private _init() {
 
-		this._galleryService.query().then(extensions => {
+		if (!this._galleryService.isEnabled()) {
+			return;
+		}
+
+		this._availableExtensions = this._getAvailableExtensions();
+
+		// don't suggest what got installed
+		this._toDispose.push(this._extensionService.onDidInstallExtension(ext => {
+			const id = `${ext.publisher}.${ext.name}`;
+			if (delete this._tips[id]) {
+				this._onDidChangeTips.fire(void 0);
+			}
+			this._availableExtensions = this._getAvailableExtensions();
+		}));
+
+
+		// we listen for editor models being added and changed
+		// when a model is added it gives 2 points, a change gives 3 points
+		// such that files you type have bigger impact on the suggest
+		// order than those you only look at
+
+		const modelListener: { [uri: string]: IDisposable } = Object.create(null);
+		this._toDispose.push({ dispose() { disposeAll(values(modelListener)) } });
+
+		this._toDispose.push(this._modelService.onModelAdded(model => {
+			const uri = model.getAssociatedResource();
+			this._suggestByResource(uri, ExtensionTipReasons.FileOpened);
+			modelListener[uri.toString()] = model.addListener2(EventType.ModelContentChanged2,
+				() => this._suggestByResource(uri, ExtensionTipReasons.FileEdited));
+		}));
+
+		this._toDispose.push(this._modelService.onModelRemoved(model => {
+			const subscription = modelListener[model.getAssociatedResource().toString()];
+			if (subscription) {
+				subscription.dispose();
+				delete modelListener[model.getAssociatedResource().toString()];
+			}
+		}));
+
+		for (let model of this._modelService.getModels()) {
+			this._suggestByResource(model.getAssociatedResource(), ExtensionTipReasons.FileOpened);
+		}
+	}
+
+	private _getAvailableExtensions(): Promise<ExtensionMap> {
+		return this._galleryService.query().then(extensions => {
 			let map: ExtensionMap = Object.create(null);
 			for (let ext of extensions) {
 				map[`${ext.publisher}.${ext.name}`] = ext;
@@ -109,62 +160,38 @@ export class ExtensionTipsService implements IExtensionTipsService {
 				}
 				return map;
 			});
-		}).then(extensions => {
-
-			// we listen for editor models being added and changed
-			// when a model is added it gives 2 points, a change gives 3 points
-			// such that files you type have bigger impact on the suggest
-			// order than those you only look at
-
-			const modelListener: { [uri: string]: IDisposable } = Object.create(null);
-			this._toDispose.push({ dispose() { disposeAll(values(modelListener)) } });
-
-			this._toDispose.push(this._modelService.onModelAdded(model => {
-				const uri = model.getAssociatedResource();
-				this._suggestByResource(extensions, uri, ExtensionTipReasons.FileOpened);
-				modelListener[uri.toString()] = model.addListener2(EventType.ModelContentChanged2,
-					() => this._suggestByResource(extensions, uri, ExtensionTipReasons.FileEdited));
-			}));
-
-			this._toDispose.push(this._modelService.onModelRemoved(model => {
-				const subscription = modelListener[model.getAssociatedResource().toString()];
-				if (subscription) {
-					subscription.dispose();
-					delete modelListener[model.getAssociatedResource().toString()];
-				}
-			}));
-
-			for (let model of this._modelService.getModels()) {
-				this._suggestByResource(extensions, model.getAssociatedResource(), ExtensionTipReasons.FileOpened);
-			}
+		}, () => {
+			return Object.create(null);
 		});
 	}
 
 	// --- suggest logic
 
-	private _suggestByResource(extensions: ExtensionMap, uri: URI, reason: ExtensionTipReasons): Promise<any> {
+	private _suggestByResource(uri: URI, reason: ExtensionTipReasons): Promise<any> {
 
 		if (!uri) {
 			return;
 		}
 
-		let change = false;
-		forEach(ExtensionTipsService._extensionByPattern, entry => {
-			let extension = extensions[entry.key];
-			if (extension && match(entry.value, uri.fsPath)) {
-				let value = this._tips[entry.key];
-				if (!value) {
-					value = this._tips[entry.key] = new ExtensionTip(extension);
+		this._availableExtensions.then(extensions => {
+			let change = false;
+			forEach(ExtensionTipsService._extensionByPattern, entry => {
+				let extension = extensions[entry.key];
+				if (extension && match(entry.value, uri.fsPath)) {
+					let value = this._tips[entry.key];
+					if (!value) {
+						value = this._tips[entry.key] = new ExtensionTip(extension);
+					}
+					if (value.resource(uri, reason)) {
+						change = true;
+					}
 				}
-				if (value.resource(uri, reason)) {
-					change = true;
-				}
-			}
-		});
+			});
 
-		if (change) {
-			this._onDidChangeTips.fire(undefined);
-		}
+			if (change) {
+				this._onDidChangeTips.fire(undefined);
+			}
+		}, onUnexpectedError);
 	}
 
 	private static _extensionByPattern: { [pattern: string]: string } = {
