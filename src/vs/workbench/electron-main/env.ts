655def280e450fc80c5875a6a9e60427820220cc
Daniel Imms <daimms@microsoft.com>
2016-02-22 07:48:53 -0800

    Remove trailing dot removal all together

--- src/vs/workbench/electron-main/env.ts
+++ src/vs/workbench/electron-main/env.ts
@@ -316,11 +316,6 @@ function massagePath(path: string): string {
 	// Trim whitespaces
 	path = strings.trim(strings.trim(path, ' '), '\t');
 
-	// Remove trailing dots
-	if (platform.isWindows) {
-		path = strings.rtrim(path, '.');
-	}
-
 	return path;
 }
 
