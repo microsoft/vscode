1ae17c0e949b4c85ad65ca6121b05c78d43221e3
isidor <inikolic@microsoft.com>
2016-02-23 00:03:41 +0100

    indentation: IndentUsingTabs and IndentUsingSpaces actions

--- src/vs/editor/contrib/indentation/common/indentation.ts
+++ src/vs/editor/contrib/indentation/common/indentation.ts
@@ -10,6 +10,7 @@ import {EditorAction} from 'vs/editor/common/editorAction';
 import {ICommonCodeEditor, IEditorActionDescriptorData} from 'vs/editor/common/editorCommon';
 import {CommonEditorRegistry, EditorActionDescriptor} from 'vs/editor/common/editorCommonExtensions';
 import {IndentationToSpacesCommand, IndentationToTabsCommand} from 'vs/editor/contrib/indentation/common/indentationCommands';
+import {IQuickOpenService} from 'vs/workbench/services/quickopen/common/quickOpenService';
 
 export class IndentationToSpacesAction extends EditorAction {
 	static ID = 'editor.action.indentationToSpaces';
@@ -22,7 +23,7 @@ export class IndentationToSpacesAction extends EditorAction {
 
 		const command = new IndentationToSpacesCommand(this.editor.getSelection(), this.editor.getIndentationOptions().tabSize);
 		this.editor.executeCommands(this.id, [command]);
-		
+
 		return TPromise.as(true);
 	}
 }
@@ -43,6 +44,43 @@ export class IndentationToTabsAction extends EditorAction {
 	}
 }
 
+abstract class Indent extends EditorAction {
+
+	constructor(descriptor: IEditorActionDescriptorData, editor: ICommonCodeEditor, private insertSpaces: boolean, private quickOpenService: IQuickOpenService) {
+		super(descriptor, editor);
+	}
+
+	public run(): TPromise<boolean> {
+		return TPromise.timeout(50 /* quick open is sensitive to being opened so soon after another */).then(() =>
+			this.quickOpenService.pick(['1', '2', '3', '4', '5', '6', '7', '8'], { placeHolder: nls.localize('selectTabWidth', "Select Tab Width")}).then(pick => {
+				this.editor.setIndentationOptions({
+					insertSpaces: this.insertSpaces,
+					tabSize: parseInt(pick)
+				});
+				return true;
+			})
+		);
+	}
+}
+
+export class IndentUsingSpaces extends Indent {
+	static ID = 'editor.action.indentUsingSpaces';
+
+	constructor(descriptor: IEditorActionDescriptorData, editor: ICommonCodeEditor, @IQuickOpenService quickOpenService: IQuickOpenService) {
+		super(descriptor, editor, true, quickOpenService);
+	}
+}
+
+export class IndentUsingTabs extends Indent {
+	static ID = 'editor.action.indentUsingTabs';
+
+	constructor(descriptor: IEditorActionDescriptorData, editor: ICommonCodeEditor, @IQuickOpenService quickOpenService: IQuickOpenService) {
+		super(descriptor, editor, false, quickOpenService);
+	}
+}
+
 // register actions
 CommonEditorRegistry.registerEditorAction(new EditorActionDescriptor(IndentationToSpacesAction, IndentationToSpacesAction.ID, nls.localize('indentationToSpaces', "Convert Indentation to Spaces")));
 CommonEditorRegistry.registerEditorAction(new EditorActionDescriptor(IndentationToTabsAction, IndentationToTabsAction.ID, nls.localize('indentationToTabs', "Convert Indentation to Tabs")));
+CommonEditorRegistry.registerEditorAction(new EditorActionDescriptor(IndentUsingSpaces, IndentUsingSpaces.ID, nls.localize('indentUsingSpaces', "Indent Using Spaces")));
+CommonEditorRegistry.registerEditorAction(new EditorActionDescriptor(IndentUsingTabs, IndentUsingTabs.ID, nls.localize('indentUsingTabs', "Indent Using Tabs")));
