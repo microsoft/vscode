d4083f3dace51197a1735e74439d4c7f150e8d2f
Joao Moreno <jomo@microsoft.com>
2016-03-03 11:00:56 +0100

    use ELECTRON_NO_ATTACH_CONSOLE

--- scripts/test.bat
+++ scripts/test.bat
@@ -1,9 +1,10 @@
 setlocal
 
 set ATOM_SHELL_INTERNAL_RUN_AS_NODE=1
+set ELECTRON_NO_ATTACH_CONSOLE=1
 
 pushd %~dp0\..
-start "" .\.build\electron\electron.exe .\node_modules\mocha\bin\_mocha %*
+.\.build\electron\electron.exe .\node_modules\mocha\bin\_mocha %*
 popd
 
 endlocal
