8c34045f0f9a1c41a52452c4f2e9b08e0c0b7027
Joao Moreno <jomo@microsoft.com>
2016-03-03 10:50:52 +0100

    use start

--- scripts/test.bat
+++ scripts/test.bat
@@ -3,7 +3,7 @@ setlocal
 set ATOM_SHELL_INTERNAL_RUN_AS_NODE=1
 
 pushd %~dp0\..
-.\.build\electron\electron.exe .\node_modules\mocha\bin\_mocha %*
+start "" .\.build\electron\electron.exe .\node_modules\mocha\bin\_mocha %*
 popd
 
 endlocal
