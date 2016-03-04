b07b57cc0e46822bb9c55f1374ccfcf1087727b7
Joao Moreno <jomo@microsoft.com>
2016-03-04 09:27:27 +0100

    gulpfile.extensions refactor

--- extensions/json/client/src/typings/ref.d.ts
+++ extensions/json/client/src/typings/ref.d.ts
@@ -3,8 +3,8 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-/// <reference path='../../../../src/vs/vscode.d.ts'/>
-/// <reference path='../../../../src/typings/mocha.d.ts'/>
-/// <reference path='../../../../extensions/node.d.ts'/>
-/// <reference path='../../../../extensions/lib.core.d.ts'/>
-/// <reference path='../../node_modules/vscode-languageclient/lib/main.d.ts'/>
+/// <reference path='../../../../../src/vs/vscode.d.ts'/>
+/// <reference path='../../../../../src/typings/mocha.d.ts'/>
+/// <reference path='../../../../../extensions/node.d.ts'/>
+/// <reference path='../../../../../extensions/lib.core.d.ts'/>
+/// <reference path='../../../node_modules/vscode-languageclient/lib/main.d.ts'/>
