46657ddea7be5f8a6f7e6cdec868d191f19726b4
Martin Aeschlimann <martinae@microsoft.com>
2016-03-04 15:22:32 +0100

    [json] fix sourcemaps

---
+++ extensions/json/server/src/typings/ref.d.ts
@@ -0,0 +1,3 @@
+/// <reference path='../../../../../src/typings/mocha.d.ts'/>
+/// <reference path='../../../../../extensions/node.d.ts'/>
+/// <reference path='../../../../../extensions/lib.core.d.ts'/>
