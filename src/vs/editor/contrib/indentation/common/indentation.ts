2992a0daf9f801485da25b0ef18ac5b4487c5213
isidor <inikolic@microsoft.com>
2016-03-08 12:03:09 +0100

    indentation: indentUsingTabs, indentUsingSpaces actions

--- src/vs/editor/contrib/indentation/common/indentation.ts
+++ src/vs/editor/contrib/indentation/common/indentation.ts
@@ -52,11 +52,10 @@ export class IndentationToTabsAction extends EditorAction {
 
 export class ChangeIndentationSizeAction extends EditorAction {
 
-	static ID = 'editor.action.changeIndentationSize';
-
 	constructor(descriptor: IEditorActionDescriptorData, editor: ICommonCodeEditor,
-		@IQuickOpenService private quickOpenService: IQuickOpenService,
-		@IConfigurationService private configurationService: IConfigurationService
+		private insertSpaces: boolean,
+		private quickOpenService: IQuickOpenService,
+		private configurationService: IConfigurationService
 	) {
 		super(descriptor, editor);
 	}
@@ -67,15 +66,16 @@ export class ChangeIndentationSizeAction extends EditorAction {
 			const picks = [1, 2, 3, 4, 5, 6, 7, 8].map(n => ({
 				id: n.toString(),
 				label: n.toString(),
-				description: n === config.tabSize ? nls.localize('configuredTabSize', "Configured Tab Size") : null
+				description: n === config.tabSize ? nls.localize('configuredIndentationSize', "Configured Indentation Size") : null
 			}));
 			const autoFocusIndex = Math.min(this.editor.getIndentationOptions().tabSize - 1, 7);
 
 			return TPromise.timeout(50 /* quick open is sensitive to being opened so soon after another */).then(() =>
-				this.quickOpenService.pick(picks, { placeHolder: nls.localize('selectTabWidth', "Select Tab Size for Current File"), autoFocus: { autoFocusIndex } }).then(pick => {
+				this.quickOpenService.pick(picks, { placeHolder: nls.localize('selectIndentationSize', "Select Indentation Size for Current File"), autoFocus: { autoFocusIndex } }).then(pick => {
 					if (pick) {
 						this.editor.updateOptions({
-							tabSize: parseInt(pick.label)
+							tabSize: parseInt(pick.label),
+							insertSpaces: this.insertSpaces
 						});
 					}
 
@@ -83,6 +83,30 @@ export class ChangeIndentationSizeAction extends EditorAction {
 				})
 			);
 		});
+	}
+}
+
+export class IndentUsingTabs extends ChangeIndentationSizeAction {
+
+	static ID = 'editor.action.indentUsingTabs';
+
+	constructor(descriptor: IEditorActionDescriptorData, editor: ICommonCodeEditor,
+		@IQuickOpenService quickOpenService: IQuickOpenService,
+		@IConfigurationService configurationService: IConfigurationService
+	) {
+		super(descriptor, editor, false, quickOpenService, configurationService);
+	}
+}
+
+export class IndentUsingSpaces extends ChangeIndentationSizeAction {
+
+	static ID = 'editor.action.indentUsingSpaces';
+
+	constructor(descriptor: IEditorActionDescriptorData, editor: ICommonCodeEditor,
+		@IQuickOpenService quickOpenService: IQuickOpenService,
+		@IConfigurationService configurationService: IConfigurationService
+	) {
+		super(descriptor, editor, true, quickOpenService, configurationService);
 	}
 }
 
@@ -105,5 +129,6 @@ export class ToggleRenderWhitespaceAction extends EditorAction {
 // register actions
 CommonEditorRegistry.registerEditorAction(new EditorActionDescriptor(IndentationToSpacesAction, IndentationToSpacesAction.ID, nls.localize('indentationToSpaces', "Convert Indentation to Spaces")));
 CommonEditorRegistry.registerEditorAction(new EditorActionDescriptor(IndentationToTabsAction, IndentationToTabsAction.ID, nls.localize('indentationToTabs', "Convert Indentation to Tabs")));
-CommonEditorRegistry.registerEditorAction(new EditorActionDescriptor(ChangeIndentationSizeAction, ChangeIndentationSizeAction.ID, nls.localize('changeIndentationSize', "Change Tab Size for Current File")));
+CommonEditorRegistry.registerEditorAction(new EditorActionDescriptor(IndentUsingSpaces, IndentUsingSpaces.ID, nls.localize('indentUsingSpaces', "Indent Using Spaces")));
+CommonEditorRegistry.registerEditorAction(new EditorActionDescriptor(IndentUsingTabs, IndentUsingTabs.ID, nls.localize('indentUsingTabs', "Indent Using Tabs")));
 CommonEditorRegistry.registerEditorAction(new EditorActionDescriptor(ToggleRenderWhitespaceAction, ToggleRenderWhitespaceAction.ID, nls.localize('toggleRenderWhitespace', "Toggle Render Whitespace")));
