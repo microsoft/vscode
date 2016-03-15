efab5dd9d0baf3f9fa126cdaf9d0ae9da018e64a
Erich Gamma <egamma@microsoft.com>
2016-03-15 13:06:02 +0100

    Delete javascript migration helper #4149

--- extensions/javascript-migration/src/typings/ref.d.ts
+++
@@ -1,7 +0,0 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
-
-/// <reference path='../../../../extensions/lib.core.d.ts'/>
-/// <reference path='../../../../extensions/declares.d.ts'/>
