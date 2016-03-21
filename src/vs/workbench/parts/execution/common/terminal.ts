aa17b93740fe79921c7b2b5cc2f689148e237e7e
pflannery <flannery.peter@ntlworld.com>
2016-03-21 16:33:01 +0000

    adds debian check for default terminal

--- src/vs/workbench/parts/execution/common/terminal.ts
+++ src/vs/workbench/parts/execution/common/terminal.ts
@@ -2,7 +2,17 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-export const defaultLinuxTerm = process.env.DESKTOP_SESSION === 'gnome' ?
-	'gnome-terminal' : 'x-terminal-emulator';
+import fs = require('fs');
+import env = require('vs/base/common/platform');
+
+export let defaultLinuxTerm = 'x-terminal-emulator';
+
+// if we're not on debian and using gnome then
+// set default to gnome-terminal
+if (env.isLinux
+	&& fs.existsSync('/etc/debian_version') === false
+	&& process.env.DESKTOP_SESSION === 'gnome') {
+	defaultLinuxTerm = 'gnome-terminal';
+}
 
 export const defaultWindowsTerm = 'cmd';
