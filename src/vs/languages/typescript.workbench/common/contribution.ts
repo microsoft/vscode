60254f1f377a2917ace0357b6bbc93a9aaf1452a
Johannes Rieken <johannes.rieken@gmail.com>
2016-03-24 09:34:04 +0100

    remove TS/JS specific flags, fixes #3916

--- src/vs/languages/typescript.workbench/common/contribution.ts
+++ src/vs/languages/typescript.workbench/common/contribution.ts
@@ -4,8 +4,6 @@
  *--------------------------------------------------------------------------------------------*/
 'use strict';
 
-import env = require('vs/base/common/flags');
-import {ModesRegistry} from 'vs/editor/common/modes/modesRegistry';
 import javascript = require('vs/languages/javascript/common/javascript.extensions');
 import typescript = require('vs/languages/typescript/common/typescript');
 import {AsyncDescriptor} from 'vs/platform/instantiation/common/descriptors';
@@ -13,14 +11,14 @@ import {AsyncDescriptor} from 'vs/platform/instantiation/common/descriptors';
 // contributes the project resolver logic to TypeScript and JavaScript
 // this guy is for the workbench, but not for the standalone editor
 
-if (env.enableJavaScriptRewriting && !env.enableTypeScriptServiceModeForJS) {
-	ModesRegistry.registerWorkerParticipant('javascript', 'vs/languages/typescript/common/js/globalVariableRewriter', 'GlobalVariableCollector');
-	ModesRegistry.registerWorkerParticipant('javascript', 'vs/languages/typescript/common/js/angularServiceRewriter', 'AngularServiceRewriter');
-	ModesRegistry.registerWorkerParticipant('javascript', 'vs/languages/typescript/common/js/requireRewriter');
-	ModesRegistry.registerWorkerParticipant('javascript', 'vs/languages/typescript/common/js/defineRewriter');
-	ModesRegistry.registerWorkerParticipant('javascript', 'vs/languages/typescript/common/js/es6PropertyDeclarator');
-	ModesRegistry.registerWorkerParticipant('javascript', 'vs/languages/typescript/common/js/importAndExportRewriter', 'ImportsAndExportsCollector');
-}
+// if () {
+// 	ModesRegistry.registerWorkerParticipant('javascript', 'vs/languages/typescript/common/js/globalVariableRewriter', 'GlobalVariableCollector');
+// 	ModesRegistry.registerWorkerParticipant('javascript', 'vs/languages/typescript/common/js/angularServiceRewriter', 'AngularServiceRewriter');
+// 	ModesRegistry.registerWorkerParticipant('javascript', 'vs/languages/typescript/common/js/requireRewriter');
+// 	ModesRegistry.registerWorkerParticipant('javascript', 'vs/languages/typescript/common/js/defineRewriter');
+// 	ModesRegistry.registerWorkerParticipant('javascript', 'vs/languages/typescript/common/js/es6PropertyDeclarator');
+// 	ModesRegistry.registerWorkerParticipant('javascript', 'vs/languages/typescript/common/js/importAndExportRewriter', 'ImportsAndExportsCollector');
+// }
 
 typescript.Extensions.setProjectResolver(new AsyncDescriptor<typescript.IProjectResolver2>(
 	'vs/languages/typescript.workbench/common/projectResolver', undefined, { files: '**/*.ts', projects: '**/tsconfig.json', maxFilesPerProject: 1500 }));
