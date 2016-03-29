a80db720b13ed9fb2db94d695fee2fc7f21f6141
Johannes Rieken <johannes.rieken@gmail.com>
2016-03-29 18:28:08 +0200

    cleaner worker management

--- src/vs/languages/typescript/common/worker/workerManager.ts
+++ src/vs/languages/typescript/common/worker/workerManager.ts
@@ -5,6 +5,7 @@
 'use strict';
 
 import {TPromise} from 'vs/base/common/winjs.base';
+import {IDisposable, disposeAll} from 'vs/base/common/lifecycle';
 import {DefaultWorkerFactory} from 'vs/base/worker/defaultWorkerFactory';
 import {SimpleWorkerClient} from 'vs/base/common/worker/simpleWorker';
 import {IModelService} from 'vs/editor/common/services/modelService';
@@ -12,66 +13,82 @@ import {Defaults} from '../typescript';
 import registerLanguageFeatures from '../languageFeatures';
 import AbstractWorker from './worker';
 
-export function create(selector: string, modelService: IModelService) {
 
-	const factory = new DefaultWorkerFactory();
-	let client: SimpleWorkerClient<AbstractWorker>;
-	let handle: number;
+class Client {
 
-	const worker = () => {
+	private _client: TPromise<AbstractWorker> = null;
+	private _clientDispose: IDisposable[];
+	private _factory = new DefaultWorkerFactory();
+	private _modelService: IModelService;
 
-		if (!client) {
-			client = new SimpleWorkerClient<AbstractWorker>(
-				factory,
-				'vs/languages/typescript/common/worker/workerImpl',
-				AbstractWorker);
+	constructor(modelService: IModelService) {
+		this._modelService = modelService;
+	}
 
-			handle = setInterval(() => {
-				if (Date.now() - client.getLastRequestTimestamp() > 1000 * 60 * 5) {
-					dispose();
-				}
-			}, 1000 * 60);
+	private _createClient(): TPromise<AbstractWorker> {
+		let client = new SimpleWorkerClient<AbstractWorker>(
+			this._factory,
+			'vs/languages/typescript/common/worker/workerImpl',
+			AbstractWorker);
 
-			client.get().acceptCompilerOptions(Defaults.getCompilerOptions());
-			let subscription = Defaults.onDidChangeCompilerOptions(options => client.get().acceptCompilerOptions(options));
+		this._clientDispose = [];
 
-			function dispose() {
-				subscription.dispose();
-				clearTimeout(handle);
-				client.dispose();
-				client = undefined;
+		const handle = setInterval(() => {
+			if (Date.now() - client.getLastRequestTimestamp() > 1000 * 60) {
+				this._clientDispose = disposeAll(this._clientDispose);
+				this._client = null;
+				client = null;
 			}
+		}, 1000 * 60);
+
+		this._clientDispose.push({ dispose() { clearInterval(handle); } });
+		this._clientDispose.push(client);
+
+		const worker = client.get();
+		this._clientDispose.push(Defaults.onDidChangeCompilerOptions(options => worker.acceptCompilerOptions(options)));
+
+		return worker.acceptCompilerOptions(Defaults.getCompilerOptions()).then(() => worker);
+	}
+
+	dispose(): void {
+		this._clientDispose = disposeAll(this._clientDispose);
+	}
+
+	get(): TPromise<AbstractWorker> {
+		if (!this._client) {
+			this._client = this._createClient();
 		}
 
-		let result = client.get();
-		// return TPromise.as(result);
+		// TODO@joh use proper model sync
+		return this._client.then(worker => {
+			let promises = this._modelService.getModels().map(model => {
+				return TPromise.as(worker.acceptNewModel({
+					url: model.getAssociatedResource().toString(),
+					versionId: model.getVersionId(),
+					value: {
+						EOL: model.getEOL(),
+						lines: model.getLinesContent(),
+						length: model.getValueLength(),
+						BOM: undefined,
+						options: undefined
+					}
+				}));
+			});
 
-		let promises = modelService.getModels().map(model => {
-			return TPromise.as(result.acceptNewModel({
-				url: model.getAssociatedResource().toString(),
-				versionId: model.getVersionId(),
-				value: {
-					EOL: model.getEOL(),
-					lines: model.getLinesContent(),
-					length: model.getValueLength(),
-					BOM: undefined,
-					options: undefined
-				}
-			}));
+			return TPromise.join(promises).then(() => worker);
 		});
+	}
+}
 
-		return TPromise.join(promises).then(() => result);
-	};
+export function create(selector: string, modelService: IModelService) {
 
-	// --- register features
-	registerLanguageFeatures(selector, modelService, worker);
+	const client = new Client(modelService);
+	const registration = registerLanguageFeatures(selector, modelService, () => client.get());
 
 	return {
 		dispose() {
-			clearTimeout(handle);
-			if (client) {
-				client.dispose();
-			}
+			client.dispose();
+			registration.dispose();
 		}
 	};
 }
