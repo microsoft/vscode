15cd6662d02bd5f154e02480b9d927c76870a930
Erich Gamma <egamma@microsoft.com>
2016-04-03 20:08:42 +0200

    Added setting `emmet.triggerExpansionOnTab`

--- src/vs/workbench/parts/emmet/node/emmet.contribution.ts
+++ src/vs/workbench/parts/emmet/node/emmet.contribution.ts
@@ -5,7 +5,11 @@
 'use strict';
 
 import nls = require('vs/nls');
+
+import {Registry} from 'vs/platform/platform';
 import {CommonEditorRegistry, EditorActionDescriptor} from 'vs/editor/common/editorCommonExtensions';
+import {IConfigurationRegistry, Extensions as ConfigurationExtensions} from 'vs/platform/configuration/common/configurationRegistry';
+
 import editorCommon = require('vs/editor/common/editorCommon');
 import {ExpandAbbreviationAction} from './emmetActions';
 import {KeybindingsRegistry} from 'vs/platform/keybinding/common/keybindingsRegistry';
@@ -24,7 +28,24 @@ KeybindingsRegistry.registerCommandRule({
 		KbExpr.has(editorCommon.KEYBINDING_CONTEXT_EDITOR_TEXT_FOCUS),
 		KbExpr.not(editorCommon.KEYBINDING_CONTEXT_EDITOR_HAS_NON_EMPTY_SELECTION),
 		KbExpr.not(editorCommon.KEYBINDING_CONTEXT_EDITOR_HAS_MULTIPLE_SELECTIONS),
-		KbExpr.not(editorCommon.KEYBINDING_CONTEXT_EDITOR_TAB_MOVES_FOCUS)
+		KbExpr.not(editorCommon.KEYBINDING_CONTEXT_EDITOR_TAB_MOVES_FOCUS),
+		KbExpr.has('config.emmet.triggerExpansionOnTab')
 	),
 	primary: KeyCode.Tab
+});
+
+// Configuration: emmet
+const configurationRegistry = <IConfigurationRegistry>Registry.as(ConfigurationExtensions.Configuration);
+configurationRegistry.registerConfiguration({
+	'id': 'emmet',
+	'order': 7,
+	'title': nls.localize('emmetConfigurationTitle', "Emmet configuration"),
+	'type': 'object',
+	'properties': {
+		'emmet.triggerExpansionOnTab': {
+			'type': 'boolean',
+			'default': true,
+			'description': nls.localize('triggerExpansionOnTab', "When enabled, emmet abbreviations are expanded when pressing TAB.")
+		}
+	}
 });
