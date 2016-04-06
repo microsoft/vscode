137384035d2ccc0beee2abe5f9d6b35567cd260e
Johannes Rieken <johannes.rieken@gmail.com>
2016-04-06 15:18:34 +0200

    improve doc about activeTextEditor being undefined from time to time, #5022

--- src/vs/vscode.d.ts
+++ src/vs/vscode.d.ts
@@ -2901,7 +2901,7 @@ declare namespace vscode {
 	export namespace window {
 
 		/**
-		 * The currently active editor or undefined. The active editor is the one
+		 * The currently active editor or `undefined`. The active editor is the one
 		 * that currently has focus or, when none has focus, the one that has changed
 		 * input most recently.
 		 */
@@ -2914,7 +2914,8 @@ declare namespace vscode {
 
 		/**
 		 * An [event](#Event) which fires when the [active editor](#window.activeTextEditor)
-		 * has changed.
+		 * has changed. *Note* that the event also fires when the activ editor changes
+		 * to `undefined`.
 		 */
 		export const onDidChangeActiveTextEditor: Event<TextEditor>;
 
