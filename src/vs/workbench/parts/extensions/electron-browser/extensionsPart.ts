57a8e0b25f146a3fbe8c0e5bab887af87892ca70
Joao Moreno <jomo@microsoft.com>
2016-04-07 16:43:03 +0200

    fix setInput

--- src/vs/workbench/parts/extensions/electron-browser/extensionsPart.ts
+++ src/vs/workbench/parts/extensions/electron-browser/extensionsPart.ts
@@ -8,11 +8,9 @@
 import { TPromise } from 'vs/base/common/winjs.base';
 import { Dimension, Builder } from 'vs/base/browser/builder';
 import { append, emmet as $ } from 'vs/base/browser/dom';
-import { EditorOptions, EditorInput } from 'vs/workbench/common/editor';
 import { BaseEditor } from 'vs/workbench/browser/parts/editor/baseEditor';
 import { Position } from 'vs/platform/editor/common/editor';
 import { ITelemetryService } from 'vs/platform/telemetry/common/telemetry';
-import { ExtensionsInput } from 'vs/workbench/parts/extensions/common/extensionsInput';
 
 export class ExtensionsPart extends BaseEditor {
 
@@ -41,33 +39,6 @@ export class ExtensionsPart extends BaseEditor {
 
 	focus(): void {
 		// TODO
-	}
-
-	setInput(input: EditorInput, options: EditorOptions): TPromise<void> {
-		if (this.input === input) {
-			return TPromise.as(undefined);
-		}
-
-		// this._model = undefined;
-		// this._modelChangeSubscription.dispose();
-
-		if (!(input instanceof ExtensionsInput)) {
-			return TPromise.wrapError<void>('Invalid input');
-		}
-
-		return TPromise.as(null);
-
-		// return this._editorService.resolveEditorModel({ resource: (<HtmlInput>input).getResource() }).then(model => {
-		// 	if (model instanceof BaseTextEditorModel) {
-		// 		this._model = model.textEditorModel;
-		// 	}
-		// 	if (!this._model) {
-		// 		return TPromise.wrapError<void>(localize('html.voidInput', "Invalid editor input."));
-		// 	}
-		// 	this._modelChangeSubscription = this._model.addListener2(EventType.ModelContentChanged2, () => this.webview.contents = this._model.getLinesContent());
-		// 	this.webview.contents = this._model.getLinesContent();
-		// 	return super.setInput(input, options);
-		// });
 	}
 
 	dispose(): void {
