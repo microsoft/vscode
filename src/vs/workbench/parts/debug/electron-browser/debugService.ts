8fe4d6defd92c04b8dda7bccddfe4dc1eaf1b983
isidor <inikolic@microsoft.com>
2016-04-13 09:53:53 +0200

    debug: improve wording #4987

--- src/vs/workbench/parts/debug/electron-browser/debugService.ts
+++ src/vs/workbench/parts/debug/electron-browser/debugService.ts
@@ -558,10 +558,10 @@ export class DebugService extends ee.EventEmitter implements debug.IDebugService
 				}
 
 				this.messageService.show(severity.Error, {
-					message: errorCount > 1 ? nls.localize('preLaunchTaskErrors', "Errors detected while running the preLaunchTask '{0}'.", configuration.preLaunchTask) :
-						errorCount === 1 ?  nls.localize('preLaunchTaskError', "Error detected while running the preLaunchTask '{0}'.", configuration.preLaunchTask) :
+					message: errorCount > 1 ? nls.localize('preLaunchTaskErrors', "Build errors have been detected during preLaunchTask '{0}'.", configuration.preLaunchTask) :
+						errorCount === 1 ?  nls.localize('preLaunchTaskError', "Build error has been detected during preLaunchTask '{0}'.", configuration.preLaunchTask) :
 						nls.localize('preLaunchTaskExitCode', "The preLaunchTask '{0}' terminated with exit code {1}.", configuration.preLaunchTask, taskSummary.exitCode),
-					actions: [CloseAction, new Action('debug.continue', nls.localize('continue', "Continue"), null, true, () => {
+					actions: [CloseAction, new Action('debug.continue', nls.localize('debugAnyway', "Debug Anyway"), null, true, () => {
 						this.messageService.hideAll();
 						return this.doCreateSession(configuration, changeViewState);
 					})]
