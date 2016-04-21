18db994bf72d7759ed6fae71d1a32e1bc1a8f6f5
Joao Moreno <jomo@microsoft.com>
2016-04-21 16:01:37 +0200

    improve postinstall

--- build/npm/postinstall.js
+++ build/npm/postinstall.js
@@ -3,10 +3,20 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-var cp = require('child_process');
+const cp = require('child_process');
 
-var extensions = ['vscode-api-tests', 'vscode-colorize-tests', 'json', 'typescript', 'php', 'javascript'];
+const extensions = [
+	'vscode-api-tests',
+	'vscode-colorize-tests',
+	'json',
+	'typescript',
+	'php',
+	'javascript'
+];
 
-extensions.forEach(function (extension) {
-	cp.execSync('npm --prefix extensions/' + extension + '/ install extensions/' + extension + '/');
+extensions.forEach(extension => {
+	cp.spawnSync('npm', ['install'], {
+		cwd: `extensions/${ extension }`,
+		stdio: 'inherit'
+	});
 });
