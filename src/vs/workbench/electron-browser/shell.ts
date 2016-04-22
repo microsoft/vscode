e772b00b70bd8e766adb99f89bc0d9860d3e87e7
Johannes Rieken <johannes.rieken@gmail.com>
2016-04-22 11:54:10 +0200

    telemetry - don't replace with <APP_ROOT> and such

--- src/vs/workbench/electron-browser/shell.ts
+++ src/vs/workbench/electron-browser/shell.ts
@@ -254,8 +254,8 @@ export class WorkbenchShell {
 		if (this.configuration.env.isBuilt && !this.configuration.env.extensionDevelopmentPath && !!this.configuration.env.enableTelemetry) {
 			this.telemetryService = new ElectronTelemetryService(this.configurationService, this.storageService, {
 				cleanupPatterns: [
-					[new RegExp(escapeRegExpCharacters(this.configuration.env.appRoot), 'gi'), '<APP_ROOT>'],
-					[new RegExp(escapeRegExpCharacters(this.configuration.env.userExtensionsHome), 'gi'), '<EXT_ROOT>']
+					[new RegExp(escapeRegExpCharacters(this.configuration.env.appRoot), 'gi'), ''],
+					[new RegExp(escapeRegExpCharacters(this.configuration.env.userExtensionsHome), 'gi'), '']
 				],
 				version: this.configuration.env.version,
 				commitHash: this.configuration.env.commitHash
