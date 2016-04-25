c0770256758e2d56db60fa8f0059ce7e60317f1c
Joao Moreno <jomo@microsoft.com>
2016-04-25 15:01:18 +0200

    cleanup argv

--- src/vs/workbench/electron-main/argv.ts
+++ src/vs/workbench/electron-main/argv.ts
@@ -55,11 +55,11 @@ ${ indent }--disable-extensions  Disable all installed extensions.
 ${ indent }-g, --goto            Open the file at path at the line and column (add
 ${ indent }                      :line[:column] to path).
 ${ indent }-h, --help            Print usage.
-${ indent }--locale=LOCALE       The locale to use (e.g. en-US or zh-TW).
+${ indent }--locale <locale>     The locale to use (e.g. en-US or zh-TW).
 ${ indent }-n, --new-window      Force a new instance of Code.
 ${ indent }-r, --reuse-window    Force opening a file or folder in the last active
 ${ indent }                      window.
-${ indent }--user-data-dir=DIR   Specifies the directory that user data is kept in,
+${ indent }--user-data-dir <dir> Specifies the directory that user data is kept in,
 ${ indent }                      useful when running as root.
 ${ indent }-v, --version         Print version.
 ${ indent }-w, --wait            Wait for the window to be closed before returning.`;
