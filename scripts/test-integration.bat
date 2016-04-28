a05cb186d7fbc65e5ed6ca11fd86be4899c7a13e
Alexandru Dima <alexdima@microsoft.com>
2016-04-28 16:15:19 +0200

    Add test-integration.bat

---
+++ scripts/test-integration.bat
@@ -0,0 +1,8 @@
+@echo off
+setlocal
+
+:: Integration Tests
+.\scripts\code.bat %~dp0\..\extensions\vscode-api-tests\testWorkspace --extensionDevelopmentPath=%~dp0\..\extensions\vscode-api-tests --extensionTestsPath=%~dp0\..\extensions\vscode-api-tests\out
+.\scripts\code.bat %~dp0\..\extensions\vscode-colorize-tests\test --extensionDevelopmentPath=%~dp0\..\extensions\vscode-colorize-tests --extensionTestsPath=%~dp0\..\extensions\vscode-colorize-tests\out
+
+endlocal
