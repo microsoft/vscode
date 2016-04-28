88fa9cd86eb392596ec97643a9cb2cf963386b30
Joao Moreno <jomo@microsoft.com>
2016-04-28 16:53:44 +0200

    use lstat in rimraf
    fixes #3755

--- src/vs/base/node/pfs.ts
+++ src/vs/base/node/pfs.ts
@@ -55,8 +55,8 @@ export function mkdirp(path: string, mode?: number): TPromise<boolean> {
 }
 
 export function rimraf(path: string): TPromise<void> {
-	return stat(path).then(stat => {
-		if (stat.isDirectory()) {
+	return lstat(path).then(stat => {
+		if (stat.isDirectory() && !stat.isSymbolicLink()) {
 			return readdir(path)
 				.then(children => TPromise.join(children.map(child => rimraf(join(path, child)))))
 				.then(() => rmdir(path));
