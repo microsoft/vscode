0e9c89080c4e15424d7cf89729eed5abb5059257
Benjamin Pasero <benjpas@microsoft.com>
2016-04-29 09:44:54 +0200

    set ELECTRON_NO_ATTACH_CONSOLE=1

--- scripts/test-integration.bat
+++ scripts/test-integration.bat
@@ -1,6 +1,8 @@
 @echo off
 setlocal
 
+set ELECTRON_NO_ATTACH_CONSOLE=1
+
 :: Integration Tests
 .\scripts\code.bat %~dp0\..\extensions\vscode-api-tests\testWorkspace --extensionDevelopmentPath=%~dp0\..\extensions\vscode-api-tests --extensionTestsPath=%~dp0\..\extensions\vscode-api-tests\out
 .\scripts\code.bat %~dp0\..\extensions\vscode-colorize-tests\test --extensionDevelopmentPath=%~dp0\..\extensions\vscode-colorize-tests --extensionTestsPath=%~dp0\..\extensions\vscode-colorize-tests\out
