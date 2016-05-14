9cff38ac885b2baf833a6346ff5fa23ac32ae2c8
Daniel Imms <daimms@microsoft.com>
2016-05-13 19:07:14 -0700

    Improve external terminal support on xfce
    Fixes #5780

--- src/vs/workbench/parts/execution/electron-browser/terminal.ts
+++ src/vs/workbench/parts/execution/electron-browser/terminal.ts
@@ -11,6 +11,10 @@ if (env.isLinux) {
 		defaultTerminalLinux = 'x-terminal-emulator';
 	} else if (process.env.DESKTOP_SESSION === 'gnome' || process.env.DESKTOP_SESSION === 'gnome-classic') {
 		defaultTerminalLinux = 'gnome-terminal';
+	} else if (process.env.COLORTERM) {
+		defaultTerminalLinux = process.env.COLORTERM;
+	} else if (process.env.TERM) {
+		defaultTerminalLinux = process.env.TERM;
 	}
 }
 
