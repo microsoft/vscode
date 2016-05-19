bea29307d58a4c35cf09fa12fea6ad846212e9c9
isidor <inikolic@microsoft.com>
2016-05-19 15:11:56 +0200

    use editorService.groupAt

--- src/vs/workbench/browser/parts/editor/sideBySideEditorControl.ts
+++ src/vs/workbench/browser/parts/editor/sideBySideEditorControl.ts
@@ -1025,7 +1025,8 @@ export class SideBySideEditorControl implements ISideBySideEditorControl, IVerti
 			const toolbar = this.doCreateToolbar(div, position);
 			this.editorTitleToolbar[position] = toolbar;
 			this.editorTitleToolbar[position].setActions([this.closeEditorActions[position]])();
-			const group = this.editorService.getStacksModel().getGroup(position);
+
+			const group = this.editorService.groupAt(position);
 			this.editorTitleToolbar[position].context = { group };
 		});
 
@@ -1047,7 +1048,8 @@ export class SideBySideEditorControl implements ISideBySideEditorControl, IVerti
 		}, (div) => {
 			const toolbar = this.doCreateToolbar(div, position);
 			this.editorActionsToolbar[position] = toolbar;
-			const group = this.editorService.getStacksModel().getGroup(position);
+
+			const group = this.editorService.groupAt(position);
 			this.editorActionsToolbar[position].context = { group };
 		});
 	}
@@ -1175,7 +1177,7 @@ export class SideBySideEditorControl implements ISideBySideEditorControl, IVerti
 	public updateTitleArea(state: ITitleAreaState): void;
 	public updateTitleArea(input: EditorInput): void;
 	public updateTitleArea(arg1: any): void {
-		
+
 		// Update all title areas that relate to given input if provided
 		if (arg1 instanceof EditorInput) {
 			const input: EditorInput = arg1;
@@ -1211,9 +1213,10 @@ export class SideBySideEditorControl implements ISideBySideEditorControl, IVerti
 		const activePosition = this.lastActivePosition;
 
 		states.forEach(state => {
-			const group = this.editorService.getStacksModel().groups[state.position];
+			const group = this.editorService.groupAt(state.position);
 			this.editorTitleToolbar[state.position].context = { group };
 			this.editorActionsToolbar[state.position].context = { group };
+
 			let editor = this.visibleEditors[state.position];
 			let input = editor ? editor.input : null;
 
