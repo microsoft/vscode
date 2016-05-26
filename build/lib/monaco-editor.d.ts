668a4ff8b1907cb70aa734447a0c9e04fd05bf19
Alex Dima <alexdima@microsoft.com>
2016-05-26 23:26:41 +0200

    Add completion API to the standalone editor

--- build/lib/monaco-editor.d.ts
+++ build/lib/monaco-editor.d.ts
@@ -22,6 +22,51 @@ declare module monaco.languages {
 
     export function registerRenameProvider(languageId: string, support: RenameProvider): IDisposable;
 
+    export enum CompletionItemKind {
+        Text = 0,
+        Method = 1,
+        Function = 2,
+        Constructor = 3,
+        Field = 4,
+        Variable = 5,
+        Class = 6,
+        Interface = 7,
+        Module = 8,
+        Property = 9,
+        Unit = 10,
+        Value = 11,
+        Enum = 12,
+        Keyword = 13,
+        Snippet = 14,
+        Color = 15,
+        File = 16,
+        Reference = 17,
+    }
+
+    export interface CompletionItem {
+        label: string;
+        kind: CompletionItemKind;
+        detail?: string;
+        documentation?: string;
+        sortText?: string;
+        filterText?: string;
+        insertText?: string;
+        textEdit?: editor.ISingleEditOperation;
+    }
+
+    export interface CompletionList {
+        isIncomplete?: boolean;
+        items: CompletionItem[];
+    }
+
+    export interface CompletionItemProvider {
+        triggerCharacters?: string[];
+        provideCompletionItems(model: editor.IReadOnlyModel, position: Position, token: CancellationToken): CompletionItem[] | Thenable<CompletionItem[]> | CompletionList | Thenable<CompletionList>;
+        resolveCompletionItem?(item: CompletionItem, token: CancellationToken): CompletionItem | Thenable<CompletionItem>;
+    }
+
+    export function registerCompletionItemProvider(languageId: string, provider: CompletionItemProvider): IDisposable;
+
     export function registerSignatureHelpProvider(languageId: string, support: SignatureHelpProvider): IDisposable;
 
     export function registerHoverProvider(languageId: string, support: HoverProvider): IDisposable;
@@ -1321,6 +1366,11 @@ declare module monaco.editor {
          * Defaults to false.
          */
         fontLigatures?: boolean;
+        /**
+         * Disable the use of `translate3d`.
+         * Defaults to false.
+         */
+        disableTranslate3d?: boolean;
         /**
          * Should the cursor be hidden in the overview ruler.
          * Defaults to false.
