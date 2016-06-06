3268f80a3639ffa5cf85749db2622d3b7c228100
Martin Aeschlimann <martinae@microsoft.com>
2016-06-06 16:34:07 +0200

    Comment SVG

--- extensions/css/client/src/cssMain.ts
+++ extensions/css/client/src/cssMain.ts
@@ -27,12 +27,12 @@ export function activate(context: vscode.ExtensionContext) {
 		}
 	};
 
-	let decorationType2 : vscode.DecorationRenderOptions = {
-		after: {
-			contentIconPath: "data:image/svg+xml;charset=utf8,%3Csvg%20xmlns%3D%22http%3A//www.w3.org/2000/svg%22%20viewBox%3D%220%200%2016%2016%22%20enable-background%3D%22new%200%200%2016%2016%22%20height%3D%2216%22%20width%3D%2216%22%3E%3Cpath%20fill%3D%22%23F6F6F6%22%20d%3D%22M7.5%202L2%2012l2%202h9l2-2L9.5%202z%22/%3E%3Cpath%20d%3D%22M9%203H8l-4.5%209%201%201h8l1-1L9%203zm0%209H8v-1h1v1zm0-2H8V6h1v4z%22%20fill%3D%22%23fc0%22/%3E%3Cpath%20d%3D%22M9%2010H8V6h1v4zm0%201H8v1h1v-1z%22/%3E%3C/svg%3E\")"
+	// let decorationType2 : vscode.DecorationRenderOptions = {
+	// 	after: {
+	// 		contentIconPath: "data:image/svg+xml;charset=utf8,%3Csvg%20xmlns%3D%22http%3A//www.w3.org/2000/svg%22%20viewBox%3D%220%200%2016%2016%22%20enable-background%3D%22new%200%200%2016%2016%22%20height%3D%2216%22%20width%3D%2216%22%3E%3Cpath%20fill%3D%22%23F6F6F6%22%20d%3D%22M7.5%202L2%2012l2%202h9l2-2L9.5%202z%22/%3E%3Cpath%20d%3D%22M9%203H8l-4.5%209%201%201h8l1-1L9%203zm0%209H8v-1h1v1zm0-2H8V6h1v4z%22%20fill%3D%22%23fc0%22/%3E%3Cpath%20d%3D%22M9%2010H8V6h1v4zm0%201H8v1h1v-1z%22/%3E%3C/svg%3E\")"
 
-		}
-	};
+	// 	}
+	// };
 
 
 //			content: "url(\"data:image/svg+xml;charset=utf8,%3Csvg xmlns='http://www.w3.org/2000/svg' width='1.2em' height='0.9em' viewBox='0 0 16 16'%3E%3Crect x='1' y='1' stroke='white' style='stroke-width: 1;' width='14' height='14' fill='#FFB6C1' shape-rendering='crispEdges' /%3E%3C/svg%3E\")",
