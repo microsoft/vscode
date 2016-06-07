24e811aaa256f9d45c10fbe2491a5938f57482c0
Joao Moreno <jomo@microsoft.com>
2016-06-07 09:13:57 +0200

    Revert ":lipstick: lifecycle service, idle monitor"
    This reverts commit 483c63bc0cbabe28f18e9a91b9637d582c75aead.

--- src/vs/base/browser/idleMonitor.ts
+++ src/vs/base/browser/idleMonitor.ts
@@ -5,8 +5,8 @@
 'use strict';
 
 import {TimeoutTimer} from 'vs/base/common/async';
-import Event, {Emitter} from 'vs/base/common/event';
-import {Disposable} from 'vs/base/common/lifecycle';
+import {EventEmitter} from 'vs/base/common/eventEmitter';
+import {Disposable, IDisposable} from 'vs/base/common/lifecycle';
 import * as dom from 'vs/base/browser/dom';
 
 export enum UserStatus {
@@ -14,22 +14,15 @@ export enum UserStatus {
 	Active
 }
 
-export interface IIdleMonitor {
-	status: UserStatus;
-	onStatusChange: Event<UserStatus>;
-}
-
 export const DEFAULT_IDLE_TIME = 60 * 60 * 1000; // 60 minutes
 
-export class IdleMonitor extends Disposable implements IIdleMonitor {
+export class IdleMonitor extends Disposable {
 
 	private _lastActiveTime: number;
 	private _idleCheckTimeout: TimeoutTimer;
 	private _status: UserStatus;
+	private _eventEmitter: EventEmitter;
 	private _idleTime: number;
-
-	private _onStatusChange: Emitter<UserStatus>;
-	get onStatusChange(): Event<UserStatus> { return this._onStatusChange.event; }
 
 	constructor(idleTime: number = DEFAULT_IDLE_TIME) {
 		super();
@@ -39,42 +32,53 @@ export class IdleMonitor extends Disposable implements IIdleMonitor {
 		this._lastActiveTime = -1;
 		this._idleTime = idleTime;
 
+		this._eventEmitter = this._register(new EventEmitter());
 		this._register(dom.addDisposableListener(document, 'mousemove', () => this._onUserActive()));
 		this._register(dom.addDisposableListener(document, 'keydown', () => this._onUserActive()));
 		this._onUserActive();
 	}
 
-	get status(): UserStatus {
+	public dispose(): void {
+		super.dispose();
+	}
+
+	public addOneTimeActiveListener(callback: () => void): IDisposable {
+		return this._eventEmitter.addOneTimeDisposableListener('onActive', callback);
+	}
+
+	public addOneTimeIdleListener(callback: () => void): IDisposable {
+		return this._eventEmitter.addOneTimeDisposableListener('onIdle', callback);
+	}
+
+	public getStatus(): UserStatus {
 		return this._status;
 	}
 
 	private _onUserActive(): void {
 		this._lastActiveTime = (new Date()).getTime();
-
 		if (this._status !== UserStatus.Active) {
 			this._status = UserStatus.Active;
 			this._scheduleIdleCheck();
-			this._onStatusChange.fire(this._status);
+			this._eventEmitter.emit('onActive');
 		}
 	}
 
 	private _onUserIdle(): void {
 		if (this._status !== UserStatus.Idle) {
 			this._status = UserStatus.Idle;
-			this._onStatusChange.fire(this._status);
+			this._eventEmitter.emit('onIdle');
 		}
 	}
 
 	private _scheduleIdleCheck(): void {
-		const minimumTimeWhenUserCanBecomeIdle = this._lastActiveTime + this._idleTime;
-		const timeout = minimumTimeWhenUserCanBecomeIdle - (new Date()).getTime();
-
-		this._idleCheckTimeout.setIfNotSet(() => this._checkIfUserIsIdle(), timeout);
+		let minimumTimeWhenUserCanBecomeIdle = this._lastActiveTime + this._idleTime;
+		this._idleCheckTimeout.setIfNotSet(() => {
+			this._checkIfUserIsIdle();
+		}, minimumTimeWhenUserCanBecomeIdle - (new Date()).getTime());
 	}
 
 	private _checkIfUserIsIdle(): void {
-		const actualIdleTime = (new Date()).getTime() - this._lastActiveTime;
-
+		let actualIdleTime = (new Date()).getTime() - this._lastActiveTime;
 		if (actualIdleTime >= this._idleTime) {
 			this._onUserIdle();
 		} else {
