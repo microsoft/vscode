b37a677c4369699898db564a2c5bcfb2ac62ca7c
Don Jayamanne <don.jayamanne@yahoo.com>
2016-06-13 09:08:40 +1000

    fixed linting

--- src/client/providers/importSortProvider.ts
+++ src/client/providers/importSortProvider.ts
@@ -1,29 +1,29 @@
-'use strict';
+"use strict";
 
-import vscode = require('vscode');
-import * as path from 'path';
-import * as fs from 'fs';
-import * as child_process from 'child_process';
+import * as vscode from "vscode";
+import * as path from "path";
+import * as fs from "fs";
+import * as child_process from "child_process";
 
 export class PythonImportSortProvider {
     public sortImports(extensionDir: string, document: vscode.TextDocument): Thenable<vscode.TextEdit[]> {
         return new Promise<vscode.TextEdit[]>((resolve, reject) => {
-            var filePath = document.uri.fsPath;
-            var importScript = path.join(extensionDir, "pythonFiles", "sortImports.py");
+            let filePath = document.uri.fsPath;
+            let importScript = path.join(extensionDir, "pythonFiles", "sortImports.py");
             if (!fs.existsSync(filePath)) {
-                vscode.window.showErrorMessage(`File ${filePath} does not exist`)
+                vscode.window.showErrorMessage(`File ${filePath} does not exist`);
                 return resolve([]);
             }
 
-            var ext = path.extname(filePath);
-            var tmp = require("tmp");
+            let ext = path.extname(filePath);
+            let tmp = require("tmp");
             tmp.file({ postfix: ext }, function _tempFileCreated(err, tmpFilePath, fd) {
                 if (err) {
                     reject(err);
                     return;
                 }
-                var documentText = document.getText();
-                fs.writeFile(tmpFilePath, documentText, ex=> {
+                let documentText = document.getText();
+                fs.writeFile(tmpFilePath, documentText, ex => {
                     if (ex) {
                         vscode.window.showErrorMessage(`Failed to create a temporary file, ${ex.message}`);
                         return;
@@ -31,7 +31,7 @@ export class PythonImportSortProvider {
 
                     child_process.exec(`python "${importScript}" "${tmpFilePath}"`, (error, stdout, stderr) => {
                         if (error || stderr) {
-                            vscode.window.showErrorMessage(`File ${filePath} does not exist`)
+                            vscode.window.showErrorMessage(`File ${filePath} does not exist`);
                             return resolve([]);
                         }
 
@@ -41,14 +41,14 @@ export class PythonImportSortProvider {
                                 return;
                             }
 
-                            var formattedText = data.toString('utf-8');
-                            //Nothing to do 
+                            let formattedText = data.toString("utf-8");
+                            // Nothing to do 
                             if (document.getText() === formattedText) {
                                 return resolve([]);
                             }
 
-                            var range = new vscode.Range(document.lineAt(0).range.start, document.lineAt(document.lineCount - 1).range.end)
-                            var txtEdit = new vscode.TextEdit(range, formattedText);
+                            let range = new vscode.Range(document.lineAt(0).range.start, document.lineAt(document.lineCount - 1).range.end);
+                            let txtEdit = new vscode.TextEdit(range, formattedText);
                             resolve([txtEdit]);
                         });
                     });
