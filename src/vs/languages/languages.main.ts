168e4bc7b3a288b7b554def57c70270d28a77823
Martin Aeschlimann <martinae@microsoft.com>
2016-06-15 10:52:36 +0200

    fixes #7699: Monaco API: `css` / `scss` / `less` aren't bundling anymore in monaco

--- src/vs/languages/languages.main.ts
+++ src/vs/languages/languages.main.ts
@@ -10,6 +10,3 @@ import 'vs/languages/handlebars/common/handlebars.contribution';
 import 'vs/languages/html/common/html.contribution';
 import 'vs/languages/markdown/common/markdown.contribution';
 import 'vs/languages/razor/common/razor.contribution';
-//import 'vs/languages/less/common/less.contribution';
-//import 'vs/languages/sass/common/sass.contribution';
-//import 'vs/languages/css/common/css.contribution';
