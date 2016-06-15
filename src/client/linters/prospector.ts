e6aaf3ed5d3316178882bb15c40f38f3bfb599f3
Don Jayamanne <don.jayamanne@yahoo.com>
2016-06-15 10:27:22 +1000

    fixed prospector linting #142

--- src/client/linters/prospector.ts
+++ src/client/linters/prospector.ts
@@ -1,13 +1,28 @@
-'use strict';
+"use strict";
 
-import * as path from 'path';
-import * as baseLinter from './baseLinter';
-import {OutputChannel, workspace} from 'vscode';
+import * as path from "path";
+import * as baseLinter from "./baseLinter";
+import {OutputChannel, workspace, window} from "vscode";
+import {sendCommand} from "./../common/childProc";
 
-const PROSPECTOR_COMMANDLINE = " --output-format=vscode";
+const PROSPECTOR_COMMANDLINE = " --absolute-paths --output-format=json";
 
-const REGEX = '(?<line>\\d+),(?<column>\\d+),(?<type>[\\w-]+),(?<code>[\\w-]+):(?<message>.*)\\r?(\\n|$)';
-
+interface IProspectorResponse {
+    messages: IProspectorMessage[];
+}
+interface IProspectorMessage {
+    source: "string";
+    message: "string";
+    code: "string";
+    location: IProspectorLocation;
+}
+interface IProspectorLocation {
+    function: string;
+    path: string;
+    line: number;
+    character: number;
+    module: "beforeFormat";
+}
 
 export class Linter extends baseLinter.BaseLinter {
     constructor(outputChannel: OutputChannel) {
@@ -22,21 +37,53 @@ export class Linter extends baseLinter.BaseLinter {
             return Promise.resolve([]);
         }
 
-        var prospectorPath = this.pythonSettings.linting.prospectorPath;
-        var prospectorSourcePath = this.pythonSettings.linting.prospectorSourcePath;
-        var prospectorExtraCommands = this.pythonSettings.linting.prospectorExtraCommands;
-        // prospector works best with relative path
-        var fileName = filePath.replace(path.join(workspace.rootPath, prospectorSourcePath, '/'), '');
-        var cmdLine = `${prospectorPath} ${PROSPECTOR_COMMANDLINE} ${prospectorExtraCommands} "${fileName}"`;
+        let prospectorPath = this.pythonSettings.linting.prospectorPath;
+        let prospectorExtraCommands = this.pythonSettings.linting.prospectorExtraCommands;
+        let cmdLine = `${prospectorPath} ${PROSPECTOR_COMMANDLINE} ${prospectorExtraCommands} "${filePath}"`;
+        let outputChannel = this.outputChannel;
+        let linterId = this.Id;
+
         return new Promise<baseLinter.ILintMessage[]>((resolve, reject) => {
-            this.run(cmdLine, filePath, txtDocumentLines, path.join(workspace.rootPath, prospectorSourcePath) , REGEX).then(messages=> {
-                //All messages in prospector are treated as warn, ings for now
-                messages.forEach(msg=> {
-                    msg.severity = baseLinter.LintMessageSeverity.Information;
+            sendCommand(cmdLine, workspace.rootPath, false).then(data => {
+                outputChannel.clear();
+                let parsedData: IProspectorResponse;
+                try {
+                    parsedData = JSON.parse(data);
+                }
+                catch (ex) {
+                    outputChannel.append(data);
+                    return resolve([]);
+                }
+                let diagnostics: baseLinter.ILintMessage[] = [];
+                parsedData.messages.filter((value, index) => index <= this.pythonSettings.linting.maxNumberOfProblems).forEach(msg => {
+
+                    let sourceLine = txtDocumentLines[msg.location.line - 1];
+                    let sourceStart = sourceLine.substring(msg.location.character);
+                    let endCol = txtDocumentLines[msg.location.line - 1].length;
+
+                    // try to get the first word from the starting position
+                    let possibleProblemWords = sourceStart.match(/\w+/g);
+                    let possibleWord: string;
+                    if (possibleProblemWords != null && possibleProblemWords.length > 0 && sourceStart.startsWith(possibleProblemWords[0])) {
+                        possibleWord = possibleProblemWords[0];
+                    }
+
+                    diagnostics.push({
+                        code: msg.code,
+                        message: msg.message,
+                        column: msg.location.character,
+                        line: msg.location.line,
+                        possibleWord: possibleWord,
+                        type: msg.code,
+                        provider: `${this.Id}-${msg.source}`
+                    });
                 });
 
-                resolve(messages);
-            }, reject);
+                resolve(diagnostics);
+            }).catch(error => {
+                outputChannel.appendLine(`Linting with ${linterId} failed. If not installed please turn if off in settings.\n ${error}`);
+                window.showInformationMessage(`Linting with ${linterId} failed. If not installed please turn if off in settings. View Python output for details.`);
+            });
         });
     }
 }
