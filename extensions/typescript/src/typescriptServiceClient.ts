2ff58c1ef0195b17aea8f1de5513725fc09c7b97
Dirk Baeumer <dirkb@microsoft.com>
2016-06-23 13:50:01 +0200

    Remove breakpoint flag

--- extensions/typescript/src/typescriptServiceClient.ts
+++ extensions/typescript/src/typescriptServiceClient.ts
@@ -210,7 +210,7 @@ export default class TypeScriptServiceClient implements ITypescriptServiceClient
 		this.servicePromise = new Promise<cp.ChildProcess>((resolve, reject) => {
 			try {
 				let options: electron.IForkOptions = {
-					execArgv: [`--debug-brk=5859`]
+					execArgv: [] // [`--debug-brk=5859`]
 				};
 				let value = process.env.TSS_DEBUG;
 				if (value) {
