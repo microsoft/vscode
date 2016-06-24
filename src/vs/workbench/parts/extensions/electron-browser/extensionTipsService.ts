c053482976b3d41b80259ae739d64dfc002dd117
Joao Moreno <jomo@microsoft.com>
2016-06-24 16:07:22 +0200

    important extension recommendations

--- src/vs/workbench/parts/extensions/electron-browser/extensionTipsService.ts
+++ src/vs/workbench/parts/extensions/electron-browser/extensionTipsService.ts
@@ -4,14 +4,20 @@
  *--------------------------------------------------------------------------------------------*/
 
 import URI from 'vs/base/common/uri';
+import { localize } from 'vs/nls';
 import {forEach} from 'vs/base/common/collections';
 import {IDisposable, dispose} from 'vs/base/common/lifecycle';
 import {TPromise as Promise} from 'vs/base/common/winjs.base';
+import {Action} from 'vs/base/common/actions';
 import {match} from 'vs/base/common/glob';
-import {IExtensionGalleryService, IExtensionTipsService, IGalleryExtension} from 'vs/platform/extensionManagement/common/extensionManagement';
+import {IExtensionManagementService, IExtensionGalleryService, IExtensionTipsService, IGalleryExtension} from 'vs/platform/extensionManagement/common/extensionManagement';
 import {IModelService} from 'vs/editor/common/services/modelService';
 import {IStorageService, StorageScope} from 'vs/platform/storage/common/storage';
-import {IWorkspaceContextService} from 'vs/platform/workspace/common/workspace';
+import product from 'vs/platform/product';
+import { IMessageService, CloseAction } from 'vs/platform/message/common/message';
+import { IInstantiationService } from 'vs/platform/instantiation/common/instantiation';
+import { ShowExtensionRecommendationsAction } from './extensionsActions';
+import Severity from 'vs/base/common/severity';
 
 export class ExtensionTipsService implements IExtensionTipsService {
 
@@ -19,25 +25,33 @@ export class ExtensionTipsService implements IExtensionTipsService {
 
 	private _recommendations: { [id: string]: boolean; } = Object.create(null);
 	private _availableRecommendations: { [pattern: string]: string[] } = Object.create(null);
+	private importantRecommendations: { [pattern: string]: string[] };
+	private importantRecommendationsIgnoreList: string[];
 	private _disposables: IDisposable[] = [];
 
 	constructor(
 		@IExtensionGalleryService private _galleryService: IExtensionGalleryService,
 		@IModelService private _modelService: IModelService,
-		@IStorageService private _storageService: IStorageService,
-		@IWorkspaceContextService contextService: IWorkspaceContextService
+		@IStorageService private storageService: IStorageService,
+		@IMessageService private messageService: IMessageService,
+		@IExtensionManagementService private extensionsService: IExtensionManagementService,
+		@IInstantiationService private instantiationService: IInstantiationService
 	) {
 		if (!this._galleryService.isEnabled()) {
 			return;
 		}
 
-		const extensionTips = contextService.getConfiguration().env.extensionTips;
+		const extensionTips = product.extensionTips;
+
 		if (!extensionTips) {
 			return;
 		}
 
+		this.importantRecommendations = product.extensionImportantTips || Object.create(null);
+		this.importantRecommendationsIgnoreList = <string[]>JSON.parse(storageService.get('extensionsAssistant/importantRecommendationsIgnore', StorageScope.GLOBAL, '[]'));
+
 		// retrieve ids of previous recommendations
-		const storedRecommendations = <string[]>JSON.parse(_storageService.get('extensionsAssistant/recommendations', StorageScope.GLOBAL, '[]'));
+		const storedRecommendations = <string[]>JSON.parse(storageService.get('extensionsAssistant/recommendations', StorageScope.GLOBAL, '[]'));
 		for (let id of storedRecommendations) {
 			this._recommendations[id] = true;
 		}
@@ -54,12 +68,8 @@ export class ExtensionTipsService implements IExtensionTipsService {
 			}
 		});
 
-		this._disposables.push(this._modelService.onModelAdded(model => {
-			this._suggest(model.uri);
-		}));
-		for (let model of this._modelService.getModels()) {
-			this._suggest(model.uri);
-		}
+		this._disposables.push(this._modelService.onModelAdded(model => this._suggest(model.uri)));
+		this._modelService.getModels().forEach(model => this._suggest(model.uri));
 	}
 
 	getRecommendations(): Promise<IGalleryExtension[]> {
@@ -91,11 +101,41 @@ export class ExtensionTipsService implements IExtensionTipsService {
 				}
 			});
 
-			this._storageService.store(
+			this.storageService.store(
 				'extensionsAssistant/recommendations',
 				JSON.stringify(Object.keys(this._recommendations)),
 				StorageScope.GLOBAL
 			);
+
+			this.extensionsService.getInstalled().done(local => {
+				Object.keys(this.importantRecommendations)
+					.filter(id => this.importantRecommendationsIgnoreList.indexOf(id) === -1)
+					.filter(id => local.every(local => `${local.manifest.publisher}.${local.manifest.name}` !== id))
+					.forEach(id => {
+						const pattern = this.importantRecommendations[id];
+
+						if (!match(pattern, uri.fsPath)) {
+							return;
+						}
+
+						const message = localize('reallyRecommended', "It is recommended to install the '{0}' extension.", id);
+						const neverAgainAction = new Action('neverShowAgain', localize('neverShowAgain', "Don't show again"), null, true, () => {
+							this.importantRecommendationsIgnoreList.push(id);
+							this.storageService.store(
+								'extensionsAssistant/importantRecommendationsIgnore',
+								JSON.stringify(this.importantRecommendationsIgnoreList),
+								StorageScope.GLOBAL
+							);
+							return Promise.as(true);
+						});
+						const recommendationsAction = this.instantiationService.createInstance(ShowExtensionRecommendationsAction, ShowExtensionRecommendationsAction.ID, localize('showRecommendations', "Show Recommendations"));
+
+						this.messageService.show(Severity.Info, {
+							message,
+							actions: [CloseAction, neverAgainAction, recommendationsAction]
+						});
+					});
+			});
 		});
 	}
 
