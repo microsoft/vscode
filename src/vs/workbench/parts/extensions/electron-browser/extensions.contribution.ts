a7d64b71de2e845498c676d88d2ff9e1fc27e444
Benjamin Pasero <benjpas@microsoft.com>
2016-06-27 08:37:08 +0200

    Quick Open "ext" and "ext install" not working (fixes #8123)

--- src/vs/workbench/parts/extensions/electron-browser/extensions.contribution.ts
+++ src/vs/workbench/parts/extensions/electron-browser/extensions.contribution.ts
@@ -24,7 +24,6 @@ import { OpenExtensionsViewletAction, ListOutdatedExtensionsAction, ShowExtensio
 import { ExtensionsInput } from './extensionsInput';
 import { ViewletRegistry, Extensions as ViewletExtensions, ViewletDescriptor } from 'vs/workbench/browser/viewlet';
 import { ExtensionEditor } from './extensionEditor';
-import { IQuickOpenRegistry, Extensions, QuickOpenHandlerDescriptor } from 'vs/workbench/browser/quickopen';
 
 // Singletons
 registerSingleton(IExtensionGalleryService, ExtensionGalleryService);
@@ -40,27 +39,6 @@ Registry.as<IWorkbenchContributionsRegistry>(WorkbenchExtensions.Workbench)
 
 Registry.as<IOutputChannelRegistry>(OutputExtensions.OutputChannels)
 	.registerChannel(ExtensionsChannelId, ExtensionsLabel);
-
-// Quickopen
-Registry.as<IQuickOpenRegistry>(Extensions.Quickopen).registerQuickOpenHandler(
-	new QuickOpenHandlerDescriptor(
-		'vs/workbench/parts/extensions/electron-browser/extensionsQuickOpen',
-		'ExtensionsHandler',
-		'ext ',
-		localize('extensionsCommands', "Manage Extensions"),
-		true
-	)
-);
-
-Registry.as<IQuickOpenRegistry>(Extensions.Quickopen).registerQuickOpenHandler(
-	new QuickOpenHandlerDescriptor(
-		'vs/workbench/parts/extensions/electron-browser/extensionsQuickOpen',
-		'GalleryExtensionsHandler',
-		'ext install ',
-		localize('galleryExtensionsCommands', "Install Gallery Extensions"),
-		true
-	)
-);
 
 // Editor
 const editorDescriptor = new EditorDescriptor(
