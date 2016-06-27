098613108759e947ec106f2f03303a65819f7f38
Erich Gamma <egamma@microsoft.com>
2016-06-27 16:34:22 +0200

    Small tweaks to PR #8155

--- src/vs/workbench/parts/emmet/node/actions/updateTag.ts
+++ src/vs/workbench/parts/emmet/node/actions/updateTag.ts
@@ -6,7 +6,7 @@
 'use strict';
 
 import nls = require('vs/nls');
-import {EmmetEditorAction} from '../emmetActions';
+import {EmmetEditorAction} from 'vs/workbench/parts/emmet/node/emmetActions';
 
 import {CommonEditorRegistry, EditorActionDescriptor} from 'vs/editor/common/editorCommonExtensions';
 import {IEditorActionDescriptorData, ICommonCodeEditor} from 'vs/editor/common/editorCommon';
@@ -23,18 +23,18 @@ class UpdateTagAction extends EmmetEditorAction {
 		super(descriptor, editor, configurationService);
 	}
 
-	public runEmmetAction(_module) {
+	public runEmmetAction(_emmet) {
 		let options: IInputOptions = {
 			prompt: nls.localize('enterTag', 'Enter Tag'),
 			placeHolder: nls.localize('tag', 'Tag')
 		};
 		this.quickOpenService.input(options).then(tag => {
-			this.wrapAbbreviation(_module, tag);
+			this.wrapAbbreviation(_emmet, tag);
 		});
 	}
 
-	private wrapAbbreviation(_module: any, tag) {
-		if (!_module.run('update_tag', this.editorAccessor, tag)) {
+	private wrapAbbreviation(_emmet: any, tag) {
+		if (!_emmet.run('update_tag', this.editorAccessor, tag)) {
 			this.editorAccessor.noExpansionOccurred();
 		}
 	}
