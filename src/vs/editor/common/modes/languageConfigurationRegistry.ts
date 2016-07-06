8d8545a70133497b62b5bbdba072591302a17713
Johannes Rieken <johannes.rieken@gmail.com>
2016-07-06 15:38:08 +0200

    always create character pair support, fixes #8662

--- src/vs/editor/common/modes/languageConfigurationRegistry.ts
+++ src/vs/editor/common/modes/languageConfigurationRegistry.ts
@@ -110,9 +110,7 @@ export class RichEditSupport {
 
 		this._handleComments(modeId, this._conf);
 
-		if (this._conf.autoClosingPairs) {
-			this.characterPair = new CharacterPairSupport(LanguageConfigurationRegistry, modeId, this._conf);
-		}
+		this.characterPair = new CharacterPairSupport(LanguageConfigurationRegistry, modeId, this._conf);
 
 		if (this._conf.__electricCharacterSupport || this._conf.brackets) {
 			this.electricCharacter = new BracketElectricCharacterSupport(LanguageConfigurationRegistry, modeId, this.brackets, this._conf.__electricCharacterSupport);
