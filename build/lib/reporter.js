b6b7e82041ad1d1c96f4f99d87250fcece02345a
Joao Moreno <jomo@microsoft.com>
2016-07-12 11:06:15 +0200

    use strict

--- build/lib/reporter.js
+++ build/lib/reporter.js
@@ -3,6 +3,8 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+'use strict';
+
 const es = require('event-stream');
 const _ = require('underscore');
 const util = require('gulp-util');
