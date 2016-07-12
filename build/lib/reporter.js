03c91e97728698a681c0fd15508942aff3a59a6a
Joao Moreno <jomo@microsoft.com>
2016-07-12 10:47:19 +0200

    remove VSCODE_BUILD_QUIET

--- build/lib/reporter.js
+++ build/lib/reporter.js
@@ -3,18 +3,21 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-var es = require('event-stream');
-var _ = require('underscore');
+const es = require('event-stream');
+const _ = require('underscore');
+const util = require('gulp-util');
 
-var allErrors = [];
-var count = 0;
+const allErrors = [];
+let startTime = null;
+let count = 0;
 
 function onStart() {
 	if (count++ > 0) {
 		return;
 	}
 
-	console.log('*** Starting...');
+	startTime = new Date().getTime();
+	util.log(util.colors.green('Starting compilation'));
 }
 
 function onEnd() {
@@ -23,8 +26,9 @@ function onEnd() {
 	}
 
 	var errors = _.flatten(allErrors);
-	errors.map(function (err) { console.error('*** Error:', err); });
-	console.log('*** Finished with', errors.length, 'errors.');
+	errors.map(err => util.log(`${ util.colors.red('Error') }: ${ err }`));
+
+	util.log(`${ util.colors.green('Finished compilation') } with ${ util.colors.red(errors.length + ' errors') } in ${ util.colors.blue((new Date().getTime() - startTime) + 'ms') }.`);
 }
 
 module.exports = function () {
