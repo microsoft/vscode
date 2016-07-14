d48f72944e5eead11d043ac07f5daa8d51f25419
Alex Dima <alexdima@microsoft.com>
2016-07-14 12:37:11 +0300

    Fixes #9257: Fix TS rename fail...

--- src/vs/workbench/api/node/mainThreadStatusBar.ts
+++ src/vs/workbench/api/node/mainThreadStatusBar.ts
@@ -21,14 +21,14 @@ export class MainThreadStatusBar extends MainThreadStatusBarShape {
 	$setEntry(id: number, text: string, tooltip: string, command: string, color: string, alignment: MainThreadStatusBarAlignment, priority: number): void {
 
 		// Dispose any old
-		this.dispose(id);
+		this.$dispose(id);
 
 		// Add new
 		let disposeable = this.statusbarService.addEntry({ text, tooltip, command, color }, alignment, priority);
 		this.mapIdToDisposable[id] = disposeable;
 	}
 
-	dispose(id: number) {
+	$dispose(id: number) {
 		let disposeable = this.mapIdToDisposable[id];
 		if (disposeable) {
 			disposeable.dispose();
