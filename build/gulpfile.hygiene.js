6d6d4d1acaf07741a6ad0d826e05d7bc22e527e1
Joao Moreno <jomo@microsoft.com>
2016-07-20 16:58:02 +0200

    add inno bitmaps

--- build/gulpfile.hygiene.js
+++ build/gulpfile.hygiene.js
@@ -25,7 +25,7 @@ var eolFilter = [
 	'!extensions/**/out/**',
 	'!**/node_modules/**',
 	'!**/fixtures/**',
-	'!**/*.{svg,exe,png,scpt,bat,cmd,cur,ttf,woff,eot}',
+	'!**/*.{svg,exe,png,bmp,scpt,bat,cmd,cur,ttf,woff,eot}',
 	'!build/{lib,tslintRules}/**/*.js',
 	'!build/monaco/**/*'
 ];
