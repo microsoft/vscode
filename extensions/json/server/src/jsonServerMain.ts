4b039398d75b1268950429d446aca705f5c3d672
Martin Aeschlimann <martinae@microsoft.com>
2016-07-22 17:32:10 +0200

    [json] set trigger characters

--- extensions/json/server/src/jsonServerMain.ts
+++ extensions/json/server/src/jsonServerMain.ts
@@ -61,7 +61,7 @@ connection.onInitialize((params: InitializeParams): InitializeResult => {
 		capabilities: {
 			// Tell the client that the server works in FULL text document sync mode
 			textDocumentSync: documents.syncKind,
-			completionProvider: { resolveProvider: true },
+			completionProvider: { resolveProvider: true, triggerCharacters: ['"', ':'] },
 			hoverProvider: true,
 			documentSymbolProvider: true,
 			documentRangeFormattingProvider: true,
