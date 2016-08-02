343ef8b7e25f050211263e38913b3e023874fc90
Martin Aeschlimann <martinae@microsoft.com>
2016-08-02 12:21:41 +0200

    [typescript] update grammar script improvements

--- extensions/typescript/build/update-grammars.js
+++ extensions/typescript/build/update-grammars.js
@@ -13,8 +13,11 @@ Promise.all([
 	download.toFile(contentBaseLocation + 'TypeScript.tmLanguage', './syntaxes/TypeScript.tmLanguage'),
 	download.toFile(contentBaseLocation + 'TypeScriptReact.tmLanguage', './syntaxes/TypeScriptReact.tmLanguage'),
 	download.toString(lastCommit).then(function (content) {
-		fs.writeFileSync('./syntaxes/grammar-version.txt', JSON.parse(content).object.url);
-		console.log('Update completed.')
+		var commitInfo = JSON.parse(content);
+		fs.writeFileSync('./syntaxes/grammar-version.txt', commitInfo.object.url);
+		console.log('Update completed.');
+		console.log('New commit comment:');
+		console.log('[typescript] update grammar (Microsoft/TypeScript-TmLanguage@' + commitInfo.object.sha.substr(0, 7) + ')');
 	}, console.error)
 ]);
 
