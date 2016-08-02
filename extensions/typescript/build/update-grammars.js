fdcbf91f27133613759ab0a2121f1f77e472d728
Martin Aeschlimann <martinae@microsoft.com>
2016-08-02 17:20:04 +0200

    [typescript] evolve update script, emit grammar as JSON

--- extensions/typescript/build/update-grammars.js
+++ extensions/typescript/build/update-grammars.js
@@ -6,20 +6,60 @@
 
 var download = require('../../../build/lib/download');
 var fs = require('fs');
+var plist = require('plist');
 
 var contentBaseLocation = 'https://raw.githubusercontent.com/Microsoft/TypeScript-TmLanguage/master/';
+
 var lastCommit = 'https://api.github.com/repos/Microsoft/TypeScript-TmLanguage/git/refs/heads/master';
-Promise.all([
-	download.toFile(contentBaseLocation + 'TypeScript.tmLanguage', './syntaxes/TypeScript.tmLanguage'),
-	download.toFile(contentBaseLocation + 'TypeScriptReact.tmLanguage', './syntaxes/TypeScriptReact.tmLanguage'),
-	download.toString(lastCommit).then(function (content) {
-		var commitInfo = JSON.parse(content);
-		fs.writeFileSync('./syntaxes/grammar-version.txt', commitInfo.object.url);
-		console.log('Update completed.');
-		console.log('New commit comment:');
-		console.log('[typescript] update grammar (Microsoft/TypeScript-TmLanguage@' + commitInfo.object.sha.substr(0, 7) + ')');
-	}, console.error)
-]);
+download.toString(lastCommit).then(function (content) {
+	var commitSha = JSON.parse(content).object.sha;
+	function writeJSON(fileName, modifyGrammar) {
+		return function(content) {
+			var grammar = plist.parse(content);
+			grammar.version = 'https://github.com/Microsoft/TypeScript-TmLanguage/commit/' + commitSha;
+			if (modifyGrammar) {
+				modifyGrammar(grammar);
+			}
+			fs.writeFileSync(fileName, JSON.stringify(grammar, null, '\t'));
+		}
+	}
+
+	return Promise.all([
+		download.toString(contentBaseLocation + 'TypeScript.tmLanguage').then(writeJSON('./syntaxes/TypeScript.tmLanguage.json'), console.error),
+		download.toString(contentBaseLocation + 'TypeScriptReact.tmLanguage').then(writeJSON('./syntaxes/TypeScriptReact.tmLanguage.json'), console.error),
+		download.toString(contentBaseLocation + 'TypeScriptReact.tmLanguage').then(writeJSON('../javascript/syntaxes/JavaScript.tmLanguage.json', adaptToJavaScript), console.error)
+	]).then(function() {
+		console.log('Update complete.');
+		console.log('[typescript] update grammar (Microsoft/TypeScript-TmLanguage@' + commitSha.substr(0, 7) + ')');
+	});
+}, console.error);
+
+function adaptToJavaScript(grammar) {
+	grammar.name = 'JavaScript (with React support)';
+	grammar.fileTypes = ['.js', '.jsx' ];
+	grammar.scopeName = 'source.js';
+
+	var fixScopeNames = function(rule) {
+		if (typeof rule.name === 'string') {
+			rule.name = rule.name.replace(/\.tsx/g, '.js');
+		}
+		for (var property in rule) {
+			var value = rule[property];
+			if (typeof value === 'object') {
+				fixScopeNames(value);
+			}
+		}
+	};
+
+	var repository = grammar.repository;
+	for (var key in repository) {
+		fixScopeNames(repository[key]);
+	}
+	// disable type parameters
+	if (repository['type-parameters']) {
+		repository['type-parameters']['begin'] = 'DO_NOT_MATCH';
+	}
+}
 
 
 
