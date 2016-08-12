cba389802259c25e4467a4ddd3017c76e456af52
Joao Moreno <jomo@microsoft.com>
2016-08-12 10:11:41 +0200

    telemetry: uninstalling -> installing

--- src/vs/workbench/parts/extensions/electron-browser/extensionsWorkbenchService.ts
+++ src/vs/workbench/parts/extensions/electron-browser/extensionsWorkbenchService.ts
@@ -307,7 +307,7 @@ export class ExtensionsWorkbenchService implements IExtensionsWorkbenchService {
 		extension.gallery = gallery;
 
 		const start = new Date();
-		const operation = Operation.Uninstalling;
+		const operation = Operation.Installing;
 		this.installing.push({ id: stripVersion(id), operation, extension, start });
 
 		this._onChange.fire();
