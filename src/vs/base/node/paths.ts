f80496d3966145f032fa03994d2d1a8099ac8715
Joao Moreno <jomo@microsoft.com>
2016-08-17 09:06:26 +0200

    revert paths.ts defaults

--- src/vs/base/node/paths.ts
+++ src/vs/base/node/paths.ts
@@ -4,40 +4,13 @@
  *--------------------------------------------------------------------------------------------*/
 
 import uri from 'vs/base/common/uri';
-import * as path from 'path';
-import * as os  from 'os';
 
 interface IPaths {
 	getAppDataPath(platform: string): string;
 	getUserDataPath(platform: string, appName: string, args: string[]): string;
 }
 
-function defaultGetAppDataPath(platform) {
-	switch (platform) {
-		case 'win32': return process.env['APPDATA'];
-		case 'darwin': return path.join(os.homedir(), 'Library', 'Application Support');
-		case 'linux': return process.env['XDG_CONFIG_HOME'] || path.join(os.homedir(), '.config');
-		default: throw new Error('Platform not supported');
-	}
-}
-
-function defaultGetUserDataPath(platform, appName) {
-	return path.join(getAppDataPath(platform), appName);
-}
-
-let _getAppDataPath: (platform: string) => string;
-let _getUserDataPath: (platform: string, appName: string, args: string[]) => string;
-
-try {
-	const pathsPath = uri.parse(require.toUrl('paths')).fsPath;
-	const paths = require.__$__nodeRequire<IPaths>(pathsPath);
-
-	_getAppDataPath = paths.getAppDataPath;
-	_getUserDataPath = paths.getUserDataPath;
-} catch (error) {
-	_getAppDataPath = (platform) => defaultGetAppDataPath(platform);
-	_getUserDataPath = (platform: string, appName: string, args: string[]) => defaultGetUserDataPath(platform, appName);
-}
-
-export const getAppDataPath = _getAppDataPath;
-export const getUserDataPath = _getUserDataPath;
+const pathsPath = uri.parse(require.toUrl('paths')).fsPath;
+const paths = require.__$__nodeRequire<IPaths>(pathsPath);
+export const getAppDataPath = paths.getAppDataPath;
+export const getUserDataPath = paths.getUserDataPath;
