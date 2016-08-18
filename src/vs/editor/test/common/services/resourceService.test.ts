4e2dfa6070689168f1d0c89adc2dff56e4e1a503
Alex Dima <alexdima@microsoft.com>
2016-08-18 16:06:13 +0200

    Clean up IResourceService

--- src/vs/editor/test/common/services/resourceService.test.ts
+++ src/vs/editor/test/common/services/resourceService.test.ts
@@ -7,72 +7,33 @@
 import * as assert from 'assert';
 import URI from 'vs/base/common/uri';
 import {createTestMirrorModelFromString} from 'vs/editor/common/model/mirrorModel';
-import {ResourceEvents} from 'vs/editor/common/services/resourceService';
 import {ResourceService} from 'vs/editor/common/services/resourceServiceImpl';
 
 suite('Editor Services - ResourceService', () => {
 
 	test('insert, remove, all', () => {
 
-		var service = new ResourceService();
+		let service = new ResourceService();
 
 		service.insert(URI.parse('test://1'), createTestMirrorModelFromString('hi'));
-		assert.equal(service.all().length, 1);
+		assert.equal(service.get(URI.parse('test://1')).getValue(), 'hi');
 
 		service.insert(URI.parse('test://2'), createTestMirrorModelFromString('hi'));
-		assert.equal(service.all().length, 2);
-
-		assert.ok(service.contains(URI.parse('test://1')));
-		assert.ok(service.contains(URI.parse('test://2')));
 
 		service.remove(URI.parse('test://1'));
 		service.remove(URI.parse('test://1'));
 		service.remove(URI.parse('test://2'));
-		assert.equal(service.all().length, 0);
+
+		assert.equal(service.get(URI.parse('test://1')), null);
 	});
 
+	test('inserting the same resource twice throws', () => {
+		let service = new ResourceService();
 
-	test('event - add, remove', () => {
+		service.insert(URI.parse('test://path/some-N1ce-name'), createTestMirrorModelFromString('hello'));
 
-		var eventCnt = 0;
-
-		var url = URI.parse('far');
-		var element = createTestMirrorModelFromString('hi');
-		var service = new ResourceService();
-		service.addListener2(ResourceEvents.ADDED, () => {
-			eventCnt++;
-			assert.ok(true);
+		assert.throws(() => {
+			service.insert(URI.parse('test://path/some-N1ce-name'), createTestMirrorModelFromString('hello again'));
 		});
-		service.addListener2(ResourceEvents.REMOVED, () => {
-			eventCnt++;
-			assert.ok(true);
-		});
-
-		service.insert(url, element);
-		service.remove(url);
-		assert.equal(eventCnt, 2, 'events');
 	});
-
-	test('event - propagation', () => {
-
-		var eventCnt = 0;
-
-		var url = URI.parse('far');
-		var element = createTestMirrorModelFromString('hi');
-		var event = {};
-
-		var service = new ResourceService();
-		service.insert(url, element);
-
-		service.addBulkListener2((events) => {
-			eventCnt++;
-			assert.equal(events.length, 1);
-			assert.equal(events[0].getData().originalEvents.length, 1);
-			assert.ok(events[0].getData().originalEvents[0].getData() === event);
-		});
-
-		element.emit('changed', event);
-		assert.equal(eventCnt, 1, 'events');
-	});
-
 });
