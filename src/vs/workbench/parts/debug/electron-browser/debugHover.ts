a6b5e06342193b86c4d6d3d555254d39527721c7
isidor <inikolic@microsoft.com>
2016-08-26 12:13:23 +0200

    debug hover: improve expression traversal
    fixes #10904

--- src/vs/workbench/parts/debug/electron-browser/debugHover.ts
+++ src/vs/workbench/parts/debug/electron-browser/debugHover.ts
@@ -5,7 +5,6 @@
 
 import lifecycle = require('vs/base/common/lifecycle');
 import {TPromise} from 'vs/base/common/winjs.base';
-import errors = require('vs/base/common/errors');
 import {CommonKeybindings} from 'vs/base/common/keyCodes';
 import dom = require('vs/base/browser/dom');
 import * as nls from 'vs/nls';
@@ -199,39 +198,31 @@ export class DebugHoverWidget implements editorbrowser.IContentWidget {
 		});
 	}
 
-	private findExpressionInStackFrame(namesToFind: string[]): TPromise<Expression> {
-		const focusedStackFrame = this.debugService.getViewModel().getFocusedStackFrame();
+	private doFindExpression(container: debug.IExpressionContainer, namesToFind: string[]): TPromise<debug.IExpression> {
+		return container.getChildren(this.debugService).then(children => {
+			// look for our variable in the list. First find the parents of the hovered variable if there are any.
+			// some languages pass the type as part of the name, so need to check if the last word of the name matches.
+			const filtered = children.filter(v => typeof v.name === 'string' && (namesToFind[0] === v.name || namesToFind[0] === v.name.substr(v.name.lastIndexOf(' ') + 1)));
+			if (filtered.length !== 1) {
+				return null;
+			}
 
-		const variables: debug.IExpression[] = [];
-		return focusedStackFrame.getScopes(this.debugService).then(scopes => {
+			if (namesToFind.length === 1) {
+				return filtered[0];
+			} else {
+				return this.doFindExpression(filtered[0], namesToFind.slice(1));
+			}
+		});
+	}
 
-			// flatten out scopes lists
-			return scopes.reduce((accum, scopes) => { return accum.concat(scopes); }, [])
-
+	private findExpressionInStackFrame(namesToFind: string[]): TPromise<debug.IExpression> {
+		return this.debugService.getViewModel().getFocusedStackFrame().getScopes(this.debugService)
 			// no expensive scopes
-			.filter((scope: debug.IScope) => !scope.expensive)
-
-			// get the scopes variables
-			.map((scope: debug.IScope) => scope.getChildren(this.debugService).done((children: debug.IExpression[]) => {
-
-				// look for our variable in the list. First find the parents of the hovered variable if there are any.
-				for (var i = 0; i < namesToFind.length && children; i++) {
-					// some languages pass the type as part of the name, so need to check if the last word of the name matches.
-					const filtered = children.filter(v => typeof v.name === 'string' && (namesToFind[i] === v.name || namesToFind[i] === v.name.substr(v.name.lastIndexOf(' ') + 1)));
-					if (filtered.length !== 1) {
-						break;
-					}
-
-					if (i === namesToFind.length - 1) {
-						variables.push(filtered[0]);
-					} else {
-						filtered[0].getChildren(this.debugService).done(c => children = c, children = null);
-					}
-				}
-			}, errors.onUnexpectedError));
-
-		// only show if there are no duplicates across scopes
-		}).then(() => variables.length === 1 ? TPromise.as(variables[0]) : TPromise.as(null));
+			.then(scopes => scopes.filter(scope => !scope.expensive))
+			.then(scopes => TPromise.join(scopes.map(scope => this.doFindExpression(scope, namesToFind))))
+			.then(expressions => expressions.filter(exp => !!exp))
+			// only show if there are no duplicates across scopes
+			.then(expressions => expressions.length === 1 ? expressions[0] : null);
 	}
 
 	private doShow(position: Position, expression: debug.IExpression, focus: boolean, forceValueHover = false): TPromise<void> {
