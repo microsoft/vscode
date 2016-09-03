188e480489e4cfc0712904b064075120c52ad9e1
heycalmdown <heyacct@gmail.com>
2016-09-03 21:00:43 +0900

    Added new command to switch between windows

--- src/vs/code/electron-main/windows.ts
+++ src/vs/code/electron-main/windows.ts
@@ -346,6 +346,15 @@ export class WindowsManager implements IWindowsService {
 			}
 		});
 
+		ipc.on('vscode:showWindow', (event, windowId: number) => {
+			this.logService.log('IPC#vscode:showWindow');
+
+			let vscodeWindow = this.getWindowById(windowId);
+			if (vscodeWindow) {
+				vscodeWindow.win.show();
+			}
+		});
+
 		ipc.on('vscode:setDocumentEdited', (event, windowId: number, edited: boolean) => {
 			this.logService.log('IPC#vscode:setDocumentEdited');
 
@@ -417,6 +426,14 @@ export class WindowsManager implements IWindowsService {
 			if (windowOnExtension) {
 				windowOnExtension.win.close();
 			}
+		});
+
+		ipc.on('vscode:requestWindows', (event, windowId: number) => {
+			const windows = this.getWindows();
+			const window = this.getWindowById(windowId);
+			window.send('vscode:respondWindows', windows.map(w => {
+				return {path: w.openedWorkspacePath, id: w.id};
+			}));
 		});
 
 		this.updateService.on('update-downloaded', (update: IUpdate) => {
