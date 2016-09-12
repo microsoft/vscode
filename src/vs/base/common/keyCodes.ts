c78077bb582a3c24b1776070bfe02e01adfa237b
Alex Dima <alexdima@microsoft.com>
2016-09-12 21:51:34 +0200

    mark KeyCode enum as const

--- src/vs/base/common/keyCodes.ts
+++ src/vs/base/common/keyCodes.ts
@@ -19,181 +19,182 @@ export interface ISimplifiedPlatform {
  * Inspired somewhat from https://msdn.microsoft.com/en-us/library/windows/desktop/dd375731(v=vs.85).aspx
  * But these are "more general", as they should work across browsers & OS`s.
  */
-export enum KeyCode {
+export const enum KeyCode {
 	/**
 	 * Placed first to cover the 0 value of the enum.
 	 */
-	Unknown,
+	Unknown = 0,
 
-	Backspace,
-	Tab,
-	Enter,
-	Shift,
-	Ctrl,
-	Alt,
-	PauseBreak,
-	CapsLock,
-	Escape,
-	Space,
-	PageUp,
-	PageDown,
-	End,
-	Home,
-	LeftArrow,
-	UpArrow,
-	RightArrow,
-	DownArrow,
-	Insert,
-	Delete,
+	Backspace = 1,
+	Tab = 2,
+	Enter = 3,
+	Shift = 4,
+	Ctrl = 5,
+	Alt = 6,
+	PauseBreak = 7,
+	CapsLock = 8,
+	Escape = 9,
+	Space = 10,
+	PageUp = 11,
+	PageDown = 12,
+	End = 13,
+	Home = 14,
+	LeftArrow = 15,
+	UpArrow = 16,
+	RightArrow = 17,
+	DownArrow = 18,
+	Insert = 19,
+	Delete = 20,
 
-	KEY_0,
-	KEY_1,
-	KEY_2,
-	KEY_3,
-	KEY_4,
-	KEY_5,
-	KEY_6,
-	KEY_7,
-	KEY_8,
-	KEY_9,
+	KEY_0 = 21,
+	KEY_1 = 22,
+	KEY_2 = 23,
+	KEY_3 = 24,
+	KEY_4 = 25,
+	KEY_5 = 26,
+	KEY_6 = 27,
+	KEY_7 = 28,
+	KEY_8 = 29,
+	KEY_9 = 30,
 
-	KEY_A,
-	KEY_B,
-	KEY_C,
-	KEY_D,
-	KEY_E,
-	KEY_F,
-	KEY_G,
-	KEY_H,
-	KEY_I,
-	KEY_J,
-	KEY_K,
-	KEY_L,
-	KEY_M,
-	KEY_N,
-	KEY_O,
-	KEY_P,
-	KEY_Q,
-	KEY_R,
-	KEY_S,
-	KEY_T,
-	KEY_U,
-	KEY_V,
-	KEY_W,
-	KEY_X,
-	KEY_Y,
-	KEY_Z,
+	KEY_A = 31,
+	KEY_B = 32,
+	KEY_C = 33,
+	KEY_D = 34,
+	KEY_E = 35,
+	KEY_F = 36,
+	KEY_G = 37,
+	KEY_H = 38,
+	KEY_I = 39,
+	KEY_J = 40,
+	KEY_K = 41,
+	KEY_L = 42,
+	KEY_M = 43,
+	KEY_N = 44,
+	KEY_O = 45,
+	KEY_P = 46,
+	KEY_Q = 47,
+	KEY_R = 48,
+	KEY_S = 49,
+	KEY_T = 50,
+	KEY_U = 51,
+	KEY_V = 52,
+	KEY_W = 53,
+	KEY_X = 54,
+	KEY_Y = 55,
+	KEY_Z = 56,
 
-	Meta,
-	ContextMenu,
+	Meta = 57,
+	ContextMenu = 58,
 
-	F1,
-	F2,
-	F3,
-	F4,
-	F5,
-	F6,
-	F7,
-	F8,
-	F9,
-	F10,
-	F11,
-	F12,
-	F13,
-	F14,
-	F15,
-	F16,
-	F17,
-	F18,
-	F19,
+	F1 = 59,
+	F2 = 60,
+	F3 = 61,
+	F4 = 62,
+	F5 = 63,
+	F6 = 64,
+	F7 = 65,
+	F8 = 66,
+	F9 = 67,
+	F10 = 68,
+	F11 = 69,
+	F12 = 70,
+	F13 = 71,
+	F14 = 72,
+	F15 = 73,
+	F16 = 74,
+	F17 = 75,
+	F18 = 76,
+	F19 = 77,
 
-	NumLock,
-	ScrollLock,
+	NumLock = 78,
+	ScrollLock = 79,
 
 	/**
 	 * Used for miscellaneous characters; it can vary by keyboard.
 	 * For the US standard keyboard, the ';:' key
 	 */
-	US_SEMICOLON,
+	US_SEMICOLON = 80,
 	/**
 	 * For any country/region, the '+' key
 	 * For the US standard keyboard, the '=+' key
 	 */
-	US_EQUAL,
+	US_EQUAL = 81,
 	/**
 	 * For any country/region, the ',' key
 	 * For the US standard keyboard, the ',<' key
 	 */
-	US_COMMA,
+	US_COMMA = 82,
 	/**
 	 * For any country/region, the '-' key
 	 * For the US standard keyboard, the '-_' key
 	 */
-	US_MINUS,
+	US_MINUS = 83,
 	/**
 	 * For any country/region, the '.' key
 	 * For the US standard keyboard, the '.>' key
 	 */
-	US_DOT,
+	US_DOT = 84,
 	/**
 	 * Used for miscellaneous characters; it can vary by keyboard.
 	 * For the US standard keyboard, the '/?' key
 	 */
-	US_SLASH,
+	US_SLASH = 85,
 	/**
 	 * Used for miscellaneous characters; it can vary by keyboard.
 	 * For the US standard keyboard, the '`~' key
 	 */
-	US_BACKTICK,
+	US_BACKTICK = 86,
 	/**
 	 * Used for miscellaneous characters; it can vary by keyboard.
 	 * For the US standard keyboard, the '[{' key
 	 */
-	US_OPEN_SQUARE_BRACKET,
+	US_OPEN_SQUARE_BRACKET = 87,
 	/**
 	 * Used for miscellaneous characters; it can vary by keyboard.
 	 * For the US standard keyboard, the '\|' key
 	 */
-	US_BACKSLASH,
+	US_BACKSLASH = 88,
 	/**
 	 * Used for miscellaneous characters; it can vary by keyboard.
 	 * For the US standard keyboard, the ']}' key
 	 */
-	US_CLOSE_SQUARE_BRACKET,
+	US_CLOSE_SQUARE_BRACKET = 89,
 	/**
 	 * Used for miscellaneous characters; it can vary by keyboard.
 	 * For the US standard keyboard, the ''"' key
 	 */
-	US_QUOTE,
+	US_QUOTE = 90,
 	/**
 	 * Used for miscellaneous characters; it can vary by keyboard.
 	 */
-	OEM_8,
+	OEM_8 = 91,
 	/**
 	 * Either the angle bracket key or the backslash key on the RT 102-key keyboard.
 	 */
-	OEM_102,
+	OEM_102 = 92,
 
-	NUMPAD_0, // VK_NUMPAD0, 0x60, Numeric keypad 0 key
-	NUMPAD_1, // VK_NUMPAD1, 0x61, Numeric keypad 1 key
-	NUMPAD_2, // VK_NUMPAD2, 0x62, Numeric keypad 2 key
-	NUMPAD_3, // VK_NUMPAD3, 0x63, Numeric keypad 3 key
-	NUMPAD_4, // VK_NUMPAD4, 0x64, Numeric keypad 4 key
-	NUMPAD_5, // VK_NUMPAD5, 0x65, Numeric keypad 5 key
-	NUMPAD_6, // VK_NUMPAD6, 0x66, Numeric keypad 6 key
-	NUMPAD_7, // VK_NUMPAD7, 0x67, Numeric keypad 7 key
-	NUMPAD_8, // VK_NUMPAD8, 0x68, Numeric keypad 8 key
-	NUMPAD_9, // VK_NUMPAD9, 0x69, Numeric keypad 9 key
+	NUMPAD_0 = 93, // VK_NUMPAD0, 0x60, Numeric keypad 0 key
+	NUMPAD_1 = 94, // VK_NUMPAD1, 0x61, Numeric keypad 1 key
+	NUMPAD_2 = 95, // VK_NUMPAD2, 0x62, Numeric keypad 2 key
+	NUMPAD_3 = 96, // VK_NUMPAD3, 0x63, Numeric keypad 3 key
+	NUMPAD_4 = 97, // VK_NUMPAD4, 0x64, Numeric keypad 4 key
+	NUMPAD_5 = 98, // VK_NUMPAD5, 0x65, Numeric keypad 5 key
+	NUMPAD_6 = 99, // VK_NUMPAD6, 0x66, Numeric keypad 6 key
+	NUMPAD_7 = 100, // VK_NUMPAD7, 0x67, Numeric keypad 7 key
+	NUMPAD_8 = 101, // VK_NUMPAD8, 0x68, Numeric keypad 8 key
+	NUMPAD_9 = 102, // VK_NUMPAD9, 0x69, Numeric keypad 9 key
 
-	NUMPAD_MULTIPLY,	// VK_MULTIPLY, 0x6A, Multiply key
-	NUMPAD_ADD,			// VK_ADD, 0x6B, Add key
-	NUMPAD_SEPARATOR,	// VK_SEPARATOR, 0x6C, Separator key
-	NUMPAD_SUBTRACT,	// VK_SUBTRACT, 0x6D, Subtract key
-	NUMPAD_DECIMAL,		// VK_DECIMAL, 0x6E, Decimal key
-	NUMPAD_DIVIDE,		// VK_DIVIDE, 0x6F,
+	NUMPAD_MULTIPLY = 103,	// VK_MULTIPLY, 0x6A, Multiply key
+	NUMPAD_ADD = 104,		// VK_ADD, 0x6B, Add key
+	NUMPAD_SEPARATOR = 105,	// VK_SEPARATOR, 0x6C, Separator key
+	NUMPAD_SUBTRACT = 106,	// VK_SUBTRACT, 0x6D, Subtract key
+	NUMPAD_DECIMAL = 107,	// VK_DECIMAL, 0x6E, Decimal key
+	NUMPAD_DIVIDE = 108,	// VK_DIVIDE, 0x6F,
 
 	/**
 	 * Placed last to cover the length of the enum.
+	 * Please do not depend on this value!
 	 */
 	MAX_VALUE
 }
@@ -403,7 +404,7 @@ let USER_SETTINGS = createMapping((TO_USER_SETTINGS_MAP) => {
 	FROM_USER_SETTINGS_MAP['OEM_102'] = KeyCode.OEM_102;
 });
 
-export namespace KeyCode {
+export namespace KeyCodeUtils {
 	export function toString(key:KeyCode): string {
 		return STRING.fromKeyCode(key);
 	}
@@ -797,7 +798,7 @@ export class ElectronAcceleratorLabelProvider implements IKeyBindingLabelProvide
 				return 'Right';
 		}
 
-		return KeyCode.toString(keyCode);
+		return KeyCodeUtils.toString(keyCode);
 	}
 }
 
@@ -831,7 +832,7 @@ export class MacUIKeyLabelProvider implements IKeyBindingLabelProvider {
 				return MacUIKeyLabelProvider.downArrowUnicodeLabel;
 		}
 
-		return KeyCode.toString(keyCode);
+		return KeyCodeUtils.toString(keyCode);
 	}
 }
 
@@ -849,7 +850,7 @@ export class AriaKeyLabelProvider implements IKeyBindingLabelProvider {
 	public modifierSeparator = '+';
 
 	public getLabelForKey(keyCode:KeyCode): string {
-		return KeyCode.toString(keyCode);
+		return KeyCodeUtils.toString(keyCode);
 	}
 }
 
@@ -867,7 +868,7 @@ export class ClassicUIKeyLabelProvider implements IKeyBindingLabelProvider {
 	public modifierSeparator = '+';
 
 	public getLabelForKey(keyCode:KeyCode): string {
-		return KeyCode.toString(keyCode);
+		return KeyCodeUtils.toString(keyCode);
 	}
 }
 
