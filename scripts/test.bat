fce36ac68d48ee686ccc7a1a9f155d490809532b
Benjamin Pasero <benjpas@microsoft.com>
2016-09-13 20:29:17 +0200

    fix tfs builds?

--- scripts/test.bat
+++ scripts/test.bat
@@ -3,6 +3,11 @@ setlocal
 
 set ELECTRON_RUN_AS_NODE=1
 
+-rem TFS Builds
+if not "%BUILD_BUILDID%" == "" (
+	set ELECTRON_NO_ATTACH_CONSOLE=1
+)
+
 pushd %~dp0\..
 
 for /f "tokens=2 delims=:," %%a in ('findstr /R /C:"\"nameShort\":.*" product.json') do set NAMESHORT=%%~a
