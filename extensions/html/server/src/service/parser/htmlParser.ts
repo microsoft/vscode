06727ce17498f3b1285a09a9537f56e282df7d1e
Martin Aeschlimann <martinae@microsoft.com>
2016-09-13 10:03:12 +0200

    [html] add endTagStart to Node

--- extensions/html/server/src/service/parser/htmlParser.ts
+++ extensions/html/server/src/service/parser/htmlParser.ts
@@ -11,10 +11,12 @@ import { isEmptyElement, isSameTag } from './htmlTags';
 export class Node {
 	public tag: string;
 	public closed: boolean;
+	public endTagStart: number;
 	constructor(public start: number, public end: number, public children: Node[], public parent: Node) {
-		this.closed = false;
+
 	}
-	private get lastChild(): Node { return this.children.length ? this.children[this.children.length - 1] : void 0; }
+	public get firstChild(): Node { return this.children[0]; }
+	public get lastChild(): Node { return this.children.length ? this.children[this.children.length - 1] : void 0; }
 
 	public findNodeBefore(offset:number) : Node {
 		let idx = findFirst(this.children, c => offset <= c.start) - 1;
@@ -33,11 +35,23 @@ export class Node {
 		}
 		return this;
 	}
+
+	public findNodeAt(offset:number) : Node {
+		let idx = findFirst(this.children, c => offset <= c.start) - 1;
+		if (idx >= 0) {
+			let child = this.children[idx];
+			if (offset > child.start && offset <= child.end) {
+				return child.findNodeBefore(offset);
+			}
+		}
+		return this;
+	}
 }
 
 export interface HTMLDocument {
 	roots: Node[];
 	findNodeBefore(offset:number) : Node;
+	findNodeAt(offset:number) : Node;
 }
 
 export function parse(text: string) : HTMLDocument {
@@ -76,13 +90,16 @@ export function parse(text: string) : HTMLDocument {
 				}
 				if (curr !== htmlDocument) {
 					curr.closed = true;
+					curr.endTagStart = endTagStart;
 				}
 				break;
 			case TokenType.StartTagSelfClose:
 				if (curr !== htmlDocument) {
 					curr.closed = true;
+					curr.end = scanner.getTokenEnd();
+					curr = curr.parent;
 				}
-				// fallthrough
+				break;
 			case TokenType.EndTagClose:
 				if (curr !== htmlDocument) {
 					curr.end = scanner.getTokenEnd();
@@ -94,13 +111,13 @@ export function parse(text: string) : HTMLDocument {
 	}
 	while (curr !== htmlDocument) {
 		curr.end = text.length;
+		curr.closed = false;
 		curr = curr.parent;
 	}
 	return {
 		roots: htmlDocument.children,
-		findNodeBefore: (offset:number) => {
-			return htmlDocument.findNodeBefore(offset);
-		}
+		findNodeBefore: htmlDocument.findNodeBefore.bind(htmlDocument),
+		findNodeAt: htmlDocument.findNodeAt.bind(htmlDocument)
 	};
 
 }
