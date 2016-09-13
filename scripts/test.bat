260d81e13eaecf1b25e7eccc7ca6e93609172f16
Benjamin Pasero <benjpas@microsoft.com>
2016-09-13 19:31:24 +0200

    remove some now obsolete ELECTRON_NO_ATTACH_CONSOLE

--- scripts/test.bat
+++ scripts/test.bat
@@ -3,16 +3,6 @@ setlocal
 
 set ELECTRON_RUN_AS_NODE=1
 
-rem TFS Builds
-if not "%BUILD_BUILDID%" == "" (
-	set ELECTRON_NO_ATTACH_CONSOLE=1
-)
-
-rem APPVEYOR Builds
-if not "%APPVEYOR%" == "" (
-	set ELECTRON_NO_ATTACH_CONSOLE=1
-)
-
 pushd %~dp0\..
 
 for /f "tokens=2 delims=:," %%a in ('findstr /R /C:"\"nameShort\":.*" product.json') do set NAMESHORT=%%~a
