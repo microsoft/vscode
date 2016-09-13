f941761db35776192ced804553fb7259c6710460
Benjamin Pasero <benjpas@microsoft.com>
2016-09-13 19:51:52 +0200

    unset ELECTRON_RUN_AS_NODE for appveyor

--- scripts/test-integration.bat
+++ scripts/test-integration.bat
@@ -1,6 +1,10 @@
 @echo off
 setlocal
 
+if not "%APPVEYOR%" == "" (
+	set ELECTRON_RUN_AS_NODE=
+)
+
 :: Integration Tests
 .\scripts\code.bat %~dp0\..\extensions\vscode-api-tests\testWorkspace --extensionDevelopmentPath=%~dp0\..\extensions\vscode-api-tests --extensionTestsPath=%~dp0\..\extensions\vscode-api-tests\out
 .\scripts\code.bat %~dp0\..\extensions\vscode-colorize-tests\test --extensionDevelopmentPath=%~dp0\..\extensions\vscode-colorize-tests --extensionTestsPath=%~dp0\..\extensions\vscode-colorize-tests\out
