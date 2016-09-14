76d1655c9948b2a4eccf0872c65c899e96129915
Benjamin Pasero <benjpas@microsoft.com>
2016-09-14 06:33:19 +0200

    fix build?

--- scripts/test.bat
+++ scripts/test.bat
@@ -3,11 +3,6 @@ setlocal
 
 set ELECTRON_RUN_AS_NODE=1
 
-rem TFS Builds
-if not "%BUILD_BUILDID%" == "" (
-	set ELECTRON_NO_ATTACH_CONSOLE=1
-)
-
 pushd %~dp0\..
 
 for /f "tokens=2 delims=:," %%a in ('findstr /R /C:"\"nameShort\":.*" product.json') do set NAMESHORT=%%~a
@@ -15,7 +10,7 @@ set NAMESHORT=%NAMESHORT: "=%
 set NAMESHORT=%NAMESHORT:"=%.exe
 set CODE=".build\electron\%NAMESHORT%"
 
-%CODE% .\node_modules\mocha\bin\_mocha %* | .\resources\win32\bin\cat
+%CODE% .\node_modules\mocha\bin\_mocha %*
 popd
 
 endlocal
