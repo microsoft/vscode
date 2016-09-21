9b64888181d1d00c8364d34c8244868e26fcfe27
Denis Malinochkin <dmalinochkin@rambler.ru>
2016-09-21 15:40:11 +0300

    Remove extra condition

--- src/vs/workbench/parts/emmet/node/editorAccessor.ts
+++ src/vs/workbench/parts/emmet/node/editorAccessor.ts
@@ -172,8 +172,7 @@ export class EditorAccessor implements emmet.Editor {
 			return syntax;
 		}
 		let languages = languageGrammar.split('.');
-		let thisLanguage = languages[languages.length - 1];
-		if (syntax !== thisLanguage || languages.length < 2) {
+		if (languages.length < 2) {
 			return syntax;
 		}
 		for (let i = 1; i < languages.length; i++) {
