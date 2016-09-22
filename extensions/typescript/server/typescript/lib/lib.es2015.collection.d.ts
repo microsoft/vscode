750550f452b8fcfd97a5cebaeb5bcd583953f0ec
Dirk Baeumer <dirkb@microsoft.com>
2016-09-22 22:29:31 +0200

    Upgrade to TS 2.0.3

--- extensions/typescript/server/typescript/lib/lib.es2015.collection.d.ts
+++ extensions/typescript/server/typescript/lib/lib.es2015.collection.d.ts
@@ -32,7 +32,6 @@ interface MapConstructor {
 declare var Map: MapConstructor;
 
 interface WeakMap<K, V> {
-    clear(): void;
     delete(key: K): boolean;
     get(key: K): V | undefined;
     has(key: K): boolean;
@@ -64,7 +63,6 @@ declare var Set: SetConstructor;
 
 interface WeakSet<T> {
     add(value: T): this;
-    clear(): void;
     delete(value: T): boolean;
     has(value: T): boolean;
 }
