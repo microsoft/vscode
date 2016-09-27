9cd85e66924a8ce8f4fb3c7a1d5e51b1352d8e31
Benjamin Pasero <benjpas@microsoft.com>
2016-09-27 20:23:20 +0200

    fix #12802

--- src/vs/workbench/electron-browser/integration.ts
+++ src/vs/workbench/electron-browser/integration.ts
@@ -6,36 +6,37 @@
 'use strict';
 
 import nls = require('vs/nls');
-import {TPromise} from 'vs/base/common/winjs.base';
+import { TPromise } from 'vs/base/common/winjs.base';
 import errors = require('vs/base/common/errors');
+import types = require('vs/base/common/types');
 import arrays = require('vs/base/common/arrays');
 import Severity from 'vs/base/common/severity';
-import {Separator} from 'vs/base/browser/ui/actionbar/actionbar';
-import {IAction, Action} from 'vs/base/common/actions';
-import {IPartService} from 'vs/workbench/services/part/common/partService';
-import {IMessageService} from 'vs/platform/message/common/message';
-import {IInstantiationService} from 'vs/platform/instantiation/common/instantiation';
-import {ITelemetryService} from 'vs/platform/telemetry/common/telemetry';
-import {IContextMenuService} from 'vs/platform/contextview/browser/contextView';
-import {ICommandService} from 'vs/platform/commands/common/commands';
-import {IKeybindingService} from 'vs/platform/keybinding/common/keybinding';
-import {IWorkspaceContextService}from 'vs/platform/workspace/common/workspace';
-import {IWindowService} from 'vs/workbench/services/window/electron-browser/windowService';
-import {AutoSaveConfiguration} from 'vs/platform/files/common/files';
-import {IConfigurationEditingService, ConfigurationTarget} from 'vs/workbench/services/configuration/common/configurationEditing';
-import {IWorkspaceConfigurationService} from 'vs/workbench/services/configuration/common/configuration';
-import {ElectronWindow} from 'vs/workbench/electron-browser/window';
+import { Separator } from 'vs/base/browser/ui/actionbar/actionbar';
+import { IAction, Action } from 'vs/base/common/actions';
+import { IPartService } from 'vs/workbench/services/part/common/partService';
+import { IMessageService } from 'vs/platform/message/common/message';
+import { IInstantiationService } from 'vs/platform/instantiation/common/instantiation';
+import { ITelemetryService } from 'vs/platform/telemetry/common/telemetry';
+import { IContextMenuService } from 'vs/platform/contextview/browser/contextView';
+import { ICommandService } from 'vs/platform/commands/common/commands';
+import { IKeybindingService } from 'vs/platform/keybinding/common/keybinding';
+import { IWorkspaceContextService } from 'vs/platform/workspace/common/workspace';
+import { IWindowService } from 'vs/workbench/services/window/electron-browser/windowService';
+import { AutoSaveConfiguration } from 'vs/platform/files/common/files';
+import { IConfigurationEditingService, ConfigurationTarget } from 'vs/workbench/services/configuration/common/configurationEditing';
+import { IWorkspaceConfigurationService } from 'vs/workbench/services/configuration/common/configuration';
+import { ElectronWindow } from 'vs/workbench/electron-browser/window';
 import * as browser from 'vs/base/browser/browser';
-import {DiffEditorInput, toDiffLabel} from 'vs/workbench/common/editor/diffEditorInput';
-import {Position} from 'vs/platform/editor/common/editor';
-import {EditorInput} from 'vs/workbench/common/editor';
-import {IPath, IOpenFileRequest, IWindowConfiguration} from 'vs/workbench/electron-browser/common';
-import {IResourceInput} from 'vs/platform/editor/common/editor';
-import {IWorkbenchEditorService} from 'vs/workbench/services/editor/common/editorService';
-import {IUntitledEditorService} from 'vs/workbench/services/untitled/common/untitledEditorService';
+import { DiffEditorInput, toDiffLabel } from 'vs/workbench/common/editor/diffEditorInput';
+import { Position } from 'vs/platform/editor/common/editor';
+import { EditorInput } from 'vs/workbench/common/editor';
+import { IPath, IOpenFileRequest, IWindowConfiguration } from 'vs/workbench/electron-browser/common';
+import { IResourceInput } from 'vs/platform/editor/common/editor';
+import { IWorkbenchEditorService } from 'vs/workbench/services/editor/common/editorService';
+import { IUntitledEditorService } from 'vs/workbench/services/untitled/common/untitledEditorService';
 import URI from 'vs/base/common/uri';
 
-import {ipcRenderer as ipc, webFrame, remote} from 'electron';
+import { ipcRenderer as ipc, webFrame, remote } from 'electron';
 
 const currentWindow = remote.getCurrentWindow();
 
@@ -283,13 +284,17 @@ export class ElectronIntegration {
 	}
 
 	private toggleAutoSave(): void {
-		const userAutoSaveConfig = this.configurationService.lookup(ElectronIntegration.AUTO_SAVE_SETTING).user;
+		const setting = this.configurationService.lookup(ElectronIntegration.AUTO_SAVE_SETTING);
+		let userAutoSaveConfig = setting.user;
+		if (types.isUndefinedOrNull(userAutoSaveConfig)) {
+			userAutoSaveConfig = setting.default; // use default if setting not defined
+		}
 
 		let newAutoSaveValue: string;
-		if (userAutoSaveConfig === AutoSaveConfiguration.OFF) {
-			newAutoSaveValue = AutoSaveConfiguration.AFTER_DELAY;
-		} else {
+		if ([AutoSaveConfiguration.AFTER_DELAY, AutoSaveConfiguration.ON_FOCUS_CHANGE, AutoSaveConfiguration.ON_WINDOW_CHANGE].some(s => s === userAutoSaveConfig)) {
 			newAutoSaveValue = AutoSaveConfiguration.OFF;
+		} else {
+			newAutoSaveValue = AutoSaveConfiguration.AFTER_DELAY;
 		}
 
 		this.configurationEditingService.writeConfiguration(ConfigurationTarget.USER, { key: ElectronIntegration.AUTO_SAVE_SETTING, value: newAutoSaveValue }).done(null, (error) => this.messageService.show(Severity.Error, error));
