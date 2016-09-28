96a643e63562e3467f824d8e683b138f7a0ffb29
Joao Moreno <jomo@microsoft.com>
2016-09-28 15:54:22 +0200

    fixes #12843

--- src/vs/platform/extensions/node/extensionValidator.ts
+++ src/vs/platform/extensions/node/extensionValidator.ts
@@ -194,10 +194,10 @@ export function isValidExtensionVersion(version: string, extensionDesc: IReduced
 		return true;
 	}
 
-	return validateVersions(version, extensionDesc.engines.vscode, notices);
+	return isVersionValid(version, extensionDesc.engines.vscode, notices);
 }
 
-export function validateVersions(currentVersion: string, requestedVersion: string, notices: string[]): boolean {
+export function isVersionValid(currentVersion: string, requestedVersion: string, notices: string[] = []): boolean {
 
 	let desiredVersion = normalizeVersion(parseVersion(requestedVersion));
 	if (!desiredVersion) {
