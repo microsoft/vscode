7c4ab7b51b23a5ff37cbc523086a8d2985441a14
Johannes Rieken <johannes.rieken@gmail.com>
2016-09-28 16:57:39 +0200

    Revert "take shortcut to fix #12111"
    This reverts commit ab6357b397481734e40edab607700c6421e5ea2f.

--- src/vs/editor/browser/services/codeEditorServiceImpl.ts
+++ src/vs/editor/browser/services/codeEditorServiceImpl.ts
@@ -6,6 +6,7 @@
 
 import * as objects from 'vs/base/common/objects';
 import * as strings from 'vs/base/common/strings';
+import URI from 'vs/base/common/uri';
 import * as dom from 'vs/base/browser/dom';
 import {IDecorationRenderOptions, IModelDecorationOptions, IModelDecorationOverviewRulerOptions, IThemeDecorationRenderOptions,
 	IContentDecorationRenderOptions, OverviewRulerLane, TrackedRangeStickiness} from 'vs/editor/common/editorCommon';
@@ -301,7 +302,7 @@ class DecorationRenderHelper {
 		if (typeof opts !== 'undefined') {
 			DecorationRenderHelper.collectBorderSettingsCSSText(opts, cssTextArr);
 			if (typeof opts.contentIconPath !== 'undefined') {
-				cssTextArr.push(strings.format(this._CSS_MAP.contentIconPath, opts.contentIconPath));
+				cssTextArr.push(strings.format(this._CSS_MAP.contentIconPath, URI.parse(opts.contentIconPath).toString()));
 			}
 			if (typeof opts.contentText !== 'undefined') {
 				let escaped = opts.contentText.replace(/\"/g, '\\\"');
@@ -323,7 +324,7 @@ class DecorationRenderHelper {
 		let cssTextArr = [];
 
 		if (typeof opts.gutterIconPath !== 'undefined') {
-			cssTextArr.push(strings.format(this._CSS_MAP.gutterIconPath, opts.gutterIconPath));
+			cssTextArr.push(strings.format(this._CSS_MAP.gutterIconPath, URI.parse(opts.gutterIconPath).toString()));
 			if (typeof opts.gutterIconSize !== 'undefined') {
 				cssTextArr.push(strings.format(this._CSS_MAP.gutterIconSize, opts.gutterIconSize));
 			}
