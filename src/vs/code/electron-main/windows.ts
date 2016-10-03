3e1efb6d5ae24d0489b9368f435321d70eeca95a
Benjamin Pasero <benjpas@microsoft.com>
2016-10-03 10:20:07 +0200

    Recent folders list changes when opening folder in extension debug window (fixes #11164)

--- src/vs/code/electron-main/windows.ts
+++ src/vs/code/electron-main/windows.ts
@@ -715,7 +715,7 @@ export class WindowsManager implements IWindowsService {
 
 		// Remember in recent document list (unless this opens for extension development)
 		// Also do not add paths when files are opened for diffing, only if opened individually
-		if (!openConfig.cli.extensionDevelopmentPath && !openConfig.cli.diff) {
+		if (!usedWindows.some(w => w.isPluginDevelopmentHost) && !openConfig.cli.diff) {
 			iPathsToOpen.forEach(iPath => {
 				if (iPath.filePath || iPath.workspacePath) {
 					app.addRecentDocument(iPath.filePath || iPath.workspacePath);
