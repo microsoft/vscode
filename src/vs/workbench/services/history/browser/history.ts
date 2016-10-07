0e930072174a0a51e04ec0c6b072e4d96fcb10b3
Benjamin Pasero <benjpas@microsoft.com>
2016-10-07 07:46:15 +0200

    history: avoid typed input for editor history (for #13283)

--- src/vs/workbench/services/history/browser/history.ts
+++ src/vs/workbench/services/history/browser/history.ts
@@ -70,9 +70,13 @@ export class EditorState {
 	}
 }
 
-interface ISerializedEditorInput {
+interface ILegacySerializedEditorInput {
 	id: string;
 	value: string;
+}
+
+interface ISerializedFileEditorInput {
+	resource: string;
 }
 
 export abstract class BaseHistoryService {
@@ -246,7 +250,7 @@ export class HistoryService extends BaseHistoryService implements IHistoryServic
 	private blockStackChanges: boolean;
 	private currentFileEditorState: EditorState;
 
-	private history: IEditorInput[];
+	private history: (IEditorInput|IResourceInput)[];
 	private recentlyClosedFiles: IRecentlyClosedFile[];
 	private loaded: boolean;
 	private registry: IEditorRegistry;
@@ -351,8 +355,7 @@ export class HistoryService extends BaseHistoryService implements IHistoryServic
 		if (entry.input instanceof EditorInput) {
 			openEditorPromise = this.editorService.openEditor(entry.input, options);
 		} else {
-			const resourceInput = entry.input as IResourceInput;
-			openEditorPromise = this.editorService.openEditor({ resource: resourceInput.resource, options });
+			openEditorPromise = this.editorService.openEditor({ resource: (entry.input as IResourceInput).resource, options });
 		}
 
 		openEditorPromise.done(() => {
@@ -382,37 +385,24 @@ export class HistoryService extends BaseHistoryService implements IHistoryServic
 
 		this.ensureLoaded();
 
+		const historyInput = this.preferResourceInput(input);
+
 		// Remove any existing entry and add to the beginning
 		this.removeFromHistory(input);
-		this.history.unshift(input);
+		this.history.unshift(historyInput);
 
 		// Respect max entries setting
 		if (this.history.length > HistoryService.MAX_HISTORY_ITEMS) {
 			this.history.pop();
 		}
 
-		// Restore on dispose
-		const onceDispose = once(input.onDispose);
-		onceDispose(() => {
-			this.restoreInHistory(input);
-		});
-	}
-
-	private restoreInHistory(input: IEditorInput): void {
-		const index = this.indexOf(input);
-		if (index < 0) {
-			return;
-		}
-
-		// Using the factory we try to recreate the input
-		const restoredInput = this.restoreInput(input);
-		if (restoredInput) {
-			this.history[index] = restoredInput;
-		}
-
-		// Factory failed, just remove entry then
-		else {
-			this.removeFromHistory(input, index);
+		// Remove this from the history unless the history input is a resource
+		// that can easily be restored even when the input gets disposed
+		if (historyInput instanceof EditorInput) {
+			const onceDispose = once(historyInput.onDispose);
+			onceDispose(() => {
+				this.removeFromHistory(input);
+			});
 		}
 	}
 
@@ -437,7 +427,7 @@ export class HistoryService extends BaseHistoryService implements IHistoryServic
 	private indexOf(input: IEditorInput): number {
 		for (let i = 0; i < this.history.length; i++) {
 			const entry = this.history[i];
-			if (entry.matches(input)) {
+			if (this.matches(input, entry)) {
 				return i;
 			}
 		}
@@ -577,20 +567,6 @@ export class HistoryService extends BaseHistoryService implements IHistoryServic
 		return s1.startLineNumber === s2.startLineNumber; // we consider the history entry same if we are on the same line
 	}
 
-	private restoreInput(input: IEditorInput): EditorInput {
-		if (input instanceof EditorInput) {
-			const factory = this.registry.getEditorInputFactory(input.getTypeId());
-			if (factory) {
-				const inputRaw = factory.serialize(input);
-				if (inputRaw) {
-					return factory.deserialize(this.instantiationService, inputRaw);
-				}
-			}
-		}
-
-		return null;
-	}
-
 	private removeFromStack(input: IEditorInput): void {
 		this.stack.forEach((e, i) => {
 			if (this.matches(input, e.input)) {
@@ -627,9 +603,7 @@ export class HistoryService extends BaseHistoryService implements IHistoryServic
 			return input.matches(typedInput);
 		}
 
-		const resourceInput = input as IResourceInput;
-
-		return this.matchesFile(resourceInput.resource, typedInput);
+		return this.matchesFile((input as IResourceInput).resource, typedInput);
 	}
 
 	private matchesFile(resource: URI, input: IEditorInput): boolean {
@@ -638,7 +612,7 @@ export class HistoryService extends BaseHistoryService implements IHistoryServic
 		return fileInput && fileInput.getResource().toString() === resource.toString();
 	}
 
-	public getHistory(): IEditorInput[] {
+	public getHistory(): (IEditorInput|IResourceInput)[] {
 		this.ensureLoaded();
 
 		return this.history.slice(0);
@@ -657,35 +631,45 @@ export class HistoryService extends BaseHistoryService implements IHistoryServic
 			return; // nothing to save because history was not used
 		}
 
-		const entries: ISerializedEditorInput[] = this.history.map((input: EditorInput) => {
-			const factory = this.registry.getEditorInputFactory(input.getTypeId());
-			if (factory) {
-				const value = factory.serialize(input);
-				if (typeof value === 'string') {
-					return {
-						id: input.getTypeId(),
-						value: value
-					};
-				}
+		const entries: ISerializedFileEditorInput[] = this.history.map(input => {
+			if (input instanceof EditorInput) {
+				return void 0; // only file resource inputs are serializable currently
 			}
 
-			return void 0;
+			return { resource: (input as IResourceInput).resource.toString() };
 		}).filter(serialized => !!serialized);
 
 		this.storageService.store(HistoryService.STORAGE_KEY, JSON.stringify(entries), StorageScope.WORKSPACE);
 	}
 
 	private load(): void {
-		let entries: ISerializedEditorInput[] = [];
+		let entries: (ILegacySerializedEditorInput|ISerializedFileEditorInput)[] = [];
+
 		const entriesRaw = this.storageService.get(HistoryService.STORAGE_KEY, StorageScope.WORKSPACE);
 		if (entriesRaw) {
 			entries = JSON.parse(entriesRaw);
 		}
 
 		this.history = entries.map(entry => {
-			const factory = this.registry.getEditorInputFactory(entry.id);
-			if (factory && typeof entry.value === 'string') {
-				return factory.deserialize(this.instantiationService, entry.value);
+			const serializedLegacyInput = entry as ILegacySerializedEditorInput;
+			const serializedFileInput = entry as ISerializedFileEditorInput;
+
+			// Legacy support (TODO@Ben remove me - migration)
+			if (serializedLegacyInput.id) {
+				const factory = this.registry.getEditorInputFactory(serializedLegacyInput.id);
+				if (factory && typeof serializedLegacyInput.value === 'string') {
+					const fileInput = asFileEditorInput(factory.deserialize(this.instantiationService, serializedLegacyInput.value));
+					if (fileInput) {
+						return { resource: fileInput.getResource() } as IResourceInput;
+					}
+
+					return void 0;
+				}
+			}
+
+			// New resource input support
+			else if (serializedFileInput.resource) {
+				return { resource: URI.parse(serializedFileInput.resource) } as IResourceInput;
 			}
 
 			return void 0;
