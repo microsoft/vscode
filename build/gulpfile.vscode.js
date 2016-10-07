3383ef7de0828bf507b7c4592e83aecdbb12b9dd
isidor <inikolic@microsoft.com>
2016-10-07 13:47:54 +0200

    use node-debug version 1.6.9
    #13336

--- build/gulpfile.vscode.js
+++ build/gulpfile.vscode.js
@@ -39,7 +39,7 @@ const nodeModules = ['electron', 'original-fs']
 // Build
 
 const builtInExtensions = [
-	{ name: 'ms-vscode.node-debug', version: '1.6.8' },
+	{ name: 'ms-vscode.node-debug', version: '1.6.9' },
 	{ name: 'ms-vscode.node-debug2', version: '0.0.7' }
 ];
 
