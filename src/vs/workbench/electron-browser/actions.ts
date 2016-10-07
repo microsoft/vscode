c42fd3626bba30ad677ab5a0e6c1928373bdc850
Benjamin Pasero <benjpas@microsoft.com>
2016-10-07 10:38:29 +0200

    less noise when setting zoom

--- src/vs/workbench/electron-browser/actions.ts
+++ src/vs/workbench/electron-browser/actions.ts
@@ -224,17 +224,12 @@ export abstract class BaseZoomAction extends Action {
 			target = ConfigurationTarget.WORKSPACE;
 		}
 
-		const applyZoom = (error?: Error) => {
+		const applyZoom = () => {
 			webFrame.setZoomLevel(level);
 			browser.setZoomLevel(level); // Ensure others can listen to zoom level changes
-
-			// Inform user
-			if (error) {
-				this.messageService.show(Severity.Error, error);
-			}
 		};
 
-		this.configurationEditingService.writeConfiguration(target, { key: BaseZoomAction.SETTING_KEY, value: level }).done(() => applyZoom(), error => applyZoom(error));
+		this.configurationEditingService.writeConfiguration(target, { key: BaseZoomAction.SETTING_KEY, value: level }).done(() => applyZoom(), error => applyZoom());
 	}
 }
 
