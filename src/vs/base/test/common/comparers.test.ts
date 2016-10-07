6e52a9f082ab01dfcb0b4cdbaa5100903aa59a78
Joao Moreno <jomo@microsoft.com>
2016-10-07 16:02:59 +0200

    fixes #13298

---
+++ src/vs/base/test/common/comparers.test.ts
@@ -0,0 +1,21 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+'use strict';
+
+import { compareFileNames } from 'vs/base/common/Comparers';
+import * as assert from 'assert';
+
+suite('Comparers', () => {
+
+	test('compareFileNames', () => {
+
+		assert(compareFileNames('', '') === 0, 'empty should be equal');
+		assert(compareFileNames('abc', 'abc') === 0, 'equal names should be equal');
+		assert(compareFileNames('.abc', '.abc') === 0, 'equal full names should be equal');
+		assert(compareFileNames('.env', '.env.example') < 0);
+		assert(compareFileNames('.env.example', '.gitattributes') < 0);
+	});
+});
