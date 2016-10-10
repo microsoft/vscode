b10d024111ff4eaf8eca52eccb1639171e5c8edf
Wade Anderson <wade.anderson7@gmail.com>
2016-10-10 08:55:20 -0700

    Updated version mismatch Info message
    Per https://github.com/Microsoft/vscode/issues/13284#issuecomment-252186657

--- extensions/typescript/src/typescriptServiceClient.ts
+++ extensions/typescript/src/typescriptServiceClient.ts
@@ -407,7 +407,7 @@ export default class TypeScriptServiceClient implements ITypescriptServiceClient
 					}
 					if (tscVersion && tscVersion !== version) {
 						window.showInformationMessage<MyMessageItem>(
-							localize('versionMismatch', 'A version mismatch between the globally installed tsc compiler ({0}) and VS Code\'s language service ({1}) has been detected. This might result in inconsistent compile errors.', tscVersion, version),
+							localize('versionMismatch', 'Version mismatch! global tsc ({0}) != VS Code\'s language service ({1}). Inconsistent compile errors might occur', tscVersion, version),
 							{
 								title: localize('moreInformation', 'More Information'),
 								id: 1
