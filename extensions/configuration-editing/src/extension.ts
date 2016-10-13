079e3b740c740f824814a90097957e72351d04f0
isidor <inikolic@microsoft.com>
2016-10-13 18:04:09 +0200

    add decorations for launch.json
    fixes microsoft/vscode#13353

--- extensions/configuration-editing/src/extension.ts
+++ extensions/configuration-editing/src/extension.ts
@@ -7,12 +7,27 @@
 
 import * as vscode from 'vscode';
 import { getLocation } from 'jsonc-parser';
+import * as path from 'path';
+
+const decoration = vscode.window.createTextEditorDecorationType({
+	color: '#b1b1b1',
+	isWholeLine: true
+});
 
 export function activate(context) {
 
 	//keybindings.json command-suggestions
 	context.subscriptions.push(registerKeybindingsCompletions());
 
+	// launch.json decorations
+	context.subscriptions.push(vscode.window.onDidChangeActiveTextEditor(editor => updateLaunchJsonDecorations(editor), null, context.subscriptions));
+	context.subscriptions.push(vscode.workspace.onDidChangeTextDocument(event => {
+		if (vscode.window.activeTextEditor && event.document === vscode.window.activeTextEditor.document) {
+			console.log('hello there');
+			updateLaunchJsonDecorations(vscode.window.activeTextEditor);
+		}
+	}, null, context.subscriptions));
+	updateLaunchJsonDecorations(vscode.window.activeTextEditor);
 }
 
 function registerKeybindingsCompletions(): vscode.Disposable {
@@ -29,6 +44,22 @@ function registerKeybindingsCompletions(): vscode.Disposable {
 			}
 		}
 	});
+}
+
+function updateLaunchJsonDecorations(editor: vscode.TextEditor) {
+	if (!editor || path.basename(editor.document.fileName) !== 'launch.json') {
+		return;
+	}
+
+	const ranges = [];
+	for (let i = 0; i < editor.document.lineCount; i++) {
+		const line = editor.document.lineAt(i);
+		if (line.text.indexOf('\"version\"') >= 0 || line.text.indexOf('\"type\"') >= 0 || line.text.indexOf('\"request\"') >= 0) {
+			ranges.push(new vscode.Range(line.range.start, line.range.start));
+		}
+	}
+
+	editor.setDecorations(decoration, ranges);
 }
 
 function newCompletionItem(text: string, range: vscode.Range, documentation?: string) {
