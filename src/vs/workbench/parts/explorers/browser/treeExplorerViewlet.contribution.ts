0cc3fec4fc16af0232b20bba3b5841671ae5bf3c
Pine Wu <t-zhwu@microsoft.com>
2016-10-14 10:00:20 -0700

    Placeholder for toggle external actions

--- src/vs/workbench/parts/explorers/browser/treeExplorerViewlet.contribution.ts
+++ src/vs/workbench/parts/explorers/browser/treeExplorerViewlet.contribution.ts
@@ -1,6 +1,6 @@
 'use strict';
 
-import {ITreeExplorerService, TreeExplorerViewletService } from 'vs/workbench/parts/explorers/browser/treeExplorerService';
+import { ITreeExplorerService, TreeExplorerViewletService } from 'vs/workbench/parts/explorers/browser/treeExplorerService';
 import { registerSingleton } from 'vs/platform/instantiation/common/extensions';
 
 registerSingleton(ITreeExplorerService, TreeExplorerViewletService);
