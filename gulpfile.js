3624dccd927ad424ec53cc67af686b15c6956b2e
Alex Dima <alexdima@microsoft.com>
2016-10-20 15:13:58 +0200

    extract mixin task to build/gulpfile.mixin.js

--- gulpfile.js
+++ gulpfile.js
@@ -9,18 +9,10 @@
 require('events').EventEmitter.defaultMaxListeners = 100;
 
 const gulp = require('gulp');
-const json = require('gulp-json-editor');
-const buffer = require('gulp-buffer');
-const filter = require('gulp-filter');
 const mocha = require('gulp-mocha');
-const es = require('event-stream');
 const util = require('./build/lib/util');
-const remote = require('gulp-remote-src');
-const zip = require('gulp-vinyl-zip');
 const path = require('path');
-const assign = require('object-assign');
 const glob = require('glob');
-const pkg = require('./package.json');
 const compilation = require('./build/lib/compilation');
 
 // Fast compile for development time
@@ -50,63 +42,6 @@ gulp.task('test', function () {
 	return gulp.src('test/all.js')
 		.pipe(mocha({ ui: 'tdd', delay: true }))
 		.once('end', function () { process.exit(); });
-});
-
-gulp.task('mixin', function () {
-	const repo = process.env['VSCODE_MIXIN_REPO'];
-
-	if (!repo) {
-		console.log('Missing VSCODE_MIXIN_REPO, skipping mixin');
-		return;
-	}
-
-	const quality = process.env['VSCODE_QUALITY'];
-
-	if (!quality) {
-		console.log('Missing VSCODE_QUALITY, skipping mixin');
-		return;
-	}
-
-	const url = `https://github.com/${ repo }/archive/${ pkg.distro }.zip`;
-	const opts = { base: '' };
-	const username = process.env['VSCODE_MIXIN_USERNAME'];
-	const password = process.env['VSCODE_MIXIN_PASSWORD'];
-
-	if (username || password) {
-		opts.auth = { user: username || '', pass: password || '' };
-	}
-
-	console.log('Mixing in sources from \'' + url + '\':');
-
-	let all = remote(url, opts)
-		.pipe(zip.src())
-		.pipe(filter(function (f) { return !f.isDirectory(); }))
-		.pipe(util.rebase(1));
-
-	if (quality) {
-		const build = all.pipe(filter('build/**'));
-		const productJsonFilter = filter('product.json', { restore: true });
-
-		const mixin = all
-			.pipe(filter('quality/' + quality + '/**'))
-			.pipe(util.rebase(2))
-			.pipe(productJsonFilter)
-			.pipe(buffer())
-			.pipe(json(function (patch) {
-				const original = require('./product.json');
-				return assign(original, patch);
-			}))
-			.pipe(productJsonFilter.restore);
-
-		all = es.merge(build, mixin);
-	}
-
-	return all
-		.pipe(es.mapSync(function (f) {
-			console.log(f.relative);
-			return f;
-		}))
-		.pipe(gulp.dest('.'));
 });
 
 var ALL_EDITOR_TASKS = [
