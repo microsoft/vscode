f3f543519506806e10ed6b28eca9be127a464c32
Martin Aeschlimann <martinae@microsoft.com>
2016-10-20 10:58:18 +0200

    [html] completion proposals for embedded CSS (for #8928)

--- extensions/html/client/src/htmlMain.ts
+++ extensions/html/client/src/htmlMain.ts
@@ -6,12 +6,32 @@
 
 import * as path from 'path';
 
-import { languages, workspace, ExtensionContext, IndentAction } from 'vscode';
-import { LanguageClient, LanguageClientOptions, ServerOptions, TransportKind } from 'vscode-languageclient';
+import { languages, workspace, ExtensionContext, IndentAction, commands, Uri, CompletionList, EventEmitter } from 'vscode';
+import { LanguageClient, LanguageClientOptions, ServerOptions, TransportKind, Position, RequestType, Protocol2Code, Code2Protocol } from 'vscode-languageclient';
+import { CompletionList as LSCompletionList } from 'vscode-languageserver-types';
 import { EMPTY_ELEMENTS } from './htmlEmptyTagsShared';
 
 import * as nls from 'vscode-nls';
 let localize = nls.loadMessageBundle();
+
+interface EmbeddedCompletionParams {
+	uri: string;
+	embeddedLanguageId: string;
+	position: Position;
+}
+
+namespace EmbeddedCompletionRequest {
+	export const type: RequestType<EmbeddedCompletionParams, LSCompletionList, any> = { get method() { return 'embedded/completion'; } };
+}
+
+interface EmbeddedContentParams {
+	uri: string;
+	embeddedLanguageId: string;
+}
+
+namespace EmbeddedContentRequest {
+	export const type: RequestType<EmbeddedContentParams, string, any> = { get method() { return 'embedded/content'; } };
+}
 
 export function activate(context: ExtensionContext) {
 
@@ -43,11 +63,49 @@ export function activate(context: ExtensionContext) {
 	// Create the language client and start the client.
 	let client = new LanguageClient('html', localize('htmlserver.name', 'HTML Language Server'), serverOptions, clientOptions);
 
+	let embeddedContentChanged = new EventEmitter<Uri>();
+
+	client.onRequest(EmbeddedCompletionRequest.type, params => {
+		let position = Protocol2Code.asPosition(params.position);
+		let virtualURI = Uri.parse('html-embedded://' + params.embeddedLanguageId + '/' + encodeURIComponent(params.uri) + '.' + params.embeddedLanguageId);
+
+		embeddedContentChanged.fire(virtualURI);
+		return workspace.openTextDocument(virtualURI).then(_ => {
+			return commands.executeCommand<CompletionList>('vscode.executeCompletionItemProvider', virtualURI, position).then(completionList => {
+				if (completionList) {
+					return {
+						isIncomplete: completionList.isIncomplete,
+						items: completionList.items.map(Code2Protocol.asCompletionItem)
+					};
+				}
+				return { isIncomplete: true, items: [] };
+			}, error => {
+				return Promise.reject(error);
+			});
+		}, error => {
+			return Promise.reject(error);
+		});
+	});
+
 	let disposable = client.start();
 
 	// Push the disposable to the context's subscriptions so that the
 	// client can be deactivated on extension deactivation
 	context.subscriptions.push(disposable);
+
+
+	context.subscriptions.push(workspace.registerTextDocumentContentProvider('html-embedded', {
+		provideTextDocumentContent: (uri, ct) => {
+			if (uri.scheme === 'html-embedded') {
+				let languageId = uri.authority;
+				let path = uri.path.substring(1, uri.path.length - languageId.length - 1); // remove leading '/' and new file extension
+				let documentURI = decodeURIComponent(path);
+				return client.sendRequest(EmbeddedContentRequest.type, { uri: documentURI, embeddedLanguageId: languageId });
+			}
+			return '';
+		},
+		onDidChange: embeddedContentChanged.event
+	}));
 
 	languages.setLanguageConfiguration('html', {
 		wordPattern: /(-?\d*\.\d\w*)|([^\`\~\!\@\$\^\&\*\(\)\=\+\[\{\]\}\\\|\;\:\'\"\,\.\<\>\/\s]+)/g,
