c967ca33111a931cd5db253809d3405d6a47383c
Alex Dima <alexdima@microsoft.com>
2016-10-20 11:48:37 +0200

    Move /build/gulpfile.common to /build/lib/optimize.ts

--- build/gulpfile.editor.js
+++ build/gulpfile.editor.js
@@ -6,7 +6,7 @@
 var gulp = require('gulp');
 var path = require('path');
 var util = require('./lib/util');
-var common = require('./gulpfile.common');
+var common = require('./lib/optimize');
 var es = require('event-stream');
 var File = require('vinyl');
 
