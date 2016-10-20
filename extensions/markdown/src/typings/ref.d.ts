093cd8b2a336fe238d11442072f0ea28105c760d
Johannes Rieken <johannes.rieken@gmail.com>
2016-10-20 12:04:01 +0200

    enable integration tests again, cleanup a little of the ///-reference mess, #14059

--- extensions/markdown/src/typings/ref.d.ts
+++ extensions/markdown/src/typings/ref.d.ts
@@ -3,6 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+/// <reference path='../../../../src/vs/vscode.d.ts'/>
 /// <reference path='../../../declares.d.ts'/>
 /// <reference path='../../../lib.core.d.ts'/>
 /// <reference path='../../../node.d.ts'/>
