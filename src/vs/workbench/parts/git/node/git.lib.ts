fa0d68c4e84b9f7568700332f424aef5ff3d75c4
Joao Moreno <jomo@microsoft.com>
2016-10-24 15:43:15 +0200

    fixes #13297

--- src/vs/workbench/parts/git/node/git.lib.ts
+++ src/vs/workbench/parts/git/node/git.lib.ts
@@ -236,9 +236,8 @@ export class Git {
 			options.stdio = ['ignore', null, null]; // Unless provided, ignore stdin and leave default streams for stdout and stderr
 		}
 
-		options.env = assign({}, options.env || {});
-		options.env = assign(options.env, this.env);
-		options.env = assign(options.env, {
+		options.env = assign({}, options.env || {}, this.env, {
+			LANG: 'en_US.UTF-8',
 			VSCODE_GIT_REQUEST_ID: UUIDv4().asHex(),
 			MONACO_GIT_COMMAND: args[0]
 		});
