988968870172253522e61e63a3811fd660021676
Johannes Rieken <johannes.rieken@gmail.com>
2016-10-25 17:26:20 +0200

    Revert "escape from old rewrite logic by adding a stupid comment, #14378"
    This reverts commit 492fda671665d921516528ac3e36a34fb974fdb7.

--- src/vs/vscode.d.ts
+++ src/vs/vscode.d.ts
@@ -3,7 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-declare module 'vscode' { //
+declare module 'vscode' {
 
 	/**
 	 * The version of the editor.
