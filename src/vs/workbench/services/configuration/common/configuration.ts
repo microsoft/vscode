86453b54fb91c140793164f1291e9e6084f744c1
Benjamin Pasero <benjpas@microsoft.com>
2016-10-31 07:34:57 +0100

    config - add extensions.json too

--- src/vs/workbench/services/configuration/common/configuration.ts
+++ src/vs/workbench/services/configuration/common/configuration.ts
@@ -8,7 +8,7 @@ import { createDecorator } from 'vs/platform/instantiation/common/instantiation'
 
 export const CONFIG_DEFAULT_NAME = 'settings';
 export const WORKSPACE_CONFIG_FOLDER_DEFAULT_NAME = '.vscode';
-export const WORKSPACE_CONFIG_DEFAULT_PATH = '.vscode/settings.json';
+export const WORKSPACE_CONFIG_DEFAULT_PATH = `${WORKSPACE_CONFIG_FOLDER_DEFAULT_NAME}/${CONFIG_DEFAULT_NAME}.json`;
 
 export const IWorkspaceConfigurationService = createDecorator<IWorkspaceConfigurationService>('configurationService');
 
@@ -30,6 +30,7 @@ export interface IWorkspaceConfigurationValue<T> extends IConfigurationValue<T> 
 }
 
 export const WORKSPACE_STANDALONE_CONFIGURATIONS = {
-	'tasks': '.vscode/tasks.json',
-	'launch': '.vscode/launch.json'
+	'tasks': `${WORKSPACE_CONFIG_FOLDER_DEFAULT_NAME}/tasks.json`,
+	'launch': `${WORKSPACE_CONFIG_FOLDER_DEFAULT_NAME}/launch.json`,
+	'extensions': `${WORKSPACE_CONFIG_FOLDER_DEFAULT_NAME}/extensions.json`
 };
