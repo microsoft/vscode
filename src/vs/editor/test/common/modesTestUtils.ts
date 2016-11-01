3f6767f768da2163a4b0a0f9622a0515d4add150
Alex Dima <alexdima@microsoft.com>
2016-11-01 15:56:23 +0100

    Capture current behaviour of `notIn` (write tests)

--- src/vs/editor/test/common/modesTestUtils.ts
+++ src/vs/editor/test/common/modesTestUtils.ts
@@ -8,9 +8,33 @@ import { ModeTransition } from 'vs/editor/common/core/modeTransition';
 import { Token } from 'vs/editor/common/core/token';
 import { LineTokens } from 'vs/editor/common/core/lineTokens';
 import { TokensBinaryEncoding, TokensInflatorMap } from 'vs/editor/common/model/tokensBinaryEncoding';
+import { createScopedLineTokens, ScopedLineTokens } from 'vs/editor/common/modes/supports';
 
 export function createFakeLineTokens(line: string, modeId: string, tokens: Token[], modeTransitions: ModeTransition[]): LineTokens {
 	let map = new TokensInflatorMap(modeId);
 	let deflatedTokens = TokensBinaryEncoding.deflateArr(map, tokens);
 	return new LineTokens(map, deflatedTokens, modeTransitions, line);
+}
+
+export interface TokenText {
+	text: string;
+	type: string;
+}
+
+export function createFakeScopedLineTokens(modeId: string, tokens: TokenText[]): ScopedLineTokens {
+	return createScopedLineTokens(createLineContextFromTokenText(modeId, tokens), 0);
+}
+
+function createLineContextFromTokenText(modeId: string, tokens: TokenText[]): LineTokens {
+	let line = '';
+	let processedTokens: Token[] = [];
+
+	let indexSoFar = 0;
+	for (let i = 0; i < tokens.length; ++i) {
+		processedTokens.push(new Token(indexSoFar, tokens[i].type));
+		line += tokens[i].text;
+		indexSoFar += tokens[i].text.length;
+	}
+
+	return createFakeLineTokens(line, modeId, processedTokens, [new ModeTransition(0, modeId)]);
 }
