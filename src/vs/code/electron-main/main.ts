c8998fbf86aa109aab25ed9d85611953b10d5c13
Benjamin Pasero <benjpas@microsoft.com>
2016-11-02 09:39:09 +0100

    window title - show user home with ~ prefix

--- src/vs/code/electron-main/main.ts
+++ src/vs/code/electron-main/main.ts
@@ -423,7 +423,7 @@ function getShellEnvironment(): TPromise<platform.IProcessEnvironment> {
 }
 
 function createPaths(environmentService: IEnvironmentService): TPromise<any> {
-	const paths = [environmentService.appSettingsHome, environmentService.userHome, environmentService.extensionsPath];
+	const paths = [environmentService.appSettingsHome, environmentService.userProductHome, environmentService.extensionsPath];
 
 	return TPromise.join(paths.map(p => mkdirp(p))) as TPromise<any>;
 }
