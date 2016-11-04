2ad69077e33c966d3d3c9b06c561871d12c72ad1
Sandeep Somavarapu <sasomava@microsoft.com>
2016-11-04 17:32:18 +0100

    #14167 Fix replace part - Disable replace action - Do not remove the match to replace. Model update does it anyways - Update the matches for the line after replace

--- src/vs/workbench/parts/search/browser/searchActions.ts
+++ src/vs/workbench/parts/search/browser/searchActions.ts
@@ -380,6 +380,7 @@ export class ReplaceAction extends AbstractSearchAndReplaceAction {
 	}
 
 	public run(): TPromise<any> {
+		this.enabled = false;
 		this.telemetryService.publicLog('replace.action.selected');
 		let elementToFocus = this.getElementToFocusAfterRemoved(this.viewer, this.element);
 		let elementToShowReplacePreview = this.getElementToShowReplacePreview(elementToFocus);
@@ -388,7 +389,7 @@ export class ReplaceAction extends AbstractSearchAndReplaceAction {
 			if (elementToFocus) {
 				this.viewer.setFocus(elementToFocus);
 			}
-			this.viewer.DOMFocus();
+				this.viewer.DOMFocus();
 			if (!elementToShowReplacePreview || this.hasToOpenFile()) {
 				this.viewlet.open(this.element, true);
 			} else {
