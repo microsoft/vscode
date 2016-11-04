c4193ca557ac980b437ff0442fe4a0b7db55a46d
Benjamin Pasero <benjpas@microsoft.com>
2016-11-04 12:58:15 +0100

    comments

--- src/vs/platform/textmodelResolver/common/resolver.ts
+++ src/vs/platform/textmodelResolver/common/resolver.ts
@@ -16,12 +16,23 @@ export const ITextModelResolverService = createDecorator<ITextModelResolverServi
 export interface ITextModelResolverService {
 	_serviceBrand: any;
 
+	/**
+	 * Given a resource, tries to resolve a ITextEditorModel out of it. Will support many schemes like file://, untitled://,
+	 * inMemory:// and for anything else fall back to the model content provider registry.
+	 */
 	resolve(resource: URI): TPromise<ITextEditorModel>;
 
+	/**
+	 * For unknown resources, allows to register a content provider such as this service is able to resolve arbritrary
+	 * resources to ITextEditorModels.
+	 */
 	registerTextModelContentProvider(scheme: string, provider: ITextModelContentProvider): IDisposable;
 }
 
 export interface ITextModelContentProvider {
 
+	/**
+	 * Given a resource, return the content of the resource as IModel.
+	 */
 	provideTextContent(resource: URI): TPromise<IModel>;
 }
