749ac8a544a1ae4f3c16f300948a5d24f43d089d
Benjamin Pasero <benjpas@microsoft.com>
2016-11-04 17:03:53 +0100

    add more tests for TextModelResolverService

--- src/vs/workbench/services/textmodelResolver/test/textModelResolverService.test.ts
+++ src/vs/workbench/services/textmodelResolver/test/textModelResolverService.test.ts
@@ -12,16 +12,22 @@ import URI from 'vs/base/common/uri';
 import { ResourceEditorInput } from 'vs/workbench/common/editor/resourceEditorInput';
 import { ResourceEditorModel } from 'vs/workbench/common/editor/resourceEditorModel';
 import { IInstantiationService } from 'vs/platform/instantiation/common/instantiation';
-import { workbenchInstantiationService } from 'vs/test/utils/servicesTestUtils';
+import { workbenchInstantiationService, TestTextFileService, toResource } from 'vs/test/utils/servicesTestUtils';
 import { ITextModelResolverService } from 'vs/platform/textmodelResolver/common/resolver';
 import { IModelService } from 'vs/editor/common/services/modelService';
 import { IModeService } from 'vs/editor/common/services/modeService';
+import { TextFileEditorModel } from 'vs/workbench/services/textfile/common/textFileEditorModel';
+import { ITextFileService } from 'vs/workbench/services/textfile/common/textfiles';
+import { IUntitledEditorService } from 'vs/workbench/services/untitled/common/untitledEditorService';
+import { TextFileEditorModelManager } from 'vs/workbench/services/textfile/common/textFileEditorModelManager';
 
 class ServiceAccessor {
 	constructor(
 		@ITextModelResolverService public textModelResolverServie: ITextModelResolverService,
 		@IModelService public modelService: IModelService,
-		@IModeService public modeService: IModeService
+		@IModeService public modeService: IModeService,
+		@ITextFileService public textFileService: TestTextFileService,
+		@IUntitledEditorService public untitledEditorService: IUntitledEditorService
 	) {
 	}
 }
@@ -30,13 +36,24 @@ suite('Workbench - TextModelResolverService', () => {
 
 	let instantiationService: IInstantiationService;
 	let accessor: ServiceAccessor;
+	let model: TextFileEditorModel;
 
 	setup(() => {
 		instantiationService = workbenchInstantiationService();
 		accessor = instantiationService.createInstance(ServiceAccessor);
 	});
 
-	test('resolve', function (done) {
+	teardown(() => {
+		if (model) {
+			model.dispose();
+			model = void 0;
+		}
+		(<TextFileEditorModelManager>accessor.textFileService.models).clear();
+		(<TextFileEditorModelManager>accessor.textFileService.models).dispose();
+		accessor.untitledEditorService.revertAll();
+	});
+
+	test('resolve resource', function (done) {
 		const dispose = accessor.textModelResolverServie.registerTextModelContentProvider('test', {
 			provideTextContent: function (resource: URI): TPromise<IModel> {
 				if (resource.scheme === 'test') {
@@ -58,6 +75,38 @@ suite('Workbench - TextModelResolverService', () => {
 
 			dispose.dispose();
 			done();
+		});
+	});
+
+	test('resolve file', function (done) {
+		model = instantiationService.createInstance(TextFileEditorModel, toResource.call(this, '/path/file_resolver.txt'), 'utf8');
+		(<TextFileEditorModelManager>accessor.textFileService.models).add(model.getResource(), model);
+		model.load().then(() => {
+			accessor.textModelResolverServie.resolve(model.getResource()).then(model => {
+				const editorModel = model.textEditorModel as IModel;
+
+				assert.ok(editorModel);
+				assert.equal(editorModel.getValue(), 'Hello Html');
+
+				done();
+			});
+		});
+	});
+
+	test('resolve untitled', function (done) {
+		const service = accessor.untitledEditorService;
+		const input = service.createOrGet();
+
+		input.resolve().then(() => {
+			accessor.textModelResolverServie.resolve(input.getResource()).then(model => {
+				const editorModel = model.textEditorModel as IModel;
+
+				assert.ok(editorModel);
+
+				input.dispose();
+
+				done();
+			});
 		});
 	});
 });
