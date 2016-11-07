23e41cc6793cd8385ecf30700f67fc27328f0a4c
Benjamin Pasero <benjpas@microsoft.com>
2016-11-07 16:58:35 +0100

    Revert "inform mac users on title change"
    This reverts commit 35ee5c2719b5599d91bf90851c6de74983813646.

--- src/vs/workbench/browser/parts/titlebar/titlebarPart.ts
+++ src/vs/workbench/browser/parts/titlebar/titlebarPart.ts
@@ -6,16 +6,10 @@
 'use strict';
 
 import 'vs/css!./media/titlebarpart';
-import * as nls from 'vs/nls';
-import { TPromise } from 'vs/base/common/winjs.base';
 import { Builder, $, Dimension } from 'vs/base/browser/builder';
 import { Part } from 'vs/workbench/browser/part';
 import { ITitleService } from 'vs/workbench/services/title/common/titleService';
 import { getZoomFactor } from 'vs/base/browser/browser';
-import { Action } from 'vs/base/common/actions';
-import { IMessageService, Severity, IMessageWithAction } from 'vs/platform/message/common/message';
-import { IStorageService, StorageScope } from 'vs/platform/storage/common/storage';
-import { isMacintosh } from 'vs/base/common/platform';
 
 export class TitlebarPart extends Part implements ITitleService {
 
@@ -25,50 +19,6 @@ export class TitlebarPart extends Part implements ITitleService {
 	private title: Builder;
 	private pendingTitle: string;
 	private initialTitleFontSize: number;
-
-	constructor(
-		id: string,
-		@IMessageService private messageService: IMessageService,
-		@IStorageService private storageService: IStorageService
-	) {
-		super(id);
-
-		this.informUser(); // TODO@Ben remove me
-	}
-
-	private informUser(): void {
-		if (isMacintosh && !this.storageService.getBoolean('customTitleBarInformUser', StorageScope.GLOBAL)) {
-			this.storageService.store('customTitleBarInformUser', true, StorageScope.GLOBAL);
-
-			const okAction = new Action(
-				'customTitle.ok',
-				nls.localize('customTitle.ok', "OK"),
-				null,
-				true,
-				() => TPromise.as(true)
-			);
-
-			const moreInfoAction = new Action(
-				'customTitle.moreInfo',
-				nls.localize('customTitle.moreInfo', "More information"),
-				null,
-				true,
-				() => {
-					window.open('https://github.com/Microsoft/vscode/issues/15098');
-
-					return TPromise.as(true);
-				}
-			);
-
-			this.messageService.show(Severity.Info, <IMessageWithAction>{
-				message: nls.localize('customTitleBarInformUser', "We have enabled a new custom title bar style on Mac. The related setting is called **window.titleBarStyle**."),
-				actions: [
-					okAction,
-					moreInfoAction
-				]
-			});
-		}
-	}
 
 	public createContentArea(parent: Builder): Builder {
 		this.titleContainer = $(parent);
