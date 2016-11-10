97ca4bcb441d0f0e9eb0265bde3096c4a62b5da3
Pine Wu <t-zhwu@microsoft.com>
2016-11-10 11:21:17 -0800

    Fix mainThreadTreeExplorers layer breakage

--- src/vs/workbench/api/node/mainThreadTreeExplorers.ts
+++ src/vs/workbench/api/node/mainThreadTreeExplorers.ts
@@ -7,7 +7,7 @@
 import { TPromise } from 'vs/base/common/winjs.base';
 import { IThreadService } from 'vs/workbench/services/thread/common/threadService';
 import { ExtHostContext, MainThreadTreeExplorersShape, ExtHostTreeExplorersShape } from './extHost.protocol';
-import { ICustomTreeExplorerService } from 'vs/workbench/parts/explorers/browser/customTreeExplorerService';
+import { ICustomTreeExplorerService } from 'vs/workbench/parts/explorers/common/customTreeExplorerService';
 import { InternalTreeExplorerNodeContent } from 'vs/workbench/parts/explorers/common/treeExplorerViewModel';
 import { IMessageService, Severity } from 'vs/platform/message/common/message';
 import { ICommandService } from 'vs/platform/commands/common/commands';
