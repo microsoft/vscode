62a0632791997a7360ec8732b6b9040f916aa110
Sandeep Somavarapu <sasomava@microsoft.com>
2016-11-15 18:56:59 +0100

    Fix #14877. Also related to #15162

--- src/vs/platform/configuration/common/model.ts
+++ src/vs/platform/configuration/common/model.ts
@@ -5,7 +5,7 @@
 'use strict';
 
 import { Registry } from 'vs/platform/platform';
-import { IConfigurationNode, IConfigurationRegistry, Extensions } from 'vs/platform/configuration/common/configurationRegistry';
+import { IConfigurationRegistry, Extensions } from 'vs/platform/configuration/common/configurationRegistry';
 
 export function getDefaultValues(): any {
 	const valueTreeRoot: any = Object.create(null);
@@ -15,79 +15,6 @@ export function getDefaultValues(): any {
 		addToValueTree(valueTreeRoot, key, value);
 	}
 	return valueTreeRoot;
-}
-
-export function getDefaultValuesContent(indent: string): string {
-	let lastEntry = -1;
-	const result: string[] = [];
-	result.push('{');
-
-	const handleConfig = (config: IConfigurationNode, hasTopLevelTitle: boolean) => {
-		if (config.title) {
-			if (!hasTopLevelTitle) {
-				result.push('');
-				result.push('// ' + config.title);
-				hasTopLevelTitle = true;
-			} else {
-				result.push(indent + '// ' + config.title);
-			}
-			result.push('');
-		}
-
-		if (config.properties) {
-			Object.keys(config.properties).forEach((key) => {
-
-				const prop = config.properties[key];
-
-				if (prop.description) {
-					result.push(indent + '// ' + prop.description);
-				}
-				let defaultValue = prop.default;
-				let valueString = JSON.stringify(defaultValue, null, indent);
-				if (valueString && (typeof defaultValue === 'object')) {
-					valueString = addIndent(valueString, indent);
-				}
-
-				if (lastEntry !== -1) {
-					result[lastEntry] += ',';
-				}
-				lastEntry = result.length;
-
-				result.push(indent + JSON.stringify(key) + ': ' + valueString);
-				result.push('');
-			});
-		}
-
-		if (config.allOf) {
-			config.allOf.forEach(c => handleConfig(c, hasTopLevelTitle));
-		}
-	};
-
-	const configurations = Registry.as<IConfigurationRegistry>(Extensions.Configuration).getConfigurations();
-	configurations.sort(compareConfigurationNodes).forEach(c => handleConfig(c, false));
-
-	result.push('}');
-
-	return result.join('\n');
-}
-
-function compareConfigurationNodes(c1: IConfigurationNode, c2: IConfigurationNode): number {
-	if (typeof c1.order !== 'number') {
-		return 1;
-	}
-	if (typeof c2.order !== 'number') {
-		return -1;
-	}
-	if (c1.order === c2.order) {
-		const title1 = c1.title || '';
-		const title2 = c2.title || '';
-		return title1.localeCompare(title2);
-	}
-	return c1.order - c2.order;
-}
-
-function addIndent(str: string, indent: string): string {
-	return str.split('\n').join('\n' + indent);
 }
 
 export function toValuesTree(properties: { [qualifiedKey: string]: any }): any {
