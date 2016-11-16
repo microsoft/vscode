cacb1d2cbc999831c8cc71d638a46b6e838d7ebe
Sandeep Somavarapu <sasomava@microsoft.com>
2016-11-16 19:59:01 +0100

    #15361  Refactoring - Create a model for default settings and key bindings - Move default settings actions as editor contributions

--- src/vs/workbench/parts/settings/common/openSettings.ts
+++ src/vs/workbench/parts/settings/common/openSettings.ts
@@ -5,14 +5,51 @@
 
 import { TPromise } from 'vs/base/common/winjs.base';
 import { createDecorator } from 'vs/platform/instantiation/common/instantiation';
+import { IRange } from 'vs/editor/common/editorCommon';
+import URI from 'vs/base/common/uri';
+import { IConfigurationValue } from 'vs/workbench/services/configuration/common/configurationEditing';
+
+export interface ISettingsGroup {
+	range?: IRange;
+	title: string;
+	sections: ISettingsSection[];
+}
+
+export interface ISettingsSection {
+	range?: IRange;
+	description?: string;
+	settings: ISetting[];
+}
+
+export interface ISetting {
+	range?: IRange;
+	key: string;
+	value: any;
+	description?: string;
+}
+
+export interface IDefaultSettings {
+	uri: URI;
+	content: string;
+
+	getSettingsGroups(): ISettingsGroup[];
+}
+
+export interface IDefaultKeybindings {
+	uri: URI;
+	content: string;
+}
 
 export const IOpenSettingsService = createDecorator<IOpenSettingsService>('openSettingsService');
 
 export interface IOpenSettingsService {
 	_serviceBrand: any;
 
+	defaultSettings: IDefaultSettings;
+	defaultKeybindings: IDefaultKeybindings;
+
 	openGlobalSettings(): TPromise<void>;
 	openWorkspaceSettings(): TPromise<void>;
 	openGlobalKeybindingSettings(): TPromise<void>;
-
+	copyConfiguration(configurationValue: IConfigurationValue): void;
 }
