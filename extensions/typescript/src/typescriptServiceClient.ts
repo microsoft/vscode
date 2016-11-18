460e6f2007f64966cc72bd864633b1d128d5ec8c
Dirk Baeumer <dirkb@microsoft.com>
2016-11-17 17:43:23 -0800

    Fixes #15683: Enable TSServer telemetry on 1.7.x

--- extensions/typescript/src/typescriptServiceClient.ts
+++ extensions/typescript/src/typescriptServiceClient.ts
@@ -405,6 +405,9 @@ export default class TypeScriptServiceClient implements ITypescriptServiceClient
 							args.push('--disableAutomaticTypingAcquisition');
 						}
 					}
+					if (this.apiVersion.has208Features()) {
+						args.push('--enableTelemetry');
+					}
 					electron.fork(modulePath, args, options, (err: any, childProcess: cp.ChildProcess) => {
 						if (err) {
 							this.lastError = err;
@@ -648,6 +651,32 @@ export default class TypeScriptServiceClient implements ITypescriptServiceClient
 					this.host.semanticDiagnosticsReceived(event as Proto.DiagnosticEvent);
 				} else if (event.event === 'configFileDiag') {
 					this.host.configFileDiagnosticsReceived(event as Proto.ConfigFileDiagnosticEvent);
+				} else if (event.event === 'telemetry') {
+					let telemetryData = (event as Proto.TelemetryEvent).body;
+					let properties: Map<string> = Object.create(null);
+					switch (telemetryData.telemetryEventName) {
+						case 'typingsInstalled':
+							let typingsInstalledPayload: Proto.TypingsInstalledTelemetryEventPayload = (telemetryData.payload as Proto.TypingsInstalledTelemetryEventPayload);
+							properties['installedPackages'] = typingsInstalledPayload.installedPackages;
+							if (is.defined(typingsInstalledPayload.installSuccess)) {
+								properties['installSuccess'] = typingsInstalledPayload.installSuccess.toString();
+							}
+							if (is.string(typingsInstalledPayload.typingsInstallerVersion)) {
+								properties['typingsInstallerVersion'] = typingsInstalledPayload.typingsInstallerVersion;
+							}
+							break;
+						default:
+							let payload = telemetryData.payload;
+							if (payload) {
+								Object.keys(payload).forEach((key) => {
+									if (payload.hasOwnProperty(key) && is.string(payload[key])) {
+										properties[key] = payload[key];
+									}
+								});
+							}
+							break;
+					}
+					this.logTelemetry(telemetryData.telemetryEventName, properties);
 				}
 			} else {
 				throw new Error('Unknown message type ' + message.type + ' recevied');
