1cbe9052ed694856fe80bd3100e3e82532f43cdb
Christof Marti <chrmarti@microsoft.com>
2016-11-17 17:54:58 -0800

    Log workspace configuration keys (fixes #13653)

--- src/vs/workbench/electron-browser/shell.ts
+++ src/vs/workbench/electron-browser/shell.ts
@@ -23,7 +23,7 @@ import timer = require('vs/base/common/timer');
 import { IStartupFingerprint } from 'vs/workbench/electron-browser/common';
 import { Workbench } from 'vs/workbench/electron-browser/workbench';
 import { StorageService, inMemoryLocalStorageInstance } from 'vs/workbench/services/storage/common/storageService';
-import { ITelemetryService, NullTelemetryService, loadExperiments } from 'vs/platform/telemetry/common/telemetry';
+import { ITelemetryService, NullTelemetryService, configurationTelemetry, loadExperiments } from 'vs/platform/telemetry/common/telemetry';
 import { ITelemetryAppenderChannel, TelemetryAppenderClient } from 'vs/platform/telemetry/common/telemetryIpc';
 import { TelemetryService, ITelemetryServiceConfig } from 'vs/platform/telemetry/common/telemetryService';
 import { IdleMonitor, UserStatus } from 'vs/platform/telemetry/browser/idleMonitor';
@@ -87,7 +87,6 @@ import { IUpdateService } from 'vs/platform/update/common/update';
 import { URLChannelClient } from 'vs/platform/url/common/urlIpc';
 import { IURLService } from 'vs/platform/url/common/url';
 import { ReloadWindowAction } from 'vs/workbench/electron-browser/actions';
-import { WorkspaceConfigurationService } from 'vs/workbench/services/configuration/node/configurationService';
 import { ExtensionHostProcessWorker } from 'vs/workbench/electron-browser/extensionHost';
 import { remote } from 'electron';
 import * as os from 'os';
@@ -341,9 +340,7 @@ export class WorkbenchShell {
 		}
 
 		serviceCollection.set(ITelemetryService, this.telemetryService);
-		if (this.configurationService instanceof WorkspaceConfigurationService) {
-			this.configurationService.telemetryService = this.telemetryService;
-		}
+		disposables.add(configurationTelemetry(this.telemetryService, this.configurationService));
 
 		this.messageService = instantiationService.createInstance(MessageService, container);
 		serviceCollection.set(IMessageService, this.messageService);
