4993c080664bee8578a80ee229a13ae2c23d1194
Johannes Rieken <johannes.rieken@gmail.com>
2016-11-22 10:49:11 +0100

    add test for #15834

--- src/vs/workbench/test/node/api/extHostConfiguration.test.ts
+++ src/vs/workbench/test/node/api/extHostConfiguration.test.ts
@@ -122,7 +122,7 @@ suite('ExtHostConfiguration', function () {
 		assert.throws(() => config['get'] = <any>'get-prop');
 	});
 
-	test('udate/section to key', function () {
+	test('update/section to key', function () {
 
 		const shape = new RecordingShape();
 		const allConfig = createExtHostConfiguration({
@@ -142,6 +142,17 @@ suite('ExtHostConfiguration', function () {
 
 		config.update('foo.bar', 42, true);
 		assert.equal(shape.lastArgs[1], 'foo.bar');
+	});
+
+	test('update, what is #15834', function () {
+		const shape = new RecordingShape();
+		const allConfig = createExtHostConfiguration({
+			['editor.formatOnSave']: createConfigurationValue(true)
+		}, shape);
+
+		allConfig.getConfiguration('editor').update('formatOnSave', { extensions: ['ts'] });
+		assert.equal(shape.lastArgs[1], 'editor.formatOnSave');
+		assert.deepEqual(shape.lastArgs[2], { extensions: ['ts'] });
 	});
 
 	test('update/error-state not OK', function () {
