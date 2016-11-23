44934b47d18c4d62c5cef8212c908e0006ae2810
Joao Moreno <jomo@microsoft.com>
2016-11-23 16:19:49 +0100

    wip: scm viewlet

--- src/vs/workbench/electron-browser/workbench.main.ts
+++ src/vs/workbench/electron-browser/workbench.main.ts
@@ -44,6 +44,7 @@ import 'vs/workbench/parts/search/browser/searchViewlet'; // can be packaged sep
 import 'vs/workbench/parts/search/browser/openAnythingHandler'; // can be packaged separately
 
 import 'vs/workbench/parts/scm/browser/scm.contribution';
+import 'vs/workbench/parts/scm/browser/scmViewlet'; // can be packaged separately
 
 import 'vs/workbench/parts/git/electron-browser/git.contribution';
 import 'vs/workbench/parts/git/browser/gitQuickOpen';
