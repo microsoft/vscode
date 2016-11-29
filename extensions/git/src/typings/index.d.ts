05c7af38cc24f602acd67f34caba0a7596e95aeb
Joao Moreno <jomo@microsoft.com>
2016-11-29 17:09:55 +0100

    git model

--- extensions/git/src/typings/index.d.ts
+++ extensions/git/src/typings/index.d.ts
@@ -1,3 +1,3 @@
+/// <reference path="globals/core-decorators/index.d.ts" />
 /// <reference path="globals/denodeify/index.d.ts" />
 /// <reference path="globals/lodash/index.d.ts" />
-/// <reference path="globals/mime/index.d.ts" />
