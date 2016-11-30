c01df4ea0b645036aaac14e21447f16a7b33b2f2
Sandeep Somavarapu <sasomava@microsoft.com>
2016-11-30 16:40:14 +0100

    Let zone widget extends widget

--- src/vs/editor/contrib/zoneWidget/browser/zoneWidget.ts
+++ src/vs/editor/contrib/zoneWidget/browser/zoneWidget.ts
@@ -7,6 +7,7 @@
 
 import 'vs/css!./zoneWidget';
 import { Disposables } from 'vs/base/common/lifecycle';
+import { Widget } from 'vs/base/browser/ui/widget';
 import * as objects from 'vs/base/common/objects';
 import * as dom from 'vs/base/browser/dom';
 import { Sash, Orientation, IHorizontalSashLayoutProvider, ISashEvent } from 'vs/base/browser/ui/sash/sash';
@@ -89,7 +90,7 @@ export class OverlayWidgetDelegate implements IOverlayWidget {
 
 }
 
-export abstract class ZoneWidget implements IHorizontalSashLayoutProvider {
+export abstract class ZoneWidget extends Widget implements IHorizontalSashLayoutProvider {
 
 	private _overlayWidget: OverlayWidgetDelegate = null;
 	private _resizeSash: Sash;
@@ -104,6 +105,7 @@ export abstract class ZoneWidget implements IHorizontalSashLayoutProvider {
 	public options: IOptions;
 
 	constructor(editor: ICodeEditor, options: IOptions = {}) {
+		super();
 		this.editor = editor;
 		this.options = objects.mixin(objects.clone(defaultOptions), options);
 		this.domNode = document.createElement('div');
