c6339bfea8beb3a6e388caf39f71ea1b47bc572d
Sandeep Somavarapu <sasomava@microsoft.com>
2016-12-02 17:14:41 +0100

    #16394 Move VSash to sash.ts

--- src/vs/workbench/browser/parts/editor/sideBySideEditor.ts
+++ src/vs/workbench/browser/parts/editor/sideBySideEditor.ts
@@ -6,16 +6,14 @@
 import 'vs/css!./media/sidebysideEditor';
 import { TPromise } from 'vs/base/common/winjs.base';
 import * as strings from 'vs/base/common/strings';
-import { Disposable } from 'vs/base/common/lifecycle';
 import * as DOM from 'vs/base/browser/dom';
 import { Dimension, Builder } from 'vs/base/browser/builder';
-import Event, { Emitter } from 'vs/base/common/event';
 
 import { Registry } from 'vs/platform/platform';
 import { IEditorRegistry, Extensions as EditorExtensions, EditorInput, EditorOptions, SideBySideEditorInput } from 'vs/workbench/common/editor';
 import { BaseEditor, EditorDescriptor } from 'vs/workbench/browser/parts/editor/baseEditor';
 import { IEditorControl, Position } from 'vs/platform/editor/common/editor';
-import { Sash, ISashEvent, IVerticalSashLayoutProvider } from 'vs/base/browser/ui/sash/sash';
+import { VSash } from 'vs/base/browser/ui/sash/sash';
 
 import { ITelemetryService } from 'vs/platform/telemetry/common/telemetry';
 import { IInstantiationService } from 'vs/platform/instantiation/common/instantiation';
@@ -134,7 +132,7 @@ export class SideBySideEditor extends BaseEditor {
 	}
 
 	private createSash(parentElement: HTMLElement): void {
-		this.sash = this._register(new VSash(parentElement));
+		this.sash = this._register(new VSash(parentElement, 220));
 		this._register(this.sash.onPositionChange(position => this.dolayout(position)));
 	}
 
@@ -174,93 +172,6 @@ export class SideBySideEditor extends BaseEditor {
 		if (this.masterEditorContainer) {
 			parentContainer.removeChild(this.masterEditorContainer);
 			this.masterEditorContainer = null;
-		}
-	}
-}
-
-class VSash extends Disposable implements IVerticalSashLayoutProvider {
-
-	private static MINIMUM_EDITOR_WIDTH = 220;
-
-	private sash: Sash;
-	private ratio: number;
-	private startPosition: number;
-	private position: number;
-	private dimension: Dimension;
-
-	private _onPositionChange: Emitter<number> = new Emitter<number>();
-	public get onPositionChange(): Event<number> { return this._onPositionChange.event; }
-
-	constructor(container: HTMLElement) {
-		super();
-		this.ratio = 0.5;
-		this.sash = new Sash(container, this);
-
-		this._register(this.sash.addListener2('start', () => this.onSashDragStart()));
-		this._register(this.sash.addListener2('change', (e: ISashEvent) => this.onSashDrag(e)));
-		this._register(this.sash.addListener2('end', () => this.onSashDragEnd()));
-		this._register(this.sash.addListener2('reset', () => this.onSashReset()));
-	}
-
-	public getVerticalSashTop(): number {
-		return 0;
-	}
-
-	public getVerticalSashLeft(): number {
-		return this.position;
-	}
-
-	public getVerticalSashHeight(): number {
-		return this.dimension.height;
-	}
-
-	public setDimenesion(dimension: Dimension) {
-		this.dimension = dimension;
-		this.compute(this.ratio);
-	}
-
-	private onSashDragStart(): void {
-		this.startPosition = this.position;
-	}
-
-	private onSashDrag(e: ISashEvent): void {
-		this.compute((this.startPosition + (e.currentX - e.startX)) / this.dimension.width);
-	}
-
-	private compute(ratio: number) {
-		this.computeSashPosition(ratio);
-		this.ratio = this.position / this.dimension.width;
-		this._onPositionChange.fire(this.position);
-	}
-
-	private onSashDragEnd(): void {
-		this.sash.layout();
-	}
-
-	private onSashReset(): void {
-		this.ratio = 0.5;
-		this._onPositionChange.fire(this.position);
-		this.sash.layout();
-	}
-
-	private computeSashPosition(sashRatio: number = this.ratio) {
-		let contentWidth = this.dimension.width;
-		let sashPosition = Math.floor((sashRatio || 0.5) * contentWidth);
-		let midPoint = Math.floor(0.5 * contentWidth);
-
-		if (contentWidth > VSash.MINIMUM_EDITOR_WIDTH * 2) {
-			if (sashPosition < VSash.MINIMUM_EDITOR_WIDTH) {
-				sashPosition = VSash.MINIMUM_EDITOR_WIDTH;
-			}
-			if (sashPosition > contentWidth - VSash.MINIMUM_EDITOR_WIDTH) {
-				sashPosition = contentWidth - VSash.MINIMUM_EDITOR_WIDTH;
-			}
-		} else {
-			sashPosition = midPoint;
-		}
-		if (this.position !== sashPosition) {
-			this.position = sashPosition;
-			this.sash.layout();
 		}
 	}
 }
