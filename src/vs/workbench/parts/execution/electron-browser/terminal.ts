7612d2d77f61f7d6baf0b80aebbefee7111e5cf8
Daniel Imms <daimms@microsoft.com>
2016-12-03 18:06:17 -0800

    Remove debug log

--- src/vs/workbench/parts/execution/electron-browser/terminal.ts
+++ src/vs/workbench/parts/execution/electron-browser/terminal.ts
@@ -11,7 +11,6 @@ import { TPromise } from 'vs/base/common/winjs.base';
 export const DEFAULT_TERMINAL_LINUX_READY = new TPromise<string>(c => {
 	if (env.isLinux) {
 		pfs.exists('/etc/debian_version').then(isDebian => {
-			console.log('isDebian' + isDebian);
 			if (isDebian) {
 				c('x-terminal-emulator');
 			} else if (process.env.DESKTOP_SESSION === 'gnome' || process.env.DESKTOP_SESSION === 'gnome-classic') {
