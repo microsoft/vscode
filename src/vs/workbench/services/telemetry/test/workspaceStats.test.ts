643004b63b6eed6e0afc6b828a6022976067b2f8
Christof Marti <chrmarti@microsoft.com>
2016-12-07 12:40:38 -0800

    Exclude port (#16680)

--- src/vs/workbench/services/telemetry/test/workspaceStats.test.ts
+++ src/vs/workbench/services/telemetry/test/workspaceStats.test.ts
@@ -14,8 +14,10 @@ suite('Telemetry - WorkspaceStats', () => {
 		const stats = new WorkspaceStats(null, null, null);
 		assert.deepStrictEqual(stats.getDomainsOfRemotes(remote('https://github.com/Microsoft/vscode.git')), ['github.com']);
 		assert.deepStrictEqual(stats.getDomainsOfRemotes(remote('https://git.example.com/gitproject.git')), ['example.com']);
-		assert.deepStrictEqual(stats.getDomainsOfRemotes(remote('https://username@github.com/username/repository.git')), ['github.com']);
-		assert.deepStrictEqual(stats.getDomainsOfRemotes(remote('https://username:password@github.com/username/repository.git')), ['github.com']);
+		assert.deepStrictEqual(stats.getDomainsOfRemotes(remote('https://username@github2.com/username/repository.git')), ['github2.com']);
+		assert.deepStrictEqual(stats.getDomainsOfRemotes(remote('https://username:password@github3.com/username/repository.git')), ['github3.com']);
+		assert.deepStrictEqual(stats.getDomainsOfRemotes(remote('https://username:password@example2.com:1234/username/repository.git')), ['example2.com']);
+		assert.deepStrictEqual(stats.getDomainsOfRemotes(remote('https://example3.com:1234/username/repository.git')), ['example3.com']);
 	});
 
 	test('SSH remotes', function () {
@@ -27,13 +29,19 @@ suite('Telemetry - WorkspaceStats', () => {
 		const stats = new WorkspaceStats(null, null, null);
 		assert.deepStrictEqual(stats.getDomainsOfRemotes(remote('git@github.com:Microsoft/vscode.git')), ['github.com']);
 		assert.deepStrictEqual(stats.getDomainsOfRemotes(remote('user@git.server.org:project.git')), ['server.org']);
-		assert.deepStrictEqual(stats.getDomainsOfRemotes(remote('git.server.org:project.git')), ['server.org']);
+		assert.deepStrictEqual(stats.getDomainsOfRemotes(remote('git.server2.org:project.git')), ['server2.org']);
 	});
 
 	test('Local remotes', function () {
 		const stats = new WorkspaceStats(null, null, null);
 		assert.deepStrictEqual(stats.getDomainsOfRemotes(remote('/opt/git/project.git')), []);
 		assert.deepStrictEqual(stats.getDomainsOfRemotes(remote('file:///opt/git/project.git')), []);
+	});
+
+	test('Multiple remotes', function () {
+		const stats = new WorkspaceStats(null, null, null);
+		const config = ['https://github.com/Microsoft/vscode.git', 'https://git.example.com/gitproject.git'].map(remote).join('');
+		assert.deepStrictEqual(stats.getDomainsOfRemotes(config).sort(), ['example.com', 'github.com']);
 	});
 
 	function remote(url: string): string {
