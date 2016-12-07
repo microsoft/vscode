ac1da74832879039144c0e07d280e9328b1e3e2c
roblou <roblourens@gmail.com>
2016-12-07 15:47:39 -0800

    node-debug2@1.8.6 for #16632

--- build/gulpfile.vscode.js
+++ build/gulpfile.vscode.js
@@ -40,7 +40,7 @@ const nodeModules = ['electron', 'original-fs']
 
 const builtInExtensions = [
 	{ name: 'ms-vscode.node-debug', version: '1.8.12' },
-	{ name: 'ms-vscode.node-debug2', version: '1.8.5' }
+	{ name: 'ms-vscode.node-debug2', version: '1.8.6' }
 ];
 
 const vscodeEntryPoints = _.flatten([
