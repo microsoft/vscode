7bbfb3612b0b594c3a623f01d59976baa214a51e
Christof Marti <chrmarti@microsoft.com>
2016-12-08 16:13:33 -0800

    Whitelist domain names (fixes #16954)

--- src/vs/workbench/services/telemetry/test/workspaceStats.test.ts
+++ src/vs/workbench/services/telemetry/test/workspaceStats.test.ts
@@ -10,38 +10,55 @@ import { WorkspaceStats } from 'vs/workbench/services/telemetry/common/workspace
 
 suite('Telemetry - WorkspaceStats', () => {
 
+	const whitelist = [
+		'github.com',
+		'github2.com',
+		'github3.com',
+		'example.com',
+		'example2.com',
+		'example3.com',
+		'server.org',
+		'server2.org',
+	];
+
 	test('HTTPS remotes', function () {
 		const stats = new WorkspaceStats(null, null, null);
-		assert.deepStrictEqual(stats.getDomainsOfRemotes(remote('https://github.com/Microsoft/vscode.git')), ['github.com']);
-		assert.deepStrictEqual(stats.getDomainsOfRemotes(remote('https://git.example.com/gitproject.git')), ['example.com']);
-		assert.deepStrictEqual(stats.getDomainsOfRemotes(remote('https://username@github2.com/username/repository.git')), ['github2.com']);
-		assert.deepStrictEqual(stats.getDomainsOfRemotes(remote('https://username:password@github3.com/username/repository.git')), ['github3.com']);
-		assert.deepStrictEqual(stats.getDomainsOfRemotes(remote('https://username:password@example2.com:1234/username/repository.git')), ['example2.com']);
-		assert.deepStrictEqual(stats.getDomainsOfRemotes(remote('https://example3.com:1234/username/repository.git')), ['example3.com']);
+		assert.deepStrictEqual(stats.getDomainsOfRemotes(remote('https://github.com/Microsoft/vscode.git'), whitelist), ['github.com']);
+		assert.deepStrictEqual(stats.getDomainsOfRemotes(remote('https://git.example.com/gitproject.git'), whitelist), ['example.com']);
+		assert.deepStrictEqual(stats.getDomainsOfRemotes(remote('https://username@github2.com/username/repository.git'), whitelist), ['github2.com']);
+		assert.deepStrictEqual(stats.getDomainsOfRemotes(remote('https://username:password@github3.com/username/repository.git'), whitelist), ['github3.com']);
+		assert.deepStrictEqual(stats.getDomainsOfRemotes(remote('https://username:password@example2.com:1234/username/repository.git'), whitelist), ['example2.com']);
+		assert.deepStrictEqual(stats.getDomainsOfRemotes(remote('https://example3.com:1234/username/repository.git'), whitelist), ['example3.com']);
 	});
 
 	test('SSH remotes', function () {
 		const stats = new WorkspaceStats(null, null, null);
-		assert.deepStrictEqual(stats.getDomainsOfRemotes(remote('ssh://user@git.server.org/project.git')), ['server.org']);
+		assert.deepStrictEqual(stats.getDomainsOfRemotes(remote('ssh://user@git.server.org/project.git'), whitelist), ['server.org']);
 	});
 
 	test('SCP-like remotes', function () {
 		const stats = new WorkspaceStats(null, null, null);
-		assert.deepStrictEqual(stats.getDomainsOfRemotes(remote('git@github.com:Microsoft/vscode.git')), ['github.com']);
-		assert.deepStrictEqual(stats.getDomainsOfRemotes(remote('user@git.server.org:project.git')), ['server.org']);
-		assert.deepStrictEqual(stats.getDomainsOfRemotes(remote('git.server2.org:project.git')), ['server2.org']);
+		assert.deepStrictEqual(stats.getDomainsOfRemotes(remote('git@github.com:Microsoft/vscode.git'), whitelist), ['github.com']);
+		assert.deepStrictEqual(stats.getDomainsOfRemotes(remote('user@git.server.org:project.git'), whitelist), ['server.org']);
+		assert.deepStrictEqual(stats.getDomainsOfRemotes(remote('git.server2.org:project.git'), whitelist), ['server2.org']);
 	});
 
 	test('Local remotes', function () {
 		const stats = new WorkspaceStats(null, null, null);
-		assert.deepStrictEqual(stats.getDomainsOfRemotes(remote('/opt/git/project.git')), []);
-		assert.deepStrictEqual(stats.getDomainsOfRemotes(remote('file:///opt/git/project.git')), []);
+		assert.deepStrictEqual(stats.getDomainsOfRemotes(remote('/opt/git/project.git'), whitelist), []);
+		assert.deepStrictEqual(stats.getDomainsOfRemotes(remote('file:///opt/git/project.git'), whitelist), []);
 	});
 
 	test('Multiple remotes', function () {
 		const stats = new WorkspaceStats(null, null, null);
 		const config = ['https://github.com/Microsoft/vscode.git', 'https://git.example.com/gitproject.git'].map(remote).join('');
-		assert.deepStrictEqual(stats.getDomainsOfRemotes(config).sort(), ['example.com', 'github.com']);
+		assert.deepStrictEqual(stats.getDomainsOfRemotes(config, whitelist).sort(), ['example.com', 'github.com']);
+	});
+
+	test('Whitelisting', function () {
+		const stats = new WorkspaceStats(null, null, null);
+		const config = ['https://github.com/Microsoft/vscode.git', 'https://git.foobar.com/gitproject.git'].map(remote).join('');
+		assert.deepStrictEqual(stats.getDomainsOfRemotes(config, whitelist).sort(), ['aaaaaa.aaa', 'github.com']);
 	});
 
 	function remote(url: string): string {
