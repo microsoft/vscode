18ed10057bd526e44d66d13775eeae7683218db6
isidor <inikolic@microsoft.com>
2016-12-13 11:21:44 +0100

    debugEditorInput import polish

--- src/vs/workbench/parts/debug/browser/debugEditorInputs.ts
+++ src/vs/workbench/parts/debug/browser/debugEditorInputs.ts
@@ -3,14 +3,14 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+import uri from 'vs/base/common/uri';
 import { TPromise } from 'vs/base/common/winjs.base';
 import { PLAINTEXT_MODE_ID } from 'vs/editor/common/modes/modesRegistry';
-import strinput = require('vs/workbench/common/editor/stringEditorInput');
-import { EditorInput, EditorModel } from 'vs/workbench/common/editor';
-import uri from 'vs/base/common/uri';
 import { IInstantiationService } from 'vs/platform/instantiation/common/instantiation';
+import { StringEditorInput } from 'vs/workbench/common/editor/stringEditorInput';
+import { EditorInput, EditorModel } from 'vs/workbench/common/editor';
 
-export class DebugStringEditorInput extends strinput.StringEditorInput {
+export class DebugStringEditorInput extends StringEditorInput {
 
 	constructor(
 		name: string,
