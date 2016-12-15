7f08800009e23cfbe8063a49aa098b004da2bd1b
Sandeep Somavarapu <sasomava@microsoft.com>
2016-12-15 10:36:22 +0100

    #17073 Replace preview: - Do not use input cache - Let content proivder listen to events and update preview model

--- src/vs/workbench/parts/search/browser/replaceService.ts
+++ src/vs/workbench/parts/search/browser/replaceService.ts
@@ -8,28 +8,29 @@ import * as errors from 'vs/base/common/errors';
 import { TPromise } from 'vs/base/common/winjs.base';
 import URI from 'vs/base/common/uri';
 import * as network from 'vs/base/common/network';
-import * as Map from 'vs/base/common/map';
+import { Disposable } from 'vs/base/common/lifecycle';
 import { IReplaceService } from 'vs/workbench/parts/search/common/replace';
 import { EditorInput } from 'vs/workbench/common/editor';
-import { IEditorService, IEditorInput } from 'vs/platform/editor/common/editor';
+import { IEditorService } from 'vs/platform/editor/common/editor';
 import { IWorkbenchEditorService } from 'vs/workbench/services/editor/common/editorService';
 import { IModelService } from 'vs/editor/common/services/modelService';
 import { IEventService } from 'vs/platform/event/common/event';
-import { Match, FileMatch, FileMatchOrMatch } from 'vs/workbench/parts/search/common/searchModel';
+import { Match, FileMatch, FileMatchOrMatch, ISearchWorkbenchService } from 'vs/workbench/parts/search/common/searchModel';
 import { BulkEdit, IResourceEdit, createBulkEdit } from 'vs/editor/common/services/bulkEdit';
 import { IProgressRunner } from 'vs/platform/progress/common/progress';
 import { IDiffEditor } from 'vs/editor/browser/editorBrowser';
 import { DiffEditorInput } from 'vs/workbench/common/editor/diffEditorInput';
 import { ITelemetryService } from 'vs/platform/telemetry/common/telemetry';
-import { ITextModelResolverService, ITextModelContentProvider, ITextEditorModel } from 'vs/editor/common/services/resolverService';
+import { ITextModelResolverService, ITextModelContentProvider } from 'vs/editor/common/services/resolverService';
 import { IWorkbenchContribution } from 'vs/workbench/common/contributions';
 import { IModel } from 'vs/editor/common/editorCommon';
+import { IInstantiationService } from 'vs/platform/instantiation/common/instantiation';
 
 
 export class ReplacePreviewContentProvider implements ITextModelContentProvider, IWorkbenchContribution {
 
 	constructor(
-		@IModelService private modelService: IModelService,
+		@IInstantiationService private instantiationService: IInstantiationService,
 		@ITextModelResolverService private textModelResolverService: ITextModelResolverService
 	) {
 		this.textModelResolverService.registerTextModelContentProvider(network.Schemas.internal, this);
@@ -41,109 +42,42 @@ export class ReplacePreviewContentProvider implements ITextModelContentProvider,
 
 	public provideTextContent(uri: URI): TPromise<IModel> {
 		if (uri.fragment === 'preview') {
-			const fileResource = uri.with({ scheme: network.Schemas.file, fragment: '' });
-			return this.textModelResolverService.createModelReference(fileResource).then(ref => {
-				const textEditorModel = ref.object.textEditorModel;
-				const model = this.modelService.createModel(textEditorModel.getValue(), textEditorModel.getMode(), uri);
-				ref.dispose();
-				return model;
-			});
+			return this.instantiationService.createInstance(ReplacePreviewModel).resolve(uri);
 		}
 		return null;
 	}
 }
 
-class EditorInputCache {
-
-	private cache: Map.LinkedMap<URI, TPromise<DiffEditorInput>>;
-
+class ReplacePreviewModel extends Disposable {
 	constructor(
-		private replaceService: ReplaceService,
-		private editorService: IWorkbenchEditorService,
-		private textModelResolverService: ITextModelResolverService
+		@IModelService private modelService: IModelService,
+		@ITextModelResolverService private textModelResolverService: ITextModelResolverService,
+		@IReplaceService private replaceService: IReplaceService,
+		@ISearchWorkbenchService private searchWorkbenchService: ISearchWorkbenchService
 	) {
-		this.cache = new Map.LinkedMap<URI, TPromise<DiffEditorInput>>();
+		super();
 	}
 
-	public hasInput(fileMatch: FileMatch): boolean {
-		return this.cache.has(fileMatch.resource());
+	resolve(replacePreviewUri: URI): TPromise<IModel> {
+		const fileResource = replacePreviewUri.with({ scheme: network.Schemas.file, fragment: '' });
+		const fileMatch = <FileMatch>this.searchWorkbenchService.searchModel.searchResult.matches().filter(match => match.resource().toString() === fileResource.toString())[0];
+		return this.textModelResolverService.createModelReference(fileResource).then(ref => {
+			ref = this._register(ref);
+			const sourceModel = ref.object.textEditorModel;
+			const replacePreviewModel = this.modelService.createModel(sourceModel.getValue(), sourceModel.getMode(), replacePreviewUri);
+			this._register(fileMatch.onChange(modelChange => this.update(sourceModel, replacePreviewModel, fileMatch, modelChange)));
+			this._register(this.searchWorkbenchService.searchModel.onReplaceTermChanged(() => this.update(sourceModel, replacePreviewModel, fileMatch)));
+			this._register(fileMatch.onDispose(() => replacePreviewModel.dispose()));
+			this._register(replacePreviewModel.onWillDispose(() => this.dispose()));
+			this._register(sourceModel.onWillDispose(() => this.dispose()));
+			return replacePreviewModel;
+		});
 	}
 
-	public getInput(fileMatch: FileMatch): TPromise<DiffEditorInput> {
-		let editorInputPromise = this.cache.get(fileMatch.resource());
-		if (!editorInputPromise) {
-			editorInputPromise = this.createInput(fileMatch);
-			this.cache.set(fileMatch.resource(), editorInputPromise);
-			this.refreshInput(fileMatch, true);
-			fileMatch.onDispose(() => this.disposeInput(fileMatch));
-			fileMatch.onChange((modelChange) => this.refreshInput(fileMatch, modelChange));
+	private update(sourceModel: IModel, replacePreviewModel: IModel, fileMatch: FileMatch, override: boolean = false): void {
+		if (!sourceModel.isDisposed() && !replacePreviewModel.isDisposed()) {
+			this.replaceService.updateReplacePreview(fileMatch, override);
 		}
-		return editorInputPromise;
-	}
-
-	public refreshInput(fileMatch: FileMatch, reloadFromSource: boolean = false): void {
-		let editorInputPromise = this.cache.get(fileMatch.resource());
-		if (editorInputPromise) {
-			editorInputPromise.then(replacePreviewInput => {
-				replacePreviewInput.modifiedInput.resolve()
-					.then(editorModel => {
-						const replaceResourceModel = (<ITextEditorModel>editorModel).textEditorModel;
-						if (reloadFromSource) {
-							this.textModelResolverService.createModelReference(fileMatch.resource()).done(ref => {
-								const model = ref.object;
-								if (model.textEditorModel) {
-									replaceResourceModel.setValue(model.textEditorModel.getValue());
-									this.replaceService.replace(fileMatch, null, replaceResourceModel.uri);
-								}
-								ref.dispose();
-							});
-						} else {
-							replaceResourceModel.undo();
-							this.replaceService.replace(fileMatch, null, replaceResourceModel.uri);
-						}
-					});
-			});
-		}
-	}
-
-	public disposeInput(fileMatch: FileMatch): void
-	public disposeInput(resource: URI): void
-	public disposeInput(arg: any): void {
-		let resourceUri = arg instanceof URI ? arg : arg instanceof FileMatch ? arg.resource() : null;
-		if (resourceUri) {
-			let editorInputPromise = this.cache.get(resourceUri);
-			if (editorInputPromise) {
-				editorInputPromise.done((diffInput) => {
-					this.cleanInput(resourceUri);
-					diffInput.dispose();
-				});
-			}
-		}
-	}
-
-	public disposeAll(): void {
-		this.cache.keys().forEach(resource => this.disposeInput(resource));
-	}
-
-	private createInput(fileMatch: FileMatch): TPromise<DiffEditorInput> {
-		return TPromise.join([this.createLeftInput(fileMatch), this.createRightInput(fileMatch)])
-			.then(([left, right]) => {
-				let editorInput = new DiffEditorInput(nls.localize('fileReplaceChanges', "{0} ↔ {1} (Replace Preview)", fileMatch.name(), fileMatch.name()), undefined, <EditorInput>left, <EditorInput>right);
-				editorInput.onDispose(() => this.cleanInput(fileMatch.resource()));
-				return editorInput;
-			});
-	}
-
-	private createLeftInput(fileMatch: FileMatch): TPromise<IEditorInput> {
-		return this.editorService.createInput({ resource: fileMatch.resource() });
-	}
-
-	private createRightInput(fileMatch: FileMatch): TPromise<IEditorInput> {
-		return this.editorService.createInput({ resource: fileMatch.resource().with({ scheme: network.Schemas.internal, fragment: 'preview' }) });
-	}
-
-	private cleanInput(resourceUri: URI): void {
-		this.cache.delete(resourceUri);
 	}
 }
 
@@ -151,16 +85,14 @@ export class ReplaceService implements IReplaceService {
 
 	public _serviceBrand: any;
 
-	private cache: EditorInputCache;
-
 	constructor(
 		@ITelemetryService private telemetryService: ITelemetryService,
 		@IEventService private eventService: IEventService,
 		@IEditorService private editorService: IWorkbenchEditorService,
-		@IModelService private modelService: IModelService,
-		@ITextModelResolverService private textModelResolverService: ITextModelResolverService
+		@IInstantiationService private instantiationService: IInstantiationService,
+		@ITextModelResolverService private textModelResolverService: ITextModelResolverService,
+		@ISearchWorkbenchService private searchWorkbenchService: ISearchWorkbenchService
 	) {
-		this.cache = new EditorInputCache(this, editorService, textModelResolverService);
 	}
 
 	public replace(match: Match): TPromise<any>
@@ -194,32 +126,37 @@ export class ReplaceService implements IReplaceService {
 		return bulkEdit.finish();
 	}
 
-	public getInput(element: FileMatch): TPromise<EditorInput> {
-		return this.cache.getInput(element);
-	}
-
-	public refreshInput(element: FileMatch, reload: boolean = false): void {
-		this.cache.refreshInput(element, reload);
-	}
-
-	public disposeAllInputs(): void {
-		this.cache.disposeAll();
-	}
-
-	public openReplacePreviewEditor(element: FileMatchOrMatch, preserveFocus?: boolean, sideBySide?: boolean, pinned?: boolean): TPromise<any> {
+	public openReplacePreview(element: FileMatchOrMatch, preserveFocus?: boolean, sideBySide?: boolean, pinned?: boolean): TPromise<any> {
 		this.telemetryService.publicLog('replace.open.previewEditor');
-		return this.getInput(element instanceof Match ? element.parent() : element).then((editorInput) => {
-			this.editorService.openEditor(editorInput, { preserveFocus, pinned, revealIfVisible: true }).then((editor) => {
-				let editorControl = (<IDiffEditor>editor.getControl());
-				if (element instanceof Match) {
-					editorControl.revealLineInCenter(element.range().startLineNumber);
-				}
-			}, errors.onUnexpectedError);
+		const fileMatch = element instanceof Match ? element.parent() : element;
+		return this.createInput(fileMatch).then((editorInput) => {
+			this.editorService.openEditor(editorInput, { preserveFocus, pinned, revealIfVisible: true })
+				.then(editor => {
+					this.updateReplacePreview(fileMatch).then(() => {
+						let editorControl = (<IDiffEditor>editor.getControl());
+						if (element instanceof Match) {
+							editorControl.revealLineInCenter(element.range().startLineNumber);
+						}
+					});
+				}, errors.onUnexpectedError);
 		}, errors.onUnexpectedError);
 	}
 
-	public isReplacePreviewEditorOpened(element: FileMatchOrMatch): boolean {
-		return this.cache.hasInput(element instanceof Match ? element.parent() : element);
+	public updateReplacePreview(fileMatch: FileMatch, override: boolean = false): TPromise<void> {
+		const replacePreviewUri = this.getReplacePreviewUri(fileMatch);
+		return TPromise.join([this.textModelResolverService.createModelReference(fileMatch.resource()), this.textModelResolverService.createModelReference(replacePreviewUri)])
+			.then(([sourceModelRef, replaceModelRef]) => {
+				const sourceModel = sourceModelRef.object.textEditorModel;
+				const replaceModel = replaceModelRef.object.textEditorModel;
+				if (override) {
+					replaceModel.setValue(sourceModel.getValue());
+				} else {
+					replaceModel.undo();
+				}
+				sourceModelRef.dispose();
+				replaceModelRef.dispose();
+				return this.replace(fileMatch, null, replacePreviewUri);
+			});
 	}
 
 	private createEdit(match: Match, text: string, resource: URI = null): IResourceEdit {
@@ -230,5 +167,16 @@ export class ReplaceService implements IReplaceService {
 			newText: text
 		};
 		return resourceEdit;
+	}
+
+	private createInput(fileMatch: FileMatch): TPromise<DiffEditorInput> {
+		return TPromise.join([this.editorService.createInput({ resource: fileMatch.resource() }), this.editorService.createInput({ resource: this.getReplacePreviewUri(fileMatch) })])
+			.then(([left, right]) => {
+				return new DiffEditorInput(nls.localize('fileReplaceChanges', "{0} ↔ {1} (Replace Preview)", fileMatch.name(), fileMatch.name()), '', <EditorInput>left, <EditorInput>right);
+			});
+	}
+
+	private getReplacePreviewUri(fileMatch: FileMatch): URI {
+		return fileMatch.resource().with({ scheme: network.Schemas.internal, fragment: 'preview' });
 	}
 }
