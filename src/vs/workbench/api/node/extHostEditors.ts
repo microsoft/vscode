75840d1f7480929d08e6640374cdc72eb4305c48
Joel Day <joelday@gmail.com>
2016-12-16 00:52:18 -0800

    Fix hygiene error.

--- src/vs/workbench/api/node/extHostEditors.ts
+++ src/vs/workbench/api/node/extHostEditors.ts
@@ -13,7 +13,7 @@ import { TPromise } from 'vs/base/common/winjs.base';
 import { IThreadService } from 'vs/workbench/services/thread/common/threadService';
 import { ExtHostDocuments, ExtHostDocumentData } from 'vs/workbench/api/node/extHostDocuments';
 import { Selection, Range, Position, EndOfLine, TextEditorRevealType, TextEditorSelectionChangeKind, TextEditorLineNumbersStyle } from './extHostTypes';
-import { IPosition, IRange, ISingleEditOperation, TextEditorCursorStyle } from 'vs/editor/common/editorCommon';
+import { ISingleEditOperation, TextEditorCursorStyle } from 'vs/editor/common/editorCommon';
 import { IResolvedTextEditorConfiguration, ISelectionChangeEvent, ITextEditorConfigurationUpdate } from 'vs/workbench/api/node/mainThreadEditorsTracker';
 import * as TypeConverters from './extHostTypeConverters';
 import { MainContext, MainThreadEditorsShape, ExtHostEditorsShape, ITextEditorAddData, ITextEditorPositionData } from './extHost.protocol';
