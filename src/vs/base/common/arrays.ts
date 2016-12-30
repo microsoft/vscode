07aa61521a0a6aaca802f1a7f148d3b160de8c83
Johannes Rieken <johannes.rieken@gmail.com>
2016-12-30 16:15:39 +0100

    debt - remove unused code

--- src/vs/base/common/arrays.ts
+++ src/vs/base/common/arrays.ts
@@ -13,19 +13,6 @@ export function tail<T>(array: T[], n: number = 0): T {
 	return array[array.length - (1 + n)];
 }
 
-/**
- * Iterates the provided array and allows to remove
- * elements while iterating.
- */
-export function forEach<T>(array: T[], callback: (element: T, remove: Function) => void): void {
-	for (var i = 0, len = array.length; i < len; i++) {
-		callback(array[i], function () {
-			array.splice(i, 1);
-			i--; len--;
-		});
-	}
-}
-
 export function equals<T>(one: T[], other: T[], itemEquals: (a: T, b: T) => boolean = (a, b) => a === b): boolean {
 	if (one.length !== other.length) {
 		return false;
