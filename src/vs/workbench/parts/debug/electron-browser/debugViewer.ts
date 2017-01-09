e78a9ea62f64886caa922965fd86f8923ffc4c85
isidor <inikolic@microsoft.com>
2017-01-09 16:03:00 +0100

    fixes #18234

--- src/vs/workbench/parts/debug/electron-browser/debugViewer.ts
+++ src/vs/workbench/parts/debug/electron-browser/debugViewer.ts
@@ -172,7 +172,7 @@ function renderRenameBox(debugService: debug.IDebugService, contextViewService: 
 }
 
 function getSourceName(source: Source, contextService: IWorkspaceContextService): string {
-	if (source.inMemory) {
+	if (source.name) {
 		return source.name;
 	}
 
