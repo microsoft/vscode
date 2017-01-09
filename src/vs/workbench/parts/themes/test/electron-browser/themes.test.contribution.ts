62d7edf34e21356bcde810c035d454ca055ac558
Alex Dima <alexdima@microsoft.com>
2017-01-09 09:43:59 +0100

    Extract findMatchingThemeRule from themes test

--- src/vs/workbench/parts/themes/test/electron-browser/themes.test.contribution.ts
+++ src/vs/workbench/parts/themes/test/electron-browser/themes.test.contribution.ts
@@ -12,13 +12,14 @@ import { IModeService } from 'vs/editor/common/services/modeService';
 import pfs = require('vs/base/node/pfs');
 import { CommandsRegistry } from 'vs/platform/commands/common/commands';
 import { IInstantiationService, ServicesAccessor } from 'vs/platform/instantiation/common/instantiation';
-import { IThemeService, IThemeDocument, IThemeSettingStyle } from 'vs/workbench/services/themes/common/themeService';
+import { IThemeService, IThemeDocument } from 'vs/workbench/services/themes/common/themeService';
 import { IWorkbenchEditorService } from 'vs/workbench/services/editor/common/editorService';
 import { toResource } from 'vs/workbench/common/editor';
 import { ITextMateService } from 'vs/editor/node/textMate/textMateService';
 import { IGrammar, StackElement } from 'vscode-textmate';
 import { TokenizationRegistry } from 'vs/editor/common/modes';
 import { TokenMetadata } from 'vs/editor/common/model/tokensBinaryEncoding';
+import { ThemeRule, findMatchingThemeRule } from 'vs/editor/electron-browser/textMate/TMHelper';
 
 interface IToken {
 	c: string;
@@ -82,112 +83,9 @@ class ThemeDocument {
 
 	private _findMatchingThemeRule(scopes: string): ThemeRule {
 		if (!this._cache[scopes]) {
-			this._cache[scopes] = this._doFindMatchingThemeRule(scopes.split(' '));
+			this._cache[scopes] = findMatchingThemeRule(this._theme, scopes.split(' '));
 		}
 		return this._cache[scopes];
-	}
-
-	private _doFindMatchingThemeRule(scopes: string[]): ThemeRule {
-		for (let i = scopes.length - 1; i >= 0; i--) {
-			let parentScopes = scopes.slice(0, i);
-			let scope = scopes[i];
-			let r = this._findMatchingThemeRule2(scope, parentScopes);
-			if (r) {
-				return r;
-			}
-		}
-		return null;
-	}
-
-	private _findMatchingThemeRule2(scope: string, parentScopes: string[]): ThemeRule {
-		let result: ThemeRule = null;
-
-		// Loop backwards, to ensure the last most specific rule wins
-		for (let i = this._theme.settings.length - 1; i >= 0; i--) {
-			let rule = this._theme.settings[i];
-			if (!rule.settings.foreground) {
-				continue;
-			}
-
-			let selectors: string[];
-			if (typeof rule.scope === 'string') {
-				selectors = rule.scope.split(/,/).map(scope => scope.trim());
-			} else if (Array.isArray(rule.scope)) {
-				selectors = rule.scope;
-			} else {
-				continue;
-			}
-
-			for (let j = 0, lenJ = selectors.length; j < lenJ; j++) {
-				let rawSelector = selectors[j];
-
-				let themeRule = new ThemeRule(rawSelector, rule.settings);
-				if (themeRule.matches(scope, parentScopes)) {
-					if (themeRule.isMoreSpecific(result)) {
-						result = themeRule;
-					}
-				}
-			}
-		}
-
-		return result;
-	}
-}
-
-class ThemeRule {
-	readonly rawSelector: string;
-	readonly settings: IThemeSettingStyle;
-	readonly scope: string;
-	readonly parentScopes: string[];
-
-	constructor(rawSelector: string, settings: IThemeSettingStyle) {
-		this.rawSelector = rawSelector;
-		this.settings = settings;
-		let rawSelectorPieces = this.rawSelector.split(/ /);
-		this.scope = rawSelectorPieces[rawSelectorPieces.length - 1];
-		this.parentScopes = rawSelectorPieces.slice(0, rawSelectorPieces.length - 1);
-	}
-
-	public matches(scope: string, parentScopes: string[]): boolean {
-		return ThemeRule._matches(this.scope, this.parentScopes, scope, parentScopes);
-	}
-
-	public isMoreSpecific(other: ThemeRule): boolean {
-		if (other === null) {
-			return true;
-		}
-		if (other.scope.length === this.scope.length) {
-			return this.parentScopes.length > other.parentScopes.length;
-		}
-		return (this.scope.length > other.scope.length);
-	}
-
-	private static _matchesOne(selectorScope: string, scope: string): boolean {
-		let selectorPrefix = selectorScope + '.';
-		if (selectorScope === scope || scope.substring(0, selectorPrefix.length) === selectorPrefix) {
-			return true;
-		}
-		return false;
-	}
-
-	private static _matches(selectorScope: string, selectorParentScopes: string[], scope: string, parentScopes: string[]): boolean {
-		if (!this._matchesOne(selectorScope, scope)) {
-			return false;
-		}
-
-		let selectorParentIndex = selectorParentScopes.length - 1;
-		let parentIndex = parentScopes.length - 1;
-		while (selectorParentIndex >= 0 && parentIndex >= 0) {
-			if (this._matchesOne(selectorParentScopes[selectorParentIndex], parentScopes[parentIndex])) {
-				selectorParentIndex--;
-			}
-			parentIndex--;
-		}
-
-		if (selectorParentIndex === -1) {
-			return true;
-		}
-		return false;
 	}
 }
 
