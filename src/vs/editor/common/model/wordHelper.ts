b70ca54a63067aa530bd5187fe27781d4a7a08aa
Johannes Rieken <johannes.rieken@gmail.com>
2017-01-09 10:04:45 +0100

    faster version of getWordAtPostFast,  #2312

--- src/vs/editor/common/model/wordHelper.ts
+++ src/vs/editor/common/model/wordHelper.ts
@@ -54,72 +54,35 @@ export function ensureValidWordDefinition(wordDefinition?: RegExp): RegExp {
 	return result;
 }
 
-function reverse(str: string): string {
-	let reversedStr = '';
-	for (let i = str.length - 1; i >= 0; i--) {
-		reversedStr += str.charAt(i);
-	}
-	return reversedStr;
-}
-
 function getWordAtPosFast(column: number, wordDefinition: RegExp, text: string, textOffset: number): IWordAtPosition {
-	// matches at the desired column, once to right
-	// and once to the left. The latter is achived
-	// by reversing the string. Falls back to getWordAtPosSlow
-	// when a word is longer than 100 characters. Will
-	// not work with regular expressions that check the
-	// shape of a word, like /aabb/
+	// find whitespace enclosed text around column and match from there
+
+	if (wordDefinition.test(' ')) {
+		return getWordAtPosSlow(column, wordDefinition, text, textOffset);
+	}
 
 	let pos = column - 1 - textOffset;
-	wordDefinition.lastIndex = pos;
-	let rightMatch = wordDefinition.exec(text);
-	if (rightMatch && rightMatch.index > pos) {
-		// |nW
-		rightMatch = null;
+	let start = text.lastIndexOf(' ', pos - 1) + 1;
+	let end = text.indexOf(' ', pos);
+	if (end === -1) {
+		end = text.length;
 	}
 
-	let leftTextReverse = reverse(text.substring(pos - 100, pos));
-	wordDefinition.lastIndex = 0;
-	let leftMatch = wordDefinition.exec(leftTextReverse);
-	if (leftMatch) {
-
-		if (leftMatch.index > 0) {
-			// |nW
-			leftMatch = null;
-
-		} else if (wordDefinition.lastIndex === 100) {
-			// |W*100 -> very long word
-			return getWordAtPosSlow(column, wordDefinition, text, textOffset);
+	wordDefinition.lastIndex = start;
+	let match: RegExpMatchArray;
+	while (match = wordDefinition.exec(text)) {
+		if (match.index <= pos && wordDefinition.lastIndex >= pos) {
+			return {
+				word: match[0],
+				startColumn: textOffset + 1 + match.index,
+				endColumn: textOffset + 1 + wordDefinition.lastIndex
+			};
 		}
 	}
 
-	if (!rightMatch && !leftMatch) {
-		// nothing matched
-		return null;
-	}
-
-	let word = '';
-	let start = pos;
-	let end = pos;
-
-	if (leftMatch) {
-		let leftWord = reverse(leftMatch[0]);
-		start -= leftWord.length;
-		word = leftWord;
-	}
-
-	if (rightMatch) {
-		let rightWord = rightMatch[0];
-		end += rightWord.length;
-		word += rightWord;
-	}
-
-	return {
-		word,
-		startColumn: textOffset + 1 + start,
-		endColumn: textOffset + 1 + end
-	};
+	return null;
 }
+
 
 function getWordAtPosSlow(column: number, wordDefinition: RegExp, text: string, textOffset: number): IWordAtPosition {
 	// matches all words starting at the beginning
