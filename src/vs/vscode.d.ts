cae0349cfa957027228ddc077782dfcbaa1a9d25
Johannes Rieken <johannes.rieken@gmail.com>
2017-01-10 17:22:18 +0100

    fix typo

--- src/vs/vscode.d.ts
+++ src/vs/vscode.d.ts
@@ -2398,7 +2398,7 @@ declare module 'vscode' {
 
 		/**
 		 * An optional set of characters that when pressed while this completion is active will accept it first and
-		 * then insert type that character. *Note* that all commit characters should have `length=1` and that superfluous
+		 * then type that character. *Note* that all commit characters should have `length=1` and that superfluous
 		 * characters will be ignored.
 		 */
 		commitCharacters?: string[];
