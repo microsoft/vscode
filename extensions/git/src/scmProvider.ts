6a92c558d53e166b6faba84192398ea7cb1d90ca
Joao Moreno <jomo@microsoft.com>
2017-01-12 11:07:06 +0100

    git: handle resource opening

--- extensions/git/src/scmProvider.ts
+++ extensions/git/src/scmProvider.ts
@@ -5,7 +5,7 @@
 
 'use strict';
 
-import { scm, Uri, Disposable, SCMProvider, SCMResourceGroup, Event, commands } from 'vscode';
+import { scm, Uri, Disposable, SCMProvider, SCMResourceGroup, Event, commands, ProviderResult } from 'vscode';
 import { Model, Status, Resource, ResourceGroup } from './model';
 import * as path from 'path';
 
@@ -28,25 +28,71 @@ export class GitSCMProvider implements SCMProvider {
 		return this.model.commit(message, { all });
 	}
 
-	open(resource: Resource): Thenable<void> {
-		const fileName = path.basename(resource.uri.fsPath);
-		const indexUri = resource.uri.with({ scheme: 'git-index' });
+	open(resource: Resource): ProviderResult<void> {
+		const left = this.getLeftResource(resource);
+		const right = this.getRightResource(resource);
+		const title = this.getTitle(resource);
 
-		switch (resource.type) {
-			case Status.UNTRACKED:
-				return commands.executeCommand<void>('vscode.open', resource.uri);
-			case Status.MODIFIED:
-				return commands.executeCommand<void>('vscode.diff', indexUri, resource.uri, `${fileName} (HEAD) ↔ ${fileName}`);
-			case Status.DELETED:
-			case Status.INDEX_ADDED:
-			case Status.INDEX_COPIED:
-			case Status.INDEX_DELETED:
-			case Status.INDEX_RENAMED:
-				return commands.executeCommand<void>('vscode.open', indexUri);
-			// TODO@joao: rest!
+		if (!left) {
+			if (!right) {
+				// TODO
+				console.error('oh no');
+				return;
+			}
+
+			return commands.executeCommand<void>('vscode.open', right);
 		}
 
-		return Promise.resolve();
+		return commands.executeCommand<void>('vscode.diff', left, right, title);
+	}
+
+	private getLeftResource(resource: Resource): Uri | undefined {
+		switch (resource.type) {
+			case Status.INDEX_MODIFIED:
+			case Status.INDEX_RENAMED:
+				return resource.uri.with({ scheme: 'git', query: 'HEAD' });
+
+			case Status.MODIFIED:
+				const uriString = resource.uri.toString();
+				const [indexStatus] = this.model.indexGroup.resources.filter(r => r.uri.toString() === uriString);
+				const query = indexStatus ? '~' : 'HEAD';
+				return resource.uri.with({ scheme: 'git', query });
+		}
+	}
+
+	private getRightResource(resource: Resource): Uri | undefined {
+		switch (resource.type) {
+			case Status.INDEX_MODIFIED:
+			case Status.INDEX_ADDED:
+			case Status.INDEX_COPIED:
+			case Status.INDEX_RENAMED:
+				return resource.uri.with({ scheme: 'git' });
+
+			case Status.INDEX_DELETED:
+			case Status.DELETED:
+				return resource.uri.with({ scheme: 'git', query: 'HEAD' });
+
+			case Status.MODIFIED:
+			case Status.UNTRACKED:
+			case Status.IGNORED:
+			case Status.BOTH_MODIFIED:
+				return resource.uri;
+		}
+	}
+
+	private getTitle(resource: Resource): string {
+		const basename = path.basename(resource.uri.fsPath);
+
+		switch (resource.type) {
+			case Status.INDEX_MODIFIED:
+			case Status.INDEX_RENAMED:
+				return `${basename} (Index)`;
+
+			case Status.MODIFIED:
+				return `${basename} (Working Tree)`;
+		}
+
+		return '';
 	}
 
 	drag(resource: Resource, resourceGroup: ResourceGroup): void {
@@ -55,10 +101,10 @@ export class GitSCMProvider implements SCMProvider {
 
 	getOriginalResource(uri: Uri): Uri | undefined {
 		if (uri.scheme !== 'file') {
-			return void 0;
+			return;
 		}
 
-		return uri.with({ scheme: 'git-index' });
+		return uri.with({ scheme: 'git' });
 	}
 
 	dispose(): void {
