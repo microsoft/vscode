c89d9256af8dad8fda0e267bdaf96e3e404f382e
Benjamin Pasero <benjpas@microsoft.com>
2017-01-13 16:35:41 +0100

    :lipstick:

--- src/vs/base/common/labels.ts
+++ src/vs/base/common/labels.ts
@@ -7,8 +7,8 @@
 import URI from 'vs/base/common/uri';
 import platform = require('vs/base/common/platform');
 import types = require('vs/base/common/types');
-import strings = require('vs/base/common/strings');
-import paths = require('vs/base/common/paths');
+import { nativeSep, isEqualOrParent, normalize } from 'vs/base/common/paths';
+import { endsWith, ltrim } from 'vs/base/common/strings';
 
 export interface ILabelProvider {
 
@@ -44,19 +44,19 @@ export function getPathLabel(resource: URI | string, basePathProvider?: URI | st
 
 	const basepath = basePathProvider && getPath(basePathProvider);
 
-	if (basepath && paths.isEqualOrParent(absolutePath, basepath)) {
+	if (basepath && isEqualOrParent(absolutePath, basepath)) {
 		if (basepath === absolutePath) {
 			return ''; // no label if pathes are identical
 		}
 
-		return paths.normalize(strings.ltrim(absolutePath.substr(basepath.length), paths.nativeSep), true);
+		return normalize(ltrim(absolutePath.substr(basepath.length), nativeSep), true);
 	}
 
 	if (platform.isWindows && absolutePath && absolutePath[1] === ':') {
-		return paths.normalize(absolutePath.charAt(0).toUpperCase() + absolutePath.slice(1), true); // convert c:\something => C:\something
+		return normalize(absolutePath.charAt(0).toUpperCase() + absolutePath.slice(1), true); // convert c:\something => C:\something
 	}
 
-	return paths.normalize(absolutePath, true);
+	return normalize(absolutePath, true);
 }
 
 function getPath(arg1: URI | string | IWorkspaceProvider): string {
@@ -74,4 +74,58 @@ function getPath(arg1: URI | string | IWorkspaceProvider): string {
 	}
 
 	return (<URI>arg1).fsPath;
+}
+
+/**
+ * Shortens the paths but keeps them easy to distinguish.
+ * Replaces not important parts with ellipsis.
+ * Every shorten path matches only one original path and vice versa.
+ */
+export function shorten(paths: string[]): string[] {
+	const ellipsis = '\u2026';
+	let shortenedPaths: string[] = new Array(paths.length);
+	let match = false;
+
+	// for every path
+	for (let path = 0; path < paths.length; path++) {
+		let segments: string[] = paths[path].split(nativeSep);
+		match = true;
+
+		// pick the first shortest subpath found
+		for (let subpathLength = 1; match && subpathLength <= segments.length; subpathLength++) {
+			for (let start = segments.length - subpathLength; match && start >= 0; start--) {
+				match = false;
+				let subpath = segments.slice(start, start + subpathLength).join(nativeSep);
+
+				// that is unique to any other path
+				for (let otherPath = 0; !match && otherPath < paths.length; otherPath++) {
+					if (otherPath !== path && paths[otherPath].indexOf(subpath) > -1) {
+						// suffix subpath treated specially as we consider no match 'x' and 'x/...'
+						let isSubpathEnding: boolean = (start + subpathLength === segments.length);
+						let isOtherPathEnding: boolean = endsWith(paths[otherPath], subpath);
+						match = !isSubpathEnding || isOtherPathEnding;
+					}
+				}
+
+				if (!match) {
+					// found unique subpath
+					let result = subpath;
+					if (start + subpathLength < segments.length) {
+						result = result + nativeSep + ellipsis;
+					}
+					if (start > 0) {
+						result = ellipsis + nativeSep + result;
+					}
+					shortenedPaths[path] = result;
+				}
+			}
+		}
+
+		if (match) {
+			// use full path if no unique subpaths found
+			shortenedPaths[path] = paths[path];
+		}
+	}
+
+	return shortenedPaths;
 }
