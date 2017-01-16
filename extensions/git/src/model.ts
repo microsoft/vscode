5ed024cb1aadf12db57d7cb1f329451c232d8a01
Joao Moreno <jomo@microsoft.com>
2017-01-16 15:51:10 +0100

    git: block concurrent commands, sync

--- extensions/git/src/model.ts
+++ extensions/git/src/model.ts
@@ -7,8 +7,8 @@
 
 import { Uri, EventEmitter, Event, SCMResource, SCMResourceDecorations, SCMResourceGroup } from 'vscode';
 import { Repository, IRef, IBranch, IRemote, IPushOptions } from './git';
-import { throttle } from './util';
-import { decorate } from 'core-decorators';
+import { throttle, anyEvent } from './util';
+import { decorate, memoize } from 'core-decorators';
 import * as path from 'path';
 
 const iconsRootPath = path.join(path.dirname(__dirname), 'resources', 'icons');
@@ -150,10 +150,57 @@ export class WorkingTreeGroup extends ResourceGroup {
 	}
 }
 
+export enum Operation {
+	Status = 0o1,
+	Stage = 0o2,
+	Unstage = 0o4,
+	Commit = 0o10,
+	Clean = 0o20,
+	Branch = 0o40,
+	Checkout = 0o100,
+	Fetch = 0o200,
+	Sync = 0o400,
+	Push = 0o1000
+}
+
+export interface Operations {
+	isRunning(operation: Operation): boolean;
+}
+
+class OperationsImpl implements Operations {
+
+	constructor(private readonly operations: number = 0) {
+		// noop
+	}
+
+	start(operation: Operation): OperationsImpl {
+		return new OperationsImpl(this.operations | operation);
+	}
+
+	end(operation: Operation): OperationsImpl {
+		return new OperationsImpl(this.operations & ~operation);
+	}
+
+	isRunning(operation: Operation): boolean {
+		return (this.operations & operation) !== 0;
+	}
+}
+
 export class Model {
 
 	private _onDidChange = new EventEmitter<SCMResourceGroup[]>();
 	readonly onDidChange: Event<SCMResourceGroup[]> = this._onDidChange.event;
+
+	private _onRunOperation = new EventEmitter<Operation>();
+	readonly onRunOperation: Event<Operation> = this._onRunOperation.event;
+
+	private _onDidRunOperation = new EventEmitter<Operation>();
+	readonly onDidRunOperation: Event<Operation> = this._onDidRunOperation.event;
+
+	@memoize
+	get onDidChangeOperations(): Event<void> {
+		return anyEvent(this.onRunOperation as Event<any>, this.onDidRunOperation as Event<any>);
+	}
 
 	private _mergeGroup = new MergeGroup([]);
 	get mergeGroup(): MergeGroup { return this._mergeGroup; }
@@ -180,6 +227,9 @@ export class Model {
 		return result;
 	}
 
+	private _operations = new OperationsImpl();
+	get operations(): Operations { return this._operations; }
+
 	constructor(private _repositoryRoot: string, private repository: Repository) {
 
 	}
@@ -205,145 +255,186 @@ export class Model {
 
 	@decorate(throttle)
 	async update(): Promise<void> {
-		const status = await this.repository.getStatus();
-		let HEAD: IBranch | undefined;
+		await this.run(Operation.Status, async () => {
+			const status = await this.repository.getStatus();
+			let HEAD: IBranch | undefined;
+
+			try {
+				HEAD = await this.repository.getHEAD();
+
+				if (HEAD.name) {
+					try {
+						HEAD = await this.repository.getBranch(HEAD.name);
+					} catch (err) {
+						// noop
+					}
+				}
+			} catch (err) {
+				// noop
+			}
+
+			const [refs, remotes] = await Promise.all([this.repository.getRefs(), this.repository.getRemotes()]);
+
+			this._HEAD = HEAD;
+			this._refs = refs;
+			this._remotes = remotes;
+
+			const index: Resource[] = [];
+			const workingTree: Resource[] = [];
+			const merge: Resource[] = [];
+
+			status.forEach(raw => {
+				const uri = Uri.file(path.join(this.repositoryRoot, raw.path));
+
+				switch (raw.x + raw.y) {
+					case '??': return workingTree.push(new Resource(uri, Status.UNTRACKED));
+					case '!!': return workingTree.push(new Resource(uri, Status.IGNORED));
+					case 'DD': return merge.push(new Resource(uri, Status.BOTH_DELETED));
+					case 'AU': return merge.push(new Resource(uri, Status.ADDED_BY_US));
+					case 'UD': return merge.push(new Resource(uri, Status.DELETED_BY_THEM));
+					case 'UA': return merge.push(new Resource(uri, Status.ADDED_BY_THEM));
+					case 'DU': return merge.push(new Resource(uri, Status.DELETED_BY_US));
+					case 'AA': return merge.push(new Resource(uri, Status.BOTH_ADDED));
+					case 'UU': return merge.push(new Resource(uri, Status.BOTH_MODIFIED));
+				}
+
+				let isModifiedInIndex = false;
+
+				switch (raw.x) {
+					case 'M': index.push(new Resource(uri, Status.INDEX_MODIFIED)); isModifiedInIndex = true; break;
+					case 'A': index.push(new Resource(uri, Status.INDEX_ADDED)); break;
+					case 'D': index.push(new Resource(uri, Status.INDEX_DELETED)); break;
+					case 'R': index.push(new Resource(uri, Status.INDEX_RENAMED/*, raw.rename*/)); break;
+					case 'C': index.push(new Resource(uri, Status.INDEX_COPIED)); break;
+				}
+
+				switch (raw.y) {
+					case 'M': workingTree.push(new Resource(uri, Status.MODIFIED/*, raw.rename*/)); break;
+					case 'D': workingTree.push(new Resource(uri, Status.DELETED/*, raw.rename*/)); break;
+				}
+			});
+
+			this._mergeGroup = new MergeGroup(merge);
+			this._indexGroup = new IndexGroup(index);
+			this._workingTreeGroup = new WorkingTreeGroup(workingTree);
+
+			this._onDidChange.fire(this.resources);
+		});
+	}
+
+	@decorate(throttle)
+	async stage(...resources: Resource[]): Promise<void> {
+		await this.run(Operation.Stage, async () => {
+			const paths = resources.map(r => r.uri.fsPath);
+			await this.repository.add(paths);
+			await this.update();
+		});
+	}
+
+	@decorate(throttle)
+	async unstage(...resources: Resource[]): Promise<void> {
+		await this.run(Operation.Unstage, async () => {
+			const paths = resources.map(r => r.uri.fsPath);
+			await this.repository.revertFiles('HEAD', paths);
+			await this.update();
+		});
+	}
+
+	@decorate(throttle)
+	async commit(message: string, opts: { all?: boolean, amend?: boolean, signoff?: boolean } = Object.create(null)): Promise<void> {
+		await this.run(Operation.Commit, async () => {
+			if (opts.all) {
+				await this.repository.add([]);
+			}
+
+			await this.repository.commit(message, opts);
+			await this.update();
+		});
+	}
+
+	@decorate(throttle)
+	async clean(...resources: Resource[]): Promise<void> {
+		await this.run(Operation.Clean, async () => {
+			const toClean: string[] = [];
+			const toCheckout: string[] = [];
+
+			resources.forEach(r => {
+				switch (r.type) {
+					case Status.UNTRACKED:
+					case Status.IGNORED:
+						toClean.push(r.uri.fsPath);
+						break;
+
+					default:
+						toCheckout.push(r.uri.fsPath);
+						break;
+				}
+			});
+
+			const promises: Promise<void>[] = [];
+
+			if (toClean.length > 0) {
+				promises.push(this.repository.clean(toClean));
+			}
+
+			if (toCheckout.length > 0) {
+				promises.push(this.repository.checkout('', toCheckout));
+			}
+
+			await Promise.all(promises);
+			await this.update();
+		});
+	}
+
+	@decorate(throttle)
+	async branch(name: string): Promise<void> {
+		await this.run(Operation.Branch, async () => {
+			await this.repository.branch(name, true);
+			await this.update();
+		});
+	}
+
+	@decorate(throttle)
+	async checkout(treeish: string): Promise<void> {
+		await this.run(Operation.Checkout, async () => {
+			await this.repository.checkout(treeish, []);
+			await this.update();
+		});
+	}
+
+	@decorate(throttle)
+	async fetch(): Promise<void> {
+		await this.run(Operation.Fetch, async () => {
+			await this.repository.fetch();
+			await this.update();
+		});
+	}
+
+	@decorate(throttle)
+	async sync(): Promise<void> {
+		await this.run(Operation.Sync, async () => {
+			await this.repository.sync();
+			await this.update();
+		});
+	}
+
+	@decorate(throttle)
+	async push(remote?: string, name?: string, options?: IPushOptions): Promise<void> {
+		await this.run(Operation.Push, async () => {
+			await this.repository.push(remote, name, options);
+			await this.update();
+		});
+	}
+
+	private async run(operation: Operation, fn: () => Promise<void>): Promise<void> {
+		this._operations = this._operations.start(operation);
+		this._onRunOperation.fire(operation);
 
 		try {
-			HEAD = await this.repository.getHEAD();
-
-			if (HEAD.name) {
-				try {
-					HEAD = await this.repository.getBranch(HEAD.name);
-				} catch (err) {
-					// noop
-				}
-			}
-		} catch (err) {
-			// noop
+			await fn();
+		} finally {
+			this._operations = this._operations.end(operation);
+			this._onDidRunOperation.fire(operation);
 		}
-
-		const [refs, remotes] = await Promise.all([this.repository.getRefs(), this.repository.getRemotes()]);
-
-		this._HEAD = HEAD;
-		this._refs = refs;
-		this._remotes = remotes;
-
-		const index: Resource[] = [];
-		const workingTree: Resource[] = [];
-		const merge: Resource[] = [];
-
-		status.forEach(raw => {
-			const uri = Uri.file(path.join(this.repositoryRoot, raw.path));
-
-			switch (raw.x + raw.y) {
-				case '??': return workingTree.push(new Resource(uri, Status.UNTRACKED));
-				case '!!': return workingTree.push(new Resource(uri, Status.IGNORED));
-				case 'DD': return merge.push(new Resource(uri, Status.BOTH_DELETED));
-				case 'AU': return merge.push(new Resource(uri, Status.ADDED_BY_US));
-				case 'UD': return merge.push(new Resource(uri, Status.DELETED_BY_THEM));
-				case 'UA': return merge.push(new Resource(uri, Status.ADDED_BY_THEM));
-				case 'DU': return merge.push(new Resource(uri, Status.DELETED_BY_US));
-				case 'AA': return merge.push(new Resource(uri, Status.BOTH_ADDED));
-				case 'UU': return merge.push(new Resource(uri, Status.BOTH_MODIFIED));
-			}
-
-			let isModifiedInIndex = false;
-
-			switch (raw.x) {
-				case 'M': index.push(new Resource(uri, Status.INDEX_MODIFIED)); isModifiedInIndex = true; break;
-				case 'A': index.push(new Resource(uri, Status.INDEX_ADDED)); break;
-				case 'D': index.push(new Resource(uri, Status.INDEX_DELETED)); break;
-				case 'R': index.push(new Resource(uri, Status.INDEX_RENAMED/*, raw.rename*/)); break;
-				case 'C': index.push(new Resource(uri, Status.INDEX_COPIED)); break;
-			}
-
-			switch (raw.y) {
-				case 'M': workingTree.push(new Resource(uri, Status.MODIFIED/*, raw.rename*/)); break;
-				case 'D': workingTree.push(new Resource(uri, Status.DELETED/*, raw.rename*/)); break;
-			}
-		});
-
-		this._mergeGroup = new MergeGroup(merge);
-		this._indexGroup = new IndexGroup(index);
-		this._workingTreeGroup = new WorkingTreeGroup(workingTree);
-
-		this._onDidChange.fire(this.resources);
-	}
-
-	async stage(...resources: Resource[]): Promise<void> {
-		const paths = resources.map(r => r.uri.fsPath);
-		await this.repository.add(paths);
-		await this.update();
-	}
-
-	async unstage(...resources: Resource[]): Promise<void> {
-		const paths = resources.map(r => r.uri.fsPath);
-		await this.repository.revertFiles('HEAD', paths);
-		await this.update();
-	}
-
-	async commit(message: string, opts: { all?: boolean, amend?: boolean, signoff?: boolean } = Object.create(null)): Promise<void> {
-		if (opts.all) {
-			await this.repository.add([]);
-		}
-
-		await this.repository.commit(message, opts);
-		await this.update();
-	}
-
-	async clean(...resources: Resource[]): Promise<void> {
-		const toClean: string[] = [];
-		const toCheckout: string[] = [];
-
-		resources.forEach(r => {
-			switch (r.type) {
-				case Status.UNTRACKED:
-				case Status.IGNORED:
-					toClean.push(r.uri.fsPath);
-					break;
-
-				default:
-					toCheckout.push(r.uri.fsPath);
-					break;
-			}
-		});
-
-		const promises: Promise<void>[] = [];
-
-		if (toClean.length > 0) {
-			promises.push(this.repository.clean(toClean));
-		}
-
-		if (toCheckout.length > 0) {
-			promises.push(this.repository.checkout('', toCheckout));
-		}
-
-		await Promise.all(promises);
-		await this.update();
-	}
-
-	async branch(name: string): Promise<void> {
-		await this.repository.branch(name, true);
-		await this.update();
-	}
-
-	async checkout(treeish: string): Promise<void> {
-		await this.repository.checkout(treeish, []);
-		await this.update();
-	}
-
-	async fetch(): Promise<void> {
-		await this.repository.fetch();
-		await this.update();
-	}
-
-	async sync(): Promise<void> {
-		await this.repository.sync();
-		await this.update();
-	}
-
-	async push(remote?: string, name?: string, options?: IPushOptions): Promise<void> {
-		await this.repository.push(remote, name, options);
-		await this.update();
 	}
 }
