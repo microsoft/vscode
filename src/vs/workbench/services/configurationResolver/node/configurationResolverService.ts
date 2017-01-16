1a304fef1bf6df59fe7bf5ad3c082cf4a163344d
isidor <inikolic@microsoft.com>
2017-01-16 18:17:44 +0100

    fixes #18601

--- src/vs/workbench/services/configurationResolver/node/configurationResolverService.ts
+++ src/vs/workbench/services/configurationResolver/node/configurationResolverService.ts
@@ -218,6 +218,7 @@ export class ConfigurationResolverService implements IConfigurationResolverServi
 			});
 		};
 		findInteractiveVariables(configuration);
+		let substitionCanceled = false;
 
 		const factory: { (): TPromise<any> }[] = Object.keys(interactiveVariablesToSubstitutes).map(interactiveVariable => {
 			return () => {
@@ -233,11 +234,13 @@ export class ConfigurationResolverService implements IConfigurationResolverServi
 						interactiveVariablesToSubstitutes[interactiveVariable].forEach(substitute =>
 							substitute.object[substitute.key] = substitute.object[substitute.key].replace(`\${command.${interactiveVariable}}`, result)
 						);
+					} else {
+						substitionCanceled = true;
 					}
 				});
 			};
 		});
 
-		return sequence(factory).then(() => configuration);
+		return sequence(factory).then(() => substitionCanceled ? null : configuration);
 	}
 }
