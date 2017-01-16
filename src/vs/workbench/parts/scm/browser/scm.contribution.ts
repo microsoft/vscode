26f6a8749ab8bbd552f34fde2862640d3b1d58bf
Joao Moreno <jomo@microsoft.com>
2017-01-16 16:06:02 +0100

    scm : fix menus

--- src/vs/workbench/parts/scm/browser/scm.contribution.ts
+++ src/vs/workbench/parts/scm/browser/scm.contribution.ts
@@ -9,8 +9,13 @@ import { localize } from 'vs/nls';
 import { Registry } from 'vs/platform/platform';
 import { IWorkbenchContributionsRegistry, Extensions as WorkbenchExtensions } from 'vs/workbench/common/contributions';
 import { DirtyDiffDecorator } from './dirtydiffDecorator';
-import { ViewletRegistry, Extensions as ViewletExtensions, ViewletDescriptor } from 'vs/workbench/browser/viewlet';
+import { ViewletRegistry, Extensions as ViewletExtensions, ViewletDescriptor, ToggleViewletAction } from 'vs/workbench/browser/viewlet';
 import { VIEWLET_ID } from 'vs/workbench/parts/scm/common/scm';
+import { IWorkbenchActionRegistry, Extensions as WorkbenchActionExtensions } from 'vs/workbench/common/actionRegistry';
+import { KeyMod, KeyCode } from 'vs/base/common/keyCodes';
+import { SyncActionDescriptor } from 'vs/platform/actions/common/actions';
+import { IViewletService } from 'vs/workbench/services/viewlet/browser/viewlet';
+import { IWorkbenchEditorService } from 'vs/workbench/services/editor/common/editorService';
 
 Registry.as<IWorkbenchContributionsRegistry>(WorkbenchExtensions.Workbench)
 	.registerWorkbenchContribution(DirtyDiffDecorator);
@@ -26,3 +31,24 @@ const viewletDescriptor = new ViewletDescriptor(
 
 Registry.as<ViewletRegistry>(ViewletExtensions.Viewlets)
 	.registerViewlet(viewletDescriptor);
+
+class OpenSCMViewletAction extends ToggleViewletAction {
+	public static ID = VIEWLET_ID;
+	public static LABEL = localize('toggleGitViewlet', "Show Git");
+
+	constructor(id: string, label: string, @IViewletService viewletService: IViewletService, @IWorkbenchEditorService editorService: IWorkbenchEditorService) {
+		super(id, label, VIEWLET_ID, viewletService, editorService);
+	}
+}
+
+// Register Action to Open Viewlet
+Registry.as<IWorkbenchActionRegistry>(WorkbenchActionExtensions.WorkbenchActions).registerWorkbenchAction(
+	new SyncActionDescriptor(OpenSCMViewletAction, VIEWLET_ID, localize('toggleSCMViewlet', "Show SCM"), {
+		primary: null,
+		win: { primary: KeyMod.CtrlCmd | KeyMod.Shift | KeyCode.KEY_G },
+		linux: { primary: KeyMod.CtrlCmd | KeyMod.Shift | KeyCode.KEY_G },
+		mac: { primary: KeyMod.WinCtrl | KeyMod.Shift | KeyCode.KEY_G }
+	}),
+	'View: Show SCM',
+	localize('view', "View")
+);
