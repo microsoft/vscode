43639ae34d52fcbc1b63a450d590334aa9c25e07
Artem Govorov <artem.govorov@gmail.com>
2017-01-19 16:53:22 +1000

    Add Toggle Output Scroll Lock action

--- src/vs/workbench/parts/output/browser/outputActions.ts
+++ src/vs/workbench/parts/output/browser/outputActions.ts
@@ -48,6 +48,23 @@ export class ClearOutputAction extends Action {
 	}
 }
 
+export class ToggleOutputScrollLockAction extends Action {
+
+	public static ID = 'workbench.output.action.toggleOutputScrollLock';
+	public static LABEL = nls.localize('toggleOutputScrollLock', "Toggle Output Scroll Lock");
+
+	constructor(id: string, label: string,
+		@IOutputService private outputService: IOutputService) {
+		super(id, label, 'output-action toggle-output-scroll-lock');
+	}
+
+	public run(): TPromise<any> {
+		this.outputService.getActiveChannel().toggleScrollLock();
+
+		return TPromise.as(true);
+	}
+}
+
 export class SwitchOutputAction extends Action {
 
 	public static ID = 'workbench.output.action.switchBetweenOutputs';
