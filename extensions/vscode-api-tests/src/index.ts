133db393dba1a653335311e53ec8eeb169cd0bf2
Johannes Rieken <johannes.rieken@gmail.com>
2017-01-20 10:50:18 +0100

    don't double install vscode.d.ts

--- extensions/vscode-api-tests/src/index.ts
+++ extensions/vscode-api-tests/src/index.ts
@@ -3,8 +3,6 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-/// <reference path="../../../src/vs/vscode.d.ts" />
-
 //
 // PLEASE DO NOT MODIFY / DELETE UNLESS YOU KNOW WHAT YOU ARE DOING
 //
