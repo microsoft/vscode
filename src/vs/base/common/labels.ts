2fd302f0142483ecefedcc7b621de72d0e8489b4
hun1ahpu <hun1ahpu@gmail.com>
2017-01-21 12:50:17 +0100

    Creating 2 separate tests for windows and not

--- src/vs/base/common/labels.ts
+++ src/vs/base/common/labels.ts
@@ -83,22 +83,19 @@ function getPath(arg1: URI | string | IWorkspaceProvider): string {
  */
 export function shorten(paths: string[]): string[] {
 	const ellipsis = '\u2026';
-	const separator = '/';
 	const shortenedPaths: string[] = new Array(paths.length);
-
-	paths = paths.map(path => normalize(path));
 
 	// for every path
 	let match = false;
 	for (let pathIndex = 0; pathIndex < paths.length; pathIndex++) {
-		const segments: string[] = paths[pathIndex].split(separator);
+		const segments: string[] = paths[pathIndex].split(nativeSep);
 		match = true;
 
 		// pick the first shortest subpath found
 		for (let subpathLength = 1; match && subpathLength <= segments.length; subpathLength++) {
 			for (let start = segments.length - subpathLength; match && start >= 0; start--) {
 				match = false;
-				const subpath = segments.slice(start, start + subpathLength).join(separator);
+				const subpath = segments.slice(start, start + subpathLength).join(nativeSep);
 
 				// that is unique to any other path
 				for (let otherPathIndex = 0; !match && otherPathIndex < paths.length; otherPathIndex++) {
@@ -116,11 +113,11 @@ export function shorten(paths: string[]): string[] {
 				if (!match) {
 					let result = subpath;
 					if (start + subpathLength < segments.length) {
-						result = result + separator + ellipsis;
+						result = result + nativeSep + ellipsis;
 					}
 
 					if (start > 0) {
-						result = ellipsis + separator + result;
+						result = ellipsis + nativeSep + result;
 					}
 
 					shortenedPaths[pathIndex] = result;
