814f92341aa9d3f772b17bf6d46a4e04f2c96959
Alex Dima <alexdima@microsoft.com>
2017-01-21 14:31:50 +0100

    Render find options by themselves temporarily (#18973)

--- src/vs/editor/contrib/find/browser/find.ts
+++ src/vs/editor/contrib/find/browser/find.ts
@@ -10,12 +10,14 @@ import { IContextKeyService } from 'vs/platform/contextkey/common/contextkey';
 import { ICodeEditor } from 'vs/editor/browser/editorBrowser';
 import { editorContribution } from 'vs/editor/browser/editorBrowserExtensions';
 import { FindWidget, IFindController } from 'vs/editor/contrib/find/browser/findWidget';
+import { FindOptionsWidget } from 'vs/editor/contrib/find/browser/findOptionsWidget';
 import { CommonFindController, FindStartFocusAction, IFindStartOptions } from 'vs/editor/contrib/find/common/findController';
 
 @editorContribution
 export class FindController extends CommonFindController implements IFindController {
 
 	private _widget: FindWidget;
+	private _findOptionsWidget: FindOptionsWidget;
 
 	constructor(
 		editor: ICodeEditor,
@@ -26,6 +28,7 @@ export class FindController extends CommonFindController implements IFindControl
 		super(editor, contextKeyService);
 
 		this._widget = this._register(new FindWidget(editor, this, this._state, contextViewService, keybindingService, contextKeyService));
+		this._findOptionsWidget = this._register(new FindOptionsWidget(editor, this._state, keybindingService));
 	}
 
 	protected _start(opts: IFindStartOptions): void {
@@ -42,7 +45,7 @@ export class FindController extends CommonFindController implements IFindControl
 		if (this._state.isRevealed) {
 			this._widget.highlightFindOptions();
 		} else {
-			// console.log('TODO@Alex - animateFindOptions');
+			this._findOptionsWidget.highlightFindOptions();
 		}
 	}
 }
