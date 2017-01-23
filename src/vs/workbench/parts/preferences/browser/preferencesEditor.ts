34fd1c67e4584314a37542dcfd442eb8fe90dd77
kieferrm <kieferrm@gmail.com>
2017-01-23 10:47:22 -0800

    Improve wording of error

--- src/vs/workbench/parts/preferences/browser/preferencesEditor.ts
+++ src/vs/workbench/parts/preferences/browser/preferencesEditor.ts
@@ -1370,7 +1370,7 @@ class UnTrustedWorkspaceSettingsRenderer extends Disposable {
 						startColumn: setting.keyRange.startColumn,
 						endLineNumber: setting.keyRange.endLineNumber,
 						endColumn: setting.keyRange.endColumn,
-						message: nls.localize('untrustedWorkspaceWithExectuables', "`{0}` is an executable property and since current workspace is untrusted, it is ignored. To enable it, open User settings and add current workspace to trusted workspaces list using setting `security.workspacesTrustedToSpecifyExecutables`", setting.key)
+						message: nls.localize('untrustedWorkspaceWithExectuables', "`{0}` specifies an executable. It is ignored because the workspace is untrusted. To mark a workspace as trusted, open the User settings and add this workspace's path  to `security.workspacesTrustedToSpecifyExecutables`", setting.key)
 					});
 				}
 			}
