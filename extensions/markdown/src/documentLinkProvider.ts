f7697a7f37cfd165fd1739906765c10c9012e1b8
Matt Bierner <matb@microsoft.com>
2017-01-27 14:36:28 -0800

    Support In-Document links inside of the markdown editor (#19411)
    * Support In Document links inside of the markdown editor
    
    Fixes #17288
    
    * Cleaning up code to reduce duplication

--- extensions/markdown/src/documentLinkProvider.ts
+++ extensions/markdown/src/documentLinkProvider.ts
@@ -8,14 +8,21 @@
 import * as vscode from 'vscode';
 import * as path from 'path';
 
+import { MarkdownEngine } from './markdownEngine';
+import { TableOfContentProvider } from './tableOfContentsProvider';
+
 export default class MarkdownDocumentLinkProvider implements vscode.DocumentLinkProvider {
 
 	private _linkPattern = /(\[[^\]]*\]\(\s*?)(\S+?)(\s+[^\)]*)?\)/g;
+
+	constructor(private engine: MarkdownEngine) { }
 
 	public provideDocumentLinks(document: vscode.TextDocument, token: vscode.CancellationToken): vscode.DocumentLink[] {
 		const results: vscode.DocumentLink[] = [];
 		const base = path.dirname(document.uri.fsPath);
 		const text = document.getText();
+
+		const toc = new TableOfContentProvider(this.engine, document);
 
 		this._linkPattern.lastIndex = 0;
 		let match: RegExpMatchArray | null;
@@ -26,20 +33,9 @@ export default class MarkdownDocumentLinkProvider implements vscode.DocumentLink
 			const linkStart = document.positionAt(offset);
 			const linkEnd = document.positionAt(offset + link.length);
 			try {
-				let uri = vscode.Uri.parse(link);
-				if (!uri.scheme) {
-					// assume it must be a file
-					let file;
-					if (uri.path[0] === '/') {
-						file = path.join(vscode.workspace.rootPath, uri.path);
-					} else {
-						file = path.join(base, uri.path);
-					}
-					uri = vscode.Uri.file(file);
-				}
 				results.push(new vscode.DocumentLink(
 					new vscode.Range(linkStart, linkEnd),
-					uri));
+					this.normalizeLink(link, base, toc)));
 			} catch (e) {
 				// noop
 			}
@@ -47,4 +43,27 @@ export default class MarkdownDocumentLinkProvider implements vscode.DocumentLink
 
 		return results;
 	}
-};
+
+	private normalizeLink(link: string, base: string, toc: TableOfContentProvider): vscode.Uri {
+		let uri = vscode.Uri.parse(link);
+		if (!uri.scheme) {
+			if (uri.fragment && !uri.path) {
+				// local link
+				const line = toc.lookup(uri.fragment);
+				if (!isNaN(line)) {
+					return vscode.Uri.parse(`command:revealLine?${encodeURIComponent(JSON.stringify({ lineNumber: line, at: 'top' }))}`);
+				}
+			}
+
+			// assume it must be a file
+			let file;
+			if (uri.path[0] === '/') {
+				file = path.join(vscode.workspace.rootPath, uri.path);
+			} else {
+				file = path.join(base, uri.path);
+			}
+			uri = vscode.Uri.file(file);
+		}
+		return uri;
+	}
+}
