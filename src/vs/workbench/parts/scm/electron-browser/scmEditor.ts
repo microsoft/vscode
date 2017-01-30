1e99f8d8f40ae1fae67e411588a3d0a845fa78c0
Joao Moreno <jomo@microsoft.com>
2017-01-30 19:45:24 +0000

    extract scm editor

--- src/vs/workbench/parts/scm/electron-browser/scmEditor.ts
+++ src/vs/workbench/parts/scm/electron-browser/scmEditor.ts
@@ -5,7 +5,8 @@
 
 'use strict';
 
-import { IEditorOptions } from 'vs/editor/common/editorCommon';
+import { IModel, IEditorOptions, IDimension } from 'vs/editor/common/editorCommon';
+import { memoize } from 'vs/base/common/decorators';
 import { EditorAction, CommonEditorRegistry } from 'vs/editor/common/editorCommonExtensions';
 import { ICodeEditorService } from 'vs/editor/common/services/codeEditorService';
 import { IEditorContributionCtor } from 'vs/editor/browser/editorBrowser';
@@ -19,8 +20,14 @@ import { ContextMenuController } from 'vs/editor/contrib/contextmenu/browser/con
 import { SuggestController } from 'vs/editor/contrib/suggest/browser/suggestController';
 import { SnippetController } from 'vs/editor/contrib/snippet/common/snippetController';
 import { TabCompletionController } from 'vs/editor/contrib/suggest/browser/tabCompletion';
+import { IModelService } from 'vs/editor/common/services/modelService';
+import { IThemeService } from 'vs/workbench/services/themes/common/themeService';
+import { append, $ } from 'vs/base/browser/dom';
+import URI from 'vs/base/common/uri';
+import { IDisposable, dispose } from 'vs/base/common/lifecycle';
+import Event, { Emitter } from 'vs/base/common/event';
 
-export class SCMCommitEditor extends CodeEditorWidget {
+class SCMCodeEditorWidget extends CodeEditorWidget {
 
 	constructor(
 		domElement: HTMLElement,
@@ -46,5 +53,84 @@ export class SCMCommitEditor extends CodeEditorWidget {
 
 	protected _getActions(): EditorAction[] {
 		return CommonEditorRegistry.getEditorActions();
+	}
+}
+
+export class SCMEditor {
+
+	private editor: SCMCodeEditorWidget;
+	private model: IModel;
+	private disposables: IDisposable[] = [];
+
+	@memoize
+	get onDidChangeContent(): Event<void> {
+		let listener: IDisposable;
+		const emitter = new Emitter<void>({
+			onFirstListenerAdd: () => listener = this.model.onDidChangeContent(() => emitter.fire()),
+			onLastListenerRemove: () => dispose(listener)
+		});
+
+		return emitter.event;
+	}
+
+	private get editorOptions(): IEditorOptions {
+		return {
+			wrappingColumn: 0,
+			overviewRulerLanes: 0,
+			glyphMargin: false,
+			lineNumbers: 'off',
+			folding: false,
+			selectOnLineNumbers: false,
+			selectionHighlight: false,
+			scrollbar: {
+				horizontal: 'hidden'
+			},
+			lineDecorationsWidth: 0,
+			scrollBeyondLastLine: false,
+			theme: this.themeService.getColorTheme().id,
+			renderLineHighlight: 'none',
+			fixedOverflowWidgets: true,
+			acceptSuggestionOnEnter: false,
+			wordWrap: true
+		};
+	}
+
+	constructor(
+		container: HTMLElement,
+		@IThemeService private themeService: IThemeService,
+		@IInstantiationService private instantiationService: IInstantiationService,
+		@IModelService private modelService: IModelService
+	) {
+		this.editor = this.instantiationService.createInstance(SCMCodeEditorWidget, container, this.editorOptions);
+		this.model = this.modelService.createModel('', null, URI.parse(`scm:input`));
+		this.editor.setModel(this.model);
+
+		this.themeService.onDidColorThemeChange(e => this.editor.updateOptions(this.editorOptions), null, this.disposables);
+	}
+
+	get lineHeight(): number {
+		return this.editor.getConfiguration().lineHeight;
+	}
+
+	// TODO@joao TODO@alex isn't there a better way to get the number of lines?
+	get lineCount(): number {
+		const modelLength = this.model.getValueLength();
+		const lastPosition = this.model.getPositionAt(modelLength);
+		const lastLineTop = this.editor.getTopForPosition(lastPosition.lineNumber, lastPosition.column);
+		const viewHeight = lastLineTop + this.lineHeight;
+
+		return viewHeight / this.lineHeight;
+	}
+
+	layout(dimension: IDimension): void {
+		this.editor.layout(dimension);
+	}
+
+	focus(): void {
+		this.editor.focus();
+	}
+
+	dispose(): void {
+		this.disposables = dispose(this.disposables);
 	}
 }
