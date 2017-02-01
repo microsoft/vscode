ae3bf7ef79f45ccbca7477c4ba2837c09e181a14
Matt Bierner <matb@microsoft.com>
2017-02-01 12:34:50 -0800

    Remove unneeded d.ts files from extensions (#19653)
    * Remove unneeded d.ts files from extensions
    
    Moves most extensions to use the lib files for the standard library that typescript provides.
    
    * Remove a few more node.d.ts references

--- extensions/php/src/typings/refs.d.ts
+++ extensions/php/src/typings/refs.d.ts
@@ -5,4 +5,3 @@
 
 /// <reference path='../../../../src/vs/vscode.d.ts'/>
 /// <reference path='../../../../extensions/node.d.ts'/>
-/// <reference path='../../../../extensions/lib.core.d.ts'/>
--- extensions/vscode-api-tests/src/typings/ref.d.ts
+++ extensions/vscode-api-tests/src/typings/ref.d.ts
@@ -7,4 +7,3 @@
 /// <reference path='../../../../src/typings/mocha.d.ts'/>
 /// <reference path='../../../../extensions/declares.d.ts'/>
 /// <reference path='../../../../extensions/node.d.ts'/>
-/// <reference path='../../../../extensions/lib.core.d.ts'/>
