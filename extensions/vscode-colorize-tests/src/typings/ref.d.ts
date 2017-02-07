38fbba35ffa282fc523791002062dec6c93451d7
Matt Bierner <matb@microsoft.com>
2017-02-07 15:57:22 -0800

    Use types tsconfig option where possible

--- extensions/vscode-colorize-tests/src/typings/ref.d.ts
+++
@@ -1,7 +0,0 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
-
-/// <reference path='../../node_modules/@types/mocha/index.d.ts'/>
-/// <reference types='@types/node'/>
