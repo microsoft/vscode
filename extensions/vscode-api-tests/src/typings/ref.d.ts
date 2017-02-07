6b033cf931cafac235a60a8f07bedbdfd28b92b2
Matt Bierner <matb@microsoft.com>
2017-02-07 15:38:04 -0800

    Use reference @types/node in extensions

--- extensions/vscode-api-tests/src/typings/ref.d.ts
+++ extensions/vscode-api-tests/src/typings/ref.d.ts
@@ -5,4 +5,4 @@
 
 /// <reference path="../../../../src/vs/vscode.d.ts" />
 /// <reference path='../../node_modules/@types/mocha/index.d.ts'/>
-/// <reference path='../../node_modules/@types/node/index.d.ts'/>
+/// <reference types='@types/node'/>
