42335c57f5eb206bff3724e10349c8538277976f
Matt Bierner <matb@microsoft.com>
2017-02-07 16:30:34 -0800

    Revert "Use types tsconfig option where possible"
    This reverts commit 38fbba35ffa282fc523791002062dec6c93451d7.

--- extensions/vscode-colorize-tests/src/typings/ref.d.ts
+++ extensions/vscode-colorize-tests/src/typings/ref.d.ts
@@ -3,6 +3,5 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-/// <reference path="../../../../src/vs/vscode.d.ts" />
 /// <reference path='../../node_modules/@types/mocha/index.d.ts'/>
 /// <reference types='@types/node'/>
