81c028ea3de4cdff593da5e3ae0a6c1c46fdd5f6
Johannes Rieken <johannes.rieken@gmail.com>
2017-02-09 13:05:01 +0100

    fix #19562

--- src/vs/editor/contrib/quickFix/test/common/quickFixModel.test.ts
+++ src/vs/editor/contrib/quickFix/test/common/quickFixModel.test.ts
@@ -5,7 +5,7 @@
 'use strict';
 
 import * as assert from 'assert';
-import { ICommonCodeEditor } from 'vs/editor/common/editorCommon';
+import { ICommonCodeEditor, IRange } from 'vs/editor/common/editorCommon';
 import URI from 'vs/base/common/uri';
 import { TPromise } from 'vs/base/common/winjs.base';
 import { Model } from 'vs/editor/common/model/model';
@@ -124,6 +124,49 @@ suite('QuickFix', () => {
 			reg.dispose();
 			oracle.dispose();
 			assert.equal(counter, 2);
+		});
+	});
+
+	test('Oracle -> selection wins over marker', () => {
+
+		let range: IRange;
+		let reg = CodeActionProviderRegistry.register(languageIdentifier.language, {
+			provideCodeActions(doc, _range) {
+				range = _range;
+				return [];
+			}
+		});
+
+		markerService.changeOne('fake', uri, [{
+			startLineNumber: 1, startColumn: 1, endLineNumber: 1, endColumn: 6,
+			message: 'error',
+			severity: 1,
+			code: '',
+			source: ''
+		}]);
+
+		let fixes: TPromise<any>[] = [];
+		let oracle = new QuickFixOracle(editor, markerService, e => {
+			fixes.push(e.fixes);
+		});
+
+		editor.setSelection({ startLineNumber: 1, startColumn: 1, endLineNumber: 1, endColumn: 13 });
+
+		return TPromise.join(fixes).then(_ => {
+
+			// assert selection
+			assert.deepEqual(range, { startLineNumber: 1, startColumn: 1, endLineNumber: 1, endColumn: 13 });
+
+			range = undefined;
+			editor.setSelection({ startLineNumber: 1, startColumn: 2, endLineNumber: 1, endColumn: 2 });
+
+			return TPromise.join(fixes).then(_ => {
+				reg.dispose();
+				oracle.dispose();
+
+				// assert marker
+				assert.deepEqual(range, { startLineNumber: 1, startColumn: 1, endLineNumber: 1, endColumn: 6 });
+			});
 		});
 	});
 
