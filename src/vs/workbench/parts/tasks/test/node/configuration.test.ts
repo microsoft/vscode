eb31c194ce7542f1d7168b08fccaed60db50f09f
Dirk Baeumer <dirkb@microsoft.com>
2017-02-10 17:29:42 +0100

    First version to support keybindings for tasks

--- src/vs/workbench/parts/tasks/test/node/configuration.test.ts
+++ src/vs/workbench/parts/tasks/test/node/configuration.test.ts
@@ -12,7 +12,7 @@ import * as Platform from 'vs/base/common/platform';
 import { ProblemMatcher, FileLocationKind, ProblemPattern, ApplyToKind } from 'vs/platform/markers/common/problemMatcher';
 
 import * as TaskSystem from 'vs/workbench/parts/tasks/common/taskSystem';
-import { parse, ParseResult, ILogger, ExternalTaskRunnerConfiguration } from 'vs/workbench/parts/tasks/node/processRunnerConfiguration';
+import { parse, ParseResult, ILogger, ExternalTaskRunnerConfiguration } from 'vs/workbench/parts/tasks/common/taskConfiguration';
 
 class Logger implements ILogger {
 	public receivedMessage: boolean = false;
