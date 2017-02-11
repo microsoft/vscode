15961d62b9a696e7e5ee6d68e439e21df3624692
jordanmkasla2009 <jordanmkasla2009@gmail.com>
2017-02-11 02:24:44 -0500

    add comment

--- src/vs/platform/windows/electron-main/windowsService.ts
+++ src/vs/platform/windows/electron-main/windowsService.ts
@@ -274,7 +274,7 @@ export class WindowsService implements IWindowsService, IDisposable {
 	private parseURIForOpen(uri: URI): string {
 		if (uri.authority === 'file') {
 			let path = uri.path.substr(1);
-			if (path.slice(0, path.indexOf('/')).length === 1) {
+			if (path.slice(0, path.indexOf('/')).length === 1) { // add a colon if the uri.path contains a valid drive letter.
 				path = path.slice(0, path.indexOf('/')) + ':' + path.slice(path.indexOf('/'));
 			}
 			else {
