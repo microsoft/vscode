b5de879545a0bff5070b6a8c9e7c5c56b86f41cb
Matt Bierner <matb@microsoft.com>
2017-02-13 16:57:26 -0800

    Add Status Bar Item for JSConfig/TSConfig
    Fixes #20356
    
    Adds a status bar item that shows which jsconfig/tsconfig a ts or js file currently belongs to. This triggers the `typescript.goToProjectConfig` command when clicked, which either jumps to the config file or shows an alert about creating a configuration file

--- extensions/typescript/src/typescriptMain.ts
+++ extensions/typescript/src/typescriptMain.ts
@@ -44,6 +44,7 @@ import * as BuildStatus from './utils/buildStatus';
 import * as ProjectStatus from './utils/projectStatus';
 import TypingsStatus, { AtaProgressReporter } from './utils/typingsStatus';
 import * as VersionStatus from './utils/versionStatus';
+import ProjectConfigStatus from './utils/projectConfigStatus';
 
 interface LanguageDescription {
 	id: string;
@@ -110,7 +111,9 @@ export function activate(context: ExtensionContext): void {
 	context.subscriptions.push(commands.registerCommand('javascript.goToProjectConfig', goToProjectConfig.bind(null, false)));
 
 	window.onDidChangeActiveTextEditor(VersionStatus.showHideStatus, null, context.subscriptions);
+
 	client.onReady().then(() => {
+		context.subscriptions.push(new ProjectConfigStatus(client));
 		context.subscriptions.push(ProjectStatus.create(client,
 			path => new Promise(resolve => setTimeout(() => resolve(clientHost.handles(path)), 750)),
 			context.workspaceState));
