2ce2d6439d7e94bc6f73d04c9e915ad919c39005
Matt Bierner <matb@microsoft.com>
2017-02-15 14:04:36 -0800

    Autofill for JSDocs (#20625)
    * Add autofill for JSDocs
    
    Adds prototype of autofill for jsdoc comments. For code like:
    
    ```js
        /**|
        function mul(x, y) {
            return x * y;
        }
    ```
    
    When you press return, we will complete it to:
    
    ```js
        /**
         *
         * @param x
         * @param y
         */
        function mul(x, y) {
            return x * y;
        }
    ```
    
    In Typescript and to:
    
    ```js
        /**
         *
         * @param {any} x
         * @param {any} y
         */
        function mul(x, y) {
            return x + y;
        }
    ```
    
    In JavaScript
    
    * Handle revert case better
    
    * Disable on enter when suggestion is visible

---
+++ extensions/typescript/src/utils/JsDocCompletionHelper.ts
@@ -0,0 +1,101 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+'use strict';
+
+import { TextEditor, Position, Range, Selection } from 'vscode';
+
+import { ITypescriptServiceClient } from '../typescriptService';
+
+import { FileLocationRequestArgs } from '../protocol';
+
+export default class JsDocCompletionHelper {
+
+	constructor(
+		private client: ITypescriptServiceClient,
+	) { }
+
+	public tryCompleteJsDoc(editor: TextEditor, position: Position): Thenable<boolean> {
+		const file = this.client.normalizePath(editor.document.uri);
+		if (!file) {
+			return Promise.resolve(false);
+		}
+
+		const line = editor.document.lineAt(position.line).text;
+
+		// Ensure line starts with '/**' then cursor
+		const prefix = line.slice(0, position.character).match(/^\s*(\/\*\*+)$/);
+		if (!prefix) {
+			return Promise.resolve(false);
+		}
+
+		// Ensure there is no content after the cursor besides possibly the end of the comment
+		const suffix = line.slice(position.character).match(/^\s*\**\/?$/);
+		if (!suffix) {
+			return Promise.resolve(false);
+		}
+
+		const start = position.translate(0, -prefix[1].length);
+		return editor.edit(
+			edits => {
+				edits.delete(new Range(start, new Position(start.line, line.length)));
+			}, {
+				undoStopBefore: true,
+				undoStopAfter: false
+			}
+		).then(removedComment => {
+			if (!removedComment) {
+				// Edit failed, nothing to revert.
+				return false;
+			}
+
+			let cancelled = false;
+			const timer = setTimeout(() => {
+				cancelled = true;
+			}, 250);
+
+			const args: FileLocationRequestArgs = {
+				file: file,
+				line: start.line + 1,
+				offset: start.character + 1
+			};
+
+			return this.client.execute('docCommentTemplate', args)
+				.then(res => {
+					clearTimeout(timer);
+					if (cancelled || !res || !res.body) {
+						return false;
+					}
+					const commentText = res.body.newText;
+					return editor.edit(
+						edits => edits.insert(start, commentText),
+						{ undoStopBefore: false, undoStopAfter: true });
+				}, () => {
+					clearTimeout(timer);
+					return false;
+				})
+				.then(didInsertComment => {
+					if (didInsertComment) {
+						const newCursorPosition = new Position(start.line + 1, editor.document.lineAt(start.line + 1).text.length);
+						editor.selection = new Selection(newCursorPosition, newCursorPosition);
+						return true;
+					}
+
+					// Revert to the original line content and restore position
+					return editor.edit(
+						edits => {
+							edits.insert(start, prefix[1] + suffix[0]);
+						}, {
+							undoStopBefore: false,
+							undoStopAfter: true
+						}
+					).then(() => {
+						editor.selection = new Selection(position, position);
+						return false;
+					});
+				});
+		});
+	}
+}
