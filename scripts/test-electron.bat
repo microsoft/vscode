efe12acaf828eb5dee632698f6e7954dd86eb716
Johannes Rieken <johannes.rieken@gmail.com>
2017-02-16 15:26:48 +0100

    add test script (win)

---
+++ scripts/test-electron.bat
@@ -0,0 +1,17 @@
+@echo off
+setlocal
+
+pushd %~dp0\..
+
+for /f "tokens=2 delims=:," %%a in ('findstr /R /C:"\"nameShort\":.*" product.json') do set NAMESHORT=%%~a
+set NAMESHORT=%NAMESHORT: "=%
+set NAMESHORT=%NAMESHORT:"=%.exe
+set CODE=".build\electron\%NAMESHORT%"
+
+rem Run tests in electron
+%CODE% .\test\electron\index.js %*
+
+popd
+
+endlocal
+exit /b %errorlevel%
