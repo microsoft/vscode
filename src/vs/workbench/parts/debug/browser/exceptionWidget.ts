321c7a592c3e91108193cc629e543f7705bba938
Michel Kaporin <michel.kaporin@gmail.com>
2017-02-17 11:21:55 +0100

    Added correct layouting.

--- src/vs/workbench/parts/debug/browser/exceptionWidget.ts
+++ src/vs/workbench/parts/debug/browser/exceptionWidget.ts
@@ -9,10 +9,12 @@ import { ZoneWidget } from 'vs/editor/contrib/zoneWidget/browser/zoneWidget';
 import { ICodeEditor } from 'vs/editor/browser/editorBrowser';
 import { IContextViewService } from 'vs/platform/contextview/browser/contextView';
 import { IDebugService } from 'vs/workbench/parts/debug/common/debug';
+import * as strings from 'vs/base/common/strings';
 
 import * as dom from 'vs/base/browser/dom';
 
 export class ExceptionWidget extends ZoneWidget {
+
 
 	constructor(editor: ICodeEditor, private lineNumber: number,
 		@IContextViewService private contextViewService: IContextViewService,
@@ -27,10 +29,15 @@ export class ExceptionWidget extends ZoneWidget {
 		dom.addClass(container, 'exception-widget monaco-editor-background');
 
 		const thread = this.debugService.getViewModel().focusedThread;
-
 		if (thread && thread.stoppedDetails) {
 			const exceptionThrown = nls.localize('exceptionThrown', "Exception occured: {0}", thread.stoppedDetails.text);
 			container.textContent = exceptionThrown;
 		}
+	}
+
+	protected _doLayout(heightInPixel: number, widthInPixel: number): void {
+		var height = Math.ceil(this.editor.getConfiguration().lineHeight * 1.2);
+		this.container.style.height = strings.format('{0}px', height);
+		this.container.style.lineHeight = this.container.style.height;
 	}
 }
