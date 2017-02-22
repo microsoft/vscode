b434faa8302db9a5d0b70ab1a48accbb149ca2bd
Alex Dima <alexdima@microsoft.com>
2017-02-22 21:26:45 +0100

    Fixes #20511: Do not render the entire token text

--- src/vs/editor/contrib/inspectTMScopes/electron-browser/inspectTMScopes.ts
+++ src/vs/editor/contrib/inspectTMScopes/electron-browser/inspectTMScopes.ts
@@ -125,6 +125,9 @@ interface IDecodedMetadata {
 }
 
 function renderTokenText(tokenText: string): string {
+	if (tokenText.length > 40) {
+		tokenText = tokenText.substr(0, 20) + '...' + tokenText.substr(tokenText.length - 20);
+	}
 	let result: string = '';
 	for (let charIndex = 0, len = tokenText.length; charIndex < len; charIndex++) {
 		let charCode = tokenText.charCodeAt(charIndex);
