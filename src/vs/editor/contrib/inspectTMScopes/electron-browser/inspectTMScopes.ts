a4e3fba911bc5e6f5a661be942839be440911b57
Alex Dima <alexdima@microsoft.com>
2017-02-24 08:48:39 +0100

    Use ellipsis when token text is large (#20511)

--- src/vs/editor/contrib/inspectTMScopes/electron-browser/inspectTMScopes.ts
+++ src/vs/editor/contrib/inspectTMScopes/electron-browser/inspectTMScopes.ts
@@ -126,7 +126,7 @@ interface IDecodedMetadata {
 
 function renderTokenText(tokenText: string): string {
 	if (tokenText.length > 40) {
-		tokenText = tokenText.substr(0, 20) + '...' + tokenText.substr(tokenText.length - 20);
+		tokenText = tokenText.substr(0, 20) + '…' + tokenText.substr(tokenText.length - 20);
 	}
 	let result: string = '';
 	for (let charIndex = 0, len = tokenText.length; charIndex < len; charIndex++) {
