5a69878889a7d683fc071f8d904e84481a527491
Daniel Imms <daimms@microsoft.com>
2017-03-01 09:54:36 -0800

    Implement terminal link validation
    Fixes #21226

--- src/vs/workbench/parts/terminal/test/electron-browser/terminalLinkHandler.test.ts
+++ src/vs/workbench/parts/terminal/test/electron-browser/terminalLinkHandler.test.ts
@@ -9,10 +9,16 @@ import * as assert from 'assert';
 import { Platform } from 'vs/base/common/platform';
 import { TerminalLinkHandler } from 'vs/workbench/parts/terminal/electron-browser/terminalLinkHandler';
 
+class TestTerminalLinkHandler extends TerminalLinkHandler {
+	public get localLinkRegex(): RegExp {
+		return this._localLinkRegex;
+	}
+}
+
 suite('Workbench - TerminalLinkHandler', () => {
 	suite('localLinkRegex', () => {
 		test('Windows', () => {
-			const regex = new TerminalLinkHandler(Platform.Windows, null, null).localLinkRegex;
+			const regex = new TestTerminalLinkHandler(Platform.Windows, null, null).localLinkRegex;
 			function testLink(link: string) {
 				assert.equal(` ${link} `.match(regex)[1], link);
 				assert.equal(`:${link}:`.match(regex)[1], link);
@@ -33,7 +39,7 @@ suite('Workbench - TerminalLinkHandler', () => {
 		});
 
 		test('Linux', () => {
-			const regex = new TerminalLinkHandler(Platform.Linux, null, null).localLinkRegex;
+			const regex = new TestTerminalLinkHandler(Platform.Linux, null, null).localLinkRegex;
 			function testLink(link: string) {
 				assert.equal(` ${link} `.match(regex)[1], link);
 				assert.equal(`:${link}:`.match(regex)[1], link);
