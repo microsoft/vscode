58ec3f08c0fbfd39d4679d3baf537075b7a4d9df
Joao Moreno <jomo@microsoft.com>
2017-03-01 10:46:43 +0100

    fixes #21576

--- scripts/npm.bat
+++ scripts/npm.bat
@@ -2,7 +2,7 @@
 setlocal
 
 set npm_config_disturl="https://atom.io/download/atom-shell"
-for /f "tokens=2 delims=:, " %%a in ('findstr /R /C:"\"electronVersion\":.*" %~dp0..\package.json') do set npm_config_target=%%~a
+for /f "tokens=2 delims=:, " %%a in ('findstr /R /C:"\"electronVersion\":.*" "%~dp0..\package.json"') do set npm_config_target=%%~a
 set npm_config_arch="ia32"
 set npm_config_runtime="electron"
 set HOME=~\.electron-gyp
