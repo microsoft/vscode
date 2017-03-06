0495fcbc8068becf4886b2b8eac005dde9095692
Benjamin Pasero <benjpas@microsoft.com>
2017-03-06 16:32:19 +0100

    build build/lib folder with latest

--- build/lib/compilation.js
+++ build/lib/compilation.js
@@ -3,6 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 'use strict';
+Object.defineProperty(exports, "__esModule", { value: true });
 var gulp = require("gulp");
 var tsb = require("gulp-tsb");
 var es = require("event-stream");
