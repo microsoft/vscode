72c87df7fe008c086de701f7bd7088a151169652
Benjamin Pasero <benjpas@microsoft.com>
2017-03-13 16:41:21 +0100

    fix #22527

--- src/vs/workbench/parts/files/browser/fileActions.ts
+++ src/vs/workbench/parts/files/browser/fileActions.ts
@@ -288,11 +288,10 @@ class RenameFileAction extends BaseRenameAction {
 	}
 
 	public runAction(newName: string): TPromise<any> {
+		let isDirtyCaseChange = false;
 
-		// 1. check for dirty files that are being moved and backup to new target
 		const dirty = this.textFileService.getDirty().filter(d => paths.isEqualOrParent(d.fsPath, this.element.resource.fsPath));
-		const dirtyRenamed: URI[] = [];
-		return TPromise.join(dirty.map(d => {
+		const dirtyRenamed = dirty.map(d => {
 			const targetPath = paths.join(this.element.parent.resource.fsPath, newName);
 			let renamed: URI;
 
@@ -306,9 +305,27 @@ class RenameFileAction extends BaseRenameAction {
 				renamed = URI.file(paths.join(targetPath, d.fsPath.substr(this.element.resource.fsPath.length + 1)));
 			}
 
-			dirtyRenamed.push(renamed);
+			// Detect if any dirty file got renamed with different case
+			if (!isDirtyCaseChange && isEqual(d.fsPath, renamed.fsPath)) {
+				isDirtyCaseChange = true;
+			}
 
-			const model = this.textFileService.models.get(d);
+			return renamed;
+		});
+
+		// Corner case in our backup story: A dirty resource is being renamed (either the file itself or one of its parent)
+		// to a different casing, but same path. Since our backup story is does not distinguish different casing on file
+		// systems that do not distinguish, we cannot backup the contents here and restore it later.
+		if (isDirtyCaseChange) {
+			const res = this.confirmDirtyCaseChange(dirty);
+			if (!res) {
+				return TPromise.as(null);
+			}
+		}
+
+		// 1. check for dirty files that are being moved and backup to new target
+		return TPromise.join(dirtyRenamed.map((renamed, index) => {
+			const model = this.textFileService.models.get(dirty[index]);
 
 			return this.backupFileService.backupResource(renamed, model.getValue(), model.getVersionId());
 		}))
@@ -327,6 +344,26 @@ class RenameFileAction extends BaseRenameAction {
 			.then(() => {
 				return TPromise.join(dirtyRenamed.map(t => this.textModelResolverService.createModelReference(t)));
 			});
+	}
+
+	private confirmDirtyCaseChange(dirty: URI[]): boolean {
+		let message: string;
+		if (this.element.isDirectory) {
+			if (dirty.length === 1) {
+				message = nls.localize('dirtyMessageFolderOne', "You are renaming a folder with unsaved changes in 1 file. Do you want to continue?");
+			} else {
+				message = nls.localize('dirtyMessageFolder', "You are renaming a folder with unsaved changes in {0} files. Do you want to continue?", dirty.length);
+			}
+		} else {
+			message = nls.localize('dirtyMessageFile', "You are renaming a file with unsaved changes. Do you want to continue?");
+		}
+
+		return this.messageService.confirm({
+			message,
+			type: 'warning',
+			detail: nls.localize('dirtyWarning', "Your changes will be lost if you don't save them."),
+			primaryButton: nls.localize({ key: 'renameLabel', comment: ['&& denotes a mnemonic'] }, "&&Rename")
+		});
 	}
 }
 
