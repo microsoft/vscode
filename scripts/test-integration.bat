ed9e53caa5579e7d5dd703d0878e87668d21da6d
Benjamin Pasero <benjpas@microsoft.com>
2017-03-17 18:19:31 +0100

    USERDATADIR => VSCODEUSERDATADIR

--- scripts/test-integration.bat
+++ scripts/test-integration.bat
@@ -4,13 +4,13 @@ setlocal
 if not "%APPVEYOR%" == "" (
 	set ELECTRON_RUN_AS_NODE=
 )
-set USERDATADIR=%TMP%\vscodeuserfolder-%RANDOM%-%TIME:~6,5%
+set VSCODEUSERDATADIR=%TMP%\vscodeuserfolder-%RANDOM%-%TIME:~6,5%
 
 :: Integration Tests
-.\scripts\code.bat %~dp0\..\extensions\vscode-api-tests\testWorkspace --extensionDevelopmentPath=%~dp0\..\extensions\vscode-api-tests --extensionTestsPath=%~dp0\..\extensions\vscode-api-tests\out --disableExtensions --user-data-dir=%USERDATADIR%
-.\scripts\code.bat %~dp0\..\extensions\vscode-colorize-tests\test --extensionDevelopmentPath=%~dp0\..\extensions\vscode-colorize-tests --extensionTestsPath=%~dp0\..\extensions\vscode-colorize-tests\out --user-data-dir=%USERDATADIR%
+.\scripts\code.bat %~dp0\..\extensions\vscode-api-tests\testWorkspace --extensionDevelopmentPath=%~dp0\..\extensions\vscode-api-tests --extensionTestsPath=%~dp0\..\extensions\vscode-api-tests\out --disableExtensions --user-data-dir=%VSCODEUSERDATADIR%
+.\scripts\code.bat %~dp0\..\extensions\vscode-colorize-tests\test --extensionDevelopmentPath=%~dp0\..\extensions\vscode-colorize-tests --extensionTestsPath=%~dp0\..\extensions\vscode-colorize-tests\out --user-data-dir=%VSCODEUSERDATADIR%
 .\scripts\test.sh --run %~dp0..\out\vs\workbench\services\search\test\node\textSearch.integrationTest.js -g integration
 
-rmdir /s /q %USERDATADIR%
+rmdir /s /q %VSCODEUSERDATADIR%
 
 endlocal
