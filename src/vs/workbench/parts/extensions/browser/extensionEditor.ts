c3232e1cc15f49a00bcd4b97c761df9ce062ed98
Alex Dima <alexdima@microsoft.com>
2017-03-17 10:54:24 +0100

    Move KeybindingIO up to /workbench/

--- src/vs/workbench/parts/extensions/browser/extensionEditor.ts
+++ src/vs/workbench/parts/extensions/browser/extensionEditor.ts
@@ -37,7 +37,7 @@ import { ActionBar } from 'vs/base/browser/ui/actionbar/actionbar';
 import { CombinedInstallAction, UpdateAction, EnableAction, DisableAction, BuiltinStatusLabelAction, ReloadAction } from 'vs/workbench/parts/extensions/browser/extensionsActions';
 import WebView from 'vs/workbench/parts/html/browser/webview';
 import { createKeybinding } from 'vs/base/common/keyCodes';
-import { KeybindingIO } from 'vs/platform/keybinding/common/keybindingIO';
+import { KeybindingIO } from 'vs/workbench/services/keybinding/common/keybindingIO';
 import { IKeybindingService } from 'vs/platform/keybinding/common/keybinding';
 import { DomScrollableElement } from 'vs/base/browser/ui/scrollbar/scrollableElement';
 import { IMessageService } from 'vs/platform/message/common/message';
