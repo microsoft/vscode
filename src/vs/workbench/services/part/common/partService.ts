e47a912cee3f7b153f65149085eb1223b827f2fc
cleidigh <cleidigh@Gmail.com>
2017-03-19 15:29:38 -0400

    base resize commits

--- src/vs/workbench/services/part/common/partService.ts
+++ src/vs/workbench/services/part/common/partService.ts
@@ -1,4 +1,3 @@
-import { AccessibilityProvider } from '../../../parts/files/browser/views/openEditorsViewer';
 /*---------------------------------------------------------------------------------------------
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
