c08ed01dc780ee06a78a6ce9c7202e3eb8575e93
Matt Bierner <matb@microsoft.com>
2017-03-21 18:33:34 -0700

    Move html preview commands docs to vscode docs repo
    Follow up on: https://github.com/Microsoft/vscode-docs/pull/896
    
    Removes the extended documentation for the html preview since this now lives in the vscode docs

--- src/vs/workbench/api/node/extHostApiCommands.ts
+++ src/vs/workbench/api/node/extHostApiCommands.ts
@@ -171,18 +171,7 @@ export class ExtHostApiCommands {
 				description: `
 					Render the html of the resource in an editor view.
 
-					Links contained in the document will be handled by VS Code whereby it supports \`file\`-resources and
-					[virtual](https://github.com/Microsoft/vscode/blob/master/src/vs/vscode.d.ts#L3295)-resources
-					as well as triggering commands using the \`command\`-scheme. Use the query part of a command-uri to pass along JSON-encoded
-					arguments - note that URL-encoding must be applied. The snippet below defines a command-link that calls the _previewHtml_
-					command and passes along an uri:
-					\`\`\`
-					let href = encodeURI('command:vscode.previewHtml?' + JSON.stringify(someUri));
-					let html = '<a href="' + href + '">Show Resource...</a>.';
-					\`\`\`
-
-					The body element of the displayed html is dynamically annotated with one of the following css classes in order to
-					communicate the kind of color theme vscode is currently using: \`vscode-light\`, \`vscode-dark\`, or \`vscode-high-contrast\'.
+					See [working with the html preview](https://code.visualstudio.com/docs/extensionAPI/vscode-api-commands#working-with-the-html-preview) for more information about the html preview's intergration with the editor and for best practices for extension authors.
 				`,
 				args: [
 					{ name: 'uri', description: 'Uri of the resource to preview.', constraint: value => value instanceof URI || typeof value === 'string' },
