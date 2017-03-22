c5ca5017a2ad91cf5eba952a3e8c704d6879e850
Michel Kaporin <michel@kaporin.com>
2017-03-22 11:48:08 +0100

    Introduced an exception abstraction, moved the exception info check logic to the debug model.

--- src/vs/workbench/parts/debug/browser/exceptionWidget.ts
+++ src/vs/workbench/parts/debug/browser/exceptionWidget.ts
@@ -9,13 +9,13 @@ import * as dom from 'vs/base/browser/dom';
 import { ZoneWidget } from 'vs/editor/contrib/zoneWidget/browser/zoneWidget';
 import { ICodeEditor } from 'vs/editor/browser/editorBrowser';
 import { IContextViewService } from 'vs/platform/contextview/browser/contextView';
-import { IDebugService } from 'vs/workbench/parts/debug/common/debug';
+import { IDebugService, IExceptionInfo } from 'vs/workbench/parts/debug/common/debug';
 import { RunOnceScheduler } from 'vs/base/common/async';
 const $ = dom.$;
 
 export class ExceptionWidget extends ZoneWidget {
 
-	constructor(editor: ICodeEditor, private exceptionInfo: DebugProtocol.ExceptionInfoResponse, private lineNumber: number,
+	constructor(editor: ICodeEditor, private exceptionInfo: IExceptionInfo, private lineNumber: number,
 		@IContextViewService private contextViewService: IContextViewService,
 		@IDebugService private debugService: IDebugService
 	) {
@@ -33,42 +33,40 @@ export class ExceptionWidget extends ZoneWidget {
 		const fontInfo = this.editor.getConfiguration().fontInfo;
 		this.container.style.fontSize = `${fontInfo.fontSize}px`;
 		this.container.style.lineHeight = `${fontInfo.lineHeight}px`;
-		const thread = this.debugService.getViewModel().focusedThread;
 
-		if (thread && thread.stoppedDetails) {
-			let title = $('.title');
-			let msg = $('.message');
+		let title = $('.title');
+		let msg = $('.message');
+		const defaultConditionMessage = nls.localize('exceptionThrown', 'Exception has occurred.');
 
-			if (this.exceptionInfo) {
-				let conditionMessage;
-				switch (this.exceptionInfo.body.breakMode) {
-					case 'never':
-						conditionMessage = nls.localize('neverException', 'User-handled exception has occurred.');
-						break;
-					case 'always':
-						conditionMessage = nls.localize('alwaysException', 'Always-breaking exception has occurred.');
-						break;
-					case 'unhandled':
-						conditionMessage = nls.localize('unhandledException', 'Unhandled exception has occurred.');
-						break;
-					case 'userUnhandled':
-						conditionMessage = nls.localize('userUnhandledException', 'User-unhandled exception has occurred.');
-						break;
-					default:
-						conditionMessage = '';
-						break;
-				}
-
-				title.textContent = `${conditionMessage} ${this.exceptionInfo.body.description}`;
-				msg.textContent = this.exceptionInfo.body.details.stackTrace;
-			} else {
-				title.textContent = nls.localize('exceptionThrown', 'Exception occurred');
-				msg.textContent = thread.stoppedDetails.text;
+		if (this.exceptionInfo.breakMode) {
+			let conditionMessage;
+			switch (this.exceptionInfo.breakMode) {
+				case 'never':
+					conditionMessage = nls.localize('neverException', 'User-handled exception has occurred.');
+					break;
+				case 'always':
+					conditionMessage = nls.localize('alwaysException', 'Always-breaking exception has occurred.');
+					break;
+				case 'unhandled':
+					conditionMessage = nls.localize('unhandledException', 'Unhandled exception has occurred.');
+					break;
+				case 'userUnhandled':
+					conditionMessage = nls.localize('userUnhandledException', 'User-unhandled exception has occurred.');
+					break;
+				default:
+					conditionMessage = defaultConditionMessage;
+					break;
 			}
 
-			dom.append(container, title);
-			dom.append(container, msg);
+			title.textContent = `${conditionMessage} ${this.exceptionInfo.description}`;
+			msg.textContent = this.exceptionInfo.details.stackTrace;
+		} else {
+			title.textContent = defaultConditionMessage;
+			msg.textContent = this.exceptionInfo.description;
 		}
+
+		dom.append(container, title);
+		dom.append(container, msg);
 	}
 
 	protected _doLayout(heightInPixel: number, widthInPixel: number): void {
