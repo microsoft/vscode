1f84bfe86a501ef62266206607d6330a394fd5d1
Michel Kaporin <michel@kaporin.com>
2017-03-23 13:07:18 +0100

    Showing original command names next to translated. Resolves #17549.

--- src/vs/workbench/parts/quickopen/browser/commandsHandler.ts
+++ src/vs/workbench/parts/quickopen/browser/commandsHandler.ts
@@ -353,15 +353,27 @@ export class CommandsHandler extends QuickOpenHandler {
 		const entries: ActionCommandEntry[] = [];
 
 		for (let action of actions) {
-			const label = action.item.category
-				? nls.localize('cat.title', "{0}: {1}", action.item.category, action.item.title)
-				: action.item.title;
+			const title = typeof action.item.title === 'string' ? action.item.title : action.item.title.value;
+			let category, label = title;
+			if (action.item.category) {
+				category = typeof action.item.category === 'string' ? action.item.category : action.item.category.value;
+				label = nls.localize('cat.title', "{0}: {1}", category, title);
+			}
+
 			if (label) {
 				const labelHighlights = wordFilter(searchValue, label);
 				const keybinding = this.keybindingService.lookupKeybinding(action.item.id);
 				const keyLabel = keybinding ? keybinding.getLabel() : '';
 				const keyAriaLabel = keybinding ? keybinding.getAriaLabel() : '';
-				const alias = action.item.alias ? action.item.alias : null;
+				// Add an 'alias' in original language when running in different locale
+				const aliasTitle = (language !== LANGUAGE_DEFAULT && typeof action.item.title !== 'string') ? action.item.title.original : null;
+				const aliasCategory = (language !== LANGUAGE_DEFAULT && category && typeof action.item.category !== 'string') ? action.item.category.original : null;
+				let alias;
+				if (aliasTitle && category) {
+					alias = aliasCategory ? `${aliasCategory}: ${aliasTitle}` : `${category}: ${aliasTitle}`;
+				} else if (aliasTitle) {
+					alias = aliasTitle;
+				}
 				const aliasHighlights = alias ? wordFilter(searchValue, alias) : null;
 				if (labelHighlights || aliasHighlights) {
 					entries.push(this.instantiationService.createInstance(ActionCommandEntry, keyLabel, keyAriaLabel, label, alias, labelHighlights, aliasHighlights, action));
