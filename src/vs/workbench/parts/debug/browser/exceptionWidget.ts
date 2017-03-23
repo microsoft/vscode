9ddc061d04656c2980a6b3b5594d882d1288e696
Martin Aeschlimann <martinae@microsoft.com>
2017-03-23 19:51:48 +0100

    [theme] exception widget theming

--- src/vs/workbench/parts/debug/browser/exceptionWidget.ts
+++ src/vs/workbench/parts/debug/browser/exceptionWidget.ts
@@ -11,20 +11,54 @@ import { ICodeEditor } from 'vs/editor/browser/editorBrowser';
 import { IContextViewService } from 'vs/platform/contextview/browser/contextView';
 import { IDebugService, IExceptionInfo } from 'vs/workbench/parts/debug/common/debug';
 import { RunOnceScheduler } from 'vs/base/common/async';
+import { IThemeService, ITheme } from "vs/platform/theme/common/themeService";
+import { Color } from "vs/base/common/color";
+import { registerColor } from "vs/platform/theme/common/colorRegistry";
 const $ = dom.$;
+
+// theming
+
+export const debugExceptionWidgetBorder = registerColor('debugExceptionWidgetBorder', { dark: '#a31515', light: '#a31515', hc: '#a31515' }, nls.localize('debugExceptionWidgetBorder', 'Exception widget border color'));
+export const debugExceptionWidgetBackground = registerColor('debugExceptionWidgetBackground', { dark: '#a3151540', light: '#a315150d', hc: '#a3151573' }, nls.localize('debugExceptionWidgetBackground', 'Exception widget background color'));
 
 export class ExceptionWidget extends ZoneWidget {
 
+	private _backgroundColor: Color;
+
+
 	constructor(editor: ICodeEditor, private exceptionInfo: IExceptionInfo, private lineNumber: number,
 		@IContextViewService private contextViewService: IContextViewService,
-		@IDebugService private debugService: IDebugService
+		@IDebugService private debugService: IDebugService,
+		@IThemeService themeService: IThemeService
 	) {
 		super(editor, { showFrame: true, showArrow: true, frameWidth: 1 });
+
+		this._backgroundColor = Color.white;
+
+		this._applyTheme(themeService.getTheme());
+		this._disposables.add(themeService.onThemeChange(this._applyTheme.bind(this)));
+
 
 		this.create();
 		const onDidLayoutChangeScheduler = new RunOnceScheduler(() => this._doLayout(undefined, undefined), 50);
 		this._disposables.add(this.editor.onDidLayoutChange(() => onDidLayoutChangeScheduler.schedule()));
 		this._disposables.add(onDidLayoutChangeScheduler);
+	}
+
+	private _applyTheme(theme: ITheme) {
+		this._backgroundColor = theme.getColor(debugExceptionWidgetBackground);
+		let frameColor = theme.getColor(debugExceptionWidgetBorder);
+		this.style({
+			arrowColor: frameColor,
+			frameColor: frameColor
+		}); // style() will trigger _applyStyles
+	}
+
+	protected _applyStyles() {
+		if (this.container) {
+			this.container.style.backgroundColor = this._backgroundColor.toString();
+		}
+		super._applyStyles();
 	}
 
 	protected _fillContainer(container: HTMLElement): void {
