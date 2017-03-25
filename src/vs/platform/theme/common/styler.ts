a878e8c220be0960a357d98418a6533bf099dc59
Benjamin Pasero <benjpas@microsoft.com>
2017-03-25 08:16:03 -0700

    theming - debug dropdown

--- src/vs/platform/theme/common/styler.ts
+++ src/vs/platform/theme/common/styler.ts
@@ -28,10 +28,17 @@ export function attachStyler(themeService: IThemeService, widget: IThemable, opt
 	return themeService.onThemeChange(applyStyles);
 }
 
-export function attachInputBoxStyler(widget: IThemable, themeService: IThemeService): IDisposable {
-	return attachStyler(themeService, widget, { inputBackground, inputForeground });
+export function attachInputBoxStyler(widget: IThemable, themeService: IThemeService, style?: { inputBackground?: ColorIdentifier, inputForeground?: ColorIdentifier }): IDisposable {
+	return attachStyler(themeService, widget, {
+		inputBackground: (style && style.inputBackground) || inputBackground,
+		inputForeground: (style && style.inputForeground) || inputForeground
+	});
 }
 
-export function attachSelectBoxStyler(widget: IThemable, themeService: IThemeService): IDisposable {
-	return attachStyler(themeService, widget, { selectBackground, selectForeground, selectBorder });
+export function attachSelectBoxStyler(widget: IThemable, themeService: IThemeService, style?: { selectBackground?: ColorIdentifier, selectForeground?: ColorIdentifier, selectBorder?: ColorIdentifier }): IDisposable {
+	return attachStyler(themeService, widget, {
+		selectBackground: (style && style.selectBackground) || selectBackground,
+		selectForeground: (style && style.selectForeground) || selectForeground,
+		selectBorder: (style && style.selectBorder) || selectBorder
+	});
 }
