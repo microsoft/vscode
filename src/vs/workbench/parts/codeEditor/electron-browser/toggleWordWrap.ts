74245ac12407668a41269405e2ce2fdee0642f64
Alex Dima <alexdima@microsoft.com>
2017-03-27 20:46:00 +0200

    Fixes #21656: Toggle Wordwrap should toggle back to configured value

--- src/vs/workbench/parts/codeEditor/electron-browser/toggleWordWrap.ts
+++ src/vs/workbench/parts/codeEditor/electron-browser/toggleWordWrap.ts
@@ -8,6 +8,9 @@ import * as nls from 'vs/nls';
 import { KeyCode, KeyMod } from 'vs/base/common/keyCodes';
 import { ICommonCodeEditor } from 'vs/editor/common/editorCommon';
 import { editorAction, ServicesAccessor, EditorAction } from 'vs/editor/common/editorCommonExtensions';
+import { ICodeEditorService } from 'vs/editor/common/services/codeEditorService';
+import { IConfigurationService } from 'vs/platform/configuration/common/configuration';
+import { DefaultConfig } from 'vs/editor/common/config/defaultConfig';
 
 @editorAction
 class ToggleWordWrapAction extends EditorAction {
@@ -26,20 +29,35 @@ class ToggleWordWrapAction extends EditorAction {
 	}
 
 	public run(accessor: ServicesAccessor, editor: ICommonCodeEditor): void {
-		let wrappingInfo = editor.getConfiguration().wrappingInfo;
-		let newWordWrap: 'on' | 'off';
-		let newWordWrapMinified: boolean;
-		if (!wrappingInfo.isViewportWrapping) {
-			newWordWrap = 'on';
-			newWordWrapMinified = true;
-		} else {
-			newWordWrap = 'off';
-			newWordWrapMinified = false;
-		}
+		const codeEditorService = accessor.get(ICodeEditorService);
+		const configurationService = accessor.get(IConfigurationService);
+		const model = editor.getModel();
 
-		editor.updateOptions({
-			wordWrap: newWordWrap,
-			wordWrapMinified: newWordWrapMinified
-		});
+		const _configuredWordWrap = configurationService.lookup<'on' | 'off' | 'wordWrapColumn' | 'bounded'>('editor.wordWrap', model.getLanguageIdentifier().language);
+		const _configuredWordWrapMinified = configurationService.lookup<boolean>('editor.wordWrapMinified', model.getLanguageIdentifier().language);
+
+		const configuredWordWrap = _configuredWordWrap.value;
+		const configuredWordWrapMinified = (typeof _configuredWordWrapMinified.value === 'undefined' ? DefaultConfig.editor.wordWrapMinified : _configuredWordWrapMinified.value);
+
+		const alreadyToggled = codeEditorService.getTransientModelProperty(model, 'toggleWordWrap');
+		if (!alreadyToggled) {
+			codeEditorService.setTransientModelProperty(model, 'toggleWordWrap', true);
+			if (configuredWordWrap !== 'off') {
+				editor.updateOptions({
+					wordWrap: 'off',
+					wordWrapMinified: false
+				});
+			} else {
+				editor.updateOptions({
+					wordWrap: 'on'
+				});
+			}
+		} else {
+			codeEditorService.setTransientModelProperty(model, 'toggleWordWrap', false);
+			editor.updateOptions({
+				wordWrap: configuredWordWrap,
+				wordWrapMinified: configuredWordWrapMinified
+			});
+		}
 	}
 }
