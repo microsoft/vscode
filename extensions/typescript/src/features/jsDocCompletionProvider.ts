70e074ac2f2d9258d6792d0b224557f56060b5d4
Jakub Synowiec <jsynowiec@users.noreply.github.com>
2017-04-03 19:35:29 +0200

    Add an option to enable/disable JSDoc autocomplete (#23704)
    * Add new option to disable/enable jsdoc autocompletion
    
    * Refactor JsDocCompletionItem
    
    - remove internal command
    - move TS service call and template generation to CompletionItem
    - only call TS service if JSDoc completion is enabled in settings
    - add local config and config update method to CompletionProvider
    
    * Change how and when JsDocCompletionHelper is registered
    
    Register JsDocCompletionHelper and update configuration in registerProviders instead of activate. Also react to configuration change.
    
    * Rename JsDocCompletionHelper to JsDocCompletionProvider
    
    * Add missing newline at the eof
    
    * Restore completion prompt for JSDoc autocomplete
    
    Restores internal command but but as a separate class. Registers command on extension activation.

--- extensions/typescript/src/features/jsDocCompletionProvider.ts
+++ extensions/typescript/src/features/jsDocCompletionProvider.ts
@@ -5,7 +5,7 @@
 
 'use strict';
 
-import { Position, Range, CompletionItemProvider, CompletionItemKind, TextDocument, CancellationToken, CompletionItem, window, commands, Uri, ProviderResult, TextEditor, SnippetString } from 'vscode';
+import { Position, Range, CompletionItemProvider, CompletionItemKind, TextDocument, CancellationToken, CompletionItem, window, Uri, ProviderResult, TextEditor, SnippetString, workspace } from 'vscode';
 
 import { ITypescriptServiceClient } from '../typescriptService';
 import { FileLocationRequestArgs, DocCommandTemplateResponse } from '../protocol';
@@ -13,31 +13,47 @@ import { FileLocationRequestArgs, DocCommandTemplateResponse } from '../protocol
 import * as nls from 'vscode-nls';
 const localize = nls.loadMessageBundle();
 
-const tryCompleteJsDocCommand = '_typeScript.tryCompleteJsDoc';
+const configurationNamespace = 'jsDocCompletion';
 
+
+interface Configuration {
+	enabled: boolean;
+}
+
+namespace Configuration {
+	export const enabled = 'enabled';
+}
 
 class JsDocCompletionItem extends CompletionItem {
-	constructor(file: Uri, position: Position) {
+	constructor(
+		file: Uri,
+		position: Position,
+		shouldGetJSDocFromTSServer: boolean,
+	) {
 		super('/** */', CompletionItemKind.Snippet);
 		this.detail = localize('typescript.jsDocCompletionItem.documentation', 'JSDoc comment');
 		this.insertText = '';
 		this.sortText = '\0';
 		this.command = {
-			title: 'Try Complete Js Doc',
-			command: tryCompleteJsDocCommand,
-			arguments: [file, position]
+			title: 'Try Complete JSDoc',
+			command: TryCompleteJsDocCommand.COMMAND_NAME,
+			arguments: [file, position, shouldGetJSDocFromTSServer]
 		};
 	}
 }
 
-export default class JsDocCompletionHelper implements CompletionItemProvider {
+export class JsDocCompletionProvider implements CompletionItemProvider {
+	private config: Configuration;
 
 	constructor(
 		private client: ITypescriptServiceClient,
 	) {
-		commands.registerCommand(
-			tryCompleteJsDocCommand,
-			(file: Uri, position: Position) => this.tryCompleteJsDoc(file, position));
+		this.config = { enabled: true };
+	}
+
+	public updateConfiguration(): void {
+		const jsDocCompletionConfig = workspace.getConfiguration(configurationNamespace);
+		this.config.enabled = jsDocCompletionConfig.get(Configuration.enabled, true);
 	}
 
 	public provideCompletionItems(document: TextDocument, position: Position, _token: CancellationToken): ProviderResult<CompletionItem[]> {
@@ -51,7 +67,7 @@ export default class JsDocCompletionHelper implements CompletionItemProvider {
 		const line = document.lineAt(position.line).text;
 		const prefix = line.slice(0, position.character);
 		if (prefix.match(/^\s*$|\/\*\*\s*$|^\s*\/\*\*+\s*$/)) {
-			return [new JsDocCompletionItem(document.uri, position)];
+			return [new JsDocCompletionItem(document.uri, position, this.config.enabled)];
 		}
 		return [];
 	}
@@ -59,12 +75,20 @@ export default class JsDocCompletionHelper implements CompletionItemProvider {
 	public resolveCompletionItem(item: CompletionItem, _token: CancellationToken) {
 		return item;
 	}
+}
+
+export class TryCompleteJsDocCommand {
+	static COMMAND_NAME = '_typeScript.tryCompleteJsDoc';
+
+	constructor(private client: ITypescriptServiceClient) {
+
+	}
 
 	/**
 	 * Try to insert a jsdoc comment, using a template provide by typescript
 	 * if possible, otherwise falling back to a default comment format.
 	 */
-	private tryCompleteJsDoc(resource: Uri, position: Position): Thenable<boolean> {
+	public tryCompleteJsDoc(resource: Uri, position: Position, shouldGetJSDocFromTSServer: boolean): Thenable<boolean> {
 		const file = this.client.normalizePath(resource);
 		if (!file) {
 			return Promise.resolve(false);
@@ -77,6 +101,10 @@ export default class JsDocCompletionHelper implements CompletionItemProvider {
 
 		return this.prepForDocCompletion(editor, position)
 			.then((start: Position) => {
+				if (!shouldGetJSDocFromTSServer) {
+					return this.tryInsertDefaultDoc(editor, start);
+				}
+
 				return this.tryInsertJsDocFromTemplate(editor, file, start)
 					.then((didInsertFromTemplate: boolean) => {
 						if (didInsertFromTemplate) {
