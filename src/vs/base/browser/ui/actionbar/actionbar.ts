ce0277bf8e30329fddcba1b67f8ba7314c10c97e
Isidor Nikolic <inikolic@microsoft.com>
2017-04-04 22:57:43 +0200

    Merge pull request #23889 from Microsoft/isidorn/action-items-lifecycle
    Isidorn/action items lifecycle fixes #23822

--- src/vs/base/browser/ui/actionbar/actionbar.ts
+++ src/vs/base/browser/ui/actionbar/actionbar.ts
@@ -554,8 +554,7 @@ export class ActionBar extends EventEmitter implements IActionRunner {
 	}
 
 	public clear(): void {
-		// Do not dispose action items if they were provided from outside
-		this.items = this.options.actionItemProvider ? [] : lifecycle.dispose(this.items);
+		this.items = lifecycle.dispose(this.items);
 		$(this.actionsList).empty();
 	}
 
