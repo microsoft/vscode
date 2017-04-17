910e113449c2c7e84c0847d8ce519f3631c84906
Matt Bierner <matb@microsoft.com>
2017-04-17 14:12:26 -0700

    Prototype TS plugin from extension support

--- extensions/typescript/src/typescriptServiceClient.ts
+++ extensions/typescript/src/typescriptServiceClient.ts
@@ -13,7 +13,7 @@ import * as os from 'os';
 import * as electron from './utils/electron';
 import { Reader } from './utils/wireProtocol';
 
-import { workspace, window, Uri, CancellationToken, Disposable, OutputChannel, Memento, MessageItem, QuickPickItem, EventEmitter, Event, commands, WorkspaceConfiguration } from 'vscode';
+import { workspace, window, extensions, Uri, CancellationToken, Disposable, OutputChannel, Memento, MessageItem, QuickPickItem, EventEmitter, Event, commands, WorkspaceConfiguration } from 'vscode';
 import * as Proto from './protocol';
 import { ITypescriptServiceClient, ITypescriptServiceClientHost, API } from './typescriptService';
 
@@ -119,6 +119,11 @@ interface MyQuickPickItem extends QuickPickItem {
 
 interface MyMessageItem extends MessageItem {
 	id: MessageAction;
+}
+
+interface TypeScriptServerPlugin {
+	path: string;
+	name: string;
 }
 
 
@@ -565,6 +570,14 @@ export default class TypeScriptServiceClient implements ITypescriptServiceClient
 						}
 					}
 
+					if (this.apiVersion.has230Features()) {
+						const plugins = this.getTypeScriptServerPlugsins();
+						if (plugins.length) {
+							args.push('--globalPlugins', plugins.map(x => x.name).join(','));
+							args.push('--pluginProbeLocations', plugins.map(x => x.path).join(','));
+						}
+					}
+
 					electron.fork(modulePath, args, options, (err: any, childProcess: cp.ChildProcess) => {
 						if (err) {
 							this.lastError = err;
@@ -791,6 +804,23 @@ export default class TypeScriptServiceClient implements ITypescriptServiceClient
 			return undefined;
 		}
 		return desc.version;
+	}
+
+	private getTypeScriptServerPlugsins(): TypeScriptServerPlugin[] {
+		const plugins: TypeScriptServerPlugin[] = [];
+		for (const extension of extensions.all) {
+			const pack = extension.packageJSON;
+			if (pack.contributes && pack.contributes.typescriptServerPlugins && Array.isArray(pack.contributes.typescriptServerPlugins)) {
+				for (const plugin of pack.contributes.typescriptServerPlugins) {
+					const fullPath = path.resolve(extension.extensionPath, plugin);
+					plugins.push({
+						name: path.basename(fullPath),
+						path: path.dirname(fullPath)
+					});
+				}
+			}
+		}
+		return plugins;
 	}
 
 	private serviceExited(restart: boolean): void {
