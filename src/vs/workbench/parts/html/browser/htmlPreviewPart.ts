b28277ac164da428fab12294bd6d7c7cc8614e8b
Matt Bierner <matb@microsoft.com>
2017-04-18 12:44:40 -0700

    Fixes #24902

--- src/vs/workbench/parts/html/browser/htmlPreviewPart.ts
+++ src/vs/workbench/parts/html/browser/htmlPreviewPart.ts
@@ -77,7 +77,7 @@ export class HtmlPreviewPart extends BaseEditor {
 	protected createEditor(parent: Builder): void {
 		this._container = document.createElement('div');
 		this._container.style.position = 'absolute';
-		this._container.style.zIndex = '300';
+		this._container.style.zIndex = '10';
 		parent.getHTMLElement().appendChild(this._container);
 	}
 
