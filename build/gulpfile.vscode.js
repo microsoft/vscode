9ac64297a3b2ace5240299ba54b03f5029378397
Rob Lourens <roblourens@gmail.com>
2017-04-18 20:08:16 -0700

    Bump node-debug2

--- build/gulpfile.vscode.js
+++ build/gulpfile.vscode.js
@@ -42,7 +42,7 @@ const nodeModules = ['electron', 'original-fs']
 
 const builtInExtensions = [
 	{ name: 'ms-vscode.node-debug', version: '1.12.4' },
-	{ name: 'ms-vscode.node-debug2', version: '1.12.0' }
+	{ name: 'ms-vscode.node-debug2', version: '1.12.1' }
 ];
 
 const vscodeEntryPoints = _.flatten([
