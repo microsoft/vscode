2dbf28154a2acad19b34926c5b31f61a1253eb44
Andre Weinand <aweinand@microsoft.com>
2017-04-25 17:55:18 +0200

    node-debug@1.12.10

--- build/gulpfile.vscode.js
+++ build/gulpfile.vscode.js
@@ -41,7 +41,7 @@ const nodeModules = ['electron', 'original-fs']
 // Build
 
 const builtInExtensions = [
-	{ name: 'ms-vscode.node-debug', version: '1.12.9' },
+	{ name: 'ms-vscode.node-debug', version: '1.12.10' },
 	{ name: 'ms-vscode.node-debug2', version: '1.12.3' }
 ];
 
