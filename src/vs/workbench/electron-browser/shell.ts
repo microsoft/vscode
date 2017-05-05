a4a4ad4a2cc0f982df02f1bb42b7b902e511d9ac
Daniel Imms <tyriar@tyriar.com>
2017-05-05 16:28:34 -0700

    Add theme key for selection
    Fixes #25966

--- src/vs/workbench/electron-browser/shell.ts
+++ src/vs/workbench/electron-browser/shell.ts
@@ -103,7 +103,7 @@ import 'vs/platform/opener/browser/opener.contribution';
 import { IWorkbenchThemeService } from 'vs/workbench/services/themes/common/workbenchThemeService';
 import { WorkbenchThemeService } from 'vs/workbench/services/themes/electron-browser/workbenchThemeService';
 import { registerThemingParticipant, ITheme, ICssStyleCollector } from 'vs/platform/theme/common/themeService';
-import { foreground, focusBorder, scrollbarShadow, scrollbarSliderActiveBackground, scrollbarSliderBackground, scrollbarSliderHoverBackground, listHighlightForeground } from 'vs/platform/theme/common/colorRegistry';
+import { foreground, selection, focusBorder, scrollbarShadow, scrollbarSliderActiveBackground, scrollbarSliderBackground, scrollbarSliderHoverBackground, listHighlightForeground } from 'vs/platform/theme/common/colorRegistry';
 
 /**
  * Services that we require for the Shell
@@ -527,6 +527,11 @@ registerThemingParticipant((theme: ITheme, collector: ICssStyleCollector) => {
 	const windowForeground = theme.getColor(foreground);
 	if (windowForeground) {
 		collector.addRule(`.monaco-shell { color: ${windowForeground}; }`);
+	}
+
+	const windowSelection = theme.getColor(selection);
+	if (windowSelection) {
+		collector.addRule(`.monaco-shell *::selection { background-color: ${windowSelection}; }`);
 	}
 
 	// List highlight
