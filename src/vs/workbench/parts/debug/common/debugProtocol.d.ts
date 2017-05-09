3b32ecfc3218258958546431a27641851b0b42d1
Andre Weinand <aweinand@microsoft.com>
2017-05-09 16:52:25 +0200

    DAP: properly extend type of TerminateEvent.restart

--- src/vs/workbench/parts/debug/common/debugProtocol.d.ts
+++ src/vs/workbench/parts/debug/common/debugProtocol.d.ts
@@ -124,7 +124,7 @@ declare module DebugProtocol {
 			/** A debug adapter may set 'restart' to true (or to an arbitrary object) to request that the front end restarts the session.
 				The value is not interpreted by the client and passed unmodified as an attribute '__restart' to the launchRequest.
 			*/
-			restart?: boolean | object;
+			restart?: any;
 		};
 	}
 
@@ -1347,3 +1347,4 @@ declare module DebugProtocol {
 		innerException?: ExceptionDetails[];
 	}
 }
+
