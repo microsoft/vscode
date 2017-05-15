4d0b7c79d3b66737208cb5165f3831eb21b5fd6f
Benjamin Pasero <benjpas@microsoft.com>
2017-05-15 10:36:22 +0200

    build: wire in mixin password as GH token

--- build/gulpfile.vscode.js
+++ build/gulpfile.vscode.js
@@ -130,7 +130,8 @@ const config = {
 	darwinCredits: darwinCreditsTemplate ? new Buffer(darwinCreditsTemplate({ commit: commit, date: new Date().toISOString() })) : void 0,
 	linuxExecutableName: product.applicationName,
 	winIcon: 'resources/win32/code.ico',
-	token: process.env['GITHUB_TOKEN'] || void 0
+	token: process.env['VSCODE_MIXIN_PASSWORD'] || process.env['GITHUB_TOKEN'] || void 0,
+	repo: product.electronRepository || void 0
 };
 
 gulp.task('clean-electron', util.rimraf('.build/electron'));
