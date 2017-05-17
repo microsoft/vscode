65d736eea88ce3e62214bd75ece6df7513aceff1
Joao Moreno <jomo@microsoft.com>
2017-05-17 16:46:08 +0200

    fix windows build

--- build/tfs/win32/1_build.ps1
+++ build/tfs/win32/1_build.ps1
@@ -8,11 +8,17 @@ Param(
 # In order to get _netrc to work, we need a HOME variable setup
 $env:HOME=$env:USERPROFILE
 
+$env:npm_config_disturl="https://atom.io/download/electron"
+$env:npm_config_target="1.6.6"
+$env:npm_config_runtime="electron"
+$env:npm_config_cache="$HOME/.npm-electron"
+mkdir -p "$env:npm_config_cache"
+
 # Create a _netrc file to download distro dependencies
 "machine monacotools.visualstudio.com password ${vsoPAT}" | Out-File "$env:USERPROFILE\_netrc" -Encoding ASCII
 
 step "Install dependencies" {
-  exec { & .\scripts\npm.bat install }
+  exec { & npm install }
 }
 
 $env:VSCODE_MIXIN_PASSWORD = $mixinPassword
