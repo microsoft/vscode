506ec62591a949c2d0f3686817b6a5f40c207fc2
Joao Moreno <jomo@microsoft.com>
2017-05-17 10:29:50 +0200

    tfs: improve win32 step

--- build/tfs/win32/1_build.ps1
+++ build/tfs/win32/1_build.ps1
@@ -9,26 +9,31 @@ Param(
 $env:HOME=$env:USERPROFILE
 
 # Create a _netrc file to download distro dependencies
-@"
-machine monacotools.visualstudio.com
-password ${vsoPAT}
-"@ | Out-File "$env:USERPROFILE\_netrc" -Encoding ASCII
+"machine monacotools.visualstudio.com password ${vsoPAT}" | Out-File "$env:USERPROFILE\_netrc" -Encoding ASCII
 
-STEP "Install dependencies"
-exec { & .\scripts\npm.bat install }
+step "Install dependencies" {
+  exec { & .\scripts\npm.bat install }
+}
 
-STEP "Mix in repository from vscode-distro"
 $env:VSCODE_MIXIN_PASSWORD = $mixinPassword
-exec { & npm run gulp -- mixin }
+step "Mix in repository from vscode-distro" {
+  exec { & npm run gulp -- mixin }
+}
 
-STEP "Install distro dependencies"
-exec { & npm run install-distro }
+step "Install distro dependencies" {
+  exec { & npm run install-distro }
+}
 
-STEP "Build minified"
-exec { & npm run gulp -- --max_old_space_size=4096 vscode-win32-min }
+step "Build minified" {
+  exec { & npm run gulp -- --max_old_space_size=4096 vscode-win32-min }
+}
 
-STEP "Run unit tests"
-exec { & .\scripts\test.bat --build --reporter dot }
+step "Run unit tests" {
+  exec { & .\scripts\test.bat --build --reporter dot }
+}
 
-# STEP "Run integration tests"
-# exec { & .\scripts\test-integration.bat }
+# step "Run integration tests" {
+#   exec { & .\scripts\test-integration.bat }
+# }
+
+done
