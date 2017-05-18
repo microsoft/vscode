55e5f69dd3ac26e761c746470f0cb9de54ecaa42
Amy Qiu <umberos@gmail.com>
2017-05-18 14:09:32 -0700

    Fix saveMemento

--- src/vs/workbench/parts/search/browser/searchViewlet.ts
+++ src/vs/workbench/parts/search/browser/searchViewlet.ts
@@ -1351,7 +1351,6 @@ export class SearchViewlet extends Viewlet {
 
 	public shutdown(): void {
 		this.viewletSettings['query.contentPattern'] = this.searchWidget.searchInput.getValue();
-		this.saveMemento();
 
 		super.shutdown();
 	}
