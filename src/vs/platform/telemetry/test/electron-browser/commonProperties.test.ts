aa81dc3e6658e892b702a759e41c4b632901f84e
Johannes Rieken <johannes.rieken@gmail.com>
2017-05-22 13:49:41 +0200

    remove assertion that checks number of common properties

--- src/vs/platform/telemetry/test/electron-browser/commonProperties.test.ts
+++ src/vs/platform/telemetry/test/electron-browser/commonProperties.test.ts
@@ -53,7 +53,6 @@ suite('Telemetry - common properties', function () {
 				assert.ok('common.sqm.machineid' in props, 'machineid');
 			}
 
-			assert.equal(Object.keys(props).length, process.platform === 'win32' ? 18 : 16);
 		});
 	});
 
