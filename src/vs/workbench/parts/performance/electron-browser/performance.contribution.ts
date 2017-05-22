cdd3d8eed2644522e22da6e45502b322a9258e3a
Johannes Rieken <johannes.rieken@gmail.com>
2017-05-22 15:37:13 +0200

    send telemetry when invalid timers have been computed

--- src/vs/workbench/parts/performance/electron-browser/performance.contribution.ts
+++ src/vs/workbench/parts/performance/electron-browser/performance.contribution.ts
@@ -25,6 +25,7 @@ import { readdir } from 'vs/base/node/pfs';
 import { release } from 'os';
 import { stopProfiling } from 'vs/base/node/profiler';
 import { virtualMachineHint } from 'vs/base/node/id';
+import { forEach } from "vs/base/common/collections";
 
 class ProfilingHint implements IWorkbenchContribution {
 
@@ -208,6 +209,36 @@ class StartupProfiler implements IWorkbenchContribution {
 	}
 }
 
+class PerformanceTelemetry implements IWorkbenchContribution {
+
+	constructor(
+		@ITimerService private readonly _timerService: ITimerService,
+		@ITelemetryService private readonly _telemetryService: ITelemetryService,
+		@IExtensionService extensionService: IExtensionService,
+	) {
+		TPromise.join<any>([
+			TPromise.timeout(7 * 1000),
+			extensionService.onReady()
+		]).then(() => this._validateTimers());
+	}
+
+	getId(): string {
+		return 'performance.PerformanceTelemetry';
+	}
+
+	private _validateTimers(): void {
+		const { startupMetrics } = this._timerService;
+		const invalidTimers: string[] = [];
+		forEach(startupMetrics.timers, (entry) => {
+			if (entry.value < 0) {
+				invalidTimers.push(entry.key);
+			}
+		});
+		this._telemetryService.publicLog('invalidTimers', { invalidTimers });
+	}
+}
+
 const registry = Registry.as<IWorkbenchContributionsRegistry>(Extensions.Workbench);
 registry.registerWorkbenchContribution(ProfilingHint);
 registry.registerWorkbenchContribution(StartupProfiler);
+registry.registerWorkbenchContribution(PerformanceTelemetry);
