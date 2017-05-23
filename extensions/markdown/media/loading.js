87c24f2b2633ae0652caac1f0df9acadb3271f5e
Matt Bierner <matb@microsoft.com>
2017-05-23 11:57:07 -0700

    Fix markdown preview csp failure. Fix #27156

--- extensions/markdown/media/loading.js
+++ extensions/markdown/media/loading.js
@@ -2,16 +2,26 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
+// @ts-check
 
 'use strict';
+
 
 (function () {
 	const unloadedStyles = [];
 
-	window.onStyleLoadError = (event) => {
+	const onStyleLoadError = (event) => {
 		const source = event.target.dataset.source;
 		unloadedStyles.push(source);
 	};
+
+	window.addEventListener('DOMContentLoaded', () => {
+		for (const link of document.getElementsByClassName('code-user-style')) {
+			if (link.dataset.source) {
+				link.onerror = onStyleLoadError;
+			}
+		}
+	})
 
 	window.addEventListener('load', () => {
 		if (!unloadedStyles.length) {
