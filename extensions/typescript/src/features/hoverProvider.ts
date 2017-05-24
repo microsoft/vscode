50cc0f90677e2bcabe3346d9c67e8c12b5f56dec
Matt Bierner <matb@microsoft.com>
2017-05-24 15:03:40 -0700

    use alwaysStrict in TS extension

--- extensions/typescript/src/features/hoverProvider.ts
+++ extensions/typescript/src/features/hoverProvider.ts
@@ -3,8 +3,6 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-'use strict';
-
 import { HoverProvider, Hover, TextDocument, Position, Range, CancellationToken } from 'vscode';
 
 import * as Proto from '../protocol';
--- extensions/typescript/src/utils/wireProtocol.ts
+++ extensions/typescript/src/utils/wireProtocol.ts
@@ -3,8 +3,6 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-'use strict';
-
 import stream = require('stream');
 
 const DefaultSize: number = 8192;
