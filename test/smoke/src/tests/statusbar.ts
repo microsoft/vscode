109435f8cff94ebe2e8119f1fa6560b1b9c6421e
Michel Kaporin <t-mikapo@microsoft.com>
2017-05-29 16:19:04 +0200

    Fixed path to module.

--- test/smoke/src/tests/statusbar.ts
+++ test/smoke/src/tests/statusbar.ts
@@ -7,7 +7,7 @@ import * as assert from 'assert';
 
 import { SpectronApplication, LATEST_PATH, WORKSPACE_PATH } from "../spectron/application";
 import { CommonActions } from '../areas/common';
-import { StatusBarElement, StatusBar } from "../areas/statusBar";
+import { StatusBarElement, StatusBar } from "../areas/statusbar";
 
 let app: SpectronApplication;
 let common: CommonActions;
