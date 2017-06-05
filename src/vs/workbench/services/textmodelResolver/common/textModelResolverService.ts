4cc11750df34b51875988e5e967eb85b098d854d
Matt Bierner <matb@microsoft.com>
2017-06-05 15:17:35 -0700

    Fix uncaught exception while opening file that does not exists
    **bug**
    Calling workspace.openTextDocument for a url that does not exist can trigger an uncaught exception. The root cause of this appears to be `destroyReferencedObject` calling `done` without and error handler.
    
    **fix**
    Add a void error sink in this case
    
    Fixes #27809

--- src/vs/workbench/services/textmodelResolver/common/textModelResolverService.ts
+++ src/vs/workbench/services/textmodelResolver/common/textModelResolverService.ts
@@ -47,7 +47,7 @@ class ResourceModelCollection extends ReferenceCollection<TPromise<ITextEditorMo
 			} else {
 				model.dispose();
 			}
-		});
+		}, () => void 0);
 	}
 
 	public registerTextModelContentProvider(scheme: string, provider: ITextModelContentProvider): IDisposable {
