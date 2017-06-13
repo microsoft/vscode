24c9b0992502302491763f25cf126fe91e9c165b
Michel Kaporin <t-mikapo@microsoft.com>
2017-06-13 10:34:03 +0200

    Fail the test if the key binding was not found.

--- test/smoke/src/spectron/application.ts
+++ test/smoke/src/spectron/application.ts
@@ -152,6 +152,10 @@ export class SpectronApplication {
 	 */
 	public command(command: string, capture?: boolean): Promise<any> {
 		const binding = this.keybindings.find(x => x['command'] === command);
+		if (!binding) {
+			throw new Error(`Key binding for ${command} was not found`);
+		}
+
 		const keys: string = binding.key;
 		let keysToPress: string[] = [];
 
