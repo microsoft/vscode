50879b0da3fbd85fa16b1bfeb68f3a330fb4413e
Daniel Imms <daimms@microsoft.com>
2017-06-14 15:56:01 -0700

    Get nsfw file watcher sending events back

--- src/vs/workbench/services/files/node/watcher/nsfw/nsfwWatcherService.ts
+++ src/vs/workbench/services/files/node/watcher/nsfw/nsfwWatcherService.ts
@@ -6,6 +6,16 @@
 import nsfw = require('nsfw');
 import { IWatcherService, IWatcherRequest } from 'vs/workbench/services/files/node/watcher/unix/watcher';
 import { TPromise } from "vs/base/common/winjs.base";
+import watcher = require('vs/workbench/services/files/node/watcher/common');
+import * as path from 'path';
+
+const nsfwEventActionToRawChangeType = {
+	0: 1, // Created
+	1: 2, // Deleted
+	2: 0, // Modified
+	// TODO: handle rename event type
+	3: null // Rename
+};
 
 export class NsfwWatcherService implements IWatcherService {
 	public watch(request: IWatcherRequest): TPromise<void> {
@@ -13,11 +23,32 @@ export class NsfwWatcherService implements IWatcherService {
 		console.log('basePath ' + request.basePath);
 		return new TPromise<void>((c, e, p) => {
 			nsfw(request.basePath, events => {
-				console.log(events);
-				p(events);
+				if (request.verboseLogging) {
+					console.log('raw events start');
+					events.forEach(e => console.log(e));
+					console.log('raw events end');
+				}
+				const convertedEvents = events.map(e => this._mapNsfwEventToRawFileChange(e)).filter(e => !!e);
+				// TODO: Utilize fileEventDelayer and watcher.normalize
+				p(convertedEvents);
 			}).then(watcher => {
 				return watcher.start();
 			});
 		});
+	}
+
+	private _mapNsfwEventToRawFileChange(nsfwEvent: any): watcher.IRawFileChange {
+		// TODO: Handle other event types (directory change?)
+		if (!nsfwEvent.directory || !nsfwEvent.file) {
+			return null;
+		}
+		const event: watcher.IRawFileChange = {
+			type: nsfwEventActionToRawChangeType[nsfwEvent.action],
+			path: path.join(nsfwEvent.directory, nsfwEvent.file)
+		};
+		if (!event.type) {
+			return null;
+		}
+		return event;
 	}
 }
