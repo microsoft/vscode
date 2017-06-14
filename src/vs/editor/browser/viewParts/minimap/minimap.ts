9f482b34502fded5b5c0df3af951b909192fb92a
Alex Dima <alexdima@microsoft.com>
2017-06-14 07:47:43 +0200

    Hint GPU layers for editor sliders

--- src/vs/editor/browser/viewParts/minimap/minimap.ts
+++ src/vs/editor/browser/viewParts/minimap/minimap.ts
@@ -413,6 +413,7 @@ export class Minimap extends ViewPart {
 		this._slider = createFastDomNode(document.createElement('div'));
 		this._slider.setPosition('absolute');
 		this._slider.setClassName('minimap-slider');
+		dom.hintGPULayer(this._slider.domNode);
 		this._domNode.appendChild(this._slider);
 
 		this._tokensColorTracker = MinimapTokensColorTracker.getInstance();
