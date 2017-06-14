8d2f5a1075d74d6023e6a8c2c2daa2fd41887a2c
Michel Kaporin <t-mikapo@microsoft.com>
2017-06-14 10:56:05 +0200

    Refactor tfs linux builds to suit smoke test.

--- build/tfs/linux/smoketest.sh
+++ build/tfs/linux/smoketest.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
+set -e
 
 . ./scripts/env.sh
 . ./build/tfs/common/common.sh
 
 export ARCH="$1"
-export VSCODE_MIXIN_PASSWORD="$2"
-export AZURE_STORAGE_ACCESS_KEY="$3"
-export AZURE_STORAGE_ACCESS_KEY_2="$4"
-export MOONCAKE_STORAGE_ACCESS_KEY="$5"
-export AZURE_DOCUMENTDB_MASTERKEY="$6"
-export LINUX_REPO_PASSWORD="$7"
-VSO_PAT="$8"
+VSO_PAT="$2"
 
 echo "machine monacotools.visualstudio.com password $VSO_PAT" > ~/.netrc
 
@@ -32,5 +27,9 @@ step "Build minified" \
 step "Run unit tests" \
 	./scripts/test.sh --xvfb --build --reporter dot
 
-step "Publish release" \
-	./build/tfs/linux/release.sh
+step "Run smoke test" \
+	pushd test/smoke
+	npm install
+	npm run compile
+	node src/main.js --latest "$AGENT_BUILDDIRECTORY/VSCode-linux-ia32/code-insiders"
+	popd
