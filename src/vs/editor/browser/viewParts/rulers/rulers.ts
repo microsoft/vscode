e8d9cb2fd679f39e065e34ed49cccd0e1238dc85
Alex Dima <alexdima@microsoft.com>
2017-06-14 16:32:56 +0200

    Fixes #9634: Set rulers width with a value that maximizez the likelihood that all rulers are rendered with equal screen width

--- src/vs/editor/browser/viewParts/rulers/rulers.ts
+++ src/vs/editor/browser/viewParts/rulers/rulers.ts
@@ -13,6 +13,7 @@ import { RenderingContext, RestrictedRenderingContext } from 'vs/editor/common/v
 import * as viewEvents from 'vs/editor/common/view/viewEvents';
 import { registerThemingParticipant } from 'vs/platform/theme/common/themeService';
 import { editorRuler } from 'vs/editor/common/view/editorColorRegistry';
+import * as dom from 'vs/base/browser/dom';
 
 export class Rulers extends ViewPart {
 
@@ -69,11 +70,12 @@ export class Rulers extends ViewPart {
 		}
 
 		if (currentCount < desiredCount) {
-			// Add more rulers
+			const rulerWidth = dom.computeScreenAwareSize(1);
 			let addCount = desiredCount - currentCount;
 			while (addCount > 0) {
 				let node = createFastDomNode(document.createElement('div'));
 				node.setClassName('view-ruler');
+				node.setWidth(rulerWidth);
 				this.domNode.appendChild(node);
 				this._renderedRulers.push(node);
 				addCount--;
