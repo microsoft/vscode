dfee02d7fa8f11c78b6296096a14524418a2784b
Matt Bierner <matb@microsoft.com>
2017-06-15 11:34:38 -0700

    Don't use let in preload

--- src/vs/workbench/electron-browser/bootstrap/preload.js
+++ src/vs/workbench/electron-browser/bootstrap/preload.js
@@ -6,8 +6,8 @@
 
 (function() {
 	function getConfig() {
-		let queryParams = window.location.search.substring(1).split('&');
-		for (let i = 0; i < queryParams.length; i++) {
+		const queryParams = window.location.search.substring(1).split('&');
+		for (var i = 0; i < queryParams.length; i++) {
 			var kv = queryParams[i].split('=');
 			if (kv[0] === 'config' && kv[1]) {
 				return JSON.parse(decodeURIComponent(kv[1]));
@@ -16,20 +16,20 @@
 		return {};
 	}
 	try {
-		let config = getConfig();
-		let document = window.document;
+		const config = getConfig();
+		const document = window.document;
 
 		// sets the base theme class ('vs', 'vs-dark', 'hc-black')
-		let baseTheme = config.baseTheme || 'vs';
+		const baseTheme = config.baseTheme || 'vs';
 		document.body.className = 'monaco-shell ' + baseTheme;
 
 		// adds a stylesheet with the backgrdound color
-		let backgroundColor = config.backgroundColor;
+		var backgroundColor = config.backgroundColor;
 		if (!backgroundColor) {
 			backgroundColor = baseTheme === 'hc-black' ? '#000000' : (baseTheme === 'vs' ? '#FFFFFF' : '#1E1E1E');
 		}
-		let foregroundColor = baseTheme === 'hc-black' ? '#FFFFFF' : (baseTheme === 'vs' ? '#6C6C6C' : '#CCCCCC');
-		let style = document.createElement('style');
+		const foregroundColor = baseTheme === 'hc-black' ? '#FFFFFF' : (baseTheme === 'vs' ? '#6C6C6C' : '#CCCCCC');
+		const style = document.createElement('style');
 		style.innerHTML = '.monaco-shell { background-color:' + backgroundColor + '; color:' + foregroundColor + '; }';
 		document.head.appendChild(style);
 
