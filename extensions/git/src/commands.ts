fd2dac6d5833ae422c2ede9c93a761bc4bcdb45a
Joao Moreno <jomo@microsoft.com>
2017-06-16 10:00:52 +0200

    fixes #28506

--- extensions/git/src/commands.ts
+++ extensions/git/src/commands.ts
@@ -5,7 +5,7 @@
 
 'use strict';
 
-import { Uri, commands, scm, Disposable, window, workspace, QuickPickItem, OutputChannel, Range, WorkspaceEdit, Position, LineChange, SourceControlResourceState } from 'vscode';
+import { Uri, commands, scm, Disposable, window, workspace, QuickPickItem, OutputChannel, Range, WorkspaceEdit, Position, LineChange, SourceControlResourceState, TextDocumentShowOptions, ViewColumn } from 'vscode';
 import { Ref, RefType, Git, GitErrorCodes } from './git';
 import { Model, Resource, Status, CommitOptions, WorkingTreeGroup, IndexGroup, MergeGroup } from './model';
 import { toGitUri, fromGitUri } from './uri';
@@ -145,11 +145,18 @@ export class CommandCenter {
 			return;
 		}
 
+		const viewColumn = window.activeTextEditor && window.activeTextEditor.viewColumn || ViewColumn.One;
+
 		if (!left) {
-			return await commands.executeCommand<void>('vscode.open', right);
+			return await commands.executeCommand<void>('vscode.open', right, viewColumn);
 		}
 
-		return await commands.executeCommand<void>('vscode.diff', left, right, title, { preview: true });
+		const opts: TextDocumentShowOptions = {
+			preview: true,
+			viewColumn
+		};
+
+		return await commands.executeCommand<void>('vscode.diff', left, right, title, opts);
 	}
 
 	private getLeftResource(resource: Resource): Uri | undefined {
@@ -289,7 +296,9 @@ export class CommandCenter {
 			return;
 		}
 
-		return await commands.executeCommand<void>('vscode.open', uri);
+		const viewColumn = window.activeTextEditor && window.activeTextEditor.viewColumn || ViewColumn.One;
+
+		return await commands.executeCommand<void>('vscode.open', uri, viewColumn);
 	}
 
 	@command('git.openChange')
@@ -329,7 +338,9 @@ export class CommandCenter {
 			return;
 		}
 
-		return await commands.executeCommand<void>('vscode.open', uriToOpen);
+		const viewColumn = window.activeTextEditor && window.activeTextEditor.viewColumn || ViewColumn.One;
+
+		return await commands.executeCommand<void>('vscode.open', uriToOpen, viewColumn);
 	}
 
 	@command('git.stage')
