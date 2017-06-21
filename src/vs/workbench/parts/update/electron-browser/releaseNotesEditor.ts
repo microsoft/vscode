ddd0b6fe66ff8932c9e5ea501574bc7783883566
Matt Bierner <matb@microsoft.com>
2017-06-21 13:48:29 -0700

    Move releasenotes baseurl into html

--- src/vs/workbench/parts/update/electron-browser/releaseNotesEditor.ts
+++ src/vs/workbench/parts/update/electron-browser/releaseNotesEditor.ts
@@ -26,6 +26,7 @@ function renderBody(body: string): string {
 	return `<!DOCTYPE html>
 		<html>
 			<head>
+				<base href="https://code.visualstudio.com/raw/">
 				<meta http-equiv="Content-type" content="text/html;charset=UTF-8">
 				<meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src https: data:; media-src https:; script-src 'none'; style-src file: https: 'unsafe-inline'; child-src 'none'; frame-src 'none';">
 				<link rel="stylesheet" type="text/css" href="${require.toUrl('./media/markdown.css')}">
@@ -94,7 +95,6 @@ export class ReleaseNotesEditor extends WebviewEditor {
 			.then(renderBody)
 			.then<void>(body => {
 				this.webview = new WebView(this.content, this.partService.getContainer(Parts.EDITOR_PART));
-				this.webview.baseUrl = `https://code.visualstudio.com/raw/`;
 
 				if (this.input && this.input instanceof ReleaseNotesInput) {
 					const state = this.loadViewState(this.input.version);
