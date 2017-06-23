3c764acd78bb012668017ed5b33eb562dc9d52b6
Rob Lourens <roblourens@gmail.com>
2017-06-23 13:50:14 -0700

    Make "find in folder" multiroot aware

--- src/vs/base/common/paths.ts
+++ src/vs/base/common/paths.ts
@@ -393,3 +393,37 @@ export function isEqualOrParent(path: string, candidate: string, ignoreCase?: bo
 
 	return path.indexOf(candidate) === 0;
 }
+
+/**
+ * Adapted from Node's path.isAbsolute functions
+ * @param path
+ */
+export function isAbsolute(path: string): boolean {
+	return isWindows ?
+		isAbsolute_win32(path) :
+		isAbsolute_posix(path);
+}
+
+export function isAbsolute_win32(path: string): boolean {
+	if (!path) {
+		return false;
+	}
+
+	const char0 = path.charCodeAt(0);
+	if (char0 === CharCode.Slash || char0 === CharCode.Backslash) {
+		return true;
+	} else if ((char0 >= CharCode.A && char0 <= CharCode.Z) || (char0 >= CharCode.a && char0 <= CharCode.z)) {
+		if (path.length > 2 && path.charCodeAt(1) === CharCode.Colon) {
+			const char2 = path.charCodeAt(2);
+			if (char2 === CharCode.Slash || char0 === CharCode.Backslash) {
+				return true;
+			}
+		}
+	}
+
+	return false;
+}
+
+export function isAbsolute_posix(path: string): boolean {
+	return path && path.charCodeAt(0) === CharCode.Slash;
+}
