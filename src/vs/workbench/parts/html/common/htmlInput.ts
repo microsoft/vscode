0effef14e178e58c8bea1779f30892a75578e37e
kieferrm <kieferrm@gmail.com>
2017-06-23 17:42:48 -0700

    fix compile errors

--- src/vs/workbench/parts/html/common/htmlInput.ts
+++ src/vs/workbench/parts/html/common/htmlInput.ts
@@ -19,7 +19,7 @@ export class HtmlInput extends ResourceEditorInput {
 		name: string,
 		description: string,
 		resource: URI,
-		private options: HtmlInputOptions,
+		public readonly options: HtmlInputOptions,
 		@ITextModelService textModelResolverService: ITextModelService
 	) {
 		super(name, description, resource, textModelResolverService);
