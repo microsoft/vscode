a3efde81118e78328316a61686006b59775d76a5
Joao Moreno <jomo@microsoft.com>
2017-06-29 16:42:39 +0200

    use path_helper in user shell in darwin
    fixes #21655

--- src/vs/code/node/shellEnv.ts
+++ src/vs/code/node/shellEnv.ts
@@ -6,22 +6,25 @@
 'use strict';
 
 import * as cp from 'child_process';
-import { assign } from 'vs/base/common/objects';
 import { generateUuid } from 'vs/base/common/uuid';
 import { TPromise } from 'vs/base/common/winjs.base';
-import { isWindows } from 'vs/base/common/platform';
+import { isWindows, isMacintosh } from 'vs/base/common/platform';
 
-function getUnixShellEnvironment(): TPromise<typeof process.env> {
+export type Enviroment = typeof process.env;
+
+export function getUnixShellEnvironment(extraEnv: Enviroment = {}): TPromise<Enviroment> {
 	const promise = new TPromise((c, e) => {
 		const runAsNode = process.env['ELECTRON_RUN_AS_NODE'];
 		const noAttach = process.env['ELECTRON_NO_ATTACH_CONSOLE'];
 		const mark = generateUuid().replace(/-/g, '').substr(0, 12);
 		const regex = new RegExp(mark + '(.*)' + mark);
 
-		const env = assign({}, process.env, {
+		const env = {
+			...process.env,
+			...extraEnv,
 			ELECTRON_RUN_AS_NODE: '1',
 			ELECTRON_NO_ATTACH_CONSOLE: '1'
-		});
+		};
 
 		const command = `'${process.execPath}' -p '"${mark}" + JSON.stringify(process.env) + "${mark}"'`;
 		const child = cp.spawn(process.env.SHELL, ['-ilc', command], {
@@ -69,20 +72,65 @@ function getUnixShellEnvironment(): TPromise<typeof process.env> {
 	return promise.then(null, () => ({}));
 }
 
+export function getDarwinPathHelperEnvironment(): TPromise<Enviroment> {
+	const promise = new TPromise((c, e) => {
+		const child = cp.spawn('/usr/libexec/path_helper', ['-s'], {
+			detached: true,
+			stdio: 'pipe'
+		});
 
-let _shellEnv: TPromise<typeof process.env>;
+		const buffers: Buffer[] = [];
+		child.on('error', () => c({}));
+		child.stdout.on('data', b => buffers.push(b as Buffer));
+
+		child.on('close', (code: number, signal: any) => {
+			if (code !== 0) {
+				return e(new Error('Failed to get environment'));
+			}
+
+			const raw = Buffer.concat(buffers).toString('utf8');
+			const regex = /^([^=]+)="(.*)"; export \1;$/mg;
+			const env: Enviroment = {};
+
+			let match: RegExpExecArray;
+
+			while (match = regex.exec(raw)) {
+				env[match[1]] = match[2];
+			}
+
+			c(env);
+		});
+	});
+
+	return promise.then(null, () => ({}));
+}
+
+export async function getDarwinShellEnvironment(): TPromise<Enviroment> {
+	const pathHelperEnv = await getDarwinPathHelperEnvironment();
+
+	try {
+		return await getUnixShellEnvironment(pathHelperEnv);
+	} catch (err) {
+		console.warn('Resorting to path_helper only');
+		return pathHelperEnv;
+	}
+}
+
+let _shellEnv: TPromise<Enviroment>;
 
 /**
  * We need to get the environment from a user's shell.
  * This should only be done when Code itself is not launched
  * from within a shell.
  */
-export function getShellEnvironment(): TPromise<typeof process.env> {
+export function getShellEnvironment(): TPromise<Enviroment> {
 	if (_shellEnv === undefined) {
 		if (isWindows) {
 			_shellEnv = TPromise.as({});
 		} else if (process.env['VSCODE_CLI'] === '1') {
 			_shellEnv = TPromise.as({});
+		} else if (isMacintosh) {
+			_shellEnv = getDarwinShellEnvironment();
 		} else {
 			_shellEnv = getUnixShellEnvironment();
 		}
