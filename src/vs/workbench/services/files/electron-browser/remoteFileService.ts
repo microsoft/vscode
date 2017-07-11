0d86cf54a5e6bbd6835e5b55aec77936d6ef1bfd
Johannes Rieken <johannes.rieken@gmail.com>
2017-07-11 17:02:02 +0200

    remove need for --remote switch just go with proposed api

--- src/vs/workbench/services/files/electron-browser/remoteFileService.ts
+++ src/vs/workbench/services/files/electron-browser/remoteFileService.ts
@@ -6,14 +6,6 @@
 
 import URI from 'vs/base/common/uri';
 import { FileService } from 'vs/workbench/services/files/electron-browser/fileService';
-import { IConfigurationService } from 'vs/platform/configuration/common/configuration';
-import { IWorkspaceContextService } from 'vs/platform/workspace/common/workspace';
-import { IWorkbenchEditorService } from 'vs/workbench/services/editor/common/editorService';
-import { IMessageService } from 'vs/platform/message/common/message';
-import { IEnvironmentService } from 'vs/platform/environment/common/environment';
-import { IEditorGroupService } from 'vs/workbench/services/group/common/groupService';
-import { ILifecycleService } from 'vs/platform/lifecycle/common/lifecycle';
-import { IStorageService } from 'vs/platform/storage/common/storage';
 import { IContent, IStreamContent, IFileStat, IResolveContentOptions, IUpdateContentOptions, FileChangesEvent, FileChangeType } from "vs/platform/files/common/files";
 import { TPromise } from "vs/base/common/winjs.base";
 import Event from "vs/base/common/event";
@@ -29,41 +21,26 @@ export interface IRemoteFileSystemProvider {
 
 export class RemoteFileService extends FileService {
 
-	private _provider: IRemoteFileSystemProvider;
-	private readonly _remoteAuthority: string;
-
-	constructor(
-		@IConfigurationService configurationService: IConfigurationService,
-		@IWorkspaceContextService contextService: IWorkspaceContextService,
-		@IWorkbenchEditorService editorService: IWorkbenchEditorService,
-		@IEnvironmentService environmentService: IEnvironmentService,
-		@IEditorGroupService editorGroupService: IEditorGroupService,
-		@ILifecycleService lifecycleService: ILifecycleService,
-		@IMessageService messageService: IMessageService,
-		@IStorageService storageService: IStorageService
-	) {
-		super(configurationService, contextService, editorService, environmentService, editorGroupService, lifecycleService, messageService, storageService);
-		this._remoteAuthority = environmentService.args['remote'];
-	}
+	private readonly _provider = new Map<string, IRemoteFileSystemProvider>();
 
 	private _shouldIntercept(resource: URI): boolean {
-		return this._provider && resource.authority === this._remoteAuthority;
+		return this._provider.has(resource.authority);
 	}
 
 	registerProvider(authority: string, provider: IRemoteFileSystemProvider): IDisposable {
-		// todo@joh make this actually work for N provider
-		if (authority !== this._remoteAuthority) {
+		if (this._provider.has(authority)) {
 			throw new Error();
 		}
-		this._provider = provider;
-		const reg = this._provider.onDidChange(e => {
+
+		this._provider.set(authority, provider);
+		const reg = provider.onDidChange(e => {
 			// forward change events
 			this._onFileChanges.fire(new FileChangesEvent([{ resource: e, type: FileChangeType.UPDATED }]));
 		});
 		return {
 			dispose: () => {
+				this._provider.delete(authority);
 				reg.dispose();
-				this._provider = undefined;
 			}
 		};
 	}
@@ -89,7 +66,7 @@ export class RemoteFileService extends FileService {
 	private async _doResolveContent(resource: URI): TPromise<IContent> {
 
 		const stat = RemoteFileService._createFakeStat(resource);
-		const value = await this._provider.resolve(resource);
+		const value = await this._provider.get(resource.authority).resolve(resource);
 		return <any>{ ...stat, value };
 	}
 
@@ -104,7 +81,7 @@ export class RemoteFileService extends FileService {
 	}
 
 	private async _doUpdateContent(resource: URI, content: string): TPromise<URI> {
-		await this._provider.update(resource, content);
+		await this._provider.get(resource.authority).update(resource, content);
 		return resource;
 	}
 
