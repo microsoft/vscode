482a61fa11b787339511ace49fd81982b3fc93c7
Rob Lourens <roblourens@gmail.com>
2017-07-11 11:01:17 -0700

    Remove terminalCursor.foreground defaults, in favor of terminal.foreground

--- src/vs/workbench/parts/terminal/electron-browser/terminalColorRegistry.ts
+++ src/vs/workbench/parts/terminal/electron-browser/terminalColorRegistry.ts
@@ -19,11 +19,7 @@ export const TERMINAL_FOREGROUND_COLOR = registerColor('terminal.foreground', {
 	dark: '#CCCCCC',
 	hc: '#FFFFFF'
 }, nls.localize('terminal.foreground', 'The foreground color of the terminal.'));
-export const TERMINAL_CURSOR_FOREGROUND_COLOR = registerColor('terminalCursor.foreground', {
-	light: '#333333',
-	dark: '#CCCCCC',
-	hc: '#FFFFFF'
-}, nls.localize('terminalCursor.foreground', 'The foreground color of the terminal cursor.'));
+export const TERMINAL_CURSOR_FOREGROUND_COLOR = registerColor('terminalCursor.foreground', null, nls.localize('terminalCursor.foreground', 'The foreground color of the terminal cursor.'));
 export const TERMINAL_CURSOR_BACKGROUND_COLOR = registerColor('terminalCursor.background', null, nls.localize('terminalCursor.background', 'The background color of the terminal cursor. Allows customizing the color of a character overlapped by a block cursor.'));
 
 // TODO: Reinstate, see #28397
