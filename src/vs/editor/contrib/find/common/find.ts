dd6de93cc4b10a1eab9cba464178efe8796f1980
Oleg Mihailik <mihailik@gmail.com>
2017-07-19 22:43:26 +0100

    Copyright header.

--- src/vs/editor/contrib/find/common/find.ts
+++ src/vs/editor/contrib/find/common/find.ts
@@ -1,3 +1,9 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+'use strict';
+
 import * as editorCommon from 'vs/editor/common/editorCommon';
 
 export function getSelectionSearchString(editor: editorCommon.ICommonCodeEditor): string {
