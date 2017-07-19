fb7e2d201316b7d050b87fd0dfd2f57ae228147b
Christof Marti <chrmarti@microsoft.com>
2017-07-19 16:51:39 -0700

    keytar Linux 32bit

--- build/tfs/linux/build-ia32.sh
+++ build/tfs/linux/build-ia32.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 set -e
+
+docker build -t build-ia32 ./build/tfs/linux/image-ia32
+docker run --rm -v "${WORK_MNT}:${VSTS_WORK}" -w="`pwd`" build-ia32 npm install --unsafe-perms --runtime=electron --target=1.6.6 --disturl=https://atom.io/download/atom-shell keytar
+
 ./build/tfs/linux/build.sh ia32 "$@"
