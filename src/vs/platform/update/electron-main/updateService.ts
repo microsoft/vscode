f1650daa406c4c30f2196170516d7e1b317ad3c5
Joao Moreno <jomo@microsoft.com>
2017-07-26 11:25:05 +0200

    Revert 32 - 64 bit update
    Revert "fixes #31226"
    
    This reverts commit eddd1f099f30b52ef29a2fe6ddf3f88611d58887.
    
    Revert "32 - 64 always bump"
    
    This reverts commit 81e812c06540d1bd2636cf70ff7d0edd1bb0e866.
    
    Revert "tweak 32 to 64 bit upgrade"
    
    This reverts commit 23be721eef4ebca4eec0010ab1a1593ec37e8000.
    
    Revert "emit events"
    
    This reverts commit 6f682d2a84652d6629df2501c5f04d3585d4b8fe.
    
    Revert "streamline update process"
    
    This reverts commit 3a35853a5df3f3b17a158f11c9f6b95edc813780.
    
    Revert "windows update noLink: true"
    
    This reverts commit 70a1c2b6e02060fd06adb49657704d0c830ccfaa.
    
    Revert "store arch in auto updater win32"
    
    This reverts commit bdc81de61bfa417a3f4d55e08892bde351b17e61.
    
    Revert "propose 64 bit windows update"
    
    This reverts commit 6081ce20e8870a2f58143a03d0d4642dfa04539a.

--- src/vs/platform/update/electron-main/updateService.ts
+++ src/vs/platform/update/electron-main/updateService.ts
@@ -5,6 +5,8 @@
 
 'use strict';
 
+import * as fs from 'original-fs';
+import * as path from 'path';
 import * as electron from 'electron';
 import { IDisposable, dispose } from 'vs/base/common/lifecycle';
 import Event, { Emitter, once, filterEvent } from 'vs/base/common/event';
@@ -20,8 +22,6 @@ import product from 'vs/platform/node/product';
 import { TPromise } from 'vs/base/common/winjs.base';
 import { IUpdateService, State, IAutoUpdater, IUpdate, IRawUpdate } from 'vs/platform/update/common/update';
 import { ITelemetryService } from 'vs/platform/telemetry/common/telemetry';
-import { getUpdateFeedUrl } from './updateFeedUrl';
-import { IStorageService } from 'vs/platform/storage/node/storage';
 
 export class UpdateService implements IUpdateService {
 
@@ -87,13 +87,10 @@ export class UpdateService implements IUpdateService {
 		@IRequestService requestService: IRequestService,
 		@ILifecycleService private lifecycleService: ILifecycleService,
 		@IConfigurationService private configurationService: IConfigurationService,
-		@ITelemetryService private telemetryService: ITelemetryService,
-		@IStorageService private storageService: IStorageService
+		@ITelemetryService private telemetryService: ITelemetryService
 	) {
-		const channel = this.getUpdateChannel();
-
 		if (process.platform === 'win32') {
-			this.raw = new Win32AutoUpdaterImpl(process.arch, channel, requestService, storageService);
+			this.raw = new Win32AutoUpdaterImpl(requestService);
 		} else if (process.platform === 'linux') {
 			this.raw = new LinuxAutoUpdaterImpl(requestService);
 		} else if (process.platform === 'darwin') {
@@ -102,7 +99,8 @@ export class UpdateService implements IUpdateService {
 			return;
 		}
 
-		const feedUrl = getUpdateFeedUrl(channel);
+		const channel = this.getUpdateChannel();
+		const feedUrl = this.getUpdateFeedUrl(channel);
 
 		if (!feedUrl) {
 			return; // updates not available
@@ -208,6 +206,36 @@ export class UpdateService implements IUpdateService {
 		const channel = config && config.channel;
 
 		return channel === 'none' ? null : product.quality;
+	}
+
+	private getUpdateFeedUrl(channel: string): string {
+		if (!channel) {
+			return null;
+		}
+
+		if (process.platform === 'win32' && !fs.existsSync(path.join(path.dirname(process.execPath), 'unins000.exe'))) {
+			return null;
+		}
+
+		if (!product.updateUrl || !product.commit) {
+			return null;
+		}
+
+		const platform = this.getUpdatePlatform();
+
+		return `${product.updateUrl}/api/update/${platform}/${channel}/${product.commit}`;
+	}
+
+	private getUpdatePlatform(): string {
+		if (process.platform === 'linux') {
+			return `linux-${process.arch}`;
+		}
+
+		if (process.platform === 'win32' && process.arch === 'x64') {
+			return 'win32-x64';
+		}
+
+		return process.platform;
 	}
 
 	quitAndInstall(): TPromise<void> {
