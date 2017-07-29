a24bbef53b787cb77158f0b00c386b8935d854d1
Ryan Adolf <ryanadolf123@gmail.com>
2017-07-28 21:39:54 -0700

    Ignore external console windows when computing terminal title

--- src/vs/workbench/parts/terminal/electron-browser/windowsShellHelper.ts
+++ src/vs/workbench/parts/terminal/electron-browser/windowsShellHelper.ts
@@ -11,8 +11,18 @@ import { Emitter, debounceEvent } from 'vs/base/common/event';
 
 const SHELL_EXECUTABLES = ['cmd.exe', 'powershell.exe', 'bash.exe'];
 
+interface IWindowsTerminalProcess {
+	pid: number;
+	executable: string;
+}
+
+interface IWindowsProcTreeItem {
+	pid: number;
+	children: IWindowsTerminalProcess[];
+}
+
 export class WindowsShellHelper {
-	private _childProcessIdStack: number[];
+	private _childProcessIdStack: IWindowsProcTreeItem[];
 	private _onCheckWindowsShell: Emitter<string>;
 	private _rootShellExecutable: string;
 	private _rootProcessId: number;
@@ -32,7 +42,7 @@ export class WindowsShellHelper {
 		});
 	}
 
-	private getChildProcessDetails(pid: number): TPromise<{ executable: string, pid: number }[]> {
+	private getChildProcessDetails(pid: number): TPromise<IWindowsTerminalProcess[]> {
 		return new TPromise((resolve, reject) => {
 			cp.execFile('wmic.exe', ['process', 'where', `parentProcessId=${pid}`, 'get', 'ExecutablePath,ProcessId'], (err, stdout, stderr) => {
 				if (err) {
@@ -51,8 +61,9 @@ export class WindowsShellHelper {
 		});
 	}
 
-	private refreshShellProcessTree(pid: number, parent: string): TPromise<string> {
-		return this.getChildProcessDetails(pid).then(result => {
+	private refreshShellProcessTree(process: IWindowsProcTreeItem, parent: string, useCached: boolean): TPromise<string> {
+		return (useCached ? TPromise.as(process.children) : this.getChildProcessDetails(process.pid)).then(result => {
+			process.children = result;
 			// When we didn't find any child processes of the process
 			if (result.length === 0) {
 				// Case where we found a child process already and are checking further down the pid tree
@@ -66,15 +77,23 @@ export class WindowsShellHelper {
 				}
 				// Otherwise, we go up the tree to find the next valid deepest child of the root
 				this._childProcessIdStack.pop();
-				return this.refreshShellProcessTree(this._childProcessIdStack[this._childProcessIdStack.length - 1], null);
+				return this.refreshShellProcessTree(this._childProcessIdStack[this._childProcessIdStack.length - 1], null, false);
 			}
 			// We only go one level deep when checking for children of processes other then shells
-			if (SHELL_EXECUTABLES.indexOf(path.basename(result[0].executable)) === -1) {
+			const baseName = path.basename(result[0].executable);
+			if (SHELL_EXECUTABLES.indexOf(baseName) === -1) {
+				if (baseName === 'conhost.exe') {
+					// We're inside an external console; go up 2 levels and remove the console from the children
+					this._childProcessIdStack.pop();
+					const grandParent = this._childProcessIdStack[this._childProcessIdStack.length - 1];
+					grandParent.children.splice(grandParent.children.map(p => p.pid).indexOf(process.pid), 1);
+					return this.refreshShellProcessTree(grandParent, null, true);
+				}
 				return TPromise.as(result[0].executable);
 			}
 			// Save the pid in the stack and keep looking for children of that child
-			this._childProcessIdStack.push(result[0].pid);
-			return this.refreshShellProcessTree(result[0].pid, result[0].executable);
+			this._childProcessIdStack.push({ pid: result[0].pid, children: [] });
+			return this.refreshShellProcessTree({ pid: result[0].pid, children: [] }, result[0].executable, false);
 		}, error => { return error; });
 	}
 
@@ -83,10 +102,10 @@ export class WindowsShellHelper {
 	 */
 	public getShellName(): TPromise<string> {
 		if (this._childProcessIdStack.length === 0) {
-			this._childProcessIdStack.push(this._rootProcessId);
+			this._childProcessIdStack.push({ pid: this._rootProcessId, children: [] });
 		}
 		return new TPromise<string>((resolve) => {
-			this.refreshShellProcessTree(this._childProcessIdStack[this._childProcessIdStack.length - 1], null).then(result => {
+			this.refreshShellProcessTree(this._childProcessIdStack[this._childProcessIdStack.length - 1], null, false).then(result => {
 				resolve(result);
 			}, error => { return error; });
 		});
