4abb7009df0f0c72ae6a38bb28b88d9b77627c64
t-amqi <umberos@gmail.com>
2017-08-08 11:32:08 -0700

    Address changes

--- src/vs/workbench/parts/terminal/browser/terminalQuickOpen.ts
+++ src/vs/workbench/parts/terminal/browser/terminalQuickOpen.ts
@@ -9,17 +9,17 @@ import strings = require('vs/base/common/strings');
 import scorer = require('vs/base/common/scorer');
 import { TPromise } from 'vs/base/common/winjs.base';
 import { Mode, IEntryRunContext, IAutoFocus, IQuickNavigateConfiguration, IModel } from 'vs/base/parts/quickopen/common/quickOpen';
-import { QuickOpenModel, QuickOpenEntryGroup, QuickOpenEntry } from 'vs/base/parts/quickopen/browser/quickOpenModel';
+import { QuickOpenModel, QuickOpenEntry } from 'vs/base/parts/quickopen/browser/quickOpenModel';
 import { QuickOpenHandler } from 'vs/workbench/browser/quickopen';
 import { ITerminalService } from 'vs/workbench/parts/terminal/common/terminal';
 import { IPanelService } from 'vs/workbench/services/panel/common/panelService';
 import { ContributableActionProvider } from 'vs/workbench/browser/actions';
 
-export class TerminalEntry extends QuickOpenEntryGroup {
+export class TerminalEntry extends QuickOpenEntry {
 
 	constructor(
 		private label: string,
-		private open: () => void
+		private terminalService: ITerminalService
 	) {
 		super();
 	}
@@ -34,18 +34,14 @@ export class TerminalEntry extends QuickOpenEntryGroup {
 
 	public run(mode: Mode, context: IEntryRunContext): boolean {
 		if (mode === Mode.OPEN) {
-			return this.runOpen(context);
+			setTimeout(() => {
+				this.terminalService.setActiveInstanceByIndex(parseInt(this.label.split(':')[0], 10) - 1);
+				this.terminalService.showPanel(true);
+			}, 0);
+			return true;
 		}
 
 		return super.run(mode, context);
-	}
-
-	private runOpen(context: IEntryRunContext): boolean {
-		setTimeout(() => {
-			this.open();
-		}, 0);
-
-		return true;
 	}
 }
 
@@ -53,7 +49,7 @@ export class CreateTerminal extends QuickOpenEntry {
 
 	constructor(
 		private label: string,
-		private open: () => void
+		private terminalService: ITerminalService
 	) {
 		super();
 	}
@@ -68,18 +64,15 @@ export class CreateTerminal extends QuickOpenEntry {
 
 	public run(mode: Mode, context: IEntryRunContext): boolean {
 		if (mode === Mode.OPEN) {
-			return this.runOpen(context);
+			setTimeout(() => {
+				const newTerminal = this.terminalService.createInstance();
+				this.terminalService.setActiveInstance(newTerminal);
+				this.terminalService.showPanel(true);
+			}, 0);
+			return true;
 		}
 
 		return super.run(mode, context);
-	}
-
-	private runOpen(context: IEntryRunContext): boolean {
-		setTimeout(() => {
-			this.open();
-		}, 0);
-
-		return true;
 	}
 }
 
@@ -97,11 +90,7 @@ export class TerminalPickerHandler extends QuickOpenHandler {
 		const normalizedSearchValueLowercase = strings.stripWildcards(searchValue).toLowerCase();
 
 		const terminalEntries: QuickOpenEntry[] = this.getTerminals();
-		terminalEntries.push(new CreateTerminal(nls.localize("'workbench.action.terminal.newplus", "$(plus) Create New Integrated Terminal"), () => {
-			const newTerminal = this.terminalService.createInstance();
-			this.terminalService.setActiveInstance(newTerminal);
-			this.terminalService.showPanel(true);
-		}));
+		terminalEntries.push(new CreateTerminal(nls.localize("'workbench.action.terminal.newplus", "$(plus) Create New Integrated Terminal"), this.terminalService));
 
 		const entries = terminalEntries.filter(e => {
 			if (!searchValue) {
@@ -124,10 +113,7 @@ export class TerminalPickerHandler extends QuickOpenHandler {
 	private getTerminals(): TerminalEntry[] {
 		const terminals = this.terminalService.getInstanceLabels();
 		const terminalEntries = terminals.map(terminal => {
-			return new TerminalEntry(terminal, () => {
-				this.terminalService.setActiveInstanceByIndex(parseInt(terminal.split(':')[0], 10) - 1);
-				this.terminalService.showPanel(true);
-			});
+			return new TerminalEntry(terminal, this.terminalService);
 		});
 		return terminalEntries;
 	}
