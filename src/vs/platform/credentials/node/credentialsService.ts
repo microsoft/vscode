0b6da14128b4e61e331193f18786e8db2792a643
Christof Marti <chrmarti@microsoft.com>
2017-08-11 12:04:04 -0700

    Add keytar back

--- src/vs/platform/credentials/node/credentialsService.ts
+++ src/vs/platform/credentials/node/credentialsService.ts
@@ -6,36 +6,22 @@
 
 import { TPromise } from 'vs/base/common/winjs.base';
 import { ICredentialsService } from 'vs/platform/credentials/common/credentials';
-import * as keytarType from 'keytar';
+import * as keytar from 'keytar';
 
 export class CredentialsService implements ICredentialsService {
 
 	_serviceBrand: any;
 
-	private keytarPromise: TPromise<typeof keytarType>;
-
 	readSecret(service: string, account: string): TPromise<string | undefined> {
-		return this.getKeytar()
-			.then(keytar => TPromise.wrap(keytar.getPassword(service, account)))
+		return TPromise.wrap(keytar.getPassword(service, account))
 			.then(result => result === null ? undefined : result);
 	}
 
 	writeSecret(service: string, account: string, secret: string): TPromise<void> {
-		return this.getKeytar()
-			.then(keytar => TPromise.wrap(keytar.setPassword(service, account, secret)));
+		return TPromise.wrap(keytar.setPassword(service, account, secret));
 	}
 
 	deleteSecret(service: string, account: string): TPromise<boolean> {
-		return this.getKeytar()
-			.then(keytar => TPromise.wrap(keytar.deletePassword(service, account)));
-	}
-
-	private getKeytar(): TPromise<typeof keytarType> {
-		if (!this.keytarPromise) {
-			this.keytarPromise = new TPromise<typeof keytarType>((c, e) => {
-				require(['keytar'], c, e);
-			});
-		}
-		return this.keytarPromise;
+		return TPromise.wrap(keytar.deletePassword(service, account));
 	}
 }
