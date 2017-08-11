a567de7cb1448165c310ddc557763b4435c47c69
Jay Rodgers <headmelted@gmail.com>
2017-08-11 10:14:54 +0000

    Adding electron-arm target for gulp

--- build/gulpfile.vscode.js
+++ build/gulpfile.vscode.js
@@ -171,6 +171,7 @@ gulp.task('clean-electron', util.rimraf('.build/electron'));
 gulp.task('electron', ['clean-electron'], getElectron(process.arch));
 gulp.task('electron-ia32', ['clean-electron'], getElectron('ia32'));
 gulp.task('electron-x64', ['clean-electron'], getElectron('x64'));
+gulp.task('electron-arm', ['clean-electron'], getElectron('arm'));
 
 
 /**
