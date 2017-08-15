b63e14f763f9fd7d796e116ee730471d33a1b6ae
Alex Dima <alexdima@microsoft.com>
2017-08-15 10:25:49 +0200

    Remove statics from proxy protocol

--- src/vs/workbench/services/extensions/node/ipcRemoteCom.ts
+++ src/vs/workbench/services/extensions/node/ipcRemoteCom.ts
@@ -4,46 +4,123 @@
  *--------------------------------------------------------------------------------------------*/
 'use strict';
 
-import winjs = require('vs/base/common/winjs.base');
-import marshalling = require('vs/base/common/marshalling');
-import errors = require('vs/base/common/errors');
+import { TPromise } from 'vs/base/common/winjs.base';
+import * as marshalling from 'vs/base/common/marshalling';
+import * as errors from 'vs/base/common/errors';
 import { IMessagePassingProtocol } from 'vs/base/parts/ipc/common/ipc';
 import { LazyPromise } from "vs/workbench/services/extensions/node/lazyPromise";
-
-let lastMessageId = 0;
-const pendingRPCReplies: { [msgId: string]: LazyPromise; } = {};
-
-class MessageFactory {
-	public static cancel(req: string): string {
-		return `{"cancel":"${req}"}`;
-	}
-
-	public static request(req: string, rpcId: string, method: string, args: any[]): string {
-		return `{"req":"${req}","rpcId":"${rpcId}","method":"${method}","args":${marshalling.stringify(args)}}`;
-	}
-
-	public static replyOK(req: string, res: any): string {
-		if (typeof res === 'undefined') {
-			return `{"seq":"${req}"}`;
-		}
-		return `{"seq":"${req}","res":${marshalling.stringify(res)}}`;
-	}
-
-	public static replyErr(req: string, err: any): string {
-		if (typeof err === 'undefined') {
-			return `{"seq":"${req}","err":null}`;
-		}
-		return `{"seq":"${req}","err":${marshalling.stringify(errors.transformErrorForSerialization(err))}}`;
-	}
-}
 
 export interface IManyHandler {
 	handle(rpcId: string, method: string, args: any[]): any;
 }
 
 export interface IRemoteCom {
-	callOnRemote(proxyId: string, path: string, args: any[]): winjs.TPromise<any>;
+	callOnRemote(proxyId: string, path: string, args: any[]): TPromise<any>;
 	setManyHandler(handler: IManyHandler): void;
+}
+
+export function createProxyProtocol(protocol: IMessagePassingProtocol): IRemoteCom {
+	return new RPCManager(protocol);
+}
+
+export class RPCManager implements IRemoteCom {
+
+	private _bigHandler: IManyHandler;
+	private _lastMessageId: number;
+	private readonly _invokedHandlers: { [req: string]: TPromise<any>; };
+	private readonly _pendingRPCReplies: { [msgId: string]: LazyPromise; };
+	private readonly _multiplexor: RPCMultiplexer;
+
+	constructor(protocol: IMessagePassingProtocol) {
+		this._bigHandler = null;
+		this._lastMessageId = 0;
+		this._invokedHandlers = Object.create(null);
+		this._pendingRPCReplies = {};
+		this._multiplexor = new RPCMultiplexer(protocol, (msg) => this._receiveOneMessage(msg));
+	}
+
+	private _receiveOneMessage(rawmsg: string): void {
+		let msg = marshalling.parse(rawmsg);
+
+		if (msg.seq) {
+			if (!this._pendingRPCReplies.hasOwnProperty(msg.seq)) {
+				console.warn('Got reply to unknown seq');
+				return;
+			}
+			let reply = this._pendingRPCReplies[msg.seq];
+			delete this._pendingRPCReplies[msg.seq];
+
+			if (msg.err) {
+				let err = msg.err;
+				if (msg.err.$isError) {
+					err = new Error();
+					err.name = msg.err.name;
+					err.message = msg.err.message;
+					err.stack = msg.err.stack;
+				}
+				reply.resolveErr(err);
+				return;
+			}
+
+			reply.resolveOk(msg.res);
+			return;
+		}
+
+		if (msg.cancel) {
+			if (this._invokedHandlers[msg.cancel]) {
+				this._invokedHandlers[msg.cancel].cancel();
+			}
+			return;
+		}
+
+		if (msg.err) {
+			console.error(msg.err);
+			return;
+		}
+
+		let rpcId = msg.rpcId;
+
+		if (!this._bigHandler) {
+			throw new Error('got message before big handler attached!');
+		}
+
+		let req = msg.req;
+
+		this._invokedHandlers[req] = this._invokeHandler(rpcId, msg.method, msg.args);
+
+		this._invokedHandlers[req].then((r) => {
+			delete this._invokedHandlers[req];
+			this._multiplexor.send(MessageFactory.replyOK(req, r));
+		}, (err) => {
+			delete this._invokedHandlers[req];
+			this._multiplexor.send(MessageFactory.replyErr(req, err));
+		});
+	}
+
+	private _invokeHandler(rpcId: string, method: string, args: any[]): TPromise<any> {
+		try {
+			return TPromise.as(this._bigHandler.handle(rpcId, method, args));
+		} catch (err) {
+			return TPromise.wrapError(err);
+		}
+	}
+
+	public callOnRemote(proxyId: string, path: string, args: any[]): TPromise<any> {
+		let req = String(++this._lastMessageId);
+		let result = new LazyPromise(() => {
+			this._multiplexor.send(MessageFactory.cancel(req));
+		});
+
+		this._pendingRPCReplies[req] = result;
+
+		this._multiplexor.send(MessageFactory.request(req, proxyId, path, args));
+
+		return result;
+	}
+
+	public setManyHandler(handler: IManyHandler): void {
+		this._bigHandler = handler;
+	}
 }
 
 /**
@@ -104,102 +181,26 @@ class RPCMultiplexer {
 	}
 }
 
-export class RPCManager implements IRemoteCom {
-
-	private _bigHandler: IManyHandler;
-	private readonly _invokedHandlers: { [req: string]: winjs.TPromise<any>; };
-	private readonly _multiplexor: RPCMultiplexer;
-
-	constructor(protocol: IMessagePassingProtocol) {
-		this._bigHandler = null;
-		this._invokedHandlers = Object.create(null);
-		this._multiplexor = new RPCMultiplexer(protocol, (msg) => this._receiveOneMessage(msg));
+class MessageFactory {
+	public static cancel(req: string): string {
+		return `{"cancel":"${req}"}`;
 	}
 
-	private _receiveOneMessage(rawmsg: string): void {
-		let msg = marshalling.parse(rawmsg);
-
-		if (msg.seq) {
-			if (!pendingRPCReplies.hasOwnProperty(msg.seq)) {
-				console.warn('Got reply to unknown seq');
-				return;
-			}
-			let reply = pendingRPCReplies[msg.seq];
-			delete pendingRPCReplies[msg.seq];
-
-			if (msg.err) {
-				let err = msg.err;
-				if (msg.err.$isError) {
-					err = new Error();
-					err.name = msg.err.name;
-					err.message = msg.err.message;
-					err.stack = msg.err.stack;
-				}
-				reply.resolveErr(err);
-				return;
-			}
-
-			reply.resolveOk(msg.res);
-			return;
-		}
-
-		if (msg.cancel) {
-			if (this._invokedHandlers[msg.cancel]) {
-				this._invokedHandlers[msg.cancel].cancel();
-			}
-			return;
-		}
-
-		if (msg.err) {
-			console.error(msg.err);
-			return;
-		}
-
-		let rpcId = msg.rpcId;
-
-		if (!this._bigHandler) {
-			throw new Error('got message before big handler attached!');
-		}
-
-		let req = msg.req;
-
-		this._invokedHandlers[req] = this._invokeHandler(rpcId, msg.method, msg.args);
-
-		this._invokedHandlers[req].then((r) => {
-			delete this._invokedHandlers[req];
-			this._multiplexor.send(MessageFactory.replyOK(req, r));
-		}, (err) => {
-			delete this._invokedHandlers[req];
-			this._multiplexor.send(MessageFactory.replyErr(req, err));
-		});
+	public static request(req: string, rpcId: string, method: string, args: any[]): string {
+		return `{"req":"${req}","rpcId":"${rpcId}","method":"${method}","args":${marshalling.stringify(args)}}`;
 	}
 
-	private _invokeHandler(rpcId: string, method: string, args: any[]): winjs.TPromise<any> {
-		try {
-			return winjs.TPromise.as(this._bigHandler.handle(rpcId, method, args));
-		} catch (err) {
-			return winjs.TPromise.wrapError(err);
+	public static replyOK(req: string, res: any): string {
+		if (typeof res === 'undefined') {
+			return `{"seq":"${req}"}`;
 		}
+		return `{"seq":"${req}","res":${marshalling.stringify(res)}}`;
 	}
 
-	public callOnRemote(proxyId: string, path: string, args: any[]): winjs.TPromise<any> {
-		let req = String(++lastMessageId);
-		let result = new LazyPromise(() => {
-			this._multiplexor.send(MessageFactory.cancel(req));
-		});
-
-		pendingRPCReplies[req] = result;
-
-		this._multiplexor.send(MessageFactory.request(req, proxyId, path, args));
-
-		return result;
+	public static replyErr(req: string, err: any): string {
+		if (typeof err === 'undefined') {
+			return `{"seq":"${req}","err":null}`;
+		}
+		return `{"seq":"${req}","err":${marshalling.stringify(errors.transformErrorForSerialization(err))}}`;
 	}
-
-	public setManyHandler(handler: IManyHandler): void {
-		this._bigHandler = handler;
-	}
-}
-
-export function createProxyProtocol(protocol: IMessagePassingProtocol): IRemoteCom {
-	return new RPCManager(protocol);
 }
