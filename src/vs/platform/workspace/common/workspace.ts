b104c7d0f862621c8372944b5ca4dcae26fa354a
hikerpig <102706325@163.com>
2017-08-15 08:23:29 -0500

    fix spelling of 'iff' and 'idenifier' (#32401)
    * fix spelling of 'iff' and 'idenifier'
    
    * Keep 'iff' in comment if it means *if and only if*

--- src/vs/platform/workspace/common/workspace.ts
+++ src/vs/platform/workspace/common/workspace.ts
@@ -17,17 +17,17 @@ export interface IWorkspaceContextService {
 	_serviceBrand: any;
 
 	/**
-	 * Returns iff the application was opened with a workspace or not.
+	 * Returns if the application was opened with a workspace or not.
 	 */
 	hasWorkspace(): boolean;
 
 	/**
-	 * Returns iff the application was opened with a folder.
+	 * Returns if the application was opened with a folder.
 	 */
 	hasFolderWorkspace(): boolean;
 
 	/**
-	 * Returns iff the application was opened with a workspace that can have one or more folders.
+	 * Returns if the application was opened with a workspace that can have one or more folders.
 	 */
 	hasMultiFolderWorkspace(): boolean;
 
@@ -65,7 +65,7 @@ export interface IWorkspaceContextService {
 	getRoot(resource: URI): URI;
 
 	/**
-	 * Returns iff the provided resource is inside the workspace or not.
+	 * Returns if the provided resource is inside the workspace or not.
 	 */
 	isInsideWorkspace(resource: URI): boolean;
 
