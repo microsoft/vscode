a573e65d70016054bbc0b423dace25414198fbf6
Benjamin Pasero <benjpas@microsoft.com>
2017-08-17 08:41:20 +0200

    Copy Selection Not Saved (fixes #20269)

--- src/vs/workbench/parts/files/browser/views/explorerViewer.ts
+++ src/vs/workbench/parts/files/browser/views/explorerViewer.ts
@@ -353,8 +353,11 @@ export class FileRenderer implements IRenderer {
 				this.state.actionProvider.runAction(tree, stat, editableData.action, { value: inputBox.value });
 			}
 
+			const restoreFocus = document.activeElement === inputBox.inputElement; // https://github.com/Microsoft/vscode/issues/20269
 			setTimeout(() => {
-				tree.DOMFocus();
+				if (restoreFocus) {
+					tree.DOMFocus();
+				}
 				lifecycle.dispose(toDispose);
 				container.removeChild(label.element);
 			}, 0);
