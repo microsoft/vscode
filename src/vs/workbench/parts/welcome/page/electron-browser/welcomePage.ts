3266cff3a237530574d316f093c6fd517c988a0b
Christof Marti <chrmarti@microsoft.com>
2017-08-17 17:04:44 -0700

    Show Azure extensions link

--- src/vs/workbench/parts/welcome/page/electron-browser/welcomePage.ts
+++ src/vs/workbench/parts/welcome/page/electron-browser/welcomePage.ts
@@ -114,8 +114,10 @@ export class WelcomePageAction extends Action {
 
 interface ExtensionSuggestion {
 	name: string;
+	title?: string;
 	id: string;
 	isKeymap?: boolean;
+	isCommand?: boolean;
 }
 
 const extensionPacks: ExtensionSuggestion[] = [
@@ -124,6 +126,7 @@ const extensionPacks: ExtensionSuggestion[] = [
 	{ name: localize('welcomePage.python', "Python"), id: 'donjayamanne.python' },
 	// { name: localize('welcomePage.go', "Go"), id: 'lukehoban.go' },
 	{ name: localize('welcomePage.php', "PHP"), id: 'felixfbecker.php-pack' },
+	{ name: localize('welcomePage.azure', "Azure"), title: localize('welcomePage.showAzureExtensions', "Show Azure extensions"), id: 'workbench.extensions.action.showAzureExtensions', isCommand: true },
 	{ name: localize('welcomePage.docker', "Docker"), id: 'PeterJausovec.vscode-docker' },
 ];
 
@@ -322,23 +325,28 @@ class WelcomePage {
 
 				const a = document.createElement('a');
 				a.innerText = extension.name;
-				a.title = extension.isKeymap ? localize('welcomePage.installKeymap', "Install {0} keymap", extension.name) : localize('welcomePage.installExtensionPack', "Install additional support for {0}", extension.name);
-				a.classList.add('installExtension');
-				a.setAttribute('data-extension', extension.id);
-				a.href = 'javascript:void(0)';
-				a.addEventListener('click', e => {
-					this.installExtension(extension, strings);
-					e.preventDefault();
-					e.stopPropagation();
-				});
-				list.appendChild(a);
+				a.title = extension.title || (extension.isKeymap ? localize('welcomePage.installKeymap', "Install {0} keymap", extension.name) : localize('welcomePage.installExtensionPack', "Install additional support for {0}", extension.name));
+				if (extension.isCommand) {
+					a.href = `command:${extension.id}`;
+					list.appendChild(a);
+				} else {
+					a.classList.add('installExtension');
+					a.setAttribute('data-extension', extension.id);
+					a.href = 'javascript:void(0)';
+					a.addEventListener('click', e => {
+						this.installExtension(extension, strings);
+						e.preventDefault();
+						e.stopPropagation();
+					});
+					list.appendChild(a);
 
-				const span = document.createElement('span');
-				span.innerText = extension.name;
-				span.title = extension.isKeymap ? localize('welcomePage.installedKeymap', "{0} keymap is already installed", extension.name) : localize('welcomePage.installedExtensionPack', "{0} support is already installed", extension.name);
-				span.classList.add('enabledExtension');
-				span.setAttribute('data-extension', extension.id);
-				list.appendChild(span);
+					const span = document.createElement('span');
+					span.innerText = extension.name;
+					span.title = extension.isKeymap ? localize('welcomePage.installedKeymap', "{0} keymap is already installed", extension.name) : localize('welcomePage.installedExtensionPack', "{0} support is already installed", extension.name);
+					span.classList.add('enabledExtension');
+					span.setAttribute('data-extension', extension.id);
+					list.appendChild(span);
+				}
 			});
 		}
 	}
