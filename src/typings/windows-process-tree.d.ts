ead952302ae3f71d94fad327c1e532264e253801
Amy Qiu <umberos@gmail.com>
2017-08-21 11:43:35 -0700

    Integrate native module for WindowsShellHelper

---
+++ src/typings/windows-process-tree.d.ts
@@ -0,0 +1,9 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+declare module 'windows-process-tree' {
+	function get(rootPid: number, callback: Function): void;
+	export = get;
+}
