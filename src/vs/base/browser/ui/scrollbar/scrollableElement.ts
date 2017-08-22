9c46f948fa2a3680b88ca878e4733fd82e0f9c56
Alex Dima <alexdima@microsoft.com>
2017-08-22 17:48:16 +0200

    Add a physical mouse wheel classifier with tests

--- src/vs/base/browser/ui/scrollbar/scrollableElement.ts
+++ src/vs/base/browser/ui/scrollbar/scrollableElement.ts
@@ -31,6 +31,126 @@ export interface IOverviewRulerLayoutInfo {
 	insertBefore: HTMLElement;
 }
 
+class MouseWheelClassifierItem {
+	public timestamp: number;
+	public deltaX: number;
+	public deltaY: number;
+	public score: number;
+
+	constructor(timestamp: number, deltaX: number, deltaY: number) {
+		this.timestamp = timestamp;
+		this.deltaX = deltaX;
+		this.deltaY = deltaY;
+		this.score = 0;
+	}
+}
+
+export class MouseWheelClassifier {
+
+	private readonly _capacity: number;
+	private _memory: MouseWheelClassifierItem[];
+	private _front: number;
+	private _rear: number;
+
+	constructor() {
+		this._capacity = 5;
+		this._memory = [];
+		this._front = -1;
+		this._rear = -1;
+	}
+
+	// private isE
+
+	public isPhysicalMouseWheel(): boolean {
+		if (this._front === -1 && this._rear === -1) {
+			// no elements
+			return false;
+		}
+
+		// 0.5 * last + 0.25 * before last + 0.125 * before before last + ...
+		let remainingInfluence = 1;
+		let score = 0;
+		let iteration = 1;
+
+		let index = this._rear;
+		do {
+			const influence = (index === this._front ? remainingInfluence : Math.pow(2, -iteration));
+			remainingInfluence -= influence;
+			score += this._memory[index].score * influence;
+
+			if (index === this._front) {
+				break;
+			}
+
+			index = (this._capacity + index - 1) % this._capacity;
+			iteration++;
+		} while (true);
+
+		return (score <= 0.5);
+	}
+
+	public accept(timestamp: number, deltaX: number, deltaY: number): void {
+		const item = new MouseWheelClassifierItem(timestamp, deltaX, deltaY);
+		item.score = this._computeScore(item);
+
+		if (this._front === -1 && this._rear === -1) {
+			this._memory[0] = item;
+			this._front = 0;
+			this._rear = 0;
+		} else {
+			this._rear = (this._rear + 1) % this._capacity;
+			if (this._rear === this._front) {
+				// Drop oldest
+				this._front = (this._front + 1) % this._capacity;
+			}
+			this._memory[this._rear] = item;
+
+			// if ()
+			// if (this._front === this._rea)
+		}
+		// this._lastIndex = (this._lastIndex + 1) % this._capacity;
+		// if (this._firstIndex === this._lastIndex) {
+		// 	this._firstIndex = (this._firstIndex + 1) % this._capacity;
+		// }
+		// this._memory[this._lastIndex] = item;
+	}
+
+	/**
+	 * A score between 0 and 1 for `item`.
+	 *  - a score towards 0 indicates that the source appears to be a physical mouse wheel
+	 *  - a score towards 1 indicates that the source appears to be a touchpad or magic mouse, etc.
+	 */
+	private _computeScore(item: MouseWheelClassifierItem): number {
+
+		if (Math.abs(item.deltaX) > 0 && Math.abs(item.deltaY) > 0) {
+			// both axes exercised => definitely not a physical mouse wheel
+			return 1;
+		}
+
+		let score: number = 0.5;
+		const prev = (this._front === -1 && this._rear === -1 ? null : this._memory[this._rear]);
+		if (prev) {
+			// const deltaT = item.timestamp - prev.timestamp;
+			// if (deltaT < 1000 / 30) {
+			// 	// sooner than X times per second => indicator that this is not a physical mouse wheel
+			// 	score += 0.25;
+			// }
+
+			// if (item.deltaX === prev.deltaX && item.deltaY === prev.deltaY) {
+			// 	// equal amplitude => indicator that this is a physical mouse wheel
+			// 	score -= 0.25;
+			// }
+		}
+
+		if (Math.abs(item.deltaX - Math.round(item.deltaX)) > 0 || Math.abs(item.deltaY - Math.round(item.deltaY)) > 0) {
+			// non-integer deltas => indicator that this is not a physical mouse wheel
+			score += 0.25;
+		}
+
+		return Math.min(Math.max(score, 0), 1);
+	}
+}
+
 export abstract class AbstractScrollableElement extends Widget {
 
 	private readonly _options: ScrollableElementResolvedOptions;
@@ -215,6 +335,8 @@ export abstract class AbstractScrollableElement extends Widget {
 
 	private _onMouseWheel(e: StandardMouseWheelEvent): void {
 		const currentMouseWheelEventTime = Date.now();
+
+		// console.log(`${Date.now()}, ${e.deltaY}, ${e.deltaX}`);
 
 		if (e.deltaY || e.deltaX) {
 			let deltaY = e.deltaY * this._options.mouseWheelScrollSensitivity;
