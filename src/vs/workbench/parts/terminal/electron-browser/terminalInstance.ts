dbc55fec87ad3a2059afc57bfb8f190559a36df9
Daniel Imms <daimms@microsoft.com>
2017-08-29 12:53:08 -0700

    Revert "Use dynamic import for WindowsShellHelper"
    This reverts commit 665b0d008324a42575c4f83ec69ca93a1785f611.

--- src/vs/workbench/parts/terminal/electron-browser/terminalInstance.ts
+++ src/vs/workbench/parts/terminal/electron-browser/terminalInstance.ts
@@ -163,9 +163,7 @@ export class TerminalInstance implements ITerminalInstance {
 		if (platform.isWindows) {
 			this._processReady.then(() => {
 				if (!this._isDisposed) {
-					import('vs/workbench/parts/terminal/electron-browser/windowsShellHelper').then((module) => {
-						this._windowsShellHelper = new module.WindowsShellHelper(this._processId, this._shellLaunchConfig.executable, this, this._xterm);
-					});
+					this._windowsShellHelper = new WindowsShellHelper(this._processId, this._shellLaunchConfig.executable, this, this._xterm);
 				}
 			});
 		}
