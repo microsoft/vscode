72af20ce23ab17b8373becd30221269d3a5f82eb
Daniel Imms <daimms@microsoft.com>
2017-09-06 16:27:25 -0700

    Partial support for new link API

--- src/typings/xterm.d.ts
+++ src/typings/xterm.d.ts
@@ -134,6 +134,16 @@ interface ILinkMatcherOptions {
 	validationCallback?: (uri: string, element: HTMLElement, callback: (isValid: boolean) => void) => void;
 
 	/**
+	 * A callback that fires when the mouse hovers over a link.
+	 */
+	hoverStartCallback?: (event: MouseEvent, uri: string) => boolean | void;
+
+	/**
+	 * A callback that fires when the mouse leaves a link that was hovered.
+	 */
+	hoverEndCallback?: (event: MouseEvent, uri: string) => boolean | void;
+
+	/**
 	 * The priority of the link matcher, this defines the order in which the link
 	 * matcher is evaluated relative to others, from highest to lowest. The
 	 * default value is 0.
