7408fc38cc7030f8768d5e5174e61b64f7eeeca0
Joao <mail@joaomoreno.com>
2017-09-06 09:38:03 +0200

    fix smoke extensions dir

--- test/smoke/src/spectron/application.ts
+++ test/smoke/src/spectron/application.ts
@@ -15,7 +15,7 @@ export const STABLE_PATH = process.env.VSCODE_STABLE_PATH || '';
 export const WORKSPACE_PATH = process.env.SMOKETEST_REPO || '';
 export const CODE_WORKSPACE_PATH = process.env.VSCODE_WORKSPACE_PATH || '';
 export const USER_DIR = path.join(__dirname, '../../test_data/temp_user_dir');
-export const EXTENSIONS_DIR = path.join(__dirname, 'test_data/temp_extensions_dir');
+export const EXTENSIONS_DIR = path.join(__dirname, '../../test_data/temp_extensions_dir');
 
 /**
  * Wraps Spectron's Application instance with its used methods.
