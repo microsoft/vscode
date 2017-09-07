77bf47f1412be4e88f16d53a9fda76df5c069cc1
isidor <inikolic@microsoft.com>
2017-09-07 11:29:05 +0200

    debug: smoke test

--- test/smoke/src/areas/debug/debug.test.ts
+++ test/smoke/src/areas/debug/debug.test.ts
@@ -4,7 +4,8 @@
  *--------------------------------------------------------------------------------------------*/
 
 import * as assert from 'assert';
-var stripJsonComments = require('strip-json-comments');
+import * as http from 'http';
+import * as stripJsonComments from 'strip-json-comments';
 import { SpectronApplication, VSCODE_BUILD } from '../../spectron/application';
 
 describe('Debug', () => {
@@ -22,6 +23,7 @@ describe('Debug', () => {
 		await app.workbench.debug.configure();
 		const content = await app.workbench.editor.getEditorVisibleText();
 		const json = JSON.parse(stripJsonComments(content));
+
 		assert.equal(json.configurations[0].request, 'launch');
 		assert.equal(json.configurations[0].type, 'node');
 		if (process.platform === 'win32') {
@@ -34,6 +36,39 @@ describe('Debug', () => {
 	it('breakpoints', async function () {
 		await app.workbench.openFile('index.js');
 		await app.workbench.debug.setBreakpointOnLine(6);
-		assert.equal(true, true);
+	});
+
+	it('start debugging', async function () {
+		await app.workbench.debug.startDebugging();
+		setTimeout(() => http.get('http://localhost:3000').on('error', e => void 0), 200);
+		await app.workbench.debug.waitForStackFrame(sf => sf.name === 'index.js' && sf.lineNumber === 6);
+	});
+
+	it('focus stack frames and variables', async function () {
+		assert.equal(await app.workbench.debug.getLocalVariableCount(), 4);
+		await app.workbench.debug.focusStackFrame('layer.js');
+		assert.equal(await app.workbench.debug.getLocalVariableCount(), 5);
+		await app.workbench.debug.focusStackFrame('route.js');
+		assert.equal(await app.workbench.debug.getLocalVariableCount(), 3);
+		await app.workbench.debug.focusStackFrame('index.js');
+		assert.equal(await app.workbench.debug.getLocalVariableCount(), 4);
+	});
+
+	it('stepOver, stepIn, stepOut', async function () {
+		await app.workbench.debug.stepIn();
+		const first = await app.workbench.debug.waitForStackFrame(sf => sf.name === 'response.js');
+		await app.workbench.debug.stepOver();
+		await app.workbench.debug.waitForStackFrame(sf => sf.name === 'response.js' && sf.lineNumber === first.lineNumber + 1);
+		await app.workbench.debug.stepOut();
+		await app.workbench.debug.waitForStackFrame(sf => sf.name === 'index.js' && sf.lineNumber === 7);
+	});
+
+	it('debug console', async function () {
+		const result = await app.workbench.debug.console('2 + 2 \n', 'number');
+		assert.equal(result, '4');
+	});
+
+	it('stop debugging', async function () {
+		await app.workbench.debug.stopDebugging();
 	});
 });
