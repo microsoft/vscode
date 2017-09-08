945e53a03e2e97a38eaab138ba9047f17d705bb9
Christof Marti <chrmarti@microsoft.com>
2017-09-08 09:51:55 -0700

    Defer loading of keytar (fixes #33998)

--- src/vs/platform/credentials/node/credentialsService.ts
+++ src/vs/platform/credentials/node/credentialsService.ts
@@ -6,22 +6,29 @@
 
 import { TPromise } from 'vs/base/common/winjs.base';
 import { ICredentialsService } from 'vs/platform/credentials/common/credentials';
-import * as keytar from 'keytar';
 
 export class CredentialsService implements ICredentialsService {
 
 	_serviceBrand: any;
 
 	readSecret(service: string, account: string): TPromise<string | undefined> {
-		return TPromise.wrap(keytar.getPassword(service, account))
+		return this.getKeytar()
+			.then(keytar => TPromise.wrap(keytar.getPassword(service, account)))
 			.then(result => result === null ? undefined : result);
 	}
 
 	writeSecret(service: string, account: string, secret: string): TPromise<void> {
-		return TPromise.wrap(keytar.setPassword(service, account, secret));
+		return this.getKeytar()
+			.then(keytar => TPromise.wrap(keytar.setPassword(service, account, secret)));
 	}
 
 	deleteSecret(service: string, account: string): TPromise<boolean> {
-		return TPromise.wrap(keytar.deletePassword(service, account));
+		return this.getKeytar()
+			.then(keytar => TPromise.wrap(keytar.deletePassword(service, account)));
+	}
+
+	private getKeytar() {
+		// Avoids https://github.com/Microsoft/vscode/issues/33998
+		return TPromise.wrap(import('keytar'));
 	}
 }
