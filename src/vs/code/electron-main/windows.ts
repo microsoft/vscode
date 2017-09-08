d961b290ee138d16dcc655f9a6a84f4f738f8c22
Benjamin Pasero <benjpas@microsoft.com>
2017-09-08 10:26:02 +0200

    electron: leverage new 'treatPackageAsDirectory' property

--- src/vs/code/electron-main/windows.ts
+++ src/vs/code/electron-main/windows.ts
@@ -1720,6 +1720,10 @@ class FileDialog {
 			options.dialogOptions.properties = ['multiSelections', options.pickFolders ? 'openDirectory' : 'openFile', 'createDirectory'];
 		}
 
+		if (isMacintosh) {
+			options.dialogOptions.properties.push('treatPackageAsDirectory'); // always drill into .app files
+		}
+
 		// Show Dialog
 		const focusedWindow = this.windowsMainService.getWindowById(options.windowId) || this.windowsMainService.getFocusedWindow();
 		dialog.showOpenDialog(focusedWindow && focusedWindow.win, options.dialogOptions, paths => {
