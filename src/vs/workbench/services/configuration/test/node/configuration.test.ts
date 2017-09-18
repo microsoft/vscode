b2c2c49d2a9cbed65a8e95f8f4b2ff827aebd8b5
Sandeep Somavarapu <sasomava@microsoft.com>
2017-09-18 18:58:23 +0200

    Fix windows tests

--- src/vs/workbench/services/configuration/test/node/configuration.test.ts
+++ src/vs/workbench/services/configuration/test/node/configuration.test.ts
@@ -100,7 +100,7 @@ suite('WorkspaceContextService - Folder', () => {
 		assert.equal(actual.folders[0].uri.fsPath, URI.file(workspaceResource).fsPath);
 		assert.equal(actual.folders[0].name, workspaceName);
 		assert.equal(actual.folders[0].index, 0);
-		assert.equal(actual.folders[0].raw.path, workspaceResource);
+		assert.equal(actual.folders[0].raw.path.toLowerCase(), workspaceResource.toLowerCase());
 		assert.ok(!actual.configuration);
 	});
 
