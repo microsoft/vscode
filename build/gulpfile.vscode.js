38f614daba960bf7629c2d607068774e0ec3142c
Andre Weinand <aweinand@microsoft.com>
2017-09-18 23:33:45 +0200

    node-debug@1.17.8 (WSL auto detection)

--- build/gulpfile.vscode.js
+++ build/gulpfile.vscode.js
@@ -43,7 +43,7 @@ const nodeModules = ['electron', 'original-fs']
 // Build
 
 const builtInExtensions = [
-	{ name: 'ms-vscode.node-debug', version: '1.17.7' },
+	{ name: 'ms-vscode.node-debug', version: '1.17.8' },
 	{ name: 'ms-vscode.node-debug2', version: '1.17.1' }
 ];
 
