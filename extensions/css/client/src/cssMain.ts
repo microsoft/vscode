1cf7612e308dcaf62a6074ae055529a848bf472b
Martin Aeschlimann <martinae@microsoft.com>
2017-09-21 12:03:08 +0200

    [css] move colorpresentations to language server

--- extensions/css/client/src/cssMain.ts
+++ extensions/css/client/src/cssMain.ts
@@ -6,14 +6,13 @@
 
 import * as path from 'path';
 
-import { languages, window, commands, ExtensionContext, TextDocument, ColorInformation, ColorPresentation, Color, TextEdit as CodeTextEdit } from 'vscode';
+import { languages, window, commands, ExtensionContext, TextDocument, ColorInformation, ColorPresentation, Color } from 'vscode';
 import { LanguageClient, LanguageClientOptions, ServerOptions, TransportKind, TextEdit } from 'vscode-languageclient';
 
-import { ConfigurationFeature } from 'vscode-languageclient/lib/proposed';
-import { DocumentColorRequest } from 'vscode-languageserver-protocol/lib/protocol.colorProvider.proposed';
+import { ConfigurationFeature } from 'vscode-languageclient/lib/configuration.proposed';
+import { DocumentColorRequest, DocumentColorParams, ColorPresentationRequest, ColorPresentationParams } from 'vscode-languageserver-protocol/lib/protocol.colorProvider.proposed';
 
 import * as nls from 'vscode-nls';
-import * as convert from 'color-convert';
 let localize = nls.loadMessageBundle();
 
 // this method is called when vs code is activated
@@ -52,16 +51,13 @@ export function activate(context: ExtensionContext) {
 	// client can be deactivated on extension deactivation
 	context.subscriptions.push(disposable);
 
-	var _toTwoDigitHex = function (n: number): string {
-		const r = n.toString(16);
-		return r.length !== 2 ? '0' + r : r;
-	};
-
 	client.onReady().then(_ => {
 		// register color provider
 		context.subscriptions.push(languages.registerColorProvider(documentSelector, {
 			provideDocumentColors(document: TextDocument): Thenable<ColorInformation[]> {
-				let params = client.code2ProtocolConverter.asDocumentSymbolParams(document);
+				let params: DocumentColorParams = {
+					textDocument: client.code2ProtocolConverter.asTextDocumentIdentifier(document)
+				};
 				return client.sendRequest(DocumentColorRequest.type, params).then(symbols => {
 					return symbols.map(symbol => {
 						let range = client.protocol2CodeConverter.asRange(symbol.range);
@@ -71,34 +67,18 @@ export function activate(context: ExtensionContext) {
 				});
 			},
 			provideColorPresentations(document: TextDocument, colorInfo: ColorInformation): ColorPresentation[] | Thenable<ColorPresentation[]> {
-				let result: ColorPresentation[] = [];
-				let color = colorInfo.color;
-				let label;
-				if (color.alpha === 1) {
-					label = `rgb(${Math.round(color.red * 255)}, ${Math.round(color.green * 255)}, ${Math.round(color.blue * 255)})`;
-				} else {
-					label = `rgba(${Math.round(color.red * 255)}, ${Math.round(color.green * 255)}, ${Math.round(color.blue * 255)}, ${color.alpha})`;
-				}
-
-				result.push({ label: label, textEdit: new CodeTextEdit(colorInfo.range, label) });
-
-				if (color.alpha === 1) {
-					label = `#${_toTwoDigitHex(Math.round(color.red * 255))}${_toTwoDigitHex(Math.round(color.green * 255))}${_toTwoDigitHex(Math.round(color.blue * 255))}`;
-				} else {
-					label = `#${_toTwoDigitHex(Math.round(color.red * 255))}${_toTwoDigitHex(Math.round(color.green * 255))}${_toTwoDigitHex(Math.round(color.blue * 255))}${_toTwoDigitHex(Math.round(color.alpha * 255))}`;
-				}
-
-				result.push({ label: label, textEdit: new CodeTextEdit(colorInfo.range, label) });
-
-				const hsl = convert.rgb.hsl(Math.round(color.red * 255), Math.round(color.green * 255), Math.round(color.blue * 255));
-				if (color.alpha === 1) {
-					label = `hsl(${hsl[0]}, ${hsl[1]}%, ${hsl[2]}%)`;
-				} else {
-					label = `hsla(${hsl[0]}, ${hsl[1]}%, ${hsl[2]}%, ${color.alpha})`;
-				}
-
-				result.push({ label: label, textEdit: new CodeTextEdit(colorInfo.range, label) });
-				return result;
+				let params: ColorPresentationParams = {
+					textDocument: client.code2ProtocolConverter.asTextDocumentIdentifier(document),
+					colorInfo: { range: client.code2ProtocolConverter.asRange(colorInfo.range), color: colorInfo.color }
+				};
+				return client.sendRequest(ColorPresentationRequest.type, params).then(presentations => {
+					return presentations.map(p => {
+						let presentation = new ColorPresentation(p.label);
+						presentation.textEdit = p.textEdit && client.protocol2CodeConverter.asTextEdit(p.textEdit);
+						presentation.additionalTextEdits = p.additionalTextEdits && client.protocol2CodeConverter.asTextEdits(p.additionalTextEdits);
+						return presentation;
+					});
+				});
 			}
 		}));
 	});
