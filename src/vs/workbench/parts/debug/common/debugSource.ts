20a2c3880f4e51ec5e8e2bf898e63b230a815a5f
Benjamin Pasero <benjpas@microsoft.com>
2017-09-25 10:42:27 +0200

    hook into debug console

--- src/vs/workbench/parts/debug/common/debugSource.ts
+++ src/vs/workbench/parts/debug/common/debugSource.ts
@@ -7,13 +7,39 @@ import * as nls from 'vs/nls';
 import { TPromise } from 'vs/base/common/winjs.base';
 import uri from 'vs/base/common/uri';
 import * as paths from 'vs/base/common/paths';
-import { DEBUG_SCHEME } from 'vs/workbench/parts/debug/common/debug';
+import { DEBUG_SCHEME, IReplElementSource } from 'vs/workbench/parts/debug/common/debug';
 import { IRange } from 'vs/editor/common/core/range';
 import { IWorkbenchEditorService } from 'vs/workbench/services/editor/common/editorService';
 
 const UNKNOWN_SOURCE_LABEL = nls.localize('unknownSource', "Unknown Source");
 
-export class Source {
+export interface ISource {
+	readonly uri: uri;
+	name: string;
+
+	openInEditor(editorService: IWorkbenchEditorService, selection: IRange, preserveFocus?: boolean, sideBySide?: boolean): TPromise<any>;
+}
+
+export class SimpleSource implements IReplElementSource {
+
+	public readonly source: ISource;
+
+	constructor(public readonly uri: uri, public readonly lineNumber: number, public readonly column: number) {
+		this.source = {
+			name: paths.basename(uri.fsPath),
+			uri,
+			openInEditor: (editorService: IWorkbenchEditorService, selection: IRange, preserveFocus?: boolean, sideBySide?: boolean) => openInEditor(
+				this.uri,
+				editorService,
+				{ startColumn: this.column, startLineNumber: this.lineNumber } as IRange,
+				preserveFocus,
+				sideBySide
+			)
+		};
+	}
+}
+
+export class Source implements ISource {
 
 	public readonly uri: uri;
 	public available: boolean;
@@ -56,16 +82,32 @@ export class Source {
 	}
 
 	public openInEditor(editorService: IWorkbenchEditorService, selection: IRange, preserveFocus?: boolean, sideBySide?: boolean): TPromise<any> {
-		return !this.available ? TPromise.as(null) : editorService.openEditor({
-			resource: this.uri,
-			description: this.origin,
-			options: {
-				preserveFocus,
-				selection,
-				revealIfVisible: true,
-				revealInCenterIfOutsideViewport: true,
-				pinned: !preserveFocus && !this.inMemory
-			}
-		}, sideBySide);
+		if (!this.available) {
+			return TPromise.as(null);
+		}
+
+		return openInEditor(this.uri, editorService, selection, preserveFocus, sideBySide, !preserveFocus && !this.inMemory, this.origin);
 	}
+}
+
+function openInEditor(
+	uri: uri,
+	editorService: IWorkbenchEditorService,
+	selection: IRange,
+	preserveFocus?: boolean,
+	sideBySide?: boolean,
+	pinned?: boolean,
+	description?: string
+): TPromise<any> {
+	return editorService.openEditor({
+		resource: uri,
+		description,
+		options: {
+			preserveFocus,
+			selection,
+			revealIfVisible: true,
+			revealInCenterIfOutsideViewport: true,
+			pinned: typeof pinned === 'boolean' ? pinned : !preserveFocus
+		}
+	}, sideBySide);
 }
