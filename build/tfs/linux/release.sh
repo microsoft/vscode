9f304b7a4922488e33d5861fc2af3c5b160d9422
Daniel Imms <daimms@microsoft.com>
2017-10-02 13:36:59 -0700

    Don't build snap packages in CI yet

--- build/tfs/linux/release.sh
+++ build/tfs/linux/release.sh
@@ -9,8 +9,8 @@ step "Build Debian package" \
 step "Build RPM package" \
 	npm run gulp -- "vscode-linux-$ARCH-build-rpm"
 
-step "Build snap package" \
-	npm run gulp -- "vscode-linux-$ARCH-build-snap"
+# step "Build snap package" \
+# 	npm run gulp -- "vscode-linux-$ARCH-build-snap"
 
 (cd $BUILD_SOURCESDIRECTORY/build/tfs/common && \
 	step "Install build dependencies" \
@@ -52,10 +52,8 @@ RPM_PATH="$REPO/.build/linux/rpm/$RPM_ARCH/$RPM_FILENAME"
 step "Publish RPM package" \
 	node build/tfs/common/publish.js $VSCODE_QUALITY $PLATFORM_RPM package $RPM_FILENAME $VERSION true $RPM_PATH
 
-SNAP_FILENAME="$(ls $REPO/.build/linux/snap/$ARCH/ | grep .snap)"
-SNAP_PATH="$REPO/.build/linux/snap/$ARCH/$SNAP_FILENAME"
-echo 'SNAP_PATH'
-echo $SNAP_PATH
+# SNAP_FILENAME="$(ls $REPO/.build/linux/snap/$ARCH/ | grep .snap)"
+# SNAP_PATH="$REPO/.build/linux/snap/$ARCH/$SNAP_FILENAME"
 
 if [ -z "$VSCODE_QUALITY" ]; then
 	echo "VSCODE_QUALITY is not set, skipping repo package publish"
