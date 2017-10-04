d78d5da119e8aacf5ef252c2314d34502d800257
Joao Moreno <jomo@microsoft.com>
2017-10-04 22:27:28 +0200

    smoke: kill all testuser processes before smoketest

--- build/tfs/linux/build.sh
+++ build/tfs/linux/build.sh
@@ -48,6 +48,7 @@ function smoketest {
 	sudo -i -u testuser -- sh -c 'git config --global user.name "VS Code Agent" &&  git config --global user.email "monacotools@microsoft.com"'
 	chown -R testuser $SCREENSHOTS
 
+	ps -o pid= -u testuser | xargs sudo kill -9
 	DISPLAY=:10 sudo -i -u testuser -- sh -c "cd $BUILD_SOURCESDIRECTORY/test/smoke && ./node_modules/.bin/mocha --build $AGENT_BUILDDIRECTORY/VSCode-linux-$ARCH --screenshots $SCREENSHOTS"
 }
 
