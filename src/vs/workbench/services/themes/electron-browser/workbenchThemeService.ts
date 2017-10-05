58f45b6460d31f34d3de9b60fd70c364523ef4a1
Martin Aeschlimann <martinae@microsoft.com>
2017-10-05 18:03:53 +0200

    Move css  generation to fileIconThemeData

--- src/vs/workbench/services/themes/electron-browser/workbenchThemeService.ts
+++ src/vs/workbench/services/themes/electron-browser/workbenchThemeService.ts
@@ -5,10 +5,7 @@
 'use strict';
 
 import { TPromise, Promise } from 'vs/base/common/winjs.base';
-import URI from 'vs/base/common/uri';
 import nls = require('vs/nls');
-import * as Paths from 'path';
-import Json = require('vs/base/common/json');
 import * as types from 'vs/base/common/types';
 import * as objects from 'vs/base/common/objects';
 import { IExtensionService } from 'vs/platform/extensions/common/extensions';
@@ -38,7 +35,6 @@ import pfs = require('vs/base/node/pfs');
 import colorThemeSchema = require('vs/workbench/services/themes/common/colorThemeSchema');
 import fileIconThemeSchema = require('vs/workbench/services/themes/common/fileIconThemeSchema');
 import { IDisposable } from 'vs/base/common/lifecycle';
-import { getParseErrorMessage } from 'vs/base/common/jsonErrorMessages';
 import { IBroadcastService } from 'vs/platform/broadcast/electron-browser/broadcastService';
 import { ColorThemeStore } from 'vs/workbench/services/themes/electron-browser/colorThemeStore';
 import { FileIconThemeStore } from 'vs/workbench/services/themes/electron-browser/fileIconThemeStore';
@@ -57,6 +53,9 @@ const oldDefaultThemeExtensionId = 'vscode-theme-colorful-defaults';
 const DEFAULT_ICON_THEME_SETTING_VALUE = 'vs-seti';
 const fileIconsEnabledClass = 'file-icons-enabled';
 
+const colorThemeRulesClassName = 'contributedColorTheme';
+const iconThemeRulesClassName = 'contributedIconTheme';
+
 const themingRegistry = Registry.as<IThemingRegistry>(ThemingExtensions.ThemingContribution);
 
 function validateThemeId(theme: string): string {
@@ -71,55 +70,9 @@ function validateThemeId(theme: string): string {
 	return theme;
 }
 
-interface IconDefinition {
-	iconPath: string;
-	fontColor: string;
-	fontCharacter: string;
-	fontSize: string;
-	fontId: string;
-}
-
-interface FontDefinition {
-	id: string;
-	weight: string;
-	style: string;
-	size: string;
-	src: { path: string; format: string; }[];
-}
-
-interface IconsAssociation {
-	folder?: string;
-	file?: string;
-	folderExpanded?: string;
-	rootFolder?: string;
-	rootFolderExpanded?: string;
-	folderNames?: { [folderName: string]: string; };
-	folderNamesExpanded?: { [folderName: string]: string; };
-	fileExtensions?: { [extension: string]: string; };
-	fileNames?: { [fileName: string]: string; };
-	languageIds?: { [languageId: string]: string; };
-}
-
-interface IconThemeDocument extends IconsAssociation {
-	iconDefinitions: { [key: string]: IconDefinition };
-	fonts: FontDefinition[];
-	light?: IconsAssociation;
-	highContrast?: IconsAssociation;
-}
-
 export interface IColorCustomizations {
 	[colorId: string]: string;
 }
-
-const noFileIconTheme: IFileIconTheme = {
-	id: '',
-	label: '',
-	settingsId: null,
-	hasFileIcons: false,
-	hasFolderIcons: false,
-	isLoaded: true,
-	extensionData: null
-};
 
 export class WorkbenchThemeService implements IWorkbenchThemeService {
 	_serviceBrand: any;
@@ -486,7 +439,7 @@ export class WorkbenchThemeService implements IWorkbenchThemeService {
 			if (newIconTheme) {
 				this.currentIconTheme = newIconTheme;
 			} else {
-				this.currentIconTheme = noFileIconTheme;
+				this.currentIconTheme = FileIconThemeData.noIconTheme();
 			}
 
 			if (this.container) {
@@ -529,205 +482,13 @@ function _applyIconTheme(data: FileIconThemeData, onApply: (theme: FileIconTheme
 		_applyRules('', iconThemeRulesClassName);
 		return TPromise.as(onApply(data));
 	}
-
-	if (data.styleSheetContent) {
-		_applyRules(data.styleSheetContent, iconThemeRulesClassName);
-		return TPromise.as(onApply(data));
-	}
-	return _loadIconThemeDocument(data.path).then(iconThemeDocument => {
-		let result = _processIconThemeDocument(data.id, data.path, iconThemeDocument);
-		data.styleSheetContent = result.content;
-		data.hasFileIcons = result.hasFileIcons;
-		data.hasFolderIcons = result.hasFolderIcons;
-		data.isLoaded = true;
-		_applyRules(data.styleSheetContent, iconThemeRulesClassName);
+	return data.ensureLoaded(this).then(styleSheetContent => {
+		_applyRules(styleSheetContent, iconThemeRulesClassName);
 		return onApply(data);
 	}, error => {
 		return TPromise.wrapError<IFileIconTheme>(new Error(nls.localize('error.cannotloadicontheme', "Unable to load {0}", data.path)));
 	});
 }
-
-function _loadIconThemeDocument(fileSetPath: string): TPromise<IconThemeDocument> {
-	return pfs.readFile(fileSetPath).then(content => {
-		let errors: Json.ParseError[] = [];
-		let contentValue = Json.parse(content.toString(), errors);
-		if (errors.length > 0) {
-			return TPromise.wrapError(new Error(nls.localize('error.cannotparseicontheme', "Problems parsing file icons file: {0}", errors.map(e => getParseErrorMessage(e.error)).join(', '))));
-		}
-		return TPromise.as(contentValue);
-	});
-}
-
-function _processIconThemeDocument(id: string, iconThemeDocumentPath: string, iconThemeDocument: IconThemeDocument): { content: string; hasFileIcons: boolean; hasFolderIcons: boolean; } {
-
-	let result = { content: '', hasFileIcons: false, hasFolderIcons: false };
-
-	if (!iconThemeDocument.iconDefinitions) {
-		return result;
-	}
-	let selectorByDefinitionId: { [def: string]: string[] } = {};
-
-	function resolvePath(path: string) {
-		const uri = URI.file(Paths.join(Paths.dirname(iconThemeDocumentPath), path));
-		return uri.toString();
-	}
-
-	function collectSelectors(associations: IconsAssociation, baseThemeClassName?: string) {
-		function addSelector(selector: string, defId: string) {
-			if (defId) {
-				let list = selectorByDefinitionId[defId];
-				if (!list) {
-					list = selectorByDefinitionId[defId] = [];
-				}
-				list.push(selector);
-			}
-		}
-		if (associations) {
-			let qualifier = '.show-file-icons';
-			if (baseThemeClassName) {
-				qualifier = baseThemeClassName + ' ' + qualifier;
-			}
-
-			let expanded = '.monaco-tree-row.expanded'; // workaround for #11453
-
-			if (associations.folder) {
-				addSelector(`${qualifier} .folder-icon::before`, associations.folder);
-				result.hasFolderIcons = true;
-			}
-
-			if (associations.folderExpanded) {
-				addSelector(`${qualifier} ${expanded} .folder-icon::before`, associations.folderExpanded);
-				result.hasFolderIcons = true;
-			}
-
-			let rootFolder = associations.rootFolder || associations.folder;
-			let rootFolderExpanded = associations.rootFolderExpanded || associations.folderExpanded;
-
-			if (rootFolder) {
-				addSelector(`${qualifier} .rootfolder-icon::before`, rootFolder);
-				result.hasFolderIcons = true;
-			}
-
-			if (rootFolderExpanded) {
-				addSelector(`${qualifier} ${expanded} .rootfolder-icon::before`, rootFolderExpanded);
-				result.hasFolderIcons = true;
-			}
-
-			if (associations.file) {
-				addSelector(`${qualifier} .file-icon::before`, associations.file);
-				result.hasFileIcons = true;
-			}
-
-			let folderNames = associations.folderNames;
-			if (folderNames) {
-				for (let folderName in folderNames) {
-					addSelector(`${qualifier} .${escapeCSS(folderName.toLowerCase())}-name-folder-icon.folder-icon::before`, folderNames[folderName]);
-					result.hasFolderIcons = true;
-				}
-			}
-			let folderNamesExpanded = associations.folderNamesExpanded;
-			if (folderNamesExpanded) {
-				for (let folderName in folderNamesExpanded) {
-					addSelector(`${qualifier} ${expanded} .${escapeCSS(folderName.toLowerCase())}-name-folder-icon.folder-icon::before`, folderNamesExpanded[folderName]);
-					result.hasFolderIcons = true;
-				}
-			}
-
-			let languageIds = associations.languageIds;
-			if (languageIds) {
-				for (let languageId in languageIds) {
-					addSelector(`${qualifier} .${escapeCSS(languageId)}-lang-file-icon.file-icon::before`, languageIds[languageId]);
-					result.hasFileIcons = true;
-				}
-			}
-			let fileExtensions = associations.fileExtensions;
-			if (fileExtensions) {
-				for (let fileExtension in fileExtensions) {
-					let selectors: string[] = [];
-					let segments = fileExtension.toLowerCase().split('.');
-					if (segments.length) {
-						for (let i = 0; i < segments.length; i++) {
-							selectors.push(`.${escapeCSS(segments.slice(i).join('.'))}-ext-file-icon`);
-						}
-						selectors.push('.ext-file-icon'); // extra segment to increase file-ext score
-					}
-					addSelector(`${qualifier} ${selectors.join('')}.file-icon::before`, fileExtensions[fileExtension]);
-					result.hasFileIcons = true;
-				}
-			}
-			let fileNames = associations.fileNames;
-			if (fileNames) {
-				for (let fileName in fileNames) {
-					let selectors: string[] = [];
-					fileName = fileName.toLowerCase();
-					selectors.push(`.${escapeCSS(fileName)}-name-file-icon`);
-					let segments = fileName.split('.');
-					if (segments.length) {
-						for (let i = 1; i < segments.length; i++) {
-							selectors.push(`.${escapeCSS(segments.slice(i).join('.'))}-ext-file-icon`);
-						}
-						selectors.push('.ext-file-icon'); // extra segment to increase file-ext score
-					}
-					addSelector(`${qualifier} ${selectors.join('')}.file-icon::before`, fileNames[fileName]);
-					result.hasFileIcons = true;
-				}
-			}
-		}
-	}
-	collectSelectors(iconThemeDocument);
-	collectSelectors(iconThemeDocument.light, '.vs');
-	collectSelectors(iconThemeDocument.highContrast, '.hc-black');
-
-	if (!result.hasFileIcons && !result.hasFolderIcons) {
-		return result;
-	}
-
-	let cssRules: string[] = [];
-
-	let fonts = iconThemeDocument.fonts;
-	if (Array.isArray(fonts)) {
-		fonts.forEach(font => {
-			let src = font.src.map(l => `url('${resolvePath(l.path)}') format('${l.format}')`).join(', ');
-			cssRules.push(`@font-face { src: ${src}; font-family: '${font.id}'; font-weigth: ${font.weight}; font-style: ${font.style}; }`);
-		});
-		cssRules.push(`.show-file-icons .file-icon::before, .show-file-icons .folder-icon::before, .show-file-icons .rootfolder-icon::before { font-family: '${fonts[0].id}'; font-size: ${fonts[0].size || '150%'}}`);
-	}
-
-	for (let defId in selectorByDefinitionId) {
-		let selectors = selectorByDefinitionId[defId];
-		let definition = iconThemeDocument.iconDefinitions[defId];
-		if (definition) {
-			if (definition.iconPath) {
-				cssRules.push(`${selectors.join(', ')} { content: ' '; background-image: url("${resolvePath(definition.iconPath)}"); }`);
-			}
-			if (definition.fontCharacter || definition.fontColor) {
-				let body = '';
-				if (definition.fontColor) {
-					body += ` color: ${definition.fontColor};`;
-				}
-				if (definition.fontCharacter) {
-					body += ` content: '${definition.fontCharacter}';`;
-				}
-				if (definition.fontSize) {
-					body += ` font-size: ${definition.fontSize};`;
-				}
-				if (definition.fontId) {
-					body += ` font-family: ${definition.fontId};`;
-				}
-				cssRules.push(`${selectors.join(', ')} { ${body} }`);
-			}
-		}
-	}
-	result.content = cssRules.join('\n');
-	return result;
-}
-
-function escapeCSS(str: string) {
-	return window['CSS'].escape(str);
-}
-
-let colorThemeRulesClassName = 'contributedColorTheme';
-let iconThemeRulesClassName = 'contributedIconTheme';
 
 function _applyRules(styleSheetContent: string, rulesClassName: string) {
 	let themeStyles = document.head.getElementsByClassName(rulesClassName);
