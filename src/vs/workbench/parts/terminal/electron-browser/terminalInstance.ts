3ffa14e5ed86b3489f86d9f8d966ae3076f94a61
Daniel Imms <daimms@microsoft.com>
2017-10-10 21:24:19 -0700

    Revert "Only set LANG env var in term if setLocaleVariables is true"
    This reverts commit a91bad463a4a90409fd6012e4e31a63364cca44b.

--- src/vs/workbench/parts/terminal/electron-browser/terminalInstance.ts
+++ src/vs/workbench/parts/terminal/electron-browser/terminalInstance.ts
@@ -780,9 +780,7 @@ export class TerminalInstance implements ITerminalInstance {
 			}
 		}
 		env['PTYCWD'] = cwd;
-		if (locale) {
-			env['LANG'] = TerminalInstance._getLangEnvVariable(locale);
-		}
+		env['LANG'] = TerminalInstance._getLangEnvVariable(locale);
 		if (cols && rows) {
 			env['PTYCOLS'] = cols.toString();
 			env['PTYROWS'] = rows.toString();
