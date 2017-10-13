6c40a968f42b464b38263c177e58108f71b973b6
BattleBas <battlebas@users.noreply.github.com>
2017-10-12 20:19:07 -0500

    Added setting "git.autoRepositoryDetection"
    When opening just one file, the setting "autoRepositoryDetection" allows the user to control whether the entire repository changes will be displayed or just the changes of the current file.

--- extensions/git/src/repository.ts
+++ extensions/git/src/repository.ts
@@ -742,9 +742,18 @@ export class Repository implements Disposable {
 		const index: Resource[] = [];
 		const workingTree: Resource[] = [];
 		const merge: Resource[] = [];
+		const repoDetection = config.get<boolean>('autoRepositoryDetection') === true;
 
 		status.forEach(raw => {
-			const uri = Uri.file(path.join(this.repository.root, raw.path));
+			const fullFilePath = path.join(this.repository.root, raw.path);
+
+			if (!repoDetection && workspace.workspaceFolders === undefined) {
+				if (!this.detectActiveFile(fullFilePath)) {
+					return;
+				}
+			}
+
+			const uri = Uri.file(fullFilePath);
 			const renameUri = raw.rename ? Uri.file(path.join(this.repository.root, raw.rename)) : undefined;
 
 			switch (raw.x + raw.y) {
@@ -800,6 +809,16 @@ export class Repository implements Disposable {
 		}
 
 		this._onDidChangeStatus.fire();
+	}
+
+	private detectActiveFile(fullFilePath: string): boolean {
+		if (window.activeTextEditor !== undefined) {
+			if (window.activeTextEditor.document.fileName === fullFilePath) {
+				return true;
+			}
+		}
+
+		return false;
 	}
 
 	private onFSChange(uri: Uri): void {
