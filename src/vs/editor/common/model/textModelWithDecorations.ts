7b9e4aebffa5a5f9b2048199051002f7a9aad891
Alex Dima <alexdima@microsoft.com>
2017-10-17 12:13:17 +0200

    Flip the switch

--- src/vs/editor/common/model/textModelWithDecorations.ts
+++ src/vs/editor/common/model/textModelWithDecorations.ts
@@ -19,7 +19,7 @@ import * as textModelEvents from 'vs/editor/common/model/textModelEvents';
 import { ThemeColor } from 'vs/platform/theme/common/themeService';
 import { IntervalNode, IntervalTree } from 'vs/editor/common/model/intervalTree';
 
-export const USE_NEW_DECORATIONS = false;
+export const USE_NEW_DECORATIONS = true;
 
 export const ClassName = {
 	EditorInfoDecoration: 'infosquiggly',
