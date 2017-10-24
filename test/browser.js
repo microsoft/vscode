28facef5116f81610d49b17a2a299aacc8355b52
Bradllez <bradllez@me.com>
2017-10-23 23:21:06 -0400

    Update browser.js

--- test/browser.js
+++ test/browser.js
@@ -2,7 +2,7 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-
+/remember to edit these first
 var express = require('express');
 var glob = require('glob');
 var path = require('path');
