509630ffe3fabf2e1f49b416bb83f5ff0877b042
Don Jayamanne <don.jayamanne@yahoo.com>
2017-10-24 21:53:24 -0700

    fix https://github.com/Microsoft/vscode/issues/36854 (#36855)

--- src/vs/workbench/api/node/extHostQuickOpen.ts
+++ src/vs/workbench/api/node/extHostQuickOpen.ts
@@ -128,12 +128,12 @@ export class ExtHostQuickOpen implements ExtHostQuickOpenShape {
 	// ---- workspace folder picker
 
 	showWorkspaceFolderPick(options?: WorkspaceFolderPickOptions, token = CancellationToken.None): Thenable<WorkspaceFolder> {
-		return this._commands.executeCommand('_workbench.pickWorkspaceFolder', [options]).then((folder: WorkspaceFolder) => {
-			if (!folder) {
+		return this._commands.executeCommand('_workbench.pickWorkspaceFolder', [options]).then((selectedFolder: WorkspaceFolder) => {
+			if (!selectedFolder) {
 				return undefined;
 			}
 
-			return this._workspace.getWorkspaceFolders().filter(folder => folder.uri.toString() === folder.uri.toString())[0];
+			return this._workspace.getWorkspaceFolders().filter(folder => folder.uri.toString() === selectedFolder.uri.toString())[0];
 		});
 	}
 }
