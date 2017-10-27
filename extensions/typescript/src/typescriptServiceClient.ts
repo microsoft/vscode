7895b18115a55a87603e10dae310296dd90ae29c
Matt Bierner <matb@microsoft.com>
2017-10-27 16:29:48 -0700

    Extract getCompilerOptions to own method

--- extensions/typescript/src/typescriptServiceClient.ts
+++ extensions/typescript/src/typescriptServiceClient.ts
@@ -466,6 +466,15 @@ export default class TypeScriptServiceClient implements ITypescriptServiceClient
 			return;
 		}
 
+		const args: Proto.SetCompilerOptionsForInferredProjectsArgs = {
+			options: this.getCompilerOptionsForInferredProjects(configuration)
+		};
+		this.execute('compilerOptionsForInferredProjects', args, true).catch((err) => {
+			this.error(`'compilerOptionsForInferredProjects' request failed with error.`, err);
+		});
+	}
+
+	private getCompilerOptionsForInferredProjects(configuration: TypeScriptServiceConfiguration): Proto.ExternalProjectCompilerOptions {
 		const compilerOptions: Proto.ExternalProjectCompilerOptions = {
 			module: 'CommonJS' as Proto.ModuleKind,
 			target: 'ES6' as Proto.ScriptTarget,
@@ -479,13 +488,7 @@ export default class TypeScriptServiceClient implements ITypescriptServiceClient
 			compilerOptions.checkJs = configuration.checkJs;
 			compilerOptions.experimentalDecorators = configuration.experimentalDecorators;
 		}
-
-		const args: Proto.SetCompilerOptionsForInferredProjectsArgs = {
-			options: compilerOptions
-		};
-		this.execute('compilerOptionsForInferredProjects', args, true).catch((err) => {
-			this.error(`'compilerOptionsForInferredProjects' request failed with error.`, err);
-		});
+		return compilerOptions;
 	}
 
 	private serviceExited(restart: boolean): void {
