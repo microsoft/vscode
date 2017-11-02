7bb32809e4eba227762893b63b32ecae9fce2d33
Benjamin Pasero <benjpas@microsoft.com>
2017-11-02 11:12:57 +0100

    icon: ensure icon refresh on macOS for new stable icon

--- src/vs/code/electron-main/app.ts
+++ src/vs/code/electron-main/app.ts
@@ -59,7 +59,7 @@ import { touch } from 'vs/base/node/pfs';
 
 export class CodeApplication {
 
-	private static APP_ICON_REFRESH_KEY = 'macOSAppIconRefresh2';
+	private static APP_ICON_REFRESH_KEY = 'macOSAppIconRefresh3';
 
 	private toDispose: IDisposable[];
 	private windowsMainService: IWindowsMainService;
