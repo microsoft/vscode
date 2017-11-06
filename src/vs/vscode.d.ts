a17e37bf48a3a79f3f5341c21ea931c6514cbc47
Benjamin Pasero <benjpas@microsoft.com>
2017-11-06 11:48:22 +0100

    api - document glob support

--- src/vs/vscode.d.ts
+++ src/vs/vscode.d.ts
@@ -1715,6 +1715,14 @@ declare module 'vscode' {
 	/**
 	 * A file glob pattern to match file paths against. This can either be a glob pattern string
 	 * (like `**∕*.{ts,js}` or `*.{ts,js}`) or a [relative pattern](#RelativePattern).
+	 *
+	 * Glob patterns can have the following syntax:
+	 * * `*` to match one or more characters in a path segment
+	 * * `?` to match on one character in a path segment
+	 * * `**` to match any number of path segments, including none
+	 * * `{}` to group conditions (e.g. `**∕*.{ts,js}` matches all TypeScript and JavaScript files)
+	 * * `[]` to declare a range of characters to match in a path segment (e.g., `example.[0-9]` to match on `example.0`, `example.1`, …)
+	 * * `[!...]` to negate a range of characters to match in a path segment (e.g., `example.[!0-9]` to match on `example.a`, `example.b`, but not `example.0`)
 	 */
 	export type GlobPattern = string | RelativePattern;
 
