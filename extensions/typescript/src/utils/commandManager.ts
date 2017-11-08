4b6df2f839ee574c1e44094316e7c03abe74a81a
Matt Bierner <matb@microsoft.com>
2017-11-08 09:42:13 -0800

    Encapsulate TS/JS diagnostics in class

--- extensions/typescript/src/utils/commandManager.ts
+++ extensions/typescript/src/utils/commandManager.ts
@@ -21,16 +21,16 @@ export class CommandManager {
 		this.commands.clear();
 	}
 
+	public register<T extends Command>(command: T): T {
+		this.registerCommand(command.id, command.execute, command);
+		return command;
+	}
+
 	private registerCommand(id: string, impl: (...args: any[]) => void, thisArg?: any) {
 		if (this.commands.has(id)) {
 			return;
 		}
 
 		this.commands.set(id, vscode.commands.registerCommand(id, impl, thisArg));
-	}
-
-	public register<T extends Command>(command: T): T {
-		this.registerCommand(command.id, command.execute, command);
-		return command;
 	}
 }
