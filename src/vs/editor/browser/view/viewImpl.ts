9c4266fb7ccf1dbc3bed5c16f193de21fff4d679
Alex Dima <alexdima@microsoft.com>
2017-11-13 12:11:42 +0100

    Render selections on top

--- src/vs/editor/browser/view/viewImpl.ts
+++ src/vs/editor/browser/view/viewImpl.ts
@@ -177,9 +177,9 @@ export class View extends ViewEventHandler {
 		let contentViewOverlays = new ContentViewOverlays(this._context);
 		this.viewParts.push(contentViewOverlays);
 		contentViewOverlays.addDynamicOverlay(new CurrentLineHighlightOverlay(this._context));
-		contentViewOverlays.addDynamicOverlay(new SelectionsOverlay(this._context));
 		contentViewOverlays.addDynamicOverlay(new DecorationsOverlay(this._context));
 		contentViewOverlays.addDynamicOverlay(new IndentGuidesOverlay(this._context));
+		contentViewOverlays.addDynamicOverlay(new SelectionsOverlay(this._context));
 
 		let marginViewOverlays = new MarginViewOverlays(this._context);
 		this.viewParts.push(marginViewOverlays);
