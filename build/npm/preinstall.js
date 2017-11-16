c4cb8bf20d02d60120415511f90c95e202c6723f
Joao Moreno <jomo@microsoft.com>
2017-11-16 10:19:03 +0100

    improve preinstall script

--- build/npm/preinstall.js
+++ build/npm/preinstall.js
@@ -3,8 +3,21 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-if (!/yarn\.js$/.test(process.env['npm_execpath'])) {
-	console.error('*** Please use yarn to install dependencies.\n');
+let err = false;
 
+const major = parseInt(/^(\d+)\./.exec(process.versions.node)[1]);
+
+if (major < 8) {
+	console.error('\033[1;31m*** Please use node>=8.\033[0;0m');
+	err = true;
+}
+
+if (!/yarn\.js$/.test(process.env['npm_execpath'])) {
+	console.error('\033[1;31m*** Please use yarn to install dependencies.\033[0;0m');
+	err = true;
+}
+
+if (err) {
+	console.error('');
 	process.exit(1);
 }
