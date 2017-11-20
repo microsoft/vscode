d2c775cff127b33ee3c2595262e7e395720e0d04
isidor <inikolic@microsoft.com>
2017-11-20 15:49:12 +0100

    fixes #38674

--- src/vs/workbench/parts/debug/electron-browser/debugService.ts
+++ src/vs/workbench/parts/debug/electron-browser/debugService.ts
@@ -718,8 +718,11 @@ export class DebugService implements debug.IDebugService {
 						if (config && config.type) {
 							return this.createProcess(root, config, sessionId);
 						}
+						if (launch) {
+							return launch.openConfigFile(false, type).then(editor => undefined);
+						}
 
-						return <any>launch.openConfigFile(false, type); // cast to ignore weird compile error
+						return undefined;
 					})
 				).then(() => wrapUpState(), err => {
 					wrapUpState();
@@ -729,7 +732,7 @@ export class DebugService implements debug.IDebugService {
 		)));
 	}
 
-	private createProcess(root: IWorkspaceFolder, config: debug.IConfig, sessionId: string): TPromise<debug.IProcess> {
+	private createProcess(root: IWorkspaceFolder, config: debug.IConfig, sessionId: string): TPromise<void> {
 		return this.textFileService.saveAll().then(() =>
 			(this.configurationManager.selectedLaunch ? this.configurationManager.selectedLaunch.resolveConfiguration(config) : TPromise.as(config)).then(resolvedConfig => {
 				if (!resolvedConfig) {
