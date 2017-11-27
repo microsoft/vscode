47b1447a8ae8da3204f589f5c5b461ee17b2a38e
Matt Bierner <matb@microsoft.com>
2017-11-27 14:28:43 -0800

    Try using tsc path next to tsconfig for ts build tasks
    Fixes #30754

--- extensions/typescript/src/features/taskProvider.ts
+++ extensions/typescript/src/features/taskProvider.ts
@@ -123,8 +123,7 @@ class TscTaskProvider implements vscode.TaskProvider {
 					workspaceFolder: folder
 				}];
 			}
-		}
-		catch (e) {
+		} catch (e) {
 			// noop
 		}
 		return [];
@@ -134,17 +133,32 @@ class TscTaskProvider implements vscode.TaskProvider {
 		return Array.from(await this.tsconfigProvider.getConfigsForWorkspace());
 	}
 
-	private async getCommand(project: TSConfig): Promise<string> {
+	private static async getCommand(project: TSConfig): Promise<string> {
 		if (project.workspaceFolder) {
-			const platform = process.platform;
-			const bin = path.join(project.workspaceFolder.uri.fsPath, 'node_modules', '.bin');
-			if (platform === 'win32' && await exists(path.join(bin, 'tsc.cmd'))) {
-				return path.join(bin, 'tsc.cmd');
-			} else if ((platform === 'linux' || platform === 'darwin') && await exists(path.join(bin, 'tsc'))) {
-				return path.join(bin, 'tsc');
+			const localTsc = await TscTaskProvider.getLocalTscAtPath(path.dirname(project.path));
+			if (localTsc) {
+				return localTsc;
+			}
+
+			const workspaceTsc = await TscTaskProvider.getLocalTscAtPath(project.workspaceFolder.uri.fsPath);
+			if (workspaceTsc) {
+				return workspaceTsc;
 			}
 		}
+
+		// Use global tsc version
 		return 'tsc';
+	}
+
+	private static async getLocalTscAtPath(folderPath: string): Promise<string | undefined> {
+		const platform = process.platform;
+		const bin = path.join(folderPath, 'node_modules', '.bin');
+		if (platform === 'win32' && await exists(path.join(bin, 'tsc.cmd'))) {
+			return path.join(bin, 'tsc.cmd');
+		} else if ((platform === 'linux' || platform === 'darwin') && await exists(path.join(bin, 'tsc'))) {
+			return path.join(bin, 'tsc');
+		}
+		return undefined;
 	}
 
 	private getActiveTypeScriptFile(): string | null {
@@ -159,7 +173,7 @@ class TscTaskProvider implements vscode.TaskProvider {
 	}
 
 	private async getTasksForProject(project: TSConfig): Promise<vscode.Task[]> {
-		const command = await this.getCommand(project);
+		const command = await TscTaskProvider.getCommand(project);
 		const label = this.getLabelForTasks(project);
 
 		const tasks: vscode.Task[] = [];
