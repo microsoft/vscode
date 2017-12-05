6aa6f0d3324950843ce995272133d874fe9974ad
Ramya Achutha Rao <ramyar@microsoft.com>
2017-12-05 15:32:08 -0800

    Remove the onEnter rules from html in php as they will never get applied in a php block #39142

--- extensions/php/src/phpMain.ts
+++ extensions/php/src/phpMain.ts
@@ -13,8 +13,6 @@ import PHPHoverProvider from './features/hoverProvider';
 import PHPSignatureHelpProvider from './features/signatureHelpProvider';
 import PHPValidationProvider from './features/validationProvider';
 
-const EMPTY_ELEMENTS: string[] = ['area', 'base', 'br', 'col', 'embed', 'hr', 'img', 'input', 'keygen', 'link', 'menuitem', 'meta', 'param', 'source', 'track', 'wbr'];
-
 export function activate(context: vscode.ExtensionContext): any {
 
 	let validator = new PHPValidationProvider(context.workspaceState);
@@ -54,15 +52,6 @@ export function activate(context: vscode.ExtensionContext): any {
 				// e.g.  *-----*/|
 				beforeText: /^(\t|(\ \ ))*\ \*[^/]*\*\/\s*$/,
 				action: { indentAction: vscode.IndentAction.None, removeText: 1 }
-			},
-			{
-				beforeText: new RegExp(`<(?!(?:${EMPTY_ELEMENTS.join('|')}))([_:\\w][_:\\w-.\\d]*)([^/>]*(?!/)>)[^<]*$`, 'i'),
-				afterText: /^<\/([_:\w][_:\w-.\d]*)\s*>/i,
-				action: { indentAction: vscode.IndentAction.IndentOutdent }
-			},
-			{
-				beforeText: new RegExp(`<(?!(?:${EMPTY_ELEMENTS.join('|')}))(\\w[\\w\\d]*)([^/>]*(?!/)>)[^<]*$`, 'i'),
-				action: { indentAction: vscode.IndentAction.Indent }
 			}
 		]
 	});
