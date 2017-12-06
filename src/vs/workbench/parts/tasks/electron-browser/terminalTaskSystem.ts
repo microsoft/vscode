b1adb2179631ff83ab6bb1e43bc343e2835cd985
Dirk Baeumer <dirkb@microsoft.com>
2017-12-06 21:21:30 +0100

    Test task for Windows console program fails unless the task is of type shell

--- src/vs/workbench/parts/tasks/electron-browser/terminalTaskSystem.ts
+++ src/vs/workbench/parts/tasks/electron-browser/terminalTaskSystem.ts
@@ -557,8 +557,9 @@ export class TerminalTaskSystem implements ITaskSystem {
 		}
 		let dir = path.dirname(command);
 		if (dir !== '.') {
-			// We have a directory. So leave the command as is.
-			return command;
+			// We have a directory. Make the path absolute
+			// to the current working directory
+			return path.join(cwd, command);
 		}
 		// We have a simple file name. We get the path variable from the env
 		// and try to find the executable on the path.
@@ -586,7 +587,7 @@ export class TerminalTaskSystem implements ITaskSystem {
 				return withExtension;
 			}
 		}
-		return command;
+		return path.join(cwd, command);
 	}
 
 	private resolveVariables(task: CustomTask | ContributedTask, value: string[]): string[] {
