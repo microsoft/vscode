22b70efaa7abc2d120e3aef972cf8df01337b4a7
Rob Lourens <roblourens@gmail.com>
2017-12-08 16:51:25 -0800

    Bump node-debug2 to fix Microsoft/vscode-node-debug2#156

--- build/gulpfile.vscode.js
+++ build/gulpfile.vscode.js
@@ -46,7 +46,7 @@ const nodeModules = ['electron', 'original-fs']
 
 const builtInExtensions = [
 	{ name: 'ms-vscode.node-debug', version: '1.19.7' },
-	{ name: 'ms-vscode.node-debug2', version: '1.19.2' }
+	{ name: 'ms-vscode.node-debug2', version: '1.19.3' }
 ];
 
 const excludedExtensions = [
