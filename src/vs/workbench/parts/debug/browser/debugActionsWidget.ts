5ddb6c20be0f242a0f279b1bbe29100baebb28a3
Alexander <usernamehw@gmail.com>
2017-12-13 04:22:08 +0300

    Remove unnecessary transparent border

--- src/vs/workbench/parts/debug/browser/debugActionsWidget.ts
+++ src/vs/workbench/parts/debug/browser/debugActionsWidget.ts
@@ -170,7 +170,7 @@ export class DebugActionsWidget extends Themable implements IWorkbenchContributi
 				this.$el.style('border', `1px solid ${contrastBorderColor}`);
 			} else {
 				this.$el.style({
-					'border': `1px solid ${borderColor ? borderColor : 'transparent'}`,
+					'border': borderColor ? `1px solid ${borderColor}` : 'none',
 					'border-width': '1px 0'
 				});
 			}
