1918a204257669b6add9cd6ee0f931540de2a1e7
Benjamin Pasero <benjpas@microsoft.com>
2017-12-18 15:24:52 +0100

    actually import the CSS

--- src/vs/base/browser/ui/selectBox/selectBoxCustom.ts
+++ src/vs/base/browser/ui/selectBox/selectBoxCustom.ts
@@ -3,6 +3,8 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+import 'vs/css!./selectBoxCustom';
+
 import * as nls from 'vs/nls';
 import { IDisposable, dispose } from 'vs/base/common/lifecycle';
 import Event, { Emitter, chain } from 'vs/base/common/event';
