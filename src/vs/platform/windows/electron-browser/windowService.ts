8ebaeee52e3dbb438eebb36be3490f0ad9646f85
Benjamin Pasero <benjpas@microsoft.com>
2017-12-20 10:53:04 +0100

    dialogs - add logging

--- src/vs/platform/windows/electron-browser/windowService.ts
+++ src/vs/platform/windows/electron-browser/windowService.ts
@@ -11,6 +11,7 @@ import { IWindowService, IWindowsService, INativeOpenDialogOptions, IEnterWorksp
 import { IRecentlyOpened } from 'vs/platform/history/common/history';
 import { ICommandAction } from 'vs/platform/actions/common/actions';
 import { IWorkspaceFolderCreationData } from 'vs/platform/workspaces/common/workspaces';
+import { ILogService } from 'vs/platform/log/common/log';
 
 export class WindowService implements IWindowService {
 
@@ -21,7 +22,8 @@ export class WindowService implements IWindowService {
 	constructor(
 		private windowId: number,
 		private configuration: IWindowConfiguration,
-		@IWindowsService private windowsService: IWindowsService
+		@IWindowsService private windowsService: IWindowsService,
+		@ILogService private logService: ILogService
 	) {
 		const onThisWindowFocus = mapEvent(filterEvent(windowsService.onWindowFocus, id => id === windowId), _ => true);
 		const onThisWindowBlur = mapEvent(filterEvent(windowsService.onWindowBlur, id => id === windowId), _ => false);
@@ -39,11 +41,15 @@ export class WindowService implements IWindowService {
 	pickFileFolderAndOpen(options: INativeOpenDialogOptions): TPromise<void> {
 		options.windowId = this.windowId;
 
+		this.logService.info('pickFileFolderAndOpen: begin');
+
 		return this.windowsService.pickFileFolderAndOpen(options);
 	}
 
 	pickFileAndOpen(options: INativeOpenDialogOptions): TPromise<void> {
 		options.windowId = this.windowId;
+
+		this.logService.info('pickFileAndOpen: begin');
 
 		return this.windowsService.pickFileAndOpen(options);
 	}
@@ -51,11 +57,15 @@ export class WindowService implements IWindowService {
 	pickFolderAndOpen(options: INativeOpenDialogOptions): TPromise<void> {
 		options.windowId = this.windowId;
 
+		this.logService.info('pickFolderAndOpen: begin');
+
 		return this.windowsService.pickFolderAndOpen(options);
 	}
 
 	pickWorkspaceAndOpen(options: INativeOpenDialogOptions): TPromise<void> {
 		options.windowId = this.windowId;
+
+		this.logService.info('pickWorkspaceAndOpen: begin');
 
 		return this.windowsService.pickWorkspaceAndOpen(options);
 	}
@@ -121,15 +131,27 @@ export class WindowService implements IWindowService {
 	}
 
 	showMessageBox(options: Electron.MessageBoxOptions): TPromise<IMessageBoxResult> {
-		return this.windowsService.showMessageBox(this.windowId, options);
+		this.logService.info('showMessageBox begin: ', options);
+		return this.windowsService.showMessageBox(this.windowId, options).then(result => {
+			this.logService.info('showMessageBox closed, response: ', result);
+			return result;
+		});
 	}
 
 	showSaveDialog(options: Electron.SaveDialogOptions): TPromise<string> {
-		return this.windowsService.showSaveDialog(this.windowId, options);
+		this.logService.info('showSaveDialog begin: ', options);
+		return this.windowsService.showSaveDialog(this.windowId, options).then(result => {
+			this.logService.info('showSaveDialog begin: ', result);
+			return result;
+		});
 	}
 
 	showOpenDialog(options: Electron.OpenDialogOptions): TPromise<string[]> {
-		return this.windowsService.showOpenDialog(this.windowId, options);
+		this.logService.info('showOpenDialog begin: ', options);
+		return this.windowsService.showOpenDialog(this.windowId, options).then(result => {
+			this.logService.info('showOpenDialog closed: ', result);
+			return result;
+		});
 	}
 
 	updateTouchBar(items: ICommandAction[][]): TPromise<void> {
