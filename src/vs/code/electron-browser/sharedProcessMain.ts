cc3c0ba924d471fa58449c05a6a236adcdfa7d09
Benjamin Pasero <benjpas@microsoft.com>
2017-12-28 12:42:59 +0100

    :lipstick:

--- src/vs/code/electron-browser/sharedProcessMain.ts
+++ src/vs/code/electron-browser/sharedProcessMain.ts
@@ -3,6 +3,8 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+'use strict';
+
 import * as fs from 'fs';
 import * as platform from 'vs/base/common/platform';
 import product from 'vs/platform/node/product';
