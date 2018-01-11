19b8e74e033004d78881138b906d99f7b42f57ad
Rachel Macfarlane <ramacfar@microsoft.com>
2018-01-11 10:59:33 -0800

    Add a unit test

--- src/vs/issue/electron-browser/issueReporter.ts
+++ src/vs/issue/electron-browser/issueReporter.ts
@@ -24,17 +24,7 @@ import { resolveCommonProperties } from 'vs/platform/telemetry/node/commonProper
 import { WindowsChannelClient } from 'vs/platform/windows/common/windowsIpc';
 import { EnvironmentService } from 'vs/platform/environment/node/environmentService';
 import { Disposable } from 'vs/base/common/lifecycle';
-
-interface IssueReporterState {
-	issueType?: number;
-	versionInfo?: any;
-	systemInfo?: any;
-	processInfo?: any;
-	workspaceInfo?: any;
-	includeSystemInfo?: boolean;
-	includeWorkspaceInfo?: boolean;
-	includeProcessInfo?: boolean;
-}
+import { IssueReporterModel, IssueReporterData } from 'vs/issue/electron-browser/issueReporterModel';
 
 export function startup(configuration: IWindowConfiguration) {
 	const issueReporter = new IssueReporter(configuration);
@@ -45,29 +35,26 @@ export class IssueReporter extends Disposable {
 	private issueService: IIssueService;
 	private environmentService: IEnvironmentService;
 	private telemetryService: ITelemetryService;
-	private state: IssueReporterState;
+	private issueReporterModel: IssueReporterModel;
 
 	constructor(configuration: IWindowConfiguration) {
 		super();
-		this.state = {
+
+		this.issueReporterModel = new IssueReporterModel({
 			issueType: 0,
 			includeSystemInfo: true,
 			includeWorkspaceInfo: true,
 			includeProcessInfo: true
-		};
+		});
 
 		this.initServices(configuration);
 		this.setEventHandlers();
 
 		// Fetch and display status data
 		this.issueService.getStatusInfo().then((info) => {
-			const { versionInfo, systemInfo, processInfo, workspaceInfo } = info;
-			this.state.versionInfo = versionInfo;
-			this.state.systemInfo = systemInfo;
-			this.state.processInfo = processInfo;
-			this.state.workspaceInfo = workspaceInfo;
+			this.issueReporterModel.update(info);
 
-			this.updateAllBlocks(this.state);
+			this.updateAllBlocks(this.issueReporterModel.getData());
 
 			const submitButton = <HTMLButtonElement>document.getElementById('github-submit-btn');
 			submitButton.disabled = false;
@@ -109,20 +96,24 @@ export class IssueReporter extends Disposable {
 
 	setEventHandlers(): void {
 		document.getElementById('issue-type').addEventListener('change', (event: Event) => {
-			this.state.issueType = parseInt((<HTMLInputElement>event.target).value);
+			this.issueReporterModel.update({ issueType: parseInt((<HTMLInputElement>event.target).value) });
 			this.render();
 		});
 
 		document.getElementById('includeSystemInfo').addEventListener('click', (event: Event) => {
-			this.state.includeSystemInfo = !this.state.includeSystemInfo;
+			this.issueReporterModel.update({ includeSystemInfo: !this.issueReporterModel.getData().includeSystemInfo });
 		});
 
 		document.getElementById('includeProcessInfo').addEventListener('click', (event: Event) => {
-			this.state.includeProcessInfo = !this.state.includeProcessInfo;
+			this.issueReporterModel.update({ includeProcessInfo: !this.issueReporterModel.getData().includeSystemInfo });
 		});
 
 		document.getElementById('includeWorkspaceInfo').addEventListener('click', (event: Event) => {
-			this.state.includeWorkspaceInfo = !this.state.includeWorkspaceInfo;
+			this.issueReporterModel.update({ includeWorkspaceInfo: !this.issueReporterModel.getData().includeWorkspaceInfo });
+		});
+
+		document.getElementById('description').addEventListener('blur', (event: Event) => {
+			this.issueReporterModel.update({ issueDescription: (<HTMLInputElement>event.target).value });
 		});
 
 		function addIssuesToList(list, issueJSON) {
@@ -166,21 +157,12 @@ export class IssueReporter extends Disposable {
 	}
 
 	render(): void {
-		this.renderIssueType();
 		this.renderBlocks();
-	}
-
-	private renderIssueType(): void {
-		const { issueType } = this.state;
-		const issueTypes = document.getElementById('issue-type').children;
-		issueTypes[0].className = issueType === 0 ? 'active' : '';
-		issueTypes[1].className = issueType === 1 ? 'active' : '';
-		issueTypes[2].className = issueType === 2 ? 'active' : '';
 	}
 
 	private renderBlocks(): void {
 		// Depending on Issue Type, we render different blocks and text
-		const { issueType } = this.state;
+		const { issueType } = this.issueReporterModel.getData();
 		const systemBlock = document.querySelector('.block-system');
 		const processBlock = document.querySelector('.block-process');
 		const workspaceBlock = document.querySelector('.block-workspace');
@@ -220,30 +202,26 @@ export class IssueReporter extends Disposable {
 	}
 
 	private validateInput(inputId: string): boolean {
-		const issueTitleElement = (<HTMLInputElement>document.getElementById(inputId));
-		if (!issueTitleElement.value) {
+		const inputElement = (<HTMLInputElement>document.getElementById(inputId));
+		if (!inputElement.value) {
 			show(document.getElementById(`${inputId}-validation-error`));
-			issueTitleElement.classList.add('invalid-input');
+			inputElement.classList.add('invalid-input');
 			return false;
 		} else {
 			hide(document.getElementById(`${inputId}-validation-error`));
-			issueTitleElement.classList.remove('invalid-input');
+			inputElement.classList.remove('invalid-input');
 			return true;
 		}
 	}
 
 	private validateInputs(): boolean {
-		return this.validateInput('issue-title') && this.validateInput('description');
-	}
+		let isValid = true;
+		['issue-title', 'description'].forEach(elementId => {
+			isValid = this.validateInput(elementId) && isValid;
 
-	private getIssueTypeTitle(): string {
-		if (this.state.issueType === 0) {
-			return 'Bug';
-		} else if (this.state.issueType === 1) {
-			return 'Performance Issue';
-		} else {
-			return 'Feature Request';
-		}
+		});
+
+		return isValid;
 	}
 
 	private createIssue(): void {
@@ -265,98 +243,11 @@ export class IssueReporter extends Disposable {
 		document.getElementById('github-submit-btn').classList.add('active');
 		const issueTitle = (<HTMLInputElement>document.getElementById('issue-title')).value;
 		const baseUrl = `https://github.com/microsoft/vscode/issues/new?title=${issueTitle}&body=`;
-		const description = (<HTMLInputElement>document.querySelector('.block-description .block-info-text textarea')).value;
 
-		let issueBody = '';
-
-		issueBody += `
-### Issue Type
-${this.getIssueTypeTitle()}
-
-### Description
-
-${description}
-
-### VS Code Info
-
-VS Code version: ${this.state.versionInfo.vscodeVersion}
-OS version: ${this.state.versionInfo.os}
-
-${this.getInfos()}
-
-<!-- Generated by VS Code Issue Helper -->
-`;
+		const issueBody = this.issueReporterModel.serialize();
 
 		document.getElementById('github-submit-btn').classList.remove('active');
 		shell.openExternal(baseUrl + encodeURIComponent(issueBody));
-	}
-
-	private getInfos(): string {
-		let info = '';
-
-		if (this.state.includeSystemInfo) {
-			info += this.generateSystemInfoMd();
-		}
-
-		// For perf issue, add process info and workspace info too
-		if (this.state.issueType === 1) {
-
-			if (this.state.includeProcessInfo) {
-				info += this.generateProcessInfoMd();
-			}
-
-			if (this.state.includeWorkspaceInfo) {
-				info += this.generateWorkspaceInfoMd();
-			}
-		}
-
-		return info;
-	}
-
-	private generateSystemInfoMd(): string {
-		let md = `<details>
-<summary>System Info</summary>
-
-|Item|Value|
-|---|---|
-`;
-
-		Object.keys(this.state.systemInfo).forEach(k => {
-			md += `|${k}|${this.state.systemInfo[k]}|\n`;
-		});
-
-		md += '\n</details>';
-
-		return md;
-	}
-
-	private generateProcessInfoMd(): string {
-		let md = `<details>
-<summary>Process Info</summary>
-
-|pid|CPU|Memory (MB)|Name|
-|---|---|---|---|
-`;
-
-		this.state.processInfo.forEach(p => {
-			md += `|${p.pid}|${p.cpu}|${p.memory}|${p.name}|\n`;
-		});
-
-		md += '\n</details>';
-
-		return md;
-	}
-
-	private generateWorkspaceInfoMd(): string {
-		return `<details>
-<summary>Workspace Info</summary>
-
-\`\`\`
-${this.state.workspaceInfo};
-\`\`\`
-
-</details>
-`;
 	}
 
 	/**
@@ -370,7 +261,7 @@ ${this.state.workspaceInfo};
 		this.updateWorkspaceInfo(state);
 	}
 
-	private updateVersionInfo = (state: IssueReporterState) => {
+	private updateVersionInfo = (state: IssueReporterData) => {
 		const version = document.getElementById('vscode-version');
 		(<HTMLInputElement>version).value = state.versionInfo.vscodeVersion;
 
