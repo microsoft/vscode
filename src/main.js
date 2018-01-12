c250d3b51124cf5d04ec60bdf91d910e8867b088
Dirk Baeumer <dirkb@microsoft.com>
2018-01-12 17:43:31 +0100

    Make core language pack aware

--- src/main.js
+++ src/main.js
@@ -5,41 +5,39 @@
 
 'use strict';
 
-var perf = require('./vs/base/common/performance');
+let perf = require('./vs/base/common/performance');
 perf.mark('main:started');
 
 // Perf measurements
 global.perfStartTime = Date.now();
 
-var app = require('electron').app;
-var fs = require('fs');
-var path = require('path');
-var minimist = require('minimist');
-var paths = require('./paths');
+let app = require('electron').app;
+let fs = require('fs');
+let path = require('path');
+let minimist = require('minimist');
+let paths = require('./paths');
 
-var args = minimist(process.argv, {
+let args = minimist(process.argv, {
 	string: ['user-data-dir', 'locale']
 });
 
 function stripComments(content) {
-	var regexp = /("(?:[^\\\"]*(?:\\.)?)*")|('(?:[^\\\']*(?:\\.)?)*')|(\/\*(?:\r?\n|.)*?\*\/)|(\/{2,}.*?(?:(?:\r?\n)|$))/g;
-	var result = content.replace(regexp, function (match, m1, m2, m3, m4) {
+	let regexp = /("(?:[^\\\"]*(?:\\.)?)*")|('(?:[^\\\']*(?:\\.)?)*')|(\/\*(?:\r?\n|.)*?\*\/)|(\/{2,}.*?(?:(?:\r?\n)|$))/g;
+	let result = content.replace(regexp, function (match, m1, m2, m3, m4) {
 		// Only one of m1, m2, m3, m4 matches
 		if (m3) {
 			// A block comment. Replace with nothing
 			return '';
-		}
-		else if (m4) {
+		} else if (m4) {
 			// A line comment. If it ends in \r?\n then keep it.
-			var length_1 = m4.length;
+			let length_1 = m4.length;
 			if (length_1 > 2 && m4[length_1 - 1] === '\n') {
 				return m4[length_1 - 2] === '\r' ? '\r\n' : '\n';
 			}
 			else {
 				return '';
 			}
-		}
-		else {
+		} else {
 			// We match a string
 			return match;
 		}
@@ -47,38 +45,165 @@ function stripComments(content) {
 	return result;
 }
 
-function getNLSConfiguration() {
-	var locale = args['locale'];
-
-	if (!locale) {
-		var userData = app.getPath('userData');
-		var localeConfig = path.join(userData, 'User', 'locale.json');
-		if (fs.existsSync(localeConfig)) {
-			try {
-				var content = stripComments(fs.readFileSync(localeConfig, 'utf8'));
-				var value = JSON.parse(content).locale;
-				if (value && typeof value === 'string') {
-					locale = value;
-				}
-			} catch (e) {
-				// noop
-			}
+let _commit;
+function getCommit() {
+	if (_commit) {
+		return _commit;
+	}
+	if (_commit === null) {
+		return undefined;
+	}
+	try {
+		let productJson = require(path.join(__dirname, '../product.json'));
+		if (productJson.commit) {
+			_commit = productJson.commit;
+		} else {
+			_commit = null;
 		}
+	} catch (exp) {
+		_commit = null;
+	}
+}
+
+function mkdirp(dir) {
+	return mkdir(dir)
+		.then(null, (err) => {
+			if (err && err.code === 'ENOENT') {
+				let parent = path.dirname(dir);
+				if (parent !== dir) { // if not arrived at root
+					return mkdirp(parent)
+						.then(() => {
+							return mkdir(dir);
+						});
+				}
+			}
+			throw err;
+		});
+}
+
+function mkdir(dir) {
+	return new Promise((resolve, reject) => {
+		fs.mkdir(dir, (err) => {
+			if (err && err.code !== 'EEXIST') {
+				reject(err);
+			} else {
+				resolve(dir);
+			}
+		});
+	});
+}
+
+function exists(file) {
+	return new Promise((resolve) => {
+		fs.exists(file, (result) => {
+			resolve(result);
+		});
+	});
+}
+
+function readFile(file) {
+	return new Promise((resolve, reject) => {
+		fs.readFile(file, 'utf8', (err, data) => {
+			if (err) {
+				reject(err);
+				return;
+			}
+			resolve(data);
+		});
+	});
+}
+
+function writeFile(file, content) {
+	return new Promise(function(resolve, reject) {
+		fs.writeFile(file, content, 'utf8', (err) => {
+			if (err) {
+				reject(err);
+				return;
+			}
+			resolve(undefined);
+		});
+	});
+}
+
+function generateLanguagePack(locale, sourceFolder, cacheFolder) {
+	let filename =  path.join(__dirname, 'nls.metadata.json');
+	return readFile(filename).then((content) => {
+		let json = JSON.parse(content);
+		let bundles = Object.keys(json.bundles);
+		let writes = [];
+		for (let bundle of bundles) {
+			let modules = json.bundles[bundle];
+			let target = Object.create(null);
+			for (let module of modules) {
+				let strings = json.messages[module];
+				let targetStrings = [];
+				for (let s of strings) {
+					targetStrings.push('\uFF3B' + s.replace(/[aouei]/g, '$&$&') + '\uFF3D');
+				}
+				target[module] = targetStrings;
+			}
+			writes.push(writeFile(path.join(cacheFolder, bundle.replace(/\//g,'!') + '.nls.' + locale + '.json'), JSON.stringify(target)));
+		}
+		return Promise.all(writes);
+	});
+}
+
+// Language tags are case insensitve however an amd loader is case sensitive
+// To make this work on case preserving & insensitive FS we do the following:
+// the language bundles have lower case language tags and we always lower case
+// the locale we receive from the user or OS.
+
+function getUserDefinedLocale() {
+	let locale = args['locale'];
+	if (locale) {
+		return Promise.resolve(locale.toLowerCase());
 	}
 
-	var appLocale = app.getLocale();
-	locale = locale || appLocale;
-	// Language tags are case insensitve however an amd loader is case sensitive
-	// To make this work on case preserving & insensitive FS we do the following:
-	// the language bundles have lower case language tags and we always lower case
-	// the locale we receive from the user or OS.
-	locale = locale ? locale.toLowerCase() : locale;
+	let userData = app.getPath('userData');
+	let localeConfig = path.join(userData, 'User', 'locale.json');
+	return exists(localeConfig).then((result) => {
+		if (result) {
+			return readFile(localeConfig).then((content) => {
+				content = stripComments(content);
+				try {
+					let value = JSON.parse(content).locale;
+					return value && typeof value === 'string' ? value.toLowerCase() : undefined;
+				} catch (e) {
+					return undefined;
+				}
+			});
+		} else {
+			return undefined;
+		}
+	});
+}
+
+function getNLSConfiguration(locale) {
 	if (locale === 'pseudo') {
-		return { locale: locale, availableLanguages: {}, pseudo: true };
+		return Promise.resolve({ locale: locale, availableLanguages: {}, pseudo: true });
 	}
-	var initialLocale = locale;
+
 	if (process.env['VSCODE_DEV']) {
-		return { locale: locale, availableLanguages: {} };
+		return Promise.resolve({ locale: locale, availableLanguages: {} });
+	}
+
+	let userData = app.getPath('userData');
+
+	// This is temporay to test language packs until we have them installed via an extension.
+	if (locale === 'lpTest') {
+		let commit = getCommit();
+		let cacheFolder = path.join(userData, 'CachedLanguagePacks', commit, locale);
+		return exists(cacheFolder).then((result) => {
+			if (result) {
+				return { locale: locale, availableLanguages: { '*': locale }, location: cacheFolder };
+			} else {
+				return mkdirp(cacheFolder).then(() => {
+					return generateLanguagePack(locale, '', cacheFolder).then(() => {
+						return { locale: locale, availableLanguages: { '*': locale }, location: cacheFolder };
+					});
+				});
+			}
+		});
 	}
 
 	// We have a built version so we have extracted nls file. Try to find
@@ -87,31 +212,54 @@ function getNLSConfiguration() {
 	// Check if we have an English locale. If so fall to default since that is our
 	// English translation (we don't ship *.nls.en.json files)
 	if (locale && (locale == 'en' || locale.startsWith('en-'))) {
-		return { locale: locale, availableLanguages: {} };
+		return Promise.resolve({ locale: locale, availableLanguages: {} });
 	}
+
+	let initialLocale = locale;
 
 	function resolveLocale(locale) {
 		while (locale) {
-			var candidate = path.join(__dirname, 'vs', 'code', 'electron-main', 'main.nls.') + locale + '.js';
+			let candidate = path.join(__dirname, 'vs', 'code', 'electron-main', 'main.nls.') + locale + '.js';
 			if (fs.existsSync(candidate)) {
 				return { locale: initialLocale, availableLanguages: { '*': locale } };
 			} else {
-				var index = locale.lastIndexOf('-');
+				let index = locale.lastIndexOf('-');
 				if (index > 0) {
 					locale = locale.substring(0, index);
 				} else {
-					locale = null;
+					locale = undefined;
 				}
 			}
 		}
-		return null;
+		return undefined;
 	}
 
-	var resolvedLocale = resolveLocale(locale);
-	if (!resolvedLocale && appLocale && appLocale !== locale) {
-		resolvedLocale = resolveLocale(appLocale);
+	let isCoreLangaguage = true;
+	if (locale) {
+		isCoreLangaguage = ['de', 'es', 'fr', 'it', 'ja', 'ko', 'ru', 'tr', 'zh-cn', 'zh-tw'].some((language) => {
+			return locale === language || locale.startsWith(language + '-');
+		});
 	}
-	return resolvedLocale ? resolvedLocale : { locale: initialLocale, availableLanguages: {} };
+
+	if (isCoreLangaguage) {
+		return Promise.resolve(resolveLocale(locale));
+	} else {
+		let defaultResult = Promise.resolve({ locale: locale, availableLanguages: {} });
+		try {
+			let commit = getCommit();
+			if (!commit) {
+				return defaultResult;
+			}
+			let lpFolder = path.join(userData, 'CachedLanguagePacks', commit, locale);
+			return mkdirp(lpFolder).then(() => {
+				// Check fo the language pack folder and generate the language pack files.
+				return { locale: locale, availableLanguages: {}, location: lpFolder };
+			});
+		} catch (exp) {
+			// Do nothing. Use English locale
+			return defaultResult;
+		}
+	}
 }
 
 function getNodeCachedDataDir() {
@@ -126,52 +274,24 @@ function getNodeCachedDataDir() {
 	}
 
 	// find commit id
-	var productJson = require(path.join(__dirname, '../product.json'));
-	if (!productJson.commit) {
+	let commit = getCommit();
+	if (!commit) {
 		return Promise.resolve(undefined);
 	}
 
-	var dir = path.join(app.getPath('userData'), 'CachedData', productJson.commit);
+	let dir = path.join(app.getPath('userData'), 'CachedData', commit);
 
 	return mkdirp(dir).then(undefined, function () { /*ignore*/ });
 }
 
-function mkdirp(dir) {
-	return mkdir(dir)
-		.then(null, function (err) {
-			if (err && err.code === 'ENOENT') {
-				var parent = path.dirname(dir);
-				if (parent !== dir) { // if not arrived at root
-					return mkdirp(parent)
-						.then(function () {
-							return mkdir(dir);
-						});
-				}
-			}
-			throw err;
-		});
-}
-
-function mkdir(dir) {
-	return new Promise(function (resolve, reject) {
-		fs.mkdir(dir, function (err) {
-			if (err && err.code !== 'EEXIST') {
-				reject(err);
-			} else {
-				resolve(dir);
-			}
-		});
-	});
-}
-
 // Set userData path before app 'ready' event and call to process.chdir
-var userData = path.resolve(args['user-data-dir'] || paths.getDefaultUserDataPath(process.platform));
+let userData = path.resolve(args['user-data-dir'] || paths.getDefaultUserDataPath(process.platform));
 app.setPath('userData', userData);
 
 // Update cwd based on environment and platform
 try {
 	if (process.platform === 'win32') {
-		process.env['VSCODE_CWD'] = process.cwd(); // remember as environment variable
+		process.env['VSCODE_CWD'] = process.cwd(); // remember as environment letiable
 		process.chdir(path.dirname(app.getPath('exe'))); // always set application folder as cwd
 	} else if (process.env['VSCODE_CWD']) {
 		process.chdir(process.env['VSCODE_CWD']);
@@ -187,8 +307,8 @@ app.on('open-file', function (event, path) {
 	global.macOpenFiles.push(path);
 });
 
-var openUrls = [];
-var onOpenUrl = function (event, url) {
+let openUrls = [];
+let onOpenUrl = function (event, url) {
 	event.preventDefault();
 	openUrls.push(url);
 };
@@ -205,7 +325,7 @@ global.getOpenUrls = function () {
 
 // use '<UserData>/CachedData'-directory to store
 // node/v8 cached data.
-var nodeCachedDataDir = getNodeCachedDataDir().then(function (value) {
+let nodeCachedDataDir = getNodeCachedDataDir().then(function (value) {
 	if (value) {
 		// store the data directory
 		process.env['VSCODE_NODE_CACHED_DATA_DIR_' + process.pid] = value;
@@ -214,15 +334,56 @@ var nodeCachedDataDir = getNodeCachedDataDir().then(function (value) {
 		// but because we generate cached data it makes subsequent startups much faster
 		app.commandLine.appendSwitch('--js-flags', '--nolazy');
 	}
+	return value;
+});
+
+let nlsConfiguration = undefined;
+let userDefinedLocale = getUserDefinedLocale();
+userDefinedLocale.then((locale) => {
+	if (locale && !nlsConfiguration) {
+		nlsConfiguration = getNLSConfiguration(locale);
+	}
 });
 
 // Load our code once ready
 app.once('ready', function () {
 	perf.mark('main:appReady');
-	var nlsConfig = getNLSConfiguration();
-	process.env['VSCODE_NLS_CONFIG'] = JSON.stringify(nlsConfig);
-
-	nodeCachedDataDir.then(function () {
-		require('./bootstrap-amd').bootstrap('vs/code/electron-main/main');
+	Promise.all([nodeCachedDataDir, userDefinedLocale]).then((values) => {
+		let locale = values[1];
+		if (locale && !nlsConfiguration) {
+			nlsConfiguration = getNLSConfiguration(locale);
+		}
+		if (!nlsConfiguration) {
+			nlsConfiguration = Promise.resolve(undefined);
+		}
+		// We first need to test a user defined locale. If it fails we try the app locale.
+		// If that fails we fall back to English.
+		nlsConfiguration.then((nlsConfig) => {
+			let boot = (nlsConfig) => {
+				process.env['VSCODE_NLS_CONFIG'] = JSON.stringify(nlsConfig);
+				require('./bootstrap-amd').bootstrap('vs/code/electron-main/main');
+			};
+			// We recevied a valid nlsConfig from a user defined locale
+			if (nlsConfig) {
+				boot(nlsConfig);
+			} else {
+				// Try to use the app locale. Please note that the app locale is only
+				// valid after we have received the app ready event. This is why the
+				// code is here.
+				let appLocale = app.getLocale();
+				if (!appLocale) {
+					boot({ locale: 'en', availableLanguages: {} });
+				} else {
+					// See above the comment about the loader and case sensitiviness
+					appLocale.toLowerCase();
+					getNLSConfiguration(appLocale).then((nlsConfig) => {
+						if (!nlsConfig) {
+							nlsConfig = { locale: appLocale, availableLanguages: {} };
+						}
+						boot(nlsConfig);
+					});
+				}
+			}
+		});
 	}, console.error);
 });
