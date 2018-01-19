9169f7681a5a36ef430947b16d45b38d54b05db8
Dirk Baeumer <dirkb@microsoft.com>
2018-01-19 17:34:38 +0100

    Make use of new bundle langange file transformer

--- build/gulpfile.extensions.js
+++ build/gulpfile.extensions.js
@@ -86,6 +86,7 @@ const tasks = compilations.map(function (tsconfigFile) {
 				.pipe(tsFilter.restore)
 				.pipe(build ? nlsDev.createAdditionalLanguageFiles(languages, i18nPath, out) : es.through())
 				.pipe(build ? nlsDev.bundleMetaDataFiles(name, 'out') : es.through())
+				.pipe(build ? nlsDev.bundleLanguageFiles() : es.through())
 				.pipe(reporter.end(emitError));
 
 			return es.duplex(input, output);
