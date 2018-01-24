b5e2d0a6bcbae4f71099715dc813239d21975eb5
Johannes Rieken <johannes.rieken@gmail.com>
2018-01-24 09:34:33 +0100

    some more jsdoc, #10659

--- src/vs/vscode.d.ts
+++ src/vs/vscode.d.ts
@@ -2495,7 +2495,8 @@ declare module 'vscode' {
 	}
 
 	/**
-	 * A workspace edit represents textual changes for many documents.
+	 * A workspace edit represents textual and files changes for
+	 * multiple resources and documents.
 	 */
 	export class WorkspaceEdit {
 
@@ -2556,7 +2557,7 @@ declare module 'vscode' {
 		/**
 		 * Get all text edits grouped by resource.
 		 *
-		 * @return An array of `[Uri, TextEdit[]]`-tuples.
+		 * @return A shallow copy of `[Uri, TextEdit[]]`-tuples.
 		 */
 		entries(): [Uri, TextEdit[]][];
 
@@ -2585,14 +2586,18 @@ declare module 'vscode' {
 		/**
 		 * Get the resource edits for this workspace edit.
 		 *
-		 * @returns A array of uri-tuples in which a rename-edit
+		 * @returns A shallow copy of uri-tuples in which a rename-edit
 		 * is represented as `[from, to]`, a delete-operation as `[from, null]`,
 		 * and a create-operation as `[null, to]`;
 		 */
 		resourceEdits(): [Uri, Uri][];
 
 		/**
+		 * Get all edits, textual changes and file changes. The order is the order
+		 * in which edits have been added to this workspace edits. Textuals edits
+		 * are grouped and the first textual edit for a resource matters.
 		 *
+		 * @returns A shallow copy of all changes.
 		 */
 		allEntries(): ([Uri, TextEdit[]] | [Uri, Uri])[];
 	}
