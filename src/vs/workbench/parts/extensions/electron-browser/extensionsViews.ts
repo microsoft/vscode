32abd36d94eb37479e9768d68d920e8e78dc687e
Sandeep Somavarapu <sasomava@microsoft.com>
2018-01-25 18:46:45 +0100

    Fix #41781

--- src/vs/workbench/parts/extensions/electron-browser/extensionsViews.ts
+++ src/vs/workbench/parts/extensions/electron-browser/extensionsViews.ts
@@ -234,7 +234,7 @@ export class ExtensionsListView extends ViewsViewletPanel {
 				const languageTag = languageName ? ` tag:"${languageName}"` : '';
 
 				// Construct a rich query
-				return `tag:"__ext_${ext}" tag:"__ext_.${ext}" ${keywords.map(tag => `tag:"${tag}"`).join(' ')}${languageTag}`;
+				return `tag:"__ext_${ext}" tag:"__ext_.${ext}" ${keywords.map(tag => `tag:"${tag}"`).join(' ')}${languageTag} tag:"${ext}"`;
 			});
 
 			if (names.length) {
