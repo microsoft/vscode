153c398b1c0596e2faa72178b5ec147a0fae84ac
Benjamin Pasero <benjpas@microsoft.com>
2018-01-29 15:12:30 +0100

    :lipstick: settings wording

--- src/vs/platform/list/browser/listService.ts
+++ src/vs/platform/list/browser/listService.ts
@@ -467,7 +467,7 @@ configurationRegistry.registerConfiguration({
 			'description': localize({
 				key: 'openModeModifier',
 				comment: ['`singleClick` and `doubleClick` refers to a value the setting can take and should not be localized.']
-			}, "Controls how to open items in trees and lists using the mouse (if supported). Set to `singleClick` to open items with a single mouse click and `doubleClick` to only open via mouse double click.")
+			}, "Controls how to open items in trees and lists using the mouse (if supported). Set to `singleClick` to open items with a single mouse click and `doubleClick` to only open via mouse double click. For parents with children in trees, this setting will control if a single click expands the parent or a double click. Note that some trees and lists might chose to ignore this setting if it is not applicable. ")
 		}
 	}
 });
