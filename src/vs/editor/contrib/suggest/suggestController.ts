80a11d95c711424ca88c898f2e52a80604fcdb10
Johannes Rieken <johannes.rieken@gmail.com>
2018-02-01 17:05:38 +0100

    more names #42477

--- src/vs/editor/contrib/suggest/suggestController.ts
+++ src/vs/editor/contrib/suggest/suggestController.ts
@@ -95,7 +95,7 @@ export class SuggestController implements IEditorContribution {
 		@IInstantiationService private _instantiationService: IInstantiationService,
 	) {
 		this._model = new SuggestModel(this._editor);
-		this._memory = _instantiationService.createInstance(SuggestMemories, this._editor.getConfiguration().contribInfo.selectSuggestions);
+		this._memory = _instantiationService.createInstance(SuggestMemories, this._editor.getConfiguration().contribInfo.suggestSelection);
 
 		this._toDispose.push(this._model.onDidTrigger(e => {
 			if (!this._widget) {
@@ -116,9 +116,9 @@ export class SuggestController implements IEditorContribution {
 		// Manage the acceptSuggestionsOnEnter context key
 		let acceptSuggestionsOnEnter = SuggestContext.AcceptSuggestionsOnEnter.bindTo(_contextKeyService);
 		let updateFromConfig = () => {
-			const { acceptSuggestionOnEnter, selectSuggestions } = this._editor.getConfiguration().contribInfo;
+			const { acceptSuggestionOnEnter, suggestSelection } = this._editor.getConfiguration().contribInfo;
 			acceptSuggestionsOnEnter.set(acceptSuggestionOnEnter === 'on' || acceptSuggestionOnEnter === 'smart');
-			this._memory.setMode(selectSuggestions);
+			this._memory.setMode(suggestSelection);
 		};
 		this._toDispose.push(this._editor.onDidChangeConfiguration((e) => updateFromConfig()));
 		updateFromConfig();
