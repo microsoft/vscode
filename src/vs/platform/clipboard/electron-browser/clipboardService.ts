82309adf49b97370cd4c14d5903f1042871c82ca
Benjamin Pasero <benjamin.pasero@gmail.com>
2018-02-06 12:11:28 +0100

    Leverage native clipboard for file copy/paste (#43029)
    * wip
    
    * wire copy action into clipboard
    
    * add tests
    
    * fix compile
    
    * macOS: leverage NSFilenamesPboardType

--- src/vs/platform/clipboard/electron-browser/clipboardService.ts
+++ src/vs/platform/clipboard/electron-browser/clipboardService.ts
@@ -7,9 +7,16 @@
 
 import { IClipboardService } from 'vs/platform/clipboard/common/clipboardService';
 import { clipboard } from 'electron';
-import * as platform from 'vs/base/common/platform';
+import URI from 'vs/base/common/uri';
+import { isMacintosh } from 'vs/base/common/platform';
+import { parse } from 'fast-plist';
 
 export class ClipboardService implements IClipboardService {
+
+	// Clipboard format for files
+	// Windows/Linux: custom
+	// macOS: native, see https://developer.apple.com/documentation/appkit/nsfilenamespboardtype
+	private static FILE_FORMAT = isMacintosh ? 'NSFilenamesPboardType' : 'code/file-list';
 
 	_serviceBrand: any;
 
@@ -22,15 +29,82 @@ export class ClipboardService implements IClipboardService {
 	}
 
 	public readFindText(): string {
-		if (platform.isMacintosh) {
+		if (isMacintosh) {
 			return clipboard.readFindText();
 		}
+
 		return '';
 	}
 
 	public writeFindText(text: string): void {
-		if (platform.isMacintosh) {
+		if (isMacintosh) {
 			clipboard.writeFindText(text);
 		}
+	}
+
+	public writeFiles(resources: URI[]): void {
+		const files = resources.filter(f => f.scheme === 'file');
+
+		if (files.length) {
+			clipboard.writeBuffer(ClipboardService.FILE_FORMAT, this.filesToBuffer(files));
+		}
+	}
+
+	public readFiles(): URI[] {
+		return this.bufferToFiles(clipboard.readBuffer(ClipboardService.FILE_FORMAT));
+	}
+
+	public hasFiles(): boolean {
+		return clipboard.has(ClipboardService.FILE_FORMAT);
+	}
+
+	private filesToBuffer(resources: URI[]): Buffer {
+		if (isMacintosh) {
+			return this.macOSFilesToBuffer(resources);
+		}
+
+		return new Buffer(resources.map(r => r.fsPath).join('\n'));
+	}
+
+	private bufferToFiles(buffer: Buffer): URI[] {
+		if (!buffer) {
+			return [];
+		}
+
+		const bufferValue = buffer.toString();
+		if (!bufferValue) {
+			return [];
+		}
+
+		try {
+			if (isMacintosh) {
+				return this.macOSBufferToFiles(bufferValue);
+			}
+
+			return bufferValue.split('\n').map(f => URI.file(f));
+		} catch (error) {
+			return []; // do not trust clipboard data
+		}
+	}
+
+	private macOSFilesToBuffer(resources: URI[]): Buffer {
+		return new Buffer(`
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+	<array>
+	${resources.map(r => `<string>${r.fsPath}</string>`).join('\n')}
+	</array>
+</plist>
+		`);
+	}
+
+	private macOSBufferToFiles(buffer: string): URI[] {
+		const result = parse(buffer) as string[];
+		if (Array.isArray(result)) {
+			return result.map(f => URI.file(f));
+		}
+
+		return [];
 	}
 }
