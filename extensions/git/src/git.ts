255781e456411d7b5ecca07aa5ea552fef3990b2
taketori <dm1@x10d.jp>
2018-02-14 19:50:53 +0900

    enclose git option (revision name) in apostrophes

--- extensions/git/src/git.ts
+++ extensions/git/src/git.ts
@@ -1160,7 +1160,7 @@ export class Repository {
 	}
 
 	async getRefs(): Promise<Ref[]> {
-		const result = await this.run(['for-each-ref', '--format', '%(refname) %(objectname)', '--sort', '-committerdate']);
+		const result = await this.run(['for-each-ref', '--format', '\'%(refname) %(objectname)\'', '--sort', '-committerdate']);
 
 		const fn = (line: string): Ref | null => {
 			let match: RegExpExecArray | null;
@@ -1220,7 +1220,7 @@ export class Repository {
 		const commit = result.stdout.trim();
 
 		try {
-			const res2 = await this.run(['rev-parse', '--symbolic-full-name', '--abbrev-ref', name + '@{u}']);
+			const res2 = await this.run(['rev-parse', '--symbolic-full-name', '--abbrev-ref', '\'' + name + '@{u}' + '\'']);
 			const upstream = res2.stdout.trim();
 
 			const res3 = await this.run(['rev-list', '--left-right', name + '...' + upstream]);
