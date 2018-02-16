74dec50ae43bf4996424a6ca5b0eae4f3db20bdd
Sriram Desikan <sriramdesikan205@gmail.com>
2018-02-16 08:34:49 +0530

    Use set instead of map and set active only on primary instance

--- src/vs/workbench/parts/execution/electron-browser/execution.contribution.ts
+++ src/vs/workbench/parts/execution/electron-browser/execution.contribution.ts
@@ -79,24 +79,24 @@ DEFAULT_TERMINAL_LINUX_READY.then(defaultTerminalLinux => {
 const OPEN_IN_TERMINAL_COMMAND_ID = 'openInTerminal';
 CommandsRegistry.registerCommand({
 	id: OPEN_IN_TERMINAL_COMMAND_ID,
-	handler: (accessor, resource: uri) => {
+	handler: (accessor, selectedResource: uri) => {
 		const configurationService = accessor.get(IConfigurationService);
 		const editorService = accessor.get(IWorkbenchEditorService);
 		const fileService = accessor.get(IFileService);
 		const integratedTerminalService = accessor.get(IIntegratedTerminalService);
 		const terminalService = accessor.get(ITerminalService);
 		let resources:uri[] = [];
-		let directoryMap:Map<string, boolean> = new Map();
-		resources = getMultiSelectedResources(resource, accessor.get(IListService), editorService);
+		let directorySet:Set<string> = new Set();
+		resources = getMultiSelectedResources(selectedResource, accessor.get(IListService), editorService);
 		return resources.map((resource) => {
 			return fileService.resolveFile(resource).then(stat => {
 				return stat.isDirectory ? stat.resource.fsPath : paths.dirname(stat.resource.fsPath);
 			}).then(directoryToOpen => {
-				if (!directoryMap.has(directoryToOpen)) {
-					directoryMap.set(directoryToOpen, true);
+				if (!directorySet.has(directoryToOpen)) {
+					directorySet.add(directoryToOpen);
 					if (configurationService.getValue<ITerminalConfiguration>().terminal.explorerKind === 'integrated') {
 						const instance = integratedTerminalService.createInstance({ cwd: directoryToOpen }, true);
-						if (instance) {
+						if (instance && (selectedResource === resource)) {
 							integratedTerminalService.setActiveInstance(instance);
 							integratedTerminalService.showPanel(true);
 						}
