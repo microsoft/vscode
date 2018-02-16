5dc3c59343e1c573bb319cb867e3aac9c9ca1e10
isidor <inikolic@microsoft.com>
2018-02-16 17:05:57 +0100

    list: select element when increasing selection range. Do not select element when removing it from selection
    fixes #43233

--- src/vs/base/browser/ui/list/listWidget.ts
+++ src/vs/base/browser/ui/list/listWidget.ts
@@ -462,14 +462,17 @@ class MouseController<T> implements IDisposable {
 		}
 
 		let reference = this.list.getFocus()[0];
-		reference = reference === undefined ? this.list.getSelection()[0] : reference;
+		const selection = this.list.getSelection();
+		reference = reference === undefined ? selection[0] : reference;
+
+		const focus = e.index;
+		if (selection.every(s => s !== focus)) {
+			this.list.setFocus([focus]);
+		}
 
 		if (this.multipleSelectionSupport && this.isSelectionRangeChangeEvent(e)) {
 			return this.changeSelection(e, reference);
 		}
-
-		const focus = e.index;
-		this.list.setFocus([focus]);
 
 		if (this.multipleSelectionSupport && this.isSelectionChangeEvent(e)) {
 			return this.changeSelection(e, reference);
