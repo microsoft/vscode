03be224fa42f295acb83728515cd7eb9e757eccd
Joao Moreno <jomo@microsoft.com>
2018-02-26 09:11:23 +0100

    use links in messages

--- extensions/git/src/autofetch.ts
+++ extensions/git/src/autofetch.ts
@@ -5,7 +5,7 @@
 
 'use strict';
 
-import { workspace, Disposable, EventEmitter, Memento, window, MessageItem, ConfigurationTarget, commands, Uri } from 'vscode';
+import { workspace, Disposable, EventEmitter, Memento, window, MessageItem, ConfigurationTarget } from 'vscode';
 import { GitErrorCodes } from './git';
 import { Repository, Operation } from './repository';
 import { eventToPromise, filterEvent, onceEvent } from './util';
@@ -54,18 +54,12 @@ export class AutoFetcher {
 		}
 
 		const yes: MessageItem = { title: localize('yes', "Yes") };
-		const readMore: MessageItem = { title: localize('read more', "Read More") };
 		const no: MessageItem = { isCloseAffordance: true, title: localize('no', "No") };
 		const askLater: MessageItem = { title: localize('not now', "Ask Me Later") };
-		const result = await window.showInformationMessage(localize('suggest auto fetch', "Would you like Code to periodically run 'git fetch'?"), yes, readMore, no, askLater);
+		const result = await window.showInformationMessage(localize('suggest auto fetch', "Would you like Code to [periodically run 'git fetch']({0})?", 'https://go.microsoft.com/fwlink/?linkid=865294'), yes, no, askLater);
 
 		if (result === askLater) {
 			return;
-		}
-
-		if (result === readMore) {
-			commands.executeCommand('vscode.open', Uri.parse('https://go.microsoft.com/fwlink/?linkid=865294'));
-			return this.onFirstGoodRemoteOperation();
 		}
 
 		if (result === yes) {
