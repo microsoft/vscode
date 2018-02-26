a9d90af1a7d3a757ef8b6aa81a3904bbc352203e
Martin Aeschlimann <martinae@microsoft.com>
2018-02-26 20:06:24 +0100

    Add tests, update version

--- extensions/html/server/src/modes/pathCompletion.ts
+++ extensions/html/server/src/modes/pathCompletion.ts
@@ -5,7 +5,7 @@
 'use strict';
 
 import { TextDocument, CompletionList, CompletionItemKind, CompletionItem, TextEdit, Range, Position } from 'vscode-languageserver-types';
-import { WorkspaceFolder } from 'vscode-languageserver-protocol/lib/protocol.workspaceFolders.proposed';
+import { Proposed } from 'vscode-languageserver-protocol';
 import * as path from 'path';
 import * as fs from 'fs';
 import URI from 'vscode-uri';
@@ -15,7 +15,7 @@ import { contains } from '../utils/arrays';
 
 export function getPathCompletionParticipant(
 	document: TextDocument,
-	workspaceFolders: WorkspaceFolder[] | undefined,
+	workspaceFolders: Proposed.WorkspaceFolder[] | undefined,
 	result: CompletionList
 ): ICompletionParticipant {
 	return {
@@ -90,7 +90,7 @@ const isDir = (p: string) => {
 	return fs.statSync(p).isDirectory();
 };
 
-function resolveWorkspaceRoot(activeDoc: TextDocument, workspaceFolders: WorkspaceFolder[]): string | undefined {
+function resolveWorkspaceRoot(activeDoc: TextDocument, workspaceFolders: Proposed.WorkspaceFolder[]): string | undefined {
 	for (let i = 0; i < workspaceFolders.length; i++) {
 		if (startsWith(activeDoc.uri, workspaceFolders[i].uri)) {
 			return path.resolve(URI.parse(workspaceFolders[i].uri).fsPath);
