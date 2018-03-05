3c4002f850337c617e04a0756c62efcde633d6d2
Matt Bierner <matb@microsoft.com>
2018-03-05 10:55:43 -0800

    Type settings

--- extensions/markdown/preview-src/settings.ts
+++ extensions/markdown/preview-src/settings.ts
@@ -3,7 +3,16 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+export interface PreviewSettings {
+	source: string;
+	line: number;
+	lineCount: number;
+	scrollPreviewWithEditor: boolean;
+	scrollEditorWithPreview: boolean;
+	disableSecurityWarnings: boolean;
+	doubleClickToSwitchToEditor: boolean;
+}
 
-export function getSettings(): any {
+export function getSettings(): PreviewSettings {
 	return JSON.parse(document.getElementById('vscode-markdown-preview-data').getAttribute('data-settings'));
 }
