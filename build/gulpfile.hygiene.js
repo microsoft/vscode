d00c936f8b93a3561d56d789aa13315c5e6da794
Joao Moreno <jomo@microsoft.com>
2018-03-06 22:24:47 +0100

    formatting is pain

--- build/gulpfile.hygiene.js
+++ build/gulpfile.hygiene.js
@@ -196,7 +196,6 @@ function hygiene(some) {
 		this.emit('data', file);
 	});
 
-	const endl = /\r\n/.test(fs.readFileSync(__filename, 'utf8')) ? '\r\n' : '\n';
 	const formatting = es.map(function (file, cb) {
 		tsfmt.processString(file.path, file.contents.toString('utf8'), {
 			verify: false,
@@ -208,12 +207,8 @@ function hygiene(some) {
 			tsconfig: undefined,
 			tslint: undefined
 		}).then(result => {
-			let original = result.src;
-			let formatted = result.dest;
-
-			if (endl === '\n') {
-				formatted = formatted.replace(/\r\n/gm, '\n');
-			}
+			let original = result.src.replace(/\r\n/gm, '\n');
+			let formatted = result.dest.replace(/\r\n/gm, '\n');
 
 			if (original !== formatted) {
 				console.error('File not formatted:', file.relative);
