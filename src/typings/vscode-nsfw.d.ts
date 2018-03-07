f22d11d49011e058240b1e2718c350758d52698b
Daniel Imms <daimms@microsoft.com>
2018-03-07 11:08:48 -0800

    Use fork of nsfw with linux symlink fix
    Fixes #36307

--- src/typings/vscode-nsfw.d.ts
+++ src/typings/vscode-nsfw.d.ts
@@ -3,7 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-declare module 'nsfw' {
+declare module 'vscode-nsfw' {
 	interface NsfwWatcher {
 		start(): any;
 		stop(): any;
