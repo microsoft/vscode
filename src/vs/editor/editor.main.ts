e3383f216a0f533cf07c88529c3983c64ede8d84
Alex Dima <alexdima@microsoft.com>
2018-03-13 11:25:05 +0100

    Remove edcore.main

--- src/vs/editor/editor.main.ts
+++ src/vs/editor/editor.main.ts
@@ -3,4 +3,15 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-export * from 'vs/editor/edcore.main';
+'use strict';
+
+import 'vs/editor/editor.all';
+import 'vs/editor/standalone/browser/accessibilityHelp/accessibilityHelp';
+import 'vs/editor/standalone/browser/inspectTokens/inspectTokens';
+import 'vs/editor/standalone/browser/iPadShowKeyboard/iPadShowKeyboard';
+import 'vs/editor/standalone/browser/quickOpen/quickOutline';
+import 'vs/editor/standalone/browser/quickOpen/gotoLine';
+import 'vs/editor/standalone/browser/quickOpen/quickCommand';
+import 'vs/editor/standalone/browser/toggleHighContrast/toggleHighContrast';
+
+export * from 'vs/editor/editor.api';
