024f5bc3b408a13d6ed71bcdc69c3680b5bc6655
Dirk Baeumer <dirkb@microsoft.com>
2018-03-13 09:24:22 +0100

    Updated task documentation

--- src/vs/vscode.d.ts
+++ src/vs/vscode.d.ts
@@ -4600,9 +4600,12 @@ declare module 'vscode' {
 
 		/**
 		 * Resolves a task that has no [`execution`](#Task.execution) set. Tasks are
-		 * often created from information found in the `task.json`-file. Such tasks miss
+		 * often created from information found in the `tasks.json`-file. Such tasks miss
 		 * the information on how to execute them and a task provider must fill in
-		 * the missing information in the `resolveTask`-method.
+		 * the missing information in the `resolveTask`-method. This method will not be
+		 * called for tasks returned from the above `provideTasks` method since those
+		 * tasks are always fully resolved. A valid default implementation for the
+		 * `resolveTask` method is to return `undefined`.
 		 *
 		 * @param task The task to resolve.
 		 * @param token A cancellation token.
