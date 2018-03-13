85b8089cbd6bc6fbfd8869964e8f369588439093
Joao Moreno <jomo@microsoft.com>
2018-03-13 18:07:56 +0100

    revert appveyor change

--- scripts/test-integration.bat
+++ scripts/test-integration.bat
@@ -3,6 +3,9 @@ setlocal
 
 pushd %~dp0\..
 
+if not "%APPVEYOR%" == "" (
+	set ELECTRON_RUN_AS_NODE=
+)
 set VSCODEUSERDATADIR=%TMP%\vscodeuserfolder-%RANDOM%-%TIME:~6,5%
 
 :: Tests in the extension host
