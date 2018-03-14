4c8fae3cca407584927ffed3c0b3b8d340b2385a
Martin Aeschlimann <martinae@microsoft.com>
2018-03-14 23:02:04 +0100

    [html] format

--- extensions/html/server/src/test/folding.test.ts
+++ extensions/html/server/src/test/folding.test.ts
@@ -128,8 +128,8 @@ suite('Object Folding', () => {
 		];
 		assertRanges(input, [r(0, 19), r(1, 18), r(2, 3), r(5, 11), r(6, 7), r(9, 10), r(13, 14), r(16, 17)], 'no limit', void 0);
 		assertRanges(input, [r(0, 19), r(1, 18), r(2, 3), r(5, 11), r(6, 7), r(9, 10), r(13, 14), r(16, 17)], 'limit 8', 8);
-		assertRanges(input, [r(0, 19), r(1, 18), r(2, 3), r(5, 11), r(13, 14), r(16, 17)], 'limit 7',7);
-		assertRanges(input, [r(0, 19), r(1, 18), r(2, 3), r(5, 11), r(13, 14), r(16, 17)], 'limit 6',6);
+		assertRanges(input, [r(0, 19), r(1, 18), r(2, 3), r(5, 11), r(13, 14), r(16, 17)], 'limit 7', 7);
+		assertRanges(input, [r(0, 19), r(1, 18), r(2, 3), r(5, 11), r(13, 14), r(16, 17)], 'limit 6', 6);
 		assertRanges(input, [r(0, 19), r(1, 18)], 'limit 5', 5);
 		assertRanges(input, [r(0, 19), r(1, 18)], 'limit 4', 4);
 		assertRanges(input, [r(0, 19), r(1, 18)], 'limit 3', 3);
