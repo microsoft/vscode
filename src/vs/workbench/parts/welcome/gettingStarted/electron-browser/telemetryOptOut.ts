722149a26b14f311ce4d3954c3cd058629c9fbae
Christof Marti <chrmarti@microsoft.com>
2018-03-16 16:27:27 +0100

    Telemetry opt-out notice

---
+++ src/vs/workbench/parts/welcome/gettingStarted/electron-browser/telemetryOptOut.ts
@@ -0,0 +1,40 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+'use strict';
+
+import { IWorkbenchContribution } from 'vs/workbench/common/contributions';
+import { IStorageService } from 'vs/platform/storage/common/storage';
+import { ITelemetryService } from 'vs/platform/telemetry/common/telemetry';
+import { IEnvironmentService } from 'vs/platform/environment/common/environment';
+import product from 'vs/platform/node/product';
+import { IOpenerService } from 'vs/platform/opener/common/opener';
+import { INotificationService, Severity } from 'vs/platform/notification/common/notification';
+import URI from 'vs/base/common/uri';
+import { localize } from 'vs/nls';
+import { onUnexpectedError } from 'vs/base/common/errors';
+
+export class TelemetryOptOut implements IWorkbenchContribution {
+
+	private static TELEMETRY_OPT_OUT_SHOWN = 'workbench.telemetryOptOutShown';
+
+	constructor(
+		@IStorageService storageService: IStorageService,
+		@IEnvironmentService environmentService: IEnvironmentService,
+		@IOpenerService openerService: IOpenerService,
+		@INotificationService notificationService: INotificationService,
+		@ITelemetryService telemetryService: ITelemetryService
+	) {
+		if (!product.telemetryOptOutUrl || storageService.get(TelemetryOptOut.TELEMETRY_OPT_OUT_SHOWN)) {
+			return;
+		}
+		storageService.store(TelemetryOptOut.TELEMETRY_OPT_OUT_SHOWN, true);
+
+		const optOutUrl = product.telemetryOptOutUrl;
+		const privacyUrl = product.privacyStatementUrl || product.telemetryOptOutUrl;
+		notificationService.prompt(Severity.Info, localize('telemetryOptOut.notice', "Help improve VS Code by allowing Microsoft to collect usage data. Read our [privacy statement]({0}) and learn how to [opt out]({1}).", privacyUrl, optOutUrl), [localize('telemetryOptOut.readMore', "Read More")])
+			.then(() => openerService.open(URI.parse(optOutUrl)))
+			.then(null, onUnexpectedError);
+	}
+}
