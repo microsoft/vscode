b98458279484a1ee5de0434a7a422340eaec7791
Christof Marti <chrmarti@microsoft.com>
2018-03-21 11:36:54 +0100

    Max height

--- src/vs/workbench/browser/parts/quickinput/quickInput.ts
+++ src/vs/workbench/browser/parts/quickinput/quickInput.ts
@@ -34,7 +34,6 @@ export class QuickInputService extends Component implements IQuickInputService {
 
 	private static readonly ID = 'workbench.component.quickinput';
 	private static readonly MAX_WIDTH = 600;				// Max total width of quick open widget
-	// private static readonly MAX_ITEMS_HEIGHT = 20 * 22;	// TODO Max height of item list below input field
 
 	private layoutDimensions: Dimension;
 	private container: HTMLElement;
