05eeed0d82d11bee08b159e07357e1146ad32a26
al <usernamehw@gmail.com>
2018-03-22 21:22:32 +0300

    Keep a link to newly created terminal until disposed

--- src/vs/workbench/parts/terminal/electron-browser/terminalActions.ts
+++ src/vs/workbench/parts/terminal/electron-browser/terminalActions.ts
@@ -46,11 +46,11 @@ export class ToggleTerminalAction extends TogglePanelAction {
 		if (this.terminalService.terminalInstances.length === 0) {
 			// If there is not yet an instance attempt to create it here so that we can suggest a
 			// new shell on Windows (and not do so when the panel is restored on reload).
-			const toDispose = this.terminalService.onInstanceProcessIdReady(() => {
-				this.terminalService.getActiveInstance().focus();
+			const newTerminalInstance = this.terminalService.createInstance(undefined, true);
+			const toDispose = newTerminalInstance.onProcessIdReady(() => {
+				newTerminalInstance.focus();
 				toDispose.dispose();
 			});
-			this.terminalService.createInstance(undefined, true);
 		}
 		return super.run();
 	}
