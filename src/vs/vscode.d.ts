e1b634199a216579665a0473c0d46d67a70195b2
Christof Marti <chrmarti@microsoft.com>
2018-03-26 22:23:09 +0200

    onDidSelectItem

--- src/vs/vscode.d.ts
+++ src/vs/vscode.d.ts
@@ -1549,7 +1549,6 @@ declare module 'vscode' {
 
 		/**
 		 * An optional function that is invoked whenever an item is selected.
-		 * (Only honored when the picker does not allow multiple selections.)
 		 */
 		onDidSelectItem?(item: QuickPickItem | string): any;
 	}
