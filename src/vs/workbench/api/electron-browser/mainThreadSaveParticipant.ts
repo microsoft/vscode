97ff53076c1c6462c13642a619f059841d87b946
Alex Dima <alexdima@microsoft.com>
2018-03-28 18:31:48 +0200

    Fix issue #46075: Validate range before checking if it is empty

--- src/vs/workbench/api/electron-browser/mainThreadSaveParticipant.ts
+++ src/vs/workbench/api/electron-browser/mainThreadSaveParticipant.ts
@@ -166,7 +166,7 @@ export class TrimFinalNewLinesParticipant implements ISaveParticipantParticipant
 			currentLineIsEmptyOrWhitespace = strings.lastNonWhitespaceIndex(currentLine) === -1;
 		}
 
-		const deletionRange = new Range(currentLineNumber + 1, 1, lineCount, 1);
+		const deletionRange = model.validateRange(new Range(currentLineNumber + 1, 1, lineCount + 1, 1));
 		if (!deletionRange.isEmpty()) {
 			model.pushEditOperations(prevSelection, [EditOperation.delete(deletionRange)], edits => prevSelection);
 		}
