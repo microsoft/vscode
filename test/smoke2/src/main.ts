3f6dd33f884be160fd897af1ea58b2791b37b6e6
Joao Moreno <jomo@microsoft.com>
2018-04-04 16:36:52 +0200

    woo: smoke infrastructure up and running

--- test/smoke2/src/main.ts
+++ test/smoke2/src/main.ts
@@ -5,14 +5,27 @@
 
 import * as path from 'path';
 import { connect } from './driver';
+import { spawn } from './code';
 
-const rootPath = path.dirname(path.dirname(path.dirname(__dirname)));
-const outPath = path.join(rootPath, 'out');
-const handlePath = path.join(rootPath, 'foo.sock');
+// const rootPath = path.dirname(path.dirname(path.dirname(__dirname)));
+// const outPath = path.join(rootPath, 'out');
+// const handlePath = path.join(rootPath, 'foo.sock');
 
-connect(outPath, handlePath).then(({ client, driver }) => {
-	return driver.getWindows().then(w => {
+// connect(outPath, handlePath).then(({ client, driver }) => {
+// 	return driver.getWindows().then(w => {
+// 		console.log(w);
+// 		client.dispose();
+// 	});
+// }, err => console.error('oh no', err));
+
+const opts = {
+	extensionsPath: '/Users/joao/Desktop/extensions',
+	userDataDir: '/Users/joao/Desktop/user-data-dir',
+};
+
+spawn(opts).then(code => {
+	return code.driver.getWindows().then(w => {
 		console.log(w);
-		client.dispose();
+		code.dispose();
 	});
 }, err => console.error('oh no', err));
