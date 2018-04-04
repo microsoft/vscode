6848956bf49388ce0a849a7435a6a251aa8b807f
Joao Moreno <jomo@microsoft.com>
2018-04-04 16:37:28 +0200

    fix prebuild scritp

--- test/smoke2/tools/prebuild.js
+++ test/smoke2/tools/prebuild.js
@@ -14,9 +14,9 @@ contents = /\/\/\*START([\s\S]*)\/\/\*END/mi.exec(contents)[1].trim();
 contents = contents.replace(/\bTPromise\b/g, 'Promise');
 
 contents = `/*---------------------------------------------------------------------------------------------
-*  Copyright (c) Microsoft Corporation. All rights reserved.
-*  Licensed under the MIT License. See License.txt in the project root for license information.
-*--------------------------------------------------------------------------------------------*/
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
 
 ${contents}
 
