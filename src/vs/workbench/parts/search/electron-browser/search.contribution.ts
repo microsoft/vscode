eb7e893b7fa79ce05abbc900ddd8f76536c8fd28
Rob Lourens <roblourens@gmail.com>
2018-04-04 14:26:29 -0700

    Remove "Preview" from search.location setting description

--- src/vs/workbench/parts/search/electron-browser/search.contribution.ts
+++ src/vs/workbench/parts/search/electron-browser/search.contribution.ts
@@ -579,7 +579,7 @@ configurationRegistry.registerConfiguration({
 		'search.location': {
 			enum: ['sidebar', 'panel'],
 			default: 'sidebar',
-			description: nls.localize('search.location', "Preview: controls if the search will be shown as a view in the sidebar or as a panel in the panel area for more horizontal space. Next release search in panel will have improved horizontal layout and this will no longer be a preview."),
+			description: nls.localize('search.location', "Controls if the search will be shown as a view in the sidebar or as a panel in the panel area for more horizontal space. Next release search in panel will have improved horizontal layout and this will no longer be a preview."),
 		},
 	}
 });
