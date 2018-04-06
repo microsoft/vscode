d3c0f36fb9dc536894fe70c4ba1f52640a7aaace
Peng Lyu <penn.lv@gmail.com>
2018-04-05 17:02:39 -0700

    bad copyright

--- src/vs/workbench/api/node/extHostComments.ts
+++ src/vs/workbench/api/node/extHostComments.ts
@@ -1,7 +1,8 @@
 /*---------------------------------------------------------------------------------------------
-*  Copyright (c) Microsoft Corporation. All rights reserved.
-*  Licensed under the MIT License. See License.txt in the project root for license information.
-*--------------------------------------------------------------------------------------------*/
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
 'use strict';
 
 import { asWinJsPromise } from 'vs/base/common/async';
