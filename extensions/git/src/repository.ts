81b3b44413a09ed2c0103d1db34084850cd1134f
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2018-04-09 22:06:07 +1000

    ref #44776 Added DeleteRef operation

--- extensions/git/src/repository.ts
+++ extensions/git/src/repository.ts
@@ -292,6 +292,7 @@ export enum Operation {
 	GetCommitTemplate = 'GetCommitTemplate',
 	DeleteBranch = 'DeleteBranch',
 	RenameBranch = 'RenameBranch',
+	DeleteRef = 'DeleteRef',
 	Merge = 'Merge',
 	Ignore = 'Ignore',
 	Tag = 'Tag',
@@ -720,6 +721,10 @@ export class Repository implements Disposable {
 
 	async reset(treeish: string, hard?: boolean): Promise<void> {
 		await this.run(Operation.Reset, () => this.repository.reset(treeish, hard));
+	}
+
+	async deleteRef(ref: string): Promise<void> {
+		await this.run(Operation.DeleteRef, () => this.repository.deleteRef(ref));
 	}
 
 	@throttle
