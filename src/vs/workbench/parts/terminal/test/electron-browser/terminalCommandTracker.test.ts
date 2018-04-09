e310a88db609bc0e943ae24a6f2c55a1a139ac68
Daniel Imms <daimms@microsoft.com>
2018-04-09 05:14:49 -0700

    Add tests for select/scroll to terminal commands

--- src/vs/workbench/parts/terminal/test/electron-browser/terminalCommandTracker.test.ts
+++ src/vs/workbench/parts/terminal/test/electron-browser/terminalCommandTracker.test.ts
@@ -9,15 +9,6 @@ import * as assert from 'assert';
 import { Terminal } from 'vscode-xterm';
 import { TerminalCommandTracker } from 'vs/workbench/parts/terminal/node/terminalCommandTracker';
 
-// function createKeyEvent(keyCode: number): KeyboardEvent {
-// 	return <KeyboardEvent>{
-// 		preventDefault: () => { },
-// 		stopPropagation: () => { },
-// 		type: 'key',
-// 		keyCode
-// 	};
-// }
-
 interface TestTerminal extends Terminal {
 	writeBuffer: string[];
 	_innerWrite(): void;
@@ -29,17 +20,21 @@ function syncWrite(term: TestTerminal, data: string): void {
 	term._innerWrite();
 }
 
+const ROWS = 10;
+const COLS = 10;
+
 suite('Workbench - TerminalCommandTracker', () => {
 	let xterm: TestTerminal;
 	let commandTracker: TerminalCommandTracker;
 
 	setup(() => {
 		xterm = (<TestTerminal>new Terminal({
-			cols: 10,
-			rows: 10
+			cols: COLS,
+			rows: ROWS
 		}));
-		for (let i = 1; i < 10; i++) {
-			syncWrite(xterm, `${i}\r`);
+		// Fill initial viewport
+		for (let i = 0; i < ROWS - 1; i++) {
+			syncWrite(xterm, `${i}\n`);
 		}
 		commandTracker = new TerminalCommandTracker(xterm);
 	});
@@ -51,12 +46,68 @@ suite('Workbench - TerminalCommandTracker', () => {
 			xterm.emit('key', '\x0d');
 			assert.equal(xterm.markers.length, 1);
 		});
-
 		test('should not track commands when the prompt is too small', () => {
 			assert.equal(xterm.markers.length, 0);
 			syncWrite(xterm, '\x1b[2G'); // Move cursor to column 2
 			xterm.emit('key', '\x0d');
 			assert.equal(xterm.markers.length, 0);
+		});
+	});
+
+	suite('Commands', () => {
+		test('should scroll to the next and previous commands', () => {
+			syncWrite(xterm, '\x1b[3G'); // Move cursor to column 3
+			xterm.emit('key', '\x0d'); // Mark line #10
+			assert.equal(xterm.markers[0].line, 9);
+
+			for (let i = 0; i < 20; i++) {
+				syncWrite(xterm, `\r\n`);
+			}
+			assert.equal(xterm.buffer.ybase, 20);
+			assert.equal(xterm.buffer.ydisp, 20);
+
+			// Scroll to marker
+			commandTracker.scrollToPreviousCommand();
+			assert.equal(xterm.buffer.ydisp, 9);
+
+			// Scroll to top boundary
+			commandTracker.scrollToPreviousCommand();
+			assert.equal(xterm.buffer.ydisp, 0);
+
+			// Scroll to marker
+			commandTracker.scrollToNextCommand();
+			assert.equal(xterm.buffer.ydisp, 9);
+
+			// Scroll to bottom boundary
+			commandTracker.scrollToNextCommand();
+			assert.equal(xterm.buffer.ydisp, 20);
+		});
+		test('should select to the next and previous commands', () => {
+			xterm.open(document.createElement('div'));
+
+			syncWrite(xterm, '\r0');
+			syncWrite(xterm, '\n\r1');
+			syncWrite(xterm, '\x1b[3G'); // Move cursor to column 3
+			xterm.emit('key', '\x0d'); // Mark line
+			assert.equal(xterm.markers[0].line, 10);
+			syncWrite(xterm, '\n\r2');
+			syncWrite(xterm, '\x1b[3G'); // Move cursor to column 3
+			xterm.emit('key', '\x0d'); // Mark line
+			assert.equal(xterm.markers[1].line, 11);
+			syncWrite(xterm, '\n\r3');
+
+			assert.equal(xterm.buffer.ybase, 3);
+			assert.equal(xterm.buffer.ydisp, 3);
+
+			assert.equal(xterm.getSelection(), '');
+			commandTracker.selectToPreviousCommand();
+			assert.equal(xterm.getSelection(), '2');
+			commandTracker.selectToPreviousCommand();
+			assert.equal(xterm.getSelection(), '1\n2');
+			commandTracker.selectToNextCommand();
+			assert.equal(xterm.getSelection(), '2');
+			commandTracker.selectToNextCommand();
+			assert.equal(xterm.getSelection(), '\n');
 		});
 	});
 });
