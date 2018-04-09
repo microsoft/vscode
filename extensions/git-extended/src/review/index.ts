206b78d864dac98992542b617c35e4bb9a216877
Peng Lyu <penn.lv@gmail.com>
2018-04-09 16:06:16 -0700

    Fast return if the content doesn't exist.

--- extensions/git-extended/src/review/index.ts
+++ extensions/git-extended/src/review/index.ts
@@ -11,13 +11,14 @@ import { PullRequest, FileChange } from '../common/treeItems';
 import { Comment } from '../common/models/comment';
 import { toGitUri } from '../common/uri';
 import { CommentsProvider } from '../commentsProvider';
+import { GitChangeType } from '../common/models/file';
 
 const REVIEW_STATE = 'git-extended.state';
 
 export function parseCommitDiff(repository: Repository, head: string, base: string, fileChanges: FileChange[]): FileChange[] {
 	let ret = fileChanges.map(fileChange => {
-		let parentFilePath = toGitUri(vscode.Uri.parse(fileChange.fileName), null, base, {});
-		let filePath = toGitUri(vscode.Uri.parse(fileChange.fileName), null, head, {});
+		let parentFilePath = toGitUri(vscode.Uri.parse(fileChange.fileName), null, fileChange.status === GitChangeType.ADD ? '' : base, {});
+		let filePath = toGitUri(vscode.Uri.parse(fileChange.fileName), null, fileChange.status === GitChangeType.DELETE ? '' : head, {});
 		return new FileChange(fileChange.prItem, fileChange.label, fileChange.status, fileChange.context, fileChange.fileName, filePath, parentFilePath, fileChange.workspaceRoot);
 	});
 
