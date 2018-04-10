83deef54a4ac715f881b08048c579596e9ead679
Rachel Macfarlane <ramacfar@microsoft.com>
2018-04-09 17:10:09 -0700

    Update processExplorer.js

--- src/vs/code/electron-browser/processExplorer/processExplorer.js
+++ src/vs/code/electron-browser/processExplorer/processExplorer.js
@@ -6,6 +6,7 @@
 'use strict';
 
 const path = require('path');
+const fs = require('fs');
 const remote = require('electron').remote;
 
 function assign(destination, source) {
@@ -23,15 +24,6 @@ function parseURLQueryArgs() {
 		.reduce(function (r, param) { r[param[0]] = decodeURIComponent(param[1]); return r; }, {});
 }
 
-function createScript(src, onload) {
-	const script = document.createElement('script');
-	script.src = src;
-	script.addEventListener('load', onload);
-
-	const head = document.getElementsByTagName('head')[0];
-	head.insertBefore(script, head.lastChild);
-}
-
 function uriFromPath(_path) {
 	var pathName = path.resolve(_path).replace(/\\/g, '/');
 	if (pathName.length > 0 && pathName.charAt(0) !== '/') {
@@ -41,11 +33,51 @@ function uriFromPath(_path) {
 	return encodeURI('file://' + pathName);
 }
 
+function readFile(file) {
+	return new Promise(function(resolve, reject) {
+		fs.readFile(file, 'utf8', function(err, data) {
+			if (err) {
+				reject(err);
+				return;
+			}
+			resolve(data);
+		});
+	});
+}
+
 function main() {
 	const args = parseURLQueryArgs();
 	const configuration = JSON.parse(args['config'] || '{}') || {};
 
 	assign(process.env, configuration.userEnv);
+
+	//#region Add support for using node_modules.asar
+	(function () {
+		const path = require('path');
+		const Module = require('module');
+		let NODE_MODULES_PATH = path.join(configuration.appRoot, 'node_modules');
+		if (/[a-z]\:/.test(NODE_MODULES_PATH)) {
+			// Make drive letter uppercase
+			NODE_MODULES_PATH = NODE_MODULES_PATH.charAt(0).toUpperCase() + NODE_MODULES_PATH.substr(1);
+		}
+		const NODE_MODULES_ASAR_PATH = NODE_MODULES_PATH + '.asar';
+
+		const originalResolveLookupPaths = Module._resolveLookupPaths;
+		Module._resolveLookupPaths = function (request, parent, newReturn) {
+			const result = originalResolveLookupPaths(request, parent, newReturn);
+
+			const paths = newReturn ? result : result[1];
+			for (let i = 0, len = paths.length; i < len; i++) {
+				if (paths[i] === NODE_MODULES_PATH) {
+					paths.splice(i, 0, NODE_MODULES_ASAR_PATH);
+					break;
+				}
+			}
+
+			return result;
+		};
+	})();
+	//#endregion
 
 	// Get the nls configuration into the process.env as early as possible.
 	var nlsConfig = { availableLanguages: {} };
@@ -55,6 +87,24 @@ function main() {
 		try {
 			nlsConfig = JSON.parse(config);
 		} catch (e) { /*noop*/ }
+	}
+
+	if (nlsConfig._resolvedLanguagePackCoreLocation) {
+		let bundles = Object.create(null);
+		nlsConfig.loadBundle = function(bundle, language, cb) {
+			let result = bundles[bundle];
+			if (result) {
+				cb(undefined, result);
+				return;
+			}
+			let bundleFile = path.join(nlsConfig._resolvedLanguagePackCoreLocation, bundle.replace(/\//g, '!') + '.nls.json');
+			readFile(bundleFile).then(function (content) {
+				let json = JSON.parse(content);
+				bundles[bundle] = json;
+				cb(undefined, json);
+			})
+			.catch(cb);
+		};
 	}
 
 	var locale = nlsConfig.availableLanguages['*'] || 'en';
@@ -88,34 +138,37 @@ function main() {
 		}
 	});
 
-	// Load the loader and start loading the workbench
+	// Load the loader
+	const loaderFilename = configuration.appRoot + '/out/vs/loader.js';
+	const loaderSource = fs.readFileSync(loaderFilename);
+	require('vm').runInThisContext(loaderSource, { filename: loaderFilename });
+	var define = global.define;
+	global.define = undefined;
+
+	window.nodeRequire = require.__$__nodeRequire;
+
+	define('fs', ['original-fs'], function (originalFS) { return originalFS; }); // replace the patched electron fs with the original node fs for all AMD code
+
+	window.MonacoEnvironment = {};
 	const rootUrl = uriFromPath(configuration.appRoot) + '/out';
 
-	// In the bundled version the nls plugin is packaged with the loader so the NLS Plugins
-	// loads as soon as the loader loads. To be able to have pseudo translation
-	createScript(rootUrl + '/vs/loader.js', function () {
-		define('fs', ['original-fs'], function (originalFS) { return originalFS; }); // replace the patched electron fs with the original node fs for all AMD code
+	require.config({
+		baseUrl: rootUrl,
+		'vs/nls': nlsConfig,
+		nodeCachedDataDir: configuration.nodeCachedDataDir,
+		nodeModules: [/*BUILD->INSERT_NODE_MODULES*/]
+	});
 
-		window.MonacoEnvironment = {};
-
-		require.config({
-			baseUrl: rootUrl,
-			'vs/nls': nlsConfig,
-			nodeCachedDataDir: configuration.nodeCachedDataDir,
-			nodeModules: [/*BUILD->INSERT_NODE_MODULES*/]
+	if (nlsConfig.pseudo) {
+		require(['vs/nls'], function (nlsPlugin) {
+			nlsPlugin.setPseudoTranslation(nlsConfig.pseudo);
 		});
+	}
 
-		if (nlsConfig.pseudo) {
-			require(['vs/nls'], function (nlsPlugin) {
-				nlsPlugin.setPseudoTranslation(nlsConfig.pseudo);
-			});
-		}
-
-		require([
-			'vs/code/electron-browser/processExplorer/processExplorerMain'
-		], function (processExplorer) {
-			processExplorer.startup(configuration.data);
-		});
+	require([
+		'vs/code/electron-browser/processExplorer/processExplorerMain'
+	], function (processExplorer) {
+		processExplorer.startup(configuration.data);
 	});
 }
 
