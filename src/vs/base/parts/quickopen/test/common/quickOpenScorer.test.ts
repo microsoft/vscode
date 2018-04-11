ebe620a124eff0567962dd7194274144ed65970a
Josh Soref <jsoref@users.noreply.github.com>
2018-04-11 00:40:11 +0000

    spelling: angular

--- src/vs/base/parts/quickopen/test/common/quickOpenScorer.test.ts
+++ src/vs/base/parts/quickopen/test/common/quickOpenScorer.test.ts
@@ -760,7 +760,7 @@ suite('Quick Open Scorer', () => {
 	});
 
 	test('compareFilesByScore - avoid match scattering (bug #35572)', function () {
-		const resourceA = URI.file('static/app/source/angluar/-admin/-organization/-settings/layout/layout.js');
+		const resourceA = URI.file('static/app/source/angular/-admin/-organization/-settings/layout/layout.js');
 		const resourceB = URI.file('static/app/source/angular/-admin/-project/-settings/_settings/settings.js');
 
 		let query = 'partisettings';
