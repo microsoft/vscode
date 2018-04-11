3bd546ef09e8e0e0c2e739466245f6a0443d12e1
Pradeep Murugesan <peeps35@gmail.com>
2018-04-11 11:29:54 +0200

    fixed the review comments 46406

--- extensions/markdown-language-features/src/features/workspaceSymbolProvider.ts
+++ extensions/markdown-language-features/src/features/workspaceSymbolProvider.ts
@@ -3,7 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { workspace, WorkspaceSymbolProvider, SymbolInformation, TextDocument } from 'vscode';
+import { workspace, WorkspaceSymbolProvider, SymbolInformation, TextDocument, Disposable } from 'vscode';
 import { isMarkdownFile } from '../util/file';
 import MDDocumentSymbolProvider from './documentSymbolProvider';
 import { Dictionary, flatMap } from 'lodash';
@@ -11,14 +11,21 @@ import { Dictionary, flatMap } from 'lodash';
 export default class MarkdownWorkspaceSymbolProvider implements WorkspaceSymbolProvider {
 	private symbolProvider: MDDocumentSymbolProvider;
 	private symbolCache: Dictionary<SymbolInformation[]> = {};
+	private symbolCachePopulated: boolean;
+	private deRegisterOnSaveEvent: Disposable;
 
 	public constructor(symbolProvider: MDDocumentSymbolProvider) {
 		this.symbolProvider = symbolProvider;
-		this.populateSymbolCache();
-		this.registerOnSaveEvent();
+		this.symbolCachePopulated = false;
+		this.deRegisterOnSaveEvent = this.registerOnSaveEvent();
 	}
 
 	public async provideWorkspaceSymbols(query: string): Promise<SymbolInformation[]> {
+		if (!this.symbolCachePopulated) {
+			await this.populateSymbolCache();
+			this.symbolCachePopulated = true;
+		}
+
 		return flatMap(this.symbolCache)
 			.filter(symbolInformation => symbolInformation.name.toLowerCase().indexOf(query.toLowerCase()) !== -1);
 	}
@@ -34,12 +41,16 @@ export default class MarkdownWorkspaceSymbolProvider implements WorkspaceSymbolP
 		}
 	}
 
+	public dispose(): void {
+		this.deRegisterOnSaveEvent.dispose();
+	}
+
 	private async getSymbol(document: TextDocument): Promise<SymbolInformation[]> {
 		return this.symbolProvider.provideDocumentSymbols(document);
 	}
 
-	private registerOnSaveEvent(): void {
-		workspace.onDidSaveTextDocument(async document => {
+	private registerOnSaveEvent(): Disposable {
+		return workspace.onDidSaveTextDocument(async document => {
 			if (isMarkdownFile(document)) {
 				const symbols = await this.getSymbol(document);
 				this.symbolCache[document.fileName] = symbols;
