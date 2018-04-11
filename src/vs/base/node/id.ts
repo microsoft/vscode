3a6536f34f7c21b0b752072db41300dbc50faa05
Josh Soref <jsoref@users.noreply.github.com>
2018-04-11 00:34:13 +0000

    spelling: address

--- src/vs/base/node/id.ts
+++ src/vs/base/node/id.ts
@@ -24,7 +24,7 @@ export const virtualMachineHint: { value(): number } = new class {
 	private _virtualMachineOUIs: TernarySearchTree<boolean>;
 	private _value: number;
 
-	private _isVirtualMachineMacAdress(mac: string): boolean {
+	private _isVirtualMachineMacAddress(mac: string): boolean {
 		if (!this._virtualMachineOUIs) {
 			this._virtualMachineOUIs = TernarySearchTree.forStrings<boolean>();
 
@@ -60,7 +60,7 @@ export const virtualMachineHint: { value(): number } = new class {
 					for (const { mac, internal } of interfaces[name]) {
 						if (!internal) {
 							interfaceCount += 1;
-							if (this._isVirtualMachineMacAdress(mac.toUpperCase())) {
+							if (this._isVirtualMachineMacAddress(mac.toUpperCase())) {
 								vmOui += 1;
 							}
 						}
