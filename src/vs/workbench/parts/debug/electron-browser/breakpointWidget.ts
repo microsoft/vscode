b896a359c98ba0e8736f0a0a3c78fa05cda57e24
Josh Soref <jsoref@users.noreply.github.com>
2018-04-11 01:30:41 +0000

    spelling: breakpoint

--- src/vs/workbench/parts/debug/electron-browser/breakpointWidget.ts
+++ src/vs/workbench/parts/debug/electron-browser/breakpointWidget.ts
@@ -36,14 +36,14 @@ import { ServiceCollection } from 'vs/platform/instantiation/common/serviceColle
 import { IDecorationOptions } from 'vs/editor/common/editorCommon';
 
 const $ = dom.$;
-const IPrivateBreakopintWidgetService = createDecorator<IPrivateBreakopintWidgetService>('privateBreakopintWidgetService');
-export interface IPrivateBreakopintWidgetService {
+const IPrivateBreakpointWidgetService = createDecorator<IPrivateBreakpointWidgetService>('privateBreakpointWidgetService');
+export interface IPrivateBreakpointWidgetService {
 	_serviceBrand: any;
 	close(success: boolean): void;
 }
 const DECORATION_KEY = 'breakpointwidgetdecoration';
 
-export class BreakpointWidget extends ZoneWidget implements IPrivateBreakopintWidgetService {
+export class BreakpointWidget extends ZoneWidget implements IPrivateBreakpointWidgetService {
 	public _serviceBrand: any;
 
 	private selectContainer: HTMLElement;
@@ -199,7 +199,7 @@ export class BreakpointWidget extends ZoneWidget implements IPrivateBreakopintWi
 		this.toDispose.push(scopedContextKeyService);
 
 		const scopedInstatiationService = this.instantiationService.createChild(new ServiceCollection(
-			[IContextKeyService, scopedContextKeyService], [IPrivateBreakopintWidgetService, this]));
+			[IContextKeyService, scopedContextKeyService], [IPrivateBreakpointWidgetService, this]));
 
 		const options = SimpleDebugEditor.getEditorOptions();
 		this.input = scopedInstatiationService.createInstance(SimpleDebugEditor, container, options);
@@ -294,7 +294,7 @@ class AcceptBreakpointWidgetInputAction extends EditorCommand {
 	}
 
 	public runEditorCommand(accessor: ServicesAccessor, editor: ICodeEditor): void {
-		accessor.get(IPrivateBreakopintWidgetService).close(true);
+		accessor.get(IPrivateBreakpointWidgetService).close(true);
 	}
 }
 
@@ -319,7 +319,7 @@ class CloseBreakpointWidgetCommand extends EditorCommand {
 			return debugContribution.closeBreakpointWidget();
 		}
 
-		accessor.get(IPrivateBreakopintWidgetService).close(false);
+		accessor.get(IPrivateBreakpointWidgetService).close(false);
 	}
 }
 
