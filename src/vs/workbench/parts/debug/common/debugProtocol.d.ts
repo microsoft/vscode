c2cc933f01a72c5f8ce37af074dcce5364e1607e
Josh Soref <jsoref@users.noreply.github.com>
2018-04-12 03:41:00 +0000

    spelling: exception

--- src/vs/workbench/parts/debug/common/debugProtocol.d.ts
+++ src/vs/workbench/parts/debug/common/debugProtocol.d.ts
@@ -1537,7 +1537,7 @@ declare module DebugProtocol {
 	/** This enumeration defines all possible conditions when a thrown exception should result in a break.
 		never: never breaks,
 		always: always breaks,
-		unhandled: breaks when excpetion unhandled,
+		unhandled: breaks when exception unhandled,
 		userUnhandled: breaks if the exception is not handled by user code.
 	*/
 	export type ExceptionBreakMode = 'never' | 'always' | 'unhandled' | 'userUnhandled';
