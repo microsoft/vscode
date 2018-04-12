4878ae3a2f875be705d3d1d7f79e3239a26ebdd2
Josh Soref <jsoref@users.noreply.github.com>
2018-04-12 07:11:13 +0000

    spelling: primary

--- src/vs/editor/contrib/snippet/snippetSession.ts
+++ src/vs/editor/contrib/snippet/snippetSession.ts
@@ -328,7 +328,7 @@ export class SnippetSession {
 			delta += snippet.toString().length - model.getValueLengthInRange(snippetSelection);
 
 			// store snippets with the index of their originating selection.
-			// that ensures the primiary cursor stays primary despite not being
+			// that ensures the primary cursor stays primary despite not being
 			// the one with lowest start position
 			edits[idx] = EditOperation.replace(snippetSelection, snippet.toString());
 			snippets[idx] = new OneSnippet(editor, snippet, offset);
