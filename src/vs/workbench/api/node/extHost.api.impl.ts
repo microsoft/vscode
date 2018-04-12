2e9910fee075647726a859958b525d40e40594a5
Dirk Baeumer <dirkb@microsoft.com>
2018-04-12 16:15:23 +0200

    Wrap task API into proposedApiFunction

--- src/vs/workbench/api/node/extHost.api.impl.ts
+++ src/vs/workbench/api/node/extHost.api.impl.ts
@@ -520,18 +520,12 @@ export function createApiFactory(
 			registerTaskProvider: (type: string, provider: vscode.TaskProvider) => {
 				return extHostTask.registerTaskProvider(extension, provider);
 			},
-			// fetchTasks: proposedApiFunction(extension, (): Thenable<vscode.Task[]> => {
-			// 	return extHostTask.executeTaskProvider();
-			// }),
-			// executeTask: proposedApiFunction(extension, (task: vscode.Task): Thenable<vscode.TaskExecution> => {
-			// 	return extHostTask.executeTask(extension, task);
-			// }),
-			fetchTasks: (): Thenable<vscode.Task[]> => {
+			fetchTasks: proposedApiFunction(extension, (): Thenable<vscode.Task[]> => {
 				return extHostTask.executeTaskProvider();
-			},
-			executeTask: (task: vscode.Task): Thenable<vscode.TaskExecution> => {
+			}),
+			executeTask: proposedApiFunction(extension, (task: vscode.Task): Thenable<vscode.TaskExecution> => {
 				return extHostTask.executeTask(extension, task);
-			},
+			}),
 			onDidStartTask: (listeners, thisArgs?, disposables?) => {
 				return extHostTask.onDidStartTask(listeners, thisArgs, disposables);
 			},
