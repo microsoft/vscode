c9fd575dbcba18182d4b67baae3138284d765495
Josh Soref <jsoref@users.noreply.github.com>
2018-04-15 03:16:19 +0000

    spelling: telemetry

--- src/vs/code/electron-main/app.ts
+++ src/vs/code/electron-main/app.ts
@@ -378,7 +378,7 @@ export class CodeApplication {
 		services.set(ILaunchService, new SyncDescriptor(LaunchService));
 		services.set(IIssueService, new SyncDescriptor(IssueService, machineId, this.userEnv));
 
-		// Telemtry
+		// Telemetry
 		if (this.environmentService.isBuilt && !this.environmentService.isExtensionDevelopment && !this.environmentService.args['disable-telemetry'] && !!product.enableTelemetry) {
 			const channel = getDelayedChannel<ITelemetryAppenderChannel>(this.sharedProcessClient.then(c => c.getChannel('telemetryAppender')));
 			const appender = new TelemetryAppenderClient(channel);
