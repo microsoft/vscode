7d8a221fc446f6e1a9dc5db0c46b4ea3f84f11ea
Josh Soref <jsoref@users.noreply.github.com>
2018-04-15 03:24:48 +0000

    spelling: value

--- src/vs/workbench/parts/debug/common/debugProtocol.d.ts
+++ src/vs/workbench/parts/debug/common/debugProtocol.d.ts
@@ -1269,7 +1269,7 @@ declare module DebugProtocol {
 	export interface Source {
 		/** The short name of the source. Every source returned from the debug adapter has a name. When sending a source to the debug adapter this name is optional. */
 		name?: string;
-		/** The path of the source to be shown in the UI. It is only used to locate and load the content of the source if no sourceReference is specified (or its vaule is 0). */
+		/** The path of the source to be shown in the UI. It is only used to locate and load the content of the source if no sourceReference is specified (or its value is 0). */
 		path?: string;
 		/** If sourceReference > 0 the contents of the source must be retrieved through the SourceRequest (even if a path is specified). A sourceReference is only valid for a session, so it must not be used to persist a source. */
 		sourceReference?: number;
