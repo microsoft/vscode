db9cddaa6eef3309ea28990fe1931de10ddd48dd
isidor <inikolic@microsoft.com>
2018-04-19 17:27:40 +0200

    explorer: do not allow to creatae a new file / folder while in the process of creating a new file
    fixes #47606

--- src/vs/workbench/parts/files/electron-browser/fileActions.ts
+++ src/vs/workbench/parts/files/electron-browser/fileActions.ts
@@ -391,6 +391,10 @@ export class BaseNewAction extends BaseFileAction {
 		if (!folder) {
 			return TPromise.wrapError(new Error('Invalid parent folder to create.'));
 		}
+		if (!!folder.getChild(NewStatPlaceholder.NAME)) {
+			// Do not allow to creatae a new file/folder while in the process of creating a new file/folder #47606
+			return TPromise.as(new Error('Parent folder is already in the process of creating a file'));
+		}
 
 		return this.tree.reveal(folder, 0.5).then(() => {
 			return this.tree.expand(folder).then(() => {
