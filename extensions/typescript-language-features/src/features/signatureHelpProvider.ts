a32ca17c385fbe2af3bf6da44578364d8b0d833b
Matt Bierner <matb@microsoft.com>
2018-04-30 12:52:17 -0700

    Don't include param tags in js signature help provider
    Fixes #48231

--- extensions/typescript-language-features/src/features/signatureHelpProvider.ts
+++ extensions/typescript-language-features/src/features/signatureHelpProvider.ts
@@ -59,7 +59,7 @@ export default class TypeScriptSignatureHelpProvider implements SignatureHelpPro
 				}
 			});
 			signature.label += Previewer.plain(item.suffixDisplayParts);
-			signature.documentation = Previewer.markdownDocumentation(item.documentation, item.tags);
+			signature.documentation = Previewer.markdownDocumentation(item.documentation, item.tags.filter(x => x.name !== 'param'));
 			result.signatures.push(signature);
 		});
 
