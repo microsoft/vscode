4f7e1d5b5197e914f50b06f02d60ba7c9f40610b
Ramya Achutha Rao <ramyar@microsoft.com>
2018-05-23 20:17:44 -0700

    Add missing file

---
+++ src/vs/platform/node/minimalTranslations.ts
@@ -0,0 +1,19 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { localize } from 'vs/nls';
+import { locale } from 'vs/base/common/platform';
+
+// The strings localized in this file will get pulled into the manifest of the language packs.
+// So that they are available for VS Code to use without downloading the entire language pack.
+
+export const minimumTranslatedStrings = {
+	searchForLanguagePacks: localize('showLanguagePackExtensions', "The Marketplace has extensions that can help localizing VS Code to '{0}' language", locale),
+	searchMarketplace: localize('searchMarketplace', "Search Marketplace"),
+	installAndRestartMessage: localize('installAndRestartMessage', "Install language pack to localize VS Code in '{0}' language. Restart VS Code after installing for the language to take effect."),
+	installAndRestart: localize('installAndRestart', "Install and Restart"),
+	install: localize('install', 'Install')
+};
+
