429a2da413b7674ea76b1472bcb838c5760a7611
Peng Lyu <penn.lv@gmail.com>
2018-05-25 09:52:05 -0700

    remove unused code.

--- extensions/git-extended/src/common/file.ts
+++ extensions/git-extended/src/common/file.ts
@@ -3,14 +3,9 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import * as fs from 'fs';
-import * as path from 'path';
-import * as tmp from 'tmp';
-import * as vscode from 'vscode';
 import { GitProcess } from 'dugite';
-import { Repository } from '../models/repository';
-import { SlimFileChange, GitChangeType, fromStatus } from '../models/file';
-
+import * as fs from 'fs';
+import * as tmp from 'tmp';
 
 export async function writeTmpFile(content: string, ext: string): Promise<string> {
 	return new Promise<string>((resolve, reject) => {
@@ -24,32 +19,6 @@ export async function writeTmpFile(content: string, ext: string): Promise<string
 				fs.appendFileSync(tmpFilePath, content);
 				resolve(tmpFilePath);
 			} catch (ex) {
-				reject(ex);
-			}
-		});
-	});
-}
-
-export async function getFile(commitSha1: string, localFilePath: string): Promise<string> {
-	const rootDir = vscode.workspace.rootPath;
-	return new Promise<string>((resolve, reject) => {
-		if (commitSha1 === undefined) {
-			resolve('fileUnavailable');
-			return;
-		}
-		let ext = path.extname(localFilePath);
-		tmp.file({ postfix: ext }, async (err: any, tmpFilePath: string) => {
-			if (err) {
-				reject(err);
-				return;
-			}
-			try {
-				let data = await getFileContent(rootDir, commitSha1, localFilePath);
-				fs.appendFileSync(tmpFilePath, data);
-				resolve(tmpFilePath);
-			}
-			catch (ex) {
-				console.log(ex);
 				reject(ex);
 			}
 		});
@@ -70,30 +39,4 @@ export async function getFileContent(rootDir: string, commitSha: string, sourceF
 	} else {
 		throw error;
 	}
-}
-
-export async function getChangedFiles(repository: Repository, sha: string): Promise<ReadonlyArray<SlimFileChange>> {
-	const args = ['log', sha, '--name-status', '--format=format:', '-z', '-1'];
-	const result = await GitProcess.exec(args, repository.path);
-
-	const out = result.stdout;
-	const lines = out.split('\0');
-	lines.splice(-1, 1);
-
-	const files: SlimFileChange[] = [];
-	for (let i = 0; i < lines.length; i++) {
-		const statusText = lines[i];
-		const status = fromStatus(statusText);
-		let originalPath: string | undefined = undefined;
-
-		if (status === GitChangeType.RENAME || status === GitChangeType.COPY) {
-			originalPath = lines[++i];
-		}
-
-		const path = lines[++i];
-
-		files.push(new SlimFileChange(path, originalPath, status, null));
-	}
-
-	return files;
 }
