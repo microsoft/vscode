3e06a9de547b52c634eeb917b52375dd42999ed6
Christof Marti <chrmarti@microsoft.com>
2018-05-28 11:22:56 +0200

    Fix getmac test (#48804)

--- src/vs/base/test/node/id.test.ts
+++ src/vs/base/test/node/id.test.ts
@@ -5,13 +5,22 @@
 'use strict';
 
 import * as assert from 'assert';
+import * as getmac from 'getmac';
 import { getMachineId } from 'vs/base/node/id';
 
 suite('ID', () => {
 
 	test('getMachineId', function () {
 		return getMachineId().then(id => {
-			assert.ok(/^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$/.test(id), `Expected a MAC address: ${id}`);
+			assert.ok(id);
+		});
+	});
+
+	test('getMac', function () {
+		return new Promise<string>((resolve, reject) => {
+			getmac.getMac((err, macAddress) => err ? reject(err) : resolve(macAddress));
+		}).then(macAddress => {
+			assert.ok(/^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$/.test(macAddress), `Expected a MAC address, got: ${macAddress}`);
 		});
 	});
 });
