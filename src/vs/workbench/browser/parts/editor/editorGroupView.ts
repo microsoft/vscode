18b7169e1d706f77f0e61ca64b837c80317f5f7a
Benjamin Pasero <benjpas@microsoft.com>
2018-05-28 12:39:35 +0200

    grid - allow to close empty group when closing all editors of a group

--- src/vs/workbench/browser/parts/editor/editorGroupView.ts
+++ src/vs/workbench/browser/parts/editor/editorGroupView.ts
@@ -1126,6 +1126,14 @@ export class EditorGroupView extends Themable implements IEditorGroupView {
 
 	closeAllEditors(): TPromise<void> {
 		if (this.isEmpty()) {
+
+			// If the group is empty and the request is to close all editors, we still close
+			// the editor group is the related setting to close empty groups is enabled for
+			// a convinient way of removing empty editor groups for the user.
+			if (this.accessor.partOptions.closeEmptyGroups) {
+				this.accessor.removeGroup(this);
+			}
+
 			return TPromise.as(void 0);
 		}
 
