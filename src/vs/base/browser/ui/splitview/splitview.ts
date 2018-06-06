395957db5fe758553a4a145f56e562851cc99183
Joao Moreno <jomo@microsoft.com>
2018-06-06 10:21:00 +0200

    support grid styles using CSS variables
    fixes #50946

--- src/vs/base/browser/ui/splitview/splitview.ts
+++ src/vs/base/browser/ui/splitview/splitview.ts
@@ -13,10 +13,20 @@ import * as dom from 'vs/base/browser/dom';
 import { clamp } from 'vs/base/common/numbers';
 import { range, firstIndex } from 'vs/base/common/arrays';
 import { Sash, Orientation, ISashEvent as IBaseSashEvent } from 'vs/base/browser/ui/sash/sash';
+import { Color } from 'vs/base/common/color';
 export { Orientation } from 'vs/base/browser/ui/sash/sash';
+
+export interface ISplitViewStyles {
+	separatorBorder: Color;
+}
+
+const defaultStyles: ISplitViewStyles = {
+	separatorBorder: Color.transparent
+};
 
 export interface ISplitViewOptions {
 	orientation?: Orientation; // default Orientation.VERTICAL
+	styles?: ISplitViewStyles;
 }
 
 export interface IView {
@@ -137,6 +147,18 @@ export class SplitView implements IDisposable {
 		this.viewContainer = document.createElement('div');
 		dom.addClass(this.viewContainer, 'split-view-container');
 		this.el.appendChild(this.viewContainer);
+
+		this.style(options.styles || defaultStyles);
+	}
+
+	style(styles: ISplitViewStyles): void {
+		if (styles.separatorBorder.isTransparent()) {
+			dom.removeClass(this.el, 'separator-border');
+			this.el.style.removeProperty('--separator-border');
+		} else {
+			dom.addClass(this.el, 'separator-border');
+			this.el.style.setProperty('--separator-border', styles.separatorBorder.toString());
+		}
 	}
 
 	addView(view: IView, size: number | Sizing, index = this.viewItems.length): void {
