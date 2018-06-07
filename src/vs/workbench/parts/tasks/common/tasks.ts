7c62da21f52deabd3a9a155820b80db22a3af1db
Dirk Baeumer <dirkb@microsoft.com>
2018-06-07 16:39:01 +0200

    Fixes #35910: Cannot Create keybinding to restart a specific Task

--- src/vs/workbench/parts/tasks/common/tasks.ts
+++ src/vs/workbench/parts/tasks/common/tasks.ts
@@ -621,7 +621,7 @@ export namespace Task {
 		if (Types.isString(key)) {
 			return key === task._label || key === task.identifier || (compareId && key === task._id);
 		}
-		let identifier = Task.getTaskDefinition(task);
+		let identifier = Task.getTaskDefinition(task, true);
 		return identifier !== void 0 && identifier._key === key._key;
 	}
 
