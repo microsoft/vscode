995ef296433b9165cc63ef190f76531ed6bc2fb7
Rob Lourens <roblourens@gmail.com>
2018-06-08 18:19:48 -0700

    Add missing methods to strings.ts

--- extensions/search-rg/src/strings.ts
+++ extensions/search-rg/src/strings.ts
@@ -1,8 +1,11 @@
 /*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
+*  Copyright (c) Microsoft Corporation. All rights reserved.
+*  Licensed under the MIT License. See License.txt in the project root for license information.
+*--------------------------------------------------------------------------------------------*/
+
 'use strict';
+
+import { CharCode } from './charCode';
 
 export function stripWildcards(pattern: string): string {
 	return pattern.replace(/\*/g, '');
@@ -29,6 +32,15 @@ export function startsWith(haystack: string, needle: string): boolean {
 	return true;
 }
 
+export function startsWithIgnoreCase(str: string, candidate: string): boolean {
+	const candidateLength = candidate.length;
+	if (candidate.length > str.length) {
+		return false;
+	}
+
+	return doEqualsIgnoreCase(str, candidate, candidateLength);
+}
+
 /**
  * Determines if haystack ends with needle.
  */
@@ -41,6 +53,61 @@ export function endsWith(haystack: string, needle: string): boolean {
 	} else {
 		return false;
 	}
+}
+
+function isLowerAsciiLetter(code: number): boolean {
+	return code >= CharCode.a && code <= CharCode.z;
+}
+
+function isUpperAsciiLetter(code: number): boolean {
+	return code >= CharCode.A && code <= CharCode.Z;
+}
+
+function isAsciiLetter(code: number): boolean {
+	return isLowerAsciiLetter(code) || isUpperAsciiLetter(code);
+}
+
+export function equalsIgnoreCase(a: string, b: string): boolean {
+	const len1 = a ? a.length : 0;
+	const len2 = b ? b.length : 0;
+
+	if (len1 !== len2) {
+		return false;
+	}
+
+	return doEqualsIgnoreCase(a, b);
+}
+
+function doEqualsIgnoreCase(a: string, b: string, stopAt = a.length): boolean {
+	if (typeof a !== 'string' || typeof b !== 'string') {
+		return false;
+	}
+
+	for (let i = 0; i < stopAt; i++) {
+		const codeA = a.charCodeAt(i);
+		const codeB = b.charCodeAt(i);
+
+		if (codeA === codeB) {
+			continue;
+		}
+
+		// a-z A-Z
+		if (isAsciiLetter(codeA) && isAsciiLetter(codeB)) {
+			let diff = Math.abs(codeA - codeB);
+			if (diff !== 0 && diff !== 32) {
+				return false;
+			}
+		}
+
+		// Any other charcode
+		else {
+			if (String.fromCharCode(codeA).toLowerCase() !== String.fromCharCode(codeB).toLowerCase()) {
+				return false;
+			}
+		}
+	}
+
+	return true;
 }
 
 /**
