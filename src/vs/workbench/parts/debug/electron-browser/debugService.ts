e58f7975e33d042a91e39ab23c6ff6fb41d6e100
isidor <inikolic@microsoft.com>
2018-06-11 10:15:30 +0200

    fixes #51245

--- src/vs/workbench/parts/debug/electron-browser/debugService.ts
+++ src/vs/workbench/parts/debug/electron-browser/debugService.ts
@@ -670,7 +670,7 @@ export class DebugService implements debug.IDebugService {
 	}
 
 	public logToRepl(value: string | debug.IExpression, sev = severity.Info, source?: debug.IReplElementSource): void {
-		const clearAnsiSequence = '\\u001b[2J';
+		const clearAnsiSequence = '\u001b[2J';
 		if (typeof value === 'string' && value.indexOf(clearAnsiSequence) >= 0) {
 			// [2J is the ansi escape sequence for clearing the display http://ascii-table.com/ansi-escape-sequences.php
 			this.model.removeReplExpressions();
