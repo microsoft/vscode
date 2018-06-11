f443d73811011b10862dcb9c8dc19f365cb5f5a0
rebornix <penn.lv@gmail.com>
2018-06-11 09:48:32 -0700

    remove empty line.

--- src/vs/workbench/browser/parts/sidebar/sidebarPart.ts
+++ src/vs/workbench/browser/parts/sidebar/sidebarPart.ts
@@ -91,7 +91,6 @@ export class SidebarPart extends CompositePart<Viewlet> {
 		const container = $(this.getContainer());
 
 		container.style('background-color', this.getColor(SIDE_BAR_BACKGROUND));
-
 		container.style('color', this.getColor(SIDE_BAR_FOREGROUND));
 
 		const borderColor = this.getColor(SIDE_BAR_BORDER) || this.getColor(contrastBorder);
