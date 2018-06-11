be4b47e4a2168d07c4c07cce1c2e95eb0e54f312
Johannes Rieken <johannes.rieken@gmail.com>
2018-06-11 15:59:25 +0200

    move filter/find action into overflow menu #49922

--- src/vs/workbench/parts/outline/electron-browser/outlinePanel.ts
+++ src/vs/workbench/parts/outline/electron-browser/outlinePanel.ts
@@ -43,7 +43,7 @@ import { KeybindingsRegistry } from 'vs/platform/keybinding/common/keybindingsRe
 import { WorkbenchTree } from 'vs/platform/list/browser/listService';
 import { IMarkerService, MarkerSeverity } from 'vs/platform/markers/common/markers';
 import { IStorageService, StorageScope } from 'vs/platform/storage/common/storage';
-import { attachInputBoxStyler, attachProgressBarStyler, attachCheckboxStyler } from 'vs/platform/theme/common/styler';
+import { attachInputBoxStyler, attachProgressBarStyler } from 'vs/platform/theme/common/styler';
 import { IThemeService } from 'vs/platform/theme/common/themeService';
 import { ViewletPanel } from 'vs/workbench/browser/parts/views/panelViewlet';
 import { IViewletViewOptions } from 'vs/workbench/browser/parts/views/viewsViewlet';
@@ -54,7 +54,6 @@ import { OutlineConfigKeys, OutlineViewFiltered, OutlineViewFocused, OutlineView
 import { OutlineElement, OutlineModel, TreeElement } from './outlineModel';
 import { OutlineController, OutlineDataSource, OutlineItemComparator, OutlineItemCompareType, OutlineItemFilter, OutlineRenderer, OutlineTreeState } from './outlineTree';
 import { IViewsService } from 'vs/workbench/common/views';
-import { Checkbox } from 'vs/base/browser/ui/checkbox/checkbox';
 
 class RequestState {
 
@@ -294,7 +293,7 @@ export class OutlinePanel extends ViewletPanel {
 		);
 
 		this._input = new InputBox(this._inputContainer, null, {
-			placeholder: this._outlineViewState.filterOnType ? localize('filter', "Filter") : localize('find', "Find")
+			placeholder: this._outlineViewState.filterOnType ? localize('filter.placeholder', "Filter") : localize('find.placeholder', "Find")
 		});
 		this._input.disable();
 
@@ -314,17 +313,6 @@ export class OutlinePanel extends ViewletPanel {
 				this._tree.domFocus();
 			}
 		}));
-
-		const checkBox = new Checkbox({
-			actionClassName: 'action-filter',
-			title: localize('checkboxFilter', "Filter element on type"),
-			isChecked: this._outlineViewState.filterOnType,
-			onChange: () => {
-				this._outlineViewState.filterOnType = !this._outlineViewState.filterOnType;
-			}
-		});
-		this._inputContainer.appendChild(checkBox.domNode);
-		this.disposables.push(attachCheckboxStyler(checkBox, this._themeService));
 
 		const $this = this;
 		const controller = new class extends OutlineController {
@@ -400,7 +388,8 @@ export class OutlinePanel extends ViewletPanel {
 			new SimpleToggleAction(localize('sortByKind', "Sort By: Type"), this._outlineViewState.sortBy === OutlineItemCompareType.ByKind, _ => this._outlineViewState.sortBy = OutlineItemCompareType.ByKind),
 		]);
 		let result = [
-			new SimpleToggleAction(localize('live', "Follow Cursor"), this._outlineViewState.followCursor, action => this._outlineViewState.followCursor = action.checked),
+			new SimpleToggleAction(localize('followCur', "Follow Cursor"), this._outlineViewState.followCursor, action => this._outlineViewState.followCursor = action.checked),
+			new SimpleToggleAction(localize('filterOnType', "Filter on Type"), this._outlineViewState.filterOnType, action => this._outlineViewState.filterOnType = action.checked),
 			new Separator(),
 			...group.actions,
 		];
