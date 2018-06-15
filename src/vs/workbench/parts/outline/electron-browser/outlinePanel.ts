a05176fd39483c3c0947d4049d40ff6ea6068fcc
Johannes Rieken <johannes.rieken@gmail.com>
2018-06-15 11:37:36 +0200

    fix #51647

--- src/vs/workbench/parts/outline/electron-browser/outlinePanel.ts
+++ src/vs/workbench/parts/outline/electron-browser/outlinePanel.ts
@@ -434,9 +434,11 @@ export class OutlinePanel extends ViewletPanel {
 		dispose(this._editorDisposables);
 
 		this._editorDisposables = new Array();
-		this._input.disable();
-		this._input.value = '';
 		this._progressBar.infinite().show(150);
+		this._input.disable();
+		if (!event) {
+			this._input.value = '';
+		}
 
 		if (!editor || !DocumentSymbolProviderRegistry.has(editor.getModel())) {
 			return this._showMessage(localize('no-editor', "There are no editors open that can provide outline information."));
@@ -510,7 +512,7 @@ export class OutlinePanel extends ViewletPanel {
 		// on first type -> capture tree state
 		// on erase -> restore captured tree state
 		let beforePatternState: OutlineTreeState;
-		this._editorDisposables.push(this._input.onDidChange(async pattern => {
+		let onInputValueChanged = async pattern => {
 
 			this._contextKeyFiltered.set(pattern.length > 0);
 
@@ -530,7 +532,9 @@ export class OutlinePanel extends ViewletPanel {
 				await OutlineTreeState.restore(this._tree, beforePatternState);
 				beforePatternState = undefined;
 			}
-		}));
+		};
+		onInputValueChanged(this._input.value);
+		this._editorDisposables.push(this._input.onDidChange(onInputValueChanged));
 
 		this._editorDisposables.push({
 			dispose: () => this._contextKeyFiltered.reset()
