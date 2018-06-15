a86d0e598d3e4b216c91a7278322a81df304982e
isidor <inikolic@microsoft.com>
2018-06-15 17:49:18 +0200

    add centeredViewLayout

---
+++ src/vs/base/browser/ui/centered/centeredViewLayout.ts
@@ -0,0 +1,45 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { SplitView, Sizing, IView, Orientation } from 'vs/base/browser/ui/splitview/splitview';
+import { $ } from 'vs/base/browser/dom';
+import { Event } from 'vs/base/common/event';
+
+export class CenteredViewLayout {
+
+	private splitView: SplitView;
+	readonly element: HTMLElement;
+
+	constructor(container: HTMLElement, view: IView) {
+		this.element = $('.centered-view-layout');
+		container.appendChild(this.element);
+
+		this.splitView = new SplitView(this.element, {
+			inverseAltBehavior: true,
+			orientation: Orientation.HORIZONTAL
+		});
+		const addEmptyView = () => {
+			this.splitView.addView({
+				element: $('.'),
+				layout: () => undefined,
+				minimumSize: 20,
+				maximumSize: Number.POSITIVE_INFINITY,
+				onDidChange: Event.None
+			}, Sizing.Distribute);
+		};
+
+		addEmptyView();
+		this.splitView.addView(view, Sizing.Distribute);
+		addEmptyView();
+	}
+
+	layout(size: number): void {
+		this.splitView.layout(size);
+	}
+
+	dispose(): void {
+		this.splitView.dispose();
+	}
+}
