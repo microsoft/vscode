d0d39157ae19b7f8739189ccd385acea2238fc06
Sandeep Somavarapu <sasomava@microsoft.com>
2018-06-19 11:36:27 +0200

    Add doc

--- src/vs/vscode.d.ts
+++ src/vs/vscode.d.ts
@@ -6091,6 +6091,9 @@ declare module 'vscode' {
 
 	}
 
+	/**
+	 * The event that is fired when there is a change in [tree view's visibility](#TreeView.visible)
+	 */
 	export interface TreeViewVisibilityChangeEvent {
 
 		/**
