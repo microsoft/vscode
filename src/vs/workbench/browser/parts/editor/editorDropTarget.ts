aa3809481ba391dbe8d0dd41607bcbdab72588b4
Benjamin Pasero <benjpas@microsoft.com>
2018-06-19 17:48:27 +0200

    grid - fix bad overlay height value when dragging

--- src/vs/workbench/browser/parts/editor/editorDropTarget.ts
+++ src/vs/workbench/browser/parts/editor/editorDropTarget.ts
@@ -356,6 +356,7 @@ class DropOverlay extends Themable {
 		this.overlay.style.top = options.top;
 		this.overlay.style.left = options.left;
 		this.overlay.style.width = options.width;
+		this.overlay.style.height = options.height;
 	}
 
 	private getOverlayOffsetHeight(): number {
