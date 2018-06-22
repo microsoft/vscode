e0425aa4735a924e167313f2fafc2913d9104139
Charles Milette <me@charlesmilette.net>
2018-06-22 14:29:32 -0400

    Add enumDescriptions for window.compositionAttribute

--- src/vs/workbench/electron-browser/main.contribution.ts
+++ src/vs/workbench/electron-browser/main.contribution.ts
@@ -471,9 +471,15 @@ configurationRegistry.registerConfiguration({
 		'window.compositionAttribute': {
 			'type': 'string',
 			'enum': ['none', 'transparent', 'blur', 'acrylic'],
+			'enumDescriptions': [
+				nls.localize('window.compositionAttribute.none', "No special effect or transparency."),
+				nls.localize('window.compositionAttribute.transparent', "Transparent. Requires at least Windows 10."),
+				nls.localize('window.compositionAttribute.blur', "Transparent and blurred."),
+				nls.localize('window.compositionAttribute.acrylic', "Transparent, blurred and grain effect. Requires the Windows 10 April 2018 update and can present various graphical glitches when there is not at least one window other than VS Code maximised.")
+			],
 			'default': 'none',
 			'scope': ConfigurationScope.APPLICATION,
-			'description': nls.localize('window.compositionAttribute', "Changes window composition attribute. Requires custom titlebar to be active on Windows 10, but not required for earlier OSes. Transparent and Acrylic are only available on Windows 10. Furthermore, Acrylic requires the April 2018 update. Needs color customizations or a transparent theme to affect appearance."),
+			'description': nls.localize('window.compositionAttribute', "Changes window composition attribute. Requires custom titlebar to be active on Windows 10, but not required for earlier versions. Needs color customizations or a transparent theme to affect appearance."),
 			'included': isWindows
 		},
 		'window.vibrancy': {
