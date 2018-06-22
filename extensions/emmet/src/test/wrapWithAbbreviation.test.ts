c3ad9789820afc6f9cab985bfa6e48175e414499
Ramya Achutha Rao <ramyar@microsoft.com>
2018-06-22 14:56:35 -0700

    Escape $ when wrapping Fixes #52640

--- extensions/emmet/src/test/wrapWithAbbreviation.test.ts
+++ extensions/emmet/src/test/wrapWithAbbreviation.test.ts
@@ -225,14 +225,14 @@ suite('Tests for Wrap with Abbreviations', () => {
 	test('Wrap individual lines with abbreviation', () => {
 		const contents = `
 	<ul class="nav main">
-		<li class="item1">img</li>
+		<li class="item1">This $10 is not a tabstop</li>
 		<li class="item2">hi.there</li>
 	</ul>
 `;
 		const wrapIndividualLinesExpected = `
 	<ul class="nav main">
 		<ul>
-			<li class="hello1"><li class="item1">img</li></li>
+			<li class="hello1"><li class="item1">This $10 is not a tabstop</li></li>
 			<li class="hello2"><li class="item2">hi.there</li></li>
 		</ul>
 	</ul>
