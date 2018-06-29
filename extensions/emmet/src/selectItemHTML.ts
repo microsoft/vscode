4f870afa5871c688b6dff58a955218100f7c4054
Jackson Kearl <jkearl11@gmail.com>
2018-06-29 13:40:38 -0700

    Enforce node boundaries in places where it matters (#53192)
    * Enfoce node boundries in places where it matters
    
    * Flip defualt emmet includeBoundries to true
    
    Explicitly set getNode's include boundries where needed
    
    * Remove defualt papameter
    
    * Add update image boundry tests
    
    * Add tests for bondries on some of the tag actions
    
    * Rest of tag tests

--- extensions/emmet/src/selectItemHTML.ts
+++ extensions/emmet/src/selectItemHTML.ts
@@ -8,7 +8,7 @@ import { getDeepestNode, findNextWord, findPrevWord, getHtmlNode } from './util'
 import { HtmlNode } from 'EmmetNode';
 
 export function nextItemHTML(selectionStart: vscode.Position, selectionEnd: vscode.Position, editor: vscode.TextEditor, rootNode: HtmlNode): vscode.Selection | undefined {
-	let currentNode = getHtmlNode(editor.document, rootNode, selectionEnd);
+	let currentNode = getHtmlNode(editor.document, rootNode, selectionEnd, false);
 	let nextNode: HtmlNode | undefined = undefined;
 
 	if (!currentNode) {
@@ -54,7 +54,7 @@ export function nextItemHTML(selectionStart: vscode.Position, selectionEnd: vsco
 }
 
 export function prevItemHTML(selectionStart: vscode.Position, selectionEnd: vscode.Position, editor: vscode.TextEditor, rootNode: HtmlNode): vscode.Selection | undefined {
-	let currentNode = getHtmlNode(editor.document, rootNode, selectionStart);
+	let currentNode = getHtmlNode(editor.document, rootNode, selectionStart, false);
 	let prevNode: HtmlNode | undefined = undefined;
 
 	if (!currentNode) {
