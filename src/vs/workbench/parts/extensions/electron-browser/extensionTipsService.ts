9bc243a31658f4d34e5e18680e527aedf1256f7f
Jackson Kearl <jkearl@mit.edu>
2018-07-03 13:51:01 -0700

    Seed random shuffle of extension recommendations

--- src/vs/workbench/parts/extensions/electron-browser/extensionTipsService.ts
+++ src/vs/workbench/parts/extensions/electron-browser/extensionTipsService.ts
@@ -86,6 +86,7 @@ export class ExtensionTipsService extends Disposable implements IExtensionTipsSe
 
 	private readonly _onRecommendationChange: Emitter<RecommendationChangeNotification> = new Emitter<RecommendationChangeNotification>();
 	onRecommendationChange: Event<RecommendationChangeNotification> = this._onRecommendationChange.event;
+	private sessionSeed: number;
 
 	constructor(
 		@IExtensionGalleryService private readonly _galleryService: IExtensionGalleryService,
@@ -108,7 +109,6 @@ export class ExtensionTipsService extends Disposable implements IExtensionTipsSe
 	) {
 		super();
 
-
 		if (!this.isEnabled()) {
 			return;
 		}
@@ -116,6 +116,8 @@ export class ExtensionTipsService extends Disposable implements IExtensionTipsSe
 		if (product.extensionsGallery && product.extensionsGallery.recommendationsUrl) {
 			this._extensionsRecommendationsUrl = product.extensionsGallery.recommendationsUrl;
 		}
+
+		this.sessionSeed = +new Date();
 
 		let globallyIgnored = <string[]>JSON.parse(this.storageService.get('extensionsAssistant/ignored_recommendations', StorageScope.GLOBAL, '[]'));
 		this._globallyIgnoredRecommendations = globallyIgnored.map(id => id.toLowerCase());
@@ -405,7 +407,7 @@ export class ExtensionTipsService extends Disposable implements IExtensionTipsSe
 				...this._dynamicWorkspaceRecommendations,
 				...Object.keys(this._experimentalRecommendations),
 			]).filter(extensionId => this.isExtensionAllowedToBeRecommended(extensionId));
-			shuffle(others);
+			shuffle(others, this.sessionSeed);
 			return others.map(extensionId => {
 				const sources: ExtensionRecommendationSource[] = [];
 				if (this._exeBasedRecommendations[extensionId]) {
