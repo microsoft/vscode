acc5bf3ad15c04e82407508621158ef481cacadc
Johannes Rieken <johannes.rieken@gmail.com>
2018-07-05 17:47:22 +0200

    :lipstick:

--- src/vs/editor/contrib/suggest/suggestModel.ts
+++ src/vs/editor/contrib/suggest/suggestModel.ts
@@ -263,49 +263,53 @@ export class SuggestModel implements IDisposable {
 
 		if (this._state === State.Idle) {
 
-			// trigger 24x7 IntelliSense when idle, enabled, when cursor
-			// moved RIGHT, and when at a good position
-			if (this._editor.getConfiguration().contribInfo.quickSuggestions !== false
-				&& (prevSelection.containsRange(this._currentSelection)
-					|| prevSelection.getEndPosition().isBeforeOrEqual(this._currentSelection.getPosition()))
-			) {
-				this.cancel();
-
-				this._triggerQuickSuggest.cancelAndSet(() => {
-					if (!LineContext.shouldAutoTrigger(this._editor)) {
-						return;
-					}
-
-					const model = this._editor.getModel();
-					const pos = this._editor.getPosition();
-					if (!model) {
-						return;
-					}
-					// validate enabled now
-					const { quickSuggestions } = this._editor.getConfiguration().contribInfo;
-					if (quickSuggestions === false) {
-						return;
-					} else if (quickSuggestions === true) {
-						// all good
-					} else {
-						// Check the type of the token that triggered this
-						model.tokenizeIfCheap(pos.lineNumber);
-						const lineTokens = model.getLineTokens(pos.lineNumber);
-						const tokenType = lineTokens.getStandardTokenType(lineTokens.findTokenIndexAtOffset(Math.max(pos.column - 1 - 1, 0)));
-						const inValidScope = quickSuggestions.other && tokenType === StandardTokenType.Other
-							|| quickSuggestions.comments && tokenType === StandardTokenType.Comment
-							|| quickSuggestions.strings && tokenType === StandardTokenType.String;
-
-						if (!inValidScope) {
-							return;
-						}
-					}
-
-					// we made it till here -> trigger now
-					this.trigger({ auto: true });
-
-				}, this._quickSuggestDelay);
+			if (this._editor.getConfiguration().contribInfo.quickSuggestions === false) {
+				// not enabled
+				return;
 			}
+
+			if (!prevSelection.containsRange(this._currentSelection) && !prevSelection.getEndPosition().isBeforeOrEqual(this._currentSelection.getPosition())) {
+				// cursor didn't move RIGHT
+				return;
+			}
+
+			this.cancel();
+
+			this._triggerQuickSuggest.cancelAndSet(() => {
+				if (!LineContext.shouldAutoTrigger(this._editor)) {
+					return;
+				}
+
+				const model = this._editor.getModel();
+				const pos = this._editor.getPosition();
+				if (!model) {
+					return;
+				}
+				// validate enabled now
+				const { quickSuggestions } = this._editor.getConfiguration().contribInfo;
+				if (quickSuggestions === false) {
+					return;
+				} else if (quickSuggestions === true) {
+					// all good
+				} else {
+					// Check the type of the token that triggered this
+					model.tokenizeIfCheap(pos.lineNumber);
+					const lineTokens = model.getLineTokens(pos.lineNumber);
+					const tokenType = lineTokens.getStandardTokenType(lineTokens.findTokenIndexAtOffset(Math.max(pos.column - 1 - 1, 0)));
+					const inValidScope = quickSuggestions.other && tokenType === StandardTokenType.Other
+						|| quickSuggestions.comments && tokenType === StandardTokenType.Comment
+						|| quickSuggestions.strings && tokenType === StandardTokenType.String;
+
+					if (!inValidScope) {
+						return;
+					}
+				}
+
+				// we made it till here -> trigger now
+				this.trigger({ auto: true });
+
+			}, this._quickSuggestDelay);
+
 		}
 	}
 
