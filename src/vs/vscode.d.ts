97aa61c04952d3781dcbac5fbc473265b503ef24
Joao Moreno <joao.moreno@microsoft.com>
2018-07-06 14:35:11 +0200

    whitespace

--- src/vs/vscode.d.ts
+++ src/vs/vscode.d.ts
@@ -6244,10 +6244,10 @@ declare module 'vscode' {
 		/**
 		 * Registers a webview panel serializer.
 		 *
-		 * Extensions that support reviving should have an`"onWebviewPanel:viewType"` activation event and
-		 * make sure that[registerWebviewPanelSerializer](#registerWebviewPanelSerializer) is called during activation.
+		 * Extensions that support reviving should have an `"onWebviewPanel:viewType"` activation event and
+		 * make sure that [registerWebviewPanelSerializer](#registerWebviewPanelSerializer) is called during activation.
 		 *
-		 * Only a single serializer may be registered at a time for a given`viewType`.
+		 * Only a single serializer may be registered at a time for a given `viewType`.
 		 *
 		 * @param viewType Type of the webview panel that can be serialized.
 		 * @param serializer Webview serializer.
