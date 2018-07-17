b4d366a7ba591c0e9f77ffbef8fbf2f3a17f878d
Rob Lourens <roblourens@gmail.com>
2018-07-17 10:03:51 -0700

    Settings editor - include Breadcrumb settings, fix #54442

--- src/vs/workbench/parts/preferences/browser/settingsLayout.ts
+++ src/vs/workbench/parts/preferences/browser/settingsLayout.ts
@@ -85,6 +85,11 @@ export const tocData: ITOCEntry = {
 					settings: ['workbench.activityBar.*', 'workbench.*color*', 'workbench.fontAliasing', 'workbench.iconTheme', 'workbench.sidebar.location', 'workbench.*.visible', 'workbench.tips.enabled', 'workbench.tree.*', 'workbench.view.*']
 				},
 				{
+					id: 'workbench/breadcrumbs',
+					label: localize('breadcrumbs', "Breadcrumbs"),
+					settings: ['breadcrumbs.*']
+				},
+				{
 					id: 'workbench/editor',
 					label: localize('editorManagement', "Editor Management"),
 					settings: ['workbench.editor.*']
