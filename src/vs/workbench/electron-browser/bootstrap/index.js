7881241762ad9c0b0fd7ec984f5e4dcdc80217aa
Johannes Rieken <johannes.rieken@gmail.com>
2018-07-18 11:26:44 +0200

    reliable way of creating workspace key

--- src/vs/workbench/electron-browser/bootstrap/index.js
+++ src/vs/workbench/electron-browser/bootstrap/index.js
@@ -85,8 +85,10 @@ function showPartsSplash(configuration) {
 
 	let key;
 	let keep = false;
+	// this is the logic of StorageService#getWorkspaceKey and StorageService#toStorageKey
 	if (configuration.folderPath) {
-		key = `storage://workspace/${configuration.folderPath.replace(/^\//, '')}/parts-splash`;
+		let workspaceKey = require('vscode-uri').default.file(configuration.folderPath).toString().replace('file:///', '').replace(/^\//, '');
+		key = `storage://workspace/${workspaceKey}/parts-splash`;
 	} else if (configuration.workspace) {
 		key = `storage://workspace/root:${configuration.workspace.id}/parts-splash`;
 	} else {
