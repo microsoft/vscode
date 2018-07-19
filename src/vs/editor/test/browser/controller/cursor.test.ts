def91279b9d805d373022fed11caf7cd306fcc2f
Paige Haskins <p.haskins95@gmail.com>
2018-07-19 16:54:38 -0400

    Fixed Indentation

--- src/vs/editor/test/browser/controller/cursor.test.ts
+++ src/vs/editor/test/browser/controller/cursor.test.ts
@@ -3943,10 +3943,10 @@ suite('autoClosingPairs', () => {
 					{ open: '\'', close: '\'', notIn: ['string', 'comment'] },
 					{ open: '\"', close: '\"', notIn: ['string'] },
 					{ open: '`', close: '`', notIn: ['string', 'comment'] },
-          { open: '/**', close: ' */', notIn: ['string'] },
-          { open: '.each', close: '{ |e| }', notIn: ['string', 'comment'], cursorPosition: 5 },
-          { open: '.indexOf', close: '()', notIn: ['string', 'comment'], cursorPosition: 1 },
-          { open: '.charAt', close: '(0)', notIn: ['string', 'comment'], cursorPosition: 2 }
+					{ open: '/**', close: ' */', notIn: ['string'] },
+          			{ open: '.each', close: '{ |e| }', notIn: ['string', 'comment'], cursorPosition: 5 },
+          			{ open: '.indexOf', close: '()', notIn: ['string', 'comment'], cursorPosition: 1 },
+          			{ open: '.charAt', close: '(0)', notIn: ['string', 'comment'], cursorPosition: 2 }
 				],
 			}));
 		}
@@ -4150,46 +4150,46 @@ suite('autoClosingPairs', () => {
 			assert.equal(model.getLineContent(8), 'teste"');
 		});
 		mode.dispose();
-  });
+  	});
   
-  test('test cursorPosition attribute of autclosePairs', () => {
-    let mode = new AutoClosingMode();
-    usingCursor({
-      text: [
-        '',
-      ],
-      languageIdentifier: mode.getLanguageIdentifier()
-    }, (model, cursor) => {
+  	test('test cursorPosition attribute of autclosePairs', () => {
+    	let mode = new AutoClosingMode();
+    	usingCursor({
+      		text: [
+        	'',
+      		],
+      		languageIdentifier: mode.getLanguageIdentifier()
+    	}, (model, cursor) => {
 
-      function typeCharacters(cursor: Cursor, chars: string): void {
-        for (let i = 0, len = chars.length; i < len; i++) {
-          cursorCommand(cursor, H.Type, { text: chars[i] }, 'keyboard');
-        }
-      }
+      		function typeCharacters(cursor: Cursor, chars: string): void {
+        		for (let i = 0, len = chars.length; i < len; i++) {
+          		cursorCommand(cursor, H.Type, { text: chars[i] }, 'keyboard');
+        		}
+      		}
 
-      cursor.setSelections('test', [new Selection(1, 1000, 1, 1000)]);
-      typeCharacters(cursor, 'str.indexOf');
-      assert.equal(model.getLineContent(1), 'str.indexOf()');
-      assertCursor(cursor, new Position(1, 13));
+      		cursor.setSelections('test', [new Selection(1, 1000, 1, 1000)]);
+      		typeCharacters(cursor, 'str.indexOf');
+      		assert.equal(model.getLineContent(1), 'str.indexOf()');
+      		assertCursor(cursor, new Position(1, 13));
 
-      moveTo(cursor, 1, model.getLineMaxColumn(1))
-      typeCharacters(cursor, '\n');
-      model.forceTokenization(model.getLineCount());
-      cursor.setSelections('test', [new Selection(2, 1000, 2, 1000)]);
-      typeCharacters(cursor, 'array.each');
-      assert.equal(model.getLineContent(2), 'array.each{ |e| }');
-      assertCursor(cursor, new Position(2, 16));
+      		moveTo(cursor, 1, model.getLineMaxColumn(1))
+      		typeCharacters(cursor, '\n');
+      		model.forceTokenization(model.getLineCount());
+      		cursor.setSelections('test', [new Selection(2, 1000, 2, 1000)]);
+      		typeCharacters(cursor, 'array.each');
+      		assert.equal(model.getLineContent(2), 'array.each{ |e| }');
+      		assertCursor(cursor, new Position(2, 16));
 
-      moveTo(cursor, 2, model.getLineMaxColumn(2))
-      typeCharacters(cursor, '\n');
-      model.forceTokenization(model.getLineCount());
-      cursor.setSelections('test', [new Selection(3, 1000, 3, 1000)]);
-      typeCharacters(cursor, 'line.charAt');
-      assert.equal(model.getLineContent(3), 'line.charAt(0)');
-      assertCursor(cursor, new Position(3, 14));
-    });
-    mode.dispose();
-  });
+      		moveTo(cursor, 2, model.getLineMaxColumn(2))
+      		typeCharacters(cursor, '\n');
+      		model.forceTokenization(model.getLineCount());
+      		cursor.setSelections('test', [new Selection(3, 1000, 3, 1000)]);
+      		typeCharacters(cursor, 'line.charAt');
+      		assert.equal(model.getLineContent(3), 'line.charAt(0)');
+      		assertCursor(cursor, new Position(3, 14));
+    	});
+    	mode.dispose();
+  	});
 
 	test('issue #15825: accents on mac US intl keyboard', () => {
 		let mode = new AutoClosingMode();
