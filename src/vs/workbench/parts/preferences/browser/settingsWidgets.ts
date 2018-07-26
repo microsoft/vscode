d770c8b2808a7a6ff9cdcc990572a920f06df956
Miguel Solorio <miguel.solorio@microsoft.com>
2018-07-26 11:16:47 -0700

    Update modified color to meet color contrast ratio

--- src/vs/workbench/parts/preferences/browser/settingsWidgets.ts
+++ src/vs/workbench/parts/preferences/browser/settingsWidgets.ts
@@ -21,7 +21,7 @@ import { ICssStyleCollector, ITheme, IThemeService, registerThemingParticipant }
 
 const $ = DOM.$;
 export const settingsHeaderForeground = registerColor('settings.headerForeground', { light: '#444444', dark: '#e7e7e7', hc: '#ffffff' }, localize('headerForeground', "(For settings editor preview) The foreground color for a section header or active title in the editor."));
-export const modifiedItemForeground = registerColor('settings.modifiedItemForeground', { light: '#019001', dark: '#73C991', hc: '#73C991' }, localize('modifiedItemForeground', "(For settings editor preview) The foreground color for a modified setting."));
+export const modifiedItemForeground = registerColor('settings.modifiedItemForeground', { light: '#018101', dark: '#73C991', hc: '#73C991' }, localize('modifiedItemForeground', "(For settings editor preview) The foreground color for a modified setting."));
 export const settingItemInactiveSelectionBorder = registerColor('settings.inactiveSelectedItemBorder', { dark: '#3F3F46', light: '#CCCEDB', hc: null }, localize('settingItemInactiveSelectionBorder', "(For settings editor preview) The color of the selected setting row border, when the settings list does not have focus."));
 
 // Enum control colors
