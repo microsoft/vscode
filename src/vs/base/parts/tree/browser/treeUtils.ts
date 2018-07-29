bd262c9ab55332be988bef6ccc8019ed7f3d7f3d
Rob Lourens <roblourens@gmail.com>
2018-07-29 14:26:25 -0700

    Settings editor - don't blink the scrollbar when toc selection changes And hide TOC correctly when the editor is narrow

--- src/vs/base/parts/tree/browser/treeUtils.ts
+++ src/vs/base/parts/tree/browser/treeUtils.ts
@@ -6,12 +6,26 @@
 
 import * as _ from 'vs/base/parts/tree/browser/tree';
 
-export function collapseAll(tree: _.ITree): void {
+export function collapseAll(tree: _.ITree, except?: any): void {
 	const nav = tree.getNavigator();
 	let cur;
 	while (cur = nav.next()) {
-		tree.collapse(cur);
+		if (!except || !isEqualOrParent(tree, except, cur)) {
+			tree.collapse(cur);
+		}
 	}
+}
+
+export function isEqualOrParent(tree: _.ITree, element: any, candidateParent: any): boolean {
+	const nav = tree.getNavigator(element);
+
+	do {
+		if (element === candidateParent) {
+			return true;
+		}
+	} while (element = nav.parent());
+
+	return false;
 }
 
 export function expandAll(tree: _.ITree): void {
