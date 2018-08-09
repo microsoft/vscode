16aa4bd4c161f234413a0cd153bf396dfabbd407
Benjamin Pasero <benjpas@microsoft.com>
2018-08-09 16:06:39 +0200

    Revert "pinch zoom is now disabled by default"
    This reverts commit 57ab236587b0745512395f67c6ad7d287a7d7c31.

--- src/vs/workbench/electron-browser/bootstrap/index.js
+++ src/vs/workbench/electron-browser/bootstrap/index.js
@@ -252,8 +252,9 @@ function main() {
 	const enableDeveloperTools = (process.env['VSCODE_DEV'] || !!configuration.extensionDevelopmentPath) && !configuration.extensionTestsPath;
 	const unbind = registerListeners(enableDeveloperTools);
 
-	// Apply zoom level early to avoid glitches
+	// disable pinch zoom & apply zoom level early to avoid glitches
 	const zoomLevel = configuration.zoomLevel;
+	webFrame.setVisualZoomLevelLimits(1, 1);
 	if (typeof zoomLevel === 'number' && zoomLevel !== 0) {
 		webFrame.setZoomLevel(zoomLevel);
 	}
