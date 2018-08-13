7ce26a4a24076262efbb321105819579df9f277f
Benjamin Pasero <benjamin.pasero@gmail.com>
2018-08-13 13:31:52 +0200

    Electron 2.0.7 (#56256)
    * update to Electron 2.0.7
    
    * disable ColorCorrectRendering (for #51791)

--- src/main.js
+++ src/main.js
@@ -81,9 +81,8 @@ const app = require('electron').app;
 // TODO@Ben Electron 2.0.x: prevent localStorage migration from SQLite to LevelDB due to issues
 app.commandLine.appendSwitch('disable-mojo-local-storage');
 
-// TODO@Ben Electron 2.0.x: force srgb color profile (for https://github.com/Microsoft/vscode/issues/51791)
-// This also seems to fix: https://github.com/Microsoft/vscode/issues/48043
-app.commandLine.appendSwitch('force-color-profile', 'srgb');
+// Force pre-Chrome-60 color profile handling (for https://github.com/Microsoft/vscode/issues/51791)
+app.commandLine.appendSwitch('disable-features', 'ColorCorrectRendering');
 
 const minimist = require('minimist');
 const paths = require('./paths');
