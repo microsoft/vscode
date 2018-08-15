2d9093bcf215189f40eafb2e301db91d0d19a8d5
Martin Aeschlimann <martinae@microsoft.com>
2018-08-15 17:18:57 +0200

    resources.join & normalize must not change windows drive letter. Fixes #56403

--- src/vs/base/common/resources.ts
+++ src/vs/base/common/resources.ts
@@ -26,21 +26,19 @@ export function basenameOrAuthority(resource: URI): string {
 }
 
 /**
- *
+ * Tests wheter a `candidate` URI is a parent or equal of a given `base` URI.
  * @param base A uri which is "longer"
  * @param parentCandidate A uri which is "shorter" then `base`
  */
 export function isEqualOrParent(base: URI, parentCandidate: URI, ignoreCase?: boolean): boolean {
 	if (base.scheme === parentCandidate.scheme) {
 		if (base.scheme === Schemas.file) {
-			return paths.isEqualOrParent(base.fsPath, parentCandidate.fsPath, ignoreCase);
+			return paths.isEqualOrParent(fsPath(base), fsPath(parentCandidate), ignoreCase);
 		}
-		if (!isEqualAuthority(base.authority, parentCandidate.authority, ignoreCase)) {
-			return false;
+		if (isEqualAuthority(base.authority, parentCandidate.authority, ignoreCase)) {
+			return paths.isEqualOrParent(base.path, parentCandidate.path, ignoreCase, '/');
 		}
-		return paths.isEqualOrParent(base.path, parentCandidate.path, ignoreCase, '/');
 	}
-
 	return false;
 }
 
@@ -95,7 +93,7 @@ export function dirname(resource: URI): URI {
 export function joinPath(resource: URI, pathFragment: string): URI {
 	let joinedPath: string;
 	if (resource.scheme === Schemas.file) {
-		joinedPath = URI.file(paths.join(resource.fsPath, pathFragment)).path;
+		joinedPath = URI.file(paths.join(fsPath(resource), pathFragment)).path;
 	} else {
 		joinedPath = paths.join(resource.path, pathFragment);
 	}
@@ -113,13 +111,32 @@ export function joinPath(resource: URI, pathFragment: string): URI {
 export function normalizePath(resource: URI): URI {
 	let normalizedPath: string;
 	if (resource.scheme === Schemas.file) {
-		normalizedPath = URI.file(paths.normalize(resource.fsPath)).path;
+		normalizedPath = URI.file(paths.normalize(fsPath(resource))).path;
 	} else {
 		normalizedPath = paths.normalize(resource.path);
 	}
 	return resource.with({
 		path: normalizedPath
 	});
+}
+
+/**
+ * Returns the fsPath of an URI where the drive letter is not normalized.
+ * See #56403.
+ */
+function fsPath(uri: URI): string {
+	let value: string;
+	if (uri.authority && uri.path.length > 1 && uri.scheme === 'file') {
+		// unc path: file://shares/c$/far/boo
+		value = `//${uri.authority}${uri.path}`;
+	} else {
+		// other path
+		value = uri.path;
+	}
+	if (isWindows) {
+		value = value.replace(/\//g, '\\');
+	}
+	return value;
 }
 
 /**
@@ -149,6 +166,13 @@ export function distinctParents<T>(items: T[], resourceAccessor: (item: T) => UR
 	return distinctParents;
 }
 
+/**
+ * Tests wheter the given URL is a file URI created by `URI.parse` instead of `URI.file`.
+ * Such URI have no scheme or scheme that consist of a single letter (windows drive letter)
+ * @param candidate The URI to test
+ * @returns A corrected, real file URI if the input seems to be malformed.
+ * Undefined is returned if the input URI looks fine.
+ */
 export function isMalformedFileUri(candidate: URI): URI | undefined {
 	if (!candidate.scheme || isWindows && candidate.scheme.match(/^[a-zA-Z]$/)) {
 		return URI.file((candidate.scheme ? candidate.scheme + ':' : '') + candidate.path);
