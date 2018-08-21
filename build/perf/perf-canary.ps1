66c69bd7a7eba9fea825f631613cf5e7c75ab80e
Johannes Rieken <johannes.rieken@gmail.com>
2018-08-21 13:05:05 +0200

    more work

--- build/perf/perf-canary.ps1
+++ build/perf/perf-canary.ps1
@@ -1,15 +1,14 @@
-#
+param (
+    [string]$OutDir = $PSScriptRoot,
+    [string]$UpdateServer = "https://vscode-update.azurewebsites.net/api/update",
+    [int]$Target = 2000
+)
 
-
-$quality = "stable"
-$app = "Visual Studio Code.app"
-
-$uri
 if ($IsMacOS) {
-    $uri = "https://vscode-update.azurewebsites.net/api/update/darwin/$quality/unknown"
+    $uri = "$UpdateServer/darwin/insider/unknown"
 }
 elseif ($IsWindows) {
-    $uri = "https://vscode-update.azurewebsites.net/api/update/win32-x64-archive/$quality/unknown"
+    $uri = "$UpdateServer/win32-x64-archive/insider/unknown"
 }
 else {
     Write-Error "unsupported platform"
@@ -19,7 +18,7 @@ else {
 $request = Invoke-WebRequest -Uri $uri
 $data = ConvertFrom-Json -InputObject $request.Content
 
-$appdir = "$PSScriptRoot/versions/$($data.version)"
+$appdir = "$OutDir/versions/$($data.version)"
 if (Test-Path -Path $appdir) {
     Write-Host "🌴 Nothing new, nothing to do..."
     Exit 0
@@ -28,7 +27,7 @@ else {
     # create foler, download, and unzip
     # * Expand-Archive breaks executable bits
     New-Item -ItemType Directory -Force -Path $appdir | Out-Null
-    $zipfile = "$PSScriptRoot/$($data.version).zip";
+    $zipfile = "$OutDir/$($data.version).zip";
     Invoke-WebRequest -Uri $data.url -OutFile $zipfile
     if ($IsWindows) {
         Expand-Archive -Path $zipfile -Force -DestinationPath $appdir
@@ -42,31 +41,31 @@ else {
 # lauch, read timer, and repeat
 $currentTry = 1;
 $maxTry = 100;
-$durTarget = 1700;
-$timers = "$PSScriptRoot/startupInfo.txt"
+$timers = "$OutDir/startupInfo.txt"
+$userdata = "$OutDir/userData";
+
 while ($true) {
     if ($IsMacOS) {
-        open "$appdir/$app" -W --args --prof-append-timers $timers
+        open "$appdir/Visual Studio Code - Insiders.app" -n -W --args --prof-append-timers $timers --user-data-dir $userdata --skip-getting-started --skip-release-notes --disable-extensions
     }
-    elseif($IsWindows) {
-        Start-Process -FilePath "$appdir/Code.exe" -ArgumentList "--prof-append-timers $timers" -Wait
+    elseif ($IsWindows) {
+        Start-Process -FilePath "$appdir/Code - Insiders.exe" -ArgumentList "--prof-append-timers $timers --user-data-dir $userdata --skip-getting-started --skip-release-notes --disable-extensions" -Wait | Out-Null
     }
     else {
         Exit 100;
     }
-    $line = Get-Content -Tail 1 $timers
-    $durLast = $line.Substring(60, 4);
-    # $durLast = $line.Substring(0, 5);
-    if ($durLast -lt $durTarget) {
+    $line = Get-Content -Tail 1 $timers 
+    $durLast = $line.Substring(0, 5);
+    if ($durLast -lt $Target) {
         Write-Host "👍 good - last startup took $($durLast)ms, tried $currentTry times"
         Write-Host $line
         Exit 0
     }
     if ($1 -gt $maxTry) {
-        Write-Host "💥 FAILURE - could not start within $($durTarget)ms, tried $maxTry-times!"
+        Write-Host "💥 FAILURE - could not start within $($Target)ms, tried $maxTry-times!"
         Exit 1
     }
-    Write-Host "🐌 too slow... took $($durLast)ms, target is $($durTarget)ms"
+    Write-Host "🐌 too slow... took $($durLast)ms, target is $($Target)ms"
     Start-Sleep -s (Get-Random -Minimum 2 -Maximum 10)
     $currentTry += 1;
 }
