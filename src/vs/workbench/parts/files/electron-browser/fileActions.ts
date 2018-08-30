712cd009f8ee594cc448a259dc8cdfec85659470
isidor <inikolic@microsoft.com>
2018-08-30 11:43:48 +0200

    Revert "Make sure prefix detection starts from start of the file name"
    This reverts commit 869dafb1bcf1c4620ba83d161195d832b33f261d.

--- src/vs/workbench/parts/files/electron-browser/fileActions.ts
+++ src/vs/workbench/parts/files/electron-browser/fileActions.ts
@@ -1048,7 +1048,7 @@ export function incrementFileName(name: string, isFolder: boolean): string {
 	}
 
 	// 1.file.txt=>2.file.txt
-	let prefixFileRegex = RegExp('^(\\d+)(' + separators + '.*)(\\..*)$');
+	let prefixFileRegex = RegExp('(\\d+)(' + separators + '.*)(\\..*)$');
 	if (!isFolder && name.match(prefixFileRegex)) {
 		return name.replace(prefixFileRegex, (match, g1?, g2?, g3?) => {
 			let number = parseInt(g1);
@@ -1059,7 +1059,7 @@ export function incrementFileName(name: string, isFolder: boolean): string {
 	}
 
 	// 1.txt=>2.txt
-	let prefixFileNoNameRegex = RegExp('^(\\d+)(\\..*)$');
+	let prefixFileNoNameRegex = RegExp('(\\d+)(\\..*)$');
 	if (!isFolder && name.match(prefixFileNoNameRegex)) {
 		return name.replace(prefixFileNoNameRegex, (match, g1?, g2?) => {
 			let number = parseInt(g1);
