be3a4a27e00d5a560f8c600c86ff6e1ee67077d6
Andre Wachsmuth <wachsmuth.andre@gmx.de>
2018-09-01 01:32:44 +0200

    fix promise polyfill, chaining promises works

--- src/vs/base/common/winjs.polyfill.promise.ts
+++ src/vs/base/common/winjs.polyfill.promise.ts
@@ -71,19 +71,47 @@ export class PolyfillPromise<T = any> implements Promise<T> {
 
 	then(onFulfilled?: any, onRejected?: any): PolyfillPromise {
 		let sync = true;
+		// To support chaining, we need to return the value of the
+		// onFulfilled and onRejected callback.
+		// WinJSPromise supports a flat-map style #then, ie. the callbacks
+		// passed to WinJSPromise#then can return a Promise.
 		let promise = new PolyfillPromise(this._winjsPromise.then(
 			onFulfilled && function (value) {
 				if (!sync) {
-					onFulfilled(value);
+					return onFulfilled(value);
 				} else {
-					platform.setImmediate(() => onFulfilled(value));
+					return new WinJSPromise((resolve, reject) => {
+						platform.setImmediate(() => {
+							let result;
+							try {
+								result = onFulfilled(value);
+							}
+							catch (err2) {
+								reject(err2);
+								return;
+							}
+							resolve(result);
+						});
+					});
 				}
 			},
 			onRejected && function (err) {
 				if (!sync) {
-					onRejected(err);
+					return onRejected(err);
 				} else {
-					platform.setImmediate(() => onRejected(err));
+					return new WinJSPromise((resolve, reject) => {
+						platform.setImmediate(() => {
+							let result;
+							try {
+								result = onRejected(err);
+							}
+							catch (err2) {
+								reject(err2);
+								return;
+							}
+							resolve(result);
+						});
+					});
 				}
 			}
 		));
