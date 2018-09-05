14e47b1f95c4de6cd1b1055e3ecb8a878e6c33eb
Sandeep Somavarapu <sasomava@microsoft.com>
2018-09-05 15:20:48 +0200

    use non workspace extensions

--- src/vs/platform/extensionManagement/common/extensionManagementUtil.ts
+++ src/vs/platform/extensionManagement/common/extensionManagementUtil.ts
@@ -121,28 +121,7 @@ export function getMaliciousExtensionsSet(report: IReportedExtension[]): Set<str
 	return result;
 }
 
-const workspaceExtensions = new Set<string>();
-[
-	'vscode.extension-editing',
-	'vscode.configuration-editing',
-	'vscode.search-rg',
-	'vscode.css-language-features',
-	'vscode.git',
-	'vscode.grunt',
-	'vscode.gulp',
-	'vscode.html-language-features',
-	'vscode.json-language-features',
-	'vscode.markdown-language-features',
-	'vscode.npm',
-	'vscode.php-language-features',
-	'vscode.typescript-language-features',
-	'ms-vscode.node-debug',
-	'ms-vscode.node-debug2',
-	'ms-python.python',
-	'eg2.tslint',
-	'dbaeumer.vscode-eslint',
-	'eamodio.gitlens'
-].forEach(extension => workspaceExtensions.add(extension));
+const nonWorkspaceExtensions = new Set<string>();
 
 export function isWorkspaceExtension(manifest: IExtensionManifest, configurationService: IConfigurationService): boolean {
 	const extensionId = getGalleryExtensionId(manifest.publisher, manifest.name);
@@ -160,7 +139,7 @@ export function isWorkspaceExtension(manifest: IExtensionManifest, configuration
 		if ((manifest.categories || []).indexOf('Workspace Extension') !== -1) {
 			return true;
 		}
-		return workspaceExtensions.has(extensionId);
+		return !nonWorkspaceExtensions.has(extensionId);
 	}
 	return false;
 }
