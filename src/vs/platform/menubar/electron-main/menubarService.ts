e0de0989ce53b1721b5ec4fff3311ffc6687d564
SteVen Batten <stbatt@microsoft.com>
2018-09-07 12:42:50 -0400

    fixes #57786

--- src/vs/platform/menubar/electron-main/menubarService.ts
+++ src/vs/platform/menubar/electron-main/menubarService.ts
@@ -6,9 +6,7 @@
 'use strict';
 
 import { IMenubarService, IMenubarData, IMenubarKeybinding } from 'vs/platform/menubar/common/menubar';
-// TODO@sbatten bad layering
-// tslint:disable-next-line:import-patterns
-import { Menubar } from 'vs/code/electron-main/menubar';
+import { Menubar } from 'vs/platform/menubar/electron-main/menubar';
 import { ILogService } from 'vs/platform/log/common/log';
 import { TPromise } from 'vs/base/common/winjs.base';
 import { IInstantiationService } from 'vs/platform/instantiation/common/instantiation';
