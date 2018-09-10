5db4761cac3c0a8d50430ff9675b7041d2ff4158
Matt Bierner <matb@microsoft.com>
2018-09-10 15:51:55 -0700

    Remove unused param

--- extensions/typescript-language-features/src/languageProvider.ts
+++ extensions/typescript-language-features/src/languageProvider.ts
@@ -67,7 +67,7 @@ export default class LanguageProvider extends Disposable {
 		this._register((await import('./features/hover')).register(selector, this.client));
 		this._register((await import('./features/implementations')).register(selector, this.client));
 		this._register((await import('./features/implementationsCodeLens')).register(selector, this.description.id, this.client, cachedResponse));
-		this._register((await import('./features/jsDocCompletions')).register(selector, this.client, this.commandManager));
+		this._register((await import('./features/jsDocCompletions')).register(selector, this.client));
 		this._register((await import('./features/organizeImports')).register(selector, this.client, this.commandManager, this.fileConfigurationManager, this.telemetryReporter));
 		this._register((await import('./features/quickFix')).register(selector, this.client, this.fileConfigurationManager, this.commandManager, this.client.diagnosticsManager, this.telemetryReporter));
 		this._register((await import('./features/refactor')).register(selector, this.client, this.fileConfigurationManager, this.commandManager, this.telemetryReporter));
