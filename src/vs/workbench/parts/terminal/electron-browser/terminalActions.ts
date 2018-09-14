652c81632efe719d288f16deec579707dac609da
Alex Ross <alros@microsoft.com>
2018-09-14 09:47:52 -0700

    Added check to handle Linux

--- src/vs/workbench/parts/terminal/electron-browser/terminalActions.ts
+++ src/vs/workbench/parts/terminal/electron-browser/terminalActions.ts
@@ -27,6 +27,7 @@ import { IWorkspaceContextService } from 'vs/platform/workspace/common/workspace
 import { PICK_WORKSPACE_FOLDER_COMMAND_ID } from 'vs/workbench/browser/actions/workspaceCommands';
 import { INotificationService } from 'vs/platform/notification/common/notification';
 import { TERMINAL_COMMAND_ID } from 'vs/workbench/parts/terminal/common/terminalCommands';
+import { isWindows } from 'vs/base/common/platform';
 
 export const TERMINAL_PICKER_PREFIX = 'term ';
 
@@ -639,8 +640,12 @@ export class RunActiveFileInTerminalAction extends Action {
 			return TPromise.as(void 0);
 		}
 		let uriPath: string = uri.fsPath;
-		if (uriPath.indexOf(' ') !== -1) {
-			uriPath = '"' + uriPath + '"';
+		if ((uriPath.indexOf(' ') !== -1) || (uriPath.indexOf('"') !== -1)) {
+			if (isWindows) {
+				uriPath = '"' + uriPath + '"';
+			} else {
+				uriPath = '\'' + uriPath + '\'';
+			}
 		}
 		instance.sendText(uriPath, true);
 		return this.terminalService.showPanel();
