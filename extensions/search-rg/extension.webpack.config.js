b0787a2b87988dcddb6c33b356e59e568c255a77
Johannes Rieken <johannes.rieken@gmail.com>
2018-09-20 09:59:24 +0200

    fix webpack entries for merge-conflict and search-rg

--- extensions/search-rg/extension.webpack.config.js
+++ extensions/search-rg/extension.webpack.config.js
@@ -12,6 +12,6 @@ const withDefaults = require('../shared.webpack.config');
 module.exports = withDefaults({
 	context: __dirname,
 	entry: {
-		main: './src/extension.ts'
+		extension: './src/extension.ts'
 	}
 });
