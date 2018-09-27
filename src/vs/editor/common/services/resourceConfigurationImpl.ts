0f477b0c58a13ae7144952e35dcd09371dafa3fd
Sandeep Somavarapu <sasomava@microsoft.com>
2018-09-27 12:26:45 +0200

    #59517 Use absolute path

--- src/vs/editor/common/services/resourceConfigurationImpl.ts
+++ src/vs/editor/common/services/resourceConfigurationImpl.ts
@@ -43,6 +43,7 @@ export class TextResourceConfigurationService extends Disposable implements ITex
 		if (model) {
 			return position ? this.modeService.getLanguageIdentifier(model.getLanguageIdAtPosition(position.lineNumber, position.column)).language : model.getLanguageIdentifier().language;
 		}
-		return this.modeService.getModeIdByFilenameOrFirstLine(basename(resource.path));
+		return this.modeService.getModeIdByFilenameOrFirstLine(resource.path);
+
 	}
 }
