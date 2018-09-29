f8c674c9be45c59d46f3d60a825182a0f900052f
Emil Hessman <emil@hessman.se>
2018-09-29 20:31:54 +0200

    Remove duplicate switch case

--- extensions/typescript-language-features/src/features/documentSymbol.ts
+++ extensions/typescript-language-features/src/features/documentSymbol.ts
@@ -22,7 +22,6 @@ const getSymbolKind = (kind: string): vscode.SymbolKind => {
 		case PConst.Kind.variable: return vscode.SymbolKind.Variable;
 		case PConst.Kind.const: return vscode.SymbolKind.Variable;
 		case PConst.Kind.localVariable: return vscode.SymbolKind.Variable;
-		case PConst.Kind.variable: return vscode.SymbolKind.Variable;
 		case PConst.Kind.function: return vscode.SymbolKind.Function;
 		case PConst.Kind.localFunction: return vscode.SymbolKind.Function;
 	}
