038017705ed148483fcdcc37b9bf8869dcee9268
pkoushik <pkoushik@akamai.com>
2018-10-04 12:55:24 +0530

    fix #59364 -  Review #3

--- src/vs/workbench/services/search/node/ripgrepTextSearch.ts
+++ src/vs/workbench/services/search/node/ripgrepTextSearch.ts
@@ -511,7 +511,7 @@ function getRgArgs(config: IRawSearch) {
 	}
 
 	args.push('--no-config');
-	if (!config.disregardGlobalIgnoreFiles) {
+	if (config.disregardGlobalIgnoreFiles) {
 		args.push('--no-ignore-global');
 	}
 
