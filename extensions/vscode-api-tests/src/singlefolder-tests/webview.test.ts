8151b1fc50ddccea673f0500c64ab10c4a126d86
Matt Bierner <matb@microsoft.com>
2018-10-04 18:35:14 -0700

    Add test for webview state persist

--- extensions/vscode-api-tests/src/singlefolder-tests/webview.test.ts
+++ extensions/vscode-api-tests/src/singlefolder-tests/webview.test.ts
@@ -6,10 +6,13 @@
 import 'mocha';
 import * as assert from 'assert';
 import * as vscode from 'vscode';
+import { join } from 'path';
+import { closeAllEditors } from '../utils';
 
 const webviewId = 'myWebview';
 
 suite('Webview tests', () => {
+	teardown(closeAllEditors);
 
 	test('webview communication', async () => {
 		const webview = vscode.window.createWebviewPanel(webviewId, 'title', { viewColumn: vscode.ViewColumn.One }, { enableScripts: true });
@@ -31,6 +34,49 @@ suite('Webview tests', () => {
 		const response = await p;
 		assert.strictEqual(response.value, 2);
 	});
+
+	test('webview preserves state when switching visibility', async () => {
+		const webview = vscode.window.createWebviewPanel(webviewId, 'title', { viewColumn: vscode.ViewColumn.One }, { enableScripts: true });
+		webview.webview.html = createHtmlDocumentWithBody(/*html*/`
+			<script>
+				const vscode = acquireVsCodeApi();
+				let value = (vscode.getState() || {}).value || 0;
+				window.addEventListener('message', (message) => {
+					switch (message.data.type) {
+						case 'get':
+							vscode.postMessage({ value });
+							break;
+
+						case 'add':
+							++value;;
+							vscode.setState({ value })
+							vscode.postMessage({ value });
+							break;
+					}
+				});
+			</script>`);
+
+		{
+			const p = messageAwaiter(webview, 1);
+			webview.webview.postMessage({ type: 'add' });
+			const [response] = await p;
+			assert.strictEqual(response.value, 1);
+		}
+
+		// Swap away from the webview
+		const doc = await vscode.workspace.openTextDocument(join(vscode.workspace.rootPath || '', './simple.txt'));
+		await vscode.window.showTextDocument(doc);
+
+		// And then back
+		webview.reveal(vscode.ViewColumn.One);
+
+		{
+			const p = messageAwaiter(webview, 1);
+			webview.webview.postMessage({ type: 'get' });
+			const [response] = await p;
+			assert.strictEqual(response.value, 1);
+		}
+	});
 });
 
 function createHtmlDocumentWithBody(body: string): string {
@@ -48,3 +94,15 @@ function createHtmlDocumentWithBody(body: string): string {
 </html>`;
 }
 
+
+function messageAwaiter(webview: vscode.WebviewPanel, expected: number): Promise<any[]> {
+	let received: any[] = [];
+	return new Promise<any>(resolve => {
+		webview.webview.onDidReceiveMessage(message => {
+			received.push(message);
+			if (received.length >= expected) {
+				resolve(received);
+			}
+		});
+	});
+}
