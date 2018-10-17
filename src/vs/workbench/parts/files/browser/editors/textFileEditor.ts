74f09acca974227474db89b5e0cd522aa1d4d856
Benjamin Pasero <benjpas@microsoft.com>
2018-10-17 08:25:16 +0200

    editor - remove code that is handled by cancellation token (for #60163)

--- src/vs/workbench/parts/files/browser/editors/textFileEditor.ts
+++ src/vs/workbench/parts/files/browser/editors/textFileEditor.ts
@@ -130,19 +130,7 @@ export class TextFileEditor extends BaseTextEditor {
 					return this.openAsBinary(input, options);
 				}
 
-				// Check Model state
 				const textFileModel = <ITextFileEditorModel>resolvedModel;
-
-				const hasInput = !!this.input;
-				const modelDisposed = textFileModel.isDisposed();
-				const inputChanged = hasInput && this.input.getResource().toString() !== textFileModel.getResource().toString();
-				if (
-					!hasInput ||		// editor got hidden meanwhile
-					modelDisposed || 	// input got disposed meanwhile
-					inputChanged 		// a different input was set meanwhile
-				) {
-					return void 0;
-				}
 
 				// Editor
 				const textEditor = this.getControl();
