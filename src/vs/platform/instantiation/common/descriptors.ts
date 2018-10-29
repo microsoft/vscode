4bd295b6e47bef7b2dc163f8dd6ef0cb33852bf6
Johannes Rieken <johannes.rieken@gmail.com>
2018-10-29 14:39:08 +0100

    fix strict null

--- src/vs/platform/instantiation/common/descriptors.ts
+++ src/vs/platform/instantiation/common/descriptors.ts
@@ -11,7 +11,7 @@ export class SyncDescriptor<T> {
 	readonly staticArguments: any[];
 	readonly supportsDelayedInstantiation: boolean;
 
-	constructor(ctor: new (...args: any[]) => T, staticArguments: any[] = [], supportsDelayedInstantiation?: boolean) {
+	constructor(ctor: new (...args: any[]) => T, staticArguments: any[] = [], supportsDelayedInstantiation: boolean = false) {
 		this.ctor = ctor;
 		this.staticArguments = staticArguments;
 		this.supportsDelayedInstantiation = supportsDelayedInstantiation;
