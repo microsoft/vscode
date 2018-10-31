15ed89c0ee60ee0190b0ca99070df36f4273f742
isidor <inikolic@microsoft.com>
2018-10-31 17:13:20 +0100

    fixes #61259

--- src/vs/workbench/parts/debug/electron-browser/debugService.ts
+++ src/vs/workbench/parts/debug/electron-browser/debugService.ts
@@ -780,7 +780,7 @@ export class DebugService implements IDebugService {
 
 		if (stackFrame) {
 			stackFrame.openInEditor(this.editorService, true).then(undefined, errors.onUnexpectedError);
-			aria.alert(nls.localize('debuggingPaused', "Debugging paused, reason {0}, {1} {2}", thread.stoppedDetails.reason, stackFrame.source ? stackFrame.source.name : '', stackFrame.range.startLineNumber));
+			aria.alert(nls.localize('debuggingPaused', "Debugging paused {0}, {1} {2}", thread.stoppedDetails ? `, reason ${thread.stoppedDetails.reason}` : '', stackFrame.source ? stackFrame.source.name : '', stackFrame.range.startLineNumber));
 		}
 
 		this.viewModel.setFocus(stackFrame, thread, session, explicit);
