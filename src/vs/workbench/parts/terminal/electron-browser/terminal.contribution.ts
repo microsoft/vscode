e87a046ca219dd722fd4f945e039e8560f57bdc6
Alex Ross <alros@microsoft.com>
2018-10-31 12:01:12 +0100

    Fixes #62175

--- src/vs/workbench/parts/terminal/electron-browser/terminal.contribution.ts
+++ src/vs/workbench/parts/terminal/electron-browser/terminal.contribution.ts
@@ -382,13 +382,13 @@ configurationRegistry.registerConfiguration({
 			default: 'JsArray'
 		},
 		'terminal.integrated.splitCwd': {
-			description: nls.localize('terminal.integrated.splitCwd', "Controls the source of the starting cwd for terminals created by splitting."),
+			description: nls.localize('terminal.integrated.splitCwd', "Controls the working directory a split terminal starts with."),
 			type: 'string',
 			enum: ['workspaceRoot', 'sourceInitialCwd', 'sourceCwd'],
 			enumDescriptions: [
-				nls.localize('terminal.integrated.splitCwd.workspaceRoot', "A new split terminal will use the workspace root as the cwd. In a multi-root workspace a choice for which root folder to use is offered."),
-				nls.localize('terminal.integrated.splitCwd.sourceInitialCwd', "A new split terminal will use the cwd that the parent terminal started with."),
-				nls.localize('terminal.integrated.splitCwd.sourceCwd', "On macOS and Linux, a new split terminal will use the cwd of the parent terminal. On Windows, this behaves the same as sourceInitialCwd."),
+				nls.localize('terminal.integrated.splitCwd.workspaceRoot', "A new split terminal will use the workspace root as the working directory. In a multi-root workspace a choice for which root folder to use is offered."),
+				nls.localize('terminal.integrated.splitCwd.sourceInitialCwd', "A new split terminal will use the working directory that the parent terminal started with."),
+				nls.localize('terminal.integrated.splitCwd.sourceCwd', "On macOS and Linux, a new split terminal will use the working directory of the parent terminal. On Windows, this behaves the same as sourceInitialCwd."),
 			],
 			default: 'sourceCwd'
 		},
