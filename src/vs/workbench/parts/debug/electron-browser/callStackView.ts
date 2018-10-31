9f4969b523479c35085d13f36007dcd735f76972
isidor <inikolic@microsoft.com>
2018-10-31 16:01:34 +0100

    do not show undefined label
    #50334

--- src/vs/workbench/parts/debug/electron-browser/callStackView.ts
+++ src/vs/workbench/parts/debug/electron-browser/callStackView.ts
@@ -555,7 +555,7 @@ class CallStackRenderer implements IRenderer {
 
 	private renderShowMore(data: ILabelTemplateData, element: IStackFrame[]): void {
 		data.label.textContent = nls.localize('showMoreStackFrames', "Show {0} More Stack Frames", element.length);
-		if (element.every(sf => sf.source && sf.source.origin === element[0].source.origin)) {
+		if (element.every(sf => sf.source && sf.source.origin && sf.source.origin === element[0].source.origin)) {
 			data.label.textContent += ` ${element[0].source.origin}`;
 		}
 	}
