270f7ddc0f32bb865f84de833cc28ca3c7da80b1
isidor <inikolic@microsoft.com>
2018-10-31 16:25:20 +0100

    fixes #62091

--- src/vs/workbench/parts/debug/common/debugModel.ts
+++ src/vs/workbench/parts/debug/common/debugModel.ts
@@ -758,9 +758,18 @@ export class DebugModel implements IDebugModel {
 	}
 
 	public addSession(session: IDebugSession): void {
-		// Make sure to remove all inactive sessions once a new session is started
-		// Also make sure to de-dupe if a session is re-intialized. In case of EH debugging we are adding a session again after an attach.
-		this.sessions = this.sessions.filter(s => s.state !== State.Inactive && s.getId() !== session.getId());
+		this.sessions = this.sessions.filter(s => {
+			if (s.getId() === session.getId()) {
+				// Make sure to de-dupe if a session is re-intialized. In case of EH debugging we are adding a session again after an attach.
+				return false;
+			}
+			if (s.state === State.Inactive && s.getLabel() === session.getLabel()) {
+				// Make sure to remove all inactive sessions that are using the same configuration as the new session
+				return false;
+			}
+
+			return true;
+		});
 		this.sessions.push(session);
 		this._onDidChangeCallStack.fire();
 	}
