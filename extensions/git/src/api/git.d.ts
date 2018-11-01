349e31fee9603711ddabfe4c8cb6fb66b72e28f3
Joao Moreno <joao.moreno@microsoft.com>
2018-11-01 15:00:36 +0100

    fixes #58918

--- extensions/git/src/api/git.d.ts
+++ extensions/git/src/api/git.d.ts
@@ -164,8 +164,15 @@ export interface API {
 
 export interface GitExtension {
 
+	readonly enabled: boolean;
+	readonly onDidChangeEnablement: Event<boolean>;
+
 	/**
 	 * Returns a specific API version.
+	 *
+	 * Throws error if git extension is disabled. You can listed to the
+	 * [GitExtension.onDidChangeEnablement](#GitExtension.onDidChangeEnablement) event
+	 * to know when the extension becomes enabled/disabled.
 	 *
 	 * @param version Version number.
 	 * @returns API instance
