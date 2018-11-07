c475d3d90684ec6c1d51e0f0293e9e0a43e7159d
isidor <inikolic@microsoft.com>
2018-11-07 11:12:34 +0100

    debug: only append stale call stack if there are more frames to come
    fixes #62305

--- src/vs/workbench/parts/debug/electron-browser/callStackView.ts
+++ src/vs/workbench/parts/debug/electron-browser/callStackView.ts
@@ -362,7 +362,7 @@ class CallStackDataSource implements IDataSource {
 		}
 
 		return callStackPromise.then(() => {
-			if (callStack.length === 1 && thread.session.capabilities.supportsDelayedStackTraceLoading) {
+			if (callStack.length === 1 && thread.session.capabilities.supportsDelayedStackTraceLoading && thread.stoppedDetails && thread.stoppedDetails.totalFrames > 1) {
 				// To reduce flashing of the call stack view simply append the stale call stack
 				// once we have the correct data the tree will refresh and we will no longer display it.
 				callStack = callStack.concat(thread.getStaleCallStack().slice(1));
