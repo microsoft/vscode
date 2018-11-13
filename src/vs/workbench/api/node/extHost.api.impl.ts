0c4395c375ae3104820ca1b6f1a7147f4472d39b
Espirate <502554248@qq.com>
2018-11-13 10:52:48 +0800

    delete proposedApiFunction of createTerminalRenderer
    delete proposedApiFunction of createTerminalRenderer

--- src/vs/workbench/api/node/extHost.api.impl.ts
+++ src/vs/workbench/api/node/extHost.api.impl.ts
@@ -468,9 +468,9 @@ export function createApiFactory(
 				}
 				return extHostTerminalService.createTerminal(<string>nameOrOptions, shellPath, shellArgs);
 			},
-			createTerminalRenderer: proposedApiFunction(extension, (name: string) => {
+			createTerminalRenderer: (name: string) => {
 				return extHostTerminalService.createTerminalRenderer(name);
-			}),
+			},
 			registerTreeDataProvider(viewId: string, treeDataProvider: vscode.TreeDataProvider<any>): vscode.Disposable {
 				return extHostTreeViews.registerTreeDataProvider(viewId, treeDataProvider, extension);
 			},
