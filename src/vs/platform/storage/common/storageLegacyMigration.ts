c33ff32cb1c317c1ddbf7a08da175f9ae883948b
Benjamin Pasero <benjpas@microsoft.com>
2018-11-19 12:05:33 +0100

    perf - make migration of empty, root and no workspace easier

--- src/vs/platform/storage/common/storageLegacyMigration.ts
+++ src/vs/platform/storage/common/storageLegacyMigration.ts
@@ -5,7 +5,6 @@
 
 import { StorageLegacyService, IStorageLegacy } from 'vs/platform/storage/common/storageLegacyService';
 import { endsWith, startsWith, rtrim } from 'vs/base/common/strings';
-import { URI } from 'vs/base/common/uri';
 
 /**
  * We currently store local storage with the following format:
@@ -28,8 +27,7 @@ import { URI } from 'vs/base/common/uri';
  * => no longer being used (used for empty workspaces previously)
  */
 
-const EMPTY_WORKSPACE_PREFIX = `${StorageLegacyService.COMMON_PREFIX}workspace/empty:`;
-const MULTI_ROOT_WORKSPACE_PREFIX = `${StorageLegacyService.COMMON_PREFIX}workspace/root:`;
+const COMMON_WORKSPACE_PREFIX = `${StorageLegacyService.COMMON_PREFIX}workspace/`;
 const NO_WORKSPACE_PREFIX = 'storage://workspace/__$noWorkspace__';
 
 export type StorageObject = { [key: string]: string };
@@ -108,19 +106,17 @@ export function parseFolderStorage(storage: IStorageLegacy, folderId: string): S
 	return folderWorkspaceStorage;
 }
 
-const noWorkspacePrefix = `${StorageLegacyService.WORKSPACE_PREFIX}__$noWorkspace__`;
-
 export function parseNoWorkspaceStorage(storage: IStorageLegacy) {
+	const noWorkspacePrefix = `${StorageLegacyService.WORKSPACE_PREFIX}__$noWorkspace__`;
+
 	const noWorkspaceStorage: StorageObject = Object.create(null);
 	for (let i = 0; i < storage.length; i++) {
 		const key = storage.key(i);
 
 		// No Workspace key is for extension development windows
-		if (startsWith(key, noWorkspacePrefix)) {
+		if (startsWith(key, noWorkspacePrefix) && !endsWith(key, StorageLegacyService.WORKSPACE_IDENTIFIER)) {
 			// storage://workspace/__$noWorkspace__someKey => someKey
-			const noWorkspaceStorageKey = key.substr(NO_WORKSPACE_PREFIX.length);
-
-			noWorkspaceStorage[noWorkspaceStorageKey] = storage.getItem(key);
+			noWorkspaceStorage[key.substr(NO_WORKSPACE_PREFIX.length)] = storage.getItem(key);
 		}
 	}
 
@@ -128,21 +124,15 @@ export function parseNoWorkspaceStorage(storage: IStorageLegacy) {
 }
 
 export function parseEmptyStorage(storage: IStorageLegacy, targetWorkspaceId: string): StorageObject {
+	const emptyStoragePrefix = `${COMMON_WORKSPACE_PREFIX}${targetWorkspaceId}/`;
+
 	const emptyWorkspaceStorage: StorageObject = Object.create(null);
 	for (let i = 0; i < storage.length; i++) {
 		const key = storage.key(i);
 
-		if (startsWith(key, EMPTY_WORKSPACE_PREFIX) && !endsWith(key, StorageLegacyService.WORKSPACE_IDENTIFIER)) {
-			// storage://workspace/empty:<id>/<key> => <id>
-			const emptyWorkspaceId = key.substring(EMPTY_WORKSPACE_PREFIX.length, key.indexOf('/', EMPTY_WORKSPACE_PREFIX.length));
-			const emptyWorkspaceResource = URI.from({ path: emptyWorkspaceId, scheme: 'empty' }).toString();
-			if (emptyWorkspaceResource !== targetWorkspaceId) {
-				continue;
-			}
-
+		if (startsWith(key, emptyStoragePrefix) && !endsWith(key, StorageLegacyService.WORKSPACE_IDENTIFIER)) {
 			// storage://workspace/empty:<id>/someKey => someKey
-			const storageKey = key.substr(EMPTY_WORKSPACE_PREFIX.length + emptyWorkspaceId.length + 1 /* trailing / */);
-			emptyWorkspaceStorage[storageKey] = storage.getItem(key);
+			emptyWorkspaceStorage[key.substr(emptyStoragePrefix.length)] = storage.getItem(key);
 		}
 	}
 
@@ -150,21 +140,17 @@ export function parseEmptyStorage(storage: IStorageLegacy, targetWorkspaceId: st
 }
 
 export function parseMultiRootStorage(storage: IStorageLegacy, targetWorkspaceId: string): StorageObject {
+	const multiRootStoragePrefix = `${COMMON_WORKSPACE_PREFIX}${targetWorkspaceId}/`;
+
 	const multiRootWorkspaceStorage: StorageObject = Object.create(null);
 	for (let i = 0; i < storage.length; i++) {
 		const key = storage.key(i);
 
-		if (startsWith(key, MULTI_ROOT_WORKSPACE_PREFIX) && !endsWith(key, StorageLegacyService.WORKSPACE_IDENTIFIER)) {
-			// storage://workspace/root:<id>/<key> => <id>
-			const multiRootWorkspaceId = key.substring(MULTI_ROOT_WORKSPACE_PREFIX.length, key.indexOf('/', MULTI_ROOT_WORKSPACE_PREFIX.length));
-			const multiRootWorkspaceResource = URI.from({ path: multiRootWorkspaceId, scheme: 'root' }).toString();
-			if (multiRootWorkspaceResource !== targetWorkspaceId) {
-				continue;
-			}
+		if (startsWith(key, multiRootStoragePrefix) && !endsWith(key, StorageLegacyService.WORKSPACE_IDENTIFIER)) {
 			// storage://workspace/root:<id>/someKey => someKey
-			const storageKey = key.substr(MULTI_ROOT_WORKSPACE_PREFIX.length + multiRootWorkspaceId.length + 1 /* trailing / */);
-			multiRootWorkspaceStorage[storageKey] = storage.getItem(key);
+			multiRootWorkspaceStorage[key.substr(multiRootStoragePrefix.length)] = storage.getItem(key);
 		}
 	}
+
 	return multiRootWorkspaceStorage;
 }
