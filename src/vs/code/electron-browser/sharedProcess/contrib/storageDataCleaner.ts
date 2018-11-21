bae612824c4eb7ed880bf6c17cc1cbae794aa99c
Benjamin Pasero <benjpas@microsoft.com>
2018-11-21 09:40:31 +0100

    :lipstick:

--- src/vs/code/electron-browser/sharedProcess/contrib/storageDataCleaner.ts
+++ src/vs/code/electron-browser/sharedProcess/contrib/storageDataCleaner.ts
@@ -12,6 +12,9 @@ import { IBackupWorkspacesFormat } from 'vs/platform/backup/common/backup';
 
 export class StorageDataCleaner extends Disposable {
 
+	// Workspace/Folder storage names are MD5 hashes (128bits / 4 due to hex presentation)
+	private static NON_EMPTY_WORKSPACE_ID_LENGTH = 128 / 4;
+
 	constructor(
 		@IEnvironmentService private environmentService: IEnvironmentService
 	) {
@@ -24,31 +27,30 @@ export class StorageDataCleaner extends Disposable {
 		let handle = setTimeout(() => {
 			handle = void 0;
 
-			// Leverage the backup workspace file to find out
-			// which empty workspace is currently in use
+			// Leverage the backup workspace file to find out which empty workspace is currently in use to
+			// determine which empty workspace storage can safely be deleted
 			readFile(this.environmentService.backupWorkspacesPath, 'utf8').then(contents => {
 				const workspaces = JSON.parse(contents) as IBackupWorkspacesFormat;
 				const emptyWorkspaces = workspaces.emptyWorkspaceInfos.map(info => info.backupFolder);
 
 				// Read all workspace storage folders that exist
-				return readdir(this.environmentService.workspaceStorageHome).then(folders => {
-
+				return readdir(this.environmentService.workspaceStorageHome).then(storageFolders => {
 					const deletes: Promise<void>[] = [];
 
-					folders.forEach(folder => {
-						if (folder.length === 32) {
-							return; // folders and workspaces are stored with a MD5 ID that has a length of 32
+					storageFolders.forEach(storageFolder => {
+						if (storageFolder.length === StorageDataCleaner.NON_EMPTY_WORKSPACE_ID_LENGTH) {
+							return;
 						}
 
-						if (emptyWorkspaces.indexOf(folder) === -1) {
-							deletes.push(rimraf(join(this.environmentService.workspaceStorageHome, folder)));
+						if (emptyWorkspaces.indexOf(storageFolder) === -1) {
+							deletes.push(rimraf(join(this.environmentService.workspaceStorageHome, storageFolder)));
 						}
 					});
 
 					return Promise.all(deletes);
 				});
 			}).then(null, onUnexpectedError);
-		}, 10 * 1000);
+		}, 30 * 1000);
 
 		this._register(toDisposable(() => clearTimeout(handle)));
 	}
