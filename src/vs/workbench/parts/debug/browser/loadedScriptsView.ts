dc518b7ce02f4e3f8a7034f74dd53e8a26bfc100
isidor <inikolic@microsoft.com>
2018-11-28 15:47:38 +0100

    comment out identityProvider due to build error

--- src/vs/workbench/parts/debug/browser/loadedScriptsView.ts
+++ src/vs/workbench/parts/debug/browser/loadedScriptsView.ts
@@ -397,7 +397,7 @@ export class LoadedScriptsView extends ViewletPanel {
 			{
 				accessibilityProvider: new LoadedSciptsAccessibilityProvider(),
 				ariaLabel: nls.localize({ comment: ['Debug is a noun in this context, not a verb.'], key: 'loadedScriptsAriaLabel' }, "Debug Loaded Scripts"),
-				identityProvider: element => element.getId()
+				// identityProvider: element => element.getId()
 			},
 			this.contextKeyService, this.listService, this.themeService, this.configurationService
 		);
