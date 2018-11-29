7cf95be4e35f5ea545d41ba70fc767e36bf04d5f
Tiago Ribeiro <tiago7b27@gmail.com>
2018-11-29 20:34:22 +0000

    description of setting fixed

--- src/vs/editor/common/config/commonEditorConfig.ts
+++ src/vs/editor/common/config/commonEditorConfig.ts
@@ -523,7 +523,7 @@ const editorConfiguration: IConfigurationNode = {
 		'editor.autoClosingExit': {
 			'type': 'boolean',
 			'default': EDITOR_DEFAULTS.autoClosingExit,
-			'description': nls.localize('autoClosingExit', "Controls whether the editor should automatically exit [],{} or () when the user types ;")
+			'description': nls.localize('autoClosingExit', "Controls whether the editor should automatically close and exit an open bracket or quote when the user types semi-colon.")
 		},
 		'editor.autoSurround': {
 			type: 'string',
