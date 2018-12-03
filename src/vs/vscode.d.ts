7150d4b3a1b8e6909ed2f2c70ef8d548528560eb
pkoushik <pkoushik@akamai.com>
2018-12-03 21:46:37 +0530

    fix-64041 - Changed ParameterInformation constructor - Changed @param label description

--- src/vs/vscode.d.ts
+++ src/vs/vscode.d.ts
@@ -3061,7 +3061,7 @@ declare module 'vscode' {
 		/**
 		 * Creates a new parameter information object.
 		 *
-		 * @param label A label string.
+		 * @param label A label string or inclusive start and exclusive end offsets within its containing.
 		 * @param documentation A doc string.
 		 */
 		constructor(label: string | [number, number], documentation?: string | MarkdownString);
