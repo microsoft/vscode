6f9b2d7593bb266299a5b140f0923e4a3ac17d2f
Rob Lourens <roblourens@gmail.com>
2018-12-03 16:19:26 -0800

    Fix #59922 - changes needed to support a cache in a  FileSearchProvider

--- src/vs/vscode.proposed.d.ts
+++ src/vs/vscode.proposed.d.ts
@@ -209,6 +209,12 @@ declare module 'vscode' {
 		 * The maximum number of results to be returned.
 		 */
 		maxResults?: number;
+
+		/**
+		 * A CancellationToken that represents the session for this search query. If the provider chooses to, this object can be used as the key for a cache,
+		 * and searches with the same session object can search the same cache. When the token is cancelled, the session is complete and the cache can be cleared.
+		 */
+		session?: CancellationToken;
 	}
 
 	/**
