438a21adbb27dde76d6f34ab1f22c2e8dbfd90a3
al <usernamehw@gmail.com>
2018-12-07 00:36:24 +0300

    Just wanted retrigger the build

--- src/vs/workbench/parts/codeEditor/electron-browser/textMate/inspectTMScopes.ts
+++ src/vs/workbench/parts/codeEditor/electron-browser/textMate/inspectTMScopes.ts
@@ -266,7 +266,7 @@ class InspectTMScopesWidget extends Disposable implements IContentWidget {
 		result += `<tr><td class="tm-metadata-key">font style</td><td class="tm-metadata-value">${this._fontStyleToString(metadata.fontStyle)}</td></tr>`;
 		result += `<tr><td class="tm-metadata-key">foreground</td><td class="tm-metadata-value">${Color.Format.CSS.formatHexA(metadata.foreground)}</td></tr>`;
 		result += `<tr><td class="tm-metadata-key">background</td><td class="tm-metadata-value">${Color.Format.CSS.formatHexA(metadata.background)}</td></tr>`;
-		if (metadata.background.rgba.a === 1 && metadata.foreground.rgba.a === 1) {
+		if (metadata.background.isOpaque() && metadata.foreground.isOpaque()) {
 			result += `<tr><td class="tm-metadata-key">contrast ratio</td><td class="tm-metadata-value">${metadata.background.getContrastRatio(metadata.foreground).toFixed(2)}</td></tr>`;
 		} else {
 			result += '<tr><td class="tm-metadata-key">Contrast ratio cannot be precise for colors that use transparency</td><td class="tm-metadata-value"></td></tr>';
