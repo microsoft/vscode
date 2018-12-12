6fb75b9a7de9576c1591b4d29b01799427a1929e
Matt Bierner <matb@microsoft.com>
2018-12-11 21:00:26 -0800

    Strict null check updateService.win32

--- src/vs/platform/update/electron-main/updateService.win32.ts
+++ src/vs/platform/update/electron-main/updateService.win32.ts
@@ -55,7 +55,7 @@ export class Win32UpdateService extends AbstractUpdateService {
 	@memoize
 	get cachePath(): Thenable<string> {
 		const result = path.join(tmpdir(), `vscode-update-${product.target}-${process.arch}`);
-		return pfs.mkdirp(result, null).then(() => result);
+		return pfs.mkdirp(result, void 0).then(() => result);
 	}
 
 	constructor(
@@ -143,7 +143,7 @@ export class Win32UpdateService extends AbstractUpdateService {
 
 							return this.requestService.request({ url }, CancellationToken.None)
 								.then(context => download(downloadPath, context))
-								.then(hash ? () => checksum(downloadPath, update.hash) : () => null)
+								.then(hash ? () => checksum(downloadPath, update.hash) : () => void 0)
 								.then(() => pfs.rename(downloadPath, updatePackagePath))
 								.then(() => updatePackagePath);
 						});
@@ -180,7 +180,9 @@ export class Win32UpdateService extends AbstractUpdateService {
 	}
 
 	protected async doDownloadUpdate(state: AvailableForDownload): Promise<void> {
-		shell.openExternal(state.update.url);
+		if (state.update.url) {
+			shell.openExternal(state.update.url);
+		}
 		this.setState(State.Idle(getUpdateType()));
 	}
 
@@ -208,11 +210,11 @@ export class Win32UpdateService extends AbstractUpdateService {
 
 	protected async doApplyUpdate(): Promise<void> {
 		if (this.state.type !== StateType.Downloaded && this.state.type !== StateType.Downloading) {
-			return Promise.resolve(null);
+			return Promise.resolve(void 0);
 		}
 
 		if (!this.availableUpdate) {
-			return Promise.resolve(null);
+			return Promise.resolve(void 0);
 		}
 
 		const update = this.state.update;
@@ -243,7 +245,7 @@ export class Win32UpdateService extends AbstractUpdateService {
 	}
 
 	protected doQuitAndInstall(): void {
-		if (this.state.type !== StateType.Ready) {
+		if (this.state.type !== StateType.Ready || !this.availableUpdate) {
 			return;
 		}
 
