46859507daf1fba1e178cef5cd2cd1326014a84d
Matt Bierner <matb@microsoft.com>
2018-12-17 17:12:08 -0800

    Update api to avoid casts in tests

--- src/vs/editor/contrib/snippet/test/snippetParser.test.ts
+++ src/vs/editor/contrib/snippet/test/snippetParser.test.ts
@@ -657,17 +657,17 @@ suite('SnippetParser', () => {
 		assert.equal(new FormatString(1, 'notKnown').resolve('input'), 'input');
 
 		// if
-		assert.equal(new FormatString(1, undefined, 'foo', undefined).resolve((undefined as any as string)), '');
+		assert.equal(new FormatString(1, undefined, 'foo', undefined).resolve(undefined), '');
 		assert.equal(new FormatString(1, undefined, 'foo', undefined).resolve(''), '');
 		assert.equal(new FormatString(1, undefined, 'foo', undefined).resolve('bar'), 'foo');
 
 		// else
-		assert.equal(new FormatString(1, undefined, undefined, 'foo').resolve((undefined as any as string)), 'foo');
+		assert.equal(new FormatString(1, undefined, undefined, 'foo').resolve(undefined), 'foo');
 		assert.equal(new FormatString(1, undefined, undefined, 'foo').resolve(''), 'foo');
 		assert.equal(new FormatString(1, undefined, undefined, 'foo').resolve('bar'), 'bar');
 
 		// if-else
-		assert.equal(new FormatString(1, undefined, 'bar', 'foo').resolve((undefined as any as string)), 'foo');
+		assert.equal(new FormatString(1, undefined, 'bar', 'foo').resolve(undefined), 'foo');
 		assert.equal(new FormatString(1, undefined, 'bar', 'foo').resolve(''), 'foo');
 		assert.equal(new FormatString(1, undefined, 'bar', 'foo').resolve('baz'), 'bar');
 	});
