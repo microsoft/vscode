b6f4a61509835ba22f2c598276d21898158d1b3c
Adrian Fernandez Garcia <adrifer@microsoft.com>
2018-12-18 19:43:04 -0800

    Add type to the other array in array.tests.ts

--- src/vs/base/test/common/arrays.test.ts
+++ src/vs/base/test/common/arrays.test.ts
@@ -262,7 +262,7 @@ suite('Arrays', () => {
 	}
 
 	test('coalesce', () => {
-		let a = arrays.coalesce([null, 1, null, 2, 3]);
+		let a: Array<number | undefined | null> = arrays.coalesce([null, 1, null, 2, 3]);
 		assert.equal(a.length, 3);
 		assert.equal(a[0], 1);
 		assert.equal(a[1], 2);
