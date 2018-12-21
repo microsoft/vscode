8151464a404796794f94aac40b5646b73ea45d22
isidor <inikolic@microsoft.com>
2018-12-21 11:34:53 +0100

    fix multi selection context computation to work with new tree

--- src/vs/workbench/parts/files/browser/files.ts
+++ src/vs/workbench/parts/files/browser/files.ts
@@ -4,10 +4,9 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { URI } from 'vs/base/common/uri';
-import { IListService } from 'vs/platform/list/browser/listService';
+import { IListService, WorkbenchAsyncDataTree } from 'vs/platform/list/browser/listService';
 import { OpenEditor } from 'vs/workbench/parts/files/common/files';
 import { toResource } from 'vs/workbench/common/editor';
-import { Tree } from 'vs/base/parts/tree/browser/treeImpl';
 import { List } from 'vs/base/browser/ui/list/listWidget';
 import { IEditorService } from 'vs/workbench/services/editor/common/editorService';
 import { ExplorerItem } from 'vs/workbench/parts/files/common/explorerModel';
@@ -45,7 +44,7 @@ export function getMultiSelectedResources(resource: URI | object, listService: I
 	const list = listService.lastFocusedList;
 	if (list && list.getHTMLElement() === document.activeElement) {
 		// Explorer
-		if (list instanceof Tree) {
+		if (list instanceof WorkbenchAsyncDataTree) {
 			const selection = list.getSelection().map((fs: ExplorerItem) => fs.resource);
 			const focus = list.getFocus();
 			const mainUriStr = URI.isUri(resource) ? resource.toString() : focus instanceof ExplorerItem ? focus.resource.toString() : undefined;
