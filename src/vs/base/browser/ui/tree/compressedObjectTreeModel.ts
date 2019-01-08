90e98c173afab8a79ce5a2989d9baefb3924a8f6
isidor <inikolic@microsoft.com>
2019-01-08 16:51:36 +0100

    compressionMap first steps

--- src/vs/base/browser/ui/tree/compressedObjectTreeModel.ts
+++ src/vs/base/browser/ui/tree/compressedObjectTreeModel.ts
@@ -8,14 +8,18 @@ import { Iterator, ISequence } from 'vs/base/common/iterator';
 import { Event } from 'vs/base/common/event';
 import { ITreeModel, ITreeNode, ITreeElement, ICollapseStateChangeEvent } from 'vs/base/browser/ui/tree/tree';
 import { IObjectTreeModelOptions, ObjectTreeModel } from 'vs/base/browser/ui/tree/objectTreeModel';
+import { isUndefinedOrNull } from 'vs/base/common/types';
 
 export interface ICompressedObjectTreeModelOptions<T, TFilterData> extends IObjectTreeModelOptions<T[], TFilterData> { }
 
 export class CompressedObjectTreeModel<T extends NonNullable<any>, TFilterData extends NonNullable<any> = void> implements ITreeModel<T | null, TFilterData, T | null> {
 
-	readonly rootRef = null;
-
 	private model: ObjectTreeModel<T[], TFilterData>;
+	private compressionMap: Map<T, T[]> = new Map<T, T[]>();
+
+	get rootRef(): any {
+		return this.model.rootRef;
+	}
 
 	get onDidChangeCollapseState(): Event<ICollapseStateChangeEvent<T, TFilterData>> {
 		throw new Error('not implemented');
@@ -43,31 +47,46 @@ export class CompressedObjectTreeModel<T extends NonNullable<any>, TFilterData e
 	}
 
 	getParentElement(ref: T | null = null): T | null {
-		throw new Error('not implemented');
+		const parentElements = this.model.getParentElement(this.compressionMap.get(ref));
+		if (isUndefinedOrNull(parentElements)) {
+			return parentElements;
+		}
+
+		return parentElements[parentElements.length - 1];
 	}
 
 	getFirstElementChild(ref: T | null = null): T | null | undefined {
-		throw new Error('not implemented');
+		const childElements = this.model.getFirstElementChild(this.compressionMap.get(ref));
+		if (isUndefinedOrNull(childElements)) {
+			return childElements;
+		}
+
+		return childElements[childElements.length - 1];
 	}
 
 	getLastElementAncestor(ref: T | null = null): T | null | undefined {
-		throw new Error('not implemented');
+		const ancestors = this.model.getLastElementAncestor(this.compressionMap.get(ref));
+		if (isUndefinedOrNull(ancestors)) {
+			return ancestors;
+		}
+
+		return ancestors[ancestors.length - 1];
 	}
 
 	getListIndex(element: T): number {
-		throw new Error('not implemented');
+		return this.model.getListIndex(this.compressionMap.get(element));
 	}
 
 	isCollapsible(element: T): boolean {
-		throw new Error('not implemented');
+		return this.model.isCollapsible(this.compressionMap.get(element));
 	}
 
 	isCollapsed(element: T): boolean {
-		throw new Error('not implemented');
+		return this.model.isCollapsed(this.compressionMap.get(element));
 	}
 
 	setCollapsed(element: T, collapsed?: boolean, recursive?: boolean): boolean {
-		throw new Error('not implemented');
+		return this.model.setCollapsed(this.compressionMap.get(element), collapsed, recursive);
 	}
 
 	getNode(element: T | null = null): ITreeNode<T | null, TFilterData> {
@@ -79,7 +98,12 @@ export class CompressedObjectTreeModel<T extends NonNullable<any>, TFilterData e
 	}
 
 	getParentNodeLocation(element: T): T | null {
-		throw new Error('not implemented');
+		const parentNodes = this.model.getParentNodeLocation(this.compressionMap.get(element));
+		if (isUndefinedOrNull(parentNodes)) {
+			return parentNodes;
+		}
+
+		return parentNodes[parentNodes.length - 1];
 	}
 
 	refilter(): void {
