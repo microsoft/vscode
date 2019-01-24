75c7e86ffae78adf25a6c9be22d3fb4d4671e199
SteVen Batten <stbatt@microsoft.com>
2019-01-24 00:10:32 +0000

    fix formatting of setting

--- src/vs/workbench/electron-browser/main.contribution.ts
+++ src/vs/workbench/electron-browser/main.contribution.ts
@@ -826,7 +826,7 @@ configurationRegistry.registerConfiguration({
 			'type': 'boolean',
 			'default': false,
 			'scope': ConfigurationScope.APPLICATION,
-			'description': nls.localize('window.doubleClickIconToClose', "If enabled, double clicking the application icon in the title bar will close the window and the window cannot be dragged by the icon. This setting only has an effect when `#window.titleBarStyle#` is set to `custom`.")
+			'markdownDescription': nls.localize('window.doubleClickIconToClose', "If enabled, double clicking the application icon in the title bar will close the window and the window cannot be dragged by the icon. This setting only has an effect when `#window.titleBarStyle#` is set to `custom`.")
 		},
 		'window.titleBarStyle': {
 			'type': 'string',
