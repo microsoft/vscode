f93786c3d5832797003f979780b3b2d209950092
Matt Bierner <matb@microsoft.com>
2019-01-28 14:30:20 -0800

    Prevent double dispatch of copy/paste commands in webviews on platforms where we don't need workaround

--- src/vs/workbench/parts/webview/electron-browser/webview.contribution.ts
+++ src/vs/workbench/parts/webview/electron-browser/webview.contribution.ts
@@ -21,6 +21,7 @@ import { WebviewEditor } from './webviewEditor';
 import { WebviewEditorInput } from './webviewEditorInput';
 import { IWebviewEditorService, WebviewEditorService } from './webviewEditorService';
 import { InputFocusedContextKey } from 'vs/platform/workbench/common/contextkeys';
+import { isMacintosh } from 'vs/base/common/platform';
 
 (Registry.as<IEditorRegistry>(EditorExtensions.Editors)).registerEditor(new EditorDescriptor(
 	WebviewEditor,
@@ -72,53 +73,56 @@ export function registerWebViewCommands(editorId: string): void {
 		}
 	})).register();
 
-	(new CopyWebviewEditorCommand({
-		id: CopyWebviewEditorCommand.ID,
-		precondition: ContextKeyExpr.and(contextKeyExpr, ContextKeyExpr.not(InputFocusedContextKey)),
-		kbOpts: {
-			primary: KeyMod.CtrlCmd | KeyCode.KEY_C,
-			weight: KeybindingWeight.EditorContrib
-		}
-	})).register();
+	// These commands are only needed on MacOS where we have to disable the menu bar commands
+	if (isMacintosh) {
+		(new CopyWebviewEditorCommand({
+			id: CopyWebviewEditorCommand.ID,
+			precondition: ContextKeyExpr.and(contextKeyExpr, ContextKeyExpr.not(InputFocusedContextKey)),
+			kbOpts: {
+				primary: KeyMod.CtrlCmd | KeyCode.KEY_C,
+				weight: KeybindingWeight.EditorContrib
+			}
+		})).register();
 
-	(new PasteWebviewEditorCommand({
-		id: PasteWebviewEditorCommand.ID,
-		precondition: ContextKeyExpr.and(contextKeyExpr, ContextKeyExpr.not(InputFocusedContextKey)),
-		kbOpts: {
-			primary: KeyMod.CtrlCmd | KeyCode.KEY_V,
-			weight: KeybindingWeight.EditorContrib
-		}
-	})).register();
+		(new PasteWebviewEditorCommand({
+			id: PasteWebviewEditorCommand.ID,
+			precondition: ContextKeyExpr.and(contextKeyExpr, ContextKeyExpr.not(InputFocusedContextKey)),
+			kbOpts: {
+				primary: KeyMod.CtrlCmd | KeyCode.KEY_V,
+				weight: KeybindingWeight.EditorContrib
+			}
+		})).register();
 
 
-	(new CutWebviewEditorCommand({
-		id: CutWebviewEditorCommand.ID,
-		precondition: ContextKeyExpr.and(contextKeyExpr, ContextKeyExpr.not(InputFocusedContextKey)),
-		kbOpts: {
-			primary: KeyMod.CtrlCmd | KeyCode.KEY_X,
-			weight: KeybindingWeight.EditorContrib
-		}
-	})).register();
+		(new CutWebviewEditorCommand({
+			id: CutWebviewEditorCommand.ID,
+			precondition: ContextKeyExpr.and(contextKeyExpr, ContextKeyExpr.not(InputFocusedContextKey)),
+			kbOpts: {
+				primary: KeyMod.CtrlCmd | KeyCode.KEY_X,
+				weight: KeybindingWeight.EditorContrib
+			}
+		})).register();
 
-	(new UndoWebviewEditorCommand({
-		id: UndoWebviewEditorCommand.ID,
-		precondition: ContextKeyExpr.and(contextKeyExpr, ContextKeyExpr.not(InputFocusedContextKey)),
-		kbOpts: {
-			primary: KeyMod.CtrlCmd | KeyCode.KEY_Z,
-			weight: KeybindingWeight.EditorContrib
-		}
-	})).register();
+		(new UndoWebviewEditorCommand({
+			id: UndoWebviewEditorCommand.ID,
+			precondition: ContextKeyExpr.and(contextKeyExpr, ContextKeyExpr.not(InputFocusedContextKey)),
+			kbOpts: {
+				primary: KeyMod.CtrlCmd | KeyCode.KEY_Z,
+				weight: KeybindingWeight.EditorContrib
+			}
+		})).register();
 
-	(new RedoWebviewEditorCommand({
-		id: RedoWebviewEditorCommand.ID,
-		precondition: ContextKeyExpr.and(contextKeyExpr, ContextKeyExpr.not(InputFocusedContextKey)),
-		kbOpts: {
-			primary: KeyMod.CtrlCmd | KeyCode.KEY_Y,
-			secondary: [KeyMod.CtrlCmd | KeyMod.Shift | KeyCode.KEY_Z],
-			mac: { primary: KeyMod.CtrlCmd | KeyMod.Shift | KeyCode.KEY_Z },
-			weight: KeybindingWeight.EditorContrib
-		}
-	})).register();
+		(new RedoWebviewEditorCommand({
+			id: RedoWebviewEditorCommand.ID,
+			precondition: ContextKeyExpr.and(contextKeyExpr, ContextKeyExpr.not(InputFocusedContextKey)),
+			kbOpts: {
+				primary: KeyMod.CtrlCmd | KeyCode.KEY_Y,
+				secondary: [KeyMod.CtrlCmd | KeyMod.Shift | KeyCode.KEY_Z],
+				mac: { primary: KeyMod.CtrlCmd | KeyMod.Shift | KeyCode.KEY_Z },
+				weight: KeybindingWeight.EditorContrib
+			}
+		})).register();
+	}
 }
 
 registerWebViewCommands(WebviewEditor.ID);
