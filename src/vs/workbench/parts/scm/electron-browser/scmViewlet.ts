07ab562666e3044feaab5a072948009257e5874b
Joao Moreno <joao.moreno@microsoft.com>
2019-01-30 17:17:21 +0100

    fixes #67378

--- src/vs/workbench/parts/scm/electron-browser/scmViewlet.ts
+++ src/vs/workbench/parts/scm/electron-browser/scmViewlet.ts
@@ -143,6 +143,9 @@ class ProviderRenderer implements IListRenderer<ISCMRepository, RepositoryTempla
 
 	readonly templateId = 'provider';
 
+	private _onDidRenderElement = new Emitter<ISCMRepository>();
+	readonly onDidRenderElement = this._onDidRenderElement.event;
+
 	constructor(
 		@ICommandService protected commandService: ICommandService,
 		@IThemeService protected themeService: IThemeService
@@ -197,6 +200,8 @@ class ProviderRenderer implements IListRenderer<ISCMRepository, RepositoryTempla
 			const count = repository.provider.count || 0;
 			toggleClass(templateData.countContainer, 'hidden', count === 0);
 			templateData.count.setCount(repository.provider.count);
+
+			this._onDidRenderElement.fire(repository);
 		};
 
 		repository.provider.onDidChange(update, null, disposables);
@@ -260,8 +265,9 @@ class MainPanel extends ViewletPanel {
 		const renderer = this.instantiationService.createInstance(ProviderRenderer);
 		const identityProvider = { getId: r => r.provider.id };
 
-		this.list = this.instantiationService.createInstance(WorkbenchList, container, delegate, [renderer], { identityProvider, horizontalScrolling: false }) as WorkbenchList<ISCMRepository>;
+		this.list = this.instantiationService.createInstance(WorkbenchList, container, delegate, [renderer], { identityProvider }) as WorkbenchList<ISCMRepository>;
 
+		renderer.onDidRenderElement(e => this.list.updateWidth(this.viewModel.repositories.indexOf(e)), null, this.disposables);
 		this.list.onSelectionChange(this.onListSelectionChange, this, this.disposables);
 		this.list.onContextMenu(this.onListContextMenu, this, this.disposables);
 
@@ -293,8 +299,8 @@ class MainPanel extends ViewletPanel {
 		}
 	}
 
-	protected layoutBody(size: number): void {
-		this.list.layout(size);
+	protected layoutBody(height: number, width: number): void {
+		this.list.layout(height, width);
 	}
 
 	private updateBodySize(): void {
