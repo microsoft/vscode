ea541de9c470f1f31d5d8b8034587d8b72c4daf9
Matt Bierner <matb@microsoft.com>
2019-02-01 14:46:33 -0800

    Remove old css vars from webviews
    These were undocumented and we now had offical css vars that extensions should use instead

--- src/vs/workbench/parts/webview/electron-browser/webviewElement.ts
+++ src/vs/workbench/parts/webview/electron-browser/webviewElement.ts
@@ -475,16 +475,6 @@ export class WebviewElement extends Disposable {
 
 
 		const styles = {
-			// Old vars
-			'font-family': fontFamily,
-			'font-weight': fontWeight,
-			'font-size': fontSize,
-			'background-color': theme.getColor(colorRegistry.editorBackground).toString(),
-			'color': theme.getColor(colorRegistry.editorForeground).toString(),
-			'link-color': theme.getColor(colorRegistry.textLinkForeground).toString(),
-			'link-active-color': theme.getColor(colorRegistry.textLinkActiveForeground).toString(),
-
-			// Offical API
 			'vscode-editor-font-family': fontFamily,
 			'vscode-editor-font-weight': fontWeight,
 			'vscode-editor-font-size': fontSize,
@@ -495,6 +485,7 @@ export class WebviewElement extends Disposable {
 		this._send('styles', styles, activeTheme);
 
 		this._webviewFindWidget.updateTheme(theme);
+
 	}
 
 	public layout(): void {
