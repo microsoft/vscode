3c2c17dd90bbb0200f238a4ee3542c280deb2ac5
isidor <inikolic@microsoft.com>
2019-02-01 12:28:38 +0100

    fixes #67708 fixes #67696

--- src/vs/workbench/parts/files/electron-browser/views/explorerView.ts
+++ src/vs/workbench/parts/files/electron-browser/views/explorerView.ts
@@ -233,6 +233,20 @@ export class ExplorerView extends ViewletPanel {
 
 	focus(): void {
 		this.tree.domFocus();
+
+		const focused = this.tree.getFocus();
+		if (focused.length === 1) {
+			if (this.autoReveal) {
+				this.tree.reveal(focused[0], 0.5);
+			}
+
+			const activeFile = this.getActiveFile();
+			if (!activeFile && !focused[0].isDirectory) {
+				// Open the focused element in the editor if there is currently no file opened #67708
+				this.editorService.openEditor({ resource: focused[0].resource, options: { preserveFocus: true, revealIfVisible: true } })
+					.then(undefined, onUnexpectedError);
+			}
+		}
 	}
 
 	private selectActiveFile(reveal?: boolean): void {
