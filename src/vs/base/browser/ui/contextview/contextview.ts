21ebf54586e17e997456a3b5379573e405d4228f
SteVen Batten <stbatt@microsoft.com>
2019-02-07 16:34:55 +0100

    move logic into context menu handler

--- src/vs/base/browser/ui/contextview/contextview.ts
+++ src/vs/base/browser/ui/contextview/contextview.ts
@@ -102,7 +102,6 @@ export class ContextView extends Disposable {
 
 	private container: HTMLElement | null;
 	private view: HTMLElement;
-	private block: HTMLElement | null;
 	private delegate: IDelegate | null;
 	private toDisposeOnClean: IDisposable | null;
 	private toDisposeOnSetContainer: IDisposable;
@@ -161,9 +160,6 @@ export class ContextView extends Disposable {
 
 		// Render content
 		this.toDisposeOnClean = delegate.render(this.view);
-
-		// Render invisible div to block mouse interaction in the rest of the UI
-		this.block = this.view.appendChild(DOM.$('.context-view-block'));
 
 		// Set active delegate
 		this.delegate = delegate;
@@ -272,11 +268,6 @@ export class ContextView extends Disposable {
 		if (this.toDisposeOnClean) {
 			this.toDisposeOnClean.dispose();
 			this.toDisposeOnClean = null;
-		}
-
-		if (this.block) {
-			this.block.remove();
-			this.block = null;
 		}
 
 		DOM.hide(this.view);
