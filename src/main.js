4ea7f234b419701c856ad8e2c5400a7b32ab219f
Mostafa Saeed <mostafasaeed483@gmail.com>
2019-02-12 19:19:49 +0200

    update main.js
    it will make the code much faster ,, we don't need to copy the any variable in an additional memory in this step 
    m = m+1 
    it means  , get m , then copy the value of it , then add 1 to the value , finally put the new value at m 
    and 
    m++
    means the same as above 
    but
    ++m means add 1 to m directly

--- src/main.js
+++ src/main.js
@@ -586,7 +586,7 @@ function getNLSConfiguration(locale) {
 									let targetStrings;
 									if (translations) {
 										targetStrings = [];
-										for (let i = 0; i < keys.length; i++) {
+										for (let i = 0; i < keys.length; ++i) {
 											const elem = keys[i];
 											const key = typeof elem === 'string' ? elem : elem.key;
 											let translatedMessage = translations[key];
