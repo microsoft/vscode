6e71414c10732f5ff052372e8bd64e94d4525423
Benjamin Pasero <benjpas@microsoft.com>
2019-02-12 20:16:52 +0100

    fix CSS import

--- src/vs/workbench/contrib/search/browser/search.contribution.ts
+++ src/vs/workbench/contrib/search/browser/search.contribution.ts
@@ -3,6 +3,8 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+import 'vs/css!./media/search.contribution';
+
 import { relative } from 'vs/base/common/paths.node';
 import { Action } from 'vs/base/common/actions';
 import { distinct } from 'vs/base/common/arrays';
@@ -13,7 +15,6 @@ import * as objects from 'vs/base/common/objects';
 import * as platform from 'vs/base/common/platform';
 import { dirname } from 'vs/base/common/resources';
 import { URI } from 'vs/base/common/uri';
-import 'vs/css!./media/search.contribution';
 import { registerLanguageCommand } from 'vs/editor/browser/editorExtensions';
 import { ICodeEditorService } from 'vs/editor/browser/services/codeEditorService';
 import { getSelectionSearchString } from 'vs/editor/contrib/find/findController';
