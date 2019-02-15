76323b1ac7821cf1e9ee4885c4339df8083d0456
Martin Aeschlimann <martinae@microsoft.com>
2019-02-15 10:06:34 +0100

    api tests for links

--- extensions/vscode-api-tests/src/utils.ts
+++ extensions/vscode-api-tests/src/utils.ts
@@ -12,9 +12,9 @@ export function rndName() {
 	return Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 10);
 }
 
-export function createRandomFile(contents = '', dir: string = os.tmpdir()): Thenable<vscode.Uri> {
+export function createRandomFile(contents = '', dir: string = os.tmpdir(), ext = ''): Thenable<vscode.Uri> {
 	return new Promise((resolve, reject) => {
-		const tmpFile = join(dir, rndName());
+		const tmpFile = join(dir, rndName() + ext);
 		fs.writeFile(tmpFile, contents, (error) => {
 			if (error) {
 				return reject(error);
