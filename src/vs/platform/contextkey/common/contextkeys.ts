9e3b5312d76f3c06251cd033681b1123e71181f0
Benjamin Pasero <benjpas@microsoft.com>
2019-02-20 11:41:45 +0100

    debt - move workbench action registration out to contrib

--- src/vs/platform/contextkey/common/contextkeys.ts
+++ src/vs/platform/contextkey/common/contextkeys.ts
@@ -8,9 +8,14 @@ import { isMacintosh, isLinux, isWindows } from 'vs/base/common/platform';
 
 export const InputFocusedContextKey = 'inputFocus';
 export const InputFocusedContext = new RawContextKey<boolean>(InputFocusedContextKey, false);
+
 export const IsMacContext = new RawContextKey<boolean>('isMac', isMacintosh);
 export const IsLinuxContext = new RawContextKey<boolean>('isLinux', isLinux);
 export const IsWindowsContext = new RawContextKey<boolean>('isWindows', isWindows);
 
+export const HasMacNativeTabsContext = new RawContextKey<boolean>('hasMacNativeTabs', false);
+
 export const SupportsWorkspacesContext = new RawContextKey<boolean>('supportsWorkspaces', true);
 export const SupportsOpenFileFolderContext = new RawContextKey<boolean>('supportsOpenFileFolder', isMacintosh);
+
+export const IsDevelopmentContext = new RawContextKey<boolean>('isDevelopment', false);
