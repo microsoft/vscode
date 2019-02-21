695537545f5b3a0516315f1aef2ebd13159f41e6
Alex Dima <alexdima@microsoft.com>
2019-02-21 11:17:24 +0100

    Move most code editor contributions to /browser/ (#69102)

--- src/vs/workbench/contrib/codeEditor/electron-browser/textMate/inspectTMScopes.ts
+++ src/vs/workbench/contrib/codeEditor/electron-browser/textMate/inspectTMScopes.ts
@@ -22,9 +22,8 @@ import { INotificationService } from 'vs/platform/notification/common/notificati
 import { editorHoverBackground, editorHoverBorder } from 'vs/platform/theme/common/colorRegistry';
 import { HIGH_CONTRAST, registerThemingParticipant } from 'vs/platform/theme/common/themeService';
 import { findMatchingThemeRule } from 'vs/workbench/services/textMate/electron-browser/TMHelper';
-import { ITextMateService } from 'vs/workbench/services/textMate/common/textMateService';
+import { ITextMateService, IGrammar, IToken, StackElement } from 'vs/workbench/services/textMate/common/textMateService';
 import { IWorkbenchThemeService } from 'vs/workbench/services/themes/common/workbenchThemeService';
-import { IGrammar, IToken, StackElement } from 'vscode-textmate';
 
 class InspectTMScopesController extends Disposable implements IEditorContribution {
 
--- src/vs/workbench/contrib/extensions/electron-browser/extensionsViewlet.ts
+++ src/vs/workbench/contrib/extensions/electron-browser/extensionsViewlet.ts
@@ -48,7 +48,7 @@ import { IPartService } from 'vs/workbench/services/part/common/partService';
 import { IAddedViewDescriptorRef } from 'vs/workbench/browser/parts/views/views';
 import { ViewletPanel } from 'vs/workbench/browser/parts/views/panelViewlet';
 import { Query } from 'vs/workbench/contrib/extensions/common/extensionQuery';
-import { SuggestEnabledInput, attachSuggestEnabledInputBoxStyler } from 'vs/workbench/contrib/codeEditor/electron-browser/suggestEnabledInput';
+import { SuggestEnabledInput, attachSuggestEnabledInputBoxStyler } from 'vs/workbench/contrib/codeEditor/browser/suggestEnabledInput/suggestEnabledInput';
 import { alert } from 'vs/base/browser/ui/aria/aria';
 import { createErrorWithActions } from 'vs/base/common/errorsWithActions';
 import { IEnvironmentService } from 'vs/platform/environment/common/environment';
