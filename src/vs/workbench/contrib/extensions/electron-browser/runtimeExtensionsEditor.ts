65a3c8a834ac3f6549cfb8211395e8b0f8422dd2
Benjamin Pasero <benjpas@microsoft.com>
2019-02-21 15:48:18 +0100

    debt - move runtime extensions input and editor close to each other

--- src/vs/workbench/contrib/extensions/electron-browser/runtimeExtensionsEditor.ts
+++ src/vs/workbench/contrib/extensions/electron-browser/runtimeExtensionsEditor.ts
@@ -32,7 +32,7 @@ import { memoize } from 'vs/base/common/decorators';
 import { isNonEmptyArray } from 'vs/base/common/arrays';
 import { Event } from 'vs/base/common/event';
 import { INotificationService } from 'vs/platform/notification/common/notification';
-import { RuntimeExtensionsInput } from 'vs/workbench/services/extensions/electron-browser/runtimeExtensionsInput';
+import { RuntimeExtensionsInput } from 'vs/workbench/contrib/extensions/electron-browser/runtimeExtensionsInput';
 import { IDebugService } from 'vs/workbench/contrib/debug/common/debug';
 import { IDialogService } from 'vs/platform/dialogs/common/dialogs';
 import { randomPort } from 'vs/base/node/ports';
