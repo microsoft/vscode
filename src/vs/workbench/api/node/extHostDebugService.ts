4215ed797c3cc009bcfe98a53903d7727432ce4c
Andre Weinand <aweinand@microsoft.com>
2019-02-21 11:40:50 +0100

    protect against undefined session; fixes #69128

--- src/vs/workbench/api/node/extHostDebugService.ts
+++ src/vs/workbench/api/node/extHostDebugService.ts
@@ -593,7 +593,11 @@ export class ExtHostDebugService implements ExtHostDebugServiceShape {
 
 	public async $acceptDebugSessionStarted(sessionDto: IDebugSessionDto): Promise<void> {
 		const session = await this.getSession(sessionDto);
-		this._onDidStartDebugSession.fire(session);
+		if (session) {
+			this._onDidStartDebugSession.fire(session);
+		} else {
+			console.error('undefined session received in acceptDebugSessionStarted');	// should not happen (but see #69128)
+		}
 	}
 
 	public async $acceptDebugSessionTerminated(sessionDto: IDebugSessionDto): Promise<void> {
