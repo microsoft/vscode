492045af81065b04c653478974b1c00706b68f02
Alex Dima <alexdima@microsoft.com>
2019-02-21 13:03:49 +0100

    Split the code editor contributions (#69102)

--- src/vs/workbench/contrib/codeEditor/electron-browser/codeEditor.contribution.ts
+++ src/vs/workbench/contrib/codeEditor/electron-browser/codeEditor.contribution.ts
@@ -3,16 +3,5 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import '../browser/menuPreventer';
-import '../browser/accessibility/accessibility';
-import '../browser/inspectKeybindings';
-import '../browser/largeFileOptimizations';
-import '../browser/selectionClipboard';
-import '../browser/inspectTMScopes/inspectTMScopes';
-import '../browser/toggleMinimap';
-import '../browser/toggleMultiCursorModifier';
-import '../browser/toggleRenderControlCharacter';
-import '../browser/toggleRenderWhitespace';
-import '../browser/toggleWordWrap';
-import '../browser/workbenchReferenceSearch';
+import '../browser/codeEditor.contribution';
 import './sleepResumeRepaintMinimap';
