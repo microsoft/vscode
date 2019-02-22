ca8749dc8e35e8e890cef7dce68f74053797eb37
Dirk Baeumer <dirkb@microsoft.com>
2019-02-22 16:33:30 +0100

    Make languagePack.js a real UMD module

--- src/vs/base/node/languagePacks.d.ts
+++ src/vs/base/node/languagePacks.d.ts
@@ -11,4 +11,13 @@ export interface NLSConfiguration {
 	pseudo?: boolean;
 }
 
+export interface InternalNLSConfiguration extends NLSConfiguration {
+	_languagePackId: string;
+	_translationsConfigFile: string;
+	_cacheRoot: string;
+	_resolvedLanguagePackCoreLocation: string;
+	_corruptedFile: string;
+	_languagePackSupport?: boolean;
+}
+
 export function getNLSConfiguration(commit: string, userDataPath: string, metaDataFile: string, locale: string): Promise<NLSConfiguration>;
