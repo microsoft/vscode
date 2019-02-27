57453375060c740573dfdc349a2aa8e399d5730c
Neeson <neesonqk@gmail.com>
2019-02-27 16:08:14 +0800

    Fixes #69240

--- extensions/git/src/commands.ts
+++ extensions/git/src/commands.ts
@@ -1213,6 +1213,7 @@ export class CommandCenter {
 
 				if (pick === saveAndCommit) {
 					await Promise.all(unsavedTextDocuments.map(d => d.save()));
+					await Promise.all(unsavedTextDocuments.map(d => repository.stage(d.uri, d.getText())));
 					await repository.status();
 				} else if (pick !== commit) {
 					return false; // do not commit on cancel
