72e339a6ce7319d94eff16a5d29f636306237eca
Rachel Macfarlane <ramacfar@microsoft.com>
2019-03-01 15:18:53 -0800

    Show only installed languages with an option to install more

--- src/vs/workbench/contrib/localizations/browser/localizationsActions.ts
+++ src/vs/workbench/contrib/localizations/browser/localizationsActions.ts
@@ -8,13 +8,15 @@ import { Action } from 'vs/base/common/actions';
 import { IEnvironmentService } from 'vs/platform/environment/common/environment';
 import { join } from 'vs/base/common/path';
 import { URI } from 'vs/base/common/uri';
-import { ILocalizationsService } from 'vs/platform/localizations/common/localizations';
+import { ILocalizationsService, LanguageType } from 'vs/platform/localizations/common/localizations';
 import { IQuickInputService, IQuickPickItem } from 'vs/platform/quickinput/common/quickInput';
 import { IJSONEditingService } from 'vs/workbench/services/configuration/common/jsonEditing';
 import { IWindowsService } from 'vs/platform/windows/common/windows';
 import { INotificationService } from 'vs/platform/notification/common/notification';
 import { language } from 'vs/base/common/platform';
 import { firstIndex } from 'vs/base/common/arrays';
+import { IExtensionsViewlet, VIEWLET_ID as EXTENSIONS_VIEWLET_ID } from 'vs/workbench/contrib/extensions/common/extensions';
+import { IViewletService } from 'vs/workbench/services/viewlet/browser/viewlet';
 
 export class ConfigureLocaleAction extends Action {
 	public static readonly ID = 'workbench.action.configureLocale';
@@ -26,19 +28,20 @@ export class ConfigureLocaleAction extends Action {
 		@IQuickInputService private readonly quickInputService: IQuickInputService,
 		@IJSONEditingService private readonly jsonEditingService: IJSONEditingService,
 		@IWindowsService private readonly windowsService: IWindowsService,
-		@INotificationService private readonly notificationService: INotificationService
+		@INotificationService private readonly notificationService: INotificationService,
+		@IViewletService private readonly viewletService: IViewletService
 	) {
 		super(id, label);
 	}
 
 	private async getLanguageOptions(): Promise<IQuickPickItem[]> {
-		return this.localizationService.getLanguageIds().then(languages => {
-			return languages.map(language => {
-				return {
-					label: language.toLowerCase()
-				};
-			});
-		});
+		// Contributed languages are those installed via extension packs, so does not include English
+		const availableLanguages = ['en', ...await this.localizationService.getLanguageIds(LanguageType.Contributed)];
+		availableLanguages.sort();
+
+		return availableLanguages
+			.map(language => { return { label: language }; })
+			.concat({ label: localize('installAdditionalLanguages', "Install additional languages...") });
 	}
 
 	public async run(event?: any): Promise<void> {
@@ -52,6 +55,14 @@ export class ConfigureLocaleAction extends Action {
 					placeHolder: localize('chooseDisplayLanguage', "Select display language. VS Code will restart to apply the change"),
 					activeItem: languageOptions[currentLanguageIndex]
 				});
+
+			if (selectedLanguage === languageOptions[languageOptions.length - 1]) {
+				return this.viewletService.openViewlet(EXTENSIONS_VIEWLET_ID, true)
+					.then(viewlet => {
+						(viewlet as IExtensionsViewlet).search('@category:"language packs"');
+						viewlet.focus();
+					});
+			}
 
 			if (selectedLanguage) {
 				const file = URI.file(join(this.environmentService.appSettingsHome, 'locale.json'));
