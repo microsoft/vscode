608035ad4951b851bb7a5497c8ab3beb22fd6570
Abby <i@abby.md>
2019-03-04 14:29:38 +0900

    Add an option to allow to open in new window after - git initializing or cloning

--- extensions/git/src/commands.ts
+++ extensions/git/src/commands.ts
@@ -3,7 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { Uri, commands, Disposable, window, workspace, QuickPickItem, OutputChannel, Range, WorkspaceEdit, Position, LineChange, SourceControlResourceState, TextDocumentShowOptions, ViewColumn, ProgressLocation, TextEditor, MessageOptions, WorkspaceFolder } from 'vscode';
+import { Uri, commands, Disposable, window, workspace, QuickPickItem, OutputChannel, Range, WorkspaceEdit, Position, LineChange, SourceControlResourceState, TextDocumentShowOptions, ViewColumn, ProgressLocation, TextEditor, MessageOptions, WorkspaceFolder, CommentThreadCollapsibleState } from 'vscode';
 import { Git, CommitOptions, Stash, ForcePushMode } from './git';
 import { Repository, Resource, ResourceGroupType } from './repository';
 import { Model } from './model';
@@ -468,13 +468,15 @@ export class CommandCenter {
 			);
 
 			const choices = [];
-			let message = localize('proposeopen', "Would you like to open the cloned repository?");
-			const open = localize('openrepo', "Open Repository");
+			let message = localize('proposeopen', "Where would you to open the cloned repository?");
+			const open = localize('openrepo', "Current Window");
+			const openNewWindow = localize('openreponew', "New Window");
 			choices.push(open);
+			choices.push(openNewWindow);
 
 			const addToWorkspace = localize('add', "Add to Workspace");
 			if (workspace.workspaceFolders) {
-				message = localize('proposeopen2', "Would you like to open the cloned repository, or add it to the current workspace?");
+				message = localize('proposeopen2', "Where would you like to open the cloned repository, or add it to the current workspace?");
 				choices.push(addToWorkspace);
 			}
 
@@ -495,6 +497,8 @@ export class CommandCenter {
 				commands.executeCommand('vscode.openFolder', uri);
 			} else if (result === addToWorkspace) {
 				workspace.updateWorkspaceFolders(workspace.workspaceFolders!.length, 0, { uri });
+			} else if (result === openNewWindow) {
+				commands.executeCommand('vscode.openFolder', uri, true);
 			}
 		} catch (err) {
 			if (/already exists and is not an empty directory/.test(err && err.stderr || '')) {
@@ -580,9 +584,11 @@ export class CommandCenter {
 		await this.git.init(repositoryPath);
 
 		const choices = [];
-		let message = localize('proposeopen init', "Would you like to open the initialized repository?");
-		const open = localize('openrepo', "Open Repository");
+		let message = localize('proposeopen init', "Where would you like to open the initialized repository?");
+		const open = localize('openrepo', "Current Window");
+		const openNewWindow = localize('openreponew', "New Window");
 		choices.push(open);
+		choices.push(openNewWindow);
 
 		if (!askToOpen) {
 			return;
@@ -590,7 +596,7 @@ export class CommandCenter {
 
 		const addToWorkspace = localize('add', "Add to Workspace");
 		if (workspace.workspaceFolders) {
-			message = localize('proposeopen2 init', "Would you like to open the initialized repository, or add it to the current workspace?");
+			message = localize('proposeopen2 init', "Where would you like to open the initialized repository, or add it to the current workspace?");
 			choices.push(addToWorkspace);
 		}
 
@@ -601,6 +607,8 @@ export class CommandCenter {
 			commands.executeCommand('vscode.openFolder', uri);
 		} else if (result === addToWorkspace) {
 			workspace.updateWorkspaceFolders(workspace.workspaceFolders!.length, 0, { uri });
+		} else if (result === openNewWindow) {
+			commands.executeCommand('vscode.openFolder', uri, true);
 		} else {
 			await this.model.openRepository(repositoryPath);
 		}
