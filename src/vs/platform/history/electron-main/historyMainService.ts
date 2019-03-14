2275bfe42848f3ddea9f907e5bb63e39d74390fb
Martin Aeschlimann <martinae@microsoft.com>
2019-03-14 17:08:36 +0100

    List of recently opened sorts weird. Fixes #70222

--- src/vs/platform/history/electron-main/historyMainService.ts
+++ src/vs/platform/history/electron-main/historyMainService.ts
@@ -48,38 +48,44 @@ export class HistoryMainService implements IHistoryMainService {
 	}
 
 	addRecentlyOpened(newlyAdded: IRecent[]): void {
-		const mru = this.getRecentlyOpened();
+		const workspaces: Array<IRecentFolder | IRecentWorkspace> = [];
+		const files: IRecentFile[] = [];
 
 		for (let curr of newlyAdded) {
 			if (isRecentWorkspace(curr)) {
-				if (!this.workspacesMainService.isUntitledWorkspace(curr.workspace) && indexOfWorkspace(mru.workspaces, curr.workspace) === -1) {
-					mru.workspaces.unshift(curr);
+				if (!this.workspacesMainService.isUntitledWorkspace(curr.workspace) && indexOfWorkspace(workspaces, curr.workspace) === -1) {
+					workspaces.push(curr);
 				}
 			} else if (isRecentFolder(curr)) {
-				if (indexOfFolder(mru.workspaces, curr.folderUri) === -1) {
-					mru.workspaces.unshift(curr);
+				if (indexOfFolder(workspaces, curr.folderUri) === -1) {
+					workspaces.push(curr);
 				}
 			} else {
-				if (indexOfFile(mru.files, curr.fileUri) === -1) {
-					mru.files.unshift(curr);
+				if (indexOfFile(files, curr.fileUri) === -1) {
+					files.push(curr);
 					// Add to recent documents (Windows only, macOS later)
 					if (isWindows && curr.fileUri.scheme === Schemas.file) {
 						app.addRecentDocument(curr.fileUri.fsPath);
 					}
 				}
 			}
+		}
 
-			// Make sure its bounded
-			mru.workspaces = mru.workspaces.slice(0, HistoryMainService.MAX_TOTAL_RECENT_ENTRIES);
-			mru.files = mru.files.slice(0, HistoryMainService.MAX_TOTAL_RECENT_ENTRIES);
+		this.addEntriesFromStorage(workspaces, files);
 
-			this.saveRecentlyOpened(mru);
-			this._onRecentlyOpenedChange.fire();
+		if (workspaces.length > HistoryMainService.MAX_TOTAL_RECENT_ENTRIES) {
+			workspaces.length = HistoryMainService.MAX_TOTAL_RECENT_ENTRIES;
+		}
+		if (files.length > HistoryMainService.MAX_TOTAL_RECENT_ENTRIES) {
+			files.length = HistoryMainService.MAX_TOTAL_RECENT_ENTRIES;
+		}
 
-			// Schedule update to recent documents on macOS dock
-			if (isMacintosh) {
-				this.macOSRecentDocumentsUpdater.trigger(() => this.updateMacOSRecentDocuments());
-			}
+		this.saveRecentlyOpened({ workspaces, files });
+		this._onRecentlyOpenedChange.fire();
+
+		// Schedule update to recent documents on macOS dock
+		if (isMacintosh) {
+			this.macOSRecentDocumentsUpdater.trigger(() => this.updateMacOSRecentDocuments());
 		}
 	}
 
@@ -177,7 +183,12 @@ export class HistoryMainService implements IHistoryMainService {
 				}
 			}
 		}
+		this.addEntriesFromStorage(workspaces, files);
 
+		return { workspaces, files };
+	}
+
+	private addEntriesFromStorage(workspaces: Array<IRecentFolder | IRecentWorkspace>, files: IRecentFile[]) {
 		// Get from storage
 		let recents = this.getRecentlyOpenedFromStorage();
 		for (let recent of recents.workspaces) {
@@ -196,7 +207,6 @@ export class HistoryMainService implements IHistoryMainService {
 				files.push(recent);
 			}
 		}
-		return { workspaces, files };
 	}
 
 	private getRecentlyOpenedFromStorage(): IRecentlyOpened {
