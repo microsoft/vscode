d932b45148b0748d0796d982dc7d2d7d4e2a6437
Alex Ross <alros@microsoft.com>
2019-03-19 12:08:31 +0100

    Fix task.contribution.css path

--- src/vs/workbench/contrib/tasks/electron-browser/task.contribution.ts
+++ src/vs/workbench/contrib/tasks/electron-browser/task.contribution.ts
@@ -3,7 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import 'vs/css!./media/task.contribution';
+import 'vs/css!../common/media/task.contribution';
 
 import * as nls from 'vs/nls';
 import * as semver from 'semver';
