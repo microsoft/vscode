fa1c88a4e1beb7a24c82d5c42e62a0b3973af12c
Rob Lourens <roblourens@gmail.com>
2019-03-22 18:10:39 +0000

    Disable extensions in smoketest

--- test/smoke/src/vscode/code.ts
+++ test/smoke/src/vscode/code.ts
@@ -113,6 +113,7 @@ export async function spawn(options: SpawnOptions): Promise<Code> {
 		'--skip-release-notes',
 		'--sticky-quickopen',
 		'--disable-telemetry',
+		'--disable-extensions',
 		'--disable-updates',
 		'--disable-crash-reporter',
 		`--extensions-dir=${options.extensionsPath}`,
