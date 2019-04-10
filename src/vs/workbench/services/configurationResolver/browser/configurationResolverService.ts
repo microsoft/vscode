2559601828efd8d7dc57f5a5bb1dd0f640922475
Alex Ross <alros@microsoft.com>
2019-04-10 17:54:23 +0200

    Use comment out tests approach

--- src/vs/workbench/services/configurationResolver/browser/configurationResolverService.ts
+++ src/vs/workbench/services/configurationResolver/browser/configurationResolverService.ts
@@ -20,6 +20,7 @@ import { isCodeEditor } from 'vs/editor/browser/editorBrowser';
 import { DiffEditorInput } from 'vs/workbench/common/editor/diffEditorInput';
 import { IQuickInputService, IInputOptions, IQuickPickItem, IPickOptions } from 'vs/platform/quickinput/common/quickInput';
 import { ConfiguredInput, IConfigurationResolverService } from 'vs/workbench/services/configurationResolver/common/configurationResolver';
+import { IWindowService } from 'vs/platform/windows/common/windows';
 import { registerSingleton } from 'vs/platform/instantiation/common/extensions';
 import { IProcessEnvironment } from 'vs/base/common/platform';
 
@@ -28,7 +29,7 @@ export class ConfigurationResolverService extends AbstractVariableResolverServic
 	static INPUT_OR_COMMAND_VARIABLES_PATTERN = /\${((input|command):(.*?))}/g;
 
 	constructor(
-		processEnvironment: IProcessEnvironment,
+		@IWindowService windowService: IWindowService,
 		@IEditorService editorService: IEditorService,
 		@IEnvironmentService environmentService: IEnvironmentService,
 		@IConfigurationService private readonly configurationService: IConfigurationService,
@@ -83,7 +84,7 @@ export class ConfigurationResolverService extends AbstractVariableResolverServic
 				}
 				return undefined;
 			}
-		}, processEnvironment ? processEnvironment : process.env as IProcessEnvironment);
+		}, process.env as IProcessEnvironment);
 	}
 
 	public resolveWithInteractionReplace(folder: IWorkspaceFolder | undefined, config: any, section?: string, variables?: IStringDictionary<string>): Promise<any> {
