38ebddbfdf7135ad2a9866ab0b40b7404c83e91b
Alex Dima <alexdima@microsoft.com>
2019-04-15 11:36:44 +0300

    Minor tweaks

--- src/vs/workbench/services/remote/common/remoteAgentService.ts
+++ src/vs/workbench/services/remote/common/remoteAgentService.ts
@@ -8,6 +8,7 @@ import { RemoteAgentConnectionContext, IRemoteAgentEnvironment } from 'vs/platfo
 import { IChannel, IServerChannel } from 'vs/base/parts/ipc/common/ipc';
 import { IDiagnosticInfoOptions, IDiagnosticInfo } from 'vs/platform/diagnostics/common/diagnosticsService';
 import { Event } from 'vs/base/common/event';
+import { PersistenConnectionEvent } from 'vs/platform/remote/common/remoteAgentConnection';
 
 export const RemoteExtensionLogFileName = 'remoteagent';
 
@@ -19,13 +20,14 @@ export interface IRemoteAgentService {
 	getConnection(): IRemoteAgentConnection | null;
 	getEnvironment(bail?: boolean): Promise<IRemoteAgentEnvironment | null>;
 	getDiagnosticInfo(options: IDiagnosticInfoOptions): Promise<IDiagnosticInfo | undefined>;
-	disableTelemetry(): Promise<void>;
 }
 
 export interface IRemoteAgentConnection {
 	readonly remoteAuthority: string;
 
+	readonly onReconnecting: Event<void>;
+	readonly onDidStateChange: Event<PersistenConnectionEvent>;
+
 	getChannel<T extends IChannel>(channelName: string): T;
 	registerChannel<T extends IServerChannel<RemoteAgentConnectionContext>>(channelName: string, channel: T): void;
-	onReconnecting: Event<void>;
 }
