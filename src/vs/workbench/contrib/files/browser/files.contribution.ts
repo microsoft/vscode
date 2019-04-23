ce74fa83bdbf20d5c038f4c10e0a0ec95df1c943
Daniel Imms <daimms@microsoft.com>
2019-04-23 11:17:02 -0700

    Set correct scope for file.hotExit setting

--- src/vs/workbench/contrib/files/browser/files.contribution.ts
+++ src/vs/workbench/contrib/files/browser/files.contribution.ts
@@ -306,6 +306,7 @@ configurationRegistry.registerConfiguration({
 		},
 		'files.hotExit': {
 			'type': 'string',
+			'scope': ConfigurationScope.APPLICATION,
 			'enum': [HotExitConfiguration.OFF, HotExitConfiguration.ON_EXIT, HotExitConfiguration.ON_EXIT_AND_WINDOW_CLOSE],
 			'default': HotExitConfiguration.ON_EXIT,
 			'markdownEnumDescriptions': [
