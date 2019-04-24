3dbb9024b44fdc9c6be3276dbaf25bd55fe7594d
kieferrm <kieferrm@gmail.com>
2019-04-24 18:49:07 +0000

    print license info

--- resources/server/bin/server.sh
+++ resources/server/bin/server.sh
@@ -3,6 +3,13 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 #
 
+echo " "
+echo "*"
+echo "* Reminder: You may only use this software with Visual Studio family products,"
+echo "* as described in the license (https://go.microsoft.com/fwlink/?linkid=2077057)"
+echo "*"
+echo " "
+
 case "$1" in
 	--inspect*) INSPECT="$1"; shift;;
 esac
