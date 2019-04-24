376d28682a67a33670191f1f380d0ce13fb5cd87
Martin Aeschlimann <martinae@microsoft.com>
2019-04-24 16:34:03 +0200

    server scripts: Use sh instead of bash

--- resources/server/bin/code.sh
+++ resources/server/bin/code.sh
@@ -1,7 +1,7 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 #
 # Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License. See License.txt in the project root for license information.
+#
 
 ROOT="$(dirname "$(dirname "$(realpath "$0")")")"
 
@@ -10,4 +10,4 @@ VERSION="@@VERSION@@"
 COMMIT="@@COMMIT@@"
 EXEC_NAME="@@APPNAME@@"
 CLI_SCRIPT="$ROOT/out/remoteCli.js"
-"$ROOT/node" "$CLI_SCRIPT" "$APP_NAME" "$VERSION" "$COMMIT" "$EXEC_NAME" "$VSCODE_CLIENT_COMMAND" $@
+"$ROOT/node" "$CLI_SCRIPT" "$APP_NAME" "$VERSION" "$COMMIT" "$EXEC_NAME" "$VSCODE_CLIENT_COMMAND" "$@"
