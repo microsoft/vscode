9cc8be3bb7aa6830016ca8208cc76cfc152b68ef
Benjamin Pasero <benjpas@microsoft.com>
2019-04-29 12:09:34 +0200

    make test assert more

--- src/vs/workbench/contrib/files/test/common/fileOnDiskProvider.test.ts
+++ src/vs/workbench/contrib/files/test/common/fileOnDiskProvider.test.ts
@@ -5,24 +5,36 @@
 
 import * as assert from 'assert';
 import { URI } from 'vs/base/common/uri';
-import { workbenchInstantiationService } from 'vs/workbench/test/workbenchTestServices';
+import { workbenchInstantiationService, TestFileService } from 'vs/workbench/test/workbenchTestServices';
 import { IInstantiationService } from 'vs/platform/instantiation/common/instantiation';
 import { FileOnDiskContentProvider, resourceToFileOnDisk } from 'vs/workbench/contrib/files/common/files';
 import { snapshotToString } from 'vs/workbench/services/textfile/common/textfiles';
+import { IFileService } from 'vs/platform/files/common/files';
+
+class ServiceAccessor {
+	constructor(
+		@IFileService public fileService: TestFileService
+	) {
+	}
+}
 
 suite('Files - FileOnDiskContentProvider', () => {
 
 	let instantiationService: IInstantiationService;
+	let accessor: ServiceAccessor;
 
 	setup(() => {
 		instantiationService = workbenchInstantiationService();
+		accessor = instantiationService.createInstance(ServiceAccessor);
 	});
 
 	test('provideTextContent', async () => {
 		const provider = instantiationService.createInstance(FileOnDiskContentProvider);
+		const uri = URI.parse('testFileOnDiskContentProvider://foo');
 
-		const content = await provider.provideTextContent(resourceToFileOnDisk('conflictResolution', URI.parse('testFileOnDiskContentProvider://foo')));
+		const content = await provider.provideTextContent(resourceToFileOnDisk('conflictResolution', uri));
 
 		assert.equal(snapshotToString(content.createSnapshot()), 'Hello Html');
+		assert.equal(accessor.fileService.getLastReadFileUri().toString(), uri.toString());
 	});
 });
