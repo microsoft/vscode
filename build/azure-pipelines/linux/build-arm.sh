446a917a3a55dcf0e8ec411e3d0b335651559463
Joao Moreno <joao.moreno@microsoft.com>
2019-05-08 11:25:24 +0200

    arm build

---
+++ build/azure-pipelines/linux/build-arm.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -e
+
+cat << EOF > ~/.netrc
+machine monacotools.visualstudio.com
+password $VSO_PAT
+machine github.com
+login vscode
+password $VSCODE_MIXIN_PASSWORD
+EOF
+
+git config user.email "vscode@microsoft.com"
+git config user.name "VSCode"
+git remote add distro "https://github.com/$VSCODE_MIXIN_REPO.git"
+git fetch distro
+git merge $(node -p "require('./package.json').distro")
+
+CHILD_CONCURRENCY=1 yarn
+yarn gulp mixin
+node build/azure-pipelines/common/installDistro.js
