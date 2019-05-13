1dd5245a83042b56172f126a203ba6de5108c9ec
Daniel Imms <daimms@microsoft.com>
2019-05-13 11:15:50 -0700

    Remove unneeded vscode-xterm function

--- src/typings/vscode-xterm.d.ts
+++ src/typings/vscode-xterm.d.ts
@@ -928,7 +928,5 @@ declare module 'vscode-xterm' {
 		 * @return Whether a result was found.
 		 */
 		findPrevious(term: string, findOptions: ISearchOptions): boolean;
-
-		addCsiHandler(flag: string, callback: (params: number[], collect: string) => boolean): IDisposable;
 	}
 }
