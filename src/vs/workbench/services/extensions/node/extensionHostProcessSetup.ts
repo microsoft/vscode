39193a5fde911feb7deba8ed524dddc420142638
Alex Dima <alexdima@microsoft.com>
2019-05-24 21:24:23 +0200

    Only wait for rich client to reconnect

--- src/vs/workbench/services/extensions/node/extensionHostProcessSetup.ts
+++ src/vs/workbench/services/extensions/node/extensionHostProcessSetup.ts
@@ -131,13 +131,21 @@ function _createExtHostProtocol(): Promise<IMessagePassingProtocol> {
 						protocol.onClose(() => onTerminate());
 						resolve(protocol);
 
-						protocol.onSocketClose(() => {
-							// The socket has closed, let's give the renderer a certain amount of time to reconnect
-							disconnectWaitTimer = setTimeout(() => {
-								disconnectWaitTimer = null;
+						if (msg.skipWebSocketFrames) {
+							// Wait for rich client to reconnect
+							protocol.onSocketClose(() => {
+								// The socket has closed, let's give the renderer a certain amount of time to reconnect
+								disconnectWaitTimer = setTimeout(() => {
+									disconnectWaitTimer = null;
+									onTerminate();
+								}, ProtocolConstants.ReconnectionGraceTime);
+							});
+						} else {
+							// Do not wait for web companion to reconnect
+							protocol.onSocketClose(() => {
 								onTerminate();
-							}, ProtocolConstants.ReconnectionGraceTime);
-						});
+							});
+						}
 					}
 				}
 			});
