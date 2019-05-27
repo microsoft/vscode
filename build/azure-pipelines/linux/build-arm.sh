32f3c4716236057022e5cc31387046c4508dc9f0
Joao Moreno <joao.moreno@microsoft.com>
2019-05-27 16:02:22 +0200

    noop arm build

--- build/azure-pipelines/linux/build-arm.sh
+++ build/azure-pipelines/linux/build-arm.sh
@@ -1,18 +1,3 @@
 #!/usr/bin/env bash
 set -e
-
-cat << EOF > ~/.netrc
-machine monacotools.visualstudio.com
-password $VSO_PAT
-machine github.com
-login vscode
-password $VSCODE_MIXIN_PASSWORD
-EOF
-
-git config user.email "vscode@microsoft.com"
-git config user.name "VSCode"
-git remote add distro "https://github.com/$VSCODE_MIXIN_REPO.git"
-git fetch distro
-git merge $(node -p "require('./package.json').distro")
-
-CHILD_CONCURRENCY=1 yarn
+echo 'noop'
