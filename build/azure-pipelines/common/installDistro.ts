d1dff7d9efc7479887e860ff60c7323e567f1e23
Joao Moreno <joao.moreno@microsoft.com>
2019-05-27 16:15:53 +0200

    split installDistro script

--- build/azure-pipelines/common/installDistro.ts
+++ build/azure-pipelines/common/installDistro.ts
@@ -6,9 +6,8 @@
 import * as cp from 'child_process';
 import * as path from 'path';
 
-function yarnInstall(packageName: string): void {
-	cp.execSync(`yarn add --no-lockfile ${packageName}`);
-	cp.execSync(`yarn add --no-lockfile ${packageName}`, { cwd: path.join( process.cwd(), 'remote') });
+function yarnInstall(packageName: string, cwd: string): void {
+	cp.execSync(`yarn add --no-lockfile ${packageName}`, { cwd });
 }
 
 const product = require('../../../product.json');
@@ -16,5 +15,6 @@ const dependencies = product.dependencies || {} as { [name: string]: string; };
 
 Object.keys(dependencies).forEach(name => {
 	const url = dependencies[name];
-	yarnInstall(url);
+	const cwd = process.argv.length < 3 ? process.cwd() : path.join(process.cwd(), process.argv[2]);
+	yarnInstall(url, cwd);
 });
