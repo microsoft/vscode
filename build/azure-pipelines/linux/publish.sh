13315b2bb7cddd8063d7c31dad16a14ca22435b1
Joao Moreno <joao.moreno@microsoft.com>
2019-05-27 14:44:51 +0200

    agent: create tar with 0 owner and group

--- build/azure-pipelines/linux/publish.sh
+++ build/azure-pipelines/linux/publish.sh
@@ -28,7 +28,7 @@ if [[ "$VSCODE_ARCH" != "ia32" ]]; then
 	SERVER_TARBALL_PATH="$ROOT/$SERVER_TARBALL_FILENAME"
 
 	rm -rf $ROOT/vscode-server-*.tar.*
-	(cd $ROOT && mv $LEGACY_SERVER_BUILD_NAME $SERVER_BUILD_NAME && tar -czf $SERVER_TARBALL_PATH $SERVER_BUILD_NAME)
+	(cd $ROOT && mv $LEGACY_SERVER_BUILD_NAME $SERVER_BUILD_NAME && tar --owner=0 --group=0 -czf $SERVER_TARBALL_PATH $SERVER_BUILD_NAME)
 
 	node build/azure-pipelines/common/publish.js "$VSCODE_QUALITY" "server-$PLATFORM_LINUX" archive-unsigned "$SERVER_TARBALL_FILENAME" "$VERSION" true "$SERVER_TARBALL_PATH"
 fi
