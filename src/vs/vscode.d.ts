a429fd13565d3392c87e31228ec9e8a2bf3708d5
Johannes Rieken <johannes.rieken@gmail.com>
2019-05-28 17:59:52 +0200

    refine remoteAuthority-api, #74188

--- src/vs/vscode.d.ts
+++ src/vs/vscode.d.ts
@@ -6042,15 +6042,19 @@ declare module 'vscode' {
 		export function openExternal(target: Uri): Thenable<boolean>;
 
 		/**
-		 * The [authority](#Uri.authority)-component. When `undefined`, extensions are
-		 * executed in the same context (e.g. operating system or machine) in which the UI
-		 * of the editor is executed. If defined, extensions are running in a different
-		 * context, for instance a remote machine.
+		 * Information about running remotely. When `undefined`, extensions are executed in the same
+		 * context (e.g. operating system or machine) in which the UI of the editor is executed.
+		 * If defined, extensions are running in a different context, for instance a remote machine.
 		 *
-		 * *Note* that no assumptions about the actual value should be made as it is defined
-		 * by extensions and not by the editor.
+		 * A remote [uri](#Uri) adheres to this format: `remote_scheme://auth_prefix+auth_rest/path` where
+		 * the `remote_scheme` is defined by the editor, `auth_prefix` and `auth_rest` are defined by extensions,
+		 * and `path` is defined by the user.
+		 *
+		 * Extensions contributing a remote are encouraged to use a stable `auth_prefix`-value,
+		 * e.g `remote_scheme://ssh+23/` and `remote_scheme://ssh+42`. Such prefix is exposed
+		 * in the `prefix`-property of the returned object.
 		 */
-		export const remoteAuthority: string | undefined;
+		export const remoteAuthority: { prefix: string } | undefined;
 	}
 
 	/**
