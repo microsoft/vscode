653a214807db46a041da27c1ed1a79614032e49a
Joao Moreno <joao.moreno@microsoft.com>
2019-05-28 08:35:58 +0200

    install distro dependencies from package.json

--- build/azure-pipelines/common/installDistro.ts
+++ build/azure-pipelines/common/installDistro.ts
@@ -5,16 +5,33 @@
 
 import * as cp from 'child_process';
 import * as path from 'path';
+import * as fs from 'fs';
 
 function yarnInstall(packageName: string, cwd: string): void {
 	cp.execSync(`yarn add --no-lockfile ${packageName}`, { cwd });
 }
 
-const product = require('../../../product.json');
-const dependencies = product.dependencies || {} as { [name: string]: string; };
+/**
+ * Install additional dependencies listed on each quality `package.json` file.
+ */
+function main() {
+	const quality = process.env['VSCODE_QUALITY'];
 
-Object.keys(dependencies).forEach(name => {
-	const url = dependencies[name];
-	const cwd = process.argv.length < 3 ? process.cwd() : path.join(process.cwd(), process.argv[2]);
-	yarnInstall(url, cwd);
-});
+	if (!quality) {
+		throw new Error('Missing VSCODE_QUALITY, can\'t install distro');
+	}
+
+	const rootPath = path.dirname(path.dirname(path.dirname(__dirname)));
+	const qualityPath = path.join(rootPath, quality);
+	const packagePath = path.join(qualityPath, 'package.json');
+	const pkg = JSON.parse(fs.readFileSync(packagePath, 'utf8'));
+	const dependencies = pkg.dependencies || {} as { [name: string]: string; };
+
+	Object.keys(dependencies).forEach(name => {
+		const url = dependencies[name];
+		const cwd = process.argv.length < 3 ? process.cwd() : path.join(process.cwd(), process.argv[2]);
+		yarnInstall(url, cwd);
+	});
+}
+
+main();
