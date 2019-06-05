58459705911b17ffd141ab50a3d3a3ff14c32ad7
Sandeep Somavarapu <sasomava@microsoft.com>
2019-06-06 00:20:01 +0200

    #74918 Move markers status contribution to markers

--- src/vs/workbench/contrib/tasks/electron-browser/task.contribution.ts
+++ src/vs/workbench/contrib/tasks/electron-browser/task.contribution.ts
@@ -29,7 +29,7 @@ import { Registry } from 'vs/platform/registry/common/platform';
 import { ILifecycleService, LifecyclePhase } from 'vs/platform/lifecycle/common/lifecycle';
 import { MenuRegistry, MenuId, SyncActionDescriptor } from 'vs/platform/actions/common/actions';
 import { registerSingleton } from 'vs/platform/instantiation/common/extensions';
-import { IMarkerService, MarkerStatistics } from 'vs/platform/markers/common/markers';
+import { IMarkerService } from 'vs/platform/markers/common/markers';
 import { ITelemetryService } from 'vs/platform/telemetry/common/telemetry';
 import { IConfigurationService, ConfigurationTarget } from 'vs/platform/configuration/common/configuration';
 import { IFileService, IFileStat } from 'vs/platform/files/common/files';
@@ -108,84 +108,18 @@ namespace ConfigureTaskAction {
 
 export class TaskStatusBarContributions extends Disposable implements IWorkbenchContribution {
 	private runningTasksStatusItem: IStatusbarEntryAccessor | undefined;
-	private problemsStatusItem: IStatusbarEntryAccessor;
-
 	private activeTasksCount: number = 0;
 
 	constructor(
 		@ITaskService private readonly taskService: ITaskService,
-		@IMarkerService private readonly markerService: IMarkerService,
 		@IStatusbarService private readonly statusbarService: IStatusbarService,
 		@IProgressService private readonly progressService: IProgressService
 	) {
 		super();
-
-		this.problemsStatusItem = this._register(this.statusbarService.addEntry(this.getProblemsItem(), StatusbarAlignment.LEFT, 50 /* Medium Priority */));
-
 		this.registerListeners();
 	}
 
-	private getProblemsItem(): IStatusbarEntry {
-		const problems = this.markerService.getStatistics();
-
-		return {
-			text: this.getProblemsText(problems),
-			tooltip: this.getProblemsTooltip(problems),
-			command: 'workbench.action.tasks.toggleProblems'
-		};
-	}
-
-	private getProblemsTooltip(stats: MarkerStatistics): string {
-		const errorTitle = (n: number) => nls.localize('totalErrors', "{0} Errors", n);
-		const warningTitle = (n: number) => nls.localize('totalWarnings', "{0} Warnings", n);
-		const infoTitle = (n: number) => nls.localize('totalInfos', "{0} Infos", n);
-
-		const titles: string[] = [];
-
-		if (stats.errors > 0) {
-			titles.push(errorTitle(stats.errors));
-		}
-
-		if (stats.warnings > 0) {
-			titles.push(warningTitle(stats.warnings));
-		}
-
-		if (stats.infos > 0) {
-			titles.push(infoTitle(stats.infos));
-		}
-
-		if (titles.length === 0) {
-			return nls.localize('noProblems', "No Problems");
-		}
-
-		return titles.join(', ');
-	}
-
-	private getProblemsText(stats: MarkerStatistics): string {
-		const problemsText: string[] = [];
-
-		// Errors
-		problemsText.push('$(error) ' + this.packNumber(stats.errors));
-
-		// Warnings
-		problemsText.push('$(warning) ' + this.packNumber(stats.warnings));
-
-		// Info (only if any)
-		if (stats.infos > 0) {
-			problemsText.push('$(info) ' + this.packNumber(stats.infos));
-		}
-
-		return problemsText.join(' ');
-	}
-
-	private packNumber(n: number): string {
-		const manyProblems = nls.localize('manyProblems', "10K+");
-
-		return n > 9999 ? manyProblems : n > 999 ? n.toString().charAt(0) + 'K' : n.toString();
-	}
-
 	private registerListeners(): void {
-		this.markerService.onMarkerChanged(() => this.updateProblemsStatus());
 		let promise: Promise<void> | undefined = undefined;
 		let resolver: (value?: void | Thenable<void>) => void;
 		this.taskService.onDidStateChange(event => {
@@ -194,7 +128,6 @@ export class TaskStatusBarContributions extends Disposable implements IWorkbench
 			}
 
 			if (!this.ignoreEventForUpdateRunningTasksCount(event)) {
-				let needsUpdate = false;
 				switch (event.kind) {
 					case TaskEventKind.Active:
 						this.activeTasksCount++;
@@ -204,7 +137,6 @@ export class TaskStatusBarContributions extends Disposable implements IWorkbench
 									resolver = resolve;
 								});
 							}
-							needsUpdate = true;
 						}
 						break;
 					case TaskEventKind.Inactive:
@@ -216,7 +148,6 @@ export class TaskStatusBarContributions extends Disposable implements IWorkbench
 								if (promise && resolver!) {
 									resolver!();
 								}
-								needsUpdate = true;
 							}
 						}
 						break;
@@ -226,13 +157,8 @@ export class TaskStatusBarContributions extends Disposable implements IWorkbench
 							if (promise && resolver!) {
 								resolver!();
 							}
-							needsUpdate = true;
 						}
 						break;
-				}
-
-				if (needsUpdate) {
-					this.updateProblemsStatus();
 				}
 			}
 
@@ -267,10 +193,6 @@ export class TaskStatusBarContributions extends Disposable implements IWorkbench
 				this.runningTasksStatusItem.update(itemProps);
 			}
 		}
-	}
-
-	private updateProblemsStatus(): void {
-		this.problemsStatusItem.update(this.getProblemsItem());
 	}
 
 	private ignoreEventForUpdateRunningTasksCount(event: TaskEvent): boolean {
