b1cc16d8c0d5b8f6027c640a4726a19ffadcb897
Martin Aeschlimann <martinae@microsoft.com>
2019-06-06 09:44:12 +0200

    sourcemaps: update comment

--- build/azure-pipelines/darwin/build.sh
+++ build/azure-pipelines/darwin/build.sh
@@ -2,4 +2,6 @@
 set -e
 yarn gulp vscode-darwin-min
 yarn gulp vscode-reh-darwin-min
+
+# upload-vscode-sourcemaps only publishes client maps (out-vscode-min), not server (out-vscode-reh-min)
 yarn gulp upload-vscode-sourcemaps
