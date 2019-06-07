e0ffc7418d4b5631b23d3691800c4cc92af3a94d
Martin Aeschlimann <martinae@microsoft.com>
2019-06-07 11:29:56 +0200

    minimize deltas

--- build/azure-pipelines/darwin/build.sh
+++ build/azure-pipelines/darwin/build.sh
@@ -2,6 +2,4 @@
 set -e
 yarn gulp vscode-darwin-min
 yarn gulp vscode-reh-darwin-min
-
-# upload-vscode-sourcemaps only publishes client maps (out-vscode-min), not server (out-vscode-reh-min)
 yarn gulp upload-vscode-sourcemaps
