65d302e1ecb18e9f5e4712a18f755e535c0949bc
Benjamin Pasero <benjpas@microsoft.com>
2019-06-10 08:46:59 +0200

    web - add --verbose option

--- resources/server/bin-dev/code-web.js
+++ resources/server/bin-dev/code-web.js
@@ -7,18 +7,30 @@ const opn = require('opn');
 const cp = require('child_process');
 const path = require('path');
 
-const proc = cp.execFile(path.join(__dirname, process.platform === 'win32' ? 'server.bat' : 'server.sh'),  process.argv);
+const verbose = process.argv.indexOf('--verbose') !== -1;
+
+const proc = cp.execFile(path.join(__dirname, process.platform === 'win32' ? 'server.bat' : 'server.sh'), process.argv);
 
 let launched = false;
 proc.stdout.on("data", data => {
+
+	// Respect --verbose
+	if (verbose) {
+		console.log(data);
+	}
+
+	// Bring up web URL when we detect the server is ready
 	if (!launched && data.toString().indexOf('Extension host agent listening on 8000') >= 0) {
 		launched = true;
 
 		setTimeout(() => {
 			const url = 'http://127.0.0.1:8000';
-			console.log(`Opening ${url} in your browser...`);
 
-			opn(url).catch(() => { console.log(`Failed to open in your browser. Please do so manually.`); });
+			if (verbose) {
+				console.log(`Opening ${url} in your browser...`);
+			}
+
+			opn(url).catch(() => { console.log(`Failed to open ${url} in your browser. Please do so manually.`); });
 		}, 100);
 	}
 });
