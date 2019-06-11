74d99f6b144df29984b1c5f80450e75229e752b6
isidor <inikolic@microsoft.com>
2019-06-11 12:56:33 +0200

    Cache the 'vsda' import, because when the same missing module is imported multiple times, The ones after the first will not throw an error. And this will break the contract of the sign method

--- src/vs/platform/sign/node/signService.ts
+++ src/vs/platform/sign/node/signService.ts
@@ -4,13 +4,21 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { ISignService } from 'vs/platform/sign/common/sign';
+import { memoize } from 'vs/base/common/decorators';
 
 export class SignService implements ISignService {
 	_serviceBrand: any;
 
+	// Cache the 'vsda' import, because when the same missing module is imported multiple times,
+	// the ones after the first will not throw an error. And this will break the contract of the sign method.
+	@memoize
+	private vsda(): Promise<typeof import('vsda')> {
+		return import('vsda');
+	}
+
 	async sign(value: string): Promise<string> {
 		try {
-			const vsda = await import('vsda');
+			const vsda = await this.vsda();
 			const signer = new vsda.signer();
 			if (signer) {
 				return signer.sign(value);
