8a71ad8a105a8905f18bf3470308297de3bfdd7f
Sandeep Somavarapu <sasomava@microsoft.com>
2019-06-12 08:38:14 +0200

    add implements IWorkbenchContribution

--- src/vs/workbench/contrib/update/electron-browser/update.ts
+++ src/vs/workbench/contrib/update/electron-browser/update.ts
@@ -215,7 +215,7 @@ export class Win3264BitContribution implements IWorkbenchContribution {
 	}
 }
 
-export class UpdateContribution extends Disposable {
+export class UpdateContribution extends Disposable implements IWorkbenchContribution {
 
 	private state: UpdateState;
 	private badgeDisposable: IDisposable = Disposable.None;
