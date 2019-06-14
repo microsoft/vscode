fa4efe8bed173954881eb992f5f068f5c6d6f5d5
Peng Lyu <penn.lv@gmail.com>
2019-06-13 17:51:03 -0700

    us,cn for linux

--- src/vs/workbench/services/keybinding/browser/keyboardLayouts/layout.contribution.linux.ts
+++ src/vs/workbench/services/keybinding/browser/keyboardLayouts/layout.contribution.linux.ts
@@ -2,3 +2,5 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
+
+import 'vs/workbench/services/keybinding/browser/keyboardLayouts/en.linux';
