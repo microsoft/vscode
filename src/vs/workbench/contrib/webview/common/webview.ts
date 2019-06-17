6cfe053d84c91e4f91d95e7ae81abad990c87e7a
Matt Bierner <matb@microsoft.com>
2019-06-17 12:00:21 -0700

    Move `Open Webview Developer tools` to electron contribution point
    I don't think we can programatically open dev tools on the web

--- src/vs/workbench/contrib/webview/common/webview.ts
+++ src/vs/workbench/contrib/webview/common/webview.ts
@@ -10,6 +10,7 @@ import * as modes from 'vs/editor/common/modes';
 import { RawContextKey } from 'vs/platform/contextkey/common/contextkey';
 import { ExtensionIdentifier } from 'vs/platform/extensions/common/extensions';
 import { createDecorator } from 'vs/platform/instantiation/common/instantiation';
+import * as nls from 'vs/nls';
 
 /**
  * Set when the find widget in a webview is visible.
@@ -83,3 +84,5 @@ export interface Webview extends IDisposable {
 	showFind(): void;
 	hideFind(): void;
 }
+
+export const webviewDeveloperCategory = nls.localize('developer', "Developer");
---
+++ src/vs/workbench/contrib/webview/electron-browser/pre/webviewCommands.ts
@@ -0,0 +1,39 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { Action } from 'vs/base/common/actions';
+import * as nls from 'vs/nls';
+import { SyncActionDescriptor } from 'vs/platform/actions/common/actions';
+import { Registry } from 'vs/platform/registry/common/platform';
+import { Extensions as ActionExtensions, IWorkbenchActionRegistry } from 'vs/workbench/common/actions';
+import { webviewDeveloperCategory } from 'vs/workbench/contrib/webview/common/webview';
+
+export class OpenWebviewDeveloperToolsAction extends Action {
+	static readonly ID = 'workbench.action.webview.openDeveloperTools';
+	static readonly LABEL = nls.localize('openToolsLabel', "Open Webview Developer Tools");
+
+	public constructor(id: string, label: string) {
+		super(id, label);
+	}
+
+	public run(): Promise<any> {
+		const elements = document.querySelectorAll('webview.ready');
+		for (let i = 0; i < elements.length; i++) {
+			try {
+				(elements.item(i) as Electron.WebviewTag).openDevTools();
+			} catch (e) {
+				console.error(e);
+			}
+		}
+		return Promise.resolve(true);
+	}
+}
+
+const actionRegistry = Registry.as<IWorkbenchActionRegistry>(ActionExtensions.WorkbenchActions);
+
+actionRegistry.registerWorkbenchAction(
+	new SyncActionDescriptor(OpenWebviewDeveloperToolsAction, OpenWebviewDeveloperToolsAction.ID, OpenWebviewDeveloperToolsAction.LABEL),
+	'Open Webview Developer Tools',
+	webviewDeveloperCategory);
