3db6aad4b3378955f6f369ae4a627d68953ed919
Christof Marti <chrmarti@microsoft.com>
2019-06-20 15:55:49 +0200

    Publish web

--- build/azure-pipelines/linux/publish-alpine.sh
+++ build/azure-pipelines/linux/publish-alpine.sh
@@ -5,6 +5,7 @@ ROOT="$REPO/.."
 
 PLATFORM_LINUX="linux-$VSCODE_ARCH"
 
+# Publish Remote Extension Host
 LEGACY_SERVER_BUILD_NAME="vscode-reh-$PLATFORM_LINUX"
 LEGACY_SERVER_BUILD_PATH="$ROOT/$LEGACY_SERVER_BUILD_NAME"
 PACKAGEJSON="$LEGACY_SERVER_BUILD_PATH/package.json"
@@ -18,3 +19,14 @@ rm -rf $ROOT/vscode-server-*.tar.*
 (cd $ROOT && mv $LEGACY_SERVER_BUILD_NAME $SERVER_BUILD_NAME && tar --owner=0 --group=0 -czf $SERVER_TARBALL_PATH $SERVER_BUILD_NAME)
 
 node build/azure-pipelines/common/publish.js "$VSCODE_QUALITY" "server-$PLATFORM_LINUX" archive-unsigned "$SERVER_TARBALL_FILENAME" "$VERSION" true "$SERVER_TARBALL_PATH"
+
+# Publish Remote Extension Host (Web)
+LEGACY_SERVER_BUILD_NAME="vscode-reh-$PLATFORM_LINUX-web"
+SERVER_BUILD_NAME="vscode-server-$PLATFORM_LINUX-web"
+SERVER_TARBALL_FILENAME="vscode-server-$PLATFORM_LINUX-web.tar.gz"
+SERVER_TARBALL_PATH="$ROOT/$SERVER_TARBALL_FILENAME"
+
+rm -rf $ROOT/vscode-server-*.tar.*
+(cd $ROOT && mv $LEGACY_SERVER_BUILD_NAME $SERVER_BUILD_NAME && tar --owner=0 --group=0 -czf $SERVER_TARBALL_PATH $SERVER_BUILD_NAME)
+
+node build/azure-pipelines/common/publish.js "$VSCODE_QUALITY" "server-$PLATFORM_LINUX-web" archive-unsigned "$SERVER_TARBALL_FILENAME" "$VERSION" true "$SERVER_TARBALL_PATH"
