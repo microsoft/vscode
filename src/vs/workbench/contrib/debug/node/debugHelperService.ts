cee6fd6284b219d0b16d794ece0e17efab7de425
Andre Weinand <aweinand@microsoft.com>
2019-06-20 10:44:50 +0200

    launch VS Code instance internally

--- src/vs/workbench/contrib/debug/node/debugHelperService.ts
+++ src/vs/workbench/contrib/debug/node/debugHelperService.ts
@@ -3,10 +3,6 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import * as objects from 'vs/base/common/objects';
-import { isUri } from 'vs/workbench/contrib/debug/common/debugUtils';
-import * as cp from 'child_process';
-import { IEnvironmentService } from 'vs/platform/environment/common/environment';
 import { IInstantiationService } from 'vs/platform/instantiation/common/instantiation';
 import { TerminalLauncher } from 'vs/workbench/contrib/debug/node/terminalSupport';
 import { ITerminalLauncher, IDebugHelperService, ILaunchVSCodeArguments } from 'vs/workbench/contrib/debug/common/debug';
@@ -16,12 +12,15 @@ import { getPathFromAmdModule } from 'vs/base/common/amd';
 import { TelemetryService } from 'vs/platform/telemetry/common/telemetryService';
 import { IConfigurationService } from 'vs/platform/configuration/common/configuration';
 import { registerSingleton } from 'vs/platform/instantiation/common/extensions';
+import { IWindowsService, IOpenSettings, IURIToOpen } from 'vs/platform/windows/common/windows';
+import { URI } from 'vs/base/common/uri';
+import { isUri } from 'vs/workbench/contrib/debug/common/debugUtils';
 
 export class NodeDebugHelperService implements IDebugHelperService {
 	_serviceBrand: any;
 
 	constructor(
-		@IEnvironmentService private readonly environmentService: IEnvironmentService
+		@IWindowsService private readonly windowsService: IWindowsService
 	) {
 	}
 
@@ -29,79 +28,58 @@ export class NodeDebugHelperService implements IDebugHelperService {
 		return instantiationService.createInstance(TerminalLauncher);
 	}
 
-	launchVsCode(vscodeArgs: ILaunchVSCodeArguments): Promise<number> {
+	async launchVsCode(vscodeArgs: ILaunchVSCodeArguments): Promise<number> {
 
-		const spawnOpts: cp.SpawnOptions = {
-			detached: false	// https://github.com/Microsoft/vscode/issues/57018
+		let options: IOpenSettings = {
+			forceNewWindow: true,
+			args: {
+				_: []
+			}
 		};
+		let urisToOpen: IURIToOpen[] = [];
 
-		if (vscodeArgs.env) {
-			// merge environment variables into a copy of the process.env
-			const envArgs = objects.mixin(objects.mixin({}, process.env), vscodeArgs.env);
-			// and delete some if necessary
-			Object.keys(envArgs).filter(k => envArgs[k] === null).forEach(key => delete envArgs[key]);
-			spawnOpts.env = envArgs;
-		}
+		for (let arg of vscodeArgs.args) {
+			if (arg.prefix) {
+				const a2 = (arg.prefix || '') + (arg.path || '');
+				const match = /^--(.+)=(.+)$/.exec(a2);
+				if (match && match.length === 3) {
+					const key = match[1];
+					const value = match[2];
+					let uri: URI;
 
-		let spawnArgs = vscodeArgs.args.map(a => {
-			if ((a.prefix === '--file-uri=' || a.prefix === '--folder-uri=') && !isUri(a.path)) {
-				return (a.path || '');
-			}
-			return (a.prefix || '') + (a.path || '');
-		});
-
-		let runtimeExecutable = this.environmentService['execPath'];
-		if (!runtimeExecutable) {
-			return Promise.reject(new Error(`VS Code executable unknown`));
-		}
-
-		// if VS Code runs out of sources, add the VS Code workspace path as the first argument so that Electron turns into VS Code
-		const electronIdx = runtimeExecutable.indexOf(process.platform === 'win32' ? '\\.build\\electron\\' : '/.build/electron/');
-		if (electronIdx > 0) {
-			// guess the VS Code workspace path from the executable
-			const vscodeWorkspacePath = runtimeExecutable.substr(0, electronIdx);
-
-			// only add VS Code workspace path if user hasn't already added that path as a (folder) argument
-			const x = spawnArgs.filter(a => a.indexOf(vscodeWorkspacePath) === 0);
-			if (x.length === 0) {
-				spawnArgs.unshift(vscodeWorkspacePath);
-			}
-		}
-
-		// Workaround for bug Microsoft/vscode#45832
-		if (process.platform === 'win32' && runtimeExecutable.indexOf(' ') > 0) {
-			let foundArgWithSpace = false;
-
-			// check whether there is one arg with a space
-			const args: string[] = [];
-			for (const a of spawnArgs) {
-				if (a.indexOf(' ') > 0) {
-					args.push(`"${a}"`);
-					foundArgWithSpace = true;
+					if (key === 'file-uri') {
+						if (isUri(arg.path)) {
+							uri = URI.parse(value);
+						} else {
+							uri = URI.file(value);
+						}
+						urisToOpen.push({
+							fileUri: uri
+						});
+					} if (key === 'folder-uri') {
+						if (isUri(arg.path)) {
+							uri = URI.parse(value);
+						} else {
+							uri = URI.file(value);
+						}
+						urisToOpen.push({
+							folderUri: uri
+						});
+					} else {
+						if (options.args) {
+							options.args[key] = value;
+						}
+					}
 				} else {
-					args.push(a);
+					if (options.args) {
+						options.args._.push(a2);
+					}
 				}
 			}
-
-			if (foundArgWithSpace) {
-				spawnArgs = args;
-				runtimeExecutable = `"${runtimeExecutable}"`;
-				spawnOpts.shell = true;
-			}
 		}
 
-		return new Promise((resolve, reject) => {
-			const process = cp.spawn(runtimeExecutable, spawnArgs, spawnOpts);
-			process.on('error', error => {
-				reject(error);
-			});
-			process.on('exit', code => {
-				if (code === 0) {
-					resolve(process.pid);
-				} else {
-					reject(new Error(`VS Code exited with ${code}`));
-				}
-			});
+		return this.windowsService.openWindow(0, urisToOpen, options).then(_ => {
+			return 9999;
 		});
 	}
 
