cc6b5c3487df7581cf4b9f198fbcdf3052057a5b
Christof Marti <chrmarti@microsoft.com>
2019-06-24 11:26:19 +0200

    Quick Open > Quick Input (#65153)

--- src/vs/workbench/common/theme.ts
+++ src/vs/workbench/common/theme.ts
@@ -439,17 +439,17 @@ export const SIDE_BAR_SECTION_HEADER_BORDER = registerColor('sideBarSectionHeade
 
 // < --- Quick Input -- >
 
-export const QUICK_OPEN_BACKGROUND = registerColor('quickOpen.background', {
+export const QUICK_INPUT_BACKGROUND = registerColor('quickInput.background', {
 	dark: SIDE_BAR_BACKGROUND,
 	light: SIDE_BAR_BACKGROUND,
 	hc: SIDE_BAR_BACKGROUND
-}, nls.localize('quickOpenBackground', "Quick open background color. The quick open palette is the container for views like the command palette and file quick picker"));
+}, nls.localize('quickInputBackground', "Quick Input background color. The Quick Input widget is the container for views like the color theme picker"));
 
-export const QUICK_OPEN_FOREGROUND = registerColor('quickOpen.foreground', {
+export const QUICK_INPUT_FOREGROUND = registerColor('quickInput.foreground', {
 	dark: SIDE_BAR_FOREGROUND,
 	light: SIDE_BAR_FOREGROUND,
 	hc: SIDE_BAR_FOREGROUND
-}, nls.localize('quickOpenForeground', "Quick open foreground color. The quick open palette is the container for views like the command palette and file quick picker"));
+}, nls.localize('quickInputForeground', "Quick Input foreground color. The Quick Input widget is the container for views like the color theme picker"));
 
 // < --- Title Bar --- >
 
