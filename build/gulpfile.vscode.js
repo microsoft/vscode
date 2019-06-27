9908dd50be240cad75ee11dae6b184d387b98719
Joao Moreno <joao.moreno@microsoft.com>
2019-06-27 14:30:58 +0200

    fix build

--- build/gulpfile.vscode.js
+++ build/gulpfile.vscode.js
@@ -315,7 +315,7 @@ function packageTask(platform, arch, sourceFolderName, destinationFolderName, op
 		// TODO the API should be copied to `out` during compile, not here
 		const api = gulp.src('src/vs/vscode.d.ts').pipe(rename('out/vs/vscode.d.ts'));
 
-		const telemetry = gulp.src('.build/telemetry/**', { base: '.build/telemetry' });
+		const telemetry = gulp.src('.build/telemetry/**', { base: '.build/telemetry', dot: true });
 
 		const depsSrc = [
 			..._.flatten(productionDependencies.map(d => path.relative(root, d.path)).map(d => [`${d}/**`, `!${d}/**/{test,tests}/**`])),
