705a78cdae5980b4dbc7a64c636595123764e930
deepak1556 <hop2deep@gmail.com>
2019-07-01 14:06:35 -0700

    chore: Bump nsfw@1.2.5

--- src/typings/nsfw.d.ts
+++ src/typings/nsfw.d.ts
@@ -3,7 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-declare module 'vscode-nsfw' {
+declare module 'nsfw' {
 	interface NsfwWatcher {
 		start(): any;
 		stop(): any;
