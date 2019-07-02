ae2cc878f89945a8e29b91599b9083308f1cf70b
Matt Bierner <matb@microsoft.com>
2019-07-02 16:35:49 -0700

    Using DisposableStore in mainThread class
    #74250

--- src/vs/workbench/api/browser/mainThreadDocuments.ts
+++ src/vs/workbench/api/browser/mainThreadDocuments.ts
@@ -4,7 +4,7 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { toErrorMessage } from 'vs/base/common/errorMessage';
-import { IDisposable, IReference, dispose } from 'vs/base/common/lifecycle';
+import { IDisposable, IReference, dispose, DisposableStore } from 'vs/base/common/lifecycle';
 import { Schemas } from 'vs/base/common/network';
 import { URI, UriComponents } from 'vs/base/common/uri';
 import { ITextModel } from 'vs/editor/common/model';
@@ -73,7 +73,7 @@ export class MainThreadDocuments implements MainThreadDocumentsShape {
 	private readonly _untitledEditorService: IUntitledEditorService;
 	private readonly _environmentService: IWorkbenchEnvironmentService;
 
-	private _toDispose: IDisposable[];
+	private readonly _toDispose = new DisposableStore();
 	private _modelToDisposeMap: { [modelUrl: string]: IDisposable; };
 	private readonly _proxy: ExtHostDocumentsShape;
 	private readonly _modelIsSynced: { [modelId: string]: boolean; };
@@ -100,23 +100,22 @@ export class MainThreadDocuments implements MainThreadDocumentsShape {
 		this._proxy = extHostContext.getProxy(ExtHostContext.ExtHostDocuments);
 		this._modelIsSynced = {};
 
-		this._toDispose = [];
-		this._toDispose.push(documentsAndEditors.onDocumentAdd(models => models.forEach(this._onModelAdded, this)));
-		this._toDispose.push(documentsAndEditors.onDocumentRemove(urls => urls.forEach(this._onModelRemoved, this)));
-		this._toDispose.push(this._modelReferenceCollection);
-		this._toDispose.push(modelService.onModelModeChanged(this._onModelModeChanged, this));
+		this._toDispose.add(documentsAndEditors.onDocumentAdd(models => models.forEach(this._onModelAdded, this)));
+		this._toDispose.add(documentsAndEditors.onDocumentRemove(urls => urls.forEach(this._onModelRemoved, this)));
+		this._toDispose.add(this._modelReferenceCollection);
+		this._toDispose.add(modelService.onModelModeChanged(this._onModelModeChanged, this));
 
-		this._toDispose.push(textFileService.models.onModelSaved(e => {
+		this._toDispose.add(textFileService.models.onModelSaved(e => {
 			if (this._shouldHandleFileEvent(e)) {
 				this._proxy.$acceptModelSaved(e.resource);
 			}
 		}));
-		this._toDispose.push(textFileService.models.onModelReverted(e => {
+		this._toDispose.add(textFileService.models.onModelReverted(e => {
 			if (this._shouldHandleFileEvent(e)) {
 				this._proxy.$acceptDirtyStateChanged(e.resource, false);
 			}
 		}));
-		this._toDispose.push(textFileService.models.onModelDirty(e => {
+		this._toDispose.add(textFileService.models.onModelDirty(e => {
 			if (this._shouldHandleFileEvent(e)) {
 				this._proxy.$acceptDirtyStateChanged(e.resource, true);
 			}
@@ -130,7 +129,7 @@ export class MainThreadDocuments implements MainThreadDocumentsShape {
 			this._modelToDisposeMap[modelUrl].dispose();
 		});
 		this._modelToDisposeMap = Object.create(null);
-		this._toDispose = dispose(this._toDispose);
+		this._toDispose.dispose();
 	}
 
 	private _shouldHandleFileEvent(e: TextFileModelChangeEvent): boolean {
