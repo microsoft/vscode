8a7157083c93a3bf61fbbc8d92131e8ef31f8139
Joao Moreno <joao.moreno@microsoft.com>
2019-07-04 22:29:28 +0200

    collect distro dependencies in build

--- build/gulpfile.reh.js
+++ build/gulpfile.reh.js
@@ -303,13 +303,22 @@ function packageTask(type, platform, arch, sourceFolderName, destinationFolderNa
 
 		const license = gulp.src(['remote/LICENSE'], { base: 'remote' });
 
-		const depsSrc = [
-			..._.flatten(productionDependencies.map(d => path.relative(REPO_ROOT, d.path)).map(d => [`${d}/**`, `!${d}/**/{test,tests}/**`, `!${d}/.bin/**`])),
-			// @ts-ignore JSON checking: dependencies is optional
-			..._.flatten(Object.keys(product.dependencies || {}).map(d => [`node_modules/${d}/**`, `!node_modules/${d}/**/{test,tests}/**`, `!node_modules/${d}/.bin/**`]))
-		];
+		const dependenciesSrc = _.flatten(productionDependencies.map(d => path.relative(REPO_ROOT, d.path)).map(d => [`${d}/**`, `!${d}/**/{test,tests}/**`, `!${d}/.bin/**`]));
 
-		const deps = gulp.src(depsSrc, { base: 'remote', dot: true })
+		// Collect distro dependencies, if any
+		if (quality) {
+			const qualityPackagePath = path.join(REPO_ROOT, 'quality', quality, 'package.json');
+
+			if (fs.existsSync(qualityPackagePath)) {
+				const pkg = JSON.parse(fs.readFileSync(qualityPackagePath, 'utf8'));
+
+				// @ts-ignore JSON checking: dependencies is optional
+				const distroDependencies = _.flatten(Object.keys(pkg.dependencies || {}).map(d => [`node_modules/${d}/**`, `!node_modules/${d}/**/{test,tests}/**`, `!node_modules/${d}/.bin/**`]));
+				dependenciesSrc.push(...distroDependencies);
+			}
+		}
+
+		const deps = gulp.src(dependenciesSrc, { base: 'remote', dot: true })
 			.pipe(filter(['**', '!**/package-lock.json']))
 			.pipe(util.cleanNodeModules(path.join(__dirname, '.nativeignore')));
 
