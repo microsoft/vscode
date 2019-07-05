54889572699a414ec233d5cfe1bcd2fc05c5b185
Alex Ross <alros@microsoft.com>
2019-07-05 15:45:13 +0200

    Fix commandless tasks (#76689)
    * Fix commandless tasks
    
    Part of #76611
    
    * Fix for custom tasks

--- src/vs/workbench/contrib/tasks/browser/terminalTaskSystem.ts
+++ src/vs/workbench/contrib/tasks/browser/terminalTaskSystem.ts
@@ -471,7 +471,8 @@ export class TerminalTaskSystem implements ITaskSystem {
 		const resolvedVariables = this.resolveVariablesFromSet(this.currentTask.systemInfo, this.currentTask.workspaceFolder!, task, variables);
 
 		return resolvedVariables.then((resolvedVariables) => {
-			if (resolvedVariables && task.command && task.command.runtime) {
+			const isCustomExecution = task.command.runtime === RuntimeType.CustomExecution;
+			if (resolvedVariables && task.command && task.command.runtime && (isCustomExecution || task.command.name)) {
 				this.currentTask.resolvedVariables = resolvedVariables;
 				return this.executeInTerminal(task, trigger, new VariableResolver(this.currentTask.workspaceFolder!, this.currentTask.systemInfo, resolvedVariables.variables, this.configurationResolverService));
 			} else {
