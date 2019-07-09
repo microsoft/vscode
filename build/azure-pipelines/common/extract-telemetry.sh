28fcc2874207d8a81d198c483ba2b619a96410c7
Logan Ramos <lramos15@gmail.com>
2019-07-09 09:07:52 -0700

    Bump telemetry tooling commit

--- build/azure-pipelines/common/extract-telemetry.sh
+++ build/azure-pipelines/common/extract-telemetry.sh
@@ -4,7 +4,7 @@ set -e
 cd $BUILD_STAGINGDIRECTORY
 git clone https://github.com/microsoft/vscode-telemetry-extractor.git
 cd vscode-telemetry-extractor
-git checkout f538e3157c84d1bd0b239dfc5ebccac226006d58
+git checkout 3e0ace196871f93a20f4a7f5ec3ae0cf5e431d78
 npm i
 npm run setup-extension-repos
 node ./out/cli-extract.js --sourceDir $BUILD_SOURCESDIRECTORY --excludedDirPattern extensions  --outputDir . --applyEndpoints --includeIsMeasurement --patchWebsiteEvents
