c35accc58b07ec357215aa5c7c11be6747e45af9
rahulku <rahulskumar@gmail.com>
2019-07-09 16:56:40 -0700

    Update vscode.d.ts

--- src/vs/vscode.d.ts
+++ src/vs/vscode.d.ts
@@ -2,7 +2,7 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-
+test tes test
 declare module 'vscode' {
 
 	/**
