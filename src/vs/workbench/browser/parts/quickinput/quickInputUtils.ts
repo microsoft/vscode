a10c8835a12f1054c836fd994c4b45b6d988528a
Benjamin Pasero <benjpas@microsoft.com>
2019-07-10 10:15:39 +0200

    quick input :lipstick: css

--- src/vs/workbench/browser/parts/quickinput/quickInputUtils.ts
+++ src/vs/workbench/browser/parts/quickinput/quickInputUtils.ts
@@ -3,7 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import 'vs/css!./quickInput';
+import 'vs/css!./media/quickInput';
 import * as dom from 'vs/base/browser/dom';
 import { URI } from 'vs/base/common/uri';
 import { IdGenerator } from 'vs/base/common/idGenerator';
