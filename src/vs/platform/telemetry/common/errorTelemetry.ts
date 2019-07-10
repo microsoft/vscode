5bd5f020974a38c53131359afd1e1ce8beee4b32
pi1024e <49824824+pi1024e@users.noreply.github.com>
2019-07-10 15:30:15 -0400

    Update errorTelemetry.ts

--- src/vs/platform/telemetry/common/errorTelemetry.ts
+++ src/vs/platform/telemetry/common/errorTelemetry.ts
@@ -10,121 +10,121 @@ import { safeStringify } from 'vs/base/common/objects';
 import { ITelemetryService } from 'vs/platform/telemetry/common/telemetry';
 
 type ErrorEventFragment = {
-	callstack: { classification: 'CallstackOrException', purpose: 'PerformanceAndHealth' };
-	msg?: { classification: 'CallstackOrException', purpose: 'PerformanceAndHealth' };
-	file?: { classification: 'CallstackOrException', purpose: 'PerformanceAndHealth' };
-	line?: { classification: 'CallstackOrException', purpose: 'PerformanceAndHealth', isMeasurement: true };
-	column?: { classification: 'CallstackOrException', purpose: 'PerformanceAndHealth', isMeasurement: true };
-	uncaught_error_name?: { classification: 'CallstackOrException', purpose: 'PerformanceAndHealth' };
-	uncaught_error_msg?: { classification: 'CallstackOrException', purpose: 'PerformanceAndHealth' };
-	count?: { classification: 'CallstackOrException', purpose: 'PerformanceAndHealth', isMeasurement: true };
+    callstack: { classification: 'CallstackOrException', purpose: 'PerformanceAndHealth' };
+    msg?: { classification: 'CallstackOrException', purpose: 'PerformanceAndHealth' };
+    file?: { classification: 'CallstackOrException', purpose: 'PerformanceAndHealth' };
+    line?: { classification: 'CallstackOrException', purpose: 'PerformanceAndHealth', isMeasurement: true };
+    column?: { classification: 'CallstackOrException', purpose: 'PerformanceAndHealth', isMeasurement: true };
+    uncaught_error_name?: { classification: 'CallstackOrException', purpose: 'PerformanceAndHealth' };
+    uncaught_error_msg?: { classification: 'CallstackOrException', purpose: 'PerformanceAndHealth' };
+    count?: { classification: 'CallstackOrException', purpose: 'PerformanceAndHealth', isMeasurement: true };
 };
 export interface ErrorEvent {
-	callstack: string;
-	msg?: string;
-	file?: string;
-	line?: number;
-	column?: number;
-	uncaught_error_name?: string;
-	uncaught_error_msg?: string;
-	count?: number;
+    callstack: string;
+    msg?: string;
+    file?: string;
+    line?: number;
+    column?: number;
+    uncaught_error_name?: string;
+    uncaught_error_msg?: string;
+    count?: number;
 }
 
 export namespace ErrorEvent {
-	export function compare(a: ErrorEvent, b: ErrorEvent) {
-		if (a.callstack < b.callstack) {
-			return -1;
-		}
-		if (a.callstack > b.callstack) {
-			return 1;
-		}
-		return 0;
-	}
+    export function compare(a: ErrorEvent, b: ErrorEvent) {
+        if (a.callstack < b.callstack) {
+            return -1;
+        }
+        if (a.callstack > b.callstack) {
+            return 1;
+        }
+        return 0;
+    }
 }
 
 export default abstract class BaseErrorTelemetry {
 
-	public static ERROR_FLUSH_TIMEOUT: number = 5 * 1000;
+    public static ERROR_FLUSH_TIMEOUT: number = 5 * 1000;
 
-	private _telemetryService: ITelemetryService;
-	private _flushDelay: number;
-	private _flushHandle: any = -1;
-	private _buffer: ErrorEvent[] = [];
-	protected readonly _disposables = new DisposableStore();
+    private _telemetryService: ITelemetryService;
+    private _flushDelay: number;
+    private _flushHandle: any = -1;
+    private _buffer: ErrorEvent[] = [];
+    protected readonly _disposables = new DisposableStore();
 
-	constructor(telemetryService: ITelemetryService, flushDelay = BaseErrorTelemetry.ERROR_FLUSH_TIMEOUT) {
-		this._telemetryService = telemetryService;
-		this._flushDelay = flushDelay;
+    constructor(telemetryService: ITelemetryService, flushDelay = BaseErrorTelemetry.ERROR_FLUSH_TIMEOUT) {
+        this._telemetryService = telemetryService;
+        this._flushDelay = flushDelay;
 
-		// (1) check for unexpected but handled errors
-		const unbind = Errors.errorHandler.addListener((err) => this._onErrorEvent(err));
-		this._disposables.add(toDisposable(unbind));
+        // (1) check for unexpected but handled errors
+        const unbind = Errors.errorHandler.addListener((err) => this._onErrorEvent(err));
+        this._disposables.add(toDisposable(unbind));
 
-		// (2) install implementation-specific error listeners
-		this.installErrorListeners();
-	}
+        // (2) install implementation-specific error listeners
+        this.installErrorListeners();
+    }
 
-	dispose() {
-		clearTimeout(this._flushHandle);
-		this._flushBuffer();
-		this._disposables.dispose();
-	}
+    dispose() {
+        clearTimeout(this._flushHandle);
+        this._flushBuffer();
+        this._disposables.dispose();
+    }
 
-	protected installErrorListeners(): void {
-		// to override
-	}
+    protected installErrorListeners(): void {
+        // to override
+    }
 
-	private _onErrorEvent(err: any): void {
+    private _onErrorEvent(err: any): void {
 
-		if (!err) {
-			return;
-		}
+        if (!err) {
+            return;
+        }
 
-		// unwrap nested errors from loader
-		if (err.detail && err.detail.stack) {
-			err = err.detail;
-		}
+        // unwrap nested errors from loader
+        if (err.detail && err.detail.stack) {
+            err = err.detail;
+        }
 
-		// work around behavior in workerServer.ts that breaks up Error.stack
-		let callstack = Array.isArray(err.stack) ? err.stack.join('\n') : err.stack;
-		let msg = err.message ? err.message : safeStringify(err);
+        // work around behavior in workerServer.ts that breaks up Error.stack
+        let callstack = Array.isArray(err.stack) ? err.stack.join('\n') : err.stack;
+        let msg = err.message ? err.message : safeStringify(err);
 
-		// errors without a stack are not useful telemetry, so we check for it
-		if (callstack) {
-			this._enqueue({ msg, callstack });
-		}
-		
+        // errors without a stack are not useful telemetry, so we check for it
+        if (callstack) {
+            this._enqueue({ msg, callstack });
+        }
 
-		
-	}
 
-	protected _enqueue(e: ErrorEvent): void {
 
-		const idx = binarySearch(this._buffer, e, ErrorEvent.compare);
-		if (idx < 0) {
-			e.count = 1;
-			this._buffer.splice(~idx, 0, e);
-		} else if (!this._buffer[idx].count) {
-			
-			this._buffer[idx].count = 0;	
-		}
-		else {
-			this._buffer[idx].count! += 1;
-		}
+    }
 
-		if (this._flushHandle === -1) {
-			this._flushHandle = setTimeout(() => {
-				this._flushBuffer();
-				this._flushHandle = -1;
-			}, this._flushDelay);
-		}
-	}
+    protected _enqueue(e: ErrorEvent): void {
 
-	private _flushBuffer(): void {
-		for (let error of this._buffer) {
-			type UnhandledErrorClassification = {} & ErrorEventFragment;
-			this._telemetryService.publicLog2<ErrorEvent, UnhandledErrorClassification>('UnhandledError', error, true);
-		}
-		this._buffer.length = 0;
-	}
+        const idx = binarySearch(this._buffer, e, ErrorEvent.compare);
+        if (idx < 0) {
+            e.count = 1;
+            this._buffer.splice(~idx, 0, e);
+        } else if (!this._buffer[idx].count) {
+
+            this._buffer[idx].count = 0;
+        }
+        else {
+            this._buffer[idx].count! += 1;
+        }
+
+        if (this._flushHandle === -1) {
+            this._flushHandle = setTimeout(() => {
+                this._flushBuffer();
+                this._flushHandle = -1;
+            }, this._flushDelay);
+        }
+    }
+
+    private _flushBuffer(): void {
+        for (let error of this._buffer) {
+            type UnhandledErrorClassification = {} & ErrorEventFragment;
+            this._telemetryService.publicLog2<ErrorEvent, UnhandledErrorClassification>('UnhandledError', error, true);
+        }
+        this._buffer.length = 0;
+    }
 }
