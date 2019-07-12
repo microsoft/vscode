7c6f40493d2271bb7874f85abc093c58335ad29f
Sandeep Somavarapu <sasomava@microsoft.com>
2019-07-12 13:38:44 +0200

    Fix microsoft/vscode-internalbacklog/issues/657

--- resources/server/web.sh
+++ resources/server/web.sh
@@ -1,2 +1,24 @@
 #!/usr/bin/env sh
-node $(dirname "$0")/bin-dev/code-web.js "$@"
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+	realpath() { [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"; }
+	ROOT=$(dirname $(dirname $(dirname $(realpath "$0"))))
+else
+	ROOT=$(dirname $(dirname $(dirname $(readlink -f $0))))
+fi
+
+function code() {
+	cd $ROOT
+
+	# Sync built-in extensions
+	yarn download-builtin-extensions
+
+	# Load remote node
+	yarn gulp node
+
+	NODE_ENV=development \
+	VSCODE_DEV=1 \
+	node $(dirname "$0")/bin-dev/code-web.js "$@"
+}
+
+code "$@"
