6ae3384011cd3a838a2c5f78705012796af8874a
Matt Bierner <matb@microsoft.com>
2019-07-15 11:13:42 -0700

    getEditor() return null instead of undefined

--- src/vs/workbench/services/editor/common/editorGroupsService.ts
+++ src/vs/workbench/services/editor/common/editorGroupsService.ts
@@ -411,7 +411,7 @@ export interface IEditorGroup {
 	/**
 	 * Returns the editor at a specific index of the group.
 	 */
-	getEditor(index: number): IEditorInput | null;
+	getEditor(index: number): IEditorInput | undefined;
 
 	/**
 	 * Get all editors that are currently opened in the group optionally
