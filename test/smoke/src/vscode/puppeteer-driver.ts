f7b8079f218bf3b08f614be2a073c341256750a1
Daniel Imms <daimms@microsoft.com>
2019-07-15 11:50:15 -0700

    Add puppeteer types and use TS in driver

--- test/smoke/src/vscode/puppeteer-driver.ts
+++ test/smoke/src/vscode/puppeteer-driver.ts
@@ -22,12 +22,12 @@ function buildDriver(browser, page) {
 		getWindowIds: () => {
 			return Promise.resolve([1]);
 		},
-		capturePage: () => Promise.result(''),
+		capturePage: () => Promise.resolve(''),
 		reloadWindow: (windowId) => Promise.resolve(),
 		exitApplication: () => browser.close(),
 		dispatchKeybinding: async (windowId, keybinding) => {
 			const keys = keybinding.split('+');
-			const keysDown = [];
+			const keysDown: string[] = [];
 			for (let i = 0; i < keys.length; i++) {
 				if (keys[i] in vscodeToPuppeteerKey) {
 					keys[i] = vscodeToPuppeteerKey[keys[i]];
@@ -314,10 +314,10 @@ function buildDriver(browser, page) {
 				})();
 			`);
 		}
-	}
+	};
 }
 
-exports.connect = function (outPath, handle) {
+export function connect(outPath: string, handle: string): Promise<{ client: IDisposable, driver: IDriver }> {
 	return new Promise(async (c) => {
 		const browser = await puppeteer.launch({
 			// Run in Edge dev on macOS
@@ -330,9 +330,59 @@ exports.connect = function (outPath, handle) {
 		await page.setViewport({ width, height });
 		await page.goto('http://127.0.0.1:9888');
 		const result = {
-			client: { dispose: () => {} },
+			client: { dispose: () => { } },
 			driver: buildDriver(browser, page)
-		}
+		};
 		c(result);
 	});
-};
+}
+
+/**
+ * Thenable is a common denominator between ES6 promises, Q, jquery.Deferred, WinJS.Promise,
+ * and others. This API makes no assumption about what promise library is being used which
+ * enables reusing existing code without migrating to a specific promise implementation. Still,
+ * we recommend the use of native promises which are available in this editor.
+ */
+export interface Thenable<T> {
+	/**
+	* Attaches callbacks for the resolution and/or rejection of the Promise.
+	* @param onfulfilled The callback to execute when the Promise is resolved.
+	* @param onrejected The callback to execute when the Promise is rejected.
+	* @returns A Promise for the completion of which ever callback is executed.
+	*/
+	then<TResult>(onfulfilled?: (value: T) => TResult | Thenable<TResult>, onrejected?: (reason: any) => TResult | Thenable<TResult>): Thenable<TResult>;
+	then<TResult>(onfulfilled?: (value: T) => TResult | Thenable<TResult>, onrejected?: (reason: any) => void): Thenable<TResult>;
+}
+
+export interface IElement {
+	tagName: string;
+	className: string;
+	textContent: string;
+	attributes: { [name: string]: string; };
+	children: IElement[];
+	top: number;
+	left: number;
+}
+
+export interface IDriver {
+	_serviceBrand: any;
+
+	getWindowIds(): Promise<number[]>;
+	capturePage(windowId: number): Promise<string>;
+	reloadWindow(windowId: number): Promise<void>;
+	exitApplication(): Promise<void>;
+	dispatchKeybinding(windowId: number, keybinding: string): Promise<void>;
+	click(windowId: number, selector: string, xoffset?: number | undefined, yoffset?: number | undefined): Promise<void>;
+	doubleClick(windowId: number, selector: string): Promise<void>;
+	setValue(windowId: number, selector: string, text: string): Promise<void>;
+	getTitle(windowId: number): Promise<string>;
+	isActiveElement(windowId: number, selector: string): Promise<boolean>;
+	getElements(windowId: number, selector: string, recursive?: boolean): Promise<IElement[]>;
+	typeInEditor(windowId: number, selector: string, text: string): Promise<void>;
+	getTerminalBuffer(windowId: number, selector: string): Promise<string[]>;
+	writeInTerminal(windowId: number, selector: string, text: string): Promise<void>;
+}
+
+export interface IDisposable {
+	dispose(): void;
+}
