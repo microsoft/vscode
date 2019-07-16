0a8ca638f1f64ee0d330b74f8a04d8a22d83efbe
Logan Ramos <lramos15@gmail.com>
2019-07-16 09:23:38 -0700

    Resolve Terminal arguments (#76895)
    * Resolved terminal argds
    
    * Move location of resolution
    
    * Fix routing of resolver in extHost
    
    * Remove unnecessary comma
    
    * Compilation errors
    
    * some async stuff
    
    * Undo changes
    
    * Variable resolver in constructor
    
    * Load resolver upon construction of extHostTerminalService
    
    * Utilize last active workspace root
    
    * Reevaluate variableReoslver whenever the workspace gets new folder
    
    * Fix types for string shellArgs
    
    * Use async one level higher
    
    * Fix compile issue
    
    * Initialize resolver when exthost is created
    
    * Fix ext host in remote case with no folder open
    
    * Resolve args that an ecxctension passes in
    
    * Remove TODO
    
    * Resolve extension arguments

--- src/vs/workbench/contrib/terminal/common/terminalEnvironment.ts
+++ src/vs/workbench/contrib/terminal/common/terminalEnvironment.ts
@@ -169,7 +169,9 @@ export function getDefaultShell(
 	defaultShell: string,
 	isWoW64: boolean,
 	windir: string | undefined,
-	platformOverride: platform.Platform = platform.platform
+	lastActiveWorkspace: IWorkspaceFolder | undefined,
+	configurationResolverService: IConfigurationResolverService | undefined,
+	platformOverride: platform.Platform = platform.platform,
 ): string {
 	const platformKey = platformOverride === platform.Platform.Windows ? 'windows' : platformOverride === platform.Platform.Mac ? 'osx' : 'linux';
 	const shellConfigValue = fetchSetting(`terminal.integrated.shell.${platformKey}`);
@@ -190,17 +192,33 @@ export function getDefaultShell(
 		executable = executable.replace(/\//g, '\\');
 	}
 
+	if (configurationResolverService) {
+		executable = configurationResolverService.resolve(lastActiveWorkspace, executable);
+	}
+
 	return executable;
 }
 
 export function getDefaultShellArgs(
 	fetchSetting: (key: string) => { user: string | string[] | undefined, value: string | string[] | undefined, default: string | string[] | undefined },
 	isWorkspaceShellAllowed: boolean,
-	platformOverride: platform.Platform = platform.platform
-): string[] {
+	lastActiveWorkspace: IWorkspaceFolder | undefined,
+	configurationResolverService: IConfigurationResolverService | undefined,
+	platformOverride: platform.Platform = platform.platform,
+): string | string[] {
 	const platformKey = platformOverride === platform.Platform.Windows ? 'windows' : platformOverride === platform.Platform.Mac ? 'osx' : 'linux';
 	const shellArgsConfigValue = fetchSetting(`terminal.integrated.shellArgs.${platformKey}`);
-	const args = (isWorkspaceShellAllowed ? <string[]>shellArgsConfigValue.value : <string[]>shellArgsConfigValue.user) || <string[]>shellArgsConfigValue.default;
+	let args = <string[] | string>((isWorkspaceShellAllowed ? shellArgsConfigValue.value : shellArgsConfigValue.user) || shellArgsConfigValue.default);
+	if (typeof args === 'string' && platformOverride === platform.Platform.Windows) {
+		return configurationResolverService ? configurationResolverService.resolve(lastActiveWorkspace, args) : args;
+	}
+	if (configurationResolverService) {
+		const resolvedArgs: string[] = [];
+		for (const arg of args) {
+			resolvedArgs.push(configurationResolverService.resolve(lastActiveWorkspace, arg));
+		}
+		args = resolvedArgs;
+	}
 	return args;
 }
 
