fbc5990ccee25d91cab59677ff6624708306a412
Joao Moreno <joao.moreno@microsoft.com>
2019-07-18 09:31:01 +0200

    fix arm

--- build/azure-pipelines/linux/multiarch/armhf/prebuild.sh
+++ build/azure-pipelines/linux/multiarch/armhf/prebuild.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
 docker run --rm --privileged multiarch/qemu-user-static:register --reset
-docker run -e VSCODE_QUALITY -v $(pwd):/root/vscode -v ~/.netrc:/root/.netrc vscodehub.azurecr.io/vscode-linux-build-agent:armhf /root/vscode/build/azure-pipelines/linux/multiarch/arm/install-dependencies.sh
+docker run -e VSCODE_QUALITY -v $(pwd):/root/vscode -v ~/.netrc:/root/.netrc vscodehub.azurecr.io/vscode-linux-build-agent:armhf /root/vscode/build/azure-pipelines/linux/multiarch/armhf/install-dependencies.sh
