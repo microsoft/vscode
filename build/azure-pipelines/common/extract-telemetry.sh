8d560cd153e9760787c8795a1b2cd0e21f57772c
Logan Ramos <lramos15@gmail.com>
2019-07-18 08:44:33 -0700

    Using vscode-telemetry-extractor npm package. (#77305)
    * Revert commit change
    
    * Revert commit change
    
    * Revert commit change
    
    * Revert commit change
    
    * Revert commit change
    
    * Initial work to move to utilizing the NPM module
    
    * Fix some paths
    
    * Bump NPM module
    
    * Update move command
    
    * Remove wrong directory in config
    
    * More build testing
    
    * Remove some debugging stuff
    
    * Fix relative dirs
    
    * Move node module to build package.json
    
    * Bump vscode-telemetry-extractor to 1.4.1
    
    * Bump telemetry extractor version

--- build/azure-pipelines/common/extract-telemetry.sh
+++ build/azure-pipelines/common/extract-telemetry.sh
@@ -2,29 +2,18 @@
 set -e
 
 cd $BUILD_STAGINGDIRECTORY
-git clone https://github.com/microsoft/vscode-telemetry-extractor.git
-cd vscode-telemetry-extractor
-git checkout 4e64f3de30f8fccb58ebdc0d85c4861a135d46cf
-npm i
-npm run compile
-cd src
-mkdir telemetry-sources
-cd telemetry-sources
+mkdir extraction
+cd extraction
 git clone --depth 1 https://github.com/Microsoft/vscode-extension-telemetry.git
 git clone --depth 1 https://github.com/Microsoft/vscode-chrome-debug-core.git
-git clone --depth 1 https://github.com/Microsoft/vscode-chrome-debug.git
 git clone --depth 1 https://github.com/Microsoft/vscode-node-debug2.git
 git clone --depth 1 https://github.com/Microsoft/vscode-node-debug.git
-git clone --depth 1 https://github.com/Microsoft/vscode-docker.git
-git clone --depth 1 https://github.com/Microsoft/vscode-go.git
-git clone --depth 1 https://github.com/Microsoft/vscode-azure-account.git
 git clone --depth 1 https://github.com/Microsoft/vscode-html-languageservice.git
 git clone --depth 1 https://github.com/Microsoft/vscode-json-languageservice.git
-git clone --depth 1 https://github.com/Microsoft/vscode-mono-debug.git
-git clone --depth 1 https://github.com/Microsoft/TypeScript.git
-cd ../../
-node ./out/cli-extract.js --sourceDir $BUILD_SOURCESDIRECTORY --excludedDirPattern extensions  --outputDir . --applyEndpoints --includeIsMeasurement
-node ./out/cli-extract-extensions.js --sourceDir ./src/telemetry-sources --outputDir . --applyEndpoints --includeIsMeasurement
+$BUILD_SOURCESDIRECTORY/build/node_modules/.bin/vscode-telemetry-extractor --sourceDir $BUILD_SOURCESDIRECTORY --excludedDir $BUILD_SOURCESDIRECTORY/extensions --outputDir . --applyEndpoints
+$BUILD_SOURCESDIRECTORY/build/node_modules/.bin/vscode-telemetry-extractor --config $BUILD_SOURCESDIRECTORY/build/azure-pipelines/common/telemetry-config.json -o .
 mkdir -p $BUILD_SOURCESDIRECTORY/.build/telemetry
 mv declarations-resolved.json $BUILD_SOURCESDIRECTORY/.build/telemetry/telemetry-core.json
-mv declarations-extensions-resolved.json $BUILD_SOURCESDIRECTORY/.build/telemetry/telemetry-extensions.json
+mv config-resolved.json $BUILD_SOURCESDIRECTORY/.build/telemetry/telemetry-extensions.json
+cd ..
+rm -rf extraction
