9def0e7f2336a3f17f612de86297ce6e3a383174
Alex Dima <alexdima@microsoft.com>
2019-07-19 13:47:06 +0200

    Ensure the last token covers the end of the text

--- src/vs/editor/common/model/tokensStore.ts
+++ src/vs/editor/common/model/tokensStore.ts
@@ -180,6 +180,9 @@ export class TokensStore {
 			tokens[1] = getDefaultMetadata(topLevelLanguageId);
 		}
 
+		// Ensure the last token covers the end of the text
+		tokens[tokens.length - 2] = lineTextLength;
+
 		if (tokens.byteOffset === 0 && tokens.byteLength === tokens.buffer.byteLength) {
 			// Store directly the ArrayBuffer pointer to save an object
 			return tokens.buffer;
