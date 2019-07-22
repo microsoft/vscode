b330068ab9a1743646bffe02eb40fbf761bf088f
Matt Bierner <matb@microsoft.com>
2019-07-22 11:55:55 -0700

    ElectronWebviewService should not extend BrowserWebviewService

--- src/vs/workbench/contrib/webview/electron-browser/webviewService.ts
+++ src/vs/workbench/contrib/webview/electron-browser/webviewService.ts
@@ -4,24 +4,30 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { IInstantiationService } from 'vs/platform/instantiation/common/instantiation';
-import { WebviewService as BrowserWebviewService } from 'vs/workbench/contrib/webview/browser/webviewService';
-import { IWebviewService, WebviewContentOptions, WebviewElement, WebviewOptions } from 'vs/workbench/contrib/webview/common/webview';
+import { DynamicWebviewEditorOverlay } from 'vs/workbench/contrib/webview/browser/DynamicWebviewEditorOverlay';
+import { IWebviewService, WebviewContentOptions, WebviewEditorOverlay, WebviewElement, WebviewOptions } from 'vs/workbench/contrib/webview/common/webview';
 import { ElectronWebviewBasedWebview } from 'vs/workbench/contrib/webview/electron-browser/webviewElement';
 
-export class ElectronWebviewService extends BrowserWebviewService implements IWebviewService {
+export class ElectronWebviewService implements IWebviewService {
 	_serviceBrand: any;
 
 	constructor(
-		@IInstantiationService private readonly instantiationService: IInstantiationService,
-	) {
-		super(instantiationService);
-	}
+		@IInstantiationService private readonly _instantiationService: IInstantiationService,
+	) { }
 
 	createWebview(
 		_id: string,
 		options: WebviewOptions,
 		contentOptions: WebviewContentOptions
 	): WebviewElement {
-		return this.instantiationService.createInstance(ElectronWebviewBasedWebview, options, contentOptions);
+		return this._instantiationService.createInstance(ElectronWebviewBasedWebview, options, contentOptions);
+	}
+
+	createWebviewEditorOverlay(
+		id: string,
+		options: WebviewOptions,
+		contentOptions: WebviewContentOptions,
+	): WebviewEditorOverlay {
+		return this._instantiationService.createInstance(DynamicWebviewEditorOverlay, id, options, contentOptions);
 	}
 }
