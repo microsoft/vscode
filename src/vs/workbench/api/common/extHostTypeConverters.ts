1762f3f713a34e7ebc61e58188adc5917bb5d159
Kamran Ayub <kamran.ayub@gmail.com>
2019-07-23 10:24:00 -0500

    Revert "Revert "Add DiagnosticTag case for MarkerTag.Deprecated""
    This reverts commit f706130a5bfe3e4004c48198ca3db4800440356c.

--- src/vs/workbench/api/common/extHostTypeConverters.ts
+++ src/vs/workbench/api/common/extHostTypeConverters.ts
@@ -108,6 +108,8 @@ export namespace DiagnosticTag {
 		switch (value) {
 			case types.DiagnosticTag.Unnecessary:
 				return MarkerTag.Unnecessary;
+			case types.DiagnosticTag.Deprecated:
+				return MarkerTag.Deprecated;
 		}
 		return undefined;
 	}
