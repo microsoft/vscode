426bdb61221a3cb9c6bfd49bd731df24310b1fc7
Daniel Imms <daimms@microsoft.com>
2019-07-29 15:27:29 -0700

    Don't fail silently

--- src/vs/workbench/api/node/extHostTerminalService.ts
+++ src/vs/workbench/api/node/extHostTerminalService.ts
@@ -597,7 +597,7 @@ export class ExtHostTerminalService implements ExtHostTerminalServiceShape {
 		// Get the initial cwd
 		const terminalConfig = configProvider.getConfiguration('terminal.integrated');
 
-		const initialCwd = terminalEnvironment.getCwd(shellLaunchConfig, os.homedir(), lastActiveWorkspace ? lastActiveWorkspace : undefined, this._variableResolver, activeWorkspaceRootUri, terminalConfig.cwd);
+		const initialCwd = terminalEnvironment.getCwd(shellLaunchConfig, os.homedir(), lastActiveWorkspace ? lastActiveWorkspace : undefined, this._variableResolver, activeWorkspaceRootUri, terminalConfig.cwd, this._logService);
 
 		const envFromConfig = this._apiInspectConfigToPlain(configProvider.getConfiguration('terminal.integrated').inspect<ITerminalEnvironment>(`env.${platformKey}`));
 		const baseEnv = terminalConfig.get<boolean>('inheritEnv', true) ? process.env as platform.IProcessEnvironment : await this._getNonInheritedEnv();
