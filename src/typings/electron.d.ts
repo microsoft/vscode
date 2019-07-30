e1ab9ef7cecb73332e62c12fadc2ffa939cc7675
deepak1556 <hop2deep@gmail.com>
2019-07-30 16:12:35 -0700

    chore: Bump electron@6.0.0

--- src/typings/electron.d.ts
+++ src/typings/electron.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for Electron 6.0.0-beta.15
+// Type definitions for Electron 6.0.0
 // Project: http://electronjs.org/
 // Definitions by: The Electron Team <https://github.com/electron/electron>
 // Definitions: https://github.com/electron/electron-typescript-definitions
