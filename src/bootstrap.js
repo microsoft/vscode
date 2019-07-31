557e81b9ecc7469017f0c529f597ec3806bfec25
deepak1556 <hop2deep@gmail.com>
2019-07-30 18:51:04 -0700

    fix: update signature of Module._resolveLookupPaths
    https://github.com/nodejs/node/pull/26983

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -36,10 +36,8 @@ exports.injectNodeModuleLookupPath = function (injectPath) {
 	const originalResolveLookupPaths = Module._resolveLookupPaths;
 
 	// @ts-ignore
-	Module._resolveLookupPaths = function (moduleName, parent, newReturn) {
-		const result = originalResolveLookupPaths(moduleName, parent, newReturn);
-
-		const paths = newReturn ? result : result[1];
+	Module._resolveLookupPaths = function (moduleName, parent) {
+		const paths = originalResolveLookupPaths(moduleName, parent);
 		for (let i = 0, len = paths.length; i < len; i++) {
 			if (paths[i] === nodeModulesPath) {
 				paths.splice(i, 0, injectPath);
@@ -47,7 +45,7 @@ exports.injectNodeModuleLookupPath = function (injectPath) {
 			}
 		}
 
-		return result;
+		return paths;
 	};
 };
 //#endregion
@@ -72,10 +70,8 @@ exports.enableASARSupport = function (nodeModulesPath) {
 	// @ts-ignore
 	const originalResolveLookupPaths = Module._resolveLookupPaths;
 	// @ts-ignore
-	Module._resolveLookupPaths = function (request, parent, newReturn) {
-		const result = originalResolveLookupPaths(request, parent, newReturn);
-
-		const paths = newReturn ? result : result[1];
+	Module._resolveLookupPaths = function (request, parent) {
+		const paths = originalResolveLookupPaths(request, parent);
 		for (let i = 0, len = paths.length; i < len; i++) {
 			if (paths[i] === NODE_MODULES_PATH) {
 				paths.splice(i, 0, NODE_MODULES_ASAR_PATH);
@@ -83,7 +79,7 @@ exports.enableASARSupport = function (nodeModulesPath) {
 			}
 		}
 
-		return result;
+		return paths;
 	};
 };
 //#endregion
