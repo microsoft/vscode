edcb4dfe7fb169c6bdfe33d7020e3215d89bb587
Joao Moreno <joao.moreno@microsoft.com>
2019-08-06 12:01:57 +0200

    :lipstick:

--- src/vs/workbench/browser/parts/sidebar/sidebarPart.ts
+++ src/vs/workbench/browser/parts/sidebar/sidebarPart.ts
@@ -67,11 +67,11 @@ export class SidebarPart extends CompositePart<Viewlet> implements IViewletServi
 
 	get onDidViewletRegister(): Event<ViewletDescriptor> { return <Event<ViewletDescriptor>>this.viewletRegistry.onDidRegister; }
 
-	private _onDidViewletDeregister = this._register(new Emitter<ViewletDescriptor>());
-	readonly onDidViewletDeregister: Event<ViewletDescriptor> = this._onDidViewletDeregister.event;
-
 	private _onDidVisibilityChange = this._register(new Emitter<boolean>());
 	readonly onDidVisibilityChange: Event<boolean> = this._onDidVisibilityChange.event;
+
+	private _onDidViewletDeregister = this._register(new Emitter<ViewletDescriptor>());
+	readonly onDidViewletDeregister: Event<ViewletDescriptor> = this._onDidViewletDeregister.event;
 
 	get onDidViewletOpen(): Event<IViewlet> { return Event.map(this.onDidCompositeOpen.event, compositeEvent => <IViewlet>compositeEvent.composite); }
 	get onDidViewletClose(): Event<IViewlet> { return this.onDidCompositeClose.event as Event<IViewlet>; }
