f123663d79c57c07e3f70b2d0938f1a58ff4d1da
Joao Moreno <joao.moreno@microsoft.com>
2019-08-07 09:52:06 +0200

    use CHILD_CONCURRENCY=1

--- build/azure-pipelines/linux/multiarch/alpine/prebuild.sh
+++ build/azure-pipelines/linux/multiarch/alpine/prebuild.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 set -e
-docker run -e VSCODE_QUALITY -v $(pwd):/root/vscode -v ~/.netrc:/root/.netrc vscodehub.azurecr.io/vscode-linux-build-agent:alpine /root/vscode/build/azure-pipelines/linux/multiarch/alpine/install-dependencies.sh
+docker run -e VSCODE_QUALITY -e CHILD_CONCURRENCY=1 -v $(pwd):/root/vscode -v ~/.netrc:/root/.netrc vscodehub.azurecr.io/vscode-linux-build-agent:alpine /root/vscode/build/azure-pipelines/linux/multiarch/alpine/install-dependencies.sh
