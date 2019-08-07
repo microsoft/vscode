e34059755e70c1e7466b0d56d095b5405a3194ba
Joao Moreno <joao.moreno@microsoft.com>
2019-08-07 15:21:22 +0200

    :lipstick: screencast css

--- src/vs/workbench/browser/actions/developerActions.ts
+++ src/vs/workbench/browser/actions/developerActions.ts
@@ -3,6 +3,8 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+import 'vs/css!./media/screencast';
+
 import { Action } from 'vs/base/common/actions';
 import { IWindowService } from 'vs/platform/windows/common/windows';
 import * as nls from 'vs/nls';
@@ -110,19 +112,7 @@ export class ToggleScreencastModeAction extends Action {
 
 		const container = this.layoutService.getWorkbenchElement();
 
-		const mouseMarker = append(container, $('div'));
-		mouseMarker.style.position = 'absolute';
-		mouseMarker.style.border = '2px solid red';
-		mouseMarker.style.borderRadius = '20px';
-		mouseMarker.style.width = '20px';
-		mouseMarker.style.height = '20px';
-		mouseMarker.style.top = '0';
-		mouseMarker.style.left = '0';
-		mouseMarker.style.zIndex = '100000';
-		mouseMarker.style.content = ' ';
-		mouseMarker.style.pointerEvents = 'none';
-		mouseMarker.style.display = 'none';
-
+		const mouseMarker = append(container, $('.screencast-mouse'));
 		const onMouseDown = domEvent(container, 'mousedown', true);
 		const onMouseUp = domEvent(container, 'mouseup', true);
 		const onMouseMove = domEvent(container, 'mousemove', true);
