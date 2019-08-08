8b85550c7d1ea44ddfdb647f5bbd962b67d7f030
Daniel Imms <daimms@microsoft.com>
2019-08-08 09:30:30 -0700

    Revert "Remove unneeded queue from ExtHostPseudoterminal"
    This reverts commit 3dc832d088c3a3983ef7e924ca65a3be816d078d.
    
    This broke the tests

--- src/vs/workbench/api/node/extHostTerminalService.ts
+++ src/vs/workbench/api/node/extHostTerminalService.ts
@@ -22,6 +22,7 @@ import { ExtHostVariableResolverService } from 'vs/workbench/api/node/extHostDeb
 import { ExtHostDocumentsAndEditors } from 'vs/workbench/api/common/extHostDocumentsAndEditors';
 import { getSystemShell, detectAvailableShells } from 'vs/workbench/contrib/terminal/node/terminal';
 import { getMainProcessParentEnv } from 'vs/workbench/contrib/terminal/node/terminalEnvironment';
+import { IDisposable } from 'vs/base/common/lifecycle';
 
 export class BaseExtHostTerminal {
 	public _id: number | undefined;
@@ -686,6 +687,9 @@ class ApiRequest {
 }
 
 class ExtHostPseudoterminal implements ITerminalChildProcess {
+	private _queuedEvents: (IQueuedEvent<string> | IQueuedEvent<number> | IQueuedEvent<{ pid: number, cwd: string }> | IQueuedEvent<ITerminalDimensions | undefined>)[] = [];
+	private _queueDisposables: IDisposable[] | undefined;
+
 	private readonly _onProcessData = new Emitter<string>();
 	public readonly onProcessData: Event<string> = this._onProcessData.event;
 	private readonly _onProcessExit = new Emitter<number>();
@@ -697,7 +701,18 @@ class ExtHostPseudoterminal implements ITerminalChildProcess {
 	private readonly _onProcessOverrideDimensions = new Emitter<ITerminalDimensions | undefined>();
 	public get onProcessOverrideDimensions(): Event<ITerminalDimensions | undefined> { return this._onProcessOverrideDimensions.event; }
 
-	constructor(private readonly _pty: vscode.Pseudoterminal) { }
+	constructor(
+		private readonly _pty: vscode.Pseudoterminal
+	) {
+		this._queueDisposables = [];
+		this._queueDisposables.push(this._pty.onDidWrite(e => this._queuedEvents.push({ emitter: this._onProcessData, data: e })));
+		if (this._pty.onDidClose) {
+			this._queueDisposables.push(this._pty.onDidClose(e => this._queuedEvents.push({ emitter: this._onProcessExit, data: 0 })));
+		}
+		if (this._pty.onDidOverrideDimensions) {
+			this._queueDisposables.push(this._pty.onDidOverrideDimensions(e => this._queuedEvents.push({ emitter: this._onProcessOverrideDimensions, data: e ? { cols: e.columns, rows: e.rows } : undefined })));
+		}
+	}
 
 	shutdown(): void {
 		this._pty.close();
@@ -728,7 +743,12 @@ class ExtHostPseudoterminal implements ITerminalChildProcess {
 	}
 
 	startSendingEvents(initialDimensions: ITerminalDimensionsDto | undefined): void {
-		// Attach the listeners
+		// Flush all buffered events
+		this._queuedEvents.forEach(e => (<any>e.emitter.fire)(e.data));
+		this._queuedEvents = [];
+		this._queueDisposables = undefined;
+
+		// Attach the real listeners
 		this._pty.onDidWrite(e => this._onProcessData.fire(e));
 		if (this._pty.onDidClose) {
 			this._pty.onDidClose(e => this._onProcessExit.fire(0));
@@ -741,3 +761,7 @@ class ExtHostPseudoterminal implements ITerminalChildProcess {
 	}
 }
 
+interface IQueuedEvent<T> {
+	emitter: Emitter<T>;
+	data: T;
+}
