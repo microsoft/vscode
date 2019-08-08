d3066ce7c50cc034f200efc4c7a29227d11794ed
Benjamin Pasero <benjpas@microsoft.com>
2019-08-08 08:27:19 +0200

    propagate editor part visibility (fix #68320)

--- src/vs/workbench/browser/parts/editor/editor.ts
+++ src/vs/workbench/browser/parts/editor/editor.ts
@@ -81,11 +81,14 @@ export interface IEditorOpeningEvent extends IEditorIdentifier {
 }
 
 export interface IEditorGroupsAccessor {
+
 	readonly groups: IEditorGroupView[];
 	readonly activeGroup: IEditorGroupView;
 
 	readonly partOptions: IEditorPartOptions;
 	readonly onDidEditorPartOptionsChange: Event<IEditorPartOptionsChangeEvent>;
+
+	readonly onDidVisibilityChange: Event<boolean>;
 
 	getGroup(identifier: GroupIdentifier): IEditorGroupView | undefined;
 	getGroups(order: GroupsOrder): IEditorGroupView[];
