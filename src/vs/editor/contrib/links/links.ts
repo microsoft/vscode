fadfce252129c236f04575b049e5c6bdf79c0dd1
mmulet <mmulet@users.noreply.github.com>
2019-08-08 14:26:24 -0700

    Fixed Issue 78731
    The error handler for OpenLinkOccurrence handled the case of 'invalid' or 'message', but the function returns an error of new Error('invalid') or new Error('missing') instead of 'invalid' or 'missing'. This causes the editor to throw an unhandled exception instead of a warning message. This fix checks for an err.message as well as err for 'invalid' or 'missing'

--- src/vs/editor/contrib/links/links.ts
+++ src/vs/editor/contrib/links/links.ts
@@ -299,10 +299,12 @@ class LinkDetector implements editorCommon.IEditorContribution {
 			return this.openerService.open(uri, { openToSide });
 
 		}, err => {
+			const errorMessage = err ? err.message : undefined;
+			const messageOrError = errorMessage || err;
 			// different error cases
-			if (err === 'invalid') {
+			if (messageOrError === 'invalid') {
 				this.notificationService.warn(nls.localize('invalid.url', 'Failed to open this link because it is not well-formed: {0}', link.url!.toString()));
-			} else if (err === 'missing') {
+			} else if (messageOrError === 'missing') {
 				this.notificationService.warn(nls.localize('missing.url', 'Failed to open this link because its target is missing.'));
 			} else {
 				onUnexpectedError(err);
