5d8c287767fbbacf642ed3023f9043d3f567025a
Alex Ross <alros@microsoft.com>
2019-08-09 15:41:12 +0200

    Add refresh queueing for tree views
    Fixes #71698

--- src/vs/workbench/api/common/extHostTreeViews.ts
+++ src/vs/workbench/api/common/extHostTreeViews.ts
@@ -177,6 +177,7 @@ class ExtHostTreeView<T> extends Disposable {
 	private _onDidChangeData: Emitter<TreeData<T>> = this._register(new Emitter<TreeData<T>>());
 
 	private refreshPromise: Promise<void> = Promise.resolve();
+	private refreshQueue: Promise<void> = Promise.resolve();
 
 	constructor(private viewId: string, options: vscode.TreeViewOptions<T>, private proxy: MainThreadTreeViewsShape, private commands: CommandsConverter, private logService: ILogService, private extension: IExtensionDescription) {
 		super();
@@ -206,9 +207,11 @@ class ExtHostTreeView<T> extends Disposable {
 			return result;
 		}, 200)(({ message, elements }) => {
 			if (elements.length) {
-				const _promiseCallback = promiseCallback;
-				refreshingPromise = null;
-				this.refresh(elements).then(() => _promiseCallback());
+				this.refreshQueue = this.refreshQueue.then(() => {
+					const _promiseCallback = promiseCallback;
+					refreshingPromise = null;
+					return this.refresh(elements).then(() => _promiseCallback());
+				});
 			}
 			if (message) {
 				this.proxy.$setMessage(this.viewId, this._message);
