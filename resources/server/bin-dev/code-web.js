03e1c530daa9335223a27209e25905f53b6a1545
Sandeep Somavarapu <sasomava@microsoft.com>
2019-08-12 16:56:18 +0200

    use .vscode-web-dev as web dev remote agent folder

--- resources/server/bin-dev/code-web.js
+++ resources/server/bin-dev/code-web.js
@@ -112,7 +112,7 @@ if (SELFHOST) {
 		});
 	}
 } else {
-	env['VSCODE_AGENT_FOLDER'] = path.join(os.homedir(), '.vscode-oss-web');
+	env['VSCODE_AGENT_FOLDER'] = path.join(os.homedir(), '.vscode-web-dev');
 	node = process.execPath;
 	entryPoint = path.join(__dirname, '..', '..', '..', 'out', 'vs', 'server', 'main.js');
 }
