52dcb723a39ae75bee1bd56b3312d7fcdc87aeed
Matt Bierner <matb@microsoft.com>
2019-08-12 15:32:21 -0700

    Inline the markdown css for release notes
    Don't depend as much on vscode-resource

--- src/vs/workbench/contrib/extensions/browser/extensionEditor.ts
+++ src/vs/workbench/contrib/extensions/browser/extensionEditor.ts
@@ -50,9 +50,9 @@ import { IExtensionService } from 'vs/workbench/services/extensions/common/exten
 import { getDefaultValue } from 'vs/platform/configuration/common/configurationRegistry';
 import { isUndefined } from 'vs/base/common/types';
 import { IWorkbenchThemeService } from 'vs/workbench/services/themes/common/workbenchThemeService';
-import { URI } from 'vs/base/common/uri';
 import { IWebviewService, Webview } from 'vs/workbench/contrib/webview/common/webview';
 import { StandardKeyboardEvent } from 'vs/base/browser/keyboardEvent';
+import { generateUuid } from 'vs/base/common/uuid';
 
 function removeEmbeddedSVGs(documentContent: string): string {
 	const newDocument = new DOMParser().parseFromString(documentContent, 'text/html');
@@ -180,7 +180,7 @@ export class ExtensionEditor extends BaseEditor {
 		@IStorageService storageService: IStorageService,
 		@IExtensionService private readonly extensionService: IExtensionService,
 		@IWorkbenchThemeService private readonly workbenchThemeService: IWorkbenchThemeService,
-		@IWebviewService private readonly webviewService: IWebviewService
+		@IWebviewService private readonly webviewService: IWebviewService,
 	) {
 		super(ExtensionEditor.ID, telemetryService, themeService, storageService);
 		this.extensionReadme = null;
@@ -543,9 +543,6 @@ export class ExtensionEditor extends BaseEditor {
 					},
 					{
 						svgWhiteList: this.extensionsWorkbenchService.allowedBadgeProviders,
-						localResourceRoots: [
-							URI.parse(require.toUrl('./media'))
-						]
 					});
 				webviewElement.mountTo(this.content);
 				this.contentDisposables.add(webviewElement.onDidFocus(() => this.fireOnDidFocus()));
@@ -572,14 +569,186 @@ export class ExtensionEditor extends BaseEditor {
 			});
 	}
 
-	private renderBody(body: string): string {
-		const styleSheetPath = require.toUrl('./media/markdown.css').replace('file://', 'vscode-resource://');
+	private async renderBody(body: string): Promise<string> {
+		const nonce = generateUuid();
 		return `<!DOCTYPE html>
 		<html>
 			<head>
 				<meta http-equiv="Content-type" content="text/html;charset=UTF-8">
-				<meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src https: data:; media-src https:; script-src 'none'; style-src vscode-resource:; child-src 'none'; frame-src 'none';">
-				<link rel="stylesheet" type="text/css" href="${styleSheetPath}">
+				<meta http-equiv="Content-Security-Policy" content="default-src 'none'; img-src https: data:; media-src https:; script-src 'none'; style-src 'nonce-${nonce}';">
+				<style nonce="${nonce}">
+					body {
+						padding: 10px 20px;
+						line-height: 22px;
+					}
+
+					img {
+						max-width: 100%;
+						max-height: 100%;
+					}
+
+					a {
+						text-decoration: none;
+					}
+
+					a:hover {
+						text-decoration: underline;
+					}
+
+					a:focus,
+					input:focus,
+					select:focus,
+					textarea:focus {
+						outline: 1px solid -webkit-focus-ring-color;
+						outline-offset: -1px;
+					}
+
+					hr {
+						border: 0;
+						height: 2px;
+						border-bottom: 2px solid;
+					}
+
+					h1 {
+						padding-bottom: 0.3em;
+						line-height: 1.2;
+						border-bottom-width: 1px;
+						border-bottom-style: solid;
+					}
+
+					h1, h2, h3 {
+						font-weight: normal;
+					}
+
+					table {
+						border-collapse: collapse;
+					}
+
+					table > thead > tr > th {
+						text-align: left;
+						border-bottom: 1px solid;
+					}
+
+					table > thead > tr > th,
+					table > thead > tr > td,
+					table > tbody > tr > th,
+					table > tbody > tr > td {
+						padding: 5px 10px;
+					}
+
+					table > tbody > tr + tr > td {
+						border-top: 1px solid;
+					}
+
+					blockquote {
+						margin: 0 7px 0 5px;
+						padding: 0 16px 0 10px;
+						border-left-width: 5px;
+						border-left-style: solid;
+					}
+
+					code {
+						font-family: Menlo, Monaco, Consolas, "Droid Sans Mono", "Courier New", monospace, "Droid Sans Fallback";
+						font-size: 14px;
+						line-height: 19px;
+					}
+
+					.mac code {
+						font-size: 12px;
+						line-height: 18px;
+					}
+
+					code > div {
+						padding: 16px;
+						border-radius: 3px;
+						overflow: auto;
+					}
+
+					#scroll-to-top {
+						position: fixed;
+						width: 40px;
+						height: 40px;
+						right: 25px;
+						bottom: 25px;
+						background-color:#444444;
+						border-radius: 50%;
+						cursor: pointer;
+						box-shadow: 1px 1px 1px rgba(0,0,0,.25);
+						outline: none;
+						display: flex;
+						justify-content: center;
+						align-items: center;
+					}
+
+					#scroll-to-top:hover {
+						background-color:#007acc;
+						box-shadow: 2px 2px 2px rgba(0,0,0,.25);
+					}
+
+					body.vscode-light #scroll-to-top {
+						background-color: #949494;
+					}
+
+					body.vscode-high-contrast #scroll-to-top:hover {
+						background-color: #007acc;
+					}
+
+					body.vscode-high-contrast #scroll-to-top {
+						background-color: black;
+						border: 2px solid #6fc3df;
+						box-shadow: none;
+					}
+					body.vscode-high-contrast #scroll-to-top:hover {
+						background-color: #007acc;
+					}
+
+					#scroll-to-top span.icon::before {
+						content: "";
+						display: block;
+						/* Chevron up icon */
+						background:url('data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz4KPCEtLSBHZW5lcmF0b3I6IEFkb2JlIElsbHVzdHJhdG9yIDE5LjIuMCwgU1ZHIEV4cG9ydCBQbHVnLUluIC4gU1ZHIFZlcnNpb246IDYuMDAgQnVpbGQgMCkgIC0tPgo8c3ZnIHZlcnNpb249IjEuMSIgaWQ9IkxheWVyXzEiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgeG1sbnM6eGxpbms9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkveGxpbmsiIHg9IjBweCIgeT0iMHB4IgoJIHZpZXdCb3g9IjAgMCAxNiAxNiIgc3R5bGU9ImVuYWJsZS1iYWNrZ3JvdW5kOm5ldyAwIDAgMTYgMTY7IiB4bWw6c3BhY2U9InByZXNlcnZlIj4KPHN0eWxlIHR5cGU9InRleHQvY3NzIj4KCS5zdDB7ZmlsbDojRkZGRkZGO30KCS5zdDF7ZmlsbDpub25lO30KPC9zdHlsZT4KPHRpdGxlPnVwY2hldnJvbjwvdGl0bGU+CjxwYXRoIGNsYXNzPSJzdDAiIGQ9Ik04LDUuMWwtNy4zLDcuM0wwLDExLjZsOC04bDgsOGwtMC43LDAuN0w4LDUuMXoiLz4KPHJlY3QgY2xhc3M9InN0MSIgd2lkdGg9IjE2IiBoZWlnaHQ9IjE2Ii8+Cjwvc3ZnPgo=');
+						width: 16px;
+						height: 16px;
+					}
+
+					/** Theming */
+					.vscode-light code > div {
+						background-color: rgba(220, 220, 220, 0.4);
+					}
+
+					.vscode-dark code > div {
+						background-color: rgba(10, 10, 10, 0.4);
+					}
+
+					.vscode-high-contrast code > div {
+						background-color: rgb(0, 0, 0);
+					}
+
+					.vscode-high-contrast h1 {
+						border-color: rgb(0, 0, 0);
+					}
+
+					.vscode-light table > thead > tr > th {
+						border-color: rgba(0, 0, 0, 0.69);
+					}
+
+					.vscode-dark table > thead > tr > th {
+						border-color: rgba(255, 255, 255, 0.69);
+					}
+
+					.vscode-light h1,
+					.vscode-light hr,
+					.vscode-light table > tbody > tr + tr > td {
+						border-color: rgba(0, 0, 0, 0.18);
+					}
+
+					.vscode-dark h1,
+					.vscode-dark hr,
+					.vscode-dark table > tbody > tr + tr > td {
+						border-color: rgba(255, 255, 255, 0.18);
+					}
+
+				</style>
 			</head>
 			<body>
 				<a id="scroll-to-top" role="button" aria-label="scroll to top" href="#"><span class="icon"></span></a>
