414a35dcf793b11617a18d1db56034ace73bde8e
Daniel Imms <daimms@microsoft.com>
2019-08-14 15:04:35 -0700

    Use new browser none arg

--- test/smoke/src/vscode/puppeteerDriver.ts
+++ test/smoke/src/vscode/puppeteerDriver.ts
@@ -189,7 +189,7 @@ export async function launch(_args: string[]): Promise<void> {
 	args = _args;
 	const webUserDataDir = args.filter(e => e.includes('--user-data-dir='))[0].replace('--user-data-dir=', '');
 	await promisify(mkdir)(webUserDataDir);
-	server = spawn(join(args[0], `resources/server/web.${process.platform === 'win32' ? 'bat' : 'sh'}`), ['--no-browser', '--driver', 'web', '--web-user-data-dir', webUserDataDir]);
+	server = spawn(join(args[0], `resources/server/web.${process.platform === 'win32' ? 'bat' : 'sh'}`), ['--browser', 'none', '--driver', 'web', '--web-user-data-dir', webUserDataDir]);
 	server.stderr.on('data', e => console.log('Server stderr: ' + e));
 	process.on('exit', teardown);
 	process.on('SIGINT', teardown);
