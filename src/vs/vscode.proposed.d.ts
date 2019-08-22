6f998109d8ff8ba2bef074eea1b5986a9443316e
Daniel Imms <daimms@microsoft.com>
2019-08-22 07:06:13 -0700

    Use void | number for onDidClose
    Fixes #79643

--- src/vs/vscode.proposed.d.ts
+++ src/vs/vscode.proposed.d.ts
@@ -881,6 +881,11 @@ declare module 'vscode' {
 		/**
 		 * An event that when fired will signal that the pty is closed and dispose of the terminal.
 		 *
+		 * A number can be used to provide an exit code for the terminal. Exit codes must be
+		 * positive and a non-zero exit codes signals failure which shows a notification for a
+		 * regular terminal and allows dependent tasks to proceed when used with the
+		 * `CustomExecution2` API.
+		 *
 		 * **Example:** Exit the terminal when "y" is pressed, otherwise show a notification.
 		 * ```typescript
 		 * const writeEmitter = new vscode.EventEmitter<string>();
@@ -899,7 +904,7 @@ declare module 'vscode' {
 		 * };
 		 * vscode.window.createTerminal({ name: 'Exit example', pty });
 		 */
-		onDidClose?: Event<void>;
+		onDidClose?: Event<void | number>;
 
 		/**
 		 * Implement to handle when the pty is open and ready to start firing events.
