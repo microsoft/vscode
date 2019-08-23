263312b6ddb97a464bed0d53b55b473d8a7fab1e
SteVen Batten <stbatt@microsoft.com>
2019-08-22 17:06:53 -0700

    add reconnect now button

--- src/vs/platform/progress/common/progress.ts
+++ src/vs/platform/progress/common/progress.ts
@@ -50,6 +50,7 @@ export interface IProgressOptions {
 	source?: string;
 	total?: number;
 	cancellable?: boolean;
+	buttons?: string[];
 }
 
 export interface IProgressNotificationOptions extends IProgressOptions {
