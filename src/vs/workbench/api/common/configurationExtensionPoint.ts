902da828e4efa2e5b44a16d576b8c58411e11a2a
Sandeep Somavarapu <sasomava@microsoft.com>
2019-08-28 12:53:41 +0200

    Fix #79975

--- src/vs/workbench/api/common/configurationExtensionPoint.ts
+++ src/vs/workbench/api/common/configurationExtensionPoint.ts
@@ -48,7 +48,7 @@ const configurationEntrySchema: IJSONSchema = {
 									nls.localize('scope.resource.description', "Configuration that can be configured in the user, remote, workspace or folder settings."),
 									nls.localize('scope.machine-overridable.description', "Machine configuration that can be configured also in workspace or folder settings.")
 								],
-								description: nls.localize('scope.description', "Scope in which the configuration is applicable. Available scopes are `application`, `machine`, `window` and `resource`.")
+								description: nls.localize('scope.description', "Scope in which the configuration is applicable. Available scopes are `application`, `machine`, `window`, `resource` and `machine-overridable`.")
 							},
 							enumDescriptions: {
 								type: 'array',
