f175024b4d7c071b3d843f5c1bd591e694e998de
Alex Ross <alros@microsoft.com>
2019-08-29 09:52:55 +0200

    Change custom view multiselect array to have all selected
    Fixes #79982

--- src/vs/vscode.d.ts
+++ src/vs/vscode.d.ts
@@ -6886,7 +6886,7 @@ declare module 'vscode' {
 		/**
 		 * Whether the tree supports multi-select. When the tree supports multi-select and a command is executed from the tree,
 		 * the first argument to the command is the tree item that the command was executed on and the second argument is an
-		 * array containing the other selected tree items.
+		 * array containing all selected tree items.
 		 */
 		canSelectMany?: boolean;
 	}
