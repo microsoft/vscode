9368e64716d65e91da11240d9cad991958df083b
Benjamin Pasero <benjpas@microsoft.com>
2019-08-30 14:14:04 +0200

    :up: chokidar@3

--- src/vs/platform/files/node/watcher/unix/chokidarWatcherService.ts
+++ src/vs/platform/files/node/watcher/unix/chokidarWatcherService.ts
@@ -3,7 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import * as chokidar from 'vscode-chokidar';
+import * as chokidar from 'chokidar';
 import * as fs from 'fs';
 import * as gracefulFs from 'graceful-fs';
 gracefulFs.gracefulify(fs);
