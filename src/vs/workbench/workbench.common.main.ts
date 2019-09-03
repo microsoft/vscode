10ae4b597f4c236f4f04dd690581269e34959d96
Sandeep Somavarapu <sasomava@microsoft.com>
2019-09-03 14:55:14 +0200

    remove imports

--- src/vs/workbench/workbench.common.main.ts
+++ src/vs/workbench/workbench.common.main.ts
@@ -75,9 +75,6 @@ import 'vs/workbench/services/themes/browser/workbenchThemeService';
 import 'vs/workbench/services/label/common/labelService';
 import 'vs/workbench/services/extensionManagement/common/extensionEnablementService';
 import 'vs/workbench/services/notification/common/notificationService';
-import 'vs/workbench/services/userData/common/userIdentityService';
-import 'vs/workbench/services/userData/common/userDataProviderService';
-import 'vs/workbench/services/userData/common/userDataSyncService';
 
 import { registerSingleton } from 'vs/platform/instantiation/common/extensions';
 import { ExtensionGalleryService } from 'vs/platform/extensionManagement/common/extensionGalleryService';
