0739b0f0692e0a2f244ce2772d2defcaad0bdc08
Benjamin Pasero <benjpas@microsoft.com>
2019-09-06 08:19:04 +0200

    url service - create proper URIs

--- src/vs/platform/url/node/urlService.ts
+++ src/vs/platform/url/node/urlService.ts
@@ -10,7 +10,11 @@ import { AbstractURLService } from 'vs/platform/url/common/urlService';
 export class URLService extends AbstractURLService {
 
 	create(options?: Partial<UriComponents>): URI {
-		const { authority, path, query, fragment } = options ? options : { authority: undefined, path: undefined, query: undefined, fragment: undefined };
+		let { authority, path, query, fragment } = options ? options : { authority: undefined, path: undefined, query: undefined, fragment: undefined };
+
+		if (authority && path && path.indexOf('/') !== 0) {
+			path = `/${path}`; // URI validation requires a path if there is an authority
+		}
 
 		return URI.from({ scheme: product.urlProtocol, authority, path, query, fragment });
 	}
