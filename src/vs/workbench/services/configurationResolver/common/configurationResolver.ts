afffb1e7a77664632e1e6a5079b7e419f015ab1f
Alex Ross <alros@microsoft.com>
2019-09-10 12:48:45 +0200

    Contributable variables for configuration resolver (#80478)
    * Contributable variables for configuration resolver
    
    Fixes https://github.com/microsoft/vscode/issues/32239

--- src/vs/workbench/services/configurationResolver/common/configurationResolver.ts
+++ src/vs/workbench/services/configurationResolver/common/configurationResolver.ts
@@ -20,7 +20,7 @@ export interface IConfigurationResolverService {
 	 * Recursively resolves all variables in the given config and returns a copy of it with substituted values.
 	 * Command variables are only substituted if a "commandValueMapping" dictionary is given and if it contains an entry for the command.
 	 */
-	resolveAny(folder: IWorkspaceFolder | undefined, config: any, commandValueMapping?: IStringDictionary<string>): any;
+	resolveAny(folder: IWorkspaceFolder | undefined, config: any, commandValueMapping?: IStringDictionary<string>): Promise<any>;
 
 	/**
 	 * Recursively resolves all variables (including commands and user input) in the given config and returns a copy of it with substituted values.
@@ -36,6 +36,12 @@ export interface IConfigurationResolverService {
 	 * Keys in the map will be of the format input:variableName or command:variableName.
 	 */
 	resolveWithInteraction(folder: IWorkspaceFolder | undefined, config: any, section?: string, variables?: IStringDictionary<string>): Promise<Map<string, string> | undefined>;
+
+	/**
+	 * Contributes a variable that can be resolved later. Consumers that use resolveAny, resolveWithInteraction,
+	 * and resolveWithInteractionReplace will have contributed variables resolved.
+	 */
+	contributeVariable(variable: string, resolution: () => Promise<string | undefined>): void;
 }
 
 export interface PromptStringInputInfo {
