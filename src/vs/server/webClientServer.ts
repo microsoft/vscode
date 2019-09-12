24d2a4b28bc54ae23b57a233df97e4811fd9e1d4
Benjamin Pasero <benjpas@microsoft.com>
2019-09-12 07:31:36 +0200

    web - serve node modules from remote/web

--- src/vs/server/webClientServer.ts
+++ src/vs/server/webClientServer.ts
@@ -155,7 +155,13 @@ export class WebClientServer extends Disposable {
 
 		// Strip `/static/` from the path
 		const normalizedPathname = decodeURIComponent(parsedUrl.pathname!); // support paths that are uri-encoded (e.g. spaces => %20)
-		const relativeFilePath = path.normalize(normalizedPathname.substr('/static/'.length));
+		let relativeFilePath = path.normalize(normalizedPathname.substr('/static/'.length));
+		if (!this._environmentService.isBuilt && relativeFilePath.indexOf('node_modules') === 0) {
+			// Running out of sources, we want to serve node_modules from remote/web folder
+			// to not accidentally pick up modules from our main node_modules folder in root
+			// see https://github.com/microsoft/vscode-internalbacklog/issues/678
+			relativeFilePath = path.join('remote', 'web', relativeFilePath);
+		}
 
 		const filePath = path.join(APP_ROOT, relativeFilePath);
 		if (!isEqualOrParent(filePath, APP_ROOT, !isLinux)) {
