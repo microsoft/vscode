832d7830008f22a83a0447abfcd69ce21312a023
Joel Spadin <joelspadin@gmail.com>
2019-09-12 19:18:17 -0500

    Re-add TextDocument to markdown.api.render
    Changed the markdown.api.render command to accept a TextDocument again, since it
    is possible to safely pass a TextDocument if you ensure that the Markdown
    extension is active first.
    
    You may now pass a TextDocument, its text as a string, or its uri.

--- extensions/markdown-language-features/src/commands/renderDocument.ts
+++ extensions/markdown-language-features/src/commands/renderDocument.ts
@@ -6,6 +6,7 @@
 import { Uri, workspace } from 'vscode';
 import { Command } from '../commandManager';
 import { MarkdownEngine } from '../markdownEngine';
+import { SkinnyTextDocument } from '../tableOfContentsProvider';
 
 export class RenderDocument implements Command {
 	public readonly id = 'markdown.api.render';
@@ -14,7 +15,7 @@ export class RenderDocument implements Command {
 		private readonly engine: MarkdownEngine
 	) { }
 
-	public async execute(arg: Uri | string): Promise<string> {
+	public async execute(arg: Uri | SkinnyTextDocument | string): Promise<string> {
 		const document = arg instanceof Uri ? await workspace.openTextDocument(arg) : arg;
 		return this.engine.render(document);
 	}
