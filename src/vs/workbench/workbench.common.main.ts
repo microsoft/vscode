c38aea395c16f3aa511498e7cb69542423076874
Sandeep Somavarapu <sasomava@microsoft.com>
2019-09-17 08:27:33 +0200

    remote invalid import

--- src/vs/workbench/workbench.common.main.ts
+++ src/vs/workbench/workbench.common.main.ts
@@ -79,7 +79,6 @@ import 'vs/workbench/services/label/common/labelService';
 import 'vs/workbench/services/extensionManagement/common/extensionEnablementService';
 import 'vs/workbench/services/notification/common/notificationService';
 import 'vs/workbench/services/extensions/common/staticExtensions';
-import 'vs/workbench/services/userData/common/userDataSyncService';
 import 'vs/workbench/services/userData/common/settingsMergeService';
 import 'vs/workbench/services/workspace/browser/workspaceEditingService';
 
