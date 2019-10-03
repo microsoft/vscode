3b5e571ef52228b287a01a04ed2effa084baf48f
isidor <inikolic@microsoft.com>
2019-10-03 08:57:30 +0200

    fixes #81868

--- src/vs/workbench/contrib/debug/browser/debug.contribution.ts
+++ src/vs/workbench/contrib/debug/browser/debug.contribution.ts
@@ -261,6 +261,7 @@ configurationRegistry.registerConfiguration({
 		},
 		'debug.onTaskErrors': {
 			enum: ['debugAnyway', 'showErrors', 'prompt'],
+			enumDescriptions: [nls.localize('debugAnyway', "Ignore task errors and start debugging."), nls.localize('showErrors', "Show the Problems view and do not start debugging."), nls.localize('prompt', "Prompt user.")],
 			description: nls.localize('debug.onTaskErrors', "Controls what to do when errors are encountered after running a preLaunchTask."),
 			default: 'prompt'
 		}
