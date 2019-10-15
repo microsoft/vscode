887528397c85dbf841efbbd96930f47a0cd4951e
Benjamin Pasero <benjpas@microsoft.com>
2019-10-15 16:49:23 +0200

    :lipstick: code-web.js

--- scripts/code-web.js
+++ scripts/code-web.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 /*---------------------------------------------------------------------------------------------
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
