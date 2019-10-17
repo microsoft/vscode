157a3ac5be2dffb07ff8339d6eae833bbe59885a
Johannes Rieken <johannes.rieken@gmail.com>
2019-10-17 12:25:21 +0200

    add failing and ignored test for #45515

--- src/vs/base/test/common/uri.test.ts
+++ src/vs/base/test/common/uri.test.ts
@@ -452,6 +452,22 @@ suite('URI', () => {
 		assert.equal(strIn, strOut);
 	});
 
+	test('Uri#parse can break path-component #45515', function () {
+		this.skip();
+		// fails!!
+		let strIn = 'https://firebasestorage.googleapis.com/v0/b/brewlangerie.appspot.com/o/products%2FzVNZkudXJyq8bPGTXUxx%2FBetterave-Sesame.jpg?alt=media&token=0b2310c4-3ea6-4207-bbde-9c3710ba0437';
+		let uri1 = URI.parse(strIn);
+		let strOut = uri1.toString();
+		let uri2 = URI.parse(strOut);
+
+		assert.equal(uri1.scheme, uri2.scheme);
+		assert.equal(uri1.authority, uri2.authority);
+		assert.equal(uri1.path, uri2.path);
+		assert.equal(uri1.query, uri2.query);
+		assert.equal(uri1.fragment, uri2.fragment);
+		assert.equal(strIn, strOut);
+	});
+
 	test('URI - (de)serialize', function () {
 
 		const values = [
