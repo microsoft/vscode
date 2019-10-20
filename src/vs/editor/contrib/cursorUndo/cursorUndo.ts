2c9acc4e585df8ed147e9afd8927c25e6614e985
Snir Broshi <snir@broshi.net>
2019-10-20 20:03:06 +0300

    Rename `cursorUndo/Redo` commands from "Soft Undo/Redo" to "Cursor Undo/Redo"

--- src/vs/editor/contrib/cursorUndo/cursorUndo.ts
+++ src/vs/editor/contrib/cursorUndo/cursorUndo.ts
@@ -119,8 +119,8 @@ export class CursorUndo extends EditorAction {
 	constructor() {
 		super({
 			id: 'cursorUndo',
-			label: nls.localize('cursor.undo', "Soft Undo"),
-			alias: 'Soft Undo',
+			label: nls.localize('cursor.undo', "Cursor Undo"),
+			alias: 'Cursor Undo',
 			precondition: undefined,
 			kbOpts: {
 				kbExpr: EditorContextKeys.textInputFocus,
@@ -139,8 +139,8 @@ export class CursorRedo extends EditorAction {
 	constructor() {
 		super({
 			id: 'cursorRedo',
-			label: nls.localize('cursor.redo', "Soft Redo"),
-			alias: 'Soft Redo',
+			label: nls.localize('cursor.redo', "Cursor Redo"),
+			alias: 'Cursor Redo',
 			precondition: undefined
 		});
 	}
