515eaf84205190e898c7ef2c550006a5545ef658
Amir Omidi <amomidi@microsoft.com>
2019-10-24 16:25:00 -0700

    toggle dev tools active on all contexts
    The UI shows this as a valid shortcut, but currently it's only valid if you're developing vscode.

--- src/vs/workbench/electron-browser/desktop.contribution.ts
+++ src/vs/workbench/electron-browser/desktop.contribution.ts
@@ -17,7 +17,7 @@ import { ContextKeyExpr } from 'vs/platform/contextkey/common/contextkey';
 import { KeybindingsRegistry, KeybindingWeight } from 'vs/platform/keybinding/common/keybindingsRegistry';
 import { CommandsRegistry } from 'vs/platform/commands/common/commands';
 import { ServicesAccessor } from 'vs/platform/instantiation/common/instantiation';
-import { IsMacContext, HasMacNativeTabsContext, IsDevelopmentContext } from 'vs/workbench/browser/contextkeys';
+import { IsMacContext, HasMacNativeTabsContext } from 'vs/workbench/browser/contextkeys';
 import { NoEditorsVisibleContext, SingleEditorGroupsContext } from 'vs/workbench/common/editor';
 import { IElectronService } from 'vs/platform/electron/node/electron';
 import { IJSONContributionRegistry, Extensions as JSONExtensions } from 'vs/platform/jsonschemas/common/jsonContributionRegistry';
@@ -97,7 +97,7 @@ import { IJSONContributionRegistry, Extensions as JSONExtensions } from 'vs/plat
 		KeybindingsRegistry.registerKeybindingRule({
 			id: ToggleDevToolsAction.ID,
 			weight: KeybindingWeight.WorkbenchContrib + 50,
-			when: IsDevelopmentContext,
+			when: undefined,
 			primary: KeyMod.CtrlCmd | KeyMod.Shift | KeyCode.KEY_I,
 			mac: { primary: KeyMod.CtrlCmd | KeyMod.Alt | KeyCode.KEY_I }
 		});
