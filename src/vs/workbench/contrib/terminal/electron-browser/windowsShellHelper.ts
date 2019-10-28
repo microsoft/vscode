9588e378509db6ee98d2296064603c97b522a964
Eric Amodio <eamodio@gmail.com>
2019-10-28 18:51:57 -0400

    Reorders to ensure shellType is set in title event

--- src/vs/workbench/contrib/terminal/electron-browser/windowsShellHelper.ts
+++ src/vs/workbench/contrib/terminal/electron-browser/windowsShellHelper.ts
@@ -82,8 +82,8 @@ export class WindowsShellHelper extends Disposable implements IWindowsShellHelpe
 		if (platform.isWindows && this._terminalInstance.isTitleSetByProcess) {
 			this.getShellName().then(title => {
 				if (!this._isDisposed) {
-					this._terminalInstance.setTitle(title, TitleEventSource.Process);
 					this._terminalInstance.setShellType(this.getShellType(title));
+					this._terminalInstance.setTitle(title, TitleEventSource.Process);
 				}
 			});
 		}
