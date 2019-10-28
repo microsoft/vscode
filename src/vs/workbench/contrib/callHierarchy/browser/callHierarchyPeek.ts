7d0267f49ed294dc2e5b85a3cf97dfc7b95575e3
Matt Bierner <matb@microsoft.com>
2019-10-28 14:30:54 -0700

    Make createInstance stricter (#83472)
    Part of #81574
    
    Makes the `createInstance` function signature more strict. Previously it inferred the argument types based on the actual argument values passed to `createInstance`. Instead, we now infer them from the constructor signature itself

--- src/vs/workbench/contrib/callHierarchy/browser/callHierarchyPeek.ts
+++ src/vs/workbench/contrib/callHierarchy/browser/callHierarchyPeek.ts
@@ -201,7 +201,7 @@ export class CallHierarchyTreePeekWidget extends PeekViewWidget {
 			ariaLabel: localize('tree.aria', "Call Hierarchy"),
 			expandOnlyOnTwistieClick: true,
 		};
-		this._tree = <any>this._instantiationService.createInstance(
+		this._tree = this._instantiationService.createInstance<typeof WorkbenchAsyncDataTree, WorkbenchAsyncDataTree<callHTree.CallHierarchyRoot, callHTree.Call, FuzzyScore>>(
 			WorkbenchAsyncDataTree,
 			'CallHierarchyPeek',
 			treeContainer,
