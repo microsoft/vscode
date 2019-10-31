67d33ad35ff5f852de8265120131a6d50f9b2bac
Rachel Macfarlane <ramacfar@microsoft.com>
2019-10-31 14:25:47 -0700

    Add back 'resolveCommonTelemetryProperties' to browser telemetry service

--- src/vs/workbench/services/telemetry/browser/telemetryService.ts
+++ src/vs/workbench/services/telemetry/browser/telemetryService.ts
@@ -55,7 +55,7 @@ export class TelemetryService extends Disposable implements ITelemetryService {
 		if (!!productService.enableTelemetry) {
 			const config: ITelemetryServiceConfig = {
 				appender: combinedAppender(new WebTelemetryAppender(logService, remoteAgentService), new LogAppender(logService)),
-				commonProperties: resolveWorkbenchCommonProperties(storageService, productService.commit, productService.version, environmentService.configuration.remoteAuthority)
+				commonProperties: resolveWorkbenchCommonProperties(storageService, productService.commit, productService.version, environmentService.configuration.remoteAuthority, environmentService.options && environmentService.options.resolveCommonTelemetryProperties)
 			};
 
 			this.impl = this._register(new BaseTelemetryService(config, configurationService));
