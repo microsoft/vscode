df018407d4998a1acd20d2ce5b74c6bebc0a5c6b
Daniel Imms <daimms@microsoft.com>
2019-10-31 18:36:00 -0700

    Fix folder arg

--- test/automation/src/puppeteerDriver.ts
+++ test/automation/src/puppeteerDriver.ts
@@ -146,7 +146,7 @@ export function connect(headless: boolean, outPath: string, handle: string): Pro
 		});
 		const page = (await browser.pages())[0];
 		await page.setViewport({ width, height });
-		await page.goto(`${endpoint}&folder=${args![1]}`);
+		await page.goto(`${endpoint}&folder=vscode-remote://localhost:9888${args![1]}`);
 		const result = {
 			client: { dispose: () => teardown },
 			driver: buildDriver(browser, page)
