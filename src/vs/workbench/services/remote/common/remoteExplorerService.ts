33e5678363c20a7ac6665cc88752ee00fc83cc0d
Alex Ross <alros@microsoft.com>
2019-11-11 17:28:26 +0100

    Change remote explorer to use drop-down UI. (#84482)
    Part of https://github.com/microsoft/vscode-remote-release/issues/1778

---
+++ src/vs/workbench/services/remote/common/remoteExplorerService.ts
@@ -0,0 +1,36 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { Event, Emitter } from 'vs/base/common/event';
+import { createDecorator } from 'vs/platform/instantiation/common/instantiation';
+import { registerSingleton } from 'vs/platform/instantiation/common/extensions';
+
+export const IRemoteExplorerService = createDecorator<IRemoteExplorerService>('remoteExplorerService');
+
+export interface IRemoteExplorerService {
+	_serviceBrand: undefined;
+	onDidChangeTargetType: Event<string>;
+	targetType: string;
+}
+
+class RemoteExplorerService implements IRemoteExplorerService {
+	public _serviceBrand: undefined;
+	private _targetType: string = '';
+	private _onDidChangeTargetType: Emitter<string> = new Emitter<string>();
+	public onDidChangeTargetType: Event<string> = this._onDidChangeTargetType.event;
+
+	set targetType(name: string) {
+		if (this._targetType !== name) {
+			const oldTarget = this._targetType;
+			this._targetType = name;
+			this._onDidChangeTargetType.fire(oldTarget);
+		}
+	}
+	get targetType(): string {
+		return this._targetType;
+	}
+}
+
+registerSingleton(IRemoteExplorerService, RemoteExplorerService, true);
