33a6efa7fd0147d2d4ca15351eb357ec5b8827ea
Johannes Rieken <johannes.rieken@gmail.com>
2019-11-11 17:58:31 +0100

    navigator work

--- src/vs/editor/contrib/documentSymbols/outlineNavigation.ts
+++ src/vs/editor/contrib/documentSymbols/outlineNavigation.ts
@@ -6,7 +6,7 @@
 import 'vs/css!./media/outlineTree';
 import 'vs/css!./media/symbol-icons';
 import { Range } from 'vs/editor/common/core/range';
-import { OutlineElement, OutlineModel } from 'vs/editor/contrib/documentSymbols/outlineModel';
+import { OutlineElement, OutlineModel, TreeElement } from 'vs/editor/contrib/documentSymbols/outlineModel';
 import { localize } from 'vs/nls';
 import { IEditorContribution } from 'vs/editor/common/editorCommon';
 import { ICodeEditor } from 'vs/editor/browser/editorBrowser';
@@ -16,12 +16,15 @@ import { EditorAction, registerEditorAction, registerEditorContribution } from '
 import { ServicesAccessor } from 'vs/platform/instantiation/common/instantiation';
 import { EditorContextKeys } from 'vs/editor/common/editorContextKeys';
 import { values } from 'vs/base/common/collections';
+import { KeybindingWeight } from 'vs/platform/keybinding/common/keybindingsRegistry';
+import { KeyCode } from 'vs/base/common/keyCodes';
+import { KeyMod } from 'vs/editor/common/standalone/standaloneBase';
 
 class Navigator {
 
-	private static readonly _instances = new WeakMap<OutlineElement, Navigator>();
+	private static readonly _instances = new WeakMap<TreeElement, Navigator>();
 
-	static for(element: OutlineElement): Navigator {
+	static for(element: TreeElement): Navigator {
 		let res = this._instances.get(element);
 		if (!res) {
 			res = new Navigator(element);
@@ -30,65 +33,55 @@ class Navigator {
 		return res;
 	}
 
-	private readonly _children: OutlineElement[] = [];
+	private readonly _children: TreeElement[] = [];
 
-	private constructor(readonly element: OutlineElement) {
+	private constructor(readonly element: TreeElement) {
 		this._children = values(element.children).sort(Navigator._compare);
 	}
 
-	parent(): OutlineElement | undefined {
-		const { parent } = this.element;
-		return parent instanceof OutlineElement ? parent : undefined;
+	navigate(up: boolean): TreeElement | undefined {
+		return up ? this._up() : this._down();
 	}
 
-	firstChild(): OutlineElement | undefined {
-		return this._children[0];
-	}
-
-	lastChild(): OutlineElement | undefined {
-		return this._children[this._children.length - 1];
-	}
-
-	nextSibling(): OutlineElement | undefined {
-		const parent = this.parent();
-		if (!parent) {
-			return undefined;
+	private _up(): TreeElement | undefined {
+		const sibling = this._sibling(true);
+		if (sibling) {
+			return sibling._child(true) ?? sibling.element;
 		}
-		const parentNav = Navigator.for(parent);
-		const idx = parentNav._children.indexOf(this.element);
-		if (idx < 0 || idx + 1 >= parentNav._children.length) {
-			return undefined;
-		}
-		return parentNav._children[idx + 1];
-	}
-
-	previousSibling(): OutlineElement | undefined {
-		const parent = this.parent();
-		if (!parent) {
-			return undefined;
-		}
-		const parentNav = Navigator.for(parent);
-		const idx = parentNav._children.indexOf(this.element);
-		if (idx - 1 < 0) {
-			return undefined;
-		}
-		return parentNav._children[idx - 1];
-	}
-
-	navigate(next: boolean): OutlineElement | undefined {
-		return next ? this._navNext() : this._navPrev();
-	}
-
-	private _navNext(): OutlineElement | undefined {
 		return undefined;
 	}
 
-	private _navPrev(): OutlineElement | undefined {
+	private _down(): TreeElement | undefined {
+		const firstChild = this._child(false);
+		if (firstChild) {
+			return firstChild;
+		}
+		const sibling = this._sibling(false);
+		if (sibling) {
+			return sibling.element;
+		}
 		return undefined;
 	}
 
-	private static _compare(a: OutlineElement, b: OutlineElement): number {
-		return Range.compareRangesUsingStarts(a.symbol.range, b.symbol.range);
+	private _sibling(up: boolean): Navigator | undefined {
+		if (!this.element.parent) {
+			return undefined;
+		}
+		const parent = Navigator.for(this.element.parent);
+		const idx = parent._children.indexOf(this.element);
+		const nexIdx = idx + (up ? -1 : +1);
+		const element = parent._children[nexIdx];
+		return element && Navigator.for(element);
+	}
+
+	private _child(last: boolean): TreeElement | undefined {
+		return this._children[last ? this._children.length - 1 : 0];
+	}
+
+	private static _compare(a: TreeElement, b: TreeElement): number {
+		return (a instanceof OutlineElement && b instanceof OutlineElement)
+			? Range.compareRangesUsingStarts(a.symbol.range, b.symbol.range)
+			: 0;
 	}
 }
 
@@ -116,7 +109,7 @@ export class OutlineNavigation implements IEditorContribution {
 		}
 	}
 
-	async goto(next: boolean) {
+	async goto(up: boolean) {
 
 		if (this._cts) {
 			this._cts.cancel();
@@ -139,9 +132,9 @@ export class OutlineNavigation implements IEditorContribution {
 		}
 
 		let nav = Navigator.for(element);
-		let nextElement = nav.navigate(next);
+		let nextElement = nav.navigate(up);
 
-		if (nextElement) {
+		if (nextElement instanceof OutlineElement) {
 			this._editor.setPosition(Range.lift(nextElement.symbol.selectionRange).getStartPosition());
 		}
 	}
@@ -158,11 +151,20 @@ registerEditorAction(class extends EditorAction {
 			label: localize('label.next', "Go to Next Symbol"),
 			alias: 'Go to Next Symbol',
 			precondition: EditorContextKeys.hasDocumentSymbolProvider,
+			kbOpts: {
+				weight: KeybindingWeight.EditorContrib,
+				kbExpr: EditorContextKeys.focus,
+				primary: KeyMod.Shift | KeyMod.Alt | KeyCode.DownArrow,
+				mac: {
+					primary: KeyMod.CtrlCmd | KeyMod.WinCtrl | KeyMod.Shift | KeyCode.DownArrow,
+					secondary: [KeyMod.WinCtrl | KeyMod.Shift | KeyCode.DownArrow],
+				},
+			}
 		});
 	}
 
 	run(_accessor: ServicesAccessor, editor: ICodeEditor): void | Promise<void> {
-		OutlineNavigation.get(editor).goto(true);
+		OutlineNavigation.get(editor).goto(false);
 	}
 });
 
@@ -174,10 +176,19 @@ registerEditorAction(class extends EditorAction {
 			label: localize('label.prev', "Go to Previous Symbol"),
 			alias: 'Go to Previous Symbol',
 			precondition: EditorContextKeys.hasDocumentSymbolProvider,
+			kbOpts: {
+				weight: KeybindingWeight.EditorContrib,
+				kbExpr: EditorContextKeys.focus,
+				primary: KeyMod.Shift | KeyMod.Alt | KeyCode.UpArrow,
+				mac: {
+					primary: KeyMod.CtrlCmd | KeyMod.WinCtrl | KeyMod.Shift | KeyCode.UpArrow,
+					secondary: [KeyMod.WinCtrl | KeyMod.Shift | KeyCode.UpArrow],
+				},
+			}
 		});
 	}
 
 	run(_accessor: ServicesAccessor, editor: ICodeEditor): void | Promise<void> {
-		OutlineNavigation.get(editor).goto(false);
+		OutlineNavigation.get(editor).goto(true);
 	}
 });
