65159d001a60c01985666dbdde7b14ba24262399
Johannes Rieken <johannes.rieken@gmail.com>
2019-11-14 11:55:52 +0100

    terminal link handler using URL, #74604

--- src/vs/workbench/contrib/terminal/browser/terminalLinkHandler.ts
+++ src/vs/workbench/contrib/terminal/browser/terminalLinkHandler.ts
@@ -266,8 +266,8 @@ export class TerminalLinkHandler {
 	}
 
 	private _handleHypertextLink(url: string): void {
-		const uri = URI.parse(url);
-		this._openerService.open(uri, { allowTunneling: !!(this._processManager && this._processManager.remoteAuthority) });
+		const urlObj = new URL(url);
+		this._openerService.open(urlObj, { allowTunneling: !!(this._processManager && this._processManager.remoteAuthority) });
 	}
 
 	private _isLinkActivationModifierDown(event: MouseEvent): boolean {
