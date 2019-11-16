6549265ca53b448227b7f4d0790ea88ca5d90c8c
Matt Bierner <matb@microsoft.com>
2019-11-15 18:23:30 -0800

    Wording

--- src/vs/vscode.d.ts
+++ src/vs/vscode.d.ts
@@ -2009,9 +2009,9 @@ declare module 'vscode' {
 		/**
 		 * Base kind for source actions: `source`
 		 *
-		 * Source code actions apply to the entire file. Source actions must be explicitly requested and will not show in the
-		 * normal [light bulb](https://code.visualstudio.com/docs/editor/editingevolved#_code-action) menu. They can be run on save
-		 * using `editor.codeActionsOnSave` and are also shown in `source` context menu.
+		 * Source code actions apply to the entire file. They must be explicitly requested and will not show in the
+		 * normal [light bulb](https://code.visualstudio.com/docs/editor/editingevolved#_code-action) menu. Source actions
+		 * can be run on save using `editor.codeActionsOnSave` and are also shown in the `source` context menu.
 		 */
 		static readonly Source: CodeActionKind;
 
