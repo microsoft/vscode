247907cde182440bcb237ee0b20b9cba6a3127a0
isidor <inikolic@microsoft.com>
2019-11-20 18:32:22 +0100

    fix Debug console stop listening output
    fixes #83906

--- src/vs/workbench/contrib/debug/common/replModel.ts
+++ src/vs/workbench/contrib/debug/common/replModel.ts
@@ -10,7 +10,7 @@ import { ExpressionContainer } from 'vs/workbench/contrib/debug/common/debugMode
 import { isString, isUndefinedOrNull, isObject } from 'vs/base/common/types';
 import { basenameOrAuthority } from 'vs/base/common/resources';
 import { URI } from 'vs/base/common/uri';
-import { endsWith } from 'vs/base/common/strings';
+import { endsWith, startsWith } from 'vs/base/common/strings';
 import { generateUuid } from 'vs/base/common/uuid';
 import { Emitter } from 'vs/base/common/event';
 
@@ -134,8 +134,9 @@ export class ReplModel {
 
 		if (typeof data === 'string') {
 			const previousElement = this.replElements.length ? this.replElements[this.replElements.length - 1] : undefined;
-			if (previousElement instanceof SimpleReplElement && previousElement.severity === sev && !endsWith(previousElement.value, '\n') && !endsWith(previousElement.value, '\r\n')) {
+			if (!startsWith(data, '\n') && previousElement instanceof SimpleReplElement && previousElement.severity === sev && !endsWith(previousElement.value, '\n') && !endsWith(previousElement.value, '\r\n')) {
 				previousElement.value += data;
+				this._onDidChangeElements.fire();
 			} else {
 				const element = new SimpleReplElement(session, `topReplElement:${topReplElementCounter++}`, data, sev, source);
 				this.addReplElement(element);
