9c8b2f2f08e03e93a79c6a643b9b43edb4e41a86
Benjamin Pasero <benjpas@microsoft.com>
2019-11-21 08:52:50 +0100

    remove leftover console.log

--- src/vs/workbench/browser/parts/editor/noTabsTitleControl.ts
+++ src/vs/workbench/browser/parts/editor/noTabsTitleControl.ts
@@ -89,10 +89,7 @@ export class NoTabsTitleControl extends TitleControl {
 		EventHelper.stop(e, false);
 
 		// delayed to let the onTitleClick() come first which can cause a focus change which can close quick open
-		setTimeout(() => {
-			console.log('quick open show');
-			this.quickOpenService.show();
-		});
+		setTimeout(() => this.quickOpenService.show());
 	}
 
 	private onTitleDoubleClick(e: MouseEvent): void {
@@ -117,9 +114,7 @@ export class NoTabsTitleControl extends TitleControl {
 			// gesture tap should open the quick open
 			// editorGroupView will focus on the editor again when there are mouse/pointer/touch down events
 			// we need to wait a bit as `GesureEvent.Tap` is generated from `touchstart` and then `touchend` evnets, which are not an atom event.
-			setTimeout(() => {
-				this.quickOpenService.show();
-			}, 50);
+			setTimeout(() => this.quickOpenService.show(), 50);
 		}
 	}
 
