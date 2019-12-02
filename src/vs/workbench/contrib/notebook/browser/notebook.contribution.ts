b56d0af57e2ba0eaa5071fe3a674a5ed8c795903
Peng Lyu <penn.lv@gmail.com>
2019-12-02 10:15:15 -0800

    remove unused imports.

--- src/vs/workbench/contrib/notebook/browser/notebook.contribution.ts
+++ src/vs/workbench/contrib/notebook/browser/notebook.contribution.ts
@@ -3,17 +3,13 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import * as nls from 'vs/nls';
 import { SyncDescriptor } from 'vs/platform/instantiation/common/descriptors';
 import { Registry } from 'vs/platform/registry/common/platform';
 import { EditorDescriptor, Extensions as EditorExtensions, IEditorRegistry } from 'vs/workbench/browser/editor';
 import { NotebookEditor } from 'vs/workbench/contrib/notebook/browser/notebookEditor';
 import { NotebookEditorInput } from 'vs/workbench/contrib/notebook/browser/notebookEditorInput';
-import { Action } from 'vs/base/common/actions';
 import { IEditorService, IOpenEditorOverride } from 'vs/workbench/services/editor/common/editorService';
 import { IInstantiationService } from 'vs/platform/instantiation/common/instantiation';
-import { IWorkbenchActionRegistry, Extensions } from 'vs/workbench/common/actions';
-import { SyncActionDescriptor } from 'vs/platform/actions/common/actions';
 import { Extensions as WorkbenchExtensions, IWorkbenchContribution, IWorkbenchContributionsRegistry } from 'vs/workbench/common/contributions';
 import { IDisposable } from 'vs/base/common/lifecycle';
 import { IEditorInput } from 'vs/workbench/common/editor';
@@ -21,7 +17,6 @@ import { IEditorOptions, ITextEditorOptions } from 'vs/platform/editor/common/ed
 import { endsWith } from 'vs/base/common/strings';
 import { IEditorGroup } from 'vs/workbench/services/editor/common/editorGroupsService';
 import { LifecyclePhase } from 'vs/platform/lifecycle/common/lifecycle';
-import { URI } from 'vs/base/common/uri';
 
 Registry.as<IEditorRegistry>(EditorExtensions.Editors).registerEditor(
 	EditorDescriptor.create(
