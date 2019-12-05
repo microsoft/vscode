ac3fab27bdc63be0825faf52f2e27ee359cc8c14
isidor <inikolic@microsoft.com>
2019-12-05 16:11:40 +0100

    fixes #86408

--- src/vs/workbench/contrib/files/browser/views/explorerView.ts
+++ src/vs/workbench/contrib/files/browser/views/explorerView.ts
@@ -297,7 +297,10 @@ export class ExplorerView extends ViewletPane {
 
 		for (const stat of this.tree.getSelection()) {
 			const controller = this.renderer.getCompressedNavigationController(stat);
-			if (controller && focusedStat && controller === this.compressedNavigationController && stat !== focusedStat) {
+			if (controller && focusedStat && controller === this.compressedNavigationController) {
+				if (stat === focusedStat) {
+					selectedStats.push(stat);
+				}
 				// Ignore stats which are selected but are part of the same compact node as the focused stat
 				continue;
 			}
