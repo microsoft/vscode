ccf9aeadc83bf0f38e96c94c208122cf6bb047a2
Sandeep Somavarapu <sasomava@microsoft.com>
2019-12-09 12:30:52 +0100

    Fix #85331 #86402

--- src/vs/workbench/contrib/userDataSync/electron-browser/userDataSync.contribution.ts
+++ src/vs/workbench/contrib/userDataSync/electron-browser/userDataSync.contribution.ts
@@ -4,21 +4,18 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { IWorkbenchContributionsRegistry, Extensions as WorkbenchExtensions, IWorkbenchContribution } from 'vs/workbench/common/contributions';
-import { ISettingsMergeService, IUserDataSyncUtilService } from 'vs/platform/userDataSync/common/userDataSync';
+import { IUserDataSyncUtilService } from 'vs/platform/userDataSync/common/userDataSync';
 import { Registry } from 'vs/platform/registry/common/platform';
 import { LifecyclePhase } from 'vs/platform/lifecycle/common/lifecycle';
 import { ISharedProcessService } from 'vs/platform/ipc/electron-browser/sharedProcessService';
-import { SettingsMergeChannel } from 'vs/platform/userDataSync/common/settingsSyncIpc';
 import { UserDataSycnUtilServiceChannel } from 'vs/platform/userDataSync/common/keybindingsSyncIpc';
 
 class UserDataSyncServicesContribution implements IWorkbenchContribution {
 
 	constructor(
-		@ISettingsMergeService settingsMergeService: ISettingsMergeService,
 		@IUserDataSyncUtilService userDataSyncUtilService: IUserDataSyncUtilService,
 		@ISharedProcessService sharedProcessService: ISharedProcessService,
 	) {
-		sharedProcessService.registerChannel('settingsMerge', new SettingsMergeChannel(settingsMergeService));
 		sharedProcessService.registerChannel('userDataSyncUtil', new UserDataSycnUtilServiceChannel(userDataSyncUtilService));
 	}
 }
