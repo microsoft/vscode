333961d3c37090b4ff2d066327d64a96acc9824c
Benjamin Pasero <benjpas@microsoft.com>
2019-12-10 10:58:07 +0100

    :lipstick:

--- src/vs/workbench/common/editor.ts
+++ src/vs/workbench/common/editor.ts
@@ -413,11 +413,6 @@ export interface IEditorInput extends IDisposable {
 	saveAs(groupId: GroupIdentifier, options?: ISaveOptions): Promise<boolean>;
 
 	/**
-	 * Handles when the input is replaced, such as by renaming its backing resource.
-	 */
-	handleMove?(groupId: GroupIdentifier, uri: URI, options?: ITextEditorOptions): IEditorInput | undefined;
-
-	/**
 	 * Reverts this input.
 	 */
 	revert(options?: IRevertOptions): Promise<boolean>;
