cf9219150209e744620967f9c00fddc1a36ef535
Andre Weinand <aweinand@microsoft.com>
2019-12-11 16:40:54 +0100

    optionally load vs da

--- src/vs/platform/sign/node/signService.ts
+++ src/vs/platform/sign/node/signService.ts
@@ -5,10 +5,31 @@
 
 import { ISignService } from 'vs/platform/sign/common/sign';
 
+/* tslint:disable */
+
+declare module vsda {
+	export class signer {
+		sign(arg: any): any;
+	}
+}
+
 export class SignService implements ISignService {
 	_serviceBrand: undefined;
 
+	private vsda(): Promise<typeof vsda> {
+		return new Promise((resolve, reject) => require(['vsda'], resolve, reject));
+	}
+
 	async sign(value: string): Promise<string> {
+		try {
+			const vsda = await this.vsda();
+			const signer = new vsda.signer();
+			if (signer) {
+				return signer.sign(value);
+			}
+		} catch (e) {
+			// ignore errors silently
+		}
 		return value;
 	}
 }
