7047ff922e3fc4131067d9969ff0f242b214267f
isidor <inikolic@microsoft.com>
2019-12-11 11:10:27 +0100

    fixes #83736

--- src/vs/workbench/contrib/files/browser/views/explorerViewer.ts
+++ src/vs/workbench/contrib/files/browser/views/explorerViewer.ts
@@ -610,7 +610,7 @@ export class FileSorter implements ITreeSorter<ExplorerItem> {
 	}
 }
 
-const fileOverwriteConfirm = (name: string) => {
+const getFileOverwriteConfirm = (name: string) => {
 	return <IConfirmation>{
 		message: localize('confirmOverwrite', "A file or folder with the name '{0}' already exists in the destination folder. Do you want to replace it?", name),
 		detail: localize('irreversible', "This action is irreversible!"),
@@ -853,7 +853,7 @@ export class FileDragAndDrop implements ITreeDragAndDrop<ExplorerItem> {
 				const name = file.name;
 				if (typeof name === 'string' && event.target?.result instanceof ArrayBuffer) {
 					if (target.getChild(name)) {
-						const { confirmed } = await this.dialogService.confirm(fileOverwriteConfirm(name));
+						const { confirmed } = await this.dialogService.confirm(getFileOverwriteConfirm(name));
 						if (!confirmed) {
 							return;
 						}
@@ -926,18 +926,16 @@ export class FileDragAndDrop implements ITreeDragAndDrop<ExplorerItem> {
 				});
 			}
 
-			const filtered = resources.filter(resource => targetNames.has(!hasToIgnoreCase(resource) ? basename(resource) : basename(resource).toLowerCase()));
-			const resourceExists = filtered.length >= 1;
-			if (resourceExists) {
-				const confirmationResult = await this.dialogService.confirm(fileOverwriteConfirm(basename(filtered[0])));
-				if (!confirmationResult.confirmed) {
-					return;
-				}
-			}
-
 			// Run add in sequence
 			const addPromisesFactory: ITask<Promise<void>>[] = [];
-			resources.forEach(resource => {
+			await Promise.all(resources.map(async resource => {
+				if (targetNames.has(!hasToIgnoreCase(resource) ? basename(resource) : basename(resource).toLowerCase())) {
+					const confirmationResult = await this.dialogService.confirm(getFileOverwriteConfirm(basename(resource)));
+					if (!confirmationResult.confirmed) {
+						return;
+					}
+				}
+
 				addPromisesFactory.push(async () => {
 					const sourceFile = resource;
 					const targetFile = joinPath(target.resource, basename(sourceFile));
@@ -956,7 +954,7 @@ export class FileDragAndDrop implements ITreeDragAndDrop<ExplorerItem> {
 						this.editorService.openEditor({ resource: stat.resource, options: { pinned: true } });
 					}
 				});
-			});
+			}));
 
 			await sequence(addPromisesFactory);
 		}
@@ -1053,13 +1051,7 @@ export class FileDragAndDrop implements ITreeDragAndDrop<ExplorerItem> {
 		} catch (error) {
 			// Conflict
 			if ((<FileOperationError>error).fileOperationResult === FileOperationResult.FILE_MOVE_CONFLICT) {
-				const confirm: IConfirmation = {
-					message: localize('confirmOverwriteMessage', "'{0}' already exists in the destination folder. Do you want to replace it?", source.name),
-					detail: localize('irreversible', "This action is irreversible!"),
-					primaryButton: localize({ key: 'replaceButtonLabel', comment: ['&& denotes a mnemonic'] }, "&&Replace"),
-					type: 'warning'
-				};
-
+				const confirm = getFileOverwriteConfirm(source.name);
 				// Move with overwrite if the user confirms
 				const { confirmed } = await this.dialogService.confirm(confirm);
 				if (confirmed) {
