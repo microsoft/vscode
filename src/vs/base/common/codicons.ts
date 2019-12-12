dc592aab90e021d50a0ae4ae072e3ca6435458c3
Eric Amodio <eamodio@microsoft.com>
2019-12-12 13:42:40 -0500

    Removes negative look-behind, not supported in FF

--- src/vs/base/common/codicons.ts
+++ src/vs/base/common/codicons.ts
@@ -3,9 +3,9 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-const escapeCodiconsRegex = /(?<!\\)\$\([a-z0-9\-]+?(?:~[a-z0-9\-]*?)?\)/gi;
+const escapeCodiconsRegex = /(\\)?\$\([a-z0-9\-]+?(?:~[a-z0-9\-]*?)?\)/gi;
 export function escapeCodicons(text: string): string {
-	return text.replace(escapeCodiconsRegex, match => `\\${match}`);
+	return text.replace(escapeCodiconsRegex, (match, escaped) => escaped ? match : `\\${match}`);
 }
 
 const markdownEscapedCodiconsRegex = /\\\$\([a-z0-9\-]+?(?:~[a-z0-9\-]*?)?\)/gi;
@@ -14,15 +14,15 @@ export function markdownEscapeEscapedCodicons(text: string): string {
 	return text.replace(markdownEscapedCodiconsRegex, match => `\\${match}`);
 }
 
-const markdownUnescapeCodiconsRegex = /(?<!\\)\$\\\(([a-z0-9\-]+?(?:~[a-z0-9\-]*?)?)\\\)/gi;
+const markdownUnescapeCodiconsRegex = /(\\)?\$\\\(([a-z0-9\-]+?(?:~[a-z0-9\-]*?)?)\\\)/gi;
 export function markdownUnescapeCodicons(text: string): string {
-	return text.replace(markdownUnescapeCodiconsRegex, (_, codicon) => `$(${codicon})`);
+	return text.replace(markdownUnescapeCodiconsRegex, (match, escaped, codicon) => escaped ? match : `$(${codicon})`);
 }
 
 const renderCodiconsRegex = /(\\)?\$\((([a-z0-9\-]+?)(?:~([a-z0-9\-]*?))?)\)/gi;
 export function renderCodicons(text: string): string {
-	return text.replace(renderCodiconsRegex, (_, escape, codicon, name, animation) => {
-		return escape
+	return text.replace(renderCodiconsRegex, (_, escaped, codicon, name, animation) => {
+		return escaped
 			? `$(${codicon})`
 			: `<span class="codicon codicon-${name}${animation ? ` codicon-animation-${animation}` : ''}"></span>`;
 	});
