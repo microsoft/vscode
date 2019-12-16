b489bc1cb0360a6892115ba4b87daadc1943f6f3
Sandeep Somavarapu <sasomava@microsoft.com>
2019-12-16 16:37:21 +0100

    deprecate using resource

--- src/vs/vscode.d.ts
+++ src/vs/vscode.d.ts
@@ -8708,13 +8708,28 @@ declare module 'vscode' {
 		 * is returned. Dots in the section-identifier are interpreted as child-access,
 		 * like `{ myExt: { setting: { doIt: true }}}` and `getConfiguration('myExt.setting').get('doIt') === true`.
 		 *
+		 * @param section A dot-separated identifier.
+		 * @return The full configuration or a subset.
+		 */
+		export function getConfiguration(section?: string): WorkspaceConfiguration;
+
+		/**
+		 *
+		 *
+		 * Get a workspace configuration object.
+		 *
+		 * When a section-identifier is provided only that part of the configuration
+		 * is returned. Dots in the section-identifier are interpreted as child-access,
+		 * like `{ myExt: { setting: { doIt: true }}}` and `getConfiguration('myExt.setting').get('doIt') === true`.
+		 *
 		 * When a scope is provided, configuration confined to that scope is returned.
 		 *
+		 * @deprecated Pass [workspaceFolder](#WorkspaceFolder) instead of [resource](#Uri)
 		 * @param section A dot-separated identifier.
 		 * @param scope A scope for which the configuration is asked for.
 		 * @return The full configuration or a subset.
 		 */
-		export function getConfiguration(section?: string, resource?: Uri | null): WorkspaceConfiguration;
+		export function getConfiguration(section: string | undefined, resource: Uri | null): WorkspaceConfiguration;
 
 		/**
 		 * Get a workspace folder configuration object for the given workspace folder.
