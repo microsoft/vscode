77c83114d7f51b0f723f110b87468a495071e352
Sandeep Somavarapu <sasomava@microsoft.com>
2019-12-16 16:49:03 +0100

    one more version to read configuration

--- src/vs/vscode.d.ts
+++ src/vs/vscode.d.ts
@@ -8712,24 +8712,7 @@ declare module 'vscode' {
 		 * @return The full configuration or a subset.
 		 */
 		export function getConfiguration(section?: string): WorkspaceConfiguration;
-
-		/**
-		 *
-		 *
-		 * Get a workspace configuration object.
-		 *
-		 * When a section-identifier is provided only that part of the configuration
-		 * is returned. Dots in the section-identifier are interpreted as child-access,
-		 * like `{ myExt: { setting: { doIt: true }}}` and `getConfiguration('myExt.setting').get('doIt') === true`.
-		 *
-		 * When a scope is provided, configuration confined to that scope is returned.
-		 *
-		 * @deprecated Pass [workspaceFolder](#WorkspaceFolder) instead of [resource](#Uri)
-		 * @param section A dot-separated identifier.
-		 * @param scope A scope for which the configuration is asked for.
-		 * @return The full configuration or a subset.
-		 */
-		export function getConfiguration(section: string | undefined, resource: Uri | null): WorkspaceConfiguration;
+		export function getConfiguration(section: string | undefined, scope: { resource: Uri, language?: string }): WorkspaceConfiguration;
 
 		/**
 		 * Get a workspace folder configuration object for the given workspace folder.
@@ -8756,6 +8739,24 @@ declare module 'vscode' {
 		 * @return The full configuration or a subset for the given text document
 		 */
 		export function getConfiguration(section: string | undefined, textDocument: TextDocument): TextDocumentConfiguration;
+
+		/**
+		 * Get a workspace configuration object for the given scope.
+		 *
+		 * When a section-identifier is provided only that part of the configuration
+		 * is returned. Dots in the section-identifier are interpreted as child-access,
+		 * like `{ myExt: { setting: { doIt: true }}}` and `getConfiguration('myExt.setting').get('doIt') === true`.
+		 *
+		 * @param section A dot-separated identifier.
+		 * @param scope A scope for which the configuration is confied to.
+		 * @return The full configuration or a subset.
+		 */
+		export function getConfiguration(section: string | undefined, scope: { resource: Uri, language?: string }): WorkspaceConfiguration;
+
+		/**
+		 * @deprecated Pass [workspaceFolder](#WorkspaceFolder) instead of [resource](#Uri)
+		 */
+		export function getConfiguration(section: string | undefined, resource: Uri | null): WorkspaceConfiguration;
 
 		/**
 		 * An event that is emitted when the [configuration](#WorkspaceConfiguration) changed.
