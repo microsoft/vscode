88304ad491ea307bf08f50bea8f530cbd9f99e09
Daniel Imms <tyriar@tyriar.com>
2019-12-30 14:58:51 -0800

    Tweak working on min contrast ratio
    Fixes #86426

--- src/vs/workbench/contrib/terminal/browser/terminal.contribution.ts
+++ src/vs/workbench/contrib/terminal/browser/terminal.contribution.ts
@@ -170,7 +170,7 @@ configurationRegistry.registerConfiguration({
 			default: DEFAULT_LINE_HEIGHT
 		},
 		'terminal.integrated.minimumContrastRatio': {
-			markdownDescription: nls.localize('terminal.integrated.minimumContrastRatio', "When set the foreground color of each cell will change to try meet the contrast ratio specified. Example values:\n\n- 1: The default, do nothing.\n- 4.5: Minimum for [WCAG AA compliance](https://www.w3.org/TR/UNDERSTANDING-WCAG20/visual-audio-contrast-contrast.html).\n- 7: Minimum for [WCAG AAA compliance](https://www.w3.org/TR/UNDERSTANDING-WCAG20/visual-audio-contrast7.html).\n- 21: White on black or black on white."),
+			markdownDescription: nls.localize('terminal.integrated.minimumContrastRatio', "When set the foreground color of each cell will change to try meet the contrast ratio specified. Example values:\n\n- 1: The default, do nothing.\n- 4.5: [WCAG AA compliance (minimum)](https://www.w3.org/TR/UNDERSTANDING-WCAG20/visual-audio-contrast-contrast.html).\n- 7: [WCAG AAA compliance (enhanced)](https://www.w3.org/TR/UNDERSTANDING-WCAG20/visual-audio-contrast7.html).\n- 21: White on black or black on white."),
 			type: 'number',
 			default: 1
 		},
