93bea490b0e6117e83d5e38db0a98d453f69b1bd
Ron Buckton <ron.buckton@microsoft.com>
2020-01-06 13:37:49 -0800

    Add call hierarchy support for TypeScript 3.8

---
+++ extensions/typescript-language-features/src/features/callHierarchy.ts
@@ -0,0 +1,72 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import * as vscode from 'vscode';
+import { ITypeScriptServiceClient } from '../typescriptService';
+import * as typeConverters from '../utils/typeConverters';
+
+class TypeScriptCallHierarchySupport implements vscode.CallHierarchyProvider {
+	public constructor(
+		private readonly client: ITypeScriptServiceClient) { }
+
+	public async prepareCallHierarchy(
+		document: vscode.TextDocument,
+		position: vscode.Position,
+		token: vscode.CancellationToken
+	): Promise<vscode.CallHierarchyItem | vscode.CallHierarchyItem[] | undefined> {
+		const filepath = this.client.toOpenedFilePath(document);
+		if (!filepath) {
+			return undefined;
+		}
+
+		const args = typeConverters.Position.toFileLocationRequestArgs(filepath, position);
+		const response = await this.client.execute('prepareCallHierarchy', args, token);
+		if (response.type !== 'response' || !response.body) {
+			return undefined;
+		}
+
+		return Array.isArray(response.body)
+			? response.body.map(typeConverters.CallHierarchyItem.fromProtocolCallHierarchyItem)
+			: typeConverters.CallHierarchyItem.fromProtocolCallHierarchyItem(response.body);
+	}
+
+	public async provideCallHierarchyIncomingCalls(item: vscode.CallHierarchyItem, token: vscode.CancellationToken): Promise<vscode.CallHierarchyIncomingCall[] | undefined> {
+		const filepath = this.client.toPath(item.uri);
+		if (!filepath) {
+			return undefined;
+		}
+
+		const args = typeConverters.Position.toFileLocationRequestArgs(filepath, item.selectionRange.start);
+		const response = await this.client.execute('provideCallHierarchyIncomingCalls', args, token);
+		if (response.type !== 'response' || !response.body) {
+			return undefined;
+		}
+
+		return response.body.map(typeConverters.CallHierarchyIncomingCall.fromProtocolCallHierchyIncomingCall);
+	}
+
+	public async provideCallHierarchyOutgoingCalls(item: vscode.CallHierarchyItem, token: vscode.CancellationToken): Promise<vscode.CallHierarchyOutgoingCall[] | undefined> {
+		const filepath = this.client.toPath(item.uri);
+		if (!filepath) {
+			return undefined;
+		}
+
+		const args = typeConverters.Position.toFileLocationRequestArgs(filepath, item.selectionRange.start);
+		const response = await this.client.execute('provideCallHierarchyOutgoingCalls', args, token);
+		if (response.type !== 'response' || !response.body) {
+			return undefined;
+		}
+
+		return response.body.map(typeConverters.CallHierarchyOutgoingCall.fromProtocolCallHierchyOutgoingCall);
+	}
+}
+
+export function register(
+	selector: vscode.DocumentSelector,
+	client: ITypeScriptServiceClient
+) {
+	return vscode.languages.registerCallHierarchyProvider(selector,
+		new TypeScriptCallHierarchySupport(client));
+}
--- extensions/typescript-language-features/src/utils/typeConverters.ts
+++ extensions/typescript-language-features/src/utils/typeConverters.ts
@@ -7,13 +7,20 @@
  * Helpers for converting FROM vscode types TO ts types
  */
 
+import * as path from 'path';
 import * as vscode from 'vscode';
 import * as Proto from '../protocol';
+import * as PConst from '../protocol.const';
 import { ITypeScriptServiceClient } from '../typescriptService';
 
 export namespace Range {
 	export const fromTextSpan = (span: Proto.TextSpan): vscode.Range =>
 		fromLocations(span.start, span.end);
+
+	export const toTextSpan = (range: vscode.Range): Proto.TextSpan => ({
+		start: Position.toLocation(range.start),
+		end: Position.toLocation(range.end)
+	});
 
 	export const fromLocations = (start: Proto.Location, end: Proto.Location): vscode.Range =>
 		new vscode.Range(
@@ -88,5 +95,73 @@ export namespace WorkspaceEdit {
 		}
 
 		return workspaceEdit;
+	}
+}
+
+export namespace SymbolKind {
+	export function fromProtocolScriptElementKind(kind: Proto.ScriptElementKind) {
+		switch (kind) {
+			case PConst.Kind.module: return vscode.SymbolKind.Module;
+			case PConst.Kind.class: return vscode.SymbolKind.Class;
+			case PConst.Kind.enum: return vscode.SymbolKind.Enum;
+			case PConst.Kind.enumMember: return vscode.SymbolKind.EnumMember;
+			case PConst.Kind.interface: return vscode.SymbolKind.Interface;
+			case PConst.Kind.indexSignature: return vscode.SymbolKind.Method;
+			case PConst.Kind.callSignature: return vscode.SymbolKind.Method;
+			case PConst.Kind.memberFunction: return vscode.SymbolKind.Method;
+			case PConst.Kind.memberVariable: return vscode.SymbolKind.Property;
+			case PConst.Kind.memberGetAccessor: return vscode.SymbolKind.Property;
+			case PConst.Kind.memberSetAccessor: return vscode.SymbolKind.Property;
+			case PConst.Kind.variable: return vscode.SymbolKind.Variable;
+			case PConst.Kind.let: return vscode.SymbolKind.Variable;
+			case PConst.Kind.const: return vscode.SymbolKind.Variable;
+			case PConst.Kind.localVariable: return vscode.SymbolKind.Variable;
+			case PConst.Kind.alias: return vscode.SymbolKind.Variable;
+			case PConst.Kind.function: return vscode.SymbolKind.Function;
+			case PConst.Kind.localFunction: return vscode.SymbolKind.Function;
+			case PConst.Kind.constructSignature: return vscode.SymbolKind.Constructor;
+			case PConst.Kind.constructorImplementation: return vscode.SymbolKind.Constructor;
+			case PConst.Kind.typeParameter: return vscode.SymbolKind.TypeParameter;
+			case PConst.Kind.string: return vscode.SymbolKind.String;
+			default: return vscode.SymbolKind.Variable;
+		}
+	}
+}
+
+export namespace CallHierarchyItem {
+	function isSourceFileItem(item: Proto.CallHierarchyItem) {
+		return item.kind === PConst.Kind.script || item.kind === PConst.Kind.module && item.selectionSpan.start.line === 0 && item.selectionSpan.start.offset === 0;
+	}
+
+	export function fromProtocolCallHierarchyItem(item: Proto.CallHierarchyItem): vscode.CallHierarchyItem {
+		const useFileName = isSourceFileItem(item);
+		const name = useFileName ? path.basename(item.file) : item.name;
+		const detail = useFileName ? vscode.workspace.asRelativePath(path.dirname(item.file)) : '';
+		return new vscode.CallHierarchyItem(
+			SymbolKind.fromProtocolScriptElementKind(item.kind),
+			name,
+			detail,
+			vscode.Uri.file(item.file),
+			Range.fromTextSpan(item.span),
+			Range.fromTextSpan(item.selectionSpan)
+		);
+	}
+}
+
+export namespace CallHierarchyIncomingCall {
+	export function fromProtocolCallHierchyIncomingCall(item: Proto.CallHierarchyIncomingCall): vscode.CallHierarchyIncomingCall {
+		return new vscode.CallHierarchyIncomingCall(
+			CallHierarchyItem.fromProtocolCallHierarchyItem(item.from),
+			item.fromSpans.map(Range.fromTextSpan)
+		);
+	}
+}
+
+export namespace CallHierarchyOutgoingCall {
+	export function fromProtocolCallHierchyOutgoingCall(item: Proto.CallHierarchyOutgoingCall): vscode.CallHierarchyOutgoingCall {
+		return new vscode.CallHierarchyOutgoingCall(
+			CallHierarchyItem.fromProtocolCallHierarchyItem(item.to),
+			item.fromSpans.map(Range.fromTextSpan)
+		);
 	}
 }
