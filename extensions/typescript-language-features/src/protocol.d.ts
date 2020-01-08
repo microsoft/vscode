71b60d0d2201e5ba0a52d9bd2b914da2cee11ab5
Ron Buckton <ron.buckton@microsoft.com>
2020-01-08 14:35:44 -0800

    Add call hierarchy support for TypeScript 3.8 (#88168)
    * Add call hierarchy support for TypeScript 3.8
    
    * Add version dependent registration for call hierarchy provider
    
    * Revert TS version, PR feedback

--- extensions/typescript-language-features/src/protocol.d.ts
+++ extensions/typescript-language-features/src/protocol.d.ts
@@ -1,2 +1,54 @@
 import * as Proto from 'typescript/lib/protocol';
 export = Proto;
+
+declare module "typescript/lib/protocol" {
+	const enum CommandTypes {
+		PrepareCallHierarchy = "prepareCallHierarchy",
+		ProvideCallHierarchyIncomingCalls = "provideCallHierarchyIncomingCalls",
+		ProvideCallHierarchyOutgoingCalls = "provideCallHierarchyOutgoingCalls",
+	}
+
+	interface CallHierarchyItem {
+		name: string;
+		kind: ScriptElementKind;
+		file: string;
+		span: TextSpan;
+		selectionSpan: TextSpan;
+	}
+
+	interface CallHierarchyIncomingCall {
+		from: CallHierarchyItem;
+		fromSpans: TextSpan[];
+	}
+
+	interface CallHierarchyOutgoingCall {
+		to: CallHierarchyItem;
+		fromSpans: TextSpan[];
+	}
+
+	interface PrepareCallHierarchyRequest extends FileLocationRequest {
+		command: CommandTypes.PrepareCallHierarchy;
+	}
+
+	interface PrepareCallHierarchyResponse extends Response {
+		readonly body: CallHierarchyItem | CallHierarchyItem[];
+	}
+
+	interface ProvideCallHierarchyIncomingCallsRequest extends FileLocationRequest {
+		command: CommandTypes.ProvideCallHierarchyIncomingCalls;
+		kind: ScriptElementKind;
+	}
+
+	interface ProvideCallHierarchyIncomingCallsResponse extends Response {
+		readonly body: CallHierarchyIncomingCall[];
+	}
+
+	interface ProvideCallHierarchyOutgoingCallsRequest extends FileLocationRequest {
+		command: CommandTypes.ProvideCallHierarchyOutgoingCalls;
+		kind: ScriptElementKind;
+	}
+
+	interface ProvideCallHierarchyOutgoingCallsResponse extends Response {
+		readonly body: CallHierarchyOutgoingCall[];
+	}
+}
