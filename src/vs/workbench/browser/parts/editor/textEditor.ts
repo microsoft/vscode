5a851e036ecfb1515298c50cf8bc23424dfa89a5
Benjamin Pasero <benjpas@microsoft.com>
2020-01-21 08:12:26 +0100

    Editor should update aria label on editor name change (fix #88981)

--- src/vs/workbench/browser/parts/editor/textEditor.ts
+++ src/vs/workbench/browser/parts/editor/textEditor.ts
@@ -22,6 +22,7 @@ import { isCodeEditor, getCodeEditor } from 'vs/editor/browser/editorBrowser';
 import { IEditorGroupsService, IEditorGroup } from 'vs/workbench/services/editor/common/editorGroupsService';
 import { CancellationToken } from 'vs/base/common/cancellation';
 import { IEditorService } from 'vs/workbench/services/editor/common/editorService';
+import { IDisposable, dispose } from 'vs/base/common/lifecycle';
 
 export interface IEditorConfiguration {
 	editor: object;
@@ -41,13 +42,14 @@ export abstract class BaseTextEditor extends BaseEditor implements ITextEditor {
 	private hasPendingConfigurationChange: boolean | undefined;
 	private lastAppliedEditorOptions?: IEditorOptions;
 	private editorMemento: IEditorMemento<IEditorViewState>;
+	private inputDisposable: IDisposable | undefined;
 
 	constructor(
 		id: string,
 		@ITelemetryService telemetryService: ITelemetryService,
-		@IInstantiationService private readonly _instantiationService: IInstantiationService,
+		@IInstantiationService protected readonly instantiationService: IInstantiationService,
 		@IStorageService storageService: IStorageService,
-		@ITextResourceConfigurationService private readonly _configurationService: ITextResourceConfigurationService,
+		@ITextResourceConfigurationService protected readonly configurationService: ITextResourceConfigurationService,
 		@IThemeService protected themeService: IThemeService,
 		@IEditorService protected editorService: IEditorService,
 		@IEditorGroupsService protected editorGroupService: IEditorGroupsService
@@ -59,16 +61,9 @@ export abstract class BaseTextEditor extends BaseEditor implements ITextEditor {
 		this._register(this.configurationService.onDidChangeConfiguration(e => {
 			const resource = this.getResource();
 			const value = resource ? this.configurationService.getValue<IEditorConfiguration>(resource) : undefined;
+
 			return this.handleConfigurationChangeEvent(value);
 		}));
-	}
-
-	protected get instantiationService(): IInstantiationService {
-		return this._instantiationService;
-	}
-
-	protected get configurationService(): ITextResourceConfigurationService {
-		return this._configurationService;
 	}
 
 	protected handleConfigurationChangeEvent(configuration?: IEditorConfiguration): void {
@@ -102,10 +97,8 @@ export abstract class BaseTextEditor extends BaseEditor implements ITextEditor {
 		let ariaLabel = this.getAriaLabel();
 
 		// Apply group information to help identify in which group we are
-		if (ariaLabel) {
-			if (this.group) {
-				ariaLabel = localize('editorLabelWithGroup', "{0}, {1}", ariaLabel, this.group.ariaLabel);
-			}
+		if (ariaLabel && this.group) {
+			ariaLabel = localize('editorLabelWithGroup', "{0}, {1}", ariaLabel, this.group.ariaLabel);
 		}
 
 		return ariaLabel;
@@ -153,8 +146,13 @@ export abstract class BaseTextEditor extends BaseEditor implements ITextEditor {
 		// editor input specific options (e.g. an ARIA label depending on the input showing)
 		this.updateEditorConfiguration();
 
+		// Update aria label on editor
 		const editorContainer = assertIsDefined(this.editorContainer);
 		editorContainer.setAttribute('aria-label', this.computeAriaLabel());
+
+		// Keep aria label up to date whenever editor label changes
+		dispose(this.inputDisposable);
+		this.inputDisposable = input.onDidChangeLabel(() => editorContainer.setAttribute('aria-label', this.computeAriaLabel()));
 	}
 
 	setOptions(options: EditorOptions | undefined): void {
@@ -302,6 +300,12 @@ export abstract class BaseTextEditor extends BaseEditor implements ITextEditor {
 	}
 
 	protected abstract getAriaLabel(): string;
+
+	clearInput(): void {
+		super.clearInput();
+
+		this.inputDisposable = dispose(this.inputDisposable);
+	}
 
 	dispose(): void {
 		this.lastAppliedEditorOptions = undefined;
