e5cc6c8f05ce05b1d71e17bcc2da6602eb328f80
maddhruv <dhruvjainpenny@gmail.com>
2020-01-23 09:16:55 +0530

    feat: use engines-ok for engines check

--- build/npm/preinstall.js
+++ build/npm/preinstall.js
@@ -5,24 +5,6 @@
 
 let err = false;
 
-const majorNodeVersion = parseInt(/^(\d+)\./.exec(process.versions.node)[1]);
-
-if (majorNodeVersion < 10 || majorNodeVersion >= 13) {
-	console.error('\033[1;31m*** Please use node >=10 and <=12.\033[0;0m');
-	err = true;
-}
-
-const cp = require('child_process');
-const yarnVersion = cp.execSync('yarn -v', { encoding: 'utf8' }).trim();
-const parsedYarnVersion = /^(\d+)\.(\d+)\./.exec(yarnVersion);
-const majorYarnVersion = parseInt(parsedYarnVersion[1]);
-const minorYarnVersion = parseInt(parsedYarnVersion[2]);
-
-if (majorYarnVersion < 1 || minorYarnVersion < 10) {
-	console.error('\033[1;31m*** Please use yarn >=1.10.1.\033[0;0m');
-	err = true;
-}
-
 if (!/yarn\.js$|yarnpkg$/.test(process.env['npm_execpath'])) {
 	console.error('\033[1;31m*** Please use yarn to install dependencies.\033[0;0m');
 	err = true;
