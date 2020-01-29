dfbe168c2d7e0fd0767bcfbd9a7ac6b989f4da42
Johannes Rieken <johannes.rieken@gmail.com>
2020-01-29 11:15:45 +0100

    ensure parent is checked when checking child, ensure child can be checked when parent is unchecked, #89500

--- src/vs/workbench/contrib/bulkEdit/browser/bulkEditTree.ts
+++ src/vs/workbench/contrib/bulkEdit/browser/bulkEditTree.ts
@@ -145,7 +145,18 @@ export class TextEditElement implements ICheckable {
 		if (model instanceof CategoryElement) {
 			model = model.parent;
 		}
-		return model.checked.updateChecked(this.edit.textEdit, value);
+
+		// check/uncheck this element
+		model.checked.updateChecked(this.edit.textEdit, value);
+
+		// make sure parent is checked when this element is checked...
+		if (value) {
+			this.parent.edit.originalEdits.forEach(edit => {
+				if (WorkspaceFileEdit.is(edit)) {
+					(<BulkFileOperations>model).checked.updateChecked(edit, value);
+				}
+			});
+		}
 	}
 
 	isDisabled(): boolean {
@@ -518,13 +529,13 @@ class TextEditElementTemplate {
 	set(element: TextEditElement) {
 		this._localDisposables.clear();
 
+		this._localDisposables.add(dom.addDisposableListener(this._checkbox, 'change', e => {
+			element.setChecked(this._checkbox.checked);
+			e.preventDefault();
+		}));
 		if (element.parent.isChecked()) {
 			this._checkbox.checked = element.isChecked();
 			this._checkbox.disabled = element.isDisabled();
-			this._localDisposables.add(dom.addDisposableListener(this._checkbox, 'change', e => {
-				element.setChecked(this._checkbox.checked);
-				e.preventDefault();
-			}));
 		} else {
 			this._checkbox.checked = element.isChecked();
 			this._checkbox.disabled = element.isDisabled();
