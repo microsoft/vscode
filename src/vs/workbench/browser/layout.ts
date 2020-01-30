0b3f37fc060f2a34700260dbb01adc25f5cafc92
SteVen Batten <stbatt@microsoft.com>
2020-01-30 08:05:15 -0800

    fixes #89567

--- src/vs/workbench/browser/layout.ts
+++ src/vs/workbench/browser/layout.ts
@@ -485,12 +485,7 @@ export abstract class Layout extends Disposable implements IWorkbenchLayoutServi
 
 		// Panel to restore
 		if (!this.state.panel.hidden) {
-			const panelRegistry = Registry.as<PanelRegistry>(PanelExtensions.Panels);
-
-			let panelToRestore = this.storageService.get(PanelPart.activePanelSettingsKey, StorageScope.WORKSPACE, panelRegistry.getDefaultPanelId());
-			if (!panelRegistry.hasPanel(panelToRestore)) {
-				panelToRestore = panelRegistry.getDefaultPanelId(); // fallback to default if panel is unknown
-			}
+			let panelToRestore = this.storageService.get(PanelPart.activePanelSettingsKey, StorageScope.WORKSPACE, Registry.as<PanelRegistry>(PanelExtensions.Panels).getDefaultPanelId());
 
 			if (panelToRestore) {
 				this.state.panel.panelToRestore = panelToRestore;
