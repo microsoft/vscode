c3dcf4ab0ffc5388b8f116f5e9171109d06676c5
Sandeep Somavarapu <sasomava@microsoft.com>
2020-01-30 19:48:26 +0100

    Fix #89602

--- src/vs/workbench/contrib/userDataSync/browser/userDataSync.ts
+++ src/vs/workbench/contrib/userDataSync/browser/userDataSync.ts
@@ -433,7 +433,6 @@ export class UserDataSyncWorkbenchContribution extends Disposable implements IWo
 		switch (result.choice) {
 			case 0:
 				this.telemetryService.publicLog2<{ action: string }, FirstTimeSyncClassification>('sync/firstTimeSync', { action: 'merge' });
-				await this.userDataSyncService.sync();
 				break;
 			case 1:
 				this.telemetryService.publicLog2<{ action: string }, FirstTimeSyncClassification>('sync/firstTimeSync', { action: 'cancelled' });
