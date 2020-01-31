af69a4f2b1d7d796af904af31328ec8e85b66bd9
isidor <inikolic@microsoft.com>
2020-01-31 11:01:40 +0100

    fixes #89717

--- src/vs/editor/browser/controller/textAreaHandler.ts
+++ src/vs/editor/browser/controller/textAreaHandler.ts
@@ -357,8 +357,9 @@ export class TextAreaHandler extends ViewPart {
 		this._accessibilitySupport = options.get(EditorOption.accessibilitySupport);
 		const accessibilityPageSize = options.get(EditorOption.accessibilityPageSize);
 		if (this._accessibilitySupport === AccessibilitySupport.Enabled && accessibilityPageSize === EditorOptions.accessibilityPageSize.defaultValue) {
-			// If a screen reader is attached and the default value is not set we shuold automatically increase the page size to 1000 for a better experience
-			this._accessibilityPageSize = 1000;
+			// If a screen reader is attached and the default value is not set we shuold automatically increase the page size to 160 for a better experience
+			// If we put more than 160 lines the nvda can not handle this https://github.com/microsoft/vscode/issues/89717
+			this._accessibilityPageSize = 160;
 		} else {
 			this._accessibilityPageSize = accessibilityPageSize;
 		}
