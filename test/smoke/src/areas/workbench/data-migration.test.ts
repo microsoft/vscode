a15803b9e740f50b7d5a7bb05b724654332b9a0b
Benjamin Pasero <benjpas@microsoft.com>
2020-02-04 14:50:47 +0100

    smoke test - add attribute for tabs to simplify lookup

--- test/smoke/src/areas/workbench/data-migration.test.ts
+++ test/smoke/src/areas/workbench/data-migration.test.ts
@@ -66,7 +66,7 @@ export function setup(stableCodePath: string, testDataPath: string) {
 			await stableApp.workbench.editors.newUntitledFile();
 
 			const untitled = 'Untitled-1';
-			const textToTypeInUntitled = untitled;
+			const textToTypeInUntitled = 'Hello from Untitled';
 			await stableApp.workbench.editor.waitForTypeInEditor(untitled, textToTypeInUntitled);
 
 			const readmeMd = 'readme.md';
@@ -86,7 +86,7 @@ export function setup(stableCodePath: string, testDataPath: string) {
 			await insidersApp.workbench.editor.waitForEditorContents(readmeMd, c => c.indexOf(textToType) > -1);
 
 			await insidersApp.workbench.editors.waitForTab(untitled, true);
-			await insidersApp.workbench.editors.selectTab(untitled, true);
+			await insidersApp.workbench.editors.selectTab(untitled);
 			await insidersApp.workbench.editor.waitForEditorContents(untitled, c => c.indexOf(textToTypeInUntitled) > -1);
 
 			await insidersApp.stop();
