ed6fae916ca47f5f2077fc41c1b2be7f06d95ca0
Alex Dima <alexdima@microsoft.com>
2020-02-05 16:03:59 +0100

    Cannot use RegExp.lastIndex to -1 because it will not find any matches afterwards on Safari (#90063)

--- src/vs/editor/common/model/pieceTreeTextBuffer/pieceTreeBase.ts
+++ src/vs/editor/common/model/pieceTreeTextBuffer/pieceTreeBase.ts
@@ -670,7 +670,7 @@ export class PieceTreeBase {
 		if (searcher._wordSeparators) {
 			searchText = buffer.buffer.substring(start, end);
 			offsetInBuffer = (offset: number) => offset + start;
-			searcher.reset(-1);
+			searcher.reset(0);
 		} else {
 			searchText = buffer.buffer;
 			offsetInBuffer = (offset: number) => offset;
