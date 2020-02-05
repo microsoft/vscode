ccb10fe28731da73edac10d5bddfabd6d1c56513
SteVen Batten <stbatt@microsoft.com>
2020-02-05 07:41:47 -0800

    readd search.location as deprecated

--- src/vs/workbench/contrib/search/browser/search.contribution.ts
+++ src/vs/workbench/contrib/search/browser/search.contribution.ts
@@ -775,6 +775,13 @@ configurationRegistry.registerConfiguration({
 			description: nls.localize('search.globalFindClipboard', "Controls whether the search view should read or modify the shared find clipboard on macOS."),
 			included: platform.isMacintosh
 		},
+		'search.location': {
+			type: 'string',
+			enum: ['sidebar', 'panel'],
+			default: 'sidebar',
+			description: nls.localize('search.location', "Controls whether the search will be shown as a view in the sidebar or as a panel in the panel area for more horizontal space."),
+			deprecationMessage: nls.localize('search.location.deprecationMessage', "This setting is deprecated. Please use the search view's context menu instead.")
+		},
 		'search.collapseResults': {
 			type: 'string',
 			enum: ['auto', 'alwaysCollapse', 'alwaysExpand'],
