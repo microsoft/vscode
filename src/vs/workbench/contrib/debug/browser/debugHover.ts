2c0e53421747629c78dcd18f82666e9eae366b4a
isidor <inikolic@microsoft.com>
2020-02-07 11:57:27 +0100

    debug: only focus back the editor if hover had focus
    fixes #90167

--- src/vs/workbench/contrib/debug/browser/debugHover.ts
+++ src/vs/workbench/contrib/debug/browser/debugHover.ts
@@ -273,11 +273,13 @@ export class DebugHoverWidget implements IContentWidget {
 			return;
 		}
 
+		if (dom.isAncestor(document.activeElement, this.domNode)) {
+			this.editor.focus();
+		}
 		this._isVisible = false;
 		this.editor.deltaDecorations(this.highlightDecorations, []);
 		this.highlightDecorations = [];
 		this.editor.layoutContentWidget(this);
-		this.editor.focus();
 	}
 
 	getPosition(): IContentWidgetPosition | null {
