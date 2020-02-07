3e19c9765ef71904d719cf791c581d8de5f090ae
Johannes Rieken <johannes.rieken@gmail.com>
2020-02-07 15:06:29 +0100

    test - remove unsupported options, fix bad file warning

--- test/unit/browser/index.js
+++ test/unit/browser/index.js
@@ -17,10 +17,10 @@ const playwright = require('playwright');
 // opts
 const defaultReporterName = process.platform === 'win32' ? 'list' : 'spec';
 const optimist = require('optimist')
-	.describe('grep', 'only run tests matching <pattern>').alias('grep', 'g').alias('grep', 'f').string('grep')
+	// .describe('grep', 'only run tests matching <pattern>').alias('grep', 'g').alias('grep', 'f').string('grep')
+	// .describe('build', 'run with build output (out-build)').boolean('build')
 	.describe('run', 'only run tests matching <relative_file_path>').string('run')
 	.describe('glob', 'only run tests matching <glob_pattern>').string('glob')
-	.describe('build', 'run with build output (out-build)').boolean('build')
 	.describe('debug', 'do not run browsers headless').boolean('debug')
 	.describe('browser', 'browsers in which tests should run').string('browser').default('browser', ['chromium'])
 	.describe('reporter', 'the mocha reporter').string('reporter').default('reporter', defaultReporterName)
@@ -84,7 +84,7 @@ const testModules = (async function () {
 		// glob patterns (--glob)
 		const defaultGlob = '**/*.test.js';
 		const pattern = argv.glob || defaultGlob
-		isDefaultModules = argv.glob === defaultGlob;
+		isDefaultModules = pattern === defaultGlob;
 
 		promise = new Promise((resolve, reject) => {
 			glob(pattern, { cwd: out }, (err, files) => {
