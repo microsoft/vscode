d7eac676ac16bd0cdd1d23c118f92731276f796e
isidor <inikolic@microsoft.com>
2020-02-07 16:16:44 +0100

    debug: when opening in editor ingore query, so different sessions open same source
    #89756

--- src/vs/workbench/contrib/debug/common/debugSource.ts
+++ src/vs/workbench/contrib/debug/common/debugSource.ts
@@ -96,7 +96,7 @@ export class Source {
 
 	openInEditor(editorService: IEditorService, selection: IRange, preserveFocus?: boolean, sideBySide?: boolean, pinned?: boolean): Promise<ITextEditor | undefined> {
 		return !this.available ? Promise.resolve(undefined) : editorService.openEditor({
-			resource: this.uri,
+			resource: this.uri.with({ query: null }),
 			description: this.origin,
 			options: {
 				preserveFocus,
