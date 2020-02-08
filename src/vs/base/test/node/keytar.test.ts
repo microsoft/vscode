1f0cc7e80354aabdbf8fd45b0399fb8cda3dc35e
Benjamin Pasero <benjpas@microsoft.com>
2020-02-08 17:51:39 +0100

    tests - keytar needs more timeout?

--- src/vs/base/test/node/keytar.test.ts
+++ src/vs/base/test/node/keytar.test.ts
@@ -7,31 +7,32 @@ import * as platform from 'vs/base/common/platform';
 
 suite('Keytar', () => {
 
-	test('loads and is functional', function (done) {
+	test('loads and is functional', async function () {
 		if (platform.isLinux) {
 			// Skip test due to set up issue with Travis.
 			this.skip();
 			return;
 		}
-		(async () => {
-			const keytar = await import('keytar');
-			const name = `VSCode Test ${Math.floor(Math.random() * 1e9)}`;
+
+		this.timeout(20000);
+
+		const keytar = await import('keytar');
+		const name = `VSCode Test ${Math.floor(Math.random() * 1e9)}`;
+		try {
+			await keytar.setPassword(name, 'foo', 'bar');
+			assert.equal(await keytar.findPassword(name), 'bar');
+			assert.equal((await keytar.findCredentials(name)).length, 1);
+			assert.equal(await keytar.getPassword(name, 'foo'), 'bar');
+			await keytar.deletePassword(name, 'foo');
+			assert.equal(await keytar.getPassword(name, 'foo'), undefined);
+		} catch (err) {
+			// try to clean up
 			try {
-				await keytar.setPassword(name, 'foo', 'bar');
-				assert.equal(await keytar.findPassword(name), 'bar');
-				assert.equal((await keytar.findCredentials(name)).length, 1);
-				assert.equal(await keytar.getPassword(name, 'foo'), 'bar');
 				await keytar.deletePassword(name, 'foo');
-				assert.equal(await keytar.getPassword(name, 'foo'), undefined);
-			} catch (err) {
-				// try to clean up
-				try {
-					await keytar.deletePassword(name, 'foo');
-				} finally {
-					// eslint-disable-next-line no-unsafe-finally
-					throw err;
-				}
+			} finally {
+				// eslint-disable-next-line no-unsafe-finally
+				throw err;
 			}
-		})().then(done, done);
+		}
 	});
 });
