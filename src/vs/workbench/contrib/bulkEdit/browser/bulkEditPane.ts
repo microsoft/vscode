c13ad9838f37e8cb82d09264303a69e49ff8deca
Johannes Rieken <johannes.rieken@gmail.com>
2020-02-10 12:23:55 +0100

    fix #89416

--- src/vs/workbench/contrib/bulkEdit/browser/bulkEditPane.ts
+++ src/vs/workbench/contrib/bulkEdit/browser/bulkEditPane.ts
@@ -196,6 +196,10 @@ export class BulkEditPane extends ViewPane {
 		});
 	}
 
+	hasInput(): boolean {
+		return Boolean(this._currentInput);
+	}
+
 	private async _setTreeInput(input: BulkFileOperations) {
 
 		const viewState = this._treeViewStates.get(this._treeDataSource.groupByFile);
@@ -243,6 +247,15 @@ export class BulkEditPane extends ViewPane {
 		this._done(false);
 	}
 
+	private _done(accept: boolean): void {
+		if (this._currentResolve) {
+			this._currentResolve(accept ? this._currentInput?.getWorkspaceEdit() : undefined);
+		}
+		this._currentInput = undefined;
+		this._setState(State.Message);
+		this._sessionDisposables.clear();
+	}
+
 	toggleChecked() {
 		const [first] = this._tree.getFocus();
 		if ((first instanceof FileElement || first instanceof TextEditElement) && !first.isDisabled()) {
@@ -278,15 +291,6 @@ export class BulkEditPane extends ViewPane {
 			this._storageService.store(BulkEditPane._memGroupByFile, this._treeDataSource.groupByFile, StorageScope.GLOBAL);
 			this._ctxGroupByFile.set(this._treeDataSource.groupByFile);
 		}
-	}
-
-	private _done(accept: boolean): void {
-		if (this._currentResolve) {
-			this._currentResolve(accept ? this._currentInput?.getWorkspaceEdit() : undefined);
-			this._currentInput = undefined;
-		}
-		this._setState(State.Message);
-		this._sessionDisposables.clear();
 	}
 
 	private async _openElementAsEditor(e: IOpenEvent<BulkEditElement | null>): Promise<void> {
