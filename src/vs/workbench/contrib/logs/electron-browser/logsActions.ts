18c6615ee2beabe75a173edb651af7212d75d1f6
Rob Lourens <roblourens@gmail.com>
2020-02-10 21:03:09 -0800

    Add a command to open the EH logs folder Fix microsoft/vscode-remote-release#412

--- src/vs/workbench/contrib/logs/electron-browser/logsActions.ts
+++ src/vs/workbench/contrib/logs/electron-browser/logsActions.ts
@@ -9,6 +9,7 @@ import { join } from 'vs/base/common/path';
 import { IEnvironmentService } from 'vs/platform/environment/common/environment';
 import { URI } from 'vs/base/common/uri';
 import { IElectronService } from 'vs/platform/electron/node/electron';
+import { IElectronEnvironmentService } from 'vs/workbench/services/electron/electron-browser/electronEnvironmentService';
 
 export class OpenLogsFolderAction extends Action {
 
@@ -24,5 +25,24 @@ export class OpenLogsFolderAction extends Action {
 
 	run(): Promise<void> {
 		return this.electronService.showItemInFolder(URI.file(join(this.environmentService.logsPath, 'main.log')).fsPath);
+	}
+}
+
+export class OpenExtensionLogsFolderAction extends Action {
+
+	static readonly ID = 'workbench.action.openExtensionLogsFolder';
+	static readonly LABEL = nls.localize('openExtensionLogsFolder', "Open Extension Logs Folder");
+
+	constructor(id: string, label: string,
+		@IEnvironmentService private readonly environmentService: IEnvironmentService,
+		@IElectronService private readonly electronService: IElectronService,
+		@IElectronEnvironmentService private readonly electronEnvironmentService: IElectronEnvironmentService,
+	) {
+		super(id, label);
+	}
+
+	run(): Promise<void> {
+		const extensionLogsPath = URI.file(join(this.environmentService.logsPath, `exthost${this.electronEnvironmentService.windowId}`, 'exthost.log')).fsPath;
+		return this.electronService.showItemInFolder(extensionLogsPath);
 	}
 }
