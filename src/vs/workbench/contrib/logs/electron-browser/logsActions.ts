f2c7dcf8d372772a9dbe2b6a9118f98edde6838b
Rob Lourens <roblourens@gmail.com>
2020-02-12 15:17:47 -0800

    Move the ext host logs path to a common location And don't hardcode the exthost.log file name

--- src/vs/workbench/contrib/logs/electron-browser/logsActions.ts
+++ src/vs/workbench/contrib/logs/electron-browser/logsActions.ts
@@ -3,13 +3,14 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import * as nls from 'vs/nls';
 import { Action } from 'vs/base/common/actions';
 import { join } from 'vs/base/common/path';
-import { IEnvironmentService } from 'vs/platform/environment/common/environment';
 import { URI } from 'vs/base/common/uri';
+import * as nls from 'vs/nls';
 import { IElectronService } from 'vs/platform/electron/node/electron';
+import { IEnvironmentService } from 'vs/platform/environment/common/environment';
 import { IElectronEnvironmentService } from 'vs/workbench/services/electron/electron-browser/electronEnvironmentService';
+import { IFileService } from 'vs/platform/files/common/files';
 
 export class OpenLogsFolderAction extends Action {
 
@@ -34,15 +35,17 @@ export class OpenExtensionLogsFolderAction extends Action {
 	static readonly LABEL = nls.localize('openExtensionLogsFolder', "Open Extension Logs Folder");
 
 	constructor(id: string, label: string,
-		@IEnvironmentService private readonly environmentService: IEnvironmentService,
-		@IElectronService private readonly electronService: IElectronService,
-		@IElectronEnvironmentService private readonly electronEnvironmentService: IElectronEnvironmentService,
+		@IElectronEnvironmentService private readonly electronEnvironmentSerice: IElectronEnvironmentService,
+		@IFileService private readonly fileService: IFileService,
+		@IElectronService private readonly electronService: IElectronService
 	) {
 		super(id, label);
 	}
 
-	run(): Promise<void> {
-		const extensionLogsPath = URI.file(join(this.environmentService.logsPath, `exthost${this.electronEnvironmentService.windowId}`, 'exthost.log')).fsPath;
-		return this.electronService.showItemInFolder(extensionLogsPath);
+	async run(): Promise<void> {
+		const folderStat = await this.fileService.resolve(this.electronEnvironmentSerice.extHostLogsPath);
+		if (folderStat.children && folderStat.children[0]) {
+			return this.electronService.showItemInFolder(folderStat.children[0].resource.fsPath);
+		}
 	}
 }
