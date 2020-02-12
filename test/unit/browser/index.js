88c120c1162e8dd14b0e8c48c35a3ce3d0f5912a
Benjamin Pasero <benjpas@microsoft.com>
2020-02-12 09:33:42 +0100

    no-sandbox

--- test/unit/browser/index.js
+++ test/unit/browser/index.js
@@ -118,7 +118,7 @@ const testModules = (async function () {
 
 
 async function runTestsInBrowser(testModules, browserType) {
-	const args = process.platform === 'linux' && browserType === 'chromium' ? ['--no-sandbox'] : undefined;
+	const args = process.platform === 'linux' && browserType === 'chromium' ? ['--no-sandbox'] : undefined; // disable sandbox to run chrome on certain Linux distros
 	const browser = await playwright[browserType].launch({ headless: !Boolean(argv.debug), dumpio: true, args });
 	const page = (await browser.defaultContext().pages())[0]
 	const target = url.pathToFileURL(path.join(__dirname, 'renderer.html'));
