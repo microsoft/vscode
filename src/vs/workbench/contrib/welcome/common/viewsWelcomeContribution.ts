c6e0aaa322323eb38f38f3d662b887594f606ee9
Joao Moreno <joao.moreno@microsoft.com>
2020-02-17 11:56:11 +0100

    views welcome: check for proposed api flag

--- src/vs/workbench/contrib/welcome/common/viewsWelcomeContribution.ts
+++ src/vs/workbench/contrib/welcome/common/viewsWelcomeContribution.ts
@@ -7,9 +7,10 @@ import { Disposable, IDisposable } from 'vs/base/common/lifecycle';
 import { ContextKeyExpr } from 'vs/platform/contextkey/common/contextkey';
 import { IWorkbenchContribution } from 'vs/workbench/common/contributions';
 import { IExtensionPoint } from 'vs/workbench/services/extensions/common/extensionsRegistry';
-import { ViewsWelcomeExtensionPoint, ViewWelcome } from './viewsWelcomeExtensionPoint';
+import { ViewsWelcomeExtensionPoint, ViewWelcome, viewsWelcomeExtensionPointDescriptor } from './viewsWelcomeExtensionPoint';
 import { Registry } from 'vs/platform/registry/common/platform';
 import { Extensions as ViewContainerExtensions, IViewsRegistry } from 'vs/workbench/common/views';
+import { localize } from 'vs/nls';
 
 const viewsRegistry = Registry.as<IViewsRegistry>(ViewContainerExtensions.ViewsRegistry);
 
@@ -22,6 +23,11 @@ export class ViewsWelcomeContribution extends Disposable implements IWorkbenchCo
 
 		extensionPoint.setHandler((_, { added, removed }) => {
 			for (const contribution of removed) {
+				// Proposed API check
+				if (!contribution.description.enableProposedApi) {
+					continue;
+				}
+
 				for (const welcome of contribution.value) {
 					const disposable = this.viewWelcomeContents.get(welcome);
 
@@ -32,6 +38,12 @@ export class ViewsWelcomeContribution extends Disposable implements IWorkbenchCo
 			}
 
 			for (const contribution of added) {
+				// Proposed API check
+				if (!contribution.description.enableProposedApi) {
+					contribution.collector.error(localize('proposedAPI.invalid', "The '{0}' contribution is a proposed API and is only available when running out of dev or with the following command line switch: --enable-proposed-api {1}", viewsWelcomeExtensionPointDescriptor.extensionPoint, contribution.description.identifier.value));
+					continue;
+				}
+
 				for (const welcome of contribution.value) {
 					const disposable = viewsRegistry.registerViewWelcomeContent(welcome.view, {
 						content: welcome.contents,
