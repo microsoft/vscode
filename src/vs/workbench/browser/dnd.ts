b1e4c4ca83987c76465b2e6192609d8e36f72c56
Benjamin Pasero <benjpas@microsoft.com>
2020-02-18 16:48:51 +0100

    Dropping a folder onto the editor should open a new tab (macOS) instead of a new window. (fix #90767)

--- src/vs/workbench/browser/dnd.ts
+++ src/vs/workbench/browser/dnd.ts
@@ -299,7 +299,7 @@ export class ResourcesDropHandler {
 
 		// Open in separate windows if we drop workspaces or just one folder
 		if (toOpen.length > folderURIs.length || folderURIs.length === 1) {
-			await this.hostService.openWindow(toOpen, { forceReuseWindow: true });
+			await this.hostService.openWindow(toOpen);
 		}
 
 		// folders.length > 1: Multiple folders: Create new workspace with folders and open
