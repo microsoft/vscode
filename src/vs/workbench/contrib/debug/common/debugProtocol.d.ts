5dd35167812c82de134ecfafe95c5d8cd08abcb0
Andre Weinand <aweinand@microsoft.com>
2020-02-18 15:18:28 +0100

    update comment in DAP

--- src/vs/workbench/contrib/debug/common/debugProtocol.d.ts
+++ src/vs/workbench/contrib/debug/common/debugProtocol.d.ts
@@ -190,36 +190,39 @@ declare module DebugProtocol {
 		};
 	}
 
-/** Event message for 'output' event type.
-	The event indicates that the target has produced some output.
-*/
-export interface OutputEvent extends Event {
-	// event: 'output';
-	body: {
-		/** The output category. If not specified, 'console' is assumed.
-			Values: 'console', 'stdout', 'stderr', 'telemetry', etc.
-		*/
-		category?: string;
-		/** The output to report. */
-		output: string;
-		/** Support for keeping an output log organized by grouping related messages.
-			'start': Start a new group in expanded mode. Subsequent output events are members of the group and should be shown indented. The 'output' attribute becomes the name of the group and is not indented.
-			'startCollapsed': Start a new group in collapsed mode. Subsequent output events are members of the group and should be shown indented (as soon as the group is expanded). The 'output' attribute becomes the name of the group and is not indented.
-			'end': End the current group and decreases the indentation of subsequent output events.
-		*/
-		group?: 'start' | 'startCollapsed' | 'end';
-		/** If an attribute 'variablesReference' exists and its value is > 0, the output contains objects which can be retrieved by passing 'variablesReference' to the 'variables' request. The value should be less than or equal to 2147483647 (2^31 - 1). */
-		variablesReference?: number;
-		/** An optional source location where the output was produced. */
-		source?: Source;
-		/** An optional source location line where the output was produced. */
-		line?: number;
-		/** An optional source location column where the output was produced. */
-		column?: number;
-		/** Optional data to report. For the 'telemetry' category the data will be sent to telemetry, for the other categories the data is shown in JSON format. */
-		data?: any;
-	};
-}
+	/** Event message for 'output' event type.
+		The event indicates that the target has produced some output.
+	*/
+	export interface OutputEvent extends Event {
+		// event: 'output';
+		body: {
+			/** The output category. If not specified, 'console' is assumed.
+				Values: 'console', 'stdout', 'stderr', 'telemetry', etc.
+			*/
+			category?: string;
+			/** The output to report. */
+			output: string;
+			/** Support for keeping an output log organized by grouping related messages.
+				'start': Start a new group in expanded mode. Subsequent output events are members of the group and should be shown indented.
+				The 'output' attribute becomes the name of the group and is not indented.
+				'startCollapsed': Start a new group in collapsed mode. Subsequent output events are members of the group and should be shown indented (as soon as the group is expanded).
+				The 'output' attribute becomes the name of the group and is not indented.
+				'end': End the current group and decreases the indentation of subsequent output events.
+				A non empty 'output' attribute is shown as the unindented end of the group.
+			*/
+			group?: 'start' | 'startCollapsed' | 'end';
+			/** If an attribute 'variablesReference' exists and its value is > 0, the output contains objects which can be retrieved by passing 'variablesReference' to the 'variables' request. The value should be less than or equal to 2147483647 (2^31 - 1). */
+			variablesReference?: number;
+			/** An optional source location where the output was produced. */
+			source?: Source;
+			/** An optional source location line where the output was produced. */
+			line?: number;
+			/** An optional source location column where the output was produced. */
+			column?: number;
+			/** Optional data to report. For the 'telemetry' category the data will be sent to telemetry, for the other categories the data is shown in JSON format. */
+			data?: any;
+		};
+	}
 
 	/** Event message for 'breakpoint' event type.
 		The event indicates that some information about a breakpoint has changed.
