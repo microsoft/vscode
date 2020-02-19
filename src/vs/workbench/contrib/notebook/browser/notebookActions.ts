a4e9073fcc9ab7ff05fcf17153c8e7b0a3bc095f
rebornix <penn.lv@gmail.com>
2020-02-18 20:41:20 -0800

    registerAction2

--- src/vs/workbench/contrib/notebook/browser/notebookActions.ts
+++ src/vs/workbench/contrib/notebook/browser/notebookActions.ts
@@ -3,205 +3,150 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { Action } from 'vs/base/common/actions';
 import { KeyCode } from 'vs/base/common/keyCodes';
-import { SyncActionDescriptor, MenuRegistry, MenuId } from 'vs/platform/actions/common/actions';
+import { MenuRegistry, MenuId, Action2, registerAction2 } from 'vs/platform/actions/common/actions';
 import { ContextKeyExpr } from 'vs/platform/contextkey/common/contextkey';
 import { InputFocusedContext, InputFocusedContextKey } from 'vs/platform/contextkey/common/contextkeys';
 import { KeybindingWeight } from 'vs/platform/keybinding/common/keybindingsRegistry';
-import { Registry } from 'vs/platform/registry/common/platform';
-import { Extensions as WorkbenchActionExtensions, IWorkbenchActionRegistry } from 'vs/workbench/common/actions';
 import { INotebookService } from 'vs/workbench/contrib/notebook/browser/notebookService';
 import { IEditorService } from 'vs/workbench/services/editor/common/editorService';
 import { NOTEBOOK_EDITOR_FOCUSED, NotebookEditor } from 'vs/workbench/contrib/notebook/browser/notebookEditor';
+import { ServicesAccessor } from 'vs/platform/instantiation/common/instantiation';
 
-// TODO@joh,peng This is outdated, registerAction2 should be used instead!
-
-export class ExecuteNotebookCellAction extends Action {
-
-	static readonly ID = 'workbench.action.executeNotebookCell';
-	static readonly LABEL = 'Execute Notebook Cell';
-
-	constructor(
-		id: string,
-		label: string,
-		@IEditorService private readonly editorService: IEditorService,
-		@INotebookService private readonly notebookService: INotebookService
-
-	) {
-		super(id, label);
+registerAction2(class extends Action2 {
+	constructor() {
+		super({
+			id: 'workbench.action.executeNotebookCell',
+			title: 'Execute Notebook Cell'
+		});
 	}
 
-	async run(): Promise<void> {
-		let resource = this.editorService.activeEditor?.getResource();
+	async run(accessor: ServicesAccessor): Promise<void> {
+		let editorService = accessor.get(IEditorService);
+		let notebookService = accessor.get(INotebookService);
 
-		if (resource) {
-			let notebookProviders = this.notebookService.getContributedNotebookProviders(resource!);
+		let resource = editorService.activeEditor?.getResource();
+		if (!resource) {
+			return;
+		}
 
-			if (notebookProviders.length > 0) {
-				let viewType = notebookProviders[0].id;
-				this.notebookService.executeNotebookActiveCell(viewType, resource);
-			}
+		let notebookProviders = notebookService.getContributedNotebookProviders(resource!);
+
+		if (notebookProviders.length > 0) {
+			let viewType = notebookProviders[0].id;
+			notebookService.executeNotebookActiveCell(viewType, resource);
 		}
 	}
-}
-export class ExecuteNotebookAction extends Action {
+});
 
-	static readonly ID = 'workbench.action.executeNotebook';
-	static readonly LABEL = 'Execute Notebook';
-
-	constructor(
-		id: string,
-		label: string,
-		@IEditorService private readonly editorService: IEditorService,
-		@INotebookService private readonly notebookService: INotebookService
-
-	) {
-		super(id, label);
+registerAction2(class extends Action2 {
+	constructor() {
+		super({
+			id: 'workbench.action.executeNotebook',
+			title: 'Execute Notebook'
+		});
 	}
 
-	async run(): Promise<void> {
-		let resource = this.editorService.activeEditor?.getResource();
+	async run(accessor: ServicesAccessor): Promise<void> {
+		let editorService = accessor.get(IEditorService);
+		let notebookService = accessor.get(INotebookService);
 
-		if (resource) {
-			let notebookProviders = this.notebookService.getContributedNotebookProviders(resource!);
+		let resource = editorService.activeEditor?.getResource();
 
-			if (notebookProviders.length > 0) {
-				let viewType = notebookProviders[0].id;
-				this.notebookService.executeNotebook(viewType, resource);
-			}
+		if (!resource) {
+			return;
+		}
+
+		let notebookProviders = notebookService.getContributedNotebookProviders(resource!);
+
+		if (notebookProviders.length > 0) {
+			let viewType = notebookProviders[0].id;
+			notebookService.executeNotebook(viewType, resource);
 		}
 	}
-}
+});
 
-
-export class QuitNotebookEditAction extends Action {
-
-	static readonly ID = 'workbench.action.quitNotebookEdit';
-	static readonly LABEL = 'Quit Notebook Cell Editing';
-
-	constructor(
-		id: string,
-		label: string,
-		@IEditorService private readonly editorService: IEditorService,
-		@INotebookService private readonly notebookService: INotebookService
-
-	) {
-		super(id, label);
+registerAction2(class extends Action2 {
+	constructor() {
+		super({
+			id: 'workbench.action.quitNotebookEdit',
+			title: 'Quit Notebook Cell Editing',
+			keybinding: {
+				when: ContextKeyExpr.and(NOTEBOOK_EDITOR_FOCUSED, InputFocusedContext),
+				primary: KeyCode.Escape,
+				weight: KeybindingWeight.EditorContrib - 5
+			}
+		});
 	}
 
-	async run(): Promise<void> {
-		let resource = this.editorService.activeEditor?.getResource();
-		let editorControl = this.editorService.activeControl;
+	async run(accessor: ServicesAccessor): Promise<void> {
+		let editorService = accessor.get(IEditorService);
+		let notebookService = accessor.get(INotebookService);
 
-		if (resource && editorControl) {
-			let notebookProviders = this.notebookService.getContributedNotebookProviders(resource!);
+		let resource = editorService.activeEditor?.getResource();
+		let editorControl = editorService.activeControl;
+		let notebookProviders = notebookService.getContributedNotebookProviders(resource!);
 
-			if (notebookProviders.length > 0) {
-				let editorViewType = (editorControl! as NotebookEditor).viewType;
-				let viewType = notebookProviders[0].id;
+		if (!resource || !editorControl || notebookProviders.length === 0) {
+			return;
+		}
 
-				if (viewType === editorViewType) {
+		let editorViewType = (editorControl! as NotebookEditor).viewType;
+		let viewType = notebookProviders[0].id;
 
-					let activeCell = (editorControl! as NotebookEditor).getActiveCell();
-					if (activeCell) {
-						(editorControl! as NotebookEditor).focusNotebookCell(activeCell, false);
-					}
-				}
-			}
+		if (viewType !== editorViewType) {
+			return;
+		}
+
+		let activeCell = (editorControl! as NotebookEditor).getActiveCell();
+		if (activeCell) {
+			(editorControl! as NotebookEditor).focusNotebookCell(activeCell, false);
 		}
 	}
-}
-export class EditNotebookCellAction extends Action {
+});
+
+registerAction2(class extends Action2 {
 
 	static readonly ID = 'workbench.action.editNotebookActiveCell';
 	static readonly LABEL = 'Edit Notebook Active Cell';
 
-	constructor(
-		id: string,
-		label: string,
-		@IEditorService private readonly editorService: IEditorService,
-		@INotebookService private readonly notebookService: INotebookService
-
-	) {
-		super(id, label);
+	constructor() {
+		super({
+			id: 'workbench.action.editNotebookActiveCell',
+			title: 'Edit Notebook Active Cell',
+			keybinding: {
+				when: ContextKeyExpr.and(NOTEBOOK_EDITOR_FOCUSED, ContextKeyExpr.not(InputFocusedContextKey)),
+				primary: KeyCode.Enter,
+				weight: KeybindingWeight.EditorContrib
+			}
+		});
 	}
 
-	async run(): Promise<void> {
-		let resource = this.editorService.activeEditor?.getResource();
-		let editorControl = this.editorService.activeControl;
+	async run(accessor: ServicesAccessor): Promise<void> {
+		let editorService = accessor.get(IEditorService);
+		let notebookService = accessor.get(INotebookService);
 
-		if (resource && editorControl) {
-			let notebookProviders = this.notebookService.getContributedNotebookProviders(resource!);
+		let resource = editorService.activeEditor?.getResource();
+		let editorControl = editorService.activeControl;
+		let notebookProviders = notebookService.getContributedNotebookProviders(resource!);
 
-			if (notebookProviders.length > 0) {
-				let editorViewType = (editorControl! as NotebookEditor).viewType;
-				let viewType = notebookProviders[0].id;
+		if (!resource || !editorControl || notebookProviders.length === 0) {
+			return;
+		}
 
-				if (viewType === editorViewType) {
+		let editorViewType = (editorControl! as NotebookEditor).viewType;
+		let viewType = notebookProviders[0].id;
 
-					let activeCell = (editorControl! as NotebookEditor).getActiveCell();
-					if (activeCell) {
-						(editorControl! as NotebookEditor).editNotebookCell(undefined, activeCell);
-					}
-				}
-			}
+		if (viewType !== editorViewType) {
+			return;
+		}
+
+		let activeCell = (editorControl! as NotebookEditor).getActiveCell();
+		if (activeCell) {
+			(editorControl! as NotebookEditor).editNotebookCell(undefined, activeCell);
 		}
 	}
-}
-
-
-const registry = Registry.as<IWorkbenchActionRegistry>(WorkbenchActionExtensions.WorkbenchActions);
-registry.registerWorkbenchAction(
-	SyncActionDescriptor.create(
-		EditNotebookCellAction,
-		EditNotebookCellAction.ID,
-		EditNotebookCellAction.LABEL,
-		{
-			primary: KeyCode.Enter,
-		},
-		ContextKeyExpr.and(NOTEBOOK_EDITOR_FOCUSED, ContextKeyExpr.not(InputFocusedContextKey)),
-	),
-	'Edit Notebook Active Cell;',
-	'Notebook'
-);
-
-registry.registerWorkbenchAction(
-	SyncActionDescriptor.create(
-		QuitNotebookEditAction,
-		QuitNotebookEditAction.ID,
-		QuitNotebookEditAction.LABEL,
-		{
-			primary: KeyCode.Escape,
-		},
-		ContextKeyExpr.and(NOTEBOOK_EDITOR_FOCUSED, InputFocusedContext),
-		// TODO: It's set to `EditorContrib - 5` to ensure all editor escape commands to work
-		// but, how about core?
-		KeybindingWeight.EditorContrib - 5
-	),
-	'Edit Notebook Active Cell;',
-	'Notebook'
-);
-
-registry.registerWorkbenchAction(
-	SyncActionDescriptor.create(
-		ExecuteNotebookAction,
-		ExecuteNotebookAction.ID,
-		ExecuteNotebookAction.LABEL
-	),
-	'Execute Notebook',
-	'Notebook'
-);
-
-registry.registerWorkbenchAction(
-	SyncActionDescriptor.create(
-		ExecuteNotebookCellAction,
-		ExecuteNotebookCellAction.ID,
-		ExecuteNotebookCellAction.LABEL
-	),
-	'Execute Notebook Cell',
-	'Notebook'
-);
+});
 
 MenuRegistry.appendMenuItem(MenuId.EditorTitle, {
 	command: {
