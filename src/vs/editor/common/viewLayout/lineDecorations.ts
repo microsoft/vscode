0792b07962e93296c85d77f514e4b358f13774be
Alex Dima <alexdima@microsoft.com>
2020-02-23 17:41:08 +0100

    Add LinePart.metadata (for #91178)

--- src/vs/editor/common/viewLayout/lineDecorations.ts
+++ src/vs/editor/common/viewLayout/lineDecorations.ts
@@ -6,6 +6,7 @@
 import * as strings from 'vs/base/common/strings';
 import { Constants } from 'vs/base/common/uint';
 import { InlineDecoration, InlineDecorationType } from 'vs/editor/common/viewModel/viewModel';
+import { LinePartMetadata } from 'vs/editor/common/viewLayout/viewLineRenderer';
 
 export class LineDecoration {
 	_lineDecorationBrand: void;
@@ -92,11 +93,13 @@ export class DecorationSegment {
 	startOffset: number;
 	endOffset: number;
 	className: string;
+	metadata: number;
 
-	constructor(startOffset: number, endOffset: number, className: string) {
+	constructor(startOffset: number, endOffset: number, className: string, metadata: number) {
 		this.startOffset = startOffset;
 		this.endOffset = endOffset;
 		this.className = className;
+		this.metadata = metadata;
 	}
 }
 
@@ -104,11 +107,21 @@ class Stack {
 	public count: number;
 	private readonly stopOffsets: number[];
 	private readonly classNames: string[];
+	private readonly metadata: number[];
 
 	constructor() {
 		this.stopOffsets = [];
 		this.classNames = [];
+		this.metadata = [];
 		this.count = 0;
+	}
+
+	private static _metadata(metadata: number[]): number {
+		let result = 0;
+		for (let i = 0, len = metadata.length; i < len; i++) {
+			result |= metadata[i];
+		}
+		return result;
 	}
 
 	public consumeLowerThan(maxStopOffset: number, nextStartOffset: number, result: DecorationSegment[]): number {
@@ -122,34 +135,37 @@ class Stack {
 			}
 
 			// Basically we are consuming the first i + 1 elements of the stack
-			result.push(new DecorationSegment(nextStartOffset, this.stopOffsets[i], this.classNames.join(' ')));
+			result.push(new DecorationSegment(nextStartOffset, this.stopOffsets[i], this.classNames.join(' '), Stack._metadata(this.metadata)));
 			nextStartOffset = this.stopOffsets[i] + 1;
 
 			// Consume them
 			this.stopOffsets.splice(0, i + 1);
 			this.classNames.splice(0, i + 1);
+			this.metadata.splice(0, i + 1);
 			this.count -= (i + 1);
 		}
 
 		if (this.count > 0 && nextStartOffset < maxStopOffset) {
-			result.push(new DecorationSegment(nextStartOffset, maxStopOffset - 1, this.classNames.join(' ')));
+			result.push(new DecorationSegment(nextStartOffset, maxStopOffset - 1, this.classNames.join(' '), Stack._metadata(this.metadata)));
 			nextStartOffset = maxStopOffset;
 		}
 
 		return nextStartOffset;
 	}
 
-	public insert(stopOffset: number, className: string): void {
+	public insert(stopOffset: number, className: string, metadata: number): void {
 		if (this.count === 0 || this.stopOffsets[this.count - 1] <= stopOffset) {
 			// Insert at the end
 			this.stopOffsets.push(stopOffset);
 			this.classNames.push(className);
+			this.metadata.push(metadata);
 		} else {
 			// Find the insertion position for `stopOffset`
 			for (let i = 0; i < this.count; i++) {
 				if (this.stopOffsets[i] >= stopOffset) {
 					this.stopOffsets.splice(i, 0, stopOffset);
 					this.classNames.splice(i, 0, className);
+					this.metadata.splice(i, 0, metadata);
 					break;
 				}
 			}
@@ -178,6 +194,13 @@ export class LineDecorationsNormalizer {
 			let startColumn = d.startColumn;
 			let endColumn = d.endColumn;
 			const className = d.className;
+			const metadata = (
+				d.type === InlineDecorationType.Before
+					? LinePartMetadata.PSEUDO_BEFORE
+					: d.type === InlineDecorationType.After
+						? LinePartMetadata.PSEUDO_AFTER
+						: 0
+			);
 
 			// If the position would end up in the middle of a high-low surrogate pair, we move it to before the pair
 			if (startColumn > 1) {
@@ -202,7 +225,7 @@ export class LineDecorationsNormalizer {
 			if (stack.count === 0) {
 				nextStartOffset = currentStartOffset;
 			}
-			stack.insert(currentEndOffset, className);
+			stack.insert(currentEndOffset, className, metadata);
 		}
 
 		stack.consumeLowerThan(Constants.MAX_SAFE_SMALL_INTEGER, nextStartOffset, result);
