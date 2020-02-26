438b39e016a6852f9ebb224b98fbf81ac8033aac
SteVen Batten <stbatt@microsoft.com>
2020-02-26 15:13:21 -0800

    fixes #91622

--- src/vs/workbench/browser/actions/layoutActions.ts
+++ src/vs/workbench/browser/actions/layoutActions.ts
@@ -553,7 +553,7 @@ export class MoveFocusedViewAction extends Action {
 
 		const viewDescriptor = this.viewDescriptorService.getViewDescriptor(focusedViewId);
 		if (!viewDescriptor || !viewDescriptor.canMoveView) {
-			this.notificationService.error(nls.localize('moveFocusedView.error.nonMovableView', "The currently focused view is not movable {0}.", focusedViewId));
+			this.notificationService.error(nls.localize('moveFocusedView.error.nonMovableView', "The currently focused view is not movable."));
 			return Promise.resolve();
 		}
 
