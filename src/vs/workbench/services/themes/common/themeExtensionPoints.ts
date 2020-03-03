516d459482c33abfd44300acfe074553a718501f
Martin Aeschlimann <martinae@microsoft.com>
2020-03-03 14:53:39 +0100

    extract theme configuration

--- src/vs/workbench/services/themes/common/themeExtensionPoints.ts
+++ src/vs/workbench/services/themes/common/themeExtensionPoints.ts
@@ -14,67 +14,70 @@ import { IExtensionService } from 'vs/workbench/services/extensions/common/exten
 import { Event, Emitter } from 'vs/base/common/event';
 import { URI } from 'vs/base/common/uri';
 
-export const colorThemesExtPoint = ExtensionsRegistry.registerExtensionPoint<IThemeExtensionPoint[]>({
-	extensionPoint: 'themes',
-	jsonSchema: {
-		description: nls.localize('vscode.extension.contributes.themes', 'Contributes textmate color themes.'),
-		type: 'array',
-		items: {
-			type: 'object',
-			defaultSnippets: [{ body: { label: '${1:label}', id: '${2:id}', uiTheme: VS_DARK_THEME, path: './themes/${3:id}.tmTheme.' } }],
-			properties: {
-				id: {
-					description: nls.localize('vscode.extension.contributes.themes.id', 'Id of the color theme as used in the user settings.'),
-					type: 'string'
+export function registerColorThemeExtensionPoint() {
+	return ExtensionsRegistry.registerExtensionPoint<IThemeExtensionPoint[]>({
+		extensionPoint: 'themes',
+		jsonSchema: {
+			description: nls.localize('vscode.extension.contributes.themes', 'Contributes textmate color themes.'),
+			type: 'array',
+			items: {
+				type: 'object',
+				defaultSnippets: [{ body: { label: '${1:label}', id: '${2:id}', uiTheme: VS_DARK_THEME, path: './themes/${3:id}.tmTheme.' } }],
+				properties: {
+					id: {
+						description: nls.localize('vscode.extension.contributes.themes.id', 'Id of the color theme as used in the user settings.'),
+						type: 'string'
+					},
+					label: {
+						description: nls.localize('vscode.extension.contributes.themes.label', 'Label of the color theme as shown in the UI.'),
+						type: 'string'
+					},
+					uiTheme: {
+						description: nls.localize('vscode.extension.contributes.themes.uiTheme', 'Base theme defining the colors around the editor: \'vs\' is the light color theme, \'vs-dark\' is the dark color theme. \'hc-black\' is the dark high contrast theme.'),
+						enum: [VS_LIGHT_THEME, VS_DARK_THEME, VS_HC_THEME]
+					},
+					path: {
+						description: nls.localize('vscode.extension.contributes.themes.path', 'Path of the tmTheme file. The path is relative to the extension folder and is typically \'./colorthemes/awesome-color-theme.json\'.'),
+						type: 'string'
+					}
 				},
-				label: {
-					description: nls.localize('vscode.extension.contributes.themes.label', 'Label of the color theme as shown in the UI.'),
-					type: 'string'
-				},
-				uiTheme: {
-					description: nls.localize('vscode.extension.contributes.themes.uiTheme', 'Base theme defining the colors around the editor: \'vs\' is the light color theme, \'vs-dark\' is the dark color theme. \'hc-black\' is the dark high contrast theme.'),
-					enum: [VS_LIGHT_THEME, VS_DARK_THEME, VS_HC_THEME]
-				},
-				path: {
-					description: nls.localize('vscode.extension.contributes.themes.path', 'Path of the tmTheme file. The path is relative to the extension folder and is typically \'./colorthemes/awesome-color-theme.json\'.'),
-					type: 'string'
-				}
-			},
-			required: ['path', 'uiTheme']
+				required: ['path', 'uiTheme']
+			}
 		}
-	}
-});
-
-export const fileIconThemesExtPoint = ExtensionsRegistry.registerExtensionPoint<IThemeExtensionPoint[]>({
-	extensionPoint: 'iconThemes',
-	jsonSchema: {
-		description: nls.localize('vscode.extension.contributes.iconThemes', 'Contributes file icon themes.'),
-		type: 'array',
-		items: {
-			type: 'object',
-			defaultSnippets: [{ body: { id: '${1:id}', label: '${2:label}', path: './fileicons/${3:id}-icon-theme.json' } }],
-			properties: {
-				id: {
-					description: nls.localize('vscode.extension.contributes.iconThemes.id', 'Id of the file icon theme as used in the user settings.'),
-					type: 'string'
+	});
+}
+export function registerFileIconThemeExtensionPoint() {
+	return ExtensionsRegistry.registerExtensionPoint<IThemeExtensionPoint[]>({
+		extensionPoint: 'iconThemes',
+		jsonSchema: {
+			description: nls.localize('vscode.extension.contributes.iconThemes', 'Contributes file icon themes.'),
+			type: 'array',
+			items: {
+				type: 'object',
+				defaultSnippets: [{ body: { id: '${1:id}', label: '${2:label}', path: './fileicons/${3:id}-icon-theme.json' } }],
+				properties: {
+					id: {
+						description: nls.localize('vscode.extension.contributes.iconThemes.id', 'Id of the file icon theme as used in the user settings.'),
+						type: 'string'
+					},
+					label: {
+						description: nls.localize('vscode.extension.contributes.iconThemes.label', 'Label of the file icon theme as shown in the UI.'),
+						type: 'string'
+					},
+					path: {
+						description: nls.localize('vscode.extension.contributes.iconThemes.path', 'Path of the file icon theme definition file. The path is relative to the extension folder and is typically \'./iconthemes/awesome-icon-theme.json\'.'),
+						type: 'string'
+					}
 				},
-				label: {
-					description: nls.localize('vscode.extension.contributes.iconThemes.label', 'Label of the file icon theme as shown in the UI.'),
-					type: 'string'
-				},
-				path: {
-					description: nls.localize('vscode.extension.contributes.iconThemes.path', 'Path of the file icon theme definition file. The path is relative to the extension folder and is typically \'./iconthemes/awesome-icon-theme.json\'.'),
-					type: 'string'
-				}
-			},
-			required: ['path', 'id']
+				required: ['path', 'id']
+			}
 		}
-	}
-});
+	});
+}
 
-export interface ThemeChangeEvent<ThemeData> {
-	themes: ThemeData[];
-	added: ThemeData[];
+export interface ThemeChangeEvent<T> {
+	themes: T[];
+	added: T[];
 }
 
 export interface IThemeData {
@@ -146,7 +149,7 @@ export class ThemeRegistry<T extends IThemeData> {
 				));
 				return;
 			}
-			if (!theme.id && this.idRequired || !types.isString(theme.id)) {
+			if (this.idRequired && (!theme.id || !types.isString(theme.id))) {
 				collector.error(nls.localize(
 					'reqid',
 					"Expected string in `contributes.{0}.id`. Provided value: {1}",
