596469e205e8e68e55e1492745055d04064af4fc
Johannes Rieken <johannes.rieken@gmail.com>
2020-03-04 10:42:14 +0100

    move `removeAccents` to normalization

--- src/vs/base/common/normalization.ts
+++ src/vs/base/common/normalization.ts
@@ -46,3 +46,17 @@ function normalize(str: string, form: string, normalizedCache: LRUCache<string, 
 
 	return res;
 }
+
+export const removeAccents: (str: string) => string = (function () {
+	if (!canNormalize) {
+		// no ES6 features...
+		return function (str: string) { return str; };
+	} else {
+		// transform into NFD form and remove accents
+		// see: https://stackoverflow.com/questions/990904/remove-accents-diacritics-in-a-string-in-javascript/37511463#37511463
+		const regex = /[\u0300-\u036f]/g;
+		return function (str: string) {
+			return normalizeNFD(str).replace(regex, '');
+		};
+	}
+})();
