2362584a626621a80ec4af3bea52d2f0513e9383
Pine Wu <octref@gmail.com>
2020-03-06 00:30:14 -0800

    Revert "Fix #73803"
    This reverts commit 67e6aef0c85ed17ef673950ecc24c12df7f2d7a8.

--- extensions/typescript-language-features/src/test/index.ts
+++ extensions/typescript-language-features/src/test/index.ts
@@ -3,48 +3,26 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import * as path from 'path';
-import * as Mocha from 'mocha';
-import * as glob from 'glob';
+//
+// PLEASE DO NOT MODIFY / DELETE UNLESS YOU KNOW WHAT YOU ARE DOING
+//
+// This file is providing the test runner to use when running extension tests.
+// By default the test runner in use is Mocha based.
+//
+// You can provide your own test runner if you want to override it by exporting
+// a function run(testRoot: string, clb: (error:Error) => void) that the extension
+// host can call to run the tests. The test runner is expected to use console.log
+// to report the results back to the caller. When the tests are finished, return
+// a possible error to the callback or null if none.
 
-export function run(): Promise<void> {
-	// Create the mocha test
-	const options: {
-		ui?: string;
-		timeout?: number;
-	} = {
-		ui: 'tdd',
-		timeout: 60000
-	};
+const testRunner = require('vscode/lib/testrunner');
 
-	const mocha = new Mocha(options);
-	if (!process.env.BUILD_ARTIFACTSTAGINGDIRECTORY && process.platform !== 'win32') {
-		mocha.useColors(true);
-	}
+// You can directly control Mocha options by uncommenting the following lines
+// See https://github.com/mochajs/mocha/wiki/Using-mocha-programmatically#set-options for more info
+testRunner.configure({
+	ui: 'tdd', 		// the TDD UI is being used in extension.test.ts (suite, test, etc.)
+	useColors: (!process.env.BUILD_ARTIFACTSTAGINGDIRECTORY && process.platform !== 'win32'), // colored output from test results (only windows cannot handle)
+	timeout: 60000,
+});
 
-	const testsRoot = path.resolve(__dirname, '..');
-
-	return new Promise((c, e) => {
-		glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
-			if (err) {
-				return e(err);
-			}
-
-			// Add files to the test suite
-			files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
-
-			try {
-				// Run the mocha test
-				mocha.run(failures => {
-					if (failures > 0) {
-						e(new Error(`${failures} tests failed.`));
-					} else {
-						c();
-					}
-				});
-			} catch (err) {
-				e(err);
-			}
-		});
-	});
-}
+export = testRunner;
