4857a9a74617758f1da94ff1c1dc19752e4fc8f7
Pine Wu <octref@gmail.com>
2020-03-06 00:29:57 -0800

    Revert "Fix integration test"
    This reverts commit 5a0ab960f04876886d500b02407734e14c92d1ff.

--- extensions/typescript-language-features/src/test/index.ts
+++ extensions/typescript-language-features/src/test/index.ts
@@ -25,7 +25,7 @@ export function run(): Promise<void> {
 	const testsRoot = path.resolve(__dirname, '..');
 
 	return new Promise((c, e) => {
-		glob('**/**.test.js', { cwd: testsRoot, ignore: '**/node_modules/**' }, (err, files) => {
+		glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
 			if (err) {
 				return e(err);
 			}
