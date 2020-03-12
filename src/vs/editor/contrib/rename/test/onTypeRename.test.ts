05235831f85c9d385b26d3c5213aa195a871a0a4
Pine Wu <octref@gmail.com>
2020-03-12 08:26:52 -0700

    Failure test cases

--- src/vs/editor/contrib/rename/test/onTypeRename.test.ts
+++ src/vs/editor/contrib/rename/test/onTypeRename.test.ts
@@ -358,23 +358,29 @@ suite('Synced regions', () => {
 		CoreEditingCommands.Undo.runEditorCommand(null, editor, null);
 	}, '<ooo></ooo>');
 
-	/**
-	 * @todo
-	 * Editor has correct behavior, test doesn't pass
-	 */
-	// testCase('Delete - left all', state, async (editor, ontypeRenameContribution) => {
-	// 	const pos = new Position(1, 3);
-	// 	editor.setPosition(pos);
-	// 	await ontypeRenameContribution.run(pos, true);
-	// 	editor.trigger('keyboard', 'deleteAllLeft', {});
-	// }, '></>');
-	// testCase('Delete - left all then undo', state, async (editor, ontypeRenameContribution) => {
-	// 	const pos = new Position(1, 5);
-	// 	editor.setPosition(pos);
-	// 	await ontypeRenameContribution.run(pos, true);
-	// 	editor.trigger('keyboard', 'deleteAllLeft', {});
-	// 	editor.trigger('keyboard', Handler.Undo, null);
-	// }, '<ooo></ooo>');
+	testCase('Delete - left all', state, async (editor, ontypeRenameContribution) => {
+		const pos = new Position(1, 3);
+		editor.setPosition(pos);
+		await ontypeRenameContribution.run(pos, true);
+		editor.trigger('keyboard', 'deleteAllLeft', {});
+	}, '></>');
+
+	testCase('Delete - left all then undo', state, async (editor, ontypeRenameContribution) => {
+		const pos = new Position(1, 5);
+		editor.setPosition(pos);
+		await ontypeRenameContribution.run(pos, true);
+		editor.trigger('keyboard', 'deleteAllLeft', {});
+		CoreEditingCommands.Undo.runEditorCommand(null, editor, null);
+	}, '></ooo>');
+
+	testCase('Delete - left all then undo twice', state, async (editor, ontypeRenameContribution) => {
+		const pos = new Position(1, 5);
+		editor.setPosition(pos);
+		await ontypeRenameContribution.run(pos, true);
+		editor.trigger('keyboard', 'deleteAllLeft', {});
+		CoreEditingCommands.Undo.runEditorCommand(null, editor, null);
+		CoreEditingCommands.Undo.runEditorCommand(null, editor, null);
+	}, '<ooo></ooo>');
 
 	testCase('Delete - selection', state, async (editor, ontypeRenameContribution) => {
 		const pos = new Position(1, 5);
@@ -391,18 +397,6 @@ suite('Synced regions', () => {
 		editor.setSelection(new Range(1, 1, 1, 3));
 		editor.trigger('keyboard', 'deleteLeft', {});
 	}, 'oo></oo>');
-
-	/**
-	 * @todo
-	 * Editor behavior not matching test case
-	 */
-	// testCase('Delete - reverse selection across boundary', state, async (editor, ontypeRenameContribution) => {
-	// 	const pos = new Position(1, 3);
-	// 	editor.setPosition(pos);
-	// 	await ontypeRenameContribution.run(pos, true);
-	// 	editor.setSelection(new Selection(1, 3, 1, 1));
-	// 	editor.trigger('keyboard', 'deleteLeft', {});
-	// }, 'oo></ooo>');
 
 	/**
 	 * Undo / redo
