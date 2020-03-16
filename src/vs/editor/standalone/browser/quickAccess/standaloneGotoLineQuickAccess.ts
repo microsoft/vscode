e2982af33d7b8baf794d14a006088d29c321644f
Benjamin Pasero <benjpas@microsoft.com>
2020-03-16 11:50:18 +0100

    quick access - remove quick open from standalone editor and use quick access instead :rocket:

--- src/vs/editor/standalone/browser/quickAccess/standaloneGotoLineQuickAccess.ts
+++ src/vs/editor/standalone/browser/quickAccess/standaloneGotoLineQuickAccess.ts
@@ -10,6 +10,11 @@ import { ICodeEditorService } from 'vs/editor/browser/services/codeEditorService
 import { withNullAsUndefined } from 'vs/base/common/types';
 import { GoToLineNLS } from 'vs/editor/common/standaloneStrings';
 import { Event } from 'vs/base/common/event';
+import { EditorAction, registerEditorAction, ServicesAccessor } from 'vs/editor/browser/editorExtensions';
+import { EditorContextKeys } from 'vs/editor/common/editorContextKeys';
+import { KeyMod, KeyCode } from 'vs/base/common/keyCodes';
+import { KeybindingWeight } from 'vs/platform/keybinding/common/keybindingsRegistry';
+import { IQuickInputService } from 'vs/platform/quickinput/common/quickInput';
 
 export class StandaloneGotoLineQuickAccessProvider extends AbstractGotoLineQuickAccessProvider {
 
@@ -26,6 +31,30 @@ export class StandaloneGotoLineQuickAccessProvider extends AbstractGotoLineQuick
 
 Registry.as<IQuickAccessRegistry>(Extensions.Quickaccess).registerQuickAccessProvider({
 	ctor: StandaloneGotoLineQuickAccessProvider,
-	prefix: AbstractGotoLineQuickAccessProvider.PREFIX,
+	prefix: StandaloneGotoLineQuickAccessProvider.PREFIX,
 	helpEntries: [{ description: GoToLineNLS.gotoLineActionLabel, needsEditor: true }]
 });
+
+export class GotoLineAction extends EditorAction {
+
+	constructor() {
+		super({
+			id: 'editor.action.gotoLine',
+			label: GoToLineNLS.gotoLineActionLabel,
+			alias: 'Go to Line...',
+			precondition: undefined,
+			kbOpts: {
+				kbExpr: EditorContextKeys.focus,
+				primary: KeyMod.CtrlCmd | KeyCode.KEY_G,
+				mac: { primary: KeyMod.WinCtrl | KeyCode.KEY_G },
+				weight: KeybindingWeight.EditorContrib
+			}
+		});
+	}
+
+	run(accessor: ServicesAccessor): void {
+		accessor.get(IQuickInputService).quickAccess.show(StandaloneGotoLineQuickAccessProvider.PREFIX);
+	}
+}
+
+registerEditorAction(GotoLineAction);
