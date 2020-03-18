98bccb4bfc30fbabb8f94d14064e3fc669f21f09
rebornix <penn.lv@gmail.com>
2020-03-18 16:35:00 -0700

    more left padding for notebook cells

--- src/vs/workbench/contrib/notebook/browser/constants.ts
+++ src/vs/workbench/contrib/notebook/browser/constants.ts
@@ -20,7 +20,7 @@ export const COPY_CELL_DOWN_COMMAND_ID = 'workbench.notebook.cell.copyDown';
 export const EXECUTE_CELL_COMMAND_ID = 'workbench.notebook.cell.execute';
 
 // Cell sizing related
-export const CELL_MARGIN = 24;
+export const CELL_MARGIN = 32;
 export const EDITOR_TOP_PADDING = 8;
 export const EDITOR_BOTTOM_PADDING = 8;
 export const EDITOR_TOOLBAR_HEIGHT = 22;
