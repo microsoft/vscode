7903d44c6c2311f870455ba0ce3e2aa6f5739572
rebornix <penn.lv@gmail.com>
2020-03-20 16:02:24 -0700

    cell metadata

--- src/vs/workbench/api/common/extHostNotebook.ts
+++ src/vs/workbench/api/common/extHostNotebook.ts
@@ -201,6 +201,7 @@ export class ExtHostNotebookDocument extends Disposable implements vscode.Notebo
 					language: cell.language,
 					cellKind: cell.cellKind,
 					outputs: outputs,
+					metadata: cell.metadata,
 					isDirty: false
 				};
 			});
