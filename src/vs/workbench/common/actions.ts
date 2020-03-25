887163e0acb93b7d9a249fe86e9de10d5792e35a
Daniel Imms <tyriar@tyriar.com>
2020-03-25 06:12:16 -0700

    Add deprecated indicator to registerWorkbenchAction
    Fixes #93317

--- src/vs/workbench/common/actions.ts
+++ src/vs/workbench/common/actions.ts
@@ -22,6 +22,7 @@ export interface IWorkbenchActionRegistry {
 	/**
 	 * Registers a workbench action to the platform. Workbench actions are not
 	 * visible by default and can only be invoked through a keybinding if provided.
+	 * @deprecated Register directly with KeybindingsRegistry and MenuRegistry instead.
 	 */
 	registerWorkbenchAction(descriptor: SyncActionDescriptor, alias: string, category?: string, when?: ContextKeyExpr): IDisposable;
 }
