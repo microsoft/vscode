c817576cd06e8b58da595d11e4e161dc1e190d52
Benjamin Pasero <benjpas@microsoft.com>
2020-03-27 12:11:17 +0100

    Revert "On file rename, use default custom editor if possible instead of always prompting"
    This reverts commit ebcc8208f8acc8aca3b98c6d670619ab82677d2f.

--- src/vs/workbench/contrib/customEditor/browser/customEditors.ts
+++ src/vs/workbench/contrib/customEditor/browser/customEditors.ts
@@ -327,8 +327,8 @@ export class CustomEditorService extends Disposable implements ICustomEditorServ
 
 	private async handleMovedFileInOpenedFileEditors(_oldResource: URI, newResource: URI): Promise<void> {
 		// See if the new resource can be opened in a custom editor
-		const possibleEditors = this.getAllCustomEditors(newResource);
-		if (!possibleEditors.allEditors.length) {
+		const possibleEditors = this.getAllCustomEditors(newResource).allEditors;
+		if (!possibleEditors.length) {
 			return;
 		}
 
@@ -354,25 +354,19 @@ export class CustomEditorService extends Disposable implements ICustomEditorServ
 			return;
 		}
 
-		let viewType: string | undefined;
-		if (possibleEditors.defaultEditor) {
-			viewType = possibleEditors.defaultEditor.id;
-		} else {
-			// If there is, show a single prompt for all editors to see if the user wants to re-open them
-			//
-			// TODO: instead of prompting eagerly, it'd likly be better to replace all the editors with
-			// ones that would prompt when they first become visible
-			await new Promise(resolve => setTimeout(resolve, 50));
-			viewType = await this.showOpenWithPrompt(newResource);
-		}
-
-		if (!viewType) {
+		// If there is, show a single prompt for all editors to see if the user wants to re-open them
+		//
+		// TODO: instead of prompting eagerly, it'd likly be better to replace all the editors with
+		// ones that would prompt when they first become visible
+		await new Promise(resolve => setTimeout(resolve, 50));
+		const pickedViewType = await this.showOpenWithPrompt(newResource);
+		if (!pickedViewType) {
 			return;
 		}
 
 		for (const [group, entries] of editorsToReplace) {
 			this.editorService.replaceEditors(entries.map(editor => {
-				const replacement = this.createInput(newResource, viewType!, group);
+				const replacement = this.createInput(newResource, pickedViewType, group);
 				return {
 					editor,
 					replacement,
