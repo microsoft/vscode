80cd0ee564caed53f0a7f9fe8fba5b781cbbf12d
Martin Aeschlimann <martinae@microsoft.com>
2020-03-30 10:55:48 +0200

    semantic styling: apply defaults only when no match

--- src/vs/workbench/services/themes/common/colorThemeData.ts
+++ src/vs/workbench/services/themes/common/colorThemeData.ts
@@ -170,25 +170,6 @@ export class ColorThemeData implements IWorkbenchColorTheme {
 				}
 			}
 		}
-		for (const rule of tokenClassificationRegistry.getTokenStylingDefaultRules()) {
-			const matchScore = rule.selector.match(type, modifiers, language);
-			if (matchScore >= 0) {
-				let style: TokenStyle | undefined;
-				if (rule.defaults.scopesToProbe) {
-					style = this.resolveScopes(rule.defaults.scopesToProbe);
-					if (style) {
-						_processStyle(matchScore, style, rule.defaults.scopesToProbe);
-					}
-				}
-				if (!style && useDefault !== false) {
-					const tokenStyleValue = rule.defaults[this.type];
-					style = this.resolveTokenStyleValue(tokenStyleValue);
-					if (style) {
-						_processStyle(matchScore, style, tokenStyleValue!);
-					}
-				}
-			}
-		}
 		for (const rule of this.tokenStylingRules) {
 			const matchScore = rule.selector.match(type, modifiers, language);
 			if (matchScore >= 0) {
@@ -199,6 +180,36 @@ export class ColorThemeData implements IWorkbenchColorTheme {
 			const matchScore = rule.selector.match(type, modifiers, language);
 			if (matchScore >= 0) {
 				_processStyle(matchScore, rule.style, rule);
+			}
+		}
+		let hasUndefinedStyleProperty = false;
+		for (let k in score) {
+			const key = k as keyof TokenStyle;
+			if (score[key] === -1) {
+				hasUndefinedStyleProperty = true;
+			} else {
+				score[key] = Number.MAX_VALUE; // set it to the max, so it won't be replaced by a default
+			}
+		}
+		if (hasUndefinedStyleProperty) {
+			for (const rule of tokenClassificationRegistry.getTokenStylingDefaultRules()) {
+				const matchScore = rule.selector.match(type, modifiers, language);
+				if (matchScore >= 0) {
+					let style: TokenStyle | undefined;
+					if (rule.defaults.scopesToProbe) {
+						style = this.resolveScopes(rule.defaults.scopesToProbe);
+						if (style) {
+							_processStyle(matchScore, style, rule.defaults.scopesToProbe);
+						}
+					}
+					if (!style && useDefault !== false) {
+						const tokenStyleValue = rule.defaults[this.type];
+						style = this.resolveTokenStyleValue(tokenStyleValue);
+						if (style) {
+							_processStyle(matchScore, style, tokenStyleValue!);
+						}
+					}
+				}
 			}
 		}
 		return TokenStyle.fromData(result);
