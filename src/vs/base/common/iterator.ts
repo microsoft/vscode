b07d3d7202782c2d6776dfc14477f560fcae4bf7
Johannes Rieken <johannes.rieken@gmail.com>
2020-04-06 18:03:10 +0200

    fix Iterable.filter and Iterable.map (#94535)
    Co-authored-by: João Moreno <joao.moreno@microsoft.com>

--- src/vs/base/common/iterator.ts
+++ src/vs/base/common/iterator.ts
@@ -52,14 +52,14 @@ export namespace Iterable {
 	export function* filter<T>(iterable: Iterable<T>, predicate: (t: T) => boolean): Iterable<T> {
 		for (const element of iterable) {
 			if (predicate(element)) {
-				return yield element;
+				yield element;
 			}
 		}
 	}
 
 	export function* map<T, R>(iterable: Iterable<T>, fn: (t: T) => R): Iterable<R> {
 		for (const element of iterable) {
-			return yield fn(element);
+			yield fn(element);
 		}
 	}
 }
