e4ceb819be55d63b1ffea41821844fe77ac95e5d
isidor <inikolic@microsoft.com>
2020-04-07 11:10:17 +0200

    debug: fix hover jumping around
    fixes #94581

--- src/vs/workbench/contrib/debug/browser/debugHover.ts
+++ src/vs/workbench/contrib/debug/browser/debugHover.ts
@@ -177,7 +177,6 @@ export class DebugHoverWidget implements IContentWidget {
 	}
 
 	async showAt(range: Range, focus: boolean): Promise<void> {
-
 		const session = this.debugService.getViewModel().focusedSession;
 
 		if (!session || !this.editor.hasModel()) {
@@ -285,12 +284,12 @@ export class DebugHoverWidget implements IContentWidget {
 		this.valueContainer.hidden = true;
 		this.complexValueContainer.hidden = false;
 
-		await this.tree.setInput(expression);
 		this.complexValueTitle.textContent = expression.value;
 		this.complexValueTitle.title = expression.value;
 		this.layoutTreeAndContainer();
 		this.editor.layoutContentWidget(this);
 		this.scrollbar.scanDomNode();
+		await this.tree.setInput(expression);
 		this.tree.scrollTop = 0;
 		this.tree.scrollLeft = 0;
 
