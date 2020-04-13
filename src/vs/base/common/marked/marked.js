f21f9ea902cb1311a475e7e0830399ad2dfafa4b
Neonit <mail@neonit.de>
2020-04-13 12:44:09 +0200

    fix tabs getting replaced in Markdown (#95096)

--- src/vs/base/common/marked/marked.js
+++ src/vs/base/common/marked/marked.js
@@ -176,7 +176,6 @@ Lexer.lex = function(src, options) {
 Lexer.prototype.lex = function(src) {
   src = src
     .replace(/\r\n|\r/g, '\n')
-    .replace(/\t/g, '    ')
     .replace(/\u00a0/g, ' ')
     .replace(/\u2424/g, '\n');
 
