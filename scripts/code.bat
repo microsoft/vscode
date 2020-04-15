335dd17b8e646f15a6a29a76fd10acd6dabefd1f
Benjamin Pasero <benjpas@microsoft.com>
2020-04-15 19:37:19 +0200

    dev - do not set ELECTRON_DEFAULT_ERROR_MODE on windows

--- scripts/code.bat
+++ scripts/code.bat
@@ -29,7 +29,7 @@ if not exist out yarn compile
 set NODE_ENV=development
 set VSCODE_DEV=1
 set VSCODE_CLI=1
-set ELECTRON_DEFAULT_ERROR_MODE=1
+REM set ELECTRON_DEFAULT_ERROR_MODE=1 TODO@ben to investigate if this helps with builds reporting stacks if renderer crashes
 set ELECTRON_ENABLE_LOGGING=1
 set ELECTRON_ENABLE_STACK_DUMPING=1
 set VSCODE_LOGS=
