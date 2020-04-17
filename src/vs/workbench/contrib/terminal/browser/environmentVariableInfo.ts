b9208b16498ab2b77781f9f803518509f2d2b5b0
Daniel Imms <tyriar@tyriar.com>
2020-04-17 09:49:21 -0700

    Localize environment strings

--- src/vs/workbench/contrib/terminal/browser/environmentVariableInfo.ts
+++ src/vs/workbench/contrib/terminal/browser/environmentVariableInfo.ts
@@ -4,9 +4,9 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { IEnvironmentVariableInfo, IMergedEnvironmentVariableCollection, IMergedEnvironmentVariableCollectionDiff, EnvironmentVariableMutatorType } from 'vs/workbench/contrib/terminal/common/environmentVariable';
-import { format } from 'vs/base/common/strings';
 import { TERMINAL_COMMAND_ID } from 'vs/workbench/contrib/terminal/common/terminal';
 import { ITerminalService } from 'vs/workbench/contrib/terminal/browser/terminal';
+import { localize } from 'vs/nls';
 
 export class EnvironmentVariableInfoStale implements IEnvironmentVariableInfo {
 	constructor(
@@ -17,8 +17,7 @@ export class EnvironmentVariableInfoStale implements IEnvironmentVariableInfo {
 	}
 
 	getInfo(): string {
-		// TODO: Localize
-		let info = 'Extensions want to make the follow changes to the terminal\'s environment:';
+		let info = localize('extensionEnvironmentContribution', "Extensions want to make the follow changes to the terminal's environment:");
 		info += `\n\n${this._summarizeDiff()}`;
 		return info;
 	}
@@ -29,7 +28,7 @@ export class EnvironmentVariableInfoStale implements IEnvironmentVariableInfo {
 
 	getActions(): { label: string, iconClass?: string, run: (target: HTMLElement) => void, commandId: string }[] {
 		return [{
-			label: 'Relaunch terminal',
+			label: localize('relaunchTerminalLabel', "Relaunch terminal"),
 			run: () => this._terminalService.getInstanceFromId(this._terminalId)?.relaunch(),
 			commandId: TERMINAL_COMMAND_ID.RELAUNCH
 		}];
@@ -39,18 +38,18 @@ export class EnvironmentVariableInfoStale implements IEnvironmentVariableInfo {
 		const summary: string[] = [];
 		this._diff.added.forEach((mutators, variable) => {
 			mutators.forEach(mutator => {
-				summary.push(`- ${format(mutatorTypeLabel(mutator.type), mutator.value, variable)}`);
+				summary.push(`- ${mutatorTypeLabel(mutator.type, mutator.value, variable)}`);
 			});
 		});
 		this._diff.changed.forEach((mutators, variable) => {
 			mutators.forEach(mutator => {
-				summary.push(`- "${format(mutatorTypeLabel(mutator.type), mutator.value, variable)}"`);
+				summary.push(`- "${mutatorTypeLabel(mutator.type, mutator.value, variable)}"`);
 			});
 		});
 		this._diff.removed.forEach((mutators, variable) => {
 			mutators.forEach(mutator => {
-				// TODO: Localize
-				summary.push(`- Remove the change "${format(mutatorTypeLabel(mutator.type), mutator.value, variable)}"`);
+				const removePrefixText = localize('removeEnvironmentVariableChange', "Remove the change {0}", mutatorTypeLabel(mutator.type, mutator.value, variable));
+				summary.push(`- ${removePrefixText}`);
 			});
 		});
 		return summary.join('\n');
@@ -64,11 +63,10 @@ export class EnvironmentVariableInfoChangesActive implements IEnvironmentVariabl
 	}
 
 	getInfo(): string {
-		// TODO: Localize
 		const info: string[] = ['Extensions have made changes to this terminal\'s environment:', ''];
 		this._collection.map.forEach((mutators, variable) => {
 			mutators.forEach(mutator => {
-				info.push(`- ${format(mutatorTypeLabel(mutator.type), mutator.value, variable)}`);
+				info.push(`- ${mutatorTypeLabel(mutator.type, mutator.value, variable)}`);
 			});
 		});
 		return info.join('\n');
@@ -79,11 +77,10 @@ export class EnvironmentVariableInfoChangesActive implements IEnvironmentVariabl
 	}
 }
 
-function mutatorTypeLabel(type: EnvironmentVariableMutatorType): string {
-	// TODO: Localize
+function mutatorTypeLabel(type: EnvironmentVariableMutatorType, value: string, variable: string): string {
 	switch (type) {
-		case EnvironmentVariableMutatorType.Prepend: return 'Add `{0}` to the beginning of `{1}`';
-		case EnvironmentVariableMutatorType.Append: return 'Add `{0}` to the end of `{1}`';
-		default: return 'Replace `{1}`\'s value with `{0}`';
+		case EnvironmentVariableMutatorType.Prepend: return localize('prependValueToEnvironmentVariableMarkdown', "Add `{0}` to the beginning of `{1}`", value, variable);
+		case EnvironmentVariableMutatorType.Append: return localize('appendValueToEnvironmentVariableMarkdown', "Add `{0}` to the end of `{1}`", value, variable);
+		default: return localize('replaceEnvironmentVariableWithValueMarkdown', "Replace `{1}`\'s value with `{0}`", value, variable);
 	}
 }
