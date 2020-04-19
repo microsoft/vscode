32d7ac9da685a09095b6317761904fb68d64d2c8
Daniel Imms <tyriar@tyriar.com>
2020-04-18 18:49:18 -0700

    Fix compile in test

--- src/vs/workbench/contrib/terminal/test/browser/links/terminalWebLinkProvider.test.ts
+++ src/vs/workbench/contrib/terminal/test/browser/links/terminalWebLinkProvider.test.ts
@@ -6,11 +6,21 @@
 import * as assert from 'assert';
 import { TerminalWebLinkProvider } from 'vs/workbench/contrib/terminal/browser/links/terminalWebLinkProvider';
 import { Terminal, ILink, IBufferRange, IBufferCellPosition } from 'xterm';
+import { TestInstantiationService } from 'vs/platform/instantiation/test/common/instantiationServiceMock';
+import { TestConfigurationService } from 'vs/platform/configuration/test/common/testConfigurationService';
+import { IConfigurationService } from 'vs/platform/configuration/common/configuration';
 
 suite('Workbench - TerminalWebLinkProvider', () => {
+	let instantiationService: TestInstantiationService;
+
+	setup(() => {
+		instantiationService = new TestInstantiationService();
+		instantiationService.stub(IConfigurationService, TestConfigurationService);
+	});
+
 	async function assertLink(text: string, expected: { text: string, range: [number, number][] }) {
 		const xterm = new Terminal();
-		const provider = new TerminalWebLinkProvider(xterm, () => { }, () => { });
+		const provider = instantiationService.createInstance(TerminalWebLinkProvider, xterm, () => { }, () => { });
 
 		// Write the text and wait for the parser to finish
 		await new Promise<void>(r => xterm.write(text, r));
