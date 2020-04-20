34223ae32fd19387a244638ec2989f0ea8f94e42
Matt Bierner <matb@microsoft.com>
2020-04-20 16:12:15 -0700

    Add `reopen with` command to editor title
    This also adds a `availableEditors` context key that consists of a comma seperated list of editor ids that can be used for `reopen with`

--- src/vs/workbench/common/editor.ts
+++ src/vs/workbench/common/editor.ts
@@ -32,6 +32,7 @@ import { IFilesConfigurationService, AutoSaveMode } from 'vs/workbench/services/
 export const DirtyWorkingCopiesContext = new RawContextKey<boolean>('dirtyWorkingCopies', false);
 export const ActiveEditorContext = new RawContextKey<string | null>('activeEditor', null);
 export const ActiveEditorIsReadonlyContext = new RawContextKey<boolean>('activeEditorIsReadonly', false);
+export const ActiveEditorAvailableEditorsContext = new RawContextKey<string>('availableEditors', '');
 export const EditorsVisibleContext = new RawContextKey<boolean>('editorIsOpen', false);
 export const EditorPinnedContext = new RawContextKey<boolean>('editorPinned', false);
 export const EditorGroupActiveEditorDirtyContext = new RawContextKey<boolean>('groupActiveEditorDirty', false);
