b005f534a60e8ba622ca1416cf8c517b674f2fcd
João Moreno <joao.moreno@microsoft.com>
2020-04-23 14:54:27 +0200

    support codicons in welcome view buttons

--- src/vs/workbench/browser/parts/views/viewPaneContainer.ts
+++ src/vs/workbench/browser/parts/views/viewPaneContainer.ts
@@ -458,7 +458,7 @@ export abstract class ViewPane extends Pane implements IView {
 
 				if (linkedText.nodes.length === 1 && typeof linkedText.nodes[0] !== 'string') {
 					const node = linkedText.nodes[0];
-					const button = new Button(this.viewWelcomeContainer, { title: node.title });
+					const button = new Button(this.viewWelcomeContainer, { title: node.title, supportCodicons: true });
 					button.label = node.label;
 					button.onDidClick(_ => {
 						this.telemetryService.publicLog2<{ viewId: string, uri: string }, WelcomeActionClassification>('views.welcomeAction', { viewId: this.id, uri: node.href });
