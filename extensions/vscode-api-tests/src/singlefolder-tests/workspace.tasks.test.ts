2025795291b4c8c175514ca63e24041fe2e33725
Daniel Imms <tyriar@tyriar.com>
2020-04-27 07:34:54 -0700

    Add ref to why test is disabled

--- extensions/vscode-api-tests/src/singlefolder-tests/workspace.tasks.test.ts
+++ extensions/vscode-api-tests/src/singlefolder-tests/workspace.tasks.test.ts
@@ -6,6 +6,8 @@
 import * as assert from 'assert';
 import { window, tasks, Disposable, TaskDefinition, Task, EventEmitter, CustomExecution, Pseudoterminal, TaskScope, commands, Task2, env, UIKind } from 'vscode';
 
+// Disable tasks tests:
+// - Web https://github.com/microsoft/vscode/issues/90528
 ((env.uiKind === UIKind.Web) ? suite.skip : suite)('vscode API - tasks', () => {
 
 	suite('Tasks', () => {
