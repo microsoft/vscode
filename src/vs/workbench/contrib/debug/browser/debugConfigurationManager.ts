7754cccc5ba9b2cf5b7fcb3532c063588ff240a9
Andre Weinand <aweinand@microsoft.com>
2020-04-27 16:06:39 +0200

    rename "Trigger" to "TriggerKind"; see #88230

--- src/vs/workbench/contrib/debug/browser/debugConfigurationManager.ts
+++ src/vs/workbench/contrib/debug/browser/debugConfigurationManager.ts
@@ -38,7 +38,7 @@ import { sequence } from 'vs/base/common/async';
 import { IHistoryService } from 'vs/workbench/services/history/common/history';
 import { first } from 'vs/base/common/arrays';
 import { getVisibleAndSorted } from 'vs/workbench/contrib/debug/common/debugUtils';
-import { DebugConfigurationProviderTrigger } from 'vs/workbench/api/common/extHostTypes';
+import { DebugConfigurationProviderTriggerKind } from 'vs/workbench/api/common/extHostTypes';
 
 const jsonRegistry = Registry.as<IJSONContributionRegistry>(JSONExtensions.JSONContribution);
 jsonRegistry.registerSchema(launchSchemaId, launchSchema);
@@ -197,12 +197,12 @@ export class ConfigurationManager implements IConfigurationManager {
 	/**
 	 * if scope is not specified,a value of DebugConfigurationProvideTrigger.Initial is assumed.
 	 */
-	hasDebugConfigurationProvider(debugType: string, trigger?: DebugConfigurationProviderTrigger): boolean {
-		if (trigger === undefined) {
-			trigger = DebugConfigurationProviderTrigger.Initial;
+	hasDebugConfigurationProvider(debugType: string, triggerKind?: DebugConfigurationProviderTriggerKind): boolean {
+		if (triggerKind === undefined) {
+			triggerKind = DebugConfigurationProviderTriggerKind.Initial;
 		}
 		// check if there are providers for the given type that contribute a provideDebugConfigurations method
-		const providers = this.configProviders.filter(p => p.provideDebugConfigurations && (p.type === debugType) && (p.trigger === trigger));
+		const providers = this.configProviders.filter(p => p.provideDebugConfigurations && (p.type === debugType) && (p.triggerKind === triggerKind));
 		return providers.length > 0;
 	}
 
@@ -241,14 +241,14 @@ export class ConfigurationManager implements IConfigurationManager {
 
 	async provideDebugConfigurations(folderUri: uri | undefined, type: string, token: CancellationToken): Promise<any[]> {
 		await this.activateDebuggers('onDebugInitialConfigurations');
-		const results = await Promise.all(this.configProviders.filter(p => p.type === type && p.trigger === DebugConfigurationProviderTrigger.Initial && p.provideDebugConfigurations).map(p => p.provideDebugConfigurations!(folderUri, token)));
+		const results = await Promise.all(this.configProviders.filter(p => p.type === type && p.triggerKind === DebugConfigurationProviderTriggerKind.Initial && p.provideDebugConfigurations).map(p => p.provideDebugConfigurations!(folderUri, token)));
 
 		return results.reduce((first, second) => first.concat(second), []);
 	}
 
 	async getDynamicProviders(): Promise<{ label: string, pick: () => Promise<{ launch: ILaunch, config: IConfig } | undefined> }[]> {
 		await this.activateDebuggers('onDebugDynamicConfigurations');
-		const dynamicProviders = this.configProviders.filter(p => p.trigger === DebugConfigurationProviderTrigger.Dynamic && p.provideDebugConfigurations);
+		const dynamicProviders = this.configProviders.filter(p => p.triggerKind === DebugConfigurationProviderTriggerKind.Dynamic && p.provideDebugConfigurations);
 		return dynamicProviders.map(provider => {
 			return {
 				label: this.getDebuggerLabel(provider.type)!,
