23f4a1ef7250396f8fc1c6e0856998c3b048809c
rebornix <penn.lv@gmail.com>
2020-04-29 11:36:24 -0700

    Fix #96563.

--- src/vs/workbench/contrib/notebook/browser/viewModel/baseCellViewModel.ts
+++ src/vs/workbench/contrib/notebook/browser/viewModel/baseCellViewModel.ts
@@ -175,7 +175,7 @@ export abstract class BaseCellViewModel extends Disposable implements ICellViewM
 	}
 
 	detachTextEditor() {
-		this._editorViewStates = this.saveViewState();
+		this.saveViewState();
 		// decorations need to be cleared first as editors can be resued.
 		this._resolvedDecorations.forEach(value => {
 			let resolvedid = value.id;
@@ -201,17 +201,17 @@ export abstract class BaseCellViewModel extends Disposable implements ICellViewM
 
 	abstract save(): void;
 
-	private saveViewState(): editorCommon.ICodeEditorViewState | null {
+	private saveViewState(): void {
 		if (!this._textEditor) {
-			return null;
+			return;
 		}
 
-		return this._textEditor.saveViewState();
+		this._editorViewStates = this._textEditor.saveViewState();
 	}
 
 	saveEditorViewState() {
 		if (this._textEditor) {
-			this._editorViewStates = this.saveViewState();
+			this._editorViewStates = this._textEditor.saveViewState();
 		}
 
 		return this._editorViewStates;
