d3b60a250a0cc6d6779df8fcf8153e12dff2b48a
Greg Van Liew <gregvanl@microsoft.com>
2020-04-30 00:57:51 -0700

    Settings description typos (#96641)

--- src/vs/workbench/browser/workbench.contribution.ts
+++ src/vs/workbench/browser/workbench.contribution.ts
@@ -103,7 +103,7 @@ import { workbenchConfigurationNodeBase } from 'vs/workbench/common/configuratio
 			},
 			'workbench.editor.showIcons': {
 				'type': 'boolean',
-				'description': nls.localize('showIcons', "Controls whether opened editors should show with an icon or not. This requires an file icon theme to be enabled as well."),
+				'description': nls.localize('showIcons', "Controls whether opened editors should show with an icon or not. This requires a file icon theme to be enabled as well."),
 				'default': true
 			},
 			'workbench.editor.enablePreview': {
