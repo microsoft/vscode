c2873850fb3eba53888f41d1f2bbce7a4f832f8d
Sandeep Somavarapu <sasomava@microsoft.com>
2020-05-04 09:37:57 +0200

    update comment

--- src/vs/workbench/services/environment/browser/environmentService.ts
+++ src/vs/workbench/services/environment/browser/environmentService.ts
@@ -117,7 +117,7 @@ export class BrowserWorkbenchEnvironmentService implements IWorkbenchEnvironment
 	/*
 	 * In Web every workspace can potentially have scoped user-data and/or extensions and if Sync state is shared then it can make
 	 * Sync error prone - say removing extensions from another workspace. Hence scope Sync state per workspace.
-	 * Sync scoped to a workspace is capable of handling even if workspaces/windows share same user data and extensions.
+	 * Sync scoped to a workspace is capable of handling opening same workspace in multiple windows.
 	 */
 	@memoize
 	get userDataSyncHome(): URI { return joinPath(this.userRoamingDataHome, 'sync', this.options.workspaceId); }
