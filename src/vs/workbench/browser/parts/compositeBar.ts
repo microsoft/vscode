bd0efff9e3f36d6b3e1045cee9887003af8034d7
SteVen Batten <steven.m.batten@outlook.com>
2020-05-05 19:12:44 -0700

    reduce flicker when moving between panel/actbar

--- src/vs/workbench/browser/parts/compositeBar.ts
+++ src/vs/workbench/browser/parts/compositeBar.ts
@@ -275,7 +275,8 @@ export class CompositeBar extends Widget implements ICompositeBar {
 		// Add to the model
 		if (this.model.add(id, name, order)) {
 			this.computeSizes([this.model.findItem(id)]);
-			this.updateCompositeSwitcher();
+			// Set timeout helps prevent flicker
+			setTimeout(() => this.updateCompositeSwitcher(), 0);
 		}
 	}
 
