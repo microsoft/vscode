71bb111a0628c03d577fca00e0d19816ba74b01e
Sandeep Somavarapu <sasomava@microsoft.com>
2020-05-11 13:28:54 +0200

    #93960 Ability to compare globalState with current state

--- src/vs/platform/userDataSync/common/extensionsSync.ts
+++ src/vs/platform/userDataSync/common/extensionsSync.ts
@@ -39,7 +39,7 @@ interface ILastSyncUserData extends IRemoteUserData {
 
 export class ExtensionsSynchroniser extends AbstractSynchroniser implements IUserDataSynchroniser {
 
-	private static readonly EXTENSIONS_DATA_URI = URI.from({ scheme: USER_DATA_SYNC_SCHEME, authority: 'extensions', path: `/data.json` });
+	private static readonly EXTENSIONS_DATA_URI = URI.from({ scheme: USER_DATA_SYNC_SCHEME, authority: 'extensions', path: `/current.json` });
 	protected readonly version: number = 2;
 	protected isEnabled(): boolean { return super.isEnabled() && this.extensionGalleryService.isEnabled(); }
 
