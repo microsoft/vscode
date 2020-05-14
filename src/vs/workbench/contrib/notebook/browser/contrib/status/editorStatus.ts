3e2df0a371fdc832240f9de5c4749c0bf1257871
rebornix <penn.lv@gmail.com>
2020-05-14 11:06:24 -0700

    hide kernel switcher

--- src/vs/workbench/contrib/notebook/browser/contrib/status/editorStatus.ts
+++ src/vs/workbench/contrib/notebook/browser/contrib/status/editorStatus.ts
@@ -3,11 +3,8 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { Disposable, DisposableStore, MutableDisposable } from 'vs/base/common/lifecycle';
 import { INotebookEditor, NOTEBOOK_EDITOR_FOCUSED, NOTEBOOK_IS_ACTIVE_EDITOR } from 'vs/workbench/contrib/notebook/browser/notebookBrowser';
-import { IWorkbenchContribution, IWorkbenchContributionsRegistry, Extensions as WorkbenchExtensions } from 'vs/workbench/common/contributions';
 import { IEditorService } from 'vs/workbench/services/editor/common/editorService';
-import { IStatusbarService, IStatusbarEntryAccessor, IStatusbarEntry, StatusbarAlignment } from 'vs/workbench/services/statusbar/common/statusbar';
 import { IQuickInputService, QuickPickInput, IQuickPickItem } from 'vs/platform/quickinput/common/quickInput';
 import { INotebookService } from 'vs/workbench/contrib/notebook/common/notebookService';
 import * as nls from 'vs/nls';
@@ -15,69 +12,66 @@ import { registerAction2, Action2 } from 'vs/platform/actions/common/actions';
 import { NOTEBOOK_ACTIONS_CATEGORY, INotebookCellActionContext } from 'vs/workbench/contrib/notebook/browser/contrib/coreActions';
 import { ContextKeyExpr } from 'vs/platform/contextkey/common/contextkey';
 import { ServicesAccessor } from 'vs/platform/instantiation/common/instantiation';
-import { INotebookKernelInfo } from 'vs/workbench/contrib/notebook/common/notebookCommon';
-import { Registry } from 'vs/platform/registry/common/platform';
-import { LifecyclePhase } from 'vs/platform/lifecycle/common/lifecycle';
 
-export class NotebookEditorStatus extends Disposable implements IWorkbenchContribution {
-	private _localStore: DisposableStore = new DisposableStore();
-	private kernelInfoElement = this._register(new MutableDisposable<IStatusbarEntryAccessor>());
+// export class NotebookEditorStatus extends Disposable implements IWorkbenchContribution {
+// 	private _localStore: DisposableStore = new DisposableStore();
+// 	private kernelInfoElement = this._register(new MutableDisposable<IStatusbarEntryAccessor>());
 
-	constructor(
-		@IEditorService private readonly editorService: IEditorService,
-		@IStatusbarService private readonly statusbarService: IStatusbarService,
-	) {
-		super();
-		this.registerListeners();
-	}
+// 	constructor(
+// 		@IEditorService private readonly editorService: IEditorService,
+// 		@IStatusbarService private readonly statusbarService: IStatusbarService,
+// 	) {
+// 		super();
+// 		this.registerListeners();
+// 	}
 
-	private registerListeners(): void {
-		this._register(this.editorService.onDidActiveEditorChange(() => this.updateStatusBar()));
-		this.updateStatusBar();
-	}
+// 	private registerListeners(): void {
+// 		this._register(this.editorService.onDidActiveEditorChange(() => this.updateStatusBar()));
+// 		this.updateStatusBar();
+// 	}
 
-	private async updateStatusBar(): Promise<void> {
-		this._localStore.clear();
+// 	private async updateStatusBar(): Promise<void> {
+// 		this._localStore.clear();
 
-		const activeEditorPane = this.editorService.activeEditorPane as any | undefined;
-		if (!activeEditorPane?.isNotebookEditor) {
-			this.kernelInfoElement.clear();
-			return;
-		}
-		const editor = activeEditorPane.getControl() as INotebookEditor;
-		this._localStore.add(editor.onDidChangeKernel(() => {
-			this.updateKernelInfo(editor.activeKernel);
-		}));
+// 		const activeEditorPane = this.editorService.activeEditorPane as any | undefined;
+// 		if (!activeEditorPane?.isNotebookEditor) {
+// 			this.kernelInfoElement.clear();
+// 			return;
+// 		}
+// 		const editor = activeEditorPane.getControl() as INotebookEditor;
+// 		this._localStore.add(editor.onDidChangeKernel(() => {
+// 			this.updateKernelInfo(editor.activeKernel);
+// 		}));
 
-		this.updateKernelInfo(editor.activeKernel);
-	}
+// 		this.updateKernelInfo(editor.activeKernel);
+// 	}
 
-	private updateKernelInfo(kernelInfo: INotebookKernelInfo | undefined) {
-		if (!kernelInfo) {
-			this.kernelInfoElement.clear();
-			return;
-		}
+// 	private updateKernelInfo(kernelInfo: INotebookKernelInfo | undefined) {
+// 		if (!kernelInfo) {
+// 			this.kernelInfoElement.clear();
+// 			return;
+// 		}
 
-		const props: IStatusbarEntry = {
-			text: kernelInfo.label,
-			ariaLabel: kernelInfo.label,
-			tooltip: nls.localize('selectKernel', "Select Notebook Kernel"),
-			command: 'notebook.selectKernel'
-		};
+// 		const props: IStatusbarEntry = {
+// 			text: kernelInfo.label,
+// 			ariaLabel: kernelInfo.label,
+// 			tooltip: nls.localize('selectKernel', "Select Notebook Kernel"),
+// 			command: 'notebook.selectKernel'
+// 		};
 
-		this.updateElement(this.kernelInfoElement, props, 'status.notebook.kernel', nls.localize('selectKernel', "Select Notebook Kernel"), StatusbarAlignment.RIGHT, 50);
-	}
+// 		this.updateElement(this.kernelInfoElement, props, 'status.notebook.kernel', nls.localize('selectKernel', "Select Notebook Kernel"), StatusbarAlignment.RIGHT, 50);
+// 	}
 
-	private updateElement(element: MutableDisposable<IStatusbarEntryAccessor>, props: IStatusbarEntry, id: string, name: string, alignment: StatusbarAlignment, priority: number) {
-		if (!element.value) {
-			element.value = this.statusbarService.addEntry(props, id, name, alignment, priority);
-		} else {
-			element.value.update(props);
-		}
-	}
-}
+// 	private updateElement(element: MutableDisposable<IStatusbarEntryAccessor>, props: IStatusbarEntry, id: string, name: string, alignment: StatusbarAlignment, priority: number) {
+// 		if (!element.value) {
+// 			element.value = this.statusbarService.addEntry(props, id, name, alignment, priority);
+// 		} else {
+// 			element.value.update(props);
+// 		}
+// 	}
+// }
 
-Registry.as<IWorkbenchContributionsRegistry>(WorkbenchExtensions.Workbench).registerWorkbenchContribution(NotebookEditorStatus, LifecyclePhase.Eventually);
+// Registry.as<IWorkbenchContributionsRegistry>(WorkbenchExtensions.Workbench).registerWorkbenchContribution(NotebookEditorStatus, LifecyclePhase.Eventually);
 
 
 registerAction2(class extends Action2 {
@@ -87,6 +81,13 @@ registerAction2(class extends Action2 {
 			category: NOTEBOOK_ACTIONS_CATEGORY,
 			title: nls.localize('notebookActions.selectKernel', "Select Notebook Kernel"),
 			precondition: ContextKeyExpr.and(NOTEBOOK_IS_ACTIVE_EDITOR, NOTEBOOK_EDITOR_FOCUSED),
+			icon: { id: 'codicon/server-environment' },
+			// menu: {
+			// 	id: MenuId.EditorTitle,
+			// 	// when: ContextKeyExpr.and(ContextKeyEqualsExpr.create('view', that.id), that.refreshContextKey),
+			// 	group: 'navigation',
+			// 	order: -2,
+			// },
 			f1: true
 		});
 	}
