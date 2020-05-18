1c98bbbbc25ff34b2415335be52c77e5d2f13511
João Moreno <joao.moreno@microsoft.com>
2020-05-18 14:13:08 +0200

    add github extension

---
+++ extensions/github/src/extension.ts
@@ -0,0 +1,10 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import * as vscode from 'vscode';
+
+export async function activate(context: vscode.ExtensionContext) {
+	console.log('github is active!');
+}
