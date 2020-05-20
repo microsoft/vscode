a67d00db9790d7caeac9a0560a549fc44593375c
João Moreno <joao.moreno@microsoft.com>
2020-05-20 14:03:57 +0200

    fixes #98157

--- extensions/github/src/publish.ts
+++ extensions/github/src/publish.ts
@@ -5,6 +5,8 @@
 
 import * as vscode from 'vscode';
 import * as nls from 'vscode-nls';
+import * as path from 'path';
+import { promises as fs } from 'fs';
 import { API as GitAPI, Repository } from './typings/git';
 import { getOctokit } from './auth';
 
@@ -42,7 +44,7 @@ export async function publishRepository(gitAPI: GitAPI, repository?: Repository)
 		folder = pick.folder;
 	}
 
-	const quickpick = vscode.window.createQuickPick<vscode.QuickPickItem & { repo?: string, auth?: 'https' | 'ssh' }>();
+	let quickpick = vscode.window.createQuickPick<vscode.QuickPickItem & { repo?: string, auth?: 'https' | 'ssh' }>();
 	quickpick.ignoreFocusOut = true;
 
 	quickpick.placeholder = 'Repository Name';
@@ -93,6 +95,39 @@ export async function publishRepository(gitAPI: GitAPI, repository?: Repository)
 
 	if (!repo) {
 		return;
+	}
+
+	quickpick = vscode.window.createQuickPick();
+	quickpick.placeholder = localize('ignore', "Select which files should be included in the repository.");
+	quickpick.canSelectMany = true;
+	quickpick.show();
+
+	try {
+		quickpick.busy = true;
+
+		const repositoryPath = folder.uri.fsPath;
+		const currentPath = path.join(repositoryPath);
+		const children = await fs.readdir(currentPath);
+		quickpick.items = children.map(name => ({ label: name }));
+		quickpick.selectedItems = quickpick.items;
+		quickpick.busy = false;
+
+		const result = await Promise.race([
+			new Promise<readonly vscode.QuickPickItem[]>(c => quickpick.onDidAccept(() => c(quickpick.selectedItems))),
+			new Promise<undefined>(c => quickpick.onDidHide(() => c(undefined)))
+		]);
+
+		if (!result) {
+			return;
+		}
+
+		const ignored = new Set(children);
+		result.forEach(c => ignored.delete(c.label));
+
+		const raw = [...ignored].map(i => `/${i}`).join('\n');
+		await fs.writeFile(path.join(repositoryPath, '.gitignore'), raw, 'utf8');
+	} finally {
+		quickpick.dispose();
 	}
 
 	const githubRepository = await vscode.window.withProgress({ location: vscode.ProgressLocation.Notification, cancellable: false, title: 'Publish to GitHub' }, async progress => {
