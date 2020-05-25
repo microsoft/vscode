7139ecdb6bb23d1470fe99fd46432ad7fef6e201
Benjamin Pasero <benjpas@microsoft.com>
2020-05-25 09:57:14 +0200

    sandbox - do not double register electron service

--- src/vs/workbench/services/electron/electron-browser/electronService.ts
+++ src/vs/workbench/services/electron/electron-browser/electronService.ts
@@ -5,7 +5,6 @@
 
 import { IElectronService } from 'vs/platform/electron/node/electron';
 import { IMainProcessService } from 'vs/platform/ipc/common/mainProcessService';
-import { registerSingleton } from 'vs/platform/instantiation/common/extensions';
 import { createChannelSender } from 'vs/base/parts/ipc/common/ipc';
 
 export class ElectronService {
@@ -18,5 +17,3 @@ export class ElectronService {
 		return createChannelSender<IElectronService>(mainProcessService.getChannel('electron'), { context: mainProcessService.windowId });
 	}
 }
-
-registerSingleton(IElectronService, ElectronService, true);
