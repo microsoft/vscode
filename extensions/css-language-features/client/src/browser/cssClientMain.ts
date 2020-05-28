1ece4c4c2549a788806913c51af2378fa002ba0a
Martin Aeschlimann <martinae@microsoft.com>
2020-05-28 18:04:11 +0200

    adopt new languageserver node modules

--- extensions/css-language-features/client/src/browser/cssClientMain.ts
+++ extensions/css-language-features/client/src/browser/cssClientMain.ts
@@ -4,27 +4,13 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { ExtensionContext } from 'vscode';
-import { CommonLanguageClient, LanguageClientOptions, MessageTransports } from 'vscode-languageclient';
+import { LanguageClientOptions } from 'vscode-languageclient';
 import { startClient, LanguageClientConstructor } from '../cssClient';
-import { BrowserMessageReader, BrowserMessageWriter } from 'vscode-jsonrpc/lib/browser/main';
+import { LanguageClient } from 'vscode-languageclient/browser';
 
 declare const Worker: {
 	new(stringUrl: string): any;
 };
-
-class BrowserLanguageClient extends CommonLanguageClient {
-
-	constructor(id: string, name: string, clientOptions: LanguageClientOptions, private worker: any) {
-		super(id, name, clientOptions);
-	}
-
-	protected createMessageTransports(_encoding: string): Promise<MessageTransports> {
-		const reader = new BrowserMessageReader(this.worker);
-		const writer = new BrowserMessageWriter(this.worker);
-		return Promise.resolve({ reader, writer });
-	}
-
-}
 
 // this method is called when vs code is activated
 export function activate(context: ExtensionContext) {
@@ -32,7 +18,7 @@ export function activate(context: ExtensionContext) {
 	try {
 		const worker = new Worker(serverMain);
 		const newLanguageClient: LanguageClientConstructor = (id: string, name: string, clientOptions: LanguageClientOptions) => {
-			return new BrowserLanguageClient(id, name, clientOptions, worker);
+			return new LanguageClient(id, name, clientOptions, worker);
 		};
 
 		startClient(context, newLanguageClient, {});
--- extensions/css-language-features/server/src/browser/cssServerMain.ts
+++ extensions/css-language-features/server/src/browser/cssServerMain.ts
@@ -3,24 +3,14 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { ProtocolConnection, createProtocolConnection, Logger, createConnection, InitializeParams, WatchDog } from 'vscode-languageserver';
+import { createConnection, BrowserMessageReader, BrowserMessageWriter } from 'vscode-languageserver/browser';
 import { startServer } from '../cssServer';
-import { BrowserMessageReader, BrowserMessageWriter } from 'vscode-jsonrpc/lib/browser/main';
 
 declare let self: any;
 
 const messageReader = new BrowserMessageReader(self);
 const messageWriter = new BrowserMessageWriter(self);
 
-const watchDog: WatchDog = {
-	shutdownReceived: false,
-	initialize(_params: InitializeParams): void { },
-	exit(_code: number): void { }
-};
-
-const connectionFactory = (logger: Logger): ProtocolConnection => {
-	return createProtocolConnection(messageReader, messageWriter, logger);
-};
-const connection = createConnection(connectionFactory, watchDog);
+const connection = createConnection(messageReader, messageWriter);
 
 startServer(connection, {});
