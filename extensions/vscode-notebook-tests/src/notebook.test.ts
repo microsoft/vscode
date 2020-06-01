aa803039784c1ca4ec2ac3c87bd381fc0d2acec7
Matt Bierner <matb@microsoft.com>
2020-06-01 16:13:37 -0700

    Fix scheme for webview test

--- extensions/vscode-notebook-tests/src/notebook.test.ts
+++ extensions/vscode-notebook-tests/src/notebook.test.ts
@@ -746,7 +746,7 @@ suite('webview', () => {
 		await vscode.commands.executeCommand('vscode.openWith', resource, 'notebookCoreTest');
 		assert.equal(vscode.notebook.activeNotebookEditor !== undefined, true, 'notebook first');
 		const uri = vscode.notebook.activeNotebookEditor!.asWebviewUri(vscode.Uri.file('./hello.png'));
-		assert.equal(uri.scheme, 'vscode-webview-resource');
+		assert.equal(uri.scheme, 'vscode-resource');
 		await vscode.commands.executeCommand('workbench.action.closeAllEditors');
 	});
 
