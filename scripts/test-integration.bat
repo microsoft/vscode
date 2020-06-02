1bfa086adb9271ff56be11821da686e4cfffb672
Matt Bierner <matb@microsoft.com>
2020-06-01 23:21:58 -0700

    Make sure we build vscode-notebook-tests on windows

--- scripts/test-integration.bat
+++ scripts/test-integration.bat
@@ -22,6 +22,7 @@ if "%INTEGRATION_TEST_ELECTRON_PATH%"=="" (
 	call yarn gulp 	compile-extension:vscode-api-tests^
 					compile-extension:vscode-colorize-tests^
 					compile-extension:markdown-language-features^
+					compile-extension:vscode-notebook-tests^
 					compile-extension:emmet^
 					compile-extension:css-language-features-server^
 					compile-extension:html-language-features-server^
