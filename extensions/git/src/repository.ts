fc797d2430903cd49c06e97ed92be84bccb4eca8
Asif Hasan <d.asif.hasan@gmail.com>
2020-06-03 18:00:11 -0500

    fix 97472

--- extensions/git/src/repository.ts
+++ extensions/git/src/repository.ts
@@ -1162,11 +1162,12 @@ export class Repository implements Disposable {
 				const fetchOnPull = config.get<boolean>('fetchOnPull');
 				const tags = config.get<boolean>('pullTags');
 
+				// When fetchOnPull is enabled, fetch all branches when pulling
 				if (fetchOnPull) {
-					await this.repository.pull(rebase, undefined, undefined, { unshallow, tags });
-				} else {
-					await this.repository.pull(rebase, remote, branch, { unshallow, tags });
+					await this.repository.fetch({ all: true });
 				}
+
+				await this.repository.pull(rebase, remote, branch, { unshallow, tags });
 			});
 		});
 	}
