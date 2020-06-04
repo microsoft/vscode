b3c2ecd6599e0f3d2c001db6b3aa7aa272531f55
Matt Bierner <matb@microsoft.com>
2020-06-04 14:17:02 -0700

    Improve viewType documentation
    Fixes #99264

--- src/vs/workbench/contrib/customEditor/common/extensionPoint.ts
+++ src/vs/workbench/contrib/customEditor/common/extensionPoint.ts
@@ -45,7 +45,7 @@ const CustomEditorsContribution: IJSONSchema = {
 		properties: {
 			[Fields.viewType]: {
 				type: 'string',
-				description: nls.localize('contributes.viewType', 'Unique identifier of the custom editor.'),
+				markdownDescription: nls.localize('contributes.viewType', 'Identifier for the custom editor. This must be unique across all custom editors, so we recommend including your extension id as part of `viewType`. The `viewType` is used when registering custom editors with `vscode.registerCustomEditorProvider` and in the `onCustomEditor:${id}` [activation event](https://code.visualstudio.com/api/references/activation-events).'),
 			},
 			[Fields.displayName]: {
 				type: 'string',
