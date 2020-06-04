c5c0989c5a7bf3b2556328f76162c9bda530250e
rebornix <penn.lv@gmail.com>
2020-06-04 10:50:57 -0700

    stop relayout if the list is disposed or cell is deleted.

--- src/vs/workbench/contrib/notebook/browser/notebookEditorWidget.ts
+++ src/vs/workbench/contrib/notebook/browser/notebookEditorWidget.ts
@@ -793,6 +793,10 @@ export class NotebookEditorWidget extends Disposable implements INotebookEditor 
 		}
 
 		let relayout = (cell: ICellViewModel, height: number) => {
+			if (this._isDisposed) {
+				return;
+			}
+
 			this.list?.updateElementHeight2(cell, height);
 		};
 
@@ -1012,6 +1016,10 @@ export class NotebookEditorWidget extends Disposable implements INotebookEditor 
 	async deleteNotebookCell(cell: ICellViewModel): Promise<boolean> {
 		if (!this.notebookViewModel!.metadata.editable) {
 			return false;
+		}
+
+		if (this.pendingLayouts.has(cell)) {
+			this.pendingLayouts.get(cell)!.dispose();
 		}
 
 		const index = this.notebookViewModel!.getCellIndex(cell);
