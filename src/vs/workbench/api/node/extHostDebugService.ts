049b0b81e72b9e1676a9e37e5a8cd0aca47fba2e
Matt Bierner <matb@microsoft.com>
2020-06-05 11:52:35 -0700

    Supress TS 4.0 delete operator error
    Cast to `any` while we work on a proper fix

--- src/vs/workbench/api/node/extHostDebugService.ts
+++ src/vs/workbench/api/node/extHostDebugService.ts
@@ -97,7 +97,7 @@ export class ExtHostDebugService extends ExtHostDebugServiceBase {
 					cwd: args.cwd,
 					name: args.title || nls.localize('debug.terminal.title', "debuggee"),
 				};
-				delete args.cwd;
+				delete (args as any).cwd; // TODO: remove this any cast
 				this._integratedTerminalInstance = this._terminalService.createTerminalFromOptions(options);
 			}
 
