5ae8f9321a5c64f67fb25c9f71ea59724539e581
Benjamin Pasero <benjpas@microsoft.com>
2020-06-08 12:38:27 +0200

    model resolver - add tests for #98653

--- src/vs/workbench/services/textmodelResolver/test/browser/textModelResolverService.test.ts
+++ src/vs/workbench/services/textmodelResolver/test/browser/textModelResolverService.test.ts
@@ -17,6 +17,7 @@ import { TextFileEditorModelManager } from 'vs/workbench/services/textfile/commo
 import { Event } from 'vs/base/common/event';
 import { timeout } from 'vs/base/common/async';
 import { UntitledTextEditorInput } from 'vs/workbench/services/untitled/common/untitledTextEditorInput';
+import { createTextBufferFactory } from 'vs/editor/common/model/textModel';
 
 suite('Workbench - TextModelResolverService', () => {
 
@@ -92,6 +93,63 @@ suite('Workbench - TextModelResolverService', () => {
 		ref.dispose();
 		await timeout(0);  // due to the reference resolving the model first which is async
 		assert.equal(disposed, true);
+	});
+
+	test('resolved dirty file eventually disposes', async function () {
+		const textModel = instantiationService.createInstance(TextFileEditorModel, toResource.call(this, '/path/file_resolver.txt'), 'utf8', undefined);
+		(<TestTextFileEditorModelManager>accessor.textFileService.files).add(textModel.resource, textModel);
+
+		const loadedModel = await textModel.load();
+
+		loadedModel.updateTextEditorModel(createTextBufferFactory('make dirty'));
+
+		const ref = await accessor.textModelResolverService.createModelReference(textModel.resource);
+
+		let disposed = false;
+		Event.once(loadedModel.onDispose)(() => {
+			disposed = true;
+		});
+
+		ref.dispose();
+		await timeout(0);
+		assert.equal(disposed, false); // not disposed because model still dirty
+
+		loadedModel.revert();
+
+		await timeout(0);
+		assert.equal(disposed, true); // now disposed because model got reverted
+	});
+
+	test('resolved dirty file does not dispose when new reference created', async function () {
+		const textModel = instantiationService.createInstance(TextFileEditorModel, toResource.call(this, '/path/file_resolver.txt'), 'utf8', undefined);
+		(<TestTextFileEditorModelManager>accessor.textFileService.files).add(textModel.resource, textModel);
+
+		const loadedModel = await textModel.load();
+
+		loadedModel.updateTextEditorModel(createTextBufferFactory('make dirty'));
+
+		const ref1 = await accessor.textModelResolverService.createModelReference(textModel.resource);
+
+		let disposed = false;
+		Event.once(loadedModel.onDispose)(() => {
+			disposed = true;
+		});
+
+		ref1.dispose();
+		await timeout(0);
+		assert.equal(disposed, false); // not disposed because model still dirty
+
+		const ref2 = await accessor.textModelResolverService.createModelReference(textModel.resource);
+
+		loadedModel.revert();
+
+		await timeout(0);
+		assert.equal(disposed, false); // not disposed because we got another ref meanwhile
+
+		ref2.dispose();
+
+		await timeout(0);
+		assert.equal(disposed, true); // now disposed because last ref got disposed
 	});
 
 	test('resolve untitled', async () => {
