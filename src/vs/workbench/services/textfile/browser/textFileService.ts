e5ad2b22c9432c45e9395b02a4e8f0f413733da6
Benjamin Pasero <benjpas@microsoft.com>
2020-06-09 09:29:16 +0200

    Cannot save deleted file with different case (fix #99650)

--- src/vs/workbench/services/textfile/browser/textFileService.ts
+++ src/vs/workbench/services/textfile/browser/textFileService.ts
@@ -231,7 +231,9 @@ export abstract class AbstractTextFileService extends Disposable implements ITex
 		// If the target is different but of same identity, we
 		// move the source to the target, knowing that the
 		// underlying file system cannot have both and then save.
-		if (this.fileService.canHandleResource(source) && this.uriIdentityService.extUri.isEqual(source, target)) {
+		// However, this will only work if the source exists
+		// and is not orphaned, so we need to check that too.
+		if (this.fileService.canHandleResource(source) && this.uriIdentityService.extUri.isEqual(source, target) && (await this.fileService.exists(source))) {
 			await this.workingCopyFileService.move(source, target);
 
 			return this.save(target, options);
