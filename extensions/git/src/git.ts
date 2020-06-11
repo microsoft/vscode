159bc687cac56b2c3246ed66b2a13a419ba6b89f
João Moreno <joao.moreno@microsoft.com>
2020-06-11 17:29:12 +0200

    fix regex

--- extensions/git/src/git.ts
+++ extensions/git/src/git.ts
@@ -422,7 +422,7 @@ export class Git {
 		const result = await this.exec(repositoryPath, ['rev-parse', '--show-toplevel']);
 
 		// Keep trailing spaces which are part of the directory name
-		const repoPath = path.normalize(result.stdout.trimLeft().replace(/(\r\n|\r|\n)+$/, ''));
+		const repoPath = path.normalize(result.stdout.trimLeft().replace(/[\r\n]+$/, ''));
 
 		if (isWindows) {
 			// On Git 2.25+ if you call `rev-parse --show-toplevel` on a mapped drive, instead of getting the mapped drive path back, you get the UNC path for the mapped drive.
