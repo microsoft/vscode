ccd7f4bec7b7e19098237af5cf6a3a7631de73be
isidor <inikolic@microsoft.com>
2020-06-17 12:11:09 +0200

    fixes #100364

--- src/vs/workbench/contrib/debug/browser/debugService.ts
+++ src/vs/workbench/contrib/debug/browser/debugService.ts
@@ -375,8 +375,6 @@ export class DebugService implements IDebugService {
 		const unresolvedConfig = deepClone(config);
 		if (options && options.noDebug) {
 			config!.noDebug = true;
-		} else {
-			delete config!.noDebug;
 		}
 
 		if (!type) {
