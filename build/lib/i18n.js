24e0a82229d2d208ae2575a99bad9489aa6b74f5
Fedor Nezhivoi <gyzerok@users.noreply.github.com>
2020-06-20 18:47:29 +1000

    Move encoding to common for #79275 (#100539)
    * move encoding.ts to common for #79275
    
    * load iconv-lite-umd and jschardet in web version for #79275
    
    * move EncodingOracle to the AbstractTextFileService for #79275
    
    * review
    
    * update to new iconv-lite-umd
    
    * add workaround for jschardet types
    
    * fix indentation
    
    * add iconv-lite-umd and jschardet to workbench.html
    
    * fix paths for modules
    
    Co-authored-by: Benjamin Pasero <benjpas@microsoft.com>

--- build/lib/i18n.js
+++ build/lib/i18n.js
@@ -1175,9 +1175,10 @@ function createIslFile(originalFilePath, messages, language, innoSetup) {
     });
     const basename = path.basename(originalFilePath);
     const filePath = `${basename}.${language.id}.isl`;
+    const encoded = iconv.encode(Buffer.from(content.join('\r\n'), 'utf8').toString(), innoSetup.codePage);
     return new File({
         path: filePath,
-        contents: iconv.encode(Buffer.from(content.join('\r\n'), 'utf8').toString(), innoSetup.codePage)
+        contents: Buffer.from(encoded),
     });
 }
 function encodeEntities(value) {
