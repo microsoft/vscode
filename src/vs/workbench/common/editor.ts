aa06a3d3b9d8914d5455bb38c405ff030d2119fe
Benjamin Pasero <benjpas@microsoft.com>
2020-06-20 09:55:02 +0200

    editors - add label property to file inputs

--- src/vs/workbench/common/editor.ts
+++ src/vs/workbench/common/editor.ts
@@ -649,9 +649,15 @@ export interface IModeSupport {
 export interface IFileEditorInput extends IEditorInput, IEncodingSupport, IModeSupport {
 
 	/**
-	 * Gets the resource this editor is about.
+	 * Gets the resource this file input is about.
 	 */
 	readonly resource: URI;
+
+	/**
+	 * Gets the label of the editor. In most cases this will
+	 * be identical to the resource.
+	 */
+	readonly label: URI;
 
 	/**
 	 * Sets the preferred label to use for this file input.
