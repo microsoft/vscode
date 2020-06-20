33676982c8eb722545b5108a8f24bd037d01089f
Rob Lourens <roblourens@gmail.com>
2020-06-20 18:52:11 -0500

    Look up mode aliases for cell language

--- src/vs/workbench/contrib/notebook/browser/view/renderers/cellRenderer.ts
+++ src/vs/workbench/contrib/notebook/browser/view/renderers/cellRenderer.ts
@@ -715,7 +715,8 @@ export class CellLanguageStatusBarItem extends Disposable {
 	}
 
 	private render(): void {
-		this.labelElement.textContent = this.modeService.getLanguageName(this.cell!.language!) || this.modeService.getLanguageName('plaintext');
+		const modeId = this.modeService.getModeIdForLanguageName(this.cell!.language) || this.cell!.language;
+		this.labelElement.textContent = this.modeService.getLanguageName(modeId) || this.modeService.getLanguageName('plaintext');
 	}
 }
 
