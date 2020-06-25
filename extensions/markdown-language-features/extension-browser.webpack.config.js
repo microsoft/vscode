16be2c98854b622c87a0842847035ebabf00c505
Martin Aeschlimann <martinae@microsoft.com>
2020-06-25 23:21:56 +0200

    used shared webpack-config for browser

--- extensions/markdown-language-features/extension-browser.webpack.config.js
+++ extensions/markdown-language-features/extension-browser.webpack.config.js
@@ -7,26 +7,11 @@
 
 'use strict';
 
-const withDefaults = require('../shared.webpack.config');
-const path = require('path');
+const withBrowserDefaults = require('../shared.webpack.config').browser;
 
-const clientConfig = withDefaults({
+module.exports = withBrowserDefaults({
 	context: __dirname,
-	target: 'webworker',
 	entry: {
 		extension: './src/extension.ts'
-	},
-	resolve: {
-		alias: {
-			'vscode-extension-telemetry': path.resolve(__dirname, '../../build/polyfills/vscode-extension-telemetry.js'),
-			'vscode-nls': path.resolve(__dirname, '../../build/polyfills/vscode-nls.js'),
-		},
-	},
-	performance: {
-		hints: false
-	},
+	}
 });
-
-clientConfig.module.rules[0].use.shift(); // remove nls loader
-
-module.exports =  clientConfig;
