b3c2e36cd477791b03c1e5a2233233cec1ab8d1f
Daniel Davis <danieldavis@daniels-mbp.home>
2020-06-26 16:55:25 +0100

    Changing commit message auto-fill to use the first non-undefined message

--- extensions/git/src/repository.ts
+++ extensions/git/src/repository.ts
@@ -864,8 +864,9 @@ export class Repository implements Disposable {
 		return toGitUri(uri, '', { replaceFileExtension: true });
 	}
 
-	async getInputTemplate(): Promise<string> {
-		const commitMessage = await Promise.race([this.repository.getMergeMessage(), this.repository.getSquashMessage()]);
+	async getInputTemplate(): Promise<string | undefined> {
+		const commitMessage = (await Promise.all([this.repository.getMergeMessage(), this.repository.getSquashMessage()])).find(msg => msg !== undefined);
+
 		if (commitMessage) {
 			return commitMessage;
 		}
