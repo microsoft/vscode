94081f67b6ad9f33c18da6f1901db82d2d9deb2e
Martin Aeschlimann <martinae@microsoft.com>
2020-06-26 10:13:08 +0200

    shared webpack browser config: add WEBWORKER definition

--- extensions/github-browser/extension-browser.webpack.config.js
+++ extensions/github-browser/extension-browser.webpack.config.js
@@ -8,20 +8,18 @@
 'use strict';
 const path = require('path');
 const withBrowserDefaults = require('../shared.webpack.config').browser;
-const webpack = require('webpack');
 
 const config = withBrowserDefaults({
 	context: __dirname,
 	node: false,
 	entry: {
-		extension: './src/extension.ts',
+		extension: './src/extension.ts'
 	},
 	resolve: {
 		alias: {
-			'node-fetch': path.resolve(__dirname, 'node_modules/node-fetch/browser.js'),
-		},
+			'node-fetch': path.resolve(__dirname, 'node_modules/node-fetch/browser.js')
+		}
 	}
 });
-config.plugins.push(new webpack.DefinePlugin({ WEBWORKER: JSON.stringify(true) }))
 
 module.exports = config;
