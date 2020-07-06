bc44023f137e1e1b15cddd16d493e03a0e3ab85b
Benjamin Pasero <benjpas@microsoft.com>
2020-07-06 07:21:51 +0200

    code :lipstick:

--- src/main.js
+++ src/main.js
@@ -199,6 +199,8 @@ function configureCommandlineSwitchesSync(cliArgs) {
 	];
 
 	if (process.platform === 'linux') {
+
+		// Force enable screen readers on Linux via this flag
 		SUPPORTED_ELECTRON_SWITCHES.push('force-renderer-accessibility');
 	}
 
@@ -216,6 +218,7 @@ function configureCommandlineSwitchesSync(cliArgs) {
 
 		// Append Electron flags to Electron
 		if (SUPPORTED_ELECTRON_SWITCHES.indexOf(argvKey) !== -1) {
+
 			// Color profile
 			if (argvKey === 'force-color-profile') {
 				if (argvValue) {
