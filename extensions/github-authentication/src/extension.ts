0a30c7a64d2c02a4e1c4a95c87ae06c95ec9a7bc
Rachel Macfarlane <ramacfar@microsoft.com>
2020-07-08 11:22:21 -0700

    Add GDPR comments for auth provider telemetry

--- extensions/github-authentication/src/extension.ts
+++ extensions/github-authentication/src/extension.ts
@@ -30,13 +30,21 @@ export async function activate(context: vscode.ExtensionContext) {
 		getSessions: () => Promise.resolve(loginService.sessions),
 		login: async (scopeList: string[]) => {
 			try {
+				/* __GDPR__
+					"login" : { }
+				*/
 				telemetryReporter.sendTelemetryEvent('login');
+
 				const session = await loginService.login(scopeList.sort().join(' '));
 				Logger.info('Login success!');
 				onDidChangeSessions.fire({ added: [session.id], removed: [], changed: [] });
 				return session;
 			} catch (e) {
+				/* __GDPR__
+					"loginFailed" : { }
+				*/
 				telemetryReporter.sendTelemetryEvent('loginFailed');
+
 				vscode.window.showErrorMessage(`Sign in failed: ${e}`);
 				Logger.error(e);
 				throw e;
@@ -44,11 +52,19 @@ export async function activate(context: vscode.ExtensionContext) {
 		},
 		logout: async (id: string) => {
 			try {
+				/* __GDPR__
+					"logout" : { }
+				*/
 				telemetryReporter.sendTelemetryEvent('logout');
+
 				await loginService.logout(id);
 				onDidChangeSessions.fire({ added: [], removed: [id], changed: [] });
 			} catch (e) {
+				/* __GDPR__
+					"logoutFailed" : { }
+				*/
 				telemetryReporter.sendTelemetryEvent('logoutFailed');
+
 				vscode.window.showErrorMessage(`Sign out failed: ${e}`);
 				Logger.error(e);
 				throw e;
