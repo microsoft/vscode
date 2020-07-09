8f19ff42af714288fe07f421f76960a72d96395c
Johannes Rieken <johannes.rieken@gmail.com>
2020-07-09 15:29:45 +0200

    extract extHost.common.services and have node- and worker-services for specifics

--- src/vs/workbench/api/common/extHost.common.services.ts
+++ src/vs/workbench/api/common/extHost.common.services.ts
@@ -15,33 +15,25 @@ import { IExtHostTask, WorkerExtHostTask } from 'vs/workbench/api/common/extHost
 import { IExtHostDebugService, WorkerExtHostDebugService } from 'vs/workbench/api/common/extHostDebugService';
 import { IExtHostSearch, ExtHostSearch } from 'vs/workbench/api/common/extHostSearch';
 import { IExtensionStoragePaths, ExtensionStoragePaths } from 'vs/workbench/api/common/extHostStoragePaths';
-import { IExtHostExtensionService } from 'vs/workbench/api/common/extHostExtensionService';
 import { IExtHostStorage, ExtHostStorage } from 'vs/workbench/api/common/extHostStorage';
-import { ExtHostExtensionService } from 'vs/workbench/api/worker/extHostExtensionService';
-import { ILogService } from 'vs/platform/log/common/log';
-import { ExtHostLogService } from 'vs/workbench/api/worker/extHostLogService';
 import { IExtHostTunnelService, ExtHostTunnelService } from 'vs/workbench/api/common/extHostTunnelService';
 import { IExtHostApiDeprecationService, ExtHostApiDeprecationService, } from 'vs/workbench/api/common/extHostApiDeprecationService';
 import { IExtHostWindow, ExtHostWindow } from 'vs/workbench/api/common/extHostWindow';
 import { ExtHostConsumerFileSystem, IExtHostConsumerFileSystem } from 'vs/workbench/api/common/extHostFileSystemConsumer';
 
-// register singleton services
-registerSingleton(ILogService, ExtHostLogService);
-registerSingleton(IExtHostApiDeprecationService, ExtHostApiDeprecationService);
-registerSingleton(IExtHostOutputService, ExtHostOutputService);
-registerSingleton(IExtHostWorkspace, ExtHostWorkspace);
-registerSingleton(IExtHostWindow, ExtHostWindow);
-registerSingleton(IExtHostDecorations, ExtHostDecorations);
-registerSingleton(IExtHostConfiguration, ExtHostConfiguration);
-registerSingleton(IExtHostCommands, ExtHostCommands);
-registerSingleton(IExtHostDocumentsAndEditors, ExtHostDocumentsAndEditors);
-registerSingleton(IExtHostStorage, ExtHostStorage);
-registerSingleton(IExtHostExtensionService, ExtHostExtensionService);
-registerSingleton(IExtHostSearch, ExtHostSearch);
-registerSingleton(IExtHostTunnelService, ExtHostTunnelService);
-registerSingleton(IExtHostConsumerFileSystem, ExtHostConsumerFileSystem);
 registerSingleton(IExtensionStoragePaths, ExtensionStoragePaths);
-
-registerSingleton(IExtHostTerminalService, WorkerExtHostTerminalService);
-registerSingleton(IExtHostTask, WorkerExtHostTask);
+registerSingleton(IExtHostApiDeprecationService, ExtHostApiDeprecationService);
+registerSingleton(IExtHostCommands, ExtHostCommands);
+registerSingleton(IExtHostConfiguration, ExtHostConfiguration);
+registerSingleton(IExtHostConsumerFileSystem, ExtHostConsumerFileSystem);
 registerSingleton(IExtHostDebugService, WorkerExtHostDebugService);
+registerSingleton(IExtHostDecorations, ExtHostDecorations);
+registerSingleton(IExtHostDocumentsAndEditors, ExtHostDocumentsAndEditors);
+registerSingleton(IExtHostOutputService, ExtHostOutputService);
+registerSingleton(IExtHostSearch, ExtHostSearch);
+registerSingleton(IExtHostStorage, ExtHostStorage);
+registerSingleton(IExtHostTask, WorkerExtHostTask);
+registerSingleton(IExtHostTerminalService, WorkerExtHostTerminalService);
+registerSingleton(IExtHostTunnelService, ExtHostTunnelService);
+registerSingleton(IExtHostWindow, ExtHostWindow);
+registerSingleton(IExtHostWorkspace, ExtHostWorkspace);
