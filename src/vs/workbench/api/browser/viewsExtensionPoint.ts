9f04bc2b53add64c5a34231d53bba68b89d301db
Alex Ross <alros@microsoft.com>
2020-07-09 16:40:51 +0200

    Add support for view initial state (collapsed or hidden) Fixes #101283

--- src/vs/workbench/api/browser/viewsExtensionPoint.ts
+++ src/vs/workbench/api/browser/viewsExtensionPoint.ts
@@ -83,10 +83,17 @@ interface IUserFriendlyViewDescriptor {
 
 	icon?: string;
 	contextualTitle?: string;
+	initialState?: string;
 
 	// From 'remoteViewDescriptor' type
 	group?: string;
 	remoteName?: string | string[];
+}
+
+enum InitialState {
+	Visible = 'visible',
+	Collapsed = 'collapsed',
+	Hidden = 'hidden'
 }
 
 const viewDescriptor: IJSONSchema = {
@@ -112,6 +119,20 @@ const viewDescriptor: IJSONSchema = {
 			description: localize('vscode.extension.contributes.view.contextualTitle', "Human-readable context for when the view is moved out of its original location. By default, the view's container name will be used. Will be shown"),
 			type: 'string'
 		},
+		initialState: {
+			description: localize('vscode.extension.contributes.view.initialState', "Initial state of the view when the extension is first installed. Once the user has changed the view state by collapsing, moving, or hiding the view, the initial state will not be used again."),
+			type: 'string',
+			enum: [
+				'visible',
+				'collapsed',
+				'hidden'
+			],
+			enumDescriptions: [
+				localize('vscode.extension.contributes.view.initialState.visible', "The default initial state for view. The view will be expanded. This may have different behavior when the view container that the view is in is built in."),
+				localize('vscode.extension.contributes.view.initialState.collapsed', "The view will show in the view container, but will be collapsed."),
+				localize('vscode.extension.contributes.view.initialState.hidden', "The view will not be shown in the view container, but will be discoverable through the views menu and other view entry points and can be un-hidden by the user.")
+			]
+		}
 	}
 };
 
@@ -419,6 +440,7 @@ class ViewsExtensionHandler implements IWorkbenchContribution {
 							: undefined;
 
 					const icon = item.icon ? resources.joinPath(extension.description.extensionLocation, item.icon) : undefined;
+					const initialState = this.convertInitialState(item.initialState);
 					const viewDescriptor = <ICustomViewDescriptor>{
 						id: item.id,
 						name: item.name,
@@ -429,12 +451,13 @@ class ViewsExtensionHandler implements IWorkbenchContribution {
 						canToggleVisibility: true,
 						canMoveView: true,
 						treeView: this.instantiationService.createInstance(CustomTreeView, item.id, item.name),
-						collapsed: this.showCollapsed(container),
+						collapsed: this.showCollapsed(container) || initialState === InitialState.Collapsed,
 						order: order,
 						extensionId: extension.description.identifier,
 						originalContainerId: entry.key,
 						group: item.group,
-						remoteAuthority: item.remoteName || (<any>item).remoteAuthority // TODO@roblou - delete after remote extensions are updated
+						remoteAuthority: item.remoteName || (<any>item).remoteAuthority, // TODO@roblou - delete after remote extensions are updated
+						hideByDefault: initialState === InitialState.Hidden
 					};
 
 					viewIds.add(viewDescriptor.id);
@@ -461,6 +484,13 @@ class ViewsExtensionHandler implements IWorkbenchContribution {
 				this.viewsRegistry.deregisterViews(removedViews, viewContainer);
 			}
 		}
+	}
+
+	private convertInitialState(value: any): InitialState | undefined {
+		if (Object.values(InitialState).includes(value)) {
+			return value;
+		}
+		return undefined;
 	}
 
 	private isValidViewDescriptors(viewDescriptors: IUserFriendlyViewDescriptor[], collector: ExtensionMessageCollector): boolean {
@@ -490,6 +520,10 @@ class ViewsExtensionHandler implements IWorkbenchContribution {
 				collector.error(localize('optstring', "property `{0}` can be omitted or must be of type `string`", 'contextualTitle'));
 				return false;
 			}
+			if (descriptor.initialState && !this.convertInitialState(descriptor.initialState)) {
+				collector.error(localize('optenum', "property `{0}` can be omitted or must be one of {1}", 'initialState', Object.values(InitialState).join(', ')));
+				return false;
+			}
 		}
 
 		return true;
@@ -506,12 +540,12 @@ class ViewsExtensionHandler implements IWorkbenchContribution {
 	}
 
 	private showCollapsed(container: ViewContainer): boolean {
-		switch (container.id) {
-			case EXPLORER:
-			case SCM:
-			case DEBUG:
-				return true;
-		}
+		// switch (container.id) {
+		// 	case EXPLORER:
+		// 	case SCM:
+		// 	case DEBUG:
+		// 		return true;
+		// }
 		return false;
 	}
 }
