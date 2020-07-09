e69f8607e5d18f0702168ff42ed586cfefc95f95
deepak1556 <hop2deep@gmail.com>
2020-07-09 10:57:34 -0700

    enable LayoutNG

--- src/main.js
+++ src/main.js
@@ -266,9 +266,6 @@ function configureCommandlineSwitchesSync(cliArgs) {
 		app.commandLine.appendSwitch('js-flags', jsFlags);
 	}
 
-	// TODO@Deepak Electron 7 workaround for https://github.com/microsoft/vscode/issues/88873
-	app.commandLine.appendSwitch('disable-features', 'LayoutNG');
-
 	return argvConfig;
 }
 
