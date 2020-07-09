5eb91c7c409826bd689eb4900d5267618e9a4454
rebornix <penn.lv@gmail.com>
2020-07-08 17:30:08 -0700

    recommend to use vscode.workspace.fs for notebook content provider.

--- src/vs/vscode.proposed.d.ts
+++ src/vs/vscode.proposed.d.ts
@@ -1772,6 +1772,10 @@ declare module 'vscode' {
 	}
 
 	export interface NotebookContentProvider {
+		/**
+		 * Content providers should always use [file system providers](#FileSystemProvider) to
+		 * resolve the raw content for `uri` as the resouce is not necessarily a file on disk.
+		 */
 		openNotebook(uri: Uri, openContext: NotebookDocumentOpenContext): NotebookData | Promise<NotebookData>;
 		resolveNotebook(document: NotebookDocument, webview: NotebookCommunication): Promise<void>;
 		saveNotebook(document: NotebookDocument, cancellation: CancellationToken): Promise<void>;
