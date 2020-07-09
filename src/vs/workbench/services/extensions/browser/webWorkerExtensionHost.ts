089e7a5956bd820b2e066c658bcfbcc8829ec61d
Johannes Rieken <johannes.rieken@gmail.com>
2020-07-09 11:31:59 +0200

    send globalStorageHome to webWorkerHost, https://github.com/microsoft/vscode/issues/101857

--- src/vs/workbench/services/extensions/browser/webWorkerExtensionHost.ts
+++ src/vs/workbench/services/extensions/browser/webWorkerExtensionHost.ts
@@ -152,7 +152,7 @@ export class WebWorkerExtensionHost implements IExtensionHost {
 				appLanguage: platform.language,
 				extensionDevelopmentLocationURI: this._environmentService.extensionDevelopmentLocationURI,
 				extensionTestsLocationURI: this._environmentService.extensionTestsLocationURI,
-				globalStorageHome: URI.parse('fake:globalStorageHome'), //todo@jrieken URI.file(this._environmentService.globalStorageHome),
+				globalStorageHome: this._environmentService.globalStorageHome,
 				webviewResourceRoot: this._environmentService.webviewResourceRoot,
 				webviewCspSource: this._environmentService.webviewCspSource,
 			},
