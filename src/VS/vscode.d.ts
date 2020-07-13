59fffcaf7b2010a6f73dc615149fd4815998d479
burapha28 <67376072+burapha28@users.noreply.github.com>
2020-07-14 02:22:52 +0700

    Update and rename src/vs/vscode.d.ts to src/VS/vscode.d.ts

--- src/VS/vscode.d.ts
+++ src/VS/vscode.d.ts
@@ -11538,3 +11538,4 @@ interface Thenable<T> {
 	then<TResult>(onfulfilled?: (value: T) => TResult | Thenable<TResult>, onrejected?: (reason: any) => TResult | Thenable<TResult>): Thenable<TResult>;
 	then<TResult>(onfulfilled?: (value: T) => TResult | Thenable<TResult>, onrejected?: (reason: any) => void): Thenable<TResult>;
 }
+
