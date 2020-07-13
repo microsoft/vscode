ec074bc35217faa17d2bf2265008da9fd792785f
Matt Bierner <matb@microsoft.com>
2020-07-13 11:54:33 -0700

    Rework copy paste and other browser events for webviews (#101958)
    Fixes #101946
    
    Webview can currently trigger some keyboard events twice. Sequence of events:
    
    - User presses ctrl+v with a webview focused
    - Webview is listening to keyboard events for rebroadcast (so that we can handle normal VS code commands even when focused on webviews)
    - We rebroadcast the keypresses back to VS Code
    - The webview then triggers the standard copy behavior on its own (I believe this is either chromium or electron)
    - VS Code gets the ctrl+v keypress event and resolves it to the 'paste' command
    - The paste command triggers the paste method on the webview
    - This calls back into the webview content to trigger a second paste
    
    This does not happen in cases where we are using native menus, which can call `setIgnoreMenuShortcuts` to disable the browser geenrated paste event.
    
    ## The fix
    To fix this, I think we want to completely block the browser generate events in all cases and instead always dispatch the events through VS Code. This should ensure more consistent behavior.
    
    This PR does this by:
    
    - In the webview, add a keypress listener for copy/paste/cut and undo/redo. When we see these events, call `preventDefault` to block them but still dispatch back to VS Code
    
    - In VS Code, more the  logic for triggering undo/redo, etc. on webviews out of the electron layer and into the browser layer. iframe based webviews have the exact same problem as electron based webviews, so we need to fix this issue for both of them.

--- src/vs/workbench/contrib/webview/electron-browser/webview.contribution.ts
+++ src/vs/workbench/contrib/webview/electron-browser/webview.contribution.ts
@@ -3,65 +3,12 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { MultiCommand, RedoCommand, SelectAllCommand, UndoCommand } from 'vs/editor/browser/editorExtensions';
-import { CopyAction, CutAction, PasteAction } from 'vs/editor/contrib/clipboard/clipboard';
 import { registerAction2 } from 'vs/platform/actions/common/actions';
 import { registerSingleton } from 'vs/platform/instantiation/common/extensions';
-import { ServicesAccessor } from 'vs/platform/instantiation/common/instantiation';
-import { IWebviewService, WebviewOverlay } from 'vs/workbench/contrib/webview/browser/webview';
-import { getActiveWebview } from 'vs/workbench/contrib/webview/browser/webviewCommands';
+import { IWebviewService } from 'vs/workbench/contrib/webview/browser/webview';
 import * as webviewCommands from 'vs/workbench/contrib/webview/electron-browser/webviewCommands';
-import { ElectronWebviewBasedWebview } from 'vs/workbench/contrib/webview/electron-browser/webviewElement';
 import { ElectronWebviewService } from 'vs/workbench/contrib/webview/electron-browser/webviewService';
-import { isMacintosh } from 'vs/base/common/platform';
-import { IConfigurationService } from 'vs/platform/configuration/common/configuration';
 
 registerSingleton(IWebviewService, ElectronWebviewService, true);
 
 registerAction2(webviewCommands.OpenWebviewDeveloperToolsAction);
-
-function getActiveElectronBasedWebview(accessor: ServicesAccessor): ElectronWebviewBasedWebview | undefined {
-	const webview = getActiveWebview(accessor);
-	if (!webview) {
-		return undefined;
-	}
-
-	// Make sure we are really focused on the webview
-	if (!['WEBVIEW', 'IFRAME'].includes(document.activeElement?.tagName ?? '')) {
-		return undefined;
-	}
-
-	if (webview instanceof ElectronWebviewBasedWebview) {
-		return webview;
-	} else if ('getInnerWebview' in (webview as WebviewOverlay)) {
-		const innerWebview = (webview as WebviewOverlay).getInnerWebview();
-		if (innerWebview instanceof ElectronWebviewBasedWebview) {
-			return innerWebview;
-		}
-	}
-
-	return undefined;
-}
-
-const PRIORITY = 100;
-
-function overrideCommandForWebview(command: MultiCommand | undefined, f: (webview: ElectronWebviewBasedWebview) => void) {
-	command?.addImplementation(PRIORITY, accessor => {
-		if (isMacintosh || accessor.get(IConfigurationService).getValue<string>('window.titleBarStyle') === 'native') {
-			const webview = getActiveElectronBasedWebview(accessor);
-			if (webview) {
-				f(webview);
-				return true;
-			}
-		}
-
-		return false;
-	});
-}
-
-overrideCommandForWebview(UndoCommand, webview => webview.undo());
-overrideCommandForWebview(RedoCommand, webview => webview.redo());
-overrideCommandForWebview(SelectAllCommand, webview => webview.selectAll());
-overrideCommandForWebview(CopyAction, webview => webview.copy());
-overrideCommandForWebview(PasteAction, webview => webview.paste());
-overrideCommandForWebview(CutAction, webview => webview.cut());
