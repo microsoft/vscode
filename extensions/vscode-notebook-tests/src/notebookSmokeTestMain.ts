5e9633712174b7113ab72d8974b78bdcfb85fb16
rebornix <penn.lv@gmail.com>
2020-07-13 10:52:52 -0700

    re #93070. test for command arguments.

--- extensions/vscode-notebook-tests/src/notebookSmokeTestMain.ts
+++ extensions/vscode-notebook-tests/src/notebookSmokeTestMain.ts
@@ -93,4 +93,15 @@ export function smokeTestActivate(context: vscode.ExtensionContext): any {
 			return;
 		},
 	}));
+
+	context.subscriptions.push(vscode.commands.registerCommand('vscode-notebook-tests.debugAction', async (cell: vscode.NotebookCell) => {
+		if (cell) {
+			const edit = new vscode.WorkspaceEdit();
+			const fullRange = new vscode.Range(0, 0, cell.document.lineCount - 1, cell.document.lineAt(cell.document.lineCount - 1).range.end.character);
+			edit.replace(cell.document.uri, fullRange, 'test');
+			await vscode.workspace.applyEdit(edit);
+		} else {
+			throw new Error('Cell not set correctly');
+		}
+	}));
 }
