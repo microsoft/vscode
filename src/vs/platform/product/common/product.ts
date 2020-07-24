2de499749410cf099f4f9a224b82e75e507f1659
Alex Dima <alexdima@microsoft.com>
2020-07-24 15:53:25 +0200

    Allow ms-vscode.references-view to use proposed API when running from sources

--- src/vs/platform/product/common/product.ts
+++ src/vs/platform/product/common/product.ts
@@ -23,7 +23,10 @@ if (isWeb) {
 			version: '1.48.0-dev',
 			nameLong: 'Visual Studio Code Web Dev',
 			nameShort: 'VSCode Web Dev',
-			urlProtocol: 'code-oss'
+			urlProtocol: 'code-oss',
+			extensionAllowedProposedApi: [
+				'ms-vscode.references-view'
+			],
 		});
 	}
 }
