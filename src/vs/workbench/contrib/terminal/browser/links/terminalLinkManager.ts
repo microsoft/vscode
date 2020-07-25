20e87c057bf09f9a352f190e4a62c6520c079c47
Dasharathi, Abhijith <abhijith.dasharathi@sap.com>
2020-07-26 02:39:29 +0530

    fix for the issue "ctrl-click in terminal doesn't handle trailing ':' in file:line:col: links #101155"

--- src/vs/workbench/contrib/terminal/browser/links/terminalLinkManager.ts
+++ src/vs/workbench/contrib/terminal/browser/links/terminalLinkManager.ts
@@ -173,6 +173,13 @@ export class TerminalLinkManager extends DisposableStore {
 				return;
 			}
 
+			//Checking if the link matches line and column clause and update it accordingly if it matches (fix for issue #101155)
+			const lineAndColumnRegex = new RegExp(`${lineAndColumnClause}`);
+			const matches: string[] | null = lineAndColumnRegex.exec(link);
+			if (matches) {
+				link = matches[0];
+			}
+
 			// Allow the link to be intercepted if there are listeners
 			if (this._hasBeforeHandleLinkListeners) {
 				const wasHandled = await this._triggerBeforeHandleLinkListeners(link);
