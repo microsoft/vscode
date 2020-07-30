227d533937b17f54bae09f94083bf6304e6262e1
Robo <hop2deep@gmail.com>
2020-07-30 08:35:43 -0700

    Revert "chore: update to electron 9-x-y (#102011)" (#103599)
    This reverts commit 3d0d50caf8d92662fa474251512001d5d96173fb.

--- src/vs/workbench/contrib/relauncher/browser/relauncher.contribution.ts
+++ src/vs/workbench/contrib/relauncher/browser/relauncher.contribution.ts
@@ -23,6 +23,7 @@ import { IProductService } from 'vs/platform/product/common/productService';
 
 interface IConfiguration extends IWindowsConfiguration {
 	update: { mode: string; };
+	telemetry: { enableCrashReporter: boolean };
 	debug: { console: { wordWrap: boolean } };
 	editor: { accessibilitySupport: 'on' | 'off' | 'auto' };
 }
@@ -34,6 +35,7 @@ export class SettingsChangeRelauncher extends Disposable implements IWorkbenchCo
 	private nativeFullScreen: boolean | undefined;
 	private clickThroughInactive: boolean | undefined;
 	private updateMode: string | undefined;
+	private enableCrashReporter: boolean | undefined;
 	private debugConsoleWordWrap: boolean | undefined;
 	private accessibilitySupport: 'on' | 'off' | 'auto' | undefined;
 
@@ -87,6 +89,12 @@ export class SettingsChangeRelauncher extends Disposable implements IWorkbenchCo
 			// Update channel
 			if (typeof config.update?.mode === 'string' && config.update.mode !== this.updateMode) {
 				this.updateMode = config.update.mode;
+				changed = true;
+			}
+
+			// Crash reporter
+			if (typeof config.telemetry?.enableCrashReporter === 'boolean' && config.telemetry.enableCrashReporter !== this.enableCrashReporter) {
+				this.enableCrashReporter = config.telemetry.enableCrashReporter;
 				changed = true;
 			}
 
