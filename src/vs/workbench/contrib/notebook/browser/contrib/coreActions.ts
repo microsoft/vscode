cf1b357443a4cc0e93b1977d2dfd873505e1ae88
rebornix <penn.lv@gmail.com>
2020-07-30 17:29:44 -0700

    fix #102518.

--- src/vs/workbench/contrib/notebook/browser/contrib/coreActions.ts
+++ src/vs/workbench/contrib/notebook/browser/contrib/coreActions.ts
@@ -510,7 +510,7 @@ abstract class InsertCellCommand extends NotebookAction {
 	}
 
 	async runWithContext(accessor: ServicesAccessor, context: INotebookActionContext): Promise<void> {
-		const newCell = context.notebookEditor.insertNotebookCell(context.cell, this.kind, this.direction, undefined, context.ui);
+		const newCell = context.notebookEditor.insertNotebookCell(context.cell, this.kind, this.direction, undefined, true);
 		if (newCell) {
 			context.notebookEditor.focusNotebookCell(newCell, 'editor');
 		}
