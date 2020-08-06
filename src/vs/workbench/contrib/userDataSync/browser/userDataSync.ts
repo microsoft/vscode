eebfafcc96523075cdca0e033a28ef3b5346af5e
Sandeep Somavarapu <sasomava@microsoft.com>
2020-08-06 10:10:27 +0200

    show synced data command when account is available and sync enablements is not required.

--- src/vs/workbench/contrib/userDataSync/browser/userDataSync.ts
+++ src/vs/workbench/contrib/userDataSync/browser/userDataSync.ts
@@ -997,7 +997,7 @@ export class UserDataSyncWorkbenchContribution extends Disposable implements IWo
 
 	private registerEnableSyncViewsAction(): void {
 		const that = this;
-		const when = ContextKeyExpr.and(CONTEXT_SYNC_ENABLEMENT, CONTEXT_ACCOUNT_STATE.isEqualTo(AccountStatus.Available), CONTEXT_SYNC_STATE.notEqualsTo(SyncStatus.Uninitialized));
+		const when = ContextKeyExpr.and(CONTEXT_ACCOUNT_STATE.isEqualTo(AccountStatus.Available), CONTEXT_SYNC_STATE.notEqualsTo(SyncStatus.Uninitialized));
 		this._register(registerAction2(class SyncStatusAction extends Action2 {
 			constructor() {
 				super({
