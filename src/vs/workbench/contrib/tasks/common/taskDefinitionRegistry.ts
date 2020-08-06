1998f254ad3f835a0372664c34b9e0863aa8d8a9
Alex Ross <alros@microsoft.com>
2020-08-06 13:01:28 +0200

    Add default for task definition when Fixes #104122

--- src/vs/workbench/contrib/tasks/common/taskDefinitionRegistry.ts
+++ src/vs/workbench/contrib/tasks/common/taskDefinitionRegistry.ts
@@ -39,7 +39,8 @@ const taskDefinitionSchema: IJSONSchema = {
 		},
 		when: {
 			type: 'string',
-			markdownDescription: nls.localize('TaskDefinition.when', 'Condition which must be true to enable this type of task. Consider using `shellExecutionSupported`, `processExecutionSupported`, and `customExecutionSupported` as appropriate for this task definition.')
+			markdownDescription: nls.localize('TaskDefinition.when', 'Condition which must be true to enable this type of task. Consider using `shellExecutionSupported`, `processExecutionSupported`, and `customExecutionSupported` as appropriate for this task definition.'),
+			default: ''
 		}
 	}
 };
