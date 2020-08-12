a275a1c69cbb7c60ae93e074a159fdb9286926d2
xtqqczze <xtqqczze@gmail.com>
2020-08-12 14:43:31 +0100

    pass 2
    /*---------------------------------------------------------------------------------------------
    *  Copyright (c) Microsoft Corporation. All rights reserved.
    *  Licensed under the MIT License. See License.txt in the project root for license information.
    *--------------------------------------------------------------------------------------------*/
    
    ->
    
    /*---------------------------------------------------------------------------------------------
     *  Copyright (c) Microsoft Corporation.
     *  Licensed under the MIT License. See LICENSE.txt in the project root for license information.
     *--------------------------------------------------------------------------------------------*/

--- extensions/emmet/src/typings/EmmetNode.d.ts
+++ extensions/emmet/src/typings/EmmetNode.d.ts
@@ -1,7 +1,7 @@
 /*---------------------------------------------------------------------------------------------
-*  Copyright (c) Microsoft Corporation. All rights reserved.
-*  Licensed under the MIT License. See License.txt in the project root for license information.
-*--------------------------------------------------------------------------------------------*/
+ *  Copyright (c) Microsoft Corporation.
+ *  Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
 
 declare module 'EmmetNode' {
     import { Position } from 'vscode';
