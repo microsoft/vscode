00d5a1bbcc08e2800ce0797fac5b06d2bdfb937d
gjsjohnmurray <johnm@georgejames.com>
2020-08-17 13:24:51 +0100

    Variant preferred by @bpasero

--- src/vs/workbench/contrib/files/common/files.ts
+++ src/vs/workbench/contrib/files/common/files.ts
@@ -164,13 +164,12 @@ export class TextFileContentProvider extends Disposable implements ITextModelCon
 	}
 
 	private static resourceToTextFile(scheme: string, resource: URI): URI {
-		// Use query string to stash original scheme and original query
 		return resource.with({ scheme, query: JSON.stringify({ scheme: resource.scheme, query: resource.query }) });
 	}
 
 	private static textFileToResource(resource: URI): URI {
-		// Restore original values stashed in query string by resourceToTextFile
-		return resource.with(JSON.parse(resource.query));
+		const { scheme, query } = JSON.parse(resource.query);
+		return resource.with({ scheme, query });
 	}
 
 	async provideTextContent(resource: URI): Promise<ITextModel> {
