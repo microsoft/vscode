28fc6c10d2ff24dccea61d9fbaa18889135ee89a
Kushagra Bansal <kushagrabansalajmer@gmail.com>
2020-08-19 18:09:22 +0530

    Update main.ts

--- src/vs/code/electron-main/main.ts
+++ src/vs/code/electron-main/main.ts
@@ -1,5 +1,5 @@
 /*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Copyright (c) Microsoft Corporation.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
