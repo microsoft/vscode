83c68bd228a2d80350fbe5d22db73867c5c92598
Matt Bierner <matb@microsoft.com>
2020-08-20 11:49:26 -0700

    Remove test view

--- extensions/image-preview/src/extension.ts
+++ extensions/image-preview/src/extension.ts
@@ -32,22 +32,4 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(vscode.commands.registerCommand('imagePreview.zoomOut', () => {
 		previewManager.activePreview?.zoomOut();
 	}));
-
-	vscode.window.registerWebviewViewProvider('cats.cat', new class implements vscode.WebviewViewProvider {
-		async resolveWebviewView(webviewView: vscode.WebviewView, _state: unknown) {
-			await new Promise(resolve => setTimeout(resolve, 2000));
-			webviewView.webview.html = `<!DOCTYPE html>
-			<html lang="en">
-			<head>
-				<meta charset="UTF-8">
-				<meta name="viewport" content="width=device-width, initial-scale=1.0">
-				<title>Document</title>
-			</head>
-			<body style="background-color: transparent">
-				<img src="https://media.giphy.com/media/E6jscXfv3AkWQ/giphy.gif">
-			</body>
-			</html>`;
-
-		}
-	});
 }
