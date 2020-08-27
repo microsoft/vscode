450c37e2f7909ee7478d4999230143c13728275e
Johannes Rieken <johannes.rieken@gmail.com>
2020-08-27 17:48:21 +0200

    also send onNotebook:XYZ event, https://github.com/microsoft/vscode/issues/105496

--- src/vs/workbench/contrib/notebook/browser/notebookServiceImpl.ts
+++ src/vs/workbench/contrib/notebook/browser/notebookServiceImpl.ts
@@ -538,6 +538,9 @@ export class NotebookService extends Disposable implements INotebookService, ICu
 			// notebook providers/kernels/renderers might use `*` as activation event.
 			await this._extensionService.activateByEvent(`*`);
 			// this awaits full activation of all matching extensions
+			await this._extensionService.activateByEvent(`onNotebook:${viewType}`);
+
+			// TODO@jrieken deprecated, remove this
 			await this._extensionService.activateByEvent(`onNotebookEditor:${viewType}`);
 		}
 		return this._notebookProviders.has(viewType);
