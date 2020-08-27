8877f5f60abbf7b290c89b609ad259a533b8510f
SteVen Batten <steven.m.batten@outlook.com>
2020-08-27 10:21:39 -0700

    fixes #105480

--- src/vs/workbench/browser/layout.ts
+++ src/vs/workbench/browser/layout.ts
@@ -1539,6 +1539,11 @@ export abstract class Layout extends Disposable implements IWorkbenchLayoutServi
 	setPanelHidden(hidden: boolean, skipLayout?: boolean): void {
 		this.state.panel.hidden = hidden;
 
+		// Return if not initialized fully #105480
+		if (!this.workbenchGrid) {
+			return;
+		}
+
 		// Adjust CSS
 		if (hidden) {
 			addClass(this.container, Classes.PANEL_HIDDEN);
