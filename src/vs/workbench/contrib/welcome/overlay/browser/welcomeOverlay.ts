bb9287bb4f11fbd5a94a84f08541411f640faa2d
Christof Marti <chrmarti@microsoft.com>
2020-08-28 13:49:08 +0200

    Revert "Use DOM API (fixes #105505)"
    This reverts commit e995f1095805e726c505ebb6477f2b6030a28cc7.

--- src/vs/workbench/contrib/welcome/overlay/browser/welcomeOverlay.ts
+++ src/vs/workbench/contrib/welcome/overlay/browser/welcomeOverlay.ts
@@ -38,43 +38,43 @@ interface Key {
 const keys: Key[] = [
 	{
 		id: 'explorer',
-		arrow: '←',
+		arrow: '&larr;',
 		label: localize('welcomeOverlay.explorer', "File explorer"),
 		command: 'workbench.view.explorer'
 	},
 	{
 		id: 'search',
-		arrow: '←',
+		arrow: '&larr;',
 		label: localize('welcomeOverlay.search', "Search across files"),
 		command: 'workbench.view.search'
 	},
 	{
 		id: 'git',
-		arrow: '←',
+		arrow: '&larr;',
 		label: localize('welcomeOverlay.git', "Source code management"),
 		command: 'workbench.view.scm'
 	},
 	{
 		id: 'debug',
-		arrow: '←',
+		arrow: '&larr;',
 		label: localize('welcomeOverlay.debug', "Launch and debug"),
 		command: 'workbench.view.debug'
 	},
 	{
 		id: 'extensions',
-		arrow: '←',
+		arrow: '&larr;',
 		label: localize('welcomeOverlay.extensions', "Manage extensions"),
 		command: 'workbench.view.extensions'
 	},
 	// {
 	// 	id: 'watermark',
-	// 	arrow: '⤹',
+	// 	arrow: '&larrpl;',
 	// 	label: localize('welcomeOverlay.watermark', "Command Hints"),
 	// 	withEditor: false
 	// },
 	{
 		id: 'problems',
-		arrow: '⤹',
+		arrow: '&larrpl;',
 		label: localize('welcomeOverlay.problems', "View errors and warnings"),
 		command: 'workbench.actions.view.problems'
 	},
@@ -85,20 +85,20 @@ const keys: Key[] = [
 	},
 	// {
 	// 	id: 'openfile',
-	// 	arrow: '⤸',
+	// 	arrow: '&cudarrl;',
 	// 	label: localize('welcomeOverlay.openfile', "File Properties"),
 	// 	arrowLast: true,
 	// 	withEditor: true
 	// },
 	{
 		id: 'commandPalette',
-		arrow: '↖',
+		arrow: '&nwarr;',
 		label: localize('welcomeOverlay.commandPalette', "Find and run all commands"),
 		command: ShowAllCommandsAction.ID
 	},
 	{
 		id: 'notifications',
-		arrow: '⤵',
+		arrow: '&cudarrr;',
 		arrowLast: true,
 		label: localize('welcomeOverlay.notifications', "Show notifications"),
 		command: 'notifications.showList'
@@ -186,7 +186,7 @@ class WelcomeOverlay extends Disposable {
 			.forEach(({ id, arrow, label, command, arrowLast }) => {
 				const div = dom.append(this._overlay, $(`.key.${id}`));
 				if (arrow && !arrowLast) {
-					dom.append(div, $('span.arrow', {}, arrow));
+					dom.append(div, $('span.arrow')).innerHTML = arrow;
 				}
 				dom.append(div, $('span.label')).textContent = label;
 				if (command) {
@@ -196,7 +196,7 @@ class WelcomeOverlay extends Disposable {
 					}
 				}
 				if (arrow && arrowLast) {
-					dom.append(div, $('span.arrow', {}, arrow));
+					dom.append(div, $('span.arrow')).innerHTML = arrow;
 				}
 			});
 	}
