59b5a2f060ac0d559e7b81097e92139ebdd5e810
Sandeep Somavarapu <sasomava@microsoft.com>
2020-08-28 12:55:13 +0200

    include extensions in the text

--- src/vs/workbench/contrib/extensions/browser/extensionRecommendationsService.ts
+++ src/vs/workbench/contrib/extensions/browser/extensionRecommendationsService.ts
@@ -330,7 +330,7 @@ export class ExtensionRecommendationsService extends Disposable implements IExte
 		const searchValue = '@recommended ';
 		this.notificationService.prompt(
 			Severity.Info,
-			localize('workspaceRecommended', "Do you want to install recommendations for this repository?"),
+			localize('workspaceRecommended', "Do you want to install recommended extensions for this repository?"),
 			[{
 				label: localize('install', "Install"),
 				run: async () => {
