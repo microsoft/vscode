495b05c1de804154cc66ad16b81ec594b9a17849
Matt Bierner <matb@microsoft.com>
2020-09-02 10:34:09 -0700

    Enable pointer lock in iframe based webviews
    #104169

--- src/vs/workbench/contrib/webview/browser/webviewElement.ts
+++ src/vs/workbench/contrib/webview/browser/webviewElement.ts
@@ -70,7 +70,7 @@ export class IFrameWebview extends BaseWebview<HTMLIFrameElement> implements Web
 		// Wait the end of the ctor when all listeners have been hooked up.
 		const element = document.createElement('iframe');
 		element.className = `webview ${options.customClasses || ''}`;
-		element.sandbox.add('allow-scripts', 'allow-same-origin', 'allow-forms');
+		element.sandbox.add('allow-scripts', 'allow-same-origin', 'allow-forms', 'allow-pointer-lock');
 		element.style.border = 'none';
 		element.style.width = '100%';
 		element.style.height = '100%';
