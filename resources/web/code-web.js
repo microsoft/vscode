e62999c15e4dc1075c168eccb019fa7ea2a26667
Benjamin Pasero <benjpas@microsoft.com>
2020-09-02 17:58:59 +0200

    :up: web playground

--- resources/web/code-web.js
+++ resources/web/code-web.js
@@ -28,7 +28,7 @@ const BUILTIN_MARKETPLACE_EXTENSIONS_ROOT = path.join(APP_ROOT, '.build', 'built
 const WEB_DEV_EXTENSIONS_ROOT = path.join(APP_ROOT, '.build', 'builtInWebDevExtensions');
 const WEB_MAIN = path.join(APP_ROOT, 'src', 'vs', 'code', 'browser', 'workbench', 'workbench-dev.html');
 
-const WEB_PLAYGROUND_VERSION = '0.0.6';
+const WEB_PLAYGROUND_VERSION = '0.0.8';
 
 const args = minimist(process.argv, {
 	boolean: [
