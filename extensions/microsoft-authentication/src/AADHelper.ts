352a39025160b234d37af128c48a9b19c4a44ff5
Oleg Demchenko <oldemche@microsoft.com>
2020-09-11 15:38:03 -0400

    Add preferred_username to the list of msft token claims (#106511)

--- extensions/microsoft-authentication/src/AADHelper.ts
+++ extensions/microsoft-authentication/src/AADHelper.ts
@@ -40,6 +40,7 @@ interface ITokenClaims {
 	tid: string;
 	email?: string;
 	unique_name?: string;
+	preferred_username?: string;
 	oid?: string;
 	altsecid?: string;
 	ipd?: string;
@@ -454,7 +455,7 @@ export class AzureActiveDirectoryService {
 			scope,
 			sessionId: existingId || `${claims.tid}/${(claims.oid || (claims.altsecid || '' + claims.ipd || ''))}/${uuid()}`,
 			account: {
-				label: claims.email || claims.unique_name || 'user@example.com',
+				label: claims.email || claims.unique_name || claims.preferred_username || 'user@example.com',
 				id: `${claims.tid}/${(claims.oid || (claims.altsecid || '' + claims.ipd || ''))}`
 			}
 		};
