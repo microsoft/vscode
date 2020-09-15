c186b3b432e39fc9cc3edf3f377284fb212f052e
Matt Bierner <matb@microsoft.com>
2020-09-14 17:38:06 -0700

    Use Object.assign over our custom assign impl
    For #103454

--- src/vs/base/common/objects.ts
+++ src/vs/base/common/objects.ts
@@ -113,18 +113,6 @@ export function mixin(destination: any, source: any, overwrite: boolean = true):
 	return destination;
 }
 
-/**
- * @deprecated ES6
- */
-export function assign<T>(destination: T): T;
-export function assign<T, U>(destination: T, u: U): T & U;
-export function assign<T, U, V>(destination: T, u: U, v: V): T & U & V;
-export function assign<T, U, V, W>(destination: T, u: U, v: V, w: W): T & U & V & W;
-export function assign(destination: any, ...sources: any[]): any {
-	sources.forEach(source => Object.keys(source).forEach(key => destination[key] = source[key]));
-	return destination;
-}
-
 export function equals(one: any, other: any): boolean {
 	if (one === other) {
 		return true;
