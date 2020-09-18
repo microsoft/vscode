92a30ed2650c31839a453bb088be9a4e7935a393
SteVen Batten <6561887+sbatten@users.noreply.github.com>
2020-09-18 10:00:39 -0700

    Move tas-client to umd version and adopt in web (#106904)
    * adopt tas-client-umd
    
    * adopt in web
    
    * fix whitespace
    
    * adding tas-client to webignore
    
    * upgrade tas-client-umd
    
    * move svc imort to common
    
    * remove unnecessary ignore

--- src/vs/workbench/workbench.common.main.ts
+++ src/vs/workbench/workbench.common.main.ts
@@ -86,6 +86,7 @@ import 'vs/workbench/services/quickinput/browser/quickInputService';
 import 'vs/workbench/services/userDataSync/browser/userDataSyncWorkbenchService';
 import 'vs/workbench/services/authentication/browser/authenticationService';
 import 'vs/workbench/services/hover/browser/hoverService';
+import 'vs/workbench/services/experiment/common/experimentService';
 
 import { registerSingleton } from 'vs/platform/instantiation/common/extensions';
 import { ExtensionGalleryService } from 'vs/platform/extensionManagement/common/extensionGalleryService';
