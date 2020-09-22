99c8d615c15fa6b92514806f66b7aa24f36914c2
Martin Aeschlimann <martinae@microsoft.com>
2020-09-22 12:04:53 +0200

    groups for viewsWelcome

--- src/vs/workbench/contrib/welcome/common/viewsWelcomeContribution.ts
+++ src/vs/workbench/contrib/welcome/common/viewsWelcomeContribution.ts
@@ -9,7 +9,7 @@ import { IWorkbenchContribution } from 'vs/workbench/common/contributions';
 import { IExtensionPoint } from 'vs/workbench/services/extensions/common/extensionsRegistry';
 import { ViewsWelcomeExtensionPoint, ViewWelcome, ViewIdentifierMap } from './viewsWelcomeExtensionPoint';
 import { Registry } from 'vs/platform/registry/common/platform';
-import { Extensions as ViewContainerExtensions, IViewsRegistry, ViewContentPriority } from 'vs/workbench/common/views';
+import { Extensions as ViewContainerExtensions, IViewsRegistry } from 'vs/workbench/common/views';
 
 const viewsRegistry = Registry.as<IViewsRegistry>(ViewContainerExtensions.ViewsRegistry);
 
@@ -34,10 +34,12 @@ export class ViewsWelcomeContribution extends Disposable implements IWorkbenchCo
 			for (const contribution of added) {
 				for (const welcome of contribution.value) {
 					const id = ViewIdentifierMap[welcome.view] ?? welcome.view;
+					const { group, order } = parseGroupAndOrder(welcome);
 					const disposable = viewsRegistry.registerViewWelcomeContent(id, {
 						content: welcome.contents,
 						when: ContextKeyExpr.deserialize(welcome.when),
-						priority: contribution.description.isBuiltin ? ViewContentPriority.Low : ViewContentPriority.Lowest
+						group,
+						order
 					});
 
 					this.viewWelcomeContents.set(welcome, disposable);
@@ -45,4 +47,19 @@ export class ViewsWelcomeContribution extends Disposable implements IWorkbenchCo
 			}
 		});
 	}
+}
+
+function parseGroupAndOrder(welcome: ViewWelcome): { group: string | undefined, order: number | undefined } {
+	let group: string | undefined;
+	let order: number | undefined;
+	if (welcome.group) {
+		const idx = welcome.group.lastIndexOf('@');
+		if (idx > 0) {
+			group = welcome.group.substr(0, idx);
+			order = Number(welcome.group.substr(idx + 1)) || undefined;
+		} else {
+			group = welcome.group;
+		}
+	}
+	return { group, order };
 }
