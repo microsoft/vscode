53c0ab95bb770a8f3f869e81bb9da200708e6700
deepak1556 <hop2deep@gmail.com>
2020-09-23 09:06:57 -0700

    build: disable publish step on mac temporarily

--- build/azure-pipelines/darwin/publish.sh
+++ build/azure-pipelines/darwin/publish.sh
@@ -3,19 +3,17 @@ set -e
 
 # publish the build
 node build/azure-pipelines/common/createAsset.js \
-	darwin-$VSCODE_ARCH \
+	darwin \
 	archive \
-	"VSCode-darwin-$VSCODE_QUALITY-$VSCODE_ARCH.zip" \
-	../VSCode-darwin-$VSCODE_ARCH.zip
+	"VSCode-darwin-$VSCODE_QUALITY.zip" \
+	../VSCode-darwin.zip
 
-if [ "$VSCODE_ARCH" == "x64" ]; then
-	# package Remote Extension Host
-	pushd .. && mv vscode-reh-darwin vscode-server-darwin && zip -Xry vscode-server-darwin.zip vscode-server-darwin && popd
+# package Remote Extension Host
+pushd .. && mv vscode-reh-darwin vscode-server-darwin && zip -Xry vscode-server-darwin.zip vscode-server-darwin && popd
 
-	# publish Remote Extension Host
-	node build/azure-pipelines/common/createAsset.js \
-		server-darwin \
-		archive-unsigned \
-		"vscode-server-darwin.zip" \
-		../vscode-server-darwin.zip
-fi
+# publish Remote Extension Host
+node build/azure-pipelines/common/createAsset.js \
+	server-darwin \
+	archive-unsigned \
+	"vscode-server-darwin.zip" \
+	../vscode-server-darwin.zip
