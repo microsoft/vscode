848236e33499635db5baee62cfad40358127c020
meganrogge <megrogge@gmail.com>
2020-10-07 09:30:28 -0700

    add header function, try to add parent range

--- extensions/markdown-language-features/src/features/smartSelect.ts
+++ extensions/markdown-language-features/src/features/smartSelect.ts
@@ -5,6 +5,7 @@
 
 import * as vscode from 'vscode';
 import { MarkdownEngine } from '../markdownEngine';
+import { TableOfContentsProvider } from '../tableOfContentsProvider';
 import { flatten } from '../util/arrays';
 
 export default class MarkdownSmartSelect implements vscode.SelectionRangeProvider {
@@ -13,24 +14,49 @@ export default class MarkdownSmartSelect implements vscode.SelectionRangeProvide
 		private readonly engine: MarkdownEngine
 	) { }
 	public async provideSelectionRanges(document: vscode.TextDocument, positions: vscode.Position[], _token: vscode.CancellationToken): Promise<vscode.SelectionRange[]> {
-		let blockRegions = await Promise.all([
+		let ranges = await Promise.all([
+			await this.getHeaderSelectionRanges(document, positions),
 			await this.getBlockSelectionRanges(document, positions)
-		]);
-		return flatten(blockRegions);
+			]);
+		return flatten(ranges);
 	}
 
-	// and header method
 	private async getBlockSelectionRanges(document: vscode.TextDocument, positions: vscode.Position[]): Promise<vscode.SelectionRange[]> {
 		let position = positions[0];
 		const tokens = await this.engine.parse(document);
-		let nearbyTokens = tokens.filter(token => token.map && (token.map[0] <= position.line && token.map[1] >= position.line));
-		let ranges = nearbyTokens.map(token => {
-			let start = token.map[0];
-			let end = token.type === 'bullet_list_open' ? token.map[1] - 1 : token.map[1];
-			let startPos = new vscode.Position(start, 0);
-			let endPos = new vscode.Position(end, 0);
+		let nearbyTokens = tokens.filter(token => token.type !== 'heading_open' && token.map && (token.map[0] <= position.line && token.map[1] >= position.line));
+		let firstToken = nearbyTokens.pop();
+		if (firstToken) {
+			let parentRange = new vscode.SelectionRange(new vscode.Range(new vscode.Position(firstToken.map[0], 0), new vscode.Position(firstToken.map[1], 0)));
+			let ranges = nearbyTokens.map(token => {
+				let start = token.map[0];
+				let end = token.type === 'bullet_list_open' ? token.map[1] - 1 : token.map[1];
+				let startPos = new vscode.Position(start, 0);
+				let endPos = new vscode.Position(end, 0);
+				if (parentRange.range.contains(new vscode.Range(startPos, endPos))) {
+					return new vscode.SelectionRange(new vscode.Range(startPos, endPos), parentRange);
+				} else {
+					return new vscode.SelectionRange(new vscode.Range(startPos, endPos));
+				}
+			});
+			return [ranges[0]];
+		}
+		return [];
+	}
+
+	private async getHeaderSelectionRanges(document: vscode.TextDocument, positions: vscode.Position[]): Promise<vscode.SelectionRange[]> {
+		let position = positions[0];
+		const tocProvider = new TableOfContentsProvider(this.engine, document);
+		const toc = await tocProvider.getToc();
+		let nearbyEntries = toc.filter(entry => entry.line === position.line);
+		return [nearbyEntries.map(entry => {
+			let endLine = entry.location.range.end.line;
+			if (document.lineAt(endLine).isEmptyOrWhitespace && endLine >= entry.line + 1) {
+				endLine = endLine - 1;
+			}
+			let startPos = entry.location.range.start;
+			let endPos = new vscode.Position(endLine, entry.location.range.end.character);
 			return new vscode.SelectionRange(new vscode.Range(startPos, endPos));
-		});
-		return ranges.length > 1 ? [ranges[0]] : ranges;
+		})][0];
 	}
 }
