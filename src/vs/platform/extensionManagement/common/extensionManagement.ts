b813399093b699c719d33bfcb419639dbbc79a7d
Sandeep Somavarapu <sasomava@microsoft.com>
2020-10-22 22:31:39 +0200

    - use --install-builtin-extension arg instaed of --builtin flag to install builtin extension - speed up installing extensions in cli

--- src/vs/platform/extensionManagement/common/extensionManagement.ts
+++ src/vs/platform/extensionManagement/common/extensionManagement.ts
@@ -152,6 +152,7 @@ export interface IExtensionGalleryService {
 	isEnabled(): boolean;
 	query(token: CancellationToken): Promise<IPager<IGalleryExtension>>;
 	query(options: IQueryOptions, token: CancellationToken): Promise<IPager<IGalleryExtension>>;
+	getExtensions(ids: string[], token: CancellationToken): Promise<IGalleryExtension[]>;
 	download(extension: IGalleryExtension, location: URI, operation: InstallOperation): Promise<void>;
 	reportStatistic(publisher: string, name: string, version: string, type: StatisticType): Promise<void>;
 	getReadme(extension: IGalleryExtension, token: CancellationToken): Promise<string>;
