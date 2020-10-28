5188d40d2b1fd40a60849f2c8125c2c9ac4fd0c9
João Moreno <joao.moreno@microsoft.com>
2020-10-28 10:46:34 +0100

    :lipstick: perf

--- src/vs/workbench/contrib/scm/common/scmService.ts
+++ src/vs/workbench/contrib/scm/common/scmService.ts
@@ -121,9 +121,7 @@ class SCMInput implements ISCMInput {
 	showNextHistoryValue(): void {
 		if (this.historyNavigator.isAtEnd()) {
 			return;
-		}
-
-		if (!this.historyNavigator.has(this.value)) {
+		} else if (!this.historyNavigator.has(this.value)) {
 			this.historyNavigator.replaceLast(this._value);
 			this.historyNavigator.resetCursor();
 		}
@@ -135,9 +133,7 @@ class SCMInput implements ISCMInput {
 	showPreviousHistoryValue(): void {
 		if (this.historyNavigator.isAtEnd()) {
 			this.historyNavigator.replaceLast(this._value);
-		}
-
-		if (!this.historyNavigator.has(this._value)) {
+		} else if (!this.historyNavigator.has(this._value)) {
 			this.historyNavigator.replaceLast(this._value);
 			this.historyNavigator.resetCursor();
 		}
