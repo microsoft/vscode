6fd760ab999f979e8ae1d878391a7f48ae7bdcda
Johannes Rieken <johannes.rieken@gmail.com>
2020-10-28 12:21:32 +0100

    more jsdoc https://github.com/microsoft/vscode/issues/95209 fyi @sandy081

--- src/vs/vscode.d.ts
+++ src/vs/vscode.d.ts
@@ -5652,12 +5652,12 @@ declare module 'vscode' {
 			 * Set the keys whose values should be synchronized across devices when synchronizing user-data
 			 * like configuration, extensions, and mementos.
 			 *
-			 * Note that this function defines the whole set of keys whoses values should be synchronized:
+			 * Note that this function defines the whole set of keys whoses values are synchronized:
 			 *  - calling it with an empty array stops synchronization for this memento
-			 *  - calling it with repeatedly with different arrays synchronizes the intersection
+			 *  - calling it with a non-empty array replaces all keys whose values are synchronized
 			 *
 			 * For any given set of keys this function needs to be called only once but there is no harm in
-			 * always repeatedly calling it.
+			 * repeatedly calling it.
 			 *
 			 * @param keys The set of keys whose values are synced.
 			 */
