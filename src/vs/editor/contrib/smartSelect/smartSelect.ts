7964dfd6e8cf52e27e3e0a121637296d679263a6
Johannes Rieken <johannes.rieken@gmail.com>
2020-11-02 15:28:13 +0100

    :lipstick: async-await

--- src/vs/editor/contrib/smartSelect/smartSelect.ts
+++ src/vs/editor/contrib/smartSelect/smartSelect.ts
@@ -48,7 +48,7 @@ class SelectionRanges {
 
 class SmartSelectController implements IEditorContribution {
 
-	public static readonly ID = 'editor.contrib.smartSelectController';
+	static readonly ID = 'editor.contrib.smartSelectController';
 
 	static get(editor: ICodeEditor): SmartSelectController {
 		return editor.getContribution<SmartSelectController>(SmartSelectController.ID);
@@ -64,24 +64,20 @@ class SmartSelectController implements IEditorContribution {
 		this._selectionListener?.dispose();
 	}
 
-	run(forward: boolean): Promise<void> | void {
+	async run(forward: boolean): Promise<void> {
 		if (!this._editor.hasModel()) {
 			return;
 		}
 
 		const selections = this._editor.getSelections();
 		const model = this._editor.getModel();
-
 		if (!modes.SelectionRangeRegistry.has(model)) {
 			return;
 		}
 
-
-		let promise: Promise<void> = Promise.resolve(undefined);
-
 		if (!this._state) {
 
-			promise = provideSelectionRanges(model, selections.map(s => s.getPosition()), this._editor.getOption(EditorOption.smartSelect), CancellationToken.None).then(ranges => {
+			await provideSelectionRanges(model, selections.map(s => s.getPosition()), this._editor.getOption(EditorOption.smartSelect), CancellationToken.None).then(ranges => {
 				if (!arrays.isNonEmptyArray(ranges) || ranges.length !== selections.length) {
 					// invalid result
 					return;
@@ -114,21 +110,18 @@ class SmartSelectController implements IEditorContribution {
 			});
 		}
 
-		return promise.then(() => {
-			if (!this._state) {
-				// no state
-				return;
-			}
-			this._state = this._state.map(state => state.mov(forward));
-			const selections = this._state.map(state => Selection.fromPositions(state.ranges[state.index].getStartPosition(), state.ranges[state.index].getEndPosition()));
-			this._ignoreSelection = true;
-			try {
-				this._editor.setSelections(selections);
-			} finally {
-				this._ignoreSelection = false;
-			}
-
-		});
+		if (!this._state) {
+			// no state
+			return;
+		}
+		this._state = this._state.map(state => state.mov(forward));
+		const newSelections = this._state.map(state => Selection.fromPositions(state.ranges[state.index].getStartPosition(), state.ranges[state.index].getEndPosition()));
+		this._ignoreSelection = true;
+		try {
+			this._editor.setSelections(newSelections);
+		} finally {
+			this._ignoreSelection = false;
+		}
 	}
 }
 
@@ -215,7 +208,7 @@ export interface SelectionRangesOptions {
 	selectLeadingAndTrailingWhitespace: boolean
 }
 
-export function provideSelectionRanges(model: ITextModel, positions: Position[], options: SelectionRangesOptions, token: CancellationToken): Promise<Range[][]> {
+export async function provideSelectionRanges(model: ITextModel, positions: Position[], options: SelectionRangesOptions, token: CancellationToken): Promise<Range[][]> {
 
 	const providers = modes.SelectionRangeRegistry.all(model);
 
@@ -245,66 +238,65 @@ export function provideSelectionRanges(model: ITextModel, positions: Position[],
 		}, onUnexpectedExternalError));
 	}
 
-	return Promise.all(work).then(() => {
+	await Promise.all(work);
 
-		return allRawRanges.map(oneRawRanges => {
+	return allRawRanges.map(oneRawRanges => {
 
-			if (oneRawRanges.length === 0) {
-				return [];
+		if (oneRawRanges.length === 0) {
+			return [];
+		}
+
+		// sort all by start/end position
+		oneRawRanges.sort((a, b) => {
+			if (Position.isBefore(a.getStartPosition(), b.getStartPosition())) {
+				return 1;
+			} else if (Position.isBefore(b.getStartPosition(), a.getStartPosition())) {
+				return -1;
+			} else if (Position.isBefore(a.getEndPosition(), b.getEndPosition())) {
+				return -1;
+			} else if (Position.isBefore(b.getEndPosition(), a.getEndPosition())) {
+				return 1;
+			} else {
+				return 0;
 			}
-
-			// sort all by start/end position
-			oneRawRanges.sort((a, b) => {
-				if (Position.isBefore(a.getStartPosition(), b.getStartPosition())) {
-					return 1;
-				} else if (Position.isBefore(b.getStartPosition(), a.getStartPosition())) {
-					return -1;
-				} else if (Position.isBefore(a.getEndPosition(), b.getEndPosition())) {
-					return -1;
-				} else if (Position.isBefore(b.getEndPosition(), a.getEndPosition())) {
-					return 1;
-				} else {
-					return 0;
-				}
-			});
-
-			// remove ranges that don't contain the former range or that are equal to the
-			// former range
-			let oneRanges: Range[] = [];
-			let last: Range | undefined;
-			for (const range of oneRawRanges) {
-				if (!last || (Range.containsRange(range, last) && !Range.equalsRange(range, last))) {
-					oneRanges.push(range);
-					last = range;
-				}
-			}
-
-			if (!options.selectLeadingAndTrailingWhitespace) {
-				return oneRanges;
-			}
-
-			// add ranges that expand trivia at line starts and ends whenever a range
-			// wraps onto the a new line
-			let oneRangesWithTrivia: Range[] = [oneRanges[0]];
-			for (let i = 1; i < oneRanges.length; i++) {
-				const prev = oneRanges[i - 1];
-				const cur = oneRanges[i];
-				if (cur.startLineNumber !== prev.startLineNumber || cur.endLineNumber !== prev.endLineNumber) {
-					// add line/block range without leading/failing whitespace
-					const rangeNoWhitespace = new Range(prev.startLineNumber, model.getLineFirstNonWhitespaceColumn(prev.startLineNumber), prev.endLineNumber, model.getLineLastNonWhitespaceColumn(prev.endLineNumber));
-					if (rangeNoWhitespace.containsRange(prev) && !rangeNoWhitespace.equalsRange(prev) && cur.containsRange(rangeNoWhitespace) && !cur.equalsRange(rangeNoWhitespace)) {
-						oneRangesWithTrivia.push(rangeNoWhitespace);
-					}
-					// add line/block range
-					const rangeFull = new Range(prev.startLineNumber, 1, prev.endLineNumber, model.getLineMaxColumn(prev.endLineNumber));
-					if (rangeFull.containsRange(prev) && !rangeFull.equalsRange(rangeNoWhitespace) && cur.containsRange(rangeFull) && !cur.equalsRange(rangeFull)) {
-						oneRangesWithTrivia.push(rangeFull);
-					}
-				}
-				oneRangesWithTrivia.push(cur);
-			}
-			return oneRangesWithTrivia;
 		});
+
+		// remove ranges that don't contain the former range or that are equal to the
+		// former range
+		let oneRanges: Range[] = [];
+		let last: Range | undefined;
+		for (const range of oneRawRanges) {
+			if (!last || (Range.containsRange(range, last) && !Range.equalsRange(range, last))) {
+				oneRanges.push(range);
+				last = range;
+			}
+		}
+
+		if (!options.selectLeadingAndTrailingWhitespace) {
+			return oneRanges;
+		}
+
+		// add ranges that expand trivia at line starts and ends whenever a range
+		// wraps onto the a new line
+		let oneRangesWithTrivia: Range[] = [oneRanges[0]];
+		for (let i = 1; i < oneRanges.length; i++) {
+			const prev = oneRanges[i - 1];
+			const cur = oneRanges[i];
+			if (cur.startLineNumber !== prev.startLineNumber || cur.endLineNumber !== prev.endLineNumber) {
+				// add line/block range without leading/failing whitespace
+				const rangeNoWhitespace = new Range(prev.startLineNumber, model.getLineFirstNonWhitespaceColumn(prev.startLineNumber), prev.endLineNumber, model.getLineLastNonWhitespaceColumn(prev.endLineNumber));
+				if (rangeNoWhitespace.containsRange(prev) && !rangeNoWhitespace.equalsRange(prev) && cur.containsRange(rangeNoWhitespace) && !cur.equalsRange(rangeNoWhitespace)) {
+					oneRangesWithTrivia.push(rangeNoWhitespace);
+				}
+				// add line/block range
+				const rangeFull = new Range(prev.startLineNumber, 1, prev.endLineNumber, model.getLineMaxColumn(prev.endLineNumber));
+				if (rangeFull.containsRange(prev) && !rangeFull.equalsRange(rangeNoWhitespace) && cur.containsRange(rangeFull) && !cur.equalsRange(rangeFull)) {
+					oneRangesWithTrivia.push(rangeFull);
+				}
+			}
+			oneRangesWithTrivia.push(cur);
+		}
+		return oneRangesWithTrivia;
 	});
 }
 
