e5285908ca9f16c7fc8c05f00d344a861fef1481
João Moreno <joao.moreno@microsoft.com>
2020-11-04 15:42:40 +0100

    :lipstick:

--- extensions/git/src/repository.ts
+++ extensions/git/src/repository.ts
@@ -1125,34 +1125,31 @@ export class Repository implements Disposable {
 
 	@throttle
 	async fetchDefault(options: { silent?: boolean } = {}): Promise<void> {
-		await this.fetchFrom({ silent: options.silent });
+		await this._fetch({ silent: options.silent });
 	}
 
 	@throttle
 	async fetchPrune(): Promise<void> {
-		await this.fetchFrom({ prune: true });
+		await this._fetch({ prune: true });
 	}
 
 	@throttle
 	async fetchAll(): Promise<void> {
-		await this.fetchFrom({ all: true });
+		await this._fetch({ all: true });
 	}
 
 	async fetch(remote?: string, ref?: string, depth?: number): Promise<void> {
-		await this.fetchFrom({ remote, ref, depth });
+		await this._fetch({ remote, ref, depth });
 	}
 
-	private async fetchFrom(options: { remote?: string, ref?: string, all?: boolean, prune?: boolean, depth?: number, silent?: boolean } = {}): Promise<void> {
-		await this.run(Operation.Fetch, async () => {
+	private async _fetch(options: { remote?: string, ref?: string, all?: boolean, prune?: boolean, depth?: number, silent?: boolean } = {}): Promise<void> {
+		if (!options.prune) {
 			const config = workspace.getConfiguration('git', Uri.file(this.root));
 			const prune = config.get<boolean>('pruneOnFetch');
+			options.prune = prune;
+		}
 
-			if (prune) {
-				options.prune = prune;
-			}
-
-			this.repository.fetch(options);
-		});
+		await this.run(Operation.Fetch, async () => this.repository.fetch(options));
 	}
 
 	@throttle
