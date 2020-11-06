50209eb9ddb9e0ea42c016e462f6f26be22e42e2
door-bell <socaltdb@gmail.com>
2020-11-05 21:14:51 -0800

    Add scroll options to list view options

--- src/vs/base/browser/ui/list/listWidget.ts
+++ src/vs/base/browser/ui/list/listWidget.ts
@@ -27,6 +27,7 @@ import { IDragAndDropData } from 'vs/base/browser/dnd';
 import { alert } from 'vs/base/browser/ui/aria/aria';
 import { IThemable } from 'vs/base/common/styler';
 import { createStyleSheet } from 'vs/base/browser/dom';
+import { ScrollableElementChangeOptions } from 'vs/base/browser/ui/scrollbar/scrollableElementOptions';
 
 interface ITraitChangeEvent {
 	indexes: number[];
@@ -852,6 +853,7 @@ export interface IListOptions<T> {
 	readonly additionalScrollHeight?: number;
 	readonly transformOptimization?: boolean;
 	readonly smoothScrolling?: boolean;
+	readonly scrollableElementChangeOptions?: ScrollableElementChangeOptions;
 }
 
 export interface IListStyles {
