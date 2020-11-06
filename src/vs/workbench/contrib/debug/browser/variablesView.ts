a1025c64c11f2c10f45469cdbfdf7f81f5274e52
isidor <inikolic@microsoft.com>
2020-11-06 16:15:47 +0100

    fixes #110077

--- src/vs/workbench/contrib/debug/browser/variablesView.ts
+++ src/vs/workbench/contrib/debug/browser/variablesView.ts
@@ -207,8 +207,8 @@ export class VariablesView extends ViewPane {
 			this.variableEvaluateName.set(!!variable.evaluateName);
 			this.breakWhenValueChangesSupported.reset();
 			if (session && session.capabilities.supportsDataBreakpoints) {
-				const response = await session.dataBreakpointInfo(variable.name, variable.parent.reference);
-				const dataBreakpointId = response?.dataId;
+				dataBreakpointInfoResponse = await session.dataBreakpointInfo(variable.name, variable.parent.reference);
+				const dataBreakpointId = dataBreakpointInfoResponse?.dataId;
 				this.breakWhenValueChangesSupported.set(!!dataBreakpointId);
 			}
 
