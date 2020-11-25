0921f711c3e6d8eb9def1fb23c307ff8ac09280b
Jackson Kearl <jakearl@microsoft.com>
2020-11-25 14:05:49 -0800

    Add GettingStartedService/Registry and initial getting started UI (#111175)
    * WIP -Getting Started Registry
    
    * Initial idea of how registry/service will work
    
    * Getting started with Getting Started
    
    * Add telemetry and touch up enablement flag
    
    * Add contrib as startup editor
    
    * Move to allowing static Getting Start blob
    
    * No newline
    
    * Remove unused enum

--- src/vs/workbench/contrib/themes/browser/themes.contribution.ts
+++ src/vs/workbench/contrib/themes/browser/themes.contribution.ts
@@ -22,6 +22,7 @@ import { onUnexpectedError } from 'vs/base/common/errors';
 import { IQuickInputService, QuickPickInput } from 'vs/platform/quickinput/common/quickInput';
 import { ConfigurationTarget } from 'vs/platform/configuration/common/configuration';
 import { DEFAULT_PRODUCT_ICON_THEME_ID } from 'vs/workbench/services/themes/browser/productIconThemeData';
+import { IGettingStartedService } from 'vs/workbench/services/gettingStarted/common/gettingStartedService';
 
 export class SelectColorThemeAction extends Action {
 
@@ -34,6 +35,7 @@ export class SelectColorThemeAction extends Action {
 		@IQuickInputService private readonly quickInputService: IQuickInputService,
 		@IWorkbenchThemeService private readonly themeService: IWorkbenchThemeService,
 		@IExtensionGalleryService private readonly extensionGalleryService: IExtensionGalleryService,
+		@IGettingStartedService private readonly gettingStartedService: IGettingStartedService,
 		@IViewletService private readonly viewletService: IViewletService
 	) {
 		super(id, label);
@@ -84,6 +86,7 @@ export class SelectColorThemeAction extends Action {
 						openExtensionViewlet(this.viewletService, `category:themes ${quickpick.value}`);
 					} else {
 						selectTheme(theme, true);
+						this.gettingStartedService.progressByEvent('themeSelected');
 					}
 					isCompleted = true;
 					quickpick.hide();
