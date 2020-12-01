f67050c0295b3765cd2c34794d348cc5b4540d92
Martin Aeschlimann <martinae@microsoft.com>
2020-12-01 15:17:18 +0100

    Missing jsdoc in LinkedEditingRanges. Fixes #111590

--- src/vs/vscode.d.ts
+++ src/vs/vscode.d.ts
@@ -4545,6 +4545,12 @@ declare module 'vscode' {
 	 * Represents a list of ranges that can be edited together along with a word pattern to describe valid range contents.
 	 */
 	export class LinkedEditingRanges {
+		/**
+		 * Create a new linked editing ranges object.
+		 *
+		 * @param ranges A list of ranges that can be edited together
+		 * @param wordPattern An optional word pattern that describes valid contents for the given ranges
+		 */
 		constructor(ranges: Range[], wordPattern?: RegExp);
 
 		/**
