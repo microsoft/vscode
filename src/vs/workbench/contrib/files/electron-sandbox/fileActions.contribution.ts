528ca4c9ea884058308eb477572d52b7283b6218
Benjamin Pasero <benjpas@microsoft.com>
2020-12-04 16:13:42 +0100

    fix duplicate imports
    //cc @isidorn

--- src/vs/workbench/contrib/files/electron-sandbox/fileActions.contribution.ts
+++ src/vs/workbench/contrib/files/electron-sandbox/fileActions.contribution.ts
@@ -14,7 +14,7 @@ import { KeybindingsRegistry, KeybindingWeight } from 'vs/platform/keybinding/co
 import { EditorContextKeys } from 'vs/editor/common/editorContextKeys';
 import { KeyMod, KeyCode, KeyChord } from 'vs/base/common/keyCodes';
 import { ServicesAccessor } from 'vs/platform/instantiation/common/instantiation';
-import { getMultiSelectedResources } from 'vs/workbench/contrib/files/browser/files';
+import { getMultiSelectedResources, IExplorerService } from 'vs/workbench/contrib/files/browser/files';
 import { IListService } from 'vs/platform/list/browser/listService';
 import { IEditorService } from 'vs/workbench/services/editor/common/editorService';
 import { revealResourcesInOS } from 'vs/workbench/contrib/files/electron-sandbox/fileCommands';
@@ -23,7 +23,6 @@ import { ResourceContextKey } from 'vs/workbench/common/resources';
 import { appendToCommandPalette, appendEditorTitleContextMenuItem } from 'vs/workbench/contrib/files/browser/fileActions.contribution';
 import { SideBySideEditor, EditorResourceAccessor } from 'vs/workbench/common/editor';
 import { ContextKeyOrExpr } from 'vs/platform/contextkey/common/contextkey';
-import { IExplorerService } from 'vs/workbench/contrib/files/browser/files';
 
 const REVEAL_IN_OS_COMMAND_ID = 'revealFileInOS';
 const REVEAL_IN_OS_LABEL = isWindows ? nls.localize('revealInWindows', "Reveal in File Explorer") : isMacintosh ? nls.localize('revealInMac', "Reveal in Finder") : nls.localize('openContainer', "Open Containing Folder");
