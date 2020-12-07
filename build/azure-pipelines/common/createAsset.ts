1e83d6f50b2c94298390faafbb80d7915e7c5b48
João Moreno <joao.moreno@microsoft.com>
2020-12-07 11:58:16 +0100

    Revert "build: create asset should still try to add asset"
    This reverts commit 5e350b1b79675cecdff224eb00f7bf62ae8789fc.

--- build/azure-pipelines/common/createAsset.ts
+++ build/azure-pipelines/common/createAsset.ts
@@ -93,11 +93,15 @@ async function main(): Promise<void> {
 	const blobExists = await doesAssetExist(blobService, quality, blobName);
 
 	if (blobExists) {
-		console.log(`Blob ${quality}, ${blobName} already exists, not uploading again.`);
-	} else {
-		await uploadBlob(blobService, quality, blobName, filePath, fileName);
-		console.log('Blobs successfully uploaded.');
+		console.log(`Blob ${quality}, ${blobName} already exists, not publishing again.`);
+		return;
 	}
+
+	console.log('Uploading blobs to Azure storage...');
+
+	await uploadBlob(blobService, quality, blobName, filePath, fileName);
+
+	console.log('Blobs successfully uploaded.');
 
 	const asset: Asset = {
 		platform,
