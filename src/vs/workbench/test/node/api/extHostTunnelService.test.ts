ab7c033e0465053ca44d41487ff511a408073483
Alex Ross <alros@microsoft.com>
2020-12-08 16:38:49 +0100

    Test findPorts

--- src/vs/workbench/test/node/api/extHostTunnelService.test.ts
+++ src/vs/workbench/test/node/api/extHostTunnelService.test.ts
@@ -3,15 +3,12 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-// import * as assert from 'assert';
-// import * as platform from 'vs/base/common/platform';
-// import * as os from 'os';
-import { findPorts } from 'vs/workbench/api/node/extHostTunnelService';
+import * as assert from 'assert';
+import * as platform from 'vs/base/common/platform';
+import { findPorts, getSockets, loadConnectionTable, loadListeningPorts } from 'vs/workbench/api/node/extHostTunnelService';
 
-suite('ExtHostTunnelService', () => {
-	test('findPorts', async function () {
-		const tcp =
-			`  sl  local_address rem_address   st tx_queue rx_queue tr tm->when retrnsmt   uid  timeout inode
+const tcp =
+	`  sl  local_address rem_address   st tx_queue rx_queue tr tm->when retrnsmt   uid  timeout inode
 	0: 00000000:0BBA 00000000:0000 0A 00000000:00000000 00:00000000 00000000  1000        0 2335214 1 0000000010173312 100 0 0 10 0
 	1: 00000000:1AF3 00000000:0000 0A 00000000:00000000 00:00000000 00000000  1000        0 2334514 1 000000008815920b 100 0 0 10 0
 	2: 0100007F:A9EA 0100007F:1AF3 01 00000000:00000000 00:00000000 00000000  1000        0 2334521 1 00000000a37d44c6 21 4 0 10 -1
@@ -19,8 +16,8 @@ suite('ExtHostTunnelService', () => {
 	4: 0100007F:866C 0100007F:8783 01 00000000:00000000 00:00000000 00000000  1000        0 2334510 1 00000000cbf670bb 21 4 30 10 -1
 	5: 0100007F:1AF3 0100007F:A9EA 01 00000000:00000000 00:00000000 00000000  1000        0 2338989 1 0000000000bace62 21 4 1 10 -1
 `;
-		const tcp6 =
-			`  sl  local_address                         remote_address                        st tx_queue rx_queue tr tm->when retrnsmt   uid  timeout inode
+const tcp6 =
+	`  sl  local_address                         remote_address                        st tx_queue rx_queue tr tm->when retrnsmt   uid  timeout inode
 	0: 00000000000000000000000000000000:815B 00000000000000000000000000000000:0000 0A 00000000:00000000 00:00000000 00000000  1000        0 2321070 1 00000000c44f3f02 100 0 0 10 0
 	1: 00000000000000000000000000000000:8783 00000000000000000000000000000000:0000 0A 00000000:00000000 00:00000000 00000000  1000        0 2334509 1 000000003915e812 100 0 0 10 0
 	2: 00000000000000000000000000000000:9907 00000000000000000000000000000000:0000 0A 00000000:00000000 00:00000000 00000000  1000        0 2284465 1 00000000f13b9374 100 0 0 10 0
@@ -33,8 +30,9 @@ suite('ExtHostTunnelService', () => {
 	9: 0000000000000000FFFF0000DFD317AC:9907 0000000000000000FFFF000001D017AC:C213 01 00000000:00000000 00:00000000 00000000  1000        0 2331501 1 00000000957fcb4a 26 4 30 10 57
 	10: 0000000000000000FFFF0000DFD317AC:9907 0000000000000000FFFF000001D017AC:C214 01 00000000:00000000 00:00000000 00000000  1000        0 2331500 1 00000000d7f87ceb 25 4 28 10 -1
 `;
-		const procSockets =
-			`ls: cannot access '/proc/8289/fd/255': No such file or directory
+
+const procSockets =
+	`ls: cannot access '/proc/8289/fd/255': No such file or directory
 			ls: cannot access '/proc/8289/fd/3': No such file or directory
 			lrwx------ 1 alex alex 64 Dec  8 14:59 /proc/230/fd/3 -> socket:[21862]
 			lrwx------ 1 alex alex 64 Dec  8 15:14 /proc/2504/fd/0 -> socket:[2311043]
@@ -115,109 +113,154 @@ suite('ExtHostTunnelService', () => {
 			lrwx------ 1 alex alex 64 Dec  8 15:22 /proc/4496/fd/3 -> socket:[2331511]
 			lrwx------ 1 alex alex 64 Dec  8 15:22 /proc/4496/fd/31 -> socket:[2338285]`;
 
-		const processes: { pid: number, cwd: string, cmd: string }[] = [
-			{
-				pid: 230,
-				cwd: '/mnt/c/WINDOWS/system32',
-				cmd: 'dockerserve--addressunix:///home/alex/.docker/run/docker-cli-api.sock',
-			},
-			{
-				pid: 2504,
-				cwd: '/mnt/c/Users/alros/AppData/Local/Programs/Microsoft VS Code Insiders',
-				cmd: '/home/alex/.vscode-server-insiders/bin/bc13785d3dd99b4b0e9da9aed17bb79809a50804/node/home/alex/.vscode-server-insiders/bin/bc13785d3dd99b4b0e9da9aed17bb79809a50804/out/bootstrap-fork--type=extensionHost--uriTransformerPath=/home/alex/.vscode-server-insiders/bin/bc13785d3dd99b4b0e9da9aed17bb79809a50804/out/vs/server/uriTransformer.js--useHostProxy=',
-			},
-			{
-				pid: 2515,
-				cwd: '/mnt/c/Users/alros/AppData/Local/Programs/Microsoft VS Code Insiders',
-				cmd: '/home/alex/.vscode-server-insiders/bin/bc13785d3dd99b4b0e9da9aed17bb79809a50804/node/home/alex/.vscode-server-insiders/bin/bc13785d3dd99b4b0e9da9aed17bb79809a50804/out/bootstrap-fork--type=watcherService'
-			},
-			{
-				pid: 2526,
-				cwd: '/home/alex/repos/Microsoft/vscode-extension-samples/helloworld-sample',
-				cmd: '/bin/bash'
-			}, {
-				pid: 2719,
-				cwd: '/mnt/c/Users/alros/AppData/Local/Programs/Microsoft VS Code Insiders',
-				cmd: '/home/alex/.vscode-server-insiders/bin/bc13785d3dd99b4b0e9da9aed17bb79809a50804/node--max-old-space-size=3072/home/alex/.vscode-server-insiders/bin/bc13785d3dd99b4b0e9da9aed17bb79809a50804/extensions/node_modules/typescript/lib/tsserver.js--serverModepartialSemantic--useInferredProjectPerProjectRoot--disableAutomaticTypingAcquisition--cancellationPipeName/tmp/vscode-typescript1000/7cfa7171c0c00aacf1ee/tscancellation-602cd80b954818b6a2f7.tmp*--logVerbosityverbose--logFile/home/alex/.vscode-server-insiders/data/logs/20201208T145954/exthost2/vscode.typescript-language-features/tsserver-log-nxBt2m/tsserver.log--globalPluginstypescript-vscode-sh-plugin--pluginProbeLocations/home/alex/.vscode-server-insiders/bin/bc13785d3dd99b4b0e9da9aed17bb79809a50804/extensions/typescript-language-features--localeen--noGetErrOnBackgroundUpdate--validateDefaultNpmLocation'
-			},
-			{
-				pid: 2725,
-				cwd: '/mnt/c/Users/alros/AppData/Local/Programs/Microsoft VS Code Insiders',
-				cmd: '/home/alex/.vscode-server-insiders/bin/bc13785d3dd99b4b0e9da9aed17bb79809a50804/node--max-old-space-size=3072/home/alex/.vscode-server-insiders/bin/bc13785d3dd99b4b0e9da9aed17bb79809a50804/extensions/node_modules/typescript/lib/tsserver.js--useInferredProjectPerProjectRoot--enableTelemetry--cancellationPipeName/tmp/vscode-typescript1000/7cfa7171c0c00aacf1ee/tscancellation-04a0b92f880c2fd535ae.tmp*--logVerbosityverbose--logFile/home/alex/.vscode-server-insiders/data/logs/20201208T145954/exthost2/vscode.typescript-language-features/tsserver-log-fqyBrs/tsserver.log--globalPluginstypescript-vscode-sh-plugin--pluginProbeLocations/home/alex/.vscode-server-insiders/bin/bc13785d3dd99b4b0e9da9aed17bb79809a50804/extensions/typescript-language-features--localeen--noGetErrOnBackgroundUpdate--validateDefaultNpmLocation'
-			},
-			{
-				pid: 2739,
-				cwd: '/mnt/c/Users/alros/AppData/Local/Programs/Microsoft VS Code Insiders',
-				cmd: '/home/alex/.vscode-server-insiders/bin/bc13785d3dd99b4b0e9da9aed17bb79809a50804/node/home/alex/.vscode-server-insiders/bin/bc13785d3dd99b4b0e9da9aed17bb79809a50804/extensions/node_modules/typescript/lib/typingsInstaller.js--globalTypingsCacheLocation/home/alex/.cache/typescript/4.1--enableTelemetry--logFile/home/alex/.vscode-server-insiders/data/logs/20201208T145954/exthost2/vscode.typescript-language-features/tsserver-log-fqyBrs/ti-2725.log--typesMapLocation/home/alex/.vscode-server-insiders/bin/bc13785d3dd99b4b0e9da9aed17bb79809a50804/extensions/node_modules/typescript/lib/typesMap.json--validateDefaultNpmLocation'
-			},
-			{
-				pid: 2795,
-				cwd: '/home/alex/repos/Microsoft/vscode-extension-samples/helloworld-sample',
-				cmd: '/home/alex/.vscode-server-insiders/bin/bc13785d3dd99b4b0e9da9aed17bb79809a50804/node/home/alex/.vscode-server-insiders/bin/bc13785d3dd99b4b0e9da9aed17bb79809a50804/extensions/json-language-features/server/dist/node/jsonServerMain--node-ipc--clientProcessId=2504'
-			},
-			{
-				pid: 286,
-				cwd: '/mnt/c/Users/alros/AppData/Local/Programs/Microsoft VS Code Insiders',
-				cmd: 'sh-c\"$VSCODE_WSL_EXT_LOCATION/ scripts / wslServer.sh\" bc13785d3dd99b4b0e9da9aed17bb79809a50804 insider .vscode-server-insiders 0  '
-			},
-			{
-				pid: 287,
-				cwd: '/mnt/c/Users/alros/AppData/Local/Programs/Microsoft VS Code Insiders',
-				cmd: 'sh/mnt/c/Users/alros/.vscode-insiders/extensions/ms-vscode-remote.remote-wsl-0.52.0/scripts/wslServer.shbc13785d3dd99b4b0e9da9aed17bb79809a50804insider.vscode-server-insiders0'
-			},
-			{
-				pid: 3058,
-				cwd: '/home/alex/repos/Microsoft/vscode-extension-samples/helloworld-sample',
-				cmd: 'npm'
-			},
-			{
-				pid: 3070,
-				cwd: '/home/alex/repos/Microsoft/vscode-extension-samples/helloworld-sample',
-				cmd: 'sh-ctsc -watch -p ./'
-			},
-			{
-				pid: 3071,
-				cwd: '/home/alex/repos/Microsoft/vscode-extension-samples/helloworld-sample',
-				cmd: 'node/home/alex/repos/Microsoft/vscode-extension-samples/helloworld-sample/node_modules/.bin/tsc-watch-p./'
-			},
-			{
-				pid: 312,
-				cwd: '/mnt/c/Users/alros/AppData/Local/Programs/Microsoft VS Code Insiders',
-				cmd: 'sh/home/alex/.vscode-server-insiders/bin/bc13785d3dd99b4b0e9da9aed17bb79809a50804/server.sh--port=0--use-host-proxy--enable-remote-auto-shutdown--print-ip-address'
-			},
-			{
-				pid: 314,
-				cwd: '/mnt/c/Users/alros/AppData/Local/Programs/Microsoft VS Code Insiders',
-				cmd: '/home/alex/.vscode-server-insiders/bin/bc13785d3dd99b4b0e9da9aed17bb79809a50804/node/home/alex/.vscode-server-insiders/bin/bc13785d3dd99b4b0e9da9aed17bb79809a50804/out/vs/server/main.js--port=0--use-host-proxy--enable-remote-auto-shutdown--print-ip-address'
-			},
-			{
-				pid: 3172,
-				cwd: '/home/alex',
-				cmd: '/bin/bash'
-			},
-			{
-				pid: 3610,
-				cwd: '/home/alex/repos/Microsoft/vscode-extension-samples/helloworld-sample',
-				cmd: '/bin/bash'
-			},
-			{
-				pid: 4412,
-				cwd: '/home/alex/repos/Microsoft/vscode-extension-samples/helloworld-sample',
-				cmd: 'http-server'
-			},
-			{
-				pid: 4496,
-				cwd: '/mnt/c/Users/alros/AppData/Local/Programs/Microsoft VS Code Insiders',
-				cmd: '/home/alex/.vscode-server-insiders/bin/bc13785d3dd99b4b0e9da9aed17bb79809a50804/node--inspect-brk=0.0.0.0:6899/home/alex/.vscode-server-insiders/bin/bc13785d3dd99b4b0e9da9aed17bb79809a50804/out/bootstrap-fork--type=extensionHost--uriTransformerPath=/home/alex/.vscode-server-insiders/bin/bc13785d3dd99b4b0e9da9aed17bb79809a50804/out/vs/server/uriTransformer.js--useHostProxy='
-			},
-			{
-				pid: 4507,
-				cwd: '/mnt/c/Users/alros/AppData/Local/Programs/Microsoft VS Code Insiders',
-				cmd: '/home/alex/.vscode-server-insiders/bin/bc13785d3dd99b4b0e9da9aed17bb79809a50804/node/home/alex/.vscode-server-insiders/bin/bc13785d3dd99b4b0e9da9aed17bb79809a50804/extensions/ms-vscode.js-debug/src/hash.bundle.js'
-			}
-		];
+const processes: { pid: number, cwd: string, cmd: string }[] = [
+	{
+		pid: 230,
+		cwd: '/mnt/c/WINDOWS/system32',
+		cmd: 'dockerserve--addressunix:///home/alex/.docker/run/docker-cli-api.sock',
+	},
+	{
+		pid: 2504,
+		cwd: '/mnt/c/Users/alros/AppData/Local/Programs/Microsoft VS Code Insiders',
+		cmd: '/home/alex/.vscode-server-insiders/bin/bc13785d3dd99b4b0e9da9aed17bb79809a50804/node/home/alex/.vscode-server-insiders/bin/bc13785d3dd99b4b0e9da9aed17bb79809a50804/out/bootstrap-fork--type=extensionHost--uriTransformerPath=/home/alex/.vscode-server-insiders/bin/bc13785d3dd99b4b0e9da9aed17bb79809a50804/out/vs/server/uriTransformer.js--useHostProxy=',
+	},
+	{
+		pid: 2515,
+		cwd: '/mnt/c/Users/alros/AppData/Local/Programs/Microsoft VS Code Insiders',
+		cmd: '/home/alex/.vscode-server-insiders/bin/bc13785d3dd99b4b0e9da9aed17bb79809a50804/node/home/alex/.vscode-server-insiders/bin/bc13785d3dd99b4b0e9da9aed17bb79809a50804/out/bootstrap-fork--type=watcherService'
+	},
+	{
+		pid: 2526,
+		cwd: '/home/alex/repos/Microsoft/vscode-extension-samples/helloworld-sample',
+		cmd: '/bin/bash'
+	}, {
+		pid: 2719,
+		cwd: '/mnt/c/Users/alros/AppData/Local/Programs/Microsoft VS Code Insiders',
+		cmd: '/home/alex/.vscode-server-insiders/bin/bc13785d3dd99b4b0e9da9aed17bb79809a50804/node--max-old-space-size=3072/home/alex/.vscode-server-insiders/bin/bc13785d3dd99b4b0e9da9aed17bb79809a50804/extensions/node_modules/typescript/lib/tsserver.js--serverModepartialSemantic--useInferredProjectPerProjectRoot--disableAutomaticTypingAcquisition--cancellationPipeName/tmp/vscode-typescript1000/7cfa7171c0c00aacf1ee/tscancellation-602cd80b954818b6a2f7.tmp*--logVerbosityverbose--logFile/home/alex/.vscode-server-insiders/data/logs/20201208T145954/exthost2/vscode.typescript-language-features/tsserver-log-nxBt2m/tsserver.log--globalPluginstypescript-vscode-sh-plugin--pluginProbeLocations/home/alex/.vscode-server-insiders/bin/bc13785d3dd99b4b0e9da9aed17bb79809a50804/extensions/typescript-language-features--localeen--noGetErrOnBackgroundUpdate--validateDefaultNpmLocation'
+	},
+	{
+		pid: 2725,
+		cwd: '/mnt/c/Users/alros/AppData/Local/Programs/Microsoft VS Code Insiders',
+		cmd: '/home/alex/.vscode-server-insiders/bin/bc13785d3dd99b4b0e9da9aed17bb79809a50804/node--max-old-space-size=3072/home/alex/.vscode-server-insiders/bin/bc13785d3dd99b4b0e9da9aed17bb79809a50804/extensions/node_modules/typescript/lib/tsserver.js--useInferredProjectPerProjectRoot--enableTelemetry--cancellationPipeName/tmp/vscode-typescript1000/7cfa7171c0c00aacf1ee/tscancellation-04a0b92f880c2fd535ae.tmp*--logVerbosityverbose--logFile/home/alex/.vscode-server-insiders/data/logs/20201208T145954/exthost2/vscode.typescript-language-features/tsserver-log-fqyBrs/tsserver.log--globalPluginstypescript-vscode-sh-plugin--pluginProbeLocations/home/alex/.vscode-server-insiders/bin/bc13785d3dd99b4b0e9da9aed17bb79809a50804/extensions/typescript-language-features--localeen--noGetErrOnBackgroundUpdate--validateDefaultNpmLocation'
+	},
+	{
+		pid: 2739,
+		cwd: '/mnt/c/Users/alros/AppData/Local/Programs/Microsoft VS Code Insiders',
+		cmd: '/home/alex/.vscode-server-insiders/bin/bc13785d3dd99b4b0e9da9aed17bb79809a50804/node/home/alex/.vscode-server-insiders/bin/bc13785d3dd99b4b0e9da9aed17bb79809a50804/extensions/node_modules/typescript/lib/typingsInstaller.js--globalTypingsCacheLocation/home/alex/.cache/typescript/4.1--enableTelemetry--logFile/home/alex/.vscode-server-insiders/data/logs/20201208T145954/exthost2/vscode.typescript-language-features/tsserver-log-fqyBrs/ti-2725.log--typesMapLocation/home/alex/.vscode-server-insiders/bin/bc13785d3dd99b4b0e9da9aed17bb79809a50804/extensions/node_modules/typescript/lib/typesMap.json--validateDefaultNpmLocation'
+	},
+	{
+		pid: 2795,
+		cwd: '/home/alex/repos/Microsoft/vscode-extension-samples/helloworld-sample',
+		cmd: '/home/alex/.vscode-server-insiders/bin/bc13785d3dd99b4b0e9da9aed17bb79809a50804/node/home/alex/.vscode-server-insiders/bin/bc13785d3dd99b4b0e9da9aed17bb79809a50804/extensions/json-language-features/server/dist/node/jsonServerMain--node-ipc--clientProcessId=2504'
+	},
+	{
+		pid: 286,
+		cwd: '/mnt/c/Users/alros/AppData/Local/Programs/Microsoft VS Code Insiders',
+		cmd: 'sh-c\"$VSCODE_WSL_EXT_LOCATION/ scripts / wslServer.sh\" bc13785d3dd99b4b0e9da9aed17bb79809a50804 insider .vscode-server-insiders 0  '
+	},
+	{
+		pid: 287,
+		cwd: '/mnt/c/Users/alros/AppData/Local/Programs/Microsoft VS Code Insiders',
+		cmd: 'sh/mnt/c/Users/alros/.vscode-insiders/extensions/ms-vscode-remote.remote-wsl-0.52.0/scripts/wslServer.shbc13785d3dd99b4b0e9da9aed17bb79809a50804insider.vscode-server-insiders0'
+	},
+	{
+		pid: 3058,
+		cwd: '/home/alex/repos/Microsoft/vscode-extension-samples/helloworld-sample',
+		cmd: 'npm'
+	},
+	{
+		pid: 3070,
+		cwd: '/home/alex/repos/Microsoft/vscode-extension-samples/helloworld-sample',
+		cmd: 'sh-ctsc -watch -p ./'
+	},
+	{
+		pid: 3071,
+		cwd: '/home/alex/repos/Microsoft/vscode-extension-samples/helloworld-sample',
+		cmd: 'node/home/alex/repos/Microsoft/vscode-extension-samples/helloworld-sample/node_modules/.bin/tsc-watch-p./'
+	},
+	{
+		pid: 312,
+		cwd: '/mnt/c/Users/alros/AppData/Local/Programs/Microsoft VS Code Insiders',
+		cmd: 'sh/home/alex/.vscode-server-insiders/bin/bc13785d3dd99b4b0e9da9aed17bb79809a50804/server.sh--port=0--use-host-proxy--enable-remote-auto-shutdown--print-ip-address'
+	},
+	{
+		pid: 314,
+		cwd: '/mnt/c/Users/alros/AppData/Local/Programs/Microsoft VS Code Insiders',
+		cmd: '/home/alex/.vscode-server-insiders/bin/bc13785d3dd99b4b0e9da9aed17bb79809a50804/node/home/alex/.vscode-server-insiders/bin/bc13785d3dd99b4b0e9da9aed17bb79809a50804/out/vs/server/main.js--port=0--use-host-proxy--enable-remote-auto-shutdown--print-ip-address'
+	},
+	{
+		pid: 3172,
+		cwd: '/home/alex',
+		cmd: '/bin/bash'
+	},
+	{
+		pid: 3610,
+		cwd: '/home/alex/repos/Microsoft/vscode-extension-samples/helloworld-sample',
+		cmd: '/bin/bash'
+	},
+	{
+		pid: 4412,
+		cwd: '/home/alex/repos/Microsoft/vscode-extension-samples/helloworld-sample',
+		cmd: 'http-server'
+	},
+	{
+		pid: 4496,
+		cwd: '/mnt/c/Users/alros/AppData/Local/Programs/Microsoft VS Code Insiders',
+		cmd: '/home/alex/.vscode-server-insiders/bin/bc13785d3dd99b4b0e9da9aed17bb79809a50804/node--inspect-brk=0.0.0.0:6899/home/alex/.vscode-server-insiders/bin/bc13785d3dd99b4b0e9da9aed17bb79809a50804/out/bootstrap-fork--type=extensionHost--uriTransformerPath=/home/alex/.vscode-server-insiders/bin/bc13785d3dd99b4b0e9da9aed17bb79809a50804/out/vs/server/uriTransformer.js--useHostProxy='
+	},
+	{
+		pid: 4507,
+		cwd: '/mnt/c/Users/alros/AppData/Local/Programs/Microsoft VS Code Insiders',
+		cmd: '/home/alex/.vscode-server-insiders/bin/bc13785d3dd99b4b0e9da9aed17bb79809a50804/node/home/alex/.vscode-server-insiders/bin/bc13785d3dd99b4b0e9da9aed17bb79809a50804/extensions/ms-vscode.js-debug/src/hash.bundle.js'
+	}
+];
 
-		const result = findPorts(tcp, tcp6, procSockets, processes);
+if (platform.isLinux) {
+	suite('ExtHostTunnelService', () => {
+		test('getSockets', function () {
+			const result = getSockets(procSockets);
+			assert.equal(result.length, 78);
+			// 4412 is the pid fo the http-server in the test data
+			assert.notEqual(result.find(value => value.pid === 4412), undefined);
+		});
 
+		test('loadConnectionTable', function () {
+			const result = loadConnectionTable(tcp);
+			assert.equal(result.length, 6);
+			assert.deepEqual(result[0], {
+				10: '1',
+				11: '0000000010173312',
+				12: '100',
+				13: '0',
+				14: '0',
+				15: '10',
+				16: '0',
+				inode: '2335214',
+				local_address: '00000000:0BBA',
+				rem_address: '00000000:0000',
+				retrnsmt: '00000000',
+				sl: '0:',
+				st: '0A',
+				timeout: '0',
+				tr: '00:00000000',
+				tx_queue: '00000000:00000000',
+				uid: '1000'
+			});
+		});
+
+		test('loadListeningPorts', function () {
+			const result = loadListeningPorts(tcp, tcp6);
+			// There should be 7 based on the input data. One of them should be 3002.
+			assert.equal(result.length, 7);
+			assert.notEqual(result.find(value => value.port === 3002), undefined);
+		});
+
+		test('findPorts', async function () {
+			const result = await findPorts(tcp, tcp6, procSockets, processes);
+			assert.equal(result.length, 1);
+			assert.equal(result[0].host, '0.0.0.0');
+			assert.equal(result[0].port, 3002);
+			assert.equal(result[0].detail, 'http-server');
+		});
 	});
-});
+}
