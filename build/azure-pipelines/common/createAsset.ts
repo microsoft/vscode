0e885aaf70ccffd3f2f6ad66c558cfd95e415719
Joao Moreno <joao.moreno@microsoft.com>
2020-12-08 09:59:21 +0100

    retry all cosmosdb ops

--- build/azure-pipelines/common/createAsset.ts
+++ build/azure-pipelines/common/createAsset.ts
@@ -11,6 +11,7 @@ import * as crypto from 'crypto';
 import * as azure from 'azure-storage';
 import * as mime from 'mime';
 import { CosmosClient } from '@azure/cosmos';
+import { retry } from './retry';
 
 interface Asset {
 	platform: string;
@@ -64,26 +65,6 @@ function getEnv(name: string): string {
 	}
 
 	return result;
-}
-
-async function retry<T>(fn: () => Promise<T>): Promise<T> {
-	for (let run = 1; run <= 10; run++) {
-		try {
-			return await fn();
-		} catch (err) {
-			if (!/ECONNRESET/.test(err.message)) {
-				throw err;
-			}
-
-			const millis = (Math.random() * 200) + (50 * Math.pow(1.5, run));
-			console.log(`Creating build asset failed with ECONNRESET, retrying in ${millis}ms...`);
-
-			// maximum delay is 10th retry: ~3 seconds
-			await new Promise(c => setTimeout(c, millis));
-		}
-	}
-
-	throw new Error('Retried too many times');
 }
 
 async function main(): Promise<void> {
