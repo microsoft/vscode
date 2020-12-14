7f01743933cd87dfdf88d50b453f9dd1626e4338
Jackson Kearl <jakearl@microsoft.com>
2020-12-14 14:15:16 -0800

    Working native scrol implementation

--- src/vs/workbench/contrib/welcome/gettingStarted/browser/vs_code_editor_getting_started.ts
+++ src/vs/workbench/contrib/welcome/gettingStarted/browser/vs_code_editor_getting_started.ts
@@ -15,6 +15,8 @@ export default () => `
 				<h1 id="product-name" class="caption">${escape(localize('gettingStarted.vscode', "Visual Studio Code"))}</h1>
 				<p class="subtitle description">${escape(localize({ key: 'gettingStarted.editingRedefined', comment: ['Shown as subtitle on the Welcome page.'] }, "Code editing. Redefined"))}</p>
 			</div>
+			<div id="getting-started-categories-scrolling-container"><div id="getting-started-categories-container"></div></div>
+			<div class="gap"></div>
 		</div>
 		<div id="gettingStartedSlideDetails" class="gettingStartedSlide detail">
 			<a class="prev-button" x-dispatch="scrollPrev"><span
@@ -35,7 +37,3 @@ export default () => `
 	</div>
 </div>
 `.replace(/\|/g, '`');
-
-
-// <div id="getting-started-categories-scrolling-container"><div id="getting-started-categories-container"></div></div>
-// 			<div class="gap"></div>
