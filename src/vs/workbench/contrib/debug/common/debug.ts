d444826441aed51d7190a58349e5b7f4049baa68
isidor <inikolic@microsoft.com>
2020-12-15 15:18:59 +0100

    Revert "debug: stop supporting enableBreakpointsFor"
    This reverts commit 9f2ce5386713184ef65d1917e84eb6da8b480395.

--- src/vs/workbench/contrib/debug/common/debug.ts
+++ src/vs/workbench/contrib/debug/common/debug.ts
@@ -630,6 +630,7 @@ export interface IDebuggerContribution extends IPlatformSpecificAdapterContribut
 
 	// supported languages
 	languages?: string[];
+	enableBreakpointsFor?: { languageIds: string[] };
 
 	// debug configuration support
 	configurationAttributes?: any;
