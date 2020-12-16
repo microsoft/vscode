2156931d38bc48f490ffef25a50ed410e19a6bae
Jackson Kearl <jakearl@microsoft.com>
2020-12-16 10:16:10 -0800

    Better side scrolling

--- src/vs/workbench/contrib/welcome/gettingStarted/browser/vs_code_editor_getting_started.ts
+++ src/vs/workbench/contrib/welcome/gettingStarted/browser/vs_code_editor_getting_started.ts
@@ -18,13 +18,16 @@ export default () => `
 		</div>
 		<div id="gettingStartedSlideDetails" class="gettingStartedSlide detail">
 			<a class="prev-button" x-dispatch="scrollPrev"><span class="scroll-button codicon codicon-chevron-left"></span>Back</a>
-			<div class="gap"></div>
-			<div id="getting-started-detail-columns">
+			<div id="gettingStartedDetailsContent">
 				<div class="gap"></div>
-				<div id="getting-started-detail-left">
-					<div id="getting-started-detail-title"></div>
+				<div id="getting-started-detail-columns">
+					<div class="gap"></div>
+					<div id="getting-started-detail-left">
+						<div id="getting-started-detail-title"></div>
+					</div>
+					<div id="getting-started-detail-right"></div>
+					<div class="gap"></div>
 				</div>
-				<div id="getting-started-detail-right"></div>
 				<div class="gap"></div>
 			</div>
 		</div>
