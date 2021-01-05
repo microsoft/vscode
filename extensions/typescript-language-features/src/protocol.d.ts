69dfa670ef1f66ee0603d263e39d655dcefe3a31
Matt Bierner <matb@microsoft.com>
2021-01-04 17:55:10 -0800

    Fix compile error if using older ts version

--- extensions/typescript-language-features/src/protocol.d.ts
+++ extensions/typescript-language-features/src/protocol.d.ts
@@ -6,13 +6,13 @@ declare enum ServerType {
 	Semantic = 'semantic',
 }
 declare module 'typescript/lib/protocol' {
+
 	interface Response {
 		readonly _serverType?: ServerType;
 	}
 
 	interface FileReferencesRequest extends FileRequest {
-		command: CommandTypes.FileReferences;
-    }
+	}
 	interface FileReferencesResponseBody {
 		/**
 		 * The file locations referencing the symbol.
