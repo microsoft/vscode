2b20162d227c6e1da4339bba03de7b806bf4c712
Daniel Imms <daimms@microsoft.com>
2021-01-07 10:29:55 -0800

    Avoid hostname resolution in lsof
    This should make the call a little faster.
    
    Partof #78438

--- src/vs/workbench/contrib/terminal/node/terminalProcess.ts
+++ src/vs/workbench/contrib/terminal/node/terminalProcess.ts
@@ -335,7 +335,7 @@ export class TerminalProcess extends Disposable implements ITerminalChildProcess
 					return;
 				}
 				this._logService.trace('IPty#pid');
-				exec('lsof -OPl -p ' + this._ptyProcess.pid + ' | grep cwd', (error, stdout, stderr) => {
+				exec('lsof -OPln -p ' + this._ptyProcess.pid + ' | grep cwd', (error, stdout, stderr) => {
 					if (stdout !== '') {
 						resolve(stdout.substring(stdout.indexOf('/'), stdout.length - 1));
 					}
