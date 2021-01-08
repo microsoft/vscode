aa73c2d435d45dcee7f843164a2b7f8a0018e3d6
Rich Chiodo <rchiodo@users.noreply.github.com>
2021-01-07 17:41:28 -0800

    Fix random focus lost issues on CTRL+1/2 for a webview (#111676)
    * Fix random focus lost issues on CTRL+1/2 for a webview
    
    * Remove unnecessary null check
    
    * Better way to handle blurring (so multiple webviews continue to work)

--- src/vs/workbench/contrib/webview/electron-browser/webviewElement.ts
+++ src/vs/workbench/contrib/webview/electron-browser/webviewElement.ts
@@ -233,9 +233,11 @@ export class ElectronWebviewBasedWebview extends BaseWebview<WebviewTag> impleme
 			return;
 		}
 
-		// Clear the existing focus first.
+		// Clear the existing focus first if not already on the webview.
 		// This is required because the next part where we set the focus is async.
-		if (document.activeElement && document.activeElement instanceof HTMLElement) {
+		if (document.activeElement && document.activeElement instanceof HTMLElement && document.activeElement !== this.element) {
+			// Don't blur if on the webview because this will also happen async and may unset the focus
+			// after the focus trigger fires below.
 			document.activeElement.blur();
 		}
 
