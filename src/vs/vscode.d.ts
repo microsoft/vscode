e4dc7b4d7966789b3724a34649a071a87d53f9fa
0dinD <zerodind@gmail.com>
2021-01-08 10:15:23 +0100

    Clarify OpenDialogOptions note (#113831) (#113998)
    This PR fixes #113831, see that issue fore a more detailed description.

--- src/vs/vscode.d.ts
+++ src/vs/vscode.d.ts
@@ -1700,8 +1700,8 @@ declare module 'vscode' {
 	/**
 	 * Options to configure the behaviour of a file open dialog.
 	 *
-	 * * Note 1: A dialog can select files, folders, or both. This is not true for Windows
-	 * which enforces to open either files or folder, but *not both*.
+	 * * Note 1: On Windows and Linux, a file dialog cannot be both a file selector and a folder selector, so if you
+	 * set both `canSelectFiles` and `canSelectFolders` to `true` on these platforms, a folder selector will be shown.
 	 * * Note 2: Explicitly setting `canSelectFiles` and `canSelectFolders` to `false` is futile
 	 * and the editor then silently adjusts the options to select files.
 	 */
