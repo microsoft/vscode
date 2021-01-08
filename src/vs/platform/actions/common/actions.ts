a2251a3b6b9ee686b7700ae0e44ee028ea00a3cc
Johannes Rieken <johannes.rieken@gmail.com>
2021-01-08 16:45:24 +0100

    do not use mnemonicTitle for all action titles, #102361

--- src/vs/platform/actions/common/actions.ts
+++ src/vs/platform/actions/common/actions.ts
@@ -364,7 +364,7 @@ export class MenuItemAction extends ExecuteCommandAction {
 		@IContextKeyService contextKeyService: IContextKeyService,
 		@ICommandService commandService: ICommandService
 	) {
-		typeof item.title === 'string' ? super(item.id, item.title, commandService) : super(item.id, item.title.mnemonicTitle ?? item.title.value, commandService);
+		typeof item.title === 'string' ? super(item.id, item.title, commandService) : super(item.id, item.title.value, commandService);
 
 		this._cssClass = undefined;
 		this._enabled = !item.precondition || contextKeyService.contextMatchesRules(item.precondition);
