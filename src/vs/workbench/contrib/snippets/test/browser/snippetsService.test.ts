013578b6e724aed01d8ad0928233e10855fe3155
Tyler Leonhardt <me@tylerleonhardt.com>
2021-01-12 15:49:25 -0800

    Snippets and Auto-closing pairs play nicer

--- src/vs/workbench/contrib/snippets/test/browser/snippetsService.test.ts
+++ src/vs/workbench/contrib/snippets/test/browser/snippetsService.test.ts
@@ -13,6 +13,7 @@ import { ISnippetsService } from 'vs/workbench/contrib/snippets/browser/snippets
 import { Snippet, SnippetSource } from 'vs/workbench/contrib/snippets/browser/snippetsFile';
 import { LanguageConfigurationRegistry } from 'vs/editor/common/modes/languageConfigurationRegistry';
 import { CompletionContext, CompletionTriggerKind } from 'vs/editor/common/modes';
+import { IDisposable } from 'vs/base/common/lifecycle';
 
 class SimpleSnippetService implements ISnippetsService {
 	declare readonly _serviceBrand: undefined;
@@ -43,11 +44,13 @@ suite('SnippetsService', function () {
 		});
 	});
 
+	let disposables: Array<IDisposable>;
 	let modeService: ModeServiceImpl;
 	let snippetService: ISnippetsService;
 	let context: CompletionContext = { triggerKind: CompletionTriggerKind.Invoke };
 
 	setup(function () {
+		disposables = new Array<IDisposable>();
 		modeService = new ModeServiceImpl();
 		snippetService = new SimpleSnippetService([new Snippet(
 			['fooLang'],
@@ -68,6 +71,11 @@ suite('SnippetsService', function () {
 		)]);
 	});
 
+	teardown(function () {
+		for (const disposable of disposables) {
+			disposable.dispose();
+		}
+	});
 
 	test('snippet completions - simple', function () {
 
@@ -364,9 +372,10 @@ suite('SnippetsService', function () {
 	});
 
 	test('issue #61296: VS code freezes when editing CSS file with emoji', async function () {
-		let toDispose = LanguageConfigurationRegistry.register(modeService.getLanguageIdentifier('fooLang')!, {
+		disposables.push(LanguageConfigurationRegistry.register(modeService.getLanguageIdentifier('fooLang')!, {
 			wordPattern: /(#?-?\d*\.\d\w*%?)|(::?[\w-]*(?=[^,{;]*[,{]))|(([@#.!])?[\w-?]+%?|[@#!.])/g
-		});
+		}));
+
 		snippetService = new SimpleSnippetService([new Snippet(
 			['fooLang'],
 			'bug',
@@ -383,8 +392,6 @@ suite('SnippetsService', function () {
 		let result = await provider.provideCompletionItems(model, new Position(1, 8), context)!;
 
 		assert.equal(result.suggestions.length, 1);
-
-		toDispose.dispose();
 	});
 
 	test('No snippets shown when triggering completions at whitespace on line that already has text #62335', async function () {
@@ -501,5 +508,36 @@ suite('SnippetsService', function () {
 		let [first] = result.suggestions;
 		assert.equal((first.range as any).insert.endColumn, 9);
 		assert.equal((first.range as any).replace.endColumn, 9);
+	});
+
+	test('Snippet will replace auto-closing pair if specified in prefix', async function () {
+		disposables.push(LanguageConfigurationRegistry.register(modeService.getLanguageIdentifier('fooLang')!, {
+			brackets: [
+				['{', '}'],
+				['[', ']'],
+				['(', ')'],
+			]
+		}));
+
+		snippetService = new SimpleSnippetService([new Snippet(
+			['fooLang'],
+			'PSCustomObject',
+			'[PSCustomObject]',
+			'',
+			'[PSCustomObject] @{ Key = Value }',
+			'',
+			SnippetSource.User
+		)]);
+
+		const provider = new SnippetCompletionProvider(modeService, snippetService);
+
+		let model = createTextModel('[psc]', undefined, modeService.getLanguageIdentifier('fooLang'));
+		let result = await provider.provideCompletionItems(model, new Position(1, 5), context)!;
+
+		assert.strictEqual(result.suggestions.length, 1);
+		let [first] = result.suggestions;
+		assert.strictEqual((first.range as any).insert.endColumn, 5);
+		// This is 6 because it should eat the `]` at the end of the text even if cursor is before it
+		assert.strictEqual((first.range as any).replace.endColumn, 6);
 	});
 });
