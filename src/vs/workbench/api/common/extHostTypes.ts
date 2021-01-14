e32e353bfde647c1270e74e1a9fbcf6452a0ea07
rebornix <penn.lv@gmail.com>
2021-01-14 15:17:15 -0800

    fix #114171.

--- src/vs/workbench/api/common/extHostTypes.ts
+++ src/vs/workbench/api/common/extHostTypes.ts
@@ -2858,7 +2858,8 @@ export enum NotebookCellStatusBarAlignment {
 export enum NotebookEditorRevealType {
 	Default = 0,
 	InCenter = 1,
-	InCenterIfOutsideViewport = 2
+	InCenterIfOutsideViewport = 2,
+	AtTop = 3
 }
 
 
