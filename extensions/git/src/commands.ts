6d5ea584c12c1c52b7a8f41d8f0b5c66548a9d0a
susiwen8 <susiwen8@gmail.com>
2021-01-14 22:54:22 +0800

    chore: remove console

--- extensions/git/src/commands.ts
+++ extensions/git/src/commands.ts
@@ -1039,16 +1039,10 @@ export class CommandCenter {
 
 		const modifiedDocument = textEditor.document;
 		const selections = textEditor.selections;
-		console.log(selections);
 		const selectedChanges = changes.filter(change => {
 			const modifiedRange = getModifiedRange(modifiedDocument, change);
-			console.log('change: ', change);
-			console.log('modifiedRange: ', modifiedRange);
-			// console.log(change.modifiedEndLineNumber, selections[0].end.line, change.modifiedEndLineNumber, selections[0].start.line)
-			return change.modifiedEndLineNumber <= (selections[0].end.line + 1) && change.modifiedEndLineNumber >= (selections[0].start.line + 1);
-			// return selections.every(selection => !selection.intersection(modifiedRange));
+			return selections.every(selection => !selection.intersection(modifiedRange));
 		});
-		console.log(selectedChanges);
 
 		if (selectedChanges.length === changes.length) {
 			return;
