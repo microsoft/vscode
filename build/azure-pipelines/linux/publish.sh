6f9f6f806c195ec2de52383bb822f486e784a5d1
Ladislau Szomoru <3372902+lszomoru@users.noreply.github.com>
2021-01-15 13:00:03 +0100

    Azure DevOps pipeline artifacts (#114405)

--- build/azure-pipelines/linux/publish.sh
+++ build/azure-pipelines/linux/publish.sh
@@ -32,7 +32,7 @@ SERVER_BUILD_NAME="vscode-server-$PLATFORM_LINUX-web"
 SERVER_TARBALL_FILENAME="vscode-server-$PLATFORM_LINUX-web.tar.gz"
 SERVER_TARBALL_PATH="$ROOT/$SERVER_TARBALL_FILENAME"
 
-rm -rf $ROOT/vscode-server-*.tar.*
+rm -rf $ROOT/vscode-server-*-web.tar.*
 (cd $ROOT && mv $LEGACY_SERVER_BUILD_NAME $SERVER_BUILD_NAME && tar --owner=0 --group=0 -czf $SERVER_TARBALL_PATH $SERVER_BUILD_NAME)
 
 node build/azure-pipelines/common/createAsset.js "server-$PLATFORM_LINUX-web" archive-unsigned "$SERVER_TARBALL_FILENAME" "$SERVER_TARBALL_PATH"
@@ -69,3 +69,7 @@ mkdir -p $REPO/.build/linux/snap-tarball
 SNAP_TARBALL_PATH="$REPO/.build/linux/snap-tarball/snap-$VSCODE_ARCH.tar.gz"
 rm -rf $SNAP_TARBALL_PATH
 (cd .build/linux && tar -czf $SNAP_TARBALL_PATH snap)
+
+# Export DEB_PATH, RPM_PATH
+echo "##vso[task.setvariable variable=DEB_PATH]$DEB_PATH"
+echo "##vso[task.setvariable variable=RPM_PATH]$RPM_PATH"
