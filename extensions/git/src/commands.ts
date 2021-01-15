6763c2bf3aae1fa1e7a9a9384a8d5a835b9e104b
Eric Amodio <eamodio@microsoft.com>
2021-01-15 00:16:41 -0500

    Fixes #114384 - recheck resources after save/add

--- extensions/git/src/commands.ts
+++ extensions/git/src/commands.ts
@@ -1329,8 +1329,8 @@ export class CommandCenter {
 
 		const enableSmartCommit = config.get<boolean>('enableSmartCommit') === true;
 		const enableCommitSigning = config.get<boolean>('enableCommitSigning') === true;
-		const noStagedChanges = repository.indexGroup.resourceStates.length === 0;
-		const noUnstagedChanges = repository.workingTreeGroup.resourceStates.length === 0;
+		let noStagedChanges = repository.indexGroup.resourceStates.length === 0;
+		let noUnstagedChanges = repository.workingTreeGroup.resourceStates.length === 0;
 
 		if (promptToSaveFilesBeforeCommit !== 'never') {
 			let documents = workspace.textDocuments
@@ -1352,6 +1352,9 @@ export class CommandCenter {
 				if (pick === saveAndCommit) {
 					await Promise.all(documents.map(d => d.save()));
 					await repository.add(documents.map(d => d.uri));
+
+					noStagedChanges = repository.indexGroup.resourceStates.length === 0;
+					noUnstagedChanges = repository.workingTreeGroup.resourceStates.length === 0;
 				} else if (pick !== commit) {
 					return false; // do not commit on cancel
 				}
