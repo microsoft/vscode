4f04118e309fccded6a6e0ab875a8c2c298e67b1
Megan Rogge <megan.rogge@microsoft.com>
2021-01-15 11:46:17 -0800

    Update src/vs/workbench/contrib/terminal/common/terminalConfiguration.ts
    Co-authored-by: Daniel Imms <daimms@microsoft.com>

--- src/vs/workbench/contrib/terminal/common/terminalConfiguration.ts
+++ src/vs/workbench/contrib/terminal/common/terminalConfiguration.ts
@@ -91,7 +91,7 @@ export const terminalConfiguration: IConfigurationNode = {
 			default: false
 		},
 		'terminal.integrated.altClickMovesCursor': {
-			description: localize('terminal.integrated.altClickMovesCursor', "If enabled, alt/option + click will move the prompt cursor to position underneath the mouse."),
+			description: localize('terminal.integrated.altClickMovesCursor', "If enabled, alt/option + click will move the prompt cursor to position underneath the mouse. This may not work reliably depending on your shell."),
 			type: 'boolean',
 			default: true
 		},
