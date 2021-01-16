dc33b3986da226f269c4d65ae5d281514d3f61fe
susiwen8 <susiwen8@gmail.com>
2021-01-16 11:35:51 +0800

    Add return type

--- src/vs/platform/native/electron-main/nativeHostMainService.ts
+++ src/vs/platform/native/electron-main/nativeHostMainService.ts
@@ -123,7 +123,7 @@ export class NativeHostMainService extends Disposable implements INativeHostMain
 		return undefined;
 	}
 
-	async getWinFocus(windowId: number | undefined) {
+	async getWinFocus(windowId: number | undefined): Promise<boolean> {
 		// https://www.electronjs.org/docs/api/browser-window#winisfocused
 		return !!(this.windowById(windowId)?.win.isFocused());
 	}
