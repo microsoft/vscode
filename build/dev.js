2bb41a14025c361bdf42a2956a842c43824fd7a6
João Moreno <joao.moreno@microsoft.com>
2021-01-18 15:47:31 +0100

    dev script

--- build/dev.js
+++ build/dev.js
@@ -6,17 +6,9 @@
 const fs = require('fs').promises;
 const path = require('path');
 const cp = require('child_process');
-const product = require('../../product.json');
-const root = path.resolve(path.join(__dirname, '..', '..', '..'));
-
-async function exists(path) {
-	try {
-		await fs.stat(path);
-		return true;
-	} catch {
-		return false;
-	}
-}
+const product = require('../product.json');
+const root = path.resolve(path.join(__dirname, '..', '..'));
+const exists = (path) => fs.stat(path).then(() => true, () => false);
 
 async function exec(cmd, opts = {}) {
 	return new Promise((c, e) => {
@@ -30,14 +22,13 @@ async function cloneOrPull(ext) {
 	const folder = path.join(root, folderName);
 
 	if (!await exists(folder)) {
-		const url = `${ext.repo}.git`;
-		await exec(`git clone ${url}`, { cwd: root });
+		await exec(`git clone ${ext.repo}.git`, { cwd: root });
 	} else {
 		await exec(`git pull`, { cwd: folder });
 	}
 }
 
-async function main() {
+async function syncExtensions() {
 	for (const ext of product.builtInExtensions) {
 		console.log(`👉 ${ext.name}`);
 		await cloneOrPull(ext);
@@ -45,8 +36,14 @@ async function main() {
 }
 
 if (require.main === module) {
-	main().catch(err => {
-		console.error(err);
-		process.exit(1);
-	});
+	const { program } = require('commander');
+
+	program.version('0.0.1');
+
+	program
+		.command('sync-extensions')
+		.description('Clone or pull extension repositories alongside vscode')
+		.action(syncExtensions);
+
+	program.parseAsync(process.argv);
 }
