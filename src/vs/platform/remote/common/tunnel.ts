38c051bf86539be9b17b8acdc7eb913b7dcc4a7c
Alex Ross <alros@microsoft.com>
2021-01-20 17:26:16 +0100

    Catch errors in tunnel providers and log

--- src/vs/platform/remote/common/tunnel.ts
+++ src/vs/platform/remote/common/tunnel.ts
@@ -42,6 +42,24 @@ export interface ITunnelProvider {
 	forwardPort(tunnelOptions: TunnelOptions, tunnelCreationOptions: TunnelCreationOptions): Promise<RemoteTunnel | undefined> | undefined;
 }
 
+export interface ITunnel {
+	remoteAddress: { port: number, host: string };
+
+	/**
+	 * The complete local address(ex. localhost:1234)
+	 */
+	localAddress: string;
+
+	public?: boolean;
+
+	/**
+	 * Implementers of Tunnel should fire onDidDispose when dispose is called.
+	 */
+	onDidDispose: Event<void>;
+
+	dispose(): Promise<void> | void;
+}
+
 export interface ITunnelService {
 	readonly _serviceBrand: undefined;
 
