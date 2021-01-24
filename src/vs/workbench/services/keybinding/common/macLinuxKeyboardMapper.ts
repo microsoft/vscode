5ed3c977bdcce2f9380fd9cfaea94097329ef167
Sean Z <contact@seanzeng.com>
2021-01-25 03:07:06 +1300

    hack to make double shift possible in widget

--- src/vs/workbench/services/keybinding/common/macLinuxKeyboardMapper.ts
+++ src/vs/workbench/services/keybinding/common/macLinuxKeyboardMapper.ts
@@ -407,7 +407,7 @@ export class MacLinuxKeyboardMapper implements IKeyboardMapper {
 				_registerAllCombos(0, 0, 0, scanCode, keyCode);
 				this._scanCodeToLabel[scanCode] = KeyCodeUtils.toString(keyCode);
 
-				if (keyCode === KeyCode.Unknown || keyCode === KeyCode.Ctrl || keyCode === KeyCode.Meta || keyCode === KeyCode.Alt || keyCode === KeyCode.Shift) {
+				if (keyCode === KeyCode.Unknown || keyCode === KeyCode.Ctrl || keyCode === KeyCode.Meta || keyCode === KeyCode.Alt) {
 					this._scanCodeToDispatch[scanCode] = null; // cannot dispatch on this ScanCode
 				} else {
 					this._scanCodeToDispatch[scanCode] = `[${ScanCodeUtils.toString(scanCode)}]`;
