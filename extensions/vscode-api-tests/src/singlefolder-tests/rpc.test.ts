d441c69b08e3cf60a1b11e92a77713059cda2b03
Johannes Rieken <johannes.rieken@gmail.com>
2021-01-29 12:12:06 +0100

    mark rpc protocol and proxies with symbols and try to find them from an API test (with success...)

---
+++ extensions/vscode-api-tests/src/singlefolder-tests/rpc.test.ts
@@ -0,0 +1,53 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import * as assert from 'assert';
+import * as vscode from 'vscode';
+
+suite('vscode', function () {
+
+	test('rpc protocol, proxies not reachable', function () {
+
+		const symProxy = Symbol.for('rpcProxy');
+		const symProtocol = Symbol.for('rpcProtocol');
+
+		const proxyPaths: string[] = [];
+		const rpcPaths: string[] = [];
+
+		function walk(obj: any, path: string, seen: Set<any>) {
+			if (!obj) {
+				return;
+			}
+			if (typeof obj !== 'object' && typeof obj !== 'function') {
+				return;
+			}
+			if (seen.has(obj)) {
+				return;
+			}
+			seen.add(obj);
+
+			if (obj[symProtocol]) {
+				rpcPaths.push(`PROTOCOL via ${path}`);
+			}
+			if (obj[symProxy]) {
+				proxyPaths.push(`PROXY '${obj[symProxy]}' via ${path}`);
+			}
+
+			for (const key in obj) {
+				walk(obj[key], `${path}.${String(key)}`, seen);
+			}
+		}
+
+		try {
+			walk(vscode, 'vscode', new Set());
+		} catch (err) {
+			assert.fail(err);
+		}
+		assert.strictEqual(rpcPaths.length, 0);
+
+		// assert.strictEqual(proxyPaths.length, 0); // proxies are accessible...
+	});
+
+});
