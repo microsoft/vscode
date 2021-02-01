76962b2583ae71f5c2c051e521b1546c831c41eb
João Moreno <joao.moreno@microsoft.com>
2021-02-01 17:08:49 +0100

    ext: ci command

--- build/ext.js
+++ build/ext.js
@@ -23,39 +23,41 @@ var __asyncValues = (this && this.__asyncValues) || function (o) {
     function settle(resolve, reject, d, v) { Promise.resolve(v).then(function(v) { resolve({ value: v, done: d }); }, reject); }
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.getExtensions = void 0;
 const fs_1 = require("fs");
 const path = require("path");
 const cp = require("child_process");
 const commander_1 = require("commander");
-const root = path.resolve(path.join(__dirname, '..'));
+const storage_blob_1 = require("@azure/storage-blob");
+const mkdirp = require("mkdirp");
+const plimit = require("p-limit");
+const rootPath = path.resolve(path.join(__dirname, '..'));
+const vsixsPath = path.join(rootPath, '.build', 'vsix');
 var ExtensionType;
 (function (ExtensionType) {
     ExtensionType["Grammar"] = "grammar";
     ExtensionType["Theme"] = "theme";
     ExtensionType["Misc"] = "misc";
 })(ExtensionType || (ExtensionType = {}));
-// const exists = (path) => fs.stat(path).then(() => true, () => false);
-// const controlFilePath = path.join(os.homedir(), '.vscode-oss-dev', 'extensions', 'control.json');
-// async function readControlFile() {
-// 	try {
-// 		return JSON.parse(await fs.readFile(controlFilePath, 'utf8'));
-// 	} catch (err) {
-// 		return {};
-// 	}
-// }
-// async function writeControlFile(control) {
-// 	await mkdirp(path.dirname(controlFilePath));
-// 	await fs.writeFile(controlFilePath, JSON.stringify(control, null, '  '));
-// }
-async function exec(cmd, args, opts = {}) {
+async function spawn(cmd, argsOrOpts, opts = {}) {
     return new Promise((c, e) => {
-        const child = cp.spawn(cmd, args, Object.assign({ stdio: 'inherit', env: process.env }, opts));
+        const child = Array.isArray(argsOrOpts)
+            ? cp.spawn(cmd, argsOrOpts, Object.assign({ stdio: 'inherit', env: process.env }, opts))
+            : cp.spawn(cmd, Object.assign({ stdio: 'inherit', env: process.env }, argsOrOpts));
         child.on('close', code => code === 0 ? c() : e(`Returned ${code}`));
+    });
+}
+async function exec(cmd, opts = {}) {
+    return new Promise((c, e) => {
+        cp.exec(cmd, Object.assign({ env: process.env }, opts), (err, stdout) => err ? e(err) : c(opts.trim ? stdout.trim() : stdout));
     });
 }
 function getExtensionType(packageJson) {
     var _a, _b, _c;
-    if (((_a = packageJson.contributes) === null || _a === void 0 ? void 0 : _a.themes) || ((_b = packageJson.contributes) === null || _b === void 0 ? void 0 : _b.iconThemes)) {
+    if (packageJson.main) {
+        return "misc" /* Misc */;
+    }
+    else if (((_a = packageJson.contributes) === null || _a === void 0 ? void 0 : _a.themes) || ((_b = packageJson.contributes) === null || _b === void 0 ? void 0 : _b.iconThemes)) {
         return "theme" /* Theme */;
     }
     else if ((_c = packageJson.contributes) === null || _c === void 0 ? void 0 : _c.grammars) {
@@ -69,19 +71,27 @@ async function getExtension(extensionPath) {
     const packageJsonPath = path.join(extensionPath, 'package.json');
     const packageJson = JSON.parse(await fs_1.promises.readFile(packageJsonPath, 'utf8'));
     const type = getExtensionType(packageJson);
+    const { name, version } = packageJson;
+    const vsixName = `${name}-${version}.vsix`;
     return {
-        name: packageJson.name,
+        name,
+        version,
         path: extensionPath,
-        type
+        type,
+        vsixPath: path.join(vsixsPath, vsixName)
     };
 }
 function getExtensions() {
     return __asyncGenerator(this, arguments, function* getExtensions_1() {
-        const extensionsPath = path.join(root, 'extensions');
+        const extensionsPath = path.join(rootPath, 'extensions');
         const children = yield __await(fs_1.promises.readdir(extensionsPath));
         for (const child of children) {
             try {
-                yield yield __await(yield __await(getExtension(path.join(extensionsPath, child))));
+                const extension = yield __await(getExtension(path.join(extensionsPath, child)));
+                if (extension.type !== "theme" /* Theme */ && extension.type !== "grammar" /* Grammar */) {
+                    continue;
+                }
+                yield yield __await(extension);
             }
             catch (err) {
                 if (/ENOENT|ENOTDIR/.test(err.message)) {
@@ -92,6 +102,7 @@ function getExtensions() {
         }
     });
 }
+exports.getExtensions = getExtensions;
 async function each([cmd, ...args], opts) {
     var e_1, _a;
     try {
@@ -101,7 +112,7 @@ async function each([cmd, ...args], opts) {
                 continue;
             }
             console.log(`👉 ${extension.name}`);
-            await exec(cmd, args, { cwd: extension.path });
+            await spawn(cmd, args, { cwd: extension.path });
         }
     }
     catch (e_1_1) { e_1 = { error: e_1_1 }; }
@@ -112,6 +123,61 @@ async function each([cmd, ...args], opts) {
         finally { if (e_1) throw e_1.error; }
     }
 }
+async function runExtensionCI(extension, opts) {
+    const vsixName = `${extension.name}-${extension.version}.vsix`;
+    const commit = await exec(`git log -1 --format="%H" -- ${extension.path}`, { trim: true });
+    const creds = new storage_blob_1.StorageSharedKeyCredential(opts.account, opts.key);
+    const service = new storage_blob_1.BlobServiceClient(`https://${opts.account}.blob.core.windows.net`, creds);
+    const container = service.getContainerClient('extensions');
+    const blobName = `${commit}/${vsixName}`;
+    const blob = container.getBlobClient(blobName);
+    try {
+        await blob.downloadToFile(extension.vsixPath);
+        console.log(`📦 [${extension.name}] Downloaded from cache (${blobName})`);
+        return;
+    }
+    catch (err) {
+        if (err.statusCode !== 404) {
+            throw err;
+        }
+    }
+    console.log(`📦 [${extension.name}] Cache miss (${blobName})`);
+    console.log(`📦 [${extension.name}] Building...`);
+    await spawn(`yarn`, { shell: true, cwd: extension.path });
+    await spawn(`vsce package --yarn -o ${vsixsPath}`, { shell: true, cwd: extension.path });
+    const blockBlob = await blob.getBlockBlobClient();
+    await blockBlob.uploadFile(extension.vsixPath);
+}
+async function ci() {
+    var e_2, _a;
+    const { 'AZURE_STORAGE_ACCOUNT_2': account, 'AZURE_STORAGE_KEY_2': key } = process.env;
+    if (!account) {
+        throw new Error('Missing env: AZURE_STORAGE_ACCOUNT_2');
+    }
+    else if (!key) {
+        throw new Error('Missing env: AZURE_STORAGE_KEY_2');
+    }
+    await mkdirp(vsixsPath);
+    const limit = plimit(10);
+    const promises = [];
+    try {
+        for (var _b = __asyncValues(getExtensions()), _c; _c = await _b.next(), !_c.done;) {
+            const extension = _c.value;
+            if (extension.type !== "theme" /* Theme */ && extension.type !== "grammar" /* Grammar */) {
+                continue;
+            }
+            promises.push(limit(() => runExtensionCI(extension, { account, key })));
+        }
+    }
+    catch (e_2_1) { e_2 = { error: e_2_1 }; }
+    finally {
+        try {
+            if (_c && !_c.done && (_a = _b.return)) await _a.call(_b);
+        }
+        finally { if (e_2) throw e_2.error; }
+    }
+    await Promise.all(promises);
+}
 if (require.main === module) {
     commander_1.program.version('0.0.1');
     commander_1.program
@@ -120,6 +186,10 @@ if (require.main === module) {
         .description('Run a command in each extension repository')
         .allowUnknownOption()
         .action(each);
+    commander_1.program
+        .command('ci')
+        .description('Run CI build steps for extensions')
+        .action(ci);
     commander_1.program.parseAsync(process.argv).catch(err => {
         console.error(err);
         process.exit(1);
--- build/ext.ts
+++ build/ext.ts
@@ -7,8 +7,12 @@ import { promises as fs } from 'fs';
 import * as path from 'path';
 import * as cp from 'child_process';
 import { program } from 'commander';
+import { BlobServiceClient, StorageSharedKeyCredential } from '@azure/storage-blob';
+import * as mkdirp from 'mkdirp';
+import * as plimit from 'p-limit';
 
-const root = path.resolve(path.join(__dirname, '..'));
+const rootPath = path.resolve(path.join(__dirname, '..'));
+const vsixsPath = path.join(rootPath, '.build', 'vsix');
 
 const enum ExtensionType {
 	Grammar = 'grammar',
@@ -18,8 +22,10 @@ const enum ExtensionType {
 
 interface IExtension {
 	readonly name: string;
+	readonly version: string;
 	readonly path: string;
 	readonly type: ExtensionType;
+	readonly vsixPath: string;
 }
 
 // const exists = (path) => fs.stat(path).then(() => true, () => false);
@@ -39,15 +45,28 @@ interface IExtension {
 // 	await fs.writeFile(controlFilePath, JSON.stringify(control, null, '  '));
 // }
 
-async function exec(cmd: string, args: string[], opts: cp.SpawnOptions = {}): Promise<void> {
+async function spawn(cmd: string, opts?: cp.SpawnOptions): Promise<void>;
+async function spawn(cmd: string, args: string[], opts?: cp.SpawnOptions): Promise<void>;
+async function spawn(cmd: string, argsOrOpts?: cp.SpawnOptions | string[], opts: cp.SpawnOptions = {}): Promise<void> {
 	return new Promise((c, e) => {
-		const child = cp.spawn(cmd, args, { stdio: 'inherit', env: process.env, ...opts });
+		const child = Array.isArray(argsOrOpts)
+			? cp.spawn(cmd, argsOrOpts, { stdio: 'inherit', env: process.env, ...opts })
+			: cp.spawn(cmd, { stdio: 'inherit', env: process.env, ...argsOrOpts });
+
 		child.on('close', code => code === 0 ? c() : e(`Returned ${code}`));
 	});
 }
 
+async function exec(cmd: string, opts: (cp.ExecOptions & { trim?: boolean }) = {}): Promise<string> {
+	return new Promise((c, e) => {
+		cp.exec(cmd, { env: process.env, ...opts }, (err, stdout) => err ? e(err) : c(opts.trim ? stdout.trim() : stdout));
+	});
+}
+
 function getExtensionType(packageJson: any): ExtensionType {
-	if (packageJson.contributes?.themes || packageJson.contributes?.iconThemes) {
+	if (packageJson.main) {
+		return ExtensionType.Misc;
+	} else if (packageJson.contributes?.themes || packageJson.contributes?.iconThemes) {
 		return ExtensionType.Theme;
 	} else if (packageJson.contributes?.grammars) {
 		return ExtensionType.Grammar;
@@ -60,21 +79,31 @@ async function getExtension(extensionPath: string): Promise<IExtension> {
 	const packageJsonPath = path.join(extensionPath, 'package.json');
 	const packageJson = JSON.parse(await fs.readFile(packageJsonPath, 'utf8'));
 	const type = getExtensionType(packageJson);
+	const { name, version } = packageJson;
+	const vsixName = `${name}-${version}.vsix`;
 
 	return {
-		name: packageJson.name,
+		name,
+		version,
 		path: extensionPath,
-		type
+		type,
+		vsixPath: path.join(vsixsPath, vsixName)
 	};
 }
 
-async function* getExtensions(): AsyncGenerator<IExtension, void, any> {
-	const extensionsPath = path.join(root, 'extensions');
+export async function* getExtensions(): AsyncGenerator<IExtension, void, any> {
+	const extensionsPath = path.join(rootPath, 'extensions');
 	const children = await fs.readdir(extensionsPath);
 
 	for (const child of children) {
 		try {
-			yield await getExtension(path.join(extensionsPath, child));
+			const extension = await getExtension(path.join(extensionsPath, child));
+
+			if (extension.type !== ExtensionType.Theme && extension.type !== ExtensionType.Grammar) {
+				continue;
+			}
+
+			yield extension;
 		} catch (err) {
 			if (/ENOENT|ENOTDIR/.test(err.message)) {
 				continue;
@@ -92,8 +121,61 @@ async function each([cmd, ...args]: string[], opts: { type?: string }) {
 		}
 
 		console.log(`👉 ${extension.name}`);
-		await exec(cmd, args, { cwd: extension.path });
+		await spawn(cmd, args, { cwd: extension.path });
 	}
+}
+
+async function runExtensionCI(extension: IExtension, opts: { account: string, key: string }): Promise<void> {
+	const vsixName = `${extension.name}-${extension.version}.vsix`;
+	const commit = await exec(`git log -1 --format="%H" -- ${extension.path}`, { trim: true });
+	const creds = new StorageSharedKeyCredential(opts.account, opts.key);
+	const service = new BlobServiceClient(`https://${opts.account}.blob.core.windows.net`, creds);
+	const container = service.getContainerClient('extensions');
+	const blobName = `${commit}/${vsixName}`;
+	const blob = container.getBlobClient(blobName);
+
+	try {
+		await blob.downloadToFile(extension.vsixPath);
+		console.log(`📦 [${extension.name}] Downloaded from cache (${blobName})`);
+		return;
+	} catch (err) {
+		if (err.statusCode !== 404) {
+			throw err;
+		}
+	}
+
+	console.log(`📦 [${extension.name}] Cache miss (${blobName})`);
+	console.log(`📦 [${extension.name}] Building...`);
+	await spawn(`yarn`, { shell: true, cwd: extension.path });
+	await spawn(`vsce package --yarn -o ${vsixsPath}`, { shell: true, cwd: extension.path });
+
+	const blockBlob = await blob.getBlockBlobClient();
+	await blockBlob.uploadFile(extension.vsixPath);
+}
+
+async function ci(): Promise<void> {
+	const { 'AZURE_STORAGE_ACCOUNT_2': account, 'AZURE_STORAGE_KEY_2': key } = process.env;
+
+	if (!account) {
+		throw new Error('Missing env: AZURE_STORAGE_ACCOUNT_2');
+	} else if (!key) {
+		throw new Error('Missing env: AZURE_STORAGE_KEY_2');
+	}
+
+	await mkdirp(vsixsPath);
+
+	const limit = plimit(10);
+	const promises = [];
+
+	for await (const extension of getExtensions()) {
+		if (extension.type !== ExtensionType.Theme && extension.type !== ExtensionType.Grammar) {
+			continue;
+		}
+
+		promises.push(limit(() => runExtensionCI(extension, { account, key })));
+	}
+
+	await Promise.all(promises);
 }
 
 if (require.main === module) {
@@ -105,6 +187,11 @@ if (require.main === module) {
 		.description('Run a command in each extension repository')
 		.allowUnknownOption()
 		.action(each);
+
+	program
+		.command('ci')
+		.description('Run CI build steps for extensions')
+		.action(ci);
 
 	program.parseAsync(process.argv).catch(err => {
 		console.error(err);
