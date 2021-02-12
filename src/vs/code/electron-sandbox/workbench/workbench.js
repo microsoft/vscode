b84ed1f6dc4182422d8366ad46cd0318a55759ab
Benjamin Pasero <benjpas@microsoft.com>
2021-02-12 14:03:48 +0100

    isolate scripts with self invoking function

--- src/vs/code/electron-sandbox/workbench/workbench.js
+++ src/vs/code/electron-sandbox/workbench/workbench.js
@@ -5,10 +5,10 @@
 
 /// <reference path="../../../../typings/require.d.ts" />
 
-//@ts-check
-'use strict';
-
+// @ts-check
 (function () {
+	'use strict';
+
 	const bootstrapWindow = bootstrapWindowLib();
 
 	// Add a perf entry right from the top
