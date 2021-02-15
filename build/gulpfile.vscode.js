4d89ae10ff8b1de9342803d8ee088d1cef4f6438
João Moreno <joao.moreno@microsoft.com>
2021-02-15 12:57:02 +0100

    fix #116523

--- build/gulpfile.vscode.js
+++ build/gulpfile.vscode.js
@@ -284,6 +284,7 @@ function packageTask(platform, arch, sourceFolderName, destinationFolderName, op
 		let result = all
 			.pipe(util.skipDirectories())
 			.pipe(util.fixWin32DirectoryPermissions())
+			.pipe(filter(['**', '!**/.github/**'], { dot: true })) // https://github.com/microsoft/vscode/issues/116523
 			.pipe(electron(_.extend({}, config, { platform, arch: arch === 'armhf' ? 'arm' : arch, ffmpegChromium: true })))
 			.pipe(filter(['**', '!LICENSE', '!LICENSES.chromium.html', '!version'], { dot: true }));
 
