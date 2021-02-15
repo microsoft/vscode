ac5b7117ec410194814ff92be79254ebc4389dfe
Johannes Rieken <johannes.rieken@gmail.com>
2021-02-15 12:52:54 +0100

    assert no rpc in notebook doc test

--- extensions/vscode-api-tests/src/singlefolder-tests/notebook.document.test.ts
+++ extensions/vscode-api-tests/src/singlefolder-tests/notebook.document.test.ts
@@ -33,6 +33,7 @@ suite('Notebook Document', function () {
 	const disposables: vscode.Disposable[] = [];
 
 	suiteTeardown(async function () {
+		utils.assertNoRpc();
 		await utils.revertAllDirty();
 		await utils.closeAllEditors();
 		utils.disposeAll(disposables);
