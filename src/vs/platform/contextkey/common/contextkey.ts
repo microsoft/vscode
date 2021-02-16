8aa800e459adc6f56610df90b1bcff30e03acb01
Johannes Rieken <johannes.rieken@gmail.com>
2021-02-16 16:15:47 +0100

    allow to exclude a context key from registry, allow to provide type eplicitly, https://github.com/microsoft/vscode/issues/114867

--- src/vs/platform/contextkey/common/contextkey.ts
+++ src/vs/platform/contextkey/common/contextkey.ts
@@ -1273,12 +1273,16 @@ export class RawContextKey<T> extends ContextKeyDefinedExpr {
 
 	private readonly _defaultValue: T | undefined;
 
-	constructor(readonly key: string, defaultValue: T | undefined, description?: string) {
+	constructor(readonly key: string, defaultValue: T | undefined, metaOrHide?: string | true | { type: string, description: string }) {
 		super(key);
 		this._defaultValue = defaultValue;
 
 		// collect all context keys into a central place
-		RawContextKey._info.push({ key, description, type: typeof defaultValue });
+		if (typeof metaOrHide === 'object') {
+			RawContextKey._info.push({ ...metaOrHide, key });
+		} else if (metaOrHide !== true) {
+			RawContextKey._info.push({ key, description: metaOrHide, type: typeof defaultValue });
+		}
 	}
 
 	public bindTo(target: IContextKeyService): IContextKey<T> {
