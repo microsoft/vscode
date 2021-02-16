3c0c0ab25827931cfd8028fc8a075e3c1ca4ebb9
deepak1556 <hop2deep@gmail.com>
2021-02-15 20:35:01 -0800

    ci: [mac] Unify tasks between different flavors

--- build/azure-pipelines/darwin/publish-server.sh
+++ build/azure-pipelines/darwin/publish-server.sh
@@ -1,20 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-# Publish DEB
-case $VSCODE_ARCH in
-	x64) ASSET_ID="darwin" ;;
-	arm64) ASSET_ID="darwin-arm64" ;;
-	universal) ASSET_ID="darwin-universal" ;;
-esac
-
-# publish the build
-node build/azure-pipelines/common/createAsset.js \
-	"$ASSET_ID" \
-	archive \
-	"VSCode-$ASSET_ID.zip" \
-	../VSCode-darwin-$VSCODE_ARCH.zip
-
 if [ "$VSCODE_ARCH" == "x64" ]; then
 	# package Remote Extension Host
 	pushd .. && mv vscode-reh-darwin vscode-server-darwin && zip -Xry vscode-server-darwin.zip vscode-server-darwin && popd
