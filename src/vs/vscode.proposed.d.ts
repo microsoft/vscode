072ec46dcfa042acfae86e26f08fd8711fd55d0b
SteVen Batten <6561887+sbatten@users.noreply.github.com>
2021-02-22 08:20:31 -0800

    new install proprosed api (#117059)
    * new install proprosed api
    
    * address comments

--- src/vs/vscode.proposed.d.ts
+++ src/vs/vscode.proposed.d.ts
@@ -2166,6 +2166,11 @@ declare module 'vscode' {
 
 	export interface ExtensionContext {
 		readonly extensionRuntime: ExtensionRuntime;
+
+		/**
+		 * Indicates that this is a fresh install of VS Code.
+		 */
+		readonly isNewInstall: boolean;
 	}
 
 	//#endregion
