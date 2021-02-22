d2a6dea32364aafce5c7c75007ba7659cb25bfac
Daniel Imms <tyriar@tyriar.com>
2021-02-22 03:52:25 -0800

    Disable conpty in integration tests again
    Fixes #117028

--- extensions/vscode-api-tests/src/singlefolder-tests/terminal.test.ts
+++ extensions/vscode-api-tests/src/singlefolder-tests/terminal.test.ts
@@ -19,6 +19,8 @@ import { assertNoRpc } from '../utils';
 		extensionContext = (global as any).testExtensionContext;
 
 		const config = workspace.getConfiguration('terminal.integrated');
+		// Disable conpty because of the hang issue https://github.com/microsoft/vscode/issues/71966
+		await config.update('windowsEnableConpty', false, ConfigurationTarget.Global);
 		// Disable exit alerts as tests may trigger then and we're not testing the notifications
 		await config.update('showExitAlert', false, ConfigurationTarget.Global);
 		// Canvas may cause problems when running in a container
@@ -637,10 +639,7 @@ import { assertNoRpc } from '../utils';
 					'~c2~c1'
 				];
 				disposables.push(window.onDidWriteTerminalData(e => {
-					try {
-						equal(terminal, e.terminal);
-					} catch (e) {
-						done(e);
+					if (terminal !== e.terminal) {
 						return;
 					}
 					// Multiple expected could show up in the same data event
