764e90d763ff1c06e2e4ae66918702d370786a37
Megan Rogge <merogge@microsoft.com>
2021-03-01 10:02:11 -0800

    fix #117649

--- src/vs/workbench/contrib/terminal/browser/terminalProcessManager.ts
+++ src/vs/workbench/contrib/terminal/browser/terminalProcessManager.ts
@@ -263,7 +263,7 @@ export class TerminalProcessManager extends Disposable implements ITerminalProce
 		const variableResolver = terminalEnvironment.createVariableResolver(lastActiveWorkspace, this._configurationResolverService);
 		const env = terminalEnvironment.createTerminalEnvironment(shellLaunchConfig, envFromConfigValue, variableResolver, isWorkspaceShellAllowed, this._productService.version, this._configHelper.config.detectLocale, baseEnv);
 
-		if (!shellLaunchConfig.strictEnv && !shellLaunchConfig.hideFromUser) {
+		if (!shellLaunchConfig.strictEnv && !shellLaunchConfig.hideFromUser && !shellLaunchConfig.isExtensionTerminal) {
 			this._extEnvironmentVariableCollection = this._environmentVariableService.mergedCollection;
 			this._register(this._environmentVariableService.onDidChangeCollections(newCollection => this._onEnvironmentVariableCollectionChange(newCollection)));
 			// For remote terminals, this is a copy of the mergedEnvironmentCollection created on
