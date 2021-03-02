bb6e95dde78cc65a7d18f71d4cb5605c8c609adf
Hsuan-An Weng Lin <lancewenglin@gmail.com>
2021-03-01 22:46:25 -0600

    Emmet: add migration function for old config

--- extensions/emmet/src/util.ts
+++ extensions/emmet/src/util.ts
@@ -50,6 +50,28 @@ export function updateEmmetExtensionsPath(forceRefresh: boolean = false) {
 }
 
 /**
+ * Migrate old configuration(string) for extensionsPath to new type(string[])
+ * https://github.com/microsoft/vscode/issues/117517
+ */
+export function migrateEmmetExtensionsPath() {
+	// Get the detail info of emmet.extensionsPath setting
+	let config = vscode.workspace.getConfiguration().inspect('emmet.extensionsPath');
+
+	// Update Global setting if the value type is string
+	if (typeof config?.globalValue === 'string') {
+		vscode.workspace.getConfiguration().update('emmet.extensionsPath', [config.globalValue], true);
+	}
+	// Update Workspace setting if the value type is string
+	if (typeof config?.workspaceValue === 'string') {
+		vscode.workspace.getConfiguration().update('emmet.extensionsPath', [config.workspaceValue], false);
+	}
+	// Update WorkspaceFolder setting if the value type is string
+	if (typeof config?.workspaceFolderValue === 'string') {
+		vscode.workspace.getConfiguration().update('emmet.extensionsPath', [config.workspaceFolderValue]);
+	}
+}
+
+/**
  * Mapping between languages that support Emmet and completion trigger characters
  */
 export const LANGUAGE_MODES: { [id: string]: string[] } = {
