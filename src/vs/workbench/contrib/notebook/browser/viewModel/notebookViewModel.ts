58bd8e075a3a5e80719da76ff256352c4bc91e74
Matt Bierner <matb@microsoft.com>
2021-03-09 12:27:43 -0800

    Fix spelling in comment

--- src/vs/workbench/contrib/notebook/browser/viewModel/notebookViewModel.ts
+++ src/vs/workbench/contrib/notebook/browser/viewModel/notebookViewModel.ts
@@ -249,7 +249,7 @@ export class NotebookViewModel extends Disposable implements EditorFoldingStateD
 				this._decorationsTree.acceptReplace(diff[0], diff[1], diff[2].length, true);
 				deletedCells.forEach(cell => {
 					this._handleToViewCellMapping.delete(cell.handle);
-					// dispsoe the cell to release ref to the cell text document
+					// dispose the cell to release ref to the cell text document
 					cell.dispose();
 				});
 
