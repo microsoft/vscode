25d779d7b080c77ef12e608745a0f7b07b1525a9
meganrogge <megrogge@gmail.com>
2021-03-17 14:49:15 -0700

    Revert "Revert "Make sure katex styles are shipped under root of""
    This reverts commit 14d72aab9c9c6948546a862f4c5a63c42b938bc8.

--- extensions/notebook-markdown-extensions/webpack.notebook.js
+++ extensions/notebook-markdown-extensions/webpack.notebook.js
@@ -3,6 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
 	entry: {
@@ -23,5 +24,20 @@ module.exports = {
 	output: {
 		filename: '[name].js',
 		path: path.resolve(__dirname, 'notebook-out')
-	}
+	},
+	plugins: [
+		// @ts-ignore
+		new CopyPlugin({
+			patterns: [
+				{
+					from: './node_modules/katex/dist/katex.min.css',
+					to: 'katex.min.css'
+				},
+				{
+					from: './node_modules/katex/dist/fonts',
+					to: 'fonts/'
+				},
+			],
+		}),
+	]
 };
