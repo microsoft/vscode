17c1ebd3f58baefa67a76240bec4577d55f273e5
Anton Kosyakov <anton@gitpod.io>
2021-03-18 14:51:05 +0000

    [workbench] align default theme with initial

--- src/vs/code/browser/workbench/workbench.ts
+++ src/vs/code/browser/workbench/workbench.ts
@@ -423,7 +423,31 @@ async function doStart(): Promise<IDisposable> {
 			tooltip: 'Editing on Gitpod'
 		},
 		initialColorTheme: {
-			themeType: ColorScheme.DARK
+			themeType: ColorScheme.DARK,
+			colors: {
+				// should be aligned with extensions/theme-defaults/themes/dark_vs.json
+				'editor.background': '#1E1E1E',
+				'editor.foreground': '#D4D4D4',
+				'editor.inactiveSelectionBackground': '#3A3D41',
+				'editorIndentGuide.background': '#404040',
+				'editorIndentGuide.activeBackground': '#707070',
+				'editor.selectionHighlightBackground': '#ADD6FF26',
+				'list.dropBackground': '#383B3D',
+				'activityBarBadge.background': '#007ACC',
+				'sideBarTitle.foreground': '#BBBBBB',
+				'input.placeholderForeground': '#A6A6A6',
+				'menu.background': '#252526',
+				'menu.foreground': '#CCCCCC',
+				'statusBarItem.remoteForeground': '#FFF',
+				'statusBarItem.remoteBackground': '#16825D',
+				'sideBarSectionHeader.background': '#0000',
+				'sideBarSectionHeader.border': '#ccc3',
+				'tab.lastPinnedBorder': '#ccc3',
+				'notebook.selectedCellBackground': '#383B3D50',
+			}
+		},
+		configurationDefaults: {
+			'workbench.colorTheme': 'Visual Studio Dark',
 		},
 		logLevel: logLevel ? parseLogLevel(logLevel) : undefined,
 		credentialsProvider,
