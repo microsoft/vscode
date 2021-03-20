e7aedd975c8e728d5b7642d18a5d21e19ca5c03e
Miguel Solorio <miguel.solorio@microsoft.com>
2021-03-19 19:04:01 -0700

    Adjust notebook cell multi-select colors

--- src/vs/workbench/contrib/notebook/browser/notebookEditorWidget.ts
+++ src/vs/workbench/contrib/notebook/browser/notebookEditorWidget.ts
@@ -2330,8 +2330,8 @@ export const focusedCellBackground = registerColor('notebook.focusedCellBackgrou
 }, nls.localize('focusedCellBackground', "The background color of a cell when the cell is focused."));
 
 export const selectedCellBackground = registerColor('notebook.selectedCellBackground', {
-	dark: Color.fromHex('#383B3D').transparent(0.5),
-	light: Color.fromHex('#c8ddf1').transparent(0.5),
+	dark: listInactiveSelectionBackground,
+	light: listInactiveSelectionBackground,
 	hc: null
 }, nls.localize('selectedCellBackground', "The background color of a cell when the cell is selected."));
 
