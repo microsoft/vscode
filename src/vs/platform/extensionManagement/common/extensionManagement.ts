d909932d2405d1a22c94a3af04834fa9e2af2186
Sandeep Somavarapu <sasomava@microsoft.com>
2021-03-22 17:43:55 +0100

    Fix #118104

--- src/vs/platform/extensionManagement/common/extensionManagement.ts
+++ src/vs/platform/extensionManagement/common/extensionManagement.ts
@@ -192,7 +192,6 @@ export interface DidUninstallExtensionEvent {
 	error?: string;
 }
 
-export const INSTALL_ERROR_NOT_SUPPORTED = 'notsupported';
 export const INSTALL_ERROR_MALICIOUS = 'malicious';
 export const INSTALL_ERROR_INCOMPATIBLE = 'incompatible';
 
