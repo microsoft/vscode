cca8a48b17a6da4628fbb33d8db83a60eace38df
Rob Lourens <roblourens@gmail.com>
2021-03-22 21:30:13 -0700

    Restore notebook smoketests,  Fix #113882

--- test/smoke/src/areas/notebook/notebook.test.ts
+++ test/smoke/src/areas/notebook/notebook.test.ts
@@ -54,7 +54,7 @@ export function setup() {
 		// 	await app.workbench.notebook.waitForMarkdownContents('p', 'Markdown Cell');
 		// });
 
-		it.skip('moves focus in and out of output', async function () { // TODO@rebornix https://github.com/microsoft/vscode/issues/113882
+		it('moves focus in and out of output', async function () { // TODO@rebornix https://github.com/microsoft/vscode/issues/113882
 			const app = this.app as Application;
 			await app.workbench.notebook.openNotebook();
 			await app.workbench.notebook.executeActiveCell();
@@ -63,11 +63,11 @@ export function setup() {
 			await app.workbench.notebook.waitForActiveCellEditorContents('code()');
 		});
 
-		it.skip('cell action execution', async function () {
+		it('cell action execution', async function () {
 			const app = this.app as Application;
 			await app.workbench.notebook.openNotebook();
 			await app.workbench.notebook.insertNotebookCell('code');
-			await app.workbench.notebook.executeCellAction('.notebook-editor .monaco-list-row.focused div.monaco-toolbar .codicon-notebook-execute');
+			await app.workbench.notebook.executeCellAction('.notebook-editor .monaco-list-row.focused div.monaco-toolbar .codicon-debug');
 			await app.workbench.notebook.waitForActiveCellEditorContents('test');
 		});
 	});
