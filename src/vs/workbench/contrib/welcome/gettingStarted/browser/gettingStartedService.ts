5c6836eb44d676cf7a80df0ba2f1c92c163f21af
Jackson Kearl <jakearl@microsoft.com>
2021-03-24 15:28:32 -0700

    Fix #119699

--- src/vs/workbench/contrib/welcome/gettingStarted/browser/gettingStartedService.ts
+++ src/vs/workbench/contrib/welcome/gettingStarted/browser/gettingStartedService.ts
@@ -540,11 +540,16 @@ registerAction2(class extends Action2 {
 	}
 
 	run(accessor: ServicesAccessor) {
+		const gettingStartedService = accessor.get(IGettingStartedService);
 		const memento = new Memento('gettingStartedService', accessor.get(IStorageService));
 		const record = memento.getMemento(StorageScope.GLOBAL, StorageTarget.USER);
 		for (const key in record) {
 			if (Object.prototype.hasOwnProperty.call(record, key)) {
-				delete record[key];
+				try {
+					gettingStartedService.deprogressTask(key);
+				} catch (e) {
+					console.error(e);
+				}
 			}
 		}
 		memento.saveMemento();
