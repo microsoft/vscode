c4ceba7cb7b1763a9a73e3d0dcab2a87868733c1
Matt Bierner <matb@microsoft.com>
2021-03-31 15:30:49 -0700

    Add events for when the active webview changes
    This attempts to move the custom code we have in `mainThreadWebviewPanels` to the normal webview services

--- src/vs/workbench/electron-sandbox/sandbox.simpleservices.ts
+++ src/vs/workbench/electron-sandbox/sandbox.simpleservices.ts
@@ -279,6 +279,7 @@ class SimpleWebviewService implements IWebviewService {
 	declare readonly _serviceBrand: undefined;
 
 	readonly activeWebview = undefined;
+	readonly onDidChangeActiveWebview = Event.None;
 
 	createWebviewElement(id: string, options: WebviewOptions, contentOptions: WebviewContentOptions, extension: WebviewExtensionDescription | undefined): WebviewElement { throw new Error('Method not implemented.'); }
 	createWebviewOverlay(id: string, options: WebviewOptions, contentOptions: WebviewContentOptions, extension: WebviewExtensionDescription | undefined): WebviewOverlay { throw new Error('Method not implemented.'); }
