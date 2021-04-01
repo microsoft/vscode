microsoft/vscode

commit df6d78a1695bdf9a1d4984231242ff1fd65247bb

debt - align editor model closer to file working copy

src/vs/workbench/contrib/preferences/browser/preferencesEditor.ts
@@ -1033,7 +1033,7 @@ export class DefaultPreferencesEditor extends BaseTextEditor {
 						return undefined;
 					}
 
-					return editorModel!.load();
+					return editorModel!.resolve();
 				})
 				.then(editorModel => {
 					if (token.isCancellationRequested) {

src/vs/editor/standalone/browser/simpleServices.ts
@@ -62,8 +62,8 @@ export class SimpleModel implements IResolvedTextEditorModel {
 		return this._onWillDispose.event;
 	}
 
-	public load(): Promise<SimpleModel> {
-		return Promise.resolve(this);
+	public resolve(): Promise<void> {
+		return Promise.resolve();
 	}
 
 	public get textEditorModel(): ITextModel {

src/vs/platform/editor/common/editor.ts
@@ -14,9 +14,14 @@ export interface IEditorModel {
 	readonly onWillDispose: Event<void>;
 
 	/**
-	 * Loads the model.
+	 * Resolves the model.
 	 */
-	load(): Promise<IEditorModel>;
+	resolve(): Promise<void>;
+
+	/**
+	 * Find out if the editor model was resolved or not.
+	 */
+	isResolved(): boolean;
 
 	/**
 	 * Find out if this model has been disposed.

src/vs/workbench/common/editor.ts
@@ -868,8 +868,8 @@ export interface ITextEditorModel extends IEditorModel {
 
 /**
  * The editor model is the heavyweight counterpart of editor input. Depending on the editor input, it
- * connects to the disk to retrieve content and may allow for saving it back or reverting it. Editor models
- * are typically cached for some while because they are expensive to construct.
+ * resolves from a file system retrieve content and may allow for saving it back or reverting it.
+ * Editor models are typically cached for some while because they are expensive to construct.
  */
 export class EditorModel extends Disposable implements IEditorModel {
 
@@ -877,19 +877,20 @@ export class EditorModel extends Disposable implements IEditorModel {
 	readonly onWillDispose = this._onWillDispose.event;
 
 	private disposed = false;
+	private resolved = false;
 
 	/**
-	 * Causes this model to load returning a promise when loading is completed.
+	 * Causes this model to resolve returning a promise when loading is completed.
 	 */
-	async load(): Promise<IEditorModel> {
-		return this;
+	async resolve(): Promise<void> {
+		this.resolved = true;
 	}
 
 	/**
 	 * Returns whether this model was loaded or not.
 	 */
 	isResolved(): boolean {
-		return true;
+		return this.resolved;
 	}
 
 	/**

src/vs/workbench/common/editor/binaryEditorModel.ts
@@ -56,7 +56,7 @@ export class BinaryEditorModel extends EditorModel {
 		return this.etag;
 	}
 
-	async load(): Promise<BinaryEditorModel> {
+	async resolve(): Promise<void> {
 
 		// Make sure to resolve up to date stat for file resources
 		if (this.fileService.canHandleResource(this.resource)) {
@@ -66,7 +66,5 @@ export class BinaryEditorModel extends EditorModel {
 				this.size = stat.size;
 			}
 		}
-
-		return this;
 	}
 }

src/vs/workbench/common/editor/diffEditorModel.ts
@@ -25,17 +25,15 @@ export class DiffEditorModel extends EditorModel {
 		this._modifiedModel = modifiedModel;
 	}
 
-	async load(): Promise<EditorModel> {
+	async resolve(): Promise<void> {
 		await Promise.all([
-			this._originalModel?.load(),
-			this._modifiedModel?.load()
+			this._originalModel?.resolve(),
+			this._modifiedModel?.resolve()
 		]);
-
-		return this;
 	}
 
 	isResolved(): boolean {
-		return this.originalModel instanceof EditorModel && this.originalModel.isResolved() && this.modifiedModel instanceof EditorModel && this.modifiedModel.isResolved();
+		return !!(this.originalModel?.isResolved() && this.modifiedModel?.isResolved());
 	}
 
 	dispose(): void {

src/vs/workbench/common/editor/textDiffEditorModel.ts
@@ -4,7 +4,6 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { IDiffEditorModel } from 'vs/editor/common/editorCommon';
-import { EditorModel } from 'vs/workbench/common/editor';
 import { BaseTextEditorModel } from 'vs/workbench/common/editor/textEditorModel';
 import { DiffEditorModel } from 'vs/workbench/common/editor/diffEditorModel';
 
@@ -32,12 +31,10 @@ export class TextDiffEditorModel extends DiffEditorModel {
 		this.updateTextDiffEditorModel();
 	}
 
-	async load(): Promise<EditorModel> {
-		await super.load();
+	async resolve(): Promise<void> {
+		await super.resolve();
 
 		this.updateTextDiffEditorModel();
-
-		return this;
 	}
 
 	private updateTextDiffEditorModel(): void {

src/vs/workbench/contrib/backup/test/browser/backupRestorer.test.ts
@@ -78,7 +78,7 @@ suite('BackupRestorer', () => {
 			const resource = editor.resource;
 			if (isEqual(resource, untitledFile1)) {
 				const model = await accessor.textFileService.untitled.resolve({ untitledResource: resource });
-				if (model.textEditorModel.getValue() !== 'untitled-1') {
+				if (model.textEditorModel?.getValue() !== 'untitled-1') {
 					const backupContents = await backupFileService.getBackupContents(untitledFile1);
 					assert.fail(`Unable to restore backup for resource ${untitledFile1.toString()}. Backup contents: ${backupContents}`);
 				}
@@ -86,21 +86,23 @@ suite('BackupRestorer', () => {
 				counter++;
 			} else if (isEqual(resource, untitledFile2)) {
 				const model = await accessor.textFileService.untitled.resolve({ untitledResource: resource });
-				if (model.textEditorModel.getValue() !== 'untitled-2') {
+				if (model.textEditorModel?.getValue() !== 'untitled-2') {
 					const backupContents = await backupFileService.getBackupContents(untitledFile2);
 					assert.fail(`Unable to restore backup for resource ${untitledFile2.toString()}. Backup contents: ${backupContents}`);
 				}
 				model.dispose();
 				counter++;
 			} else if (isEqual(resource, fooFile)) {
-				const model = await accessor.textFileService.files.get(fooFile!)?.load();
+				const model = accessor.textFileService.files.get(fooFile);
+				await model?.resolve();
 				if (model?.textEditorModel?.getValue() !== 'fooFile') {
 					const backupContents = await backupFileService.getBackupContents(fooFile);
 					assert.fail(`Unable to restore backup for resource ${fooFile.toString()}. Backup contents: ${backupContents}`);
 				}
 				counter++;
 			} else {
-				const model = await accessor.textFileService.files.get(barFile!)?.load();
+				const model = accessor.textFileService.files.get(barFile);
+				await model?.resolve();
 				if (model?.textEditorModel?.getValue() !== 'barFile') {
 					const backupContents = await backupFileService.getBackupContents(barFile);
 					assert.fail(`Unable to restore backup for resource ${barFile.toString()}. Backup contents: ${backupContents}`);

commit 570094c1f86c3d4e3ce17e4e92659e9d221a914b

Fix DefaultPreferencesEditor setInput Regression from df6d78a1695bdf9a1d4984231242ff1fd65247bb

src/vs/workbench/contrib/preferences/browser/preferencesEditor.ts
@@ -1025,24 +1025,16 @@ export class DefaultPreferencesEditor extends BaseTextEditor {
 		return options;
 	}
 
-	setInput(input: DefaultPreferencesEditorInput, options: EditorOptions | undefined, context: IEditorOpenContext, token: CancellationToken): Promise<void> {
-		return super.setInput(input, options, context, token)
-			.then(() => this.input!.resolve()
-				.then<any>(editorModel => {
-					if (token.isCancellationRequested) {
-						return undefined;
-					}
+	async setInput(input: DefaultPreferencesEditorInput, options: EditorOptions | undefined, context: IEditorOpenContext, token: CancellationToken): Promise<void> {
+		await super.setInput(input, options, context, token);
 
-					return editorModel!.resolve();
-				})
-				.then(editorModel => {
-					if (token.isCancellationRequested) {
-						return;
-					}
+		const editorModel = await input.resolve();
+		if (token.isCancellationRequested) {
+			return;
+		}
 
-					const editor = assertIsDefined(this.getControl());
-					editor.setModel((<ResourceEditorModel>editorModel).textEditorModel);
-				}));
+		const editor = assertIsDefined(this.getControl());
+		editor.setModel((<ResourceEditorModel>editorModel).textEditorModel);
 	}
 
 	clearInput(): void {