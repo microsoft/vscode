f2bdb4f37abdabd4132c88b96d2edf68a0148923
Daniel Imms <2193314+Tyriar@users.noreply.github.com>
2021-04-05 11:08:30 -0700

    Remove task icon, maybe status is enough?

--- src/vs/workbench/contrib/tasks/browser/terminalTaskSystem.ts
+++ src/vs/workbench/contrib/tasks/browser/terminalTaskSystem.ts
@@ -1180,9 +1180,6 @@ export class TerminalTaskSystem implements ITaskSystem {
 				return [undefined, undefined, new TaskError(Severity.Error, nls.localize('TerminalTaskSystem', 'Can\'t execute a shell command on an UNC drive using cmd.exe.'), TaskErrors.UnknownError)];
 			}
 		}
-		if (this.currentTask.shellLaunchConfig) {
-			this.currentTask.shellLaunchConfig.icon = 'window';
-		}
 
 		let prefersSameTerminal = presentationOptions.panel === PanelKind.Dedicated;
 		let allowsSharedTerminal = presentationOptions.panel === PanelKind.Shared;
