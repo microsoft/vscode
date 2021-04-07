55b1c5bca8582e7f6d8fb812e7cfa77eeb4b131c
meganrogge <megrogge@gmail.com>
2021-04-07 07:11:45 -0700

    sash working!

--- src/vs/workbench/contrib/terminal/browser/terminalTabbedView.ts
+++ src/vs/workbench/contrib/terminal/browser/terminalTabbedView.ts
@@ -13,7 +13,9 @@ export class TerminalTabbedView extends Disposable {
 	private _splitView!: SplitView;
 	private _terminalContainer: HTMLElement | undefined;
 	private _terminalTabTree: HTMLElement | undefined;
-	private _displayTabs: boolean;
+	private _showTabs: boolean;
+	private TABS_WIDGET_INDEX: number;
+	private TERMINALS_INDEX: number;
 
 	constructor(
 		parentElement: HTMLElement,
@@ -21,12 +23,16 @@ export class TerminalTabbedView extends Disposable {
 		@IConfigurationService _configurationService: IConfigurationService
 	) {
 		super();
-		this._displayTabs = _terminalService.configHelper.config.showTabs;
+		this._showTabs = _terminalService.configHelper.config.showTabs;
 		this._createSplitView(parentElement);
+
+		this.TABS_WIDGET_INDEX = 0;
+		this.TERMINALS_INDEX = 0;
 
 		_configurationService.onDidChangeConfiguration(e => {
 			if (e.affectsConfiguration('terminal.integrated.showTabs')) {
-				this._displayTabs = _terminalService.configHelper.config.showTabs;
+				this._showTabs = _terminalService.configHelper.config.showTabs;
+				this._updateVisibility();
 			}
 		});
 	}
@@ -42,25 +48,31 @@ export class TerminalTabbedView extends Disposable {
 		this._terminalTabTree = document.createElement('div');
 		this._configureViews();
 
-		if (this._displayTabs) {
-			// show tab tree
-			this._splitView.addView({
-				element: this._terminalTabTree,
-				layout: size => undefined,
-				minimumSize: 600,
-				maximumSize: Number.POSITIVE_INFINITY,
-				onDidChange: () => Disposable.None
-			}, Sizing.Distribute);
-		}
+		this._splitView.addView({
+			element: this._terminalTabTree,
+			layout: size => undefined,
+			minimumSize: 600,
+			maximumSize: Number.POSITIVE_INFINITY,
+			onDidChange: () => Disposable.None,
+			setVisible: () => this._showTabs
+		}, Sizing.Distribute);
+
 
 		this._splitView.addView({
-			// always show terminals
 			element: this._terminalContainer,
 			layout: size => undefined,
 			minimumSize: 220,
 			maximumSize: Number.POSITIVE_INFINITY,
 			onDidChange: () => Disposable.None
 		}, Sizing.Distribute);
+	}
+
+	private _updateVisibility() {
+		if (!this._splitView) {
+			return;
+		}
+		this._splitView.setViewVisible(this.TABS_WIDGET_INDEX, this._showTabs);
+		this._splitView.setViewVisible(this.TERMINALS_INDEX, true);
 	}
 
 	private _configureViews(): void {
