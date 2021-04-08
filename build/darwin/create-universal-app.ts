5904b7b686e9274631893d560fdbc55a5567f878
deepak1556 <hop2deep@gmail.com>
2021-04-08 10:33:02 -0700

    chore: info.plist => Info.plist

--- build/darwin/create-universal-app.ts
+++ build/darwin/create-universal-app.ts
@@ -38,7 +38,7 @@ async function main() {
 			'Credits.rtf',
 			'CodeResources',
 			'fsevents.node',
-			'info.plist', // TODO@deepak1556: regressed with 11.4.2 internal builds
+			'Info.plist', // TODO@deepak1556: regressed with 11.4.2 internal builds
 			'.npmrc'
 		],
 		outAppPath,
