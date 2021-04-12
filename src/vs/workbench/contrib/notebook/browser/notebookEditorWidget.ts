a546db679a212985dc88257be90efe041cf7fd79
rebornix <penn.lv@gmail.com>
2021-04-12 12:08:09 -0700

    re #119599. Avoid flickering when outputs are cleared.

--- src/vs/workbench/contrib/notebook/browser/notebookEditorWidget.ts
+++ src/vs/workbench/contrib/notebook/browser/notebookEditorWidget.ts
@@ -216,7 +216,7 @@ export class NotebookEditorWidget extends Disposable implements INotebookEditor 
 	private _eventDispatcher: NotebookEventDispatcher | undefined;
 	private _notebookViewModel: NotebookViewModel | undefined;
 	private _localStore: DisposableStore = this._register(new DisposableStore());
-	private _localCellStateListeners: IDisposable[] = [];
+	private _localCellStateListeners: DisposableStore[] = [];
 	private _fontInfo: FontInfo | undefined;
 	private _dimension: DOM.Dimension | null = null;
 	private _shadowElementViewInfo: { height: number, width: number, top: number; left: number; } | null = null;
@@ -1233,11 +1233,7 @@ export class NotebookEditorWidget extends Disposable implements INotebookEditor 
 		mark(textModel.uri, 'customMarkdownLoaded');
 
 		// model attached
-		this._localCellStateListeners = this.viewModel.viewCells.map(cell => cell.onDidChangeLayout(e => {
-			if (e.totalHeight !== undefined || e.outerWidth) {
-				this.layoutNotebookCell(cell, cell.layoutInfo.totalHeight);
-			}
-		}));
+		this._localCellStateListeners = this.viewModel.viewCells.map(cell => this._bindCellListener(cell));
 
 		this._localStore.add(this.viewModel.onDidChangeViewCells((e) => {
 			if (this._isDisposed) {
@@ -1247,11 +1243,7 @@ export class NotebookEditorWidget extends Disposable implements INotebookEditor 
 			// update resize listener
 			e.splices.reverse().forEach(splice => {
 				const [start, deleted, newCells] = splice;
-				const deletedCells = this._localCellStateListeners.splice(start, deleted, ...newCells.map(cell => cell.onDidChangeLayout(e => {
-					if (e.totalHeight !== undefined || e.outerWidth) {
-						this.layoutNotebookCell(cell, cell.layoutInfo.totalHeight);
-					}
-				})));
+				const deletedCells = this._localCellStateListeners.splice(start, deleted, ...newCells.map(cell => this._bindCellListener(cell)));
 
 				dispose(deletedCells);
 			});
@@ -1267,6 +1259,24 @@ export class NotebookEditorWidget extends Disposable implements INotebookEditor 
 
 		// restore list state at last, it must be after list layout
 		this.restoreListViewState(viewState);
+	}
+
+	private _bindCellListener(cell: ICellViewModel) {
+		const store = new DisposableStore();
+
+		store.add(cell.onDidChangeLayout(e => {
+			if (e.totalHeight !== undefined || e.outerWidth) {
+				this.layoutNotebookCell(cell, cell.layoutInfo.totalHeight);
+			}
+		}));
+
+		if (cell.cellKind === CellKind.Code) {
+			store.add((cell as CodeCellViewModel).onDidRemoveOutputs((outputs) => {
+				outputs.forEach(output => this.removeInset(output));
+			}));
+		}
+
+		return store;
 	}
 
 	private async _warmupWithMarkdownRenderer(viewModel: NotebookViewModel, viewState: INotebookEditorViewState | undefined) {
