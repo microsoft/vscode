936bc4f8d286ab64d227db3da83cd595623532d4
Tyler Leonhardt <me@tylerleonhardt.com>
2021-04-19 09:57:51 -0700

    factor out logic to function

--- src/vs/base/parts/quickinput/browser/quickInputList.ts
+++ src/vs/base/parts/quickinput/browser/quickInputList.ts
@@ -27,6 +27,7 @@ import { IQuickInputOptions } from 'vs/base/parts/quickinput/browser/quickInput'
 import { IListOptions, List, IListStyles, IListAccessibilityProvider } from 'vs/base/browser/ui/list/listWidget';
 import { KeybindingLabel } from 'vs/base/browser/ui/keybindingLabel/keybindingLabel';
 import { localize } from 'vs/nls';
+import { getCodiconAriaLabel } from 'vs/base/common/codicons';
 
 const $ = dom.$;
 
@@ -240,7 +241,6 @@ export enum QuickInputListFocus {
 
 export class QuickInputList {
 
-	private static readonly CODICON_REGEXP = /\$\((.*?)\)/g;
 	readonly id: string;
 	private container: HTMLElement;
 	private list: List<ListElement>;
@@ -428,7 +428,7 @@ export class QuickInputList {
 				const saneDescription = item.description && item.description.replace(/\r?\n/g, ' ');
 				const saneDetail = item.detail && item.detail.replace(/\r?\n/g, ' ');
 				const saneAriaLabel = item.ariaLabel || [saneLabel, saneDescription, saneDetail]
-					.map(s => s && s.replace(QuickInputList.CODICON_REGEXP, (_match, codiconName) => ` ${codiconName} `))
+					.map(s => getCodiconAriaLabel(s))
 					.filter(s => !!s)
 					.join(', ');
 
