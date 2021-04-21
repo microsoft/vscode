ef68f690fbbe6fabd68813ad362b615618a403ca
jeanp413 <jeanp413@hotmail.com>
2021-04-21 17:16:51 -0500

    Fixes #121873

--- src/vs/workbench/contrib/webview/browser/baseWebviewElement.ts
+++ src/vs/workbench/contrib/webview/browser/baseWebviewElement.ts
@@ -212,10 +212,16 @@ export abstract class BaseWebview<T extends HTMLElement> extends Disposable {
 
 		this._register(this.on(WebviewMessageChannels.loadResource, (entry: { id: number, path: string, query: string, ifNoneMatch?: string }) => {
 			const rawPath = entry.path;
-			const normalizedPath = decodeURIComponent(rawPath);
-			const uri = URI.parse(normalizedPath.replace(/^\/([\w\-]+)\/(.+)$/, (_, scheme, path) => scheme + ':/' + path)).with({
+			const uri = URI.parse(rawPath.replace(/^\/([\w\-]+)(\/{1,2})/, (_: string, scheme: string, sep: string) => {
+				if (sep.length === 1) {
+					return `${scheme}:///`; // Add empty authority.
+				} else {
+					return `${scheme}://`; // Url has own authority.
+				}
+			})).with({
 				query: decodeURIComponent(entry.query),
 			});
+
 			this.loadResource(entry.id, rawPath, uri, entry.ifNoneMatch);
 		}));
 
