b683e5a8ade9e13b43603a2eea96481f418cdebe
meganrogge <megrogge@gmail.com>
2021-04-27 14:48:50 -0700

    fix #122334

--- src/vs/platform/actions/common/actions.ts
+++ src/vs/platform/actions/common/actions.ts
@@ -162,6 +162,7 @@ export class MenuId {
 	static readonly TerminalTabsWidgetContext = new MenuId('TerminalTabsWidgetContext');
 	static readonly TerminalTabsWidgetEmptyContext = new MenuId('TerminalTabsWidgetEmptyContext');
 	static readonly TerminalSingleTabContext = new MenuId('TerminalSingleTabContext');
+	static readonly TerminalTabInlineActions = new MenuId('TerminalTabInlineActions');
 
 	readonly id: number;
 	readonly _debugName: string;
--- src/vs/workbench/contrib/terminal/browser/terminalActions.ts
+++ src/vs/workbench/contrib/terminal/browser/terminalActions.ts
@@ -1394,7 +1394,7 @@ export function registerTerminalActions() {
 		constructor() {
 			super({
 				id: TERMINAL_COMMAND_ID.SPLIT_INSTANCE,
-				title: { value: localize('workbench.action.terminal.split', "Split Terminal"), original: 'Split Terminal' },
+				title: { value: localize('workbench.action.terminal.splitInstance', "Split Terminal"), original: 'Split Terminal' },
 				f1: false,
 				category,
 				precondition: KEYBINDING_CONTEXT_TERMINAL_PROCESS_SUPPORTED,
@@ -1415,12 +1415,20 @@ export function registerTerminalActions() {
 		}
 		async run(accessor: ServicesAccessor) {
 			const instances = getSelectedInstances(accessor);
+			console.log('instances', instances);
 			if (instances) {
 				for (const instance of instances) {
 					accessor.get(ITerminalService).splitInstance(instance);
 				}
 			}
 		}
+	});
+	MenuRegistry.appendMenuItem(MenuId.TerminalTabInlineActions, {
+		command: {
+			id: TERMINAL_COMMAND_ID.SPLIT_INSTANCE,
+			title: localize('workbench.action.terminal.splitInstance', "Split Terminal"),
+		},
+		group: ContextMenuGroup.Create
 	});
 	registerAction2(class extends Action2 {
 		constructor() {
@@ -1586,7 +1594,7 @@ export function registerTerminalActions() {
 			super({
 				id: TERMINAL_COMMAND_ID.KILL_INSTANCE,
 				title: {
-					value: localize('workbench.action.terminal.kill.short', "Kill Terminal"), original: 'Kill Terminal'
+					value: localize('workbench.action.terminal.killInstance', "Kill Terminal"), original: 'Kill Terminal'
 				},
 				f1: false,
 				category,
@@ -1608,7 +1616,16 @@ export function registerTerminalActions() {
 		}
 		async run(accessor: ServicesAccessor) {
 			getSelectedInstances(accessor)?.forEach(instance => instance.dispose(true));
+			accessor.get(ITerminalService).focusTabs();
+			focusNext(accessor);
 		}
+	});
+	MenuRegistry.appendMenuItem(MenuId.TerminalTabInlineActions, {
+		command: {
+			id: TERMINAL_COMMAND_ID.KILL_INSTANCE,
+			title: localize('workbench.action.terminal.killInstance', "Kill Terminal")
+		},
+		group: ContextMenuGroup.Kill
 	});
 	registerAction2(class extends Action2 {
 		constructor() {
@@ -1888,4 +1905,9 @@ function getSelectedInstances(accessor: ServicesAccessor): ITerminalInstance[] |
 		}
 	}
 	return instances;
+}
+
+function focusNext(accessor: ServicesAccessor): void {
+	const listService = accessor.get(IListService);
+	listService.lastFocusedList?.focusNext();
 }
