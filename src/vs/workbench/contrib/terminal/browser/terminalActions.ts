af328eaecbfa6a62f6e832ca7687783d66b716c7
meganrogge <megrogge@gmail.com>
2021-04-28 16:21:47 -0700

    call createTerminalMenus at the end of registerTerminalActions

--- src/vs/workbench/contrib/terminal/browser/terminalActions.ts
+++ src/vs/workbench/contrib/terminal/browser/terminalActions.ts
@@ -31,6 +31,7 @@ import { IWorkspaceContextService, IWorkspaceFolder } from 'vs/platform/workspac
 import { PICK_WORKSPACE_FOLDER_COMMAND_ID } from 'vs/workbench/browser/actions/workspaceCommands';
 import { FindInFilesCommand, IFindInFilesArgs } from 'vs/workbench/contrib/search/browser/searchActions';
 import { Direction, IRemoteTerminalService, ITerminalInstance, ITerminalService } from 'vs/workbench/contrib/terminal/browser/terminal';
+import { createTerminalMenus } from 'vs/workbench/contrib/terminal/browser/terminalMenus';
 import { TerminalQuickAccessProvider } from 'vs/workbench/contrib/terminal/browser/terminalQuickAccess';
 import { IRemoteTerminalAttachTarget, ITerminalConfigHelper, ITerminalProfile, KEYBINDING_CONTEXT_TERMINAL_A11Y_TREE_FOCUS, KEYBINDING_CONTEXT_TERMINAL_ALT_BUFFER_ACTIVE, KEYBINDING_CONTEXT_TERMINAL_FIND_FOCUSED, KEYBINDING_CONTEXT_TERMINAL_FIND_NOT_VISIBLE, KEYBINDING_CONTEXT_TERMINAL_FIND_VISIBLE, KEYBINDING_CONTEXT_TERMINAL_FOCUS, KEYBINDING_CONTEXT_TERMINAL_IS_OPEN, KEYBINDING_CONTEXT_TERMINAL_PROCESS_SUPPORTED, KEYBINDING_CONTEXT_TERMINAL_TABS_FOCUS, KEYBINDING_CONTEXT_TERMINAL_TABS_SINGULAR_SELECTION, KEYBINDING_CONTEXT_TERMINAL_TEXT_SELECTED, TERMINAL_ACTION_CATEGORY, TERMINAL_COMMAND_ID, TERMINAL_VIEW_ID, TitleEventSource } from 'vs/workbench/contrib/terminal/common/terminal';
 import { ITerminalContributionService } from 'vs/workbench/contrib/terminal/common/terminalExtensionPoints';
@@ -1722,6 +1723,7 @@ export function registerTerminalActions() {
 			ContextKeyExpr.not('config.terminal.integrated.tabs.enabled')
 		]),
 	});
+	createTerminalMenus();
 }
 
 interface IRemoteTerminalPick extends IQuickPickItem {
--- src/vs/workbench/contrib/terminal/browser/terminalMenus.ts
+++ src/vs/workbench/contrib/terminal/browser/terminalMenus.ts
@@ -22,255 +22,257 @@ export const enum ContextMenuTabsGroup {
 	Configure = '3_configure'
 }
 
-MenuRegistry.appendMenuItems(
-	[
-		{
-			id: MenuId.TerminalContainerContext, item: {
-				group: ContextMenuGroup.Create,
-				command: {
-					id: TERMINAL_COMMAND_ID.SPLIT,
-					title: localize('workbench.action.terminal.split', "Split Terminal")
+export function createTerminalMenus(): void {
+	MenuRegistry.appendMenuItems(
+		[
+			{
+				id: MenuId.TerminalContainerContext, item: {
+					group: ContextMenuGroup.Create,
+					command: {
+						id: TERMINAL_COMMAND_ID.SPLIT,
+						title: localize('workbench.action.terminal.split', "Split Terminal")
+					}
+				}
+			},
+			{
+				id: MenuId.TerminalContainerContext, item: {
+					command: {
+						id: TERMINAL_COMMAND_ID.NEW,
+						title: localize('workbench.action.terminal.new.short', "New Terminal")
+					},
+					group: ContextMenuGroup.Create
+				}
+			},
+			{
+				id: MenuId.TerminalContainerContext, item: {
+					command: {
+						id: TERMINAL_COMMAND_ID.KILL,
+						title: localize('workbench.action.terminal.kill.short', "Kill Terminal")
+					},
+					group: ContextMenuGroup.Kill
+				}
+			},
+			{
+				id: MenuId.TerminalContainerContext, item: {
+					command: {
+						id: TERMINAL_COMMAND_ID.COPY_SELECTION,
+						title: localize('workbench.action.terminal.copySelection.short', "Copy")
+					},
+					group: ContextMenuGroup.Edit,
+					order: 1
+				}
+			},
+			{
+				id: MenuId.TerminalContainerContext, item: {
+					command: {
+						id: TERMINAL_COMMAND_ID.PASTE,
+						title: localize('workbench.action.terminal.paste.short', "Paste")
+					},
+					group: ContextMenuGroup.Edit,
+					order: 2
+				}
+			},
+			{
+				id: MenuId.TerminalContainerContext, item: {
+					command: {
+						id: TERMINAL_COMMAND_ID.CLEAR,
+						title: localize('workbench.action.terminal.clear', "Clear")
+					},
+					group: ContextMenuGroup.Clear,
+				}
+			},
+			{
+				id: MenuId.TerminalContainerContext, item: {
+					command: {
+						id: TERMINAL_COMMAND_ID.SHOW_TABS,
+						title: localize('workbench.action.terminal.showsTabs', "Show Tabs")
+					},
+					when: ContextKeyExpr.not('config.terminal.integrated.tabs.enabled'),
+					group: ContextMenuGroup.Config
+				}
+			},
+			{
+				id: MenuId.TerminalContainerContext, item: {
+					command: {
+						id: TERMINAL_COMMAND_ID.SELECT_ALL,
+						title: localize('workbench.action.terminal.selectAll', "Select All"),
+					},
+					group: ContextMenuGroup.Edit,
+					order: 3
+				}
+			},
+		]
+	);
+
+	MenuRegistry.appendMenuItems(
+		[
+			{
+				id: MenuId.TerminalTabsWidgetEmptyContext, item: {
+					command: {
+						id: TERMINAL_COMMAND_ID.NEW_WITH_PROFILE,
+						title: localize('workbench.action.terminal.newWithProfile.short', "New Terminal With Profile")
+					},
+					group: ContextMenuGroup.Create
+				}
+			},
+			{
+				id: MenuId.TerminalTabsWidgetEmptyContext, item: {
+					command: {
+						id: TERMINAL_COMMAND_ID.NEW,
+						title: localize('workbench.action.terminal.new.short', "New Terminal")
+					},
+					group: ContextMenuGroup.Create
 				}
 			}
-		},
-		{
-			id: MenuId.TerminalContainerContext, item: {
-				command: {
-					id: TERMINAL_COMMAND_ID.NEW,
-					title: localize('workbench.action.terminal.new.short', "New Terminal")
-				},
-				group: ContextMenuGroup.Create
-			}
-		},
-		{
-			id: MenuId.TerminalContainerContext, item: {
-				command: {
-					id: TERMINAL_COMMAND_ID.KILL,
-					title: localize('workbench.action.terminal.kill.short', "Kill Terminal")
-				},
-				group: ContextMenuGroup.Kill
-			}
-		},
-		{
-			id: MenuId.TerminalContainerContext, item: {
-				command: {
-					id: TERMINAL_COMMAND_ID.COPY_SELECTION,
-					title: localize('workbench.action.terminal.copySelection.short', "Copy")
-				},
-				group: ContextMenuGroup.Edit,
-				order: 1
-			}
-		},
-		{
-			id: MenuId.TerminalContainerContext, item: {
-				command: {
-					id: TERMINAL_COMMAND_ID.PASTE,
-					title: localize('workbench.action.terminal.paste.short', "Paste")
-				},
-				group: ContextMenuGroup.Edit,
-				order: 2
-			}
-		},
-		{
-			id: MenuId.TerminalContainerContext, item: {
-				command: {
-					id: TERMINAL_COMMAND_ID.CLEAR,
-					title: localize('workbench.action.terminal.clear', "Clear")
-				},
-				group: ContextMenuGroup.Clear,
-			}
-		},
-		{
-			id: MenuId.TerminalContainerContext, item: {
-				command: {
-					id: TERMINAL_COMMAND_ID.SHOW_TABS,
-					title: localize('workbench.action.terminal.showsTabs', "Show Tabs")
-				},
-				when: ContextKeyExpr.not('config.terminal.integrated.tabs.enabled'),
-				group: ContextMenuGroup.Config
-			}
-		},
-		{
-			id: MenuId.TerminalContainerContext, item: {
-				command: {
-					id: TERMINAL_COMMAND_ID.SELECT_ALL,
-					title: localize('workbench.action.terminal.selectAll', "Select All"),
-				},
-				group: ContextMenuGroup.Edit,
-				order: 3
-			}
-		},
-	]
-);
+		]
+	);
 
-MenuRegistry.appendMenuItems(
-	[
-		{
-			id: MenuId.TerminalTabsWidgetEmptyContext, item: {
-				command: {
-					id: TERMINAL_COMMAND_ID.NEW_WITH_PROFILE,
-					title: localize('workbench.action.terminal.newWithProfile.short', "New Terminal With Profile")
-				},
-				group: ContextMenuGroup.Create
-			}
-		},
-		{
-			id: MenuId.TerminalTabsWidgetEmptyContext, item: {
-				command: {
-					id: TERMINAL_COMMAND_ID.NEW,
-					title: localize('workbench.action.terminal.new.short', "New Terminal")
-				},
-				group: ContextMenuGroup.Create
-			}
-		}
-	]
-);
-
-MenuRegistry.appendMenuItems(
-	[
-		{
-			id: MenuId.TerminalToolbarContext, item: {
-				command: {
-					id: TERMINAL_COMMAND_ID.SELECT_DEFAULT_PROFILE,
-					title: { value: localize('workbench.action.terminal.selectDefaultProfile', "Select Default Profile"), original: 'Select Default Profile' }
-				},
-				group: ContextMenuTabsGroup.Configure
-			}
-		},
-		{
-			id: MenuId.TerminalToolbarContext, item: {
-				command: {
-					id: TERMINAL_COMMAND_ID.CONFIGURE_TERMINAL_SETTINGS,
-					title: localize('workbench.action.terminal.openSettings', "Configure Terminal Settings")
-				},
-				group: ContextMenuTabsGroup.Configure
-			}
-		}
-	]
-);
-
-MenuRegistry.appendMenuItems(
-	[
-		{
-			id: MenuId.TerminalTabInlineActions, item: {
-				command: {
-					id: TERMINAL_COMMAND_ID.SPLIT_INSTANCE,
-					title: localize('workbench.action.terminal.splitInstance', "Split Terminal"),
-				},
-				group: ContextMenuGroup.Create
-			}
-		},
-		{
-			id: MenuId.TerminalTabInlineActions, item: {
-				command: {
-					id: TERMINAL_COMMAND_ID.KILL_INSTANCE,
-					title: localize('workbench.action.terminal.killInstance', "Kill Terminal")
-				},
-				group: ContextMenuGroup.Kill
-			}
-		}
-	]
-);
-
-MenuRegistry.appendMenuItem(MenuId.ViewTitle, {
-	group: 'navigation',
-	command: {
-		id: TERMINAL_COMMAND_ID.SPLIT,
-		title: localize('workbench.action.terminal.split', "Split Terminal")
-	},
-	order: 2,
-	when: ContextKeyAndExpr.create([
-		ContextKeyEqualsExpr.create('view', TERMINAL_VIEW_ID),
-		ContextKeyExpr.not('config.terminal.integrated.tabs.enabled')
-	])
-});
-MenuRegistry.appendMenuItems(
-	[
-		{
-			id: MenuId.TerminalSingleTabContext, item: {
-				command: {
-					id: TERMINAL_COMMAND_ID.CHANGE_ICON,
-					title: localize('workbench.action.terminal.changeIcon', "Change Icon...")
-				},
-				group: ContextMenuGroup.Edit,
-				order: 3
-			}
-		},
-		{
-			id: MenuId.TerminalSingleTabContext, item: {
-				command: {
-					id: TERMINAL_COMMAND_ID.RENAME,
-					title: localize('workbench.action.terminal.rename', "Rename...")
+	MenuRegistry.appendMenuItems(
+		[
+			{
+				id: MenuId.TerminalToolbarContext, item: {
+					command: {
+						id: TERMINAL_COMMAND_ID.SELECT_DEFAULT_PROFILE,
+						title: { value: localize('workbench.action.terminal.selectDefaultProfile', "Select Default Profile"), original: 'Select Default Profile' }
+					},
+					group: ContextMenuTabsGroup.Configure
+				}
+			},
+			{
+				id: MenuId.TerminalToolbarContext, item: {
+					command: {
+						id: TERMINAL_COMMAND_ID.CONFIGURE_TERMINAL_SETTINGS,
+						title: localize('workbench.action.terminal.openSettings', "Configure Terminal Settings")
+					},
+					group: ContextMenuTabsGroup.Configure
 				}
 			}
-		},
-		{
-			id: MenuId.TerminalSingleTabContext, item: {
-				command: {
-					id: TERMINAL_COMMAND_ID.RENAME_INSTANCE,
-					title: localize('workbench.action.terminal.renameInstance', "Rename...")
-				},
-				group: ContextMenuGroup.Edit
-			}
-		},
-		{
-			id: MenuId.TerminalSingleTabContext, item: {
-				group: ContextMenuGroup.Create,
-				command: {
-					id: TERMINAL_COMMAND_ID.SPLIT,
-					title: localize('workbench.action.terminal.split', "Split Terminal")
+		]
+	);
+
+	MenuRegistry.appendMenuItems(
+		[
+			{
+				id: MenuId.TerminalTabInlineActions, item: {
+					command: {
+						id: TERMINAL_COMMAND_ID.SPLIT_INSTANCE,
+						title: localize('workbench.action.terminal.splitInstance', "Split Terminal"),
+					},
+					group: ContextMenuGroup.Create
+				}
+			},
+			{
+				id: MenuId.TerminalTabInlineActions, item: {
+					command: {
+						id: TERMINAL_COMMAND_ID.KILL_INSTANCE,
+						title: localize('workbench.action.terminal.killInstance', "Kill Terminal")
+					},
+					group: ContextMenuGroup.Kill
 				}
 			}
-		},
-		{
-			id: MenuId.TerminalSingleTabContext, item: {
-				command: {
-					id: TERMINAL_COMMAND_ID.KILL,
-					title: localize('workbench.action.terminal.kill.short', "Kill Terminal")
-				},
-				group: ContextMenuGroup.Kill
-			}
-		}
-	]
-);
+		]
+	);
 
-MenuRegistry.appendMenuItems(
-	[
-		{
-			id: MenuId.TerminalTabsWidgetContext, item: {
-				command: {
-					id: TERMINAL_COMMAND_ID.RENAME_INSTANCE,
-					title: localize('workbench.action.terminal.renameInstance', "Rename...")
-				},
-				group: ContextMenuGroup.Edit
-			}
+	MenuRegistry.appendMenuItem(MenuId.ViewTitle, {
+		group: 'navigation',
+		command: {
+			id: TERMINAL_COMMAND_ID.SPLIT,
+			title: localize('workbench.action.terminal.split', "Split Terminal")
 		},
-		{
-			id: MenuId.TerminalTabsWidgetContext, item: {
-				command: {
-					id: TERMINAL_COMMAND_ID.CHANGE_ICON_INSTANCE,
-					title: localize('workbench.action.terminal.changeIcon', "Change Icon...")
-				},
-				group: ContextMenuGroup.Edit
+		order: 2,
+		when: ContextKeyAndExpr.create([
+			ContextKeyEqualsExpr.create('view', TERMINAL_VIEW_ID),
+			ContextKeyExpr.not('config.terminal.integrated.tabs.enabled')
+		])
+	});
+	MenuRegistry.appendMenuItems(
+		[
+			{
+				id: MenuId.TerminalSingleTabContext, item: {
+					command: {
+						id: TERMINAL_COMMAND_ID.CHANGE_ICON,
+						title: localize('workbench.action.terminal.changeIcon', "Change Icon...")
+					},
+					group: ContextMenuGroup.Edit,
+					order: 3
+				}
+			},
+			{
+				id: MenuId.TerminalSingleTabContext, item: {
+					command: {
+						id: TERMINAL_COMMAND_ID.RENAME,
+						title: localize('workbench.action.terminal.rename', "Rename...")
+					}
+				}
+			},
+			{
+				id: MenuId.TerminalSingleTabContext, item: {
+					command: {
+						id: TERMINAL_COMMAND_ID.RENAME_INSTANCE,
+						title: localize('workbench.action.terminal.renameInstance', "Rename...")
+					},
+					group: ContextMenuGroup.Edit
+				}
+			},
+			{
+				id: MenuId.TerminalSingleTabContext, item: {
+					group: ContextMenuGroup.Create,
+					command: {
+						id: TERMINAL_COMMAND_ID.SPLIT,
+						title: localize('workbench.action.terminal.split', "Split Terminal")
+					}
+				}
+			},
+			{
+				id: MenuId.TerminalSingleTabContext, item: {
+					command: {
+						id: TERMINAL_COMMAND_ID.KILL,
+						title: localize('workbench.action.terminal.kill.short', "Kill Terminal")
+					},
+					group: ContextMenuGroup.Kill
+				}
 			}
-		},
-		{
-			id: MenuId.TerminalTabsWidgetContext, item: {
-				command: {
-					id: TERMINAL_COMMAND_ID.SPLIT_INSTANCE,
-					title: localize('workbench.action.terminal.splitInstance', "Split Terminal"),
-				},
-				group: ContextMenuGroup.Create
+		]
+	);
+
+	MenuRegistry.appendMenuItems(
+		[
+			{
+				id: MenuId.TerminalTabsWidgetContext, item: {
+					command: {
+						id: TERMINAL_COMMAND_ID.RENAME_INSTANCE,
+						title: localize('workbench.action.terminal.renameInstance', "Rename...")
+					},
+					group: ContextMenuGroup.Edit
+				}
+			},
+			{
+				id: MenuId.TerminalTabsWidgetContext, item: {
+					command: {
+						id: TERMINAL_COMMAND_ID.CHANGE_ICON_INSTANCE,
+						title: localize('workbench.action.terminal.changeIcon', "Change Icon...")
+					},
+					group: ContextMenuGroup.Edit
+				}
+			},
+			{
+				id: MenuId.TerminalTabsWidgetContext, item: {
+					command: {
+						id: TERMINAL_COMMAND_ID.SPLIT_INSTANCE,
+						title: localize('workbench.action.terminal.splitInstance', "Split Terminal"),
+					},
+					group: ContextMenuGroup.Create
+				}
+			},
+			{
+				id: MenuId.TerminalTabsWidgetContext, item: {
+					command: {
+						id: TERMINAL_COMMAND_ID.KILL_INSTANCE,
+						title: localize('workbench.action.terminal.killInstance', "Kill Terminal")
+					},
+					group: ContextMenuGroup.Kill,
+				}
 			}
-		},
-		{
-			id: MenuId.TerminalTabsWidgetContext, item: {
-				command: {
-					id: TERMINAL_COMMAND_ID.KILL_INSTANCE,
-					title: localize('workbench.action.terminal.killInstance', "Kill Terminal")
-				},
-				group: ContextMenuGroup.Kill,
-			}
-		}
-	]
-);
+		]
+	);
+}
