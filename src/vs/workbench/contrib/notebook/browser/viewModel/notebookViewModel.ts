5082e72921d92448c88be22529577901805e991e
Rob Lourens <roblourens@gmail.com>
2021-04-28 10:25:28 -0700

    Avoid cell execution icon flickering due to rerendering spinner

--- src/vs/workbench/contrib/notebook/browser/viewModel/notebookViewModel.ts
+++ src/vs/workbench/contrib/notebook/browser/viewModel/notebookViewModel.ts
@@ -35,6 +35,7 @@ import { MultiModelEditStackElement, SingleModelEditStackElement } from 'vs/edit
 import { ResourceNotebookCellEdit } from 'vs/workbench/contrib/bulkEdit/browser/bulkCellEdits';
 import { NotebookCellSelectionCollection } from 'vs/workbench/contrib/notebook/browser/viewModel/cellSelectionCollection';
 import { ITextModelService } from 'vs/editor/common/services/resolverService';
+import { groupByNumber } from 'vs/base/common/collections';
 
 export interface INotebookEditorViewState {
 	editingCells: { [key: number]: boolean };
@@ -732,25 +733,24 @@ export class NotebookViewModel extends Disposable implements EditorFoldingStateD
 	}
 
 	deltaCellStatusBarItems(oldItems: string[], newItems: INotebookDeltaCellStatusBarItems[]): string[] {
-		oldItems.forEach(id => {
-			const handle = this._statusBarItemIdToCellMap.get(id);
-
-			if (handle !== undefined) {
-				const cell = this.getCellByHandle(handle);
-				cell?.deltaCellStatusBarItems([id], []);
-			}
-		});
+		const deletesByHandle = groupByNumber(oldItems, id => this._statusBarItemIdToCellMap.get(id) ?? -1);
 
 		const result: string[] = [];
-
 		newItems.forEach(itemDelta => {
 			const cell = this.getCellByHandle(itemDelta.handle);
-			const ret = cell?.deltaCellStatusBarItems([], itemDelta.items) || [];
+			const deleted = deletesByHandle.get(itemDelta.handle) ?? [];
+			deletesByHandle.delete(itemDelta.handle);
+			const ret = cell?.deltaCellStatusBarItems(deleted, itemDelta.items) || [];
 			ret.forEach(id => {
 				this._statusBarItemIdToCellMap.set(id, itemDelta.handle);
 			});
 
 			result.push(...ret);
+		});
+
+		deletesByHandle.forEach((ids, handle) => {
+			const cell = this.getCellByHandle(handle);
+			cell?.deltaCellStatusBarItems(ids, []);
 		});
 
 		return result;
