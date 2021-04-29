3d6f88eb92583c264d287cd5b7c6317b319a056a
João Moreno <joao.moreno@microsoft.com>
2021-04-29 16:32:57 +0200

    fixes #120828

--- src/vs/editor/contrib/suggest/suggestWidgetDetails.ts
+++ src/vs/editor/contrib/suggest/suggestWidgetDetails.ts
@@ -345,6 +345,8 @@ export class SuggestDetailsOverlay implements IOverlayWidget {
 	}
 
 	hide(sessionEnded: boolean = false): void {
+		this._resizable.clearSashHoverState();
+
 		if (this._added) {
 			this._editor.removeOverlayWidget(this);
 			this._added = false;
