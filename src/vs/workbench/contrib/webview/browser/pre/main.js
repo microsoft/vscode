c52ae19f04f24a42a685bab8adbc169db24a3c94
Benjamin Pasero <benjamin.pasero@microsoft.com>
2021-05-03 10:10:34 +0200

    Revert "Try to block webviews from cancelling unloads"
    This reverts commit dee149aebe8264cff4f69fe515d79d69f780a7ed.

--- src/vs/workbench/contrib/webview/browser/pre/main.js
+++ src/vs/workbench/contrib/webview/browser/pre/main.js
@@ -187,35 +187,6 @@ function getVsCodeApiScript(allowMultipleAPIAcquire, useParentPostMessage, state
 			delete window.parent;
 			delete window.top;
 			delete window.frameElement;
-
-			// Try to block webviews from cancelling unloads.
-			// This blocking is not perfect but should block common patterns
-			(function() {
-				const createUnloadEventProxy = (e) => {
-					return new Proxy(e, {
-						set: (target, prop, receiver) => {
-							if (prop === 'returnValue') {
-								// Don't allow setting return value to block window unload
-								return;
-							}
-							target[prop] = value;
-						}
-					});
-				};
-
-				Object.defineProperty(window, 'onbeforeunload', { value: null, writable: false });
-
-				const originalAddEventListener = window.addEventListener.bind(window);
-				window.addEventListener = (type, listener, ...args) => {
-					if (type === 'beforeunload') {
-						return originalAddEventListener(type, (e) => {
-							return createUnloadEventProxy(listener);
-						}, ...args);
-					} else {
-						return originalAddEventListener(type, listener, ...args);
-					}
-				}
-			})();
 		`;
 }
 
