72aa675fc9baf76fd42308e32e3c045c60d42146
Logan Ramos <lramos15@gmail.com>
2021-05-04 14:59:03 -0400

    Update API tests to strict equal

--- extensions/vscode-api-tests/src/singlefolder-tests/configuration.test.ts
+++ extensions/vscode-api-tests/src/singlefolder-tests/configuration.test.ts
@@ -25,24 +25,24 @@ suite('vscode API - configuration', () => {
 		const config = vscode.workspace.getConfiguration('farboo');
 
 		assert.ok(config.has('config0'));
-		assert.equal(config.get('config0'), true);
-		assert.equal(config.get('config4'), '');
-		assert.equal(config['config0'], true);
-		assert.equal(config['config4'], '');
+		assert.strictEqual(config.get('config0'), true);
+		assert.strictEqual(config.get('config4'), '');
+		assert.strictEqual(config['config0'], true);
+		assert.strictEqual(config['config4'], '');
 
 		assert.throws(() => (<any>config)['config4'] = 'valuevalue');
 
 		assert.ok(config.has('nested.config1'));
-		assert.equal(config.get('nested.config1'), 42);
+		assert.strictEqual(config.get('nested.config1'), 42);
 		assert.ok(config.has('nested.config2'));
-		assert.equal(config.get('nested.config2'), 'Das Pferd frisst kein Reis.');
+		assert.strictEqual(config.get('nested.config2'), 'Das Pferd frisst kein Reis.');
 	});
 
 	test('configuration, name vs property', () => {
 		const config = vscode.workspace.getConfiguration('farboo');
 
 		assert.ok(config.has('get'));
-		assert.equal(config.get('get'), 'get-prop');
+		assert.strictEqual(config.get('get'), 'get-prop');
 		assert.deepEqual(config['get'], config.get);
 		assert.throws(() => config['get'] = <any>'get-prop');
 	});
