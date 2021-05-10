0b43503922703a8b505b8f55025dc51a1039c5d8
meganrogge <megrogge@gmail.com>
2021-05-10 16:56:40 -0700

    address feedback

--- src/vs/workbench/contrib/terminal/common/terminal.ts
+++ src/vs/workbench/contrib/terminal/common/terminal.ts
@@ -31,7 +31,7 @@ export const KEYBINDING_CONTEXT_TERMINAL_IS_TABS_NARROW_FOCUS = new RawContextKe
 export const KEYBINDING_CONTEXT_TERMINAL_TABS_FOCUS = new RawContextKey<boolean>('terminalTabsFocus', false, nls.localize('terminalTabsFocusContextKey', "Whether the terminal tabs widget is focused"));
 
 /** A context key that is set when the integrated terminal tabs widget has the mouse focus. */
-export const KEYBINDING_CONTEXT_TERMINAL_TABS_MOUSE = new RawContextKey<boolean>('terminalTabsMouse', false, nls.localize('terminalTabsMouseContextKey', "Whether the mouse is within the terminal tabs widget"));
+export const KEYBINDING_CONTEXT_TERMINAL_TABS_MOUSE = new RawContextKey<boolean>('terminalTabsMouse', false, undefined);
 
 export const KEYBINDING_CONTEXT_TERMINAL_SHELL_TYPE_KEY = 'terminalShellType';
 /** A context key that is set to the detected shell for the most recently active terminal, this is set to the last known value when no terminals exist. */
