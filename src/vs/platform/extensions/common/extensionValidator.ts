36e8f2b3019cd44aee8e2ef7f038d6b06dbfd1ed
Connor Peet <connor@peet.io>
2021-05-11 16:46:01 -0700

    extensions: don't use named regex groups to avoid build err

--- src/vs/platform/extensions/common/extensionValidator.ts
+++ src/vs/platform/extensions/common/extensionValidator.ts
@@ -29,7 +29,7 @@ export interface INormalizedVersion {
 }
 
 const VERSION_REGEXP = /^(\^|>=)?((\d+)|x)\.((\d+)|x)\.((\d+)|x)(\-.*)?$/;
-const NOT_BEFORE_REGEXP = /^-(?<year>\d{4})(?<month>\d{2})(?<day>\d{2})$/;
+const NOT_BEFORE_REGEXP = /^-(\d{4})(\d{2})(\d{2})$/;
 
 export function isValidVersionStr(version: string): boolean {
 	version = version.trim();
@@ -98,8 +98,9 @@ export function normalizeVersion(version: IParsedVersion | null): INormalizedVer
 	let notBefore = 0;
 	if (version.preRelease) {
 		const match = NOT_BEFORE_REGEXP.exec(version.preRelease);
-		if (match?.groups) {
-			notBefore = Date.UTC(Number(match.groups.year), Number(match.groups.month) - 1, Number(match.groups.day));
+		if (match) {
+			const [, year, month, day] = match;
+			notBefore = Date.UTC(Number(year), Number(month) - 1, Number(day));
 		}
 	}
 
