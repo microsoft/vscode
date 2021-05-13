9bc642fa228a365753df204885c6a50ca9294585
Benjamin Pasero <benjamin.pasero@microsoft.com>
2021-05-13 17:56:59 +0200

    files - validate readonly flag in some operations

--- src/vs/vscode.proposed.d.ts
+++ src/vs/vscode.proposed.d.ts
@@ -3300,6 +3300,11 @@ declare module 'vscode' {
 	export enum FilePermission {
 		/**
 		 * The file is readonly.
+		 *
+		 * *Note:* All `FileStat` from a `FileSystemProvider` that is registered  with
+		 * the option `isReadonly: true` will be implicitly handled as if `FilePermission.Readonly`
+		 * is set. As a consequence, it is not possible to have a readonly file system provider
+		 * registered where some `FileStat` are not readonly.
 		 */
 		Readonly = 1
 	}
