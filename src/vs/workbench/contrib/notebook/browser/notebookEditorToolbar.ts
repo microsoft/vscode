8f1408281072b352a0b90c3b62d7cce1a3f8632f
rebornix <penn.lv@gmail.com>
2021-05-18 17:09:22 -0700

    fix mouse click double background

--- src/vs/workbench/contrib/notebook/browser/notebookEditorToolbar.ts
+++ src/vs/workbench/contrib/notebook/browser/notebookEditorToolbar.ts
@@ -16,6 +16,8 @@ import { IContextKeyService } from 'vs/platform/contextkey/common/contextkey';
 import { IContextMenuService } from 'vs/platform/contextview/browser/contextView';
 import { IInstantiationService, optional } from 'vs/platform/instantiation/common/instantiation';
 import { IKeybindingService } from 'vs/platform/keybinding/common/keybinding';
+import { toolbarActiveBackground } from 'vs/platform/theme/common/colorRegistry';
+import { registerThemingParticipant } from 'vs/platform/theme/common/themeService';
 import { SELECT_KERNEL_ID } from 'vs/workbench/contrib/notebook/browser/contrib/coreActions';
 import { INotebookEditor, NOTEBOOK_EDITOR_ID } from 'vs/workbench/contrib/notebook/browser/notebookBrowser';
 import { NotebooKernelActionViewItem } from 'vs/workbench/contrib/notebook/browser/notebookKernelActionViewItem';
@@ -214,3 +216,14 @@ export class NotebookEditorToolbar extends Disposable {
 		super.dispose();
 	}
 }
+
+registerThemingParticipant((theme, collector) => {
+	const toolbarActiveBackgroundColor = theme.getColor(toolbarActiveBackground);
+	if (toolbarActiveBackgroundColor) {
+		collector.addRule(`
+		.monaco-workbench .notebookOverlay .notebook-toolbar-container .monaco-action-bar:not(.vertical) .action-item.active {
+			background-color: ${toolbarActiveBackgroundColor};
+		}
+		`);
+	}
+});
