c1a697d369c9c9c3884083b07332ab6ce3fd02a2
meganrogge <megrogge@gmail.com>
2021-05-26 13:10:41 -0700

    make test profile resolver work

--- src/vs/workbench/test/browser/workbenchTestServices.ts
+++ src/vs/workbench/test/browser/workbenchTestServices.ts
@@ -1590,6 +1590,7 @@ export class TestTerminalProfileResolverService implements ITerminalProfileResol
 	async getEnvironment(): Promise<IProcessEnvironment> { return process.env; }
 	getSafeConfigValue(key: string, os: OperatingSystem): unknown | undefined { return undefined; }
 	getSafeConfigValueFullKey(key: string): unknown | undefined { return undefined; }
+	createProfileFromShellAndShellArgs(shell?: unknown, shellArgs?: unknown): Promise<ITerminalProfile | undefined> { throw new Error('Method not implemented.'); }
 }
 
 export class TestLocalTerminalService implements ILocalTerminalService {
