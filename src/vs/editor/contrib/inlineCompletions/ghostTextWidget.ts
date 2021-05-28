df03e4caadcee89ee75c3cc22c786182cdbd7931
Henning Dieterichs <hdieterichs@microsoft.com>
2021-05-28 09:32:05 +0200

    Removed unused MarkdownString.

--- src/vs/editor/contrib/inlineCompletions/ghostTextWidget.ts
+++ src/vs/editor/contrib/inlineCompletions/ghostTextWidget.ts
@@ -21,7 +21,6 @@ import { IModelDeltaDecoration } from 'vs/editor/common/model';
 import { IThemeService, registerThemingParticipant } from 'vs/platform/theme/common/themeService';
 import { editorSuggestPreviewBorder, editorSuggestPreviewOpacity } from 'vs/editor/common/view/editorColorRegistry';
 import { RGBA, Color } from 'vs/base/common/color';
-import { MarkdownString } from 'vs/base/common/htmlContent';
 import { CursorColumns } from 'vs/editor/common/controller/cursorCommon';
 
 const ttPolicy = window.trustedTypes?.createPolicy('editorGhostText', { createHTML: value => value });
