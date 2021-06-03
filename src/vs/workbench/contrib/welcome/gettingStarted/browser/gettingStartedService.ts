b26e1aba58c2ad7b3a5e8fb95ae0991d89765c3a
Jackson Kearl <jakearl@microsoft.com>
2021-06-03 13:57:56 -0700

    Remove hashes from override

--- src/vs/workbench/contrib/welcome/gettingStarted/browser/gettingStartedService.ts
+++ src/vs/workbench/contrib/welcome/gettingStarted/browser/gettingStartedService.ts
@@ -706,7 +706,7 @@ export class GettingStartedService extends Disposable implements IGettingStarted
 			this._onDidAddCategory.fire();
 		}
 
-		this.tasExperimentService?.getTreatment<string>(`gettingStarted.overrideCategory.${categoryDescriptor.id}.when`).then(override => {
+		this.tasExperimentService?.getTreatment<string>(`gettingStarted.overrideCategory.${categoryDescriptor.id.replace('#', '.')}.when`).then(override => {
 			if (override) {
 				const old = category.when;
 				const gnu = ContextKeyExpr.deserialize(override) ?? old;
