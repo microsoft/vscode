7dacb10b591c1d22d116bf3b0b2f2f6d04cfdd05
rebornix <penn.lv@gmail.com>
2021-06-02 17:30:04 -0700

    fix #125339.

--- src/vs/workbench/contrib/notebook/browser/notebookEditorWidget.ts
+++ src/vs/workbench/contrib/notebook/browser/notebookEditorWidget.ts
@@ -668,7 +668,12 @@ export class NotebookEditorWidget extends Disposable implements INotebookEditor 
 			styleSheets.push(`
 			.monaco-workbench .notebookOverlay .monaco-list:focus-within .monaco-list-row.focused .cell-inner-container .cell-focus-indicator-left:before {
 				border-color: var(--notebook-focused-cell-border-color) !important;
-			}`);
+			}
+
+			.monaco-workbench .notebookOverlay .monaco-list .monaco-list-row.focused .cell-inner-container .cell-focus-indicator-left:before {
+				border-color: var(--notebook-inactive-focused-cell-border-color) !important;
+			}
+			`);
 		}
 
 		// between cell insert toolbar
