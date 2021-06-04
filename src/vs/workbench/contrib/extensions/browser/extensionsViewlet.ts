0e6d07052c82dbafb4e387e7e0d5b9d565895491
SteVen Batten <stbatt@microsoft.com>
2021-06-04 09:42:52 -0700

    remove untrustedWorkspace search for now

--- src/vs/workbench/contrib/extensions/browser/extensionsViewlet.ts
+++ src/vs/workbench/contrib/extensions/browser/extensionsViewlet.ts
@@ -59,7 +59,6 @@ import { ICommandService } from 'vs/platform/commands/common/commands';
 import { isIOS, isWeb } from 'vs/base/common/platform';
 import { installLocalInRemoteIcon } from 'vs/workbench/contrib/extensions/browser/extensionsIcons';
 import { registerAction2, Action2, MenuId } from 'vs/platform/actions/common/actions';
-import { IWorkspaceTrustManagementService } from 'vs/platform/workspace/common/workspaceTrust';
 import { WorkspaceTrustContext } from 'vs/workbench/services/workspaces/common/workspaceTrust';
 
 const SearchMarketplaceExtensionsContext = new RawContextKey<boolean>('searchMarketplaceExtensions', false);
@@ -472,7 +471,6 @@ export class ExtensionsViewPaneContainer extends ViewPaneContainer implements IE
 		@IExtensionManagementServerService private readonly extensionManagementServerService: IExtensionManagementServerService,
 		@INotificationService private readonly notificationService: INotificationService,
 		@IViewletService private readonly viewletService: IViewletService,
-		@IWorkspaceTrustManagementService private readonly workspaceTrustService: IWorkspaceTrustManagementService,
 		@IThemeService themeService: IThemeService,
 		@IConfigurationService configurationService: IConfigurationService,
 		@IStorageService storageService: IStorageService,
@@ -530,14 +528,7 @@ export class ExtensionsViewPaneContainer extends ViewPaneContainer implements IE
 		const header = append(this.root, $('.header'));
 		const placeholder = localize('searchExtensions', "Search Extensions in Marketplace");
 
-		let searchValue = this.searchViewletState['query.value'];
-		if (ExtensionsListView.isSearchWorkspaceUnsupportedExtensionsQuery(searchValue)) {
-			searchValue = undefined;
-		}
-
-		if (searchValue === undefined || searchValue === '') {
-			searchValue = this.workspaceTrustService.isWorkpaceTrusted() ? '' : '@workspaceUnsupported';
-		}
+		const searchValue = this.searchViewletState['query.value'] ? this.searchViewletState['query.value'] : '';
 
 		this.searchBox = this._register(this.instantiationService.createInstance(SuggestEnabledInput, `${VIEWLET_ID}.searchbox`, header, {
 			triggerCharacters: ['@'],
