b915673f7b2de2d8859617937fef341a7e3e727c
Ladislau Szomoru <3372902+lszomoru@users.noreply.github.com>
2021-06-07 14:10:11 +0200

    Fix #125569 (#125650)

--- src/vs/workbench/contrib/workspace/browser/workspace.contribution.ts
+++ src/vs/workbench/contrib/workspace/browser/workspace.contribution.ts
@@ -442,7 +442,9 @@ export class WorkspaceTrustUXHandler extends Disposable implements IWorkbenchCon
 			}));
 			// TODO: Consider moving the check into setWorkspaceTrust()
 			if (this.workspaceTrustManagementService.canSetWorkspaceTrust()) {
-				this.workspaceTrustManagementService.setWorkspaceTrust(this.configurationService.getValue<boolean>(WORKSPACE_TRUST_EMPTY_WINDOW) ?? false);
+				if (this.configurationService.getValue<boolean>(WORKSPACE_TRUST_EMPTY_WINDOW)) {
+					this.workspaceTrustManagementService.setWorkspaceTrust(true);
+				}
 			}
 		}
 	}
