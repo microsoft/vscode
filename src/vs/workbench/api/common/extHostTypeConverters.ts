d5ede18fef572a3f0e7978510e977a24cf10cf74
Don Jayamanne <don.jayamanne@yahoo.com>
2021-06-07 21:46:03 -0700

    Why is the test failing

--- src/vs/workbench/api/common/extHostTypeConverters.ts
+++ src/vs/workbench/api/common/extHostTypeConverters.ts
@@ -1460,7 +1460,8 @@ export namespace NotebookData {
 
 	export function from(data: vscode.NotebookData): notebooks.NotebookDataDto {
 		const res: notebooks.NotebookDataDto = {
-			metadata: isEmptyObject(data.metadata) ? Object.create(null) : data.metadata,
+			metadata: Object.create(null),
+			// metadata: isEmptyObject(data.metadata) ? Object.create(null) : data.metadata,
 			cells: [],
 		};
 		for (let cell of data.cells) {
