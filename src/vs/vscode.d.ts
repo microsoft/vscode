73db949c97816fb6d9896596f94113be3ea16181
Daniel Imms <2193314+Tyriar@users.noreply.github.com>
2021-06-15 03:33:23 -0700

    Add jsdoc for terminal profile/link APIs

--- src/vs/vscode.d.ts
+++ src/vs/vscode.d.ts
@@ -5812,6 +5812,16 @@ declare module 'vscode' {
 		 */
 		tooltip?: string;
 
+		/**
+		 * Creates a new terminal link.
+		 * @param startIndex The start index of the link on {@link TerminalLinkContext.line}.
+		 * @param length The length of the link on {@link TerminalLinkContext.line}.
+		 * @param tooltip The tooltip text when you hover over this link.
+		 *
+		 * If a tooltip is provided, is will be displayed in a string that includes instructions on
+		 * how to trigger the link, such as `{0} (ctrl + click)`. The specific instructions vary
+		 * depending on OS, user settings, and localization.
+		 */
 		constructor(startIndex: number, length: number, tooltip?: string);
 	}
 
@@ -5837,6 +5847,10 @@ declare module 'vscode' {
 		 */
 		options: TerminalOptions | ExtensionTerminalOptions;
 
+		/**
+		 * Creates a new terminal profile.
+		 * @param options The options that the terminal will launch with.
+		 */
 		constructor(options: TerminalOptions | ExtensionTerminalOptions);
 	}
 
