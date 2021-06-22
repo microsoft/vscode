71771bbb53e2b18ce527c6f743e87ced8bd732ca
rebornix <penn.lv@gmail.com>
2021-06-21 18:11:45 -0700

    Revert "dispose restored editor input."
    This reverts commit 4da81059653e0c0c2d61ca92da89e8b1d498afa7.

--- src/vs/workbench/services/history/browser/history.ts
+++ src/vs/workbench/services/history/browser/history.ts
@@ -951,11 +951,7 @@ export class HistoryService extends Disposable implements IHistoryService {
 
 			// Cleanup any listeners associated with the input when removing from history
 			if (matches) {
-				this.clearOnEditorDispose(e, this.editorHistoryListeners);
-				if (e instanceof EditorInput && e.resource && !this.editorService.isOpened({ resource: e.resource, typeId: e.typeId })) {
-					// the editor input in history is never opened in editors (restored from history)
-					e.dispose();
-				}
+				this.clearOnEditorDispose(arg1, this.editorHistoryListeners);
 			}
 
 			return !matches;
