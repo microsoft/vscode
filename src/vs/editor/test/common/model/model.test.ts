be9ba61e2a952424e246eb77a8bee5543df41a68
Henning Dieterichs <hdieterichs@microsoft.com>
2021-06-24 17:00:25 +0200

    Fixes test.

--- src/vs/editor/test/common/model/model.test.ts
+++ src/vs/editor/test/common/model/model.test.ts
@@ -133,7 +133,7 @@ suite('Editor Model - Model', () => {
 		assert.deepStrictEqual(e, new ModelRawContentChangedEvent(
 			[
 				new ModelRawLineChanged(1, 'My new line', null),
-				new ModelRawLinesInserted(2, 2, ['No longer First Line'], []),
+				new ModelRawLinesInserted(2, 2, ['No longer First Line'], [null]),
 			],
 			2,
 			false,
