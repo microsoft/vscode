d98cd850ae8727b06dc871d14503b6237b28b87b
Alex Dima <alexdima@microsoft.com>
2021-06-27 22:35:17 +0200

    Add support for unique web worker extension host origins

--- src/vs/base/common/product.ts
+++ src/vs/base/common/product.ts
@@ -51,6 +51,7 @@ export interface IProductConfiguration {
 	readonly downloadUrl?: string;
 	readonly updateUrl?: string;
 	readonly webEndpointUrl?: string;
+	readonly webEndpointUrlTemplate?: string;
 	readonly target?: string;
 
 	readonly settingsSearchBuildId?: number;
