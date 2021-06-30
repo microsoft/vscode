16ed03a205348d585222398f8a63e66febe67888
Daniel Imms <2193314+Tyriar@users.noreply.github.com>
2021-06-30 13:19:37 -0700

    Remove order from changeicon/color menu entries
    Fixes #127704

--- src/vs/workbench/contrib/terminal/browser/terminalMenus.ts
+++ src/vs/workbench/contrib/terminal/browser/terminalMenus.ts
@@ -394,8 +394,7 @@ export function setupTerminalMenus(): void {
 						id: TerminalCommandId.ChangeIcon,
 						title: localize('workbench.action.terminal.changeIcon', "Change Icon...")
 					},
-					group: ContextMenuGroup.Edit,
-					order: 3
+					group: ContextMenuGroup.Edit
 				}
 			},
 			{
@@ -405,8 +404,7 @@ export function setupTerminalMenus(): void {
 						id: TerminalCommandId.ChangeColor,
 						title: localize('workbench.action.terminal.changeColor', "Change Color...")
 					},
-					group: ContextMenuGroup.Edit,
-					order: 4
+					group: ContextMenuGroup.Edit
 				}
 			},
 			{
