5358f4b40971b4b82b871ed5fe6225f80390e803
daviddossett <ddossett@microsoft.com>
2021-06-30 16:46:26 -0700

    Move SVG styling to address PR feedback

--- src/vs/workbench/contrib/markdown/common/markdownDocumentRenderer.ts
+++ src/vs/workbench/contrib/markdown/common/markdownDocumentRenderer.ts
@@ -27,16 +27,6 @@ img {
 	max-height: 100%;
 }
 
-svg {
-	width: 100%;
-	height: 100%;
-	position: fixed;
-	top: 0;
-	left: 0;
-	bottom: 0;
-	right: 0;
-}
-
 a {
 	text-decoration: none;
 }
--- src/vs/workbench/contrib/welcome/gettingStarted/browser/gettingStarted.ts
+++ src/vs/workbench/contrib/welcome/gettingStarted/browser/gettingStarted.ts
@@ -665,6 +665,15 @@ export class GettingStartedPage extends EditorPane {
 				<style nonce="${nonce}">
 					${DEFAULT_MARKDOWN_STYLES}
 					${css}
+					svg {
+						width: 100%;
+						height: 100%;
+						position: fixed;
+						top: 0;
+						left: 0;
+						bottom: 0;
+						right: 0;
+					}
 				</style>
 			</head>
 			<body>
