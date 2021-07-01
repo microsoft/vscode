578face03e79ba1380ac0928f308d7bfe8c2e317
Jackson Kearl <jakearl@microsoft.com>
2021-07-01 09:19:39 -0600

    Add elipses ref #127428

--- src/vs/workbench/contrib/welcome/gettingStarted/browser/gettingStarted.ts
+++ src/vs/workbench/contrib/welcome/gettingStarted/browser/gettingStarted.ts
@@ -1045,7 +1045,7 @@ export class GettingStartedPage extends EditorPane {
 			5,
 			undefined,
 			undefined,
-			$('button.button-link.see-all-walkthroughs', { 'tabindex': '0', 'x-dispatch': 'seeAllWalkthroughs' }, localize('seeMore', "Show All")),
+			$('button.button-link.see-all-walkthroughs', { 'tabindex': '0', 'x-dispatch': 'seeAllWalkthroughs' }, localize('showAll', "Show All...")),
 			renderGetttingStaredWalkthrough);
 
 		gettingStartedList.onDidChange(() => {
