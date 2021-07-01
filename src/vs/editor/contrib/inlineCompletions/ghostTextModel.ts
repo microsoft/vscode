60f21d95258e1611d9cda450e303d126ccdcc560
Henning Dieterichs <hdieterichs@microsoft.com>
2021-07-01 10:42:19 +0200

    Caches ghost text to improve performance.

--- src/vs/editor/contrib/inlineCompletions/ghostTextModel.ts
+++ src/vs/editor/contrib/inlineCompletions/ghostTextModel.ts
@@ -18,21 +18,31 @@ export abstract class DelegatingModel extends Disposable implements GhostTextWid
 	private readonly onDidChangeEmitter = new Emitter<void>();
 	public readonly onDidChange = this.onDidChangeEmitter.event;
 
+	private hasCachedGhostText = false;
+	private cachedGhostText: GhostText | undefined;
+
 	private readonly currentModelRef = this._register(new MutableDisposable<IReference<GhostTextWidgetModel>>());
 	protected get targetModel(): GhostTextWidgetModel | undefined {
 		return this.currentModelRef.value?.object;
 	}
 
 	protected setTargetModel(model: GhostTextWidgetModel | undefined): void {
+		this.currentModelRef.clear();
 		this.currentModelRef.value = model ? createDisposableRef(model, model.onDidChange(() => {
+			this.hasCachedGhostText = false;
 			this.onDidChangeEmitter.fire();
 		})) : undefined;
 
+		this.hasCachedGhostText = false;
 		this.onDidChangeEmitter.fire();
 	}
 
 	public get ghostText(): GhostText | undefined {
-		return this.targetModel?.ghostText;
+		if (!this.hasCachedGhostText) {
+			this.cachedGhostText = this.currentModelRef.value?.object?.ghostText;
+			this.hasCachedGhostText = true;
+		}
+		return this.cachedGhostText;
 	}
 
 	public setExpanded(expanded: boolean): void {
