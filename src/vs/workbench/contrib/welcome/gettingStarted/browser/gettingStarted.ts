4de68c74a0cfb627b7becaae588f1c171006d637
daviddossett <ddossett@microsoft.com>
2021-07-07 14:45:40 -0700

    Initial commit

--- src/vs/workbench/contrib/welcome/gettingStarted/browser/gettingStarted.ts
+++ src/vs/workbench/contrib/welcome/gettingStarted/browser/gettingStarted.ts
@@ -667,13 +667,12 @@ export class GettingStartedPage extends EditorPane {
 					${DEFAULT_MARKDOWN_STYLES}
 					${css}
 					svg {
-						width: 100%;
-						height: 100%;
 						position: fixed;
-						top: 0;
-						left: 0;
-						bottom: 0;
-						right: 0;
+						height: 100%;
+						width: 80%;
+						left: 50%;
+						top: 50%;
+						transform: translate(-50%,-50%);
 					}
 				</style>
 			</head>
