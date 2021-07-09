afc650cec6926ac31d201224bb0b7e80e78f6d8a
SteVen Batten <steven.m.batten@outlook.com>
2021-07-09 10:44:31 -0700

    fix symbol gulp task for win32

--- build/gulpfile.scan.js
+++ build/gulpfile.scan.js
@@ -49,7 +49,7 @@ BUILD_TARGETS.forEach(buildTarget => {
 	if (platform === 'win32') {
 		tasks.push(
 			() => electron.dest(destinationPdb, _.extend({}, config, { platform, arch: arch === 'armhf' ? 'arm' : arch, pdbs: true })),
-			() => util.rimraf(path.join(destinationExe, 'd3dcompiler_47.dll')));
+			util.rimraf(path.join(destinationExe, 'd3dcompiler_47.dll')));
 	}
 
 	// cleanup and node modules
