69a882951c9b5fe99f20fdefb2f218b022035d08
Johannes Rieken <johannes.rieken@gmail.com>
2021-07-19 12:48:18 +0200

    some more jsdoc for https://github.com/microsoft/vscode/issues/15533

--- src/vs/vscode.proposed.d.ts
+++ src/vs/vscode.proposed.d.ts
@@ -3068,41 +3068,65 @@ declare module 'vscode' {
 
 
 	//#region https://github.com/microsoft/vscode/issues/15533 --- Type hierarchy --- @eskibear
+
+	/**
+	 * Represents an item of a type hierarchy, like a class or an interface.
+	 */
 	export class TypeHierarchyItem {
 		/**
 		 * The name of this item.
 		 */
 		name: string;
+
 		/**
 		 * The kind of this item.
 		 */
 		kind: SymbolKind;
+
 		/**
 		 * Tags for this item.
 		 */
 		tags?: ReadonlyArray<SymbolTag>;
+
 		/**
 		 * More detail for this item, e.g. the signature of a function.
 		 */
 		detail?: string;
+
 		/**
 		 * The resource identifier of this item.
 		 */
 		uri: Uri;
+
 		/**
 		 * The range enclosing this symbol not including leading/trailing whitespace
 		 * but everything else, e.g. comments and code.
 		 */
 		range: Range;
+
 		/**
 		 * The range that should be selected and revealed when this symbol is being
 		 * picked, e.g. the name of a class. Must be contained by the {@link TypeHierarchyItem.range range}-property.
 		 */
 		selectionRange: Range;
 
+		/**
+		 * Creates a new type hierarchy item.
+		 *
+		 * @param kind The kind of the item.
+		 * @param name The name of the item.
+		 * @param detail The details of the item.
+		 * @param uri The Uri of the item.
+		 * @param range The whole range of the item.
+		 * @param selectionRange The selection range of the item.
+		 */
 		constructor(kind: SymbolKind, name: string, detail: string, uri: Uri, range: Range, selectionRange: Range);
 	}
 
+	/**
+	 * The type hierarchy provider interface describes the contract between extensions
+	 * and the type hierarchy feature.
+	 */
 	export interface TypeHierarchyProvider {
 
 		/**
