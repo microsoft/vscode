3f9cd31660859ea03860d76399540918042198f9
Connor Peet <connor@peet.io>
2021-07-23 16:45:12 -0700

    testing: peek and explorer ux feedback

--- src/vs/workbench/common/actions.ts
+++ src/vs/workbench/common/actions.ts
@@ -128,6 +128,7 @@ Registry.add(Extensions.WorkbenchActions, new class implements IWorkbenchActionR
 export const CATEGORIES = {
 	View: { value: localize('view', "View"), original: 'View' },
 	Help: { value: localize('help', "Help"), original: 'Help' },
+	Test: { value: localize('test', "Test"), original: 'Test' },
 	Preferences: { value: localize('preferences', "Preferences"), original: 'Preferences' },
 	Developer: { value: localize({ key: 'developer', comment: ['A developer on Code itself or someone diagnosing issues in Code'] }, "Developer"), original: 'Developer' }
 };
