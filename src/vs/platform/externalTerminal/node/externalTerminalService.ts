34419f85beaa090c80c92d540ad0f11f97a4132c
Daniel Imms <2193314+Tyriar@users.noreply.github.com>
2021-07-27 13:38:23 -0700

    Clone process.env to ensure source of truth is not changed
    Part of #128745

--- src/vs/platform/externalTerminal/node/externalTerminalService.ts
+++ src/vs/platform/externalTerminal/node/externalTerminalService.ts
@@ -312,7 +312,7 @@ export class LinuxExternalTerminalService extends ExternalTerminalService implem
 }
 
 function getSanitizedEnvironment(process: NodeJS.Process) {
-	const env = process.env;
+	const env = { ...process.env };
 	// Refs https://github.com/microsoft/vscode/issues/128745#issuecomment-885981676
 	sanitizeProcessEnvironment(env, 'VSCODE_BROWSER_CODE_LOADING');
 	return env;
