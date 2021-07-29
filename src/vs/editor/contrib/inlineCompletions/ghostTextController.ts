7ec64f0dad5dbf5fbc4fe2b1f9383844c29095b7
Henning Dieterichs <hdieterichs@microsoft.com>
2021-07-29 23:27:10 +0200

    Improves inline suggestions to work with screenreaders.

--- src/vs/editor/contrib/inlineCompletions/ghostTextController.ts
+++ src/vs/editor/contrib/inlineCompletions/ghostTextController.ts
@@ -15,6 +15,7 @@ import * as nls from 'vs/nls';
 import { ContextKeyExpr, IContextKeyService, RawContextKey } from 'vs/platform/contextkey/common/contextkey';
 import { IInstantiationService } from 'vs/platform/instantiation/common/instantiation';
 import { GhostTextModel } from 'vs/editor/contrib/inlineCompletions/ghostTextModel';
+import { KeybindingsRegistry } from 'vs/platform/keybinding/common/keybindingsRegistry';
 
 export class GhostTextController extends Disposable {
 	public static readonly inlineSuggestionVisible = new RawContextKey<boolean>('inlineSuggestionVisible', false, nls.localize('inlineSuggestionVisible', "Whether an inline suggestion is visible"));
@@ -28,7 +29,7 @@ export class GhostTextController extends Disposable {
 
 	private triggeredExplicitly = false;
 	protected readonly activeController = this._register(new MutableDisposable<ActiveGhostTextController>());
-	private get activeModel(): GhostTextModel | undefined {
+	public get activeModel(): GhostTextModel | undefined {
 		return this.activeController.value?.model;
 	}
 
@@ -168,21 +169,23 @@ const GhostTextCommand = EditorCommand.bindToContribution(GhostTextController.ge
 
 export const commitInlineSuggestionAction = new GhostTextCommand({
 	id: 'editor.action.inlineSuggest.commit',
-	precondition: ContextKeyExpr.and(
-		GhostTextController.inlineSuggestionVisible,
-		GhostTextController.inlineSuggestionHasIndentation.toNegated(),
-		EditorContextKeys.tabMovesFocus.toNegated()
-	),
-	kbOpts: {
-		weight: 200,
-		primary: KeyCode.Tab,
-	},
+	precondition: GhostTextController.inlineSuggestionVisible,
 	handler(x) {
 		x.commit();
 		x.editor.focus();
 	}
 });
 registerEditorCommand(commitInlineSuggestionAction);
+KeybindingsRegistry.registerKeybindingRule({
+	primary: KeyCode.Tab,
+	weight: 200,
+	id: commitInlineSuggestionAction.id,
+	when: ContextKeyExpr.and(
+		commitInlineSuggestionAction.precondition,
+		EditorContextKeys.tabMovesFocus.toNegated(),
+		GhostTextController.inlineSuggestionHasIndentation.toNegated()
+	),
+});
 
 registerEditorCommand(new GhostTextCommand({
 	id: 'editor.action.inlineSuggest.hide',
