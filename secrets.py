fe99ddb1f4a8e58624fdb35f0e45982cd97d0275
Jacob Lerner <jlerner@chanzuckerberg.com>
2021-08-03 11:21:43 -0400

    test python secrets

---
+++ secrets.py
@@ -0,0 +1,5 @@
+# ruleid: detected-aws-secret-access-key
+aws_secret_access_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+
+# ruleid: detected-aws-secret-access-key
+aws_secret_access_key: "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
