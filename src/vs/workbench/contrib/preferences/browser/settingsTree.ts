38695658dc342f70f61da56a4968ced1b5b189ed
Matt Bierner <matb@microsoft.com>
2021-08-03 12:59:54 -0700

    Register disposable

--- src/vs/workbench/contrib/preferences/browser/settingsTree.ts
+++ src/vs/workbench/contrib/preferences/browser/settingsTree.ts
@@ -645,7 +645,7 @@ export abstract class AbstractSettingRenderer extends Disposable implements ITre
 	) {
 		super();
 
-		this.markdownRenderer = _instantiationService.createInstance(MarkdownRenderer, {});
+		this.markdownRenderer = this._register(_instantiationService.createInstance(MarkdownRenderer, {}));
 
 		this.ignoredSettings = getIgnoredSettings(getDefaultIgnoredSettings(), this._configService);
 		this._register(this._configService.onDidChangeConfiguration(e => {
