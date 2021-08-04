a9fa47da31e43a63665abd1580d4c8b101916396
Rob Lourens <roblourens@gmail.com>
2021-08-04 13:49:13 -0700

    new

--- extensions/vscode-api-tests/src/singlefolder-tests/ipynb.test.ts
+++ extensions/vscode-api-tests/src/singlefolder-tests/ipynb.test.ts
@@ -6,8 +6,9 @@
 import * as assert from 'assert';
 import 'mocha';
 import * as vscode from 'vscode';
+import { createRandomFile } from '../utils';
 
-suite('ipynb NotebookSerializer', function () {
+suite.only('ipynb NotebookSerializer', function () {
 	test('Can open an ipynb notebook', async () => {
 		console.log(`1`);
 		assert.ok(vscode.workspace.workspaceFolders);
@@ -20,6 +21,24 @@ suite('ipynb NotebookSerializer', function () {
 		console.log('stat', stat);
 		console.log('size:' + stat.size);
 		const notebook = await vscode.workspace.openNotebookDocument(uri);
+		console.log(`3`);
+		await vscode.window.showNotebookDocument(notebook);
+		console.log(`4`);
+
+		const notebookEditor = vscode.window.activeNotebookEditor;
+		assert.ok(notebookEditor);
+
+		assert.strictEqual(notebookEditor.document.cellCount, 2);
+		assert.strictEqual(notebookEditor.document.cellAt(0).kind, vscode.NotebookCellKind.Markup);
+		assert.strictEqual(notebookEditor.document.cellAt(1).kind, vscode.NotebookCellKind.Code);
+		assert.strictEqual(notebookEditor.document.cellAt(1).outputs.length, 1);
+		console.log(`5`);
+	});
+	test('Can open an ipynb notebook - tmp', async () => {
+		console.log(`1`);
+		const randomFile = await createRandomFile('', undefined, '.ipynb');
+		console.log(`2`);
+		const notebook = await vscode.workspace.openNotebookDocument(randomFile);
 		console.log(`3`);
 		await vscode.window.showNotebookDocument(notebook);
 		console.log(`4`);
