cf3e81f96a648535578978ba1f64a1de89611648
Rob Lourens <roblourens@gmail.com>
2021-08-05 11:31:10 -0700

    tmp contents

--- extensions/vscode-api-tests/src/singlefolder-tests/ipynb.test.ts
+++ extensions/vscode-api-tests/src/singlefolder-tests/ipynb.test.ts
@@ -34,8 +34,8 @@ suite.only('ipynb NotebookSerializer', function () {
 		assert.strictEqual(notebookEditor.document.cellAt(1).outputs.length, 1);
 		console.log(`5`);
 	});
-	
-	test('Can open an ipynb notebook - tmp', async () => {
+
+	test('Can open an ipynb notebook - tmp empty', async () => {
 		console.log(`1`);
 		const randomFile = await createRandomFile('', undefined, '.ipynb');
 		console.log(`2`);
@@ -52,27 +52,74 @@ suite.only('ipynb NotebookSerializer', function () {
 		console.log(`5`);
 	});
 
-	test('Can open an ipynb notebook 2', async () => {
+	test('Can open an ipynb notebook - tmp contents', async () => {
+		const contents = `
+{
+ "cells": [
+  {
+   "cell_type": "markdown",
+   "source": [
+    "## Header"
+   ],
+   "metadata": {}
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 2,
+   "source": [
+    "print('hello 1')\n",
+    "print('hello 2')"
+   ],
+   "outputs": [
+    {
+     "output_type": "stream",
+     "name": "stdout",
+     "text": [
+      "hello 1\n",
+      "hello 2\n"
+     ]
+    }
+   ],
+   "metadata": {}
+  }
+ ],
+ "metadata": {
+  "interpreter": {
+   "hash": "815c6b7592bf74925ca002a1774bcf064bae9d6a27e7933fd9109275fb484258"
+  },
+  "kernelspec": {
+   "name": "python3",
+   "display_name": "Python 3.9.5 64-bit ('myvenv': venv)"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.9.5"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+} `;
 		console.log(`1`);
-		assert.ok(vscode.workspace.workspaceFolders);
-		const workspace = vscode.workspace.workspaceFolders[0];
-		const uri = vscode.Uri.joinPath(workspace.uri, 'test.ipynb');
+		const randomFile = await createRandomFile(contents, undefined, '.ipynb');
 		console.log(`2`);
-		console.log(uri.toString());
-		const stat = await vscode.workspace.fs.stat(uri);
-		console.log(`2a`);
-		console.log('stat', stat);
-		console.log('size:' + stat.size);
-		await vscode.commands.executeCommand('vscode.openWith', uri, 'jupyter-notebook');
+		const notebook = await vscode.workspace.openNotebookDocument(randomFile);
 		console.log(`3`);
+		await vscode.window.showNotebookDocument(notebook);
+		console.log(`4`);
 
 		const notebookEditor = vscode.window.activeNotebookEditor;
 		assert.ok(notebookEditor);
 
-		assert.strictEqual(notebookEditor.document.cellCount, 2);
-		assert.strictEqual(notebookEditor.document.cellAt(0).kind, vscode.NotebookCellKind.Markup);
-		assert.strictEqual(notebookEditor.document.cellAt(1).kind, vscode.NotebookCellKind.Code);
-		assert.strictEqual(notebookEditor.document.cellAt(1).outputs.length, 1);
+		assert.strictEqual(notebookEditor.document.cellCount, 1);
+		assert.strictEqual(notebookEditor.document.cellAt(0).kind, vscode.NotebookCellKind.Code);
 		console.log(`5`);
 	});
 });
