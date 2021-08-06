ca904bb4c7e5085990947b9a7c5e076cca595841
Henning Dieterichs <hdieterichs@microsoft.com>
2021-08-06 11:27:21 +0200

    Fixes #127879.

--- src/vs/editor/common/viewModel/splitLinesCollection.ts
+++ src/vs/editor/common/viewModel/splitLinesCollection.ts
@@ -286,24 +286,12 @@ export class SplitLinesCollection implements IViewModelLinesCollection {
 
 		const linesContent = this.model.getLinesContent();
 		const injectedTextDecorations = this.model.getInjectedTextDecorations(this._editorId);
-		const injectedText = LineInjectedText.fromDecorations(injectedTextDecorations);
 		const lineCount = linesContent.length;
 		const lineBreaksComputer = this.createLineBreaksComputer();
-		const injectedTextLength = injectedText.length;
-		let injectedTextIndex = 0;
-		let nextLineNumberWithInjectedText = (injectedTextIndex < injectedTextLength ? injectedText[injectedTextIndex].lineNumber : lineCount + 1);
-		for (let i = 0; i < lineCount; i++) {
-			let lineInjectedText: LineInjectedText[] | null = null;
-			if (i + 1 === nextLineNumberWithInjectedText) {
-				// There is some injected text on this line
-				lineInjectedText = [];
-				while (i + 1 === nextLineNumberWithInjectedText && injectedTextIndex < injectedTextLength) {
-					lineInjectedText.push(injectedText[injectedTextIndex]);
-					injectedTextIndex++;
-					nextLineNumberWithInjectedText = (injectedTextIndex < injectedTextLength ? injectedText[injectedTextIndex].lineNumber : lineCount + 1);
-				}
-			}
 
+		const injectedTextQueue = new arrays.ArrayQueue(LineInjectedText.fromDecorations(injectedTextDecorations));
+		for (let i = 0; i < lineCount; i++) {
+			const lineInjectedText = injectedTextQueue.takeWhile(t => t.lineNumber === i + 1);
 			lineBreaksComputer.addRequest(linesContent[i], lineInjectedText, previousLineBreaks ? previousLineBreaks[i] : null);
 		}
 		const linesBreaks = lineBreaksComputer.finalize();
