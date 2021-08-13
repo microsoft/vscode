57e8b9fc2bf632c4a86d7423fa81879631ee15d3
gjsjohnmurray <johnm@georgejames.com>
2021-08-13 09:13:05 +0100

    Suppress 'Open launch.json' command on error dialog if DA provided a command (#124020)

--- src/vs/workbench/contrib/debug/browser/rawDebugSession.ts
+++ src/vs/workbench/contrib/debug/browser/rawDebugSession.ts
@@ -713,9 +713,12 @@ export class RawDebugSession implements IDisposable {
 		const url = error?.url;
 		if (error && url) {
 			const label = error.urlLabel ? error.urlLabel : nls.localize('moreInfo', "More Info");
+			const uri = URI.parse(url);
+			// Use a suffixed id if uri invokes a command, so default 'Open launch.json' command is suppressed on dialog
+			const actionId = uri.scheme === 'command' ? 'debug.moreInfo.command' : 'debug.moreInfo';
 			return errors.createErrorWithActions(userMessage, {
-				actions: [new Action('debug.moreInfo', label, undefined, true, async () => {
-					this.openerService.open(URI.parse(url), { allowCommands: true });
+				actions: [new Action(actionId, label, undefined, true, async () => {
+					this.openerService.open(uri, { allowCommands: true });
 				})]
 			});
 		}
