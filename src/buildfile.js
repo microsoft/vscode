b3ae959c76666fbe81fecd859db9b81df8960fa4
Alex Dima <alexdima@microsoft.com>
2021-08-13 18:04:11 +0300

    Avoid duplicating modules in worker bundles

--- src/buildfile.js
+++ src/buildfile.js
@@ -3,11 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-const { createModuleDescription } = require('./vs/base/buildfile');
-
-function entrypoint(name) {
-	return [{ name: name, include: [], exclude: ['vs/css', 'vs/nls'] }];
-}
+const { createModuleDescription, createEditorWorkerModuleDescription } = require('./vs/base/buildfile');
 
 exports.base = [{
 	name: 'vs/base/common/worker/simpleWorker',
@@ -17,9 +13,9 @@ exports.base = [{
 	dest: 'vs/base/worker/workerMain.js'
 }];
 
-exports.workerExtensionHost = [createModuleDescription('vs/workbench/services/extensions/worker/extensionHostWorker')];
-exports.workerNotebook = [createModuleDescription('vs/workbench/contrib/notebook/common/services/notebookSimpleWorker')];
-exports.workerLanguageDetection = [createModuleDescription('vs/workbench/services/languageDetection/browser/languageDetectionSimpleWorker')];
+exports.workerExtensionHost = [createEditorWorkerModuleDescription('vs/workbench/services/extensions/worker/extensionHostWorker')];
+exports.workerNotebook = [createEditorWorkerModuleDescription('vs/workbench/contrib/notebook/common/services/notebookSimpleWorker')];
+exports.workerLanguageDetection = [createEditorWorkerModuleDescription('vs/workbench/services/languageDetection/browser/languageDetectionSimpleWorker')];
 
 exports.workbenchDesktop = require('./vs/workbench/buildfile.desktop').collectModules();
 exports.workbenchWeb = require('./vs/workbench/buildfile.web').collectModules();
