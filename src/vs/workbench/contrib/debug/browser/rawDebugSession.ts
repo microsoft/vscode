3cb18e4115e79186d3313a09374a9dc355a3243a
gjsjohnmurray <johnm@georgejames.com>
2021-08-13 10:58:37 +0100

    use Schemas.command constant instead of string

--- src/vs/workbench/contrib/debug/browser/rawDebugSession.ts
+++ src/vs/workbench/contrib/debug/browser/rawDebugSession.ts
@@ -18,6 +18,7 @@ import { IDisposable, dispose } from 'vs/base/common/lifecycle';
 import { CancellationToken } from 'vs/base/common/cancellation';
 import { INotificationService, Severity } from 'vs/platform/notification/common/notification';
 import { IDialogService } from 'vs/platform/dialogs/common/dialogs';
+import { Schemas } from 'vs/base/common/network';
 
 /**
  * This interface represents a single command line argument split into a "prefix" and a "path" half.
@@ -715,7 +716,7 @@ export class RawDebugSession implements IDisposable {
 			const label = error.urlLabel ? error.urlLabel : nls.localize('moreInfo', "More Info");
 			const uri = URI.parse(url);
 			// Use a suffixed id if uri invokes a command, so default 'Open launch.json' command is suppressed on dialog
-			const actionId = uri.scheme === 'command' ? 'debug.moreInfo.command' : 'debug.moreInfo';
+			const actionId = uri.scheme === Schemas.command ? 'debug.moreInfo.command' : 'debug.moreInfo';
 			return errors.createErrorWithActions(userMessage, {
 				actions: [new Action(actionId, label, undefined, true, async () => {
 					this.openerService.open(uri, { allowCommands: true });
