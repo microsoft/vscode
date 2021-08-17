1c1b2dea5cb7aefa18bb6cbd2056f710c8c652a9
Andre Weinand <aweinand@microsoft.com>
2021-08-17 15:30:08 +0200

    DAP: add memory event

--- src/vs/workbench/contrib/debug/common/debugProtocol.d.ts
+++ src/vs/workbench/contrib/debug/common/debugProtocol.d.ts
@@ -398,6 +398,23 @@ declare module DebugProtocol {
 		};
 	}
 
+	/** Event message for 'memory' event type.
+		This event indicates that some memory range has been updated. It should only be sent if the debug adapter has received a value true for the `supportsMemoryEvent` capability of the `initialize` request.
+		Clients typically react to the event by re-issuing a `readMemory` request if they show the memory identified by the `memoryReference` and if the updated memory range overlaps the displayed range. Clients should not make assumptions how individual memory references relate to each other, so they should not assume that they are part of a single continuous address range and might overlap.
+		Debug adapters can use this event to indicate that the contents of a memory range has changed due to some other DAP request like `setVariable` or `setExpression`. Debug adapters are not expected to emit this event for each and every memory change of a running program, because that information is typically not available from debuggers and it would flood clients with too many events.
+	*/
+	export interface MemoryEvent extends Event {
+		// event: 'memory';
+		body: {
+			/** Memory reference of a memory range that has been updated. */
+			memoryReference: string;
+			/** Starting offset in bytes where memory has been updated. Can be negative. */
+			offset: number;
+			/** Number of bytes updated. */
+			count: number;
+		};
+	}
+
 	/** RunInTerminal request; value of command field is 'runInTerminal'.
 		This optional request is sent from the debug adapter to the client to run a command in a terminal.
 		This is typically used to launch the debuggee in a terminal provided by the client.
@@ -474,6 +491,8 @@ declare module DebugProtocol {
 		supportsProgressReporting?: boolean;
 		/** Client supports the invalidated event. */
 		supportsInvalidatedEvent?: boolean;
+		/** Client supports the memory event. */
+		supportsMemoryEvent?: boolean;
 	}
 
 	/** Response to 'initialize' request. */
