7e02e93edfb8ced599921360db28e9fb3eed5ff1
Rob Lourens <roblourens@gmail.com>
2021-08-20 19:56:16 -0700

    Force EH search by default in Insiders

--- src/vs/workbench/contrib/search/browser/search.contribution.ts
+++ src/vs/workbench/contrib/search/browser/search.contribution.ts
@@ -24,6 +24,7 @@ import { registerSingleton } from 'vs/platform/instantiation/common/extensions';
 import { IInstantiationService, ServicesAccessor } from 'vs/platform/instantiation/common/instantiation';
 import { KeybindingsRegistry, KeybindingWeight } from 'vs/platform/keybinding/common/keybindingsRegistry';
 import { IListService, WorkbenchListFocusContextKey, WorkbenchObjectTree } from 'vs/platform/list/browser/listService';
+import product from 'vs/platform/product/common/product';
 import { Extensions as QuickAccessExtensions, IQuickAccessRegistry } from 'vs/platform/quickinput/common/quickAccess';
 import { IQuickInputService } from 'vs/platform/quickinput/common/quickInput';
 import { Registry } from 'vs/platform/registry/common/platform';
@@ -1020,6 +1021,11 @@ configurationRegistry.registerConfiguration({
 				nls.localize('searchSortOrder.countAscending', "Results are sorted by count per file, in ascending order.")
 			],
 			'description': nls.localize('search.sortOrder', "Controls sorting order of search results.")
+		},
+		'search.experimental.forceExtensionHostSearch': {
+			type: 'boolean',
+			default: product.quality !== 'stable',
+			description: nls.localize('search.experimental.forceExtensionHostSearch', "When enabled, search always runs in the extension host instead of a separate search process.")
 		}
 	}
 });
