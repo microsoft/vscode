577cf13db95f16f49ab87533455ea8b765ce9503
Alex Dima <alexdima@microsoft.com>
2021-08-24 15:59:50 +0200

    Add Open in browser troubleshooting action

--- src/vs/workbench/services/remote/electron-sandbox/remoteAgentServiceImpl.ts
+++ src/vs/workbench/services/remote/electron-sandbox/remoteAgentServiceImpl.ts
@@ -12,13 +12,15 @@ import { AbstractRemoteAgentService } from 'vs/workbench/services/remote/common/
 import { ISignService } from 'vs/platform/sign/common/sign';
 import { ILogService } from 'vs/platform/log/common/log';
 import { IWorkbenchEnvironmentService } from 'vs/workbench/services/environment/common/environmentService';
-import { INotificationService, Severity } from 'vs/platform/notification/common/notification';
+import { INotificationService, IPromptChoice, Severity } from 'vs/platform/notification/common/notification';
 import { Registry } from 'vs/platform/registry/common/platform';
 import { IWorkbenchContribution, IWorkbenchContributionsRegistry, Extensions } from 'vs/workbench/common/contributions';
 import { LifecyclePhase } from 'vs/workbench/services/lifecycle/common/lifecycle';
 import { ITelemetryService } from 'vs/platform/telemetry/common/telemetry';
 import { getRemoteName } from 'vs/platform/remote/common/remoteHosts';
 import { INativeHostService } from 'vs/platform/native/electron-sandbox/native';
+import { URI } from 'vs/base/common/uri';
+import { IOpenerService } from 'vs/platform/opener/common/opener';
 
 export class RemoteAgentService extends AbstractRemoteAgentService implements IRemoteAgentService {
 	constructor(
@@ -35,14 +37,16 @@ export class RemoteAgentService extends AbstractRemoteAgentService implements IR
 class RemoteConnectionFailureNotificationContribution implements IWorkbenchContribution {
 
 	constructor(
-		@IRemoteAgentService remoteAgentService: IRemoteAgentService,
+		@IRemoteAgentService private readonly _remoteAgentService: IRemoteAgentService,
 		@INotificationService notificationService: INotificationService,
 		@IWorkbenchEnvironmentService environmentService: IWorkbenchEnvironmentService,
 		@ITelemetryService telemetryService: ITelemetryService,
 		@INativeHostService nativeHostService: INativeHostService,
+		@IRemoteAuthorityResolverService private readonly _remoteAuthorityResolverService: IRemoteAuthorityResolverService,
+		@IOpenerService openerService: IOpenerService,
 	) {
 		// Let's cover the case where connecting to fetch the remote extension info fails
-		remoteAgentService.getRawEnvironment()
+		this._remoteAgentService.getRawEnvironment()
 			.then(undefined, err => {
 
 				type RemoteConnectionFailureClassification = {
@@ -62,16 +66,42 @@ class RemoteConnectionFailureNotificationContribution implements IWorkbenchContr
 				});
 
 				if (!RemoteAuthorityResolverError.isHandled(err)) {
+					const choices: IPromptChoice[] = [
+						{
+							label: nls.localize('devTools', "Open Developer Tools"),
+							run: () => nativeHostService.openDevTools()
+						}
+					];
+					const troubleshootingURL = this._getTroubleshootingURL();
+					if (troubleshootingURL) {
+						choices.push({
+							label: nls.localize('directUrl', "Open in browser"),
+							run: () => openerService.open(troubleshootingURL, { openExternal: true })
+						});
+					}
 					notificationService.prompt(
 						Severity.Error,
 						nls.localize('connectionError', "Failed to connect to the remote extension host server (Error: {0})", err ? err.message : ''),
-						[{
-							label: nls.localize('devTools', "Open Developer Tools"),
-							run: () => nativeHostService.openDevTools()
-						}]
+						choices
 					);
 				}
 			});
+	}
+
+	private _getTroubleshootingURL(): URI | null {
+		const remoteAgentConnection = this._remoteAgentService.getConnection();
+		if (!remoteAgentConnection) {
+			return null;
+		}
+		const connectionData = this._remoteAuthorityResolverService.getConnectionData(remoteAgentConnection.remoteAuthority);
+		if (!connectionData) {
+			return null;
+		}
+		return URI.from({
+			scheme: 'http',
+			authority: `${connectionData.host}:${connectionData.port}`,
+			path: `/version`
+		});
 	}
 
 }
