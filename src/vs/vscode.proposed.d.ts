b2f0855a7d139a62cbeb64896ffe613f5a208018
Daniel Imms <2193314+Tyriar@users.noreply.github.com>
2021-08-24 04:46:29 -0700

    Clarify extensions/commands can trigger interactedWith
    Fixes #131497

--- src/vs/vscode.proposed.d.ts
+++ src/vs/vscode.proposed.d.ts
@@ -896,8 +896,8 @@ declare module 'vscode' {
 		/**
 		 * Whether the {@link Terminal} has been interacted with. Interaction means that the
 		 * terminal has sent data to the process which depending on the terminal's _mode_. By
-		 * default input is sent when a key is pressed but based on the terminal's mode it can also
-		 * happen on:
+		 * default input is sent when a key is pressed or when a command or extension sends text,
+		 * but based on the terminal's mode it can also happen on:
 		 *
 		 * - a pointer click event
 		 * - a pointer scroll event
