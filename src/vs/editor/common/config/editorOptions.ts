6f6bcfe764f0a1e7eca38fa037c93df8fc86135d
Henning Dieterichs <hdieterichs@microsoft.com>
2021-08-25 09:55:39 +0200

    Fixes CI failure.

--- src/vs/editor/common/config/editorOptions.ts
+++ src/vs/editor/common/config/editorOptions.ts
@@ -12,7 +12,6 @@ import { USUAL_WORD_SEPARATORS } from 'vs/editor/common/model/wordHelper';
 import { AccessibilitySupport } from 'vs/platform/accessibility/common/accessibility';
 import { IConfigurationPropertySchema } from 'vs/platform/configuration/common/configurationRegistry';
 import { IJSONSchema } from 'vs/base/common/jsonSchema';
-import product from 'vs/platform/product/common/product';
 
 //#region typed options
 
