c4c7a3e0649cbe6bb8f8e34a79bae2c9f368db9e
Jack Clayton <77730378+jackos@users.noreply.github.com>
2021-09-01 21:59:28 +0800

    Added close field in vscode.d.ts interface TaskPresntationOptions (#131127)

--- src/vs/vscode.proposed.d.ts
+++ src/vs/vscode.proposed.d.ts
@@ -1062,6 +1062,11 @@ declare module 'vscode' {
 		 * Controls whether the task is executed in a specific terminal group using split panes.
 		 */
 		group?: string;
+
+		/** 
+		 * Controls whether the terminal is closed after executing the task.
+		 */
+		close?: boolean;
 	}
 	//#endregion
 
