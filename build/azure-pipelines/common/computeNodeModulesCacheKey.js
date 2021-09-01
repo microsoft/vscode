0afa4cd1eb29a446c0a6bceec5b0341cba7a6072
Alexandru Dima <alexdima@microsoft.com>
2021-09-01 11:41:17 +0200

    push compilation output

--- build/azure-pipelines/common/computeNodeModulesCacheKey.js
+++ build/azure-pipelines/common/computeNodeModulesCacheKey.js
@@ -13,8 +13,17 @@ const shasum = crypto.createHash('sha1');
 shasum.update(fs.readFileSync(path.join(ROOT, 'build/.cachesalt')));
 shasum.update(fs.readFileSync(path.join(ROOT, '.yarnrc')));
 shasum.update(fs.readFileSync(path.join(ROOT, 'remote/.yarnrc')));
-// Add `yarn.lock` files
+// Add `package.json` and `yarn.lock` files
 for (let dir of dirs) {
+    const packageJsonPath = path.join(ROOT, dir, 'package.json');
+    const packageJson = JSON.parse(fs.readFileSync(packageJsonPath).toString());
+    const relevantPackageJsonSections = {
+        dependencies: packageJson.dependencies,
+        devDependencies: packageJson.devDependencies,
+        optionalDependencies: packageJson.optionalDependencies,
+        resolutions: packageJson.resolutions
+    };
+    shasum.update(JSON.stringify(relevantPackageJsonSections));
     const yarnLockPath = path.join(ROOT, dir, 'yarn.lock');
     shasum.update(fs.readFileSync(yarnLockPath));
 }
