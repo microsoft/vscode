45a1f368f0e4a63d4a99a700ac4d0ede6065bbf6
Alex Ross <alros@microsoft.com>
2021-09-02 11:32:14 +0200

    Tree dnd notes

--- src/vs/vscode.proposed.d.ts
+++ src/vs/vscode.proposed.d.ts
@@ -1034,7 +1034,7 @@ declare module 'vscode' {
 		 * The type for tree elements is text/treeitem.
 		 * For example, you can reconstruct the your tree elements:
 		 * ```ts
-		 * JSON.parse(await (items.get('text/treeitems')!.asString()))
+		 * JSON.parse(await (items.get('text/treeitem')!.asString()))
 		 * ```
 		 */
 		items: { get: (mimeType: string) => TreeDataTransferItem | undefined };
@@ -1043,7 +1043,19 @@ declare module 'vscode' {
 	export interface DragAndDropController<T> extends Disposable {
 		readonly supportedTypes: string[];
 
-		// todo@API maybe
+		/**
+		 * todo@API maybe
+		 *
+		 * When the user drops an item from this DragAndDropController on **another tree item** in **the same tree**,
+		 * `onWillDrop` will be called with the dropped tree item. This is the DragAndDropController's opportunity to
+		 * package the data from the dropped tree item into whatever format they want the target tree item to receive.
+		 *
+		 * The returned `TreeDataTransfer` will be merged with the original`TreeDataTransfer` for the operation.
+		 *
+		 * Note for implementation later: This means that the `text/treeItem` mime type will go away.
+		 *
+		 * @param source
+		 */
 		// onWillDrop?(source: T): Thenable<TreeDataTransfer>;
 
 		/**
