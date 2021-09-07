e4fb11a1e759042aebcbd10376e62227f53ea5fc
AkatQuas <295140755@qq.com>
2021-09-07 16:28:21 +0800

    fix: early return on cancellation

--- extensions/html-language-features/server/src/utils/runner.ts
+++ extensions/html-language-features/server/src/utils/runner.ts
@@ -23,6 +23,7 @@ export function runSafe<T>(runtime: RuntimeEnvironment, func: () => Thenable<T>,
 		runtime.timer.setImmediate(() => {
 			if (token.isCancellationRequested) {
 				resolve(cancelValue());
+				return;
 			}
 			return func().then(result => {
 				if (token.isCancellationRequested) {
