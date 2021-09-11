5fdbacb4d3ec4951cf92ce7a129a280141b458fd
L <82917144+l198881@users.noreply.github.com>
2021-09-11 10:36:38 -0300

    Update and rename editorService.test.ts to ModuleName_and_editorService.test.ts

--- src/vs/workbench/services/editor/test/browser/ModuleName_and_editorService.test.ts
+++ src/vs/workbench/services/editor/test/browser/ModuleName_and_editorService.test.ts
@@ -1,3 +1,16 @@
+# Retrieve the name of the module that converts the root path of your project to a underscore, i.e. Fw/Types becomes Fw_Types.
+get_module_name("${CMAKE_CURRENT_LIST_DIR}")
+
+# Tell the compiler the path to the library header file.
+include_directories("${PROJECT_SOURCE_DIR}/libs")
+
+# Find all the libraries contained in the directory you specified.
+find_library(LIBQUATERNION Quaternion "${PROJECT_SOURCE_DIR}/libs")
+# Note: in my case the LIBQUATERNION equals /home/ThibFrgsGmz/Documents/CODES/fprime-poc/libs/libQuaternion.a
+
+# Specify to the linker the libraries to use when linking the given component and its dependents. 
+target_link_libraries("${MODULE_NAME}" ${LIBQUATERNION})
+
 /*---------------------------------------------------------------------------------------------
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
