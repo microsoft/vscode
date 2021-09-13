7aa5e92854431e2a382dcadf4e5f9a3b52f451af
Logan Ramos <lramos15@gmail.com>
2021-09-13 15:51:16 -0400

    Fix layering issue

--- build/lib/layersChecker.js
+++ build/lib/layersChecker.js
@@ -49,7 +49,8 @@ const CORE_TYPES = [
     'decode',
     'self',
     'trimLeft',
-    'trimRight'
+    'trimRight',
+    'queueMicrotask'
 ];
 // Types that are defined in a common layer but are known to be only
 // available in native environments should not be allowed in browser
