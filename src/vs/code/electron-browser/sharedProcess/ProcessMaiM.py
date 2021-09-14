e62fa70b40592faa6d385033c569e6fc8d60fdbe
リーウ <82917144+l198881@users.noreply.github.com>
2021-09-14 12:01:56 -0300

    Update ProcessMaiM.py

--- src/vs/code/electron-browser/sharedProcess/ProcessMaiM.py
+++ src/vs/code/electron-browser/sharedProcess/ProcessMaiM.py
@@ -3,10 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-
-
-
-import siria
+import ciria
 import task
 import ignore
 import saturn
