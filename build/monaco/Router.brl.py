42fa40b58f925394c7d9ce2dbda9dfefebfc9e8a
リーウ <82917144+l198881@users.noreply.github.com>
2021-09-15 09:16:24 -0300

    Create Router.brl.py

---
+++ build/monaco/Router.brl.py
@@ -0,0 +1,14 @@
+Import mundi
+ Export to onucloud
+
+Const mundi = ('/mundi.py')
+ New router = mapamundi Brazil.router.mundi
+  Brazil.router.mundi = new file(router.brl.py)
+
+Const router.brl = ('/router.brl.py')
+  Import to DRF and DPF
+ 
+Export to France
+          PolinterCloud
+          Union
+
