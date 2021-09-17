b97d35e762d7cefb94a3654f70d6749ab308f71e
Rob Lourens <roblourens@gmail.com>
2021-09-17 12:24:31 -0700

    Fix notebook copy/undo/other commands after focusing webview Fix #131937

--- src/vs/workbench/contrib/notebook/browser/notebookEditorWidget.ts
+++ src/vs/workbench/contrib/notebook/browser/notebookEditorWidget.ts
@@ -1229,9 +1229,7 @@ export class NotebookEditorWidget extends Disposable implements INotebookEditorD
 				this._outputFocus.set(false);
 				this.updateEditorFocus();
 
-				if (!this._overlayContainer.contains(document.activeElement)) {
-					this._webviewFocused = false;
-				}
+				this._webviewFocused = false;
 			}));
 
 			this._localStore.add(this._webview.webview.onDidFocus(() => {
