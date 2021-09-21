9b0c64ca5019ff6958cb56ad68a75c85e8821c07
Henning Dieterichs <hdieterichs@microsoft.com>
2021-09-21 12:46:31 +0200

    Revert "Fixes #133353 by hiding ghost text on editor blur.", as this causes issues with the hover that steals the focus of the editor.
    This reverts commit c79d31a9ea119a0a69c1f4f09eaf200fc1d2bb8d.

--- src/vs/editor/contrib/inlineCompletions/inlineCompletionsModel.ts
+++ src/vs/editor/contrib/inlineCompletions/inlineCompletionsModel.ts
@@ -69,10 +69,6 @@ export class InlineCompletionsModel extends Disposable implements GhostTextWidge
 		this._register(toDisposable(() => {
 			this.disposed = true;
 		}));
-
-		this._register(this.editor.onDidBlurEditorText(() => {
-			this.hide();
-		}));
 	}
 
 	private handleUserInput() {
