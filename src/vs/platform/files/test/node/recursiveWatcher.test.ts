d940a2790d971ce1290b9aa1c644876bdc4979f5
Benjamin Pasero <benjamin.pasero@microsoft.com>
2021-09-22 18:32:35 +0200

    enable case rename on windows

--- src/vs/platform/files/test/node/recursiveWatcher.test.ts
+++ src/vs/platform/files/test/node/recursiveWatcher.test.ts
@@ -109,29 +109,29 @@ flakySuite('Recursive Watcher', () => {
 		await Promises.rename(newFolderPath, renamedFolderPath);
 		await changeFuture;
 
-		// TODO: case rename is currently broken
-		// if (isWindows || isMacintosh) {
+		// TODO: case rename is currently broken on macOS
+		if (isWindows) {
 
-		// 	// Rename file (same name, different case)
-		// 	const caseRenamedFilePath = join(testDir, 'deep', 'RenamedFile.txt');
-		// 	changeFuture = Promise.all([
-		// 		awaitEvent(service, renamedFilePath, FileChangeType.DELETED),
-		// 		awaitEvent(service, caseRenamedFilePath, FileChangeType.ADDED)
-		// 	]);
-		// 	await Promises.rename(renamedFilePath, caseRenamedFilePath);
-		// 	await changeFuture;
-		// 	renamedFilePath = caseRenamedFilePath;
+			// Rename file (same name, different case)
+			const caseRenamedFilePath = join(testDir, 'deep', 'RenamedFile.txt');
+			changeFuture = Promise.all([
+				awaitEvent(service, renamedFilePath, FileChangeType.DELETED),
+				awaitEvent(service, caseRenamedFilePath, FileChangeType.ADDED)
+			]);
+			await Promises.rename(renamedFilePath, caseRenamedFilePath);
+			await changeFuture;
+			renamedFilePath = caseRenamedFilePath;
 
-		// 	// Rename folder (same name, different case)
-		// 	const caseRenamedFolderPath = join(testDir, 'deep', 'REnamed Folder');
-		// 	changeFuture = Promise.all([
-		// 		awaitEvent(service, renamedFolderPath, FileChangeType.DELETED),
-		// 		awaitEvent(service, caseRenamedFolderPath, FileChangeType.ADDED)
-		// 	]);
-		// 	await Promises.rename(renamedFolderPath, caseRenamedFolderPath);
-		// 	await changeFuture;
-		// 	renamedFolderPath = caseRenamedFolderPath;
-		// }
+			// Rename folder (same name, different case)
+			const caseRenamedFolderPath = join(testDir, 'deep', 'REnamed Folder');
+			changeFuture = Promise.all([
+				awaitEvent(service, renamedFolderPath, FileChangeType.DELETED),
+				awaitEvent(service, caseRenamedFolderPath, FileChangeType.ADDED)
+			]);
+			await Promises.rename(renamedFolderPath, caseRenamedFolderPath);
+			await changeFuture;
+			renamedFolderPath = caseRenamedFolderPath;
+		}
 
 		// Move file
 		const movedFilepath = join(testDir, 'movedFile.txt');
