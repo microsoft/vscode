0ad3cca6b549b4fb6a268b2081959d38cf6fef04
SteVen Batten <6561887+sbatten@users.noreply.github.com>
2021-09-22 11:44:17 -0700

    Support cross-product FS-provided imports (#133116)
    * add the basics
    
    * remove unused param
    
    * increase clarity
    
    * activate provider
    
    * :lipstick: `doImport`
    
    * :lipstick:
    
    * fix issues
    
    Co-authored-by: Benjamin Pasero <benjamin.pasero@microsoft.com>

--- src/vs/workbench/contrib/files/browser/views/explorerViewer.ts
+++ src/vs/workbench/contrib/files/browser/views/explorerViewer.ts
@@ -54,7 +54,7 @@ import { EditorInput } from 'vs/workbench/common/editor/editorInput';
 import { IUriIdentityService } from 'vs/workbench/services/uriIdentity/common/uriIdentity';
 import { ResourceFileEdit } from 'vs/editor/browser/services/bulkEditService';
 import { IExplorerService } from 'vs/workbench/contrib/files/browser/files';
-import { BrowserFileUpload, NativeFileImport, getMultipleFilesOverwriteConfirm } from 'vs/workbench/contrib/files/browser/fileImportExport';
+import { BrowserFileUpload, ExternalFileImport, getMultipleFilesOverwriteConfirm } from 'vs/workbench/contrib/files/browser/fileImportExport';
 import { toErrorMessage } from 'vs/base/common/errorMessage';
 
 export class ExplorerDelegate implements IListVirtualDelegate<ExplorerItem> {
@@ -839,7 +839,7 @@ export class FileDragAndDrop implements ITreeDragAndDrop<ExplorerItem> {
 			if (!containsDragType(originalEvent, DataTransfers.FILES, CodeDataTransfers.FILES, DataTransfers.RESOURCES)) {
 				return false;
 			}
-			if (isWeb && originalEvent.dataTransfer?.types.indexOf('Files') === -1) {
+			if (isWeb && !containsDragType(originalEvent, 'Files')) {
 				// DnD from vscode to web is not supported #115535. Only if we are dragging from native finder / explorer then the "Files" data transfer will be set
 				return false;
 			}
@@ -980,14 +980,19 @@ export class FileDragAndDrop implements ITreeDragAndDrop<ExplorerItem> {
 
 		try {
 
-			// Desktop DND (Import file)
+			// External file DND (Import/Upload file)
 			if (data instanceof NativeDragAndDropData) {
-				if (isWeb) {
+				// Native OS file DND into Web
+				if (containsDragType(originalEvent, 'Files') && isWeb) {
 					const browserUpload = this.instantiationService.createInstance(BrowserFileUpload);
 					await browserUpload.upload(target, originalEvent);
-				} else {
-					const nativeImport = this.instantiationService.createInstance(NativeFileImport);
-					await nativeImport.import(resolvedTarget, originalEvent);
+				}
+				// 2 Cases handled for import:
+				// FS-Provided file DND into Web/Desktop
+				// Native OS file DND into Desktop
+				else {
+					const fileImport = this.instantiationService.createInstance(ExternalFileImport);
+					await fileImport.import(resolvedTarget, originalEvent);
 				}
 			}
 
