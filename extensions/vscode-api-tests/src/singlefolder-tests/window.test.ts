620a61f5a3dba1756b7978cdaef95a078581f4c1
Logan Ramos <lramos15@gmail.com>
2021-09-24 09:09:30 -0400

    Add more move tests

--- extensions/vscode-api-tests/src/singlefolder-tests/window.test.ts
+++ extensions/vscode-api-tests/src/singlefolder-tests/window.test.ts
@@ -436,6 +436,12 @@ suite('vscode API - window', () => {
 		// Tabs should now be B -> C -> A
 		assert.strictEqual(tabs[1].resource?.toString(), docC.uri.toString());
 
+		await tabs[1].move(1000, ViewColumn.Two);
+		assert.strictEqual(tabs.length, 3);
+		tabs = window.tabs;
+		// Tabs should still be B -> C -> A
+		assert.strictEqual(tabs[1].resource?.toString(), docC.uri.toString());
+
 		await tabs[1].move(0, ViewColumn.Three);
 		assert.strictEqual(tabs.length, 3);
 		tabs = window.tabs;
