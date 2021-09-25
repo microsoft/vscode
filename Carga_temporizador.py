bd777be6067e109f37911a605b7701d27d89f6b6
リーウ <82917144+l198881@users.noreply.github.com>
2021-09-25 02:38:20 -0300

    Create Carga_temporizador.py

---
+++ Carga_temporizador.py
@@ -0,0 +1,14 @@
+
+export to CI
+export to union
+export to OceaniaCloud
+import datetime from watch.sec
+import sthereos from arc('/Saturn.py') & arc('/mundi.py') 
+
+import watch.sec.carga
+ .start (count_down.time)-(count_watch.time)=watch.sec.now.online;
+  Return, pass 'carga' ? true or false;
+   Result =.     And import & add ciview to list (./farenheight.py)
+    Close watch.sec.carga
+
+
