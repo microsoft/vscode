a36d0f3d1188f95f895c5ce52a28a409e30b0440
jeanp413 <jeanp413@hotmail.com>
2021-09-29 09:25:46 -0500

    Compile integration test when running from build

--- resources/server/test/test-web-integration.sh
+++ resources/server/test/test-web-integration.sh
@@ -10,6 +10,28 @@ fi
 
 cd $ROOT
 
+if [[ -z "$VSCODE_REMOTE_SERVER_PATH" ]]; then
+	echo "Running Web integration tests out of sources."
+else
+	# Run from a built: need to compile all test extensions
+	# because we run extension tests from their source folders
+	# and the build bundles extensions into .build webpacked
+	yarn gulp 	compile-extension:vscode-api-tests \
+				compile-extension:vscode-colorize-tests \
+				compile-extension:vscode-custom-editor-tests \
+				compile-extension:markdown-language-features \
+				compile-extension:typescript-language-features \
+				compile-extension:emmet \
+				compile-extension:css-language-features-server \
+				compile-extension:html-language-features-server \
+				compile-extension:json-language-features-server \
+				compile-extension:git \
+				compile-extension:ipynb \
+				compile-extension-media
+
+	echo "Running Web integration tests with '$VSCODE_REMOTE_SERVER_PATH' as build."
+fi
+
 # Tests in the extension host
 TEST_SCRIPT="$ROOT/test/integration/browser/out/index.js"
 
