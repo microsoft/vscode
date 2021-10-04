54ffd1db0841b5451d39b17bc62f1f86ab899cf5
Johannes Rieken <johannes.rieken@gmail.com>
2021-10-04 14:54:15 +0200

    simplify equalsIgnoreCase and startsWithIgnoreCase, let them use compareSubstringIgnoreCase

--- src/vs/base/common/strings.ts
+++ src/vs/base/common/strings.ts
@@ -361,40 +361,8 @@ export function isUpperAsciiLetter(code: number): boolean {
 	return code >= CharCode.A && code <= CharCode.Z;
 }
 
-function isAsciiLetter(code: number): boolean {
-	return isLowerAsciiLetter(code) || isUpperAsciiLetter(code);
-}
-
 export function equalsIgnoreCase(a: string, b: string): boolean {
-	return a.length === b.length && doEqualsIgnoreCase(a, b);
-}
-
-function doEqualsIgnoreCase(a: string, b: string, stopAt = a.length): boolean {
-	for (let i = 0; i < stopAt; i++) {
-		const codeA = a.charCodeAt(i);
-		const codeB = b.charCodeAt(i);
-
-		if (codeA === codeB) {
-			continue;
-		}
-
-		// a-z A-Z
-		if (isAsciiLetter(codeA) && isAsciiLetter(codeB)) {
-			const diff = Math.abs(codeA - codeB);
-			if (diff !== 0 && diff !== 32) {
-				return false;
-			}
-		}
-
-		// Any other charcode
-		else {
-			if (String.fromCharCode(codeA).toLowerCase() !== String.fromCharCode(codeB).toLowerCase()) {
-				return false;
-			}
-		}
-	}
-
-	return true;
+	return a.length === b.length && compareSubstringIgnoreCase(a, b) === 0;
 }
 
 export function startsWithIgnoreCase(str: string, candidate: string): boolean {
@@ -403,7 +371,7 @@ export function startsWithIgnoreCase(str: string, candidate: string): boolean {
 		return false;
 	}
 
-	return doEqualsIgnoreCase(str, candidate, candidateLength);
+	return compareSubstringIgnoreCase(str, candidate, 0, candidateLength) === 0;
 }
 
 /**
