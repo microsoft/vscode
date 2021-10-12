a2827f81b5928349c7104660dec4b091ea07ac15
Don Jayamanne <don.jayamanne@yahoo.com>
2021-10-11 17:58:08 -0700

    oops

--- extensions/ipynb/src/cellIdService.ts
+++ extensions/ipynb/src/cellIdService.ts
@@ -3,7 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { ExtensionContext, NotebookCell, NotebookCellsChangeEvent, NotebookDocument, notebooks, workspace, WorkspaceEdit } from 'vscode';
+import { ExtensionContext, NotebookCellsChangeEvent, NotebookDocument, notebooks, workspace, WorkspaceEdit } from 'vscode';
 import { v4 as uuid } from 'uuid';
 import { getCellMetadata } from './serializers';
 import { CellMetadata } from './common';
