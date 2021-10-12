aa5a6405d2da32e46eb75d635e91cde9669b93c6
Henning Dieterichs <hdieterichs@microsoft.com>
2021-10-12 09:44:24 +0200

    Fixes test.

--- src/vs/editor/test/common/modes/testLanguageConfigurationService.ts
+++ src/vs/editor/test/common/modes/testLanguageConfigurationService.ts
@@ -3,26 +3,28 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 import { Emitter } from 'vs/base/common/event';
-import { Disposable } from 'vs/base/common/lifecycle';
+import { IDisposable } from 'vs/base/common/lifecycle';
 import { URI } from 'vs/base/common/uri';
 import { LanguageId, LanguageIdentifier } from 'vs/editor/common/modes';
 import { ILanguageConfigurationService, LanguageConfigurationRegistry, LanguageConfigurationServiceChangeEvent, ResolvedLanguageConfiguration } from 'vs/editor/common/modes/languageConfigurationRegistry';
 
-export class TestLanguageConfigurationService extends Disposable implements ILanguageConfigurationService {
+export class TestLanguageConfigurationService implements ILanguageConfigurationService {
 	_serviceBrand: undefined;
 
-	private readonly onDidChangeEmitter = new Emitter<LanguageConfigurationServiceChangeEvent>();
-	public readonly onDidChange = this.onDidChangeEmitter.event;
+	private registration: IDisposable | undefined = undefined;
 
-	constructor() {
-		super();
-
-		this._register(
-			LanguageConfigurationRegistry.onDidChange((e) => {
+	private readonly onDidChangeEmitter = new Emitter<LanguageConfigurationServiceChangeEvent>({
+		onFirstListenerAdd: () => {
+			this.registration = LanguageConfigurationRegistry.onDidChange((e) => {
 				this.onDidChangeEmitter.fire(new LanguageConfigurationServiceChangeEvent(e.languageIdentifier));
-			})
-		);
-	}
+			});
+		},
+		onLastListenerRemove: () => {
+			this.registration?.dispose();
+			this.registration = undefined;
+		}
+	});
+	public readonly onDidChange = this.onDidChangeEmitter.event;
 
 	getLanguageConfiguration(languageId: LanguageId, resource?: URI): ResolvedLanguageConfiguration {
 		return LanguageConfigurationRegistry.getLanguageConfiguration(languageId) ??
