1a2749d798846ddfae1ca309c09e1eca180d6352
Henning Dieterichs <hdieterichs@microsoft.com>
2021-10-13 16:19:54 +0200

    Fixes #132802 by making maxTokenizationLineLength configurable per language and reducing the default for TypeScript.

--- src/vs/workbench/services/textMate/browser/abstractTextMateService.ts
+++ src/vs/workbench/services/textMate/browser/abstractTextMateService.ts
@@ -280,7 +280,7 @@ export abstract class AbstractTextMateService extends Disposable implements ITex
 						this._onDidEncounterLanguage.fire(languageId);
 					}
 				});
-				return new TMTokenizationSupport(r.languageId, tokenization, this._configurationService);
+				return new TMTokenizationSupport(languageIdentifier, tokenization, this._configurationService);
 			} catch (err) {
 				onUnexpectedError(err);
 				return null;
@@ -418,16 +418,20 @@ class TMTokenizationSupport implements ITokenizationSupport {
 	private _maxTokenizationLineLength: number;
 
 	constructor(
-		languageId: LanguageId,
+		languageId: LanguageIdentifier,
 		actual: TMTokenization,
 		@IConfigurationService private readonly _configurationService: IConfigurationService,
 	) {
-		this._languageId = languageId;
+		this._languageId = languageId.id;
 		this._actual = actual;
-		this._maxTokenizationLineLength = this._configurationService.getValue<number>('editor.maxTokenizationLineLength');
+		this._maxTokenizationLineLength = this._configurationService.getValue<number>('editor.maxTokenizationLineLength', {
+			overrideIdentifier: languageId.language
+		});
 		this._configurationService.onDidChangeConfiguration(e => {
 			if (e.affectsConfiguration('editor.maxTokenizationLineLength')) {
-				this._maxTokenizationLineLength = this._configurationService.getValue<number>('editor.maxTokenizationLineLength');
+				this._maxTokenizationLineLength = this._configurationService.getValue<number>('editor.maxTokenizationLineLength', {
+					overrideIdentifier: languageId.language
+				});
 			}
 		});
 	}
