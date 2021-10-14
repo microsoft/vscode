46e8f2054fb0ebe943572c567ae04365f13aa269
Benjamin Pasero <benjamin.pasero@microsoft.com>
2021-10-14 08:45:05 +0200

    sandbox - make acquiring message port from sandbox reusable

--- src/vs/workbench/services/ipc/electron-sandbox/sharedProcessService.ts
+++ src/vs/workbench/services/ipc/electron-sandbox/sharedProcessService.ts
@@ -3,16 +3,14 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { Event } from 'vs/base/common/event';
-import { ipcMessagePort, ipcRenderer } from 'vs/base/parts/sandbox/electron-sandbox/globals';
 import { Client as MessagePortClient } from 'vs/base/parts/ipc/common/ipc.mp';
 import { IChannel, IServerChannel, getDelayedChannel } from 'vs/base/parts/ipc/common/ipc';
-import { generateUuid } from 'vs/base/common/uuid';
 import { ILogService } from 'vs/platform/log/common/log';
 import { Disposable } from 'vs/base/common/lifecycle';
 import { ISharedProcessService } from 'vs/platform/ipc/electron-sandbox/services';
 import { mark } from 'vs/base/common/performance';
 import { Barrier, timeout } from 'vs/base/common/async';
+import { acquirePort } from 'vs/base/parts/ipc/electron-sandbox/ipc.mp';
 
 export class SharedProcessService extends Disposable implements ISharedProcessService {
 
@@ -44,20 +42,9 @@ export class SharedProcessService extends Disposable implements ISharedProcessSe
 		// is more cruicial.
 		await Promise.race([this.restoredBarrier.wait(), timeout(2000)]);
 
+		// Acquire a message port connected to the shared process
 		mark('code/willConnectSharedProcess');
-
-		// Ask to create message channel inside the window
-		// and send over a UUID to correlate the response
-		const nonce = generateUuid();
-		ipcMessagePort.acquire('vscode:createSharedProcessMessageChannelResult', nonce);
-		ipcRenderer.send('vscode:createSharedProcessMessageChannel', nonce);
-
-		// Wait until the main side has returned the `MessagePort`
-		// We need to filter by the `nonce` to ensure we listen
-		// to the right response.
-		const onMessageChannelResult = Event.fromDOMEventEmitter<{ nonce: string, port: MessagePort, source: unknown }>(window, 'message', (e: MessageEvent) => ({ nonce: e.data, port: e.ports[0], source: e.source }));
-		const { port } = await Event.toPromise(Event.once(Event.filter(onMessageChannelResult, e => e.nonce === nonce && e.source === window)));
-
+		const port = await acquirePort('vscode:createSharedProcessMessageChannel', 'vscode:createSharedProcessMessageChannelResult');
 		mark('code/didConnectSharedProcess');
 		this.logService.trace('Renderer->SharedProcess#connect: connection established');
 
