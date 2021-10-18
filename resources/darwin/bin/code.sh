2006bb708ad54c472568c25ebaa02aa479d80862
Rob Tomson <rtomson@blizzard.com>
2021-10-18 16:28:56 -0700

    Remove python need for path lookup

--- resources/darwin/bin/code.sh
+++ resources/darwin/bin/code.sh
@@ -3,8 +3,7 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 
-function realpath() { python -c "import os,sys; print(os.path.realpath(sys.argv[1]))" "$0"; }
-CONTENTS="$(dirname "$(dirname "$(dirname "$(dirname "$(realpath "$0")")")")")"
+CONTENTS="$(cd "$(dirname "$0")/../../.."; pwd -P)"
 ELECTRON="$CONTENTS/MacOS/Electron"
 CLI="$CONTENTS/Resources/app/out/cli.js"
 ELECTRON_RUN_AS_NODE=1 "$ELECTRON" "$CLI" "$@"
