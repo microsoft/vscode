39e34688d5445f32dca6af2d5cc9fa1ed8ac2b29
SteVen Batten <sbatten@microsoft.com>
2021-10-19 12:18:24 -0700

    scm fix proposal

--- src/vs/workbench/services/progress/browser/progressService.ts
+++ src/vs/workbench/services/progress/browser/progressService.ts
@@ -46,8 +46,8 @@ export class ProgressService extends Disposable implements IProgressService {
 
 	async withProgress<R = unknown>(options: IProgressOptions, task: (progress: IProgress<IProgressStep>) => Promise<R>, onDidCancel?: (choice?: number) => void): Promise<R> {
 		const { location } = options;
-		if (typeof location === 'string') {
 
+		const handleStringLocation = (location: string) => {
 			const viewContainer = this.viewDescriptorService.getViewContainerById(location);
 			if (viewContainer) {
 				const viewContainerLocation = this.viewDescriptorService.getViewContainerLocation(viewContainer);
@@ -61,6 +61,10 @@ export class ProgressService extends Disposable implements IProgressService {
 			}
 
 			throw new Error(`Bad progress location: ${location}`);
+		};
+
+		if (typeof location === 'string') {
+			return handleStringLocation(location);
 		}
 
 		switch (location) {
@@ -78,7 +82,7 @@ export class ProgressService extends Disposable implements IProgressService {
 			case ProgressLocation.Explorer:
 				return this.withPaneCompositeProgress('workbench.view.explorer', ViewContainerLocation.Sidebar, task, { ...options, location });
 			case ProgressLocation.Scm:
-				return this.withPaneCompositeProgress('workbench.view.scm', ViewContainerLocation.Sidebar, task, { ...options, location });
+				return handleStringLocation('workbench.scm');
 			case ProgressLocation.Extensions:
 				return this.withPaneCompositeProgress('workbench.view.extensions', ViewContainerLocation.Sidebar, task, { ...options, location });
 			case ProgressLocation.Dialog:
