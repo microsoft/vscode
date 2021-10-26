dc1a6699060423b8c4d2ced736ad70195378fddf
Alex Ross <alros@microsoft.com>
2021-10-26 10:32:17 +0200

    Revert "Hardcode commit for webview endpoint in public product.json"
    This reverts commit 1100001c70e5cb8e99b67a009615d5801420edb3.

--- src/vs/base/common/product.ts
+++ src/vs/base/common/product.ts
@@ -144,6 +144,8 @@ export interface IProductConfiguration {
 	readonly 'configurationSync.store'?: ConfigurationSyncStore;
 
 	readonly darwinUniversalAssetId?: string;
+
+	readonly webviewContentExternalBaseUrlTemplate?: string;
 }
 
 export type ImportantExtensionTip = { name: string; languages?: string[]; pattern?: string; isExtensionPack?: boolean };
