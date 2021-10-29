9e096252ba36b3a3530897cc7924549df3f69865
SteVen Batten <6561887+sbatten@users.noreply.github.com>
2021-10-29 12:58:22 -0700

    fixes #136022 (#136054)

--- src/vs/workbench/browser/layout.ts
+++ src/vs/workbench/browser/layout.ts
@@ -1136,6 +1136,7 @@ export abstract class Layout extends Disposable implements IWorkbenchLayoutServi
 			this.state.zenMode.wasAuxiliaryBarPartVisible = this.isVisible(Parts.AUXILIARYBAR_PART);
 
 			this.setPanelHidden(true, true);
+			this.setAuxiliaryBarHidden(true, true);
 			this.setSideBarHidden(true, true);
 
 			if (config.hideActivityBar) {
@@ -1176,6 +1177,10 @@ export abstract class Layout extends Disposable implements IWorkbenchLayoutServi
 		else {
 			if (this.state.zenMode.wasPanelVisible) {
 				this.setPanelHidden(false, true);
+			}
+
+			if (this.state.zenMode.wasAuxiliaryBarPartVisible) {
+				this.setAuxiliaryBarHidden(false, true);
 			}
 
 			if (this.state.zenMode.wasSideBarVisible) {
