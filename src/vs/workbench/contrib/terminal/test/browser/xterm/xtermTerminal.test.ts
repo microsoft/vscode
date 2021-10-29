f7cfa23249f41dd0eeb3a84b614da6e8b07f85b4
Daniel Imms <2193314+Tyriar@users.noreply.github.com>
2021-10-29 09:37:57 -0700

    Test terminal react to theme changes

--- src/vs/workbench/contrib/terminal/test/browser/xterm/xtermTerminal.test.ts
+++ src/vs/workbench/contrib/terminal/test/browser/xterm/xtermTerminal.test.ts
@@ -8,14 +8,15 @@ import { XtermTerminal } from 'vs/workbench/contrib/terminal/browser/xterm/xterm
 import { TerminalConfigHelper } from 'vs/workbench/contrib/terminal/browser/terminalConfigHelper';
 import { TestInstantiationService } from 'vs/platform/instantiation/test/common/instantiationServiceMock';
 import { ITerminalConfigHelper, ITerminalConfiguration } from 'vs/workbench/contrib/terminal/common/terminal';
-import { strictEqual } from 'assert';
+import { deepStrictEqual, strictEqual } from 'assert';
 import { IConfigurationService } from 'vs/platform/configuration/common/configuration';
 import { TestConfigurationService } from 'vs/platform/configuration/test/common/testConfigurationService';
-import { TestThemeService } from 'vs/platform/theme/test/common/testThemeService';
+import { TestColorTheme, TestThemeService } from 'vs/platform/theme/test/common/testThemeService';
 import { IThemeService } from 'vs/platform/theme/common/themeService';
 import { IViewDescriptor, IViewDescriptorService, ViewContainerLocation } from 'vs/workbench/common/views';
 import { IEditorOptions } from 'vs/editor/common/config/editorOptions';
 import { Emitter } from 'vs/base/common/event';
+import { TERMINAL_BACKGROUND_COLOR, TERMINAL_FOREGROUND_COLOR, TERMINAL_CURSOR_FOREGROUND_COLOR, TERMINAL_CURSOR_BACKGROUND_COLOR, TERMINAL_SELECTION_BACKGROUND_COLOR } from 'vs/workbench/contrib/terminal/common/terminalColorRegistry';
 
 // async function writeP(terminal: XtermTerminal, data: string): Promise<void> {
 // 	return new Promise<void>(r => terminal.raw.write(data, r));
@@ -31,6 +32,7 @@ class TestViewDescriptorService implements Partial<IViewDescriptorService> {
 suite.only('LineDataEventAddon', () => {
 	let instantiationService: TestInstantiationService;
 	let configurationService: TestConfigurationService;
+	let themeService: TestThemeService;
 
 	let xterm: XtermTerminal;
 	let configHelper: ITerminalConfigHelper;
@@ -51,9 +53,11 @@ suite.only('LineDataEventAddon', () => {
 			}
 		});
 
+		themeService = new TestThemeService();
+
 		instantiationService = new TestInstantiationService();
 		instantiationService.stub(IConfigurationService, configurationService);
-		instantiationService.stub(IThemeService, new TestThemeService());
+		instantiationService.stub(IThemeService, themeService);
 		instantiationService.stub(IViewDescriptorService, new TestViewDescriptorService());
 
 		configHelper = instantiationService.createInstance(TerminalConfigHelper);
@@ -63,5 +67,101 @@ suite.only('LineDataEventAddon', () => {
 		xterm = instantiationService.createInstance(XtermTerminal, Terminal, configHelper);
 		strictEqual(xterm.raw.getOption('cols'), 80);
 		strictEqual(xterm.raw.getOption('rows'), 30);
+	});
+
+	test('should react to and apply theme changes', () => {
+		themeService.setTheme(new TestColorTheme({
+			[TERMINAL_BACKGROUND_COLOR]: '#000100',
+			[TERMINAL_FOREGROUND_COLOR]: '#000200',
+			[TERMINAL_CURSOR_FOREGROUND_COLOR]: '#000300',
+			[TERMINAL_CURSOR_BACKGROUND_COLOR]: '#000400',
+			[TERMINAL_SELECTION_BACKGROUND_COLOR]: '#000500',
+			'terminal.ansiBlack': '#010000',
+			'terminal.ansiRed': '#020000',
+			'terminal.ansiGreen': '#030000',
+			'terminal.ansiYellow': '#040000',
+			'terminal.ansiBlue': '#050000',
+			'terminal.ansiMagenta': '#060000',
+			'terminal.ansiCyan': '#070000',
+			'terminal.ansiWhite': '#080000',
+			'terminal.ansiBrightBlack': '#090000',
+			'terminal.ansiBrightRed': '#100000',
+			'terminal.ansiBrightGreen': '#110000',
+			'terminal.ansiBrightYellow': '#120000',
+			'terminal.ansiBrightBlue': '#130000',
+			'terminal.ansiBrightMagenta': '#140000',
+			'terminal.ansiBrightCyan': '#150000',
+			'terminal.ansiBrightWhite': '#160000',
+		}));
+		xterm = instantiationService.createInstance(XtermTerminal, Terminal, configHelper);
+		deepStrictEqual(xterm.raw.getOption('theme'), {
+			background: '#000100',
+			foreground: '#000200',
+			cursor: '#000300',
+			cursorAccent: '#000400',
+			selection: '#000500',
+			black: '#010000',
+			green: '#030000',
+			red: '#020000',
+			yellow: '#040000',
+			blue: '#050000',
+			magenta: '#060000',
+			cyan: '#070000',
+			white: '#080000',
+			brightBlack: '#090000',
+			brightRed: '#100000',
+			brightGreen: '#110000',
+			brightYellow: '#120000',
+			brightBlue: '#130000',
+			brightMagenta: '#140000',
+			brightCyan: '#150000',
+			brightWhite: '#160000',
+		});
+		themeService.setTheme(new TestColorTheme({
+			[TERMINAL_BACKGROUND_COLOR]: '#00010f',
+			[TERMINAL_FOREGROUND_COLOR]: '#00020f',
+			[TERMINAL_CURSOR_FOREGROUND_COLOR]: '#00030f',
+			[TERMINAL_CURSOR_BACKGROUND_COLOR]: '#00040f',
+			[TERMINAL_SELECTION_BACKGROUND_COLOR]: '#00050f',
+			'terminal.ansiBlack': '#01000f',
+			'terminal.ansiRed': '#02000f',
+			'terminal.ansiGreen': '#03000f',
+			'terminal.ansiYellow': '#04000f',
+			'terminal.ansiBlue': '#05000f',
+			'terminal.ansiMagenta': '#06000f',
+			'terminal.ansiCyan': '#07000f',
+			'terminal.ansiWhite': '#08000f',
+			'terminal.ansiBrightBlack': '#09000f',
+			'terminal.ansiBrightRed': '#10000f',
+			'terminal.ansiBrightGreen': '#11000f',
+			'terminal.ansiBrightYellow': '#12000f',
+			'terminal.ansiBrightBlue': '#13000f',
+			'terminal.ansiBrightMagenta': '#14000f',
+			'terminal.ansiBrightCyan': '#15000f',
+			'terminal.ansiBrightWhite': '#16000f',
+		}));
+		deepStrictEqual(xterm.raw.getOption('theme'), {
+			background: '#00010f',
+			foreground: '#00020f',
+			cursor: '#00030f',
+			cursorAccent: '#00040f',
+			selection: '#00050f',
+			black: '#01000f',
+			green: '#03000f',
+			red: '#02000f',
+			yellow: '#04000f',
+			blue: '#05000f',
+			magenta: '#06000f',
+			cyan: '#07000f',
+			white: '#08000f',
+			brightBlack: '#09000f',
+			brightRed: '#10000f',
+			brightGreen: '#11000f',
+			brightYellow: '#12000f',
+			brightBlue: '#13000f',
+			brightMagenta: '#14000f',
+			brightCyan: '#15000f',
+			brightWhite: '#16000f',
+		});
 	});
 });
