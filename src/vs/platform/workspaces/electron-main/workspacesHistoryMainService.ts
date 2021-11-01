6f3f66426186f834caf3eff8f46e15b1b3fe5d2a
MVelez94 <william.giraldo@correounivalle.edu.co>
2021-11-01 02:47:06 -0500

    Fix #131708 (#135741)
    Co-authored-by: William Giraldo <william.giraldo@nuorder.com>

--- src/vs/platform/workspaces/electron-main/workspacesHistoryMainService.ts
+++ src/vs/platform/workspaces/electron-main/workspacesHistoryMainService.ts
@@ -41,7 +41,7 @@ export interface IWorkspacesHistoryMainService {
 
 export class WorkspacesHistoryMainService extends Disposable implements IWorkspacesHistoryMainService {
 
-	private static readonly MAX_TOTAL_RECENT_ENTRIES = 100;
+	private static readonly MAX_TOTAL_RECENT_ENTRIES = 500;
 
 	private static readonly MAX_MACOS_DOCK_RECENT_WORKSPACES = 7; 		// prefer higher number of workspaces...
 	private static readonly MAX_MACOS_DOCK_RECENT_ENTRIES_TOTAL = 10; 	// ...over number of files
