a8e37a4b43398363fbe832bc47e5314853be96a9
Alex Dima <alexdima@microsoft.com>
2021-11-08 11:09:21 +0100

    Kill extension host processes immediately when doing extension test

--- src/vs/platform/extensions/electron-main/directMainProcessExtensionHostStarter.ts
+++ src/vs/platform/extensions/electron-main/directMainProcessExtensionHostStarter.ts
@@ -3,6 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+import { IEnvironmentMainService } from 'vs/platform/environment/electron-main/environmentMainService';
 import { ExtensionHostStarter, IPartialLogService } from 'vs/platform/extensions/node/extensionHostStarter';
 import { ILifecycleMainService } from 'vs/platform/lifecycle/electron-main/lifecycleMainService';
 import { ILogService } from 'vs/platform/log/common/log';
@@ -11,16 +12,24 @@ export class DirectMainProcessExtensionHostStarter extends ExtensionHostStarter 
 
 	constructor(
 		@ILogService logService: IPartialLogService,
-		@ILifecycleMainService lifecycleMainService: ILifecycleMainService
+		@ILifecycleMainService lifecycleMainService: ILifecycleMainService,
+		@IEnvironmentMainService environmentService: IEnvironmentMainService,
 	) {
 		super(logService);
 
 		lifecycleMainService.onWillShutdown((e) => {
-			const exitPromises: Promise<void>[] = [];
-			for (const [, extHost] of this._extHosts) {
-				exitPromises.push(extHost.waitForExit(6000));
+			if (environmentService.extensionTestsLocationURI) {
+				// extension testing => don't wait for graceful shutdown
+				for (const [, extHost] of this._extHosts) {
+					extHost.kill();
+				}
+			} else {
+				const exitPromises: Promise<void>[] = [];
+				for (const [, extHost] of this._extHosts) {
+					exitPromises.push(extHost.waitForExit(6000));
+				}
+				e.join(Promise.all(exitPromises).then(() => { }));
 			}
-			e.join(Promise.all(exitPromises).then(() => { }));
 		});
 	}
 
