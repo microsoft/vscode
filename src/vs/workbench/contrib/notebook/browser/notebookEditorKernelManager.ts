f87615dfb076d157772048f11f0b4e25c76f5c29
Don Jayamanne <don.jayamanne@outlook.com>
2021-11-10 08:10:10 -0800

    Use suggested kernel if we have only one kernel (#136548)

--- src/vs/workbench/contrib/notebook/browser/notebookEditorKernelManager.ts
+++ src/vs/workbench/contrib/notebook/browser/notebookEditorKernelManager.ts
@@ -25,7 +25,7 @@ export class NotebookEditorKernelManager extends Disposable {
 	getSelectedOrSuggestedKernel(notebook: INotebookTextModel): INotebookKernel | undefined {
 		// returns SELECTED or the ONLY available kernel
 		const info = this._notebookKernelService.getMatchingKernel(notebook);
-		return info.selected ?? info.suggestions[0];
+		return info.selected ?? (info.all.length === 1 ? info.all[0] : undefined);
 	}
 
 	async executeNotebookCells(notebook: INotebookTextModel, cells: Iterable<ICellViewModel>): Promise<void> {
