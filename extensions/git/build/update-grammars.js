5590631c300e0b1cd5565025e7c28aacceb3b13c
Ladislau Szomoru <3372902+lszomoru@users.noreply.github.com>
2021-11-11 15:53:15 +0100

    Extract diff language into its own extension

--- extensions/git/build/update-grammars.js
+++ extensions/git/build/update-grammars.js
@@ -8,9 +8,3 @@ var updateGrammar = require('vscode-grammar-updater');
 
 updateGrammar.update('textmate/git.tmbundle', 'Syntaxes/Git%20Commit%20Message.tmLanguage', './syntaxes/git-commit.tmLanguage.json');
 updateGrammar.update('textmate/git.tmbundle', 'Syntaxes/Git%20Rebase%20Message.tmLanguage', './syntaxes/git-rebase.tmLanguage.json');
-updateGrammar.update('textmate/diff.tmbundle', 'Syntaxes/Diff.plist', './syntaxes/diff.tmLanguage.json');
-
-
-
-
-
