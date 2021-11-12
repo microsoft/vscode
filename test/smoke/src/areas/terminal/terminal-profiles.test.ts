a8e93d9afd6b86f2b4219e4b9c8b11e3ce1d43d0
Megan Rogge <merogge@microsoft.com>
2021-11-12 14:22:35 -0800

    improve readability

--- test/smoke/src/areas/terminal/terminal-profiles.test.ts
+++ test/smoke/src/areas/terminal/terminal-profiles.test.ts
@@ -8,14 +8,17 @@ import { ParsedArgs } from 'minimist';
 import { Application, IElement } from '../../../../automation';
 import { afterSuite, beforeSuite } from '../../utils';
 
-const DROPDOWN_BUTTON_SELECTOR = 'li.action-item.monaco-dropdown-with-primary > div.dropdown-action-container > div > div > a';
-const PLUS_BUTTON_SELECTOR = 'li.action-item.monaco-dropdown-with-primary > div.action-container.menu-entry > a';
-const ACTION_ITEMS_SELECTOR = 'li.action-item.monaco-dropdown-with-primary > div.dropdown-action-container > div > div.shadow-root-host';
-const SINGLE_TAB_SELECTOR = '.single-terminal-tab';
-const TABS_LIST_SELECTOR = '#terminal > div > div > div.monaco-scrollable-element > div.split-view-container > div > div > div.pane-body.integrated-terminal.wide > div.monaco-split-view2.horizontal > div.monaco-scrollable-element > div.split-view-container > div:nth-child(2) > div > div > div > div > div > div.monaco-list-rows';
-const FIRST_TAB_LABEL_SELECTOR = '#list_id_1_0 > div > div > div.monaco-icon-label-container > span.monaco-icon-name-container > a > span';
-const SECOND_TAB_LABEL_SELECTOR = '#list_id_1_1 > div > div > div.monaco-icon-label-container > span.monaco-icon-name-container > a > span';
-const CONTRIBUTED_PROFILE_NAME = `JavaScript Debug Terminal`;
+declare enum Selector {
+	DropdownButton = 'li.action-item.monaco-dropdown-with-primary > div.dropdown-action-container > div > div > a',
+	PlusButton = 'li.action-item.monaco-dropdown-with-primary > div.action-container.menu-entry > a',
+	ActionItems = 'li.action-item.monaco-dropdown-with-primary > div.dropdown-action-container > div > div.shadow-root-host',
+	SingleTab = '.single-terminal-tab',
+	TabsList = '#terminal > div > div > div.monaco-scrollable-element > div.split-view-container > div > div > div.pane-body.integrated-terminal.wide > div.monaco-split-view2.horizontal > div.monaco-scrollable-element > div.split-view-container > div:nth-child(2) > div > div > div > div > div > div.monaco-list-rows',
+	FirstTabLabel = '#list_id_1_0 > div > div > div.monaco-icon-label-container > span.monaco-icon-name-container > a > span',
+	SecondTabLabel = '#list_id_1_1 > div > div > div.monaco-icon-label-container > span.monaco-icon-name-container > a > span'
+}
+
+const ContributedProfileName = `JavaScript Debug Terminal`;
 
 export function setup(opts: ParsedArgs) {
 	describe.only('Terminal Profiles', () => {
@@ -34,14 +37,14 @@ export function setup(opts: ParsedArgs) {
 
 		it('should launch the default profile', async function () {
 			await app.workbench.terminal.showTerminal();
-			const singleTab = await app.code.waitForElement(SINGLE_TAB_SELECTOR);
-			ok(!singleTab.textContent.includes(CONTRIBUTED_PROFILE_NAME));
+			const singleTab = await app.code.waitForElement(Selector.SingleTab);
+			ok(!singleTab.textContent.includes(ContributedProfileName));
 		});
 
 		it('dropdown menu should be populated with profiles', async function () {
 			await app.workbench.terminal.showTerminal();
-			await app.code.waitAndClick(DROPDOWN_BUTTON_SELECTOR);
-			const actionItems = await app.code.waitForElements(ACTION_ITEMS_SELECTOR, true);
+			await app.code.waitAndClick(Selector.DropdownButton);
+			const actionItems = await app.code.waitForElements(Selector.ActionItems, true);
 			const labels: string[] = [];
 			const actionLabels: IElement[] = [];
 			for (const c of actionItems[0].children) {
@@ -55,65 +58,65 @@ export function setup(opts: ParsedArgs) {
 
 		it('clicking the plus button should create a terminal and display the tabs view', async function () {
 			await app.workbench.terminal.showTerminal();
-			await app.code.waitAndClick(PLUS_BUTTON_SELECTOR);
-			const tabs = await app.code.waitForElements(TABS_LIST_SELECTOR, true);
+			await app.code.waitAndClick(Selector.PlusButton);
+			const tabs = await app.code.waitForElements(Selector.TabsList, true);
 			deepStrictEqual(tabs[0].children.length, 2);
 			await app.workbench.terminal.killTerminal();
 		});
 
 		it('createWithProfile command should create a terminal with the default profile', async function () {
 			await app.workbench.terminal.runProfileCommand('createInstance');
-			const singleTab = await app.code.waitForElement(SINGLE_TAB_SELECTOR);
-			ok(!singleTab.textContent.includes(CONTRIBUTED_PROFILE_NAME));
+			const singleTab = await app.code.waitForElement(Selector.SingleTab);
+			ok(!singleTab.textContent.includes(ContributedProfileName));
 		});
 
 		it('createWithProfile command should create a terminal with a contributed profile', async function () {
 			await app.workbench.terminal.runProfileCommand('createInstance', true);
-			const singleTab = await app.code.waitForElement(SINGLE_TAB_SELECTOR);
-			ok(singleTab.textContent.includes(CONTRIBUTED_PROFILE_NAME));
+			const singleTab = await app.code.waitForElement(Selector.SingleTab);
+			ok(singleTab.textContent.includes(ContributedProfileName));
 		});
 
 		it('createWithProfile command should create a split terminal with the default profile', async function () {
 			await app.workbench.terminal.showTerminal();
 			await app.workbench.terminal.runProfileCommand('createInstance', undefined, true);
-			const tabs = await app.code.waitForElements(TABS_LIST_SELECTOR, true);
+			const tabs = await app.code.waitForElements(Selector.TabsList, true);
 			deepStrictEqual(tabs[0].children.length, 2);
-			const firstTab = await app.code.waitForElements(FIRST_TAB_LABEL_SELECTOR, true);
-			const secondTab = await app.code.waitForElements(SECOND_TAB_LABEL_SELECTOR, true);
+			const firstTab = await app.code.waitForElements(Selector.FirstTabLabel, true);
+			const secondTab = await app.code.waitForElements(Selector.SecondTabLabel, true);
 			ok(firstTab[0].textContent.includes('┌'));
 			ok(secondTab[0].textContent.includes('└'));
-			ok(!firstTab[0].textContent.includes(CONTRIBUTED_PROFILE_NAME));
-			ok(!secondTab[0].textContent.includes(CONTRIBUTED_PROFILE_NAME));
+			ok(!firstTab[0].textContent.includes(ContributedProfileName));
+			ok(!secondTab[0].textContent.includes(ContributedProfileName));
 			await app.workbench.terminal.killTerminal();
 		});
 
 		it('createWithProfile command should create a split terminal with a contributed profile', async function () {
 			await app.workbench.terminal.showTerminal();
 			await app.workbench.terminal.runProfileCommand('createInstance', true, true);
-			const tabs = await app.code.waitForElements(TABS_LIST_SELECTOR, true);
+			const tabs = await app.code.waitForElements(Selector.TabsList, true);
 			deepStrictEqual(tabs[0].children.length, 2);
-			const firstTab = await app.code.waitForElements(FIRST_TAB_LABEL_SELECTOR, true);
-			const secondTab = await app.code.waitForElements(SECOND_TAB_LABEL_SELECTOR, true);
+			const firstTab = await app.code.waitForElements(Selector.FirstTabLabel, true);
+			const secondTab = await app.code.waitForElements(Selector.SecondTabLabel, true);
 			ok(firstTab[0].textContent.includes('┌'));
 			ok(secondTab[0].textContent.includes('└'));
-			ok(!firstTab[0].textContent.includes(CONTRIBUTED_PROFILE_NAME));
-			ok(secondTab[0].textContent.includes(CONTRIBUTED_PROFILE_NAME));
+			ok(!firstTab[0].textContent.includes(ContributedProfileName));
+			ok(secondTab[0].textContent.includes(ContributedProfileName));
 			await app.workbench.terminal.killTerminal();
 		});
 
 		it('should set the default profile', async function () {
 			await app.workbench.terminal.runProfileCommand('setDefault', undefined);
 			await app.workbench.terminal.createNew();
-			const singleTab = await app.code.waitForElement(SINGLE_TAB_SELECTOR);
-			ok(!singleTab.textContent.includes(CONTRIBUTED_PROFILE_NAME));
+			const singleTab = await app.code.waitForElement(Selector.SingleTab);
+			ok(!singleTab.textContent.includes(ContributedProfileName));
 			await app.workbench.terminal.killTerminal();
 		});
 
 		it('should set the default profile to a contributed one', async function () {
 			await app.workbench.terminal.runProfileCommand('setDefault', true);
 			await app.workbench.terminal.createNew();
-			const singleTab = await app.code.waitForElement(SINGLE_TAB_SELECTOR);
-			ok(singleTab.textContent.includes(CONTRIBUTED_PROFILE_NAME));
+			const singleTab = await app.code.waitForElement(Selector.SingleTab);
+			ok(singleTab.textContent.includes(ContributedProfileName));
 		});
 	});
 }
