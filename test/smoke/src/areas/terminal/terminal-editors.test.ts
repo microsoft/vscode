140e1e4b3c14edd390ba9996fee705d69ce589b9
Megan Rogge <merogge@microsoft.com>
2021-11-17 08:59:49 -0800

    add a bunch of tests

--- test/smoke/src/areas/terminal/terminal-editors.test.ts
+++ test/smoke/src/areas/terminal/terminal-editors.test.ts
@@ -11,7 +11,7 @@ export function setup(opts: ParsedArgs) {
 	describe.only('Terminal Editors', () => {
 		let code: Code;
 		let terminal: Terminal;
-
+		const editorGroupSelector = '.editor > div.content > div.grid-view-container > div > div > div > div.monaco-scrollable-element > div.split-view-container';
 		// TODO: Move into automation/terminal
 		const enum TerminalCommandId {
 			Rename = 'workbench.action.terminal.rename',
@@ -20,6 +20,7 @@ export function setup(opts: ParsedArgs) {
 			KillAll = 'workbench.action.terminal.killAll',
 			CreateNewEditor = 'workbench.action.createTerminalEditor',
 			SplitEditor = 'workbench.action.createTerminalEditorSide',
+			Split = 'workbench.action.terminal.split',
 			MoveToPanel = 'workbench.action.terminal.moveToTerminalPanel'
 		}
 
@@ -62,6 +63,25 @@ export function setup(opts: ParsedArgs) {
 			await terminal.runCommand(TerminalCommandId.CreateNewEditor);
 			await terminal.runCommand(TerminalCommandId.MoveToPanel);
 			await code.waitForElement('.single-terminal-tab');
+		});
+
+		it('should open a terminal in a new group for open to the side', async () => {
+			await terminal.runCommand(TerminalCommandId.CreateNewEditor);
+			await terminal.runCommand(TerminalCommandId.SplitEditor);
+			await code.waitForElements(editorGroupSelector, true, e => e && e.length > 0 ? e[0].children.length === 2 : false);
+		});
+
+		it.skip('should open a terminal in a new group when the split button is pressed', async () => {
+			await terminal.runCommand(TerminalCommandId.CreateNewEditor);
+			//TODO: split button
+			// await terminal.runCommand(TerminalCommandId.SplitEditor);
+			await code.waitForElements(editorGroupSelector, true, e => e && e.length > 0 ? e[0].children.length === 2 : false);
+		});
+
+		it('should open a terminal in the active editor group', async () => {
+			await terminal.runCommand(TerminalCommandId.CreateNewEditor);
+			await terminal.runCommand(TerminalCommandId.MoveToPanel);
+			await code.waitForElements(editorGroupSelector, true, e => e && e.length > 0 ? e[0].children.length === 1 : false);
 		});
 	});
 }
