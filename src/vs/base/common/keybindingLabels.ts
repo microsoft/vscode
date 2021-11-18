2c6d3b27bbcb00587d26e3f4ad75265301a0ff41
Alex Dima <alexdima@microsoft.com>
2021-11-19 00:11:57 +0100

    Change Alt to Option only in the aria label provider and adopt change in tests

--- src/vs/base/common/keybindingLabels.ts
+++ src/vs/base/common/keybindingLabels.ts
@@ -84,7 +84,7 @@ export const AriaLabelProvider = new ModifierLabelProvider(
 	{
 		ctrlKey: nls.localize({ key: 'ctrlKey.long', comment: ['This is the long form for the Control key on the keyboard'] }, "Control"),
 		shiftKey: nls.localize({ key: 'shiftKey.long', comment: ['This is the long form for the Shift key on the keyboard'] }, "Shift"),
-		altKey: nls.localize({ key: 'optKey.long', comment: ['This is the long form for the Alt key on the keyboard'] }, "Option"),
+		altKey: nls.localize({ key: 'optKey.long', comment: ['This is the long form for the Alt/Option key on the keyboard'] }, "Option"),
 		metaKey: nls.localize({ key: 'cmdKey.long', comment: ['This is the long form for the Command key on the keyboard'] }, "Command"),
 		separator: '+',
 	},
@@ -112,7 +112,7 @@ export const ElectronAcceleratorLabelProvider = new ModifierLabelProvider(
 	{
 		ctrlKey: 'Ctrl',
 		shiftKey: 'Shift',
-		altKey: 'Opt',
+		altKey: 'Alt',
 		metaKey: 'Cmd',
 		separator: '+',
 	},
@@ -132,7 +132,7 @@ export const UserSettingsLabelProvider = new ModifierLabelProvider(
 	{
 		ctrlKey: 'ctrl',
 		shiftKey: 'shift',
-		altKey: 'opt',
+		altKey: 'alt',
 		metaKey: 'cmd',
 		separator: '+',
 	},
