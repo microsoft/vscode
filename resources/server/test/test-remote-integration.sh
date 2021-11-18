9d9c462cfe227e125d23a962dbbde0d21036ca2e
Benjamin Pasero <benjamin.pasero@microsoft.com>
2021-11-18 09:38:18 +0100

    align with changes

--- resources/server/test/test-remote-integration.sh
+++ resources/server/test/test-remote-integration.sh
@@ -68,7 +68,7 @@ else
 fi
 
 if [ -z "$INTEGRATION_TEST_APP_NAME" ]; then
-	after_suite() { killall Electron || true; }
+	after_suite() { true; }
 else
 	after_suite() { killall $INTEGRATION_TEST_APP_NAME || true; }
 fi
