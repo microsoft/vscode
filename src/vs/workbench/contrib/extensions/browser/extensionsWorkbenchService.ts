2eaf2f61aa14c643a8fa1220b7735bb81ee6ae5e
Sandeep Somavarapu <sasomava@microsoft.com>
2021-12-13 14:34:07 +0100

    Revert "check pre-release flag on local extension while syncing - to avoid multiple queries to get compatible extension"
    This reverts commit 7334561f2b142970f11b616f541c1bc87db1e6d0.

--- src/vs/workbench/contrib/extensions/browser/extensionsWorkbenchService.ts
+++ src/vs/workbench/contrib/extensions/browser/extensionsWorkbenchService.ts
@@ -1005,41 +1005,27 @@ export class ExtensionsWorkbenchService extends Disposable implements IExtension
 			.then(undefined, err => null);
 	}
 
-	private async syncWithGallery(): Promise<void> {
-		const ids: string[] = [], preReleaseIds: string[] = [], names: string[] = [], preReleaseNames: string[] = [];
+	private syncWithGallery(): Promise<void> {
+		const ids: string[] = [], names: string[] = [];
 		for (const installed of this.local) {
 			if (installed.type === ExtensionType.User) {
 				if (installed.identifier.uuid) {
-					if (installed.local?.isPreReleaseVersion || installed.local?.preRelease) {
-						preReleaseIds.push(installed.identifier.uuid);
-					} else {
-						ids.push(installed.identifier.uuid);
-					}
+					ids.push(installed.identifier.uuid);
 				} else {
-					if (installed.local?.isPreReleaseVersion || installed.local?.preRelease) {
-						preReleaseNames.push(installed.identifier.id);
-					} else {
-						names.push(installed.identifier.id);
-					}
+					names.push(installed.identifier.id);
 				}
 			}
 		}
 
 		const promises: Promise<IPager<IExtension>>[] = [];
 		if (ids.length) {
-			promises.push(this.queryGallery({ ids, pageSize: ids.length, includePreRelease: false }, CancellationToken.None));
-		}
-		if (preReleaseIds.length) {
-			promises.push(this.queryGallery({ ids: preReleaseIds, pageSize: preReleaseIds.length, includePreRelease: true }, CancellationToken.None));
+			promises.push(this.queryGallery({ ids, pageSize: ids.length }, CancellationToken.None));
 		}
 		if (names.length) {
-			promises.push(this.queryGallery({ names, pageSize: names.length, includePreRelease: false }, CancellationToken.None));
-		}
-		if (preReleaseNames.length) {
-			promises.push(this.queryGallery({ names: preReleaseNames, pageSize: names.length, includePreRelease: true }, CancellationToken.None));
+			promises.push(this.queryGallery({ names, pageSize: names.length }, CancellationToken.None));
 		}
 
-		await Promises.settled(promises);
+		return Promises.settled(promises).then(() => undefined);
 	}
 
 	private eventuallyAutoUpdateExtensions(): void {
