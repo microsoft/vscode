59b42fd43d348fe5b4f3dbd4dbd0df3c0e3e4cfe
Henning Dieterichs <hdieterichs@microsoft.com>
2022-01-06 16:08:14 +0100

    Adds test for runAndSubscribeWithStore.

--- src/vs/base/test/common/event.test.ts
+++ src/vs/base/test/common/event.test.ts
@@ -7,7 +7,7 @@ import { timeout } from 'vs/base/common/async';
 import { CancellationToken } from 'vs/base/common/cancellation';
 import { errorHandler, setUnexpectedErrorHandler } from 'vs/base/common/errors';
 import { AsyncEmitter, DebounceEmitter, Emitter, Event, EventBufferer, EventMultiplexer, IWaitUntil, MicrotaskEmitter, PauseableEmitter, Relay } from 'vs/base/common/event';
-import { DisposableStore, IDisposable } from 'vs/base/common/lifecycle';
+import { DisposableStore, IDisposable, toDisposable } from 'vs/base/common/lifecycle';
 
 namespace Samples {
 
@@ -955,5 +955,35 @@ suite('Event utils', () => {
 			e2.fire(6);
 			assert.deepStrictEqual(result, [2, 4]);
 		});
+	});
+
+	test('runAndSubscribeWithStore', () => {
+		const eventEmitter = new Emitter();
+		const event = eventEmitter.event;
+
+		let i = 0;
+		let log = new Array<any>();
+		const disposable = Event.runAndSubscribeWithStore(event, (e, disposables) => {
+			const idx = i++;
+			log.push({ label: 'handleEvent', data: e || null, idx });
+			disposables.add(toDisposable(() => {
+				log.push({ label: 'dispose', idx });
+			}));
+		});
+
+		log.push({ label: 'fire' });
+		eventEmitter.fire('someEventData');
+
+		log.push({ label: 'disposeAll' });
+		disposable.dispose();
+
+		assert.deepStrictEqual(log, [
+			{ label: 'handleEvent', data: null, idx: 0 },
+			{ label: 'fire' },
+			{ label: 'dispose', idx: 0 },
+			{ label: 'handleEvent', data: 'someEventData', idx: 1 },
+			{ label: 'disposeAll' },
+			{ label: 'dispose', idx: 1 },
+		]);
 	});
 });
