c8bf01c51c7ab9bd36c5b75cef05e7644521d5c1
Henning Dieterichs <hdieterichs@microsoft.com>
2022-01-10 14:41:38 +0100

    Includes *.webm files in vscode resources so that audio files are included in the installer.

--- build/gulpfile.vscode.js
+++ build/gulpfile.vscode.js
@@ -56,7 +56,7 @@ const vscodeResources = [
 	'out-build/bootstrap-amd.js',
 	'out-build/bootstrap-node.js',
 	'out-build/bootstrap-window.js',
-	'out-build/vs/**/*.{svg,png,html,jpg}',
+	'out-build/vs/**/*.{svg,png,html,jpg,webm}',
 	'!out-build/vs/code/browser/**/*.html',
 	'!out-build/vs/editor/standalone/**/*.svg',
 	'out-build/vs/base/common/performance.js',
