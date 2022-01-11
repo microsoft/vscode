b3b6fb8989852630dd4d168a9848840f6486c96b
Alex Dima <alexdima@microsoft.com>
2022-01-11 13:28:22 +0100

    Set `VSCODE_INJECT_NODE_MODULE_LOOKUP_PATH` only when running out of source

--- src/vs/server/main.js
+++ src/vs/server/main.js
@@ -250,9 +250,14 @@ function loadCode() {
 	return new Promise((resolve, reject) => {
 		const path = require('path');
 
-		// Set default remote native node modules path, if unset
-		process.env['VSCODE_INJECT_NODE_MODULE_LOOKUP_PATH'] = process.env['VSCODE_INJECT_NODE_MODULE_LOOKUP_PATH'] || path.join(__dirname, '..', '..', '..', 'remote', 'node_modules');
-		require('../../bootstrap-node').injectNodeModuleLookupPath(process.env['VSCODE_INJECT_NODE_MODULE_LOOKUP_PATH']);
+		if (process.env['VSCODE_DEV']) {
+			// When running out of sources, we need to load node modules from remote/node_modules,
+			// which are compiled against nodejs, not electron
+			process.env['VSCODE_INJECT_NODE_MODULE_LOOKUP_PATH'] = process.env['VSCODE_INJECT_NODE_MODULE_LOOKUP_PATH'] || path.join(__dirname, '..', '..', '..', 'remote', 'node_modules');
+			require('../../bootstrap-node').injectNodeModuleLookupPath(process.env['VSCODE_INJECT_NODE_MODULE_LOOKUP_PATH']);
+		} else {
+			delete process.env['VSCODE_INJECT_NODE_MODULE_LOOKUP_PATH'];
+		}
 		require('../../bootstrap-amd').load('vs/server/remoteExtensionHostAgent', resolve, reject);
 	});
 }
