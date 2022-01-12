d4198abc02ae875bf897c6b110493c8b8efb86e7
Alex Ross <alros@microsoft.com>
2022-01-12 14:40:42 +0100

    Add doc comment to proposal

--- src/vscode-dts/vscode.proposed.commentTimestamp.d.ts
+++ src/vscode-dts/vscode.proposed.commentTimestamp.d.ts
@@ -5,6 +5,11 @@
 
 declare module 'vscode' {
 	export interface Comment {
+		/**
+		 * An optional detail that will be displayed less prominently than the `author`.
+		 * If a date is provided, then the date will be formatted according to the user's
+		 * locale and settings.
+		 */
 		detail?: Date | string
 	}
 }
