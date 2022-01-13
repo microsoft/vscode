405cffcb55a05e359cc7fa22d344003c8d7ecdfc
SteVen Batten <sbatten@microsoft.com>
2022-01-13 14:00:01 -0800

    remove side panel setting

--- src/vs/workbench/browser/parts/auxiliarybar/auxiliaryBarPart.ts
+++ src/vs/workbench/browser/parts/auxiliarybar/auxiliaryBarPart.ts
@@ -26,9 +26,6 @@ import { HoverPosition } from 'vs/base/browser/ui/hover/hoverWidget';
 import { IAction, Separator } from 'vs/base/common/actions';
 import { ToggleAuxiliaryBarAction } from 'vs/workbench/browser/parts/auxiliarybar/auxiliaryBarActions';
 import { assertIsDefined } from 'vs/base/common/types';
-import { IConfigurationService } from 'vs/platform/configuration/common/configuration';
-
-export const AUXILIARYBAR_ENABLED: string = 'workbench.experimental.sidePanel.enabled';
 
 export class AuxiliaryBarPart extends BasePanelPart {
 	static readonly activePanelSettingsKey = 'workbench.auxiliarybar.activepanelid';
@@ -36,13 +33,12 @@ export class AuxiliaryBarPart extends BasePanelPart {
 	static readonly placeholdeViewContainersKey = 'workbench.auxiliarybar.placeholderPanels';
 
 	// Use the side bar dimensions
-	override readonly minimumWidth: number = this.isEnabled ? 170 : 0;
-	override readonly maximumWidth: number = this.isEnabled ? Number.POSITIVE_INFINITY : 0;
+	override readonly minimumWidth: number = 170;
+	override readonly maximumWidth: number = Number.POSITIVE_INFINITY;
 	override readonly minimumHeight: number = 0;
 	override readonly maximumHeight: number = Number.POSITIVE_INFINITY;
 
 	constructor(
-		@IConfigurationService private readonly configurationService: IConfigurationService,
 		@INotificationService notificationService: INotificationService,
 		@IStorageService storageService: IStorageService,
 		@ITelemetryService telemetryService: ITelemetryService,
@@ -82,10 +78,6 @@ export class AuxiliaryBarPart extends BasePanelPart {
 				borderWidth: () => (this.getColor(SIDE_BAR_BORDER) || this.getColor(contrastBorder)) ? 1 : 0,
 			}
 		);
-	}
-
-	get isEnabled(): boolean {
-		return !!this.configurationService.getValue<boolean>(AUXILIARYBAR_ENABLED);
 	}
 
 	override updateStyles(): void {
