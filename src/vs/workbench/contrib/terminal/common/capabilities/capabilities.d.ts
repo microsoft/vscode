ed9318820e03549b64c7c788bc21ddc914e3cf5a
Daniel Imms <2193314+Tyriar@users.noreply.github.com>
2022-01-13 08:51:45 -0800

    Add jsdoc to capabilities module

--- src/vs/workbench/contrib/terminal/common/capabilities/capabilities.d.ts
+++ src/vs/workbench/contrib/terminal/common/capabilities/capabilities.d.ts
@@ -10,14 +10,41 @@ import { CwdDetectionCapability } from 'vs/workbench/contrib/terminal/common/cap
 import { NaiveCwdDetectionCapability } from 'vs/workbench/contrib/terminal/common/capabilities/naiveCwdDetectionCapability';
 import { PartialCommandDetectionCapability } from 'vs/workbench/contrib/terminal/common/capabilities/partialCommandDetectionCapability';
 
+/**
+ * An object that keeps track of additional capabilities and their implementations for features that
+ * are not available for all terminals.
+ */
 export interface ITerminalCapabilityStore {
+	/**
+	 * An iterable of all capabilities in the store.
+	 */
 	readonly items: IterableIterator<TerminalCapability>;
-	readonly onDidRemoveCapability: Event<TerminalCapability>;
+
+	/**
+	 * Fired when a capability is added.
+	 */
 	readonly onDidAddCapability: Event<TerminalCapability>;
+
+	/**
+	 * Fired when a capability is removed.
+	 */
+	readonly onDidRemoveCapability: Event<TerminalCapability>;
+
+	/**
+	 * Gets whether the capability exists in the store.
+	 */
 	has(capability: TerminalCapability): boolean;
+
+	/**
+	 * Gets the implementation of a capability if it has been added to the store.
+	 */
 	get<T extends TerminalCapability>(capability: T): ITerminalCapabilityImplMap[T] | undefined;
 }
 
+/**
+ * Maps capability types to their implementation, enabling strongly typed fetching of
+ * implementations.
+ */
 export interface ITerminalCapabilityImplMap {
 	[TerminalCapability.CwdDetection]: InstanceType<typeof CwdDetectionCapability>;
 	[TerminalCapability.CommandDetection]: InstanceType<typeof CommandDetectionCapability>;
