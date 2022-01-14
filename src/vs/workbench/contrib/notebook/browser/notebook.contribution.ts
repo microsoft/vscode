513afb58938bcf398a4921e97569f79112f699d6
Matt Bierner <matb@microsoft.com>
2022-01-14 13:28:42 -0800

    Clairify that font size is in pixels
    Fixes #139935

--- src/vs/workbench/contrib/notebook/browser/notebook.contribution.ts
+++ src/vs/workbench/contrib/notebook/browser/notebook.contribution.ts
@@ -806,7 +806,7 @@ configurationRegistry.registerConfiguration({
 			tags: ['notebookLayout']
 		},
 		[NotebookSetting.markupFontSize]: {
-			markdownDescription: nls.localize('notebook.markup.fontSize', "Controls the font size of rendered markup in notebooks. When set to `0`, 120% of `#editor.fontSize#` is used."),
+			markdownDescription: nls.localize('notebook.markup.fontSize', "Controls the font size in pixels of rendered markup in notebooks. When set to `0`, 120% of `#editor.fontSize#` is used."),
 			type: 'number',
 			default: 0,
 			tags: ['notebookLayout']
