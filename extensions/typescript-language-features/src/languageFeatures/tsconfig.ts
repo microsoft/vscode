c62c36dea352c49337725ba3df0feb927dde8fe0
Matt Bierner <matb@microsoft.com>
2022-01-18 16:23:10 -0800

    Remove extra assertions

--- extensions/typescript-language-features/src/languageFeatures/tsconfig.ts
+++ extensions/typescript-language-features/src/languageFeatures/tsconfig.ts
@@ -91,17 +91,17 @@ class TsconfigLinkProvider implements vscode.DocumentLinkProvider {
 	}
 
 	private getFileTarget(document: vscode.TextDocument, node: jsonc.Node): vscode.Uri {
-		return vscode.Uri.file(join(dirname(document.uri.fsPath), node!.value));
+		return vscode.Uri.file(join(dirname(document.uri.fsPath), node.value));
 	}
 
 	private getFolderTarget(document: vscode.TextDocument, node: jsonc.Node): vscode.Uri {
-		return vscode.Uri.file(join(dirname(document.uri.fsPath), node!.value, 'tsconfig.json'));
+		return vscode.Uri.file(join(dirname(document.uri.fsPath), node.value, 'tsconfig.json'));
 	}
 
 	private getRange(document: vscode.TextDocument, node: jsonc.Node) {
-		const offset = node!.offset;
+		const offset = node.offset;
 		const start = document.positionAt(offset + 1);
-		const end = document.positionAt(offset + (node!.length - 1));
+		const end = document.positionAt(offset + (node.length - 1));
 		return new vscode.Range(start, end);
 	}
 }
