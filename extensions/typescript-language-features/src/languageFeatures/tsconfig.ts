47c9c3c732473cefb4c18e373914479133ac06b9
Vitaly Turovsky <vital2580@icloud.com>
2022-01-20 14:16:20 +0300

    fix(ts-features-extension): resolve correct path for `extends` of tsconfig fixes #131643

--- extensions/typescript-language-features/src/languageFeatures/tsconfig.ts
+++ extensions/typescript-language-features/src/languageFeatures/tsconfig.ts
@@ -7,6 +7,8 @@ import * as jsonc from 'jsonc-parser';
 import { basename, dirname, join } from 'path';
 import * as vscode from 'vscode';
 import { coalesce, flatten } from '../utils/arrays';
+import { exists, resolveNodeModulesPath } from '../utils/fs';
+import { Utils } from 'vscode-uri';
 
 function mapChildren<R>(node: jsonc.Node | undefined, f: (x: jsonc.Node) => R): R[] {
 	return node && node.type === 'array' && node.children
@@ -16,44 +18,61 @@ function mapChildren<R>(node: jsonc.Node | undefined, f: (x: jsonc.Node) => R): 
 
 class TsconfigLinkProvider implements vscode.DocumentLinkProvider {
 
-	public provideDocumentLinks(
+	public async provideDocumentLinks(
 		document: vscode.TextDocument,
 		_token: vscode.CancellationToken
-	): vscode.DocumentLink[] {
+	): Promise<vscode.DocumentLink[]> {
 		const root = jsonc.parseTree(document.getText());
 		if (!root) {
 			return [];
 		}
 
 		return coalesce([
-			this.getExtendsLink(document, root),
+			await this.getExtendsLink(document, root),
 			...this.getFilesLinks(document, root),
 			...this.getReferencesLinks(document, root)
 		]);
 	}
 
-	private getExtendsLink(document: vscode.TextDocument, root: jsonc.Node): vscode.DocumentLink | undefined {
+	// Behavior reference: https://github.com/microsoft/TypeScript/blob/febfd442cdba343771f478cf433b0892f213ad2f/src/compiler/commandLineParser.ts#L3005
+	private async getExtendsLink(document: vscode.TextDocument, root: jsonc.Node): Promise<vscode.DocumentLink | undefined> {
 		const extendsNode = jsonc.findNodeAtLocation(root, ['extends']);
 		if (!this.isPathValue(extendsNode)) {
 			return undefined;
 		}
+		const extendsValue = extendsNode.value;
 
-		if (extendsNode.value.startsWith('.')) {
-			return new vscode.DocumentLink(
-				this.getRange(document, extendsNode),
-				vscode.Uri.file(join(dirname(document.uri.fsPath), extendsNode.value + (extendsNode.value.endsWith('.json') ? '' : '.json')))
-			);
-		}
+		const tsconfigUri = await this.getTsconfigPath(Utils.dirname(document.uri), extendsValue);
+		return tsconfigUri === undefined ? undefined : new vscode.DocumentLink(
+			this.getRange(document, extendsNode),
+			tsconfigUri
+		);
+	}
 
-		const workspaceFolder = vscode.workspace.getWorkspaceFolder(document.uri);
-		if (!workspaceFolder) {
+	private async getTsconfigPath(baseDirUri: vscode.Uri, extendsValue: string): Promise<vscode.Uri | undefined> {
+		// Don't take into account a case, where tsconfig might be resolved from the root (see the reference)
+		// e.g. C:/projects/shared-tsconfig/tsconfig.json (note that C: prefix is optional)
+
+		const isRelativePath = ['./', '../'].some(str => extendsValue.startsWith(str));
+		if (isRelativePath) {
+			const absolutePath = vscode.Uri.joinPath(baseDirUri, extendsValue);
+			for (const pathCandidate of [
+				absolutePath,
+				absolutePath.with({
+					path: `${absolutePath.path}.json`
+				})
+			]) {
+				if (await exists(pathCandidate)) { return pathCandidate; }
+			}
 			return undefined;
 		}
 
-		return new vscode.DocumentLink(
-			this.getRange(document, extendsNode),
-			vscode.Uri.joinPath(workspaceFolder.uri, 'node_modules', extendsNode.value + (extendsNode.value.endsWith('.json') ? '' : '.json'))
-		);
+		// Resolve like a module
+		return resolveNodeModulesPath(baseDirUri, [
+			extendsValue,
+			`${extendsValue}.json`,
+			`${extendsValue}/tsconfig.json`,
+		]);
 	}
 
 	private getFilesLinks(document: vscode.TextDocument, root: jsonc.Node) {
--- extensions/typescript-language-features/src/utils/fs.ts
+++ extensions/typescript-language-features/src/utils/fs.ts
@@ -4,6 +4,7 @@
  *--------------------------------------------------------------------------------------------*/
 
 import * as vscode from 'vscode';
+import { posix } from 'path';
 
 export const exists = async (resource: vscode.Uri): Promise<boolean> => {
 	try {
@@ -13,4 +14,32 @@ export const exists = async (resource: vscode.Uri): Promise<boolean> => {
 	} catch {
 		return false;
 	}
+};
+
+/** A lighter Node's node_modules resolution algorithm */
+export const resolveNodeModulesPath = async (currentUriDir: vscode.Uri, pathCandidates: string[]): Promise<vscode.Uri | undefined> => {
+	const { fs } = vscode.workspace;
+
+	// clone Uri
+	let currentUri = vscode.Uri.joinPath(currentUriDir, '.');
+	while (true) {
+		const nodeModulesUri = vscode.Uri.joinPath(currentUri, 'node_modules');
+		let nodeModulesStat: vscode.FileStat | undefined;
+		try {
+			// eslint-disable-next-line no-await-in-loop
+			nodeModulesStat = await fs.stat(nodeModulesUri);
+		} catch (err) { }
+		if (nodeModulesStat && (nodeModulesStat.type & vscode.FileType.Directory)) {
+			for (const uriCandidate of pathCandidates.map((relativePath) => vscode.Uri.joinPath(nodeModulesUri, relativePath))) {
+				if (await exists(uriCandidate)) {
+					return uriCandidate;
+				}
+			}
+		}
+		// reached the root
+		if (posix.relative(currentUri.path, '/') === '') { return; }
+
+		currentUri = vscode.Uri.joinPath(currentUri, '..');
+	}
+
 };
