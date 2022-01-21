204854b7868761e9b37f40358ba4d14788f66f57
Tyler James Leonhardt <me@tylerleonhardt.com>
2022-01-21 02:59:31 +0000

    have inmemory fallback

--- src/vs/platform/credentials/node/credentialsMainService.ts
+++ src/vs/platform/credentials/node/credentialsMainService.ts
@@ -16,6 +16,8 @@ interface ChunkedPassword {
 	hasNextChunk: boolean;
 }
 
+type KeytarModule = typeof import('keytar');
+
 export class CredentialsMainService extends Disposable implements ICredentialsMainService {
 
 	private static readonly MAX_PASSWORD_LENGTH = 2500;
@@ -24,6 +26,8 @@ export class CredentialsMainService extends Disposable implements ICredentialsMa
 
 	private _onDidChangePassword: Emitter<ICredentialsChangeEvent> = this._register(new Emitter());
 	readonly onDidChangePassword = this._onDidChangePassword.event;
+
+	private _keytarCache: KeytarModule | undefined;
 
 	// If the credentials service is running on the server, we add a suffix -server to differentiate from the location that the
 	// client would store the credentials.
@@ -139,12 +143,26 @@ export class CredentialsMainService extends Disposable implements ICredentialsMa
 		return keytar.findCredentials(service);
 	}
 
-	private async withKeytar(): Promise<typeof import('keytar')> {
-		if (this.environmentMainService.disableKeytar) {
-			throw new Error('keytar has been disabled via --disable-keytar option');
+	private async withKeytar(): Promise<KeytarModule> {
+		if (this._keytarCache) {
+			return this._keytarCache;
 		}
 
-		return await import('keytar');
+		if (this.environmentMainService.disableKeytar) {
+			this.logService.info('Keytar is disabled. Using in-memory credential store instead.');
+			this._keytarCache = new InMemoryKeytar();
+			return this._keytarCache;
+		}
+
+		try {
+			this._keytarCache = await import('keytar');
+			// Try using keytar to see if it throws or not.
+			await this._keytarCache.findCredentials('test-keytar-loads');
+		} catch (e) {
+			this.logService.warn(`Switching to using in-memory credential store instead because Keytar failed to load: ${e.message}`);
+			this._keytarCache = new InMemoryKeytar();
+		}
+		return this._keytarCache;
 	}
 
 	// This class doesn't implement the clear() function because we don't know
@@ -152,5 +170,47 @@ export class CredentialsMainService extends Disposable implements ICredentialsMa
 	// TODO: should we clear credentials for the built-in auth extensions?
 	public clear(): Promise<void> {
 		return Promise.resolve();
+	}
+}
+
+interface ISecretVault {
+	[service: string]: { [account: string]: string } | undefined;
+}
+
+// This class is used when we are unable to load Keytar properly it keeps credentials in-memory for
+// the duration of the process.
+class InMemoryKeytar implements KeytarModule {
+	private readonly secretVault: ISecretVault = {};
+
+	async getPassword(service: string, account: string): Promise<string | null> {
+		return this.secretVault[service]?.[account] ?? null;
+	}
+
+	async setPassword(service: string, account: string, password: string): Promise<void> {
+		this.secretVault[service] = this.secretVault[service] ?? {};
+		this.secretVault[service]![account] = password;
+	}
+
+	async deletePassword(service: string, account: string): Promise<boolean> {
+		if (!this.secretVault[service]?.[account]) {
+			return false;
+		}
+		delete this.secretVault[service]![account];
+		if (Object.keys(this.secretVault[service]!).length === 0) {
+			delete this.secretVault[service];
+		}
+		return true;
+	}
+
+	async findPassword(service: string): Promise<string | null> {
+		return JSON.stringify(this.secretVault[service]) ?? null;
+	}
+
+	async findCredentials(service: string): Promise<Array<{ account: string, password: string }>> {
+		const credentials: { account: string, password: string }[] = [];
+		for (const account of Object.keys(this.secretVault[service] || {})) {
+			credentials.push({ account, password: this.secretVault[service]![account] });
+		}
+		return credentials;
 	}
 }
