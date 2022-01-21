d61f3643a9263ea407dc1645efe528172fae8298
Martin Aeschlimann <martinae@microsoft.com>
2022-01-21 16:59:48 +0100

    add missing semicolon

--- src/vs/server/node/remoteExtensionHostAgentServer.ts
+++ src/vs/server/node/remoteExtensionHostAgentServer.ts
@@ -1006,7 +1006,7 @@ export async function createServer(address: string | net.AddressInfo | null, arg
 
 	if (hasWebClient && address && typeof address !== 'string') {
 		// ships the web ui!
-		const queryPart = (connectionToken.type !== ServerConnectionTokenType.None ? `?tkn=${connectionToken.value}` : '')
+		const queryPart = (connectionToken.type !== ServerConnectionTokenType.None ? `?tkn=${connectionToken.value}` : '');
 		console.log(`Web UI available at http://localhost${address.port === 80 ? '' : `:${address.port}`}/${queryPart}`);
 	}
 
