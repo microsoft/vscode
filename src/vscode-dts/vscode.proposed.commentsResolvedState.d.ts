6c8e99ecbdc4cf220f0dfa1c27cb688f4cd07e94
Alex Ross <alros@microsoft.com>
2022-01-21 16:59:50 +0100

    A comment state proposal Part of #127473

---
+++ src/vscode-dts/vscode.proposed.commentsResolvedState.d.ts
@@ -0,0 +1,46 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+declare module 'vscode' {
+
+	// https://github.com/microsoft/vscode/issues/127473
+
+	export interface CommentState {
+		/**
+		 * The human-readable label for the comment state. This may be shown in the UI (ex. "Mark comment as <label>").
+		 */
+		readonly label: string;
+
+		/**
+		 * Icon for the state. This may be shown in the UI.
+		 */
+		readonly iconPath: string | Uri | ThemeIcon;
+
+		/**
+		 * An optional color that may be used to indicate the comment's state.
+		 */
+		readonly color?: ThemeColor;
+	}
+
+	export interface CommentController {
+		/**
+		 * Optional state handler for adding or modifying the state of a comment. This can be used
+		 * for setting a comment's state to "resolved" or "unresolved" for example.
+		 */
+		stateHandler?: (comment: Comment, state: CommentState) => Thenable<void>;
+	}
+
+	export interface Comment {
+		/**
+		 * Optional possible states of the {@link Comment}
+		 */
+		states?: CommentState[];
+
+		/**
+		 * Optional current state of the {@link Comment}
+		 */
+		state?: CommentState;
+	}
+}
