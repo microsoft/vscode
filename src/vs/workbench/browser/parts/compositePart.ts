e22707465173f01f23445dd953da92ccdbbb28ae
SteVen Batten <sbatten@microsoft.com>
2022-01-26 15:21:29 -0800

    compositePart should use createActionViewItem helper fixes #141399

--- src/vs/workbench/browser/parts/compositePart.ts
+++ src/vs/workbench/browser/parts/compositePart.ts
@@ -31,6 +31,7 @@ import { INotificationService } from 'vs/platform/notification/common/notificati
 import { Dimension, append, $, hide, show } from 'vs/base/browser/dom';
 import { AnchorAlignment } from 'vs/base/browser/ui/contextview/contextview';
 import { assertIsDefined, withNullAsUndefined } from 'vs/base/common/types';
+import { createActionViewItem } from 'vs/platform/actions/browser/menuEntryActionViewItem';
 
 export interface ICompositeTitleLabel {
 
@@ -436,7 +437,7 @@ export abstract class CompositePart<T extends Composite> extends Part {
 			return this.activeComposite.getActionViewItem(action);
 		}
 
-		return undefined;
+		return createActionViewItem(this.instantiationService, action);
 	}
 
 	protected actionsContextProvider(): unknown {
