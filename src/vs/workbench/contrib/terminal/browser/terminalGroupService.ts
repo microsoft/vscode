d7a80ad9c1d3ad856d359cfc9684972c26754667
SteVen Batten <sbatten@microsoft.com>
2022-01-26 17:02:49 -0800

    fixes #141404

--- src/vs/workbench/contrib/terminal/browser/terminalGroupService.ts
+++ src/vs/workbench/contrib/terminal/browser/terminalGroupService.ts
@@ -77,13 +77,10 @@ export class TerminalGroupService extends Disposable implements ITerminalGroupSe
 
 	hidePanel(): void {
 		// Hide the panel if the terminal is in the panel and it has no sibling views
-		const location = this._viewDescriptorService.getViewLocationById(TERMINAL_VIEW_ID);
-		if (location === ViewContainerLocation.Panel) {
-			const panel = this._viewDescriptorService.getViewContainerByViewId(TERMINAL_VIEW_ID);
-			if (panel && this._viewDescriptorService.getViewContainerModel(panel).activeViewDescriptors.length === 1) {
-				this._viewsService.closeView(TERMINAL_VIEW_ID);
-				TerminalContextKeys.tabsMouse.bindTo(this._contextKeyService).set(false);
-			}
+		const panel = this._viewDescriptorService.getViewContainerByViewId(TERMINAL_VIEW_ID);
+		if (panel && this._viewDescriptorService.getViewContainerModel(panel).activeViewDescriptors.length === 1) {
+			this._viewsService.closeView(TERMINAL_VIEW_ID);
+			TerminalContextKeys.tabsMouse.bindTo(this._contextKeyService).set(false);
 		}
 	}
 
