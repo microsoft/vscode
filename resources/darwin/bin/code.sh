ecf4182c9e982c6c6b535f1d44206171c416e0e4
n8felton <n8felton@gmail.com>
2022-01-31 13:32:08 -0500

    Updates code cli to remove python requirement.
    Uses less subshells and more bash-native syntax.
    
    Fixes #141880

--- resources/darwin/bin/code.sh
+++ resources/darwin/bin/code.sh
@@ -3,9 +3,9 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 
-function realpath() { python -c "import os,sys; print(os.path.realpath(sys.argv[1]))" "$0"; }
-CONTENTS="$(dirname "$(dirname "$(dirname "$(dirname "$(realpath "$0")")")")")"
-ELECTRON="$CONTENTS/MacOS/Electron"
-CLI="$CONTENTS/Resources/app/out/cli.js"
-ELECTRON_RUN_AS_NODE=1 "$ELECTRON" "$CLI" --ms-enable-electron-run-as-node "$@"
-exit $?
+CODE_PATH=$(readlink "${0}")
+APP_BUNDLE=${CODE_PATH%%${CODE_PATH#*.app}}
+ELECTRON="${APP_BUNDLE}/Contents/MacOS/Electron"
+CLI="${APP_BUNDLE}/Contents/Resources/app/out/cli.js"
+ELECTRON_RUN_AS_NODE=1 "${ELECTRON}" "${CLI}" --ms-enable-electron-run-as-node "${@}"
+exit ${?}
