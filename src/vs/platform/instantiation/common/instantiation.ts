cdd2af7919c5a31532688aa99fee5a2ac4ffcbe5
Matt Bierner <matb@microsoft.com>
2022-02-02 13:52:48 -0800

    Improve comments

--- src/vs/platform/instantiation/common/instantiation.ts
+++ src/vs/platform/instantiation/common/instantiation.ts
@@ -49,27 +49,26 @@ export interface IInstantiationService {
 	readonly _serviceBrand: undefined;
 
 	/**
-	 * Synchronously creates an instance that is denoted by
-	 * the descriptor
+	 * Synchronously creates an instance that is denoted by the descriptor
 	 */
 	createInstance<T>(descriptor: descriptors.SyncDescriptor0<T>): T;
-	createInstance<Ctor extends new (...args: any[]) => any, R extends InstanceType<Ctor>>(t: Ctor, ...args: GetLeadingNonServiceArgs<ConstructorParameters<Ctor>>): R;
+	createInstance<Ctor extends new (...args: any[]) => any, R extends InstanceType<Ctor>>(ctor: Ctor, ...args: GetLeadingNonServiceArgs<ConstructorParameters<Ctor>>): R;
 
 	/**
-	 *
+	 * Calls a function with a service accessor.
 	 */
 	invokeFunction<R, TS extends any[] = []>(fn: (accessor: ServicesAccessor, ...args: TS) => R, ...args: TS): R;
 
 	/**
-	 * Creates a child of this service which inherts all current services
-	 * and adds/overwrites the given services
+	 * Creates a child of this service which inherits all current services
+	 * and adds/overwrites the given services.
 	 */
 	createChild(services: ServiceCollection): IInstantiationService;
 }
 
 
 /**
- * Identifies a service of type T
+ * Identifies a service of type `T`.
  */
 export interface ServiceIdentifier<T> {
 	(...args: any[]): void;
