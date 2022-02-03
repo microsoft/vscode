69896100ed4c84cda2b605a10ad339a5da8913bb
Matt Bierner <matb@microsoft.com>
2022-02-03 08:51:49 -0800

    Add MarkdownString.baseUri proposal (#142055)
    For #142051

--- src/vs/workbench/api/common/extHostTypeConverters.ts
+++ src/vs/workbench/api/common/extHostTypeConverters.ts
@@ -321,7 +321,7 @@ export namespace MarkdownString {
 			const { language, value } = markup;
 			res = { value: '```' + language + '\n' + value + '\n```\n' };
 		} else if (types.MarkdownString.isMarkdownString(markup)) {
-			res = { value: markup.value, isTrusted: markup.isTrusted, supportThemeIcons: markup.supportThemeIcons, supportHtml: markup.supportHtml };
+			res = { value: markup.value, isTrusted: markup.isTrusted, supportThemeIcons: markup.supportThemeIcons, supportHtml: markup.supportHtml, baseUri: markup.baseUri };
 		} else if (typeof markup === 'string') {
 			res = { value: markup };
 		} else {
@@ -387,6 +387,7 @@ export namespace MarkdownString {
 		const result = new types.MarkdownString(value.value, value.supportThemeIcons);
 		result.isTrusted = value.isTrusted;
 		result.supportHtml = value.supportHtml;
+		result.baseUri = value.baseUri ? URI.from(value.baseUri) : undefined;
 		return result;
 	}
 
