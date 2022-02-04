b50b7fc1ce7ee5cb829f618c3c28c7defb856414
Robert Jin <rzj17@ic.ac.uk>
2022-02-04 23:25:29 +0000

    Update setting name, only check sibling once

--- src/vs/workbench/contrib/files/browser/views/explorerViewer.ts
+++ src/vs/workbench/contrib/files/browser/views/explorerViewer.ts
@@ -598,7 +598,7 @@ export class FilesRenderer implements ICompressibleTreeRenderer<ExplorerItem, Fu
 	}
 }
 
-export interface CachedParsedExpression {
+interface CachedParsedExpression {
 	original: glob.IExpression;
 	parsed: glob.ParsedExpression;
 }
