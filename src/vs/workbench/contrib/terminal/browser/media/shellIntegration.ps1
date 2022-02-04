f8947fe5d3b7c7264f8cdcd2db5dfb5057082e7e
Luan Vitor Simião Oliveira <luanv.oliveira@outlook.com>
2022-02-04 14:12:39 -0300

    this is shorter

--- src/vs/workbench/contrib/terminal/browser/media/shellIntegration.ps1
+++ src/vs/workbench/contrib/terminal/browser/media/shellIntegration.ps1
@@ -3,7 +3,7 @@
 #   Licensed under the MIT License. See License.txt in the project root for license information.
 # ---------------------------------------------------------------------------------------------
 
-$Global:__VSCodeOriginalPrompt = (Get-Command Prompt).ScriptBlock
+$Global:__VSCodeOriginalPrompt = $function:Prompt
 
 function Global:__VSCode-Get-LastExitCode {
   if ($? -eq $True) {
