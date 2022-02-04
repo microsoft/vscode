52be2e7c5727bf0a9467300fc70e7eb9b7690d04
Martin Aeschlimann <martinae@microsoft.com>
2022-02-04 16:40:46 +0100

    fix import

--- src/vs/editor/contrib/folding/browser/hiddenRangeModel.ts
+++ src/vs/editor/contrib/folding/browser/hiddenRangeModel.ts
@@ -11,7 +11,7 @@ import { IRange, Range } from 'vs/editor/common/core/range';
 import { Selection } from 'vs/editor/common/core/selection';
 import { IModelContentChangedEvent } from 'vs/editor/common/textModelEvents';
 import { countEOL } from 'vs/editor/common/core/eolCounter';
-import { FoldingModel } from './foldingModel';
+import { FoldingModel } from 'vs/editor/contrib/folding/browser/foldingModel';
 
 export class HiddenRangeModel {
 
