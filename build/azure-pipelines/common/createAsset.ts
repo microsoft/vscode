82ab761d0f205dc2a51c4babaf5fb8d8ab26605e
Ladislau Szomoru <3372902+lszomoru@users.noreply.github.com>
2022-02-07 11:18:51 +0100

    Re-enable task for Windows

--- build/azure-pipelines/common/createAsset.ts
+++ build/azure-pipelines/common/createAsset.ts
@@ -40,6 +40,8 @@ function getPlatform(product: string, os: string, arch: string, type: string): s
 					switch (type) {
 						case 'archive':
 							return `${asset}-archive`;
+						case 'sbom':
+							return `${asset}-sbom`;
 						case 'setup':
 							return asset;
 						case 'user-setup':
@@ -48,11 +50,13 @@ function getPlatform(product: string, os: string, arch: string, type: string): s
 							throw new Error(`Unrecognized: ${product} ${os} ${arch} ${type}`);
 					}
 				}
-				case 'server':
+				case 'server': {
 					if (arch === 'arm64') {
 						throw new Error(`Unrecognized: ${product} ${os} ${arch} ${type}`);
 					}
-					return arch === 'ia32' ? 'server-win32' : `server-win32-${arch}`;
+					const asset = arch === 'ia32' ? 'server-win32' : `server-win32-${arch}`;
+					return type === 'sbom' ? `${asset}-sbom` : asset;
+				}
 				case 'web':
 					if (arch === 'arm64') {
 						throw new Error(`Unrecognized: ${product} ${os} ${arch} ${type}`);
