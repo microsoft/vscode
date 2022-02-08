9e60d1b70bafc7e85e5cf5235d4820369c52cb82
Ladislau Szomoru <3372902+lszomoru@users.noreply.github.com>
2022-02-08 21:32:46 +0100

    Fix compilation errors

--- src/vs/workbench/electron-sandbox/desktop.main.ts
+++ src/vs/workbench/electron-sandbox/desktop.main.ts
@@ -284,7 +284,7 @@ export class DesktopMain extends Disposable {
 		const workspaceTrustEnablementService = new WorkspaceTrustEnablementService(configurationService, environmentService);
 		serviceCollection.set(IWorkspaceTrustEnablementService, workspaceTrustEnablementService);
 
-		const workspaceTrustManagementService = new WorkspaceTrustManagementService(configurationService, remoteAuthorityResolverService, storageService, uriIdentityService, environmentService, configurationService, workspaceTrustEnablementService);
+		const workspaceTrustManagementService = new WorkspaceTrustManagementService(configurationService, remoteAuthorityResolverService, storageService, uriIdentityService, environmentService, configurationService, workspaceTrustEnablementService, logService);
 		serviceCollection.set(IWorkspaceTrustManagementService, workspaceTrustManagementService);
 
 		// Update workspace trust so that configuration is updated accordingly
