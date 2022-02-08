5cc07958381ec596d0d779cfc9002102e3c6fe09
Johannes Rieken <johannes.rieken@gmail.com>
2022-02-08 09:28:41 +0100

    tiny todo-api for tree dnd, fyi @alexr00

--- src/vscode-dts/vscode.proposed.treeViewDragAndDrop.d.ts
+++ src/vscode-dts/vscode.proposed.treeViewDragAndDrop.d.ts
@@ -116,6 +116,7 @@ declare module 'vscode' {
 		 * @param target The target tree element that the drop is occurring on.
 		 * @param token TODO @alexr00: When would this operation actually be cancelled?
 		 */
+		// TODO@API align order of TreeDataTransfer and T with handleDrag
 		handleDrop(source: TreeDataTransfer, target: T, token: CancellationToken): Thenable<void> | void;
 	}
 }
