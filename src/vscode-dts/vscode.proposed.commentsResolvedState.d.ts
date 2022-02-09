1712a239c036f1ceb973eb3378575e40d957c899
Alex Ross <alros@microsoft.com>
2022-02-09 15:17:58 +0100

    Comment resolved/unresolved proposal

--- src/vscode-dts/vscode.proposed.commentsResolvedState.d.ts
+++ src/vscode-dts/vscode.proposed.commentsResolvedState.d.ts
@@ -7,37 +7,12 @@ declare module 'vscode' {
 
 	// https://github.com/microsoft/vscode/issues/127473
 
-	export class CommentThreadDecorations {
-		static HighEmphasis: CommentThreadDecorations;
-		static LowEmphasis: CommentThreadDecorations;
-
-		/**
-		 * Icon that will show in association with a comment thread.
-		 */
-		readonly iconPath?: string | Uri | ThemeIcon;
-
-		/**
-		 * Color that will show in association with the comment thread.
-		 */
-		readonly color?: ThemeColor;
-
-		constructor(iconPath?: string | Uri | ThemeIcon, color?: ThemeColor);
+	export enum CommentThreadState {
+		Unresolved = 0,
+		Resolved = 1
 	}
 
 	export interface CommentThread {
-		/**
-		 * The decorations for a {@link CommentThread}.
-		 */
-		decoration?: CommentThreadDecorations;
-
-		/**
-		 * The accessibility information associated with the label of the {@link CommentThread}.
-		 */
-		labelAccessibilityInformation?: AccessibilityInformation;
-
-		/**
-		 * The tooltip associated with the label of hte {@link CommentThread}.
-		 */
-		labelTooltip?: string;
+		state?: CommentThreadState;
 	}
 }
