3ee750262f8a4e829c16f3e5d340ae10d48024bf
SteVen Batten <sbatten@microsoft.com>
2022-02-09 11:34:08 -0800

    additional handling of the panel position with panel alignment

--- src/vs/workbench/browser/parts/auxiliarybar/auxiliaryBarPart.ts
+++ src/vs/workbench/browser/parts/auxiliarybar/auxiliaryBarPart.ts
@@ -27,6 +27,7 @@ import { IAction, Separator, toAction } from 'vs/base/common/actions';
 import { ToggleAuxiliaryBarAction } from 'vs/workbench/browser/parts/auxiliarybar/auxiliaryBarActions';
 import { assertIsDefined } from 'vs/base/common/types';
 import { MoveSidePanelToPanelAction } from 'vs/workbench/browser/parts/panel/panelActions';
+import { LayoutPriority } from 'vs/base/browser/ui/splitview/splitview';
 
 export class AuxiliaryBarPart extends BasePanelPart {
 	static readonly activePanelSettingsKey = 'workbench.auxiliarybar.activepanelid';
@@ -38,6 +39,8 @@ export class AuxiliaryBarPart extends BasePanelPart {
 	override readonly maximumWidth: number = Number.POSITIVE_INFINITY;
 	override readonly minimumHeight: number = 0;
 	override readonly maximumHeight: number = Number.POSITIVE_INFINITY;
+
+	readonly priority: LayoutPriority = LayoutPriority.Low;
 
 	constructor(
 		@INotificationService notificationService: INotificationService,
