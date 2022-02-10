0b9f06310734fddd573c263ee549dd675a6dc1a9
deepak1556 <hop2deep@gmail.com>
2022-02-10 12:19:34 +0900

    chore: switch validating decoder

--- scripts/test.sh
+++ scripts/test.sh
@@ -8,7 +8,7 @@ else
 	ROOT=$(dirname $(dirname $(readlink -f $0)))
 	# --disable-dev-shm-usage --use-gl=swiftshader: when run on docker containers where size of /dev/shm
 	# partition < 64MB which causes OOM failure for chromium compositor that uses the partition for shared memory
-	LINUX_EXTRA_ARGS="--disable-dev-shm-usage --use-gl=swiftshader"
+	LINUX_EXTRA_ARGS="--disable-dev-shm-usage --use-gl=swiftshader --use-cmd-decoder=validating"
 fi
 
 cd $ROOT
