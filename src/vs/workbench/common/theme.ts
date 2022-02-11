7465af97646cb8b6fe0018c31b8d1f43a235d2a0
David Dossett <ddossett@microsoft.com>
2022-02-11 10:47:23 -0800

    Add border to status bar items on keyboard focus (#142665)
    * Add outline to items on :focus-visible
    
    * Add offset
    
    * Update comments
    
    * Clean up outline logic
    
    * Add status bar item focus token

--- src/vs/workbench/common/theme.ts
+++ src/vs/workbench/common/theme.ts
@@ -394,6 +394,12 @@ export const STATUS_BAR_ITEM_ACTIVE_BACKGROUND = registerColor('statusBarItem.ac
 	hc: Color.white.transparent(0.18)
 }, localize('statusBarItemActiveBackground', "Status bar item background color when clicking. The status bar is shown in the bottom of the window."));
 
+export const STATUS_BAR_ITEM_FOCUS_BORDER = registerColor('statusBarItem.focusBorder', {
+	dark: STATUS_BAR_FOREGROUND,
+	light: STATUS_BAR_FOREGROUND,
+	hc: null,
+}, localize('statusBarItemFocusBorder', "Status bar item border color when focused on keyboard navigation. The status bar is shown in the bottom of the window."));
+
 export const STATUS_BAR_ITEM_HOVER_BACKGROUND = registerColor('statusBarItem.hoverBackground', {
 	dark: Color.white.transparent(0.12),
 	light: Color.white.transparent(0.12),
