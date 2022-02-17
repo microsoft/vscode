7c2b9d37f627c49bdfe03e4e54f93269471f6325
rebornix <penn.lv@gmail.com>
2022-02-16 18:10:47 -0800

    ICodeBlockHighlightRequest

--- src/vs/workbench/contrib/notebook/browser/view/renderers/webviewMessages.ts
+++ src/vs/workbench/contrib/notebook/browser/view/renderers/webviewMessages.ts
@@ -139,24 +139,22 @@ export interface IInitializedMarkupMessage extends BaseToWebviewMessage {
 	readonly type: 'initializedMarkup';
 }
 
+export interface ICodeBlockHighlightRequest {
+	readonly id: string;
+	readonly value: string;
+	readonly lang: string;
+}
+
 export interface IRenderedMarkupMessage extends BaseToWebviewMessage {
 	readonly type: 'renderedMarkup';
 	readonly cellId: string;
 	readonly html: string;
-	readonly codeBlocks: ReadonlyArray<{
-		readonly id: string;
-		readonly value: string;
-		readonly lang: string;
-	}>;
+	readonly codeBlocks: ReadonlyArray<ICodeBlockHighlightRequest>;
 }
 
 export interface IRenderedCellOutputMessage extends BaseToWebviewMessage {
 	readonly type: 'renderedCellOutput';
-	readonly codeBlocks: ReadonlyArray<{
-		readonly id: string;
-		readonly value: string;
-		readonly lang: string;
-	}>;
+	readonly codeBlocks: ReadonlyArray<ICodeBlockHighlightRequest>;
 }
 
 export interface IClearMessage {
