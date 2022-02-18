3567a2f2bc651bfbc9331e573dd118d5adfbbf9e
rebornix <penn.lv@gmail.com>
2022-02-18 14:54:40 -0800

    Show codelens to ipynb json to open in notebook editor

--- extensions/ipynb/src/ipynbMain.ts
+++ extensions/ipynb/src/ipynbMain.ts
@@ -37,6 +37,16 @@ export function activate(context: vscode.ExtensionContext) {
 		}
 	}));
 
+	vscode.languages.registerCodeLensProvider({ pattern: '**/*.ipynb' }, {
+		provideCodeLenses: (document) => {
+			if (document.uri.scheme === 'vscode-notebook-cell') {
+				return [];
+			}
+			const codelens = new vscode.CodeLens(new vscode.Range(0, 0, 0, 0), { title: 'Open in Notebook Editor', command: 'ipynb.openIpynbInNotebookEditor', arguments: [document.uri] });
+			return [codelens];
+		}
+	});
+
 	context.subscriptions.push(vscode.commands.registerCommand('ipynb.newUntitledIpynb', async () => {
 		const language = 'python';
 		const cell = new vscode.NotebookCellData(vscode.NotebookCellKind.Code, '', language);
@@ -53,6 +63,14 @@ export function activate(context: vscode.ExtensionContext) {
 		};
 		const doc = await vscode.workspace.openNotebookDocument('jupyter-notebook', data);
 		await vscode.window.showNotebookDocument(doc);
+	}));
+
+	context.subscriptions.push(vscode.commands.registerCommand('ipynb.openIpynbInNotebookEditor', async (uri: vscode.Uri) => {
+		if (vscode.window.activeTextEditor?.document.uri.toString() === uri.toString()) {
+			await vscode.commands.executeCommand('workbench.action.closeActiveEditor');
+		}
+		const document = await vscode.workspace.openNotebookDocument(uri);
+		await vscode.window.showNotebookDocument(document);
 	}));
 
 	// Update new file contribution
