2af466f5a1e34ab28e76b28949d78fdd1bbb844f
Benjamin Pasero <benjamin.pasero@microsoft.com>
2022-02-22 07:38:45 +0100

    Crash from sqlite3 module with remote integration tests (fix #143186)

--- src/vs/platform/storage/electron-main/storageMain.ts
+++ src/vs/platform/storage/electron-main/storageMain.ts
@@ -335,3 +335,10 @@ export class WorkspaceStorageMain extends BaseStorageMain implements IStorageMai
 		}
 	}
 }
+
+export class InMemoryStorageMain extends BaseStorageMain {
+
+	protected async doCreate(): Promise<IStorage> {
+		return new Storage(new InMemoryStorageDatabase());
+	}
+}
