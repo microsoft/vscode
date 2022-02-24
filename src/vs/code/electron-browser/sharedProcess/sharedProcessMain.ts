d496f0388d5c6e99a7b4024932519bea6c583f45
deepak1556 <hop2deep@gmail.com>
2022-02-24 23:44:34 +0900

    chore: rm crash reporter from shared process

--- src/vs/code/electron-browser/sharedProcess/sharedProcessMain.ts
+++ src/vs/code/electron-browser/sharedProcess/sharedProcessMain.ts
@@ -198,7 +198,7 @@ class SharedProcessMain extends Disposable {
 		services.set(ILogService, logService);
 
 		// Worker
-		this.sharedProcessWorkerService = new SharedProcessWorkerService(logService, productService, environmentService);
+		this.sharedProcessWorkerService = new SharedProcessWorkerService(logService);
 		services.set(ISharedProcessWorkerService, this.sharedProcessWorkerService);
 
 		// Files
