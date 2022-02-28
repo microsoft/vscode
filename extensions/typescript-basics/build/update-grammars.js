76a647c33c04d0b9db566dec0757f953ea838d3c
Matt Bierner <matb@microsoft.com>
2022-02-28 14:49:29 -0800

    Fix tested pattern

--- extensions/typescript-basics/build/update-grammars.js
+++ extensions/typescript-basics/build/update-grammars.js
@@ -9,7 +9,7 @@ var updateGrammar = require('vscode-grammar-updater');
 
 function removeDom(grammar) {
 	grammar.repository['support-objects'].patterns = grammar.repository['support-objects'].patterns.filter(pattern => {
-		if (pattern.match && /\b(HTMLElement|ATTRIBUTE_NODE|stopImmediatePropagation)\b/g.test(pattern)) {
+		if (pattern.match && /\b(HTMLElement|ATTRIBUTE_NODE|stopImmediatePropagation)\b/g.test(pattern.match)) {
 			return false;
 		}
 		return true;
