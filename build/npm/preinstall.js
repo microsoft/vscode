37f3aa632f87c9ebf919d1a6f174a24fff493090
Matt Bierner <matb@microsoft.com>
2022-02-28 15:06:43 -0800

    Adding jsconfig for npm scripts

--- build/npm/preinstall.js
+++ build/npm/preinstall.js
@@ -2,7 +2,6 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-//@ts-check
 let err = false;
 
 const nodeVersion = /^(\d+)\.(\d+)\.(\d+)/.exec(process.versions.node);
