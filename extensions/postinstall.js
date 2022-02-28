d45c89bc65c6580c5edfed84c8be7cbe0380abce
Matt Bierner <matb@microsoft.com>
2022-02-28 15:26:48 -0800

    Pick up TS 4.6.1
    Fixes #142460
    
    We're picking up the insiders version because TS is holding off on publishing the stable due to current events. However we don't want `insiders` to show in the version string, so patch the version string to look like the stable version

--- extensions/postinstall.js
+++ extensions/postinstall.js
@@ -55,5 +55,13 @@ function processLib() {
 	}
 }
 
+function patchVersion() {
+	const packageJsonPath = path.join(root, 'package.json');
+	const json = JSON.parse(fs.readFileSync(packageJsonPath).toString());
+	json.version = '4.6.1';
+	fs.writeFileSync(packageJsonPath, JSON.stringify(json, null, 2));
+}
+
 processRoot();
 processLib();
+patchVersion();
