2cd2f539ef3616f6419633bf9baf25b651275c31
Karan Kakroo <karan_kakroo@infosys.com>
2022-03-03 00:16:45 +0000

    Scroll to top button not getting themed (#144224)
    * updated background-color for scroll to top
    
    * updated colors as per button specs

--- src/vs/workbench/contrib/extensions/browser/extensionEditor.ts
+++ src/vs/workbench/contrib/extensions/browser/extensionEditor.ts
@@ -824,7 +824,7 @@ export class ExtensionEditor extends EditorPane {
 						height: 40px;
 						right: 25px;
 						bottom: 25px;
-						background-color:#444444;
+						background-color:var(--vscode-button-background);
 						border-radius: 50%;
 						cursor: pointer;
 						box-shadow: 1px 1px 1px rgba(0,0,0,.25);
@@ -835,7 +835,7 @@ export class ExtensionEditor extends EditorPane {
 					}
 
 					#scroll-to-top:hover {
-						background-color:#007acc;
+						background-color:var(--vscode-button-hoverBackground);
 						box-shadow: 2px 2px 2px rgba(0,0,0,.25);
 					}
 
