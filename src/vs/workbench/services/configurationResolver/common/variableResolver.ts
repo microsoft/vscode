3c7170e1d41ac3cbc36653b2636c4570559c79b4
Alex Ross <alros@microsoft.com>
2022-03-07 18:11:00 +0100

    ${env:PATH} not detected on terminal (#144573)
    Fixes #144339

--- src/vs/workbench/services/configurationResolver/common/variableResolver.ts
+++ src/vs/workbench/services/configurationResolver/common/variableResolver.ts
@@ -67,7 +67,7 @@ export class AbstractVariableResolverService implements IConfigurationResolverSe
 	}
 
 	public resolveWithEnvironment(environment: IProcessEnvironment, root: IWorkspaceFolder | undefined, value: string): string {
-		return this.recursiveResolve(this.prepareEnv(environment), root ? root.uri : undefined, value);
+		return this.recursiveResolve({ env: this.prepareEnv(environment) }, root ? root.uri : undefined, value);
 	}
 
 	public async resolveAsync(root: IWorkspaceFolder | undefined, value: string): Promise<string>;
