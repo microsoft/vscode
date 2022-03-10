8348698b2b60b6729d40ba81069730919058005d
David Dossett <ddossett@microsoft.com>
2022-03-10 15:22:01 -0800

    Update welcome page registrations

--- src/vs/workbench/contrib/welcomeGettingStarted/browser/gettingStartedColors.ts
+++ src/vs/workbench/contrib/welcomeGettingStarted/browser/gettingStartedColors.ts
@@ -9,7 +9,7 @@ import { localize } from 'vs/nls';
 // Seprate from main module to break dependency cycles between welcomePage and gettingStarted.
 export const welcomePageBackground = registerColor('welcomePage.background', { light: null, dark: null, hcDark: null, hcLight: null }, localize('welcomePage.background', 'Background color for the Welcome page.'));
 
-export const welcomePageTileBackground = registerColor('welcomePage.tileBackground', { dark: editorWidgetBackground, light: editorWidgetBackground, hcDark: '#000' }, localize('welcomePage.tileBackground', 'Background color for the tiles on the Get Started page.'));
+export const welcomePageTileBackground = registerColor('welcomePage.tileBackground', { dark: editorWidgetBackground, light: editorWidgetBackground, hcDark: '#000', hcLight: editorWidgetBackground }, localize('welcomePage.tileBackground', 'Background color for the tiles on the Get Started page.'));
 export const welcomePageTileHoverBackground = registerColor('welcomePage.tileHoverBackground', { dark: lighten(editorWidgetBackground, .2), light: darken(editorWidgetBackground, .1), hcDark: null, hcLight: null }, localize('welcomePage.tileHoverBackground', 'Hover background color for the tiles on the Get Started.'));
 export const welcomePageTileShadow = registerColor('welcomePage.tileShadow', { light: widgetShadow, dark: widgetShadow, hcDark: null, hcLight: null }, localize('welcomePage.tileShadow', 'Shadow color for the Welcome page walkthrough category buttons.'));
 
