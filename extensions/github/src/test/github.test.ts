1093535d6c1f776befecac8c900998a1636af413
Babak K. Shandiz <babak.k.shandiz@gmail.com>
2022-03-13 07:57:07 +0000

    Exclude deleted files from test case expected result
    Signed-off-by: Babak K. Shandiz <babak.k.shandiz@gmail.com>

--- extensions/github/src/test/github.test.ts
+++ extensions/github/src/test/github.test.ts
@@ -20,16 +20,13 @@ suite('github smoke test', function () {
 		const expectedValuesSorted = [
 			'/PULL_REQUEST_TEMPLATE/a.md',
 			'/PULL_REQUEST_TEMPLATE/b.md',
-			'/docs/pull_request_template.md',
 			'/docs/PULL_REQUEST_TEMPLATE.md',
 			'/docs/PULL_REQUEST_TEMPLATE/a.md',
 			'/docs/PULL_REQUEST_TEMPLATE/b.md',
 			'/.github/PULL_REQUEST_TEMPLATE.md',
-			'/.github/PULL_REQUEST_TEMPLATE.md',
 			'/.github/PULL_REQUEST_TEMPLATE/a.md',
 			'/.github/PULL_REQUEST_TEMPLATE/b.md',
-			'/pull_request_template.md',
-			'/PULL_REQUEST_TEMPLATE.md',
+			'/PULL_REQUEST_TEMPLATE.md'
 		];
 		expectedValuesSorted.sort();
 
