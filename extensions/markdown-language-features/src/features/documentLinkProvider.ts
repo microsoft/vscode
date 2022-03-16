518a06991bd810c4c8a1e2bdc3247f98b3742a20
Jefferson Chen <jc@Jeffersons-MBP.attlocal.net>
2022-03-15 21:52:33 -0700

    add lookahead to exclude [tag]: <url> case

--- extensions/markdown-language-features/src/features/documentLinkProvider.ts
+++ extensions/markdown-language-features/src/features/documentLinkProvider.ts
@@ -104,7 +104,7 @@ export function stripAngleBrackets(link: string) {
 }
 
 const linkPattern = /(\[((!\[[^\]]*?\]\(\s*)([^\s\(\)]+?)\s*\)\]|(?:\\\]|[^\]])*\])\(\s*)(([^\s\(\)]|\([^\s\(\)]*?\))+)\s*(".*?")?\)/g;
-const referenceLinkPattern = /((?<=^|[^\]])\[((?:\\\]|[^\]])+)\])(?:[^\[]|$|\[\s*?([^\s\]]*?)\])/g;
+const referenceLinkPattern = /((?<=^|[^\]])\[((?:\\\]|[^\]])+)\])(?!:)(?:[^\[]|$|\[\s*?([^\s\]]*?)\])/g;
 const definitionPattern = /^([\t ]*\[(?!\^)((?:\\\]|[^\]])+)\]:\s*)([^<]\S*|<[^>]+>)/gm;
 const inlineCodePattern = /(?:^|[^`])(`+)(?:.+?|.*?(?:(?:\r?\n).+?)*?)(?:\r?\n)?\1(?:$|[^`])/gm;
 
