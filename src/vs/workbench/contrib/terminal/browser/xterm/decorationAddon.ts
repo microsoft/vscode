4affeba37298c9f9630f570a26bad7e429e6e293
meganrogge <merogge@microsoft.com>
2022-03-16 16:44:24 -0400

    add positions

--- src/vs/workbench/contrib/terminal/browser/xterm/decorationAddon.ts
+++ src/vs/workbench/contrib/terminal/browser/xterm/decorationAddon.ts
@@ -199,7 +199,7 @@ export class DecorationAddon extends Disposable implements ITerminalAddon {
 		} else {
 			color = '';
 		}
-		const decoration = this._terminal.registerDecoration({ marker: command.marker, overviewRulerOptions: { color } });
+		const decoration = this._terminal.registerDecoration({ marker: command.marker, overviewRulerOptions: { color, position: command.exitCode ? 'right' : 'left' } });
 		if (!decoration) {
 			return undefined;
 		}
