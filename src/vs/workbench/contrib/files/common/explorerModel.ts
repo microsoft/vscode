21fb3e66b45aecb5002827f831a88fb82c9e287e
Jackson Kearl <jakearl@microsoft.com>
2022-03-17 18:30:58 -0700

    Fix #142390

--- src/vs/workbench/contrib/files/common/explorerModel.ts
+++ src/vs/workbench/contrib/files/common/explorerModel.ts
@@ -369,7 +369,7 @@ export class ExplorerItem {
 			.filter(entry =>
 				typeof (entry[0]) === 'string' && typeof (entry[1]) === 'string' && entry[0] && entry[1])
 			.map(([parentPattern, childrenPatterns]) =>
-				[parentPattern.trim(), childrenPatterns.split(',').map(p => p.trim())] as [string, string[]]);
+				[parentPattern.trim(), childrenPatterns.split(',').map(p => this.getPlatformAwareName(p.trim()))] as [string, string[]]);
 
 		return new ExplorerFileNestingTrie(patterns);
 	}
