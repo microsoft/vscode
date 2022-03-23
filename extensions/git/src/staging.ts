09eba807c10d2685513e0fcd7f7ef684554ef8d3
Joao Moreno <joao.moreno@microsoft.com>
2022-03-23 16:28:09 +0100

    address heuristic when stage ranges act on same line number diff
    Co-authored-by: @lszomoru
    Fixes: #126989

--- extensions/git/src/staging.ts
+++ extensions/git/src/staging.ts
@@ -109,12 +109,28 @@ export function intersectDiffWithRange(textDocument: TextDocument, diff: LineCha
 	if (diff.modifiedEndLineNumber === 0) {
 		return diff;
 	} else {
-		return {
-			originalStartLineNumber: diff.originalStartLineNumber,
-			originalEndLineNumber: diff.originalEndLineNumber,
-			modifiedStartLineNumber: intersection.start.line + 1,
-			modifiedEndLineNumber: intersection.end.line + 1
-		};
+		const modifiedStartLineNumber = intersection.start.line + 1;
+		const modifiedEndLineNumber = intersection.end.line + 1;
+
+		// heuristic: same number of lines on both sides, let's assume line by line
+		if (diff.originalEndLineNumber - diff.originalStartLineNumber === diff.modifiedEndLineNumber - diff.modifiedStartLineNumber) {
+			const delta = modifiedStartLineNumber - diff.modifiedStartLineNumber;
+			const length = modifiedEndLineNumber - modifiedStartLineNumber;
+
+			return {
+				originalStartLineNumber: diff.originalStartLineNumber + delta,
+				originalEndLineNumber: diff.originalStartLineNumber + delta + length,
+				modifiedStartLineNumber,
+				modifiedEndLineNumber
+			};
+		} else {
+			return {
+				originalStartLineNumber: diff.originalStartLineNumber,
+				originalEndLineNumber: diff.originalEndLineNumber,
+				modifiedStartLineNumber,
+				modifiedEndLineNumber
+			};
+		}
 	}
 }
 
