6786b0ad7dc638db71e4f219e9315ec5f2ae2298
Dirk Baeumer <dirkb@microsoft.com>
2022-03-24 09:04:05 +0100

    Revert e7fffbf1c9169087f1098aedfe54c59c079fa3ac

--- extensions/markdown-language-features/esbuild-preview.js
+++ extensions/markdown-language-features/esbuild-preview.js
@@ -2,10 +2,8 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-// @ts-check
 const path = require('path');
 const esbuild = require('esbuild');
-const watcher = require('@parcel/watcher');
 
 const args = process.argv.slice(2);
 
@@ -17,29 +15,20 @@ if (outputRootIndex >= 0) {
 	outputRoot = args[outputRootIndex + 1];
 }
 
-const srcDir = path.join(__dirname, 'preview-src');
 const outDir = path.join(outputRoot, 'media');
 
-function build() {
-	return esbuild.build({
-		entryPoints: [
-			path.join(srcDir, 'index.ts'),
-			path.join(srcDir, 'pre'),
-		],
-		bundle: true,
-		minify: true,
-		sourcemap: false,
-		format: 'esm',
-		outdir: outDir,
-		platform: 'browser',
-		target: ['es2020'],
-	});
-}
-
-build().catch(() => process.exit(1));
-
-if (isWatch) {
-	watcher.subscribe(srcDir, () => {
-		return build();
-	});
-}
+esbuild.build({
+	entryPoints: [
+		path.join(__dirname, 'preview-src', 'index.ts'),
+		path.join(__dirname, 'preview-src', 'pre'),
+	],
+	bundle: true,
+	minify: true,
+	sourcemap: false,
+	format: 'esm',
+	outdir: outDir,
+	platform: 'browser',
+	target: ['es2020'],
+	watch: isWatch,
+	incremental: isWatch,
+}).catch(() => process.exit(1));
--- extensions/simple-browser/esbuild-preview.js
+++ extensions/simple-browser/esbuild-preview.js
@@ -2,11 +2,9 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-// @ts-check
 const path = require('path');
 const fs = require('fs');
 const esbuild = require('esbuild');
-const watcher = require('@parcel/watcher');
 
 const args = process.argv.slice(2);
 
@@ -18,36 +16,27 @@ if (outputRootIndex >= 0) {
 	outputRoot = args[outputRootIndex + 1];
 }
 
-const srcDir = path.join(__dirname, 'preview-src');
 const outDir = path.join(outputRoot, 'media');
 
-async function build() {
-	fs.copyFileSync(
-		path.join(__dirname, 'node_modules', 'vscode-codicons', 'dist', 'codicon.css'),
-		path.join(outDir, 'codicon.css'));
+fs.copyFileSync(
+	path.join(__dirname, 'node_modules', 'vscode-codicons', 'dist', 'codicon.css'),
+	path.join(outDir, 'codicon.css'));
 
-	fs.copyFileSync(
-		path.join(__dirname, 'node_modules', 'vscode-codicons', 'dist', 'codicon.ttf'),
-		path.join(outDir, 'codicon.ttf'));
+fs.copyFileSync(
+	path.join(__dirname, 'node_modules', 'vscode-codicons', 'dist', 'codicon.ttf'),
+	path.join(outDir, 'codicon.ttf'));
 
-	await esbuild.build({
-		entryPoints: [
-			path.join(srcDir, 'index.ts')
-		],
-		bundle: true,
-		minify: true,
-		sourcemap: false,
-		format: 'esm',
-		outdir: outDir,
-		platform: 'browser',
-		target: ['es2020'],
-	});
-}
-
-build().catch(() => process.exit(1));
-
-if (isWatch) {
-	watcher.subscribe(srcDir, () => {
-		return build();
-	});
-}
+esbuild.build({
+	entryPoints: [
+		path.join(__dirname, 'preview-src', 'index.ts')
+	],
+	bundle: true,
+	minify: true,
+	sourcemap: false,
+	format: 'esm',
+	outdir: outDir,
+	platform: 'browser',
+	target: ['es2020'],
+	watch: isWatch,
+	incremental: isWatch,
+}).catch(() => process.exit(1));
