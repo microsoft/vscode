474babbd6503002e117a760c7591023610700db1
Joe Previte <jjprevite@gmail.com>
2022-03-28 15:23:43 -0700

    refactor(remoteLangPacks): use stripComments from common/json

--- src/vs/server/node/remoteLanguagePacks.ts
+++ src/vs/server/node/remoteLanguagePacks.ts
@@ -4,6 +4,7 @@
  *--------------------------------------------------------------------------------------------*/
 
 import * as fs from 'fs';
+import { stripComments } from 'vs/base/common/json';
 import { FileAccess } from 'vs/base/common/network';
 import * as path from 'vs/base/common/path';
 
@@ -61,28 +62,4 @@ export const getLocaleFromConfig = async (argvResource: string): Promise<string>
 		}
 		return 'en';
 	}
-};
-
-const stripComments = (content: string): string => {
-	const regexp = /('(?:[^\\']*(?:\\.)?)*')|('(?:[^\\']*(?:\\.)?)*')|(\/\*(?:\r?\n|.)*?\*\/)|(\/{2,}.*?(?:(?:\r?\n)|$))/g;
-
-	return content.replace(regexp, (match, _m1, _m2, m3, m4) => {
-		// Only one of m1, m2, m3, m4 matches
-		if (m3) {
-			// A block comment. Replace with nothing
-			return '';
-		} else if (m4) {
-			// A line comment. If it ends in \r?\n then keep it.
-			const length_1 = m4.length;
-			if (length_1 > 2 && m4[length_1 - 1] === '\n') {
-				return m4[length_1 - 2] === '\r' ? '\r\n' : '\n';
-			}
-			else {
-				return '';
-			}
-		} else {
-			// We match a string
-			return match;
-		}
-	});
 };
