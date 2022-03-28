b1fb11e18cbdd3aba09e4aef0b4be07c0731d86f
Tyler Leonhardt <me@tylerleonhardt.com>
2022-03-28 09:05:19 -0700

    have code-oss use device code flow

--- extensions/github-authentication/src/common/env.ts
+++ extensions/github-authentication/src/common/env.ts
@@ -7,7 +7,11 @@ import { Uri } from 'vscode';
 const VALID_DESKTOP_CALLBACK_SCHEMES = [
 	'vscode',
 	'vscode-insiders',
-	'code-oss',
+	// On Windows, some browsers don't seem to redirect back to OSS properly.
+	// As a result, you get stuck in the auth flow. We exclude this from the
+	// list until we can figure out a way to fix this behavior in browsers.
+	// The behavior was experienced on Windows.
+	// 'code-oss',
 	'vscode-wsl',
 	'vscode-exploration'
 ];
