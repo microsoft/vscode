ede16c326949ad0bb83460ded404e4ff86072331
rebornix <penn.lv@gmail.com>
2022-03-28 20:37:36 -0700

    fix #146227.

--- src/vs/workbench/contrib/notebook/browser/view/cellParts/codeCell.ts
+++ src/vs/workbench/contrib/notebook/browser/view/cellParts/codeCell.ts
@@ -54,7 +54,7 @@ export class CodeCell extends Disposable {
 		super();
 
 		const cellEditorOptions = this._register(new CellEditorOptions(this.notebookEditor, this.notebookEditor.notebookOptions, this.configurationService, this.viewCell.language));
-		this._outputContainerRenderer = this.instantiationService.createInstance(CellOutputContainer, notebookEditor, viewCell, templateData, { limit: 2 });
+		this._outputContainerRenderer = this.instantiationService.createInstance(CellOutputContainer, notebookEditor, viewCell, templateData, { limit: 500 });
 		this.cellParts = [...templateData.cellParts, cellEditorOptions, this._outputContainerRenderer];
 
 		const editorHeight = this.calculateInitEditorHeight();
