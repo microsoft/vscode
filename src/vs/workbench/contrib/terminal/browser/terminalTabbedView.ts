a938c990ba80b54b55dc9fb2bd32c6cb79f35465
meganrogge <merogge@microsoft.com>
2022-03-30 15:58:46 -0400

    add support for find count

--- src/vs/workbench/contrib/terminal/browser/terminalTabbedView.ts
+++ src/vs/workbench/contrib/terminal/browser/terminalTabbedView.ts
@@ -150,7 +150,9 @@ export class TerminalTabbedView extends Disposable {
 		});
 
 		this._splitView = new SplitView(parentElement, { orientation: Orientation.HORIZONTAL, proportionalLayout: false });
-
+		this._terminalService.onDidCreateInstance(instance => instance.onDidChangeFindResults(() => {
+			this._findWidget.updateResultCount();
+		}));
 		this._setupSplitView(terminalOuterContainer);
 	}
 
