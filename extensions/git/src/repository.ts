e35886666417a0169f2e07b15fb8ee718b8dca91
Ladislau Szomoru <3372902+lszomoru@users.noreply.github.com>
2022-04-07 15:25:06 +0200

    Update extensions/git/src/repository.ts
    Co-authored-by: João Moreno <mail@joaomoreno.com>

--- extensions/git/src/repository.ts
+++ extensions/git/src/repository.ts
@@ -545,7 +545,7 @@ class DotGitWatcher implements IFileWatcher {
 		// People might be using the watchman fsmonitor hook (https://git-scm.com/docs/githooks#_fsmonitor_watchman)
 		// on large repos. Watchman creates a cookie file inside the git directory whenever a query is run (https://facebook.github.io/watchman/docs/cookies.html)
 		// We need to ignore it to avoid an infinite loop of git operation -> watchman cookie -> git operation
-		const filteredRootWatcher = filterEvent(rootWatcher.event, uri => !/\/\.git(\/index\.lock)?$/.test(uri.path) && !/\/\.watchman-cookie.*/.test(uri.path));
+		const filteredRootWatcher = filterEvent(rootWatcher.event, uri => !/\/\.git(\/index\.lock)?$|\/\.watchman-cookie-/.test(uri.path));
 		this.event = anyEvent(filteredRootWatcher, this.emitter.event);
 
 		repository.onDidRunGitStatus(this.updateTransientWatchers, this, this.disposables);
