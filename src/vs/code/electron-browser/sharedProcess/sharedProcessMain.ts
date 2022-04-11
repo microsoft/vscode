5669b27cacc7aa7f558d844791aafaead7516b4f
Sandeep Somavarapu <sasomava@microsoft.com>
2022-04-11 11:25:48 +0530

    Fix #147103

--- src/vs/code/electron-browser/sharedProcess/sharedProcessMain.ts
+++ src/vs/code/electron-browser/sharedProcess/sharedProcessMain.ts
@@ -99,7 +99,7 @@ import { DiskFileSystemProviderClient, LOCAL_FILE_SYSTEM_CHANNEL_NAME } from 'vs
 import { InspectProfilingService as V8InspectProfilingService } from 'vs/platform/profiling/node/profilingService';
 import { IV8InspectProfilingService } from 'vs/platform/profiling/common/profiling';
 import { IExtensionsScannerService } from 'vs/platform/extensionManagement/common/extensionsScannerService';
-import { ExtensionsScannerService } from 'vs/platform/extensionManagement/electron-sandbox/extensionsScannerService';
+import { ExtensionsScannerService } from 'vs/platform/extensionManagement/node/extensionsScannerService';
 
 class SharedProcessMain extends Disposable {
 
