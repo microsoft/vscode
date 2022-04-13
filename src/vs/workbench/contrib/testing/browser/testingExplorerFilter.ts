5650c1f4846986acfeefcc69995f23482a9dedba
Aman Khalid <aakhalid@umich.edu>
2022-04-13 00:49:45 -0400

    Close #143241: Testing UI filter survives window reload

--- src/vs/workbench/contrib/testing/browser/testingExplorerFilter.ts
+++ src/vs/workbench/contrib/testing/browser/testingExplorerFilter.ts
@@ -65,6 +65,12 @@ export class TestingExplorerFilter extends BaseActionViewItem {
 		const wrapper = this.wrapper = dom.$('.testing-filter-wrapper');
 		container.appendChild(wrapper);
 
+		const history = this.history.get([]);
+		const previousValue = history ? history[history.length - 1] : null;
+		if (previousValue) {
+			this.state.setText(previousValue);
+		}
+
 		const input = this.input = this._register(this.instantiationService.createInstance(ContextScopedSuggestEnabledInputWithHistory, {
 			id: 'testing.explorer.filter',
 			ariaLabel: localize('testExplorerFilterLabel', "Filter text for tests in the explorer"),
@@ -89,7 +95,7 @@ export class TestingExplorerFilter extends BaseActionViewItem {
 				value: this.state.text.value,
 				placeholderText: localize('testExplorerFilter', "Filter (e.g. text, !exclude, @tag)"),
 			},
-			history: this.history.get([])
+			history: history
 		}));
 		this._register(attachSuggestEnabledInputBoxStyler(input, this.themeService));
 
