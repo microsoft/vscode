ac6b490b2d09e4d6831f8fdaf480e4f42bffbdca
Benjamin Pasero <benjamin.pasero@microsoft.com>
2022-04-14 11:34:22 +0200

    retr (#147442)

--- test/smoke/src/areas/extensions/extensions.test.ts
+++ test/smoke/src/areas/extensions/extensions.test.ts
@@ -4,7 +4,7 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { Application, Logger } from '../../../../automation';
-import { installAllHandlers } from '../../utils';
+import { installAllHandlers, retryWithRestart } from '../../utils';
 
 export function setup(logger: Logger) {
 	describe('Extensions', () => {
@@ -13,18 +13,19 @@ export function setup(logger: Logger) {
 		installAllHandlers(logger);
 
 		it('install and enable vscode-smoketest-check extension', async function () {
-			this.retries(3); // https://github.com/microsoft/vscode/issues/146800
+			this.timeout(2 * 60 * 1000); // https://github.com/microsoft/vscode/issues/146800
 
 			const app = this.app as Application;
+			await retryWithRestart(app, async () => {
+				await app.workbench.extensions.openExtensionsViewlet();
+				await app.workbench.extensions.installExtension('ms-vscode.vscode-smoketest-check', true);
 
-			await app.workbench.extensions.openExtensionsViewlet();
-			await app.workbench.extensions.installExtension('ms-vscode.vscode-smoketest-check', true);
+				// Close extension editor because keybindings dispatch is not working when web views are opened and focused
+				// https://github.com/microsoft/vscode/issues/110276
+				await app.workbench.extensions.closeExtension('vscode-smoketest-check');
 
-			// Close extension editor because keybindings dispatch is not working when web views are opened and focused
-			// https://github.com/microsoft/vscode/issues/110276
-			await app.workbench.extensions.closeExtension('vscode-smoketest-check');
-
-			await app.workbench.quickaccess.runCommand('Smoke Test Check');
+				await app.workbench.quickaccess.runCommand('Smoke Test Check');
+			});
 		});
 	});
 }
