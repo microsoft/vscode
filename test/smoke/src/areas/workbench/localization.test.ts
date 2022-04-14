f99ef3a16b793f95021b4039bd9096e00fe790f0
Benjamin Pasero <benjamin.pasero@microsoft.com>
2022-04-14 10:25:03 +0200

    retr

--- test/smoke/src/areas/workbench/localization.test.ts
+++ test/smoke/src/areas/workbench/localization.test.ts
@@ -4,7 +4,7 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { Logger, Application } from '../../../../automation';
-import { installAllHandlers } from '../../utils';
+import { installAllHandlers, retryWithRestart } from '../../utils';
 
 export function setup(logger: Logger) {
 
@@ -14,27 +14,30 @@ export function setup(logger: Logger) {
 		installAllHandlers(logger);
 
 		it('starts with "DE" locale and verifies title and viewlets text is in German', async function () {
-			this.retries(3); // https://github.com/microsoft/vscode/issues/146800
+			this.timeout(2 * 60 * 1000); // https://github.com/microsoft/vscode/issues/146800
 
 			const app = this.app as Application;
-			await app.workbench.extensions.openExtensionsViewlet();
-			await app.workbench.extensions.installExtension('ms-ceintl.vscode-language-pack-de', false);
-			await app.restart({ extraArgs: ['--locale=DE'] });
 
-			const result = await app.workbench.localization.getLocalizedStrings();
-			const localeInfo = await app.workbench.localization.getLocaleInfo();
+			await retryWithRestart(app, async () => {
+				await app.workbench.extensions.openExtensionsViewlet();
+				await app.workbench.extensions.installExtension('ms-ceintl.vscode-language-pack-de', false);
+				await app.restart({ extraArgs: ['--locale=DE'] });
 
-			if (localeInfo.locale === undefined || localeInfo.locale.toLowerCase() !== 'de') {
-				throw new Error(`The requested locale for VS Code was not German. The received value is: ${localeInfo.locale === undefined ? 'not set' : localeInfo.locale}`);
-			}
+				const result = await app.workbench.localization.getLocalizedStrings();
+				const localeInfo = await app.workbench.localization.getLocaleInfo();
 
-			if (localeInfo.language.toLowerCase() !== 'de') {
-				throw new Error(`The UI language is not German. It is ${localeInfo.language}`);
-			}
+				if (localeInfo.locale === undefined || localeInfo.locale.toLowerCase() !== 'de') {
+					throw new Error(`The requested locale for VS Code was not German. The received value is: ${localeInfo.locale === undefined ? 'not set' : localeInfo.locale}`);
+				}
 
-			if (result.open.toLowerCase() !== 'öffnen' || result.close.toLowerCase() !== 'schließen' || result.find.toLowerCase() !== 'finden') {
-				throw new Error(`Received wrong German localized strings: ${JSON.stringify(result, undefined, 0)}`);
-			}
+				if (localeInfo.language.toLowerCase() !== 'de') {
+					throw new Error(`The UI language is not German. It is ${localeInfo.language}`);
+				}
+
+				if (result.open.toLowerCase() !== 'öffnen' || result.close.toLowerCase() !== 'schließen' || result.find.toLowerCase() !== 'finden') {
+					throw new Error(`Received wrong German localized strings: ${JSON.stringify(result, undefined, 0)}`);
+				}
+			});
 		});
 	});
 }
