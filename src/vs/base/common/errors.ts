d1e27d8f7d1fdac8446cc6e7335286331d85c5bc
Henning Dieterichs <hdieterichs@microsoft.com>
2022-04-19 15:55:59 +0200

    Improves utility classes.

--- src/vs/base/common/errors.ts
+++ src/vs/base/common/errors.ts
@@ -248,3 +248,20 @@ export class ErrorNoTelemetry extends Error {
 
 	readonly logTelemetry = false;
 }
+
+/**
+ * This error indicates a bug.
+ * Do not throw this for invalid user input.
+ * Only catch this error to recover gracefully from bugs.
+ */
+export class BugIndicatingError extends Error {
+	constructor(message: string) {
+		super(message);
+		Object.setPrototypeOf(this, BugIndicatingError.prototype);
+
+		// Because we know for sure only buggy code throws this,
+		// we definitely want to break here and fix the bug.
+		// eslint-disable-next-line no-debugger
+		debugger;
+	}
+}
