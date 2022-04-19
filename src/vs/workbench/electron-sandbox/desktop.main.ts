02a92ee13f8fa6bded3b7c85f08d572fe8ab99c0
Ladislau Szomoru <3372902+lszomoru@users.noreply.github.com>
2022-04-19 13:55:44 +0200

    Revert "Workspace trust - add tracing to track down integration test failure (#142609)"
    This reverts commit 0fb0a6df31fe49ab17a67ef33192aca9e1434699.

--- src/vs/workbench/electron-sandbox/desktop.main.ts
+++ src/vs/workbench/electron-sandbox/desktop.main.ts
@@ -279,7 +279,7 @@ export class DesktopMain extends Disposable {
 		const workspaceTrustEnablementService = new WorkspaceTrustEnablementService(configurationService, environmentService);
 		serviceCollection.set(IWorkspaceTrustEnablementService, workspaceTrustEnablementService);
 
-		const workspaceTrustManagementService = new WorkspaceTrustManagementService(configurationService, remoteAuthorityResolverService, storageService, uriIdentityService, environmentService, configurationService, workspaceTrustEnablementService, logService);
+		const workspaceTrustManagementService = new WorkspaceTrustManagementService(configurationService, remoteAuthorityResolverService, storageService, uriIdentityService, environmentService, configurationService, workspaceTrustEnablementService);
 		serviceCollection.set(IWorkspaceTrustManagementService, workspaceTrustManagementService);
 
 		// Update workspace trust so that configuration is updated accordingly
