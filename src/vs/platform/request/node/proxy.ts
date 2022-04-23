158c81b4ea830a8dbc4a9f750a2547369c599a82
jeanp413 <jeanp413@hotmail.com>
2022-04-23 12:32:43 -0500

    Respect NO_PROXY in getSystemProxyURI

--- src/vs/platform/request/node/proxy.ts
+++ src/vs/platform/request/node/proxy.ts
@@ -9,13 +9,33 @@ import { isBoolean } from 'vs/base/common/types';
 export type Agent = any;
 
 function getSystemProxyURI(requestURL: Url, env: typeof process.env): string | null {
-	if (requestURL.protocol === 'http:') {
-		return env.HTTP_PROXY || env.http_proxy || null;
-	} else if (requestURL.protocol === 'https:') {
-		return env.HTTPS_PROXY || env.https_proxy || env.HTTP_PROXY || env.http_proxy || null;
+	const proxy = requestURL.protocol === 'http:'
+		? env.HTTP_PROXY || env.http_proxy || null
+		: requestURL.protocol === 'https:'
+			? env.HTTPS_PROXY || env.https_proxy || env.HTTP_PROXY || env.http_proxy || null
+			: null;
+
+	const noProxy = (env.NO_PROXY || env.no_proxy || '').trim().toLowerCase();
+	if (noProxy === '*') {
+		return null;
 	}
 
-	return null;
+	const filters = noProxy
+		.split(',')
+		.map(s => s.trim().split(':', 2))
+		.map(([name, port]) => ({ name, port }))
+		.filter(filter => !!filter.name)
+		.map(({ name, port }) => {
+			const domain = name[0] === '.' ? name : `.${name}`;
+			return { domain, port };
+		});
+	const hostname = requestURL.hostname?.toLowerCase();
+	const port = requestURL.port || (requestURL.protocol === 'https:' ? '443' : '80');
+	if (hostname && filters.some(({ domain, port: filterPort }) => `.${hostname}`.endsWith(domain) && (!filterPort || port === filterPort))) {
+		return null;
+	}
+
+	return proxy;
 }
 
 export interface IOptions {
@@ -39,7 +59,7 @@ export async function getProxyAgent(rawRequestURL: string, env: typeof process.e
 
 	const opts = {
 		host: proxyEndpoint.hostname || '',
-		port: proxyEndpoint.port || (proxyEndpoint.protocol === 'https' ? '443' : '80'),
+		port: proxyEndpoint.port || (proxyEndpoint.protocol === 'https:' ? '443' : '80'),
 		auth: proxyEndpoint.auth,
 		rejectUnauthorized: isBoolean(options.strictSSL) ? options.strictSSL : true,
 	};
