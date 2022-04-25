872a78f6e9cbabce6ca54ffa20023be6531e451c
Jackson Kearl <jakearl@microsoft.com>
2022-04-25 16:53:52 -0700

    Close #148090

--- src/vs/workbench/contrib/files/common/files.ts
+++ src/vs/workbench/contrib/files/common/files.ts
@@ -98,12 +98,14 @@ export interface IFilesConfiguration extends PlatformIFilesConfiguration, IWorkb
 			badges: boolean;
 		};
 		incrementalNaming: 'simple' | 'smart';
+		fileNesting: {
+			enabled: boolean;
+			expand: boolean;
+			patterns: { [parent: string]: string };
+		};
 		experimental: {
 			fileNesting: {
-				enabled: boolean;
 				operateAsGroup: boolean;
-				expand: boolean;
-				patterns: { [parent: string]: string };
 			};
 		};
 	};
