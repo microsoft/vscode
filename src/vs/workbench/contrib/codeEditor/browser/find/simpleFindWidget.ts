339e1a73cf7d2717b2a3076d5d94cf73f3ccf6ad
meganrogge <merogge@microsoft.com>
2022-04-26 15:20:37 -0700

    fix

--- src/vs/workbench/contrib/codeEditor/browser/find/simpleFindWidget.ts
+++ src/vs/workbench/contrib/codeEditor/browser/find/simpleFindWidget.ts
@@ -310,12 +310,12 @@ export abstract class SimpleFindWidget extends Widget {
 		this._matchesCount.innerText = '';
 		let label = '';
 		this._matchesCount.classList.toggle('no-results', false);
-		if (count?.resultCount === undefined || count?.resultCount <= 0) {
+		if (count?.resultCount && count?.resultCount <= 0) {
 			label = NLS_NO_RESULTS;
 			if (!!this.inputValue) {
 				this._matchesCount.classList.toggle('no-results', true);
 			}
-		} else if (count?.resultCount > 0) {
+		} else if (count?.resultCount) {
 			label = strings.format(NLS_MATCHES_LOCATION, count.resultIndex + 1, count?.resultCount);
 		}
 		this._matchesCount.appendChild(document.createTextNode(label));
