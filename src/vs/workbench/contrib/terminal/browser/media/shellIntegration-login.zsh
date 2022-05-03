c980fc19de6ead399b26cc6fe3c23fb7a7264588
meganrogge <merogge@microsoft.com>
2022-05-03 08:14:24 -0700

    in login file, set zdotdir to be user's zdotdir

--- src/vs/workbench/contrib/terminal/browser/media/shellIntegration-login.zsh
+++ src/vs/workbench/contrib/terminal/browser/media/shellIntegration-login.zsh
@@ -6,6 +6,4 @@ if [[ $options[norcs] = off && -o "login" &&  -f $USER_ZDOTDIR/.zlogin ]]; then
 	. $USER_ZDOTDIR/.zlogin
 fi
 
-# Now that the init script is running, unset ZDOTDIR to ensure ~/.zlogout runs as expected as well
-# as prevent problems that may occur if the user's init scripts depend on ZDOTDIR not being set.
-builtin unset ZDOTDIR
+ZDOTDIR=$USER_ZDOTDIR
