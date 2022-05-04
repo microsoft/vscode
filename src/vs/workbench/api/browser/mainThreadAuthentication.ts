91f6eb803c1e1b28411628904c78dbfdb250ce74
Tyler Leonhardt <me@tylerleonhardt.com>
2022-05-04 16:31:08 -0700

    add owner and comment to a telemetry event

--- src/vs/workbench/api/browser/mainThreadAuthentication.ts
+++ src/vs/workbench/api/browser/mainThreadAuthentication.ts
@@ -273,8 +273,8 @@ export class MainThreadAuthentication extends Disposable implements MainThreadAu
 
 		if (session) {
 			type AuthProviderUsageClassification = {
-				extensionId: { classification: 'SystemMetaData'; purpose: 'FeatureInsight' };
-				providerId: { classification: 'SystemMetaData'; purpose: 'FeatureInsight' };
+				extensionId: { classification: 'SystemMetaData'; purpose: 'FeatureInsight'; owner: '@TylerLeonhardt'; comment: 'Used to see which extensions are using which providers' };
+				providerId: { classification: 'SystemMetaData'; purpose: 'FeatureInsight'; owner: '@TylerLeonhardt'; comment: 'Used to see which extensions are using which providers' };
 			};
 			this.telemetryService.publicLog2<{ extensionId: string; providerId: string }, AuthProviderUsageClassification>('authentication.providerUsage', { providerId, extensionId });
 
