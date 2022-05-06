187a384283ffdb78c554de621a5422c447281e4a
Sandeep Somavarapu <sasomava@microsoft.com>
2022-05-06 18:36:02 +0200

    fix #148926

--- src/vs/workbench/browser/web.main.ts
+++ src/vs/workbench/browser/web.main.ts
@@ -334,7 +334,7 @@ export class BrowserMain extends Disposable {
 		// User data
 		let userDataProvider;
 		if (indexedDB) {
-			userDataProvider = new IndexedDBFileSystemProvider(logsPath.scheme, indexedDB, userDataStore, false);
+			userDataProvider = new IndexedDBFileSystemProvider(Schemas.vscodeUserData, indexedDB, userDataStore, true);
 			this.registerDeveloperActions(<IndexedDBFileSystemProvider>userDataProvider);
 		} else {
 			logService.info('Using in-memory user data provider');
