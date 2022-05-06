a5f0a295270dcae74b153e8c641dc3d9ebbdf3d2
Daniel Imms <2193314+Tyriar@users.noreply.github.com>
2022-05-06 08:11:50 -0700

    Fix, re-enable or disable skipped terminal smoke tests
    Fixes #146700

--- test/smoke/src/areas/terminal/terminal.test.ts
+++ test/smoke/src/areas/terminal/terminal.test.ts
@@ -17,25 +17,17 @@ export function setup(logger: Logger) {
 	describe('Terminal', function () {
 
 		// Retry tests 3 times to minimize build failures due to any flakiness
-		this.retries(3);
+		// this.retries(3);
 
 		// Shared before/after handling
 		installAllHandlers(logger);
 
+		let app: Application;
 		let terminal: Terminal;
 		before(async function () {
 			// Fetch terminal automation API
-			const app = this.app as Application;
+			app = this.app as Application;
 			terminal = app.workbench.terminal;
-
-			// Always show tabs to make getting terminal groups easier
-			await app.workbench.settingsEditor.addUserSetting('terminal.integrated.tabs.hideCondition', '"never"');
-			// Use the DOM renderer for smoke tests so they can be inspected in the playwright trace
-			// viewer
-			await app.workbench.settingsEditor.addUserSetting('terminal.integrated.gpuAcceleration', '"off"');
-
-			// Close the settings editor
-			await app.workbench.quickaccess.runCommand('workbench.action.closeAllEditors');
 		});
 
 		afterEach(async () => {
