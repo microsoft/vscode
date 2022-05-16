d8e76554d1d4b36ebc216ce9689cba609660a397
Henning Dieterichs <hdieterichs@microsoft.com>
2022-05-16 17:08:31 +0200

    Avoid symbol instantiation.

--- src/vs/workbench/contrib/mergeEditor/browser/mergeEditorModel.ts
+++ src/vs/workbench/contrib/mergeEditor/browser/mergeEditorModel.ts
@@ -65,7 +65,7 @@ export class MergeEditorModelFactory {
 	}
 }
 
-const InternalSymbol = Symbol();
+const InternalSymbol: unique symbol = null!;
 
 export class MergeEditorModel extends EditorModel {
 	private resultEdits = new ResultEdits([], this.base, this.result, this.editorWorkerService);
