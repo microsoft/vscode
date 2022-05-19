d3bd94f7e2c2c3a8d5455cf1c020888354b5f3db
meganrogge <merogge@microsoft.com>
2022-05-19 14:54:01 -0700

    fix tests

--- src/vs/workbench/contrib/tasks/test/browser/taskTerminalStatus.test.ts
+++ src/vs/workbench/contrib/tasks/test/browser/taskTerminalStatus.test.ts
@@ -3,7 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { strictEqual } from 'assert';
+import { deepStrictEqual, ok, strictEqual } from 'assert';
 import { Emitter, Event } from 'vs/base/common/event';
 import { TestConfigurationService } from 'vs/platform/configuration/test/common/testConfigurationService';
 import { TestInstantiationService } from 'vs/platform/instantiation/test/common/instantiationServiceMock';
@@ -12,7 +12,7 @@ import { AbstractProblemCollector } from 'vs/workbench/contrib/tasks/common/prob
 import { CommonTask, TaskEvent, TaskEventKind, TaskRunType } from 'vs/workbench/contrib/tasks/common/tasks';
 import { ITaskService, Task } from 'vs/workbench/contrib/tasks/common/taskService';
 import { ITerminalInstance } from 'vs/workbench/contrib/terminal/browser/terminal';
-import { TerminalStatusList } from 'vs/workbench/contrib/terminal/browser/terminalStatusList';
+import { ITerminalStatus, ITerminalStatusList, TerminalStatusList } from 'vs/workbench/contrib/terminal/browser/terminalStatusList';
 
 class TestTaskService implements Partial<ITaskService> {
 	private readonly _onDidStateChange: Emitter<TaskEvent> = new Emitter();
@@ -57,18 +57,29 @@ suite('Task Terminal Status', () => {
 	});
 	test('Should add failed status when there is an exit code on task end', async () => {
 		taskTerminalStatus.addTerminal(testTask, testTerminal, problemCollector);
+		taskService.triggerStateChange({ kind: TaskEventKind.ProcessStarted });
+		ok(assertStatus(testTerminal.statusList, ACTIVE_TASK_STATUS));
+		taskService.triggerStateChange({ kind: TaskEventKind.Inactive });
+		ok(assertStatus(testTerminal.statusList, SUCCEEDED_TASK_STATUS));
 		taskService.triggerStateChange({ kind: TaskEventKind.End, exitCode: 1 });
-		setTimeout(() => strictEqual(testTerminal.statusList.statuses, [FAILED_TASK_STATUS]), 200);
+		ok(assertStatus(testTerminal.statusList, FAILED_TASK_STATUS));
 	});
 	test('Should add active status when a non-background task is run for a second time in the same terminal', async () => {
-		await taskTerminalStatus.addTerminal(testTask, testTerminal, problemCollector);
+		taskTerminalStatus.addTerminal(testTask, testTerminal, problemCollector);
 		taskService.triggerStateChange({ kind: TaskEventKind.ProcessStarted });
-		await setTimeout(() => strictEqual(testTerminal.statusList.statuses, [ACTIVE_TASK_STATUS]), 200);
+		ok(assertStatus(testTerminal.statusList, ACTIVE_TASK_STATUS));
 		taskService.triggerStateChange({ kind: TaskEventKind.Inactive });
-		await setTimeout(() => strictEqual(testTerminal.statusList.statuses, [SUCCEEDED_TASK_STATUS]), 200);
+		ok(assertStatus(testTerminal.statusList, SUCCEEDED_TASK_STATUS));
 		taskService.triggerStateChange({ kind: TaskEventKind.ProcessStarted, runType: TaskRunType.SingleRun });
-		await setTimeout(() => strictEqual(testTerminal.statusList.statuses, [ACTIVE_TASK_STATUS]), 200);
+		ok(assertStatus(testTerminal.statusList, ACTIVE_TASK_STATUS));
 		taskService.triggerStateChange({ kind: TaskEventKind.Inactive });
-		await setTimeout(() => strictEqual(testTerminal.statusList.statuses, [SUCCEEDED_TASK_STATUS]), 200);
+		ok(assertStatus(testTerminal.statusList, SUCCEEDED_TASK_STATUS));
 	});
 });
+
+function assertStatus(actual: ITerminalStatusList, expected: ITerminalStatus): boolean {
+	if (actual.statuses.length !== 1) {
+		return false;
+	}
+	return actual.statuses[0].id === expected.id;
+}
