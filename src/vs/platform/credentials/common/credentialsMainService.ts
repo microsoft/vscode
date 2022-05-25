f9a9a82bc7cf03fa873cc686db25fb2dfdcd22e9
Ian Huff <ianhuff@Ians-MacBook-Pro.local>
2022-05-25 11:20:41 -0700

    update comment with more info

--- src/vs/platform/credentials/common/credentialsMainService.ts
+++ src/vs/platform/credentials/common/credentialsMainService.ts
@@ -166,8 +166,12 @@ export abstract class BaseCredentialsMainService extends Disposable implements I
 				}
 			}
 		} catch {
-			// Don't fail on trying to JSON.parse the password chunk deletion. Just go ahead with the results
-			// of didDelete
+			// When the password is saved the entire JSON payload is encrypted then stored, thus the result from getPassword might not be valid JSON
+			// https://github.com/microsoft/vscode/blob/c22cb87311b5eb1a3bf5600d18733f7485355dc0/src/vs/workbench/api/browser/mainThreadSecretState.ts#L83
+			// However in the chunked case we JSONify each chunk after encryption so for the chunked case we do expect valid JSON here
+			// https://github.com/microsoft/vscode/blob/708cb0c507d656b760f9d08115b8ebaf8964fd73/src/vs/platform/credentials/common/credentialsMainService.ts#L128
+			// Empty catch here just as in getPassword because we expect to handle both JSON cases and non JSON cases here it's not an error case to fail to parse
+			// https://github.com/microsoft/vscode/blob/708cb0c507d656b760f9d08115b8ebaf8964fd73/src/vs/platform/credentials/common/credentialsMainService.ts#L76
 		}
 
 		if (didDelete) {
