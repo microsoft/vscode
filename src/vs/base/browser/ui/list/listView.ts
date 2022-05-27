6071a910bbcd6ae1d6587f33ecc0f2225b581c7b
Rob Lourens <roblourens@gmail.com>
2022-05-27 14:26:26 -0700

    Revert "Update scroll dimensions before scrolltop, otherwise the scrollTop will be clamped when a cell above the viewport got larger"
    This reverts commit 35c61b2786544bf3c8c7a10f12f7472b24c1cb3d.

--- src/vs/base/browser/ui/list/listView.ts
+++ src/vs/base/browser/ui/list/listView.ts
@@ -448,10 +448,10 @@ export class ListView<T> implements ISpliceable<T>, IDisposable {
 		this.items[index].size = size;
 
 		this.render(lastRenderRange, Math.max(0, this.lastRenderTop + heightDiff), this.lastRenderHeight, undefined, undefined, true);
+		this.setScrollTop(this.lastRenderTop);
 
 		// Must update scrollHeight before scrollTop, in case scrollTop won't fit in the old scrollHeight
 		this.eventuallyUpdateScrollDimensions();
-		this.setScrollTop(this.lastRenderTop);
 
 		if (this.supportDynamicHeights) {
 			this._rerender(this.lastRenderTop, this.lastRenderHeight);
