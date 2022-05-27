48bc303a8fd47c1d8d2a8ec3b810a12adb649bc2
Daniel Keller <redink@google.com>
2022-05-27 11:50:34 +0200

    Fix webview issue with CSP in Firefox

--- src/vs/workbench/contrib/webview/browser/webviewElement.ts
+++ src/vs/workbench/contrib/webview/browser/webviewElement.ts
@@ -510,10 +510,7 @@ export class WebviewElement extends Disposable implements IWebview, WebviewFindD
 
 		const queryString = new URLSearchParams(params).toString();
 
-		// Workaround for https://bugzilla.mozilla.org/show_bug.cgi?id=1754872
-		const fileName = isFirefox ? 'index-no-csp.html' : 'index.html';
-
-		this.element!.setAttribute('src', `${this.webviewContentEndpoint(encodedWebviewOrigin)}/${fileName}?${queryString}`);
+		this.element!.setAttribute('src', `${this.webviewContentEndpoint(encodedWebviewOrigin)}/index.html?${queryString}`);
 	}
 
 	public mountTo(parent: HTMLElement) {
