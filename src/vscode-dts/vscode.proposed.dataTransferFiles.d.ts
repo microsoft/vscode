c8a7a02799950508d5ff4f8dd36caa012b37a532
Matt Bierner <matb@microsoft.com>
2022-06-09 15:30:36 -0700

    Clean up DataTransferKind docs

--- src/vscode-dts/vscode.proposed.dataTransferFiles.d.ts
+++ src/vscode-dts/vscode.proposed.dataTransferFiles.d.ts
@@ -7,15 +7,6 @@ declare module 'vscode' {
 
 	// https://github.com/microsoft/vscode/issues/147481
 
-	enum DataTransferItemKind {
-		String = 1,
-		File = 2,
-	}
-
-	interface DataTransferItem {
-		readonly kind: DataTransferItemKind;
-	}
-
 	/**
 	 * A file associated with a {@linkcode DataTransferItem}.
 	 */
@@ -38,13 +29,42 @@ declare module 'vscode' {
 		data(): Thenable<Uint8Array>;
 	}
 
+	/**
+	 * Identifies the kind of a {@link DataTransferItem}. May either be {@linkcode DataTransferItemKind.String String} or {@linkcode DataTransferItemKind.File File}.
+	 */
+	enum DataTransferItemKind {
+
+		/**
+		 * The {@link DataTransferItem} is a string.
+		 *
+		 * Use {@link DataTransferItem.asString} to get a string representation of this item or
+		 * {@link DataTransferItem.value} to access the original value if them item was created
+		 * by an extension.
+		 */
+		String = 1,
+
+		/**
+		 * The {@link DataTransferItem} is for a file.
+		 *
+		 * Use {@link DataTransferItem.asFile} to get the underlying file data.
+		 */
+		File = 2,
+	}
+
 	export interface DataTransferItem {
+
+		/**
+		 * The kind of the {@link DataTransferItem}.
+		 */
+		readonly kind: DataTransferItemKind;
+
 		/**
 		 * Try getting the file associated with this data transfer item.
 		 *
 		 * Note that the file object is only valid for the scope of the drag and drop operation.
 		 *
-		 * @returns The file for the data transfer or `undefined` if the item is not a file.
+		 * @returns The file for the data transfer or `undefined` if the item is either not a file or the
+		 * file data cannot be accessed.
 		 */
 		asFile(): DataTransferFile | undefined;
 	}
