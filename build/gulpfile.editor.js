daed394d449be23dbf1842cec2ab746e10526c95
Alex Dima <alexdima@microsoft.com>
2022-06-15 16:10:06 +0200

    Bring the css loader plugin into our sources

--- build/gulpfile.editor.js
+++ build/gulpfile.editor.js
@@ -112,9 +112,6 @@ const createESMSourcesAndResourcesTask = task.define('extract-editor-esm', () =>
 			'vs/nls.ts',
 			'vs/nls.build.js',
 			'vs/nls.d.ts',
-			'vs/css.js',
-			'vs/css.build.js',
-			'vs/css.d.ts',
 			'vs/base/worker/workerMain.ts',
 		],
 		renames: {
