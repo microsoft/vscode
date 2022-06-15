a665d60f78c1e61fd7b1ead12497606525b351d8
Ladislau Szomoru <3372902+lszomoru@users.noreply.github.com>
2022-06-15 15:13:55 +0200

    Introduce the idea of ITerminalEnvironmentProvider

--- extensions/git/src/gitEditor.ts
+++ extensions/git/src/gitEditor.ts
@@ -5,20 +5,29 @@
 import * as path from 'path';
 import { TabInputText, Uri, window, workspace } from 'vscode';
 import { IIPCHandler, IIPCServer } from './ipc/ipcServer';
+import { ITerminalEnvironmentProvider } from './terminal';
 import { EmptyDisposable, IDisposable } from './util';
 
 interface GitEditorRequest {
 	commitMessagePath?: string;
 }
 
-export class GitEditor implements IIPCHandler {
+export class GitEditor implements IIPCHandler, ITerminalEnvironmentProvider {
 
+	private env: { [key: string]: string };
 	private disposable: IDisposable = EmptyDisposable;
 
-	constructor(private ipc?: IIPCServer) {
+	constructor(ipc?: IIPCServer) {
 		if (ipc) {
 			this.disposable = ipc.registerHandler('git-editor', this);
 		}
+
+		this.env = {
+			GIT_EDITOR: `"${path.join(__dirname, ipc ? 'git-editor.sh' : 'git-editor-empty.sh')}"`,
+			VSCODE_GIT_EDITOR_NODE: process.execPath,
+			VSCODE_GIT_EDITOR_EXTRA_ARGS: (process.versions['electron'] && process.versions['microsoft-build']) ? '--ms-enable-electron-run-as-node' : '',
+			VSCODE_GIT_EDITOR_MAIN: path.join(__dirname, 'git-editor-main.js')
+		};
 	}
 
 	async handle({ commitMessagePath }: GitEditorRequest): Promise<any> {
@@ -40,16 +49,12 @@ export class GitEditor implements IIPCHandler {
 
 	getEnv(): { [key: string]: string } {
 		const config = workspace.getConfiguration('git');
-		if (config.get<boolean>('useEditorAsCommitInput')) {
-			return {
-				GIT_EDITOR: `"${path.join(__dirname, this.ipc ? 'git-editor.sh' : 'git-editor-empty.sh')}"`,
-				VSCODE_GIT_EDITOR_NODE: process.execPath,
-				VSCODE_GIT_EDITOR_EXTRA_ARGS: (process.versions['electron'] && process.versions['microsoft-build']) ? '--ms-enable-electron-run-as-node' : '',
-				VSCODE_GIT_EDITOR_MAIN: path.join(__dirname, 'git-editor-main.js')
-			};
-		}
+		return config.get<boolean>('useEditorAsCommitInput') ? this.env : {};
+	}
 
-		return {};
+	getTerminalEnv(): { [key: string]: string } {
+		const config = workspace.getConfiguration('git');
+		return config.get<boolean>('useEditorAsCommitInput') && config.get<boolean>('terminalGitEditor') ? this.env : {};
 	}
 
 	dispose(): void {
--- extensions/git/src/terminal.ts
+++ extensions/git/src/terminal.ts
@@ -6,33 +6,34 @@
 import { ExtensionContext, workspace } from 'vscode';
 import { filterEvent, IDisposable } from './util';
 
+export interface ITerminalEnvironmentProvider {
+	getTerminalEnv(): { [key: string]: string };
+}
+
 export class TerminalEnvironmentManager {
 
 	private readonly disposable: IDisposable;
 
-	constructor(private readonly context: ExtensionContext, private readonly env: { [key: string]: string }) {
-		this.disposable = filterEvent(workspace.onDidChangeConfiguration, e =>
-			e.affectsConfiguration('git.enabled') ||
-			e.affectsConfiguration('git.terminalAuthentication') ||
-			e.affectsConfiguration('git.terminalGitEditor')
-		)(this.refresh, this);
+	constructor(private readonly context: ExtensionContext, private readonly envProviders: (ITerminalEnvironmentProvider | undefined)[]) {
+		this.disposable = filterEvent(workspace.onDidChangeConfiguration, e => e.affectsConfiguration('git'))
+			(this.refresh, this);
 
 		this.refresh();
 	}
 
 	private refresh(): void {
-		this.context.environmentVariableCollection.clear();
 		const config = workspace.getConfiguration('git', null);
+		this.context.environmentVariableCollection.clear();
 
 		if (!config.get<boolean>('enabled', true)) {
 			return;
 		}
 
-		for (const name of Object.keys(this.env)) {
-			if ((name.toUpperCase() !== 'GIT_ASKPASS' && name.toUpperCase() !== 'GIT_EDITOR') ||
-				(name.toUpperCase() === 'GIT_ASKPASS' && config.get<boolean>('terminalAuthentication', true)) ||
-				(name.toUpperCase() === 'GIT_EDITOR' && config.get<boolean>('terminalGitEditor', true))) {
-				this.context.environmentVariableCollection.replace(name, this.env[name]);
+		for (const envProvider of this.envProviders) {
+			const terminalEnv = envProvider?.getTerminalEnv() ?? {};
+
+			for (const name of Object.keys(terminalEnv)) {
+				this.context.environmentVariableCollection.replace(name, terminalEnv[name]);
 			}
 		}
 	}
