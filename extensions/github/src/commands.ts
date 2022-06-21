5ca0628be656c6cc9953d782bfee4d19c6d14e95
Alex Ross <alros@microsoft.com>
2022-06-21 18:21:07 +0200

    Add vscod.dev command in github extension

--- extensions/github/src/commands.ts
+++ extensions/github/src/commands.ts
@@ -7,6 +7,7 @@ import * as vscode from 'vscode';
 import { API as GitAPI } from './typings/git';
 import { publishRepository } from './publish';
 import { DisposableStore } from './util';
+import { getPermalink } from './links';
 
 export function registerCommands(gitAPI: GitAPI): vscode.Disposable {
 	const disposables = new DisposableStore();
@@ -14,6 +15,17 @@ export function registerCommands(gitAPI: GitAPI): vscode.Disposable {
 	disposables.add(vscode.commands.registerCommand('github.publish', async () => {
 		try {
 			publishRepository(gitAPI);
+		} catch (err) {
+			vscode.window.showErrorMessage(err.message);
+		}
+	}));
+
+	disposables.add(vscode.commands.registerCommand('github.copyVscodeDevLink', async () => {
+		try {
+			const permalink = getPermalink(gitAPI, 'https://vscode.dev/github');
+			if (permalink) {
+				vscode.env.clipboard.writeText(permalink);
+			}
 		} catch (err) {
 			vscode.window.showErrorMessage(err.message);
 		}
