730e8dea244b38cfbdf2561224ddee26e0c15e70
Matt Bierner <matb@microsoft.com>
2022-06-21 08:32:57 -0700

    Standardize text/uri-list handling
    This creates a common set of functions for creating and parsing the `text/uri-list` mime type. As part of this, I also aligned us with the standard, which uses `\r\n` between lines instead of just `\n`

--- src/vs/editor/contrib/copyPaste/browser/copyPasteController.ts
+++ src/vs/editor/contrib/copyPaste/browser/copyPasteController.ts
@@ -11,7 +11,7 @@ import { createStringDataTransferItem, VSDataTransfer } from 'vs/base/common/dat
 import { Disposable } from 'vs/base/common/lifecycle';
 import { Mimes } from 'vs/base/common/mime';
 import { generateUuid } from 'vs/base/common/uuid';
-import { toVSDataTransfer } from 'vs/editor/browser/dnd';
+import { toVSDataTransfer, UriList } from 'vs/editor/browser/dnd';
 import { ICodeEditor } from 'vs/editor/browser/editorBrowser';
 import { IBulkEditService, ResourceEdit } from 'vs/editor/browser/services/bulkEditService';
 import { EditorOption } from 'vs/editor/common/config/editorOptions';
@@ -177,8 +177,7 @@ export class CopyPasteController extends Disposable implements IEditorContributi
 			if (!dataTransfer.has(Mimes.uriList)) {
 				const resources = await this._clipboardService.readResources();
 				if (resources.length) {
-					const value = resources.join('\n');
-					dataTransfer.append(Mimes.uriList, createStringDataTransferItem(value));
+					dataTransfer.append(Mimes.uriList, createStringDataTransferItem(UriList.create(resources)));
 				}
 			}
 
