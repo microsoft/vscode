6d35288333802f2164e1165c38ee447293a2042b
Sandeep Somavarapu <sasomava@microsoft.com>
2022-06-23 18:37:10 +0200

    CLI: initialize state and profiles (#152994)
    - create state service (readonly)  in node
    - split state service between node and main
    - split profiles service between node and main
    - set up and init state and profiles services in cli

--- src/vs/platform/state/node/state.ts
+++ src/vs/platform/state/node/state.ts
@@ -4,18 +4,14 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { createDecorator } from 'vs/platform/instantiation/common/instantiation';
-import { IStateService } from 'vs/platform/state/node/state';
 
-export const IStateMainService = createDecorator<IStateMainService>('stateMainService');
+export const IStateService = createDecorator<IStateService>('stateService');
 
-export interface IStateMainService extends IStateService {
+export interface IStateService {
 
 	readonly _serviceBrand: undefined;
 
-	setItem(key: string, data?: object | string | number | boolean | undefined | null): void;
-	setItems(items: readonly { key: string; data?: object | string | number | boolean | undefined | null }[]): void;
+	getItem<T>(key: string, defaultValue: T): T;
+	getItem<T>(key: string, defaultValue?: T): T | undefined;
 
-	removeItem(key: string): void;
-
-	close(): Promise<void>;
 }
