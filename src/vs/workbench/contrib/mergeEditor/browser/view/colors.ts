8b896308ac1da5355bec3770d8336fda82317d4b
Henning Dieterichs <hdieterichs@microsoft.com>
2022-06-24 16:23:15 +0200

    Fixes #152950. Cleans up colors/css & uses diff editor decorations as default colors.

--- src/vs/workbench/contrib/mergeEditor/browser/view/colors.ts
+++ src/vs/workbench/contrib/mergeEditor/browser/view/colors.ts
@@ -6,103 +6,90 @@
 import { localize } from 'vs/nls';
 import { registerColor } from 'vs/platform/theme/common/colorRegistry';
 
-export const modifiedBaseRangeInput1 = registerColor(
-	'mergeEditor.modifiedBaseRange.input1',
-	{
-		dark: '#a44c9d53',
-		light: '#a44c9d38',
-		hcDark: '#a44c9d53',
-		hcLight: '#a44c9d38',
-	},
-	localize(
-		'mergeEditor.modifiedBaseRange.input1',
-		'The foreground color for changes in input 1.'
-	)
-);
-
-export const modifiedBaseRangeInput2 = registerColor(
-	'mergeEditor.modifiedBaseRange.input2',
-	{
-		dark: '#878feb53',
-		light: '#878feb53',
-		hcDark: '#878feb53',
-		hcLight: '#878feb53',
-	},
-	localize(
-		'mergeEditor.modifiedBaseRange.input2',
-		'The foreground color for changes in input 2.'
-	)
-);
-
-export const modifiedBaseRangeCombination = registerColor(
-	'mergeEditor.modifiedBaseRange.combination',
-	{
-		dark: '#8a4d249e',
-		light: '#8a4d243e',
-		hcDark: '#8a4d249e',
-		hcLight: '#8a4d243e',
-	},
-	localize(
-		'mergeEditor.modifiedBaseRange.combination',
-		'The foreground color for combined changes in the result editor.'
-	)
-);
-
-export const diffInput1 = registerColor(
-	'mergeEditor.diff.input1',
-	{
-		dark: '#e571db21',
-		light: '#e571db21',
-		hcDark: '#e571db21',
-		hcLight: '#e571db21',
-	},
-	localize(
-		'mergeEditor.diff.input1',
-		'The foreground color for changed words in input 1.'
-	)
-);
-
 export const diff = registerColor(
 	'mergeEditor.diff',
 	{
-		dark: '#d3d3d321',
-		light: '#d3d3d321',
-		hcDark: '#d3d3d321',
-		hcLight: '#d3d3d321',
+		dark: '#9bb95533',
+		light: '#9bb95533',
+		hcDark: '#9bb95533',
+		hcLight: '#9bb95533',
 	},
 	localize(
 		'mergeEditor.diff',
-		'The foreground color for changes in the result.'
+		'The foreground color for changes.'
 	)
 );
 
 export const diffWord = registerColor(
 	'mergeEditor.diff.word',
 	{
-		dark: '#e571db21',
-		light: '#e571db21',
-		hcDark: '#e571db21',
-		hcLight: '#e571db21',
+		dark: '#9bb9551e',
+		light: '#9bb9551e',
+		hcDark: '#9bb9551e',
+		hcLight: '#9bb9551e',
 	},
 	localize(
 		'mergeEditor.diff.word',
-		'The foreground color for word changes in the result.'
+		'The foreground color for word changes.'
 	)
 );
 
-export const diffInput2 = registerColor(
-	'mergeEditor.diff.input2',
+export const conflictBorderUnhandledUnfocused = registerColor(
+	'mergeEditor.conflictBorder.unhandledUnfocused',
 	{
-		dark: '#878feb53',
-		light: '#878feb53',
-		hcDark: '#878feb53',
-		hcLight: '#878feb53',
+		dark: '#ffa6007a',
+		light: '#ffa6007a',
+		hcDark: '#ffa6007a',
+		hcLight: '#ffa6007a',
 	},
 	localize(
-		'mergeEditor.diff.input2',
-		'The foreground color for changed words in input 2.'
+		'mergeEditor.conflictBorder.unhandledUnfocused',
+		'The border color of unhandled unfocused conflicts.'
 	)
 );
+
+export const conflictBorderUnhandledFocused = registerColor(
+	'mergeEditor.conflictBorder.unhandledFocused',
+	{
+		dark: '#ffa600',
+		light: '#ffa600',
+		hcDark: '#ffa600',
+		hcLight: '#ffa600',
+	},
+	localize(
+		'mergeEditor.conflictBorder.unhandledFocused',
+		'The border color of unhandled focused conflicts.'
+	)
+);
+
+export const conflictBorderHandledUnfocused = registerColor(
+	'mergeEditor.conflictBorder.handledUnfocused',
+	{
+		dark: '#86868649',
+		light: '#86868649',
+		hcDark: '#86868649',
+		hcLight: '#86868649',
+	},
+	localize(
+		'mergeEditor.conflictBorder.handledUnfocused',
+		'The border color of handled unfocused conflicts.'
+	)
+);
+
+export const conflictBorderHandledFocused = registerColor(
+	'mergeEditor.conflictBorder.handledFocused',
+	{
+		dark: '#c1c1c1cc',
+		light: '#c1c1c1cc',
+		hcDark: '#c1c1c1cc',
+		hcLight: '#c1c1c1cc',
+	},
+	localize(
+		'mergeEditor.conflictBorder.handledFocused',
+		'The border color of handled focused conflicts.'
+	)
+);
+
 
 export const handledConflictMinimapOverViewRulerColor = registerColor(
 	'mergeEditor.conflict-handled.minimapOverViewRuler',
@@ -118,7 +105,6 @@ export const handledConflictMinimapOverViewRulerColor = registerColor(
 	)
 );
 
-
 export const unhandledConflictMinimapOverViewRulerColor = registerColor(
 	'mergeEditor.conflict-unhandled.minimapOverViewRuler',
 	{
@@ -132,4 +118,3 @@ export const unhandledConflictMinimapOverViewRulerColor = registerColor(
 		'The foreground color for changes in input 1.'
 	)
 );
-
