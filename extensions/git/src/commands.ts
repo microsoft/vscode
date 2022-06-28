6af066db358f7d568fe3ba3685edccbb66651fd2
Ladislau Szomoru <3372902+lszomoru@users.noreply.github.com>
2022-06-28 10:39:44 +0200

    Fix create branch from issue

--- extensions/git/src/commands.ts
+++ extensions/git/src/commands.ts
@@ -26,8 +26,9 @@ const localize = nls.loadMessageBundle();
 class CheckoutItem implements QuickPickItem {
 
 	protected get shortCommit(): string { return (this.ref.commit || '').substr(0, 8); }
-	get label(): string { return this.ref.name || this.shortCommit; }
+	get label(): string { return `${this.repository.isBranchProtected(this.ref.name ?? '') ? '$(lock)' : '$(git-branch)'} ${this.ref.name || this.shortCommit}`; }
 	get description(): string { return this.shortCommit; }
+	get refName(): string | undefined { return this.ref.name; }
 
 	constructor(protected repository: Repository, protected ref: Ref) { }
 
@@ -44,6 +45,7 @@ class CheckoutItem implements QuickPickItem {
 
 class CheckoutTagItem extends CheckoutItem {
 
+	override get label(): string { return `$(tag) ${this.ref.name || this.shortCommit}`; }
 	override get description(): string {
 		return localize('tag at', "Tag at {0}", this.shortCommit);
 	}
@@ -51,6 +53,7 @@ class CheckoutTagItem extends CheckoutItem {
 
 class CheckoutRemoteHeadItem extends CheckoutItem {
 
+	override get label(): string { return `$(git-branch) ${this.ref.name || this.shortCommit}`; }
 	override get description(): string {
 		return localize('remote branch at', "Remote branch at {0}", this.shortCommit);
 	}
@@ -1996,7 +1999,9 @@ export class CommandCenter {
 				return;
 			}
 
-			target = choice.label;
+			if (choice instanceof CheckoutItem && choice.refName) {
+				target = choice.refName;
+			}
 		}
 
 		await repository.branch(branchName, true, target);
