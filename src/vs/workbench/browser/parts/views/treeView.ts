290210f496ca2299a21f715e2a0630a4ec102b68
Matt Bierner <matb@microsoft.com>
2022-06-29 13:30:08 -0700

    Allow tree to specify `'files'` as drop type (#153751)
    For https://github.com/microsoft/vscode/issues/153404#issuecomment-1170400253
    
    This special type indicates that the tree wants to recieve any type of file that is dropped. This matches how the dom drop apis work

--- src/vs/workbench/browser/parts/views/treeView.ts
+++ src/vs/workbench/browser/parts/views/treeView.ts
@@ -1499,7 +1499,7 @@ export class CustomTreeViewDragAndDrop implements ITreeDragAndDrop<ITreeItem> {
 
 		const outDataTransfer = new VSDataTransfer();
 		for (const [type, item] of originalDataTransfer.entries()) {
-			if (type === this.treeMimeType || dndController.dropMimeTypes.indexOf(type) >= 0) {
+			if (type === this.treeMimeType || dndController.dropMimeTypes.includes(type) || (item.asFile() && dndController.dropMimeTypes.includes(DataTransfers.FILES.toLowerCase()))) {
 				outDataTransfer.append(type, item);
 				if (type === this.treeMimeType) {
 					try {
