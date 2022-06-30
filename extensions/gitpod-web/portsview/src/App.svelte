arwinneil/openvscode-server

Support port view to unify with vscode

extensions/gitpod-web/portsview/src/App.svelte
@@ -1,0 +1,26 @@
+<!-----------------------------------------------------------------------------------------------
+	Copyright (c) Gitpod. All rights reserved.
+------------------------------------------------------------------------------------------------>
+<script lang="ts">
+	import "@vscode/codicons/dist/codicons.css";
+	import { vscode } from "./utils/vscode";
+	import PortTable from "./porttable/PortTable.svelte";
+	import type { GitpodPortObject } from "./types/gitpod";
+
+	let ports: GitpodPortObject[] = [];
+
+	window.addEventListener("message", (event) => {
+		if (event.data.command === "updatePorts") {
+			// TODO: sort with status first, then port number?
+			ports = event.data.ports.sort(
+				(a: GitpodPortObject, b: GitpodPortObject) =>
+					a.status.localPort - b.status.localPort
+			);
+		}
+	});
+	vscode.postMessage({ command: "queryPortData" });
+</script>
+
+<main>
+	<PortTable {ports} />
+</main>

extensions/gitpod-web/portsview/src/components/ContextMenu.svelte
@@ -1,0 +1,85 @@
+<!-----------------------------------------------------------------------------------------------
+	Copyright (c) Gitpod. All rights reserved.
+------------------------------------------------------------------------------------------------>
+<script lang="ts">
+	import { createEventDispatcher } from "svelte";
+	import type { MenuOption } from "../types/components";
+
+	export let x = 0;
+	export let y = 0;
+	export let show = false;
+
+	export let options: Array<MenuOption | null> = [];
+
+	let menuEl: HTMLDivElement;
+
+	$: ((x: number, y: number) => {
+		if (!show || !menuEl) return;
+		const rect = menuEl.getBoundingClientRect();
+		x = Math.min(window.innerWidth - rect.width, x);
+		if (y > window.innerHeight - rect.height) y -= rect.height;
+	})(x, y);
+
+	const dispatch = createEventDispatcher<{
+		command: string;
+		clickoutside: string;
+	}>();
+
+	function onPageClick(e: MouseEvent) {
+		if (!show) return;
+		// @ts-ignore
+		if (e.target === menuEl || menuEl.contains(e.target)) return;
+		dispatch("clickoutside");
+	}
+</script>
+
+<main>
+	{#if show}
+		<div class="menu" bind:this={menuEl} style="top: {y}px; left: {x}px;">
+			{#each options as menu}
+				{#if menu == null}
+					<div class="divider" />
+				{:else}
+					<div class="opt" on:click={() => dispatch("command", menu.command)}>
+						{menu.label}
+						{#if menu.desc?.length > 0}
+							<span>{menu.desc}</span>
+						{/if}
+					</div>
+				{/if}
+			{/each}
+		</div>
+	{/if}
+</main>
+
+<svelte:window on:click={onPageClick} />
+
+<style>
+	.menu {
+		padding: 4px 0;
+		position: fixed;
+		display: grid;
+		width: 230px;
+		border: 1px solid var(--vscode-menu-selectionBorder);
+		background-color: var(--vscode-menu-background);
+		z-index: 1000;
+		box-shadow: 0 10px 15px -3px var(--vscode-widget-shadow),
+			0 4px 6px -4px var(--vscode-widget-shadow);
+		border-radius: 4px;
+	}
+	.opt {
+		cursor: pointer;
+		padding: 6px 16px;
+		position: relative;
+	}
+	.opt > span {
+		float: right;
+	}
+	.divider {
+		margin: 4px 8px;
+		border-bottom: 1px solid var(--vscode-textSeparator-foreground);
+	}
+	.opt:hover {
+		background-color: var(--vscode-focusBorder);
+	}
+</style>

extensions/gitpod-web/portsview/src/components/HoverOptions.svelte
@@ -1,0 +1,85 @@
+<!-----------------------------------------------------------------------------------------------
+	Copyright (c) Gitpod. All rights reserved.
+------------------------------------------------------------------------------------------------>
+<script lang="ts">
+	import { createEventDispatcher } from "svelte";
+	import {
+		provideVSCodeDesignSystem,
+		vsCodeButton,
+	} from "@vscode/webview-ui-toolkit";
+	import type { HoverOption } from "../types/components";
+
+	provideVSCodeDesignSystem().register(vsCodeButton());
+
+	export let alwaysShow = false;
+	export let options: HoverOption[] = [
+		{ icon: "copy", command: "copy", title: "Copy Url" },
+	];
+
+	const dispatch = createEventDispatcher<{ command: string }>();
+	function clickOption(command: string) {
+		dispatch("command", command);
+	}
+</script>
+
+<main>
+	<div class="container">
+		<div class="slot">
+			<slot />
+		</div>
+		<span class="opts" class:hide={!alwaysShow}>
+			{#each options as opt}
+				{#if opt.icon != null}
+					<span
+						title={opt.title}
+						class="svg-container"
+						on:click={(e) => clickOption(opt.command)}
+					>
+						<vscode-button appearance="icon" aria-label={opt.title}>
+							<span class={"codicon codicon-" + opt.icon} />
+						</vscode-button>
+					</span>
+				{/if}
+			{/each}
+		</span>
+	</div>
+</main>
+
+<style>
+	.opts {
+		display: inline-flex;
+		flex: none;
+		padding-left: 4px;
+		box-sizing: border-box;
+		align-content: center;
+	}
+	.hide {
+		display: none;
+	}
+
+	.slot {
+		flex: 0 1 auto;
+		min-width: 0;
+		overflow: hidden;
+		text-overflow: ellipsis;
+		white-space: nowrap;
+		display: inline-flex;
+		align-content: center;
+	}
+
+	.container {
+		display: flex;
+		justify-content: space-between;
+	}
+
+	.hide:hover,
+	.container:hover > .hide {
+		display: inline-flex;
+	}
+	.svg-container {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		height: 16px;
+	}
+</style>

extensions/gitpod-web/portsview/src/porttable/PortInfo.svelte
@@ -1,0 +1,52 @@
+<!-----------------------------------------------------------------------------------------------
+	Copyright (c) Gitpod. All rights reserved.
+------------------------------------------------------------------------------------------------>
+<script lang="ts">
+	import type { GitpodPortObject } from "../types/gitpod";
+
+	export let port: GitpodPortObject;
+
+	$: title = port.status.name || port.status.localPort.toString();
+
+	$: showPortDetail =
+		port.status.remotePort != null &&
+		port.status.localPort !== port.status.remotePort;
+
+	function getPortDetail(port: GitpodPortObject) {
+		if (showPortDetail) {
+			return title === port.status.name
+				? ` (${port.status.localPort}:${port.status.remotePort})`
+				: ` :${port.status.remotePort}`;
+		} else {
+			return title === port.status.name ? `(${port.status.localPort})` : "";
+		}
+	}
+	$: portDetail = getPortDetail(port);
+</script>
+
+<main>
+	<div class="container" title={port.info.tooltip}>
+		<span class="title">{title}</span>
+		<span class="port-detail">{portDetail}</span>
+	</div>
+</main>
+
+<style>
+	.container {
+		display: flex;
+		width: 100%;
+	}
+	span {
+		display: inline-block;
+	}
+	.title {
+		flex: none;
+		min-width: 0;
+		overflow: hidden;
+		text-overflow: ellipsis;
+		white-space: nowrap;
+	}
+	.port-detail {
+		display: inline-block;
+	}
+</style>

extensions/gitpod-web/portsview/src/porttable/PortLocalAddress.svelte
@@ -1,0 +1,62 @@
+<!-----------------------------------------------------------------------------------------------
+	Copyright (c) Gitpod. All rights reserved.
+------------------------------------------------------------------------------------------------>
+<script lang="ts">
+	import { createEventDispatcher } from "svelte";
+	import HoverOptions from "../components/HoverOptions.svelte";
+	import type { HoverOption } from "../types/components";
+	import type { GitpodPortObject, PortCommand } from "../types/gitpod";
+	import { commandIconMap, getCommands, getNLSTitle } from "../utils/commands";
+
+	export let port: GitpodPortObject;
+
+	const copyOpt: HoverOption = {
+		icon: "copy",
+		title: "Copy Url",
+		command: "urlCopy",
+	};
+
+	function getHoverOption(port?: GitpodPortObject) {
+		if (port == null) {
+			return [];
+		}
+		const opts: HoverOption[] = getCommands(port).map((e) => ({
+			icon: commandIconMap[e],
+			title: getNLSTitle(e),
+			command: e,
+		}));
+		opts.unshift(copyOpt);
+		return opts;
+	}
+
+	$: hoverOpts = getHoverOption(port);
+	const dispatch = createEventDispatcher<{
+		command: { command: PortCommand; port: GitpodPortObject };
+	}>();
+	function onHoverCommand(command: string) {
+		dispatch("command", { command: command as PortCommand, port });
+	}
+</script>
+
+<HoverOptions
+	alwaysShow
+	options={hoverOpts}
+	on:command={(e) => {
+		onHoverCommand(e.detail);
+	}}
+>
+	<a href={port.status.exposed.url}>{port.status.exposed.url}</a>
+</HoverOptions>
+
+<style>
+	a {
+		color: var(--vscode-foreground);
+		overflow: hidden;
+		text-overflow: ellipsis;
+		white-space: nowrap;
+		display: inline-block;
+	}
+	a:focus {
+		outline: none;
+	}
+</style>

extensions/gitpod-web/portsview/src/porttable/PortStatus.svelte
@@ -1,0 +1,45 @@
+<!-----------------------------------------------------------------------------------------------
+	Copyright (c) Gitpod. All rights reserved.
+------------------------------------------------------------------------------------------------>
+<script lang="ts">
+	import "@vscode/codicons/dist/codicon.css";
+	import type { IconStatus } from "../types/gitpod";
+
+	export let status: IconStatus = "NotServed";
+
+	const fillArr: IconStatus[] = ["Detecting", "Served"];
+
+	$: circleFill = fillArr.includes(status);
+</script>
+
+<main>
+	<div class="container">
+		{#if status === "ExposureFailed"}
+			<i class="codicon codicon-warning ExposureFailed" />
+		{:else if circleFill}
+			<i class={"codicon codicon-circle-filled " + status} />
+		{:else}
+			<i class={"codicon codicon-circle-outline " + status} />
+		{/if}
+	</div>
+</main>
+
+<style>
+	.container {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+	.Served {
+		color: var(--vscode-ports-iconRunningProcessForeground);
+	}
+	.NotServed {
+		color: var(--vscode-foreground);
+	}
+	.Detecting {
+		color: var(--vscode-editorWarning-foreground);
+	}
+	.ExposureFailed {
+		color: var(--vscode-editorWarning-foreground);
+	}
+</style>

extensions/gitpod-web/portsview/src/utils/commands.ts
@@ -1,0 +1,61 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Gitpod. All rights reserved.
+ *--------------------------------------------------------------------------------------------*/
+
+import nlsFile from '../../public/package.nls.json';
+import type { GitpodPortObject, PortCommand } from '../types/gitpod';
+
+// TODO: use vscode-nls
+export function getNLSTitle(command: PortCommand) {
+	let name: string = command;
+	switch (name) {
+		case 'preview':
+			name = 'openPreview';
+	}
+	return nlsFile[name] ?? command as string;
+}
+
+export const commandIconMap: Record<PortCommand, string> = {
+	tunnelNetwork: 'eye',
+	tunnelHost: 'eye-closed',
+	makePublic: 'lock',
+	makePrivate: 'unlock',
+	preview: 'open-preview',
+	openBrowser: 'globe',
+	retryAutoExpose: 'refresh',
+	urlCopy: 'copy',
+	queryPortData: '',
+};
+
+export function getCommands(port: GitpodPortObject): PortCommand[] {
+	return getSplitCommands(port).filter(e => e != null);
+}
+
+export function getSplitCommands(port: GitpodPortObject) {
+	const opts: Array<null | PortCommand> = [];
+	const viewItem = port.info.contextValue;
+	if (viewItem.includes('host') && viewItem.includes('tunneled')) {
+		opts.push('tunnelNetwork');
+	}
+	if (viewItem.includes('network') && viewItem.includes('tunneled')) {
+		opts.push('tunnelHost');
+	}
+	// eslint-disable-next-line code-no-unused-expressions
+	opts.length > 0 && opts.push(null);
+	if (viewItem.includes('private')) {
+		opts.push('makePublic');
+	}
+	if (viewItem.includes('public')) {
+		opts.push('makePrivate');
+	}
+	if (viewItem.includes('exposed') || viewItem.includes('tunneled')) {
+		opts.push('preview');
+		opts.push('openBrowser');
+	}
+	if (viewItem.includes('failed')) {
+		// eslint-disable-next-line code-no-unused-expressions
+		opts.length > 0 && opts.push(null);
+		opts.push('retryAutoExpose');
+	}
+	return opts;
+}

microsoft/vscode

extensions/gitpod-web/portsview/src/App.svelte
@@ -1,0 +1,26 @@
+<!-----------------------------------------------------------------------------------------------
+	Copyright (c) Gitpod. All rights reserved.
+------------------------------------------------------------------------------------------------>
+<script lang="ts">
+	import "@vscode/codicons/dist/codicons.css";
+	import { vscode } from "./utils/vscode";
+	import PortTable from "./porttable/PortTable.svelte";
+	import type { GitpodPortObject } from "./protocol/gitpod";
+
+	let ports: GitpodPortObject[] = [];
+
+	window.addEventListener("message", (event) => {
+		if (event.data.command === "updatePorts") {
+			// TODO: sort with status first, then port number?
+			ports = event.data.ports.sort(
+				(a: GitpodPortObject, b: GitpodPortObject) =>
+					a.status.localPort - b.status.localPort
+			);
+		}
+	});
+	vscode.postMessage({ command: "queryPortData" });
+</script>
+
+<main>
+	<PortTable {ports} />
+</main>

extensions/gitpod-web/portsview/src/components/ContextMenu.svelte
@@ -1,0 +1,85 @@
+<!-----------------------------------------------------------------------------------------------
+	Copyright (c) Gitpod. All rights reserved.
+------------------------------------------------------------------------------------------------>
+<script lang="ts">
+	import { createEventDispatcher } from "svelte";
+	import type { MenuOption } from "../protocol/components";
+
+	export let x = 0;
+	export let y = 0;
+	export let show = false;
+
+	export let options: Array<MenuOption | null> = [];
+
+	let menuEl: HTMLDivElement;
+
+	$: ((x: number, y: number) => {
+		if (!show || !menuEl) return;
+		const rect = menuEl.getBoundingClientRect();
+		x = Math.min(window.innerWidth - rect.width, x);
+		if (y > window.innerHeight - rect.height) y -= rect.height;
+	})(x, y);
+
+	const dispatch = createEventDispatcher<{
+		command: string;
+		clickoutside: string;
+	}>();
+
+	function onPageClick(e: MouseEvent) {
+		if (!show) return;
+		// @ts-ignore
+		if (e.target === menuEl || menuEl.contains(e.target)) return;
+		dispatch("clickoutside");
+	}
+</script>
+
+<main>
+	{#if show}
+		<div class="menu" bind:this={menuEl} style="top: {y}px; left: {x}px;">
+			{#each options as menu}
+				{#if menu == null}
+					<div class="divider" />
+				{:else}
+					<div class="opt" on:click={() => dispatch("command", menu.command)}>
+						{menu.label}
+						{#if menu.desc?.length > 0}
+							<span>{menu.desc}</span>
+						{/if}
+					</div>
+				{/if}
+			{/each}
+		</div>
+	{/if}
+</main>
+
+<svelte:window on:click={onPageClick} />
+
+<style>
+	.menu {
+		padding: 4px 0;
+		position: fixed;
+		display: grid;
+		width: 230px;
+		border: 1px solid var(--vscode-menu-selectionBorder);
+		background-color: var(--vscode-menu-background);
+		z-index: 1000;
+		box-shadow: 0 10px 15px -3px var(--vscode-widget-shadow),
+			0 4px 6px -4px var(--vscode-widget-shadow);
+		border-radius: 4px;
+	}
+	.opt {
+		cursor: pointer;
+		padding: 6px 16px;
+		position: relative;
+	}
+	.opt > span {
+		float: right;
+	}
+	.divider {
+		margin: 4px 8px;
+		border-bottom: 1px solid var(--vscode-textSeparator-foreground);
+	}
+	.opt:hover {
+		background-color: var(--vscode-focusBorder);
+	}
+</style>

extensions/gitpod-web/portsview/src/components/HoverOptions.svelte
@@ -1,0 +1,85 @@
+<!-----------------------------------------------------------------------------------------------
+	Copyright (c) Gitpod. All rights reserved.
+------------------------------------------------------------------------------------------------>
+<script lang="ts">
+	import { createEventDispatcher } from "svelte";
+	import {
+		provideVSCodeDesignSystem,
+		vsCodeButton,
+	} from "@vscode/webview-ui-toolkit";
+	import type { HoverOption } from "../protocol/components";
+
+	provideVSCodeDesignSystem().register(vsCodeButton());
+
+	export let alwaysShow = false;
+	export let options: HoverOption[] = [
+		{ icon: "copy", command: "copy", title: "Copy Url" },
+	];
+
+	const dispatch = createEventDispatcher<{ command: string }>();
+	function clickOption(command: string) {
+		dispatch("command", command);
+	}
+</script>
+
+<main>
+	<div class="container">
+		<div class="slot">
+			<slot />
+		</div>
+		<span class="opts" class:hide={!alwaysShow}>
+			{#each options as opt}
+				{#if opt.icon != null}
+					<span
+						title={opt.title}
+						class="svg-container"
+						on:click={(e) => clickOption(opt.command)}
+					>
+						<vscode-button appearance="icon" aria-label={opt.title}>
+							<span class={"codicon codicon-" + opt.icon} />
+						</vscode-button>
+					</span>
+				{/if}
+			{/each}
+		</span>
+	</div>
+</main>
+
+<style>
+	.opts {
+		display: inline-flex;
+		flex: none;
+		padding-left: 4px;
+		box-sizing: border-box;
+		align-content: center;
+	}
+	.hide {
+		display: none;
+	}
+
+	.slot {
+		flex: 0 1 auto;
+		min-width: 0;
+		overflow: hidden;
+		text-overflow: ellipsis;
+		white-space: nowrap;
+		display: inline-flex;
+		align-content: center;
+	}
+
+	.container {
+		display: flex;
+		justify-content: space-between;
+	}
+
+	.hide:hover,
+	.container:hover > .hide {
+		display: inline-flex;
+	}
+	.svg-container {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		height: 16px;
+	}
+</style>

extensions/gitpod-web/portsview/src/porttable/PortInfo.svelte
@@ -1,0 +1,52 @@
+<!-----------------------------------------------------------------------------------------------
+	Copyright (c) Gitpod. All rights reserved.
+------------------------------------------------------------------------------------------------>
+<script lang="ts">
+	import type { GitpodPortObject } from "../protocol/gitpod";
+
+	export let port: GitpodPortObject;
+
+	$: title = port.status.name || port.status.localPort.toString();
+
+	$: showPortDetail =
+		port.status.remotePort != null &&
+		port.status.localPort !== port.status.remotePort;
+
+	function getPortDetail(port: GitpodPortObject) {
+		if (showPortDetail) {
+			return title === port.status.name
+				? ` (${port.status.localPort}:${port.status.remotePort})`
+				: ` :${port.status.remotePort}`;
+		} else {
+			return title === port.status.name ? `(${port.status.localPort})` : "";
+		}
+	}
+	$: portDetail = getPortDetail(port);
+</script>
+
+<main>
+	<div class="container" title={port.info.tooltip}>
+		<span class="title">{title}</span>
+		<span class="port-detail">{portDetail}</span>
+	</div>
+</main>
+
+<style>
+	.container {
+		display: flex;
+		width: 100%;
+	}
+	span {
+		display: inline-block;
+	}
+	.title {
+		flex: none;
+		min-width: 0;
+		overflow: hidden;
+		text-overflow: ellipsis;
+		white-space: nowrap;
+	}
+	.port-detail {
+		display: inline-block;
+	}
+</style>

extensions/gitpod-web/portsview/src/porttable/PortLocalAddress.svelte
@@ -1,0 +1,62 @@
+<!-----------------------------------------------------------------------------------------------
+	Copyright (c) Gitpod. All rights reserved.
+------------------------------------------------------------------------------------------------>
+<script lang="ts">
+	import { createEventDispatcher } from "svelte";
+	import HoverOptions from "../components/HoverOptions.svelte";
+	import type { HoverOption } from "../protocol/components";
+	import type { GitpodPortObject, PortCommand } from "../protocol/gitpod";
+	import { commandIconMap, getCommands, getNLSTitle } from "../utils/commands";
+
+	export let port: GitpodPortObject;
+
+	const copyOpt: HoverOption = {
+		icon: "copy",
+		title: "Copy Url",
+		command: "urlCopy",
+	};
+
+	function getHoverOption(port?: GitpodPortObject) {
+		if (port == null) {
+			return [];
+		}
+		const opts: HoverOption[] = getCommands(port).map((e) => ({
+			icon: commandIconMap[e],
+			title: getNLSTitle(e),
+			command: e,
+		}));
+		opts.unshift(copyOpt);
+		return opts;
+	}
+
+	$: hoverOpts = getHoverOption(port);
+	const dispatch = createEventDispatcher<{
+		command: { command: PortCommand; port: GitpodPortObject };
+	}>();
+	function onHoverCommand(command: string) {
+		dispatch("command", { command: command as PortCommand, port });
+	}
+</script>
+
+<HoverOptions
+	alwaysShow
+	options={hoverOpts}
+	on:command={(e) => {
+		onHoverCommand(e.detail);
+	}}
+>
+	<a href={port.status.exposed.url}>{port.status.exposed.url}</a>
+</HoverOptions>
+
+<style>
+	a {
+		color: var(--vscode-foreground);
+		overflow: hidden;
+		text-overflow: ellipsis;
+		white-space: nowrap;
+		display: inline-block;
+	}
+	a:focus {
+		outline: none;
+	}
+</style>

extensions/gitpod-web/portsview/src/porttable/PortStatus.svelte
@@ -1,0 +1,45 @@
+<!-----------------------------------------------------------------------------------------------
+	Copyright (c) Gitpod. All rights reserved.
+------------------------------------------------------------------------------------------------>
+<script lang="ts">
+	import "@vscode/codicons/dist/codicon.css";
+	import type { IconStatus } from "../protocol/gitpod";
+
+	export let status: IconStatus = "NotServed";
+
+	const fillArr: IconStatus[] = ["Detecting", "Served"];
+
+	$: circleFill = fillArr.includes(status);
+</script>
+
+<main>
+	<div class="container">
+		{#if status === "ExposureFailed"}
+			<i class="codicon codicon-warning ExposureFailed" />
+		{:else if circleFill}
+			<i class={"codicon codicon-circle-filled " + status} />
+		{:else}
+			<i class={"codicon codicon-circle-outline " + status} />
+		{/if}
+	</div>
+</main>
+
+<style>
+	.container {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+	.Served {
+		color: var(--vscode-ports-iconRunningProcessForeground);
+	}
+	.NotServed {
+		color: var(--vscode-foreground);
+	}
+	.Detecting {
+		color: var(--vscode-editorWarning-foreground);
+	}
+	.ExposureFailed {
+		color: var(--vscode-editorWarning-foreground);
+	}
+</style>

extensions/gitpod-web/portsview/src/utils/commands.ts
@@ -1,0 +1,61 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Gitpod. All rights reserved.
+ *--------------------------------------------------------------------------------------------*/
+
+import nlsFile from '../../public/package.nls.json';
+import type { GitpodPortObject, PortCommand } from '../protocol/gitpod';
+
+// TODO: use vscode-nls
+export function getNLSTitle(command: PortCommand) {
+	let name: string = command;
+	switch (name) {
+		case 'preview':
+			name = 'openPreview';
+	}
+	return nlsFile[name] ?? command as string;
+}
+
+export const commandIconMap: Record<PortCommand, string> = {
+	tunnelNetwork: 'eye',
+	tunnelHost: 'eye-closed',
+	makePublic: 'lock',
+	makePrivate: 'unlock',
+	preview: 'open-preview',
+	openBrowser: 'globe',
+	retryAutoExpose: 'refresh',
+	urlCopy: 'copy',
+	queryPortData: '',
+};
+
+export function getCommands(port: GitpodPortObject): PortCommand[] {
+	return getSplitCommands(port).filter(e => e != null);
+}
+
+export function getSplitCommands(port: GitpodPortObject) {
+	const opts: Array<null | PortCommand> = [];
+	const viewItem = port.info.contextValue;
+	if (viewItem.includes('host') && viewItem.includes('tunneled')) {
+		opts.push('tunnelNetwork');
+	}
+	if (viewItem.includes('network') && viewItem.includes('tunneled')) {
+		opts.push('tunnelHost');
+	}
+	// eslint-disable-next-line code-no-unused-expressions
+	opts.length > 0 && opts.push(null);
+	if (viewItem.includes('private')) {
+		opts.push('makePublic');
+	}
+	if (viewItem.includes('public')) {
+		opts.push('makePrivate');
+	}
+	if (viewItem.includes('exposed') || viewItem.includes('tunneled')) {
+		opts.push('preview');
+		opts.push('openBrowser');
+	}
+	if (viewItem.includes('failed')) {
+		// eslint-disable-next-line code-no-unused-expressions
+		opts.length > 0 && opts.push(null);
+		opts.push('retryAutoExpose');
+	}
+	return opts;
+}