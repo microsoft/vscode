9193befba441e75aa7d06a9fec4d52b0dec6389b
meganrogge <merogge@microsoft.com>
2022-07-05 09:56:38 -0400

    fix #149095

--- src/vs/workbench/contrib/tasks/browser/taskQuickPick.ts
+++ src/vs/workbench/contrib/tasks/browser/taskQuickPick.ts
@@ -24,7 +24,7 @@ import { TaskQuickPickEntryType } from 'vs/workbench/contrib/tasks/browser/abstr
 
 export const QUICKOPEN_DETAIL_CONFIG = 'task.quickOpen.detail';
 export const QUICKOPEN_SKIP_CONFIG = 'task.quickOpen.skip';
-
+const HIDDEN_TASK_PREFIX = '__';
 export function isWorkspaceFolder(folder: IWorkspace | IWorkspaceFolder): folder is IWorkspaceFolder {
 	return 'uri' in folder;
 }
@@ -108,7 +108,9 @@ export class TaskQuickPick extends Disposable {
 		groupLabel: string, extraButtons: IQuickInputButton[] = []) {
 		entries.push({ type: 'separator', label: groupLabel });
 		tasks.forEach(task => {
-			entries.push(this._createTaskEntry(task, extraButtons));
+			if (!task._label.startsWith(HIDDEN_TASK_PREFIX)) {
+				entries.push(this._createTaskEntry(task, extraButtons));
+			}
 		});
 	}
 
@@ -304,7 +306,7 @@ export class TaskQuickPick extends Disposable {
 	private async _doPickerSecondLevel(picker: IQuickPick<ITaskTwoLevelQuickPickEntry>, type: string) {
 		picker.busy = true;
 		if (type === SHOW_ALL) {
-			const items = (await this._taskService.tasks()).sort((a, b) => this._sorter.compare(a, b)).map(task => this._createTaskEntry(task));
+			const items = (await this._taskService.tasks()).filter(t => !t._label.startsWith(HIDDEN_TASK_PREFIX)).sort((a, b) => this._sorter.compare(a, b)).map(task => this._createTaskEntry(task));
 			items.push(...TaskQuickPick.allSettingEntries(this._configurationService));
 			picker.items = items;
 		} else {
@@ -355,7 +357,7 @@ export class TaskQuickPick extends Disposable {
 		const tasks = (await this._taskService.tasks({ type })).sort((a, b) => this._sorter.compare(a, b));
 		let taskQuickPickEntries: QuickPickInput<ITaskTwoLevelQuickPickEntry>[];
 		if (tasks.length > 0) {
-			taskQuickPickEntries = tasks.map(task => this._createTaskEntry(task));
+			taskQuickPickEntries = tasks.filter(t => !t._label.startsWith(HIDDEN_TASK_PREFIX)).map(task => this._createTaskEntry(task));
 			taskQuickPickEntries.push({
 				type: 'separator'
 			}, {
