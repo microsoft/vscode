2ee1dfbd856143f5bdd2fd3d4e821684773bdb54
Benjamin Pasero <benjamin.pasero@microsoft.com>
2022-07-05 07:06:13 +0200

    fix some js errors

--- src/vs/code/electron-sandbox/workbench/workbench.js
+++ src/vs/code/electron-sandbox/workbench/workbench.js
@@ -61,7 +61,7 @@
 				window.requestIdleCallback(() => {
 					const canvas = document.createElement('canvas');
 					const context = canvas.getContext('2d');
-					context.clearRect(0, 0, canvas.width, canvas.height);
+					context?.clearRect(0, 0, canvas.width, canvas.height);
 					canvas.remove();
 				}, { timeout: 50 });
 			}
