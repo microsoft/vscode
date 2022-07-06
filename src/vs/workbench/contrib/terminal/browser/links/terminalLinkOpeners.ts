2e0e882ac5997526583fd4e9e204ee77bdd534d8
Daniel Imms <2193314+Tyriar@users.noreply.github.com>
2022-07-06 07:37:31 -0700

    Improve comment

--- src/vs/workbench/contrib/terminal/browser/links/terminalLinkOpeners.ts
+++ src/vs/workbench/contrib/terminal/browser/links/terminalLinkOpeners.ts
@@ -218,12 +218,13 @@ export class TerminalSearchLinkOpener implements ITerminalLinkOpener {
 
 	private async _tryOpenExactLink(text: string, link: ITerminalSimpleLink): Promise<boolean> {
 		const sanitizedLink = text.replace(/:\d+(:\d+)?$/, '');
-		// For only file links disallow exact link matching, for example searching for `foo.txt`
-		// when no cwd information is available should search when only the initial cwd is available
-		// as it's ambiguous if there are multiple matches.
+		// For links made up of only a file name (no folder), disallow exact link matching. For
+		// example searching for `foo.txt` when there is no cwd information available (ie. only the
+		// initial cwd) should NOT search  as it's ambiguous if there are multiple matches.
 		//
-		// However, for `src/foo.txt`, if there's an exact match for `src/foo.txt` in any folder we
-		// want to take it, even if there are partial matches like `src2/foo.txt` available.
+		// However, for a link like `src/foo.txt`, if there's an exact match for `src/foo.txt` in
+		// any folder we want to take it, even if there are partial matches like `src2/foo.txt`
+		// available.
 		if (!sanitizedLink.match(/[\\/]/)) {
 			return false;
 		}
