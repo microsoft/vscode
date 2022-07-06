9c5408c04a614e641d7a69680686cb9346e0d7a8
Stephen Sigwart <ssigwart@gmail.com>
2022-07-05 20:39:22 -0400

    Detect terminal links with space, then line:col (#153957)

--- src/vs/workbench/contrib/terminal/browser/links/terminalLocalLinkDetector.ts
+++ src/vs/workbench/contrib/terminal/browser/links/terminalLocalLinkDetector.ts
@@ -49,6 +49,7 @@ export const winLocalLinkClause = '((' + winPathPrefix + '|(' + winExcludedPathC
 /** As xterm reads from DOM, space in that case is nonbreaking char ASCII code - 160,
 replacing space with nonBreakningSpace or space ASCII code - 32. */
 export const lineAndColumnClause = [
+	'(([^:\\s\\(\\)<>\'\"\\[\\]]*) ((\\d+))(:(\\d+)))', // (file path) 336:9 [see #140780]
 	'((\\S*)[\'"], line ((\\d+)( column (\\d+))?))', // "(file path)", line 45 [see #40468]
 	'((\\S*)[\'"],((\\d+)(:(\\d+))?))', // "(file path)",45 [see #78205]
 	'((\\S*) on line ((\\d+)(, column (\\d+))?))', // (file path) on line 8, column 13
