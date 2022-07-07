00eb9c4356c520299e7e2c95eb2089698b30b7fd
Tyler James Leonhardt <me@tylerleonhardt.com>
2022-07-07 13:38:32 -0700

    Polish up restart dialogs and switch to using Language instead of language (#154382)

--- src/vs/workbench/contrib/localization/browser/localeService.ts
+++ src/vs/workbench/contrib/localization/browser/localeService.ts
@@ -3,31 +3,62 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { language } from 'vs/base/common/platform';
+import { localize } from 'vs/nls';
+import { Language } from 'vs/base/common/platform';
+import { IDialogService } from 'vs/platform/dialogs/common/dialogs';
 import { ILanguagePackItem } from 'vs/platform/languagePacks/common/languagePacks';
 import { ILocaleService } from 'vs/workbench/contrib/localization/common/locale';
+import { IHostService } from 'vs/workbench/services/host/browser/host';
+import { IProductService } from 'vs/platform/product/common/productService';
 
 export class WebLocaleService implements ILocaleService {
 	declare readonly _serviceBrand: undefined;
 
-	async setLocale(languagePackItem: ILanguagePackItem): Promise<boolean> {
+	constructor(
+		@IDialogService private readonly dialogService: IDialogService,
+		@IHostService private readonly hostService: IHostService,
+		@IProductService private readonly productService: IProductService
+	) { }
+
+	async setLocale(languagePackItem: ILanguagePackItem): Promise<void> {
 		const locale = languagePackItem.id;
-		if (locale === language || (!locale && language === navigator.language)) {
-			return false;
+		if (locale === Language.value() || (!locale && Language.value() === navigator.language)) {
+			return;
 		}
 		if (locale) {
 			window.localStorage.setItem('vscode.nls.locale', locale);
 		} else {
 			window.localStorage.removeItem('vscode.nls.locale');
 		}
-		return true;
+
+		const restartDialog = await this.dialogService.confirm({
+			type: 'info',
+			message: localize('relaunchDisplayLanguageMessage', "{0} needs to reload to change the display language", this.productService.nameLong),
+			detail: localize('relaunchDisplayLanguageDetail', "Press the reload button to refresh the page and set the display language to {0}.", languagePackItem.label),
+			primaryButton: localize({ key: 'reload', comment: ['&& denotes a mnemonic character'] }, "&&Reload"),
+		});
+
+		if (restartDialog.confirmed) {
+			this.hostService.restart();
+		}
 	}
 
-	async clearLocalePreference(): Promise<boolean> {
-		if (language === navigator.language) {
-			return false;
-		}
+	async clearLocalePreference(): Promise<void> {
 		window.localStorage.removeItem('vscode.nls.locale');
-		return true;
+
+		if (Language.value() === navigator.language) {
+			return;
+		}
+
+		const restartDialog = await this.dialogService.confirm({
+			type: 'info',
+			message: localize('clearDisplayLanguageMessage', "{0} needs to reload to change the display language", this.productService.nameLong),
+			detail: localize('clearDisplayLanguageDetail', "Press the reload button to refresh the page and use your browser's language."),
+			primaryButton: localize({ key: 'reload', comment: ['&& denotes a mnemonic character'] }, "&&Reload"),
+		});
+
+		if (restartDialog.confirmed) {
+			this.hostService.restart();
+		}
 	}
 }
