517600ee1f99f27abd202e62d69ecc600db20c68
Benjamin Pasero <benjamin.pasero@microsoft.com>
2022-07-13 13:11:45 +0200

    set `cancelId`

--- src/vs/workbench/contrib/bulkEdit/browser/preview/bulkEdit.contribution.ts
+++ src/vs/workbench/contrib/bulkEdit/browser/preview/bulkEdit.contribution.ts
@@ -123,10 +123,13 @@ class BulkEditPreviewContribution {
 				Severity.Info,
 				localize('overlap', "Another refactoring is being previewed."),
 				[localize('continue', "Continue"), localize('cancel', "Cancel")],
-				{ detail: localize('detail', "Press 'Continue' to discard the previous refactoring and continue with the current refactoring.") }
+				{
+					detail: localize('detail', "Press 'Continue' to discard the previous refactoring and continue with the current refactoring."),
+					cancelId: 1
+				}
 			);
 
-			if (choice.choice === 0) {
+			if (choice.choice === 1) {
 				// this refactoring is being cancelled
 				return [];
 			}
