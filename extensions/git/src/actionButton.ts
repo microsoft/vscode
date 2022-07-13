15daddd7af3d86714ff93195d964e0a70f594a2f
Ladislau Szomoru <3372902+lszomoru@users.noreply.github.com>
2022-07-13 16:39:18 +0200

    git - Add localization comment for Publish Branch action button (#155053)
    * Add localization comment for Publish Branch action button
    
    * Pull request feedback

--- extensions/git/src/actionButton.ts
+++ extensions/git/src/actionButton.ts
@@ -189,10 +189,10 @@ export class ActionButtonCommand {
 		return {
 			command: {
 				command: 'git.publish',
-				title: localize('scm publish branch action button title', "{0} Publish Branch", '$(cloud-upload)'),
+				title: localize({ key: 'scm publish branch action button title', comment: ['{Locked="Branch"}', 'Do not translate "Branch" as it is a git term'] }, "{0} Publish Branch", '$(cloud-upload)'),
 				tooltip: this.state.isSyncInProgress ?
-					localize('scm button publish branch running', "Publishing Branch...") :
-					localize('scm button publish branch', "Publish Branch"),
+					localize({ key: 'scm button publish branch running', comment: ['{Locked="Branch"}', 'Do not translate "Branch" as it is a git term'] }, "Publishing Branch...") :
+					localize({ key: 'scm button publish branch', comment: ['{Locked="Branch"}', 'Do not translate "Branch" as it is a git term'] }, "Publish Branch"),
 				arguments: [this.repository.sourceControl],
 			},
 			enabled: !this.state.isSyncInProgress
