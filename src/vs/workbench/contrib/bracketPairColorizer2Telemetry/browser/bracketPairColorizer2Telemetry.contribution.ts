d29ab10eb6fcf1247d9d3e3123eb5e575ea2a6e1
Henning Dieterichs <hdieterichs@microsoft.com>
2022-07-18 16:55:11 +0200

    Implements telemetry for bracket colorizer 2 extension usage

---
+++ src/vs/workbench/contrib/bracketPairColorizer2Telemetry/browser/bracketPairColorizer2Telemetry.contribution.ts
@@ -0,0 +1,54 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { onUnexpectedError } from 'vs/base/common/errors';
+import { IConfigurationService } from 'vs/platform/configuration/common/configuration';
+import { Registry } from 'vs/platform/registry/common/platform';
+import { ITelemetryService } from 'vs/platform/telemetry/common/telemetry';
+import { Extensions as WorkbenchExtensions, IWorkbenchContributionsRegistry } from 'vs/workbench/common/contributions';
+import { IExtensionsWorkbenchService } from 'vs/workbench/contrib/extensions/common/extensions';
+import { EnablementState } from 'vs/workbench/services/extensionManagement/common/extensionManagement';
+import { LifecyclePhase } from 'vs/workbench/services/lifecycle/common/lifecycle';
+
+class BracketPairColorizer2TelemetryContribution {
+	constructor(
+		@IConfigurationService private readonly configurationService: IConfigurationService,
+		@IExtensionsWorkbenchService private readonly extensionsWorkbenchService: IExtensionsWorkbenchService,
+		@ITelemetryService private readonly telemetryService: ITelemetryService
+	) {
+		this.init().catch(onUnexpectedError);
+	}
+
+	private async init(): Promise<void> {
+		const bracketPairColorizerId = 'coenraads.bracket-pair-colorizer-2';
+
+		await this.extensionsWorkbenchService.queryLocal();
+		const extension = this.extensionsWorkbenchService.installed.find(e => e.identifier.id === bracketPairColorizerId);
+		if (
+			!extension ||
+			((extension.enablementState !== EnablementState.EnabledGlobally) &&
+				(extension.enablementState !== EnablementState.EnabledWorkspace))
+		) {
+			return;
+		}
+
+		const nativeBracketPairColorizationEnabledKey = 'editor.bracketPairColorization.enabled';
+		const nativeColorizationEnabled = !!this.configurationService.getValue(nativeBracketPairColorizationEnabledKey);
+
+		type BracketPairColorizer2InstalledClassification = {
+			owner: 'hediet';
+			nativeColorizationEnabled: { classification: 'SystemMetaData'; purpose: 'FeatureInsight' };
+		};
+		type BracketPairColorizer2Event = {
+			nativeColorizationEnabled: boolean;
+		};
+		this.telemetryService.publicLog2<BracketPairColorizer2Event, BracketPairColorizer2InstalledClassification>('bracketPairColorizer2Usage', {
+			nativeColorizationEnabled
+		});
+	}
+}
+
+Registry.as<IWorkbenchContributionsRegistry>(WorkbenchExtensions.Workbench).registerWorkbenchContribution(BracketPairColorizer2TelemetryContribution, LifecyclePhase.Restored);
+
