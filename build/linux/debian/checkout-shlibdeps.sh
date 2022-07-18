da8ebe81df6589f6eecbf6a992092e6d2741f2e3
Raymond Zhao <7199958+rzhao271@users.noreply.github.com>
2022-07-18 07:33:11 -0700

    Use git ppa

--- build/linux/debian/checkout-shlibdeps.sh
+++ build/linux/debian/checkout-shlibdeps.sh
@@ -1,13 +1,7 @@
 # Get a newer version of git
-sudo apt-get install -y dh-autoreconf libcurl4-gnutls-dev libexpat1-dev \
-gettext libz-dev libssl-dev
-curl -sL https://github.com/git/git/archive/refs/tags/v2.37.1.tar.gz -o git-source.tar.gz
-tar xzf git-source.tar.gz
-cd git-2.37.1
-make configure
-./configure --prefix=/usr
-make all
-sudo make install
+sudo add-apt-repository ppa:git-core/ppa
+sudo apt update
+sudo apt install git
 
 # Do a sparse checkout. Ref https://stackoverflow.com/a/63786181
 git clone --filter=blob:none --no-checkout --depth 1 --sparse https://github.com/chromium/chromium.git
