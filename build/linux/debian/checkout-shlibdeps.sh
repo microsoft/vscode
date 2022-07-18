c7a2420673b07c190be4727acf28441bb0d0a035
Raymond Zhao <7199958+rzhao271@users.noreply.github.com>
2022-07-18 09:36:42 -0700

    Stablize script

--- build/linux/debian/checkout-shlibdeps.sh
+++ build/linux/debian/checkout-shlibdeps.sh
@@ -2,12 +2,13 @@
 
 # Get add-apt-repository command
 sudo apt-get update
-sudo apt-get install software-properties-common
+sudo apt-get install -y software-properties-common
+sudo apt-get update
 
 # Get a newer version of git
 sudo add-apt-repository ppa:git-core/ppa
 sudo apt-get update
-sudo apt-get install git
+sudo apt-get install -y git
 
 # Do a sparse checkout. Ref https://stackoverflow.com/a/63786181
 git clone --filter=blob:none --no-checkout --depth 1 --sparse https://github.com/chromium/chromium.git
