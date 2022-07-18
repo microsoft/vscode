7d75b25561060567110973cfd4c82cffba234879
Raymond Zhao <7199958+rzhao271@users.noreply.github.com>
2022-07-18 14:07:42 -0700

    Try using both approaches

--- build/linux/debian/checkout-shlibdeps.sh
+++ build/linux/debian/checkout-shlibdeps.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
 
-# Get add-apt-repository command
-sudo apt-get update
-sudo apt-get install -y software-properties-common dirmngr
-
-# Get a newer version of git
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E1DD270288B4E6030699E45FA1715D88E1DF1F24 > /dev/null
-sudo apt-get update
-sudo add-apt-repository ppa:git-core/ppa
-sudo apt-get update
-sudo apt-get install -y git
-
-# Do a sparse checkout. Ref https://stackoverflow.com/a/63786181
-git clone --filter=blob:none --no-checkout --depth 1 --sparse https://github.com/chromium/chromium.git
-cd chromium
-git sparse-checkout init --cone
-git sparse-checkout add third_party/dpkg-shlibdeps
-git checkout
+# Try cloning the repository in a newer way
+git clone --filter=blob:none --no-checkout --depth 1 --sparse https://github.com/chromium/chromium.git 2> /dev/null
+if [ $? = 0 ]; then
+	# Do a sparse checkout using newer commands. Ref https://stackoverflow.com/a/63786181
+	cd chromium
+	git sparse-checkout init --cone
+	git sparse-checkout add third_party/dpkg-shlibdeps
+	git checkout
+else
+	# Do a sparse checkout the old way.
+	# Ref https://stackoverflow.com/a/13738951
+	mkdir chromium
+	cd chromium
+	git init
+	git remote add -f origin https://github.com/chromium/chromium.git
+	git config core.sparseCheckout true
+	echo "third_party/dpkg-shlibdeps" >> .git/info/sparse-checkout
+	git pull origin main
+fi
