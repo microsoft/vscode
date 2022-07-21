78da5e3df40a794b3b546dc34404edcfcf543a26
Eric Amodio <eamodio@gmail.com>
2022-07-21 15:13:48 -0400

    Closes #54285 adds webview/context contribution (#154524)
    * Closes #54285 adds webview/context contribution
    
    * Adds additional context support for context menus
    
    Adds `webViewItem` read from nearest `data-vscode-context-menu-item`
    Adds `webViewItemElement` read from `data-vscode-context-menu-item-element`
    
    * Adds `preventDefaultContextMenuItems` option
    
    This allows webviews to opt-out of the default (cut/copy/paste) context menu items
    
    * Adds optional `itemValue` context value for handlers
    
    * Adds missing proposed check
    
    * Adds webview command to generate csp hash
    
    * Switches to use single JSON data attr for context
    Aggregates all ancestor context
    
    * Revert "Adds `preventDefaultContextMenuItems` option"
    
    This reverts commit 71f0180504c1e2ce399bb0af908418bc32965838.
    
    Also renames proposal from webviewContextMenus to contribWebviewContext to align with other empty contribution point proposals

--- src/vs/workbench/services/actions/common/menusExtensionPoint.ts
+++ src/vs/workbench/services/actions/common/menusExtensionPoint.ts
@@ -272,7 +272,14 @@ const apiMenus: IAPIMenu[] = [
 		id: MenuId.MergeToolbar,
 		description: localize('merge.toolbar', "The prominent botton in the merge editor"),
 		proposed: 'contribMergeEditorToolbar'
-	}
+	},
+	{
+		key: 'webview/context',
+		id: MenuId.WebviewContext,
+		description: localize('webview.context', "The webview context menu"),
+		proposed: 'contribWebviewContext'
+	},
+
 ];
 
 namespace schema {
