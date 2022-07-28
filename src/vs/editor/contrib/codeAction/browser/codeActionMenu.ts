30ee9f0dde549104bf86ba25f138587ad149d770
Justin Chen <t-justinchen@microsoft.com>
2022-07-28 11:11:34 -0700

    modifed var types

--- src/vs/editor/contrib/codeAction/browser/codeActionMenu.ts
+++ src/vs/editor/contrib/codeAction/browser/codeActionMenu.ts
@@ -161,7 +161,7 @@ export class CodeActionMenu extends Disposable implements IEditorContribution {
 	private _ctxMenuWidgetVisible: IContextKey<boolean>;
 	private viewItems: ICodeActionMenuItem[] = [];
 	private focusedEnabledItem: number | undefined;
-	private currSelectedItem: number | null = 0;
+	private currSelectedItem: number | undefined;
 	private hasSeperator: boolean = false;
 	private block?: HTMLElement;
 
@@ -221,7 +221,7 @@ export class CodeActionMenu extends Disposable implements IEditorContribution {
 
 	private _onListHover(e: IListMouseEvent<ICodeActionMenuItem>): void {
 		if (!e.element) {
-			this.currSelectedItem = null;
+			this.currSelectedItem = undefined;
 			this.codeActionList.value?.setFocus([]);
 		} else {
 			if (e.element?.isEnabled) {
@@ -229,7 +229,7 @@ export class CodeActionMenu extends Disposable implements IEditorContribution {
 				this.focusedEnabledItem = this.viewItems.indexOf(e.element);
 				this.currSelectedItem = e.element.index;
 			} else {
-				this.currSelectedItem = null;
+				this.currSelectedItem = undefined;
 				this.codeActionList.value?.setFocus([e.element.index]);
 			}
 		}
@@ -321,7 +321,7 @@ export class CodeActionMenu extends Disposable implements IEditorContribution {
 
 		// List selection
 		if (this.viewItems.length < 1 || this.viewItems.every(item => item.isDocumentation)) {
-			this.currSelectedItem = null;
+			this.currSelectedItem = undefined;
 		} else {
 			this.focusedEnabledItem = 0;
 			this.currSelectedItem = this.viewItems[0].index;
@@ -394,8 +394,7 @@ export class CodeActionMenu extends Disposable implements IEditorContribution {
 	}
 
 	public onEnterSet() {
-		// 0 case when first enabled item is the 0th element in list.
-		if (this.currSelectedItem === 0 || this.currSelectedItem) {
+		if (typeof this.currSelectedItem === 'number') {
 			this.codeActionList.value?.setSelection([this.currSelectedItem]);
 		}
 	}
@@ -409,7 +408,7 @@ export class CodeActionMenu extends Disposable implements IEditorContribution {
 		this.options = [];
 		this.viewItems = [];
 		this.focusedEnabledItem = 0;
-		this.currSelectedItem = null;
+		this.currSelectedItem = undefined;
 		this.hasSeperator = false;
 		this._contextViewService.hideContextView({ source: this });
 	}
