9fecfbe505eae82df8d6bea238a9e3d0cf5d282a
Michael Lively <t-mlively@microsoft.com>
2022-08-01 16:12:54 -0700

    paste screenshot works fully

--- extensions/ipynb/src/notebookImagePaste.ts
+++ extensions/ipynb/src/notebookImagePaste.ts
@@ -14,55 +14,64 @@ class CopyPasteEditProvider implements vscode.DocumentPasteEditProvider {
 		_token: vscode.CancellationToken
 	): Promise<vscode.DocumentPasteEdit | undefined> {
 
+		// get foundational notebook data
 		const activeNotebook = vscode.window.activeNotebookEditor?.notebook;
-		const currentSelection = vscode.window.activeNotebookEditor?.selection;
-		const currentCell = activeNotebook?.getCells(currentSelection);
-		if (currentCell?.length !== 1) { // somehow user selected multiple cells? not sure how this works. later problem
-			return undefined; // FIXME: this may not be the correct behavior, tbd
+		const activeSelection = vscode.window.activeNotebookEditor?.selection;
+		const currentCell = activeNotebook?.getCells(activeSelection);
+		if (!currentCell || currentCell?.length !== 1) {
+			return undefined;
 		}
 		const initialAttachments = currentCell[0].metadata?.custom?.attachments;
+		const initialize = !initialAttachments ? true : false;
 
-		// get data from paste
+		// get b64 data from paste
 		const dataItem = dataTransfer.get('image/png');
 		if (!dataItem) {
 			return undefined;
 		}
-		let pasteFilename = dataItem.asFile()?.name;
 		const fileDataAsUint8 = await dataItem.asFile()?.data();
 		if (!fileDataAsUint8) {
 			return undefined;
 		}
 		const b64string = encodeBase64(fileDataAsUint8);
 
-		// create updated metadata
-		let attachmentFilename: keyof typeof initialAttachments;
-		for (attachmentFilename in initialAttachments) {
-			if (attachmentFilename === pasteFilename) { // screenshot example, all come in as 'image.png'
-				const objEntries = Object.entries(initialAttachments[attachmentFilename]);
-				if (objEntries.length) { // check that mime:b64 are present
-					const [, attachmentb64] = objEntries[0];
-					if (attachmentb64 !== b64string) {
-						// append a "-#" here. same name, diff data. this matches jupyter behavior
-						const split = pasteFilename.split('.');
-						pasteFilename = split[0].concat('-2.') + split[1]; //TODO: add numbering that actually updates...
+		// get filename data from paste
+		let pasteFilename = dataItem.asFile()?.name;
+		const filePrefix = pasteFilename?.split('.')[0];
+		const fileSuffix = pasteFilename?.split('.')[1];
+		if (!filePrefix || !fileSuffix) {
+			return undefined;
+		}
+
+		// create updated metadata for cell (prep for WorkspaceEdit)
+		if (!initialize) {
+			let appendValue = 2;
+			let attachmentFilename: keyof typeof initialAttachments;
+			const sortedFilenames = sortFilenames(initialAttachments);
+			for (attachmentFilename of sortedFilenames) {
+				if (attachmentFilename === pasteFilename) {
+					const objEntries = Object.entries(initialAttachments[pasteFilename]);
+					if (objEntries.length) { // check that mime:b64 are present
+						const [, attachmentb64] = objEntries[0];
+						if (attachmentb64 !== b64string) {	// append a "-#" here. same name, diff data. this matches jupyter behavior
+							pasteFilename = filePrefix.concat(`-${appendValue++}.`) + fileSuffix;
+						}
 					}
 				}
 			}
 		}
-
 		if (!pasteFilename) {
 			return undefined;
 		}
-		const updatedMetadata = {
-			'custom': {
-				'attachments': {
-					[pasteFilename]: { 'image/png': b64string }
-				}
+		const updatedMetadata = { 'custom': { 'attachments': { [pasteFilename]: { 'image/png': b64string } } } };
+		if (!initialize) {
+			let attachmentFilename: keyof typeof initialAttachments;
+			for (attachmentFilename in initialAttachments) {
+				updatedMetadata.custom.attachments[attachmentFilename] = initialAttachments[attachmentFilename];
 			}
-		};
-		for (attachmentFilename in initialAttachments) {
-			updatedMetadata.custom.attachments[attachmentFilename] = initialAttachments[attachmentFilename];
 		}
+
+		// create WorkspaceEdit and apply to active notebook
 		let newCell;
 		if (updatedMetadata) {
 			newCell = vscode.NotebookEdit.updateCellMetadata(currentCell[0].index, updatedMetadata);
@@ -73,7 +82,7 @@ class CopyPasteEditProvider implements vscode.DocumentPasteEditProvider {
 		}
 		vscode.workspace.applyEdit(edit);
 
-		// Build a snippet to insert
+		// Build a snippet to paste
 		const snippet = new vscode.SnippetString();
 		snippet.appendText(`![${pasteFilename}](attachment:${pasteFilename})`);
 		return { insertText: snippet };
@@ -124,4 +133,13 @@ function encodeBase64(buffer: Uint8Array, padded = true, urlSafe = false) {
 	}
 
 	return output;
+}
+
+function sortFilenames(initialAttachments: any) {
+	return Object.keys(initialAttachments).sort(function (a, b) {
+		function getRaw(s: string) {
+			return s.replace('-', '');
+		}
+		return getRaw(a).localeCompare(getRaw(b));
+	});
 }
