817d4180c1b79ec2c29fa3d3192d5c626059820b
Daniel Fiori <zeroimpl@gmail.com>
2022-08-01 14:02:34 -0400

    Fix typo in documentation for tabSize
    Co-authored-by: Magnus Ihse Bursie <mag@icus.se>

--- src/vscode-dts/vscode.d.ts
+++ src/vscode-dts/vscode.d.ts
@@ -642,7 +642,7 @@ declare module 'vscode' {
 		 * The size in spaces a tab takes. This is used for two purposes:
 		 *  - the rendering width of a tab character;
 		 *  - the number of spaces to insert when {@link TextEditorOptions.insertSpaces insertSpaces} is true
-		 *    and `indentSize` is set to `"tab"`.
+		 *    and `indentSize` is set to `"tabSize"`.
 		 *
 		 * When getting a text editor's options, this property will always be a number (resolved).
 		 * When setting a text editor's options, this property is optional and it can be a number or `"auto"`.
