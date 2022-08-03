918454bf993400b683456bb261e8c315bd771387
Joyce Er <joyce.er@microsoft.com>
2022-08-03 11:00:59 -0700

    Enable edit sessions by default

--- src/vs/workbench/contrib/editSessions/browser/editSessions.contribution.ts
+++ src/vs/workbench/contrib/editSessions/browser/editSessions.contribution.ts
@@ -596,7 +596,7 @@ Registry.as<IConfigurationRegistry>(Extensions.Configuration).registerConfigurat
 		'workbench.experimental.editSessions.enabled': {
 			'type': 'boolean',
 			'tags': ['experimental', 'usesOnlineServices'],
-			'default': false,
+			'default': true,
 			'markdownDescription': localize('editSessionsEnabled', "Controls whether to display cloud-enabled actions to store and resume uncommitted changes when switching between web, desktop, or devices."),
 		},
 	}
