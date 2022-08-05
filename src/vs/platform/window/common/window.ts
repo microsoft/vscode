70020e80c0742f2db4f4386baf41c805646500f8
Benjamin Pasero <benjamin.pasero@microsoft.com>
2022-08-05 09:18:00 +0200

    themes - disable window border on windows

--- src/vs/platform/window/common/window.ts
+++ src/vs/platform/window/common/window.ts
@@ -138,11 +138,6 @@ export interface IWindowSettings {
 	readonly experimental?: { useSandbox: boolean };
 }
 
-interface IWindowBorderColors {
-	readonly 'window.activeBorder'?: string;
-	readonly 'window.inactiveBorder'?: string;
-}
-
 export function getTitleBarStyle(configurationService: IConfigurationService): 'native' | 'custom' {
 	if (isWeb) {
 		return 'custom';
@@ -158,11 +153,6 @@ export function getTitleBarStyle(configurationService: IConfigurationService): '
 		const useSimpleFullScreen = isMacintosh && configuration.nativeFullScreen === false;
 		if (useSimpleFullScreen) {
 			return 'native'; // simple fullscreen does not work well with custom title style (https://github.com/microsoft/vscode/issues/63291)
-		}
-
-		const colorCustomizations = configurationService.getValue<IWindowBorderColors | undefined>('workbench.colorCustomizations');
-		if (colorCustomizations?.['window.activeBorder'] || colorCustomizations?.['window.inactiveBorder']) {
-			return 'custom'; // window border colors do not work with native title style
 		}
 
 		const style = configuration.titleBarStyle;
