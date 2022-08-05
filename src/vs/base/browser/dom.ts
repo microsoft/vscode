6d07aabb375b42dc1319eb8d2d3da51562f641aa
Matt Bierner <matb@microsoft.com>
2022-08-04 17:33:23 -0700

    Remove event.stopEvent
    This function has the same function as EventHelper.stop but is only used in one place

--- src/vs/base/browser/dom.ts
+++ src/vs/base/browser/dom.ts
@@ -858,11 +858,12 @@ export interface EventLike {
 }
 
 export const EventHelper = {
-	stop: function (e: EventLike, cancelBubble?: boolean) {
+	stop: <T extends EventLike>(e: T, cancelBubble?: boolean): T => {
 		e.preventDefault();
 		if (cancelBubble) {
 			e.stopPropagation();
 		}
+		return e;
 	}
 };
 
