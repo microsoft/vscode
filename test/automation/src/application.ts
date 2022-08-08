db3358640eb1b7cdbd4f0c3b1c959a0c73d70b74
deepak1556 <hop2deep@gmail.com>
2022-08-08 20:04:28 +0900

    chore: only use window event in Electron

--- test/automation/src/application.ts
+++ test/automation/src/application.ts
@@ -115,7 +115,9 @@ export class Application {
 	}
 
 	private async checkWindowReady(code: Code): Promise<void> {
-		await measureAndLog(code.didFinishLoad(), 'Application#checkWindowReady: wait for navigation to be committed', this.logger);
+		if (!this.web) {
+			await measureAndLog(code.didFinishLoad(), 'Application#checkWindowReady: wait for navigation to be committed', this.logger);
+		}
 
 		// We need a rendered workbench
 		await measureAndLog(code.waitForElement('.monaco-workbench'), 'Application#checkWindowReady: wait for .monaco-workbench element', this.logger);
