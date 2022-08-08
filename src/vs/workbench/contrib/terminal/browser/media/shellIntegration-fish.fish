a79956ac8478981da9ef8d633bef86752c29e44b
Z. Grace Moreau <zgracem@users.noreply.github.com>
2022-08-08 14:49:38 -0600

    try exempting `*.fish` files from copyright requirements
    This appears consistent with the treatment of other shell scripts.

--- src/vs/workbench/contrib/terminal/browser/media/shellIntegration-fish.fish
+++ src/vs/workbench/contrib/terminal/browser/media/shellIntegration-fish.fish
@@ -1,8 +1,8 @@
-# ---------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
+#   Visual Studio Code terminal integration for fish
 #   Copyright (c) Microsoft Corporation. All rights reserved.
 #   Licensed under the MIT License. See License.txt in the project root for license information.
-# ---------------------------------------------------------------------------------------------
-
+# ----------------------------------------------------------------------------
 # Manual installation:
 #
 #   (1) Add the following to the end of `$__fish_config_dir/config.fish`:
@@ -11,11 +11,10 @@
 #         and . (code --locate-shell-integration-path fish)
 #
 #   (2) Restart fish.
-
-# ---------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
 # TODO: Confirm all escape sequences once they are finalized.
 # See microsoft/vscode#155639 and microsoft/vscode#139400 for discussion.
-# ---------------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
 
 # Don't run in scripts, other terminals, or more than once per session.
 status is-interactive
