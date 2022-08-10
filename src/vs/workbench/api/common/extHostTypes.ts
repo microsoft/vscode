cdbc59ce504e3f499abea5c86853e8f70c2cc0fb
Alex Ross <alros@microsoft.com>
2022-08-10 14:48:20 +0200

    Fix invalid tree item check for iconPath Fixes #157273

--- src/vs/workbench/api/common/extHostTypes.ts
+++ src/vs/workbench/api/common/extHostTypes.ts
@@ -2448,8 +2448,11 @@ export class TreeItem {
 			return false;
 		}
 		if ((treeItemThing.iconPath !== undefined) && !isString(treeItemThing.iconPath) && !URI.isUri(treeItemThing.iconPath) && !isString((treeItemThing.iconPath as vscode.ThemeIcon).id)) {
-			console.log('INVALID tree item, invalid iconPath', treeItemThing.iconPath);
-			return false;
+			const asLightAndDarkThing = treeItemThing.iconPath as { light: string | URI; dark: string | URI };
+			if (!isString(asLightAndDarkThing.light) && !URI.isUri(asLightAndDarkThing.light) && !isString(asLightAndDarkThing.dark) && !URI.isUri(asLightAndDarkThing.dark)) {
+				console.log('INVALID tree item, invalid iconPath', treeItemThing.iconPath);
+				return false;
+			}
 		}
 		if ((treeItemThing.description !== undefined) && !isString(treeItemThing.description) && (typeof treeItemThing.description !== 'boolean')) {
 			console.log('INVALID tree item, invalid description', treeItemThing.description);
