127c1e2e872ca9b8f5f34dd4c1bc44f87ac8d185
WJKM <54790970+wmentha@users.noreply.github.com>
2022-08-14 20:25:58 +1000

    Remove unused `hoverOptions`

--- src/vs/workbench/electron-sandbox/parts/titlebar/titlebarPart.ts
+++ src/vs/workbench/electron-sandbox/parts/titlebar/titlebarPart.ts
@@ -171,12 +171,6 @@ export class TitlebarPart extends BrowserTitleBarPart {
 		// Window Controls (Native Windows/Linux)
 		const hasWindowControlsOverlay = typeof (navigator as any).windowControlsOverlay !== 'undefined';
 		if (!isMacintosh && getTitleBarStyle(this.configurationService) !== 'native' && !hasWindowControlsOverlay && this.windowControls) {
-			const hoverOptions = {
-				hoverPosition: HoverPosition.BELOW,
-				showPointer: false,
-				//compact: true,
-				//forcePosition: true,
-			};
 			// Minimize
 			const minimizeIcon = append(this.windowControls, $('div.window-icon.window-minimize' + Codicon.chromeMinimize.cssSelector));
 			this._register(addDisposableListener(minimizeIcon, EventType.CLICK, e => {
