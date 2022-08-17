93005be9d64a1c238217025446b990f3444453a4
Justin Chen <54879025+justschen@users.noreply.github.com>
2022-08-17 06:33:09 -0700

    Code action widget accessibility for screen reader (#158301)
    * bux fix on code actions not showing up
    
    * bugfix on accesibility in code action widget

--- src/vs/editor/contrib/codeAction/browser/codeActionMenu.ts
+++ src/vs/editor/contrib/codeAction/browser/codeActionMenu.ts
@@ -329,7 +329,23 @@ export class CodeActionMenu extends Disposable implements IEditorContribution {
 		}, [this.listRenderer],
 			{
 				keyboardSupport: false,
-
+				accessibilityProvider: {
+					getAriaLabel: element => {
+						if (element.action instanceof CodeActionAction) {
+							const label = element.action.label;
+							if (!element.action.enabled) {
+								if (element.action instanceof CodeActionAction) {
+									localize({ key: 'customCodeActionWidget.labels', comment: ['Code action labels for accessibility.'] }, "{0}, Disabled Reason: {1}", label, element.action.action.disabled);
+								}
+							}
+							return label;
+						}
+						return null;
+					},
+					getWidgetAriaLabel: () => localize({ key: 'customCodeActionWidget', comment: ['A Code Action Option'] }, "Code Action Widget"),
+					getRole: () => 'option',
+					getWidgetRole: () => 'code-action-widget'
+				}
 			}
 		);
 
