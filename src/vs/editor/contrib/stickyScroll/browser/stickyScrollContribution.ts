30615299cf5d4d4a8c1f797963d4a72db0965f09
aiday-mar <t-aidaym@microsoft.com>
2022-08-17 15:25:03 +0200

    Renaming the stickyScroll to stickyScrollController

--- src/vs/editor/contrib/stickyScroll/browser/stickyScrollContribution.ts
+++ src/vs/editor/contrib/stickyScroll/browser/stickyScrollContribution.ts
@@ -5,7 +5,7 @@
 
 import { registerEditorContribution } from 'vs/editor/browser/editorExtensions';
 import { ToggleStickyScroll } from 'vs/editor/contrib/stickyScroll/browser/stickyScrollActions';
-import { StickyScrollController } from 'vs/editor/contrib/stickyScroll/browser/stickyScroll';
+import { StickyScrollController } from 'vs/editor/contrib/stickyScroll/browser/stickyScrollController';
 import { registerAction2 } from 'vs/platform/actions/common/actions';
 
 registerEditorContribution(StickyScrollController.ID, StickyScrollController);
