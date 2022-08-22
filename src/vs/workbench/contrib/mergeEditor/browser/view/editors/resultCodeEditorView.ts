17e6e980dff6d1f0bd1964c796961c96746b6ddd
Johannes <johannes.rieken@gmail.com>
2022-08-22 12:21:09 +0200

    Expose editor "blue button" as menu
    * rename proposed `MergeToolbar` to `EditorContent` menu
    * adopt GIT and sync conflicts usage
    * use content menu for "open in 3wm" command
    * add new `ctxIsMergeResultEditor` context key
    
    Better fix for https://github.com/microsoft/vscode/issues/153800

--- src/vs/workbench/contrib/mergeEditor/browser/view/editors/resultCodeEditorView.ts
+++ src/vs/workbench/contrib/mergeEditor/browser/view/editors/resultCodeEditorView.ts
@@ -5,16 +5,19 @@
 
 import { CompareResult } from 'vs/base/common/arrays';
 import { BugIndicatingError } from 'vs/base/common/errors';
+import { toDisposable } from 'vs/base/common/lifecycle';
 import { autorun, derived } from 'vs/base/common/observable';
 import { EditorExtensionsRegistry, IEditorContributionDescription } from 'vs/editor/browser/editorExtensions';
 import { IModelDeltaDecoration, MinimapPosition, OverviewRulerLane } from 'vs/editor/common/model';
 import { localize } from 'vs/nls';
+import { IContextKeyService } from 'vs/platform/contextkey/common/contextkey';
 import { IInstantiationService } from 'vs/platform/instantiation/common/instantiation';
 import { MergeMarkersController } from 'vs/workbench/contrib/mergeEditor/browser/mergeMarkers/mergeMarkersController';
 import { LineRange } from 'vs/workbench/contrib/mergeEditor/browser/model/lineRange';
 import { applyObservableDecorations, join } from 'vs/workbench/contrib/mergeEditor/browser/utils';
 import { handledConflictMinimapOverViewRulerColor, unhandledConflictMinimapOverViewRulerColor } from 'vs/workbench/contrib/mergeEditor/browser/view/colors';
 import { EditorGutter } from 'vs/workbench/contrib/mergeEditor/browser/view/editorGutter';
+import { ctxIsMergeResultEditor } from 'vs/workbench/contrib/mergeEditor/common/mergeEditor';
 import { CodeEditorView } from './codeEditorView';
 
 export class ResultCodeEditorView extends CodeEditorView {
@@ -110,6 +113,13 @@ export class ResultCodeEditorView extends CodeEditorView {
 		@IInstantiationService instantiationService: IInstantiationService
 	) {
 		super(instantiationService);
+
+		this.editor.invokeWithinContext(accessor => {
+			const contextKeyService = accessor.get(IContextKeyService);
+			const isMergeResultEditor = ctxIsMergeResultEditor.bindTo(contextKeyService);
+			isMergeResultEditor.set(true);
+			this._register(toDisposable(() => isMergeResultEditor.reset()));
+		});
 
 		this._register(applyObservableDecorations(this.editor, this.decorations));
 
