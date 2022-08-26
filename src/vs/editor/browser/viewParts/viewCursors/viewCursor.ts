41d3a8d2287d322b3114297ebd32773fffa4609e
Jacek Kopecky <jacek@jacek.cz>
2022-08-26 16:39:28 +0100

    Remove ill-advised tweak

--- src/vs/editor/browser/viewParts/viewCursors/viewCursor.ts
+++ src/vs/editor/browser/viewParts/viewCursors/viewCursor.ts
@@ -149,11 +149,7 @@ export class ViewCursor {
 				width = dom.computeScreenAwareSize(1);
 			}
 
-			let left = visibleRange.left;
-			if (width >= 2 && left >= 1) {
-				// try to center cursor
-				left -= 1;
-			}
+			const left = visibleRange.left;
 
 			const top = ctx.getVerticalOffsetForLineNumber(position.lineNumber) - ctx.bigNumbersDelta;
 			return new ViewCursorRenderData(top, left, width, this._lineHeight, textContent, '');
