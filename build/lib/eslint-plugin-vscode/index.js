456f401ebbe1c8eb5cfef3f56bbf84627210b4a8
Matt Bierner <matb@microsoft.com>
2022-08-29 08:13:52 -0700

    Enable transpileOnly

--- build/lib/eslint-plugin-vscode/index.js
+++ build/lib/eslint-plugin-vscode/index.js
@@ -1,7 +1,7 @@
 const glob = require('glob');
 const path = require('path');
 
-require('ts-node').register({ experimentalResolver: true });
+require('ts-node').register({ experimentalResolver: true, transpileOnly: true });
 
 // Re-export all .ts files as rules
 const rules = {};
