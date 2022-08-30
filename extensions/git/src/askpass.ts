d600bc8d691e28b72f91e0ad996c4dc2ac24f490
Ladislau Szomoru <3372902+lszomoru@users.noreply.github.com>
2022-08-30 17:00:07 +0200

    Tweak implementation based on feedback

--- extensions/git/src/askpass.ts
+++ extensions/git/src/askpass.ts
@@ -3,7 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { window, InputBoxOptions, Uri, Disposable, workspace } from 'vscode';
+import { window, InputBoxOptions, Uri, Disposable, workspace, QuickPickOptions } from 'vscode';
 import { IDisposable, EmptyDisposable, toDisposable } from './util';
 import * as path from 'path';
 import { IIPCHandler, IIPCServer } from './ipc/ipcServer';
@@ -23,16 +23,21 @@ export class Askpass implements IIPCHandler, ITerminalEnvironmentProvider {
 		}
 
 		this.env = {
+			// GIT_ASKPASS
 			GIT_ASKPASS: path.join(__dirname, this.ipc ? 'askpass.sh' : 'askpass-empty.sh'),
-			SSH_ASKPASS: path.join(__dirname, this.ipc ? 'askpass.sh' : 'askpass-empty.sh'),
+			// SSH_ASKPASS
+			SSH_ASKPASS: path.join(__dirname, this.ipc ? 'ssh-askpass.sh' : 'ssh-askpass-empty.sh'),
 			SSH_ASKPASS_REQUIRE: 'force',
+			// VSCODE_GIT_ASKPASS
 			VSCODE_GIT_ASKPASS_NODE: process.execPath,
 			VSCODE_GIT_ASKPASS_EXTRA_ARGS: (process.versions['electron'] && process.versions['microsoft-build']) ? '--ms-enable-electron-run-as-node' : '',
 			VSCODE_GIT_ASKPASS_MAIN: path.join(__dirname, 'askpass-main.js'),
 		};
 	}
 
-	async handle({ request, data }: { request: string; data: string }): Promise<string> {
+	async handle(payload:
+		{ askpassType: 'https'; request: string; host: string } |
+		{ askpassType: 'ssh'; request: string; host?: string; file?: string; fingerprint?: string }): Promise<string> {
 		const config = workspace.getConfiguration('git', null);
 		const enabled = config.get<boolean>('enabled');
 
@@ -40,44 +45,71 @@ export class Askpass implements IIPCHandler, ITerminalEnvironmentProvider {
 			return '';
 		}
 
-		const username = /username/i.test(request);
+		// https
+		if (payload.askpassType === 'https') {
+			return await this.handleAskpass(payload.request, payload.host);
+		}
+
+		// ssh
+		return await this.handleSSHAskpass(payload.request, payload.host, payload.file, payload.fingerprint);
+	}
+
+	async handleAskpass(request: string, host: string): Promise<string> {
+		const uri = Uri.parse(host);
+		const authority = uri.authority.replace(/^.*@/, '');
 		const password = /password/i.test(request);
-		const passphrase = /passphrase/i.test(request);
+		const cached = this.cache.get(authority);
 
-		if (username || password) {
-			// HTTPS (Username/Password)
-			const uri = Uri.parse(data);
-			const authority = uri.authority.replace(/^.*@/, '');
-			const cached = this.cache.get(authority);
+		if (cached && password) {
+			this.cache.delete(authority);
+			return cached.password;
+		}
 
-			if (cached && password) {
-				this.cache.delete(authority);
-				return cached.password;
-			}
+		if (!password) {
+			for (const credentialsProvider of this.credentialsProviders) {
+				try {
+					const credentials = await credentialsProvider.getCredentials(uri);
 
-			if (username) {
-				for (const credentialsProvider of this.credentialsProviders) {
-					try {
-						const credentials = await credentialsProvider.getCredentials(uri);
-
-						if (credentials) {
-							this.cache.set(authority, credentials);
-							setTimeout(() => this.cache.delete(authority), 60_000);
-							return credentials.username;
-						}
-					} catch { }
-				}
+					if (credentials) {
+						this.cache.set(authority, credentials);
+						setTimeout(() => this.cache.delete(authority), 60_000);
+						return credentials.username;
+					}
+				} catch { }
 			}
 		}
 
 		const options: InputBoxOptions = {
-			password: password || passphrase,
+			password,
 			placeHolder: request,
-			prompt: passphrase ? `SSH Key: ${data}` : `Git: ${data}`,
+			prompt: `Git: ${host}`,
 			ignoreFocusOut: true
 		};
 
 		return await window.showInputBox(options) || '';
+	}
+
+	async handleSSHAskpass(request: string, host?: string, file?: string, fingerprint?: string): Promise<string> {
+		// passphrase
+		if (/passphrase/i.test(request)) {
+			const options: InputBoxOptions = {
+				password: true,
+				placeHolder: 'Passphrase',
+				prompt: `SSH Key: ${file}`,
+				ignoreFocusOut: true
+			};
+
+			return await window.showInputBox(options) || '';
+		}
+
+		// authenticity
+		const options: QuickPickOptions = {
+			canPickMany: false,
+			ignoreFocusOut: true,
+			placeHolder: 'Are you sure you want to continue connecting?',
+			title: `"${host}" has fingerprint "${fingerprint}".`
+		};
+		return await window.showQuickPick(['yes', 'no'], options) ?? '';
 	}
 
 	getEnv(): { [key: string]: string } {
