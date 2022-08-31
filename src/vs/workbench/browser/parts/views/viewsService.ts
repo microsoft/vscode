a0eff7668cda665dd43b52c26b0e83f5eb00df57
Sandeep Somavarapu <sasomava@microsoft.com>
2022-08-31 17:32:34 +0200

    update comment

--- src/vs/workbench/browser/parts/views/viewsService.ts
+++ src/vs/workbench/browser/parts/views/viewsService.ts
@@ -648,4 +648,4 @@ export function getPartByLocation(viewContainerLocation: ViewContainerLocation):
 	}
 }
 
-registerSingleton(IViewsService, ViewsService, false /* Eager because of its cyclic dependency: ViewsService -> PaneCompositeParts -> CompositePart -> CompositeProgressIndicator -> ViewsService */);
+registerSingleton(IViewsService, ViewsService, false /* Eager because of the cyclic dependency when creating registering PaneComposites in the constructor : ViewsService -> PaneCompositeParts -> CompositePart -> CompositeProgressIndicator -> ViewsService */);
