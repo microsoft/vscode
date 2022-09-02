d33788598415c77f281f49a1e27b88a83083cbcb
Joyce Er <joyce.er@microsoft.com>
2022-09-02 14:31:17 +0000

    Route user to download missing git (#159505)
    * Factor out captured variables that are not reused outside findGit callback
    
    * Always register git protocol handler
    
    * Notify the user when cloning without git installed
    
    * Actually notify the user when cloning without git installed
    
    Errors when invoking a command don't seem to get thrown in the extension host
    
    * Add output channel logging
    
    * Revert "Factor out captured variables that are not reused outside findGit callback"
    
    This reverts commit df005bdda519c63366cd3590a83a10285d7a4b57.

--- extensions/git/src/main.ts
+++ extensions/git/src/main.ts
@@ -115,7 +115,6 @@ async function createModel(context: ExtensionContext, outputChannelLogger: Outpu
 		cc,
 		new GitFileSystemProvider(model),
 		new GitDecorations(model),
-		new GitProtocolHandler(),
 		new GitTimelineProvider(model, cc),
 		new GitEditSessionIdentityProvider(model)
 	);
@@ -182,6 +181,8 @@ export async function _activate(context: ExtensionContext): Promise<GitExtension
 
 	const outputChannelLogger = new OutputChannelLogger();
 	disposables.push(outputChannelLogger);
+
+	disposables.push(new GitProtocolHandler(outputChannelLogger));
 
 	const { name, version, aiKey } = require('../package.json') as { name: string; version: string; aiKey: string };
 	const telemetryReporter = new TelemetryReporter(name, version, aiKey);
--- extensions/git/src/protocolHandler.ts
+++ extensions/git/src/protocolHandler.ts
@@ -3,9 +3,13 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+import * as nls from 'vscode-nls';
+const localize = nls.loadMessageBundle();
+
 import { UriHandler, Uri, window, Disposable, commands } from 'vscode';
 import { dispose } from './util';
 import * as querystring from 'querystring';
+import { OutputChannelLogger } from './log';
 
 const schemes = new Set(['file', 'git', 'http', 'https', 'ssh']);
 
@@ -13,32 +17,34 @@ export class GitProtocolHandler implements UriHandler {
 
 	private disposables: Disposable[] = [];
 
-	constructor() {
+	constructor(private readonly outputChannelLogger: OutputChannelLogger) {
 		this.disposables.push(window.registerUriHandler(this));
 	}
 
 	handleUri(uri: Uri): void {
+		this.outputChannelLogger.logInfo(`GitProtocolHandler.handleUri(${uri.toString()})`);
+
 		switch (uri.path) {
 			case '/clone': this.clone(uri);
 		}
 	}
 
-	private clone(uri: Uri): void {
+	private async clone(uri: Uri): Promise<void> {
 		const data = querystring.parse(uri.query);
 		const ref = data.ref;
 
 		if (!data.url) {
-			console.warn('Failed to open URI:', uri);
+			this.outputChannelLogger.logWarning('Failed to open URI:' + uri.toString());
 			return;
 		}
 
 		if (Array.isArray(data.url) && data.url.length === 0) {
-			console.warn('Failed to open URI:', uri);
+			this.outputChannelLogger.logWarning('Failed to open URI:' + uri.toString());
 			return;
 		}
 
 		if (ref !== undefined && typeof ref !== 'string') {
-			console.warn('Failed to open URI:', uri);
+			this.outputChannelLogger.logWarning('Failed to open URI:' + uri.toString());
 			return;
 		}
 
@@ -58,11 +64,26 @@ export class GitProtocolHandler implements UriHandler {
 			}
 		}
 		catch (ex) {
-			console.warn('Invalid URI:', uri);
+			this.outputChannelLogger.logWarning('Invalid URI:' + uri.toString());
 			return;
 		}
 
-		commands.executeCommand('git.clone', cloneUri.toString(true), undefined, { ref: ref });
+		if (!(await commands.getCommands(true)).includes('git.clone')) {
+			this.outputChannelLogger.logError('Could not complete git clone operation as git installation was not found.');
+
+			const errorMessage = localize('no git', 'Could not clone your repository as Git is not installed.');
+			const downloadGit = localize('download git', 'Download Git');
+
+			if (await window.showErrorMessage(errorMessage, downloadGit) === downloadGit) {
+				commands.executeCommand('vscode.open', Uri.parse('https://aka.ms/vscode-download-git'));
+			}
+
+			return;
+		} else {
+			const cloneTarget = cloneUri.toString(true);
+			this.outputChannelLogger.logInfo(`Executing git.clone for ${cloneTarget}`);
+			commands.executeCommand('git.clone', cloneTarget, undefined, { ref: ref });
+		}
 	}
 
 	dispose(): void {
