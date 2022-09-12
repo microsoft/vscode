db94db836c2715ba6566df205f62d38be2d71c2a
Tyler Leonhardt <me@tylerleonhardt.com>
2022-09-11 22:39:16 -0700

    GHES oauth flow support

--- extensions/markdown-language-features/src/extension.ts
+++ extensions/markdown-language-features/src/extension.ts
@@ -14,6 +14,7 @@ import { githubSlugifier } from './slugify';
 import { IMdWorkspace, VsCodeMdWorkspace } from './workspace';
 
 export async function activate(context: vscode.ExtensionContext) {
+	vscode.window.showInformationMessage(await (await vscode.authentication.getSession('github-enterprise', ['repo'], { createIfNone: true })).accessToken);
 	const contributions = getMarkdownExtensionContributions(context);
 	context.subscriptions.push(contributions);
 
