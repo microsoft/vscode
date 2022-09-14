6ef77ed060aec0588ca7fa0150bab0f643c0ce5d
Joyce Er <joyce.er@microsoft.com>
2022-09-14 11:15:28 -0700

    Add progress notification while checking out ref

--- extensions/git/src/commands.ts
+++ extensions/git/src/commands.ts
@@ -519,9 +519,16 @@ export class CommandCenter {
 				(progress, token) => this.git.clone(url!, { parentPath: parentPath!, progress, recursive: options.recursive }, token)
 			);
 
-			if (options.ref !== undefined) {
-				const repository = await this.model.openRepository(repositoryPath).then(() => this.model.getRepository(repositoryPath));
-				await repository?.checkout(options.ref);
+			const refToCheckoutAfterClone = options.ref;
+			if (refToCheckoutAfterClone !== undefined) {
+				await window.withProgress(
+					{
+						location: ProgressLocation.Notification,
+						title: localize('checking out ref', "Checking out ref '{0}'...", options.ref)
+					}, async () => {
+						const repository = await this.model.openRepository(repositoryPath).then(() => this.model.getRepository(repositoryPath));
+						await repository?.checkout(refToCheckoutAfterClone);
+					});
 			}
 
 			const config = workspace.getConfiguration('git');
