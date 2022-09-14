3f82b63376e4079320d448442d0f7f88039a59aa
meganrogge <merogge@microsoft.com>
2022-09-14 11:36:24 -0700

    Revert "fix #160920"
    This reverts commit 0a35bc6012d8c5b0f43c652c01f9f46109bb2f4b.

--- src/vs/workbench/contrib/terminal/browser/media/shellIntegration-env.zsh
+++ src/vs/workbench/contrib/terminal/browser/media/shellIntegration-env.zsh
@@ -2,12 +2,12 @@
 #   Copyright (c) Microsoft Corporation. All rights reserved.
 #   Licensed under the MIT License. See License.txt in the project root for license information.
 # ---------------------------------------------------------------------------------------------
-VSCODE_ZDOTDIR=$ZDOTDIR
 if [[ -f $USER_ZDOTDIR/.zshenv ]]; then
+	VSCODE_ZDOTDIR=$ZDOTDIR
 	ZDOTDIR=$USER_ZDOTDIR
+
 	. $USER_ZDOTDIR/.zshenv
-elif [[ -f ~/.zshenv ]]; then
-	ZDOTDIR=~
-	. ~/.zshenv
+
+	USER_ZDOTDIR=$ZDOTDIR
+	ZDOTDIR=$VSCODE_ZDOTDIR
 fi
-ZDOTDIR=$VSCODE_ZDOTDIR
