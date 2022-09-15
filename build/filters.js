cfc01197555cb14a0d2d3463b331ce41de5733be
Benjamin Pasero <benjamin.pasero@microsoft.com>
2022-09-14 23:26:22 -0700

    Revert "JavaScript and Node.js walkthrough" (#160959)
    Revert "JavaScript and Node.js walkthrough (#157965)"
    
    This reverts commit 0cbcb1b1ab485cc7a16ca918728e600e41dbb29b.

--- build/filters.js
+++ build/filters.js
@@ -82,7 +82,6 @@ module.exports.indentationFilter = [
 	'!test/monaco/out/**',
 	'!test/smoke/out/**',
 	'!extensions/typescript-language-features/test-workspace/**',
-	'!extensions/typescript-language-features/resources/walkthroughs/**',
 	'!extensions/markdown-math/notebook-out/**',
 	'!extensions/vscode-api-tests/testWorkspace/**',
 	'!extensions/vscode-api-tests/testWorkspace2/**',
