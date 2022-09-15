001f7d5b4c20220ef7294f18616ad52df1039048
Megan Rogge <merogge@microsoft.com>
2022-09-15 09:34:28 -0700

    add audio cue for task end and terminal bell (#160828)

--- src/vs/workbench/contrib/tasks/browser/abstractTaskService.ts
+++ src/vs/workbench/contrib/tasks/browser/abstractTaskService.ts
@@ -1928,7 +1928,7 @@ export abstract class AbstractTaskService extends Disposable implements ITaskSer
 			this._contextService, this._environmentService,
 			AbstractTaskService.OutputChannelId, this._fileService, this._terminalProfileResolverService,
 			this._pathService, this._viewDescriptorService, this._logService, this._configurationService, this._notificationService,
-			this,
+			this, this._instantiationService,
 			(workspaceFolder: IWorkspaceFolder | undefined) => {
 				if (workspaceFolder) {
 					return this._getTaskSystemInfo(workspaceFolder.uri.scheme);
