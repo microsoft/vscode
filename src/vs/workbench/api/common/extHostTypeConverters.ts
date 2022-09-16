6e8bc02be42611b78c2cc5dab3caed339ddd24fe
Peng Lyu <penn.lv@gmail.com>
2022-09-16 14:12:53 -0700

    Cell attachment cleanup tool improvement in diff editor (#161132)
    * Move attachment out of custom metadata, prep for attachment clean up in diff editor
    
    * recover attachments from dirty notebook document
    
    * Allow metadata to be restored when content changed/reverted in nb diff editor

--- src/vs/workbench/api/common/extHostTypeConverters.ts
+++ src/vs/workbench/api/common/extHostTypeConverters.ts
@@ -1709,7 +1709,8 @@ export namespace NotebookDocumentContentOptions {
 		return {
 			transientOutputs: options?.transientOutputs ?? false,
 			transientCellMetadata: options?.transientCellMetadata ?? {},
-			transientDocumentMetadata: options?.transientDocumentMetadata ?? {}
+			transientDocumentMetadata: options?.transientDocumentMetadata ?? {},
+			cellContentMetadata: options?.cellContentMetadata ?? {}
 		};
 	}
 }
