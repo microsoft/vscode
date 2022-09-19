e02895941df19c09bdb6ac20e6e8336ef683fc52
Connor Peet <connor@peet.io>
2022-09-19 16:40:39 -0700

    build: include the integrated CLI in product builds

--- src/cli/build.rs
+++ src/cli/build.rs
@@ -3,10 +3,8 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-const FILE_HEADER: &[u8] = b"/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
- *--------------------------------------------------------------------------------------------*/";
+
+const FILE_HEADER: &str = "/*---------------------------------------------------------------------------------------------\n *  Copyright (c) Microsoft Corporation. All rights reserved.\n *  Licensed under the MIT License. See License.txt in the project root for license information.\n *--------------------------------------------------------------------------------------------*/";
 
 use std::{env, fs, io, path::PathBuf, process};
 
@@ -17,10 +15,14 @@ fn main() {
 
 fn ensure_file_headers(files: &[PathBuf]) -> Result<(), io::Error> {
     let mut ok = true;
+
+	let crlf_header_str = str::replace(FILE_HEADER, "\n", "\r\n");
+	let crlf_header = crlf_header_str.as_bytes();
+	let lf_header = FILE_HEADER.as_bytes();
     for file in files {
         let contents = fs::read(file)?;
 
-        if !contents.starts_with(FILE_HEADER) {
+        if !(contents.starts_with(lf_header) || contents.starts_with(crlf_header)) {
             eprintln!("File missing copyright header: {}", file.display());
             ok = false;
         }
