b2da058401bda3daf9c021d5562abdf3ae031d96
Johannes Rieken <johannes.rieken@gmail.com>
2022-09-21 15:05:58 +0200

    Add information about type of change to `IMenu#onDidChange` (#161397)
    helps with https://github.com/microsoft/vscode/issues/161334#event-7430166381

--- src/vs/platform/actions/common/actions.ts
+++ src/vs/platform/actions/common/actions.ts
@@ -199,8 +199,15 @@ export interface IMenuActionOptions {
 	renderShortTitle?: boolean;
 }
 
+export interface IMenuChangeEvent {
+	readonly menu: IMenu;
+	readonly isStructuralChange: boolean;
+	readonly isToggleChange: boolean;
+	readonly isEnablementChange: boolean;
+}
+
 export interface IMenu extends IDisposable {
-	readonly onDidChange: Event<IMenu>;
+	readonly onDidChange: Event<IMenuChangeEvent>;
 	getActions(options?: IMenuActionOptions): [string, Array<MenuItemAction | SubmenuItemAction>][];
 }
 
