59df7f97fbaa49810972bd8377e175fdba504770
Benjamin Pasero <benjamin.pasero@microsoft.com>
2022-09-23 07:47:19 +0200

    move code into window

--- src/vs/workbench/workbench.common.main.ts
+++ src/vs/workbench/workbench.common.main.ts
@@ -339,9 +339,6 @@ import 'vs/workbench/contrib/workspace/browser/workspace.contribution';
 // Workspaces
 import 'vs/workbench/contrib/workspaces/browser/workspaces.contribution';
 
-// WorkspaceUnderAppRoot
-import 'vs/workbench/contrib/workspaceUnderAppRoot/browser/workspaceUnderAppRoot.contribution';
-
 // List
 import 'vs/workbench/contrib/list/browser/list.contribution';
 
