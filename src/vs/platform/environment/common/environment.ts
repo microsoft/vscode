e7e9a506467a92a58cea8f223c5a17c3b470bdd4
Benjamin Pasero <benjamin.pasero@microsoft.com>
2022-09-26 00:14:15 -0700

    fix #138815 (#161613)
    * fix #138815
    
    * add linux
    
    * fix compile and clarify

--- src/vs/platform/environment/common/environment.ts
+++ src/vs/platform/environment/common/environment.ts
@@ -124,6 +124,13 @@ export interface INativeEnvironmentService extends IEnvironmentService {
 	args: NativeParsedArgs;
 
 	// --- data paths
+	/**
+	 * Root path of the JavaScript sources.
+	 *
+	 * Note: This is NOT the installation root
+	 * directory itself but contained in it at
+	 * a level that is platform dependent.
+	 */
 	appRoot: string;
 	userHome: URI;
 	appSettingsHome: URI;
