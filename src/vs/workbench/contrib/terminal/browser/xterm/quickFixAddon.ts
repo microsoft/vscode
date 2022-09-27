2cbf3aaabc42329c6c9d00d7e9ead2d439970f31
meganrogge <merogge@microsoft.com>
2022-09-27 15:52:18 -0700

    fix #161598

--- src/vs/workbench/contrib/terminal/browser/xterm/quickFixAddon.ts
+++ src/vs/workbench/contrib/terminal/browser/xterm/quickFixAddon.ts
@@ -15,6 +15,10 @@ import { DecorationSelector, updateLayout } from 'vs/workbench/contrib/terminal/
 import { IConfigurationService } from 'vs/platform/configuration/common/configuration';
 import { Terminal, IDecoration } from 'xterm';
 import { IAction } from 'vs/base/common/actions';
+import { IColorTheme, ICssStyleCollector, registerThemingParticipant } from 'vs/platform/theme/common/themeService';
+import { PANEL_BACKGROUND } from 'vs/workbench/common/theme';
+import { TERMINAL_BACKGROUND_COLOR } from 'vs/workbench/contrib/terminal/common/terminalColorRegistry';
+import { Color } from 'vs/base/common/color';
 
 export interface ITerminalQuickFix {
 	showMenu(): void;
@@ -115,8 +119,12 @@ export class TerminalQuickFixAddon extends Disposable implements ITerminalAddon,
 		decoration?.onRender((e: HTMLElement) => {
 			if (!this._decorationMarkerIds.has(decoration.marker.id)) {
 				this._currentQuickFixElement = e;
-				e.classList.add(DecorationSelector.QuickFix, DecorationSelector.Codicon, DecorationSelector.CommandDecoration, DecorationSelector.XtermDecoration);
+				e.classList.add(DecorationSelector.QuickFix, DecorationSelector.LightBulb, DecorationSelector.Codicon, DecorationSelector.CommandDecoration, DecorationSelector.XtermDecoration);
 				e.style.color = '#ffcc00';
+				// must be inlined to override the styles
+				// applied inline in xterm.js
+				e.style.zIndex = '7';
+				e.style.borderRadius = '5px';
 				updateLayout(this._configurationService, e);
 				if (actions) {
 					this._decorationMarkerIds.add(decoration.marker.id);
@@ -168,3 +176,12 @@ export function getQuickFixes(command: ITerminalCommand, actionOptions: Map<stri
 	}
 	return actions.length === 0 ? undefined : actions;
 }
+
+let backgroundColor: string | Color | undefined;
+registerThemingParticipant((theme: IColorTheme, collector: ICssStyleCollector) => {
+	backgroundColor = theme.getColor(TERMINAL_BACKGROUND_COLOR) || theme.getColor(PANEL_BACKGROUND);
+
+	if (backgroundColor) {
+		collector.addRule(`.${DecorationSelector.CommandDecoration}.${DecorationSelector.QuickFix} { background-color: ${backgroundColor.toString()}; } `);
+	}
+});
