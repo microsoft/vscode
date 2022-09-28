a83ecc69c6d5fcbe23a14a05c185e41c674fc041
Matt Bierner <matb@microsoft.com>
2022-09-28 14:19:39 -0700

    Swap auto scroll lock/unlock icons
    Fixes #162263

--- src/vs/workbench/contrib/output/browser/output.contribution.ts
+++ src/vs/workbench/contrib/output/browser/output.contribution.ts
@@ -166,10 +166,10 @@ registerAction2(class extends Action2 {
 				group: 'navigation',
 				order: 3,
 			},
-			icon: Codicon.unlock,
+			icon: Codicon.lock,
 			toggled: {
 				condition: CONTEXT_OUTPUT_SCROLL_LOCK,
-				icon: Codicon.lock,
+				icon: Codicon.unlock,
 				tooltip: nls.localize('outputScrollOn', "Turn Auto Scrolling On")
 			}
 		});
