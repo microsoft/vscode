df8ed0fc0860f5ee2a9d833367ca615fc9cb0e7d
Megan Rogge <merogge@microsoft.com>
2022-10-04 07:45:46 -0700

    Update src/vs/workbench/contrib/terminal/browser/xterm/decorationStyles.ts
    Co-authored-by: Daniel Imms <2193314+Tyriar@users.noreply.github.com>

--- src/vs/workbench/contrib/terminal/browser/xterm/decorationStyles.ts
+++ src/vs/workbench/contrib/terminal/browser/xterm/decorationStyles.ts
@@ -2,6 +2,7 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
+
 import * as dom from 'vs/base/browser/dom';
 import { Delayer } from 'vs/base/common/async';
 import { fromNow } from 'vs/base/common/date';
