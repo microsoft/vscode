f9ba5ee29557e4b01e4f85a7efba93d54f4dc694
Ladislau Szomoru <3372902+lszomoru@users.noreply.github.com>
2022-10-05 09:40:18 +0200

    💄 IWorkspaceTrustRequestService delayed insta

--- src/vs/workbench/services/workspaces/common/workspaceTrust.ts
+++ src/vs/workbench/services/workspaces/common/workspaceTrust.ts
@@ -887,4 +887,4 @@ class WorkspaceTrustMemento {
 	}
 }
 
-registerSingleton(IWorkspaceTrustRequestService, WorkspaceTrustRequestService, false);
+registerSingleton(IWorkspaceTrustRequestService, WorkspaceTrustRequestService, true);
