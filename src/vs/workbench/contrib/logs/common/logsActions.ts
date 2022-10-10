77f39a3f87be1d96bd235860c15e530005208dce
Sandeep Somavarapu <sasomava@microsoft.com>
2022-10-10 15:34:57 +0200

    use registerAction2 (#163174)

--- src/vs/workbench/contrib/logs/common/logsActions.ts
+++ src/vs/workbench/contrib/logs/common/logsActions.ts
@@ -16,7 +16,7 @@ import { IEditorService } from 'vs/workbench/services/editor/common/editorServic
 export class SetLogLevelAction extends Action {
 
 	static readonly ID = 'workbench.action.setLogLevel';
-	static readonly LABEL = nls.localize('setLogLevel', "Set Log Level...");
+	static readonly TITLE = { value: nls.localize('setLogLevel', "Set Log Level..."), original: 'Set Log Level...' };
 
 	constructor(id: string, label: string,
 		@IQuickInputService private readonly quickInputService: IQuickInputService,
@@ -61,7 +61,7 @@ export class SetLogLevelAction extends Action {
 export class OpenWindowSessionLogFileAction extends Action {
 
 	static readonly ID = 'workbench.action.openSessionLogFile';
-	static readonly LABEL = nls.localize('openSessionLogFile', "Open Window Log File (Session)...");
+	static readonly TITLE = { value: nls.localize('openSessionLogFile', "Open Window Log File (Session)..."), original: 'Open Window Log File (Session)...' };
 
 	constructor(id: string, label: string,
 		@IWorkbenchEnvironmentService private readonly environmentService: IWorkbenchEnvironmentService,
