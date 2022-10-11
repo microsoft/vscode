6d3139c5b3afa49c655434a32c8a5a17d35037ed
Milan Hauth <milahu@gmail.com>
2022-10-11 20:25:49 +0200

    update lockfile

--- build/lib/policies.js
+++ build/lib/policies.js
@@ -96,6 +96,10 @@ class BooleanPolicy extends BasePolicy {
     }
 }
 class IntPolicy extends BasePolicy {
+    constructor(name, category, minimumVersion, description, moduleName, defaultValue) {
+        super(PolicyType.StringEnum, name, category, minimumVersion, description, moduleName);
+        this.defaultValue = defaultValue;
+    }
     static from(name, category, minimumVersion, description, moduleName, settingNode) {
         const type = getStringProperty(settingNode, 'type');
         if (type !== 'number') {
@@ -106,10 +110,6 @@ class IntPolicy extends BasePolicy {
             throw new Error(`Missing required 'default' property.`);
         }
         return new IntPolicy(name, category, minimumVersion, description, moduleName, defaultValue);
-    }
-    constructor(name, category, minimumVersion, description, moduleName, defaultValue) {
-        super(PolicyType.StringEnum, name, category, minimumVersion, description, moduleName);
-        this.defaultValue = defaultValue;
     }
     renderADMXElements() {
         return [
@@ -140,6 +140,11 @@ class StringPolicy extends BasePolicy {
     }
 }
 class StringEnumPolicy extends BasePolicy {
+    constructor(name, category, minimumVersion, description, moduleName, enum_, enumDescriptions) {
+        super(PolicyType.StringEnum, name, category, minimumVersion, description, moduleName);
+        this.enum_ = enum_;
+        this.enumDescriptions = enumDescriptions;
+    }
     static from(name, category, minimumVersion, description, moduleName, settingNode) {
         const type = getStringProperty(settingNode, 'type');
         if (type !== 'string') {
@@ -160,11 +165,6 @@ class StringEnumPolicy extends BasePolicy {
             throw new Error(`Property 'enumDescriptions' should be localized.`);
         }
         return new StringEnumPolicy(name, category, minimumVersion, description, moduleName, enum_, enumDescriptions);
-    }
-    constructor(name, category, minimumVersion, description, moduleName, enum_, enumDescriptions) {
-        super(PolicyType.StringEnum, name, category, minimumVersion, description, moduleName);
-        this.enum_ = enum_;
-        this.enumDescriptions = enumDescriptions;
     }
     renderADMXElements() {
         return [
