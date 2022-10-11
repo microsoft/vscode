43957ccfe1d7f387def153528812e5b513c7f11c
Don Jayamanne <don.jayamanne@outlook.com>
2022-10-12 08:43:46 +1100

    Compress streams in notebook outputs (#160946)
    * Revert "Compress notebook output streams before rendering (#160667)"
    
    This reverts commit 4230c22a08a0438af4809d33e4cfd561f714b127.
    
    * Compress stream output items
    
    * Minor perf improvements
    
    * Misc
    
    * Comments
    
    * Added tests
    
    * Merge issue
    
    * More merge issues
    
    * Misc
    
    * Address code review comments

--- src/vs/workbench/contrib/notebook/browser/view/renderers/backLayerWebView.ts
+++ src/vs/workbench/contrib/notebook/browser/view/renderers/backLayerWebView.ts
@@ -35,7 +35,7 @@ import { NOTEBOOK_WEBVIEW_BOUNDARY } from 'vs/workbench/contrib/notebook/browser
 import { preloadsScriptStr } from 'vs/workbench/contrib/notebook/browser/view/renderers/webviewPreloads';
 import { transformWebviewThemeVars } from 'vs/workbench/contrib/notebook/browser/view/renderers/webviewThemeMapping';
 import { MarkupCellViewModel } from 'vs/workbench/contrib/notebook/browser/viewModel/markupCellViewModel';
-import { CellUri, INotebookRendererInfo, isTextStreamMime, NotebookSetting, RendererMessagingSpec } from 'vs/workbench/contrib/notebook/common/notebookCommon';
+import { CellUri, INotebookRendererInfo, NotebookSetting, RendererMessagingSpec } from 'vs/workbench/contrib/notebook/common/notebookCommon';
 import { INotebookKernel } from 'vs/workbench/contrib/notebook/common/notebookKernelService';
 import { IScopedRendererMessaging } from 'vs/workbench/contrib/notebook/common/notebookRendererMessagingService';
 import { INotebookService } from 'vs/workbench/contrib/notebook/common/notebookService';
@@ -44,7 +44,6 @@ import { WebviewWindowDragMonitor } from 'vs/workbench/contrib/webview/browser/w
 import { IEditorGroupsService } from 'vs/workbench/services/editor/common/editorGroupsService';
 import { IWorkbenchEnvironmentService } from 'vs/workbench/services/environment/common/environmentService';
 import { FromWebviewMessage, IAckOutputHeight, IClickedDataUrlMessage, ICodeBlockHighlightRequest, IContentWidgetTopRequest, IControllerPreload, ICreationContent, ICreationRequestMessage, IFindMatch, IMarkupCellInitialization, RendererMetadata, ToWebviewMessage } from './webviewMessages';
-import { compressOutputItemStreams } from 'vs/workbench/contrib/notebook/browser/view/renderers/stdOutErrorPreProcessor';
 import { DeferredPromise } from 'vs/base/common/async';
 
 export interface ICachedInset<K extends ICommonCellInfo> {
@@ -1277,14 +1276,12 @@ var requirejs = (function() {
 		let updatedContent: ICreationContent | undefined = undefined;
 		if (content.type === RenderOutputType.Extension) {
 			const output = content.source.model;
-			const firstBuffer = isTextStreamMime(content.mimeType) ?
-				compressOutputItemStreams(content.mimeType, output.outputs) :
-				output.outputs.find(op => op.mime === content.mimeType)!.data.buffer;
+			const firstBuffer = output.outputs.find(op => op.mime === content.mimeType)!.data;
 			updatedContent = {
 				type: RenderOutputType.Extension,
 				outputId: outputCache.outputId,
 				mimeType: content.mimeType,
-				valueBytes: firstBuffer,
+				valueBytes: firstBuffer.buffer,
 				metadata: output.metadata,
 			};
 		}
