0c9f707f9a892c76d9b922354ced570f403b7b94
Milan Hauth <milahu@gmail.com>
2022-10-11 22:12:29 +0200

    fix TS2351: This expression is not constructable.

--- build/azure-pipelines/upload-cdn.ts
+++ build/azure-pipelines/upload-cdn.ts
@@ -4,7 +4,7 @@
  *--------------------------------------------------------------------------------------------*/
 
 import * as es from 'event-stream';
-import * as Vinyl from 'vinyl';
+import Vinyl from 'vinyl';
 import * as vfs from 'vinyl-fs';
 import filter from 'gulp-filter';
 import gzip from 'gulp-gzip';
