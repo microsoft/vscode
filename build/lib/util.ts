7f64bb1fa6a7d472b982a92230e8fb01fe47e2ed
Milan Hauth <milahu@gmail.com>
2022-10-11 22:29:49 +0200

    fix error TS2507: Type 'typeof File' is not a constructor function type.

--- build/lib/util.ts
+++ build/lib/util.ts
@@ -11,7 +11,7 @@ import * as _ from 'underscore';
 import * as path from 'path';
 import * as fs from 'fs';
 import * as _rimraf from 'rimraf';
-import * as VinylFile from 'vinyl';
+import VinylFile from 'vinyl';
 import { ThroughStream } from 'through';
 import * as sm from 'source-map-sync';
 import * as git from './git';
