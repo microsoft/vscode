32b93788e427d7beb459f034b6c03ed44b8e6dff
Tyler Leonhardt <me@tylerleonhardt.com>
2022-10-12 16:52:08 -0700

    Plumbing for desktop to pick up strings from language packs
    Web will come in the next PR (hence the TODO)
    
    Also this includes the smallest translation change which will be the ultimate test that this is all working.

--- src/vs/platform/languagePacks/browser/languagePacks.ts
+++ src/vs/platform/languagePacks/browser/languagePacks.ts
@@ -3,9 +3,23 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+import { URI } from 'vs/base/common/uri';
 import { ILanguagePackItem, LanguagePackBaseService } from 'vs/platform/languagePacks/common/languagePacks';
 
 export class WebLanguagePacksService extends LanguagePackBaseService {
+	// TODO: support builtin extensions using unpkg service
+	// constructor(
+	// 	@IExtensionResourceLoaderService extensionResourceLoaderService: IExtensionResourceLoaderService,
+	// 	@IExtensionGalleryService extensionGalleryService: IExtensionGalleryService,
+	// 	@ILogService private readonly logService: ILogService
+	// ) {
+	// 	super(extensionGalleryService);
+	// }
+
+	getTranslationsUri(id: string): Promise<URI | undefined> {
+		return Promise.resolve(undefined);
+	}
+
 	// Web doesn't have a concept of language packs, so we just return an empty array
 	getInstalledLanguages(): Promise<ILanguagePackItem[]> {
 		return Promise.resolve([]);
