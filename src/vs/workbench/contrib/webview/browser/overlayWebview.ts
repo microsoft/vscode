46e6b68743bd06db07d355db0c86fd78f8db9c22
Matt Bierner <matb@microsoft.com>
2022-10-14 11:12:46 -0700

    Move webview animation workaround to viewPane (#163675)
    This workaround is only required for webview views, so let's move the code there

--- src/vs/workbench/contrib/webview/browser/overlayWebview.ts
+++ src/vs/workbench/contrib/webview/browser/overlayWebview.ts
@@ -31,7 +31,6 @@ export class OverlayWebview extends Disposable implements IOverlayWebview {
 	private _html: string = '';
 	private _initialScrollProgress: number = 0;
 	private _state: string | undefined = undefined;
-	private _repositionTimeout: any | undefined = undefined;
 
 	private _extension: WebviewExtensionDescription | undefined;
 	private _contentOptions: WebviewContentOptions;
@@ -84,8 +83,6 @@ export class OverlayWebview extends Disposable implements IOverlayWebview {
 			msg.resolve(false);
 		}
 		this._firstLoadPendingMessages.clear();
-
-		clearTimeout(this._repositionTimeout);
 
 		this._onDidDispose.fire();
 
@@ -163,17 +160,6 @@ export class OverlayWebview extends Disposable implements IOverlayWebview {
 	}
 
 	public layoutWebviewOverElement(element: HTMLElement, dimension?: Dimension, clippingContainer?: HTMLElement) {
-		this.doLayoutWebviewOverElement(element, dimension, clippingContainer);
-
-		// Temporary fix for https://github.com/microsoft/vscode/issues/110450
-		// There is an animation that lasts about 200ms, update the webview positioning once this animation is complete.
-		clearTimeout(this._repositionTimeout);
-		this._repositionTimeout = setTimeout(() => {
-			this.doLayoutWebviewOverElement(element, dimension, clippingContainer);
-		}, 200);
-	}
-
-	public doLayoutWebviewOverElement(element: HTMLElement, dimension?: Dimension, clippingContainer?: HTMLElement) {
 		if (!this._container || !this._container.parentElement) {
 			return;
 		}
