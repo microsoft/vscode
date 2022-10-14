25bb57566d7b9d3528220b15b0dac05b5bebbf90
Andrea Mah <31675041+andreamah@users.noreply.github.com>
2022-10-14 09:29:15 -0700

    add restrict search to folder (#163597)

--- src/vs/workbench/contrib/search/common/constants.ts
+++ src/vs/workbench/contrib/search/common/constants.ts
@@ -57,6 +57,7 @@ export const FileMatchOrFolderMatchFocusKey = new RawContextKey<boolean>('fileMa
 export const FileMatchOrFolderMatchWithResourceFocusKey = new RawContextKey<boolean>('fileMatchOrFolderMatchWithResourceFocus', false); // Excludes "Other files"
 export const FileFocusKey = new RawContextKey<boolean>('fileMatchFocus', false);
 export const FolderFocusKey = new RawContextKey<boolean>('folderMatchFocus', false);
+export const ResourceFolderFocusKey = new RawContextKey<boolean>('folderMatchWithResourceFocus', false);
 export const MatchFocusKey = new RawContextKey<boolean>('matchFocus', false);
 export const ViewHasSearchPatternKey = new RawContextKey<boolean>('viewHasSearchPattern', false);
 export const ViewHasReplacePatternKey = new RawContextKey<boolean>('viewHasReplacePattern', false);
