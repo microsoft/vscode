2204a16fb0808665178abb34c0e9033621b46592
Sandeep Somavarapu <sasomava@microsoft.com>
2022-10-14 14:23:42 +0200

    Fix #163343

--- src/vs/platform/userDataSync/common/userDataSyncService.ts
+++ src/vs/platform/userDataSync/common/userDataSyncService.ts
@@ -418,6 +418,10 @@ export class UserDataSyncService extends Disposable implements IUserDataSyncServ
 				return isUndefined(result) ? null : result;
 			}
 
+			if (this.userDataProfilesService.isEnabled()) {
+				return null;
+			}
+
 			if (this.environmentService.isBuilt && (!this.productService.enableSyncingProfiles || isEqual(this.userDataSyncStoreManagementService.userDataSyncStore?.url, this.userDataSyncStoreManagementService.userDataSyncStore?.stableUrl))) {
 				return null;
 			}
@@ -543,7 +547,7 @@ class ProfileSynchronizer extends Disposable {
 		@ITelemetryService private readonly telemetryService: ITelemetryService,
 		@IUserDataSyncLogService private readonly logService: IUserDataSyncLogService,
 		@IProductService private readonly productService: IProductService,
-		@IUserDataProfilesService userDataProfilesService: IUserDataProfilesService,
+		@IUserDataProfilesService private readonly userDataProfilesService: IUserDataProfilesService,
 		@IConfigurationService private readonly configurationService: IConfigurationService,
 		@IEnvironmentService private readonly environmentService: IEnvironmentService,
 	) {
@@ -588,6 +592,9 @@ class ProfileSynchronizer extends Disposable {
 		}
 		if (syncResource === SyncResource.Profiles) {
 			if (!this._profile.isDefault) {
+				return;
+			}
+			if (!this.userDataProfilesService.isEnabled()) {
 				return;
 			}
 			if (this.environmentService.isBuilt && (!this.productService.enableSyncingProfiles || isEqual(this.userDataSyncStoreManagementService.userDataSyncStore?.url, this.userDataSyncStoreManagementService.userDataSyncStore?.stableUrl))) {
