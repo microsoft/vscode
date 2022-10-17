b987cb47f4a653839862849c8ded24a99cb48ad8
Connor Peet <connor@peet.io>
2022-10-17 13:49:50 -0700

    fix integrated cli check

--- cli/src/util/is_integrated.rs
+++ cli/src/util/is_integrated.rs
@@ -5,27 +5,25 @@
 
 use std::{env, io};
 
-use crate::constants::VSCODE_CLI_QUALITY;
-
+/// Gets whether the current CLI seems like it's running in integrated mode,
+/// by looking at the location of the exe and known VS Code files.
 pub fn is_integrated_cli() -> io::Result<bool> {
 	let exe = env::current_exe()?;
-	let parent = match exe.parent().and_then(|p| p.parent()) {
+
+	let parent = match exe.parent() {
+		Some(parent) if parent.file_name().and_then(|n| n.to_str()) == Some("bin") => parent,
+		_ => return Ok(false),
+	};
+
+	let parent = match parent.parent() {
 		Some(p) => p,
 		None => return Ok(false),
 	};
 
-	let expected_file = if cfg!(windows) {
-		match VSCODE_CLI_QUALITY {
-			Some("insider") => "Code - Insiders.exe",
-			Some("exploration") => "Code - Exploration.exe",
-			_ => "Code.exe",
-		}
+	let expected_file = if cfg!(target_os = "darwin") {
+		"node_modules.asar"
 	} else {
-		match VSCODE_CLI_QUALITY {
-			Some("insider") => "code-insiders",
-			Some("exploration") => "code-exploration",
-			_ => "code",
-		}
+		"resources.pak"
 	};
 
 	Ok(parent.join(expected_file).exists())
