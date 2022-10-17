fe1f6bf2f2ec3ef4f7bdd71dcdaf17755480ebe6
Sandeep Somavarapu <sasomava@microsoft.com>
2022-10-17 21:06:33 +0200

    add logging

--- src/vs/workbench/electron-sandbox/window.ts
+++ src/vs/workbench/electron-sandbox/window.ts
@@ -895,7 +895,7 @@ export class NativeWindow extends Disposable {
 		const diffMode = !!(request.filesToDiff && (request.filesToDiff.length === 2));
 		const mergeMode = !!(request.filesToMerge && (request.filesToMerge.length === 4));
 
-		const inputs = coalesce(await pathsToEditors(mergeMode ? request.filesToMerge : diffMode ? request.filesToDiff : request.filesToOpenOrCreate, this.fileService));
+		const inputs = coalesce(await pathsToEditors(mergeMode ? request.filesToMerge : diffMode ? request.filesToDiff : request.filesToOpenOrCreate, this.fileService, this.logService));
 		if (inputs.length) {
 			const openedEditorPanes = await this.openResources(inputs, diffMode, mergeMode);
 
