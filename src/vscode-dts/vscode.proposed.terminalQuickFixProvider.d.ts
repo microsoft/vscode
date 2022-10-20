55bac09e302d2066130aebd5a63013032810df5d
meganrogge <merogge@microsoft.com>
2022-10-20 09:25:47 -0400

    Revert "tweak"
    This reverts commit 189984333432da24412dc1adbec1c3a3f15fcaf1.

--- src/vscode-dts/vscode.proposed.terminalQuickFixProvider.d.ts
+++ src/vscode-dts/vscode.proposed.terminalQuickFixProvider.d.ts
@@ -12,7 +12,7 @@ declare module 'vscode' {
 	 * @param provider The provider that provides the quick fixes.
 	 * @return Disposable that unregisters the provider.
 	 */
-	export function registerTerminalQuickFixProvider(provider: TerminalQuickFixProvider): Disposable;
+	export function registerTerminalLinkProvider(provider: TerminalLinkProvider): Disposable;
 
 	/**
 	 * A provider for terminal quick fixes.
