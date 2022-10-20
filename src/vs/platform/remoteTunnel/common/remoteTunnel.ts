929c645c9a3727541c8fb30f4d62765f5c672ff9
Martin Aeschlimann <martinae@microsoft.com>
2022-10-20 09:37:17 +0200

    more tunnel access commands (#164076)
    more remote tunnel access commands

--- src/vs/platform/remoteTunnel/common/remoteTunnel.ts
+++ src/vs/platform/remoteTunnel/common/remoteTunnel.ts
@@ -17,7 +17,9 @@ export interface IRemoteTunnelService {
 	readonly _serviceBrand: undefined;
 
 	readonly onDidTokenFailed: Event<boolean>;
+
 	readonly onDidChangeTunnelStatus: Event<TunnelStatus>;
+	getTunnelStatus(): Promise<TunnelStatus>;
 
 	getAccount(): Promise<IRemoteTunnelAccount | undefined>;
 	readonly onDidChangeAccount: Event<IRemoteTunnelAccount | undefined>;
@@ -25,11 +27,31 @@ export interface IRemoteTunnelService {
 
 }
 
-export const enum TunnelStatus {
-	Uninitialized = 'uninitialized',
-	Disconnected = 'disconnected',
-	Connecting = 'connecting',
-	Connected = 'connected',
+export type TunnelStatus = TunnelStates.Connected | TunnelStates.Disconnected | TunnelStates.Connecting | TunnelStates.Uninitialized;
+
+export namespace TunnelStates {
+	export interface Uninitialized {
+		type: 'uninitialized';
+	}
+	export interface Connecting {
+		type: 'connecting';
+		progress?: string;
+	}
+
+	export interface Connected {
+		type: 'connected';
+		link: string;
+	}
+	export interface Disconnected {
+		type: 'disconnected';
+	}
+
+	export const disconnected: Disconnected = { type: 'disconnected' };
+	export const uninitialized: Uninitialized = { type: 'uninitialized' };
+	export const connected = (link: string): Connected => ({ type: 'connected', link });
+	export const connecting = (progress?: string): Connecting => ({ type: 'connecting', progress });
+
 }
 
-export const HOST_NAME_CONFIGURATION_KEY = 'remoteTunnel.hostNameOverride';
+export const CONFIGURATION_KEY_PREFIX = 'remote.tunnels.access';
+export const CONFIGURATION_KEY_HOST_NAME = CONFIGURATION_KEY_PREFIX + '.hostNameOverride';
