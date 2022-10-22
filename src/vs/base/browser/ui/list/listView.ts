417705a6739ba82a2b4ea95343af861c6f25691b
Matt Bierner <matb@microsoft.com>
2022-10-22 10:51:05 -0700

    Try to skip removing list elements from the dom when a list is being rapidly scrolled
    When rapidly scrolling a list, we always and up removing an element from the dom. This element is then inserted back into the dom on the next update. It would be better if we simply skipped removing the element from the dom in this case
    
    I've tried to fix this but there may be a better approach. This PR makes the following changes:
    
    - Switch the order of remove and insert in `ListView.render`. Now we always call remove before calling insert. This lets the removed row be reused within the same `render` frame
    
    - In the `rowCache`, introduce the concept of a transaction. This lets us defer actually removing nodes from the dom until the transaction is completed. In many cases (especially when scrolling rapidly), the row's dom node will actually be reused by `alloc` before this happens so we can skip the removal entirely

--- src/vs/base/browser/ui/list/listView.ts
+++ src/vs/base/browser/ui/list/listView.ts
@@ -759,17 +759,19 @@ export class ListView<T> implements ISpliceable<T>, IDisposable {
 			}
 		}
 
-		for (const range of rangesToInsert) {
-			for (let i = range.start; i < range.end; i++) {
-				this.insertItemInDOM(i, beforeElement);
+		this.cache.transact(() => {
+			for (const range of rangesToRemove) {
+				for (let i = range.start; i < range.end; i++) {
+					this.removeItemFromDOM(i);
+				}
 			}
-		}
 
-		for (const range of rangesToRemove) {
-			for (let i = range.start; i < range.end; i++) {
-				this.removeItemFromDOM(i);
+			for (const range of rangesToInsert) {
+				for (let i = range.start; i < range.end; i++) {
+					this.insertItemInDOM(i, beforeElement);
+				}
 			}
-		}
+		});
 
 		if (renderLeft !== undefined) {
 			this.rowsContainer.style.left = `-${renderLeft}px`;
@@ -790,8 +792,15 @@ export class ListView<T> implements ISpliceable<T>, IDisposable {
 	private insertItemInDOM(index: number, beforeElement: HTMLElement | null, row?: IRow): void {
 		const item = this.items[index];
 
+		let isStale = false;
 		if (!item.row) {
-			item.row = row ?? this.cache.alloc(item.templateId);
+			if (row) {
+				item.row = row;
+			} else {
+				const result = this.cache.alloc(item.templateId);
+				item.row = result.row;
+				isStale = result.isReusingConnectedDomNode;
+			}
 		}
 
 		const role = this.accessibilityProvider.getRole(item.element) || 'listitem';
@@ -807,7 +816,7 @@ export class ListView<T> implements ISpliceable<T>, IDisposable {
 			item.checkedDisposable = checked.onDidChange(update);
 		}
 
-		if (!item.row.domNode.parentElement) {
+		if (isStale || !item.row.domNode.parentElement) {
 			if (beforeElement) {
 				this.rowsContainer.insertBefore(item.row.domNode, beforeElement);
 			} else {
@@ -1373,7 +1382,7 @@ export class ListView<T> implements ISpliceable<T>, IDisposable {
 			return item.size - size;
 		}
 
-		const row = this.cache.alloc(item.templateId);
+		const { row } = this.cache.alloc(item.templateId);
 		row.domNode.style.height = '';
 		this.rowsContainer.appendChild(row.domNode);
 
