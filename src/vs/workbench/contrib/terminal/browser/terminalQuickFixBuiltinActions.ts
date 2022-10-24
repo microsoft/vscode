ecb3421521db4764becfd198a79ca88b6aad73c2
Megan Rogge <merogge@microsoft.com>
2022-10-24 19:53:30 -0400

    move builtin terminal quick fixes to contribution model (#164099)

--- src/vs/workbench/contrib/terminal/browser/terminalQuickFixBuiltinActions.ts
+++ src/vs/workbench/contrib/terminal/browser/terminalQuickFixBuiltinActions.ts
@@ -6,21 +6,21 @@
 import { localize } from 'vs/nls';
 import { TerminalQuickFixMatchResult, ITerminalQuickFixOptions, ITerminalInstance, TerminalQuickFixAction } from 'vs/workbench/contrib/terminal/browser/terminal';
 import { ITerminalCommand } from 'vs/workbench/contrib/terminal/common/terminal';
-import { URI } from 'vs/base/common/uri';
-
+import { IExtensionTerminalQuickFix } from 'vs/platform/terminal/common/terminal';
 export const GitCommandLineRegex = /git/;
 export const GitPushCommandLineRegex = /git\s+push/;
 export const GitTwoDashesRegex = /error: did you mean `--(.+)` \(with two dashes\)\?/;
 export const AnyCommandLineRegex = /.+/;
-export const GitSimilarOutputRegex = /(?:(most similar (command|commands) (is|are)))((\n\s*[^\s]+)+)/m;
+export const GitSimilarOutputRegex = /(?:(most similar (command|commands) (is|are)))((\n\s*(?<fixedCommand>[^\s]+))+)/m;
 export const FreePortOutputRegex = /address already in use (0\.0\.0\.0|127\.0\.0\.1|localhost|::):(?<portNumber>\d{4,5})|Unable to bind [^ ]*:(\d{4,5})|can't listen on port (\d{4,5})|listen EADDRINUSE [^ ]*:(\d{4,5})/;
-export const GitPushOutputRegex = /git push --set-upstream origin ([^\s]+)/;
+export const GitPushOutputRegex = /git push --set-upstream origin (?<branchName>[^\s]+)/;
 // The previous line starts with "Create a pull request for \'([^\s]+)\' on GitHub by visiting:\s*"
 // it's safe to assume it's a github pull request if the URL includes `/pull/`
-export const GitCreatePrOutputRegex = /remote:\s*(https:\/\/github\.com\/.+\/.+\/pull\/new\/.+)/;
+export const GitCreatePrOutputRegex = /remote:\s*(?<link>https:\/\/github\.com\/.+\/.+\/pull\/new\/.+)/;
 
-export function gitSimilarCommand(): ITerminalQuickFixOptions {
+export function gitSimilar(): ITerminalQuickFixOptions {
 	return {
+		source: 'builtin',
 		id: 'Git Similar',
 		commandLineMatcher: GitCommandLineRegex,
 		outputMatcher: {
@@ -50,8 +50,10 @@ export function gitSimilarCommand(): ITerminalQuickFixOptions {
 		}
 	};
 }
+
 export function gitTwoDashes(): ITerminalQuickFixOptions {
 	return {
+		source: 'builtin',
 		id: 'Git Two Dashes',
 		commandLineMatcher: GitCommandLineRegex,
 		outputMatcher: {
@@ -76,6 +78,7 @@ export function gitTwoDashes(): ITerminalQuickFixOptions {
 }
 export function freePort(terminalInstance?: Partial<ITerminalInstance>): ITerminalQuickFixOptions {
 	return {
+		source: 'builtin',
 		id: 'Free Port',
 		commandLineMatcher: AnyCommandLineRegex,
 		outputMatcher: {
@@ -97,12 +100,15 @@ export function freePort(terminalInstance?: Partial<ITerminalInstance>): ITermin
 				id: 'terminal.freePort',
 				label,
 				enabled: true,
-				run: async () => terminalInstance?.freePortKillProcess?.(port)
+				run: async () => {
+					await terminalInstance?.freePortKillProcess?.(port, command.command);
+				}
 			};
 		}
 	};
 }
-export function gitPushSetUpstream(): ITerminalQuickFixOptions {
+
+export function gitPushSetUpstream(): IExtensionTerminalQuickFix {
 	return {
 		id: 'Git Push Set Upstream',
 		commandLineMatcher: GitPushCommandLineRegex,
@@ -113,21 +119,12 @@ export function gitPushSetUpstream(): ITerminalQuickFixOptions {
 			length: 5
 		},
 		exitStatus: false,
-		getQuickFixes: (matchResult: TerminalQuickFixMatchResult, command: ITerminalCommand) => {
-			const branch = matchResult?.outputMatch?.[1];
-			if (!branch) {
-				return;
-			}
-			return {
-				type: 'command',
-				command: `git push --set-upstream origin ${branch}`,
-				addNewLine: true
-			};
-		}
+		commandToRun: 'git push --set-upstream origin ${group:branchName}',
+		extensionIdentifier: 'git'
 	};
 }
 
-export function gitCreatePr(): ITerminalQuickFixOptions {
+export function gitCreatePr(): IExtensionTerminalQuickFix {
 	return {
 		id: 'Git Create Pr',
 		commandLineMatcher: GitPushCommandLineRegex,
@@ -138,18 +135,7 @@ export function gitCreatePr(): ITerminalQuickFixOptions {
 			length: 5
 		},
 		exitStatus: true,
-		getQuickFixes: (matchResult: TerminalQuickFixMatchResult, command?: ITerminalCommand) => {
-			if (!command) {
-				return;
-			}
-			const link = matchResult?.outputMatch?.[1];
-			if (!link) {
-				return;
-			}
-			return {
-				type: 'opener',
-				uri: URI.parse(link)
-			};
-		}
+		linkToOpen: '${group:link}',
+		extensionIdentifier: 'git'
 	};
 }
--- src/vscode-dts/vscode.proposed.contribTerminalQuickFixes.d.ts
+++ src/vscode-dts/vscode.proposed.contribTerminalQuickFixes.d.ts
@@ -3,4 +3,4 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-// empty placeholder declaration for the `notebookPreload` contribution point
+// empty placeholder declaration for the `terminal quickFixes` contribution point
