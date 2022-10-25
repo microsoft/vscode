812c9bedf2b73c71cf74eba7640bf35ef55659d1
Joyce Er <joyce.er@microsoft.com>
2022-10-25 15:27:41 -0700

    Fix cloning with specific branch (#164619)

--- extensions/git/src/git.ts
+++ extensions/git/src/git.ts
@@ -457,7 +457,7 @@ export class Git {
 				command.push('--recursive');
 			}
 			if (options.ref) {
-				command.push('--branch', `'${options.ref}'`);
+				command.push('--branch', options.ref);
 			}
 			await this.exec(options.parentPath, command, {
 				cancellationToken,
