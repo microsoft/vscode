b7cb47c19d189b2c8cd5fe0664a0acb94b70bc47
TakutoOnikubo <onikubo-takuto386@g.ecc.u-tokyo.ac.jp>
2022-10-27 03:01:52 +0900

    debug

--- src/vs/editor/browser/editorExtensions.ts
+++ src/vs/editor/browser/editorExtensions.ts
@@ -25,6 +25,7 @@ import { IDisposable } from 'vs/base/common/lifecycle';
 import { KeyMod, KeyCode } from 'vs/base/common/keyCodes';
 import { ILogService } from 'vs/platform/log/common/log';
 
+const CLIPBOARD_CONTEXT_MENU_GROUP = '9_cutcopypaste';
 
 export type ServicesAccessor = InstantiationServicesAccessor;
 export type IEditorContributionCtor = IConstructorSignature<IEditorContribution, [ICodeEditor]>;
@@ -630,5 +631,15 @@ export const SelectAllCommand = registerCommand(new MultiCommand({
 		group: '',
 		title: nls.localize('selectAll', "Select All"),
 		order: 1
+	}, {
+		menuId: MenuId.EditorContext,
+		group: CLIPBOARD_CONTEXT_MENU_GROUP,
+		title: nls.localize('selectAll', "Select All"),
+		order: 3
+	}, {
+		menuId: MenuId.SimpleEditorContext.id,
+		group: CLIPBOARD_CONTEXT_MENU_GROUP,
+		title: nls.localize('selectAll', "Select All"),
+		order: 3
 	}]
 }));
