65f603b8a25a99269370c479fa7cee2da75dde6e
Johannes <johannes.rieken@gmail.com>
2022-11-03 17:27:47 +0100

    restrict this check to `_XYZService` instead

--- .eslintplugin/code-only-privates-use-underscore.ts
+++ .eslintplugin/code-only-privates-use-underscore.ts
@@ -8,17 +8,24 @@ import * as eslint from 'eslint';
 export = new class OnlyPrivatesUseUnderscorePrefix implements eslint.Rule.RuleModule {
 
 	create(context: eslint.Rule.RuleContext): eslint.Rule.RuleListener {
+
+		const propertyNamePattern = '/^_\\w+Service$/'
+
+		function reportNode(node: any) {
+			context.report({
+				node,
+				message: `Properties matching '${propertyNamePattern}' must be marked as private`
+			});
+		}
+
 		return {
-			['PropertyDefinition[key.name=/^_.*/]:not([accessibility="private"])']: (node: any) => {
-
-				if (String(node.key.name).endsWith('Brand')) {
-					return;
+			[`PropertyDefinition[key.name=${propertyNamePattern}]:not([accessibility="private"])`]: (node: any) => {
+				if (!String(node.key.name).endsWith('Brand')) {
+					return reportNode(node);
 				}
-
-				return context.report({
-					node: node.key,
-					message: 'Properties starting with an underscore should be marked as private'
-				});
+			},
+			[`TSParameterProperty[parameter.name=${propertyNamePattern}]:not([accessibility="private"])`]: (node: any) => {
+				return reportNode(node);
 			}
 		};
 	}
