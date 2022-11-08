aa412069763ae1ecf13e200f4b2fb437b45b6e4b
Matt Bierner <matb@microsoft.com>
2022-11-08 14:23:22 -0800

    Allow trailing commas while minifying json resources
    I noticed a few of our built-in extension's json resources were not being minified. The root cause appears to be trailing commas in the json

--- build/lib/extensions.ts
+++ build/lib/extensions.ts
@@ -39,7 +39,7 @@ function minifyExtensionResources(input: Stream): Stream {
 		.pipe(buffer())
 		.pipe(es.mapSync((f: File) => {
 			const errors: jsoncParser.ParseError[] = [];
-			const value = jsoncParser.parse(f.contents.toString('utf8'), errors);
+			const value = jsoncParser.parse(f.contents.toString('utf8'), errors, { allowTrailingComma: true });
 			if (errors.length === 0) {
 				// file parsed OK => just stringify to drop whitespace and comments
 				f.contents = Buffer.from(JSON.stringify(value));
