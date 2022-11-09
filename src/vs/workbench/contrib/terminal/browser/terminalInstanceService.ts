507ddda8cf43535c20c3fc1b91768b1f786055f3
Rob Lourens <roblourens@gmail.com>
2022-11-08 20:00:38 -0800

    feedback

--- src/vs/workbench/contrib/terminal/browser/terminalInstanceService.ts
+++ src/vs/workbench/contrib/terminal/browser/terminalInstanceService.ts
@@ -85,11 +85,13 @@ export class TerminalInstanceService extends Disposable implements ITerminalInst
 	}
 
 	async getBackend(remoteAuthority?: string): Promise<ITerminalBackend | undefined> {
-		if (remoteAuthority) {
+		let backend = Registry.as<ITerminalBackendRegistry>(TerminalExtensions.Backend).getTerminalBackend(remoteAuthority);
+		if (!backend) {
+			// Ensure all backends are initialized and try again
 			await this._lifecycleService.when(LifecyclePhase.Restored);
+			backend = Registry.as<ITerminalBackendRegistry>(TerminalExtensions.Backend).getTerminalBackend(remoteAuthority);
 		}
-
-		return Registry.as<ITerminalBackendRegistry>(TerminalExtensions.Backend).getTerminalBackend(remoteAuthority);
+		return backend;
 	}
 }
 
