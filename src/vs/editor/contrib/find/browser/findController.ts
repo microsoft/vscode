a5700e4d85236e99fc1bb56d6cd4f3579c1a055b
nikita-r <Nikita.Retyunskiy@outlook.com>
2022-11-09 22:48:14 -0500

    Try and fix #165977: make `editor.action.*SelectionMatchFind*` ignore the `editor.find.seedSearchStringFromSelection` setting

--- src/vs/editor/contrib/find/browser/findController.ts
+++ src/vs/editor/contrib/find/browser/findController.ts
@@ -739,18 +739,18 @@ export abstract class SelectionMatchFindAction extends EditorAction {
 			return;
 		}
 
-		const seedSearchStringFromNonEmptySelection = editor.getOption(EditorOption.find).seedSearchStringFromSelection === 'selection';
-		let selectionSearchString = null;
-		if (editor.getOption(EditorOption.find).seedSearchStringFromSelection !== 'never') {
-			selectionSearchString = getSelectionSearchString(editor, 'single', seedSearchStringFromNonEmptySelection);
-		}
+		const seedSearchStringFromSelection = 'single';
+		const seedSearchStringFromNonEmptySelection = false;
+
+		const selectionSearchString = getSelectionSearchString(editor, seedSearchStringFromSelection, seedSearchStringFromNonEmptySelection);
 		if (selectionSearchString) {
 			controller.setSearchString(selectionSearchString);
 		}
+
 		if (!this._run(controller)) {
 			await controller.start({
 				forceRevealReplace: false,
-				seedSearchStringFromSelection: editor.getOption(EditorOption.find).seedSearchStringFromSelection !== 'never' ? 'single' : 'none',
+				seedSearchStringFromSelection: seedSearchStringFromSelection,
 				seedSearchStringFromNonEmptySelection: seedSearchStringFromNonEmptySelection,
 				seedSearchStringFromGlobalClipboard: false,
 				shouldFocus: FindStartFocusAction.NoFocusChange,
