cbffea710e7a0a7428616180219ae75774d98fa0
deepak1556 <hop2deep@gmail.com>
2022-11-16 10:25:08 +0900

    chore: bump electron@22.0.0-beta.5

--- build/linux/dependencies-generator.ts
+++ build/linux/dependencies-generator.ts
@@ -23,15 +23,13 @@ import { isRpmArchString, RpmArchString } from './rpm/types';
 // are valid, are in dep-lists.ts
 const FAIL_BUILD_FOR_NEW_DEPENDENCIES: boolean = true;
 
-// Based on https://source.chromium.org/chromium/chromium/src/+/refs/tags/98.0.4758.109:chrome/installer/linux/BUILD.gn;l=64-80
+// Based on https://source.chromium.org/chromium/chromium/src/+/refs/tags/108.0.5359.40:chrome/installer/linux/BUILD.gn;l=64-80
 // and the Linux Archive build
 // Shared library dependencies that we already bundle.
 const bundledDeps = [
 	'libEGL.so',
 	'libGLESv2.so',
 	'libvulkan.so.1',
-	'swiftshader_libEGL.so',
-	'swiftshader_libGLESv2.so',
 	'libvk_swiftshader.so',
 	'libffmpeg.so'
 ];
