94c9bb6b3401bfbf011731d27f9cbac512680e99
weartist <weartistt@gmail.com>
2022-11-16 09:54:03 +0800

    💄

--- src/vs/editor/contrib/find/browser/findOptionsWidget.ts
+++ src/vs/editor/contrib/find/browser/findOptionsWidget.ts
@@ -14,7 +14,6 @@ import { FindReplaceState } from 'vs/editor/contrib/find/browser/findState';
 import { IKeybindingService } from 'vs/platform/keybinding/common/keybinding';
 import { inputActiveOptionBackground, inputActiveOptionBorder, inputActiveOptionForeground } from 'vs/platform/theme/common/colorRegistry';
 import { IColorTheme, IThemeService } from 'vs/platform/theme/common/themeService';
-import 'vs/css!./findOptionsWidget';
 
 export class FindOptionsWidget extends Widget implements IOverlayWidget {
 
