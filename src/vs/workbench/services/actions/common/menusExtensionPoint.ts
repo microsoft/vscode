6ce31d9e1cfec1a264116de9f24a0beb455b87ae
Alex Ross <alros@microsoft.com>
2022-11-17 12:13:33 +0100

    Remove split button from comments additional actions (#166568)
    and add a max number of 4 actions
    Part of #163281

--- src/vs/workbench/services/actions/common/menusExtensionPoint.ts
+++ src/vs/workbench/services/actions/common/menusExtensionPoint.ts
@@ -163,7 +163,7 @@ const apiMenus: IAPIMenu[] = [
 		key: 'comments/commentThread/additionalActions',
 		id: MenuId.CommentThreadAdditionalActions,
 		description: localize('commentThread.actions', "The contributed comment thread context menu, rendered as buttons below the comment editor"),
-		supportsSubmenus: true,
+		supportsSubmenus: false,
 		proposed: 'contribCommentThreadAdditionalMenu'
 	},
 	{
