0d2f5ea2ed624016007aa191e5b478d2fee93b0d
Daniel Martinez Olivas <danielmartinezolivas@gmail.com>
2022-11-18 00:49:54 -0600

    moves css rules for severityIcon

--- src/vs/platform/severityIcon/common/severityIcon.ts
+++ src/vs/platform/severityIcon/common/severityIcon.ts
@@ -5,8 +5,6 @@
 
 import { Codicon } from 'vs/base/common/codicons';
 import Severity from 'vs/base/common/severity';
-import { problemsErrorIconForeground, problemsInfoIconForeground, problemsWarningIconForeground } from 'vs/platform/theme/common/colorRegistry';
-import { registerThemingParticipant } from 'vs/platform/theme/common/themeService';
 
 export namespace SeverityIcon {
 
@@ -25,48 +23,3 @@ export namespace SeverityIcon {
 		}
 	}
 }
-
-registerThemingParticipant((theme, collector) => {
-
-	const errorIconForeground = theme.getColor(problemsErrorIconForeground);
-	if (errorIconForeground) {
-		const errorCodiconSelector = Codicon.error.cssSelector;
-		collector.addRule(`
-			.monaco-editor .zone-widget ${errorCodiconSelector},
-			.markers-panel .marker-icon${errorCodiconSelector},
-			.text-search-provider-messages .providerMessage ${errorCodiconSelector},
-			.extensions-viewlet > .extensions ${errorCodiconSelector} {
-				color: ${errorIconForeground};
-			}
-		`);
-	}
-
-	const warningIconForeground = theme.getColor(problemsWarningIconForeground);
-	if (warningIconForeground) {
-		const warningCodiconSelector = Codicon.warning.cssSelector;
-		collector.addRule(`
-			.monaco-editor .zone-widget ${warningCodiconSelector},
-			.markers-panel .marker-icon${warningCodiconSelector},
-			.extensions-viewlet > .extensions ${warningCodiconSelector},
-			.extension-editor ${warningCodiconSelector},
-			.text-search-provider-messages .providerMessage ${warningCodiconSelector},
-			.preferences-editor ${warningCodiconSelector} {
-				color: ${warningIconForeground};
-			}
-		`);
-	}
-
-	const infoIconForeground = theme.getColor(problemsInfoIconForeground);
-	if (infoIconForeground) {
-		const infoCodiconSelector = Codicon.info.cssSelector;
-		collector.addRule(`
-			.monaco-editor .zone-widget ${infoCodiconSelector},
-			.markers-panel .marker-icon${infoCodiconSelector},
-			.extensions-viewlet > .extensions ${infoCodiconSelector},
-			.text-search-provider-messages .providerMessage ${infoCodiconSelector},
-			.extension-editor ${infoCodiconSelector} {
-				color: ${infoIconForeground};
-			}
-		`);
-	}
-});
