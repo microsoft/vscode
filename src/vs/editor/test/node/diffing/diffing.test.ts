98d1aa3550b24f3fed0b23c52fc85dc6c2d86b70
Henning Dieterichs <hdieterichs@microsoft.com>
2022-11-21 14:54:48 +0100

    Improves diffing fixture test

--- src/vs/editor/test/node/diffing/diffing.test.ts
+++ src/vs/editor/test/node/diffing/diffing.test.ts
@@ -4,7 +4,7 @@
  *--------------------------------------------------------------------------------------------*/
 
 import * as assert from 'assert';
-import { readdirSync, readFileSync, existsSync, writeFileSync } from 'fs';
+import { readdirSync, readFileSync, existsSync, writeFileSync, rmSync } from 'fs';
 import { join, resolve } from 'path';
 import { FileAccess } from 'vs/base/common/network';
 import { SmartLinesDiffComputer } from 'vs/editor/common/diff/smartLinesDiffComputer';
@@ -17,67 +17,78 @@ suite('diff fixtures', () => {
 	const fixturesSrcDir = resolve(fixturesOutDir).replaceAll('\\', '/').replace('/out/vs/editor/', '/src/vs/editor/');
 	const folders = readdirSync(fixturesSrcDir);
 
+	function runTest(folder: string, diffingAlgoName: 'smart' | 'experimental') {
+		const folderPath = join(fixturesSrcDir, folder);
+		const files = readdirSync(folderPath);
+
+		const firstFileName = files.find(f => f.startsWith('1.'))!;
+		const secondFileName = files.find(f => f.startsWith('2.'))!;
+
+		const firstContentLines = readFileSync(join(folderPath, firstFileName), 'utf8').split(/\r\n|\r|\n/);
+		const secondContentLines = readFileSync(join(folderPath, secondFileName), 'utf8').split(/\r\n|\r|\n/);
+
+		const diffingAlgo = diffingAlgoName === 'smart' ? new SmartLinesDiffComputer() : new StandardLinesDiffComputer();
+
+		const diff = diffingAlgo.computeDiff(firstContentLines, secondContentLines, { ignoreTrimWhitespace: false, maxComputationTimeMs: Number.MAX_SAFE_INTEGER });
+
+		const actualDiffingResult: DiffingResult = {
+			originalFileName: `./${firstFileName}`,
+			modifiedFileName: `./${secondFileName}`,
+			diffs: diff.changes.map<IDetailedDiff>(c => ({
+				originalRange: c.originalRange.toString(),
+				modifiedRange: c.modifiedRange.toString(),
+				innerChanges: c.innerChanges?.map<IDiff>(c => ({
+					originalRange: c.originalRange.toString(),
+					modifiedRange: c.modifiedRange.toString(),
+				})) || null
+			}))
+		};
+
+		const expectedFilePath = join(folderPath, `${diffingAlgoName}.expected.diff.json`);
+		const invalidFilePath = join(folderPath, `${diffingAlgoName}.invalid.diff.json`);
+
+		const expectedFileContentFromActual = JSON.stringify(actualDiffingResult, null, '\t');
+
+		const invalidExists = existsSync(invalidFilePath);
+
+		if (!existsSync(expectedFilePath)) {
+			writeFileSync(expectedFilePath, expectedFileContentFromActual);
+			writeFileSync(invalidFilePath, '');
+			throw new Error('No expected file! Expected and invalid files were written. Delete the invalid file to make the test pass.');
+		} else {
+			const expectedFileContent = readFileSync(invalidExists ? invalidFilePath : expectedFilePath, 'utf8');
+			if (invalidExists && expectedFileContent === '') {
+				throw new Error('Delete the invalid file to make the test pass.');
+			}
+			const expectedFileDiffResult: DiffingResult = JSON.parse(expectedFileContent);
+
+			try {
+				assert.deepStrictEqual(actualDiffingResult, expectedFileDiffResult);
+			} catch (e) {
+				if (!invalidExists) {
+					writeFileSync(invalidFilePath, expectedFileContent);
+				}
+				writeFileSync(expectedFilePath, expectedFileContentFromActual);
+				throw e;
+			}
+		}
+
+		if (invalidExists) {
+			rmSync(invalidFilePath);
+		}
+	}
+
 	for (const folder of folders) {
-		for (const diffingAlgoName of ['smart', 'experimental']) {
+		for (const diffingAlgoName of ['smart', 'experimental'] as const) {
 			test(`${folder}-${diffingAlgoName}`, () => {
-				const folderPath = join(fixturesSrcDir, folder);
-				const files = readdirSync(folderPath);
-
-				const firstFileName = files.find(f => f.startsWith('1.'))!;
-				const secondFileName = files.find(f => f.startsWith('2.'))!;
-
-				const firstContentLines = readFileSync(join(folderPath, firstFileName), 'utf8').split(/\r\n|\r|\n/);
-				const secondContentLines = readFileSync(join(folderPath, secondFileName), 'utf8').split(/\r\n|\r|\n/);
-
-				const diffingAlgo = diffingAlgoName === 'smart' ? new SmartLinesDiffComputer() : new StandardLinesDiffComputer();
-
-				const diff = diffingAlgo.computeDiff(firstContentLines, secondContentLines, { ignoreTrimWhitespace: false, maxComputationTimeMs: Number.MAX_SAFE_INTEGER });
-
-				const actualDiffingResult: DiffingResult = {
-					originalFileName: `./${firstFileName}`,
-					modifiedFileName: `./${secondFileName}`,
-					diffs: diff.changes.map<IDetailedDiff>(c => ({
-						originalRange: c.originalRange.toString(),
-						modifiedRange: c.modifiedRange.toString(),
-						innerChanges: c.innerChanges?.map<IDiff>(c => ({
-							originalRange: c.originalRange.toString(),
-							modifiedRange: c.modifiedRange.toString(),
-						})) || null
-					}))
-				};
-
-				const expectedFilePath = join(folderPath, `${diffingAlgoName}.expected.diff.json`);
-				const invalidFilePath = join(folderPath, `${diffingAlgoName}.invalid.diff.json`);
-
-				const expectedFileContentFromActual = JSON.stringify(actualDiffingResult, null, '\t');
-
-				const invalidExists = existsSync(invalidFilePath);
-
-				if (!existsSync(expectedFilePath)) {
-					writeFileSync(expectedFilePath, expectedFileContentFromActual);
-					writeFileSync(invalidFilePath, '');
-					throw new Error('No expected file! Expected and invalid files were written. Delete the invalid file to make the test pass.');
-				} else {
-					const expectedFileContent = readFileSync(invalidExists ? invalidFilePath : expectedFilePath, 'utf8');
-					const expectedFileDiffResult: DiffingResult = JSON.parse(expectedFileContent);
-
-					try {
-						assert.deepStrictEqual(actualDiffingResult, expectedFileDiffResult);
-					} catch (e) {
-						if (!invalidExists) {
-							writeFileSync(invalidFilePath, expectedFileContent);
-						}
-						writeFileSync(expectedFilePath, expectedFileContentFromActual);
-						throw e;
-					}
-				}
-
-				if (invalidExists) {
-					throw new Error('Invalid file exists and agrees with expected file! Delete the invalid file to make the test pass.');
-				}
+				runTest(folder, diffingAlgoName);
 			});
 		}
 	}
+
+	test(`debug`, () => {
+		runTest('penalize-fragmentation', 'experimental');
+	});
 });
 
 interface DiffingResult {
