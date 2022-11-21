064612297dae2944c5516daf01f6b214e592a6c3
Henning Dieterichs <notify.henning.dieterichs@live.de>
2022-11-21 15:18:14 +0100

    Fixes diffing assertion failures (#166855)
    * Fixes diffing assertion failures
    
    * Improves diffing fixture test

--- src/vs/workbench/contrib/mergeEditor/browser/model/diffComputer.ts
+++ src/vs/workbench/contrib/mergeEditor/browser/model/diffComputer.ts
@@ -5,8 +5,6 @@
 
 import { assertFn, checkAdjacentItems } from 'vs/base/common/assert';
 import { IReader, observableFromEvent } from 'vs/base/common/observable';
-import { isDefined } from 'vs/base/common/types';
-import { Range } from 'vs/editor/common/core/range';
 import { LineRange as DiffLineRange, RangeMapping as DiffRangeMapping } from 'vs/editor/common/diff/linesDiffComputer';
 import { ITextModel } from 'vs/editor/common/model';
 import { IEditorWorkerService } from 'vs/editor/common/services/editorWorker';
@@ -60,17 +58,18 @@ export class MergeDiffComputer implements IMergeDiffComputer {
 				textModel1,
 				toLineRange(c.modifiedRange),
 				textModel2,
-				c.innerChanges?.map(ic => normalizeRangeMapping(toRangeMapping(ic), textModel1, textModel2)).filter(isDefined)
+				c.innerChanges?.map(ic => toRangeMapping(ic))
 			)
 		);
 
 		assertFn(() => {
-			return checkAdjacentItems(changes,
-				(m1, m2) => m2.inputRange.startLineNumber - m1.inputRange.endLineNumberExclusive === m2.outputRange.startLineNumber - m1.outputRange.endLineNumberExclusive &&
-					// There has to be an unchanged line in between (otherwise both diffs should have been joined)
-					m1.inputRange.endLineNumberExclusive < m2.inputRange.startLineNumber &&
-					m1.outputRange.endLineNumberExclusive < m2.outputRange.startLineNumber,
-			);
+			return changes[0].inputRange.startLineNumber === changes[0].outputRange.startLineNumber &&
+				checkAdjacentItems(changes,
+					(m1, m2) => m2.inputRange.startLineNumber - m1.inputRange.endLineNumberExclusive === m2.outputRange.startLineNumber - m1.outputRange.endLineNumberExclusive &&
+						// There has to be an unchanged line in between (otherwise both diffs should have been joined)
+						m1.inputRange.endLineNumberExclusive < m2.inputRange.startLineNumber &&
+						m1.outputRange.endLineNumberExclusive < m2.outputRange.startLineNumber,
+				);
 		});
 
 		return {
@@ -85,80 +84,4 @@ export function toLineRange(range: DiffLineRange): LineRange {
 
 export function toRangeMapping(mapping: DiffRangeMapping): RangeMapping {
 	return new RangeMapping(mapping.originalRange, mapping.modifiedRange);
-}
-
-export function normalizeRangeMapping(rangeMapping: RangeMapping, inputTextModel: ITextModel, outputTextModel: ITextModel): RangeMapping | undefined {
-	const inputRangeEmpty = rangeMapping.inputRange.isEmpty();
-	const outputRangeEmpty = rangeMapping.outputRange.isEmpty();
-
-	if (inputRangeEmpty && outputRangeEmpty) {
-		return undefined;
-	}
-
-	if (rangeMapping.inputRange.startLineNumber > inputTextModel.getLineCount()
-		|| rangeMapping.outputRange.startLineNumber > outputTextModel.getLineCount()) {
-		return rangeMapping;
-	}
-
-	const originalStartsAtEndOfLine = isAtEndOfLine(rangeMapping.inputRange.startLineNumber, rangeMapping.inputRange.startColumn, inputTextModel);
-	const modifiedStartsAtEndOfLine = isAtEndOfLine(rangeMapping.outputRange.startLineNumber, rangeMapping.outputRange.startColumn, outputTextModel);
-
-	if (!inputRangeEmpty && !outputRangeEmpty && originalStartsAtEndOfLine && modifiedStartsAtEndOfLine) {
-		// a b c [\n] x y z \n
-		// d e f [\n a] \n
-		// ->
-		// a b c \n [] x y z \n
-		// d e f \n [a] \n
-
-		return new RangeMapping(
-			rangeMapping.inputRange.setStartPosition(rangeMapping.inputRange.startLineNumber + 1, 1),
-
-			rangeMapping.outputRange.setStartPosition(rangeMapping.outputRange.startLineNumber + 1, 1),
-		);
-	}
-
-	if (
-		modifiedStartsAtEndOfLine &&
-		originalStartsAtEndOfLine &&
-		((inputRangeEmpty && rangeEndsAtEndOfLine(rangeMapping.outputRange, outputTextModel)) ||
-			(outputRangeEmpty && rangeEndsAtEndOfLine(rangeMapping.inputRange, inputTextModel)))
-	) {
-		// o: a b c [] \n x y z \n
-		// m: d e f [\n a] \n
-		// ->
-		// o: a b c \n [] x y z \n
-		// m: d e f \n [a \n]
-
-		// or
-
-		// a b c [\n x y z] \n
-		// d e f [] \n a \n
-		// ->
-		// a b c \n [x y z \n]
-		// d e f \n [] a \n
-
-		return new RangeMapping(
-			moveRange(rangeMapping.inputRange),
-			moveRange(rangeMapping.outputRange)
-		);
-	}
-
-	return rangeMapping;
-}
-
-function isAtEndOfLine(lineNumber: number, column: number, model: ITextModel): boolean {
-	return column >= model.getLineMaxColumn(lineNumber);
-}
-
-function rangeEndsAtEndOfLine(range: Range, model: ITextModel,): boolean {
-	return isAtEndOfLine(range.endLineNumber, range.endColumn, model);
-}
-
-function moveRange(range: Range): Range {
-	return new Range(
-		range.startLineNumber + 1,
-		1,
-		range.endLineNumber + 1,
-		1,
-	);
 }
