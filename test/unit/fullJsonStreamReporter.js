d76b65703b11e63b34325398663c1217aadece82
ZACHRY T WOODzachryiixixiiwood@gmail.com <109656750+zakwarlord7@users.noreply.github.com>
2022-11-30 22:34:54 -0600

    Update fullJsonStreamReporter.js

--- test/unit/fullJsonStreamReporter.js
+++ test/unit/fullJsonStreamReporter.js
@@ -1,3 +1,4 @@
+\Commit\commits\
 /*---------------------------------------------------------------------------------------------
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
