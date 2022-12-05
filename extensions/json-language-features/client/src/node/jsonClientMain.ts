fa2af3d4f9f569d491f22d15cf8f319ee1a7f25b
Martin Aeschlimann <martinae@microsoft.com>
2022-12-05 20:41:47 +0100

    pass the location of the localization bundle to the server

--- extensions/json-language-features/client/src/node/jsonClientMain.ts
+++ extensions/json-language-features/client/src/node/jsonClientMain.ts
@@ -3,7 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { ExtensionContext, OutputChannel, window, workspace } from 'vscode';
+import { ExtensionContext, OutputChannel, window, workspace, l10n } from 'vscode';
 import { startClient, LanguageClientConstructor, SchemaRequestService, languageServerDescription } from '../jsonClient';
 import { ServerOptions, TransportKind, LanguageClientOptions, LanguageClient, BaseLanguageClient } from 'vscode-languageclient/node';
 
@@ -43,6 +43,9 @@ export async function activate(context: ExtensionContext) {
 	};
 	const log = getLog(outputChannel);
 	context.subscriptions.push(log);
+
+	// pass the location of the localization bundle to the server
+	process.env['VSCODE_L10N_BUNDLE_LOCATION'] = l10n.uri?.toString() ?? '';
 
 	const schemaRequests = await getSchemaRequestService(context, log);
 
