1bb5bcc58e5ecdc9bf016cfbd471ba6dd160d825
rebornix <penn.lv@gmail.com>
2022-12-08 10:13:00 -0800

    Fix #161738. Assert index.

--- src/vs/workbench/contrib/notebook/common/services/notebookSimpleWorker.ts
+++ src/vs/workbench/contrib/notebook/common/services/notebookSimpleWorker.ts
@@ -123,16 +123,25 @@ class MirrorNotebookDocument {
 				const cell = this.cells[e.index];
 				cell.outputs = e.outputs;
 			} else if (e.kind === NotebookCellsChangeType.ChangeCellLanguage) {
+				this._assertIndex(e.index);
 				const cell = this.cells[e.index];
 				cell.language = e.language;
 			} else if (e.kind === NotebookCellsChangeType.ChangeCellMetadata) {
+				this._assertIndex(e.index);
 				const cell = this.cells[e.index];
 				cell.metadata = e.metadata;
 			} else if (e.kind === NotebookCellsChangeType.ChangeCellInternalMetadata) {
+				this._assertIndex(e.index);
 				const cell = this.cells[e.index];
 				cell.internalMetadata = e.internalMetadata;
 			}
 		});
+	}
+
+	private _assertIndex(index: number): void {
+		if (index < 0 || index >= this.cells.length) {
+			throw new Error(`Illegal index ${index}. Cells length: ${this.cells.length}`);
+		}
 	}
 
 	_spliceNotebookCells(splices: NotebookCellTextModelSplice<IMainCellDto>[]) {
