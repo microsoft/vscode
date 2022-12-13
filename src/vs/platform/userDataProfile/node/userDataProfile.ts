1c7c0f60c9f8de63dabbd08eaec750600977bb3d
Sandeep Somavarapu <sasomava@microsoft.com>
2022-12-13 14:23:16 +0100

    Fix #168560

--- src/vs/platform/userDataProfile/node/userDataProfile.ts
+++ src/vs/platform/userDataProfile/node/userDataProfile.ts
@@ -12,16 +12,34 @@ import { IStateService } from 'vs/platform/state/node/state';
 import { IUriIdentityService } from 'vs/platform/uriIdentity/common/uriIdentity';
 import { IUserDataProfilesService, UserDataProfilesService as BaseUserDataProfilesService, StoredUserDataProfile, StoredProfileAssociations } from 'vs/platform/userDataProfile/common/userDataProfile';
 
-export class UserDataProfilesService extends BaseUserDataProfilesService implements IUserDataProfilesService {
+export class ServerUserDataProfilesService extends BaseUserDataProfilesService implements IUserDataProfilesService {
 
 	constructor(
-		@IStateService private readonly stateService: IStateService,
 		@IUriIdentityService uriIdentityService: IUriIdentityService,
-		@INativeEnvironmentService private readonly nativeEnvironmentService: INativeEnvironmentService,
+		@INativeEnvironmentService protected readonly nativeEnvironmentService: INativeEnvironmentService,
 		@IFileService fileService: IFileService,
 		@ILogService logService: ILogService,
 	) {
 		super(nativeEnvironmentService, fileService, uriIdentityService, logService);
+	}
+
+	protected override getDefaultProfileExtensionsLocation(): URI {
+		return this.uriIdentityService.extUri.joinPath(URI.file(this.nativeEnvironmentService.extensionsPath).with({ scheme: this.profilesHome.scheme }), 'extensions.json');
+	}
+
+}
+
+
+export class UserDataProfilesService extends ServerUserDataProfilesService implements IUserDataProfilesService {
+
+	constructor(
+		@IStateService private readonly stateService: IStateService,
+		@IUriIdentityService uriIdentityService: IUriIdentityService,
+		@INativeEnvironmentService nativeEnvironmentService: INativeEnvironmentService,
+		@IFileService fileService: IFileService,
+		@ILogService logService: ILogService,
+	) {
+		super(uriIdentityService, nativeEnvironmentService, fileService, logService);
 	}
 
 	protected override getStoredProfiles(): StoredUserDataProfile[] {
