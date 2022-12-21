a25db44d1d2ae2839a4224a56af7fe178bcfbb7c
Daniel Imms <2193314+Tyriar@users.noreply.github.com>
2022-12-21 08:41:03 -0800

    Allow disallowing 'now' in fromNow and use in settings editor
    Fixes #93990

--- src/vs/workbench/contrib/preferences/browser/settingsEditor2.ts
+++ src/vs/workbench/contrib/preferences/browser/settingsEditor2.ts
@@ -1744,7 +1744,7 @@ class SyncControls extends Disposable {
 		const last = this.userDataSyncService.lastSyncTime;
 		let label: string;
 		if (typeof last === 'number') {
-			const d = fromNow(last, true);
+			const d = fromNow(last, true, undefined, true);
 			label = localize('lastSyncedLabel', "Last synced: {0}", d);
 		} else {
 			label = '';
