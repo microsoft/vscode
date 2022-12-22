50d9c1e58227ef92a84eda16d8524fa574ee6423
Logan Ramos <lramos15@gmail.com>
2022-12-22 16:16:14 -0500

    Debounce the onDidChangeEvent (#169884)

--- src/vs/platform/telemetry/common/telemetryUtils.ts
+++ src/vs/platform/telemetry/common/telemetryUtils.ts
@@ -6,6 +6,7 @@
 import { IDisposable } from 'vs/base/common/lifecycle';
 import { cloneAndChange, safeStringify } from 'vs/base/common/objects';
 import { isObject } from 'vs/base/common/types';
+import { Event } from 'vs/base/common/event';
 import { URI } from 'vs/base/common/uri';
 import { ConfigurationTarget, ConfigurationTargetToString, IConfigurationService } from 'vs/platform/configuration/common/configuration';
 import { IEnvironmentService } from 'vs/platform/environment/common/environment';
@@ -91,7 +92,13 @@ export interface URIDescriptor {
 }
 
 export function configurationTelemetry(telemetryService: ITelemetryService, configurationService: IConfigurationService): IDisposable {
-	return configurationService.onDidChangeConfiguration(event => {
+	// Debounce the event by 1000 ms and merge all affected keys into one event
+	const debouncedConfigService = Event.debounce(configurationService.onDidChangeConfiguration, (last, cur) => {
+		const newAffectedKeys: ReadonlySet<string> = last ? new Set([...last.affectedKeys, ...cur.affectedKeys]) : cur.affectedKeys;
+		return { ...cur, affectedKeys: newAffectedKeys };
+	}, 1000, true);
+
+	return debouncedConfigService(event => {
 		if (event.source !== ConfigurationTarget.DEFAULT) {
 			type UpdateConfigurationClassification = {
 				owner: 'lramos15, sbatten';
@@ -105,7 +112,7 @@ export function configurationTelemetry(telemetryService: ITelemetryService, conf
 			};
 			telemetryService.publicLog2<UpdateConfigurationEvent, UpdateConfigurationClassification>('updateConfiguration', {
 				configurationSource: ConfigurationTargetToString(event.source),
-				configurationKeys: flattenKeys(event.sourceConfig)
+				configurationKeys: flattenKeys(event.affectedKeys)
 			});
 		}
 	});
