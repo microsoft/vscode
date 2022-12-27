a4bf1010d805604fecd2944dd439e141bb271655
Alex Ross <alros@microsoft.com>
2022-12-27 17:44:55 +0100

    Remote explorer help and feedback section shows prompt (#170118)
    Fixes #150426

--- src/vs/workbench/contrib/remote/browser/remote.ts
+++ src/vs/workbench/contrib/remote/browser/remote.ts
@@ -53,6 +53,7 @@ import { ITimerService } from 'vs/workbench/services/timer/browser/timerService'
 import { getRemoteName } from 'vs/platform/remote/common/remoteHosts';
 import { IActionViewItem } from 'vs/base/browser/ui/actionbar/actionbar';
 import { connectionHealthToString } from 'vs/base/parts/ipc/common/ipc.net';
+import { getVirtualWorkspaceLocation } from 'vs/platform/workspace/common/virtualWorkspace';
 
 interface HelpInformation {
 	extensionDescription: IExtensionDescription;
@@ -61,6 +62,7 @@ interface HelpInformation {
 	feedback?: string;
 	issues?: string;
 	remoteName?: string[] | string;
+	virtualWorkspace?: string;
 }
 
 const remoteHelpExtPoint = ExtensionsRegistry.registerExtensionPoint<HelpInformation>({
@@ -162,7 +164,8 @@ class HelpModel {
 		quickInputService: IQuickInputService,
 		commandService: ICommandService,
 		remoteExplorerService: IRemoteExplorerService,
-		environmentService: IWorkbenchEnvironmentService
+		environmentService: IWorkbenchEnvironmentService,
+		workspaceContextService: IWorkspaceContextService
 	) {
 		const helpItems: IHelpItem[] = [];
 		const getStarted = viewModel.helpInformation.filter(info => info.getStarted);
@@ -174,12 +177,14 @@ class HelpModel {
 				getStarted.map((info: HelpInformation) => (new HelpItemValue(commandService,
 					info.extensionDescription,
 					(typeof info.remoteName === 'string') ? [info.remoteName] : info.remoteName,
+					info.virtualWorkspace,
 					info.getStarted!)
 				)),
 				quickInputService,
 				environmentService,
 				openerService,
-				remoteExplorerService
+				remoteExplorerService,
+				workspaceContextService
 			));
 		}
 
@@ -192,12 +197,14 @@ class HelpModel {
 				documentation.map((info: HelpInformation) => (new HelpItemValue(commandService,
 					info.extensionDescription,
 					(typeof info.remoteName === 'string') ? [info.remoteName] : info.remoteName,
+					info.virtualWorkspace,
 					info.documentation!)
 				)),
 				quickInputService,
 				environmentService,
 				openerService,
-				remoteExplorerService
+				remoteExplorerService,
+				workspaceContextService
 			));
 		}
 
@@ -210,12 +217,14 @@ class HelpModel {
 				feedback.map((info: HelpInformation) => (new HelpItemValue(commandService,
 					info.extensionDescription,
 					(typeof info.remoteName === 'string') ? [info.remoteName] : info.remoteName,
+					info.virtualWorkspace,
 					info.feedback!)
 				)),
 				quickInputService,
 				environmentService,
 				openerService,
-				remoteExplorerService
+				remoteExplorerService,
+				workspaceContextService
 			));
 		}
 
@@ -228,12 +237,14 @@ class HelpModel {
 				issues.map((info: HelpInformation) => (new HelpItemValue(commandService,
 					info.extensionDescription,
 					(typeof info.remoteName === 'string') ? [info.remoteName] : info.remoteName,
+					info.virtualWorkspace,
 					info.issues!)
 				)),
 				quickInputService,
 				environmentService,
 				openerService,
-				remoteExplorerService
+				remoteExplorerService,
+				workspaceContextService
 			));
 		}
 
@@ -243,12 +254,14 @@ class HelpModel {
 				nls.localize('remote.help.report', "Report Issue"),
 				viewModel.helpInformation.map(info => (new HelpItemValue(commandService,
 					info.extensionDescription,
-					(typeof info.remoteName === 'string') ? [info.remoteName] : info.remoteName
+					(typeof info.remoteName === 'string') ? [info.remoteName] : info.remoteName,
+					info.virtualWorkspace
 				))),
 				quickInputService,
 				environmentService,
 				commandService,
-				remoteExplorerService
+				remoteExplorerService,
+				workspaceContextService
 			));
 		}
 
@@ -260,7 +273,7 @@ class HelpModel {
 
 class HelpItemValue {
 	private _url: string | undefined;
-	constructor(private commandService: ICommandService, public extensionDescription: IExtensionDescription, public remoteAuthority: string[] | undefined, private urlOrCommand?: string) { }
+	constructor(private commandService: ICommandService, public extensionDescription: IExtensionDescription, public readonly remoteAuthority: string[] | undefined, public readonly virtualWorkspace: string | undefined, private urlOrCommand?: string) { }
 
 	get url(): Promise<string> {
 		return this.getUrl();
@@ -295,7 +308,8 @@ abstract class HelpItemBase implements IHelpItem {
 		public values: HelpItemValue[],
 		private quickInputService: IQuickInputService,
 		private environmentService: IWorkbenchEnvironmentService,
-		private remoteExplorerService: IRemoteExplorerService
+		private remoteExplorerService: IRemoteExplorerService,
+		private workspaceContextService: IWorkspaceContextService
 	) {
 		this.iconClasses.push(...ThemeIcon.asClassNameArray(icon));
 		this.iconClasses.push('remote-help-tree-node-item-icon');
@@ -315,6 +329,23 @@ abstract class HelpItemBase implements IHelpItem {
 								}
 							}
 						}
+					}
+				}
+			}
+		} else {
+			const virtualWorkspace = getVirtualWorkspaceLocation(this.workspaceContextService.getWorkspace())?.scheme;
+			if (virtualWorkspace) {
+				for (let i = 0; i < this.remoteExplorerService.targetType.length; i++) {
+					for (const value of this.values) {
+						if (value.virtualWorkspace && value.remoteAuthority) {
+							for (const authority of value.remoteAuthority) {
+								if (this.remoteExplorerService.targetType[i].startsWith(authority) && virtualWorkspace.startsWith(value.virtualWorkspace)) {
+									await this.takeAction(value.extensionDescription, await value.url);
+									return;
+								}
+							}
+						}
+
 					}
 				}
 			}
@@ -352,9 +383,10 @@ class HelpItem extends HelpItemBase {
 		quickInputService: IQuickInputService,
 		environmentService: IWorkbenchEnvironmentService,
 		private openerService: IOpenerService,
-		remoteExplorerService: IRemoteExplorerService
+		remoteExplorerService: IRemoteExplorerService,
+		workspaceContextService: IWorkspaceContextService
 	) {
-		super(icon, label, values, quickInputService, environmentService, remoteExplorerService);
+		super(icon, label, values, quickInputService, environmentService, remoteExplorerService, workspaceContextService);
 	}
 
 	protected async takeAction(extensionDescription: IExtensionDescription, url: string): Promise<void> {
@@ -370,9 +402,10 @@ class IssueReporterItem extends HelpItemBase {
 		quickInputService: IQuickInputService,
 		environmentService: IWorkbenchEnvironmentService,
 		private commandService: ICommandService,
-		remoteExplorerService: IRemoteExplorerService
+		remoteExplorerService: IRemoteExplorerService,
+		workspaceContextService: IWorkspaceContextService
 	) {
-		super(icon, label, values, quickInputService, environmentService, remoteExplorerService);
+		super(icon, label, values, quickInputService, environmentService, remoteExplorerService, workspaceContextService);
 	}
 
 	protected async takeAction(extensionDescription: IExtensionDescription): Promise<void> {
@@ -401,6 +434,7 @@ class HelpPanel extends ViewPane {
 		@IWorkbenchEnvironmentService protected readonly environmentService: IWorkbenchEnvironmentService,
 		@IThemeService themeService: IThemeService,
 		@ITelemetryService telemetryService: ITelemetryService,
+		@IWorkspaceContextService private readonly workspaceContextService: IWorkspaceContextService
 	) {
 		super(options, keybindingService, contextMenuService, configurationService, contextKeyService, viewDescriptorService, instantiationService, openerService, themeService, telemetryService);
 	}
@@ -429,7 +463,7 @@ class HelpPanel extends ViewPane {
 			}
 		);
 
-		const model = new HelpModel(this.viewModel, this.openerService, this.quickInputService, this.commandService, this.remoteExplorerService, this.environmentService);
+		const model = new HelpModel(this.viewModel, this.openerService, this.quickInputService, this.commandService, this.remoteExplorerService, this.environmentService, this.workspaceContextService);
 
 		this.tree.setInput(model);
 
@@ -511,7 +545,8 @@ class RemoteViewPaneContainer extends FilterViewPaneContainer implements IViewMo
 			documentation: extension.value.documentation,
 			feedback: extension.value.feedback,
 			issues: extension.value.issues,
-			remoteName: extension.value.remoteName
+			remoteName: extension.value.remoteName,
+			virtualWorkspace: extension.value.virtualWorkspace
 		});
 	}
 
