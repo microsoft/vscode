a5f605822175826fab4a493e0039b29feb5fc318
Daniel Imms <2193314+Tyriar@users.noreply.github.com>
2022-12-27 15:06:14 -0800

    Set FAIL_BUILD_FOR_NEW_DEPENDENCIES

--- build/linux/dependencies-generator.ts
+++ build/linux/dependencies-generator.ts
@@ -21,7 +21,7 @@ import { isRpmArchString, RpmArchString } from './rpm/types';
 // If true, we fail the build if there are new dependencies found during that task.
 // The reference dependencies, which one has to update when the new dependencies
 // are valid, are in dep-lists.ts
-const FAIL_BUILD_FOR_NEW_DEPENDENCIES: boolean = true;
+const FAIL_BUILD_FOR_NEW_DEPENDENCIES: boolean = false;
 
 // Based on https://source.chromium.org/chromium/chromium/src/+/refs/tags/98.0.4758.109:chrome/installer/linux/BUILD.gn;l=64-80
 // and the Linux Archive build
