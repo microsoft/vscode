cd02536e89759fcd6908426470a801574d94799a
meganrogge <merogge@microsoft.com>
2023-01-12 17:24:12 -0600

    Revert "fix #170849"
    This reverts commit a201125e552f85b644d7c2534e8d57fb73720df9.

--- src/vs/workbench/contrib/terminal/common/terminalExtensionPoints.ts
+++ src/vs/workbench/contrib/terminal/common/terminalExtensionPoints.ts
@@ -18,7 +18,7 @@ export interface ITerminalContributionService {
 	readonly _serviceBrand: undefined;
 
 	readonly terminalProfiles: ReadonlyArray<IExtensionTerminalProfile>;
-	readonly quickFixes: Promise<Array<ITerminalCommandSelector>>;
+	readonly quickFixes: Array<ITerminalCommandSelector>;
 }
 
 export const ITerminalContributionService = createDecorator<ITerminalContributionService>('terminalContributionsService');
@@ -29,18 +29,18 @@ export class TerminalContributionService implements ITerminalContributionService
 	private _terminalProfiles: ReadonlyArray<IExtensionTerminalProfile> = [];
 	get terminalProfiles() { return this._terminalProfiles; }
 
-	quickFixes: Promise<Array<ITerminalCommandSelector>>;
+	private _quickFixes: Array<ITerminalCommandSelector> = [];
+	get quickFixes() { return this._quickFixes; }
 
 	constructor() {
-		this.quickFixes = new Promise((r) => terminalsExtPoint.setHandler(contributions => {
+		terminalsExtPoint.setHandler(contributions => {
 			this._terminalProfiles = contributions.map(c => {
 				return c.value?.profiles?.filter(p => hasValidTerminalIcon(p)).map(e => {
 					return { ...e, extensionIdentifier: c.description.identifier.value };
 				}) || [];
 			}).flat();
-			const quickFixes = (contributions.filter(c => isProposedApiEnabled(c.description, 'terminalQuickFixProvider')).map(c => c.value.quickFixes ? c.value.quickFixes.map(fix => { return { ...fix, extensionIdentifier: c.description.identifier.value }; }) : [])).flat();
-			r(quickFixes);
-		}));
+			this._quickFixes = (contributions.filter(c => isProposedApiEnabled(c.description, 'terminalQuickFixProvider')).map(c => c.value.quickFixes ? c.value.quickFixes.map(fix => { return { ...fix, extensionIdentifier: c.description.identifier.value }; }) : [])).flat();
+		});
 	}
 }
 
