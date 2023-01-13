9a3f9a83e50f6885a3beba041bd37eb215b4a8cf
Derek Kniffin <derek.kniffin@gmail.com>
2023-01-12 23:51:20 -0500

    Add vscode.getEditorLayout command
    Fixes #94817

--- src/vs/workbench/browser/parts/editor/editorPart.ts
+++ src/vs/workbench/browser/parts/editor/editorPart.ts
@@ -10,7 +10,7 @@ import { Event, Emitter, Relay } from 'vs/base/common/event';
 import { contrastBorder, editorBackground } from 'vs/platform/theme/common/colorRegistry';
 import { GroupDirection, IAddGroupOptions, GroupsArrangement, GroupOrientation, IMergeGroupOptions, MergeGroupMode, GroupsOrder, GroupLocation, IFindGroupScope, EditorGroupLayout, GroupLayoutArgument, IEditorGroupsService, IEditorSideGroup } from 'vs/workbench/services/editor/common/editorGroupsService';
 import { IInstantiationService } from 'vs/platform/instantiation/common/instantiation';
-import { IView, orthogonal, LayoutPriority, IViewSize, Direction, SerializableGrid, Sizing, ISerializedGrid, Orientation, GridBranchNode, isGridBranchNode, GridNode, createSerializedGrid, Grid } from 'vs/base/browser/ui/grid/grid';
+import { IView, orthogonal, LayoutPriority, IViewSize, Direction, SerializableGrid, Sizing, ISerializedGrid, ISerializedNode, Orientation, GridBranchNode, isGridBranchNode, GridNode, createSerializedGrid, Grid } from 'vs/base/browser/ui/grid/grid';
 import { GroupIdentifier, EditorInputWithOptions, IEditorPartOptions, IEditorPartOptionsChangeEvent, GroupModelChangeKind } from 'vs/workbench/common/editor';
 import { EDITOR_GROUP_BORDER, EDITOR_PANE_BACKGROUND } from 'vs/workbench/common/theme';
 import { distinct, coalesce, firstOrDefault } from 'vs/base/common/arrays';
@@ -457,6 +457,28 @@ export class EditorPart extends Part implements IEditorGroupsService, IEditorGro
 		// Restore focus as needed
 		if (restoreFocus) {
 			this._activeGroup.focus();
+		}
+	}
+
+	getLayout(): EditorGroupLayout {
+		/* example return value:
+			{ orientation: 0, groups: [ { groups: [ { size: 0.4 }, { size: 0.6 } ], size: 0.5 }, { groups: [ {}, {} ], size: 0.5 } ] }
+		*/
+		const serializedGrid: ISerializedGrid = this.gridWidget.serialize()
+		const orientation: GroupOrientation = serializedGrid.orientation == Orientation.HORIZONTAL ? GroupOrientation.HORIZONTAL : GroupOrientation.VERTICAL
+		const root: GroupLayoutArgument = this.serializedNodeToGroupLayoutArgument(serializedGrid.root)
+
+		return {
+			orientation,
+			groups: root.groups as GroupLayoutArgument[]
+		}
+	}
+
+	private serializedNodeToGroupLayoutArgument(serializedNode: ISerializedNode): GroupLayoutArgument {
+		if (serializedNode.type == "branch") {
+			return { size: serializedNode.size, groups: serializedNode.data.map(node => this.serializedNodeToGroupLayoutArgument(node)) }
+		} else {
+			return { size: serializedNode.size }
 		}
 	}
 
