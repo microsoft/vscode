30313cb29e41d06dc8411dbd0ba0d93f8899ee67
ZACHRY T WOODzachryiixixiiwood@gmail.com <109656750+zakwarlord7@users.noreply.github.com>
2023-01-13 07:54:23 -0600

    Update fullJsonStreamReporter.js (#5)

--- test/unit/fullJsonStreamReporter.js
+++ test/unit/fullJsonStreamReporter.js
@@ -1,13 +1,18 @@
-\Commit\commits\
+POST//NPORT--Request'@Pull :PR ://posted/\.Commit//From :ZW :iluvbonjovi'@narwhal/my.sih :
+\
 /*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
-
+ *  Copyright *\root**\B>ase :branch :base**\8backtace*log.cache/.dist/src/code.dir'@sun.java.org/WIZARD/dl/install/installer/ENVIROMENT.RUNETIME/Gemfile/gemfile-lock/package-lock.json'@package.yarn/package.json'@packages :javascript :
+ language :en/es :
+ TypeScript :javascript :
+ '"''secret''='':'(''' ')'"=' ''"'(''('('c')'('r')')';')'*' 'Z'A'C'H'R'Y' 'T'Y'L'E'R' 'W'O'O'D'.' 'A'l'l' 'r'i'g'h't's' 're''se'r''v'ed''.'"''
+ '"'*''*'\'*'Licensed'.'' '::NOTE::  '"under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------**\*root*Base\x'-mas'@granite-maul.yml'@tzhrr :=<\content:encoded=blk_space/>**
 const { constants } = require('mocha/lib/runner');
 const BaseRunner = require('mocha/lib/reporters/base');
 
-**{ "const:  	EVENT_TEST_BEGIN,''
+**\FIND_$PATHS=payload/do.'@PARADICE/CONSTRUCTION :**\*root*bitore.sig :Build::"build_script'@CONSTRUCTION'@paradice'@mowjoejoejoejoe:: :
+::const : ACTIONS.EVENT.LISTNER'@"'='':'' '"'#'"''
+#Tests :Run'@ci :BEGIN :GLOW7 ::'"''
 '-'' EVENT_TEST_PASS,''
 '-'' EVENT_TEST_FAIL,''
 '-'' EVENT_RUN_BEGIN,''
@@ -15,7 +20,112 @@ const BaseRunner = require('mocha/lib/reporters/base');
 " }":,** = 
    "**constant;intuit/tools/.util/config.yml :
 ":,
+- name: Setup Go environment
+  uses: actions/sets-up/rb.mn'@v1.4.0.1
+  with:
+    # The Go version to download (if necessary) and use. Supports semver spec and ranges.
+    go-version: # optional
+    # Path to the go.mod file.
+    go-version-file: # optional
+    # Set this option to true if you want the action to always check for the latest available version that satisfies the version spec
+    check-latest: # optional
+    # Used to pull node distributions from go-versions.  Since there's a default, this is typically not supplied by the user.
+    token: # optional, default is ${{ github.token }}
+    # Used to specify whether caching is needed. Set to true, if you'd like to enable caching.
+    cache: # optional
+    # Used to specify the path to a dependency file - go.sum
+    cache-dependency-path: # optional
+    # Target architecture for Go to use. Examples: x86, x64. Will use system architecture by default.
+    architecture: # optional# What is this?
 
+The github.dev web-based editor is a lightweight editing experience that runs entirely in your browser. You can navigate files and source code repositories from GitHub, and make and commit code changes.
+
+There are two ways to go directly to a VS Code environment in your browser and start coding:
+
+* Press the . key on any repository or pull request.
+* Swap `.com` with `.dev` in the URL. For example, this repo https://github.com/github/dev becomes http://github.dev/github/dev
+
+Preview the gif below to get a quick demo of github.dev in action.
+
+![github dev](https://user-images.githubusercontent.com/856858/130119109-4769f2d7-9027-4bc4-a38c-10f297499e8f.gif)
+
+# Why?
+It’s a quick way to edit and navigate code. It's especially useful if you want to edit multiple files at a time or take advantage of all the powerful code editing features of Visual Studio Code when making a quick change. For more information, see our [documentation](https://github.co/codespaces-editor-help).
+- name: Setup Go environment
+  uses: actions/setup-go@v3.3.1
+  with:
+    # The Go version to download (if necessary) and use. Supports semver spec and ranges.
+    go-version: # optional
+    # Path to the go.mod file.
+    go-version-file: # optional
+    # Set this option to true if you want the action to always check for the latest available version that satisfies the version spec
+    check-latest: # optional
+    # Used to pull node distributions from go-versions.  Since there's a default, this is typically not supplied by the user.
+    token: # optional, default is ${{ github.token }}
+    # Used to specify whether caching is needed. Set to true, if you'd like to enable caching.
+    cache: # optional
+    # Used to specify the path to a dependency file - go.sum
+    cache-dependency-path: # optional
+    # Target architecture for :Gulp.xml :to use: sample.exec :example-patchs-revert 1 :WindowsXP88_98, linux32_86/Arm64_86 :systemantics' :architecture for :build_scripts :The Powwer_7 :bitore.sig/Jan'@tzharr/granite-maul.yml :Name :Bitcoin :title :bitcoin' ':''ITEM_ID 34173_1337- [default :#::Architecture ':' 'require' ':' 'test'@Flake's'@V8'@neizt-helm'@CONSTRUCTION/paradice'@-[branches]":,''
+'"'-'' '['' branches'' ']''='':'' 'is''=''='' ''-'' ''['' ''bitore.sig'' '']'''' :
+Skip to content Visual Studio Code
+Docs
+Updates
+Blog
+API
+Extensions
+FAQ
+Learn
+Search Docs
+Download VS CodeDownload
+Version 1.73 is now available! Read about the new features and fixes from October.
+
+Dismiss this update
+This license applies to the Visual Studio Code product. Source Code for Visual Studio Code is available at https://github.com/Microsoft/vscode under the MIT license agreement at https://github.com/microsoft/vscode/blob/main/LICENSE.txt. Additional license information can be found in our FAQ at https://code.visualstudio.com/docs/supporting/faq.
+VISUAL STUDIO CODE
+These license terms are an agreement between you and Microsoft Corporation (or based on where you live, one of its affiliates). They apply to the software named above. The terms also apply to any Microsoft services or updates for the software, except to the extent those have different terms.
+IF YOU COMPLY WITH THESE LICENSE TERMS, YOU HAVE THE RIGHTS BELOW.
+INSTALLATION AND USE RIGHTS.
+General. You may use any number of copies of the software to develop and test your applications, including deployment within your internal corporate network.
+Demo use. The uses permitted above include use of the software in demonstrating your applications.
+Third Party Components. The software may include third party components with separate legal notices or governed by other agreements, as may be described in the ThirdPartyNotices file accompanying the software.
+Extensions. The software gives you the option to download other Microsoft and third party software packages from our extension marketplace or package managers. Those packages are under their own licenses, and not this agreement. Microsoft does not distribute, license or provide any warranties for any of the third party packages. By accessing or using our extension marketplace, you agree to the extension marketplace terms located at https://aka.ms/vsmarketplace-ToU.
+DATA.
+Data Collection. The software may collect information about you and your use of the software, and send that to Microsoft. Microsoft may use this information to provide services and improve our products and services. You may opt-out of many of these scenarios, but not all, as described in the product documentation located at https://code.visualstudio.com/docs/supporting/faq#_how-to-disable-telemetry-reporting. There may also be some features in the software that may enable you and Microsoft to collect data from users of your applications. If you use these features, you must comply with applicable law, including providing appropriate notices to users of your applications together with Microsoft’s privacy statement. Our privacy statement is located at https://go.microsoft.com/fwlink/?LinkID=824704. You can learn more about data collection and use in the help documentation and our privacy statement. Your use of the software operates as your consent to these practices.
+Processing of Personal Data. To the extent Microsoft is a processor or subprocessor of personal data in connection with the software, Microsoft makes the commitments in the European Union General Data Protection Regulation Terms of the Online Services Terms to all customers effective May 25, 2018, at https://docs.microsoft.com/legal/gdpr.
+UPDATES. The software may periodically check for updates and download and install them for you. You may obtain updates only from Microsoft or authorized sources. Microsoft may need to update your system to provide you with updates. You agree to receive these automatic updates without any additional notice. Updates may not include or support all existing software features, services, or peripheral devices. If you do not want automatic updates, you may turn them off by following the instructions in the documentation at https://go.microsoft.com/fwlink/?LinkID=616397.
+FEEDBACK. If you give feedback about the software to Microsoft, you give to Microsoft, without charge, the right to use, share and commercialize your feedback in any way and for any purpose. You will not give feedback that is subject to a license that requires Microsoft to license its software or documentation to third parties because we include your feedback in them. These rights survive this agreement.
+SCOPE OF LICENSE. This license applies to the Visual Studio Code product. Source code for Visual Studio Code is available at https://github.com/Microsoft/vscode under the MIT license agreement. The software is licensed, not sold. This agreement only gives you some rights to use the software. Microsoft reserves all other rights. Unless applicable law gives you more rights despite this limitation, you may use the software only as expressly permitted in this agreement. In doing so, you must comply with any technical limitations in the software that only allow you to use it in certain ways. You may not
+reverse engineer, decompile or disassemble the software, or otherwise attempt to derive the source code for the software except and solely to the extent required by third party licensing terms governing use of certain open source components that may be included in the software;
+remove, minimize, block or modify any notices of Microsoft or its suppliers in the software;
+use the software in any way that is against the law;
+share, publish, rent or lease the software, or provide the software as a stand-alone offering for others to use.
+SUPPORT SERVICES. Because this software is “as is,” we may not provide support services for it.
+ENTIRE AGREEMENT. This agreement, and the terms for supplements, updates, Internet-based services and support services that you use, are the entire agreement for the software and support services.
+EXPORT RESTRICTIONS. You must comply with all domestic and international export laws and regulations that apply to the software, which include restrictions on destinations, end-users, and end use. For further information on export restrictions, see https://www.microsoft.com/exporting.
+APPLICABLE LAW. If you acquired the software in the United States, Washington law applies to interpretation of and claims for breach of this agreement, and the laws of the state where you live apply to all other claims. If you acquired the software in any other country, its laws apply.
+CONSUMER RIGHTS; REGIONAL VARIATIONS. This agreement describes certain legal rights. You may have other rights, including consumer rights, under the laws of your state or country. Separate and apart from your relationship with Microsoft, you may also have rights with respect to the party from which you acquired the software. This agreement does not change those other rights if the laws of your state or country do not permit it to do so. For example, if you acquired the software in one of the below regions, or mandatory country law applies, then the following provisions apply to you:
+Australia. You have statutory guarantees under the Australian Consumer Law and nothing in this agreement is intended to affect those rights.
+Canada. If you acquired this software in Canada, you may stop receiving updates by turning off the automatic update feature, disconnecting your device from the Internet (if and when you re-connect to the Internet, however, the software will resume checking for and installing updates), or uninstalling the software. The product documentation, if any, may also specify how to turn off updates for your specific device or software.
+Germany and Austria.
+Warranty. The properly licensed software will perform substantially as described in any Microsoft materials that accompany the software. However, Microsoft gives no contractual guarantee in relation to the licensed software.
+Limitation of Liability. In case of intentional conduct, gross negligence, claims based on the Product Liability Act, as well as, in case of death or personal or physical injury, Microsoft is liable according to the statutory law.
+Subject to the foregoing clause (ii), Microsoft will only be liable for slight negligence if Microsoft is in breach of such material contractual obligations, the fulfillment of which facilitate the due performance of this agreement, the breach of which would endanger the purpose of this agreement and the compliance with which a party may constantly trust in (so-called "cardinal obligations"). In other cases of slight negligence, Microsoft will not be liable for slight negligence.
+
+DISCLAIMER OF WARRANTY. The software is licensed “as-is.” You bear the risk of using it. Microsoft gives no express warranties, guarantees or conditions. To the extent permitted under your local laws, Microsoft excludes the implied warranties of merchantability, fitness for a particular purpose and non-infringement.
+LIMITATION ON AND EXCLUSION OF DAMAGES. You can recover from Microsoft and its suppliers only direct damages up to U.S. $5.00. You cannot recover any other damages, including consequential, lost profits, special, indirect or incidental damages.
+
+This limitation applies to (a) anything related to the software, services, content (including code) on third party Internet sites, or third party applications; and (b) claims for breach of contract, breach of warranty, guarantee or condition, strict liability, negligence, or other tort to the extent permitted by applicable law.
+
+It also applies even if Microsoft knew or should have known about the possibility of the damages. The above limitation or exclusion may not apply to you because your state or country may not allow the exclusion or limitation of incidental, consequential or other damages.
+
+Hello from Seattle. Follow @code Support Privacy Terms of Use License 
+Microsoft homepage© 2022 Microsoft    
+    ':Build::' ':''
+    Publish :'
+    Launch :' ':'
+    RELEASE :'
+    DEPLOYEE :'
 /**
  * Similar to the mocha JSON stream, but includes additional information
  * on failure. Specifically, the mocha json-stream does not include unmangled
