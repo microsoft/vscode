100dafcfa0b51831c25181aa20ccc423818034ef
Martin Aeschlimann <martinae@microsoft.com>
2023-01-16 17:35:12 +0100

    fix loading

--- extensions/html-language-features/server/src/node/htmlServerNodeMain.ts
+++ extensions/html-language-features/server/src/node/htmlServerNodeMain.ts
@@ -4,17 +4,28 @@
  *--------------------------------------------------------------------------------------------*/
 import * as l10n from '@vscode/l10n';
 import { URI } from 'vscode-uri';
+import { promises as fs } from 'fs';
 
 async function setupMain() {
+	const l10nLog: string[] = [];
+
 	const i10lLocation = process.env['VSCODE_L10N_BUNDLE_LOCATION'];
 	if (i10lLocation) {
-		const uri = URI.parse(i10lLocation);
-		if (uri.scheme === 'file') {
-			l10n.config({ fsPath: uri.fsPath });
-		} else {
-			await l10n.config({ uri: i10lLocation });
+		try {
+			const uri = URI.parse(i10lLocation);
+			if (uri.scheme === 'file') {
+				const contents = await (await fs.readFile(uri.fsPath)).toString();
+				const bundle = JSON.parse(contents).contents.bundle;
+				l10n.config({ contents: bundle });
+			} else {
+				await l10n.config({ uri: i10lLocation });
+			}
+			l10nLog.push(`l10n: Configured to ${i10lLocation.toString()}`);
+		} catch (e) {
+			l10nLog.push(`l10n: Problems loading ${i10lLocation.toString()} : ${e}`);
 		}
-		await import('./htmlServerMain');
 	}
+	await import('./htmlServerMain');
+	l10nLog.forEach(console.log);
 }
 setupMain();
