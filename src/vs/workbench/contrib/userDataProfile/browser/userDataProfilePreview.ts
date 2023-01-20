136e8fbb4ed9b004e476dfef913681f971cebc7d
Sandeep Somavarapu <sasomava@microsoft.com>
2023-01-20 16:46:44 +0100

    skip uninstalling built in extensions

--- src/vs/workbench/contrib/userDataProfile/browser/userDataProfilePreview.ts
+++ src/vs/workbench/contrib/userDataProfile/browser/userDataProfilePreview.ts
@@ -3,8 +3,10 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+import { getErrorMessage } from 'vs/base/common/errors';
 import { Disposable } from 'vs/base/common/lifecycle';
 import { URI } from 'vs/base/common/uri';
+import { ILogService } from 'vs/platform/log/common/log';
 import { IWorkbenchContribution } from 'vs/workbench/common/contributions';
 import { IBrowserWorkbenchEnvironmentService } from 'vs/workbench/services/environment/browser/environmentService';
 import { IUserDataProfileImportExportService } from 'vs/workbench/services/userDataProfile/common/userDataProfile';
@@ -13,11 +15,13 @@ export class UserDataProfilePreviewContribution extends Disposable implements IW
 
 	constructor(
 		@IBrowserWorkbenchEnvironmentService environmentService: IBrowserWorkbenchEnvironmentService,
-		@IUserDataProfileImportExportService userDataProfileImportExportService: IUserDataProfileImportExportService
+		@IUserDataProfileImportExportService userDataProfileImportExportService: IUserDataProfileImportExportService,
+		@ILogService logService: ILogService
 	) {
 		super();
 		if (environmentService.options?.profileToPreview) {
-			userDataProfileImportExportService.importProfile(URI.revive(environmentService.options.profileToPreview), { donotPrompt: true, previewAsTempProfile: true });
+			userDataProfileImportExportService.importProfile(URI.revive(environmentService.options.profileToPreview), { donotPrompt: true, previewAsTempProfile: true })
+				.then(null, error => logService.error('Error while previewing the profile', getErrorMessage(error)));
 		}
 	}
 
