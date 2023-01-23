e91229dc0c033691d8f4e573574165a6c5adbf7f
Johannes <johannes.rieken@gmail.com>
2023-01-23 15:50:59 +0100

    fix indentation

--- src/vscode-dts/vscode.d.ts
+++ src/vscode-dts/vscode.d.ts
@@ -16683,9 +16683,9 @@ declare module 'vscode' {
 	}
 
 	/**
- * A special value wrapper denoting a value that is safe to not clean.
- * This is to be used when you can guarantee no identifiable information is contained in the value and the cleaning is improperly redacting it.
- */
+	 * A special value wrapper denoting a value that is safe to not clean.
+	 * This is to be used when you can guarantee no identifiable information is contained in the value and the cleaning is improperly redacting it.
+	 */
 	export class TelemetryTrustedValue<T = any> {
 		readonly value: T;
 
