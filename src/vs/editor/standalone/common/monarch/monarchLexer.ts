6f7c400a4e12ce9da6cd672333c9259d3fe99f05
Dmitriy Pereverza <pereverzadmitriy@gmail.com>
2023-01-24 19:22:16 +0300

    Check nextEmbedded: '@pop' in action cases (issue#172061)

--- src/vs/editor/standalone/common/monarch/monarchLexer.ts
+++ src/vs/editor/standalone/common/monarch/monarchLexer.ts
@@ -518,7 +518,7 @@ export class MonarchTokenizer implements languages.ITokenizationSupport {
 		let hasEmbeddedPopRule = false;
 
 		for (const rule of rules) {
-			if (!monarchCommon.isIAction(rule.action) || rule.action.nextEmbedded !== '@pop') {
+			if (!monarchCommon.isIAction(rule.action) || !(rule.action.nextEmbedded === '@pop' || rule.action.hasEmbeddedEndInCases)) {
 				continue;
 			}
 			hasEmbeddedPopRule = true;
