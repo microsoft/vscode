38d8b9e0fb308eeff5b0637e162693efe4eb4b36
Ladislau Szomoru <3372902+lszomoru@users.noreply.github.com>
2023-01-30 21:03:33 +0100

    Git - remove gitlens-git from timeline provider

--- extensions/git/src/timelineProvider.ts
+++ extensions/git/src/timelineProvider.ts
@@ -254,7 +254,7 @@ export class GitTimelineProvider implements TimelineProvider {
 
 	private ensureProviderRegistration() {
 		if (this.providerDisposable === undefined) {
-			this.providerDisposable = workspace.registerTimelineProvider(['file', 'git', 'vscode-remote', 'gitlens-git', 'vscode-local-history'], this);
+			this.providerDisposable = workspace.registerTimelineProvider(['file', 'git', 'vscode-remote', 'vscode-local-history'], this);
 		}
 	}
 
