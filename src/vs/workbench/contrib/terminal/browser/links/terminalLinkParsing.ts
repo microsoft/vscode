8aa8f3fcabeff028a275a767d6acfdfa8e556f2d
Daniel Imms <2193314+Tyriar@users.noreply.github.com>
2023-01-30 15:14:33 -0800

    Helpers for detecting link suffix first

--- src/vs/workbench/contrib/terminal/browser/links/terminalLinkParsing.ts
+++ src/vs/workbench/contrib/terminal/browser/links/terminalLinkParsing.ts
@@ -5,10 +5,27 @@
 
 import { Lazy } from 'vs/base/common/lazy';
 
+export interface IParsedLink {
+	path: ILinkPartialRange;
+	prefix?: ILinkPartialRange;
+	suffix?: ILinkSuffix;
+}
+
+export interface ILinkSuffix {
+	row: number | undefined;
+	col: number | undefined;
+	suffix: ILinkPartialRange;
+}
+
+export interface ILinkPartialRange {
+	index: number;
+	text: string;
+}
+
 /**
  * A regex that extracts the link suffix which contains line and column information.
  */
-const linkSuffixRegex = new Lazy<RegExp>(() => {
+const linkSuffixRegexEol = new Lazy<RegExp>(() => {
 	let ri = 0;
 	let ci = 0;
 	function l(): string {
@@ -65,6 +82,56 @@ const linkSuffixRegex = new Lazy<RegExp>(() => {
 	return new RegExp(`(${suffixClause})`);
 });
 
+const linkSuffixRegex = new Lazy<RegExp>(() => {
+	let ri = 0;
+	let ci = 0;
+	function l(): string {
+		return `(?<row${ri++}>\\d+)`;
+	}
+	function c(): string {
+		return `(?<col${ci++}>\\d+)`;
+	}
+
+	const lineAndColumnRegexClauses = [
+		// foo:339
+		// foo:339:12
+		// foo 339
+		// foo 339:12                    [#140780]
+		// "foo",339
+		// "foo",339:12
+		`(?::| |['"],)${l()}(:${c()})?`,
+		// The quotes below are optional [#171652]
+		// foo, line 339                [#40468]
+		// foo, line 339, col 12
+		// foo, line 339, column 12
+		// "foo":line 339
+		// "foo":line 339, col 12
+		// "foo":line 339, column 12
+		// "foo": line 339
+		// "foo": line 339, col 12
+		// "foo": line 339, column 12
+		// "foo" on line 339
+		// "foo" on line 339, col 12
+		// "foo" on line 339, column 12
+		`['"]?(?:, |: ?| on )line ${l()}(, col(?:umn)? ${c()})?`,
+		// foo(339)
+		// foo(339,12)
+		// foo(339, 12)
+		// foo (339)
+		// foo (339,12)
+		// foo (339, 12)
+		` ?[\\[\\(]${l()}(?:, ?${c()})?[\\]\\)]`,
+	];
+
+	const suffixClause = lineAndColumnRegexClauses
+		// Join all clauses together
+		.join('|')
+		// Convert spaces to allow the non-breaking space char (ascii 160)
+		.replace(/ /g, `[${'\u00A0'} ]`);
+
+	return new RegExp(`(${suffixClause})`, 'g');
+});
+
 /**
  * Removes the optional link suffix which contains line and column information.
  * @param link The link to parse.
@@ -81,8 +148,8 @@ export function removeLinkSuffix(link: string): string {
  * Returns the optional link suffix which contains line and column information.
  * @param link The link to parse.
  */
-export function getLinkSuffix(link: string): { row: number | undefined; col: number | undefined; suffix: { index: number; text: string } } | null {
-	const matches = linkSuffixRegex.value.exec(link);
+export function getLinkSuffix(link: string): ILinkSuffix | null {
+	const matches = linkSuffixRegexEol.value.exec(link);
 	const groups = matches?.groups;
 	if (!groups || matches.length < 1) {
 		return null;
@@ -94,4 +161,84 @@ export function getLinkSuffix(link: string): { row: number | undefined; col: num
 		col: colString !== undefined ? parseInt(colString) : undefined,
 		suffix: { index: matches.index, text: matches[0] }
 	};
+}
+
+export function detectLinkSuffixes(line: string): ILinkSuffix[] {
+	// Find all suffixes on the line. Since the regex global flag is used, lastIndex will be updated
+	// in place such that there are no overlapping matches.
+	let match: RegExpExecArray | null;
+	const results: ILinkSuffix[] = [];
+	linkSuffixRegex.value.lastIndex = 0;
+	while ((match = linkSuffixRegex.value.exec(line)) !== null) {
+		const suffix = toLinkSuffix(match);
+		if (suffix === null) {
+			break;
+		}
+		results.push(suffix);
+	}
+	return results;
+}
+
+export function toLinkSuffix(match: RegExpExecArray | null): ILinkSuffix | null {
+	const groups = match?.groups;
+	if (!groups || match.length < 1) {
+		return null;
+	}
+	const rowString = groups.row0 || groups.row1 || groups.row2;
+	const colString = groups.col0 || groups.col1 || groups.col2;
+	return {
+		row: rowString !== undefined ? parseInt(rowString) : undefined,
+		col: colString !== undefined ? parseInt(colString) : undefined,
+		suffix: { index: match.index, text: match[0] }
+	};
+}
+
+// TODO: Handle ", ', [, ], etc.
+const linkWithSuffixPathCharacters = /(?<path>[^\s]+)$/;
+
+export function detectLinks(line: string) {
+	const results: IParsedLink[] = [];
+
+	// 1: Detect link suffixes on the line
+	const suffixes = detectLinkSuffixes(line);
+	for (const suffix of suffixes) {
+		const beforeSuffix = line.substring(0, suffix.suffix.index);
+		const possiblePathMatch = beforeSuffix.match(linkWithSuffixPathCharacters);
+		if (possiblePathMatch && possiblePathMatch.index !== undefined && possiblePathMatch.groups?.path) {
+			const linkStartIndex = possiblePathMatch.index;
+			let path = possiblePathMatch.groups.path;
+			// Extract a path prefix if it exists (not part of the path, but part of the underlined
+			// section)
+			let prefix: ILinkPartialRange | undefined = undefined;
+			const prefixMatch = path.match(/^(?<prefix>['"]+)/);
+			if (prefixMatch?.groups?.prefix) {
+				prefix = {
+					index: linkStartIndex,
+					text: prefixMatch?.groups?.prefix
+				};
+				path = path.substring(prefix.text.length);
+			}
+			results.push({
+				path: {
+					index: linkStartIndex + (prefix?.text.length || 0),
+					text: path
+				},
+				prefix,
+				suffix
+			});
+		}
+	}
+
+
+	// TODO: Annotate link prefix, here or in path resolve?
+
+	// Return only link suffixes if they were found
+	if (results.length > 0) {
+		return results;
+	}
+
+	// 2: Detect paths with no suffix
+	// TODO: ...
+
+	return results;
 }
--- src/vs/workbench/contrib/terminal/test/browser/links/terminalLinkParsing.test.ts
+++ src/vs/workbench/contrib/terminal/test/browser/links/terminalLinkParsing.test.ts
@@ -3,11 +3,12 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { deepStrictEqual } from 'assert';
-import { getLinkSuffix, removeLinkSuffix } from 'vs/workbench/contrib/terminal/browser/links/terminalLinkParsing';
+import { deepStrictEqual, ok, strictEqual } from 'assert';
+import { detectLinks, detectLinkSuffixes, getLinkSuffix, IParsedLink, removeLinkSuffix } from 'vs/workbench/contrib/terminal/browser/links/terminalLinkParsing';
 
 interface ITestLink {
 	link: string;
+	prefix: string | undefined;
 	suffix: string | undefined;
 	hasRow: boolean;
 	hasCol: boolean;
@@ -17,82 +18,82 @@ const testRow = 339;
 const testCol = 12;
 const testLinks: ITestLink[] = [
 	// Simple
-	{ link: 'foo', suffix: undefined, hasRow: false, hasCol: false },
-	{ link: 'foo:339', suffix: ':339', hasRow: true, hasCol: false },
-	{ link: 'foo:339:12', suffix: ':339:12', hasRow: true, hasCol: true },
-	{ link: 'foo 339', suffix: ' 339', hasRow: true, hasCol: false },
-	{ link: 'foo 339:12', suffix: ' 339:12', hasRow: true, hasCol: true },
+	{ link: 'foo', prefix: undefined, suffix: undefined, hasRow: false, hasCol: false },
+	{ link: 'foo:339', prefix: undefined, suffix: ':339', hasRow: true, hasCol: false },
+	{ link: 'foo:339:12', prefix: undefined, suffix: ':339:12', hasRow: true, hasCol: true },
+	{ link: 'foo 339', prefix: undefined, suffix: ' 339', hasRow: true, hasCol: false },
+	{ link: 'foo 339:12', prefix: undefined, suffix: ' 339:12', hasRow: true, hasCol: true },
 
 	// Double quotes
-	{ link: '"foo",339', suffix: '",339', hasRow: true, hasCol: false },
-	{ link: '"foo",339:12', suffix: '",339:12', hasRow: true, hasCol: true },
-	{ link: '"foo", line 339', suffix: '", line 339', hasRow: true, hasCol: false },
-	{ link: '"foo", line 339, col 12', suffix: '", line 339, col 12', hasRow: true, hasCol: true },
-	{ link: '"foo", line 339, column 12', suffix: '", line 339, column 12', hasRow: true, hasCol: true },
-	{ link: '"foo":line 339', suffix: '":line 339', hasRow: true, hasCol: false },
-	{ link: '"foo":line 339, col 12', suffix: '":line 339, col 12', hasRow: true, hasCol: true },
-	{ link: '"foo":line 339, column 12', suffix: '":line 339, column 12', hasRow: true, hasCol: true },
-	{ link: '"foo": line 339', suffix: '": line 339', hasRow: true, hasCol: false },
-	{ link: '"foo": line 339, col 12', suffix: '": line 339, col 12', hasRow: true, hasCol: true },
-	{ link: '"foo": line 339, column 12', suffix: '": line 339, column 12', hasRow: true, hasCol: true },
-	{ link: '"foo" on line 339', suffix: '" on line 339', hasRow: true, hasCol: false },
-	{ link: '"foo" on line 339, col 12', suffix: '" on line 339, col 12', hasRow: true, hasCol: true },
-	{ link: '"foo" on line 339, column 12', suffix: '" on line 339, column 12', hasRow: true, hasCol: true },
+	{ link: '"foo",339', prefix: '"', suffix: '",339', hasRow: true, hasCol: false },
+	{ link: '"foo",339:12', prefix: '"', suffix: '",339:12', hasRow: true, hasCol: true },
+	{ link: '"foo", line 339', prefix: '"', suffix: '", line 339', hasRow: true, hasCol: false },
+	{ link: '"foo", line 339, col 12', prefix: '"', suffix: '", line 339, col 12', hasRow: true, hasCol: true },
+	{ link: '"foo", line 339, column 12', prefix: '"', suffix: '", line 339, column 12', hasRow: true, hasCol: true },
+	{ link: '"foo":line 339', prefix: '"', suffix: '":line 339', hasRow: true, hasCol: false },
+	{ link: '"foo":line 339, col 12', prefix: '"', suffix: '":line 339, col 12', hasRow: true, hasCol: true },
+	{ link: '"foo":line 339, column 12', prefix: '"', suffix: '":line 339, column 12', hasRow: true, hasCol: true },
+	{ link: '"foo": line 339', prefix: '"', suffix: '": line 339', hasRow: true, hasCol: false },
+	{ link: '"foo": line 339, col 12', prefix: '"', suffix: '": line 339, col 12', hasRow: true, hasCol: true },
+	{ link: '"foo": line 339, column 12', prefix: '"', suffix: '": line 339, column 12', hasRow: true, hasCol: true },
+	{ link: '"foo" on line 339', prefix: '"', suffix: '" on line 339', hasRow: true, hasCol: false },
+	{ link: '"foo" on line 339, col 12', prefix: '"', suffix: '" on line 339, col 12', hasRow: true, hasCol: true },
+	{ link: '"foo" on line 339, column 12', prefix: '"', suffix: '" on line 339, column 12', hasRow: true, hasCol: true },
 
 	// Single quotes
-	{ link: '\'foo\',339', suffix: '\',339', hasRow: true, hasCol: false },
-	{ link: '\'foo\',339:12', suffix: '\',339:12', hasRow: true, hasCol: true },
-	{ link: '\'foo\', line 339', suffix: '\', line 339', hasRow: true, hasCol: false },
-	{ link: '\'foo\', line 339, col 12', suffix: '\', line 339, col 12', hasRow: true, hasCol: true },
-	{ link: '\'foo\', line 339, column 12', suffix: '\', line 339, column 12', hasRow: true, hasCol: true },
-	{ link: '\'foo\':line 339', suffix: '\':line 339', hasRow: true, hasCol: false },
-	{ link: '\'foo\':line 339, col 12', suffix: '\':line 339, col 12', hasRow: true, hasCol: true },
-	{ link: '\'foo\':line 339, column 12', suffix: '\':line 339, column 12', hasRow: true, hasCol: true },
-	{ link: '\'foo\': line 339', suffix: '\': line 339', hasRow: true, hasCol: false },
-	{ link: '\'foo\': line 339, col 12', suffix: '\': line 339, col 12', hasRow: true, hasCol: true },
-	{ link: '\'foo\': line 339, column 12', suffix: '\': line 339, column 12', hasRow: true, hasCol: true },
-	{ link: '\'foo\' on line 339', suffix: '\' on line 339', hasRow: true, hasCol: false },
-	{ link: '\'foo\' on line 339, col 12', suffix: '\' on line 339, col 12', hasRow: true, hasCol: true },
-	{ link: '\'foo\' on line 339, column 12', suffix: '\' on line 339, column 12', hasRow: true, hasCol: true },
+	{ link: '\'foo\',339', prefix: '\'', suffix: '\',339', hasRow: true, hasCol: false },
+	{ link: '\'foo\',339:12', prefix: '\'', suffix: '\',339:12', hasRow: true, hasCol: true },
+	{ link: '\'foo\', line 339', prefix: '\'', suffix: '\', line 339', hasRow: true, hasCol: false },
+	{ link: '\'foo\', line 339, col 12', prefix: '\'', suffix: '\', line 339, col 12', hasRow: true, hasCol: true },
+	{ link: '\'foo\', line 339, column 12', prefix: '\'', suffix: '\', line 339, column 12', hasRow: true, hasCol: true },
+	{ link: '\'foo\':line 339', prefix: '\'', suffix: '\':line 339', hasRow: true, hasCol: false },
+	{ link: '\'foo\':line 339, col 12', prefix: '\'', suffix: '\':line 339, col 12', hasRow: true, hasCol: true },
+	{ link: '\'foo\':line 339, column 12', prefix: '\'', suffix: '\':line 339, column 12', hasRow: true, hasCol: true },
+	{ link: '\'foo\': line 339', prefix: '\'', suffix: '\': line 339', hasRow: true, hasCol: false },
+	{ link: '\'foo\': line 339, col 12', prefix: '\'', suffix: '\': line 339, col 12', hasRow: true, hasCol: true },
+	{ link: '\'foo\': line 339, column 12', prefix: '\'', suffix: '\': line 339, column 12', hasRow: true, hasCol: true },
+	{ link: '\'foo\' on line 339', prefix: '\'', suffix: '\' on line 339', hasRow: true, hasCol: false },
+	{ link: '\'foo\' on line 339, col 12', prefix: '\'', suffix: '\' on line 339, col 12', hasRow: true, hasCol: true },
+	{ link: '\'foo\' on line 339, column 12', prefix: '\'', suffix: '\' on line 339, column 12', hasRow: true, hasCol: true },
 
 	// No quotes
-	{ link: 'foo, line 339', suffix: ', line 339', hasRow: true, hasCol: false },
-	{ link: 'foo, line 339, col 12', suffix: ', line 339, col 12', hasRow: true, hasCol: true },
-	{ link: 'foo, line 339, column 12', suffix: ', line 339, column 12', hasRow: true, hasCol: true },
-	{ link: 'foo:line 339', suffix: ':line 339', hasRow: true, hasCol: false },
-	{ link: 'foo:line 339, col 12', suffix: ':line 339, col 12', hasRow: true, hasCol: true },
-	{ link: 'foo:line 339, column 12', suffix: ':line 339, column 12', hasRow: true, hasCol: true },
-	{ link: 'foo: line 339', suffix: ': line 339', hasRow: true, hasCol: false },
-	{ link: 'foo: line 339, col 12', suffix: ': line 339, col 12', hasRow: true, hasCol: true },
-	{ link: 'foo: line 339, column 12', suffix: ': line 339, column 12', hasRow: true, hasCol: true },
-	{ link: 'foo on line 339', suffix: ' on line 339', hasRow: true, hasCol: false },
-	{ link: 'foo on line 339, col 12', suffix: ' on line 339, col 12', hasRow: true, hasCol: true },
-	{ link: 'foo on line 339, column 12', suffix: ' on line 339, column 12', hasRow: true, hasCol: true },
+	{ link: 'foo, line 339', prefix: undefined, suffix: ', line 339', hasRow: true, hasCol: false },
+	{ link: 'foo, line 339, col 12', prefix: undefined, suffix: ', line 339, col 12', hasRow: true, hasCol: true },
+	{ link: 'foo, line 339, column 12', prefix: undefined, suffix: ', line 339, column 12', hasRow: true, hasCol: true },
+	{ link: 'foo:line 339', prefix: undefined, suffix: ':line 339', hasRow: true, hasCol: false },
+	{ link: 'foo:line 339, col 12', prefix: undefined, suffix: ':line 339, col 12', hasRow: true, hasCol: true },
+	{ link: 'foo:line 339, column 12', prefix: undefined, suffix: ':line 339, column 12', hasRow: true, hasCol: true },
+	{ link: 'foo: line 339', prefix: undefined, suffix: ': line 339', hasRow: true, hasCol: false },
+	{ link: 'foo: line 339, col 12', prefix: undefined, suffix: ': line 339, col 12', hasRow: true, hasCol: true },
+	{ link: 'foo: line 339, column 12', prefix: undefined, suffix: ': line 339, column 12', hasRow: true, hasCol: true },
+	{ link: 'foo on line 339', prefix: undefined, suffix: ' on line 339', hasRow: true, hasCol: false },
+	{ link: 'foo on line 339, col 12', prefix: undefined, suffix: ' on line 339, col 12', hasRow: true, hasCol: true },
+	{ link: 'foo on line 339, column 12', prefix: undefined, suffix: ' on line 339, column 12', hasRow: true, hasCol: true },
 
 	// Parentheses
-	{ link: 'foo(339)', suffix: '(339)', hasRow: true, hasCol: false },
-	{ link: 'foo(339,12)', suffix: '(339,12)', hasRow: true, hasCol: true },
-	{ link: 'foo(339, 12)', suffix: '(339, 12)', hasRow: true, hasCol: true },
-	{ link: 'foo (339)', suffix: ' (339)', hasRow: true, hasCol: false },
-	{ link: 'foo (339,12)', suffix: ' (339,12)', hasRow: true, hasCol: true },
-	{ link: 'foo (339, 12)', suffix: ' (339, 12)', hasRow: true, hasCol: true },
+	{ link: 'foo(339)', prefix: undefined, suffix: '(339)', hasRow: true, hasCol: false },
+	{ link: 'foo(339,12)', prefix: undefined, suffix: '(339,12)', hasRow: true, hasCol: true },
+	{ link: 'foo(339, 12)', prefix: undefined, suffix: '(339, 12)', hasRow: true, hasCol: true },
+	{ link: 'foo (339)', prefix: undefined, suffix: ' (339)', hasRow: true, hasCol: false },
+	{ link: 'foo (339,12)', prefix: undefined, suffix: ' (339,12)', hasRow: true, hasCol: true },
+	{ link: 'foo (339, 12)', prefix: undefined, suffix: ' (339, 12)', hasRow: true, hasCol: true },
 
 	// Square brackets
-	{ link: 'foo[339]', suffix: '[339]', hasRow: true, hasCol: false },
-	{ link: 'foo[339,12]', suffix: '[339,12]', hasRow: true, hasCol: true },
-	{ link: 'foo[339, 12]', suffix: '[339, 12]', hasRow: true, hasCol: true },
-	{ link: 'foo [339]', suffix: ' [339]', hasRow: true, hasCol: false },
-	{ link: 'foo [339,12]', suffix: ' [339,12]', hasRow: true, hasCol: true },
-	{ link: 'foo [339, 12]', suffix: ' [339, 12]', hasRow: true, hasCol: true },
+	{ link: 'foo[339]', prefix: undefined, suffix: '[339]', hasRow: true, hasCol: false },
+	{ link: 'foo[339,12]', prefix: undefined, suffix: '[339,12]', hasRow: true, hasCol: true },
+	{ link: 'foo[339, 12]', prefix: undefined, suffix: '[339, 12]', hasRow: true, hasCol: true },
+	{ link: 'foo [339]', prefix: undefined, suffix: ' [339]', hasRow: true, hasCol: false },
+	{ link: 'foo [339,12]', prefix: undefined, suffix: ' [339,12]', hasRow: true, hasCol: true },
+	{ link: 'foo [339, 12]', prefix: undefined, suffix: ' [339, 12]', hasRow: true, hasCol: true },
 
 	// Non-breaking space
-	{ link: 'foo\u00A0339:12', suffix: '\u00A0339:12', hasRow: true, hasCol: true },
-	{ link: '"foo" on line 339,\u00A0column 12', suffix: '" on line 339,\u00A0column 12', hasRow: true, hasCol: true },
-	{ link: '\'foo\' on line\u00A0339, column 12', suffix: '\' on line\u00A0339, column 12', hasRow: true, hasCol: true },
-	{ link: 'foo (339,\u00A012)', suffix: ' (339,\u00A012)', hasRow: true, hasCol: true },
-	{ link: 'foo\u00A0[339, 12]', suffix: '\u00A0[339, 12]', hasRow: true, hasCol: true },
-
+	{ link: 'foo\u00A0339:12', prefix: undefined, suffix: '\u00A0339:12', hasRow: true, hasCol: true },
+	{ link: '"foo" on line 339,\u00A0column 12', prefix: '"', suffix: '" on line 339,\u00A0column 12', hasRow: true, hasCol: true },
+	{ link: '\'foo\' on line\u00A0339, column 12', prefix: '\'', suffix: '\' on line\u00A0339, column 12', hasRow: true, hasCol: true },
+	{ link: 'foo (339,\u00A012)', prefix: undefined, suffix: ' (339,\u00A012)', hasRow: true, hasCol: true },
+	{ link: 'foo\u00A0[339, 12]', prefix: undefined, suffix: '\u00A0[339, 12]', hasRow: true, hasCol: true },
 ];
+const testLinksWithSuffix = testLinks.filter(e => !!e.suffix);
 
 suite('TerminalLinkParsing', () => {
 	suite('removeLinkSuffix', () => {
@@ -121,5 +122,210 @@ suite('TerminalLinkParsing', () => {
 				);
 			});
 		}
+	});
+	suite.only('detectLinkSuffixes', () => {
+		for (const testLink of testLinks) {
+			test('`' + testLink.link + '`', () => {
+				deepStrictEqual(
+					detectLinkSuffixes(testLink.link),
+					testLink.suffix === undefined ? [] : [{
+						row: testLink.hasRow ? testRow : undefined,
+						col: testLink.hasCol ? testCol : undefined,
+						suffix: {
+							index: testLink.link.length - testLink.suffix.length,
+							text: testLink.suffix
+						}
+					} as ReturnType<typeof getLinkSuffix>]
+				);
+			});
+		}
+
+		test('foo(1, 2) bar[3, 4] baz on line 5', () => {
+			deepStrictEqual(
+				detectLinkSuffixes('foo(1, 2) bar[3, 4] baz on line 5'),
+				[
+					{
+						col: 2,
+						row: 1,
+						suffix: {
+							index: 3,
+							text: '(1, 2)'
+						}
+					},
+					{
+						col: 4,
+						row: 3,
+						suffix: {
+							index: 13,
+							text: '[3, 4]'
+						}
+					},
+					{
+						col: undefined,
+						row: 5,
+						suffix: {
+							index: 23,
+							text: ' on line 5'
+						}
+					}
+				]
+			);
+		});
+	});
+	suite.only('detectLinks', () => {
+		test('foo(1, 2) bar[3, 4] "baz" on line 5', () => {
+			deepStrictEqual(
+				detectLinks('foo(1, 2) bar[3, 4] "baz" on line 5'),
+				[
+					{
+						prefix: undefined,
+						path: {
+							index: 0,
+							text: 'foo'
+						},
+						suffix: {
+							col: 2,
+							row: 1,
+							suffix: {
+								index: 3,
+								text: '(1, 2)'
+							}
+						}
+					},
+					{
+						prefix: undefined,
+						path: {
+							index: 10,
+							text: 'bar'
+						},
+						suffix: {
+							col: 4,
+							row: 3,
+							suffix: {
+								index: 13,
+								text: '[3, 4]'
+							}
+						}
+					},
+					{
+						prefix: {
+							index: 20,
+							text: '"'
+						},
+						path: {
+							index: 21,
+							text: 'baz'
+						},
+						suffix: {
+							col: undefined,
+							row: 5,
+							suffix: {
+								index: 24,
+								text: '" on line 5'
+							}
+						}
+					}
+				]
+			);
+		});
+
+		test('should extract the link prefix', () => {
+			deepStrictEqual(
+				detectLinks('"foo", line 5, col 6'),
+				[
+					{
+						prefix: {
+							index: 0,
+							text: '"',
+						},
+						path: {
+							index: 1,
+							text: 'foo'
+						},
+						suffix: {
+							row: 5,
+							col: 6,
+							suffix: {
+								index: 4,
+								text: '", line 5, col 6'
+							}
+						}
+					} as IParsedLink,
+				]
+			);
+		});
+
+		suite('should detect 3 suffix links on single line', () => {
+			for (let i = 0; i < testLinksWithSuffix.length - 2; i++) {
+				const link1 = testLinksWithSuffix[i];
+				const link2 = testLinksWithSuffix[i + 1];
+				const link3 = testLinksWithSuffix[i + 2];
+				const line = ` ${link1.link} ${link2.link} ${link3.link} `;
+				test('`' + line + '`', () => {
+					strictEqual(detectLinks(line).length, 3);
+					ok(link1.suffix);
+					ok(link2.suffix);
+					ok(link3.suffix);
+					const detectedLink1: IParsedLink = {
+						prefix: link1.prefix ? {
+							index: 1,
+							text: link1.prefix
+						} : undefined,
+						path: {
+							index: 1 + (link1.prefix?.length ?? 0),
+							text: link1.link.replace(link1.suffix, '').replace(link1.prefix || '', '')
+						},
+						suffix: {
+							row: link1.hasRow ? testRow : undefined,
+							col: link1.hasCol ? testCol : undefined,
+							suffix: {
+								index: 1 + (link1.link.length - link1.suffix.length),
+								text: link1.suffix
+							}
+						}
+					};
+					const detectedLink2: IParsedLink = {
+						prefix: link2.prefix ? {
+							index: (detectedLink1.prefix?.index ?? detectedLink1.path.index) + link1.link.length + 1,
+							text: link2.prefix
+						} : undefined,
+						path: {
+							index: (detectedLink1.prefix?.index ?? detectedLink1.path.index) + link1.link.length + 1 + (link2.prefix ?? '').length,
+							text: link2.link.replace(link2.suffix, '').replace(link2.prefix ?? '', '')
+						},
+						suffix: {
+							row: link2.hasRow ? testRow : undefined,
+							col: link2.hasCol ? testCol : undefined,
+							suffix: {
+								index: (detectedLink1.prefix?.index ?? detectedLink1.path.index) + link1.link.length + 1 + (link2.link.length - link2.suffix.length),
+								text: link2.suffix
+							}
+						}
+					};
+					const detectedLink3: IParsedLink = {
+						prefix: link3.prefix ? {
+							index: (detectedLink2.prefix?.index ?? detectedLink2.path.index) + link2.link.length + 1,
+							text: link3.prefix
+						} : undefined,
+						path: {
+							index: (detectedLink2.prefix?.index ?? detectedLink2.path.index) + link2.link.length + 1 + (link3.prefix ?? '').length,
+							text: link3.link.replace(link3.suffix, '').replace(link3.prefix ?? '', '')
+						},
+						suffix: {
+							row: link3.hasRow ? testRow : undefined,
+							col: link3.hasCol ? testCol : undefined,
+							suffix: {
+								index: (detectedLink2.prefix?.index ?? detectedLink2.path.index) + link2.link.length + 1 + (link3.link.length - link3.suffix.length),
+								text: link3.suffix
+							}
+						}
+					};
+					deepStrictEqual(
+						detectLinks(line),
+						[detectedLink1, detectedLink2, detectedLink3]
+					);
+				});
+			}
+		});
 	});
 });
