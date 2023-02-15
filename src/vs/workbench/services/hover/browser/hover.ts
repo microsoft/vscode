3a877e7d4932b0bb9c83e8568970bfe04abf1f4b
Daniel Imms <2193314+Tyriar@users.noreply.github.com>
2023-02-15 07:55:43 -0800

    Allow string in IHoverOptions.id

--- src/vs/workbench/services/hover/browser/hover.ts
+++ src/vs/workbench/services/hover/browser/hover.ts
@@ -63,7 +63,7 @@ export interface IHoverOptions {
 	 * {@link IHoverService.showHover} the options object itself is used to determine if the hover
 	 * is the same one that is already showing, when this is set, the ID will be used instead.
 	 */
-	id?: number;
+	id?: number | string;
 
 	/**
 	 * A set of actions for the hover's "status bar".
