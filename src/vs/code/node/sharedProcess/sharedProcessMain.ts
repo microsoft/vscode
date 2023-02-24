e1a7fed60fe2495cd86ab600e334290f0a82c83e
Benjamin Pasero <benjamin.pasero@microsoft.com>
2023-02-24 14:30:50 +0100

    move `remoteTunnelService`

--- src/vs/code/node/sharedProcess/sharedProcessMain.ts
+++ src/vs/code/node/sharedProcess/sharedProcessMain.ts
@@ -104,6 +104,7 @@ import { LogService } from 'vs/platform/log/common/logService';
 import { ipcUtilityProcessWorkerChannelName, IUtilityProcessWorkerConfiguration } from 'vs/platform/utilityProcess/common/utilityProcessWorkerService';
 import { isUtilityProcess } from 'vs/base/parts/sandbox/node/electronTypes';
 import { ISharedProcessLifecycleService, SharedProcessLifecycleService } from 'vs/platform/lifecycle/node/sharedProcessLifecycleService';
+import { RemoteTunnelService } from 'vs/platform/remoteTunnel/node/remoteTunnelService';
 
 /* eslint-disable local/code-layering, local/code-import-patterns */
 // TODO@bpasero layer is not allowed in utility process
@@ -119,7 +120,6 @@ import { UserDataAutoSyncService } from 'vs/platform/userDataSync/electron-sandb
 import { UserDataProfileStorageService } from 'vs/platform/userDataProfile/electron-sandbox/userDataProfileStorageService';
 import { SharedProcessWorkerService } from 'vs/platform/sharedProcess/electron-browser/sharedProcessWorkerService';
 import { SharedProcessRequestService } from 'vs/platform/request/electron-browser/sharedProcessRequestService';
-import { RemoteTunnelService } from 'vs/platform/remoteTunnel/electron-browser/remoteTunnelService';
 import { ExtensionsProfileScannerService } from 'vs/platform/extensionManagement/electron-sandbox/extensionsProfileScannerService';
 
 class SharedProcessMain extends Disposable {
