3c6127d25ba74ce8a42828a3acb4855dc9d3158e
Alex Dima <alexdima@microsoft.com>
2023-02-25 16:21:05 +0100

    Extract `ExtensionsProposedApi` to a separate file

--- src/vs/workbench/services/extensions/common/abstractExtensionService.ts
+++ src/vs/workbench/services/extensions/common/abstractExtensionService.ts
@@ -3,7 +3,6 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { isNonEmptyArray } from 'vs/base/common/arrays';
 import { Barrier } from 'vs/base/common/async';
 import { Emitter, Event } from 'vs/base/common/event';
 import { Disposable, IDisposable, toDisposable } from 'vs/base/common/lifecycle';
@@ -32,7 +31,7 @@ import { ExtensionHostKind, ExtensionRunningPreference, determineExtensionHostKi
 import { IExtensionHostManager, createExtensionHostManager } from 'vs/workbench/services/extensions/common/extensionHostManager';
 import { IExtensionManifestPropertiesService } from 'vs/workbench/services/extensions/common/extensionManifestPropertiesService';
 import { ActivationKind, ActivationTimes, ExtensionActivationReason, ExtensionPointContribution, ExtensionRunningLocation, IExtensionHost, IExtensionService, IExtensionsStatus, IInternalExtensionService, IMessage, IResponsiveStateChangeEvent, IWillActivateEvent, LocalProcessRunningLocation, LocalWebWorkerRunningLocation, RemoteRunningLocation, toExtension } from 'vs/workbench/services/extensions/common/extensions';
-import { ApiProposalName, allApiProposals } from 'vs/workbench/services/extensions/common/extensionsApiProposals';
+import { ExtensionsProposedApi } from 'vs/workbench/services/extensions/common/extensionsProposedApi';
 import { ExtensionMessageCollector, ExtensionPoint, ExtensionsRegistry, IExtensionPoint, IExtensionPointUser } from 'vs/workbench/services/extensions/common/extensionsRegistry';
 import { ResponsiveState } from 'vs/workbench/services/extensions/common/rpcProtocol';
 import { IExtensionActivationHost as IWorkspaceContainsActivationHost, checkActivateWorkspaceContainsExtension, checkGlobFileExists } from 'vs/workbench/services/extensions/common/workspaceContains';
@@ -139,7 +138,7 @@ export abstract class AbstractExtensionService extends Disposable implements IEx
 	private readonly _isDev: boolean;
 	private readonly _extensionsMessages: Map<string, IMessage[]>;
 	private readonly _allRequestedActivateEvents = new Set<string>();
-	private readonly _proposedApiController: ProposedApiController;
+	private readonly _extensionsProposedApi: ExtensionsProposedApi;
 	private readonly _isExtensionDevHost: boolean;
 	protected readonly _isExtensionDevTestFromCli: boolean;
 
@@ -192,7 +191,7 @@ export abstract class AbstractExtensionService extends Disposable implements IEx
 		this._installedExtensionsReady = new Barrier();
 		this._isDev = !this._environmentService.isBuilt || this._environmentService.isExtensionDevelopment;
 		this._extensionsMessages = new Map<string, IMessage[]>();
-		this._proposedApiController = _instantiationService.createInstance(ProposedApiController);
+		this._extensionsProposedApi = _instantiationService.createInstance(ExtensionsProposedApi);
 
 		this._extensionHostManagers = [];
 		this._extensionHostActiveExtensions = new Map<string, ExtensionIdentifier>();
@@ -1133,7 +1132,7 @@ export abstract class AbstractExtensionService extends Disposable implements IEx
 
 	protected _checkEnableProposedApi(extensions: IExtensionDescription[]): void {
 		for (const extension of extensions) {
-			this._proposedApiController.updateEnabledApiProposals(extension);
+			this._extensionsProposedApi.updateEnabledApiProposals(extension);
 		}
 	}
 
@@ -1350,101 +1349,6 @@ export abstract class AbstractExtensionService extends Disposable implements IEx
 	protected abstract _scanAndHandleExtensions(): Promise<void>;
 	protected abstract _scanSingleExtension(extension: IExtension): Promise<IExtensionDescription | null>;
 	public abstract _onExtensionHostExit(code: number): void;
-}
-
-class ProposedApiController {
-
-	private readonly _envEnablesProposedApiForAll: boolean;
-	private readonly _envEnabledExtensions: Set<string>;
-	private readonly _productEnabledExtensions: Map<string, string[]>;
-
-	constructor(
-		@ILogService private readonly _logService: ILogService,
-		@IWorkbenchEnvironmentService private readonly _environmentService: IWorkbenchEnvironmentService,
-		@IProductService productService: IProductService
-	) {
-
-		this._envEnabledExtensions = new Set((_environmentService.extensionEnabledProposedApi ?? []).map(id => ExtensionIdentifier.toKey(id)));
-
-		this._envEnablesProposedApiForAll =
-			!_environmentService.isBuilt || // always allow proposed API when running out of sources
-			(_environmentService.isExtensionDevelopment && productService.quality !== 'stable') || // do not allow proposed API against stable builds when developing an extension
-			(this._envEnabledExtensions.size === 0 && Array.isArray(_environmentService.extensionEnabledProposedApi)); // always allow proposed API if --enable-proposed-api is provided without extension ID
-
-		this._productEnabledExtensions = new Map<string, ApiProposalName[]>();
-
-
-		// NEW world - product.json spells out what proposals each extension can use
-		if (productService.extensionEnabledApiProposals) {
-			for (const [k, value] of Object.entries(productService.extensionEnabledApiProposals)) {
-				const key = ExtensionIdentifier.toKey(k);
-				const proposalNames = value.filter(name => {
-					if (!allApiProposals[<ApiProposalName>name]) {
-						_logService.warn(`Via 'product.json#extensionEnabledApiProposals' extension '${key}' wants API proposal '${name}' but that proposal DOES NOT EXIST. Likely, the proposal has been finalized (check 'vscode.d.ts') or was abandoned.`);
-						return false;
-					}
-					return true;
-				});
-				this._productEnabledExtensions.set(key, proposalNames);
-			}
-		}
-	}
-
-	updateEnabledApiProposals(_extension: IExtensionDescription): void {
-
-		// this is a trick to make the extension description writeable...
-		type Writeable<T> = { -readonly [P in keyof T]: Writeable<T[P]> };
-		const extension = <Writeable<IExtensionDescription>>_extension;
-		const key = ExtensionIdentifier.toKey(_extension.identifier);
-
-		// warn about invalid proposal and remove them from the list
-		if (isNonEmptyArray(extension.enabledApiProposals)) {
-			extension.enabledApiProposals = extension.enabledApiProposals.filter(name => {
-				const result = Boolean(allApiProposals[<ApiProposalName>name]);
-				if (!result) {
-					this._logService.error(`Extension '${key}' wants API proposal '${name}' but that proposal DOES NOT EXIST. Likely, the proposal has been finalized (check 'vscode.d.ts') or was abandoned.`);
-				}
-				return result;
-			});
-		}
-
-
-		if (this._productEnabledExtensions.has(key)) {
-			// NOTE that proposals that are listed in product.json override whatever is declared in the extension
-			// itself. This is needed for us to know what proposals are used "in the wild". Merging product.json-proposals
-			// and extension-proposals would break that.
-
-			const productEnabledProposals = this._productEnabledExtensions.get(key)!;
-
-			// check for difference between product.json-declaration and package.json-declaration
-			const productSet = new Set(productEnabledProposals);
-			const extensionSet = new Set(extension.enabledApiProposals);
-			const diff = new Set([...extensionSet].filter(a => !productSet.has(a)));
-			if (diff.size > 0) {
-				this._logService.error(`Extension '${key}' appears in product.json but enables LESS API proposals than the extension wants.\npackage.json (LOSES): ${[...extensionSet].join(', ')}\nproduct.json (WINS): ${[...productSet].join(', ')}`);
-
-				if (this._environmentService.isExtensionDevelopment) {
-					this._logService.error(`Proceeding with EXTRA proposals (${[...diff].join(', ')}) because extension is in development mode. Still, this EXTENSION WILL BE BROKEN unless product.json is updated.`);
-					productEnabledProposals.push(...diff);
-				}
-			}
-
-			extension.enabledApiProposals = productEnabledProposals;
-			return;
-		}
-
-		if (this._envEnablesProposedApiForAll || this._envEnabledExtensions.has(key)) {
-			// proposed API usage is not restricted and allowed just like the extension
-			// has declared it
-			return;
-		}
-
-		if (!extension.isBuiltin && isNonEmptyArray(extension.enabledApiProposals)) {
-			// restrictive: extension cannot use proposed API in this context and its declaration is nulled
-			this._logService.error(`Extension '${extension.identifier.value} CANNOT USE these API proposals '${extension.enabledApiProposals?.join(', ') || '*'}'. You MUST start in extension development mode or use the --enable-proposed-api command line flag`);
-			extension.enabledApiProposals = [];
-		}
-	}
 }
 
 interface IExtensionHostCrashInfo {
