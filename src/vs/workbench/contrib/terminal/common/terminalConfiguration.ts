6c0786a80aa7997724fc7711c865ce7e7af43f9e
Megan Rogge <merogge@microsoft.com>
2023-03-02 13:51:17 -0600

    Update src/vs/workbench/contrib/terminal/common/terminalConfiguration.ts
    Co-authored-by: Daniel Imms <2193314+Tyriar@users.noreply.github.com>

--- src/vs/workbench/contrib/terminal/common/terminalConfiguration.ts
+++ src/vs/workbench/contrib/terminal/common/terminalConfiguration.ts
@@ -107,7 +107,7 @@ const terminalConfiguration: IConfigurationNode = {
 			description: localize('terminal.integrated.tabs.location', "Controls the location of the terminal tabs, either to the left or right of the actual terminal(s).")
 		},
 		[TerminalSettingId.TabFocusMode]: {
-			markdownDescription: localize('tabFocusMode', "Controls whether the terminal receives tabs or defers them to the workbench for navigation. This overrides {0} when the terminal is focused.", '`#editor.tabFocusMode#`'),
+			markdownDescription: localize('tabFocusMode', "Controls whether the terminal receives tabs or defers them to the workbench for navigation. When set, this overrides {0} when the terminal is focused.", '`#editor.tabFocusMode#`'),
 			type: ['boolean', 'null'],
 			default: null
 		},
