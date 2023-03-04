26d834c735b7873c6666e9e4a33d65a458bf4781
Nidula Fernando <70999272+nidnidulafernando@users.noreply.github.com>
2023-03-04 10:44:24 +0530

    Update asar.js

--- build/lib/asar.js
+++ build/lib/asar.js
@@ -1,4 +1,5 @@
 "use strict";
+//Thank you for creating vs code for open source community
 /*---------------------------------------------------------------------------------------------
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
