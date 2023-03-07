6efbb211d4053ca48b4bc98ac458be9187f444b2
Rob Lourens <roblourens@gmail.com>
2023-03-07 17:24:28 -0500

    Remove todo

--- src/vs/workbench/contrib/interactiveSession/browser/interactiveSessionListRenderer.ts
+++ src/vs/workbench/contrib/interactiveSession/browser/interactiveSessionListRenderer.ts
@@ -183,7 +183,7 @@ export class InteractiveListItemRenderer extends Disposable implements ITreeRend
 	private renderFollowup(container: HTMLElement, templateData: IInteractiveListItemTemplate, followup: string | IInteractiveSessionResponseCommandFollowup): void {
 		const button = templateData.elementDisposables.add(new Button(container, { ...defaultButtonStyles, supportIcons: typeof followup !== 'string' }));
 		const label = typeof followup === 'string' ? `"${followup}"` : followup.title;
-		button.label = label; // todo icons
+		button.label = label;
 		if (typeof followup === 'string') {
 			// This should probably be a command as well?
 			templateData.elementDisposables.add(button.onDidClick(() => this._onDidSelectFollowup.fire(followup)));
