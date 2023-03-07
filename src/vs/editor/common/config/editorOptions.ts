7b39d0e24ae473beb9d1d28d8372e960681ddda9
Megan Rogge <merogge@microsoft.com>
2023-03-07 13:43:40 -0600

    MacOS -> macOS (#176428)

--- src/vs/editor/common/config/editorOptions.ts
+++ src/vs/editor/common/config/editorOptions.ts
@@ -4926,7 +4926,7 @@ export const EditorOptions = {
 	screenReaderAnnounceInlineSuggestion: register(new EditorBooleanOption(
 		EditorOption.screenReaderAnnounceInlineSuggestion, 'screenReaderAnnounceInlineSuggestion', false,
 		{
-			description: nls.localize('screenReaderAnnounceInlineSuggestion', "Control whether inline suggestions are announced by a screen reader. Note that this does not work on MacOS with VoiceOver."),
+			description: nls.localize('screenReaderAnnounceInlineSuggestion', "Control whether inline suggestions are announced by a screen reader. Note that this does not work on macOS with VoiceOver."),
 			tags: ['accessibility']
 		}
 	)),
