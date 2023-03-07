c8381f91ee1e089283e4c80cee631edaf2c0e263
Rob Lourens <roblourens@gmail.com>
2023-03-07 15:10:16 -0500

    Fix issue when debugging copilot extension

--- src/vscode-dts/vscode.proposed.interactive.d.ts
+++ src/vscode-dts/vscode.proposed.interactive.d.ts
@@ -50,7 +50,7 @@ declare module 'vscode' {
 		/**
 		 * Either a full URI or a relative path to the icon of the participant.
 		 */
-		iconPath?: Uri | string;
+		icon?: Uri;
 	}
 
 	export interface InteractiveSession {
