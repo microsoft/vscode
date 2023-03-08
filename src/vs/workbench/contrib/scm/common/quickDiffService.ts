dd8623dded335cd03f191d280a2acb8c978640be
Alex Ross <alros@microsoft.com>
2023-03-08 11:54:20 +0100

    PR feedback

--- src/vs/workbench/contrib/scm/common/quickDiffService.ts
+++ src/vs/workbench/contrib/scm/common/quickDiffService.ts
@@ -59,10 +59,11 @@ export class QuickDiffService extends Disposable implements IQuickDiffService {
 	}
 
 	async getQuickDiffs(uri: URI, language: string = '', isSynchronized: boolean = false): Promise<QuickDiff[]> {
-		const sorted = Array.from(this.quickDiffProviders).sort(createProviderComparer(uri));
-		const withMatchingRootUri = sorted.filter(provider => !provider.rootUri || isEqualOrParent(uri, provider.rootUri));
+		const providers = Array.from(this.quickDiffProviders)
+			.filter(provider => !provider.rootUri || isEqualOrParent(uri, provider.rootUri))
+			.sort(createProviderComparer(uri));
 
-		const diffs = await Promise.all(Array.from(withMatchingRootUri.values()).map(async (provider) => {
+		const diffs = await Promise.all(providers.map(async provider => {
 			const scoreValue = provider.selector ? score(provider.selector, uri, language, isSynchronized, undefined, undefined) : 10;
 			const diff: Partial<QuickDiff> = {
 				originalResource: scoreValue > 0 ? withNullAsUndefined(await provider.getOriginalResource(uri)) : undefined,
