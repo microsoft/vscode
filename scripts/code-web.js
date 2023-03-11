5c9d293f232404961ce822f91cfbb00c22be2fae
yiliang114 <1204183885@qq.com>
2023-03-11 20:22:46 +0800

    chore: use of recovery --no-playground parameters

--- scripts/code-web.js
+++ scripts/code-web.js
@@ -75,7 +75,7 @@ async function main() {
 
 	serverArgs.push('--sourcesPath', APP_ROOT);
 
-	serverArgs.push(...process.argv.slice(2).filter(v => !v.startsWith('--playground')));
+	serverArgs.push(...process.argv.slice(2).filter(v => !v.startsWith('--playground') && v !== '--no-playground'));
 
 	startServer(serverArgs);
 	if (openSystemBrowser) {
