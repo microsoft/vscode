95ee78fd9ada5361edb97a51122cb84e8b46c243
Rob Lourens <roblourens@gmail.com>
2023-03-12 12:55:20 -0500

    Make interactive session codeblocks "simple widgets" to disable context menu actions (#176870)

--- src/vs/workbench/contrib/interactiveSession/browser/interactiveSessionListRenderer.ts
+++ src/vs/workbench/contrib/interactiveSession/browser/interactiveSessionListRenderer.ts
@@ -467,7 +467,7 @@ class CodeBlockPart extends Disposable implements IInteractiveResultCodeBlockPar
 				alwaysConsumeMouseWheel: false
 			}
 		}, {
-			isSimpleWidget: false,
+			isSimpleWidget: true,
 			contributions: EditorExtensionsRegistry.getSomeEditorContributions([
 				MenuPreventer.ID,
 				SelectionClipboardContributionID,
