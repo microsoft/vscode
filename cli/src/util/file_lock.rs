bae7ea125153ed66dcb82b07e594fc497c68c82b
Connor Peet <connor@peet.io>
2023-03-13 14:54:24 -0700

    fix clippy

--- cli/src/util/file_lock.rs
+++ cli/src/util/file_lock.rs
@@ -87,7 +87,7 @@ impl FileLock {
 			return Ok(Lock::AlreadyLocked(file));
 		}
 
-		return Err(CodeError::SingletonLockfileOpenFailed(err));
+		Err(CodeError::SingletonLockfileOpenFailed(err))
 	}
 
 	pub fn file(&self) -> &File {
