0162d3386b80c608d3f5b39535ca4cea9c7470c6
Rob Lourens <roblourens@gmail.com>
2023-03-13 11:01:23 -0700

    Rerender request items once slash commands are loaded

--- src/vs/workbench/contrib/interactiveSession/browser/interactiveSessionWidget.ts
+++ src/vs/workbench/contrib/interactiveSession/browser/interactiveSessionWidget.ts
@@ -112,7 +112,9 @@ export class InteractiveSessionWidget extends Disposable implements IInteractive
 
 		this.slashCommandsPromise = undefined;
 		this.lastSlashCommands = undefined;
-		this.getSlashCommands(); // start the refresh
+		this.getSlashCommands().then(() => {
+			this.onDidChangeItems();
+		});
 
 		this._onDidChangeViewModel.fire();
 	}
@@ -186,8 +188,8 @@ export class InteractiveSessionWidget extends Disposable implements IInteractive
 
 			this.tree.setChildren(null, treeItems, {
 				diffIdentityProvider: {
-					getId(element) {
-						return element.id;
+					getId: (element) => {
+						return element.id + `${isRequestVM(element) && !!this.lastSlashCommands ? '_scLoaded' : ''}`;
 					},
 				}
 			});
