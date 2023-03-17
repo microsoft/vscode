570e1d888f4cfc98fa6dea9c5d556e677b913924
Rob Lourens <roblourens@gmail.com>
2023-03-17 14:30:22 -0600

    Don't give slash command completions unless the user started typing a slash

--- src/vs/workbench/contrib/interactiveSession/browser/interactiveSession.contribution.ts
+++ src/vs/workbench/contrib/interactiveSession/browser/interactiveSession.contribution.ts
@@ -118,4 +118,4 @@ registerSingleton(IInteractiveSessionService, InteractiveSessionService, Instant
 registerSingleton(IInteractiveSessionContributionService, InteractiveSessionContributionService, InstantiationType.Delayed);
 registerSingleton(IInteractiveSessionWidgetService, InteractiveSessionWidgetService, InstantiationType.Delayed);
 
-import 'vs/workbench/contrib/interactiveSession/browser/contrib/interactiveSessionInputEditorDecorations';
+import 'vs/workbench/contrib/interactiveSession/browser/contrib/interactiveSessionInputEditorContrib';
