6098dc2d7c76da47892111165d896e91d1bbe755
Bhavya U <bhavyau@microsoft.com>
2023-03-23 10:53:10 -0700

    Update src/vs/workbench/contrib/welcomeGettingStarted/browser/featuredExtensionService.ts
    Co-authored-by: Connor Peet <connor@peet.io>

--- src/vs/workbench/contrib/welcomeGettingStarted/browser/featuredExtensionService.ts
+++ src/vs/workbench/contrib/welcomeGettingStarted/browser/featuredExtensionService.ts
@@ -99,7 +99,7 @@ export class FeaturedExtensionsService extends Disposable implements IFeaturedEx
 
 		if (this.treatment) {
 			const installed = await this.extensionManagementService.getInstalled();
-			for (const [_, extension] of Object.entries(this.treatment.extensions)) {
+			for (const extension of Object.values(this.treatment.extensions)) {
 				if (installed.some(e => ExtensionIdentifier.equals(e.identifier.id, extension))) {
 					this.ignoredExtensions.add(extension);
 				}
