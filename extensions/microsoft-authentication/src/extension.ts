393c6de93a75c0ad95ff14f84640194be87e1ddf
Brandon Waterloo [MSFT] <36966225+bwateratmsft@users.noreply.github.com>
2023-03-23 14:33:58 -0400

    Init Azure cloud auth provider and reinit as needed

--- extensions/microsoft-authentication/src/extension.ts
+++ extensions/microsoft-authentication/src/extension.ts
@@ -8,6 +8,72 @@ import { AzureActiveDirectoryService } from './AADHelper';
 import { UriEventHandler } from './UriEventHandler';
 import TelemetryReporter from '@vscode/extension-telemetry';
 
+async function initAzureCloudAuthProvider(context: vscode.ExtensionContext, telemetryReporter: TelemetryReporter, uriHandler: UriEventHandler): Promise<vscode.Disposable | undefined> {
+	const settingValue = vscode.workspace.getConfiguration().get<string>('azureCloud.endpoint');
+	if (!settingValue) {
+		return undefined;
+	}
+
+	// validate user value
+	let uri: vscode.Uri;
+	try {
+		uri = vscode.Uri.parse(settingValue, true);
+	} catch (e) {
+		vscode.window.showErrorMessage(vscode.l10n.t('Azure Cloud login URI is not a valid URI: {0}', e.message ?? e));
+		return;
+	}
+
+	const azureEnterpriseAuthProvider = new AzureActiveDirectoryService(context, uriHandler, settingValue);
+	await azureEnterpriseAuthProvider.initialize();
+
+	const disposable = vscode.authentication.registerAuthenticationProvider('azure-cloud', 'Azure Cloud', {
+		onDidChangeSessions: azureEnterpriseAuthProvider.onDidChangeSessions,
+		getSessions: (scopes: string[]) => azureEnterpriseAuthProvider.getSessions(scopes),
+		createSession: async (scopes: string[]) => {
+			try {
+				/* __GDPR__
+					"login" : {
+						"owner": "TylerLeonhardt",
+						"comment": "Used to determine the usage of the Azure Cloud Auth Provider.",
+						"scopes": { "classification": "PublicNonPersonalData", "purpose": "FeatureInsight", "comment": "Used to determine what scope combinations are being requested." }
+					}
+				*/
+				telemetryReporter.sendTelemetryEvent('loginAzureCloud', {
+					// Get rid of guids from telemetry.
+					scopes: JSON.stringify(scopes.map(s => s.replace(/[0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{12}/i, '{guid}'))),
+				});
+
+				return await azureEnterpriseAuthProvider.createSession(scopes.sort());
+			} catch (e) {
+				/* __GDPR__
+					"loginFailed" : { "owner": "TylerLeonhardt", "comment": "Used to determine how often users run into issues with the login flow." }
+				*/
+				telemetryReporter.sendTelemetryEvent('loginAzureCloudFailed');
+
+				throw e;
+			}
+		},
+		removeSession: async (id: string) => {
+			try {
+				/* __GDPR__
+					"logout" : { "owner": "TylerLeonhardt", "comment": "Used to determine how often users log out." }
+				*/
+				telemetryReporter.sendTelemetryEvent('logoutAzureCloud');
+
+				await azureEnterpriseAuthProvider.removeSessionById(id);
+			} catch (e) {
+				/* __GDPR__
+					"logoutFailed" : { "owner": "TylerLeonhardt", "comment": "Used to determine how often fail to log out." }
+				*/
+				telemetryReporter.sendTelemetryEvent('logoutAzureCloudFailed');
+			}
+		}
+	}, { supportsMultipleAccounts: true });
+
+	context.subscriptions.push(disposable);
+	return disposable;
+}
+
 export async function activate(context: vscode.ExtensionContext) {
 	const { name, version, aiKey } = context.extension.packageJSON as { name: string; version: string; aiKey: string };
 	const telemetryReporter = new TelemetryReporter(aiKey);
@@ -62,6 +128,15 @@ export async function activate(context: vscode.ExtensionContext) {
 			}
 		}
 	}, { supportsMultipleAccounts: true }));
+
+	let azureCloudAuthProviderDisposable = await initAzureCloudAuthProvider(context, telemetryReporter, uriHandler);
+
+	context.subscriptions.push(vscode.workspace.onDidChangeConfiguration(async e => {
+		if (e.affectsConfiguration('azureCloud.endpoint')) {
+			azureCloudAuthProviderDisposable?.dispose();
+			azureCloudAuthProviderDisposable = await initAzureCloudAuthProvider(context, telemetryReporter, uriHandler);
+		}
+	}));
 
 	return;
 }
