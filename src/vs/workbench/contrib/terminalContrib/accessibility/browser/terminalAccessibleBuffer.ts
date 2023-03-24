0632baf6f30ebdb29a8e79010e469ed1beaaf06b
meganrogge <megan.rogge@microsoft.com>
2023-03-24 11:08:00 -0700

    fix #178253

--- src/vs/workbench/contrib/terminalContrib/accessibility/browser/terminalAccessibleBuffer.ts
+++ src/vs/workbench/contrib/terminalContrib/accessibility/browser/terminalAccessibleBuffer.ts
@@ -106,7 +106,6 @@ export class AccessibleBufferWidget extends DisposableStore {
 				case KeyCode.Escape:
 					// On escape, hide the accessible buffer and force focus onto the terminal
 					this._hide();
-					this._xterm.raw.focus();
 					break;
 			}
 		});
@@ -136,6 +135,7 @@ export class AccessibleBufferWidget extends DisposableStore {
 	private _hide(): void {
 		this._accessibleBuffer.classList.remove(CssClass.Active);
 		this._xtermElement.classList.remove(CssClass.Hide);
+		this._xterm.raw.focus();
 	}
 
 	private async _updateModel(insertion?: boolean): Promise<ITextModel> {
