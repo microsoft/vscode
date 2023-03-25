783da9dbb9f246b6a4fceb0a8d67513314bc0fe2
Tyler Leonhardt <me@tylerleonhardt.com>
2023-03-24 17:24:43 -0700

    Add proxy support to auth extensions

--- extensions/microsoft-authentication/src/node/fetch.ts
+++ extensions/microsoft-authentication/src/node/fetch.ts
@@ -2,6 +2,18 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-import fetch from 'node-fetch';
+import { workspace } from 'vscode';
+import fetch, { RequestInfo, RequestInit, Response } from 'node-fetch';
+import { HttpsProxyAgent } from 'https-proxy-agent';
 
-export const fetching = fetch;
+export function fetching(url: RequestInfo, init?: RequestInit | undefined): Promise<Response> {
+	const instanceOptions: RequestInit = {
+		...init
+	};
+	const httpProxy = workspace.getConfiguration('http').get('proxy', process.env.HTTP_PROXY);
+	if (!init?.agent && httpProxy) {
+		instanceOptions.agent = new HttpsProxyAgent(httpProxy);
+	}
+
+	return fetch(url, instanceOptions);
+}
