cf6971dbbac1b9a7a1a9dc6a0c90d66f55c2023a
Benjamin Pasero <benjamin.pasero@microsoft.com>
2023-03-29 10:34:49 +0200

    perf - do not embedd unicode characters in sources

--- src/vs/editor/common/languages/linkComputer.ts
+++ src/vs/editor/common/languages/linkComputer.ts
@@ -154,8 +154,7 @@ function getClassifier(): CharacterClassifier<CharacterClass> {
 	if (_classifier === null) {
 		_classifier = new CharacterClassifier<CharacterClass>(CharacterClass.None);
 
-		// allow-any-unicode-next-line
-		const FORCE_TERMINATION_CHARACTERS = ' \t<>\'\"、。｡､，．：；‘〈「『〔（［｛｢｣｝］）〕』」〉’｀～…';
+		const FORCE_TERMINATION_CHARACTERS = ' \t<>\'\"\u3001\u3002\uff61\uff64\uff0c\uff0e\uff1a\uff1b\u2018\u3008\u300c\u300e\u3014\uff08\uff3b\uff5b\uff62\uff63\uff5d\uff3d\uff09\u3015\u300f\u300d\u3009\u2019\uff40\uff5e\u2026';
 		for (let i = 0; i < FORCE_TERMINATION_CHARACTERS.length; i++) {
 			_classifier.set(FORCE_TERMINATION_CHARACTERS.charCodeAt(i), CharacterClass.ForceTermination);
 		}
