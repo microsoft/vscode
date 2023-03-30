a0c35db9cda04c3883bafd1760a7af10d53dc541
Henning Dieterichs <hdieterichs@microsoft.com>
2023-03-30 19:14:40 +0200

    Moves viewport tokenization responsibility into GrammarTokenization. Fixes #178582

--- src/vs/monaco.d.ts
+++ src/vs/monaco.d.ts
@@ -2311,6 +2311,15 @@ declare namespace monaco.editor {
 	 */
 	export class LineRange {
 		/**
+		 * @param lineRanges An array of sorted line ranges.
+		 */
+		static joinMany(lineRanges: readonly (readonly LineRange[])[]): readonly LineRange[];
+		/**
+		 * @param lineRanges1 Must be sorted.
+		 * @param lineRanges2 Must be sorted.
+		 */
+		static join(lineRanges1: readonly LineRange[], lineRanges2: readonly LineRange[]): readonly LineRange[];
+		/**
 		 * The start line number.
 		 */
 		readonly startLineNumber: number;
@@ -2346,6 +2355,7 @@ declare namespace monaco.editor {
 		 */
 		intersect(other: LineRange): LineRange | undefined;
 		overlapOrTouch(other: LineRange): boolean;
+		equals(b: LineRange): boolean;
 	}
 
 	/**
