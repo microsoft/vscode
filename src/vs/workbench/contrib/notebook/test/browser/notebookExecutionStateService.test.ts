3ffa5a52e77a47f333bcfbe61a702de902361ebe
Don Jayamanne <don.jayamanne@outlook.com>
2023-04-05 22:51:04 +1000

    oops

--- src/vs/workbench/contrib/notebook/test/browser/notebookExecutionStateService.test.ts
+++ src/vs/workbench/contrib/notebook/test/browser/notebookExecutionStateService.test.ts
@@ -188,18 +188,16 @@ suite('NotebookExecutionStateService', () => {
 
 			const deferred = new DeferredPromise<void>();
 			disposables.add(executionStateService.onDidChangeCellExecution(e => {
-				if (typeof e.cellHandle === 'number') {
-					const cellUri = CellUri.generate(e.notebook, e.cellHandle);
-					const exe = executionStateService.getCellExecution(cellUri);
-					assert.ok(exe);
-					assert.strictEqual(e.notebook.toString(), exe.notebook.toString());
-					assert.strictEqual(e.cellHandle, exe.cellHandle);
+				const cellUri = CellUri.generate(e.notebook, e.cellHandle);
+				const exe = executionStateService.getCellExecution(cellUri);
+				assert.ok(exe);
+				assert.strictEqual(e.notebook.toString(), exe.notebook.toString());
+				assert.strictEqual(e.cellHandle, exe.cellHandle);
 
-					assert.strictEqual(exe.notebook.toString(), e.changed?.notebook.toString());
-					assert.strictEqual(exe.cellHandle, e.changed?.cellHandle);
+				assert.strictEqual(exe.notebook.toString(), e.changed?.notebook.toString());
+				assert.strictEqual(exe.cellHandle, e.changed?.cellHandle);
 
-					deferred.complete();
-				}
+				deferred.complete();
 			}));
 
 			executionStateService.createCellExecution(viewModel.uri, cell.handle);
