cfeb94d366303a143f1be4724c275bbf4325a030
Raymond Zhao <7199958+rzhao271@users.noreply.github.com>
2023-04-05 10:30:37 -0700

    Mention DefinitelyTyped

--- src/typings/windows-mutex.d.ts
+++ src/typings/windows-mutex.d.ts
@@ -6,7 +6,7 @@
 // Copied from the `@types/windows-mutex` package.
 // The dependency is an optional dependency that is only used on Windows,
 // but we need the typings to compile on all platforms.
-// The types package also maps to `windows-mutex`,
+// The types package exported from DefinitelyTyped also maps to `windows-mutex`,
 // whereas we are now using `@vscode/windows-mutex`.
 declare module '@vscode/windows-mutex' {
 	export class Mutex {
