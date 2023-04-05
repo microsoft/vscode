00937ffafbfa7eb5ad3024bec00ea6f3206c99ec
Sandeep Somavarapu <sasomava@microsoft.com>
2023-04-05 22:34:52 +0200

    move product config reading to web.main

--- src/vs/platform/product/common/product.ts
+++ src/vs/platform/product/common/product.ts
@@ -55,28 +55,6 @@ else {
 	// Built time configuration (do NOT modify)
 	product = { /*BUILD->INSERT_PRODUCT_CONFIGURATION*/ } as IProductConfiguration;
 
-	// Running out of sources
-	if (Object.keys(product).length === 0) {
-		let productOverrides = {};
-		const productOverridesElement = document.getElementById('vscode-workbench-product-overrides');
-		const productOverridesElementAttribute = productOverridesElement ? productOverridesElement.getAttribute('data-settings') : undefined;
-		if (productOverridesElementAttribute) {
-			try { productOverrides = JSON.parse(productOverridesElementAttribute); } catch (error) { /* ignore error*/ }
-		}
-		Object.assign(product, {
-			version: '1.72.0-dev',
-			nameShort: 'Code - OSS Dev',
-			nameLong: 'Code - OSS Dev',
-			applicationName: 'code-oss',
-			dataFolderName: '.vscode-oss',
-			urlProtocol: 'code-oss',
-			reportIssueUrl: 'https://github.com/microsoft/vscode/issues/new',
-			licenseName: 'MIT',
-			licenseUrl: 'https://github.com/microsoft/vscode/blob/main/LICENSE.txt',
-			serverLicenseUrl: 'https://github.com/microsoft/vscode/blob/main/LICENSE.txt',
-			...productOverrides
-		});
-	}
 }
 
 /**
