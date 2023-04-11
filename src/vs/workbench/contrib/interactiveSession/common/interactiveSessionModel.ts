8242ea41d78e39a6b1d9e73ab12ad987cbb1fb8a
Rob Lourens <roblourens@gmail.com>
2023-04-10 21:39:05 -0700

    Avoid using .at

--- src/vs/workbench/contrib/interactiveSession/common/interactiveSessionModel.ts
+++ src/vs/workbench/contrib/interactiveSession/common/interactiveSessionModel.ts
@@ -234,7 +234,7 @@ export class InteractiveSessionModel extends Disposable implements IInteractiveS
 	}
 
 	get requestInProgress(): boolean {
-		const lastRequest = this._requests.at(-1);
+		const lastRequest = this._requests[this._requests.length - 1];
 		return !!lastRequest && !!lastRequest.response && !lastRequest.response.isComplete;
 	}
 
