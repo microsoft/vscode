e5512217bcfb2b644fe7ad6c465fe0b45d23265b
Matt Bierner <matb@microsoft.com>
2023-04-11 18:43:34 -0700

    Use inline progress widget for long running copy pastes (#179730)
    Extracts the inline progress widget for use with the copy paste too

--- src/vs/editor/contrib/dropIntoEditor/browser/dropIntoEditorContribution.ts
+++ src/vs/editor/contrib/dropIntoEditor/browser/dropIntoEditorContribution.ts
@@ -18,7 +18,6 @@ import { IEditorContribution } from 'vs/editor/common/editorCommon';
 import { DocumentOnDropEdit, WorkspaceEdit } from 'vs/editor/common/languages';
 import { TrackedRangeStickiness } from 'vs/editor/common/model';
 import { ILanguageFeaturesService } from 'vs/editor/common/services/languageFeatures';
-import { DropProgressManager as DropProgressWidgetManager } from 'vs/editor/contrib/dropIntoEditor/browser/dropProgressWidget';
 import { PostDropWidgetManager } from 'vs/editor/contrib/dropIntoEditor/browser/postDropWidget';
 import { CodeEditorStateFlag, EditorStateCancellationTokenSource } from 'vs/editor/contrib/editorState/browser/editorState';
 import { SnippetParser } from 'vs/editor/contrib/snippet/browser/snippetParser';
@@ -26,6 +25,8 @@ import { IInstantiationService } from 'vs/platform/instantiation/common/instanti
 import { IWorkspaceContextService } from 'vs/platform/workspace/common/workspace';
 import { registerDefaultDropProviders } from './defaultOnDropProviders';
 import { EditorOption } from 'vs/editor/common/config/editorOptions';
+import { InlineProgressManager } from 'vs/editor/contrib/inlineProgress/browser/inlineProgress';
+import { localize } from 'vs/nls';
 
 
 export class DropIntoEditorController extends Disposable implements IEditorContribution {
@@ -35,7 +36,7 @@ export class DropIntoEditorController extends Disposable implements IEditorContr
 	private operationIdPool = 0;
 	private _currentOperation?: { readonly id: number; readonly promise: CancelablePromise<void> };
 
-	private readonly _dropProgressWidgetManager: DropProgressWidgetManager;
+	private readonly _dropProgressManager: InlineProgressManager;
 	private readonly _postDropWidgetManager: PostDropWidgetManager;
 
 	constructor(
@@ -47,7 +48,7 @@ export class DropIntoEditorController extends Disposable implements IEditorContr
 	) {
 		super();
 
-		this._dropProgressWidgetManager = this._register(new DropProgressWidgetManager(editor, instantiationService));
+		this._dropProgressManager = this._register(new InlineProgressManager('dropIntoEditor', editor, instantiationService));
 		this._postDropWidgetManager = this._register(new PostDropWidgetManager(editor, instantiationService));
 
 		this._register(editor.onDropIntoEditor(e => this.onDropIntoEditor(editor, e.position, e.event)));
@@ -61,17 +62,17 @@ export class DropIntoEditorController extends Disposable implements IEditorContr
 		}
 
 		this._currentOperation?.promise.cancel();
-		this._dropProgressWidgetManager.clear();
+		this._dropProgressManager.clear();
 
 		editor.focus();
 		editor.setPosition(position);
 
-		const id = this.operationIdPool++;
+		const operationId = this.operationIdPool++;
 
 		const p = createCancelablePromise(async (token) => {
 			const tokenSource = new EditorStateCancellationTokenSource(editor, CodeEditorStateFlag.Value, undefined, token);
 
-			this._dropProgressWidgetManager.setAtPosition(position, {
+			this._dropProgressManager.setAtPosition(position, localize('dropIntoEditorProgress', "Running drop handlers. Click to cancel"), {
 				cancel: () => tokenSource.cancel()
 			});
 
@@ -110,14 +111,14 @@ export class DropIntoEditorController extends Disposable implements IEditorContr
 			} finally {
 				tokenSource.dispose();
 
-				if (this._currentOperation?.id === id) {
-					this._dropProgressWidgetManager.clear();
+				if (this._currentOperation?.id === operationId) {
+					this._dropProgressManager.clear();
 					this._currentOperation = undefined;
 				}
 			}
 		});
 
-		this._currentOperation = { id, promise: p };
+		this._currentOperation = { id: operationId, promise: p };
 	}
 
 	private async extractDataTransferData(dragEvent: DragEvent): Promise<VSDataTransfer> {
