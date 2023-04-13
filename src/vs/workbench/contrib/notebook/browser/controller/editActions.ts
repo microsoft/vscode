304c2179830d588c0a6bf8024ea81275a0ab2122
weartist <weartistt@gmail.com>
2023-04-13 09:21:22 +0800

    optimize the dialog message

--- src/vs/workbench/contrib/notebook/browser/controller/editActions.ts
+++ src/vs/workbench/contrib/notebook/browser/controller/editActions.ts
@@ -171,7 +171,7 @@ registerAction2(class DeleteCellAction extends NotebookCellAction {
 
 			confirmation = await dialogService.confirm({
 				type: 'question',
-				message: localize('confirmDeleteButtonMessage', "Are you sure you want to delete a running cell?"),
+				message: localize('confirmDeleteButtonMessage', "This cell is running, are you sure you want to delete it?"),
 				detail: localize('confirmDeleteButtonDetail', "After deletion, the cell may not be recovered."),
 				primaryButton: primaryButton,
 				checkbox: {
