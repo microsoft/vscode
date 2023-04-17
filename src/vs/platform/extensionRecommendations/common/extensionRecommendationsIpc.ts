87392794b889a8ce87c547f57125622887517f36
Sandeep Somavarapu <sasomava@microsoft.com>
2023-04-17 15:59:53 +0200

    fix #179004 Show publishers in the recommendation prompt

--- src/vs/platform/extensionRecommendations/common/extensionRecommendationsIpc.ts
+++ src/vs/platform/extensionRecommendations/common/extensionRecommendationsIpc.ts
@@ -5,7 +5,7 @@
 
 import { Event } from 'vs/base/common/event';
 import { IChannel, IServerChannel } from 'vs/base/parts/ipc/common/ipc';
-import { IExtensionRecommendationNotificationService, RecommendationsNotificationResult, RecommendationSource } from 'vs/platform/extensionRecommendations/common/extensionRecommendations';
+import { IExtensionRecommendationNotificationService, IExtensionRecommendations, RecommendationsNotificationResult } from 'vs/platform/extensionRecommendations/common/extensionRecommendations';
 
 export class ExtensionRecommendationNotificationServiceChannelClient implements IExtensionRecommendationNotificationService {
 
@@ -15,8 +15,8 @@ export class ExtensionRecommendationNotificationServiceChannelClient implements 
 
 	get ignoredRecommendations(): string[] { throw new Error('not supported'); }
 
-	promptImportantExtensionsInstallNotification(extensionIds: string[], message: string, searchValue: string, priority: RecommendationSource): Promise<RecommendationsNotificationResult> {
-		return this.channel.call('promptImportantExtensionsInstallNotification', [extensionIds, message, searchValue, priority]);
+	promptImportantExtensionsInstallNotification(extensionRecommendations: IExtensionRecommendations): Promise<RecommendationsNotificationResult> {
+		return this.channel.call('promptImportantExtensionsInstallNotification', [extensionRecommendations]);
 	}
 
 	promptWorkspaceRecommendations(recommendations: string[]): Promise<void> {
@@ -39,7 +39,7 @@ export class ExtensionRecommendationNotificationServiceChannel implements IServe
 
 	call(_: unknown, command: string, args?: any): Promise<any> {
 		switch (command) {
-			case 'promptImportantExtensionsInstallNotification': return this.service.promptImportantExtensionsInstallNotification(args[0], args[1], args[2], args[3]);
+			case 'promptImportantExtensionsInstallNotification': return this.service.promptImportantExtensionsInstallNotification(args[0]);
 		}
 
 		throw new Error(`Call not found: ${command}`);
