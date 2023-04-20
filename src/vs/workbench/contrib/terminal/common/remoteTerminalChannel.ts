9e4fa31a4af5bde742a14669f15976e2f7ce5c99
Kartik Raj <karraj@microsoft.com>
2023-04-20 21:30:54 +0000

    Attempt to use correct workspace for remote terminals

--- src/vs/workbench/contrib/terminal/common/remoteTerminalChannel.ts
+++ src/vs/workbench/contrib/terminal/common/remoteTerminalChannel.ts
@@ -10,7 +10,7 @@ import { IChannel } from 'vs/base/parts/ipc/common/ipc';
 import { IWorkbenchConfigurationService } from 'vs/workbench/services/configuration/common/configuration';
 import { ILogService } from 'vs/platform/log/common/log';
 import { IRemoteAuthorityResolverService } from 'vs/platform/remote/common/remoteAuthorityResolver';
-import { IWorkspaceContextService } from 'vs/platform/workspace/common/workspace';
+import { IWorkspaceContextService, IWorkspaceFolder } from 'vs/platform/workspace/common/workspace';
 import { serializeEnvironmentVariableCollection } from 'vs/platform/terminal/common/environmentVariableShared';
 import { IConfigurationResolverService } from 'vs/workbench/services/configurationResolver/common/configurationResolver';
 import { SideBySideEditor, EditorResourceAccessor } from 'vs/workbench/common/editor';
@@ -51,6 +51,8 @@ export interface ICreateTerminalProcessArguments {
 	rows: number;
 	unicodeVersion: '6' | '11';
 	resolverEnv: { [key: string]: string | null } | undefined;
+	// TODO: Remove this when last active workspace is fixed
+	getWorkspaceFolder: (resource: URI) => IWorkspaceFolder | null;
 }
 
 export interface ICreateTerminalProcessResult {
@@ -182,7 +184,8 @@ export class RemoteTerminalChannelClient implements IPtyHostController {
 			cols,
 			rows,
 			unicodeVersion,
-			resolverEnv
+			resolverEnv,
+			getWorkspaceFolder: this._workspaceContextService.getWorkspaceFolder.bind(this._workspaceContextService),
 		};
 		return await this._channel.call<ICreateTerminalProcessResult>('$createProcess', args);
 	}
--- src/vs/workbench/services/configurationResolver/common/terminalResolver.ts
+++ src/vs/workbench/services/configurationResolver/common/terminalResolver.ts
@@ -5,16 +5,16 @@
 
 import { withNullAsUndefined } from 'vs/base/common/types';
 import { URI } from 'vs/base/common/uri';
-import { IWorkspaceContextService, IWorkspaceFolder } from 'vs/platform/workspace/common/workspace';
+import { IWorkspaceFolder } from 'vs/platform/workspace/common/workspace';
 import { IHistoryService } from 'vs/workbench/services/history/common/history';
 
-export function getWorkspaceForTerminal(cwd: URI | string | undefined, workspaceContextService: IWorkspaceContextService, historyService: IHistoryService): IWorkspaceFolder | undefined {
+export function getWorkspaceForTerminal(cwd: URI | string | undefined, workspaceContextService: { getWorkspaceFolder(resource: URI): IWorkspaceFolder | null }, historyService: IHistoryService | undefined): IWorkspaceFolder | undefined {
 	const cwdUri = typeof cwd === 'string' ? URI.parse(cwd) : cwd;
 	let workspaceFolder = cwdUri ? withNullAsUndefined(workspaceContextService.getWorkspaceFolder(cwdUri)) : undefined;
 	if (!workspaceFolder) {
 		// fallback to last active workspace if cwd is not available or it is not in workspace
 		// TOOD: last active workspace is known to be unreliable, we should remove this fallback eventually
-		const activeWorkspaceRootUri = historyService.getLastActiveWorkspaceRoot();
+		const activeWorkspaceRootUri = historyService?.getLastActiveWorkspaceRoot();
 		workspaceFolder = activeWorkspaceRootUri ? withNullAsUndefined(workspaceContextService.getWorkspaceFolder(activeWorkspaceRootUri)) : undefined;
 	}
 	return workspaceFolder;
