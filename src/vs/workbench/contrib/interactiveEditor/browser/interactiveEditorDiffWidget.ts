eea7aa49af12f2c80bc791bbfd37b695b74467f0
Johannes <johannes.rieken@gmail.com>
2023-04-20 12:45:30 +0200

    tweak hidden lines math

--- src/vs/workbench/contrib/interactiveEditor/browser/interactiveEditorDiffWidget.ts
+++ src/vs/workbench/contrib/interactiveEditor/browser/interactiveEditorDiffWidget.ts
@@ -6,10 +6,10 @@
 import { Dimension, h } from 'vs/base/browser/dom';
 import { DisposableStore } from 'vs/base/common/lifecycle';
 import { assertType } from 'vs/base/common/types';
-import { IActiveCodeEditor, IDiffEditor } from 'vs/editor/browser/editorBrowser';
+import { IActiveCodeEditor, ICodeEditor, IDiffEditor } from 'vs/editor/browser/editorBrowser';
 import { EmbeddedDiffEditorWidget } from 'vs/editor/browser/widget/embeddedCodeEditorWidget';
 import { EditorOption } from 'vs/editor/common/config/editorOptions';
-import { IRange, Range } from 'vs/editor/common/core/range';
+import { Range } from 'vs/editor/common/core/range';
 import { ITextModel } from 'vs/editor/common/model';
 import { ZoneWidget } from 'vs/editor/contrib/zoneWidget/browser/zoneWidget';
 import { IInstantiationService } from 'vs/platform/instantiation/common/instantiation';
@@ -21,6 +21,7 @@ import { LineRange } from 'vs/editor/common/core/lineRange';
 import { LineRangeMapping } from 'vs/editor/common/diff/linesDiffComputer';
 import { Position } from 'vs/editor/common/core/position';
 import { EditorExtensionsRegistry } from 'vs/editor/browser/editorExtensions';
+import { ScrollType } from 'vs/editor/common/editorCommon';
 
 export class InteractiveEditorDiffWidget extends ZoneWidget {
 
@@ -97,10 +98,7 @@ export class InteractiveEditorDiffWidget extends ZoneWidget {
 
 		const modified = this.editor.getModel();
 		const ranges = this._computeHiddenRanges(modified, range, changes);
-		if (!ranges) {
-			return undefined;
-		}
-		return ranges.modifiedHidden.getEndPosition();
+		return ranges?.anchor;
 	}
 
 	showDiff(range: () => Range, changes: LineRangeMapping[]): void {
@@ -125,18 +123,19 @@ export class InteractiveEditorDiffWidget extends ZoneWidget {
 			return;
 		}
 
-		this.editor.setHiddenAreas([ranges.modifiedHidden], InteractiveEditorDiffWidget._hideId);
-		this._diffEditor.getOriginalEditor().setHiddenAreas(ranges.originalDiffHidden, InteractiveEditorDiffWidget._hideId);
-		this._diffEditor.getModifiedEditor().setHiddenAreas(ranges.modifiedDiffHidden, InteractiveEditorDiffWidget._hideId);
+		InteractiveEditorDiffWidget._hide(this.editor, [ranges.modifiedHidden]);
+		InteractiveEditorDiffWidget._hide(this._diffEditor.getModifiedEditor(), ranges.modifiedDiffHidden);
+		InteractiveEditorDiffWidget._hide(this._diffEditor.getOriginalEditor(), ranges.originalDiffHidden);
 
-		const lineCountModified = ranges.modifiedHidden.endLineNumber - ranges.modifiedHidden.startLineNumber;
-		const lineCountOriginal = ranges.originalHidden.endLineNumber - ranges.originalHidden.startLineNumber;
+		this._diffEditor.revealLine(ranges.modifiedHidden.startLineNumber, ScrollType.Immediate);
 
-		const lineHeightDiff = 1 + Math.max(lineCountModified, lineCountOriginal);
+		const lineCountModified = ranges.modifiedHidden.length;
+		const lineCountOriginal = ranges.originalHidden.length;
+
+		const lineHeightDiff = Math.max(lineCountModified, lineCountOriginal);
 		const lineHeightPadding = (this.editor.getOption(EditorOption.lineHeight) / 12) /* padding-top/bottom*/;
 
-		const position = ranges.modifiedHidden.getEndPosition();
-		super.show(position, lineHeightDiff + lineHeightPadding);
+		super.show(ranges.anchor, lineHeightDiff + lineHeightPadding);
 	}
 
 	private _computeHiddenRanges(model: ITextModel, range: Range, changes: LineRangeMapping[]) {
@@ -163,13 +162,26 @@ export class InteractiveEditorDiffWidget extends ZoneWidget {
 			originalLineRange = new LineRange(originalLineRange.startLineNumber, originalLineRange.endLineNumberExclusive + endDelta);
 		}
 
+		const originalDiffHidden = invert(originalLineRange, this._textModelv0);
+		const modifiedDiffHidden = invert(modifiedLineRange, model);
 
-		const originalHidden = asRange(originalLineRange, this._textModelv0);
-		const originalDiffHidden = invertRange(originalHidden, this._textModelv0);
-		const modifiedHidden = asRange(modifiedLineRange, model);
-		const modifiedDiffHidden = invertRange(modifiedHidden, model);
+		return {
+			originalHidden: originalLineRange,
+			originalDiffHidden,
+			modifiedHidden: modifiedLineRange,
+			modifiedDiffHidden,
+			anchor: new Position(modifiedLineRange.endLineNumberExclusive - 1, 1)
+		};
+	}
 
-		return { originalHidden, originalDiffHidden, modifiedHidden, modifiedDiffHidden };
+	private static _hide(editor: ICodeEditor, lineRanges: LineRange[]): void {
+		lineRanges = lineRanges.filter(range => !range.isEmpty);
+		if (lineRanges.length === 0) {
+			// todo?
+		} else {
+			const ranges = lineRanges.map(r => new Range(r.startLineNumber, 1, r.endLineNumberExclusive - 1, 1));
+			editor.setHiddenAreas(ranges, this._hideId);
+		}
 	}
 
 	override hide(): void {
@@ -177,6 +189,10 @@ export class InteractiveEditorDiffWidget extends ZoneWidget {
 		this._diffEditor.getOriginalEditor().setHiddenAreas([], InteractiveEditorDiffWidget._hideId);
 		this._diffEditor.getModifiedEditor().setHiddenAreas([], InteractiveEditorDiffWidget._hideId);
 		super.hide();
+	}
+
+	protected override revealRange(range: Range, isLastLine: boolean): void {
+		// ignore
 	}
 
 	// --- layout -------------------------
@@ -197,29 +213,12 @@ export class InteractiveEditorDiffWidget extends ZoneWidget {
 	}
 }
 
-function invertRange(range: IRange, model: ITextModel): Range[] {
-	const result: Range[] = [];
-	if (Range.isEmpty(range)) {
-		//result.push(model.getFullModelRange());
-		// todo@jrieken
-		// cannot hide everything, return [] instead
-
-	} else {
-		if (range.startLineNumber > 1) {
-			result.push(new Range(1, 1, range.startLineNumber - 1, 1));
-		}
-		if (range.endLineNumber < model.getLineCount()) {
-			result.push(new Range(range.endLineNumber + 1, 1, model.getLineCount(), 1));
-		}
+function invert(range: LineRange, model: ITextModel): LineRange[] {
+	if (range.isEmpty) {
+		return [];
 	}
-	return result;
-}
-
-function asRange(lineRange: LineRange, model: ITextModel): Range {
-	if (lineRange.isEmpty) {
-		return new Range(lineRange.startLineNumber, 1, lineRange.startLineNumber, 1);
-	} else {
-		const endLine = lineRange.endLineNumberExclusive - 1;
-		return new Range(lineRange.startLineNumber, 1, endLine, model.getLineMaxColumn(endLine));
-	}
+	const result: LineRange[] = [];
+	result.push(new LineRange(1, range.startLineNumber));
+	result.push(new LineRange(range.endLineNumberExclusive, model.getLineCount()));
+	return result.filter(r => !r.isEmpty);
 }
