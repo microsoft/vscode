d8cc1d33d0970b0b0cba3420429075cc172d2282
Alexandru Dima <alexdima@microsoft.com>
2023-04-21 08:48:16 +0200

    Revert "add accessibility verbosity settings for terminal and diff editor aria hints"

--- src/vs/editor/browser/widget/diffEditorWidget.ts
+++ src/vs/editor/browser/widget/diffEditorWidget.ts
@@ -58,7 +58,6 @@ import { defaultInsertColor, defaultRemoveColor, diffDiagonalFill, diffInserted,
 import { registerIcon } from 'vs/platform/theme/common/iconRegistry';
 import { getThemeTypeSelector, IColorTheme, IThemeService, registerThemingParticipant } from 'vs/platform/theme/common/themeService';
 import { ThemeIcon } from 'vs/base/common/themables';
-import { IConfigurationService } from 'vs/platform/configuration/common/configuration';
 
 export interface IDiffCodeEditorWidgetOptions {
 	originalEditor?: ICodeEditorWidgetOptions;
@@ -250,16 +249,11 @@ export class DiffEditorWidget extends Disposable implements editorBrowser.IDiffE
 		@IThemeService themeService: IThemeService,
 		@INotificationService notificationService: INotificationService,
 		@IContextMenuService contextMenuService: IContextMenuService,
-		@IEditorProgressService private readonly _editorProgressService: IEditorProgressService,
-		@IConfigurationService private readonly _configurationService: IConfigurationService
+		@IEditorProgressService private readonly _editorProgressService: IEditorProgressService
 	) {
 		super();
 		codeEditorService.willCreateDiffEditor();
-		this._register(this._configurationService.onDidChangeConfiguration(e => {
-			if (e.affectsConfiguration('accessibility.verbosity.diff-editor')) {
-				this.updateOptions(this._options);
-			}
-		}));
+
 		this._documentDiffProvider = this._register(instantiationService.createInstance(WorkerBasedDocumentDiffProvider, options));
 		this._register(this._documentDiffProvider.onDidChange(e => this._beginUpdateDecorationsSoon()));
 
@@ -1278,7 +1272,7 @@ export class DiffEditorWidget extends Disposable implements editorBrowser.IDiffE
 		if (options.originalAriaLabel) {
 			result.ariaLabel = options.originalAriaLabel;
 		}
-		this._updateAriaLabel(result);
+		result.ariaLabel += ariaNavigationTip;
 		result.readOnly = !this._options.originalEditable;
 		result.dropIntoEditor = { enabled: !result.readOnly };
 		result.extraEditorClassName = 'original-in-monaco-diff-editor';
@@ -1291,22 +1285,12 @@ export class DiffEditorWidget extends Disposable implements editorBrowser.IDiffE
 		};
 	}
 
-	private _updateAriaLabel(options: IEditorConstructionOptions): void {
-		let ariaLabel = options.ariaLabel;
-		if (this._configurationService.getValue('accessibility.verbosity.diff-editor')) {
-			ariaLabel += ariaNavigationTip;
-		} else if (ariaLabel) {
-			ariaLabel = ariaLabel.replaceAll(ariaNavigationTip, '');
-		}
-		options.ariaLabel = ariaLabel;
-	}
-
 	private _adjustOptionsForRightHandSide(options: Readonly<editorBrowser.IDiffEditorConstructionOptions>): IEditorConstructionOptions {
 		const result = this._adjustOptionsForSubEditor(options);
 		if (options.modifiedAriaLabel) {
 			result.ariaLabel = options.modifiedAriaLabel;
 		}
-		this._updateAriaLabel(result);
+		result.ariaLabel += ariaNavigationTip;
 		result.wordWrapOverride1 = this._options.diffWordWrap;
 		result.revealHorizontalRightPadding = EditorOptions.revealHorizontalRightPadding.defaultValue + DiffEditorWidget.ENTIRE_DIFF_OVERVIEW_WIDTH;
 		result.scrollbar!.verticalHasArrows = false;
