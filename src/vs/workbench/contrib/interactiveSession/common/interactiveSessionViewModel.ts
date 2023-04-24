23bca5a35038becb0d815ec50e57806135ad77e8
Rob Lourens <roblourens@gmail.com>
2023-04-24 14:56:13 -0700

    Restore welcome message when restoring chat session

--- src/vs/workbench/contrib/interactiveSession/common/interactiveSessionViewModel.ts
+++ src/vs/workbench/contrib/interactiveSession/common/interactiveSessionViewModel.ts
@@ -10,7 +10,7 @@ import { URI } from 'vs/base/common/uri';
 import { localize } from 'vs/nls';
 import { IInstantiationService } from 'vs/platform/instantiation/common/instantiation';
 import { ILogService } from 'vs/platform/log/common/log';
-import { IInteractiveRequestModel, IInteractiveResponseModel, IInteractiveSessionModel, IInteractiveSessionWelcomeMessageModel, IInteractiveWelcomeMessageContent } from 'vs/workbench/contrib/interactiveSession/common/interactiveSessionModel';
+import { IInteractiveRequestModel, IInteractiveResponseModel, IInteractiveSessionModel, IInteractiveWelcomeMessageContent } from 'vs/workbench/contrib/interactiveSession/common/interactiveSessionModel';
 import { IInteractiveResponseErrorDetails, IInteractiveSessionReplyFollowup, IInteractiveSessionResponseCommandFollowup, InteractiveSessionVoteDirection } from 'vs/workbench/contrib/interactiveSession/common/interactiveSessionService';
 import { countWords } from 'vs/workbench/contrib/interactiveSession/common/interactiveSessionWordCounter';
 
@@ -31,10 +31,9 @@ export interface IInteractiveSessionViewModel {
 	readonly sessionId: string;
 	readonly onDidDisposeModel: Event<void>;
 	readonly onDidChange: Event<void>;
-	readonly welcomeMessage: IInteractiveWelcomeMessageViewModel | undefined;
 	readonly requestInProgress: boolean;
 	readonly inputPlaceholder?: string;
-	getItems(): (IInteractiveRequestViewModel | IInteractiveResponseViewModel)[];
+	getItems(): (IInteractiveRequestViewModel | IInteractiveResponseViewModel | IInteractiveWelcomeMessageViewModel)[];
 }
 
 export interface IInteractiveRequestViewModel {
@@ -93,10 +92,6 @@ export class InteractiveSessionViewModel extends Disposable implements IInteract
 		return this._model.inputPlaceholder;
 	}
 
-	get welcomeMessage() {
-		return this._model.welcomeMessage;
-	}
-
 	get sessionId() {
 		return this._model.sessionId;
 	}
@@ -147,7 +142,7 @@ export class InteractiveSessionViewModel extends Disposable implements IInteract
 	}
 
 	getItems() {
-		return [...this._items];
+		return [...(this._model.welcomeMessage ? [this._model.welcomeMessage] : []), ...this._items];
 	}
 
 	override dispose() {
@@ -325,24 +320,4 @@ export interface IInteractiveWelcomeMessageViewModel {
 	readonly username: string;
 	readonly avatarIconUri?: URI;
 	readonly content: IInteractiveWelcomeMessageContent[];
-}
-
-export class InteractiveWelcomeMessageViewModel implements IInteractiveWelcomeMessageViewModel {
-	get id() {
-		return this._model.id;
-	}
-
-	get username() {
-		return this._model.username;
-	}
-
-	get avatarIconUri() {
-		return this._model.avatarIconUri;
-	}
-
-	get content() {
-		return this._model.content;
-	}
-
-	constructor(readonly _model: IInteractiveSessionWelcomeMessageModel) { }
 }
