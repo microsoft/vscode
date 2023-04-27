dbc17a438c3dfc7daa18440cff43fa8e9c907213
Michael Lively <milively@microsoft.com>
2023-04-27 11:28:13 -0700

    should not push automatically?

--- src/vs/workbench/contrib/notebook/browser/contrib/saveParticipants/saveParticipants.ts
+++ src/vs/workbench/contrib/notebook/browser/contrib/saveParticipants/saveParticipants.ts
@@ -98,6 +98,7 @@ class CodeActionOnSaveParticipant implements IStoredFileWorkingCopySaveParticipa
 	}
 
 	// testing edits here -- more edits for verification
+	//ok final change
 
 	async participate(workingCopy: IStoredFileWorkingCopy<IStoredFileWorkingCopyModel>, context: { reason: SaveReason }, progress: IProgress<IProgressStep>, _token: CancellationToken): Promise<void> {
 		const isTrusted = this.workspaceTrustManagementService.isWorkspaceTrusted();
