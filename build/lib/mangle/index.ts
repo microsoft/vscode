0587382509308e54558eeb1706e43eb37199ec47
Matt Bierner <matb@microsoft.com>
2023-05-04 08:03:21 -0700

    Exempt another file

--- build/lib/mangle/index.ts
+++ build/lib/mangle/index.ts
@@ -326,6 +326,7 @@ const skippedExportMangledProjects = [
 	// These projects use webpack to dynamically rewrite imports, which messes up our mangling
 	'configuration-editing',
 	'microsoft-authentication',
+	'github-authentication',
 ];
 
 class DeclarationData {
