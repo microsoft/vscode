4fcec5e8e35129b21acd247bf335eb2320827fd1
meganrogge <megan.rogge@microsoft.com>
2023-05-09 13:08:13 -0700

    include keybinding

--- src/vs/workbench/contrib/debug/browser/debugActionViewItems.ts
+++ src/vs/workbench/contrib/debug/browser/debugActionViewItems.ts
@@ -75,7 +75,7 @@ export class StartDebugActionViewItem extends BaseActionViewItem {
 		const keybindingLabel = keybinding ? ` (${keybinding})` : '';
 		this.start.title = this.action.label + keybindingLabel;
 		this.start.setAttribute('role', 'button');
-		this.start.ariaLabel = nls.localize('startDebug', "Start Debugging");
+		this.start.ariaLabel = this.start.title;
 
 		this.toDispose.push(dom.addDisposableListener(this.start, dom.EventType.CLICK, () => {
 			this.start.blur();
