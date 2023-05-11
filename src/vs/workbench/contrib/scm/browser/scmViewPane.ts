1a5cb4072ea1bda79c81ec5542e257d71f69f506
Peng Lyu <penn.lv@gmail.com>
2023-05-11 09:28:07 -0700

    Handle arrow key conflicts (#181981)
    * Handle arrow key conflicts
    
    * Fix arrow keys for livepreview mode.
    
    * fix merge issues

--- src/vs/workbench/contrib/scm/browser/scmViewPane.ts
+++ src/vs/workbench/contrib/scm/browser/scmViewPane.ts
@@ -1087,7 +1087,7 @@ class ViewModel {
 		this._onDidChangeMode.fire(mode);
 		this.modeContextKey.set(mode);
 
-		this.storageService.store(`scm.viewMode`, mode, StorageScope.WORKSPACE, StorageTarget.MACHINE);
+		this.storageService.store(`scm.viewMode`, mode, StorageScope.WORKSPACE, StorageTarget.USER);
 	}
 
 	get sortKey(): ViewModelSortKey { return this._sortKey; }
@@ -1103,7 +1103,7 @@ class ViewModel {
 		this.sortKeyContextKey.set(sortKey);
 
 		if (this._mode === ViewModelMode.List) {
-			this.storageService.store(`scm.viewSortKey`, sortKey, StorageScope.WORKSPACE, StorageTarget.MACHINE);
+			this.storageService.store(`scm.viewSortKey`, sortKey, StorageScope.WORKSPACE, StorageTarget.USER);
 		}
 	}
 
