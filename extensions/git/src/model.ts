e55a0a7e77e76854eb2ccab86d4a568603b75929
Ladislau Szomoru <3372902+lszomoru@users.noreply.github.com>
2023-05-15 20:30:05 +0200

    GitHub - fix an issue with toggling branch protection setting

--- extensions/git/src/model.ts
+++ extensions/git/src/model.ts
@@ -152,7 +152,7 @@ export class Model implements IBranchProtectionProviderRegistry, IRemoteSourcePu
 	private _onDidChangePostCommitCommandsProviders = new EventEmitter<void>();
 	readonly onDidChangePostCommitCommandsProviders = this._onDidChangePostCommitCommandsProviders.event;
 
-	private branchProtectionProviders = new Map<Uri, Set<BranchProtectionProvider>>();
+	private branchProtectionProviders = new Map<string, Set<BranchProtectionProvider>>();
 
 	private _onDidChangeBranchProtectionProviders = new EventEmitter<Uri>();
 	readonly onDidChangeBranchProtectionProviders = this._onDidChangeBranchProtectionProviders.event;
@@ -769,17 +769,18 @@ export class Model implements IBranchProtectionProviderRegistry, IRemoteSourcePu
 	registerBranchProtectionProvider(root: Uri, provider: BranchProtectionProvider): Disposable {
 		const providerDisposables: Disposable[] = [];
 
-		this.branchProtectionProviders.set(root, (this.branchProtectionProviders.get(root) ?? new Set()).add(provider));
+		const rootPath = root.fsPath;
+		this.branchProtectionProviders.set(rootPath, (this.branchProtectionProviders.get(rootPath) ?? new Set()).add(provider));
 		providerDisposables.push(provider.onDidChangeBranchProtection(uri => this._onDidChangeBranchProtectionProviders.fire(uri)));
 
 		this._onDidChangeBranchProtectionProviders.fire(root);
 
 		return toDisposable(() => {
-			const providers = this.branchProtectionProviders.get(root);
+			const providers = this.branchProtectionProviders.get(rootPath);
 
 			if (providers && providers.has(provider)) {
 				providers.delete(provider);
-				this.branchProtectionProviders.set(root, providers);
+				this.branchProtectionProviders.set(rootPath, providers);
 				this._onDidChangeBranchProtectionProviders.fire(root);
 			}
 
@@ -788,7 +789,7 @@ export class Model implements IBranchProtectionProviderRegistry, IRemoteSourcePu
 	}
 
 	getBranchProtectionProviders(root: Uri): BranchProtectionProvider[] {
-		return [...(this.branchProtectionProviders.get(root) ?? new Set()).values()];
+		return [...(this.branchProtectionProviders.get(root.fsPath) ?? new Set()).values()];
 	}
 
 	registerPostCommitCommandsProvider(provider: PostCommitCommandsProvider): Disposable {
