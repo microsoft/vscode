5749e3d9ff808fed0730b29589c8b45199dd3e93
Kartik Raj <karraj@microsoft.com>
2023-05-18 18:40:24 +0000

    Revert "Fix error"
    This reverts commit affca3b3f17f044bb63d8e594f6e5c89d050eb81.

--- src/vscode-dts/vscode.proposed.envCollectionWorkspace.d.ts
+++ src/vscode-dts/vscode.proposed.envCollectionWorkspace.d.ts
@@ -7,22 +7,22 @@ declare module 'vscode' {
 
 	// https://github.com/microsoft/vscode/issues/182069
 
-	// export interface ExtensionContext {
-	// 	/**
-	// 	 * Gets the extension's environment variable collection for this workspace, enabling changes
-	// 	 * to be applied to terminal environment variables.
-	// 	 *
-	// 	 * @param scope The scope to which the environment variable collection applies to.
-	// 	 */
-	// 	readonly environmentVariableCollection: EnvironmentVariableCollection & { getScopedEnvironmentVariableCollection(scope: EnvironmentVariableScope): EnvironmentVariableCollection };
-	// }
+	export interface ExtensionContext {
+		/**
+		 * Gets the extension's environment variable collection for this workspace, enabling changes
+		 * to be applied to terminal environment variables.
+		 *
+		 * @param scope The scope to which the environment variable collection applies to.
+		 */
+		readonly environmentVariableCollection: EnvironmentVariableCollection & { getScopedEnvironmentVariableCollection(scope: EnvironmentVariableScope): EnvironmentVariableCollection };
+	}
 
-	// export type EnvironmentVariableScope = {
-	// 	/**
-	// 	* Any specific workspace folder to get collection for. If unspecified, collection applicable to all workspace folders is returned.
-	// 	*/
-	// 	workspaceFolder?: WorkspaceFolder;
-	// };
+	export type EnvironmentVariableScope = {
+		/**
+		* Any specific workspace folder to get collection for. If unspecified, collection applicable to all workspace folders is returned.
+		*/
+		workspaceFolder?: WorkspaceFolder;
+	};
 
 	export interface EnvironmentVariableCollection extends Iterable<[variable: string, mutator: EnvironmentVariableMutator]> {
 		/**
