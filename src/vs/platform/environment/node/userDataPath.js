8c1dc591031a8f8fe7bfb763e189488f2a432793
Roberta Wheeler <rob3rt.233@gmail.com>
2023-05-21 14:23:33 +0800

    Update userDataPath.js

--- src/vs/platform/environment/node/userDataPath.js
+++ src/vs/platform/environment/node/userDataPath.js
@@ -91,13 +91,9 @@
 				case 'darwin':
 					appDataPath = path.join(os.homedir(), 'Library', 'Application Support');
 					break;
-				case 'linux':
-				case 'android':
-				case 'freebsd':
+				default:
 					appDataPath = process.env['XDG_CONFIG_HOME'] || path.join(os.homedir(), '.config');
 					break;
-				default:
-					throw new Error('Platform not supported');
 			}
 
 			return path.join(appDataPath, productName);
