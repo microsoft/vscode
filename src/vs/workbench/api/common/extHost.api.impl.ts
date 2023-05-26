6313571614b0e1c1c90b2391080084e7fa2ca72c
Rob Lourens <roblourens@gmail.com>
2023-05-25 18:32:56 -0700

    Restore proposed API check Now that the copilot chat extension is updated and this won't break it

--- src/vs/workbench/api/common/extHost.api.impl.ts
+++ src/vs/workbench/api/common/extHost.api.impl.ts
@@ -1292,8 +1292,7 @@ export function createApiFactoryAndRegisterActors(accessor: ServicesAccessor): I
 				return extHostChat.sendInteractiveRequestToProvider(providerId, message);
 			},
 			get onDidPerformUserAction() {
-				// TODO this needs to be staged with a copilot chat update
-				// checkProposedApiEnabled(extension, 'interactiveUserActions');
+				checkProposedApiEnabled(extension, 'interactiveUserActions');
 				return extHostChat.onDidPerformUserAction;
 			}
 		};
