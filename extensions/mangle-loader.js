f95de8d210b8ca841b1764ef1a774f726b00ae88
Matt Bierner <matb@microsoft.com>
2023-06-12 23:31:05 -0700

    Add extra argument to Mangler
    Missed updating this

--- extensions/mangle-loader.js
+++ extensions/mangle-loader.js
@@ -25,7 +25,7 @@ function getMangledFileContents(projectPath) {
 	if (!entry) {
 		const log = (...data) => fancyLog(ansiColors.blue('[mangler]'), ...data);
 		log(`Mangling ${projectPath}`);
-		const ts2tsMangler = new Mangler(projectPath, log);
+		const ts2tsMangler = new Mangler(projectPath, log, { mangleExports: true, manglePrivateFields: true });
 		entry = ts2tsMangler.computeNewFileContents();
 		mangleMap.set(projectPath, entry);
 	}
