2166ffffdf168fe20d1d0e6c6d5d63098e38d04d
Benjamin Pasero <benjamin.pasero@microsoft.com>
2023-06-19 15:16:15 +0200

    wip

--- src/vs/workbench/contrib/recorder/browser/recorder.contribution.ts
+++ src/vs/workbench/contrib/recorder/browser/recorder.contribution.ts
@@ -8,6 +8,11 @@ import { Registry } from 'vs/platform/registry/common/platform';
 import { LifecyclePhase } from 'vs/workbench/services/lifecycle/common/lifecycle';
 import { IStatusbarService, StatusbarAlignment } from 'vs/workbench/services/statusbar/browser/statusbar';
 import { CommandsRegistry } from 'vs/platform/commands/common/commands';
+import { triggerDownload } from 'vs/base/browser/dom';
+import { URI } from 'vs/base/common/uri';
+import { Action2, registerAction2 } from 'vs/platform/actions/common/actions';
+import { ServicesAccessor } from 'vs/platform/instantiation/common/instantiation';
+import { IDialogService } from 'vs/platform/dialogs/common/dialogs';
 
 export class RecorderContribution implements IWorkbenchContribution {
 	constructor(
@@ -15,7 +20,8 @@ export class RecorderContribution implements IWorkbenchContribution {
 	) {
 		let audioRecorder: MediaRecorder;
 		let audio: HTMLAudioElement;
-
+		let url: string;
+		let blob: Blob;
 
 		const entry = this.statusbarService.addEntry({
 			name: 'Audio Recorder',
@@ -26,6 +32,11 @@ export class RecorderContribution implements IWorkbenchContribution {
 		}, 'status.recorder', StatusbarAlignment.LEFT, 1000);
 
 		CommandsRegistry.registerCommand('workbench.action.startAudioRecording', async () => {
+			const audioDevice = await navigator.mediaDevices.getUserMedia({ audio: true });
+
+			audioRecorder = new MediaRecorder(audioDevice, { mimeType: 'audio/webm;codecs=opus' });
+			audioRecorder.start();
+
 			entry.update({
 				name: 'Audio Recorder',
 				text: '$(stop-circle) Stop Recording',
@@ -34,17 +45,15 @@ export class RecorderContribution implements IWorkbenchContribution {
 				command: 'workbench.action.stopAudioRecording'
 			});
 
-			const audioDevice = await navigator.mediaDevices.getUserMedia({ audio: true });
-			audioRecorder = new MediaRecorder(audioDevice);
-			audioRecorder.start();
-
 			const chunks: Blob[] = [];
 			audioRecorder.ondataavailable = e => {
 				chunks.push(e.data);
 			};
 
 			audioRecorder.onstop = () => {
-				audio = new Audio(window.URL.createObjectURL(new Blob(chunks, { 'type': 'audio/ogg; codecs=vorbis' })));
+				blob = new Blob(chunks);
+				url = window.URL.createObjectURL(blob);
+				audio = new Audio(url);
 			};
 		});
 
@@ -75,13 +84,61 @@ export class RecorderContribution implements IWorkbenchContribution {
 		CommandsRegistry.registerCommand('workbench.action.stopAudioPlayback', () => {
 			entry.update({
 				name: 'Audio Recorder',
+				text: '$(cloud-download) Download Recording',
+				ariaLabel: 'Download Recording',
+				tooltip: 'Download Recording',
+				command: 'workbench.action.downloadAudioRecording'
+			});
+
+			audio.pause();
+		});
+
+		CommandsRegistry.registerCommand('workbench.action.downloadAudioRecording', () => {
+			entry.update({
+				name: 'Audio Recorder',
 				text: '$(record) Start Recording',
 				ariaLabel: 'Start Recording',
 				tooltip: 'Start Recording',
 				command: 'workbench.action.startAudioRecording'
 			});
 
-			audio.pause();
+			triggerDownload(URI.parse(url), 'recording.webm');
+		});
+
+		registerAction2(class extends Action2 {
+			constructor() {
+				super({
+					id: 'workbench.actions.transscribe',
+					title: { value: 'OpenAI: Transcribe', original: 'OpenAI: Transcribe' },
+					f1: true
+				});
+			}
+
+			async run(accessor: ServicesAccessor): Promise<void> {
+				const dialogService = accessor.get(IDialogService);
+
+				const { values } = await dialogService.input({
+					message: 'Please enter your OpenAI token',
+					inputs: [{
+						type: 'text'
+					}]
+				});
+
+				const formData = new FormData();
+				formData.set('file', blob);
+				formData.set('model', 'whisper-1');
+
+				const response = await fetch('https://api.openai.com/v1/audio/transcriptions', {
+					method: 'POST',
+					body: formData,
+					headers: {
+						'Authorization': `Bearer ${values![0]}`,
+						'Content-Type': 'multipart/form-data'
+					}
+				});
+
+				console.log(response);
+			}
 		});
 	}
 }
