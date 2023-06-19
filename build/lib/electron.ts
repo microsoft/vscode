ee7bdc1ec158435d75661d3b70149cfc3bd604fa
deepak1556 <hop2deep@gmail.com>
2023-06-19 21:07:30 +0900

    chore: add checksum for electron assets

--- build/lib/electron.ts
+++ build/lib/electron.ts
@@ -191,7 +191,9 @@ export const config = {
 	linuxExecutableName: product.applicationName,
 	winIcon: 'resources/win32/code.ico',
 	token: process.env['GITHUB_TOKEN'],
-	repo: product.electronRepository || undefined
+	repo: product.electronRepository || undefined,
+	validateChecksum: true,
+	checksumFile: path.join(root, 'build', 'checksums', 'electron.txt'),
 };
 
 function getElectron(arch: string): () => NodeJS.ReadWriteStream {
