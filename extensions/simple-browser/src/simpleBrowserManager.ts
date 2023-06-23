9394015308bc92e2d0e344d83fdcbafac9456cdb
SecondPort <gomezmanulm@gmail.com>
2023-06-22 23:54:37 -0300

    issue #182795

--- extensions/simple-browser/src/simpleBrowserManager.ts
+++ extensions/simple-browser/src/simpleBrowserManager.ts
@@ -9,6 +9,7 @@ import { ShowOptions, SimpleBrowserView } from './simpleBrowserView';
 export class SimpleBrowserManager {
 
 	private _activeView?: SimpleBrowserView;
+	private _restoring: boolean = false; // Add this line to track if we are in the restore process
 
 	constructor(
 		private readonly extensionUri: vscode.Uri,
@@ -20,6 +21,11 @@ export class SimpleBrowserManager {
 	}
 
 	public show(inputUri: string | vscode.Uri, options?: ShowOptions): void {
+		// Check if we are in the restoration process. If so, it just returns and does nothing.
+		if (this._restoring) {
+			return;
+		}
+
 		const url = typeof inputUri === 'string' ? inputUri : inputUri.toString(true);
 		if (this._activeView) {
 			this._activeView.show(url, options);
@@ -32,10 +38,14 @@ export class SimpleBrowserManager {
 	}
 
 	public restore(panel: vscode.WebviewPanel, state: any): void {
+		this._restoring = true; // Mark that we are in the restoration process
+
 		const url = state?.url ?? '';
 		const view = SimpleBrowserView.restore(this.extensionUri, url, panel);
 		this.registerWebviewListeners(view);
 		this._activeView ??= view;
+
+		this._restoring = false; // Mark that we are done with restoration
 	}
 
 	private registerWebviewListeners(view: SimpleBrowserView) {
