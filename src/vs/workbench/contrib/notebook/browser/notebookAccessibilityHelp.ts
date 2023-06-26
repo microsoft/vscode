9a3ddb3bf7b0c75121f5e3891637f6ec6c6c4340
Aaron Munger <aamunger@microsoft.com>
2023-06-26 15:28:15 -0700

    more content

--- src/vs/workbench/contrib/notebook/browser/notebookAccessibilityHelp.ts
+++ src/vs/workbench/contrib/notebook/browser/notebookAccessibilityHelp.ts
@@ -14,7 +14,22 @@ export function getAccessibilityHelpText(accessor: ServicesAccessor): string {
 	const keybindingService = accessor.get(IKeybindingService);
 	const content = [];
 	content.push(localize('notebook.overview', 'The notebook view is a collection of code and markdown cells. Code cells can be executed and will produce output directly below the cell.'));
-	content.push(descriptionForCommand('notebook.execute', localize('notebook.cell.executeAndFocusContainer', 'The Execute Cell command ({0}) executes the cell that currently has focus.',), localize('notebook.cell.executeAndFocusContainerNoKb', 'The Execute Cell command executes the cell that currently has focus and is currently not triggerable by a keybinding.'), keybindingService));
+	content.push(descriptionForCommand('notebook.cell.edit',
+		localize('notebook.cell.edit', 'The Edit Cell command ({0}) will focus on the cell input.'),
+		localize('notebook.cell.editNoKb', 'The Edit Cell command will focus on the cell input and is currently not triggerable by a keybinding.'), keybindingService));
+	content.push(descriptionForCommand('notebook.cell.quitEdit',
+		localize('notebook.cell.quitEdit', 'The Quit Edit command ({0}) will set focus on the cell container.'), // pressing escape twice may be necessary for screen reader users
+		localize('notebook.cell.quitEditNoKb', 'The Quit Edit command will set focus on the cell container and is currently not triggerable by a keybinding.'), keybindingService));
+	content.push(descriptionForCommand('notebook.cell.focusInOutput',
+		localize('notebook.cell.focusInOutput', 'The Focus Output command ({0}) will set focus in the cell\'s output.'),
+		localize('notebook.cell.focusInOutputNoKb', 'The Quit Edit command will set focus in the cell\'s output and is currently not triggerable by a keybinding.'), keybindingService));
+	content.push(localize('notebook.cellNavigation', 'The up and down arrows will move focus between cells while focused on the outer cell container'));
+	content.push(descriptionForCommand('notebook.cell.executeAndFocusContainer',
+		localize('notebook.cell.executeAndFocusContainer', 'The Execute Cell command ({0}) executes the cell that currently has focus.',),
+		localize('notebook.cell.executeAndFocusContainerNoKb', 'The Execute Cell command executes the cell that currently has focus and is currently not triggerable by a keybinding.'), keybindingService));
+	content.push(localize('notebook.cell.insertCodeCellBelowAndFocusContainer', 'The Insert Cell Above/Below commands will create new empty code cells'));
+	content.push(localize('notebook.changeCellType', 'The Change Cell to Code/Markdown commands are used to switch between cell types.'));
+
 
 	return content.join('\n');
 }
