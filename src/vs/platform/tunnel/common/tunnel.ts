a803196aafae4077f3f17c722f942f738b5da78d
Josh Abernathy <josh@github.com>
2023-06-28 17:07:26 -0400

    Await the tunnel promise

--- src/vs/platform/tunnel/common/tunnel.ts
+++ src/vs/platform/tunnel/common/tunnel.ts
@@ -416,7 +416,7 @@ export abstract class AbstractTunnelService implements ITunnelService {
 		const hostMap = this._tunnels.get(remoteHost);
 		if (hostMap) {
 			const tunnel = hostMap.get(remotePort);
-			const tunnelResult = await tunnel;
+			const tunnelResult = await tunnel.value;
 			if (!tunnelResult) {
 				hostMap.delete(remotePort);
 			}
