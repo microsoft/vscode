b17ce63968bdc18ad7f9694119505b6030c5b702
Aiday Marlen Kyzy <amarlenkyzy@microsoft.com>
2023-07-11 15:43:18 +0200

    changing the code so as to use dom.Dimension instead of storing in two separate fields

--- src/vs/editor/contrib/hover/browser/contentHover.ts
+++ src/vs/editor/contrib/hover/browser/contentHover.ts
@@ -459,8 +459,7 @@ const CONTAINER_HEIGHT_PADDING = 6;
 export class ContentHoverWidget extends ResizableContentWidget {
 
 	public static ID = 'editor.contrib.resizableContentHoverWidget';
-	private static _lastHeight: number = 0;
-	private static _lastWidth: number = 0;
+	private static _lastDimensions: dom.Dimension = new dom.Dimension(0, 0);
 
 	private _visibleData: ContentHoverVisibleData | undefined;
 	private _positionPreference: ContentWidgetPositionPreference | undefined;
@@ -584,8 +583,7 @@ export class ContentHoverWidget extends ResizableContentWidget {
 	}
 
 	protected override _resize(size: dom.Dimension): void {
-		ContentHoverWidget._lastHeight = size.height;
-		ContentHoverWidget._lastWidth = size.width;
+		ContentHoverWidget._lastDimensions = new dom.Dimension(size.width, size.height);
 		this._setAdjustedHoverWidgetDimensions(size);
 		this._resizableNode.layout(size.height, size.width);
 		this._setResizableNodeMaxDimensions();
@@ -658,8 +656,8 @@ export class ContentHoverWidget extends ResizableContentWidget {
 	}
 
 	private _layout(): void {
-		const height = Math.max(this._editor.getLayoutInfo().height / 4, 250, ContentHoverWidget._lastHeight);
-		const width = Math.max(this._editor.getLayoutInfo().width * 0.66, 500, ContentHoverWidget._lastWidth);
+		const height = Math.max(this._editor.getLayoutInfo().height / 4, 250, ContentHoverWidget._lastDimensions.height);
+		const width = Math.max(this._editor.getLayoutInfo().width * 0.66, 500, ContentHoverWidget._lastDimensions.width);
 		const { fontSize, lineHeight } = this._editor.getOption(EditorOption.fontInfo);
 		const contentsDomNode = this._hover.contentsDomNode;
 		contentsDomNode.style.fontSize = `${fontSize}px`;
@@ -685,8 +683,8 @@ export class ContentHoverWidget extends ResizableContentWidget {
 	}
 
 	private _updateContentsDomNodeMaxDimensions() {
-		const width = Math.max(this._editor.getLayoutInfo().width * 0.66, 500, ContentHoverWidget._lastWidth);
-		const height = Math.max(this._editor.getLayoutInfo().height / 4, 250, ContentHoverWidget._lastHeight);
+		const height = Math.max(this._editor.getLayoutInfo().height / 4, 250, ContentHoverWidget._lastDimensions.height);
+		const width = Math.max(this._editor.getLayoutInfo().width * 0.66, 500, ContentHoverWidget._lastDimensions.width);
 		this._setContentsDomNodeMaxDimensions(width, height);
 	}
 
