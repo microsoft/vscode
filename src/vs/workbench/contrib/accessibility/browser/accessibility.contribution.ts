3dc0d6d7b406ecd11e75dc95194107b5cac494d6
meganrogge <megan.rogge@microsoft.com>
2023-07-12 08:34:52 -0700

    fix #187687

--- src/vs/workbench/contrib/accessibility/browser/accessibility.contribution.ts
+++ src/vs/workbench/contrib/accessibility/browser/accessibility.contribution.ts
@@ -32,7 +32,7 @@ class AccessibilityHelpProvider implements IAccessibleContentProvider {
 		this._editor.focus();
 	}
 	options: IAccessibleViewOptions = { type: AccessibleViewType.HelpMenu, ariaLabel: localize('editor-help', "editor accessibility help"), readMoreUrl: 'https://go.microsoft.com/fwlink/?linkid=851010' };
-	id: string = 'editor';
+	verbositySettingKey: string = 'editor';
 	constructor(
 		private readonly _editor: ICodeEditor,
 		@IKeybindingService private readonly _keybindingService: IKeybindingService
@@ -115,7 +115,7 @@ class HoverAccessibleViewContribution extends Disposable {
 				return false;
 			}
 			accessibleViewService.show({
-				id: 'hover',
+				verbositySettingKey: 'hover',
 				provideContent() { return content; },
 				onClose() {
 					controller.focus();
