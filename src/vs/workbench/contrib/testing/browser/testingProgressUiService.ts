1ce4bf8040e9a4258ea8245dee17eb1c011a8d19
gjsjohnmurray <johnm@georgejames.com>
2023-07-21 12:18:10 +0100

    Show placeholder text in testing view progress area (fix #141181)

--- src/vs/workbench/contrib/testing/browser/testingProgressUiService.ts
+++ src/vs/workbench/contrib/testing/browser/testingProgressUiService.ts
@@ -115,7 +115,7 @@ export class TestingProgressUiService extends Disposable implements ITestingProg
 				this.updateCountsEmitter.fire(collected);
 				this.updateTextEmitter.fire(getTestProgressText(false, collected));
 			} else {
-				this.updateTextEmitter.fire('');
+				this.updateTextEmitter.fire(localize('testProgress.noneYet', 'Progress will report here'));
 				this.updateCountsEmitter.fire(collectTestStateCounts(false));
 			}
 
