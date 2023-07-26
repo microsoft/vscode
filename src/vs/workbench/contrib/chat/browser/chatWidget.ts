c59da6def09e0fecfab19b8d19f9a3794926b9ab
Joyce Er <joyce.er@microsoft.com>
2023-07-26 11:00:12 -0700

    Fix `/clear` from suggest widget

--- src/vs/workbench/contrib/chat/browser/chatWidget.ts
+++ src/vs/workbench/contrib/chat/browser/chatWidget.ts
@@ -434,7 +434,7 @@ export class ChatWidget extends Disposable implements IChatWidget {
 			const editorValue = this.inputPart.inputEditor.getValue();
 
 			// Shortcut for /clear command
-			if (!query && editorValue.trim() === '/clear') {
+			if (editorValue.trim() === '/clear' || typeof query === 'string' && query.trim() === '/clear') {
 				// Small hack, if this becomes a repeated pattern, we should have a real internal slash command provider system
 				this.instantiationService.invokeFunction(clearChatSession, this);
 				return;
