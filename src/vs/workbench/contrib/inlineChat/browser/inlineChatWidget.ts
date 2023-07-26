5aed09c897b48219dbaea241bb8edfcaf582fe1b
Joyce Er <joyce.er@microsoft.com>
2023-07-26 14:43:26 -0700

    Executing slash command should not select command

--- src/vs/workbench/contrib/inlineChat/browser/inlineChatWidget.ts
+++ src/vs/workbench/contrib/inlineChat/browser/inlineChatWidget.ts
@@ -465,8 +465,16 @@ export class InlineChatWidget {
 		this._inputEditor.setPosition(this._inputModel.getFullModelRange().getEndPosition());
 	}
 
-	selectAll() {
-		this._inputEditor.setSelection(this._inputModel.getFullModelRange());
+	selectAll(includeSlashCommand: boolean = true) {
+		let selection = this._inputModel.getFullModelRange();
+
+		if (!includeSlashCommand) {
+			const firstLine = this._inputModel.getLineContent(1);
+			const slashCommand = this._slashCommandDetails.find(c => firstLine.startsWith(`/${c.command} `));
+			selection = slashCommand ? new Range(1, slashCommand.command.length + 3, selection.endLineNumber, selection.endColumn) : selection;
+		}
+
+		this._inputEditor.setSelection(selection);
 	}
 
 	set placeholder(value: string) {
