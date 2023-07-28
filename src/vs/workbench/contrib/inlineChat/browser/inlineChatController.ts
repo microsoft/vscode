8a32813449f0a7bbd95512f6f6ae001e1f2f9443
Aiday Marlen Kyzy <amarlenkyzy@microsoft.com>
2023-07-28 12:45:32 +0200

    cleaning the code

--- src/vs/workbench/contrib/inlineChat/browser/inlineChatController.ts
+++ src/vs/workbench/contrib/inlineChat/browser/inlineChatController.ts
@@ -198,7 +198,6 @@ export class InlineChatController implements IEditorContribution {
 	// ---- state machine
 
 	private _showWidget(initialRender: boolean = false, position?: IPosition) {
-		console.log('position inside of _showWidget : ', position);
 		assertType(this._editor.hasModel());
 
 		let widgetPosition: Position;
@@ -491,7 +490,6 @@ export class InlineChatController implements IEditorContribution {
 			wholeRange: this._activeSession.wholeRange.value,
 			live: this._activeSession.editMode !== EditMode.Preview // TODO@jrieken let extension know what document is used for previewing
 		};
-		console.log('request from make request : ', request);
 		this._chatAccessibilityService.acceptRequest();
 
 		const progressEdits: TextEdit[][] = [];
