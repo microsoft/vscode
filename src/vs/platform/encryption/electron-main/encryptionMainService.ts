6b9f367f346753cd7dd7e2154877e3bc183d2b71
Benjamin Pasero <benjamin.pasero@microsoft.com>
2023-08-02 16:49:07 +0200

    disable

--- src/vs/platform/encryption/electron-main/encryptionMainService.ts
+++ src/vs/platform/encryption/electron-main/encryptionMainService.ts
@@ -69,11 +69,7 @@ export class EncryptionMainService implements IEncryptionMainService {
 	}
 
 	isEncryptionAvailable(): Promise<boolean> {
-		this.logService.info('[EncryptionMainService] isEncryptionAvailable - before from: ' + new Error().stack);
-		const res = Promise.resolve(safeStorage.isEncryptionAvailable());
-		this.logService.info('[EncryptionMainService] isEncryptionAvailable - after');
-
-		return res;
+		return Promise.resolve(false);
 	}
 
 	getKeyStorageProvider(): Promise<KnownStorageProvider> {
