361d2f0438c7f6ae326122ae7657ad99364f516c
Johannes <johannes.rieken@gmail.com>
2023-08-11 14:22:55 +0200

    more explicit error handling/logging when post messages fails
    https://github.com/microsoft/vscode/issues/188347

--- src/vs/base/browser/defaultWorkerFactory.ts
+++ src/vs/base/browser/defaultWorkerFactory.ts
@@ -4,6 +4,7 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { createTrustedTypesPolicy } from 'vs/base/browser/trustedTypes';
+import { onUnexpectedError } from 'vs/base/common/errors';
 import { COI } from 'vs/base/common/network';
 import { IWorker, IWorkerCallback, IWorkerFactory, logOnceWebWorkerWarning } from 'vs/base/common/worker/simpleWorker';
 
@@ -113,7 +114,13 @@ class WebWorker implements IWorker {
 	}
 
 	public postMessage(message: any, transfer: Transferable[]): void {
-		this.worker?.then(w => w.postMessage(message, transfer));
+		this.worker?.then(w => {
+			try {
+				w.postMessage(message, transfer);
+			} catch (err) {
+				onUnexpectedError(err);
+			}
+		});
 	}
 
 	public dispose(): void {
