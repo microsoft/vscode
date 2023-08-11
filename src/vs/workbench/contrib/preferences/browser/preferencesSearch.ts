c3b778ca32b719d89e4f6ea5be698e2b6e75398d
Raymond Zhao <7199958+rzhao271@users.noreply.github.com>
2023-08-11 09:52:32 -0700

    Add similarity provider

--- src/vs/workbench/contrib/preferences/browser/preferencesSearch.ts
+++ src/vs/workbench/contrib/preferences/browser/preferencesSearch.ts
@@ -3,7 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { ISettingsEditorModel, ISetting, ISettingsGroup, ISearchResult, IGroupFilter, SettingMatchType } from 'vs/workbench/services/preferences/common/preferences';
+import { ISettingsEditorModel, ISetting, ISettingsGroup, ISearchResult, IGroupFilter, SettingMatchType, ISettingMatch } from 'vs/workbench/services/preferences/common/preferences';
 import { IRange } from 'vs/editor/common/core/range';
 import { distinct } from 'vs/base/common/arrays';
 import * as strings from 'vs/base/common/strings';
@@ -19,8 +19,8 @@ import { IWorkbenchExtensionEnablementService } from 'vs/workbench/services/exte
 import { CancellationToken } from 'vs/base/common/cancellation';
 import { ExtensionType } from 'vs/platform/extensions/common/extensions';
 import { IConfigurationService } from 'vs/platform/configuration/common/configuration';
-import { IProductService } from 'vs/platform/product/common/productService';
 import { InstantiationType, registerSingleton } from 'vs/platform/instantiation/common/extensions';
+import { ISemanticSimilarityService } from 'vs/workbench/services/semanticSimilarity/common/semanticSimilarityService';
 
 export interface IEndpointDetails {
 	urlBase?: string;
@@ -36,7 +36,6 @@ export class PreferencesSearchService extends Disposable implements IPreferences
 	constructor(
 		@IInstantiationService private readonly instantiationService: IInstantiationService,
 		@IConfigurationService private readonly configurationService: IConfigurationService,
-		@IProductService private readonly productService: IProductService,
 		@IExtensionManagementService private readonly extensionManagementService: IExtensionManagementService,
 		@IWorkbenchExtensionEnablementService private readonly extensionEnablementService: IWorkbenchExtensionEnablementService
 	) {
@@ -52,39 +51,29 @@ export class PreferencesSearchService extends Disposable implements IPreferences
 		});
 	}
 
-	// @ts-expect-error disable remote search for now, ref https://github.com/microsoft/vscode/issues/172411
 	private get remoteSearchAllowed(): boolean {
 		const workbenchSettings = this.configurationService.getValue<IWorkbenchSettingsConfiguration>().workbench.settings;
-		if (!workbenchSettings.enableNaturalLanguageSearch) {
-			return false;
-		}
-
-		return !!this._endpoint.urlBase;
+		return workbenchSettings.enableNaturalLanguageSearch;
 	}
 
-	private get _endpoint(): IEndpointDetails {
-		const workbenchSettings = this.configurationService.getValue<IWorkbenchSettingsConfiguration>().workbench.settings;
-		if (workbenchSettings.naturalLanguageSearchEndpoint) {
-			return {
-				urlBase: workbenchSettings.naturalLanguageSearchEndpoint,
-				key: workbenchSettings.naturalLanguageSearchKey
-			};
-		} else {
-			return {
-				urlBase: this.productService.settingsSearchUrl
-			};
-		}
-	}
-
-	getRemoteSearchProvider(filter: string, newExtensionsOnly = false): ISearchProvider | undefined {
-		// Disable for now because Bing search isn't supported anymore.
-		// Ref https://github.com/microsoft/vscode/issues/172411
-		return undefined;
+	getRemoteSearchProvider(filter: string, newExtensionsOnly = false): RemoteSearchProvider | undefined {
+		return this.remoteSearchAllowed ?
+			this.instantiationService.createInstance(RemoteSearchProvider, filter) :
+			undefined;
 	}
 
 	getLocalSearchProvider(filter: string): LocalSearchProvider {
 		return this.instantiationService.createInstance(LocalSearchProvider, filter);
 	}
+}
+
+function cleanFilter(filter: string): string {
+	// Remove " and : which are likely to be copypasted as part of a setting name.
+	// Leave other special characters which the user might want to search for.
+	return filter
+		.replace(/[":]/g, ' ')
+		.replace(/  /g, ' ')
+		.trim();
 }
 
 export class LocalSearchProvider implements ISearchProvider {
@@ -95,12 +84,7 @@ export class LocalSearchProvider implements ISearchProvider {
 		private _filter: string,
 		@IConfigurationService private readonly configurationService: IConfigurationService
 	) {
-		// Remove " and : which are likely to be copypasted as part of a setting name.
-		// Leave other special characters which the user might want to search for.
-		this._filter = this._filter
-			.replace(/[":]/g, ' ')
-			.replace(/  /g, ' ')
-			.trim();
+		this._filter = cleanFilter(this._filter);
 	}
 
 	searchModel(preferencesModel: ISettingsEditorModel, token?: CancellationToken): Promise<ISearchResult | null> {
@@ -302,6 +286,58 @@ export class SettingMatches {
 			endLineNumber: setting.valueRange.startLineNumber,
 			endColumn: setting.valueRange.startColumn + match.end + 1
 		};
+	}
+}
+
+export class RemoteSearchProvider implements ISearchProvider {
+	private static readonly SEMANTIC_SIMILARITY_THRESHOLD = 0.8;
+	private static readonly SEMANTIC_SIMILARITY_MAX_PICKS = 10;
+
+	constructor(
+		private _filter: string,
+		@ISemanticSimilarityService private readonly semanticSimilarityService: ISemanticSimilarityService,
+	) {
+		this._filter = cleanFilter(this._filter);
+	}
+
+	searchModel(preferencesModel: ISettingsEditorModel, token?: CancellationToken | undefined): Promise<ISearchResult | null> {
+		if (!this.semanticSimilarityService.isEnabled()) {
+			return Promise.resolve(null);
+		}
+
+		const settingKeys: string[] = [];
+		const settingMap: Record<string, ISetting> = {};
+		for (const group of preferencesModel.settingsGroups) {
+			for (const section of group.sections) {
+				for (const setting of section.settings) {
+					settingKeys.push(setting.key);
+					settingMap[setting.key] = setting;
+				}
+			}
+		}
+		return this.semanticSimilarityService.getSimilarityScore(this._filter, settingKeys, token ?? CancellationToken.None).then((scores) => {
+			const filterMatches: ISettingMatch[] = [];
+			const sortedIndices = scores.map((_, i) => i).sort((a, b) => scores[b] - scores[a]);
+			let numOfSmartPicks = 0;
+			for (const i of sortedIndices) {
+				const score = scores[i];
+				if (score < RemoteSearchProvider.SEMANTIC_SIMILARITY_THRESHOLD || numOfSmartPicks === RemoteSearchProvider.SEMANTIC_SIMILARITY_MAX_PICKS) {
+					break;
+				}
+
+				const pick = settingKeys[i];
+				filterMatches.push({
+					setting: settingMap[pick],
+					matches: [settingMap[pick].range],
+					matchType: SettingMatchType.RemoteMatch,
+					score
+				});
+				numOfSmartPicks++;
+			}
+			return {
+				filterMatches
+			};
+		});
 	}
 }
 
