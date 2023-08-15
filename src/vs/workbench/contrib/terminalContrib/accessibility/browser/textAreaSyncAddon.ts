e11d85a9218c9377b4dcba574a303f1a00b08c52
meganrogge <megan.rogge@microsoft.com>
2023-08-15 11:16:48 -0700

    use MutableDisposable

--- src/vs/workbench/contrib/terminalContrib/accessibility/browser/textAreaSyncAddon.ts
+++ src/vs/workbench/contrib/terminalContrib/accessibility/browser/textAreaSyncAddon.ts
@@ -3,7 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { Disposable } from 'vs/base/common/lifecycle';
+import { Disposable, MutableDisposable } from 'vs/base/common/lifecycle';
 import { IAccessibilityService } from 'vs/platform/accessibility/common/accessibility';
 import { ITerminalCapabilityStore, TerminalCapability } from 'vs/platform/terminal/common/capabilities/capabilities';
 import type { Terminal, ITerminalAddon } from 'xterm';
@@ -15,9 +15,12 @@ export interface ITextAreaData {
 
 export class TextAreaSyncAddon extends Disposable implements ITerminalAddon {
 	private _terminal: Terminal | undefined;
+	private _onCursorMoveListener = this._register(new MutableDisposable());
 	activate(terminal: Terminal): void {
 		this._terminal = terminal;
-		this._register(this._terminal.onCursorMove(() => this._refreshTextArea()));
+		if (this._accessibilityService.isScreenReaderOptimized()) {
+			this._onCursorMoveListener.value = this._terminal.onCursorMove(() => this._refreshTextArea());
+		}
 	}
 
 	constructor(
@@ -25,47 +28,65 @@ export class TextAreaSyncAddon extends Disposable implements ITerminalAddon {
 		@IAccessibilityService private readonly _accessibilityService: IAccessibilityService
 	) {
 		super();
+		this._register(this._accessibilityService.onDidChangeScreenReaderOptimized(() => {
+			if (this._accessibilityService.isScreenReaderOptimized() && this._terminal) {
+				this._refreshTextArea()
+				this._onCursorMoveListener.value = this._terminal.onCursorMove(() => this._refreshTextArea());
+			} else {
+				this._onCursorMoveListener.clear();
+			}
+		}));
 	}
 
 	private _refreshTextArea(): void {
 		if (!this._terminal) {
 			return;
 		}
-		if (this._accessibilityService.isScreenReaderOptimized()) {
-			const commandCapability = this._capabilities.get(TerminalCapability.CommandDetection);
-			const currentCommand = commandCapability?.currentCommand;
-			if (!currentCommand) {
-				return;
-			}
-			const buffer = this._terminal.buffer.active;
-			const line = buffer.getLine(buffer.cursorY)?.translateToString(true);
-			let content: string = '';
-			let commandStartX: number | undefined;
-			if (!line) {
-				return;
-			}
-			if (currentCommand.commandStartX) {
-				// Left prompt
-				content = line.substring(currentCommand.commandStartX).trim();
-				commandStartX = currentCommand.commandStartX;
-			} else if (currentCommand.commandRightPromptStartX) {
-				// Right prompt
-				content = line.substring(0, currentCommand.commandRightPromptStartX).trim();
-				commandStartX = 0;
-			}
-			if (commandStartX) {
-				const textArea = this._terminal.textarea;
-				if (!textArea) {
-					return;
-				}
-				// Sync the textarea using shell integration
-				textArea.textContent = content;
-				const cursorX = buffer.cursorX - commandStartX;
-				textArea.selectionStart = cursorX;
-				textArea.selectionEnd = cursorX;
-				// TODO: cursorY?
-			}
-		}
-	}
 
+		const commandCapability = this._capabilities.get(TerminalCapability.CommandDetection);
+		const currentCommand = commandCapability?.currentCommand;
+		if (!currentCommand) {
+			return;
+		}
+		const buffer = this._terminal.buffer.active;
+		const line = buffer.getLine(buffer.cursorY)?.translateToString(true);
+		let commandStartX: number | undefined;
+		if (!line) {
+			return;
+		}
+		let content: string | undefined;
+		if (currentCommand.commandStartX) {
+			// Left prompt
+			content = line.substring(currentCommand.commandStartX);
+			commandStartX = currentCommand.commandStartX;
+		} else if (currentCommand.commandRightPromptStartX) {
+			// Right prompt
+			content = line.substring(0, currentCommand.commandRightPromptStartX);
+			commandStartX = 0;
+		}
+
+		if (!content) {
+			return;
+		}
+
+		if (commandStartX === undefined) {
+			return;
+		}
+
+		const textArea = this._terminal.textarea;
+		if (!textArea) {
+			return;
+		}
+
+		if (content !== textArea.textContent) {
+			textArea.textContent = content;
+		}
+
+		const cursorX = buffer.cursorX - commandStartX;
+		if (cursorX !== textArea.selectionStart) {
+			textArea.selectionStart = cursorX;
+			textArea.selectionEnd = cursorX;
+		}
+		// TODO: cursorY?
+	}
 }
