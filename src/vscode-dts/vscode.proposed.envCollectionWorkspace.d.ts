02c388ce8896104e1560f009954e93361089f099
Kartik Raj <karraj@microsoft.com>
2023-08-15 23:01:29 +0000

    Update scoped environment collection API

--- src/vscode-dts/vscode.proposed.envCollectionWorkspace.d.ts
+++ src/vscode-dts/vscode.proposed.envCollectionWorkspace.d.ts
@@ -9,25 +9,30 @@ declare module 'vscode' {
 
 	export interface ExtensionContext {
 		/**
-		 * Gets the extension's environment variable collection for this workspace, enabling changes
-		 * to be applied to terminal environment variables.
-		 *
-		 * @deprecated Use {@link getEnvironmentVariableCollection} instead.
+		 * Gets the extension's global environment variable collection for this workspace, enabling changes to be
+		 * applied to terminal environment variables.
 		 */
-		readonly environmentVariableCollection: EnvironmentVariableCollection;
+		readonly environmentVariableCollection: GlobalEnvironmentVariableCollection;
+	}
+
+	interface GlobalEnvironmentVariableCollection extends EnvironmentVariableCollection {
 		/**
-		 * Gets the extension's environment variable collection for this scope, enabling changes
-		 * to be applied to terminal environment variables.
+		 * Gets scope-specific environment variable collection for the extension. This enables alterations to
+		 * terminal environment variables solely within the designated scope, and is applied in addition to (and
+		 * after) the global collection.
+		 *
+		 * Each object obtained through this method is isolated and does not impact objects for other scopes,
+		 * including the global collection.
 		 *
 		 * @param scope The scope to which the environment variable collection applies to.
 		 */
-		getEnvironmentVariableCollection(scope?: EnvironmentVariableScope): EnvironmentVariableCollection;
+		getScoped(scope: EnvironmentVariableScope): EnvironmentVariableCollection;
 	}
 
 	export type EnvironmentVariableScope = {
 		/**
-		* Any specific workspace folder to get collection for. If unspecified, collection applicable to all workspace folders is returned.
-		*/
+		 * Any specific workspace folder to get collection for. If unspecified, collection applicable to all workspace folders is returned.
+		 */
 		workspaceFolder?: WorkspaceFolder;
 	};
 }
