f0f36dff5efa066df997197e1d3a78e2727b70a9
Daniel Imms <2193314+Tyriar@users.noreply.github.com>
2023-08-18 08:19:47 -0700

    Remove commented out setting

--- src/vs/workbench/contrib/accessibility/browser/accessibilityConfiguration.ts
+++ src/vs/workbench/contrib/accessibility/browser/accessibilityConfiguration.ts
@@ -95,14 +95,6 @@ const configuration: IConfigurationNode = {
 			description: localize('verbosity.editor.untitledhint', 'Provide information about relevant actions in an untitled text editor.'),
 			...baseProperty
 		}
-		// [AccessibilitySettingId.UnfocusedViewOpacity]: {
-		// 	description: localize('unfocusedViewOpacity', 'The opacity fraction (0.2 to 1.0) to use for unfocused editors and terminals. This will dim inactive views to make the focused view more obvious.'),
-		// 	type: 'number',
-		// 	minimum: 0.2,
-		// 	maximum: 1,
-		// 	default: 1,
-		// 	tags: ['accessibility']
-		// }
 	}
 };
 
