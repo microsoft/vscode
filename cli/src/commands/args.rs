1fe8359ed0ef9c52bd6986565da97c395607a130
Connor Peet <connor@peet.io>
2023-08-22 17:29:51 -0700

    cli: implement 'server of server' for a local web server (#191014)
    Closes https://github.com/microsoft/vscode/issues/168492
    
    This implements @aeschli's 'server server' concept in a new
    `code serve-web` command.
    
    Command line args are similar to the standalone web server. The first
    time a user hits that page, the latest version of the VS Code web server
    will be downloaded and run. Thanks to Martin's previous PRs, all
    resources the page requests are prefixed with `/<quality-<commit>`.
    
    The latest release version is cached, but when the page is loaded again
    and there's a new release, a the new server version will be downloaded
    and started up.
    
    Behind the scenes the servers all listen on named pipes/sockets and the
    CLI acts as a proxy server to those sockets. Servers without connections
    for an hour will be shut down automatically.

--- cli/src/commands/args.rs
+++ cli/src/commands/args.rs
@@ -172,9 +172,40 @@ pub enum Commands {
 	/// Changes the version of the editor you're using.
 	Version(VersionArgs),
 
+	/// Runs a local web version of VS Code.
+	ServeWeb(ServeWebArgs),
+
 	/// Runs the control server on process stdin/stdout
 	#[clap(hide = true)]
 	CommandShell(CommandShellArgs),
+}
+
+#[derive(Args, Debug, Clone)]
+pub struct ServeWebArgs {
+	/// Host to listen on, defaults to 'localhost'
+	#[clap(long)]
+	pub host: Option<String>,
+	/// Port to listen on. If 0 is passed a random free port is picked.
+	#[clap(long, default_value_t = 8000)]
+	pub port: u16,
+	/// A secret that must be included with all requests.
+	#[clap(long)]
+	pub connection_token: Option<String>,
+	/// Run without a connection token. Only use this if the connection is secured by other means.
+	#[clap(long)]
+	pub without_connection_token: bool,
+	/// If set, the user accepts the server license terms and the server will be started without a user prompt.
+	#[clap(long)]
+	pub accept_server_license_terms: bool,
+	/// Specifies the directory that server data is kept in.
+	#[clap(long)]
+	pub server_data_dir: Option<String>,
+	/// Specifies the directory that user data is kept in. Can be used to open multiple distinct instances of Code.
+	#[clap(long)]
+	pub user_data_dir: Option<String>,
+	/// Set the root path for extensions.
+	#[clap(long)]
+	pub extensions_dir: Option<String>,
 }
 
 #[derive(Args, Debug, Clone)]
