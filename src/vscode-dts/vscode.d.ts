d0b353aa4d1d6c7ec62039628c22919b4f8ccfe9
Daniel Imms <2193314+Tyriar@users.noreply.github.com>
2023-08-24 06:00:42 -0700

    Add default values to EnvironmentVariableMutatorOptions

--- src/vscode-dts/vscode.d.ts
+++ src/vscode-dts/vscode.d.ts
@@ -11339,13 +11339,14 @@ declare module 'vscode' {
 	 */
 	export interface EnvironmentVariableMutatorOptions {
 		/**
-		 * Apply to the environment just before the process is created.
+		 * Apply to the environment just before the process is created. Defaults to false.
 		 */
 		applyAtProcessCreation?: boolean;
 
 		/**
 		 * Apply to the environment in the shell integration script. Note that this _will not_ apply
-		 * the mutator if shell integration is disabled or not working for some reason.
+		 * the mutator if shell integration is disabled or not working for some reason. Defaults to
+		 * false.
 		 */
 		applyAtShellIntegration?: boolean;
 	}
