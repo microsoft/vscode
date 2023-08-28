f59113c430beecd03597f5e2d37c9e39cd7b7b44
Aiday Marlen Kyzy <amarlenkyzy@microsoft.com>
2023-08-28 17:44:19 +0200

    removing a useless import

--- src/vs/editor/contrib/hover/browser/hover.ts
+++ src/vs/editor/contrib/hover/browser/hover.ts
@@ -5,7 +5,7 @@
 
 import { IKeyboardEvent } from 'vs/base/browser/keyboardEvent';
 import { KeyChord, KeyCode, KeyMod } from 'vs/base/common/keyCodes';
-import { DisposableStore, IDisposable } from 'vs/base/common/lifecycle';
+import { DisposableStore } from 'vs/base/common/lifecycle';
 import { ICodeEditor, IEditorMouseEvent, IPartialEditorMouseEvent, MouseTargetType } from 'vs/editor/browser/editorBrowser';
 import { EditorAction, EditorContributionInstantiation, registerEditorAction, registerEditorContribution, ServicesAccessor } from 'vs/editor/browser/editorExtensions';
 import { ConfigurationChangedEvent, EditorOption } from 'vs/editor/common/config/editorOptions';
