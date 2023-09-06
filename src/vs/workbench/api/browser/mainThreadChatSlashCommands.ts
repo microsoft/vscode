e3231579f74b48fa092ba500cbc3cf0d6f81eabc
Joyce Er <joyce.er@microsoft.com>
2023-09-05 17:06:34 -0700

    Revive URIs in tree data

--- src/vs/workbench/api/browser/mainThreadChatSlashCommands.ts
+++ src/vs/workbench/api/browser/mainThreadChatSlashCommands.ts
@@ -4,6 +4,7 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { DisposableMap } from 'vs/base/common/lifecycle';
+import { revive } from 'vs/base/common/marshalling';
 import { IProgress } from 'vs/platform/progress/common/progress';
 import { ExtHostChatSlashCommandsShape, ExtHostContext, MainContext, MainThreadChatSlashCommandsShape } from 'vs/workbench/api/common/extHost.protocol';
 import { IChatSlashCommandService, IChatSlashFragment } from 'vs/workbench/contrib/chat/common/chatSlashCommands';
@@ -51,7 +52,7 @@ export class MainThreadChatSlashCommands implements MainThreadChatSlashCommandsS
 	}
 
 	async $handleProgressChunk(requestId: number, chunk: IChatSlashFragment): Promise<void> {
-		this._pendingProgress.get(requestId)?.report(chunk);
+		this._pendingProgress.get(requestId)?.report(revive(chunk));
 	}
 
 	$unregisterCommand(handle: number): void {
