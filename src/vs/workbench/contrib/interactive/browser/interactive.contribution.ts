2b34189854c0e168b89ab90eebc8b5436764e0d9
Aaron Munger <aamunger@microsoft.com>
2023-09-11 09:52:03 -0700

    use object rather than string literal

--- src/vs/workbench/contrib/interactive/browser/interactive.contribution.ts
+++ src/vs/workbench/contrib/interactive/browser/interactive.contribution.ts
@@ -53,6 +53,7 @@ import { NotebookEditorWidget } from 'vs/workbench/contrib/notebook/browser/note
 import * as icons from 'vs/workbench/contrib/notebook/browser/notebookIcons';
 import { INotebookEditorService } from 'vs/workbench/contrib/notebook/browser/services/notebookEditorService';
 import { CellEditType, CellKind, CellUri, INTERACTIVE_WINDOW_EDITOR_ID, NotebookWorkingCopyTypeIdentifier } from 'vs/workbench/contrib/notebook/common/notebookCommon';
+import { InteractiveWindowOpen } from 'vs/workbench/contrib/notebook/common/notebookContextKeys';
 import { INotebookKernelService } from 'vs/workbench/contrib/notebook/common/notebookKernelService';
 import { INotebookService } from 'vs/workbench/contrib/notebook/common/notebookService';
 import { columnToEditorGroup } from 'vs/workbench/services/editor/common/editorGroupColumn';
@@ -701,9 +702,9 @@ registerAction2(class extends Action2 {
 			category: interactiveWindowCategory,
 			menu: {
 				id: MenuId.CommandPalette,
-				when: ContextKeyExpr.equals('interactiveWindowOpen', true),
+				when: ContextKeyExpr.equals(InteractiveWindowOpen.key, true),
 			},
-			precondition: ContextKeyExpr.equals('interactiveWindowOpen', true),
+			precondition: ContextKeyExpr.equals(InteractiveWindowOpen.key, true),
 		});
 	}
 
