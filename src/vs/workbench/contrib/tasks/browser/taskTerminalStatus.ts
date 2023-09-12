0f129dad4c0f099f88624744f40cb643b40d84fc
Megan Rogge <merogge@microsoft.com>
2023-09-12 13:53:41 -0500

    Update src/vs/workbench/contrib/tasks/browser/taskTerminalStatus.ts

--- src/vs/workbench/contrib/tasks/browser/taskTerminalStatus.ts
+++ src/vs/workbench/contrib/tasks/browser/taskTerminalStatus.ts
@@ -49,7 +49,6 @@ export class TaskTerminalStatus extends Disposable {
 				case TaskEventKind.Inactive: this.eventInactive(event); break;
 				case TaskEventKind.ProcessEnded: this.eventEnd(event); break;
 			}
-			console.log(event.kind);
 		}));
 	}
 
