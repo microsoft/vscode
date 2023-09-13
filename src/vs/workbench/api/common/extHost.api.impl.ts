869f013f05e4177be60e316af118d613a98b21de
Johannes <johannes.rieken@gmail.com>
2023-09-13 16:59:33 +0200

    inline - some more API tweaks

--- src/vs/workbench/api/common/extHost.api.impl.ts
+++ src/vs/workbench/api/common/extHost.api.impl.ts
@@ -1299,7 +1299,7 @@ export function createApiFactoryAndRegisterActors(accessor: ServicesAccessor): I
 
 			registerInteractiveEditorSessionProvider(provider: vscode.InteractiveEditorSessionProvider, metadata?: vscode.InteractiveEditorSessionProviderMetadata) {
 				checkProposedApiEnabled(extension, 'interactive');
-				return extHostInteractiveEditor.registerProvider(extension, provider, metadata = { label: provider.label ?? extension.displayName ?? extension.name });
+				return extHostInteractiveEditor.registerProvider(extension, provider, metadata = { label: metadata?.label ?? extension.displayName ?? extension.name });
 			},
 			registerInteractiveSessionProvider(id: string, provider: vscode.InteractiveSessionProvider) {
 				checkProposedApiEnabled(extension, 'interactive');
--- src/vscode-dts/vscode.proposed.interactive.d.ts
+++ src/vscode-dts/vscode.proposed.interactive.d.ts
@@ -27,9 +27,7 @@ declare module 'vscode' {
 
 	// todo@API make classes
 	export interface InteractiveEditorRequest {
-		session: InteractiveEditorSession;
 		prompt: string;
-
 		selection: Selection;
 		wholeRange: Range;
 		attempt: number;
@@ -67,23 +65,11 @@ declare module 'vscode' {
 	}
 
 	export interface InteractiveEditorSessionProvider<S extends InteractiveEditorSession = InteractiveEditorSession, R extends InteractiveEditorResponse | InteractiveEditorMessageResponse = InteractiveEditorResponse | InteractiveEditorMessageResponse> {
-		/**
-		 * @deprecated
-		 */
-		label: string;
 
 		// Create a session. The lifetime of this session is the duration of the editing session with the input mode widget.
 		prepareInteractiveEditorSession(context: TextDocumentContext, token: CancellationToken): ProviderResult<S>;
 
-		provideInteractiveEditorResponse(session: S, request: Omit<InteractiveEditorRequest, 'session'>, progress: Progress<{ message: string; edits: TextEdit[] }>, token: CancellationToken): ProviderResult<R>;
-
-		/**
-		 * @deprecated
-		 */
-		provideInteractiveEditorResponse2?(request: InteractiveEditorRequest, progress: Progress<{ message: string; edits: TextEdit[] }>, token: CancellationToken): ProviderResult<R>;
-
-		// eslint-disable-next-line local/vscode-dts-provider-naming
-		releaseInteractiveEditorSession?(session: S): any;
+		provideInteractiveEditorResponse(session: S, request: InteractiveEditorRequest, progress: Progress<{ message: string; edits: TextEdit[] }>, token: CancellationToken): ProviderResult<R>;
 
 		// eslint-disable-next-line local/vscode-dts-provider-naming
 		handleInteractiveEditorResponseFeedback?(session: S, response: R, kind: InteractiveEditorResponseFeedbackKind): void;
