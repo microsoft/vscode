97ade9631a6be05d30255b567d65d92dc07a3b54
Your Name <justchen@microsoft.com>
2023-09-14 10:22:22 -0700

    changed to reflect new ux decisions - inline only, cursor no longer jumps, cleaned up logic in cases we don't need to retrigger

--- src/vs/editor/contrib/codeAction/browser/codeActionModel.ts
+++ src/vs/editor/contrib/codeAction/browser/codeActionModel.ts
@@ -200,37 +200,48 @@ export class CodeActionModel extends Disposable {
 				let startPosition = trigger.selection.getStartPosition();
 				let actions = createCancelablePromise(token => getCodeActions(this._registry, model, trigger.selection, trigger.trigger, Progress.None, token));
 				const codeActionSet = await actions;
+
 				// const codeActionSet = createCancellablePromise(token => actions);
 				// if ((await.codeActionSet).validActions.length === 0 && trigger.trigger.type === CodeActionTriggerType.Invoke) {
 
-				if (codeActionSet.validActions.length === 0 && trigger.trigger.type === CodeActionTriggerType.Invoke) {
-					// retrigger
-					const allMarkers = this._markerService.read({ resource: model.uri });
+				let foundQuickfix = false;
+				if (codeActionSet.validActions) {
+					for (const action of codeActionSet.validActions) {
+						if (action.action.kind === 'quickfix') {
+							foundQuickfix = true;
+						}
+					}
+				}
+
+				const allMarkers = this._markerService.read({ resource: model.uri });
+
+				// If markers exists, and there are no quickfixes found or length is zero, check for quickfixes on that line.
+				if (allMarkers.length > 0 && (!foundQuickfix || codeActionSet.validActions.length === 0) && trigger.trigger.type === CodeActionTriggerType.Invoke) {
 					const currPosition: Position = trigger.selection.getPosition();
 					let trackedPosition: Position = currPosition;
-					let rowDistance = Number.MAX_VALUE;
-					let colDistance = Number.MAX_VALUE;
+					let toBeModified = false;
 
 					allMarkers.forEach((marker: IMarker) => {
 						const col = marker.endColumn;
 						const row = marker.endLineNumber;
 
-						if (row <= currPosition.lineNumber) {
-							const calculatedRowDistance = Math.abs(row - currPosition.lineNumber);
-							const calculatedColDistance = Math.abs(col - currPosition.column);
-							if (calculatedRowDistance <= rowDistance || (calculatedRowDistance < rowDistance && calculatedColDistance < colDistance)) {
-								rowDistance = calculatedRowDistance;
-								colDistance = calculatedColDistance;
-								trackedPosition = new Position(row, col);
-							}
+						if (row === currPosition.lineNumber) {
+							trackedPosition = new Position(row, col);
+							toBeModified = true;
+
 						}
 					});
 
-					actions = createCancelablePromise(token => getCodeActions(this._registry, model, trigger.selection.toPositions(trackedPosition), trigger.trigger, Progress.None, token));
-					const checkActions = await actions;
-					if (checkActions.validActions.length !== 0) {
-						this._editor.setPosition({ lineNumber: trackedPosition.lineNumber, column: trackedPosition.column });
-						startPosition = trigger.selection.setPositionFromStart(trackedPosition.lineNumber, trackedPosition.column);
+
+					// Only retriggers if actually foound quickfix on the line
+					if (toBeModified) {
+						actions = createCancelablePromise(token => getCodeActions(this._registry, model, trigger.selection.toPositions(trackedPosition), trigger.trigger, Progress.None, token));
+						const checkActions = await actions;
+						if (checkActions.validActions.length !== 0) {
+							// this._editor.setPosition({ lineNumber: trackedPosition.lineNumber, column: trackedPosition.column });
+							startPosition = trigger.selection.setPositionFromStart(trackedPosition.lineNumber, trackedPosition.column);
+						}
+
 					}
 				}
 
