be3b94d63a320c4213b36c0bbe4166371eaafd6a
Krzysztof Cieslak <krzysztof_cieslak@windowslive.com>
2023-09-18 15:23:19 +0200

    Remove old code

--- src/vs/editor/contrib/multiGhostText/browser/multiGhostText.contribution.ts
+++ src/vs/editor/contrib/multiGhostText/browser/multiGhostText.contribution.ts
@@ -5,14 +5,10 @@
 
 import { EditorContributionInstantiation, registerEditorAction, registerEditorContribution } from 'vs/editor/browser/editorExtensions';
 import { AcceptAndNextGhostText, RejectGhostText, ShowMultiGhostText } from 'vs/editor/contrib/multiGhostText/browser/commands';
-import { MultiGhostTextController2 } from 'vs/editor/contrib/multiGhostText/browser/multiGhostTextController';
+import { MultiGhostTextController } from 'vs/editor/contrib/multiGhostText/browser/multiGhostTextController';
 
 registerEditorAction(ShowMultiGhostText);
 registerEditorAction(AcceptAndNextGhostText);
 registerEditorAction(RejectGhostText);
-// registerEditorAction(SelectNextGhostText);
-// registerEditorAction(SelectPreviousGhostText);
-// registerEditorAction(AcceptAllGhostText);
-// registerEditorAction(AcceptSelectedGhostText);
-registerEditorContribution(MultiGhostTextController2.ID, MultiGhostTextController2, EditorContributionInstantiation.Eventually);
+registerEditorContribution(MultiGhostTextController.ID, MultiGhostTextController, EditorContributionInstantiation.Eventually);
 
