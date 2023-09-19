885abea843a3601c904ea063377cf90ee0d52e8c
Your Name <justchen@microsoft.com>
2023-09-19 12:35:50 -0700

    code cleanup

--- src/vs/code/electron-sandbox/issue/issueReporterService.ts
+++ src/vs/code/electron-sandbox/issue/issueReporterService.ts
@@ -1056,7 +1056,6 @@ export class IssueReporter extends Disposable {
 		if (extensionsSelector) {
 			const { selectedExtension } = this.issueReporterModel.getData();
 			reset(extensionsSelector, this.makeOption('', localize('selectExtension', "Select extension"), true), ...extensionOptions.map(extension => makeOption(extension, selectedExtension)));
-			// extensionsSelector.replaceChild(this.makeOption('', localize('selectExtension', "Select extension"), true), extensionsSelector.firstChild!);
 			extensionsSelector.selectedIndex = 0;
 
 			this.addEventListener('extension-selector', 'change', (e: Event) => {
