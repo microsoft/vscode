5c0aac4bc44e3dd04abcc375d8837bc572ffe34e
meganrogge <megan.rogge@microsoft.com>
2023-09-19 12:30:44 -0700

    await

--- src/vs/workbench/contrib/tasks/browser/runAutomaticTasks.ts
+++ src/vs/workbench/contrib/tasks/browser/runAutomaticTasks.ts
@@ -31,13 +31,9 @@ export class RunAutomaticTasks extends Disposable implements IWorkbenchContribut
 		if (this._taskService.isReconnected) {
 			this._tryRunTasks();
 		} else {
-			this._register(Event.once(this._taskService.onDidReconnectToTasks)(() => this._tryRunTasks()));
+			this._register(Event.once(this._taskService.onDidReconnectToTasks)(async () => await this._tryRunTasks()));
 		}
-		this._register(this._workspaceTrustManagementService.onDidChangeTrust(async trusted => {
-			if (trusted) {
-				await this._tryRunTasks();
-			}
-		}));
+		this._register(this._workspaceTrustManagementService.onDidChangeTrust(async () => await this._tryRunTasks()));
 	}
 
 	private async _tryRunTasks() {
