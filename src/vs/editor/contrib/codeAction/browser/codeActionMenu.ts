cc8daf34c23b1211577272819b860a72ad5a0039
Aiday Marlen Kyzy <amarlenkyzy@microsoft.com>
2023-10-02 18:09:43 +0200

    adding sparkle icon for copilot code action

--- src/vs/editor/contrib/codeAction/browser/codeActionMenu.ts
+++ src/vs/editor/contrib/codeAction/browser/codeActionMenu.ts
@@ -68,10 +68,12 @@ export function toMenuItems(
 		if (menuEntry.actions.length) {
 			allMenuItems.push({ kind: ActionListItemKind.Header, group: menuEntry.group });
 			for (const action of menuEntry.actions) {
+				const isCopilot = action.action.isCopilot;
+				const group = menuEntry.group;
 				allMenuItems.push({
 					kind: ActionListItemKind.Action,
 					item: action,
-					group: menuEntry.group,
+					group: isCopilot ? { title: group.title, kind: group.kind, icon: Codicon.sparkle } : group,
 					label: action.action.title,
 					disabled: !!action.action.disabled,
 					keybinding: keybindingResolver(action.action),
