08b2508d6e64ab393ca46d76ff9f7f8b9b12a036
Alex Ross <alros@microsoft.com>
2023-10-02 14:07:36 +0200

    Switch Remote button not working after hiding Fixes #194569 Part of #194603

--- src/vs/workbench/contrib/remote/browser/explorerViewItems.ts
+++ src/vs/workbench/contrib/remote/browser/explorerViewItems.ts
@@ -4,45 +4,56 @@
  *--------------------------------------------------------------------------------------------*/
 
 import * as nls from 'vs/nls';
-import { IAction } from 'vs/base/common/actions';
-import { IContextViewService } from 'vs/platform/contextview/browser/contextView';
 import { IRemoteExplorerService, REMOTE_EXPLORER_TYPE_KEY } from 'vs/workbench/services/remote/common/remoteExplorerService';
 import { ISelectOptionItem } from 'vs/base/browser/ui/selectBox/selectBox';
 import { IViewDescriptor } from 'vs/workbench/common/views';
 import { isStringArray } from 'vs/base/common/types';
 import { IWorkbenchEnvironmentService } from 'vs/workbench/services/environment/common/environmentService';
 import { IStorageService, StorageScope } from 'vs/platform/storage/common/storage';
-import { ContextKeyExpr, IContextKeyService } from 'vs/platform/contextkey/common/contextkey';
-import { SelectActionViewItem } from 'vs/base/browser/ui/actionbar/actionViewItems';
-import { Action2, MenuId } from 'vs/platform/actions/common/actions';
-import { ServicesAccessor } from 'vs/platform/instantiation/common/instantiation';
+import { ContextKeyExpr, IContextKey, IContextKeyService, RawContextKey } from 'vs/platform/contextkey/common/contextkey';
+import { Action2, MenuId, MenuRegistry, registerAction2 } from 'vs/platform/actions/common/actions';
 import { VIEWLET_ID } from 'vs/workbench/contrib/remote/browser/remoteExplorer';
-import { defaultSelectBoxStyles } from 'vs/platform/theme/browser/defaultStyles';
 import { getVirtualWorkspaceLocation } from 'vs/platform/workspace/common/virtualWorkspace';
 import { IWorkspaceContextService } from 'vs/platform/workspace/common/workspace';
+import { Disposable } from 'vs/base/common/lifecycle';
 
 interface IRemoteSelectItem extends ISelectOptionItem {
 	authority: string[];
 	virtualWorkspace?: string;
 }
 
-export class SwitchRemoteViewItem extends SelectActionViewItem<IRemoteSelectItem> {
+export const SELECTED_REMOTE_IN_EXPLORER = new RawContextKey<string>('selectedRemoteInExplorer', '');
+
+export class SwitchRemoteViewItem extends Disposable {
+	private switchRemoteMenu: MenuId;
+	private options: IRemoteSelectItem[] = [];
+	private completedRemotes: Set<string> = new Set();
+	private readonly selectedRemoteContext: IContextKey<string>;
 
 	constructor(
-		action: IAction,
-		private readonly optionsItems: IRemoteSelectItem[],
-		@IContextViewService contextViewService: IContextViewService,
+		@IContextKeyService private readonly contextKeyService: IContextKeyService,
 		@IRemoteExplorerService private remoteExplorerService: IRemoteExplorerService,
 		@IWorkbenchEnvironmentService private environmentService: IWorkbenchEnvironmentService,
 		@IStorageService private readonly storageService: IStorageService,
 		@IWorkspaceContextService private readonly workspaceContextService: IWorkspaceContextService
 	) {
-		super(null, action, optionsItems, 0, contextViewService, defaultSelectBoxStyles, { ariaLabel: nls.localize('remotes', 'Switch Remote') });
+		super();
+		this.selectedRemoteContext = SELECTED_REMOTE_IN_EXPLORER.bindTo(contextKeyService);
+
+		this.switchRemoteMenu = new MenuId('workbench.remote.menu.switchRemoteMenu');
+		this._register(MenuRegistry.appendMenuItem(MenuId.ViewContainerTitle, {
+			submenu: this.switchRemoteMenu,
+			title: nls.localize('switchRemote.label', "Switch Remote"),
+			group: 'navigation',
+			when: ContextKeyExpr.equals('activeViewlet', VIEWLET_ID),
+			order: 1,
+			isSelection: true
+		}));
 	}
 
 	public setSelectionForConnection(): boolean {
 		let isSetForConnection = false;
-		if (this.optionsItems.length > 0) {
+		if (this.options.length > 0) {
 			let index = 0;
 			const remoteAuthority = this.environmentService.remoteAuthority;
 			let virtualWorkspace: string | undefined;
@@ -56,26 +67,25 @@ export class SwitchRemoteViewItem extends SelectActionViewItem<IRemoteSelectItem
 			if (explorerType !== undefined) {
 				index = this.getOptionIndexForExplorerType(explorerType);
 			}
-			this.select(index);
-			this.remoteExplorerService.targetType = this.optionsItems[index].authority;
+			this.select(this.options[index].authority);
 		}
 		return isSetForConnection;
 	}
 
-	public setSelection() {
-		const index = this.getOptionIndexForExplorerType(this.remoteExplorerService.targetType);
-		this.select(index);
+	private select(authority: string[]) {
+		this.selectedRemoteContext.set(authority[0]);
+		this.remoteExplorerService.targetType = authority;
 	}
 
 	private getOptionIndexForExplorerType(explorerType: string[]): number {
 		let index = 0;
-		for (let optionIterator = 0; (optionIterator < this.optionsItems.length) && (index === 0); optionIterator++) {
-			for (let authorityIterator = 0; authorityIterator < this.optionsItems[optionIterator].authority.length; authorityIterator++) {
+		for (let optionIterator = 0; (optionIterator < this.options.length) && (index === 0); optionIterator++) {
+			for (let authorityIterator = 0; authorityIterator < this.options[optionIterator].authority.length; authorityIterator++) {
 				for (let i = 0; i < explorerType.length; i++) {
-					if (this.optionsItems[optionIterator].authority[authorityIterator] === explorerType[i]) {
+					if (this.options[optionIterator].authority[authorityIterator] === explorerType[i]) {
 						index = optionIterator;
 						break;
-					} else if (this.optionsItems[optionIterator].virtualWorkspace === explorerType[i]) {
+					} else if (this.options[optionIterator].virtualWorkspace === explorerType[i]) {
 						index = optionIterator;
 						break;
 					}
@@ -85,47 +95,39 @@ export class SwitchRemoteViewItem extends SelectActionViewItem<IRemoteSelectItem
 		return index;
 	}
 
-	override render(container: HTMLElement) {
-		if (this.optionsItems.length > 1) {
-			super.render(container);
-			container.classList.add('switch-remote');
-		}
-	}
-
-	protected override getActionContext(_: string, index: number): IRemoteSelectItem {
-		return this.optionsItems[index];
-	}
-
-	static createOptionItems(views: IViewDescriptor[], contextKeyService: IContextKeyService): IRemoteSelectItem[] {
-		const options: IRemoteSelectItem[] = [];
-		views.forEach(view => {
-			if (view.group && view.group.startsWith('targets') && view.remoteAuthority && (!view.when || contextKeyService.contextMatchesRules(view.when))) {
-				options.push({ text: view.name, authority: isStringArray(view.remoteAuthority) ? view.remoteAuthority : [view.remoteAuthority], virtualWorkspace: view.virtualWorkspace });
+	public createOptionItems(views: IViewDescriptor[]) {
+		const startingCount = this.completedRemotes.size;
+		for (const view of views) {
+			if (view.group && view.group.startsWith('targets') && view.remoteAuthority && (!view.when || this.contextKeyService.contextMatchesRules(view.when))) {
+				const text = view.name;
+				const authority = isStringArray(view.remoteAuthority) ? view.remoteAuthority : [view.remoteAuthority];
+				if (authority.some(a => this.completedRemotes.has(a))) {
+					return;
+				}
+				const thisCapture = this;
+				this._register(registerAction2(class extends Action2 {
+					constructor() {
+						super({
+							id: `workbench.action.remoteExplorer.show.${authority[0]}`,
+							title: text,
+							toggled: SELECTED_REMOTE_IN_EXPLORER.isEqualTo(authority[0]),
+							menu: {
+								id: thisCapture.switchRemoteMenu
+							}
+						});
+					}
+					async run(): Promise<void> {
+						thisCapture.select(authority);
+					}
+				}));
+				this.options.push({ text, authority });
+				for (const authorityOption of authority) {
+					this.completedRemotes.add(authorityOption);
+				}
 			}
-		});
-		return options;
-	}
-}
-
-export class SwitchRemoteAction extends Action2 {
-
-	public static readonly ID = 'remote.explorer.switch';
-	public static readonly LABEL = nls.localize('remote.explorer.switch', "Switch Remote");
-
-	constructor() {
-		super({
-			id: SwitchRemoteAction.ID,
-			title: SwitchRemoteAction.LABEL,
-			menu: [{
-				id: MenuId.ViewContainerTitle,
-				when: ContextKeyExpr.equals('viewContainer', VIEWLET_ID),
-				group: 'navigation',
-				order: 1
-			}],
-		});
-	}
-
-	public async run(accessor: ServicesAccessor, args: IRemoteSelectItem): Promise<any> {
-		accessor.get(IRemoteExplorerService).targetType = args.authority;
+		}
+		if (this.completedRemotes.size > startingCount) {
+			this.setSelectionForConnection();
+		}
 	}
 }
