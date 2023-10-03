3d5c07a88c160c4f19cba3afbed0a448a9f5f4f3
Rob Lourens <roblourens@gmail.com>
2023-10-03 09:56:37 -0700

    Notes from api sync

--- src/vscode-dts/vscode.proposed.chatAgents.d.ts
+++ src/vscode-dts/vscode.proposed.chatAgents.d.ts
@@ -32,7 +32,7 @@ declare module 'vscode' {
 		// Should be able to compute these async, because they typically will involve a separate LLM call.
 		// That can be a separate call (provideFollowups) or ChatAgentResult contains a promise to it (so ChatAgent returns a promise to a promise).
 		// Or, we can just be ok with the UI showing that the response is still continuing when the actual response is done but the followups are being computed.
-		followUp?: InteractiveSessionFollowup[];
+		provideFollowups?(token: CancellationToken): ProviderResult<InteractiveSessionFollowup[]>;
 	}
 
 	export interface SlashCommandMetadata {
@@ -42,12 +42,14 @@ declare module 'vscode' {
 	export interface SlashCommand {
 		readonly name: string;
 		readonly metadata: SlashCommandMetadata;
-		invoke(request: InteractiveRequest, context: ChatAgentContext, progress: Progress<SlashResponse>, token: CancellationToken): Thenable<SlashResult | void>;
+		invoke: ChatAgentHandler;
 	}
 
+	// All agent and slashCommand details must be fully dynamic because they can be loaded from a remote server (github copilot extensibility).
+	// But could be declared in package.json a well.
 	interface ChatAgent {
 		slashCommands: SlashCommand[];
-		onDidPerformAction: Event<{ sessionId: string; responseId: string; action: InteractiveSessionUserAction }>;
+		onDidPerformAction: Event<{ action: InteractiveSessionUserAction }>;
 		dispose(): void;
 	}
 
@@ -57,11 +59,12 @@ declare module 'vscode' {
 		icon?: Uri;
 	}
 
+	// Could include "slashCommand: SlashCommand | undefined" here instead of the invoke method.
 	export type ChatAgentHandler = (request: InteractiveRequest, context: ChatAgentContext, progress: Progress<InteractiveProgress>, token: CancellationToken) => ProviderResult<InteractiveResponseForProgress>;
 
 	export namespace chat {
 		// Invoking slash commands vs the agent with no slash command?
 		// Could be a separate handler or a slash command with a '' id
-		export function createChatAgent(id: string, description: string, fullName?: string, icon?: Uri, defaultHandler?: ChatAgentHandler): ChatAgent;
+		export function createChatAgent(id: string, description: string, fullName?: string, icon?: Uri, handler?: ChatAgentHandler): ChatAgent;
 	}
 }
