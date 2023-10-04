e71dd63554b9a3d07053db2dec7a552ed0db8a3e
Raymond Zhao <7199958+rzhao271@users.noreply.github.com>
2023-10-04 11:13:27 -0700

    Add smoke test

--- test/smoke/src/areas/preferences/preferences.test.ts
+++ test/smoke/src/areas/preferences/preferences.test.ts
@@ -62,5 +62,23 @@ export function setup(logger: Logger) {
 			await app.workbench.editors.selectTab('Untitled-1');
 			await app.code.waitForElements('.line-numbers', false, elements => !elements || elements.length === 0);
 		});
+
+		it('hides the toc when searching depending on the search behavior', async function () {
+			const app = this.app as Application;
+
+			await app.workbench.settingsEditor.searchSettingsUI('workbench.settings.settingsSearchTocBehavior');
+			await app.code.waitAndClick('.settings-editor .monaco-list-rows .setting-item-control select', 2, 2);
+			await app.code.waitAndClick('.context-view .option-text:nth-child(1)', 2, 2);
+			await app.workbench.settingsEditor.searchSettingsUI('test');
+			await app.code.waitForElements('.settings-editor .settings-toc-container', false, elements => !elements || elements.length === 0);
+			await app.code.waitForElements('.settings-editor .settings-body .monaco-sash', false, elements => !elements || elements.length === 0);
+
+			await app.workbench.settingsEditor.searchSettingsUI('workbench.settings.settingsSearchTocBehavior');
+			await app.code.waitAndClick('.settings-editor .monaco-list-rows .setting-item-control select', 2, 2);
+			await app.code.waitAndClick('.context-view .option-text:nth-child(2)', 2, 2);
+			await app.workbench.settingsEditor.searchSettingsUI('test');
+			await app.code.waitForElements('.settings-editor .settings-toc-container', false, elements => !!elements.length);
+			await app.code.waitForElements('.settings-editor .settings-body .monaco-sash', false, elements => !!elements.length);
+		});
 	});
 }
