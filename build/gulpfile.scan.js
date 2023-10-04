fed68b4b056f5af0edf56d7bf6b3ae981ce24b7f
Raymond Zhao <7199958+rzhao271@users.noreply.github.com>
2023-10-04 15:15:06 -0700

    Fix type mismatch

--- build/gulpfile.scan.js
+++ build/gulpfile.scan.js
@@ -47,7 +47,7 @@ BUILD_TARGETS.forEach(buildTarget => {
 	if (platform === 'win32') {
 		tasks.push(
 			() => electron.dest(destinationPdb, { ...config, platform, arch: arch === 'armhf' ? 'arm' : arch, pdbs: true }),
-			() => util.rimraf(path.join(destinationExe, 'd3dcompiler_47.dll')),
+			util.rimraf(path.join(destinationExe, 'd3dcompiler_47.dll')),
 			() => confirmPdbsExist(destinationExe, destinationPdb)
 		);
 	}
