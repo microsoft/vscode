5774b08f18b9cacec9f91727cda8d956da78822d
Rob Lourens <roblourens@gmail.com>
2023-10-05 16:56:56 -0700

    debug

--- build/bin/node-gyp.cmd
+++ build/bin/node-gyp.cmd
@@ -1,5 +1,11 @@
+@echo off
+setlocal
 if not defined npm_config_node_gyp (
-  node "%~dp0\..\..\node_modules\node-gyp\bin\node-gyp.js" %*
+  for /f "delims=" %%i in ('where node') do (set nodeDir=%%~dpi)
+	echo %nodeDir%
+	dir %nodeDir%
+	dir %nodeDir%\node_modules
 ) else (
   node "%npm_config_node_gyp%" %*
 )
+endlocal
