66316a82dc0c472ceab5e034c6a44f5c43d3d3b9
Rob Lourens <roblourens@gmail.com>
2023-10-05 17:17:10 -0700

    debug

--- build/bin/node-gyp.cmd
+++ build/bin/node-gyp.cmd
@@ -4,6 +4,8 @@ if not defined npm_config_node_gyp (
 	for /f "delims=" %%i in ('where node') do (set nodeDir=%%~dpi)
 	echo nodeDir is
 	echo %nodeDir%
+	echo "%nodeDir%\node_modules\npm\node_modules\node-gyp\bin\node-gyp.js"
+	dir "%nodeDir%\node_modules\npm\node_modules\node-gyp\bin\node-gyp.js"
 	node "%nodeDir%\node_modules\npm\node_modules\node-gyp\bin\node-gyp.js" %*
 ) else (
 	node "%npm_config_node_gyp%" %*
