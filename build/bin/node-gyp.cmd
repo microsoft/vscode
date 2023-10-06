110309204897a9a8bf3e816fcbe9a613f266752e
Rob Lourens <roblourens@gmail.com>
2023-10-05 17:00:41 -0700

    Right cmd path

--- build/bin/node-gyp.cmd
+++ build/bin/node-gyp.cmd
@@ -1,11 +1,9 @@
 @echo off
 setlocal
 if not defined npm_config_node_gyp (
-  for /f "delims=" %%i in ('where node') do (set nodeDir=%%~dpi)
-	echo %nodeDir%
-	dir %nodeDir%
-	dir %nodeDir%\node_modules
+	for /f "delims=" %%i in ('where node') do (set nodeDir=%%~dpi)
+	node "%nodeDir%\node_modules\npm\node_modules\node-gyp\bin\node-gyp.js" %*
 ) else (
-  node "%npm_config_node_gyp%" %*
+	node "%npm_config_node_gyp%" %*
 )
 endlocal
