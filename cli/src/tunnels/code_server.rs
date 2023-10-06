e250c8066d5b81cf580ebcb2e54d9663f8acf459
Connor Peet <connor@peet.io>
2023-10-06 09:09:47 -0700

    cli: fix command prompt showing up on windows (#194946)
    cli: fix wsl prompt up on windows machine
    
    Fixes #190425 again

--- cli/src/tunnels/code_server.rs
+++ cli/src/tunnels/code_server.rs
@@ -14,7 +14,9 @@ use crate::tunnels::paths::{get_server_folder_name, SERVER_FOLDER_NAME};
 use crate::update_service::{
 	unzip_downloaded_release, Platform, Release, TargetKind, UpdateService,
 };
-use crate::util::command::{capture_command, capture_command_and_check_status, kill_tree};
+use crate::util::command::{
+	capture_command, capture_command_and_check_status, kill_tree, new_script_command,
+};
 use crate::util::errors::{wrap, AnyError, CodeError, ExtensionInstallFailed, WrappedError};
 use crate::util::http::{self, BoxedHttp};
 use crate::util::io::SilentCopyProgress;
@@ -587,17 +589,7 @@ impl<'a> ServerBuilder<'a> {
 	}
 
 	fn get_base_command(&self) -> Command {
-		#[cfg(not(windows))]
-		let mut cmd = Command::new(&self.server_paths.executable);
-		#[cfg(windows)]
-		let mut cmd = {
-			let mut cmd = Command::new("cmd");
-			cmd.arg("/Q");
-			cmd.arg("/C");
-			cmd.arg(&self.server_paths.executable);
-			cmd
-		};
-
+		let mut cmd = new_script_command(&self.server_paths.executable);
 		cmd.stdin(std::process::Stdio::null())
 			.args(self.server_params.code_server_args.command_arguments());
 		cmd
