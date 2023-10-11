ec3f9a60dc11c69a37690d5ca1a821659e949497
Benjamin Pasero <benjamin.pasero@microsoft.com>
2023-10-11 15:01:01 +0200

    speech - scaffold a basic core service for registration

---
+++ src/vs/workbench/contrib/speech/common/speechService.ts
@@ -0,0 +1,49 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { CancellationToken } from 'vs/base/common/cancellation';
+import { IDisposable, toDisposable } from 'vs/base/common/lifecycle';
+import { createDecorator } from 'vs/platform/instantiation/common/instantiation';
+
+export const ISpeechService = createDecorator<ISpeechService>('speechService');
+
+export interface ISpeechProvider {
+	speechToText(token: CancellationToken): Promise<unknown>;
+}
+
+export interface ISpeechService {
+
+	readonly _serviceBrand: undefined;
+
+	registerSpeechProvider(identifier: string, provider: ISpeechProvider): IDisposable;
+
+	speechToText(identifier: string, token: CancellationToken): Promise<unknown>;
+}
+
+export class SpeechService implements ISpeechService {
+
+	readonly _serviceBrand: undefined;
+
+	private readonly providers = new Map<string, ISpeechProvider>();
+
+	registerSpeechProvider(identifier: string, provider: ISpeechProvider): IDisposable {
+		if (this.providers.has(identifier)) {
+			throw new Error(`Speech provider with identifier ${identifier} is already registered.`);
+		}
+
+		this.providers.set(identifier, provider);
+
+		return toDisposable(() => this.providers.delete(identifier));
+	}
+
+	speechToText(identifier: string, token: CancellationToken): Promise<unknown> {
+		const provider = this.providers.get(identifier);
+		if (!provider) {
+			throw new Error(`Speech provider with identifier ${identifier} is not registered.`);
+		}
+
+		return provider.speechToText(token);
+	}
+}
