b9c3542565a704d60e15c0b76b842fa507d09358
Benjamin Pasero <benjamin.pasero@microsoft.com>
2023-10-11 16:03:08 +0200

    tests - skip flaky tests again in our CI (#195367)

--- src/vs/platform/files/test/node/nodejsWatcher.integrationTest.ts
+++ src/vs/platform/files/test/node/nodejsWatcher.integrationTest.ts
@@ -26,7 +26,7 @@ import { addUNCHostToAllowlist } from 'vs/base/node/unc';
 // mocha but generally). as such they will run only on demand
 // whenever we update the watcher library.
 
-flakySuite('File Watcher (node.js)', () => {
+((process.env['BUILD_SOURCEVERSION'] || process.env['CI']) ? suite.skip : flakySuite)('File Watcher (node.js)', () => {
 
 	class TestNodeJSWatcher extends NodeJSWatcher {
 
