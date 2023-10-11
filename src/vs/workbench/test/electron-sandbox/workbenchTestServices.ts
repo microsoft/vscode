57b677dbe5138c907df6f4ad0b30bc9e7921bb53
Benjamin Pasero <benjamin.pasero@microsoft.com>
2023-10-11 11:29:10 +0200

    aux window - implement `focusWindow` via native host service

--- src/vs/workbench/test/electron-sandbox/workbenchTestServices.ts
+++ src/vs/workbench/test/electron-sandbox/workbenchTestServices.ts
@@ -97,7 +97,7 @@ export class TestNativeHostService implements INativeHostService {
 	async updateWindowControls(options: { height?: number; backgroundColor?: string; foregroundColor?: string }): Promise<void> { }
 	async setMinimumSize(width: number | undefined, height: number | undefined): Promise<void> { }
 	async saveWindowSplash(value: IPartsSplash): Promise<void> { }
-	async focusWindow(options?: { windowId?: number | undefined } | undefined): Promise<void> { }
+	async focusWindow(options?: { targetWindowId?: number | undefined } | undefined): Promise<void> { }
 	async showMessageBox(options: Electron.MessageBoxOptions): Promise<Electron.MessageBoxReturnValue> { throw new Error('Method not implemented.'); }
 	async showSaveDialog(options: Electron.SaveDialogOptions): Promise<Electron.SaveDialogReturnValue> { throw new Error('Method not implemented.'); }
 	async showOpenDialog(options: Electron.OpenDialogOptions): Promise<Electron.OpenDialogReturnValue> { throw new Error('Method not implemented.'); }
