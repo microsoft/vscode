bd7f0be397824f10acace02d992f7015752aa217
Isidor Nikolic <inikolic@microsoft.com>
2023-10-12 20:47:40 +0000

    add sqm id for windows (#195377)
    * add sqm id for windows
    
    * Update src/vs/platform/windows/electron-main/windowsMainService.ts
    
    * Update src/vs/platform/sharedProcess/node/sharedProcess.ts
    
    * react on review comments
    
    * The reg entry is called MachineId not MachineGuid
    
    * fix compile error
    
    * no need for \\ prefix in reg path
    
    * Wait for 1s max (as to not block the startup) to read the SQM value
    
    ---------
    
    Co-authored-by: Benjamin Pasero <benjamin.pasero@microsoft.com>

--- src/vs/workbench/services/telemetry/test/node/commonProperties.test.ts
+++ src/vs/workbench/services/telemetry/test/node/commonProperties.test.ts
@@ -22,7 +22,7 @@ suite('Telemetry - common properties', function () {
 	ensureNoDisposablesAreLeakedInTestSuite();
 
 	test('default', function () {
-		const props = resolveWorkbenchCommonProperties(testStorageService, release(), hostname(), commit, version, 'someMachineId', false, process);
+		const props = resolveWorkbenchCommonProperties(testStorageService, release(), hostname(), commit, version, 'someMachineId', 'someSqmId', false, process);
 		assert.ok('commitHash' in props);
 		assert.ok('sessionID' in props);
 		assert.ok('timestamp' in props);
@@ -46,14 +46,14 @@ suite('Telemetry - common properties', function () {
 
 		testStorageService.store('telemetry.lastSessionDate', new Date().toUTCString(), StorageScope.APPLICATION, StorageTarget.MACHINE);
 
-		const props = resolveWorkbenchCommonProperties(testStorageService, release(), hostname(), commit, version, 'someMachineId', false, process);
+		const props = resolveWorkbenchCommonProperties(testStorageService, release(), hostname(), commit, version, 'someMachineId', 'someSqmId', false, process);
 		assert.ok('common.lastSessionDate' in props); // conditional, see below
 		assert.ok('common.isNewSession' in props);
 		assert.strictEqual(props['common.isNewSession'], '0');
 	});
 
 	test('values chance on ask', async function () {
-		const props = resolveWorkbenchCommonProperties(testStorageService, release(), hostname(), commit, version, 'someMachineId', false, process);
+		const props = resolveWorkbenchCommonProperties(testStorageService, release(), hostname(), commit, version, 'someMachineId', 'someSqmId', false, process);
 		let value1 = props['common.sequence'];
 		let value2 = props['common.sequence'];
 		assert.ok(value1 !== value2, 'seq');
