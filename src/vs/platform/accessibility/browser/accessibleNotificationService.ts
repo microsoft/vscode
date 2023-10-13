c9d0dc937ec70d03d1803cf203beb20de5d14966
meganrogge <megan.rogge@microsoft.com>
2023-10-13 10:43:13 -0700

    add saved cue

--- src/vs/platform/accessibility/browser/accessibleNotificationService.ts
+++ src/vs/platform/accessibility/browser/accessibleNotificationService.ts
@@ -5,26 +5,41 @@
 
 import { Disposable } from 'vs/base/common/lifecycle';
 import { localize } from 'vs/nls';
-import { IAccessibilityService, IAccessibleNotificationService } from 'vs/platform/accessibility/common/accessibility';
+import { AccessibleNotificationEvent, IAccessibilityService, IAccessibleNotificationService } from 'vs/platform/accessibility/common/accessibility';
 import { AudioCue, IAudioCueService } from 'vs/platform/audioCues/browser/audioCueService';
 import { IConfigurationService } from 'vs/platform/configuration/common/configuration';
 
 export class AccessibleNotificationService extends Disposable implements IAccessibleNotificationService {
 	declare readonly _serviceBrand: undefined;
-
+	private _events: Map<AccessibleNotificationEvent, { audioCue: AudioCue; alertMessage: string }> = new Map();
 	constructor(
 		@IAudioCueService private readonly _audioCueService: IAudioCueService,
 		@IConfigurationService private readonly _configurationService: IConfigurationService,
 		@IAccessibilityService private readonly _accessibilityService: IAccessibilityService) {
 		super();
+		this._events.set(AccessibleNotificationEvent.Clear, { audioCue: AudioCue.clear, alertMessage: localize('cleared', "Cleared") });
+		this._events.set(AccessibleNotificationEvent.Save, { audioCue: AudioCue.save, alertMessage: localize('saved', "Saved") });
 	}
 
-	notifyCleared(): void {
-		const audioCueValue = this._configurationService.getValue(AudioCue.clear.settingsKey);
+	notify(event: AccessibleNotificationEvent): void {
+		const { audioCue, alertMessage } = this._events.get(event)!;
+		const audioCueValue = this._configurationService.getValue(audioCue.settingsKey);
 		if (audioCueValue === 'on' || audioCueValue === 'auto' && this._accessibilityService.isScreenReaderOptimized()) {
-			this._audioCueService.playAudioCue(AudioCue.clear);
+			this._audioCueService.playAudioCue(audioCue);
 		} else {
-			alert(localize('cleared', "Cleared"));
+			alert(alertMessage);
+		}
+	}
+	notifySaved(userGesture: boolean): void {
+		const { audioCue, alertMessage } = this._events.get(AccessibleNotificationEvent.Save)!;
+		const audioCueSetting = this._configurationService.getValue(audioCue.settingsKey);
+		if (audioCueSetting === 'off') {
+			alert(alertMessage);
+			return;
+		} else if (audioCueSetting === 'always') {
+			this._audioCueService.playAudioCue(audioCue);
+		} else if (audioCueSetting === 'userGesture' && userGesture) {
+			this._audioCueService.playAudioCue(audioCue);
 		}
 	}
 }
@@ -33,5 +48,6 @@ export class TestAccessibleNotificationService extends Disposable implements IAc
 
 	declare readonly _serviceBrand: undefined;
 
-	notifyCleared(): void { }
+	notify(event: AccessibleNotificationEvent): void { }
+	notifySaved(userGesture: boolean): void { }
 }
--- src/vs/workbench/services/editor/browser/editorService.ts
+++ src/vs/workbench/services/editor/browser/editorService.ts
@@ -33,6 +33,7 @@ import { IWorkspaceTrustRequestService, WorkspaceTrustUriResponse } from 'vs/pla
 import { IHostService } from 'vs/workbench/services/host/browser/host';
 import { findGroup } from 'vs/workbench/services/editor/common/editorGroupFinder';
 import { ITextEditorService } from 'vs/workbench/services/textfile/common/textEditorService';
+import { IAccessibleNotificationService } from 'vs/platform/accessibility/common/accessibility';
 
 export class EditorService extends Disposable implements EditorServiceImpl {
 
@@ -70,7 +71,8 @@ export class EditorService extends Disposable implements EditorServiceImpl {
 		@IEditorResolverService private readonly editorResolverService: IEditorResolverService,
 		@IWorkspaceTrustRequestService private readonly workspaceTrustRequestService: IWorkspaceTrustRequestService,
 		@IHostService private readonly hostService: IHostService,
-		@ITextEditorService private readonly textEditorService: ITextEditorService
+		@ITextEditorService private readonly textEditorService: ITextEditorService,
+		@IAccessibleNotificationService private readonly accessibleNotificationService: IAccessibleNotificationService
 	) {
 		super();
 
@@ -972,9 +974,12 @@ export class EditorService extends Disposable implements EditorServiceImpl {
 				}
 			}
 		}
-
+		const success = saveResults.every(result => !!result);
+		if (success) {
+			this.accessibleNotificationService.notifySaved(options?.reason === SaveReason.EXPLICIT);
+		}
 		return {
-			success: saveResults.every(result => !!result),
+			success,
 			editors: coalesce(saveResults)
 		};
 	}
