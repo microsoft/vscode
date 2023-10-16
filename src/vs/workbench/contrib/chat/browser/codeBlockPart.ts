6c7da03819dd9f5d3af20571315db086a4c64b69
Martin Aeschlimann <martinae@microsoft.com>
2023-10-16 08:54:06 +0200

    separate css

--- src/vs/workbench/contrib/chat/browser/codeBlockPart.ts
+++ src/vs/workbench/contrib/chat/browser/codeBlockPart.ts
@@ -3,6 +3,8 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+import 'vs/css!./codeBlockPart';
+
 import * as dom from 'vs/base/browser/dom';
 import { Emitter, Event } from 'vs/base/common/event';
 import { Disposable } from 'vs/base/common/lifecycle';
