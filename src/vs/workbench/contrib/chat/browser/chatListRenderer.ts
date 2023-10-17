2972307c7b7048209248a3e20dd8cbee3af8835d
Rob Lourens <roblourens@gmail.com>
2023-10-16 20:35:35 -0700

    Move references list to the top (#195751)

--- src/vs/workbench/contrib/chat/browser/chatListRenderer.ts
+++ src/vs/workbench/contrib/chat/browser/chatListRenderer.ts
@@ -221,8 +221,8 @@ export class ChatListItemRenderer extends Disposable implements ITreeRenderer<Ch
 		const user = dom.append(header, $('.user'));
 		const avatar = dom.append(user, $('.avatar'));
 		const username = dom.append(user, $('h3.username'));
-		const value = dom.append(rowContainer, $('.value'));
 		const referencesListContainer = dom.append(rowContainer, $('.referencesListContainer'));
+		const value = dom.append(rowContainer, $('.value'));
 		const elementDisposables = new DisposableStore();
 
 		const contextKeyService = templateDisposables.add(this.contextKeyService.createScoped(rowContainer));
