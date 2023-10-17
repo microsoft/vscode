68230ccffe3eb95a6c4d666a15a99c6cd0a26268
meganrogge <megan.rogge@microsoft.com>
2023-10-17 13:42:35 -0700

    revert some changes, handle everything in shouldFocusTerminal

--- src/vs/workbench/contrib/terminal/browser/terminal.ts
+++ src/vs/workbench/contrib/terminal/browser/terminal.ts
@@ -993,12 +993,6 @@ export interface ITerminalInstance {
 	 * Gets a terminal contribution by its ID.
 	 */
 	getContribution<T extends ITerminalContribution>(id: string): T | null;
-
-	/**
-	 * Whether the event should be handled by xterm.js or the workbench.
-	 * @param event the event to process
-	 */
-	shouldProcessKeyEvent(event: KeyboardEvent): boolean;
 }
 
 export const enum XtermTerminalConstants {
