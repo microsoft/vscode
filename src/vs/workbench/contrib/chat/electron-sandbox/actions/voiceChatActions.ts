3ec6c068e3ee8e2e2439fb8b84dd545533189a2d
Benjamin Pasero <benjamin.pasero@microsoft.com>
2023-10-23 09:12:30 +0200

    voice - stop recording animation with reduced motion enabled (#196230)

--- src/vs/workbench/contrib/chat/electron-sandbox/actions/voiceChatActions.ts
+++ src/vs/workbench/contrib/chat/electron-sandbox/actions/voiceChatActions.ts
@@ -683,8 +683,8 @@ registerThemingParticipant((theme, collector) => {
 
 	// Show a "microphone" icon when recording is in progress that glows via outline.
 	collector.addRule(`
-		.monaco-workbench .interactive-input-part .monaco-action-bar .action-label.codicon-loading.codicon-modifier-spin:not(.disabled):not(:hover),
-		.monaco-workbench .inline-chat .monaco-action-bar .action-label.codicon-loading.codicon-modifier-spin:not(.disabled):not(:hover) {
+		.monaco-workbench:not(.reduce-motion) .interactive-input-part .monaco-action-bar .action-label.codicon-loading.codicon-modifier-spin:not(.disabled):not(:hover),
+		.monaco-workbench:not(.reduce-motion) .inline-chat .monaco-action-bar .action-label.codicon-loading.codicon-modifier-spin:not(.disabled):not(:hover) {
 			color: ${activeRecordingColor};
 			outline: 1px solid ${activeRecordingColor};
 			outline-offset: -1px;
