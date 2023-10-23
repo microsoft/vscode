e3ba835cf6310c16889afd7d78664878cffc0bf1
Benjamin Pasero <benjamin.pasero@microsoft.com>
2023-10-23 19:39:55 +0200

    voice - tweak actions (#196303)

--- src/vs/workbench/contrib/chat/electron-sandbox/chat.contribution.ts
+++ src/vs/workbench/contrib/chat/electron-sandbox/chat.contribution.ts
@@ -3,7 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { InlineVoiceChatAction, QuickVoiceChatAction, StartVoiceChatAction, StopInlineVoiceChatAction, StopQuickVoiceChatAction, StopVoiceChatAction, StopVoiceChatAndSubmitAction, StopVoiceChatInChatEditorAction, StopVoiceChatInChatViewAction, VoiceChatInChatViewAction } from 'vs/workbench/contrib/chat/electron-sandbox/actions/voiceChatActions';
+import { InlineVoiceChatAction, QuickVoiceChatAction, StartVoiceChatAction, StopListeningInInlineChatAction, StopListeningInQuickChatAction, StopListeningInChatEditorAction, StopListeningInChatViewAction, VoiceChatInChatViewAction, StopListeningAction, StopListeningAndSubmitAction } from 'vs/workbench/contrib/chat/electron-sandbox/actions/voiceChatActions';
 import { registerAction2 } from 'vs/platform/actions/common/actions';
 
 registerAction2(StartVoiceChatAction);
@@ -12,11 +12,11 @@ registerAction2(VoiceChatInChatViewAction);
 registerAction2(QuickVoiceChatAction);
 registerAction2(InlineVoiceChatAction);
 
-registerAction2(StopVoiceChatAction);
-registerAction2(StopVoiceChatAndSubmitAction);
+registerAction2(StopListeningAction);
+registerAction2(StopListeningAndSubmitAction);
 
-registerAction2(StopVoiceChatInChatViewAction);
-registerAction2(StopVoiceChatInChatEditorAction);
-registerAction2(StopQuickVoiceChatAction);
-registerAction2(StopInlineVoiceChatAction);
+registerAction2(StopListeningInChatViewAction);
+registerAction2(StopListeningInChatEditorAction);
+registerAction2(StopListeningInQuickChatAction);
+registerAction2(StopListeningInInlineChatAction);
 
