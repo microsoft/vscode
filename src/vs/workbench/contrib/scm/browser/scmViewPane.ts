8c0b491626d5364ab22894a93e24f2ae44bc24fb
Ladislau Szomoru <3372902+lszomoru@users.noreply.github.com>
2023-10-23 12:56:46 +0200

    SCM - fix rendering issue with input box placeholder (#196241)

--- src/vs/workbench/contrib/scm/browser/scmViewPane.ts
+++ src/vs/workbench/contrib/scm/browser/scmViewPane.ts
@@ -1960,6 +1960,9 @@ class SCMInputWidget {
 
 		// ActionBar
 		const onDidChangeActionButton = () => {
+			// Update placeholder width to accommodate for the action bar
+			this.placeholderTextContainer.style.width = input.actionButton ? 'calc(100% - 26px)' : '100%';
+
 			this.actionBar.clear();
 			if (!input.actionButton) {
 				return;
@@ -1973,9 +1976,6 @@ class SCMInputWidget {
 				() => this.commandService.executeCommand(input.actionButton!.command.id, ...(input.actionButton!.command.arguments || [])));
 
 			this.actionBar.push(action, { icon: true, label: false });
-
-			// Update placeholder width to accommodate for the action bar
-			this.placeholderTextContainer.style.width = input.actionButton ? 'calc(100% - 26px)' : '100%';
 		};
 
 		this.repositoryDisposables.add(input.onDidChangeActionButton(onDidChangeActionButton, this));
