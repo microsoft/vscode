39b267ca20dac3982b04a7f9d2b857b59393bf80
João Moreno <joao.moreno@microsoft.com>
2023-10-27 21:10:55 +0200

    make postinstall log prettier

--- build/npm/postinstall.js
+++ build/npm/postinstall.js
@@ -11,8 +11,16 @@ const { dirs } = require('./dirs');
 const { setupBuildNpmrc } = require('./setupBuildNpmrc');
 const root = path.dirname(path.dirname(__dirname));
 
+function log(dir, message) {
+	if (process.stdout.isTTY) {
+		console.log(`\x1b[34m[${dir}]\x1b[0m`, message);
+	} else {
+		console.log(`[${dir}]`, message);
+	}
+}
+
 function run(command, args, opts) {
-	console.log('$ ' + command + ' ' + args.join(' '));
+	log(opts.cwd || '.', '$ ' + command + ' ' + args.join(' '));
 
 	const result = cp.spawnSync(command, args, opts);
 
@@ -41,7 +49,6 @@ function npmInstall(dir, opts) {
 
 	if (process.env['VSCODE_REMOTE_DEPENDENCIES_CONTAINER_NAME'] && /^(.build\/distro\/npm\/)?remote$/.test(dir)) {
 		const userinfo = os.userInfo();
-		console.log(`Installing dependencies in ${dir} inside container ${process.env['VSCODE_REMOTE_DEPENDENCIES_CONTAINER_NAME']}...`);
 
 		opts.cwd = root;
 		if (process.env['npm_config_arch'] === 'arm64') {
@@ -50,7 +57,6 @@ function npmInstall(dir, opts) {
 		run('sudo', ['docker', 'run', '-e', 'GITHUB_TOKEN', '-e', 'npm_config_arch', '-v', `${process.env['VSCODE_HOST_MOUNT']}:/root/vscode`, '-v', `${process.env['VSCODE_HOST_MOUNT']}/.build/.netrc:/root/.netrc`, '-w', dir, process.env['VSCODE_REMOTE_DEPENDENCIES_CONTAINER_NAME'], 'npm', command], opts);
 		run('sudo', ['chown', '-R', `${userinfo.uid}:${userinfo.gid}`, `${dir}/node_modules`], opts);
 	} else {
-		console.log(`Installing dependencies in ${dir}...`);
 		run('npm', [command], opts);
 	}
 }
