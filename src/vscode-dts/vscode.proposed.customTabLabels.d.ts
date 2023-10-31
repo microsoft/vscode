548dd9f6e1f0a17ef634b80906fbc200fa75236f
Alex0007 <Alex0007lolpvp@gmail.com>
2023-10-31 08:20:32 +0400

    Move Tab interface to root level

--- src/vscode-dts/vscode.proposed.customTabLabels.d.ts
+++ src/vscode-dts/vscode.proposed.customTabLabels.d.ts
@@ -4,60 +4,60 @@
  *--------------------------------------------------------------------------------------------*/
 
 declare module 'vscode' {
+	/**
+	 * Represents a tab within a {@link TabGroup group of tabs}.
+	 * Tabs are merely the graphical representation within the editor area.
+	 * A backing editor is not a guarantee.
+	 */
+	export interface Tab {
+
+		/**
+		 * The text displayed on the tab.
+		 */
+		readonly label: string;
+
+		/**
+		 * The group which the tab belongs to.
+		 */
+		readonly group: TabGroup;
+
+		/**
+		 * Defines the structure of the tab i.e. text, notebook, custom, etc.
+		 * Resource and other useful properties are defined on the tab kind.
+		 */
+		readonly input: TabInputText | TabInputTextDiff | TabInputCustom | TabInputWebview | TabInputNotebook | TabInputNotebookDiff | TabInputTerminal | unknown;
+
+		/**
+		 * Whether or not the tab is currently active.
+		 * This is dictated by being the selected tab in the group.
+		 */
+		readonly isActive: boolean;
+
+		/**
+		 * Whether or not the dirty indicator is present on the tab.
+		 */
+		readonly isDirty: boolean;
+
+		/**
+		 * Whether or not the tab is pinned (pin icon is present).
+		 */
+		readonly isPinned: boolean;
+
+		/**
+		 * Whether or not the tab is in preview mode.
+		 */
+		readonly isPreview: boolean;
+
+		/**
+		 * Whether or not the tab has custom label
+		 */
+		readonly isLabelChanged: boolean;
+	}
+
 	export namespace window {
 		export interface TabLabelInput {
 			name: string;
 			description: string;
-		}
-
-		/**
-		 * Represents a tab within a {@link TabGroup group of tabs}.
-		 * Tabs are merely the graphical representation within the editor area.
-		 * A backing editor is not a guarantee.
-		 */
-		export interface Tab {
-
-			/**
-			 * The text displayed on the tab.
-			 */
-			readonly label: string;
-
-			/**
-			 * The group which the tab belongs to.
-			 */
-			readonly group: TabGroup;
-
-			/**
-			 * Defines the structure of the tab i.e. text, notebook, custom, etc.
-			 * Resource and other useful properties are defined on the tab kind.
-			 */
-			readonly input: TabInputText | TabInputTextDiff | TabInputCustom | TabInputWebview | TabInputNotebook | TabInputNotebookDiff | TabInputTerminal | unknown;
-
-			/**
-			 * Whether or not the tab is currently active.
-			 * This is dictated by being the selected tab in the group.
-			 */
-			readonly isActive: boolean;
-
-			/**
-			 * Whether or not the dirty indicator is present on the tab.
-			 */
-			readonly isDirty: boolean;
-
-			/**
-			 * Whether or not the tab is pinned (pin icon is present).
-			 */
-			readonly isPinned: boolean;
-
-			/**
-			 * Whether or not the tab is in preview mode.
-			 */
-			readonly isPreview: boolean;
-
-			/**
-			 * Whether or not the tab has custom label
-			 */
-			readonly isLabelChanged: boolean;
 		}
 
 		/**
