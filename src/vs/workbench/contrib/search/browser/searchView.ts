147995eb043a6308c5a0a553ec26f07d83efa13f
Andrea Mah <31675041+andreamah@users.noreply.github.com>
2023-10-31 12:10:03 -0700

    Opening quick text search in a diff editor doesn't put selected text in input (#197096)
    Fixes #194675

--- src/vs/workbench/contrib/search/browser/searchView.ts
+++ src/vs/workbench/contrib/search/browser/searchView.ts
@@ -1280,13 +1280,6 @@ export class SearchView extends ViewPane {
 		}
 
 		editor = editor ?? this.editorService.activeTextEditorControl;
-		if (isDiffEditor(editor)) {
-			if (editor.getOriginalEditor().hasTextFocus()) {
-				editor = editor.getOriginalEditor();
-			} else {
-				editor = editor.getModifiedEditor();
-			}
-		}
 
 		if (!editor) {
 			return null;
@@ -2146,7 +2139,17 @@ export function getEditorSelectionFromMatch(element: FileMatchOrMatch, viewModel
 	return undefined;
 }
 
-export function getSelectionTextFromEditor(allowUnselectedWord: boolean, editor: IEditor): string | null {
+export function getSelectionTextFromEditor(allowUnselectedWord: boolean, activeEditor: IEditor): string | null {
+
+	let editor = activeEditor;
+
+	if (isDiffEditor(editor)) {
+		if (editor.getOriginalEditor().hasTextFocus()) {
+			editor = editor.getOriginalEditor();
+		} else {
+			editor = editor.getModifiedEditor();
+		}
+	}
 
 	if (!isCodeEditor(editor) || !editor.hasModel()) {
 		return null;
