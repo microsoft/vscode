8b9b3214e81eef335bcb2190de06cf1b6fe500b8
Benjamin Pasero <benjamin.pasero@microsoft.com>
2023-11-04 15:10:49 +0100

    aux window - :lipstick:

--- src/vs/workbench/services/host/browser/browserHostService.ts
+++ src/vs/workbench/services/host/browser/browserHostService.ts
@@ -38,7 +38,6 @@ import { ITextEditorOptions } from 'vs/platform/editor/common/editor';
 import { IUserDataProfileService } from 'vs/workbench/services/userDataProfile/common/userDataProfile';
 import { coalesce } from 'vs/base/common/arrays';
 import { disposableInterval } from 'vs/base/common/async';
-import { isAuxiliaryWindow } from 'vs/workbench/services/auxiliaryWindow/browser/auxiliaryWindowService';
 
 /**
  * A workspace to open in the workbench can either be:
@@ -228,16 +227,14 @@ export class BrowserHostService extends Disposable implements IHostService {
 			// Emit via interval: immediately when opening an auxiliary window,
 			// it is possible that document focus has not yet changed, so we
 			// poll for a while to ensure we catch the event.
-			if (isAuxiliaryWindow(window)) {
-				disposables.add(disposableInterval(() => {
-					const hasFocus = window.document.hasFocus();
-					if (hasFocus) {
-						emitter.fire(windowId);
-					}
+			disposables.add(disposableInterval(() => {
+				const hasFocus = window.document.hasFocus();
+				if (hasFocus) {
+					emitter.fire(windowId);
+				}
 
-					return hasFocus;
-				}, 100, 20));
-			}
+				return hasFocus;
+			}, 100, 20));
 		}, { window, disposables: this._store }));
 
 		return Event.map(Event.latch(emitter.event, undefined, this._store), () => undefined, this._store);
