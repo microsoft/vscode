cfb672d3de17e657d0545276b51220dffa068afc
Aiday Marlen Kyzy <amarlenkyzy@microsoft.com>
2023-11-10 13:59:16 +0100

    checking that the first word is covered or null, doing the same for the last word

--- src/vs/workbench/contrib/inlineChat/browser/inlineChatDecorations.ts
+++ src/vs/workbench/contrib/inlineChat/browser/inlineChatDecorations.ts
@@ -125,25 +125,33 @@ export class InlineChatDecorationsContribution extends Disposable implements IEd
 	}
 
 	private _onSelectionOrContentChanged(editor: IActiveCodeEditor): void {
-		console.log('inside of selection change');
 		const selection = editor.getSelection();
 		const selectionIsEmpty = selection.isEmpty();
+		const startLineNumber = selection.startLineNumber;
 		const model = editor.getModel();
 		const uri = model.uri;
 
-		const isInlineChatVisible = this._inlineChatSessionService.getSession(editor, uri);
-		const startLineNumber = selection.startLineNumber;
 		const hasBreakpoint = this._debugService.getModel().getBreakpoints({ uri: uri, lineNumber: startLineNumber }).length > 0;
 
-		const startPosition = selection.getStartPosition();
-		const endPosition = selection.getEndPosition();
-		const startWord = model.getWordAtPosition(startPosition);
-		const endWord = model.getWordAtPosition(endPosition);
-		const shouldBeVisibleOnNonEmptySelection = startWord && endWord && startPosition.column <= startWord.startColumn && endPosition.column >= endWord.endColumn;
+		let isEnabled = false;
+		if (selectionIsEmpty) {
+			if (/^\s*$/g.test(model.getLineContent(startLineNumber))) {
+				isEnabled = true;
+			}
+		} else {
+			const startPosition = selection.getStartPosition();
+			const endPosition = selection.getEndPosition();
+			const startWord = model.getWordAtPosition(startPosition);
+			const endWord = model.getWordAtPosition(endPosition);
+			const isFirstWordCoveredOrNull = !!startWord ? startPosition.column <= startWord.startColumn : true;
+			const isLastWordCoveredOrNull = !!endWord ? endPosition.column >= endWord.endColumn : true;
+			isEnabled = isFirstWordCoveredOrNull && isLastWordCoveredOrNull;
 
-		const isEnabled = !isInlineChatVisible && !hasBreakpoint &&
-			(selectionIsEmpty && /^\s*$/g.test(model.getLineContent(startLineNumber))
-				|| !selectionIsEmpty && shouldBeVisibleOnNonEmptySelection);
+		}
+
+		// const isEnabled = !hasBreakpoint &&
+		// 	(selectionIsEmpty && /^\s*$/g.test(model.getLineContent(startLineNumber))
+		// 		|| !selectionIsEmpty && shouldBeVisibleOnNonEmptySelection);
 
 		if (isEnabled) {
 			if (this._gutterDecorationID === undefined) {
