5e27a2845a87be4b4bede3e51073f94609445e51
Henning Dieterichs <hdieterichs@microsoft.com>
2023-11-13 12:23:58 +0100

    Caches view model on input.

--- src/vs/workbench/contrib/multiDiffEditor/browser/multiDiffEditorInput.ts
+++ src/vs/workbench/contrib/multiDiffEditor/browser/multiDiffEditorInput.ts
@@ -7,6 +7,9 @@ import { localize } from 'vs/nls';
 import { URI } from 'vs/base/common/uri';
 import { DEFAULT_EDITOR_ASSOCIATION, EditorInputCapabilities } from 'vs/workbench/common/editor';
 import { EditorInput } from 'vs/workbench/common/editor/editorInput';
+import { ConstLazyPromise, IDiffEntry, IMultiDocumentDiffEditorModel } from 'vs/editor/browser/widget/multiDiffEditorWidget/model';
+import { ITextModelService } from 'vs/editor/common/services/resolverService';
+import { toDisposable } from 'vs/base/common/lifecycle';
 
 export class MultiDiffEditorInput extends EditorInput {
 	static readonly ID: string = 'workbench.input.multiDiffEditor';
@@ -31,8 +34,38 @@ export class MultiDiffEditorInput extends EditorInput {
 		return DEFAULT_EDITOR_ASSOCIATION.id;
 	}
 
-	constructor(readonly label: string | undefined, readonly resources: readonly MultiDiffEditorInputData[]) {
+	private _viewModel: IMultiDocumentDiffEditorModel | undefined;
+
+	constructor(
+		readonly label: string | undefined,
+		readonly resources: readonly MultiDiffEditorInputData[],
+		@ITextModelService private readonly _textModelService: ITextModelService,
+	) {
 		super();
+	}
+
+	async getViewModel(): Promise<IMultiDocumentDiffEditorModel> {
+		if (!this._viewModel) {
+			this._viewModel = await this._createViewModel();
+		}
+		return this._viewModel;
+	}
+
+	private async _createViewModel(): Promise<IMultiDocumentDiffEditorModel> {
+		const rs = await Promise.all(this.resources.map(async r => ({
+			originalRef: await this._textModelService.createModelReference(r.original!),
+			modifiedRef: await this._textModelService.createModelReference(r.modified!),
+			title: r.resource.fsPath,
+		})));
+
+		return {
+			onDidChange: () => toDisposable(() => { }),
+			diffs: rs.map(r => new ConstLazyPromise<IDiffEntry>({
+				original: r.originalRef.object.textEditorModel,
+				modified: r.modifiedRef.object.textEditorModel,
+				title: r.title,
+			})),
+		};
 	}
 }
 
