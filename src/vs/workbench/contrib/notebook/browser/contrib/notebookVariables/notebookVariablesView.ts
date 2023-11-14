02aedaef3c5b2d76273893c79789ca520866f286
aamunger <aamunger@microsoft.com>
2023-11-13 16:31:44 -0800

    fix tree layout, add proposed API

--- src/vs/workbench/contrib/notebook/browser/contrib/notebookVariables/notebookVariablesView.ts
+++ src/vs/workbench/contrib/notebook/browser/contrib/notebookVariables/notebookVariablesView.ts
@@ -11,14 +11,17 @@ import { IContextKeyService } from 'vs/platform/contextkey/common/contextkey';
 import { IContextMenuService } from 'vs/platform/contextview/browser/contextView';
 import { IInstantiationService } from 'vs/platform/instantiation/common/instantiation';
 import { IKeybindingService } from 'vs/platform/keybinding/common/keybinding';
+import { WorkbenchObjectTree } from 'vs/platform/list/browser/listService';
 import { IOpenerService } from 'vs/platform/opener/common/opener';
 import { IQuickInputService } from 'vs/platform/quickinput/common/quickInput';
 import { ITelemetryService } from 'vs/platform/telemetry/common/telemetry';
 import { IThemeService } from 'vs/platform/theme/common/themeService';
 import { IViewPaneOptions, ViewPane } from 'vs/workbench/browser/parts/views/viewPane';
 import { IViewDescriptorService } from 'vs/workbench/common/views';
-import { MockVariables, NotebookVariableAccessibilityProvider, NotebookVariableRenderer, NotebookVariablesDataSource, NotebookVariablesDelegate, NotebookVariablesTree } from 'vs/workbench/contrib/notebook/browser/contrib/notebookVariables/notebookVariablesTree';
+import { mockVariables, NotebookVariableAccessibilityProvider, NotebookVariableRenderer, INotebookVariableElement, NotebookVariablesDelegate, NotebookVariablesTree } from 'vs/workbench/contrib/notebook/browser/contrib/notebookVariables/notebookVariablesTree';
 import { getNotebookEditorFromEditorPane } from 'vs/workbench/contrib/notebook/browser/notebookBrowser';
+import { NotebookTextModel } from 'vs/workbench/contrib/notebook/common/model/notebookTextModel';
+import { ICellExecutionStateChangedEvent, IExecutionStateChangedEvent, INotebookExecutionStateService } from 'vs/workbench/contrib/notebook/common/notebookExecutionStateService';
 import { INotebookKernelService } from 'vs/workbench/contrib/notebook/common/notebookKernelService';
 import { IEditorService } from 'vs/workbench/services/editor/common/editorService';
 
@@ -28,11 +31,13 @@ export class NotebookVariablesView extends ViewPane {
 	static readonly TITLE: ILocalizedString = nls.localize2('notebook.notebookVariables', "Notebook Variables");
 
 	private tree: NotebookVariablesTree | undefined;
+	private activeNotebook: NotebookTextModel | undefined;
 
 	constructor(
 		options: IViewPaneOptions,
 		@IEditorService private readonly editorService: IEditorService,
 		@INotebookKernelService private readonly notebookKernelService: INotebookKernelService,
+		@INotebookExecutionStateService private readonly notebookExecutionStateService: INotebookExecutionStateService,
 		@IKeybindingService keybindingService: IKeybindingService,
 		@IContextMenuService contextMenuService: IContextMenuService,
 		@IContextKeyService contextKeyService: IContextKeyService,
@@ -47,29 +52,62 @@ export class NotebookVariablesView extends ViewPane {
 	) {
 		super(options, keybindingService, contextMenuService, configurationService, contextKeyService, viewDescriptorService, instantiationService, openerService, themeService, telemetryService);
 
-		this._register(this.editorService.onDidActiveEditorChange(this.handleActiveEditorChange));
+		this._register(this.editorService.onDidActiveEditorChange(this.handleActiveEditorChange.bind(this)));
+		this._register(this.notebookExecutionStateService.onDidChangeExecution(this.handleExecutionStateChange.bind(this)));
 	}
 
 	public override renderBody(container: HTMLElement): void {
 		super.renderBody(container);
 
-		this.tree = this.instantiationService.createInstance(NotebookVariablesTree, 'notebookVariablesView', container,
+		this.tree = <NotebookVariablesTree>this.instantiationService.createInstance(
+			WorkbenchObjectTree,
+			'notebookVariablesTree',
+			container,
 			new NotebookVariablesDelegate(),
 			[new NotebookVariableRenderer()],
-			new NotebookVariablesDataSource(),
-			{ accessibilityProvider: new NotebookVariableAccessibilityProvider() });
+			{
+				identityProvider: { getId: (e: INotebookVariableElement) => e.id },
+				horizontalScrolling: false,
+				supportDynamicHeights: true,
+				hideTwistiesOfChildlessElements: true,
+				accessibilityProvider: new NotebookVariableAccessibilityProvider(),
+				setRowLineHeight: false,
+			});
 
-		this.tree.setInput(new MockVariables());
+		this.tree.layout();
+		this.tree?.setChildren(null, []);
+	}
+
+	protected override layoutBody(height: number, width: number): void {
+		super.layoutBody(height, width);
+		this.tree?.layout(height, width);
 	}
 
 	private handleActiveEditorChange() {
 		const activeEditorPane = this.editorService.activeEditorPane;
 		if (activeEditorPane && activeEditorPane.getId() === 'workbench.editor.notebook') {
-			const notebookModel = getNotebookEditorFromEditorPane(activeEditorPane)?.textModel;
-			if (notebookModel) {
-				const selectedKernel = this.notebookKernelService.getMatchingKernel(notebookModel).selected;
-				console.log(selectedKernel?.id);
+			const notebookDocument = getNotebookEditorFromEditorPane(activeEditorPane)?.getViewModel()?.notebookDocument;
+			if (notebookDocument && notebookDocument !== this.activeNotebook) {
+				this.activeNotebook = notebookDocument;
+				this.updateVariables(this.activeNotebook);
 			}
+		}
+	}
+
+	private handleExecutionStateChange(event: ICellExecutionStateChangedEvent | IExecutionStateChangedEvent) {
+		if (this.activeNotebook) {
+			// changed === undefined -> excecution ended
+			if (event.changed === undefined && event.affectsNotebook(this.activeNotebook?.uri)) {
+				this.updateVariables(this.activeNotebook);
+			}
+		}
+	}
+
+	private updateVariables(notebook: NotebookTextModel) {
+		const selectedKernel = this.notebookKernelService.getMatchingKernel(notebook).selected;
+		if (selectedKernel) {
+
+			this.tree?.setChildren(null, mockVariables(notebook.uri.toString(), selectedKernel.label));
 		}
 	}
 }
