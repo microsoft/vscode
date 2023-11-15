90c408ea998ded2bcb2b6b09739a99981f89c077
João Moreno <joao.moreno@microsoft.com>
2023-11-15 15:30:27 +0100

    cleanup logs

--- build/azure-pipelines/common/retry.ts
+++ build/azure-pipelines/common/retry.ts
@@ -8,7 +8,6 @@ export async function retry<T>(fn: (attempt: number) => Promise<T>): Promise<T> 
 
 	for (let run = 1; run <= 10; run++) {
 		try {
-			console.log(`[retry] Attempt ${run}...`);
 			return await fn(run);
 		} catch (err) {
 			if (!/fetch failed|timeout|TimeoutError|Timeout Error|RestError|Client network socket disconnected|socket hang up|ECONNRESET|CredentialUnavailableError|endpoints_resolution_error|Audience validation failed/i.test(err.message)) {
@@ -16,17 +15,13 @@ export async function retry<T>(fn: (attempt: number) => Promise<T>): Promise<T> 
 			}
 
 			lastError = err;
-			const millis = Math.floor((Math.random() * 200) + (50 * Math.pow(1.5, run)));
-			console.log(`[retry] Request failed, retrying in ${millis}ms...`);
-			console.error(err);
 
 			// maximum delay is 10th retry: ~3 seconds
+			const millis = Math.floor((Math.random() * 200) + (50 * Math.pow(1.5, run)));
 			await new Promise(c => setTimeout(c, millis));
-
-			console.log('[retry] After delay, retrying request...');
 		}
 	}
 
-	console.log(`[retry] Too many retries, aborting.`);
+	console.error(`Too many retries, aborting.`);
 	throw lastError;
 }
