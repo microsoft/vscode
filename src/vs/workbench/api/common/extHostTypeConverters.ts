da5ab29656a9f0bef63602fbce10968a7f4db2b5
Rob Lourens <roblourens@gmail.com>
2023-11-20 12:02:17 -0600

    Don't throw when receiving an unknown chat progress update (#198533)
    * style update
    
    * Don't throw when getting a bad progress update

--- src/vs/workbench/api/common/extHostTypeConverters.ts
+++ src/vs/workbench/api/common/extHostTypeConverters.ts
@@ -2295,7 +2295,7 @@ export namespace InteractiveEditorResponseFeedbackKind {
 }
 
 export namespace ChatResponseProgress {
-	export function from(extension: IExtensionDescription, progress: vscode.ChatAgentExtendedProgress): extHostProtocol.IChatProgressDto {
+	export function from(extension: IExtensionDescription, progress: vscode.ChatAgentExtendedProgress): extHostProtocol.IChatProgressDto | undefined {
 		if ('placeholder' in progress && 'resolvedContent' in progress) {
 			return { content: progress.placeholder, kind: 'asyncContent' } satisfies extHostProtocol.IChatAsyncContentDto;
 		} else if ('markdownContent' in progress) {
@@ -2349,7 +2349,7 @@ export namespace ChatResponseProgress {
 		} else if ('message' in progress) {
 			return { content: progress.message, kind: 'progressMessage' };
 		} else {
-			throw new Error('Invalid progress type: ' + JSON.stringify(progress));
+			return undefined;
 		}
 	}
 }
