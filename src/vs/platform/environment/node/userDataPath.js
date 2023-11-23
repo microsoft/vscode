e85cb623587f02cba3819b1e8d4bd2b29598b5f2
zongou <zongou@outlook.com>
2023-11-24 06:14:40 +0800

    fix pty host  to use remote shell on android

--- src/vs/platform/environment/node/userDataPath.js
+++ src/vs/platform/environment/node/userDataPath.js
@@ -91,6 +91,7 @@
 				case 'darwin':
 					appDataPath = path.join(os.homedir(), 'Library', 'Application Support');
 					break;
+				case 'android':
 				case 'linux':
 					appDataPath = process.env['XDG_CONFIG_HOME'] || path.join(os.homedir(), '.config');
 					break;
