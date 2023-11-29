7698a1076d60b2427f2ded88bcbe2efdfa1f10d8
Simon Siefke <simon.siefke@gmail.com>
2023-11-29 18:36:25 +0100

    update script

--- build/updateCssImports.mjs
+++ build/updateCssImports.mjs
@@ -9,6 +9,7 @@ const root = join(__dirname, '..')
 const src = join(root, 'src')
 
 const getAllFiles = root => {
+	// @ts-ignore
 	return readdir(root, { recursive: true })
 }
 
@@ -21,14 +22,83 @@ const hasCssImport = content => {
 	return content.includes(`import 'vs/css!`)
 }
 
+const isImportLine = line => {
+	return line.startsWith('import ')
+}
+
+
+const getLastImportLineIndex = lines => {
+	let lastIndex = 0
+	for (let i = 0; i < lines.length; i++) {
+		const line = lines[i]
+		if (isImportLine(line)) {
+			lastIndex = i
+		}
+	}
+	return lastIndex
+}
+
+const getNewContentTop = (lines) => {
+	const newLinesTop = []
+	const newCssImports = []
+	for (const line of lines) {
+		if (!isImportLine(line)) {
+			newLinesTop.push(line)
+		} else if (hasCssImport(line)) {
+			newCssImports.push(line)
+		}
+	}
+	return {
+		newLinesTop,
+		newCssImports
+	}
+}
+
+
+const RE_VS_CSS_IMPORT = /^import 'vs\/css!(.*)'/
+
+const parseNewCssImport = line => {
+	const match = line.match(RE_VS_CSS_IMPORT)
+	if (!match) {
+		throw new Error(`failed to parse css import line ${line}`)
+	}
+	const path = match[1]
+	return path
+}
+
+const getNewCssImportLines = (path, newCssImports) => {
+	const newCssImportLines = []
+	newCssImportLines.push(`import { importCss } from 'vs/base/browser/importCss';`)
+	newCssImportLines.push('')
+	for (const newCssImport of newCssImports) {
+		const path = parseNewCssImport(newCssImport)
+		newCssImportLines.push(`importCss('${path}', import.meta.url)`)
+	}
+	return newCssImportLines
+}
+
+const getNewContent = content => {
+	const lines = content.split('\n')
+	const lastImportLineIndex = getLastImportLineIndex(lines)
+	if (lastImportLineIndex === -1) {
+		throw new Error(`no imports found`)
+	}
+	const { newLinesTop, newCssImports } = getNewContentTop(lines.slice(0, lastImportLineIndex))
+	const newCssImportLines = getNewCssImportLines(newCssImports)
+	const newLinesBottom = lines.slice(lastImportLineIndex)
+	const newLines = [...newLinesTop, ...newCssImportLines, ...newLinesBottom]
+	const newContent = newLines.join('\n')
+	return newContent
+}
+
 const updateImportsPath = async (path) => {
 	const absolutePath = join(src, path)
 	const content = await fs.readFile(absolutePath, 'utf8')
 	if (!hasCssImport(content)) {
 		return
 	}
-	const lines = content.split('\n')
-
+	const newContent = getNewContent(content)
+	await fs.writeFile(absolutePath, newContent)
 }
 
 const updateImportsPaths = async paths => {
