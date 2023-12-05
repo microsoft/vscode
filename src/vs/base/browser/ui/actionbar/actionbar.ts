9574cbaab5969090f11c946f048838b09f0d461f
meganrogge <megan.rogge@microsoft.com>
2023-12-05 12:32:58 -0600

    Fix hover showing when navigating between ActionBar sections

--- src/vs/base/browser/ui/actionbar/actionbar.ts
+++ src/vs/base/browser/ui/actionbar/actionbar.ts
@@ -201,7 +201,7 @@ export class ActionBar extends Disposable implements IActionRunner {
 			}
 
 			// Recompute focused item
-			else if (event.equals(KeyCode.Tab) || event.equals(KeyMod.Shift | KeyCode.Tab)) {
+			else if (event.equals(KeyCode.Tab) || event.equals(KeyMod.Shift | KeyCode.Tab) || event.equals(KeyCode.UpArrow) || event.equals(KeyCode.DownArrow)) {
 				this.updateFocusedItem();
 			}
 		}));
