0c1afa1ff8017b01fcadfb873a955c94c0edb383
BeniBenj <besimmonds@microsoft.com>
2023-12-05 14:42:38 +0100

    :lipstick:

--- src/vs/workbench/browser/parts/editor/editorGroupView.ts
+++ src/vs/workbench/browser/parts/editor/editorGroupView.ts
@@ -1900,10 +1900,6 @@ export class EditorGroupView extends Themable implements IEditorGroupView {
 	}
 
 	private canLock(): boolean {
-		if (this.groupsView.isAuxiliary) {
-			return true; // always allow locking in auxiliary parts, even single groups
-		}
-
 		return this.editorPartsView.groups.length > 1; // only allow locking if more than 1 group is opened
 	}
 
