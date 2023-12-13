5da45d17d14a7e159a72a594198535112baa9298
Benjamin Simmonds <44439583+benibenj@users.noreply.github.com>
2023-12-13 09:32:59 +0100

    remove highlight for panel (#200717)

--- src/vs/workbench/browser/parts/paneCompositePart.ts
+++ src/vs/workbench/browser/parts/paneCompositePart.ts
@@ -306,7 +306,6 @@ export abstract class AbstractPaneCompositePart extends CompositePart<PaneCompos
 			orientation: ActionsOrientation.HORIZONTAL,
 			getKeyBinding: action => this.keybindingService.lookupKeybinding(action.id),
 			anchorAlignmentProvider: () => this.getTitleAreaDropDownAnchorAlignment(),
-			highlightToggledItems: true,
 			toggleMenuTitle: localize('moreActions', "More Actions...")
 		}));
 
