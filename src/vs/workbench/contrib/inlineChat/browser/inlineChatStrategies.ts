400734ae933f22bd75c762011beb059eace8298f
Johannes <johannes.rieken@gmail.com>
2023-12-19 17:41:16 +0100

    fix https://github.com/microsoft/vscode/issues/201197

--- src/vs/workbench/contrib/inlineChat/browser/inlineChatStrategies.ts
+++ src/vs/workbench/contrib/inlineChat/browser/inlineChatStrategies.ts
@@ -75,8 +75,6 @@ export abstract class EditModeStrategy {
 	abstract renderChanges(response: ReplyResponse): Promise<Position | undefined>;
 
 	abstract hasFocus(): boolean;
-
-	abstract needsMargin(): boolean;
 }
 
 export class PreviewStrategy extends EditModeStrategy {
@@ -164,10 +162,6 @@ export class PreviewStrategy extends EditModeStrategy {
 
 	hasFocus(): boolean {
 		return this._zone.widget.hasFocus();
-	}
-
-	needsMargin(): boolean {
-		return true;
 	}
 }
 
@@ -306,11 +300,6 @@ export class LivePreviewStrategy extends EditModeStrategy {
 		}
 		this._zone.widget.updateStatus(message);
 	}
-
-	override needsMargin(): boolean {
-		return true;
-	}
-
 
 	private async _updateDiffZones() {
 		const diff = await this._editorWorkerService.computeDiff(this._session.textModel0.uri, this._session.textModelN.uri, { ignoreTrimWhitespace: false, maxComputationTimeMs: 5000, computeMoves: false }, 'advanced');
@@ -899,10 +888,6 @@ export class LiveStrategy extends EditModeStrategy {
 			message = localize('lines.NM', "{0} changes", hunkCount);
 		}
 		this._zone.widget.updateStatus(message);
-	}
-
-	override needsMargin(): boolean {
-		return true;
 	}
 
 	hasFocus(): boolean {
