70edf73ce78042b5fc43092250be3c8d053872f8
João Moreno <joao.moreno@microsoft.com>
2023-12-20 16:36:23 +0100

    :lipstick:

--- src/vs/workbench/browser/actions/listCommands.ts
+++ src/vs/workbench/browser/actions/listCommands.ts
@@ -488,8 +488,8 @@ function selectElement(accessor: ServicesAccessor, retainCurrentFocus: boolean):
 	// List
 	if (focused instanceof List || focused instanceof PagedList || focused instanceof Table) {
 		const list = focused;
-		list.setSelection(list.getFocus(), fakeKeyboardEvent);
 		list.setAnchor(list.getFocus()[0]);
+		list.setSelection(list.getFocus(), fakeKeyboardEvent);
 	}
 
 	// Trees
