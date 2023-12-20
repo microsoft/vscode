e3c70a49babb1a86797d440569437c02ab17b373
Daniel Imms <2193314+Tyriar@users.noreply.github.com>
2023-12-20 07:06:23 -0800

    Package and explain how to watch

--- scripts/xterm-symlink.ps1
+++ scripts/xterm-symlink.ps1
@@ -8,20 +8,27 @@ Param(
 	$XtermFolder
 )
 
-$TargetFolder = "./node_modules/xterm"
+$TargetFolder = "./node_modules/@xterm/xterm"
 
 if (Test-Path $TargetFolder -PathType Container)
 {
-	Write-Host "Deleting $TargetFolder"
+	Write-Host -ForegroundColor Green ":: Deleting $TargetFolder`n"
 	Remove-Item -Path $TargetFolder
 }
 
 if (Test-Path $XtermFolder -PathType Container)
 {
-	Write-Host "Creating symlink $TargetFolder -> $XtermFolder"
+	Write-Host -ForegroundColor Green "`n:: Creating symlink $TargetFolder -> $XtermFolder`n"
 	New-Item -Path $TargetFolder -ItemType SymbolicLink -Value $XtermFolder
+
+	Write-Host -ForegroundColor Green "`n:: Packaging xterm.js`n"
+	Set-Location $TargetFolder
+	yarn package -- --mode development
+	Set-Location -
+
+	Write-Host -ForegroundColor Green "`n:: Finished! To watch changes, open the VS Code terminal in the xterm.js repo and run:`n`n    yarn package -- --mode development --watch"
 }
 else
 {
-	Write-Host "$XtermFolder is not a valid folder"
+	Write-Error -ForegroundColor Red "`n:: $XtermFolder is not a valid folder"
 }
