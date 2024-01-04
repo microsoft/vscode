173a02bb12d82a03a52bc0150b7c1c79c71dc68a
Benjamin Pasero <benjamin.pasero@microsoft.com>
2024-01-04 12:44:20 +0100

    introduce `zoomPerWindow` setting

--- src/vs/workbench/electron-sandbox/desktop.contribution.ts
+++ src/vs/workbench/electron-sandbox/desktop.contribution.ts
@@ -10,7 +10,7 @@ import { IConfigurationRegistry, Extensions as ConfigurationExtensions, Configur
 import { KeyMod, KeyCode } from 'vs/base/common/keyCodes';
 import { isLinux, isMacintosh, isWindows } from 'vs/base/common/platform';
 import { ConfigureRuntimeArgumentsAction, ToggleDevToolsAction, ReloadWindowWithExtensionsDisabledAction, OpenUserDataFolderAction } from 'vs/workbench/electron-sandbox/actions/developerActions';
-import { ZoomResetAllWindowsAction, ZoomOutAllWindowsAction, ZoomInAllWindowsAction, CloseWindowAction, SwitchWindowAction, QuickSwitchWindowAction, NewWindowTabHandler, ShowPreviousWindowTabHandler, ShowNextWindowTabHandler, MoveWindowTabToNewWindowHandler, MergeWindowTabsHandlerHandler, ToggleWindowTabsBarHandler, ZoomInActiveWindowAction, ZoomOutActiveWindowAction, ZoomResetActiveWindowAction } from 'vs/workbench/electron-sandbox/actions/windowActions';
+import { ZoomResetAction, ZoomOutAction, ZoomInAction, CloseWindowAction, SwitchWindowAction, QuickSwitchWindowAction, NewWindowTabHandler, ShowPreviousWindowTabHandler, ShowNextWindowTabHandler, MoveWindowTabToNewWindowHandler, MergeWindowTabsHandlerHandler, ToggleWindowTabsBarHandler } from 'vs/workbench/electron-sandbox/actions/windowActions';
 import { ContextKeyExpr } from 'vs/platform/contextkey/common/contextkey';
 import { KeybindingsRegistry, KeybindingWeight } from 'vs/platform/keybinding/common/keybindingsRegistry';
 import { CommandsRegistry } from 'vs/platform/commands/common/commands';
@@ -27,18 +27,15 @@ import { ShutdownReason } from 'vs/workbench/services/lifecycle/common/lifecycle
 import { NativeWindow } from 'vs/workbench/electron-sandbox/window';
 import { ModifierKeyEmitter } from 'vs/base/browser/dom';
 import { applicationConfigurationNodeBase, securityConfigurationNodeBase } from 'vs/workbench/common/configuration';
+import product from 'vs/platform/product/common/product';
 
 // Actions
 (function registerActions(): void {
 
 	// Actions: Zoom
-	registerAction2(ZoomInAllWindowsAction);
-	registerAction2(ZoomOutAllWindowsAction);
-	registerAction2(ZoomResetAllWindowsAction);
-
-	registerAction2(ZoomInActiveWindowAction);
-	registerAction2(ZoomOutActiveWindowAction);
-	registerAction2(ZoomResetActiveWindowAction);
+	registerAction2(ZoomInAction);
+	registerAction2(ZoomOutAction);
+	registerAction2(ZoomResetAction);
 
 	// Actions: Window
 	registerAction2(SwitchWindowAction);
@@ -194,9 +191,16 @@ import { applicationConfigurationNodeBase, securityConfigurationNodeBase } from 
 			'window.zoomLevel': {
 				'type': 'number',
 				'default': 0,
-				'minimum': -5,
-				'description': localize('zoomLevel', "Adjust the zoom level of the window. The original size is 0 and each increment above (e.g. 1) or below (e.g. -1) represents zooming 20% larger or smaller. You can also enter decimals to adjust the zoom level with a finer granularity."),
+				'minimum': -8,
+				'maximum': 8,
+				'markdownDescription': localize({ comment: ['{0} will be a setting name rendered as a link'], key: 'zoomLevel' }, "Adjust the default zoom level for all windows. The original size is `0` and each increment above (e.g. `1`) or below (e.g. `-1`) represents zooming `20%` larger or smaller. You can also enter decimals to adjust the zoom level with a finer granularity. See {0} for configuring if the 'Zoom In' and 'Zoom Out' commands apply the zoom level to all windows or only the active window.", '`#window.zoomPerWindow#`'),
 				ignoreSync: true,
+				tags: ['accessibility']
+			},
+			'window.zoomPerWindow': {
+				'type': 'boolean',
+				'default': product.quality !== 'stable', // TODO@bpasero flip the default eventually
+				'markdownDescription': localize({ comment: ['{0} will be a setting name rendered as a link'], key: 'zoomPerWindow' }, "Controls if the 'Zoom In' and 'Zoom Out' commands apply the zoom level to all windows or only the active window. See {0} for configuring a default zoom level for all windows.", '`#window.zoomLevel#`'),
 				tags: ['accessibility']
 			},
 			'window.newWindowDimensions': {
