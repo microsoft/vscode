e3beebfdd2360a0fde077b767be82396e9c805c4
João Moreno <joao.moreno@microsoft.com>
2024-01-05 16:12:57 +0100

    fixes #201890

--- src/vs/workbench/contrib/files/browser/views/explorerView.ts
+++ src/vs/workbench/contrib/files/browser/views/explorerView.ts
@@ -693,9 +693,7 @@ export class ExplorerView extends ViewPane implements IExplorerView {
 		}
 
 		const toRefresh = item || this.tree.getInput();
-		return this.tree.updateChildren(toRefresh, recursive, !!item, {
-			diffIdentityProvider: identityProvider
-		});
+		return this.tree.updateChildren(toRefresh, recursive, !!item);
 	}
 
 	override getOptimalWidth(): number {
