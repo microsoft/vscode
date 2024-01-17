7fd2edcf4696f1707be45d38c5bfc1b14dc47f41
Robo <hop2deep@gmail.com>
2024-01-17 20:18:31 +0900

    fix: ensure requirements check works for libc development versions (#202660)

--- resources/server/bin/helpers/check-requirements-linux.sh
+++ resources/server/bin/helpers/check-requirements-linux.sh
@@ -83,10 +83,10 @@ if [ -n "$(ldd --version | grep -v musl)" ]; then
         libc_real_path=$(readlink -f "$libc_path")
         if [ -x "$libc_real_path" ]; then
             # get version from executable
-            libc_version=$($libc_real_path --version | sed -n 's/.*stable release version \([0-9]\+\.[0-9]\+\).*/\1/p')
+            libc_version=$($libc_real_path --version | sed -n 's/.*release version \([0-9]\+\.[0-9]\+\).*/\1/p')
         else
             # .so is not executable on this host; try getting from strings
-            libc_version=$(cat "$libc_real_path" | sed -n 's/.*stable release version \([0-9]\+\.[0-9]\+\).*/\1/p')
+            libc_version=$(cat "$libc_real_path" | sed -n 's/.*release version \([0-9]\+\.[0-9]\+\).*/\1/p')
         fi
         if [ "$(printf '%s\n' "2.28" "$libc_version" | sort -V | head -n1)" = "2.28" ]; then
             found_required_glibc=1
