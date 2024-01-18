5870187e32f4b394e490e8d25f7dd84386d86ab4
Simon Siefke <simon.siefke@gmail.com>
2024-01-18 19:03:39 +0100

    Discard changes to src/vs/base/browser/ui/codicons/codiconStyles.ts

--- src/vs/base/browser/ui/codicons/codiconStyles.ts
+++ src/vs/base/browser/ui/codicons/codiconStyles.ts
@@ -3,7 +3,5 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { importCss } from 'vs/base/browser/importCss';
-
-importCss('./codicon/codicon.css', import.meta.url)
-importCss('./codicon/codicon-modifiers.css', import.meta.url)
+import 'vs/css!./codicon/codicon';
+import 'vs/css!./codicon/codicon-modifiers';
