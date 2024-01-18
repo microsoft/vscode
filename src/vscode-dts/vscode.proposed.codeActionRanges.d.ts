d2bed31175980d0c20ea8ee117ffa2a22513491b
Your Name <justchen@microsoft.com>
2024-01-18 08:21:13 -0800

    fixed spacing

--- src/vscode-dts/vscode.proposed.codeActionRanges.d.ts
+++ src/vscode-dts/vscode.proposed.codeActionRanges.d.ts
@@ -8,10 +8,8 @@ declare module 'vscode' {
 	export interface CodeAction {
 		/**
 		 *
-		 *
-		 *
 		 * The range to which this Code Action applies to, which will be highlighted.
-
+		 *
 		 * Ex: A refactoring action will highlight the range of text that will be affected.
 		 */
 		ranges?: Range[];
