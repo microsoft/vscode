2fa0caeaa637f4bd2b2e9abed29b3524b6e05091
Simon Siefke <simon.siefke@gmail.com>
2024-01-18 19:17:39 +0100

    Discard changes to src/vs/editor/contrib/colorPicker/browser/standaloneColorPickerWidget.ts

--- src/vs/editor/contrib/colorPicker/browser/standaloneColorPickerWidget.ts
+++ src/vs/editor/contrib/colorPicker/browser/standaloneColorPickerWidget.ts
@@ -26,10 +26,7 @@ import { IModelService } from 'vs/editor/common/services/model';
 import { ILanguageConfigurationService } from 'vs/editor/common/languages/languageConfigurationRegistry';
 import { DefaultDocumentColorProvider } from 'vs/editor/contrib/colorPicker/browser/defaultDocumentColorProvider';
 import * as dom from 'vs/base/browser/dom';
-import { importCss } from 'vs/base/browser/importCss';
-
-importCss('./colorPicker.css', import.meta.url)
-
+import 'vs/css!./colorPicker';
 
 export class StandaloneColorPickerController extends Disposable implements IEditorContribution {
 
