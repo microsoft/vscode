259a49a949007b989bc7eb14d78f33c53b6f7ec9
Simon Siefke <simon.siefke@gmail.com>
2024-01-18 19:20:35 +0100

    Discard changes to src/vs/editor/standalone/browser/quickInput/standaloneQuickInputService.ts

--- src/vs/editor/standalone/browser/quickInput/standaloneQuickInputService.ts
+++ src/vs/editor/standalone/browser/quickInput/standaloneQuickInputService.ts
@@ -3,6 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+import 'vs/css!./standaloneQuickInput';
 import { Event } from 'vs/base/common/event';
 import { ICodeEditor, IOverlayWidget, IOverlayWidgetPosition, OverlayWidgetPositionPreference } from 'vs/editor/browser/editorBrowser';
 import { EditorContributionInstantiation, registerEditorContribution } from 'vs/editor/browser/editorExtensions';
@@ -18,10 +19,6 @@ import { QuickInputController, IQuickInputControllerHost } from 'vs/platform/qui
 import { QuickInputService } from 'vs/platform/quickinput/browser/quickInputService';
 import { createSingleCallFunction } from 'vs/base/common/functional';
 import { IQuickAccessController } from 'vs/platform/quickinput/common/quickAccess';
-import { importCss } from 'vs/base/browser/importCss';
-
-importCss('./standaloneQuickInput.css', import.meta.url)
-
 import { IConfigurationService } from 'vs/platform/configuration/common/configuration';
 import { IHoverService } from 'vs/platform/hover/browser/hover';
 
