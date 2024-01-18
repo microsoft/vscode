892b7aee72d76c9cb8039faf33df9e18a5858e27
Simon Siefke <simon.siefke@gmail.com>
2024-01-18 19:26:45 +0100

    Discard changes to src/vs/workbench/contrib/comments/browser/commentsView.ts

--- src/vs/workbench/contrib/comments/browser/commentsView.ts
+++ src/vs/workbench/contrib/comments/browser/commentsView.ts
@@ -3,6 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+import 'vs/css!./media/panel';
 import * as nls from 'vs/nls';
 import * as dom from 'vs/base/browser/dom';
 import { basename } from 'vs/base/common/resources';
@@ -37,10 +38,6 @@ import { Iterable } from 'vs/base/common/iterator';
 import { CommentController } from 'vs/workbench/contrib/comments/browser/commentsController';
 import { Range } from 'vs/editor/common/core/range';
 import { registerNavigableContainer } from 'vs/workbench/browser/actions/widgetNavigationCommands';
-import { importCss } from 'vs/base/browser/importCss';
-
-importCss('./media/panel.css', import.meta.url)
-
 import { CommentsModel, ICommentsModel } from 'vs/workbench/contrib/comments/browser/commentsModel';
 
 export const CONTEXT_KEY_HAS_COMMENTS = new RawContextKey<boolean>('commentsView.hasComments', false);
