97141c5cb239b1684a929819ea5c78d28dfd1431
Simon Siefke <simon.siefke@gmail.com>
2024-01-18 19:09:55 +0100

    Discard changes to src/vs/code/electron-sandbox/issue/issueReporterMain.ts

--- src/vs/code/electron-sandbox/issue/issueReporterMain.ts
+++ src/vs/code/electron-sandbox/issue/issueReporterMain.ts
@@ -4,8 +4,10 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { safeInnerHtml } from 'vs/base/browser/dom';
+import 'vs/base/browser/ui/codicons/codiconStyles'; // make sure codicon css is loaded
 import { isLinux, isWindows } from 'vs/base/common/platform';
 import BaseHtml from 'vs/code/electron-sandbox/issue/issueReporterPage';
+import 'vs/css!./media/issueReporter';
 import { SyncDescriptor } from 'vs/platform/instantiation/common/descriptors';
 import { getSingletonServiceDescriptors } from 'vs/platform/instantiation/common/extensions';
 import { InstantiationService } from 'vs/platform/instantiation/common/instantiationService';
@@ -18,10 +20,6 @@ import { INativeHostService } from 'vs/platform/native/common/native';
 import { NativeHostService } from 'vs/platform/native/common/nativeHostService';
 import { IssueReporter } from './issueReporterService';
 import { mainWindow } from 'vs/base/browser/window';
-import { importCss } from 'vs/base/browser/importCss';
-
-importCss('./media/issueReporter.css', import.meta.url)
-importCss('vs/base/browser/ui/codicons/codiconStyles.css', import.meta.url) // make sure codicon css is loaded
 
 export function startup(configuration: IssueReporterWindowConfiguration) {
 	const platformClass = isWindows ? 'windows' : isLinux ? 'linux' : 'mac';
