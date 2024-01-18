2bf9714ac4748132aaa0de71c24cb1334bc67713
Simon Siefke <simon.siefke@gmail.com>
2024-01-18 19:21:33 +0100

    Discard changes to src/vs/platform/opener/browser/link.ts

--- src/vs/platform/opener/browser/link.ts
+++ src/vs/platform/opener/browser/link.ts
@@ -11,10 +11,7 @@ import { Event } from 'vs/base/common/event';
 import { KeyCode } from 'vs/base/common/keyCodes';
 import { Disposable } from 'vs/base/common/lifecycle';
 import { IOpenerService } from 'vs/platform/opener/common/opener';
-import { importCss } from 'vs/base/browser/importCss';
-
-importCss('./link.css', import.meta.url)
-
+import 'vs/css!./link';
 
 export interface ILinkDescriptor {
 	readonly label: string | HTMLElement;
