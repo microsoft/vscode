1a0756993027b001555374300b1aacfaa7aad7ef
Simon Siefke <simon.siefke@gmail.com>
2024-01-18 19:25:11 +0100

    Discard changes to src/vs/workbench/browser/parts/titlebar/titlebarPart.ts

--- src/vs/workbench/browser/parts/titlebar/titlebarPart.ts
+++ src/vs/workbench/browser/parts/titlebar/titlebarPart.ts
@@ -3,6 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+import 'vs/css!./media/titlebarpart';
 import { localize, localize2 } from 'vs/nls';
 import { MultiWindowParts, Part } from 'vs/workbench/browser/part';
 import { ITitleService } from 'vs/workbench/services/title/browser/titleService';
@@ -53,10 +54,6 @@ import { IEditorCommandsContext, IEditorPartOptionsChangeEvent, IToolbarActions 
 import { mainWindow } from 'vs/base/browser/window';
 import { ACCOUNTS_ACTIVITY_TILE_ACTION, GLOBAL_ACTIVITY_TITLE_ACTION } from 'vs/workbench/browser/parts/titlebar/titlebarActions';
 import { IView } from 'vs/base/browser/ui/grid/grid';
-import { importCss } from 'vs/base/browser/importCss';
-
-importCss('./media/titlebarpart.css', import.meta.url)
-
 
 export interface ITitleProperties {
 	isPure?: boolean;
