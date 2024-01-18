f3910191a96468f0ce304a68f81f53b78c2c289d
Johannes <johannes.rieken@gmail.com>
2024-01-18 16:55:25 +0100

    don't count changes but make sure error'd edits don't make it into buffer
    fixes https://github.com/microsoft/vscode/issues/202702

--- src/vs/workbench/contrib/inlineChat/test/browser/inlineChatController.test.ts
+++ src/vs/workbench/contrib/inlineChat/test/browser/inlineChatController.test.ts
@@ -382,6 +382,8 @@ suite('InteractiveChatController', function () {
 		assert.strictEqual(editor.getModel().getValue(), valueThen);
 	});
 
+
+
 	test('UI is streaming edits minutes after the response is finished #3345', async function () {
 
 		configurationService.setUserConfiguration(InlineChatConfigKeys.Mode, EditMode.Live);
@@ -411,8 +413,8 @@ suite('InteractiveChatController', function () {
 			});
 
 
-			let modelChangeCounter = 0;
-			store.add(editor.getModel().onDidChangeContent(() => { modelChangeCounter++; }));
+			// let modelChangeCounter = 0;
+			// store.add(editor.getModel().onDidChangeContent(() => { modelChangeCounter++; }));
 
 			store.add(d);
 			ctrl = instaService.createInstance(TestController, editor);
@@ -420,17 +422,18 @@ suite('InteractiveChatController', function () {
 			const r = ctrl.run({ message: 'Hello', autoSend: true });
 			await p;
 
-			assert.ok(modelChangeCounter > 0); // some changes have been made
+			// assert.ok(modelChangeCounter > 0, modelChangeCounter.toString()); // some changes have been made
+			// const modelChangeCounterNow = modelChangeCounter;
 
-			const modelChangeCounterNow = modelChangeCounter;
-
+			assert.ok(!editor.getModel().getValue().includes('DONE'));
 			await timeout(10);
 
-			assert.strictEqual(modelChangeCounterNow, modelChangeCounter);
+			// assert.strictEqual(modelChangeCounterNow, modelChangeCounter);
 			assert.ok(!editor.getModel().getValue().includes('DONE'));
 
 			await ctrl.cancelSession();
 			await r;
 		});
 	});
+
 });
