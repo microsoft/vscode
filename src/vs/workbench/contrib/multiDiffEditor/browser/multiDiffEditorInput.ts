f73a212381a509029ef73c1d3c0959e47b6f4d2a
Henning Dieterichs <hdieterichs@microsoft.com>
2024-01-22 18:29:38 +0100

    Improves rendering of deleted and renamed files in multi diff editor.

--- src/vs/workbench/contrib/multiDiffEditor/browser/multiDiffEditorInput.ts
+++ src/vs/workbench/contrib/multiDiffEditor/browser/multiDiffEditorInput.ts
@@ -142,9 +142,8 @@ export class MultiDiffEditorInput extends EditorInput implements ILanguageSuppor
 
 		const documentsWithPromises = mapObservableArrayCached(undefined, source.resources, async (r, store) => {
 			/** @description documentsWithPromises */
-			let originalTextModel: ITextModel;
-			let modifiedTextModel: ITextModel;
-			let modifiedRef: IReference<IResolvedTextEditorModel> | undefined;
+			let original: IReference<IResolvedTextEditorModel> | undefined;
+			let modified: IReference<IResolvedTextEditorModel> | undefined;
 			const store2 = new DisposableStore();
 			store.add(toDisposable(() => {
 				// Mark the text model references as garbage when they get stale (don't dispose them yet)
@@ -152,14 +151,12 @@ export class MultiDiffEditorInput extends EditorInput implements ILanguageSuppor
 			}));
 
 			try {
-				[originalTextModel, modifiedTextModel] = await Promise.all([
-					r.original
-						? store2.add(await this._textModelService.createModelReference(r.original)).object.textEditorModel
-						: store2.add(this._modelService.createModel('', null)),
-					r.modified
-						? store2.add(modifiedRef = await this._textModelService.createModelReference(r.modified)).object.textEditorModel
-						: store2.add(this._modelService.createModel('', null)),
+				[original, modified] = await Promise.all([
+					r.original ? this._textModelService.createModelReference(r.original) : undefined,
+					r.modified ? this._textModelService.createModelReference(r.modified) : undefined,
 				]);
+				if (original) { store.add(original); }
+				if (modified) { store.add(modified); }
 			} catch (e) {
 				// e.g. "File seems to be binary and cannot be opened as text"
 				console.error(e);
@@ -169,11 +166,11 @@ export class MultiDiffEditorInput extends EditorInput implements ILanguageSuppor
 
 			const uri = (r.modified ?? r.original)!;
 			return new ConstLazyPromise<IDocumentDiffItem>({
-				original: originalTextModel,
-				modified: modifiedTextModel,
+				original: original?.object.textEditorModel,
+				modified: modified?.object.textEditorModel,
 				get options() {
 					return {
-						...getReadonlyConfiguration(modifiedRef?.object.isReadonly() ?? true),
+						...getReadonlyConfiguration(modified?.object.isReadonly() ?? true),
 						...computeOptions(textResourceConfigurationService.getValue(uri)),
 					} satisfies IDiffEditorOptions;
 				},
