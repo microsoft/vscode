8baf105cbd35e538d8133a857d4b4fede89f902c
Robo <hop2deep@gmail.com>
2024-01-22 17:51:44 +0900

    fix: skip glibc requirements check on nixos (#202982)

--- resources/server/bin/helpers/check-requirements-linux.sh
+++ resources/server/bin/helpers/check-requirements-linux.sh
@@ -10,6 +10,13 @@ ARCH=$(uname -m)
 found_required_glibc=0
 found_required_glibcxx=0
 
+# Extract the ID value from /etc/os-release
+OS_ID="$(cat /etc/os-release | grep -Eo 'ID=([^"]+)' | sed 's/ID=//')"
+if [ "$OS_ID" = "nixos" ]; then
+  echo "Warning: NixOS detected, skipping GLIBC check"
+  exit 0
+fi
+
 # Based on https://github.com/bminor/glibc/blob/520b1df08de68a3de328b65a25b86300a7ddf512/elf/cache.c#L162-L245
 case $ARCH in
 	x86_64) LDCONFIG_ARCH="x86-64";;
