afd4f631aa6703773767567d8d291e431ae77bc4
Johannes Rieken <johannes.rieken@gmail.com>
2024-01-24 10:40:58 +0100

    inline chat fixes (#203297)
    * Pause state should be more relaxed and expect partial state
    
    fixes https://github.com/microsoft/vscode/issues/203173
    
    * clear stashed session before starting a new session
    
    fixes https://github.com/microsoft/vscode-copilot/issues/3719

--- src/vs/workbench/contrib/inlineChat/browser/inlineChatController.ts
+++ src/vs/workbench/contrib/inlineChat/browser/inlineChatController.ts
@@ -247,6 +247,7 @@ export class InlineChatController implements IEditorContribution {
 			}
 			this._historyOffset = -1;
 			this._historyCandidate = '';
+			this._stashedSession.clear();
 			this._onWillStartSession.fire();
 			this._currentRun = this._nextState(State.CREATE_SESSION, options);
 			await this._currentRun;
@@ -815,12 +816,10 @@ export class InlineChatController implements IEditorContribution {
 	}
 
 	private async[State.PAUSE]() {
-		assertType(this._session);
-		assertType(this._strategy);
 
 		this._resetWidget();
 
-		this._strategy.pause?.();
+		this._strategy?.pause?.();
 		this._session = undefined;
 	}
 
