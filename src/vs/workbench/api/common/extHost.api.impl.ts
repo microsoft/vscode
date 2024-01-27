dc02245dca07cf579270008916ffcf9a66a9257f
Benjamin Pasero <benjamin.pasero@microsoft.com>
2024-01-27 10:04:45 +0100

    ext host - log error when attempting to open/show a document of invalid remote URI (#199844)

--- src/vs/workbench/api/common/extHost.api.impl.ts
+++ src/vs/workbench/api/common/extHost.api.impl.ts
@@ -675,6 +675,9 @@ export function createApiFactoryAndRegisterActors(accessor: ServicesAccessor): I
 				return extHostTerminalService.terminals;
 			},
 			async showTextDocument(documentOrUri: vscode.TextDocument | vscode.Uri, columnOrOptions?: vscode.ViewColumn | vscode.TextDocumentShowOptions, preserveFocus?: boolean): Promise<vscode.TextEditor> {
+				if (URI.isUri(documentOrUri) && documentOrUri.scheme === Schemas.vscodeRemote && !documentOrUri.authority) {
+					extHostLogService.error(`Extension '${extension.identifier.value}' called 'showTextDocument()' with an invalid 'vscode-remote' URI that has no authority.`);
+				}
 				const document = await (URI.isUri(documentOrUri)
 					? Promise.resolve(workspace.openTextDocument(documentOrUri))
 					: Promise.resolve(<vscode.TextDocument>documentOrUri));
@@ -1004,6 +1007,9 @@ export function createApiFactoryAndRegisterActors(accessor: ServicesAccessor): I
 				}
 
 				return uriPromise.then(uri => {
+					if (uri.scheme === Schemas.vscodeRemote && !uri.authority) {
+						extHostLogService.error(`Extension '${extension.identifier.value}' called 'openTextDocument()' with an invalid 'vscode-remote' URI that has no authority.`);
+					}
 					return extHostDocuments.ensureDocumentData(uri).then(documentData => {
 						return documentData.document;
 					});
