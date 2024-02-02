e76bbf6236024b6589498e4c9f08bf0c9cf0a1a2
Aiday Marlen Kyzy <amarlenkyzy@microsoft.com>
2024-02-02 11:39:33 +0100

    adding enw lines in some places

--- src/vs/editor/contrib/inlineCompletions/browser/inlineCompletionsModel.ts
+++ src/vs/editor/contrib/inlineCompletions/browser/inlineCompletionsModel.ts
@@ -307,7 +307,10 @@ export class InlineCompletionsModel extends Disposable {
 			SnippetController2.get(editor)?.insert(completion.snippetInfo.snippet, { undoStopBefore: false });
 		} else {
 			const edits = this._getEdits(editor, completion.toSingleTextEdit());
-			editor.executeEdits('inlineSuggestion.accept', [...edits.edits.map(edit => EditOperation.replaceMove(edit.range, edit.text)), ...completion.additionalTextEdits]);
+			editor.executeEdits('inlineSuggestion.accept', [
+				...edits.edits.map(edit => EditOperation.replaceMove(edit.range, edit.text)),
+				...completion.additionalTextEdits
+			]);
 			editor.setSelections(edits.editorSelections, 'inlineCompletionAccept');
 		}
 
@@ -408,7 +411,9 @@ export class InlineCompletionsModel extends Disposable {
 			try {
 				editor.pushUndoStop();
 				const replaceRange = Range.fromPositions(completion.range.getStartPosition(), position);
-				const newText = completion.insertText.substring(0, firstPart.column - completion.range.startColumn + acceptUntilIndexExclusive);
+				const newText = completion.insertText.substring(
+					0,
+					firstPart.column - completion.range.startColumn + acceptUntilIndexExclusive);
 				const singleTextEdit = new SingleTextEdit(replaceRange, newText);
 				const edits = this._getEdits(editor, singleTextEdit);
 				editor.executeEdits('inlineSuggestion.accept', edits.edits.map(edit => EditOperation.replaceMove(edit.range, edit.text)));
