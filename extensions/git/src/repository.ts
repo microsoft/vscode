628662c7895c9ee6895a1d4ea53523cb4bcbaae2
Ladislau Szomoru <3372902+lszomoru@users.noreply.github.com>
2024-02-07 12:27:12 +0100

    Git - adjust branch base computation

--- extensions/git/src/repository.ts
+++ extensions/git/src/repository.ts
@@ -1484,11 +1484,6 @@ export class Repository implements Disposable {
 
 	async getBranchBase(ref: string): Promise<Branch | undefined> {
 		const branch = await this.getBranch(ref);
-		const branchUpstream = await this.getUpstreamBranch(branch);
-
-		if (branchUpstream) {
-			return branchUpstream;
-		}
 
 		// Git config
 		const mergeBaseConfigKey = `branch.${branch.name}.vscode-merge-base`;
