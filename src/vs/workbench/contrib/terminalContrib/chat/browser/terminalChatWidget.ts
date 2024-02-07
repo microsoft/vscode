bd49e9543a9a1650d7bba37dfea09eb9c70dcdcd
meganrogge <megan.rogge@microsoft.com>
2024-02-07 14:41:40 -0600

    rm some stuff

--- src/vs/workbench/contrib/terminalContrib/chat/browser/terminalChatWidget.ts
+++ src/vs/workbench/contrib/terminalContrib/chat/browser/terminalChatWidget.ts
@@ -15,30 +15,22 @@ import { TerminalContextKeys } from 'vs/workbench/contrib/terminal/common/termin
 export class TerminalChatWidget extends Widget {
 	private readonly _focusTracker: dom.IFocusTracker;
 	private readonly _domNode: HTMLElement;
-	// private readonly _innerDomNode: HTMLElement;
 	private _isVisible: boolean = false;
 	private _width: number = 0;
 	private _chatWidgetFocused: IContextKey<boolean>;
 	private _chatWidgetVisible: IContextKey<boolean>;
 
 	constructor(
-		private _instance: ITerminalInstance | IDetachedTerminalInstance,
+		instance: ITerminalInstance | IDetachedTerminalInstance,
 		@IContextViewService _contextViewService: IContextViewService,
 		@IKeybindingService keybindingService: IKeybindingService,
 		@IContextKeyService private readonly _contextKeyService: IContextKeyService,
 		@IContextMenuService _contextMenuService: IContextMenuService,
-		// @IThemeService private readonly _themeService: IThemeService,
-		// @IConfigurationService private readonly _configurationService: IConfigurationService
 	) {
 		super();
 
-		// this._innerDomNode = document.createElement('div');
-		// this._innerDomNode.classList.add('terminal-chat-widget');
-		// this._innerDomNode.textContent = 'Chat Widget';
-
 		this._domNode = document.createElement('div');
-		this._domNode.classList.add('terminal-chat-widget-wrapper');
-		// this._domNode.appendChild(this._innerDomNode);
+		this._domNode.classList.add('terminal-chat-widget');
 
 		this.onkeyup(this._domNode, e => {
 			if (e.equals(KeyCode.Escape)) {
@@ -111,7 +103,6 @@ export class TerminalChatWidget extends Widget {
 	}
 
 	protected _onFocusTrackerBlur() {
-		this._instance.xterm?.clearActiveSearchDecoration();
 		this._chatWidgetFocused.reset();
 	}
 }
