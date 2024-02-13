b7f707963d7416ddf7a870c76f5427478df37be6
Anthony Kim <anthonykim@microsoft.com>
2024-02-13 13:22:30 -0800

    remove comment

--- src/vs/workbench/contrib/terminal/browser/terminalInstanceService.ts
+++ src/vs/workbench/contrib/terminal/browser/terminalInstanceService.ts
@@ -56,7 +56,7 @@ export class TerminalInstanceService extends Disposable implements ITerminalInst
 		);
 		if (instance.shellLaunchConfig.name === 'Python') {
 			// If title is Python, manually set the shell type to Python.
-			instance.setShellType(PosixShellType.Python); // Why Doesnt This Have Any Effect??????????????????????
+			instance.setShellType(PosixShellType.Python);
 			// instance.shellType = PosixShellType.Python;
 		}
 		instance.target = target;
