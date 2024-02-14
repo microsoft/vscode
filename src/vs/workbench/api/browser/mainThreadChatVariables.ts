facfcbc502fc89a2932a87fa5b1c5268c62d14ad
Rob Lourens <roblourens@gmail.com>
2024-02-14 17:04:58 -0300

    Fix

--- src/vs/workbench/api/browser/mainThreadChatVariables.ts
+++ src/vs/workbench/api/browser/mainThreadChatVariables.ts
@@ -30,7 +30,7 @@ export class MainThreadChatVariables implements MainThreadChatVariablesShape {
 	$registerVariable(handle: number, data: IChatVariableData): void {
 		const registration = this._chatVariablesService.registerVariable(data, async (messageText, _arg, model, progress, token) => {
 			const varRequestId = `${model.sessionId}-${handle}`;
-			this._pendingProgress.set(model.sessionId, progress);
+			this._pendingProgress.set(varRequestId, progress);
 			const result = revive<IChatRequestVariableValue[]>(await this._proxy.$resolveVariable(handle, varRequestId, messageText, token));
 
 			this._pendingProgress.delete(varRequestId);
