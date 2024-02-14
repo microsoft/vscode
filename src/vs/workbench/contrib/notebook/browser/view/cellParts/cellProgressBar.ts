be9348f3c4bcc0c1857b3efde312a1f8a4925549
allen-li1231 <supermrli@hotmail.com>
2024-02-14 20:17:14 +0800

    apply cell progress bar settings from emitted ExecutionStateChangedEvent

--- src/vs/workbench/contrib/notebook/browser/view/cellParts/cellProgressBar.ts
+++ src/vs/workbench/contrib/notebook/browser/view/cellParts/cellProgressBar.ts
@@ -8,7 +8,7 @@ import { defaultProgressBarStyles } from 'vs/platform/theme/browser/defaultStyle
 import { ICellViewModel } from 'vs/workbench/contrib/notebook/browser/notebookBrowser';
 import { CellViewModelStateChangeEvent } from 'vs/workbench/contrib/notebook/browser/notebookViewEvents';
 import { CellContentPart } from 'vs/workbench/contrib/notebook/browser/view/cellPart';
-import { NotebookCellExecutionState } from 'vs/workbench/contrib/notebook/common/notebookCommon';
+import { NotebookCellExecutionState, NotebookCellExecutionProgress } from 'vs/workbench/contrib/notebook/common/notebookCommon';
 import { ICellExecutionStateChangedEvent, INotebookExecutionStateService } from 'vs/workbench/contrib/notebook/common/notebookExecutionStateService';
 
 export class CellProgressBar extends CellContentPart {
@@ -59,6 +59,8 @@ export class CellProgressBar extends CellContentPart {
 
 	private _updateForExecutionState(element: ICellViewModel, e?: ICellExecutionStateChangedEvent): void {
 		const exeState = e?.changed ?? this._notebookExecutionStateService.getCellExecution(element.uri);
+		setProgressBar(this._collapsedProgressBar, exeState?.progress);
+		setProgressBar(this._progressBar, exeState?.progress);
 		const progressBar = element.isInputCollapsed ? this._collapsedProgressBar : this._progressBar;
 		if (exeState?.state === NotebookCellExecutionState.Executing && (!exeState.didPause || element.isInputCollapsed)) {
 			showProgressBar(progressBar);
@@ -69,5 +71,27 @@ export class CellProgressBar extends CellContentPart {
 }
 
 function showProgressBar(progressBar: ProgressBar): void {
-	progressBar.infinite().show(500);
+	if (progressBar.hasTotal()) {
+		progressBar.show(500);
+	}
+	else {
+		progressBar.infinite().show(500);
+	}
+}
+
+function setProgressBar(progressBar: ProgressBar, progress: NotebookCellExecutionProgress | undefined): void {
+	if (progress?.total) {
+		progressBar.total(progress.total);
+	}
+
+	if (!progressBar.hasTotal() && (progress?.increment !== undefined || progress?.progress !== undefined)) {
+		progressBar.total(100);
+	}
+
+	if (progress?.progress) {
+		progressBar.setWorked(progress?.progress);
+	}
+	if (progress?.increment) {
+		progressBar.worked(progress?.increment);
+	}
 }
