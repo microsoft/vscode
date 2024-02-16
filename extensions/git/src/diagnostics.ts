f0c78417f7d96ceec0e27a198e7d01e62b8d6783
Ladislau Szomoru <3372902+lszomoru@users.noreply.github.com>
2024-02-16 10:35:01 +0100

    Git - add "Hard wrap all lines" code action is there are multiple long lines

--- extensions/git/src/diagnostics.ts
+++ extensions/git/src/diagnostics.ts
@@ -83,6 +83,15 @@ export class GitCommitInputBoxCodeActionsProvider implements CodeActionProvider 
 		const codeActions: CodeAction[] = [];
 		const diagnostics = this.diagnosticsManager.getDiagnostics(document.uri);
 
+		// Hard wrap all lines code action
+		let hardWrapAllLinesCodeAction: CodeAction | undefined;
+		const lineLengthDiagnostics = diagnostics.filter(d => d.code === DiagnosticCodes.line_length);
+
+		if (lineLengthDiagnostics.length > 1) {
+			hardWrapAllLinesCodeAction = new CodeAction(l10n.t('Hard wrap all lines'), CodeActionKind.QuickFix);
+			hardWrapAllLinesCodeAction.edit = this.getWrapAllLinesWorkspaceEdit(document, lineLengthDiagnostics);
+		}
+
 		for (const diagnostic of diagnostics) {
 			if (!diagnostic.range.contains(range)) {
 				continue;
@@ -96,17 +105,23 @@ export class GitCommitInputBoxCodeActionsProvider implements CodeActionProvider 
 					const codeAction = new CodeAction(l10n.t('Remove empty characters'), CodeActionKind.QuickFix);
 					codeAction.diagnostics = [diagnostic];
 					codeAction.edit = workspaceEdit;
-
 					codeActions.push(codeAction);
+
 					break;
 				}
 				case DiagnosticCodes.line_length: {
 					const workspaceEdit = this.getWrapLineWorkspaceEdit(document, diagnostic.range);
+
 					const codeAction = new CodeAction(l10n.t('Hard wrap line'), CodeActionKind.QuickFix);
 					codeAction.diagnostics = [diagnostic];
 					codeAction.edit = workspaceEdit;
-
 					codeActions.push(codeAction);
+
+					if (hardWrapAllLinesCodeAction) {
+						hardWrapAllLinesCodeAction.diagnostics = [diagnostic];
+						codeActions.push(hardWrapAllLinesCodeAction);
+					}
+
 					break;
 				}
 			}
@@ -116,13 +131,33 @@ export class GitCommitInputBoxCodeActionsProvider implements CodeActionProvider 
 	}
 
 	private getWrapLineWorkspaceEdit(document: TextDocument, range: Range): WorkspaceEdit {
+		const lineSegments = this.wrapTextDocumentLine(document, range.start.line);
+
+		const workspaceEdit = new WorkspaceEdit();
+		workspaceEdit.replace(document.uri, range, lineSegments.join('\n'));
+
+		return workspaceEdit;
+	}
+
+	private getWrapAllLinesWorkspaceEdit(document: TextDocument, diagnostics: Diagnostic[]): WorkspaceEdit {
+		const workspaceEdit = new WorkspaceEdit();
+
+		for (const diagnostic of diagnostics) {
+			const lineSegments = this.wrapTextDocumentLine(document, diagnostic.range.start.line);
+			workspaceEdit.replace(document.uri, diagnostic.range, lineSegments.join('\n'));
+		}
+
+		return workspaceEdit;
+	}
+
+	private wrapTextDocumentLine(document: TextDocument, line: number): string[] {
 		const config = workspace.getConfiguration('git');
 		const inputValidationLength = config.get<number>('inputValidationLength', 50);
 		const inputValidationSubjectLength = config.get<number | undefined>('inputValidationSubjectLength', undefined);
-		const lineLengthThreshold = range.start.line === 0 ? inputValidationSubjectLength ?? inputValidationLength : inputValidationLength;
+		const lineLengthThreshold = line === 0 ? inputValidationSubjectLength ?? inputValidationLength : inputValidationLength;
 
 		const lineSegments: string[] = [];
-		const lineText = document.lineAt(range.start.line).text;
+		const lineText = document.lineAt(line).text;
 
 		let position = 0;
 		while (lineText.length - position > lineLengthThreshold) {
@@ -147,10 +182,7 @@ export class GitCommitInputBoxCodeActionsProvider implements CodeActionProvider 
 			lineSegments.push(lineText.substring(position));
 		}
 
-		const workspaceEdit = new WorkspaceEdit();
-		workspaceEdit.replace(document.uri, range, lineSegments.join('\n'));
-
-		return workspaceEdit;
+		return lineSegments;
 	}
 
 	dispose() {
