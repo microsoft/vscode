757b6b2556c08f0cc0774aa10a4d9d32606fd810
Orgad Shaneh <orgad.shaneh@audiocodes.com>
2024-02-18 12:40:24 +0200

    Tunnel: Add unit tests for port mapping

---
+++ src/vs/platform/tunnel/test/common/tunnel.test.ts
@@ -0,0 +1,37 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+import * as assert from 'assert';
+import { URI } from 'vs/base/common/uri';
+import { extractLocalHostUriMetaDataForPortMapping } from 'vs/platform/tunnel/common/tunnel';
+import { ensureNoDisposablesAreLeakedInTestSuite } from 'vs/base/test/common/utils';
+
+
+suite('Tunnel', () => {
+	ensureNoDisposablesAreLeakedInTestSuite();
+
+	function portMappingDoTest(uri: string,
+		expectedAddress?: string,
+		expectedPort?: number) {
+		const res = extractLocalHostUriMetaDataForPortMapping(URI.parse(uri));
+		assert.strictEqual(!expectedAddress, !res);
+		assert.strictEqual(res?.address, expectedAddress);
+		assert.strictEqual(res?.port, expectedPort);
+	}
+
+	function portMappingTest(uri: string, expectedAddress?: string, expectedPort?: number) {
+		portMappingDoTest(uri, expectedAddress, expectedPort);
+	}
+
+	test('portMapping', () => {
+		portMappingTest('file:///foo.bar/baz');
+		portMappingTest('http://foo.bar:1234');
+		portMappingTest('http://localhost:8080', 'localhost', 8080);
+		portMappingTest('https://localhost:443', 'localhost', 443);
+		portMappingTest('http://127.0.0.1:3456', '127.0.0.1', 3456);
+		portMappingTest('http://0.0.0.0:7654', '0.0.0.0', 7654);
+		portMappingTest('http://localhost:8080/path?foo=bar', 'localhost', 8080);
+		portMappingTest('http://localhost:8080/path?foo=http%3A%2F%2Flocalhost%3A8081', 'localhost', 8080);
+	});
+});
