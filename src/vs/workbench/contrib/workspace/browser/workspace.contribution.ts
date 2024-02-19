677ac4be083e44beafcc0ac597299f38aafc1c33
Benjamin Pasero <benjamin.pasero@microsoft.com>
2024-02-19 10:35:18 +0100

    Other files don't open when launched with an unsaved workspace (fix #205453)

--- src/vs/workbench/contrib/workspace/browser/workspace.contribution.ts
+++ src/vs/workbench/contrib/workspace/browser/workspace.contribution.ts
@@ -104,10 +104,11 @@ export class WorkspaceTrustRequestHandler extends Disposable implements IWorkben
 	}
 
 	private async registerListeners(): Promise<void> {
-		await this.workspaceTrustManagementService.workspaceResolved;
 
 		// Open files trust request
 		this._register(this.workspaceTrustRequestService.onDidInitiateOpenFilesTrustRequest(async () => {
+			await this.workspaceTrustManagementService.workspaceResolved;
+
 			// Details
 			const markdownDetails = [
 				this.workspaceContextService.getWorkbenchState() !== WorkbenchState.EMPTY ?
@@ -148,6 +149,8 @@ export class WorkspaceTrustRequestHandler extends Disposable implements IWorkben
 
 		// Workspace trust request
 		this._register(this.workspaceTrustRequestService.onDidInitiateWorkspaceTrustRequest(async requestOptions => {
+			await this.workspaceTrustManagementService.workspaceResolved;
+
 			// Title
 			const message = this.useWorkspaceLanguage ?
 				localize('workspaceTrust', "Do you trust the authors of the files in this workspace?") :
