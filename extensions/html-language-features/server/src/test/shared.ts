021113fba18ce52187fcf9ac7140d38a74ff821a
Johnson Chu <johnsoncodehk@gmail.com>
2024-02-27 09:55:16 +0800

    Add missing copyright

--- extensions/html-language-features/server/src/test/shared.ts
+++ extensions/html-language-features/server/src/test/shared.ts
@@ -1,3 +1,8 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
 import { ClientCapabilities, DiagnosticModel } from '@volar/language-server';
 import { startLanguageServer } from '@volar/test-utils';
 
