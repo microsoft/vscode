8fd15a863481726f1ea4bdd4bd4f26f40f3e592d
Johannes Rieken <johannes.rieken@gmail.com>
2024-02-27 18:50:12 +0100

    add `LanguageModelError`-type (#206361)
    https://github.com/microsoft/vscode/issues/206265

--- src/vscode-dts/vscode.proposed.languageModels.d.ts
+++ src/vscode-dts/vscode.proposed.languageModels.d.ts
@@ -105,6 +105,36 @@ declare module 'vscode' {
 	}
 
 	/**
+	 * An error type for language model specific errors.
+	 *
+	 * Consumers of language models should check the code property to determine specific
+	 * failure causes, like `if(someError.code === vscode.LanguageModelError.NotFound.name) {...}`
+	 * for the case of referring to an unknown language model.
+	 */
+	export class LanguageModelError extends Error {
+
+		/**
+		 * The language model does not exist.
+		 */
+		static NotFound(message?: string): LanguageModelError;
+
+		/**
+		 * The requestor does not have permissions to use this
+		 * language model
+		 */
+		static NoPermissions(message?: string): LanguageModelError;
+
+		/**
+		 * A code that identifies this error.
+		 *
+		 * Possible values are names of errors, like {@linkcode LanguageModelError.NotFound NotFound},
+		 * or `Unknown` for unspecified errors from the language model itself. In the latter case the
+		 * `cause`-property will contain the actual error.
+		 */
+		readonly code: string;
+	}
+
+	/**
 	 * Options for making a chat request using a language model.
 	 *
 	 * @see {@link lm.chatRequest}
@@ -151,9 +181,9 @@ declare module 'vscode' {
 		 * @returns A thenable that resolves to a {@link LanguageModelChatResponse}. The promise will reject when making the request failed.
 		 */
 		// TODO@API refine doc
-		// TODO@API define specific error types?
-		// TODO@API NAME: sendChatRequest, fetchChatResponse, makeChatRequest, chat, chatRequest sendChatRequest
 		// TODO@API ExtensionContext#permission#languageModels: { languageModel: string: LanguageModelAccessInformation}
+		// TODO@API ✅ define specific error types?
+		// TODO@API ✅ NAME: sendChatRequest, fetchChatResponse, makeChatRequest, chat, chatRequest sendChatRequest
 		// TODO@API ✅ NAME: LanguageModelChatXYZMessage
 		// TODO@API ✅ errors on everything that prevents us to make the actual request
 		// TODO@API ✅ double auth
