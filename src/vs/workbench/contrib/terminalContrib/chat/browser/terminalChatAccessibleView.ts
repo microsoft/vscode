8f062388605e48a3b8d5cbe5eb30c7cc7f2e8558
meganrogge <megan.rogge@microsoft.com>
2024-03-04 13:33:43 -0800

    fix context key issue for accessible view

--- src/vs/workbench/contrib/terminalContrib/chat/browser/terminalChatAccessibleView.ts
+++ src/vs/workbench/contrib/terminalContrib/chat/browser/terminalChatAccessibleView.ts
@@ -4,11 +4,9 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { Disposable } from 'vs/base/common/lifecycle';
-import { ContextKeyExpr } from 'vs/platform/contextkey/common/contextkey';
 import { AccessibilityVerbositySettingId, AccessibleViewProviderId } from 'vs/workbench/contrib/accessibility/browser/accessibilityConfiguration';
 import { AccessibleViewType, IAccessibleViewService } from 'vs/workbench/contrib/accessibility/browser/accessibleView';
 import { AccessibleViewAction } from 'vs/workbench/contrib/accessibility/browser/accessibleViewActions';
-import { CTX_INLINE_CHAT_RESPONSE_FOCUSED } from 'vs/workbench/contrib/inlineChat/common/inlineChat';
 import { ITerminalService } from 'vs/workbench/contrib/terminal/browser/terminal';
 import { TerminalChatContextKeys } from 'vs/workbench/contrib/terminalContrib/chat/browser/terminalChat';
 import { TerminalChatController } from 'vs/workbench/contrib/terminalContrib/chat/browser/terminalChatController';
@@ -35,6 +33,6 @@ export class TerminalInlineChatAccessibleViewContribution extends Disposable {
 				options: { type: AccessibleViewType.View }
 			});
 			return true;
-		}, ContextKeyExpr.and(TerminalChatContextKeys.focused, CTX_INLINE_CHAT_RESPONSE_FOCUSED)));
+		}, TerminalChatContextKeys.focused));
 	}
 }
