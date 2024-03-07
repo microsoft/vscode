0d1ee3d434fcf56c0338291d4e4355324d5cae3e
Benjamin Pasero <benjamin.pasero@microsoft.com>
2024-03-07 06:36:29 +0100

    debt - fix more memory leaks

--- src/vs/workbench/browser/parts/editor/editorStatus.ts
+++ src/vs/workbench/browser/parts/editor/editorStatus.ts
@@ -328,7 +328,7 @@ class EditorStatus extends Disposable {
 	private readonly metadataElement = this._register(new MutableDisposable<IStatusbarEntryAccessor>());
 
 	private readonly currentMarkerStatus = this._register(this.instantiationService.createInstance(ShowCurrentMarkerInStatusbarContribution));
-	private readonly tabFocusMode = this.instantiationService.createInstance(TabFocusMode);
+	private readonly tabFocusMode = this._register(this.instantiationService.createInstance(TabFocusMode));
 
 	private readonly state = new State();
 	private toRender: StateChange | undefined = undefined;
