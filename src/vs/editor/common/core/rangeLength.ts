a278ef641dae4291f6d403eaf55acdc973fcf168
Henning Dieterichs <hdieterichs@microsoft.com>
2024-03-07 16:44:35 +0100

    Moves utilities into RangeLength

--- src/vs/editor/common/core/rangeLength.ts
+++ src/vs/editor/common/core/rangeLength.ts
@@ -2,6 +2,8 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
+import { Position } from 'vs/editor/common/core/position';
+import { Range } from 'vs/editor/common/core/range';
 
 /**
  * Represents a non-negative length of text in terms of line and column count.
@@ -18,6 +20,32 @@ export class RangeLength {
 		} else {
 			return new RangeLength(end.lineCount - start.lineCount, end.columnCount);
 		}
+	}
+
+	public static betweenPositions(position1: Position, position2: Position): RangeLength {
+		if (position1.lineNumber === position2.lineNumber) {
+			return new RangeLength(0, position2.column - position1.column);
+		} else {
+			return new RangeLength(position2.lineNumber - position1.lineNumber, position2.column - 1);
+		}
+	}
+
+	public static ofRange(range: Range) {
+		return RangeLength.betweenPositions(range.getStartPosition(), range.getEndPosition());
+	}
+
+	public static ofText(text: string): RangeLength {
+		let line = 0;
+		let column = 0;
+		for (const c of text) {
+			if (c === '\n') {
+				line++;
+				column = 0;
+			} else {
+				column++;
+			}
+		}
+		return new RangeLength(line, column);
 	}
 
 	constructor(
@@ -67,6 +95,14 @@ export class RangeLength {
 			return new Range(startPosition.lineNumber, startPosition.column, startPosition.lineNumber, startPosition.column + this.columnCount);
 		} else {
 			return new Range(startPosition.lineNumber, startPosition.column, startPosition.lineNumber + this.lineCount, this.columnCount + 1);
+		}
+	}
+
+	public addToPosition(position: Position): Position {
+		if (this.lineCount === 0) {
+			return new Position(position.lineNumber, position.column + this.columnCount);
+		} else {
+			return new Position(position.lineNumber + this.lineCount, this.columnCount + 1);
 		}
 	}
 
