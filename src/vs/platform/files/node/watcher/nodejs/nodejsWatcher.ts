1598fa2347d198e89b728bb023bf57fc88be6d29
Benjamin Pasero <benjamin.pasero@microsoft.com>
2024-03-13 20:37:31 +0100

    watcher - fix correlation to actually work

--- src/vs/platform/files/node/watcher/nodejs/nodejsWatcher.ts
+++ src/vs/platform/files/node/watcher/nodejs/nodejsWatcher.ts
@@ -9,6 +9,7 @@ import { BaseWatcher } from 'vs/platform/files/node/watcher/baseWatcher';
 import { isLinux } from 'vs/base/common/platform';
 import { INonRecursiveWatchRequest, INonRecursiveWatcher } from 'vs/platform/files/common/watcher';
 import { NodeJSFileWatcherLibrary } from 'vs/platform/files/node/watcher/nodejs/nodejsWatcherLib';
+import { isEqual } from 'vs/base/common/extpath';
 
 export interface INodeJSWatcherInstance {
 
@@ -27,50 +28,68 @@ export class NodeJSWatcher extends BaseWatcher implements INonRecursiveWatcher {
 
 	readonly onDidError = Event.None;
 
-	protected readonly watchers = new Map<string, INodeJSWatcherInstance>();
+	protected readonly watchers = new Set<INodeJSWatcherInstance>();
 
 	private verboseLogging = false;
 
 	protected override async doWatch(requests: INonRecursiveWatchRequest[]): Promise<void> {
 
 		// Figure out duplicates to remove from the requests
-		const normalizedRequests = this.normalizeRequests(requests);
+		requests = this.removeDuplicateRequests(requests);
 
-		// Gather paths that we should start watching
-		const requestsToStartWatching = normalizedRequests.filter(request => {
-			const watcher = this.watchers.get(request.path);
-			if (!watcher) {
-				return true; // not yet watching that path
+		// Figure out which watchers to start and which to stop
+		const requestsToStart: INonRecursiveWatchRequest[] = [];
+		const watchersToStop = new Set(Array.from(this.watchers));
+		for (const request of requests) {
+			const watcher = this.findWatcher(request);
+			if (watcher && patternsEquals(watcher.request.excludes, request.excludes) && patternsEquals(watcher.request.includes, request.includes)) {
+				watchersToStop.delete(watcher);
+				continue; // skip over requests that are already watched with same patterns
 			}
 
-			// Re-watch path if excludes or includes have changed
-			return !patternsEquals(watcher.request.excludes, request.excludes) || !patternsEquals(watcher.request.includes, request.includes);
-		});
-
-		// Gather paths that we should stop watching
-		const pathsToStopWatching = Array.from(this.watchers.values()).filter(({ request }) => {
-			return !normalizedRequests.find(normalizedRequest => normalizedRequest.path === request.path && patternsEquals(normalizedRequest.excludes, request.excludes) && patternsEquals(normalizedRequest.includes, request.includes));
-		}).map(({ request }) => request.path);
+			requestsToStart.push(request);
+		}
 
 		// Logging
 
-		if (requestsToStartWatching.length) {
-			this.trace(`Request to start watching: ${requestsToStartWatching.map(request => `${request.path} (excludes: ${request.excludes.length > 0 ? request.excludes : '<none>'}, includes: ${request.includes && request.includes.length > 0 ? JSON.stringify(request.includes) : '<all>'}, correlationId: ${typeof request.correlationId === 'number' ? request.correlationId : '<none>'})`).join(',')}`);
+		if (requestsToStart.length) {
+			this.trace(`Request to start watching: ${requestsToStart.map(request => `${request.path} (excludes: ${request.excludes.length > 0 ? request.excludes : '<none>'}, includes: ${request.includes && request.includes.length > 0 ? JSON.stringify(request.includes) : '<all>'}, correlationId: ${typeof request.correlationId === 'number' ? request.correlationId : '<none>'})`).join(',')}`);
 		}
 
-		if (pathsToStopWatching.length) {
-			this.trace(`Request to stop watching: ${pathsToStopWatching.join(',')}`);
+		if (watchersToStop.size) {
+			this.trace(`Request to stop watching: ${Array.from(watchersToStop).map(watcher => `${watcher.request.path} (correlationId: ${typeof watcher.request.correlationId === 'number' ? watcher.request.correlationId : '<none>'})`).join(',')}`);
 		}
 
 		// Stop watching as instructed
-		for (const pathToStopWatching of pathsToStopWatching) {
-			this.stopWatching(pathToStopWatching);
+		for (const watcher of watchersToStop) {
+			this.stopWatching(watcher);
 		}
 
 		// Start watching as instructed
-		for (const request of requestsToStartWatching) {
+		for (const request of requestsToStart) {
 			this.startWatching(request);
 		}
+	}
+
+	private findWatcher(request: INonRecursiveWatchRequest): INodeJSWatcherInstance | undefined {
+		for (const watcher of this.watchers) {
+
+			// Requests or watchers with correlation always match on that
+			if (typeof request.correlationId === 'number' || typeof watcher.request.correlationId === 'number') {
+				if (watcher.request.correlationId === request.correlationId) {
+					return watcher;
+				}
+			}
+
+			// Non-correlated requests or watchers match on path
+			else {
+				if (isEqual(watcher.request.path, request.path, !isLinux /* ignorecase */)) {
+					return watcher;
+				}
+			}
+		}
+
+		return undefined;
 	}
 
 	private startWatching(request: INonRecursiveWatchRequest): void {
@@ -80,29 +99,24 @@ export class NodeJSWatcher extends BaseWatcher implements INonRecursiveWatcher {
 
 		// Remember as watcher instance
 		const watcher: INodeJSWatcherInstance = { request, instance };
-		this.watchers.set(request.path, watcher);
+		this.watchers.add(watcher);
 	}
 
 	override async stop(): Promise<void> {
 		await super.stop();
 
-		for (const [path] of this.watchers) {
-			this.stopWatching(path);
-		}
-
-		this.watchers.clear();
-	}
-
-	private stopWatching(path: string): void {
-		const watcher = this.watchers.get(path);
-		if (watcher) {
-			this.watchers.delete(path);
-
-			watcher.instance.dispose();
+		for (const watcher of this.watchers) {
+			this.stopWatching(watcher);
 		}
 	}
 
-	private normalizeRequests(requests: INonRecursiveWatchRequest[]): INonRecursiveWatchRequest[] {
+	private stopWatching(watcher: INodeJSWatcherInstance): void {
+		this.watchers.delete(watcher);
+
+		watcher.instance.dispose();
+	}
+
+	private removeDuplicateRequests(requests: INonRecursiveWatchRequest[]): INonRecursiveWatchRequest[] {
 		const mapCorrelationtoRequests = new Map<number | undefined /* correlation */, Map<string, INonRecursiveWatchRequest>>();
 
 		// Ignore requests for the same paths that have the same correlation
@@ -124,7 +138,7 @@ export class NodeJSWatcher extends BaseWatcher implements INonRecursiveWatcher {
 	async setVerboseLogging(enabled: boolean): Promise<void> {
 		this.verboseLogging = enabled;
 
-		for (const [, watcher] of this.watchers) {
+		for (const watcher of this.watchers) {
 			watcher.instance.setVerboseLogging(enabled);
 		}
 	}
