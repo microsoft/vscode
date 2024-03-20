31452c40b3b7cb8777947ebb7474a72f14e8704b
Simon Siefke <simon.siefke@gmail.com>
2024-03-20 20:58:45 +0100

    fix some tests

--- test/unit/electron/import-map/fs.js
+++ test/unit/electron/import-map/fs.js
@@ -1,5 +1,5 @@
 import { fs } from './testGlobals.js'
 
-const { createWriteStream, existsSync, readFileSync, readdirSync, rmSync, writeFileSync, unwatchFile, watchFile, watch, readFile } = fs
+const { createWriteStream, existsSync, readFileSync, readdirSync, rmSync, writeFileSync, unwatchFile, watchFile, watch, readFile, mkdir, chmod, link, unlink, truncate, copyFile, read, access, open } = fs
 
-export { createWriteStream, existsSync, readFileSync, readdirSync, rmSync, writeFileSync, unwatchFile, watchFile, watch, readFile }
+export { createWriteStream, existsSync, readFileSync, readdirSync, rmSync, writeFileSync, unwatchFile, watchFile, watch, readFile, mkdir, chmod, link, unlink, truncate, copyFile, read, access, open }
