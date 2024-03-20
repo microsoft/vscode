9f278ba6e16c1a0bef4a8cfa64220022de7e6911
Sandeep Somavarapu <sasomava@microsoft.com>
2024-03-20 01:53:09 +0100

    fix #208076 (#208153)

--- src/vs/workbench/services/extensionManagement/common/extensionManagement.ts
+++ src/vs/workbench/services/extensionManagement/common/extensionManagement.ts
@@ -9,6 +9,7 @@ import { IExtension, ExtensionType, IExtensionManifest, IExtensionIdentifier } f
 import { IExtensionManagementService, IGalleryExtension, ILocalExtension, InstallOptions, InstallExtensionEvent, DidUninstallExtensionEvent, InstallExtensionResult, Metadata, UninstallExtensionEvent } from 'vs/platform/extensionManagement/common/extensionManagement';
 import { URI } from 'vs/base/common/uri';
 import { FileAccess } from 'vs/base/common/network';
+import { localize } from 'vs/nls';
 
 export type DidChangeProfileEvent = { readonly added: ILocalExtension[]; readonly removed: ILocalExtension[] };
 
@@ -75,6 +76,13 @@ export interface IWorkbenchExtensionManagementService extends IProfileAwareExten
 
 	updateFromGallery(gallery: IGalleryExtension, extension: ILocalExtension, installOptions?: InstallOptions): Promise<ILocalExtension>;
 }
+
+export const extensionsConfigurationNodeBase = {
+	id: 'extensions',
+	order: 30,
+	title: localize('extensionsConfigurationTitle', "Extensions"),
+	type: 'object'
+};
 
 export const enum EnablementState {
 	DisabledByTrustRequirement,
