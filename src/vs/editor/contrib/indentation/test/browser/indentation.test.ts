6bc3ed7222a854591cff541bd3a687ec7a5ec80f
Aiday Marlen Kyzy <amarlenkyzy@microsoft.com>
2024-03-21 16:20:22 +0100

    adding the skip statement once again

--- src/vs/editor/contrib/indentation/test/browser/indentation.test.ts
+++ src/vs/editor/contrib/indentation/test/browser/indentation.test.ts
@@ -883,7 +883,7 @@ suite('`Full` Auto Indent On Type - TypeScript/JavaScript', () => {
 		});
 	});
 
-	test('issue #43244: indent when lambda arrow function is detected, outdent when end is reached', () => {
+	test.skip('issue #43244: indent when lambda arrow function is detected, outdent when end is reached', () => {
 
 		// https://github.com/microsoft/vscode/issues/43244
 
