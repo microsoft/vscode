8e596d10bf72901003ff8b0c321bdc6df489ff00
Simon Siefke <simon.siefke@gmail.com>
2024-03-21 19:18:07 +0100

    add sinon.mock

--- test/unit/electron/import-map/sinon.js
+++ test/unit/electron/import-map/sinon.js
@@ -1,8 +1,8 @@
 import { sinon } from './testGlobals.js'
 
 
-const { stub, createSandbox, sandbox, useFakeTimers, spy, restore, createStubInstance, expectation, clock, defaultConfig, addBehaviour, setFormatter, assert } = sinon
+const { stub, createSandbox, sandbox, useFakeTimers, spy, restore, createStubInstance, expectation, clock, defaultConfig, addBehaviour, setFormatter, assert, mock } = sinon
 
-export { stub, createSandbox, sandbox, useFakeTimers, spy, restore, createStubInstance, expectation, clock, defaultConfig, addBehaviour, setFormatter, assert }
+export { stub, createSandbox, sandbox, useFakeTimers, spy, restore, createStubInstance, expectation, clock, defaultConfig, addBehaviour, setFormatter, assert, mock }
 
 export default sinon
