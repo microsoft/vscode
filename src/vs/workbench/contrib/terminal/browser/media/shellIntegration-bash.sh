22eb0cde1c763d40e26bda26e513ba227da9e539
Daniel Imms <2193314+Tyriar@users.noreply.github.com>
2024-03-28 09:44:03 -0700

    Fix indentation

--- src/vs/workbench/contrib/terminal/browser/media/shellIntegration-bash.sh
+++ src/vs/workbench/contrib/terminal/browser/media/shellIntegration-bash.sh
@@ -275,7 +275,7 @@ __vsc_prompt_cmd_original() {
 	__vsc_restore_exit_code "${__vsc_status}"
 	# Evaluate the original PROMPT_COMMAND similarly to how bash would normally
 	# See https://unix.stackexchange.com/a/672843 for technique
- 	local cmd
+	local cmd
 	for cmd in "${__vsc_original_prompt_command[@]}"; do
 		eval "${cmd:-}"
 	done
