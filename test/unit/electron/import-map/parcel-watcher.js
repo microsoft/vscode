c2c79802b796c13616aed6c07fb4257a496e74af
Simon Siefke <simon.siefke@gmail.com>
2024-03-29 08:17:55 +0100

    add copyright statements

--- test/unit/electron/import-map/parcel-watcher.js
+++ test/unit/electron/import-map/parcel-watcher.js
@@ -1,3 +1,8 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
 import { parcelWatcher } from './testGlobals.js';
 
 const { subscribe, writeSnapshot, getEventsSince } = parcelWatcher
--- test/unit/electron/import-map/xterm.js
+++ test/unit/electron/import-map/xterm.js
@@ -1,4 +1,8 @@
-import { xterm } from './testGlobals.js';
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
 
+import { xterm } from './testGlobals.js';
 
 export default xterm
