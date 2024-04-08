8db0d353a692e85fe6ac01bb75e79416291354f8
Benjamin Pasero <benjamin.pasero@microsoft.com>
2024-04-08 17:17:08 +0200

    Aux windows: fails to restore size in multi-monitor setups with different resolutions (fix #208542) (#209813)

--- src/vs/platform/auxiliaryWindow/electron-main/auxiliaryWindowsMainService.ts
+++ src/vs/platform/auxiliaryWindow/electron-main/auxiliaryWindowsMainService.ts
@@ -51,11 +51,25 @@ export class AuxiliaryWindowsMainService extends Disposable implements IAuxiliar
 		// is created.
 
 		app.on('browser-window-created', (_event, browserWindow) => {
+
+			// This is an auxiliary window, try to claim it
 			const auxiliaryWindow = this.getWindowByWebContents(browserWindow.webContents);
 			if (auxiliaryWindow) {
 				this.logService.trace('[aux window] app.on("browser-window-created"): Trying to claim auxiliary window');
 
 				auxiliaryWindow.tryClaimWindow();
+			}
+
+			// This is a main window, listen to child windows getting created to claim it
+			else {
+				browserWindow.webContents.on('did-create-window', (browserWindow, details) => {
+					const auxiliaryWindow = this.getWindowByWebContents(browserWindow.webContents);
+					if (auxiliaryWindow) {
+						this.logService.trace('[aux window] window.on("did-create-window"): Trying to claim auxiliary window');
+
+						auxiliaryWindow.tryClaimWindow(details.options);
+					}
+				});
 			}
 		});
 
@@ -73,9 +87,7 @@ export class AuxiliaryWindowsMainService extends Disposable implements IAuxiliar
 
 	createWindow(details: HandlerDetails): BrowserWindowConstructorOptions {
 		return this.instantiationService.invokeFunction(defaultBrowserWindowOptions, this.validateWindowState(details), {
-			webPreferences: {
-				preload: FileAccess.asFileUri('vs/base/parts/sandbox/electron-sandbox/preload-aux.js').fsPath
-			}
+			preload: FileAccess.asFileUri('vs/base/parts/sandbox/electron-sandbox/preload-aux.js').fsPath
 		});
 	}
 
@@ -126,7 +138,9 @@ export class AuxiliaryWindowsMainService extends Disposable implements IAuxiliar
 	}
 
 	getWindowByWebContents(webContents: WebContents): AuxiliaryWindow | undefined {
-		return this.windows.get(webContents.id);
+		const window = this.windows.get(webContents.id);
+
+		return window?.matches(webContents) ? window : undefined;
 	}
 
 	getFocusedWindow(): IAuxiliaryWindow | undefined {
