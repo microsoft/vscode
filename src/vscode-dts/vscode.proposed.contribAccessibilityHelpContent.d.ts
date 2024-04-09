fd871b4ed097de1fece0220c58ab411a90124643
meganrogge <megan.rogge@microsoft.com>
2024-04-09 09:14:05 -0700

    fix grammar

--- src/vscode-dts/vscode.proposed.contribAccessibilityHelpContent.d.ts
+++ src/vscode-dts/vscode.proposed.contribAccessibilityHelpContent.d.ts
@@ -8,7 +8,7 @@
 // https://github.com/microsoft/vscode/issues/209855 @meganrogge
 
 /**
- * View contributions can include a `accessibilityHelpContent` property that provides help content for screen readers
+ * View contributions can include an `accessibilityHelpContent` property that provides help content for screen readers
  * when the accessibility help dialog is invoked by the user with focus in the view.
  *
  * The content is provided as a markdown string and can contain commands that will be resolved along with their keybindings.
