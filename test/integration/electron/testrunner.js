b0c93511b29fa1e8681df0281eadfb979a6dfc2a
Simon Siefke <simon.siefke@gmail.com>
2024-04-19 21:50:04 +0200

    use cjs for test runner

--- test/integration/electron/testrunner.js
+++ test/integration/electron/testrunner.js
@@ -6,32 +6,30 @@
 //@ts-check
 'use strict';
 
-import * as  paths from 'path';
-import glob from 'glob';
+const paths = require('path');
+const glob = require('glob');
 // Linux: prevent a weird NPE when mocha on Linux requires the window size from the TTY
 // Since we are not running in a tty environment, we just implement the method statically
-import tty from 'tty';
+const tty = require('tty');
 // @ts-ignore
 if (!tty.getWindowSize) {
 	// @ts-ignore
 	tty.getWindowSize = function () { return [80, 75]; };
 }
-import Mocha from 'mocha';
+const Mocha = require('mocha');
 
 let mocha = new Mocha({
 	ui: 'tdd',
 	color: true
 });
 
-export const configure = function configure(opts) {
+exports.configure = function configure(opts) {
 	mocha = new Mocha(opts);
 };
 
-export const run = async function run(testsRoot, clb) {
+exports.run = function run(testsRoot, clb) {
 	// Enable source map support
-	// TODO perhaps could also use nodejs builtin source map option here
-	const sourceMapSupport = await import('source-map-support')
-	sourceMapSupport.install()
+	require('source-map-support').install();
 
 	// Glob test files
 	glob('**/**.test.js', { cwd: testsRoot }, function (error, files) {
