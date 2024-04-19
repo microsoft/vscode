2f81f8bea372192976a347ab87e4a203da825b7c
Daniel Imms <2193314+Tyriar@users.noreply.github.com>
2024-04-19 11:12:46 -0700

    Remove ghost text from command line on execute
    Fixes #210777

--- src/vs/platform/terminal/common/capabilities/commandDetection/promptInputModel.ts
+++ src/vs/platform/terminal/common/capabilities/commandDetection/promptInputModel.ts
@@ -153,7 +153,13 @@ export class PromptInputModel extends Disposable implements IPromptInputModel {
 		}
 
 		this._cursorIndex = -1;
-		this._ghostTextIndex = -1;
+
+		// Remove any ghost text from the input if it exists on execute
+		if (this._ghostTextIndex !== -1) {
+			this._value = this._value.substring(0, this._ghostTextIndex);
+			this._ghostTextIndex = -1;
+		}
+
 		const event = this._createStateObject();
 		if (this._lastUserInput === '\u0003') {
 			this._onDidInterrupt.fire(event);
