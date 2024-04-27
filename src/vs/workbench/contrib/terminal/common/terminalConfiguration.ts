5305ce56806f0d8939559ff443f6a6c113574349
Daniel Imms <2193314+Tyriar@users.noreply.github.com>
2024-04-27 08:06:26 -0700

    Order settings

--- src/vs/workbench/contrib/terminal/common/terminalConfiguration.ts
+++ src/vs/workbench/contrib/terminal/common/terminalConfiguration.ts
@@ -627,13 +627,6 @@ const terminalConfiguration: IConfigurationNode = {
 			type: 'number',
 			default: 100
 		},
-		[TerminalSettingId.SuggestEnabled]: {
-			restricted: true,
-			markdownDescription: localize('terminal.integrated.shellIntegration.suggestEnabled', "Enables experimental terminal Intellisense suggestions for supported shells when {0} is set to {1}. If shell integration is installed manually, {2} needs to be set to {3} before calling the script.", '`#terminal.integrated.shellIntegration.enabled#`', '`true`', '`VSCODE_SUGGEST`', '`1`'),
-			type: 'boolean',
-			default: false,
-			markdownDeprecationMessage: localize('suggestEnabled.deprecated', 'This is an experimental setting and may break the terminal! Use at your own risk.')
-		},
 		[TerminalSettingId.SmoothScrolling]: {
 			markdownDescription: localize('terminal.integrated.smoothScrolling', "Controls whether the terminal will scroll using an animation."),
 			type: 'boolean',
@@ -689,6 +682,15 @@ const terminalConfiguration: IConfigurationNode = {
 				: localize('terminal.integrated.mouseWheelZoom', "Zoom the font of the terminal when using mouse wheel and holding `Ctrl`."),
 			type: 'boolean',
 			default: false
+		},
+
+		// terminal.suggest.contribution
+		[TerminalSettingId.SuggestEnabled]: {
+			restricted: true,
+			markdownDescription: localize('terminal.integrated.suggest.enabled', "Enables experimental terminal Intellisense suggestions for supported shells when {0} is set to {1}. If shell integration is installed manually, {2} needs to be set to {3} before calling the script.", '`#terminal.integrated.shellIntegration.enabled#`', '`true`', '`VSCODE_SUGGEST`', '`1`'),
+			type: 'boolean',
+			default: false,
+			markdownDeprecationMessage: localize('suggestEnabled.deprecated', 'This is an experimental setting and may break the terminal! Use at your own risk.')
 		},
 	}
 };
