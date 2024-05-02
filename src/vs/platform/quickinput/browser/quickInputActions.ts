76840dd77a1272b37891fb32df1c5806b0eb5043
Tyler James Leonhardt <me@tylerleonhardt.com>
2024-05-02 14:51:08 -0700

    Refactor to use `secondary` keybindings instead

--- src/vs/platform/quickinput/browser/quickInputActions.ts
+++ src/vs/platform/quickinput/browser/quickInputActions.ts
@@ -3,8 +3,9 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { KeyCode, KeyCodeUtils, KeyMod } from 'vs/base/common/keyCodes';
+import { KeyCode, KeyMod } from 'vs/base/common/keyCodes';
 import { isMacintosh } from 'vs/base/common/platform';
+import { PartialExcept } from 'vs/base/common/types';
 import { localize } from 'vs/nls';
 import { ICommandHandler } from 'vs/platform/commands/common/commands';
 import { ContextKeyExpr } from 'vs/platform/contextkey/common/contextkey';
@@ -12,50 +13,53 @@ import { ICommandAndKeybindingRule, KeybindingWeight, KeybindingsRegistry } from
 import { inQuickInputContext, quickInputTypeContextKeyValue } from 'vs/platform/quickinput/browser/quickInput';
 import { IQuickInputService, IQuickPick, QuickInputType, QuickPickFocus } from 'vs/platform/quickinput/common/quickInput';
 
-// function registerQuickPickCommandAndKeybindingRule(id: string, primary: number, handler: ICommandHandler, description?: string) {
-// 	KeybindingsRegistry.registerCommandAndKeybindingRule({
-// 		id,
-// 		weight: KeybindingWeight.WorkbenchContrib,
-// 		when: ContextKeyExpr.and(ContextKeyExpr.equals(quickInputTypeContextKeyValue, QuickInputType.QuickPick), inQuickInputContext),
-// 		primary,
-// 		handler,
-// 		metadata: description ? { description } : undefined
-// 	});
-// }
+const defaultCommandAndKeybindingRule = {
+	weight: KeybindingWeight.WorkbenchContrib,
+	when: ContextKeyExpr.and(ContextKeyExpr.equals(quickInputTypeContextKeyValue, QuickInputType.QuickPick), inQuickInputContext),
+};
+function registerQuickPickCommandAndKeybindingRule(rule: PartialExcept<ICommandAndKeybindingRule, 'id' | 'handler'>, options: { withAltMod?: boolean; withCtrlMod?: boolean; withCmdMod?: boolean } = {}) {
+	KeybindingsRegistry.registerCommandAndKeybindingRule({
+		...defaultCommandAndKeybindingRule,
+		id: rule.id,
+		handler: rule.handler,
+		primary: rule.primary,
+		secondary: getSecondary(rule.primary!, rule.secondary ?? [], options)
+	});
+}
 
-// function registerQuickPickCommandAndKeybindingRules(baseId: string, primary: number, handler: ICommandHandler, options: { withAltMod?: boolean; withCtrlMod?: boolean; withCmdMod?: boolean } = {}) {
-// 	registerQuickPickCommandAndKeybindingRule(baseId, primary, handler);
-// 	let index = 1;
-// 	const getName = () => baseId + 'Alternative' + index++;
-// 	const getDesc = (modifiers: KeyCode[]) => localize('alternateKeybinding', "An alternative keybinding for the command '{0}'. Usually set to what that command is set to with additional modifier(s): {1}", baseId, modifiers.map(KeyCodeUtils.toString).join(', '));
-// 	if (options.withAltMod) {
-// 		registerQuickPickCommandAndKeybindingRule(getName(), KeyMod.Alt + primary, handler, getDesc([KeyCode.Alt]));
-// 	}
-// 	const ctrlKeyMod = isMacintosh ? KeyMod.WinCtrl : KeyMod.CtrlCmd;
-// 	if (options.withCtrlMod) {
-// 		registerQuickPickCommandAndKeybindingRule(getName(), ctrlKeyMod + primary, handler, getDesc([KeyCode.Ctrl]));
-// 	}
-// 	if (options.withAltMod && options.withCtrlMod) {
-// 		registerQuickPickCommandAndKeybindingRule(getName(), KeyMod.Alt + ctrlKeyMod + primary, handler, getDesc([KeyCode.Alt, KeyCode.Ctrl]));
-// 	}
-// 	if (options.withCmdMod && isMacintosh) {
-// 		registerQuickPickCommandAndKeybindingRule(getName(), KeyMod.CtrlCmd + primary, handler, getDesc([KeyCode.Meta]));
-// 		registerQuickPickCommandAndKeybindingRule(getName(), KeyMod.CtrlCmd + KeyMod.Alt + primary, handler, getDesc([KeyCode.Meta, KeyCode.Alt]));
-// 		registerQuickPickCommandAndKeybindingRule(getName(), KeyMod.CtrlCmd + KeyMod.WinCtrl + primary, handler, getDesc([KeyCode.Meta, KeyCode.Ctrl]));
-// 		registerQuickPickCommandAndKeybindingRule(getName(), KeyMod.CtrlCmd + KeyMod.Alt + KeyMod.WinCtrl + primary, handler, getDesc([KeyCode.Meta, KeyCode.Alt, KeyCode.Ctrl]));
-// 	}
-// }
+function getSecondary(primary: number, secondary: number[], options: { withAltMod?: boolean; withCtrlMod?: boolean; withCmdMod?: boolean } = {}): number[] {
+	if (options.withAltMod) {
+		secondary.push(KeyMod.Alt + primary);
+	}
+	const ctrlKeyMod = isMacintosh ? KeyMod.WinCtrl : KeyMod.CtrlCmd;
+	if (options.withCtrlMod) {
+		secondary.push(ctrlKeyMod + primary);
+		if (options.withAltMod) {
+			secondary.push(KeyMod.Alt + ctrlKeyMod + primary);
+		}
+	}
 
-// //#region Navigation
+	if (options.withCmdMod && isMacintosh) {
+		secondary.push(KeyMod.CtrlCmd + primary);
+		if (options.withCtrlMod) {
+			secondary.push(KeyMod.CtrlCmd + KeyMod.WinCtrl + primary);
+		}
+		if (options.withAltMod) {
+			secondary.push(KeyMod.CtrlCmd + KeyMod.Alt + primary);
+			if (options.withCtrlMod) {
+				secondary.push(KeyMod.CtrlCmd + KeyMod.Alt + KeyMod.WinCtrl + primary);
+			}
+		}
+	}
 
-/**
- * Generate the handler for the command being registered
- * @param focus What to focus
- * @param focusOnQuickNatigate What to focus if we are in quick navigate mode
- * @returns A command handler
- */
+	return secondary;
+}
+
+//#region Navigation
+
 function focusHandler(focus: QuickPickFocus, focusOnQuickNatigate?: QuickPickFocus): ICommandHandler {
 	return accessor => {
+		// Assuming this is a quick pick due to above when clause
 		const currentQuickPick = accessor.get(IQuickInputService).currentQuickInput as IQuickPick<any> | undefined;
 		if (!currentQuickPick) {
 			return;
@@ -67,50 +71,101 @@ function focusHandler(focus: QuickPickFocus, focusOnQuickNatigate?: QuickPickFoc
 	};
 }
 
-// registerQuickPickCommandAndKeybindingRules('quickInput.pageNext', KeyCode.PageDown, focusHandler(QuickPickFocus.NextPage), { withAltMod: true, withCtrlMod: true, withCmdMod: true });
-// registerQuickPickCommandAndKeybindingRules('quickInput.pagePrevious', KeyCode.PageUp, focusHandler(QuickPickFocus.PreviousPage), { withAltMod: true, withCtrlMod: true, withCmdMod: true });
-// registerQuickPickCommandAndKeybindingRules('quickInput.first', KeyCode.Home, focusHandler(QuickPickFocus.First), { withAltMod: true, withCtrlMod: true, withCmdMod: true });
-// registerQuickPickCommandAndKeybindingRules('quickInput.last', KeyCode.End, focusHandler(QuickPickFocus.Last), { withAltMod: true, withCtrlMod: true, withCmdMod: true });
+registerQuickPickCommandAndKeybindingRule(
+	{ id: 'quickInput.pageNext', primary: KeyCode.PageDown, handler: focusHandler(QuickPickFocus.NextPage) },
+	{ withAltMod: true, withCtrlMod: true, withCmdMod: true }
+);
+registerQuickPickCommandAndKeybindingRule(
+	{ id: 'quickInput.pagePrevious', primary: KeyCode.PageUp, handler: focusHandler(QuickPickFocus.PreviousPage) },
+	{ withAltMod: true, withCtrlMod: true, withCmdMod: true }
+);
+registerQuickPickCommandAndKeybindingRule(
+	{ id: 'quickInput.first', primary: KeyCode.Home, handler: focusHandler(QuickPickFocus.First) },
+	{ withAltMod: true, withCtrlMod: true, withCmdMod: true }
+);
+registerQuickPickCommandAndKeybindingRule(
+	{ id: 'quickInput.last', primary: KeyCode.End, handler: focusHandler(QuickPickFocus.Last) },
+	{ withAltMod: true, withCtrlMod: true, withCmdMod: true }
+);
+registerQuickPickCommandAndKeybindingRule(
+	{ id: 'quickInput.next', primary: KeyCode.DownArrow, handler: focusHandler(QuickPickFocus.Next) },
+	{ withCtrlMod: true }
+);
+registerQuickPickCommandAndKeybindingRule(
+	{ id: 'quickInput.previous', primary: KeyCode.UpArrow, handler: focusHandler(QuickPickFocus.Previous) },
+	{ withCtrlMod: true }
+);
 
-// registerQuickPickCommandAndKeybindingRules('quickInput.next', KeyCode.DownArrow, focusHandler(QuickPickFocus.Next), { withCtrlMod: true });
-// registerQuickPickCommandAndKeybindingRules('quickInput.previous', KeyCode.UpArrow, focusHandler(QuickPickFocus.Previous), { withCtrlMod: true });
+const nextSeparatorFallbackDesc = localize('quickInput.nextSeparatorWithQuickAccessFallback', "If we're in quick access mode, this will navigate to the next item. If we are not in quick access mode, this will navigate to the next separator.");
+const prevSeparatorFallbackDesc = localize('quickInput.previousSeparatorWithQuickAccessFallback', "If we're in quick access mode, this will navigate to the previous item. If we are not in quick access mode, this will navigate to the previous separator.");
+if (isMacintosh) {
+	registerQuickPickCommandAndKeybindingRule(
+		{
+			id: 'quickInput.nextSeparatorWithQuickAccessFallback',
+			primary: KeyMod.CtrlCmd + KeyCode.DownArrow,
+			handler: focusHandler(QuickPickFocus.NextSeparator, QuickPickFocus.Next),
+			metadata: { description: nextSeparatorFallbackDesc }
+		},
+	);
+	registerQuickPickCommandAndKeybindingRule(
+		{
+			id: 'quickInput.nextSeparator',
+			primary: KeyMod.CtrlCmd + KeyMod.Alt + KeyCode.DownArrow,
+			secondary: [KeyMod.CtrlCmd + KeyMod.WinCtrl + KeyCode.DownArrow],
+			handler: focusHandler(QuickPickFocus.NextSeparator)
+		},
+		{ withCtrlMod: true }
+	);
 
-// const nextSeparatorFallbackDesc = localize('quickInput.nextSeparatorWithQuickAccessFallback', "Navigates to the next separator but only if we are not in quick access mode.");
-// const prevSeparatorFallbackDesc = localize('quickInput.previousSeparatorWithQuickAccessFallback', "Navigates to the previous separator but only if we are not in quick access mode.");
-// if (isMacintosh) {
-// 	registerQuickPickCommandAndKeybindingRule('quickInput.nextSeparatorWithQuickAccessFallback', KeyMod.CtrlCmd + KeyCode.DownArrow, focusHandler(QuickPickFocus.NextSeparator, QuickPickFocus.Next), nextSeparatorFallbackDesc);
-// 	registerQuickPickCommandAndKeybindingRules('quickInput.nextSeparator', KeyMod.Alt + KeyMod.CtrlCmd + KeyCode.DownArrow, focusHandler(QuickPickFocus.NextSeparator), { withCtrlMod: true });
+	registerQuickPickCommandAndKeybindingRule(
+		{
+			id: 'quickInput.previousSeparatorWithQuickAccessFallback',
+			primary: KeyMod.CtrlCmd + KeyCode.UpArrow,
+			handler: focusHandler(QuickPickFocus.PreviousSeparator, QuickPickFocus.Previous),
+			metadata: { description: prevSeparatorFallbackDesc }
+		},
+	);
+	registerQuickPickCommandAndKeybindingRule(
+		{
+			id: 'quickInput.previousSeparator',
+			primary: KeyMod.CtrlCmd + KeyMod.Alt + KeyCode.UpArrow,
+			secondary: [KeyMod.CtrlCmd + KeyMod.WinCtrl + KeyCode.UpArrow],
+			handler: focusHandler(QuickPickFocus.PreviousSeparator)
+		},
+		{ withCtrlMod: true }
+	);
+} else {
+	registerQuickPickCommandAndKeybindingRule(
+		{
+			id: 'quickInput.nextSeparatorWithQuickAccessFallback',
+			primary: KeyMod.Alt + KeyCode.DownArrow,
+			handler: focusHandler(QuickPickFocus.NextSeparator, QuickPickFocus.Next),
+			metadata: { description: nextSeparatorFallbackDesc }
+		},
+	);
+	registerQuickPickCommandAndKeybindingRule(
+		{
+			id: 'quickInput.nextSeparator',
+			primary: KeyMod.CtrlCmd + KeyMod.Alt + KeyCode.DownArrow,
+			handler: focusHandler(QuickPickFocus.NextSeparator)
+		},
+	);
 
-// 	registerQuickPickCommandAndKeybindingRules('quickInput.previousSeparatorWithQuickAccessFallback', KeyMod.CtrlCmd + KeyCode.UpArrow, focusHandler(QuickPickFocus.PreviousSeparator, QuickPickFocus.Previous), { withCtrlMod: true });
-// 	registerQuickPickCommandAndKeybindingRules('quickInput.previousSeparator', KeyMod.Alt + KeyMod.CtrlCmd + KeyCode.UpArrow, focusHandler(QuickPickFocus.PreviousSeparator), { withCtrlMod: true });
-// } else {
-// 	registerQuickPickCommandAndKeybindingRule('quickInput.nextSeparatorWithQuickAccessFallback', KeyMod.Alt + KeyCode.DownArrow, focusHandler(QuickPickFocus.NextSeparator, QuickPickFocus.Next), nextSeparatorFallbackDesc);
-// 	registerQuickPickCommandAndKeybindingRule('quickInput.nextSeparator', KeyMod.CtrlCmd + KeyMod.Alt + KeyCode.DownArrow, focusHandler(QuickPickFocus.NextSeparator));
+	registerQuickPickCommandAndKeybindingRule(
+		{
+			id: 'quickInput.previousSeparatorWithQuickAccessFallback',
+			primary: KeyMod.Alt + KeyCode.UpArrow,
+			handler: focusHandler(QuickPickFocus.PreviousSeparator, QuickPickFocus.Previous),
+			metadata: { description: prevSeparatorFallbackDesc }
+		},
+	);
+	registerQuickPickCommandAndKeybindingRule(
+		{
+			id: 'quickInput.previousSeparator',
+			primary: KeyMod.CtrlCmd + KeyMod.Alt + KeyCode.UpArrow,
+			handler: focusHandler(QuickPickFocus.PreviousSeparator)
+		},
+	);
+}
 
-// 	registerQuickPickCommandAndKeybindingRule('quickInput.previousSeparatorWithQuickAccessFallback', KeyMod.Alt + KeyCode.UpArrow, focusHandler(QuickPickFocus.PreviousSeparator, QuickPickFocus.Previous), prevSeparatorFallbackDesc);
-// 	registerQuickPickCommandAndKeybindingRule('quickInput.previousSeparator', KeyMod.CtrlCmd + KeyMod.Alt + KeyCode.UpArrow, focusHandler(QuickPickFocus.PreviousSeparator));
-// }
-
-// //#endregion
-
-// // interface IRegisterOptions { id: string; weight?: KeybindingWeight; primary?: number; handler: ICommandHandler; descriptin }
-
-// // function registerQuickPickCommandAndKeybindingRule(options: { id: string; weight?: KeybindingWeight; primary?: number; handler: ICommandHandler; descriptin }) {
-// // 	KeybindingsRegistry.registerCommandAndKeybindingRule({
-// // 		id,
-// // 		weight: KeybindingWeight.WorkbenchContrib,
-// // 		when: ContextKeyExpr.and(ContextKeyExpr.equals(quickInputTypeContextKeyValue, QuickInputType.QuickPick), inQuickInputContext),
-// // 		primary,
-// // 		handler,
-// // 		metadata: description ? { description } : undefined
-// // 	});
-// // }
-
-// KeybindingsRegistry.registerCommandAndKeybindingRule({
-// 	id: 'quickInput.next',
-// 	weight: KeybindingWeight.WorkbenchContrib,
-// 	when: ContextKeyExpr.and(ContextKeyExpr.equals(quickInputTypeContextKeyValue, QuickInputType.QuickPick), inQuickInputContext),
-// 	primary: KeyCode.DownArrow,
-// 	secondary: [KeyMod.CtrlCmd + KeyCode.DownArrow, KeyMod.Alt + KeyCode.DownArrow, KeyMod.CtrlCmd + KeyMod.Alt + KeyCode.DownArrow],
-// 	handler: focusHandler(QuickPickFocus.Next),
-// });
+//#endregion
