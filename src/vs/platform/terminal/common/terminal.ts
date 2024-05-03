881b796c55bc1d8caffa94b79a17f29dbee334e1
Daniel Imms <2193314+Tyriar@users.noreply.github.com>
2024-05-03 05:11:39 -0700

    Add whitespace back

--- src/vs/platform/terminal/common/terminal.ts
+++ src/vs/platform/terminal/common/terminal.ts
@@ -115,6 +115,7 @@ export const enum TerminalSettingId {
 	SmoothScrolling = 'terminal.integrated.smoothScrolling',
 	IgnoreBracketedPasteMode = 'terminal.integrated.ignoreBracketedPasteMode',
 	FocusAfterRun = 'terminal.integrated.focusAfterRun',
+
 	// Debug settings that are hidden from user
 
 	/** Simulated latency applied to all calls made to the pty host */
