366fac3cfdbdf949a33678ed7da05faea0bbfec5
Daniel Imms <2193314+Tyriar@users.noreply.github.com>
2024-05-03 07:24:54 -0700

    Adjust prompt based on last prompt line
    Part of #211945

--- src/vs/platform/terminal/common/capabilities/commandDetection/promptInputModel.ts
+++ src/vs/platform/terminal/common/capabilities/commandDetection/promptInputModel.ts
@@ -54,6 +54,7 @@ export class PromptInputModel extends Disposable implements IPromptInputModel {
 
 	private _commandStartMarker: IMarker | undefined;
 	private _commandStartX: number = 0;
+	private _lastPromptLine: string | undefined;
 	private _continuationPrompt: string | undefined;
 
 	private _lastUserInput: string = '';
@@ -117,6 +118,11 @@ export class PromptInputModel extends Disposable implements IPromptInputModel {
 		this._continuationPrompt = value;
 	}
 
+	setLastPromptLine(value: string): void {
+		this._lastPromptLine = value;
+		this._sync();
+	}
+
 	setConfidentCommandLine(value: string): void {
 		if (this._value !== value) {
 			this._value = value;
@@ -153,6 +159,17 @@ export class PromptInputModel extends Disposable implements IPromptInputModel {
 		this._cursorIndex = 0;
 		this._onDidStartInput.fire(this._createStateObject());
 		this._onDidChangeInput.fire(this._createStateObject());
+
+		// Trigger a sync if prompt terminator is set as that could adjust the command start X
+		if (this._lastPromptLine) {
+			if (this._commandStartX !== this._lastPromptLine.length) {
+				const line = this._xterm.buffer.active.getLine(this._commandStartMarker.line);
+				if (line?.translateToString(true).startsWith(this._lastPromptLine)) {
+					this._commandStartX = this._lastPromptLine.length;
+					this._sync();
+				}
+			}
+		}
 	}
 
 	private _handleCommandExecuted() {
@@ -189,6 +206,9 @@ export class PromptInputModel extends Disposable implements IPromptInputModel {
 		if (commandStartY === undefined) {
 			return;
 		}
+
+		console.log('commandStartX', this._commandStartX);
+		console.log('promptTerminator', this._lastPromptLine);
 
 		const buffer = this._xterm.buffer.active;
 		let line = buffer.getLine(commandStartY);
