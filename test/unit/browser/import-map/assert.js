669fd23d0fc7c1241505b52a8e1bd1703a23b4e9
Simon Siefke <simon.siefke@gmail.com>
2024-05-06 20:16:37 +0200

    fix assert path

--- test/unit/browser/import-map/assert.js
+++ test/unit/browser/import-map/assert.js
@@ -2,5 +2,10 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
+import { importAmdModule, root } from './amdx.js'
 
-export { }
+const module = await importAmdModule(
+	`${root}/test/unit/assert.js`,
+)
+
+export default module
