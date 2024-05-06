ca87edb5563d89e9a42ecf8d14cda50ac646a7c7
Simon Siefke <simon.siefke@gmail.com>
2024-05-06 20:51:52 +0200

    fix missing exports

--- test/unit/browser/import-map/assert.js
+++ test/unit/browser/import-map/assert.js
@@ -8,8 +8,8 @@ const module = await importAmdModule(
 	`${root}/test/unit/assert.js`,
 )
 
-const { AssertionError, deepEqual, deepStrictEqual, doesNotReject, doesNotThrow, equal, fail, ifError, notDeepEqual, notDeepStrictEqual, ok, rejects, strictEqual, throws } = module
+const { AssertionError, deepEqual, deepStrictEqual, doesNotReject, doesNotThrow, equal, fail, ifError, notDeepEqual, notDeepStrictEqual, ok, rejects, strictEqual, throws, notStrictEqual, notEqual } = module
 
-export { AssertionError, deepEqual, deepStrictEqual, doesNotReject, doesNotThrow, equal, fail, ifError, notDeepEqual, notDeepStrictEqual, ok, rejects, strictEqual, throws }
+export { AssertionError, deepEqual, deepStrictEqual, doesNotReject, doesNotThrow, equal, fail, ifError, notDeepEqual, notDeepStrictEqual, ok, rejects, strictEqual, throws, notStrictEqual, notEqual }
 
 export default module
