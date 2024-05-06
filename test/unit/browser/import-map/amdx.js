cc1b8bf141e873e581257eb11fd0720da8084857
Simon Siefke <simon.siefke@gmail.com>
2024-05-06 20:12:57 +0200

    import absolute path

--- test/unit/browser/import-map/amdx.js
+++ test/unit/browser/import-map/amdx.js
@@ -2,10 +2,10 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-import { importAmdModule } from './amdx.js'
 
-const module = await importAmdModule(
-	"node_modules/sinon/pkg/sinon.js",
-)
+export const root = new URL('../../../../', import.meta.url).toString()
 
-export default module
+export const importAmdModule = async (absolutePath) => {
+	console.log({ absolutePath })
+	await new Promise(() => { })
+}
