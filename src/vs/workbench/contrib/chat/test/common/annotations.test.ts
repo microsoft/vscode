5c0213f74c48b988e0184287d29251f42890bd5f
Simon Siefke <simon.siefke@gmail.com>
2024-05-07 23:16:28 +0200

    fix formatting

--- src/vs/workbench/contrib/chat/test/common/annotations.test.ts
+++ src/vs/workbench/contrib/chat/test/common/annotations.test.ts
@@ -3,36 +3,25 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { MarkdownString } from "vs/base/common/htmlContent";
-import { assertSnapshot } from "vs/base/test/common/snapshot";
-import { ensureNoDisposablesAreLeakedInTestSuite } from "vs/base/test/common/utils";
-import { IChatMarkdownContent } from "vs/workbench/contrib/chat/common/chatService";
-import {
-	annotateSpecialMarkdownContent,
-	extractVulnerabilitiesFromText,
-} from "../../common/annotations";
+import { MarkdownString } from 'vs/base/common/htmlContent';
+import { assertSnapshot } from 'vs/base/test/common/snapshot';
+import { ensureNoDisposablesAreLeakedInTestSuite } from 'vs/base/test/common/utils';
+import { IChatMarkdownContent } from 'vs/workbench/contrib/chat/common/chatService';
+import { annotateSpecialMarkdownContent, extractVulnerabilitiesFromText } from '../../common/annotations';
 
 function content(str: string): IChatMarkdownContent {
-	return { kind: "markdownContent", content: new MarkdownString(str) };
+	return { kind: 'markdownContent', content: new MarkdownString(str) };
 }
 
-suite("Annotations", function () {
+suite('Annotations', function () {
 	ensureNoDisposablesAreLeakedInTestSuite();
 
-	suite("extractVulnerabilitiesFromText", () => {
-		test("single line", async () => {
-			const before = "some code ";
-			const vulnContent = "content with vuln";
-			const after = " after";
-			const annotatedResult = annotateSpecialMarkdownContent([
-				content(before),
-				{
-					kind: "markdownVuln",
-					content: new MarkdownString(vulnContent),
-					vulnerabilities: [{ title: "title", description: "vuln" }],
-				},
-				content(after),
-			]);
+	suite('extractVulnerabilitiesFromText', () => {
+		test('single line', async () => {
+			const before = 'some code ';
+			const vulnContent = 'content with vuln';
+			const after = ' after';
+			const annotatedResult = annotateSpecialMarkdownContent([content(before), { kind: 'markdownVuln', content: new MarkdownString(vulnContent), vulnerabilities: [{ title: 'title', description: 'vuln' }] }, content(after)]);
 			await assertSnapshot(annotatedResult);
 
 			const markdown = annotatedResult[0] as IChatMarkdownContent;
@@ -40,19 +29,11 @@ suite("Annotations", function () {
 			await assertSnapshot(result);
 		});
 
-		test("multiline", async () => {
-			const before = "some code\nover\nmultiple lines ";
-			const vulnContent = "content with vuln\nand\nnewlines";
-			const after = "more code\nwith newline";
-			const annotatedResult = annotateSpecialMarkdownContent([
-				content(before),
-				{
-					kind: "markdownVuln",
-					content: new MarkdownString(vulnContent),
-					vulnerabilities: [{ title: "title", description: "vuln" }],
-				},
-				content(after),
-			]);
+		test('multiline', async () => {
+			const before = 'some code\nover\nmultiple lines ';
+			const vulnContent = 'content with vuln\nand\nnewlines';
+			const after = 'more code\nwith newline';
+			const annotatedResult = annotateSpecialMarkdownContent([content(before), { kind: 'markdownVuln', content: new MarkdownString(vulnContent), vulnerabilities: [{ title: 'title', description: 'vuln' }] }, content(after)]);
 			await assertSnapshot(annotatedResult);
 
 			const markdown = annotatedResult[0] as IChatMarkdownContent;
@@ -60,23 +41,15 @@ suite("Annotations", function () {
 			await assertSnapshot(result);
 		});
 
-		test("multiple vulns", async () => {
-			const before = "some code\nover\nmultiple lines ";
-			const vulnContent = "content with vuln\nand\nnewlines";
-			const after = "more code\nwith newline";
+		test('multiple vulns', async () => {
+			const before = 'some code\nover\nmultiple lines ';
+			const vulnContent = 'content with vuln\nand\nnewlines';
+			const after = 'more code\nwith newline';
 			const annotatedResult = annotateSpecialMarkdownContent([
 				content(before),
-				{
-					kind: "markdownVuln",
-					content: new MarkdownString(vulnContent),
-					vulnerabilities: [{ title: "title", description: "vuln" }],
-				},
+				{ kind: 'markdownVuln', content: new MarkdownString(vulnContent), vulnerabilities: [{ title: 'title', description: 'vuln' }] },
 				content(after),
-				{
-					kind: "markdownVuln",
-					content: new MarkdownString(vulnContent),
-					vulnerabilities: [{ title: "title", description: "vuln" }],
-				},
+				{ kind: 'markdownVuln', content: new MarkdownString(vulnContent), vulnerabilities: [{ title: 'title', description: 'vuln' }] },
 			]);
 			await assertSnapshot(annotatedResult);
 
