2aa1079dbb407a998f9aa9b56049c291a586496e
Connor Peet <connor@peet.io>
2024-05-08 15:42:43 -0700

    just use v8-to-istanbul

--- .vscode/extensions/vscode-selfhost-test-provider/src/coverageProvider.ts
+++ .vscode/extensions/vscode-selfhost-test-provider/src/coverageProvider.ts
@@ -3,121 +3,104 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+import { TraceMap } from '@jridgewell/trace-mapping';
 import { IstanbulCoverageContext } from 'istanbul-to-vscode';
-import { SourceMapStore } from './testOutputScanner';
+import { fileURLToPath } from 'url';
 import * as vscode from 'vscode';
-import { IScriptCoverage, OffsetToPosition, RangeCoverageTracker } from './v8CoverageWrangling';
-import * as v8ToIstanbul from 'v8-to-istanbul';
+import { SourceMapStore } from './sourceMapStore';
+import v8ToIstanbul = require('v8-to-istanbul');
 
 export const istanbulCoverageContext = new IstanbulCoverageContext();
 
+export interface ICoverageRange {
+	start: number;
+	end: number;
+	covered: boolean;
+}
+
+export interface IV8FunctionCoverage {
+	functionName: string;
+	isBlockCoverage: boolean;
+	ranges: IV8CoverageRange[];
+}
+
+export interface IV8CoverageRange {
+	startOffset: number;
+	endOffset: number;
+	count: number;
+}
+
+/** V8 Script coverage data */
+export interface IScriptCoverage {
+	scriptId: string;
+	url: string;
+	// Script source added by the runner the first time the script is emitted.
+	source?: string;
+	functions: IV8FunctionCoverage[];
+}
+
 /**
  * Tracks coverage in per-script coverage mode. There are two modes of coverage
- * in this extension: generic istanbul reports, and reports from the runtime
+ * in this extension: generic istanbul reports, and V8 reports from the runtime
  * sent before and after each test case executes. This handles the latter.
  */
 export class PerTestCoverageTracker {
 	private readonly scripts = new Map</* script ID */ string, Script>();
 
-	constructor(
-		private readonly initialCoverage: IScriptCoverage,
-		private readonly maps: SourceMapStore,
-	) {}
+	constructor(private readonly maps: SourceMapStore) { }
 
-	public add(coverage: IScriptCoverage, test?: vscode.TestItem) {
-		const script = this.scripts.get(coverage.scriptId);
-		if (script) {
-			return script.add(coverage, test);
-		}
-		if (!coverage.source) {
-			throw new Error('expected to have source the first time a script is seen');
+	/** Adds new coverage data to the run, optionally for a test item. */
+	public add(run: vscode.TestRun, coverage: IScriptCoverage, test?: vscode.TestItem) {
+		let script = this.scripts.get(coverage.scriptId);
+		if (!script) {
+			if (!coverage.source) {
+				throw new Error('expected to have source the first time a script is seen');
+			}
+
+			script = new Script(coverage.url, coverage.source, this.maps);
+			this.scripts.set(coverage.scriptId, script);
 		}
 
-		const src = new Script(coverage.url, coverage.source, this.maps);
+		return script.add(run, coverage, test);
 	}
 }
 
 class Script {
-	private converter: OffsetToPosition;
-
-	/** Tracking the overall coverage for the file */
-	private overall = new ScriptProjection();
-	/** Range tracking per-test item */
-	private readonly perItem = new Map<vscode.TestItem, ScriptProjection>();
+	private sourceMap?: Promise<TraceMap | undefined>;
+	private originalContent?: Promise<string | undefined>;
 
 	constructor(
 		public readonly url: string,
-		source: string,
+		private readonly source: string,
 		private readonly maps: SourceMapStore,
 	) {
-		this.converter = new OffsetToPosition(source);
 	}
 
-	public add(coverage: IScriptCoverage, test?: vscode.TestItem) {
-		this.overall.add(coverage);
-		if (test) {
-			const p = new ScriptProjection();
-			p.add(coverage);
-			this.perItem.set(test, p);
-		}
-	}
-
-	public report(run: vscode.TestRun) {
-
-	}
-}
-
-class ScriptProjection {
-	/** Range tracking for non-block coverage in the file */
-	private file = new RangeCoverageTracker();
-	/** Range tracking for block coverage in the file */
-	private readonly blocks = new Map<string, RangeCoverageTracker>();
-
-	public add(coverage: IScriptCoverage) {
-
-		for (const fn of coverage.functions) {
-			if (fn.isBlockCoverage) {
-				const key = `${fn.ranges[0].startOffset}/${fn.ranges[0].endOffset}`;
-				const block = this.blocks.get(key);
-				if (block) {
-					for (let i = 1; i < fn.ranges.length; i++) {
-						block.setCovered(fn.ranges[i].startOffset, fn.ranges[i].endOffset, fn.ranges[i].count > 0);
-					}
-				} else {
-					this.blocks.set(key, RangeCoverageTracker.initializeBlock(fn.ranges));
-				}
-			} else {
-				for (const range of fn.ranges) {
-					this.file.setCovered(range.startOffset, range.endOffset, range.count > 0);
-				}
-			}
-		}
-	}
-
-	public report(run: vscode.TestRun, convert: OffsetToPosition, item?: vscode.TestItem) {
-		const ranges = [...this.file];
-		for (const block of this.blocks.values()) {
-			for (const range of block) {
-				ranges.push(range);
-			}
+	public async add(run: vscode.TestRun, coverage: IScriptCoverage, test?: vscode.TestItem) {
+		if (!coverage.url.startsWith('file://')) {
+			return;
 		}
 
-		let ri = 0;
-		ranges.sort((a, b) => a.end - b.end);
+		const sourceMap = await (this.sourceMap ??= this.maps.loadSourceMap(coverage.url));
+		const originalSource = await (this.originalContent ??= this.maps.getSourceFileContents(coverage.url));
+		const istanbuled = v8ToIstanbul(fileURLToPath(coverage.url), undefined, sourceMap && originalSource
+			? { source: this.source, originalSource, sourceMap: { sourcemap: sourceMap } }
+			: { source: this.source }
+		);
 
-		let offset = 0;
-		for (let i = 0; i < convert.lines.length; i++) {
-			const lineEnd = offset + convert.lines[i] + 1;
+		await istanbuled.load();
 
-			const coverage = new RangeCoverageTracker();
-			for (let i = ri; i < ranges.length && ranges[i].start < lineEnd; i++) {
-				coverage.setCovered(ranges[i].start - offset, ranges[i].end - offset, ranges[i].covered);
+		const coverages = await istanbulCoverageContext.fromJson(istanbuled.toIstanbul(), {
+			mapFileUri: uri => this.maps.getSourceFile(uri.toString()),
+			mapLocation: (uri, position) =>
+				this.maps.getSourceLocation(uri.toString(), position.line, position.character),
+		});
+
+		for (const coverage of coverages) {
+			if (test) {
+				(coverage as vscode.FileCoverage2).testItem = test;
 			}
-
-			while (ri < ranges.length && ranges[ri].end < lineEnd) {
-				ri++;
-			}
+			run.addCoverage(coverage);
 		}
-
 	}
 }
--- .vscode/extensions/vscode-selfhost-test-provider/src/testOutputScanner.ts
+++ .vscode/extensions/vscode-selfhost-test-provider/src/testOutputScanner.ts
@@ -3,19 +3,13 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import {
-	GREATEST_LOWER_BOUND,
-	LEAST_UPPER_BOUND,
-	originalPositionFor,
-	TraceMap,
-} from '@jridgewell/trace-mapping';
 import * as styles from 'ansi-styles';
 import { ChildProcessWithoutNullStreams } from 'child_process';
 import * as vscode from 'vscode';
-import { IScriptCoverage, istanbulCoverageContext } from './coverageProvider';
+import { IScriptCoverage, PerTestCoverageTracker, istanbulCoverageContext } from './coverageProvider';
 import { attachTestMessageMetadata } from './metadata';
 import { snapshotComment } from './snapshot';
-import { getContentFromFilesystem } from './testTree';
+import { SourceMapStore } from './sourceMapStore';
 import { StreamSplitter } from './streamSplitter';
 
 export const enum MochaEvent {
@@ -174,6 +168,7 @@ export async function scanTestOutput(
 
 	let lastTest: vscode.TestItem | undefined;
 	let ranAnyTest = false;
+	let perTestCoverage: PerTestCoverageTracker | undefined;
 
 	try {
 		if (cancellation.isCancellationRequested) {
@@ -319,6 +314,19 @@ export async function scanTestOutput(
 					case MochaEvent.End:
 						// no-op, we wait until the process exits to ensure coverage is written out
 						break;
+					case MochaEvent.CoverageInit:
+						perTestCoverage ??= new PerTestCoverageTracker(store);
+						enqueueExitBlocker(perTestCoverage.add(task, evt[1]));
+						break;
+					case MochaEvent.CoverageIncrement: {
+						const { fullTitle, coverage } = evt[1];
+						const tcase = tests.get(fullTitle);
+						if (tcase) {
+							perTestCoverage ??= new PerTestCoverageTracker(store);
+							enqueueExitBlocker(perTestCoverage.add(task, coverage, tcase));
+						}
+						break;
+					}
 				}
 			});
 		});
@@ -399,90 +407,6 @@ const tryMakeMarkdown = (message: string) => {
 	lines.push('```');
 	return new vscode.MarkdownString(lines.join('\n'));
 };
-
-const inlineSourcemapRe = /^\/\/# sourceMappingURL=data:application\/json;base64,(.+)/m;
-const sourceMapBiases = [GREATEST_LOWER_BOUND, LEAST_UPPER_BOUND] as const;
-
-export class SourceMapStore {
-	private readonly cache = new Map</* file uri */ string, Promise<TraceMap | undefined>>();
-
-	async getSourceLocation(fileUri: string, line: number, col = 1) {
-		const sourceMap = await this.loadSourceMap(fileUri);
-		if (!sourceMap) {
-			return undefined;
-		}
-
-		for (const bias of sourceMapBiases) {
-			const position = originalPositionFor(sourceMap, { column: col, line: line + 1, bias });
-			if (position.line !== null && position.column !== null && position.source !== null) {
-				return new vscode.Location(
-					this.completeSourceMapUrl(sourceMap, position.source),
-					new vscode.Position(position.line - 1, position.column)
-				);
-			}
-		}
-
-		return undefined;
-	}
-
-	async getSourceFile(compiledUri: string) {
-		const sourceMap = await this.loadSourceMap(compiledUri);
-		if (!sourceMap) {
-			return undefined;
-		}
-
-		if (sourceMap.sources[0]) {
-			return this.completeSourceMapUrl(sourceMap, sourceMap.sources[0]);
-		}
-
-		for (const bias of sourceMapBiases) {
-			const position = originalPositionFor(sourceMap, { column: 0, line: 1, bias });
-			if (position.source !== null) {
-				return this.completeSourceMapUrl(sourceMap, position.source);
-			}
-		}
-
-		return undefined;
-	}
-
-	private completeSourceMapUrl(sm: TraceMap, source: string) {
-		if (sm.sourceRoot) {
-			try {
-				return vscode.Uri.parse(new URL(source, sm.sourceRoot).toString());
-			} catch {
-				// ignored
-			}
-		}
-
-		return vscode.Uri.parse(source);
-	}
-
-	private loadSourceMap(fileUri: string) {
-		const existing = this.cache.get(fileUri);
-		if (existing) {
-			return existing;
-		}
-
-		const promise = (async () => {
-			try {
-				const contents = await getContentFromFilesystem(vscode.Uri.parse(fileUri));
-				const sourcemapMatch = inlineSourcemapRe.exec(contents);
-				if (!sourcemapMatch) {
-					return;
-				}
-
-				const decoded = Buffer.from(sourcemapMatch[1], 'base64').toString();
-				return new TraceMap(decoded, fileUri);
-			} catch (e) {
-				console.warn(`Error parsing sourcemap for ${fileUri}: ${(e as Error).stack}`);
-				return;
-			}
-		})();
-
-		this.cache.set(fileUri, promise);
-		return promise;
-	}
-}
 
 const locationRe = /(file:\/{3}.+):([0-9]+):([0-9]+)/g;
 
