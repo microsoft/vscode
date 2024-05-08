e92a9ec2cd959b2ca1736bbcdd47f315ce3d0f99
Simon Siefke <simon.siefke@gmail.com>
2024-05-09 00:35:39 +0200

    skip debug test

--- extensions/vscode-api-tests/src/singlefolder-tests/debug.test.ts
+++ extensions/vscode-api-tests/src/singlefolder-tests/debug.test.ts
@@ -60,7 +60,7 @@ suite('vscode API - debug', function () {
 		assert.strictEqual(functionBreakpoint.functionName, 'func');
 	});
 
-	test('start debugging', async function () {
+	test.skip('start debugging', async function () {
 		let stoppedEvents = 0;
 		let variablesReceived: () => void;
 		let initializedReceived: () => void;
