b9d504b8e4668a55dee1f09f65f1305c87a43cac
Frank <frankli0324@hotmail.com>
2024-05-16 10:44:40 +0800

    inherit process env when execFile npm view

--- extensions/npm/src/features/packageJSONContribution.ts
+++ extensions/npm/src/features/packageJSONContribution.ts
@@ -293,6 +293,7 @@ export class PackageJSONContribution implements IJSONContribution {
 			// COREPACK_ENABLE_PROJECT_SPEC makes the npm view command succeed
 			//   even if packageManager specified a package manager other than npm.
 			const env = { COREPACK_ENABLE_AUTO_PIN: "0", COREPACK_ENABLE_PROJECT_SPEC: "0" };
+			Object.assign(env, process.env}
 			cp.execFile(npmCommandPath, args, { cwd, env }, (error, stdout) => {
 				if (!error) {
 					try {
