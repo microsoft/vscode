26b6c37f1b61a0ea02c2618573d2ea3c929c9d1c
Simon Siefke <simon.siefke@gmail.com>
2024-05-17 20:09:24 +0200

    update minimist imports

--- src/vs/platform/environment/node/argv.ts
+++ src/vs/platform/environment/node/argv.ts
@@ -3,7 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import * as minimist from 'minimist';
+import minimist from 'minimist';
 import { isWindows } from 'vs/base/common/platform';
 import { localize } from 'vs/nls';
 import { NativeParsedArgs } from 'vs/platform/environment/common/argv';
