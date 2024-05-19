4a5e2e9a3e073b571726c83909790d8e0ace5fca
Simon Siefke <simon.siefke@gmail.com>
2024-05-19 12:22:45 +0200

    import contribution

--- src/vs/workbench/workbench.desktop.main.ts
+++ src/vs/workbench/workbench.desktop.main.ts
@@ -149,6 +149,9 @@ import 'vs/workbench/contrib/tasks/electron-sandbox/taskService';
 // External terminal
 import 'vs/workbench/contrib/externalTerminal/electron-sandbox/externalTerminal.contribution';
 
+// WebUtils
+import 'vs/workbench/contrib/webUtils/electron-sandbox/webUtils.contribution'
+
 // Webview
 import 'vs/workbench/contrib/webview/electron-sandbox/webview.contribution';
 
