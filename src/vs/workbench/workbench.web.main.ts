454d277308fcc02b79fb725548e5f0b030e60e08
Simon Siefke <simon.siefke@gmail.com>
2024-05-22 23:35:51 +0200

    remove import

--- src/vs/workbench/workbench.web.main.ts
+++ src/vs/workbench/workbench.web.main.ts
@@ -136,9 +136,6 @@ import 'vs/workbench/contrib/welcomeBanner/browser/welcomeBanner.contribution';
 // Welcome Dialog
 import 'vs/workbench/contrib/welcomeDialog/browser/welcomeDialog.contribution';
 
-// WebUtils
-import 'vs/workbench/contrib/webUtils/browser/webUtils.web.contribution'
-
 // Webview
 import 'vs/workbench/contrib/webview/browser/webview.web.contribution';
 
