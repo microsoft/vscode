878cb471aa63428a5e7d37efd48695f7dfea31b1
Rob Lourens <roblourens@gmail.com>
2024-05-22 19:42:18 -0700

    Fix fillInIncompleteTokens for ordered lists (#213280)

--- src/vs/base/browser/markdownRenderer.ts
+++ src/vs/base/browser/markdownRenderer.ts
@@ -715,8 +715,14 @@ function completeListItemPattern(list: marked.Tokens.List): marked.Tokens.List |
 
 	const previousListItemsText = mergeRawTokenText(list.items.slice(0, -1));
 
-	// Grabbing the `- ` off the list item because I can't find a better way to do this
-	const newListItemText = lastListItem.raw.slice(0, 2) +
+	// Grabbing the `- ` or `1. ` off the list item because I can't find a better way to do this
+	const lastListItemLead = lastListItem.raw.match(/^(\s*(-|\d+\.) +)/)?.[0];
+	if (!lastListItemLead) {
+		// Is badly formatted
+		return;
+	}
+
+	const newListItemText = lastListItemLead +
 		mergeRawTokenText(lastListItem.tokens.slice(0, -1)) +
 		newToken.raw;
 
