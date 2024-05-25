b842b2dba6c9cc258711111c7ee59a3eee8a96be
Simon Siefke <simon.siefke@gmail.com>
2024-05-25 11:12:54 +0200

    fix formatting

--- src/vs/platform/theme/common/colorUtils.ts
+++ src/vs/platform/theme/common/colorUtils.ts
@@ -149,7 +149,8 @@ class ColorRegistry implements IColorRegistry {
 	public registerColor(id: string, defaults: ColorDefaults | null, description: string, needsTransparency = false, deprecationMessage?: string): ColorIdentifier {
 		const colorContribution: ColorContribution = { id, description, defaults, needsTransparency, deprecationMessage };
 		this.colorsById[id] = colorContribution;
-		const propertySchema: IJSONSchema = { type: 'string', description, format: 'color-hex', defaultSnippets: [{ body: '${1:#ff0000}' }] }; if (deprecationMessage) {
+		const propertySchema: IJSONSchema = { type: 'string', description, format: 'color-hex', defaultSnippets: [{ body: '${1:#ff0000}' }] };
+		if (deprecationMessage) {
 			propertySchema.deprecationMessage = deprecationMessage;
 		}
 		if (needsTransparency) {
