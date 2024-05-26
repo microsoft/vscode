8378ff48416ca5b5d06cf4cdf4506f685ec15250
Simon Siefke <simon.siefke@gmail.com>
2024-05-26 11:39:55 +0200

    fix colors

--- src/vs/editor/contrib/find/browser/findDecorations.ts
+++ src/vs/editor/contrib/find/browser/findDecorations.ts
@@ -287,7 +287,8 @@ export class FindDecorations implements IDisposable {
 		description: 'current-find-match',
 		stickiness: TrackedRangeStickiness.NeverGrowsWhenTypingAtEdges,
 		zIndex: 13,
-		inlineClassName: 'currentFindMatch',
+		className: 'currentFindMatch',
+		inlineClassName: 'currentFindMatchInline',
 		showIfCollapsed: true,
 		overviewRuler: {
 			color: themeColorFromId(overviewRulerFindMatchForeground),
@@ -299,11 +300,20 @@ export class FindDecorations implements IDisposable {
 		}
 	});
 
+	// public static readonly _CURRENT_FIND_MATCH_DECORATION_INLINE = ModelDecorationOptions.register({
+	// 	description: 'current-find-match-inline',
+	// 	stickiness: TrackedRangeStickiness.NeverGrowsWhenTypingAtEdges,
+	// 	zIndex: 13,
+	// 	inlineClassName: 'currentFindMatchInline',
+	// 	showIfCollapsed: true,
+	// });
+
 	public static readonly _FIND_MATCH_DECORATION = ModelDecorationOptions.register({
 		description: 'find-match',
 		stickiness: TrackedRangeStickiness.NeverGrowsWhenTypingAtEdges,
 		zIndex: 10,
-		inlineClassName: 'findMatch',
+		className: 'findMatch',
+		inlineClassName: 'findMatchInline',
 		showIfCollapsed: true,
 		overviewRuler: {
 			color: themeColorFromId(overviewRulerFindMatchForeground),
@@ -314,6 +324,14 @@ export class FindDecorations implements IDisposable {
 			position: MinimapPosition.Inline
 		}
 	});
+
+	// public static readonly _FIND_MATCH_DECORATION_INLINE = ModelDecorationOptions.register({
+	// 	description: 'find-match-inline',
+	// 	stickiness: TrackedRangeStickiness.NeverGrowsWhenTypingAtEdges,
+	// 	zIndex: 10,
+	// 	inlineClassName: 'findMatchInline',
+	// 	showIfCollapsed: true,
+	// });
 
 	public static readonly _FIND_MATCH_NO_OVERVIEW_DECORATION = ModelDecorationOptions.register({
 		description: 'find-match-no-overview',
