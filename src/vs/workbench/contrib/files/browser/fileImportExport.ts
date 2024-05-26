1ddc4b24a488ac4a395c683f40c154ce9713fcdb
Simon Siefke <simon.siefke@gmail.com>
2024-05-26 12:06:45 +0200

    format code

--- src/vs/workbench/contrib/files/browser/fileImportExport.ts
+++ src/vs/workbench/contrib/files/browser/fileImportExport.ts
@@ -564,7 +564,7 @@ export class ExternalFileImport {
 			});
 
 			// if we only add one file, just open it directly
-			const autoOpen = this.configurationService.getValue<IFilesConfiguration>().explorer.autoOpenDroppedFile
+			const autoOpen = this.configurationService.getValue<IFilesConfiguration>().explorer.autoOpenDroppedFile;
 			if (autoOpen && resourceFileEdits.length === 1) {
 				const item = this.explorerService.findClosest(resourceFileEdits[0].newResource!);
 				if (item && !item.isDirectory) {
--- src/vs/workbench/contrib/files/common/files.ts
+++ src/vs/workbench/contrib/files/common/files.ts
@@ -108,7 +108,7 @@ export interface IFilesConfiguration extends PlatformIFilesConfiguration, IWorkb
 			expand: boolean;
 			patterns: { [parent: string]: string };
 		};
-		autoOpenDroppedFile: boolean
+		autoOpenDroppedFile: boolean;
 	};
 	editor: IEditorOptions;
 }
