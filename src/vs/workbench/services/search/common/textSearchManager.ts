ddc3155ee8c5a016e263a8317b6959b7d86ec429
Simon Siefke <simon.siefke@gmail.com>
2024-05-30 22:27:24 +0200

    Discard changes to src/vs/workbench/services/search/common/textSearchManager.ts

--- src/vs/workbench/services/search/common/textSearchManager.ts
+++ src/vs/workbench/services/search/common/textSearchManager.ts
@@ -213,8 +213,7 @@ export class TextSearchManager {
 			maxResults: this.query.maxResults ?? Number.MAX_SAFE_INTEGER,
 			previewOptions: this.query.previewOptions,
 			afterContext: this.query.afterContext,
-			beforeContext: this.query.beforeContext,
-			threads: this.query.threads
+			beforeContext: this.query.beforeContext
 		};
 		if ('usePCRE2' in this.query) {
 			(<IExtendedExtensionSearchOptions>options).usePCRE2 = this.query.usePCRE2;
