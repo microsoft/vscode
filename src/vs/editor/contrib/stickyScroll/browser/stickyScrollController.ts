0c06def6ea708760affab3d664cea47176ee7bb6
Aiday Marlen Kyzy <amarlenkyzy@microsoft.com>
2024-05-30 16:34:51 +0200

    using the next rebuild from line variable

--- src/vs/editor/contrib/stickyScroll/browser/stickyScrollController.ts
+++ src/vs/editor/contrib/stickyScroll/browser/stickyScrollController.ts
@@ -488,24 +488,23 @@ export class StickyScrollController extends Disposable implements IEditorContrib
 		}
 
 		const nextRebuildFromLine = this._updateAndGetMinRebuildFromLine(rebuildFromLine);
-
 		const stickyWidgetVersion = this._stickyLineCandidateProvider.getVersionId();
 		const shouldUpdateState = stickyWidgetVersion === undefined || stickyWidgetVersion === model.getVersionId();
 
 		if (shouldUpdateState) {
 			if (!this._focused) {
-				await this._updateState(rebuildFromLine);
+				await this._updateState(nextRebuildFromLine);
 			} else {
 				// Suppose that previously the sticky scroll widget had height 0, then if there are visible lines, set the last line as focused
 				if (this._focusedStickyElementIndex === -1) {
-					await this._updateState(rebuildFromLine);
+					await this._updateState(nextRebuildFromLine);
 					this._focusedStickyElementIndex = this._stickyScrollWidget.lineNumberCount - 1;
 					if (this._focusedStickyElementIndex !== -1) {
 						this._stickyScrollWidget.focusLineWithIndex(this._focusedStickyElementIndex);
 					}
 				} else {
 					const focusedStickyElementLineNumber = this._stickyScrollWidget.lineNumbers[this._focusedStickyElementIndex];
-					await this._updateState(rebuildFromLine);
+					await this._updateState(nextRebuildFromLine);
 					// Suppose that after setting the state, there are no sticky lines, set the focused index to -1
 					if (this._stickyScrollWidget.lineNumberCount === 0) {
 						this._focusedStickyElementIndex = -1;
