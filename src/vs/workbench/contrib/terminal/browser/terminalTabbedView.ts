9c7c28b8bd733a62f32ca9c34cf33b6b2de12af2
Simon Siefke <simon.siefke@gmail.com>
2024-05-30 17:50:31 +0200

    Discard changes to src/vs/workbench/contrib/terminal/browser/terminalTabbedView.ts

--- src/vs/workbench/contrib/terminal/browser/terminalTabbedView.ts
+++ src/vs/workbench/contrib/terminal/browser/terminalTabbedView.ts
@@ -69,7 +69,6 @@ export class TerminalTabbedView extends Disposable {
 
 	constructor(
 		parentElement: HTMLElement,
-		getPathForFile: undefined | ((file: File) => string),
 		@ITerminalService private readonly _terminalService: ITerminalService,
 		@ITerminalConfigurationService private readonly _terminalConfigurationService: ITerminalConfigurationService,
 		@ITerminalGroupService private readonly _terminalGroupService: ITerminalGroupService,
@@ -94,7 +93,7 @@ export class TerminalTabbedView extends Disposable {
 		this._tabsListMenu = this._register(menuService.createMenu(MenuId.TerminalTabContext, contextKeyService));
 		this._tabsListEmptyMenu = this._register(menuService.createMenu(MenuId.TerminalTabEmptyAreaContext, contextKeyService));
 
-		this._tabList = this._register(this._instantiationService.createInstance(TerminalTabList, this._tabListElement, getPathForFile));
+		this._tabList = this._register(this._instantiationService.createInstance(TerminalTabList, this._tabListElement));
 
 		const terminalOuterContainer = $('.terminal-outer-container');
 		this._terminalContainer = $('.terminal-groups-container');
