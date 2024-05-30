c1f070b51b3ffc29cfeec8633b6661a4a39b485c
Simon Siefke <simon.siefke@gmail.com>
2024-05-30 22:27:04 +0200

    remove unused code

--- src/vs/workbench/services/search/common/search.ts
+++ src/vs/workbench/services/search/common/search.ts
@@ -93,7 +93,6 @@ export interface ICommonQueryProps<U extends UriComponents> {
 
 	maxResults?: number;
 	usingSearchPaths?: boolean;
-	threads?: number
 }
 
 export interface IFileQueryProps<U extends UriComponents> extends ICommonQueryProps<U> {
