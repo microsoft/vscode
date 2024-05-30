953862f4c317d72220e2cbd6b29b2612015fea24
Simon Siefke <simon.siefke@gmail.com>
2024-05-30 22:30:00 +0200

    fix types

--- src/vs/workbench/services/search/common/search.ts
+++ src/vs/workbench/services/search/common/search.ts
@@ -93,6 +93,7 @@ export interface ICommonQueryProps<U extends UriComponents> {
 
 	maxResults?: number;
 	usingSearchPaths?: boolean;
+	threads?: boolean;
 }
 
 export interface IFileQueryProps<U extends UriComponents> extends ICommonQueryProps<U> {
