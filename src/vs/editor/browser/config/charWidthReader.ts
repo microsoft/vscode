0d9cea2dbc9c62200374571d5b76e171bdd6fba3
Remco Haszing <remcohaszing@gmail.com>
2024-06-04 20:54:01 +0200

    Update src/vs/editor/browser/config/charWidthReader.ts
    Co-authored-by: Logan Ramos <lramos15@gmail.com>

--- src/vs/editor/browser/config/charWidthReader.ts
+++ src/vs/editor/browser/config/charWidthReader.ts
@@ -56,7 +56,7 @@ class DomCharWidthReader {
 		this._readFromDomElements();
 
 		// Remove the container from the DOM
-		this._container!.remove();
+		this._container?.remove();
 
 		this._container = null;
 		this._testElements = null;
