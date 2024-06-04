54be91767a42cbaf429785e6d8be54339a87a12c
Simon Siefke <simon.siefke@gmail.com>
2024-06-04 19:25:13 +0200

    Discard changes to extensions/npm/src/preferred-pm.ts

--- extensions/npm/src/preferred-pm.ts
+++ extensions/npm/src/preferred-pm.ts
@@ -4,9 +4,9 @@
  *--------------------------------------------------------------------------------------------*/
 
 import findWorkspaceRoot = require('../node_modules/find-yarn-workspace-root');
-import findUp from 'find-up';
+import * as findUp from 'find-up';
 import * as path from 'path';
-import whichPM from 'which-pm';
+import * as whichPM from 'which-pm';
 import { Uri, workspace } from 'vscode';
 
 interface PreferredProperties {
