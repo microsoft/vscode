microsoft/vscode

commit a131a88231e3152d34d1e03eb9c0b0eb36e749cc

Replace `removeChild` with `remove` (#213465)
* Replace removeChild with remove

This replaces most uses of `parent.removeChild(child)` with
`child.remove()`.

The two are almost equivalent. The only difference is that
`parent.removeChild(child)` throws if the given node is not a child of
the parent, whereas `child.remove()` never throws. There is no noticable
performance difference. The only reason to use `removeChild` is to
support Internet Explorer, but that’s no longer supported by Monaco
editor.

* Fix broken CSP hash

The script content changed, so the sha256 hash changed too.

* Update src/vs/editor/browser/config/charWidthReader.ts

Co-authored-by: Logan Ramos <lramos15@gmail.com>

---------

Co-authored-by: Logan Ramos <lramos15@gmail.com>

src/vs/base/browser/ui/selectBox/selectBoxCustom.ts
@@ -520,12 +520,7 @@ export class SelectBoxList extends Disposable implements ISelectBoxDelegate, ILi
 		return {
 			dispose: () => {
 				// contextView will dispose itself if moving from one View to another
-				try {
-					container.removeChild(this.selectDropDownContainer); // remove to take out the CSS rules we add
-				}
-				catch (error) {
-					// Ignore, removed already by change of focus
-				}
+				this.selectDropDownContainer.remove(); // remove to take out the CSS rules we add
 			}
 		};
 	}
@@ -612,20 +607,20 @@ export class SelectBoxList extends Disposable implements ISelectBoxDelegate, ILi
 					&& this.options.length > maxVisibleOptionsBelow
 				) {
 					this._dropDownPosition = AnchorPosition.ABOVE;
-					this.selectDropDownContainer.removeChild(this.selectDropDownListContainer);
-					this.selectDropDownContainer.removeChild(this.selectionDetailsPane);
-					this.selectDropDownContainer.appendChild(this.selectionDetailsPane);
-					this.selectDropDownContainer.appendChild(this.selectDropDownListContainer);
+					this.selectDropDownListContainer.remove();
+					this.selectionDetailsPane.remove();
+					this.selectionDetailsPane.remove();
+					this.selectDropDownListContainer.remove();
 
 					this.selectionDetailsPane.classList.remove('border-top');
 					this.selectionDetailsPane.classList.add('border-bottom');
 
 				} else {
 					this._dropDownPosition = AnchorPosition.BELOW;
-					this.selectDropDownContainer.removeChild(this.selectDropDownListContainer);
-					this.selectDropDownContainer.removeChild(this.selectionDetailsPane);
-					this.selectDropDownContainer.appendChild(this.selectDropDownListContainer);
-					this.selectDropDownContainer.appendChild(this.selectionDetailsPane);
+					this.selectDropDownListContainer.remove();
+					this.selectionDetailsPane.remove();
+					this.selectDropDownListContainer.remove();
+					this.selectionDetailsPane.remove();
 
 					this.selectionDetailsPane.classList.remove('border-bottom');
 					this.selectionDetailsPane.classList.add('border-top');
@@ -879,7 +874,7 @@ export class SelectBoxList extends Disposable implements ISelectBoxDelegate, ILi
 
 				const tagName = child.tagName && child.tagName.toLowerCase();
 				if (tagName === 'img') {
-					element.removeChild(child);
+					child.remove();
 				} else {
 					cleanRenderedMarkdown(child);
 				}

commit 0470f48f5e91f78cd449f1b018227e4f2d74f60f

Fix opening select boxes
This fixes a regression caused by #213465

Closes #214303
Closes #214345

src/vs/base/browser/ui/selectBox/selectBoxCustom.ts
@@ -609,8 +609,8 @@ export class SelectBoxList extends Disposable implements ISelectBoxDelegate, ILi
 					this._dropDownPosition = AnchorPosition.ABOVE;
 					this.selectDropDownListContainer.remove();
 					this.selectionDetailsPane.remove();
-					this.selectionDetailsPane.remove();
-					this.selectDropDownListContainer.remove();
+					this.selectDropDownContainer.appendChild(this.selectionDetailsPane);
+					this.selectDropDownContainer.appendChild(this.selectDropDownListContainer);
 
 					this.selectionDetailsPane.classList.remove('border-top');
 					this.selectionDetailsPane.classList.add('border-bottom');
@@ -619,8 +619,8 @@ export class SelectBoxList extends Disposable implements ISelectBoxDelegate, ILi
 					this._dropDownPosition = AnchorPosition.BELOW;
 					this.selectDropDownListContainer.remove();
 					this.selectionDetailsPane.remove();
-					this.selectDropDownListContainer.remove();
-					this.selectionDetailsPane.remove();
+					this.selectDropDownContainer.appendChild(this.selectDropDownListContainer);
+					this.selectDropDownContainer.appendChild(this.selectionDetailsPane);
 
 					this.selectionDetailsPane.classList.remove('border-bottom');
 					this.selectionDetailsPane.classList.add('border-top');