e13fa101b71b8fa6f82d3647cf4f8cd31add8b6e
Benjamin Pasero <benjamin.pasero@microsoft.com>
2024-06-05 11:35:19 +0200

    cleanup

--- extensions/git/src/encoding.ts
+++ extensions/git/src/encoding.ts
@@ -49,38 +49,27 @@ const JSCHARDET_TO_ICONV_ENCODINGS: { [name: string]: string } = {
 	'big5': 'cp950'
 };
 
-const JSCHARDET_SUPPORTED_GUESS_ENCODINGS = [
-	'Big5',
-	'EUC-JP',
-	'EUC-KR',
-	'EUC-TW',
-	'GB2312',
-	'HZ-GB-2312',
-	'IBM855',
-	'IBM866',
-	'ISO-2022-CN',
-	'ISO-2022-JP',
-	'ISO-2022-KR',
-	'ISO-8859-2',
-	'ISO-8859-5',
-	'ISO-8859-7',
-	'KOI8-R',
-	'SHIFT_JIS',
-	'TIS-620',
-	'UTF-16BE',
-	'UTF-16LE',
-	'UTF-32BE',
-	'UTF-32LE',
-	'UTF-8',
-	'X-ISO-10646-UCS-4-2143',
-	'X-ISO-10646-UCS-4-3412',
-	'windows-1250',
-	'windows-1251',
-	'windows-1252',
-	'windows-1253',
-	'windows-1255',
-	'x-mac-cyrillic',
-];
+const MAP_CANDIDATE_GUESS_ENCODING_TO_JSCHARDET: { [key: string]: string } = {
+	utf8: 'UTF-8',
+	utf16le: 'UTF-16LE',
+	utf16be: 'UTF-16BE',
+	windows1252: 'windows-1252',
+	windows1250: 'windows-1250',
+	iso88592: 'ISO-8859-2',
+	windows1251: 'windows-1251',
+	cp866: 'IBM866',
+	iso88595: 'ISO-8859-5',
+	koi8r: 'KOI8-R',
+	windows1253: 'windows-1253',
+	iso88597: 'ISO-8859-7',
+	windows1255: 'windows-1255',
+	iso88598: 'ISO-8859-8',
+	cp950: 'Big5',
+	shiftjis: 'SHIFT_JIS',
+	eucjp: 'EUC-JP',
+	euckr: 'EUC-KR',
+	gb2312: 'GB2312'
+};
 
 export function detectEncoding(buffer: Buffer, candidateGuessEncodings: string[]): string | null {
 	const result = detectEncodingByBOM(buffer);
@@ -89,17 +78,9 @@ export function detectEncoding(buffer: Buffer, candidateGuessEncodings: string[]
 		return result;
 	}
 
-	let encodings: string[] | undefined = normalizedEncodings(candidateGuessEncodings);
-	if (encodings.length === 0) {
-		encodings = undefined;
-	}
+	candidateGuessEncodings = candidateGuessEncodings.map(e => MAP_CANDIDATE_GUESS_ENCODING_TO_JSCHARDET[e]).filter(e => !!e);
 
-	const detected = jschardet.detect(buffer, {
-		// Lower the threshold to make sure we have a result
-		minimumThreshold: 0,
-		detectEncodings: encodings,
-	});
-
+	const detected = jschardet.detect(buffer, candidateGuessEncodings.length > 0 ? { detectEncodings: candidateGuessEncodings } : undefined);
 	if (!detected || !detected.encoding) {
 		return null;
 	}
@@ -116,14 +97,4 @@ export function detectEncoding(buffer: Buffer, candidateGuessEncodings: string[]
 	const mapped = JSCHARDET_TO_ICONV_ENCODINGS[normalizedEncodingName];
 
 	return mapped || normalizedEncodingName;
-}
-
-function normalizedEncodings(encodings: string[]): string[] {
-	const normalizedEncodings = encodings.map(encoding => encoding.replace(/[^a-zA-Z0-9]/g, '').toLowerCase());
-	const validEncodings = JSCHARDET_SUPPORTED_GUESS_ENCODINGS.filter(supportEncoding => {
-		const normalizedSupportEncoding = supportEncoding.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
-		return normalizedEncodings.indexOf(normalizedSupportEncoding) !== -1;
-	});
-
-	return validEncodings;
 }
