microsoft/vscode

commit a131a88231e3152d34d1e03eb9c0b0eb36e749cc

Replace `removeChild` with `remove` (#213465)
* Replace removeChild with remove

This replaces most uses of `parent.removeChild(child)` with
`child.remove()`.

The two are almost equivalent. The only difference is that
`parent.removeChild(child)` throws if the given node is not a child of
the parent, whereas `child.remove()` never throws. There is no noticable
performance difference. The only reason to use `removeChild` is to
support Internet Explorer, but that’s no longer supported by Monaco
editor.

* Fix broken CSP hash

The script content changed, so the sha256 hash changed too.

* Update src/vs/editor/browser/config/charWidthReader.ts

Co-authored-by: Logan Ramos <lramos15@gmail.com>

---------

Co-authored-by: Logan Ramos <lramos15@gmail.com>

src/vs/workbench/contrib/splash/browser/partsSplash.ts
@@ -110,8 +110,6 @@ export class PartsSplash {
 
 		// remove initial colors
 		const defaultStyles = mainWindow.document.head.getElementsByClassName('initialShellColors');
-		if (defaultStyles.length) {
-			mainWindow.document.head.removeChild(defaultStyles[0]);
-		}
+		defaultStyles?.[0].remove();
 	}
 }

commit c71e3a9e73ded6fdb8daa3a5540bb7a5dcbf8ff7

Fix another regression caused by #213465

src/vs/workbench/contrib/splash/browser/partsSplash.ts
@@ -110,6 +110,6 @@ export class PartsSplash {
 
 		// remove initial colors
 		const defaultStyles = mainWindow.document.head.getElementsByClassName('initialShellColors');
-		defaultStyles?.[0].remove();
+		defaultStyles[0]?.remove();
 	}
 }