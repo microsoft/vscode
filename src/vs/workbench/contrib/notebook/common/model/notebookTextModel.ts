7dfcb45cc482d62c57bb8640d1773ee080b11819
Aaron Munger <aamunger@microsoft.com>
2024-06-07 15:49:43 -0700

    remove unused method

--- src/vs/workbench/contrib/notebook/common/model/notebookTextModel.ts
+++ src/vs/workbench/contrib/notebook/common/model/notebookTextModel.ts
@@ -207,16 +207,6 @@ export class NotebookTextModel extends Disposable implements INotebookTextModel 
 		return this._cells;
 	}
 
-	*cellsForView(): Iterable<NotebookCellTextModel> {
-		if (this.viewType === 'repl') {
-			for (let i = 0; i < this._cells.length - 1; i++) {
-				yield this._cells[i];
-			}
-		} else {
-			yield* this._cells;
-		}
-	}
-
 	get versionId() {
 		return this._versionId;
 	}
