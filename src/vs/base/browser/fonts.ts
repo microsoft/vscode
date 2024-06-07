8028356281a4702a727a3b8d36ced6637bc8b170
Simon Siefke <simon.siefke@gmail.com>
2024-06-07 08:41:27 +0200

    add function to query fonts

--- src/vs/base/browser/fonts.ts
+++ src/vs/base/browser/fonts.ts
@@ -3,6 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+import { mainWindow } from 'vs/base/browser/window';
 import { isMacintosh, isWindows } from 'vs/base/common/platform';
 
 /**
@@ -14,3 +15,19 @@ import { isMacintosh, isWindows } from 'vs/base/common/platform';
  * Note: this currently does not adjust for different locales.
  */
 export const DEFAULT_FONT_FAMILY = isWindows ? '"Segoe WPC", "Segoe UI", sans-serif' : isMacintosh ? '-apple-system, BlinkMacSystemFont, sans-serif' : 'system-ui, "Ubuntu", "Droid Sans", sans-serif';
+
+interface FontData {
+	readonly family: string;
+}
+
+export const getFonts = async (): Promise<readonly string[]> => {
+	try {
+		// @ts-ignore
+		const fonts = mainWindow.queryLocalFonts() as FontData[];
+		const families = fonts.map(font => font.family);
+		return families;
+	} catch (error) {
+		console.error(`Failed to query fonts: ${error}`);
+		return [];
+	}
+};
