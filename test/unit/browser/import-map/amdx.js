44c9e0d1679ddb3fe7a01bb747487f32c7269b54
Simon Siefke <simon.siefke@gmail.com>
2024-06-09 17:11:31 +0200

    format code

--- test/unit/browser/import-map/amdx.js
+++ test/unit/browser/import-map/amdx.js
@@ -3,45 +3,45 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-export const root = new URL('../../../../', import.meta.url).toString().slice(0, -1)
+export const root = new URL('../../../../', import.meta.url).toString().slice(0, -1);
 
 const getResult = defineCall => {
 	if (typeof defineCall.callback === 'function') {
-		return defineCall.callback([])
+		return defineCall.callback([]);
 	}
-	return defineCall.callback
-}
+	return defineCall.callback;
+};
 
 export const importAmdModule = async (absolutePath) => {
-	const defineCalls = []
+	const defineCalls = [];
 	globalThis.define = (id, dependencies, callback) => {
 		if (typeof id !== 'string') {
-			callback = dependencies
-			dependencies = id
-			id = null
+			callback = dependencies;
+			dependencies = id;
+			id = null;
 		}
 		if (typeof dependencies !== 'object' || !Array.isArray(dependencies)) {
-			callback = dependencies
-			dependencies = null
+			callback = dependencies;
+			dependencies = null;
 		}
 		defineCalls.push({
 			id, dependencies, callback
-		})
-	}
+		});
+	};
 
 	globalThis.define.amd = true;
-	globalThis.exports = Object.create(null)
-	await import(absolutePath)
+	globalThis.exports = Object.create(null);
+	await import(absolutePath);
 	if (Object.keys(globalThis.exports).length > 0) {
-		return globalThis.exports
+		return globalThis.exports;
 	}
 	if (defineCalls.length === 0) {
-		throw new Error('no module was defined')
+		throw new Error('no module was defined');
 	}
-	const defineCall = defineCalls.pop()
+	const defineCall = defineCalls.pop();
 	if (Array.isArray(defineCall.dependencies) && defineCall.dependencies.length > 0) {
-		throw new Error(`dependencies are not supported`)
+		throw new Error(`dependencies are not supported`);
 	}
-	const result = getResult(defineCall)
-	return result
-}
+	const result = getResult(defineCall);
+	return result;
+};
--- test/unit/browser/import-map/xterm.js
+++ test/unit/browser/import-map/xterm.js
@@ -2,10 +2,10 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-import { importAmdModule, root } from './amdx.js'
+import { importAmdModule, root } from './amdx.js';
 
 const module = await importAmdModule(
 	`${root}/node_modules/@xterm/xterm/lib/xterm.js`,
-)
+);
 
-export default module
+export default module;
