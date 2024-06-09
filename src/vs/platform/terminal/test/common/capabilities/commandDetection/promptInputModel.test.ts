1720e55c82d811a2e7ad4328289d37108690b187
Simon Siefke <simon.siefke@gmail.com>
2024-06-09 09:32:03 +0200

    undo xterm import change

--- src/vs/platform/terminal/test/common/capabilities/commandDetection/promptInputModel.test.ts
+++ src/vs/platform/terminal/test/common/capabilities/commandDetection/promptInputModel.test.ts
@@ -4,7 +4,8 @@
  *--------------------------------------------------------------------------------------------*/
 
 // eslint-disable-next-line local/code-import-patterns, local/code-amd-node-module
-import xterm from '@xterm/headless';
+import { Terminal } from '@xterm/headless';
+
 import { ensureNoDisposablesAreLeakedInTestSuite } from 'vs/base/test/common/utils';
 import { NullLogService } from 'vs/platform/log/common/log';
 import { PromptInputModel, type IPromptInputModelState } from 'vs/platform/terminal/common/capabilities/commandDetection/promptInputModel';
@@ -13,13 +14,11 @@ import type { ITerminalCommand } from 'vs/platform/terminal/common/capabilities/
 import { notDeepStrictEqual, strictEqual } from 'assert';
 import { timeout } from 'vs/base/common/async';
 
-const { Terminal } = xterm;
-
 suite('PromptInputModel', () => {
 	const store = ensureNoDisposablesAreLeakedInTestSuite();
 
 	let promptInputModel: PromptInputModel;
-	let xterm: xterm.Terminal;
+	let xterm: Terminal;
 	let onCommandStart: Emitter<ITerminalCommand>;
 	let onCommandExecuted: Emitter<ITerminalCommand>;
 
