a7a94fbdbdf34b88db92e4852b22060dfaf3a255
Robo <hop2deep@gmail.com>
2024-06-10 17:54:19 +0900

    fix: force software gl backend for snap (#214549)
    * fix: force software gl backend for snap
    
    Refs https://github.com/microsoft/vscode/issues/212494
    
    When using the default ANGLE gl/gles backend, v1.90 attempts
    to get display refresh rate for vsync from an internal implementation
    instead of using the gl extension EGL_CHROMIUM_sync_control,
    this eventually ends up loading display info from XRandr and chromium
    attempts to set a label for the data from the shipped resource pak
    file that is not accessible to the gpu process triggering a SIGTRAP
    in ui::ResourceBundle::GetSharedInstance.
    
    * fix: snap packaging for argument parsing (#214560)
    
    * chore: add use-gl and use-angle to known chromium flags

--- src/vs/platform/environment/common/argv.ts
+++ src/vs/platform/environment/common/argv.ts
@@ -145,4 +145,6 @@ export interface NativeParsedArgs {
 	'trace-startup-format'?: string;
 	'trace-startup-file'?: string;
 	'trace-startup-duration'?: string;
+	'use-gl'?: string;
+	'use-angle'?: string;
 }
