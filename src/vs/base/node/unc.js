e88c703d09cc03ab26dc5f864a1606a0706f61f1
Benjamin Pasero <benjamin.pasero@microsoft.com>
2024-06-18 20:12:00 +0200

    fix require

--- src/vs/base/node/unc.js
+++ src/vs/base/node/unc.js
@@ -3,6 +3,8 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+/// <reference path="../../../typings/require.d.ts" />
+
 //@ts-check
 'use strict';
 
