c3515864b9461e6ddce5137853f90fff7e47c865
Matt Bierner <matb@microsoft.com>
2024-06-18 09:08:04 -0700

    Enable use strict on `unc.js` (#216515)
    Fixes #216424
    
    Reveals a few errors too that should be fixed by future changes

--- src/vs/base/node/unc.js
+++ src/vs/base/node/unc.js
@@ -3,9 +3,8 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-'use strict';
-
 //@ts-check
+'use strict';
 
 (function () {
 	function factory() {
