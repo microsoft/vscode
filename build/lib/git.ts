b22e95825694d465d1e8efc9294ef3816501f104
Juan Campa <juancampa@gmail.com>
2024-06-19 13:39:59 -0400

    Fix build as a submodule (.git folder is hoisted)

--- build/lib/git.ts
+++ build/lib/git.ts
@@ -9,7 +9,23 @@ import * as fs from 'fs';
  * Returns the sha1 commit version of a repository or undefined in case of failure.
  */
 export function getVersion(repo: string): string | undefined {
-	const git = path.join(repo, '.git');
+	// MEMBRANE: make `getVersion` work when vscode is a submodule and it's .git was hoisted.
+	const maybeGit = path.join(repo, '.git');
+	const stat = fs.statSync(maybeGit);
+	let git: string;
+	if (stat.isFile()) {
+		const data = fs.readFileSync(maybeGit, 'utf8');
+		const gitdir = data.match(/^gitdir: (.*)$/m)?.[1];
+		if (!gitdir) {
+			throw new Error(`Failed to parse .git submodule info in ${maybeGit}`);
+		}
+		git = path.join(repo, gitdir);
+	} else if (stat.isDirectory()) {
+		git = maybeGit;
+	} else {
+		return undefined;
+	}
+
 	const headPath = path.join(git, 'HEAD');
 	let head: string;
 
