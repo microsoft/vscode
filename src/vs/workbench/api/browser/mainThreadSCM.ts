bf74ad22b47ce51f23877ea92f02fe3357b1863a
Ladislau Szomoru <3372902+lszomoru@users.noreply.github.com>
2024-06-19 14:12:58 +0200

    SCM - keep logging but switch the trace level (#216611)

--- src/vs/workbench/api/browser/mainThreadSCM.ts
+++ src/vs/workbench/api/browser/mainThreadSCM.ts
@@ -28,7 +28,6 @@ import { ITextModelContentProvider, ITextModelService } from 'vs/editor/common/s
 import { Schemas } from 'vs/base/common/network';
 import { ITextModel } from 'vs/editor/common/model';
 import { ILogService } from 'vs/platform/log/common/log';
-import { IWorkbenchEnvironmentService } from 'vs/workbench/services/environment/common/environmentService';
 
 function getIconFromIconDto(iconDto?: UriComponents | { light: UriComponents; dark: UriComponents } | ThemeIcon): URI | { light: URI; dark: URI } | ThemeIcon | undefined {
 	if (iconDto === undefined) {
@@ -273,7 +272,6 @@ class MainThreadSCMProvider implements ISCMProvider, QuickDiffProvider {
 		private readonly _quickDiffService: IQuickDiffService,
 		private readonly _uriIdentService: IUriIdentityService,
 		private readonly _workspaceContextService: IWorkspaceContextService,
-		private readonly _environmentService: IWorkbenchEnvironmentService,
 		private readonly _logService: ILogService
 	) {
 		if (_rootUri) {
@@ -299,9 +297,7 @@ class MainThreadSCMProvider implements ISCMProvider, QuickDiffProvider {
 		}
 
 		if (typeof features.statusBarCommands !== 'undefined') {
-			if (this._environmentService.enableSmokeTestDriver) {
-				this._logService.info(`MainThreadSCMProvider#updateSourceControl (${this._id}): ${features.statusBarCommands.map(c => c.title).join(', ')}`);
-			}
+			this._logService.trace(`MainThreadSCMProvider#updateSourceControl (${this._id}): ${features.statusBarCommands.map(c => c.title).join(', ')}`);
 			this._statusBarCommands.set(features.statusBarCommands, undefined);
 		}
 
@@ -483,7 +479,6 @@ export class MainThreadSCM implements MainThreadSCMShape {
 		@IQuickDiffService private readonly quickDiffService: IQuickDiffService,
 		@IUriIdentityService private readonly _uriIdentService: IUriIdentityService,
 		@IWorkspaceContextService private readonly workspaceContextService: IWorkspaceContextService,
-		@IWorkbenchEnvironmentService private readonly environmentService: IWorkbenchEnvironmentService,
 		@ILogService private readonly logService: ILogService
 	) {
 		this._proxy = extHostContext.getProxy(ExtHostContext.ExtHostSCM);
@@ -505,7 +500,7 @@ export class MainThreadSCM implements MainThreadSCMShape {
 		this._repositoryBarriers.set(handle, new Barrier());
 
 		const inputBoxTextModelRef = await this.textModelService.createModelReference(URI.revive(inputBoxDocumentUri));
-		const provider = new MainThreadSCMProvider(this._proxy, handle, id, label, rootUri ? URI.revive(rootUri) : undefined, inputBoxTextModelRef.object.textEditorModel, this.quickDiffService, this._uriIdentService, this.workspaceContextService, this.environmentService, this.logService);
+		const provider = new MainThreadSCMProvider(this._proxy, handle, id, label, rootUri ? URI.revive(rootUri) : undefined, inputBoxTextModelRef.object.textEditorModel, this.quickDiffService, this._uriIdentService, this.workspaceContextService, this.logService);
 		const repository = this.scmService.registerSCMProvider(provider);
 		this._repositories.set(handle, repository);
 
