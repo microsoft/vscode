b10f6a106e46ee9f7c0dccb04a3ea964ad98f1b1
Rob Lourens <roblourens@gmail.com>
2024-06-20 15:29:55 -0700

    Add lmTools API version

--- src/vs/platform/extensions/common/extensionsApiProposals.ts
+++ src/vs/platform/extensions/common/extensionsApiProposals.ts
@@ -224,6 +224,7 @@ const _allApiProposals = {
 	},
 	lmTools: {
 		proposal: 'https://raw.githubusercontent.com/microsoft/vscode/main/src/vscode-dts/vscode.proposed.lmTools.d.ts',
+		version: 2
 	},
 	mappedEditsProvider: {
 		proposal: 'https://raw.githubusercontent.com/microsoft/vscode/main/src/vscode-dts/vscode.proposed.mappedEditsProvider.d.ts',
