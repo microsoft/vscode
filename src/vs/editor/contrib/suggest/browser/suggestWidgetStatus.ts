09bfb6411e6e1834866bf7cf2f0c14be02b6cad7
Johannes <johannes.rieken@gmail.com>
2024-06-21 12:09:59 +0200

    * fixes https://github.com/microsoft/vscode/issues/216692 * make text-btn with command but with a little color

--- src/vs/editor/contrib/suggest/browser/suggestWidgetStatus.ts
+++ src/vs/editor/contrib/suggest/browser/suggestWidgetStatus.ts
@@ -30,7 +30,7 @@ export class SuggestWidgetStatus {
 		this.element = dom.append(container, dom.$('.suggest-status-bar'));
 
 		const actionViewItemProvider = <IActionViewItemProvider>(action => {
-			return action instanceof MenuItemAction ? instantiationService.createInstance(TextOnlyMenuEntryActionViewItem, action, undefined) : undefined;
+			return action instanceof MenuItemAction ? instantiationService.createInstance(TextOnlyMenuEntryActionViewItem, action, { useComma: true }) : undefined;
 		});
 		this._leftActions = new ActionBar(this.element, { actionViewItemProvider });
 		this._rightActions = new ActionBar(this.element, { actionViewItemProvider });
