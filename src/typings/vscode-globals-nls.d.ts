4f089a8b903986838fc6159df83e222e624a182d
Benjamin Pasero <benjamin.pasero@microsoft.com>
2024-06-24 14:23:51 +0200

    jsdoc

--- src/typings/vscode-globals-nls.d.ts
+++ src/typings/vscode-globals-nls.d.ts
@@ -5,11 +5,23 @@
 
 // AMD2ESM mirgation relevant
 
+/**
+ * NLS Globals:
+ *
+ * Every entry point (electron-main, electron-sandbox, node.js,
+ * workers, monaco editor, utility process) that uses `localize`
+ * or `localize2` must include bootstrap code to fill in below
+ * globals before executing. That is because during build time
+ * we strip out all english strings from the resulting JS code
+ * and replace it with a <number> that is then looked up from
+ * the `_VSCODE_NLS_MESSAGES` array.
+ */
 declare global {
 
 	/**
 	 * All NLS messages produced by `localize` and `localize2` calls
-	 * under `src/vs`.
+	 * under `src/vs` translated to the language as indicated by
+	 * `_VSCODE_NLS_LANGUAGE`.
 	 */
 	var _VSCODE_NLS_MESSAGES: string[];
 	/**
