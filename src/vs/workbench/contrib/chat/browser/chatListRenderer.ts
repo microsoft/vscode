89675a91874dcb5d80cb5b8bf862dac64c47601f
Rob Lourens <roblourens@gmail.com>
2024-06-25 17:54:13 -0700

    Still fix incomplete markdown when the response is complete but still streaming Fix #217915

--- src/vs/workbench/contrib/chat/browser/chatListRenderer.ts
+++ src/vs/workbench/contrib/chat/browser/chatListRenderer.ts
@@ -822,7 +822,7 @@ export class ChatListItemRenderer extends Disposable implements ITreeRenderer<Ch
 
 	private renderMarkdown(markdown: IMarkdownString, templateData: IChatListItemTemplate, context: IChatContentPartRenderContext): IChatContentPart {
 		const element = context.element;
-		const fillInIncompleteTokens = isResponseVM(element) && (!element.isComplete || element.isCanceled || element.errorDetails?.responseIsFiltered || element.errorDetails?.responseIsIncomplete);
+		const fillInIncompleteTokens = isResponseVM(element) && (!element.isComplete || element.isCanceled || element.errorDetails?.responseIsFiltered || element.errorDetails?.responseIsIncomplete || !!element.renderData);
 		const codeBlockStartIndex = context.preceedingContentParts.reduce((acc, part) => acc + (part instanceof ChatMarkdownContentPart ? part.codeblocks.length : 0), 0);
 		const markdownPart = this.instantiationService.createInstance(ChatMarkdownContentPart, markdown, context, this._editorPool, fillInIncompleteTokens, codeBlockStartIndex, this.renderer, this._currentLayoutWidth, this.codeBlockModelCollection, this.rendererOptions);
 		markdownPart.addDisposable(markdownPart.onDidChangeHeight(() => {
