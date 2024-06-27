4c0517e1409deae7bf9568af673caab628c61f2e
Connor Peet <connor@peet.io>
2024-06-27 12:35:29 -0700

    net: gracefully disconnect better from remote
    This adds in a step to gracefully disconnect (send a "disconnect"
    message and then await the flush) before closing the workbench. In some
    cases, like Remote - SSH, sending messages is more async than others. In
    the exec server connection we handle a `zlib.flate` stream to compress
    data to and from the VS Code server, and its API is asynchronous, so
    this lets us ensure the stream is drained before giving the go-ahead
    to close up shop
    
    This lifecycle phase is a little awkward and I ended it putting it
    directly in the lifecycle service: we don't want to do this in
    `onWillShutdown` because some contributions want to save data to the
    remote workspace, and if shutdown is vetoed it would leave a broken
    state. But `onDidShutdown` is synchronous and already depended upon by
    several other points, and changing that also felt a bit risky.
    
    cc @alexdima
    
    Refs #211462, will require some small adoption in Remote - SSH to close.

--- src/vs/workbench/test/browser/workbenchTestServices.ts
+++ src/vs/workbench/test/browser/workbenchTestServices.ts
@@ -2117,6 +2117,7 @@ export class TestRemoteAgentService implements IRemoteAgentService {
 	async logTelemetry(eventName: string, data?: ITelemetryData): Promise<void> { }
 	async flushTelemetry(): Promise<void> { }
 	async getRoundTripTime(): Promise<number | undefined> { return undefined; }
+	async endConnection(): Promise<void> { }
 }
 
 export class TestRemoteExtensionsScannerService implements IRemoteExtensionsScannerService {
