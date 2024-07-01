c642a1f28d8751c3d8a6436ab4644268668039e9
Henning Dieterichs <hdieterichs@microsoft.com>
2024-07-01 19:27:00 +0200

    Fixes wrapInReloadableClass* functions.

--- src/vs/platform/observable/common/wrapInReloadableClass.ts
+++ src/vs/platform/observable/common/wrapInReloadableClass.ts
@@ -6,44 +6,57 @@ import { isHotReloadEnabled } from 'vs/base/common/hotReload';
 import { readHotReloadableExport } from 'vs/base/common/hotReloadHelpers';
 import { IDisposable } from 'vs/base/common/lifecycle';
 import { autorunWithStore } from 'vs/base/common/observable';
-import { IInstantiationService } from 'vs/platform/instantiation/common/instantiation';
+import { BrandedService, GetLeadingNonServiceArgs, IInstantiationService } from 'vs/platform/instantiation/common/instantiation';
 
 /**
  * Wrap a class in a reloadable wrapper.
  * When the wrapper is created, the original class is created.
  * When the original class changes, the instance is re-created.
 */
-export function wrapInReloadableClass(getClass: () => (new (...args: any[]) => any)): (new (...args: any[]) => any) {
-	// Disables this function as it does not work.
-	if (1 === 1) {
-		// TODO@hediet fix this asap.
-		return getClass();
-	}
-	if (!isHotReloadEnabled()) {
-		return getClass();
-	}
+export function wrapInReloadableClass0<TArgs extends BrandedService[]>(getClass: () => Result<TArgs>): Result<GetLeadingNonServiceArgs<TArgs>> {
+	return !isHotReloadEnabled() ? getClass() : createWrapper(getClass, BaseClass0);
+}
 
-	return class ReloadableWrapper extends BaseClass {
+type Result<TArgs extends any[]> = new (...args: TArgs) => IDisposable;
+
+class BaseClass {
+	constructor(
+		protected readonly instantiationService: IInstantiationService,
+	) { }
+
+	init(...params: any[]): void { }
+}
+
+function createWrapper<T extends any[]>(getClass: () => any, B: new (...args: T) => BaseClass) {
+	return (class ReloadableWrapper extends B {
 		private _autorun: IDisposable | undefined = undefined;
 
-		override init() {
+		override init(...params: any[]) {
 			this._autorun = autorunWithStore((reader, store) => {
 				const clazz = readHotReloadableExport(getClass(), reader);
-				store.add(this.instantiationService.createInstance(clazz));
+				store.add(this.instantiationService.createInstance(clazz as any, ...params) as IDisposable);
 			});
 		}
 
 		dispose(): void {
 			this._autorun?.dispose();
 		}
-	};
+	}) as any;
 }
-class BaseClass {
-	constructor(
-		@IInstantiationService protected readonly instantiationService: IInstantiationService
-	) {
-		this.init();
-	}
 
-	init(): void { }
+class BaseClass0 extends BaseClass {
+	constructor(@IInstantiationService i: IInstantiationService) { super(i); this.init(); }
+}
+
+/**
+ * Wrap a class in a reloadable wrapper.
+ * When the wrapper is created, the original class is created.
+ * When the original class changes, the instance is re-created.
+*/
+export function wrapInReloadableClass1<TArgs extends [any, ...BrandedService[]]>(getClass: () => Result<TArgs>): Result<GetLeadingNonServiceArgs<TArgs>> {
+	return !isHotReloadEnabled() ? getClass() as any : createWrapper(getClass, BaseClass1);
+}
+
+class BaseClass1 extends BaseClass {
+	constructor(param1: any, @IInstantiationService i: IInstantiationService,) { super(i); this.init(param1); }
 }
