75dda164bb3169847a724d88f6bd97b962df2360
Simon Siefke <simon.siefke@gmail.com>
2024-07-04 19:36:01 +0200

    update fallback

--- src/vs/platform/native/electron-main/nativeHostMainService.ts
+++ src/vs/platform/native/electron-main/nativeHostMainService.ts
@@ -500,6 +500,7 @@ export class NativeHostMainService extends Disposable implements INativeHostMain
 				}
 			});
 		} catch {
+			// fall back to open url using electron shell
 			shell.openExternal(url);
 		}
 	}
