fec8c055ed10e8daa5c655ce338365bbabfd7d06
Benjamin Pasero <benjamin.pasero@microsoft.com>
2024-07-08 18:20:01 +0200

    can use `assert` now

--- src/vs/platform/environment/node/argvHelper.ts
+++ src/vs/platform/environment/node/argvHelper.ts
@@ -3,22 +3,11 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+import assert from 'assert';
 import { IProcessEnvironment } from 'vs/base/common/platform';
 import { localize } from 'vs/nls';
 import { NativeParsedArgs } from 'vs/platform/environment/common/argv';
 import { ErrorReporter, NATIVE_CLI_COMMANDS, OPTIONS, parseArgs } from 'vs/platform/environment/node/argv';
-
-// ESM-comment-begin
-import assert from 'assert';
-// ESM-comment-end
-// ESM-uncomment-begin
-// function assert(condition: boolean, message: string) {
-// 	if (!condition) {
-// 		throw new Error(message);
-// 	}
-// }
-// ESM-uncomment-end
-
 
 function parseAndValidate(cmdLineArgs: string[], reportWarnings: boolean): NativeParsedArgs {
 	const onMultipleValues = (id: string, val: string) => {
