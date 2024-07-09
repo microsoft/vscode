0ca4c6c3ceb2c52bab718767855df16479bd696c
Benjamin Pasero <benjamin.pasero@microsoft.com>
2024-07-09 08:38:34 +0200

    bad mere

--- src/bootstrap-meta.js
+++ src/bootstrap-meta.js
@@ -18,17 +18,17 @@
 // ESM-uncomment-end
 
 /** @type Partial<IProductConfiguration> & { BUILD_INSERT_PRODUCT_CONFIGURATION?: string } */
-module.exports.product = { BUILD_INSERT_PRODUCT_CONFIGURATION: 'BUILD_INSERT_PRODUCT_CONFIGURATION' }; // DO NOT MODIFY, PATCHED DURING BUILD
-if (module.exports.product['BUILD_INSERT_PRODUCT_CONFIGURATION']) {
+let product = { BUILD_INSERT_PRODUCT_CONFIGURATION: 'BUILD_INSERT_PRODUCT_CONFIGURATION' }; // DO NOT MODIFY, PATCHED DURING BUILD
+if (product['BUILD_INSERT_PRODUCT_CONFIGURATION']) {
 	// @ts-ignore
-	module.exports.product = require('../product.json'); // Running out of sources
+	product = require('../product.json'); // Running out of sources
 }
 
 /** @type object & { BUILD_INSERT_PACKAGE_CONFIGURATION?: string } */
-module.exports.pkg = { BUILD_INSERT_PACKAGE_CONFIGURATION: 'BUILD_INSERT_PACKAGE_CONFIGURATION' }; // DO NOT MODIFY, PATCHED DURING BUILD
-if (module.exports.pkg['BUILD_INSERT_PACKAGE_CONFIGURATION']) {
+let pkg = { BUILD_INSERT_PACKAGE_CONFIGURATION: 'BUILD_INSERT_PACKAGE_CONFIGURATION' }; // DO NOT MODIFY, PATCHED DURING BUILD
+if (pkg['BUILD_INSERT_PACKAGE_CONFIGURATION']) {
 	// @ts-ignore
-	module.exports.pkg = require('../package.json'); // Running out of sources
+	pkg = require('../package.json'); // Running out of sources
 }
 
 module.exports.product = product;
