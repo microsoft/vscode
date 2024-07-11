957cc7306068f8ea1d67ebcb87eb0f872d6dc9c0
Daniel Imms <2193314+Tyriar@users.noreply.github.com>
2024-07-11 16:04:59 -0700

    Disable whole suite

--- extensions/vscode-api-tests/src/singlefolder-tests/terminal.shellIntegration.test.ts
+++ extensions/vscode-api-tests/src/singlefolder-tests/terminal.shellIntegration.test.ts
@@ -10,7 +10,8 @@ import { assertNoRpc } from '../utils';
 
 // Terminal integration tests are disabled on web https://github.com/microsoft/vscode/issues/92826
 // Windows images will often not have functional shell integration
-(env.uiKind === UIKind.Web || platform() === 'win32' ? suite.skip : suite)('vscode API - Terminal.shellIntegration', () => {
+// TODO: Linux https://github.com/microsoft/vscode/issues/221399
+(env.uiKind === UIKind.Web || platform() === 'win32' || platform() === 'linux' ? suite.skip : suite)('vscode API - Terminal.shellIntegration', () => {
 	const disposables: Disposable[] = [];
 
 	suiteSetup(async () => {
@@ -196,8 +197,7 @@ import { assertNoRpc } from '../utils';
 		await closeTerminalAsync(terminal);
 	});
 
-	// TODO: https://github.com/microsoft/vscode/issues/221399
-	(platform() === 'linux' ? test.skip : test)('executeCommand(commandLine)', async () => {
+	test('executeCommand(commandLine)', async () => {
 		const { terminal, shellIntegration } = await createTerminalAndWaitForShellIntegration();
 		const { execution, endEvent } = executeCommandAsync(shellIntegration, 'echo hello');
 		const executionSync = await execution;
@@ -212,8 +212,7 @@ import { assertNoRpc } from '../utils';
 		await closeTerminalAsync(terminal);
 	});
 
-	// TODO: https://github.com/microsoft/vscode/issues/221399
-	(platform() === 'linux' ? test.skip : test)('executeCommand(executable, args)', async () => {
+	test('executeCommand(executable, args)', async () => {
 		const { terminal, shellIntegration } = await createTerminalAndWaitForShellIntegration();
 		const { execution, endEvent } = executeCommandAsync(shellIntegration, 'echo', ['hello']);
 		const executionSync = await execution;
