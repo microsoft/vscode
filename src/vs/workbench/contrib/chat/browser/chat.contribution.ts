665dd6371d7d568341ed4c2a9386e9b317265024
Johannes Rieken <johannes.rieken@gmail.com>
2024-07-11 17:10:18 +0200

    enable `chat.experimental.variables.editor` by default (#221471)
    https://github.com/microsoft/vscode-copilot/issues/6622
    
    fyi @aeschli

--- src/vs/workbench/contrib/chat/browser/chat.contribution.ts
+++ src/vs/workbench/contrib/chat/browser/chat.contribution.ts
@@ -104,7 +104,7 @@ configurationRegistry.registerConfiguration({
 		'chat.experimental.variables.editor': {
 			type: 'boolean',
 			description: nls.localize('chat.experimental.variables.editor', "Enables variables for editor chat."),
-			default: false
+			default: true
 		},
 		'chat.experimental.variables.notebook': {
 			type: 'boolean',
