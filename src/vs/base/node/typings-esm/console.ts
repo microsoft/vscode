151f81941cf4a6aadd5544fd1177dc78d325837a
Johannes <johannes.rieken@gmail.com>
2024-07-12 16:35:58 +0200

    Revert "remove `typings-esm` because they aren't needed anymore"
    This reverts commit 10417ae0fd0a9741d4044066506066077b8cfe7c.

---
+++ src/vs/base/node/typings-esm/console.ts
@@ -0,0 +1,35 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+// ESM-comment-begin
+import * as console from 'console';
+// ESM-comment-end
+
+// ESM-uncomment-begin
+// const console = globalThis._VSCODE_NODE_MODULES.console;
+// ESM-uncomment-end
+
+export const assert = console.assert;
+export const clear = console.clear;
+export const count = console.count;
+export const countReset = console.countReset;
+export const debug = console.debug;
+export const dir = console.dir;
+export const dirxml = console.dirxml;
+export const error = console.error;
+export const group = console.group;
+export const groupCollapsed = console.groupCollapsed;
+export const groupEnd = console.groupEnd;
+export const info = console.info;
+export const log = console.log;
+export const profile = console.profile;
+export const profileEnd = console.profileEnd;
+export const table = console.table;
+export const time = console.time;
+export const timeEnd = console.timeEnd;
+export const timeLog = console.timeLog;
+export const timeStamp = console.timeStamp;
+export const trace = console.trace;
+export const warn = console.warn;
