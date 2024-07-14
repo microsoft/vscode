a9be17b7e366e9926013b36b4f2858acd38eac42
Himanshu Singh Chauhan <him.speedo@gmail.com>
2024-07-14 08:51:46 +0530

    fix detached head after submodule init (#159)
    * win.setup.ps1: fix-detached-head-after-submodule-init
    
    * unix.setup.sh fix symlink create order

--- scripts/pearai/setup-environment.ps1
+++ scripts/pearai/setup-environment.ps1
@@ -19,15 +19,36 @@ function Initialize-BaseFunctionality {
     Write-Host "`nInitializing sub-modules..." -ForegroundColor White
     Invoke-CMD -Command "git submodule update --init --recursive" -ErrorMessage "Failed to initialize git submodules"
 
+    Create-SymLink
+
     Set-Location .\extensions\pearai-submodule
+
+    # Checkout main - because submodule update leaves detached head.
+    Write-Host "`nSetting the submodule directory to match origin/main's latest changes..." -ForegroundColor White
+    Invoke-CMD -Command "git reset origin/main" -ErrorMessage "Failed to git reset to origin/main"
+    Invoke-CMD -Command "git reset --hard" -ErrorMessage "Failed to reset --hard"
+    Write-Host "`nChecking out the 'main' branch in submodule..." -ForegroundColor White
+    Invoke-CMD -Command "git checkout main" -ErrorMessage "Failed to checkout the 'main' branch in the submodule"
+    Invoke-CMD -Command "git fetch origin" -ErrorMessage "Failed to fetch latest changes from origin"
+    Invoke-CMD -Command "git pull origin main" -ErrorMessage "Failed to pull latest changes from origin/main"
+
 
 	$script = Join-Path -Path $modulePath -ChildPath 'scripts\install-dependencies.ps1'
     Invoke-CMD -Command "powershell.exe -ExecutionPolicy Bypass -File $script" -ErrorMessage "Failed to install dependencies for the submodule"
+
+    # Discard the package.json and package-lock.json version update changes     
+    Invoke-CMD -Command "git reset --hard" -ErrorMessage "Failed to reset --hard after submodule dependencies install"
 
     Set-Location $currentDir
 
     Write-Host "`nSetting up root application..." -ForegroundColor White
     Invoke-CMD -Command "yarn install" -ErrorMessage "Failed to install dependencies with yarn"
+}
+
+function Create-SymLink {
+    Write-Host "`nCreating symbolic link 'extensions\pearai-submodule\extensions\vscode' -> 'extensions\pearai-extension'" -ForegroundColor White
+    Start-Process powershell.exe -Verb RunAs -ArgumentList ("-ExecutionPolicy Bypass ", "-Command", "powershell.exe -ExecutionPolicy Bypass -File '$createLinkScript' '$targetPath' '$linkPath'")
+    Start-Sleep 1
 }
 
 # Setup all necessary paths for this script
@@ -36,12 +57,6 @@ $modulePath = Join-Path -Path $currentDir -ChildPath 'extensions\pearai-submodul
 $targetPath = Join-Path -Path $modulePath -ChildPath 'extensions\vscode'
 $linkPath = Join-Path -Path $currentDir -ChildPath 'extensions\pearai-ref'
 $createLinkScript = Join-Path -Path (Get-Item $MyInvocation.MyCommand.Path).Directory -ChildPath 'create-symlink.ps1'
-
-# Check if the symbolic link exists
-
-Write-Host "`nCreating symbolic link 'extensions\pearai-submodule\extensions\vscode' -> 'extensions\pearai-extension'" -ForegroundColor White
-Start-Process powershell.exe -Verb RunAs -ArgumentList ("-ExecutionPolicy Bypass ", "-Command", "powershell.exe -ExecutionPolicy Bypass -File '$createLinkScript' '$targetPath' '$linkPath'")
-Start-Sleep 1
 
 # Run the base functionality
 Initialize-BaseFunctionality
