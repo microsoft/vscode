ec6e30180adabaa3194e906f81752d608c46aaf1
Nang <nathanang2000@gmail.com>
2024-07-15 18:04:04 +0800

    Added capitalization to walkthrough (#184)

--- src/vs/workbench/contrib/welcomeGettingStarted/common/gettingStartedContent.ts
+++ src/vs/workbench/contrib/welcomeGettingStarted/common/gettingStartedContent.ts
@@ -170,7 +170,7 @@ const Button = (title: string, href: string) => `[${title}](${href})`;
 export const walkthroughs: GettingStartedWalkthroughContent = [
 	{
 		id: 'Setup',
-		title: localize('gettingStarted.setup.title', "Get Started with the PearAI editor"),
+		title: localize('gettingStarted.setup.title', "Get Started with the PearAI Editor"),
 		description: localize('gettingStarted.setup.description', "Customize your editor, learn the basics, and start coding"),
 		isFeatured: true,
 		icon: setupIcon,
