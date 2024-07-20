d6ae4f6cf3184ce1e160e616e6f207798ac42e24
Kortin Zhou <50259158+kortin99@users.noreply.github.com>
2024-07-21 06:46:57 +0800

    feat(snippets): add support for kebab-case in snippets tmLanguage syntax

--- src/vs/editor/contrib/snippet/browser/snippetParser.ts
+++ src/vs/editor/contrib/snippet/browser/snippetParser.ts
@@ -387,6 +387,8 @@ export class FormatString extends Marker {
 			return !value ? '' : this._toPascalCase(value);
 		} else if (this.shorthandName === 'camelcase') {
 			return !value ? '' : this._toCamelCase(value);
+		} else if (this.shorthandName === 'kebabcase') {
+			return !value ? '' : this._toKebabCase(value);
 		} else if (Boolean(value) && typeof this.ifValue === 'string') {
 			return this.ifValue;
 		} else if (!Boolean(value) && typeof this.elseValue === 'string') {
@@ -394,6 +396,27 @@ export class FormatString extends Marker {
 		} else {
 			return value || '';
 		}
+	}
+
+	private _toKebabCase(value: string): string {
+		const match = value.match(/[a-z0-9]+/gi);
+		if (!match) {
+			return value;
+		}
+
+		if (!value.match(/[a-z0-9]/)) {
+			return value
+				.trim()
+				.toLowerCase()
+				.replace(/^_+|_+$/g, '')
+				.replace(/[\s_]+/g, '-')
+		}
+
+		return value
+			.trim()
+			.match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g)
+			.map(x => x.toLowerCase())
+			.join('-');
 	}
 
 	private _toPascalCase(value: string): string {
