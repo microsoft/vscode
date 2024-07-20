41d73f93f28f3dccacbf23d1ee27ab0208ade16e
Daniel Imms <2193314+Tyriar@users.noreply.github.com>
2024-07-20 12:13:43 -0700

    Remove unneeded file

--- src/vs/workbench/contrib/terminal/browser/media/GetGlobalCommands.ps1
+++
@@ -1,6 +0,0 @@
-function Compress-Completions($completions) {
-	$completions | ForEach-Object { ,@($_.CompletionText, $_.ResultType, $_.tooltip) }
-}
-
-$commands = [System.Management.Automation.CompletionCompleters]::CompleteCommand('')
-Compress-Completions($commands) | ConvertTo-Json -Compress
