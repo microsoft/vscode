da208e3d24ceda3037c7c3029c568a7dd2c9cba5
Daniel Imms <2193314+Tyriar@users.noreply.github.com>
2024-07-20 12:22:15 -0700

    Add fork license inline

--- src/vs/workbench/contrib/terminal/browser/media/GitTabExpansion.psm1
+++ src/vs/workbench/contrib/terminal/browser/media/GitTabExpansion.psm1
@@ -3,6 +3,29 @@
 #   Licensed under the MIT License. See License.txt in the project root for license information.
 # ---------------------------------------------------------------------------------------------
 
+# This is a fork of posh-git that has been modified to add additional features and custom VS Code
+# specific integrations.
+#
+# Copyright (c) 2010-2018 Keith Dahlby, Keith Hill, and contributors
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 # Need this variable as long as we support PS v2
 $ModuleBasePath = Split-Path $MyInvocation.MyCommand.Path -Parent
 
