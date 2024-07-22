1041a4b833e12f27c48f7b458985a2444856818a
TJ <crzytaz19@hotmail.com>
2024-07-22 05:02:47 -0400

    Update vscode.proposed.lmTools.d.ts

--- src/vscode-dts/vscode.proposed.lmTools.d.ts
+++ src/vscode-dts/vscode.proposed.lmTools.d.ts
@@ -3,89 +3,49 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-// version: 2
-// https://github.com/microsoft/vscode/issues/213274
-
 declare module 'vscode' {
 
-	// TODO@API capabilities
+	export namespace chat {
+		/**
+		 * TODO@API also statically registered
+		 * TODO@API ?? canBeInvokedExplicitlyByUser: whether the tool shows up in the chat input suggest widget
+		 */
+		export function registerTool(tool: ChatTool, options: { canBeInvokedExplicitlyByUser: boolean }): Disposable;
 
-	export type JSONSchema = object;
+		export const tools: ReadonlyArray<ChatToolDescription>;
 
-	// API -> LM: an tool/function that is available to the language model
-	export interface LanguageModelChatFunction {
-		name: string;
+		/**
+		 * For non-chat AI actions to invoke tools arbitrarily
+		 * TODO@API is chat namespace right?
+		 */
+		export function invokeTool(toolId: string, parameters: Object, token: CancellationToken): Thenable<any>;
+	}
+
+	export interface ChatToolDescription {
+		id: string;
+		displayName: string;
 		description: string;
-		parametersSchema?: JSONSchema;
+		parametersSchema: any; // JSON schema
+
+		// TODO@API Is output only a string, or can it be structured data? Both?
+		// Does it stream?
+		returnValueSchema: any; // JSON schema
 	}
 
-	// API -> LM: add tools as request option
-	export interface LanguageModelChatRequestOptions {
-		// TODO@API this will a heterogeneous array of different types of tools
-		tools?: LanguageModelChatFunction[];
+	export interface ChatToolContext {
+		prompt: string;
 	}
 
-	// LM -> USER: function that should be used
-	export class LanguageModelChatResponseFunctionUsePart {
-		name: string;
-		parameters: any;
-
-		constructor(name: string, parameters: any);
+	// Are these just vscode commands with a schema for parameters?
+	export interface ChatTool extends ChatToolDescription {
+		// TODO@API Does it stream?
+		// How does it ask for confirmation? This resolver would get some other resolver/accessor object that lets it ask to render some confirm dialog in chat.
+		// Differences from variables: no 'level'
+		invoke(parameters: any, context: ChatToolContext, token: CancellationToken): ProviderResult<any>;
 	}
 
-	// LM -> USER: text chunk
-	export class LanguageModelChatResponseTextPart {
-		value: string;
-
-		constructor(value: string);
-	}
-
-	export interface LanguageModelChatResponse {
-
-		stream: AsyncIterable<LanguageModelChatResponseTextPart | LanguageModelChatResponseFunctionUsePart>;
-	}
-
-
-	// USER -> LM: the result of a function call
-	export class LanguageModelChatMessageFunctionResultPart {
-		name: string;
-		content: string;
-		isError: boolean;
-
-		constructor(name: string, content: string, isError?: boolean);
-	}
-
-	export interface LanguageModelChatMessage {
-		content2: string | LanguageModelChatMessageFunctionResultPart;
-	}
-
-	// Tool registration/invoking between extensions
-
-	export namespace lm {
-		/**
-		 * Register a LanguageModelTool. The tool must also be registered in the package.json `languageModelTools` contribution point.
-		 */
-		export function registerTool(name: string, tool: LanguageModelTool): Disposable;
-
-		/**
-		 * A list of all available tools.
-		 */
-		export const tools: ReadonlyArray<LanguageModelToolDescription>;
-
-		/**
-		 * Invoke a tool with the given parameters.
-		 */
-		export function invokeTool(name: string, parameters: Object, token: CancellationToken): Thenable<string>;
-	}
-
-	// Is the same as LanguageModelChatFunction now, but could have more details in the future
-	export interface LanguageModelToolDescription {
-		name: string;
-		description: string;
-		parametersSchema?: JSONSchema;
-	}
-
-	export interface LanguageModelTool {
-		invoke(parameters: any, token: CancellationToken): Thenable<string>;
+	// TODO@API name? "invoker"??
+	export interface ChatToolAccessor {
+		invokeTool(toolId: string, parameters: Object, token: CancellationToken): Thenable<any>;
 	}
 }
