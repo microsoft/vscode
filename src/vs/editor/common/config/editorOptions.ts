cfafb19c234675ea98618c683d9c95d3ee199eaf
Simon Siefke <simon.siefke@gmail.com>
2024-07-22 20:58:35 +0200

    feature: add option to disable overviewRuler

--- src/vs/editor/common/config/editorOptions.ts
+++ src/vs/editor/common/config/editorOptions.ts
@@ -218,6 +218,11 @@ export interface IEditorOptions {
 	 */
 	overviewRulerBorder?: boolean;
 	/**
+	 * Whether or not overviewRuler is enabled.
+	 * Defaults to `true`.
+	 */
+	overviewRulerEnabled?: boolean;
+	/**
 	 * Control the cursor animation style, possible values are 'blink', 'smooth', 'phase', 'expand' and 'solid'.
 	 * Defaults to 'blink'.
 	 */
@@ -5398,6 +5403,7 @@ export const enum EditorOption {
 	multiCursorLimit,
 	occurrencesHighlight,
 	overviewRulerBorder,
+	overviewRulerEnabled,
 	overviewRulerLanes,
 	padding,
 	pasteAs,
@@ -5914,6 +5920,10 @@ export const EditorOptions = {
 	overviewRulerBorder: register(new EditorBooleanOption(
 		EditorOption.overviewRulerBorder, 'overviewRulerBorder', true,
 		{ description: nls.localize('overviewRulerBorder', "Controls whether a border should be drawn around the overview ruler.") }
+	)),
+	overviewRulerEnabled: register(new EditorBooleanOption(
+		EditorOption.overviewRulerEnabled, 'overviewRulerEnabled', true,
+		{ description: nls.localize('overviewRulerEnabled', "Controls whether the overview ruler is enabled.") }
 	)),
 	overviewRulerLanes: register(new EditorIntOption(
 		EditorOption.overviewRulerLanes, 'overviewRulerLanes',
