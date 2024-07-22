f4d588261219b1e50cea751dde36254c775902eb
Aiday Marlen Kyzy <amarlenkyzy@microsoft.com>
2024-07-22 15:50:32 +0200

    adding css code

--- src/vs/editor/browser/controller/editContext/native/nativeEditContext.ts
+++ src/vs/editor/browser/controller/editContext/native/nativeEditContext.ts
@@ -3,6 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+import 'vs/css!./nativeEditContext';
 import { FastDomNode } from 'vs/base/browser/fastDomNode';
 import { StandardKeyboardEvent } from 'vs/base/browser/keyboardEvent';
 import { IDisposable } from 'vs/base/common/lifecycle';
