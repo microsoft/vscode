8508955ba5593073e5773bf8bf27421ffe61d3bd
Rob Lourens <roblourens@gmail.com>
2024-07-26 13:47:34 -0700

    Build

--- src/vs/workbench/contrib/chat/browser/contrib/chatDynamicVariables.ts
+++ src/vs/workbench/contrib/chat/browser/contrib/chatDynamicVariables.ts
@@ -46,7 +46,6 @@ export class ChatDynamicVariableModel extends Disposable implements IChatWidgetC
 		this._register(widget.inputEditor.onDidChangeModelContent(e => {
 			e.changes.forEach(c => {
 				// Don't mutate entries in _variables, since they will be returned from the getter
-				let didModifyState = false;
 				this._variables = coalesce(this._variables.map(ref => {
 					const intersection = Range.intersectRanges(ref.range, c.range);
 					if (intersection && !intersection.isEmpty()) {
@@ -59,11 +58,9 @@ export class ChatDynamicVariableModel extends Disposable implements IChatWidgetC
 								text: '',
 							}]);
 						}
-						didModifyState = true;
 						return null;
 					} else if (Range.compareRangesUsingStarts(ref.range, c.range) > 0) {
 						const delta = c.text.length - c.rangeLength;
-						didModifyState = true;
 						return {
 							...ref,
 							range: {
