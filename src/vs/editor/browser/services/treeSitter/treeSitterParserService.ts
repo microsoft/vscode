e23a9db6f353dfab4a5fba0a99897385d49481ec
Alex Ross <alros@microsoft.com>
2024-07-31 15:13:58 +0200

    Cannot parse treesitter telemetry (#224387)
    Fixes #224310

--- src/vs/editor/browser/services/treeSitter/treeSitterParserService.ts
+++ src/vs/editor/browser/services/treeSitter/treeSitterParserService.ts
@@ -74,6 +74,11 @@ export class TextModelTreeSitter extends Disposable {
 	}
 }
 
+const enum TelemetryParseType {
+	Full = 'fullParse',
+	Incremental = 'incrementalParse'
+}
+
 export class TreeSitterTree implements IDisposable {
 	private _tree: Parser.Tree | undefined;
 	private _isDisposed: boolean = false;
@@ -131,16 +136,14 @@ export class TreeSitterTree implements IDisposable {
 	}
 
 	private parse(model: ITextModel): Promise<Parser.Tree | undefined> {
-		let telemetryTag: string;
+		let parseType: TelemetryParseType = TelemetryParseType.Full;
 		if (this.tree) {
-			telemetryTag = 'incrementalParse';
-		} else {
-			telemetryTag = 'fullParse';
+			parseType = TelemetryParseType.Incremental;
 		}
-		return this._parseAndYield(model, telemetryTag);
+		return this._parseAndYield(model, parseType);
 	}
 
-	private async _parseAndYield(model: ITextModel, telemetryTag: string): Promise<Parser.Tree | undefined> {
+	private async _parseAndYield(model: ITextModel, parseType: TelemetryParseType): Promise<Parser.Tree | undefined> {
 		const language = model.getLanguageId();
 		let tree: Parser.Tree | undefined;
 		let time: number = 0;
@@ -163,7 +166,7 @@ export class TreeSitterTree implements IDisposable {
 				return;
 			}
 		} while (!tree);
-		this.sendParseTimeTelemetry(telemetryTag, language, time, passes);
+		this.sendParseTimeTelemetry(parseType, language, time, passes);
 		return tree;
 	}
 
@@ -171,8 +174,8 @@ export class TreeSitterTree implements IDisposable {
 		return textModel.getTextBuffer().getNearestChunk(index);
 	}
 
-	private sendParseTimeTelemetry(eventName: string, languageId: string, time: number, passes: number): void {
-		this._logService.debug(`Tree parsing (${eventName}) took ${time} ms and ${passes} passes.`);
+	private sendParseTimeTelemetry(parseType: TelemetryParseType, languageId: string, time: number, passes: number): void {
+		this._logService.debug(`Tree parsing (${parseType}) took ${time} ms and ${passes} passes.`);
 		type ParseTimeClassification = {
 			owner: 'alros';
 			comment: 'Used to understand how long it takes to parse a tree-sitter tree';
@@ -180,7 +183,11 @@ export class TreeSitterTree implements IDisposable {
 			time: { classification: 'SystemMetaData'; purpose: 'FeatureInsight'; comment: 'The ms it took to parse' };
 			passes: { classification: 'SystemMetaData'; purpose: 'FeatureInsight'; comment: 'The number of passes it took to parse' };
 		};
-		this._telemetryService.publicLog2<{ languageId: string; time: number; passes: number }, ParseTimeClassification>(`treeSitter.${eventName}`, { languageId, time, passes });
+		if (parseType === TelemetryParseType.Full) {
+			this._telemetryService.publicLog2<{ languageId: string; time: number; passes: number }, ParseTimeClassification>(`treeSitter.fullParse`, { languageId, time, passes });
+		} else {
+			this._telemetryService.publicLog2<{ languageId: string; time: number; passes: number }, ParseTimeClassification>(`treeSitter.incrementalParse`, { languageId, time, passes });
+		}
 	}
 }
 
