9be2a2630822c21493dccdfc6b1899db79236a85
meganrogge <megan.rogge@microsoft.com>
2024-07-31 09:56:45 -0700

    fixes #223959

--- src/vs/workbench/contrib/debug/browser/repl.ts
+++ src/vs/workbench/contrib/debug/browser/repl.ts
@@ -1032,7 +1032,15 @@ registerAction2(class extends ViewAction<Repl> {
 				id: MenuId.DebugConsoleContext,
 				group: 'z_commands',
 				order: 20
-			}]
+			}],
+			keybinding: [{
+				primary: 0,
+				mac: { primary: KeyMod.CtrlCmd | KeyCode.KeyK },
+				// Weight is higher than work workbench contributions so the keybinding remains
+				// highest priority when chords are registered afterwards
+				weight: KeybindingWeight.WorkbenchContrib + 1,
+				when: ContextKeyExpr.equals('focusedView', 'workbench.panel.repl.view')
+			}],
 		});
 	}
 
