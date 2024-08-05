fe1bb5654783da1787956fcfe2cdf4f0c99fba2a
Daniel Imms <2193314+Tyriar@users.noreply.github.com>
2024-08-05 08:06:22 -0700

    Update to node-pty and adopt useConptyDll
    Part of #224488

--- src/vs/platform/terminal/node/terminalProcess.ts
+++ src/vs/platform/terminal/node/terminalProcess.ts
@@ -153,6 +153,7 @@ export class TerminalProcess extends Disposable implements ITerminalChildProcess
 		this._properties[ProcessPropertyType.InitialCwd] = this._initialCwd;
 		this._properties[ProcessPropertyType.Cwd] = this._initialCwd;
 		const useConpty = this._options.windowsEnableConpty && process.platform === 'win32' && getWindowsBuildNumber() >= 18309;
+		const useConptyDll = useConpty && this._options.windowsUseConptyDll;
 		this._ptyOptions = {
 			name,
 			cwd,
@@ -161,6 +162,7 @@ export class TerminalProcess extends Disposable implements ITerminalChildProcess
 			cols,
 			rows,
 			useConpty,
+			useConptyDll,
 			// This option will force conpty to not redraw the whole viewport on launch
 			conptyInheritCursor: useConpty && !!shellLaunchConfig.initialText
 		};
