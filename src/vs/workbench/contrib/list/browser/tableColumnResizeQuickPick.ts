aeebace3875b07ebba6c0897d9aec9b7449372a2
meganrogge <megan.rogge@microsoft.com>
2024-08-07 11:29:27 -0700

    🧹

--- src/vs/workbench/contrib/list/browser/tableColumnResizeQuickPick.ts
+++ src/vs/workbench/contrib/list/browser/tableColumnResizeQuickPick.ts
@@ -7,14 +7,13 @@ import { Table } from 'vs/base/browser/ui/table/tableWidget';
 import { DisposableStore } from 'vs/base/common/lifecycle';
 import Severity from 'vs/base/common/severity';
 import { localize } from 'vs/nls';
-import { IQuickInputService, IQuickPickItem } from 'vs/platform/quickinput/common/quickInput';
+import { IQuickInputService, IQuickPick, IQuickPickItem } from 'vs/platform/quickinput/common/quickInput';
 
 interface IColumnResizeQuickPickItem extends IQuickPickItem {
 	index: number;
 }
 
 export class TableColumnResizeQuickPick extends DisposableStore {
-
 	constructor(
 		private readonly _table: Table<any>,
 		@IQuickInputService private readonly _quickInputService: IQuickInputService,
@@ -23,44 +22,59 @@ export class TableColumnResizeQuickPick extends DisposableStore {
 	}
 
 	async show(): Promise<void> {
-		const pick = this._quickInputService.createQuickPick<IColumnResizeQuickPickItem>({ useSeparators: true });
-		const picks: IColumnResizeQuickPickItem[] = [];
+		const quickPick = this._quickInputService.createQuickPick<IColumnResizeQuickPickItem>();
+		const items: IColumnResizeQuickPickItem[] = [];
 		this._table.getColumnLabels().forEach((label, index) => {
 			if (label) {
-				picks.push({ label, index });
+				items.push({ label, index });
 			}
 		});
-		pick.items = picks;
-		pick.placeholder = localize('table.column.selection', "Select the column to open, type to filter.");
-		pick.sortByLabel = false;
-		pick.show();
-		this.add(pick.onDidAccept(() => {
-			const index = pick.selectedItems[0].index;
+		quickPick.items = items;
+		quickPick.placeholder = localize('table.column.selection', "Select the column to resize, type to filter.");
+		quickPick.sortByLabel = false;
+		quickPick.canSelectMany = false;
+		quickPick.show();
+		this.add(quickPick.onDidAccept(() => {
+			const index = quickPick.selectedItems?.[0].index;
 			if (index !== undefined) {
-				pick.items = [];
-				pick.placeholder = localize('table.column.resizeValue', "Please enter a width in percentage for the column.");
-				pick.show();
-				this.add(pick.onDidAccept(() => {
-					const percentage = Number(pick.value);
-					if (!isNaN(percentage)) {
-						this._table.resizeColumn(index, percentage);
-					}
-					pick.hide();
-					this.dispose();
-				}));
-				this.add(pick.onDidChangeValue(() => {
-					const value = Number.parseInt(pick.value);
-					if (isNaN(value) || Number(value) < 0 || Number(value) >= 100) {
-						pick.validationMessage = localize('table.column.resizeValue.invalid', "Please enter a valid number.");
-						pick.severity = Severity.Error;
-					}
-				}));
+				this._selectColumn(quickPick, index);
 			}
-			this.add(pick.onDidHide(() => {
-				pick.dispose();
-				this.dispose();
-			}));
+		}));
+		this.add(quickPick.onDidHide(() => {
+			quickPick.dispose();
+			this.dispose();
+		}));
+	}
+
+	private _selectColumn(quickPick: IQuickPick<IColumnResizeQuickPickItem>, index: number): void {
+		quickPick.items = [];
+		quickPick.placeholder = localize('table.column.resizeValue', "Please enter a width in percentage for the column.");
+		quickPick.show();
+		this.add(quickPick.onDidAccept(() => {
+			const percentage = Number.parseInt(quickPick.value);
+			if (!isNaN(percentage)) {
+				this._table.resizeColumn(index, percentage);
+				quickPick.hide();
+				return;
+			}
+		}));
+		this.add(quickPick.onDidChangeValue(() => this._validateColumnResizeValue(quickPick)));
+	}
+
+	private _validateColumnResizeValue(quickPick: IQuickPick<IColumnResizeQuickPickItem>): number | undefined {
+		const percentage = Number.parseInt(quickPick.value);
+		if (!Number.isInteger(percentage)) {
+			quickPick.validationMessage = localize('table.column.resizeValue.invalidType', "Please enter an integer.");
+			quickPick.severity = Severity.Error;
+			return;
+		} else if (percentage < 0 || percentage >= 100) {
+			quickPick.validationMessage = localize('table.column.resizeValue.invalidRange', "Please enter a number between 0 and 100.");
+			quickPick.severity = Severity.Error;
+			return;
+		} else {
+			quickPick.validationMessage = undefined;
+			quickPick.severity = Severity.Info;
+			return percentage;
 		}
-		));
 	}
 }
