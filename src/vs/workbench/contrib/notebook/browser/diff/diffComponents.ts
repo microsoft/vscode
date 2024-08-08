58bdf1d753b9e09280d6d5015a71b24e8446a5ab
Don Jayamanne <don.jayamanne@outlook.com>
2024-08-09 09:51:42 +1000

    Ensure metadadata is visible in nb diff view

--- src/vs/workbench/contrib/notebook/browser/diff/diffComponents.ts
+++ src/vs/workbench/contrib/notebook/browser/diff/diffComponents.ts
@@ -1758,7 +1758,10 @@ export class ModifiedElement extends AbstractElementRenderer {
 			if (state.metadataHeight || state.outerWidth) {
 				if (this._metadataEditorContainer) {
 					this._metadataEditorContainer.style.height = `${this.cell.layoutInfo.metadataHeight}px`;
-					this._metadataEditor?.layout();
+					this._metadataEditor?.layout({
+						width: this._editor?.getViewWidth() || this.cell.getComputedCellContainerWidth(this.notebookEditor.getLayoutInfo(), false, true),
+						height: this.cell.layoutInfo.metadataHeight
+					});
 				}
 			}
 
