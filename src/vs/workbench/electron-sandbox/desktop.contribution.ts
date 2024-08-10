b812f290b418b68f0c7cfb5d1bc7c15732b895bf
Benjamin Pasero <benjamin.pasero@microsoft.com>
2024-08-10 08:04:09 +0200

    desc

--- src/vs/workbench/electron-sandbox/desktop.contribution.ts
+++ src/vs/workbench/electron-sandbox/desktop.contribution.ts
@@ -239,6 +239,7 @@ import product from 'vs/platform/product/common/product';
 			'window.experimentalControlOverlay': {
 				'type': 'boolean',
 				'included': isLinux,
+				'markdownDescription': localize('window.experimentalControlOverlay', "Show the native window controls when {0} is set to `custom`.", '`#window.titleBarStyle#`'),
 				'default': product.quality !== 'stable', // disable by default in stable for now (TODO@bpasero TODO@benibenj flip when custom title is default)
 			},
 			'window.customTitleBarVisibility': {
