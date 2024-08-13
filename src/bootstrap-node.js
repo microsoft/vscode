5c45bc90bcceda414cbbe36f054b3927c6aa22f0
Benjamin Pasero <benjamin.pasero@microsoft.com>
2024-08-13 15:08:34 +0200

    more use of `original-fs`

--- src/bootstrap-node.js
+++ src/bootstrap-node.js
@@ -15,7 +15,7 @@ const isESM = false;
 // ESM-comment-end
 // ESM-uncomment-begin
 // import * as path from 'path';
-// import * as fs from 'fs';
+// import * as fs from 'original-fs';
 // import { fileURLToPath } from 'url';
 // import { createRequire } from 'node:module';
 //
