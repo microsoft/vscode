7a32578ea64387048f3edca600207529d13babec
deepak1556 <hop2deep@gmail.com>
2024-08-14 16:20:22 +0900

    chore: skip yarn.lock files

--- .github/workflows/check-clean-git-state.sh
+++ .github/workflows/check-clean-git-state.sh
@@ -1,4 +1,4 @@
-R=`git status --porcelain | wc -l`
+R=`git status --porcelain | grep -v "yarn.lock" | wc -l`
 if [ "$R" -ne "0" ]; then
   echo "The git repo is not clean after compiling the /build/ folder. Did you forget to commit .js output for .ts files?";
   git status --porcelain
