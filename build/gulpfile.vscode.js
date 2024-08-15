1e9788895c9f20dbb068a12b29a7192fb6965e71
deepak1556 <hop2deep@gmail.com>
2024-08-15 10:37:09 +0900

    build: ignore extension dev dependency for rcedit

--- build/gulpfile.vscode.js
+++ build/gulpfile.vscode.js
@@ -502,7 +502,7 @@ function patchWin32DependenciesTask(destinationFolderName) {
 	const cwd = path.join(path.dirname(root), destinationFolderName);
 
 	return async () => {
-		const deps = await glob('**/*.node', { cwd });
+		const deps = await glob('**/*.node', { cwd, ignore: 'extensions/node_modules/@parcel/watcher/**' });
 		const packageJson = JSON.parse(await fs.promises.readFile(path.join(cwd, 'resources', 'app', 'package.json'), 'utf8'));
 		const product = JSON.parse(await fs.promises.readFile(path.join(cwd, 'resources', 'app', 'product.json'), 'utf8'));
 		const baseVersion = packageJson.version.replace(/-.*$/, '');
