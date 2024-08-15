02fcc994ac88bad318234f9f58f2a55d3174724e
Benjamin Pasero <benjamin.pasero@microsoft.com>
2024-08-15 19:58:52 +0200

    esm - port over unit test changes (#225711)

---
+++ scripts/test-esm.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+set -e
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+	realpath() { [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"; }
+	ROOT=$(dirname $(dirname $(realpath "$0")))
+else
+	ROOT=$(dirname $(dirname $(readlink -f $0)))
+	# --disable-dev-shm-usage: when run on docker containers where size of /dev/shm
+	# partition < 64MB which causes OOM failure for chromium compositor that uses the partition for shared memory
+	LINUX_EXTRA_ARGS="--disable-dev-shm-usage"
+fi
+
+cd $ROOT
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+	NAME=`node -p "require('./product.json').nameLong"`
+	CODE="./.build/electron/$NAME.app/Contents/MacOS/Electron"
+else
+	NAME=`node -p "require('./product.json').applicationName"`
+	CODE=".build/electron/$NAME"
+fi
+
+VSCODECRASHDIR=$ROOT/.build/crashes
+
+# Node modules
+test -d node_modules || yarn
+
+# Get electron
+yarn electron
+
+# Unit Tests
+if [[ "$OSTYPE" == "darwin"* ]]; then
+	cd $ROOT ; ulimit -n 4096 ; \
+		ELECTRON_ENABLE_LOGGING=1 \
+		"$CODE" \
+		test/unit/electron/index.esm.js --crash-reporter-directory=$VSCODECRASHDIR "$@"
+else
+	cd $ROOT ; \
+		ELECTRON_ENABLE_LOGGING=1 \
+		"$CODE" \
+		test/unit/electron/index.esm.js --crash-reporter-directory=$VSCODECRASHDIR $LINUX_EXTRA_ARGS "$@"
+fi
