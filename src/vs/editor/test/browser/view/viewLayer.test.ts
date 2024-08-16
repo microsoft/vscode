04488d8a7a4ecc96d31362220ebde88db8acee53
Henning Dieterichs <hdieterichs@microsoft.com>
2024-08-16 14:54:44 +0200

    Fixes CI

--- src/vs/editor/test/browser/view/viewLayer.test.ts
+++ src/vs/editor/test/browser/view/viewLayer.test.ts
@@ -45,7 +45,7 @@ suite('RenderedLinesCollection onLinesDeleted', () => {
 	ensureNoDisposablesAreLeakedInTestSuite();
 
 	function testOnModelLinesDeleted(deleteFromLineNumber: number, deleteToLineNumber: number, expectedDeleted: string[], expectedState: ILinesCollectionState): void {
-		const col = new RenderedLinesCollection<TestLine>(() => new TestLine('new'));
+		const col = new RenderedLinesCollection<TestLine>({ createLine: () => new TestLine('new') });
 		col._set(6, [
 			new TestLine('old6'),
 			new TestLine('old7'),
@@ -322,7 +322,7 @@ suite('RenderedLinesCollection onLineChanged', () => {
 	ensureNoDisposablesAreLeakedInTestSuite();
 
 	function testOnModelLineChanged(changedLineNumber: number, expectedPinged: boolean, expectedState: ILinesCollectionState): void {
-		const col = new RenderedLinesCollection<TestLine>(() => new TestLine('new'));
+		const col = new RenderedLinesCollection<TestLine>({ createLine: () => new TestLine('new') });
 		col._set(6, [
 			new TestLine('old6'),
 			new TestLine('old7'),
