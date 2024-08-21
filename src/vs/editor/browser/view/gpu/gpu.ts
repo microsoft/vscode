930d0c2ed6fa0480b1c985f713940ebde5e7c7ba
Daniel Imms <2193314+Tyriar@users.noreply.github.com>
2024-08-21 09:32:41 -0700

    Split render strategy into own file

---
+++ src/vs/editor/browser/view/gpu/gpu.ts
@@ -0,0 +1,32 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import type { IVisibleLine } from 'vs/editor/browser/view/viewLayer';
+
+export const enum BindingId {
+	GlyphInfo0,
+	GlyphInfo1,
+	Cells,
+	TextureSampler,
+	Texture,
+	CanvasDimensionsUniform,
+	AtlasDimensionsUniform,
+	ScrollOffset,
+}
+
+export interface IRendererContext<T extends IVisibleLine> {
+	rendLineNumberStart: number;
+	lines: T[];
+	linesLength: number;
+}
+
+export interface IRenderStrategy<T extends IVisibleLine> {
+	readonly wgsl: string;
+	readonly bindGroupEntries: GPUBindGroupEntry[];
+
+	initBuffers(): void;
+	update(ctx: IRendererContext<T>, startLineNumber: number, stopLineNumber: number, deltaTop: number[]): number;
+	draw?(pass: GPURenderPassEncoder, ctx: IRendererContext<T>, startLineNumber: number, stopLineNumber: number, deltaTop: number[]): void;
+}
