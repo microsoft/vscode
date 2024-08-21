3a92a9274c106d53d424ebe510af5b4a9b580a35
Alex Ross <alros@microsoft.com>
2024-08-21 15:09:28 +0200

    Add isMeasurement to tree sitter Part of #210475

--- src/vs/editor/browser/services/treeSitter/treeSitterParserService.ts
+++ src/vs/editor/browser/services/treeSitter/treeSitterParserService.ts
@@ -214,8 +214,8 @@ export class TreeSitterParseResult implements IDisposable, ITreeSitterParseResul
 			owner: 'alros';
 			comment: 'Used to understand how long it takes to parse a tree-sitter tree';
 			languageId: { classification: 'SystemMetaData'; purpose: 'FeatureInsight'; comment: 'The programming language ID.' };
-			time: { classification: 'SystemMetaData'; purpose: 'FeatureInsight'; comment: 'The ms it took to parse' };
-			passes: { classification: 'SystemMetaData'; purpose: 'FeatureInsight'; comment: 'The number of passes it took to parse' };
+			time: { classification: 'SystemMetaData'; purpose: 'FeatureInsight'; isMeasurement: true; comment: 'The ms it took to parse' };
+			passes: { classification: 'SystemMetaData'; purpose: 'FeatureInsight'; isMeasurement: true; comment: 'The number of passes it took to parse' };
 		};
 		if (parseType === TelemetryParseType.Full) {
 			this._telemetryService.publicLog2<{ languageId: string; time: number; passes: number }, ParseTimeClassification>(`treeSitter.fullParse`, { languageId, time, passes });
