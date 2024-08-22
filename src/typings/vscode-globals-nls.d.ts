b8e4e034b6c39783f1388c6eef3e69b33273c24c
Aiday Marlen Kyzy <amarlenkyzy@microsoft.com>
2024-08-22 15:16:22 +0200

    Extracting the global NLS variables into functions (#226078)
    * extracting the constants
    
    * exporting from the specific file

--- src/typings/vscode-globals-nls.d.ts
+++ src/typings/vscode-globals-nls.d.ts
@@ -24,10 +24,14 @@ declare global {
 	 * All NLS messages produced by `localize` and `localize2` calls
 	 * under `src/vs` translated to the language as indicated by
 	 * `_VSCODE_NLS_LANGUAGE`.
+	 *
+	 * Instead of accessing this global variable directly, use function getNLSMessages.
 	 */
 	var _VSCODE_NLS_MESSAGES: string[];
 	/**
 	 * The actual language of the NLS messages (e.g. 'en', de' or 'pt-br').
+	 *
+	 * Instead of accessing this global variable directly, use function getNLSLanguage.
 	 */
 	var _VSCODE_NLS_LANGUAGE: string | undefined;
 }
