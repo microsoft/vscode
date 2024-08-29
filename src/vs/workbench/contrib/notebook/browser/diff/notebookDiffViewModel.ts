392c9b8c73cdc3b1fc333bf2ef709f72bee4be12
Don Jayamanne <don.jayamanne@outlook.com>
2024-08-30 05:48:55 +1000

    nb diff perf improvements by comparing hash values

--- src/vs/workbench/contrib/notebook/browser/diff/notebookDiffViewModel.ts
+++ src/vs/workbench/contrib/notebook/browser/diff/notebookDiffViewModel.ts
@@ -443,7 +443,7 @@ function createDiffViewModels(instantiationService: IInstantiationService, confi
 			case 'unchanged': {
 				const originalCell = originalModel.cells[diff.originalCellIndex];
 				const modifiedCell = modifiedModel.cells[diff.modifiedCellIndex];
-				const type = (originalCell.textModel?.getValue() !== modifiedCell.textModel?.getValue()) ? 'modified' : 'unchanged';
+				const type = (originalCell.getHashValue() !== modifiedCell.getHashValue()) ? 'modified' : 'unchanged';
 				return new SideBySideDiffElementViewModel(
 					model.modified.notebook,
 					model.original.notebook,
