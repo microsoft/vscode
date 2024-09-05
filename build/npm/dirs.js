29acbcd9cea356ece743dd80f666e807d020cf95
deepak1556 <hop2deep@gmail.com>
2024-09-05 22:51:19 +0900

    chore: add vscode-selfhost-import-aid to postinstall dirs

--- build/npm/dirs.js
+++ build/npm/dirs.js
@@ -51,6 +51,7 @@ const dirs = [
 	'test/integration/browser',
 	'test/monaco',
 	'test/smoke',
+	'.vscode/extensions/vscode-selfhost-import-aid',
 	'.vscode/extensions/vscode-selfhost-test-provider',
 ];
 
