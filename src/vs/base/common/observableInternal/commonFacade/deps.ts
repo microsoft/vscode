da0c8b62a6ccd3c652c90bb390f1ead10d399bc8
Henning Dieterichs <hdieterichs@microsoft.com>
2024-09-10 12:09:45 +0200

    Fixes CI

--- src/vs/base/common/observableInternal/commonFacade/deps.ts
+++ src/vs/base/common/observableInternal/commonFacade/deps.ts
@@ -4,7 +4,7 @@
  *--------------------------------------------------------------------------------------------*/
 
 export { assertFn } from '../../assert.js';
-export { EqualityComparer, strictEquals } from '../../equals.js';
+export { type EqualityComparer, strictEquals } from '../../equals.js';
 export { BugIndicatingError, onBugIndicatingError } from '../../errors.js';
-export { Event, IValueWithChangeEvent } from '../../event.js';
-export { DisposableStore, IDisposable, markAsDisposed, toDisposable, trackDisposable } from '../../lifecycle.js';
+export { Event, type IValueWithChangeEvent } from '../../event.js';
+export { DisposableStore, type IDisposable, markAsDisposed, toDisposable, trackDisposable } from '../../lifecycle.js';
--- src/vs/base/common/observableInternal/index.ts
+++ src/vs/base/common/observableInternal/index.ts
@@ -5,81 +5,19 @@
 
 // This is a facade for the observable implementation. Only import from here!
 
-import { observableValueOpts } from './api.js';
-import { autorun, autorunDelta, autorunHandleChanges, autorunOpts, autorunWithStore, autorunWithStoreHandleChanges } from './autorun.js';
-import { asyncTransaction, disposableObservableValue, globalTransaction, observableValue, subtransaction, transaction, TransactionImpl, type IChangeContext, type IChangeTracker, type IObservable, type IObserver, type IReader, type ISettable, type ISettableObservable, type ITransaction, } from './base.js';
-import { derived, derivedDisposable, derivedHandleChanges, derivedOpts, derivedWithSetter, derivedWithStore } from './derived.js';
-import { ObservableLazy, ObservableLazyPromise, ObservablePromise, PromiseResult, } from './promise.js';
-import { derivedWithCancellationToken, waitForState } from './utilsCancellation.js';
-import { constObservable, debouncedObservable, derivedConstOnceDefined, derivedObservableWithCache, derivedObservableWithWritableCache, keepObserved, latestChangedValue, mapObservableArrayCached, observableFromEvent, observableFromEventOpts, observableFromPromise, observableFromValueWithChangeEvent, observableSignal, observableSignalFromEvent, recomputeInitiallyAndOnChange, runOnChange, runOnChangeWithStore, signalFromObservable, ValueWithChangeEventFromObservable, wasEventTriggeredRecently, type IObservableSignal, } from './utils.js';
-
-export type {
-	DebugOwner, IChangeContext,
-	IChangeTracker,
-	IObservable,
-	IObservableSignal,
-	IObserver,
-	IReader,
-	ISettable,
-	ISettableObservable,
-	ITransaction
-};
-
-export {
-	asyncTransaction,
-	autorun,
-	autorunDelta,
-	autorunHandleChanges,
-	autorunOpts,
-	autorunWithStore,
-	autorunWithStoreHandleChanges,
-	constObservable,
-	debouncedObservable,
-	derived,
-	derivedConstOnceDefined,
-	derivedDisposable,
-	derivedHandleChanges,
-	derivedObservableWithCache,
-	derivedObservableWithWritableCache,
-	derivedOpts,
-	derivedWithCancellationToken,
-	derivedWithSetter,
-	derivedWithStore,
-	disposableObservableValue,
-	globalTransaction,
-	keepObserved,
-	latestChangedValue,
-	mapObservableArrayCached,
-	observableFromEvent,
-	observableFromEventOpts,
-	observableFromPromise,
-	observableFromValueWithChangeEvent,
-	ObservableLazy,
-	ObservableLazyPromise,
-	ObservablePromise,
-	observableSignal,
-	observableSignalFromEvent,
-	observableValue,
-	observableValueOpts,
-	PromiseResult,
-	recomputeInitiallyAndOnChange,
-	runOnChange,
-	runOnChangeWithStore,
-	subtransaction,
-	transaction,
-	TransactionImpl,
-	ValueWithChangeEventFromObservable,
-	waitForState,
-	wasEventTriggeredRecently,
-	signalFromObservable,
-};
+export { observableValueOpts } from './api.js';
+export { autorun, autorunDelta, autorunHandleChanges, autorunOpts, autorunWithStore, autorunWithStoreHandleChanges } from './autorun.js';
+export { asyncTransaction, disposableObservableValue, globalTransaction, observableValue, subtransaction, transaction, TransactionImpl, type IChangeContext, type IChangeTracker, type IObservable, type IObserver, type IReader, type ISettable, type ISettableObservable, type ITransaction, } from './base.js';
+export { derived, derivedDisposable, derivedHandleChanges, derivedOpts, derivedWithSetter, derivedWithStore } from './derived.js';
+export { ObservableLazy, ObservableLazyPromise, ObservablePromise, PromiseResult, } from './promise.js';
+export { derivedWithCancellationToken, waitForState } from './utilsCancellation.js';
+export { constObservable, debouncedObservable, derivedConstOnceDefined, derivedObservableWithCache, derivedObservableWithWritableCache, keepObserved, latestChangedValue, mapObservableArrayCached, observableFromEvent, observableFromEventOpts, observableFromPromise, observableFromValueWithChangeEvent, observableSignal, observableSignalFromEvent, recomputeInitiallyAndOnChange, runOnChange, runOnChangeWithStore, signalFromObservable, ValueWithChangeEventFromObservable, wasEventTriggeredRecently, type IObservableSignal, } from './utils.js';
+export { type DebugOwner } from './debugName.js';
 
 import {
 	ConsoleObservableLogger,
 	setLogger
 } from './logging.js';
-
-import { DebugOwner } from './debugName.js';
 
 // Remove "//" in the next line to enable logging
 const enableLogging = false
