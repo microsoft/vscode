0578b3996f9a074d7cdd4c47bd93049e977882e3
meganrogge <megan.rogge@microsoft.com>
2024-09-10 13:41:16 -0700

    rm entry

--- src/vs/workbench/contrib/welcomeGettingStarted/common/gettingStartedContent.ts
+++ src/vs/workbench/contrib/welcomeGettingStarted/common/gettingStartedContent.ts
@@ -426,16 +426,6 @@ export const walkthroughs: GettingStartedWalkthroughContent = [
 					}
 				},
 				{
-					id: 'pickColorThemeAccessibility',
-					title: localize('gettingStarted.pickTheme.title', "Choose your theme"),
-					description: localize('gettingStarted.pickTheme.description.interpolated', "The right theme helps you focus on your code, is easy on your eyes, and is simply more fun to use. We offer high contrast themes to best support users with visual impairments. \n{0}", Button(localize('titleID', "Browse Color Themes"), 'command:workbench.action.selectTheme')),
-					completionEvents: [
-						'onSettingChanged:workbench.colorTheme',
-						'onCommand:workbench.action.selectTheme'
-					],
-					media: { type: 'markdown', path: 'empty' }
-				},
-				{
 					id: 'accessibilitySettings',
 					title: localize('gettingStarted.accessibilitySettings.title', "Configure accessibility settings"),
 					description: localize('gettingStarted.accessibilitySettings.description.interpolated', "Accessibility settings can be configured by running the Open Accessibility Settings command.\n{0}", Button(localize('openAccessibilitySettings', "Open Accessibility Settings"), 'command:workbench.action.openAccessibilitySettings')),
