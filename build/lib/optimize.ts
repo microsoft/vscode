9c6a493bc96acfb8f26353b47f047d0c7f3c5d12
Benjamin Pasero <benjamin.pasero@microsoft.com>
2024-09-11 21:09:40 +0200

    esm - try to restore sourcemaps

--- build/lib/optimize.ts
+++ build/lib/optimize.ts
@@ -334,6 +334,7 @@ function optimizeESMTask(opts: IOptimizeAMDTaskOpts, cjsOpts?: IOptimizeCommonJS
 				packages: 'external', // "external all the things", see https://esbuild.github.io/api/#packages
 				platform: 'neutral', // makes esm
 				format: 'esm',
+				sourcemap: 'external',
 				plugins: [boilerplateTrimmer],
 				target: ['es2022'],
 				loader: {
@@ -358,6 +359,7 @@ function optimizeESMTask(opts: IOptimizeAMDTaskOpts, cjsOpts?: IOptimizeCommonJS
 				for (const file of res.outputFiles) {
 
 					let contents = file.contents;
+					let sourceMapFile: esbuild.OutputFile | undefined = undefined;
 
 					if (file.path.endsWith('.js')) {
 
@@ -371,13 +373,17 @@ function optimizeESMTask(opts: IOptimizeAMDTaskOpts, cjsOpts?: IOptimizeCommonJS
 							}
 							contents = Buffer.from(newText);
 						}
+
+						sourceMapFile = res.outputFiles.find(f => f.path === `${file.path}.map`);
 					}
 
-					files.push(new VinylFile({
+					const fileProps = {
 						contents: Buffer.from(contents),
+						sourceMap: sourceMapFile ? JSON.parse(sourceMapFile.text) : undefined, // support gulp-sourcemaps
 						path: file.path,
 						base: path.join(REPO_ROOT_PATH, opts.src)
-					}));
+					};
+					files.push(new VinylFile(fileProps));
 				}
 			});
 
@@ -569,13 +575,6 @@ export function minifyTask(src: string, sourceMapBaseUrl?: string): (cb: any) =>
 			svgFilter,
 			svgmin(),
 			svgFilter.restore,
-			(<any>sourcemaps).mapSources((sourcePath: string) => {
-				if (sourcePath === 'bootstrap-fork.js') {
-					return 'bootstrap-fork.orig.js';
-				}
-
-				return sourcePath;
-			}),
 			sourcemaps.write('./', {
 				sourceMappingURL,
 				sourceRoot: undefined,
