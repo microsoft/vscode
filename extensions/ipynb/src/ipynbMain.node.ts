b264b1e9ac347d69e25742adaeee7ca2240a08f6
Don Jayamanne <don.jayamanne@outlook.com>
2024-09-12 18:26:09 +1000

    Ensure node worker is used only in desktop bundle of ipynb extension

--- extensions/ipynb/src/ipynbMain.node.ts
+++ extensions/ipynb/src/ipynbMain.node.ts
@@ -5,9 +5,10 @@
 
 import * as vscode from 'vscode';
 import * as main from './ipynbMain';
+import { NotebookSerializer } from './notebookSerializer.node';
 
 export function activate(context: vscode.ExtensionContext) {
-	return main.activate(context, false);
+	return main.activate(context, new NotebookSerializer(context));
 }
 
 export function deactivate() {
