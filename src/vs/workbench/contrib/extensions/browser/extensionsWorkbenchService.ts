c507fa1baf54504a62d9f366b98743f2c0462f9f
Sandeep Somavarapu <sasomava@microsoft.com>
2024-09-12 20:41:24 +0200

    skip workspace scoped extensions (#228375)

--- src/vs/workbench/contrib/extensions/browser/extensionsWorkbenchService.ts
+++ src/vs/workbench/contrib/extensions/browser/extensionsWorkbenchService.ts
@@ -1359,7 +1359,7 @@ export class ExtensionsWorkbenchService extends Disposable implements IExtension
 
 	private computeExtensionsNotification(): Omit<IExtensionsNotification, 'dismiss'> | undefined {
 
-		const invalidExtensions = this.local.filter(e => e.enablementState === EnablementState.DisabledByInvalidExtension);
+		const invalidExtensions = this.local.filter(e => e.enablementState === EnablementState.DisabledByInvalidExtension && !e.isWorkspaceScoped);
 		if (invalidExtensions.length) {
 			if (invalidExtensions.some(e => e.local &&
 				(!isEngineValid(e.local.manifest.engines.vscode, this.productService.version, this.productService.date) || areApiProposalsCompatible([...e.local.manifest.enabledApiProposals ?? []]))
