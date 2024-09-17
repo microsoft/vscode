a7c9ee57c3241f1e13237719b061ddc703c2a7bf
Ladislau Szomoru <3372902+lszomoru@users.noreply.github.com>
2024-09-17 09:09:30 +0200

    SCM - 💄 remove negative view order (#228795)

--- src/vs/workbench/contrib/scm/browser/scm.contribution.ts
+++ src/vs/workbench/contrib/scm/browser/scm.contribution.ts
@@ -82,13 +82,27 @@ viewsRegistry.registerViewWelcomeContent(VIEW_PANE_ID, {
 });
 
 viewsRegistry.registerViews([{
+	id: REPOSITORIES_VIEW_PANE_ID,
+	name: localize2('source control repositories', "Source Control Repositories"),
+	ctorDescriptor: new SyncDescriptor(SCMRepositoriesViewPane),
+	canToggleVisibility: true,
+	hideByDefault: true,
+	canMoveView: true,
+	weight: 20,
+	order: 0,
+	when: ContextKeyExpr.and(ContextKeyExpr.has('scm.providerCount'), ContextKeyExpr.notEquals('scm.providerCount', 0)),
+	// readonly when = ContextKeyExpr.or(ContextKeyExpr.equals('config.scm.alwaysShowProviders', true), ContextKeyExpr.and(ContextKeyExpr.notEquals('scm.providerCount', 0), ContextKeyExpr.notEquals('scm.providerCount', 1)));
+	containerIcon: sourceControlViewIcon
+}], viewContainer);
+
+viewsRegistry.registerViews([{
 	id: VIEW_PANE_ID,
 	name: localize2('source control', 'Source Control'),
 	ctorDescriptor: new SyncDescriptor(SCMViewPane),
 	canToggleVisibility: true,
 	canMoveView: true,
 	weight: 40,
-	order: -999,
+	order: 1,
 	containerIcon: sourceControlViewIcon,
 	openCommandActionDescriptor: {
 		id: viewContainer.id,
@@ -104,27 +118,13 @@ viewsRegistry.registerViews([{
 }], viewContainer);
 
 viewsRegistry.registerViews([{
-	id: REPOSITORIES_VIEW_PANE_ID,
-	name: localize2('source control repositories', "Source Control Repositories"),
-	ctorDescriptor: new SyncDescriptor(SCMRepositoriesViewPane),
-	canToggleVisibility: true,
-	hideByDefault: true,
-	canMoveView: true,
-	weight: 20,
-	order: -1000,
-	when: ContextKeyExpr.and(ContextKeyExpr.has('scm.providerCount'), ContextKeyExpr.notEquals('scm.providerCount', 0)),
-	// readonly when = ContextKeyExpr.or(ContextKeyExpr.equals('config.scm.alwaysShowProviders', true), ContextKeyExpr.and(ContextKeyExpr.notEquals('scm.providerCount', 0), ContextKeyExpr.notEquals('scm.providerCount', 1)));
-	containerIcon: sourceControlViewIcon
-}], viewContainer);
-
-viewsRegistry.registerViews([{
 	id: HISTORY_VIEW_PANE_ID,
 	name: localize2('source control history', "Source Control Graph"),
 	ctorDescriptor: new SyncDescriptor(SCMHistoryViewPane),
 	canToggleVisibility: true,
 	canMoveView: true,
 	weight: 40,
-	order: 2, /* https://github.com/microsoft/vscode/issues/226447 */
+	order: 2,
 	when: ContextKeyExpr.and(
 		// Repository Count
 		ContextKeyExpr.and(
