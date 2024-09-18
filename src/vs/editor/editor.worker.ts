1ce171dcc58089b4a0b3db4cf9e2299fd1c131ab
Aiday Marlen Kyzy <amarlenkyzy@microsoft.com>
2024-09-18 12:56:31 +0200

    Add patch for webpack smoketest failure

--- src/vs/editor/editor.worker.ts
+++ src/vs/editor/editor.worker.ts
@@ -3,4 +3,30 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-export * from 'vs/editor/common/services/editorWorkerBootstrap';
+import { IWorkerServer, SimpleWorkerServer } from 'vs/base/common/worker/simpleWorker';
+import { EditorSimpleWorker } from 'vs/editor/common/services/editorSimpleWorker';
+import { EditorWorkerHost } from 'vs/editor/common/services/editorWorkerHost';
+
+let initialized = false;
+
+export function initialize(foreignModule: any) {
+	if (initialized) {
+		return;
+	}
+	initialized = true;
+
+	const simpleWorker = new SimpleWorkerServer((msg) => {
+		globalThis.postMessage(msg);
+	}, (workerServer: IWorkerServer) => new EditorSimpleWorker(EditorWorkerHost.getChannel(workerServer), foreignModule));
+
+	globalThis.onmessage = (e: MessageEvent) => {
+		simpleWorker.onmessage(e.data);
+	};
+}
+
+globalThis.onmessage = (e: MessageEvent) => {
+	// Ignore first message in this case and initialize if not yet initialized
+	if (!initialized) {
+		initialize(null);
+	}
+};
