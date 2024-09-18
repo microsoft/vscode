ac5efe11ce4bb575b2335987779736e0f4150d7a
Tyler Leonhardt <me@tylerleonhardt.com>
2024-09-18 15:12:14 -0700

    Expose Account Preference in Extension context menu
    So we have more entry points to account preference.
    
    To do this, I added to the `AuthenticationUsageService` the ability to query if an extension "uses auth". I'm not totally sold on that being where this is... but it works for now.

--- src/vs/workbench/services/authentication/browser/authenticationUsageService.ts
+++ src/vs/workbench/services/authentication/browser/authenticationUsageService.ts
@@ -5,7 +5,9 @@
 
 import { InstantiationType, registerSingleton } from '../../../../platform/instantiation/common/extensions.js';
 import { createDecorator } from '../../../../platform/instantiation/common/instantiation.js';
+import { IProductService } from '../../../../platform/product/common/productService.js';
 import { IStorageService, StorageScope, StorageTarget } from '../../../../platform/storage/common/storage.js';
+import { IAuthenticationService } from '../common/authentication.js';
 
 export interface IAccountUsage {
 	extensionId: string;
@@ -16,15 +18,57 @@ export interface IAccountUsage {
 export const IAuthenticationUsageService = createDecorator<IAuthenticationUsageService>('IAuthenticationUsageService');
 export interface IAuthenticationUsageService {
 	readonly _serviceBrand: undefined;
+	/**
+	 * Initializes the cache of extensions that use authentication. Ideally used in a contribution that can be run eventually after the workspace is loaded.
+	 */
+	initializeExtensionUsageCache(): Promise<void>;
+	/**
+	 * Checks if an extension uses authentication
+	 * @param extensionId The id of the extension to check
+	 */
+	extensionUsesAuth(extensionId: string): Promise<boolean>;
+	/**
+	 * Reads the usages for an account
+	 * @param providerId The id of the authentication provider to get usages for
+	 * @param accountName The name of the account to get usages for
+	 */
 	readAccountUsages(providerId: string, accountName: string,): IAccountUsage[];
+	/**
+	 *
+	 * @param providerId The id of the authentication provider to get usages for
+	 * @param accountName The name of the account to get usages for
+	 */
 	removeAccountUsage(providerId: string, accountName: string): void;
+	/**
+	 * Adds a usage for an account
+	 * @param providerId The id of the authentication provider to get usages for
+	 * @param accountName The name of the account to get usages for
+	 * @param extensionId The id of the extension to add a usage for
+	 * @param extensionName The name of the extension to add a usage for
+	 */
 	addAccountUsage(providerId: string, accountName: string, extensionId: string, extensionName: string): void;
 }
 
 export class AuthenticationUsageService implements IAuthenticationUsageService {
 	_serviceBrand: undefined;
 
-	constructor(@IStorageService private readonly _storageService: IStorageService) { }
+	private _initializedPromise: Promise<void> | undefined;
+	private _extensionsUsingAuth = new Set<string>();
+
+	constructor(
+		@IStorageService private readonly _storageService: IStorageService,
+		@IAuthenticationService private readonly _authenticationService: IAuthenticationService,
+		@IProductService private readonly _productService: IProductService,
+	) { }
+
+	initializeExtensionUsageCache(): Promise<void> {
+		return this._initExtensionsUsingAuth();
+	}
+
+	async extensionUsesAuth(extensionId: string): Promise<boolean> {
+		await this._initExtensionsUsingAuth();
+		return this._extensionsUsingAuth.has(extensionId);
+	}
 
 	readAccountUsages(providerId: string, accountName: string): IAccountUsage[] {
 		const accountKey = `${providerId}-${accountName}-usages`;
@@ -64,6 +108,44 @@ export class AuthenticationUsageService implements IAuthenticationUsageService {
 		}
 
 		this._storageService.store(accountKey, JSON.stringify(usages), StorageScope.APPLICATION, StorageTarget.MACHINE);
+		this._extensionsUsingAuth.add(extensionId);
+	}
+
+	private _initExtensionsUsingAuth() {
+		if (this._initializedPromise) {
+			return this._initializedPromise;
+		}
+
+		// If an extension is listed in `trustedExtensionAuthAccess` we should consider it as using auth
+		const trustedExtensionAuthAccess = this._productService.trustedExtensionAuthAccess;
+		if (Array.isArray(trustedExtensionAuthAccess)) {
+			for (const extensionId of trustedExtensionAuthAccess) {
+				this._extensionsUsingAuth.add(extensionId);
+			}
+		} else if (trustedExtensionAuthAccess) {
+			for (const extensions of Object.values(trustedExtensionAuthAccess)) {
+				for (const extensionId of extensions) {
+					this._extensionsUsingAuth.add(extensionId);
+				}
+			}
+		}
+
+		this._initializedPromise = (async () => {
+			for (const providerId of this._authenticationService.getProviderIds()) {
+				try {
+					const accounts = await this._authenticationService.getAccounts(providerId);
+					for (const account of accounts) {
+						const usage = this.readAccountUsages(providerId, account.label);
+						for (const u of usage) {
+							this._extensionsUsingAuth.add(u.extensionId);
+						}
+					}
+				} catch (e) {
+					// ignore
+				}
+			}
+		})();
+		return this._initializedPromise;
 	}
 }
 
