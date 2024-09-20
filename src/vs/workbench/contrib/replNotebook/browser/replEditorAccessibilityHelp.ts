b89ce54bf0356ed011cf9ba9b407c188674e3c6f
Aaron Munger <aamunger@microsoft.com>
2024-09-20 13:17:46 -0700

    help view for REPL editor (#229228)

--- src/vs/workbench/contrib/replNotebook/browser/replEditorAccessibilityHelp.ts
+++ src/vs/workbench/contrib/replNotebook/browser/replEditorAccessibilityHelp.ts
@@ -4,7 +4,6 @@
  *--------------------------------------------------------------------------------------------*/
 import { ServicesAccessor } from '../../../../platform/instantiation/common/instantiation.js';
 import { IAccessibleViewImplentation } from '../../../../platform/accessibility/browser/accessibleViewRegistry.js';
-import { NOTEBOOK_EDITOR_FOCUSED } from '../common/notebookContextKeys.js';
 import { localize } from '../../../../nls.js';
 import { ICodeEditor } from '../../../../editor/browser/editorBrowser.js';
 import { AccessibleViewProviderId, AccessibleViewType, AccessibleContentProvider } from '../../../../platform/accessibility/browser/accessibleView.js';
@@ -12,11 +11,12 @@ import { AccessibilityVerbositySettingId } from '../../accessibility/browser/acc
 import { IEditorService } from '../../../services/editor/common/editorService.js';
 import { IVisibleEditorPane } from '../../../common/editor.js';
 import { ICodeEditorService } from '../../../../editor/browser/services/codeEditorService.js';
+import { IS_COMPOSITE_NOTEBOOK } from '../../notebook/common/notebookContextKeys.js';
 
-export class NotebookAccessibilityHelp implements IAccessibleViewImplentation {
+export class ReplEditorAccessibilityHelp implements IAccessibleViewImplentation {
 	readonly priority = 105;
-	readonly name = 'notebook';
-	readonly when = NOTEBOOK_EDITOR_FOCUSED;
+	readonly name = 'REPL Editor';
+	readonly when = IS_COMPOSITE_NOTEBOOK;
 	readonly type: AccessibleViewType = AccessibleViewType.Help;
 	getProvider(accessor: ServicesAccessor) {
 		const activeEditor = accessor.get(ICodeEditorService).getActiveCodeEditor()
@@ -32,23 +32,19 @@ export class NotebookAccessibilityHelp implements IAccessibleViewImplentation {
 
 function getAccessibilityHelpText(): string {
 	return [
-		localize('notebook.overview', 'The notebook view is a collection of code and markdown cells. Code cells can be executed and will produce output directly below the cell.'),
-		localize('notebook.cell.edit', 'The Edit Cell command{0} will focus on the cell input.', '<keybinding:notebook.cell.edit>'),
-		localize('notebook.cell.quitEdit', 'The Quit Edit command{0} will set focus on the cell container. The default (Escape) key may need to be pressed twice first exit the virtual cursor if active.', '<keybinding:notebook.cell.quitEdit>'),
-		localize('notebook.cell.focusInOutput', 'The Focus Output command{0} will set focus in the cell\'s output.', '<keybinding:notebook.cell.focusInOutput>'),
-		localize('notebook.focusNextEditor', 'The Focus Next Cell Editor command{0} will set focus in the next cell\'s editor.', '<keybinding:notebook.focusNextEditor>'),
-		localize('notebook.focusPreviousEditor', 'The Focus Previous Cell Editor command{0} will set focus in the previous cell\'s editor.', '<keybinding:notebook.focusPreviousEditor>'),
-		localize('notebook.cellNavigation', 'The up and down arrows will also move focus between cells while focused on the outer cell container.'),
-		localize('notebook.cell.executeAndFocusContainer', 'The Execute Cell command{0} executes the cell that currently has focus.', '<keybinding:notebook.cell.executeAndFocusContainer>'),
-		localize('notebook.cell.insertCodeCellBelowAndFocusContainer', 'The Insert Cell Above{0} and Below{1} commands will create new empty code cells.', '<keybinding:notebook.cell.insertCodeCellAbove>', '<keybinding:notebook.cell.insertCodeCellBelow>'),
-		localize('notebook.changeCellType', 'The Change Cell to Code/Markdown commands are used to switch between cell types.')
+		localize('replEditor.overview', 'You are in a REPL Editor which contains in input box to evaluate expressions and a list of previously executed expressions and their output.'),
+		localize('replEditor.execute', 'The Execute command{0} will evaluate the expression in the input box.', '<keybinding:repl.execute>'),
+		localize('replEditor.focusHistory', 'The Focus History command{0} will move focus to the list of previously executed items.', '<keybinding:interactive.history.focus>'),
+		localize('replEditor.focusReplInput', 'The Focus Input Editor command{0} will move focus to the REPL input box.', '<keybinding:interactive.input.focus>'),
+		localize('replEditor.cellNavigation', 'The up and down arrows will also move focus between previously executed items.'),
+		localize('replEditor.focusInOutput', 'The Focus Output command{0} will set focus on the output when focused on a previously executed item.', '<keybinding:notebook.cell.focusInOutput>'),
 	].join('\n');
 }
 
 function getAccessibilityHelpProvider(accessor: ServicesAccessor, editor: ICodeEditor | IVisibleEditorPane) {
 	const helpText = getAccessibilityHelpText();
 	return new AccessibleContentProvider(
-		AccessibleViewProviderId.Notebook,
+		AccessibleViewProviderId.ReplEditor,
 		{ type: AccessibleViewType.Help },
 		() => helpText,
 		() => editor.focus(),
