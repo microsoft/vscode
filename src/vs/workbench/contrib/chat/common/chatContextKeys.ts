62f3158f918296bae463e5cff2bf3e139460e9e9
Harald Kirschner <hkirschner@microsoft.com>
2024-09-23 16:43:19 -0700

    Remove vote-up context/treatment

--- src/vs/workbench/contrib/chat/common/chatContextKeys.ts
+++ src/vs/workbench/contrib/chat/common/chatContextKeys.ts
@@ -8,7 +8,6 @@ import { RawContextKey } from '../../../../platform/contextkey/common/contextkey
 import { ChatAgentLocation } from './chatAgents.js';
 
 export const CONTEXT_RESPONSE_VOTE = new RawContextKey<string>('chatSessionResponseVote', '', { type: 'string', description: localize('interactiveSessionResponseVote', "When the response has been voted up, is set to 'up'. When voted down, is set to 'down'. Otherwise an empty string.") });
-export const CONTEXT_VOTE_UP_ENABLED = new RawContextKey<boolean>('chatVoteUpEnabled', false, { type: 'boolean', description: localize('chatVoteUpEnabled', "True when the chat vote up action is enabled.") });
 export const CONTEXT_RESPONSE_DETECTED_AGENT_COMMAND = new RawContextKey<boolean>('chatSessionResponseDetectedAgentOrCommand', false, { type: 'boolean', description: localize('chatSessionResponseDetectedAgentOrCommand', "When the agent or command was automatically detected") });
 export const CONTEXT_CHAT_RESPONSE_SUPPORT_ISSUE_REPORTING = new RawContextKey<boolean>('chatResponseSupportsIssueReporting', false, { type: 'boolean', description: localize('chatResponseSupportsIssueReporting', "True when the current chat response supports issue reporting.") });
 export const CONTEXT_RESPONSE_FILTERED = new RawContextKey<boolean>('chatSessionResponseFiltered', false, { type: 'boolean', description: localize('chatResponseFiltered', "True when the chat response was filtered out by the server.") });
