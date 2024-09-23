3c704ae65f5214eaa787e9ea10f25793ba3d7d8d
David Dossett <ddossett@microsoft.com>
2024-09-23 11:01:06 -0700

    Update codicons and attach/mention icons

--- src/vs/workbench/contrib/chat/browser/chatWidget.ts
+++ src/vs/workbench/contrib/chat/browser/chatWidget.ts
@@ -504,7 +504,7 @@ export class ChatWidget extends Disposable implements IChatWidget {
 			const messageResult = this._register(renderer.render(welcomeContent.message));
 			dom.append(message, messageResult.element);
 
-			const tipsString = new MarkdownString(localize('chatWidget.tips', "{0} to attach context\n\n{1} to chat with extensions", '$(attach)', '$(plus)'), { supportThemeIcons: true });
+			const tipsString = new MarkdownString(localize('chatWidget.tips', "{0} to attach context\n\n{1} to chat with extensions", '$(attach)', '$(mention)'), { supportThemeIcons: true });
 			const tipsResult = this._register(renderer.render(tipsString));
 			tips.appendChild(tipsResult.element);
 		}
