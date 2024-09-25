e4f49cd3b672831a512180bc6e668484cb6fa28f
Johannes Rieken <johannes.rieken@gmail.com>
2024-09-25 13:11:43 +0200

    fix https://github.com/microsoft/vscode-copilot/issues/8127 (#229698)

--- src/vs/workbench/contrib/inlineChat/browser/inlineChatWidget.ts
+++ src/vs/workbench/contrib/inlineChat/browser/inlineChatWidget.ts
@@ -584,7 +584,13 @@ export class EditorBasedInlineChatWidget extends InlineChatWidget {
 		@IChatService chatService: IChatService,
 		@IHoverService hoverService: IHoverService,
 	) {
-		super(location, { ...options, chatWidgetViewOptions: { ...options.chatWidgetViewOptions, editorOverflowWidgetsDomNode: _parentEditor.getOverflowWidgetsDomNode() } }, instantiationService, contextKeyService, keybindingService, accessibilityService, configurationService, accessibleViewService, textModelResolverService, chatService, hoverService);
+		super(location, {
+			...options,
+			chatWidgetViewOptions: {
+				...options.chatWidgetViewOptions,
+				editorOverflowWidgetsDomNode: _parentEditor.getOverflowWidgetsDomNode() ?? _parentEditor.getDomNode() ?? undefined
+			}
+		}, instantiationService, contextKeyService, keybindingService, accessibilityService, configurationService, accessibleViewService, textModelResolverService, chatService, hoverService);
 	}
 
 	// --- layout
