9784b5e1e62f81587e18ce01f438e3a6e21766e4
Benjamin Pasero <benjamin.pasero@microsoft.com>
2024-09-25 21:48:02 +0200

    esm - restore our copyright header in built files (#229751)

--- build/lib/optimize.ts
+++ build/lib/optimize.ts
@@ -304,7 +304,10 @@ function optimizeESMTask(opts: IOptimizeAMDTaskOpts, cjsOpts?: IOptimizeCommonJS
 			const dest = entryPoint.dest?.replace(/\.[^/.]+$/, '') ?? entryPoint.name;
 
 			// boilerplate massage
-			const banner = { js: '' };
+			const banner = {
+				js: DEFAULT_FILE_HEADER,
+				css: DEFAULT_FILE_HEADER
+			};
 			const tslibPath = path.join(require.resolve('tslib'), '../tslib.es6.js');
 			banner.js += await fs.promises.readFile(tslibPath, 'utf-8');
 
