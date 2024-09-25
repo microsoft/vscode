cd43d54113a2b704783daa5528bd13997677372e
Joyce Er <joyce.er@microsoft.com>
2024-09-25 10:00:49 +0200

    fix: New Chat should clear chat editing widget

--- src/vs/workbench/contrib/chat/browser/chatWidget.ts
+++ src/vs/workbench/contrib/chat/browser/chatWidget.ts
@@ -260,7 +260,7 @@ export class ChatWidget extends Disposable implements IChatWidget {
 			}
 		}));
 		this._register(this.chatEditingService.onDidDisposeEditingSession((session) => {
-			if (session.chatSessionId === this.viewModel?.sessionId) {
+			if (session.chatSessionId === this.viewModel?.sessionId || !this.viewModel) {
 				shouldRerenderEditingStateDisposable?.dispose();
 				this.renderChatEditingSessionState(null);
 			}
