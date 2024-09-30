58695c5524293c79863ac39c296dfba187febe9d
Matt Bierner <matb@microsoft.com>
2024-09-30 15:56:21 -0700

    Add one more suppression

--- src/vs/base/parts/ipc/node/ipc.cp.ts
+++ src/vs/base/parts/ipc/node/ipc.cp.ts
@@ -102,6 +102,7 @@ export class Client implements IChannelClient, IDisposable {
 	getChannel<T extends IChannel>(channelName: string): T {
 		const that = this;
 
+		// eslint-disable-next-line local/code-no-dangerous-type-assertions
 		return {
 			call<T>(command: string, arg?: any, cancellationToken?: CancellationToken): Promise<T> {
 				return that.requestPromise<T>(channelName, command, arg, cancellationToken);
@@ -109,7 +110,7 @@ export class Client implements IChannelClient, IDisposable {
 			listen(event: string, arg?: any) {
 				return that.requestEvent(channelName, event, arg);
 			}
-		};
+		} as T;
 	}
 
 	protected requestPromise<T>(channelName: string, name: string, arg?: any, cancellationToken = CancellationToken.None): Promise<T> {
