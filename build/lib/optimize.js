9b11ef47e730e79e91f569ffb89428755554f00d
Benjamin Pasero <benjamin.pasero@microsoft.com>
2024-09-30 18:08:50 +0200

    esm - inline `minimist`

--- build/lib/optimize.js
+++ build/lib/optimize.js
@@ -64,6 +64,14 @@ function optimizeESMTask(opts) {
                     });
                 }
             };
+            const overrideExternalPlugin = {
+                name: 'override-external',
+                setup(build) {
+                    build.onResolve({ filter: /^minimist$/ }, () => {
+                        return { path: path.join(REPO_ROOT_PATH, 'node_modules', 'minimist', 'index.js'), external: false };
+                    });
+                },
+            };
             const task = esbuild.build({
                 bundle: true,
                 external: entryPoint.exclude,
@@ -71,7 +79,7 @@ function optimizeESMTask(opts) {
                 platform: 'neutral', // makes esm
                 format: 'esm',
                 sourcemap: 'external',
-                plugins: [boilerplateTrimmer],
+                plugins: [boilerplateTrimmer, overrideExternalPlugin],
                 target: ['es2022'],
                 loader: {
                     '.ttf': 'file',
