7c3234f2e695f7b22ac8569303acf7cde3559191
Benjamin Pasero <benjamin.pasero@microsoft.com>
2024-10-02 08:09:17 +0200

    esm - more cleanup

--- src/vs/nls.ts
+++ src/vs/nls.ts
@@ -3,9 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-// eslint-disable-next-line local/code-import-patterns
 import { getNLSLanguage, getNLSMessages } from './nls.messages.js';
-// eslint-disable-next-line local/code-import-patterns
 export { getNLSLanguage, getNLSMessages } from './nls.messages.js';
 
 const isPseudo = getNLSLanguage() === 'pseudo' || (typeof document !== 'undefined' && document.location && typeof document.location.hash === 'string' && document.location.hash.indexOf('pseudo=true') >= 0);
