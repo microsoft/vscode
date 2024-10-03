7cd5677e9401644c3fe72686991336a6ae4a5903
Matt Bierner <matb@microsoft.com>
2024-10-03 13:26:23 -0700

    Add import ext

--- src/vs/base/browser/cssValue.ts
+++ src/vs/base/browser/cssValue.ts
@@ -2,8 +2,8 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-import { FileAccess } from '../common/network';
-import { URI } from '../common/uri';
+import { FileAccess } from '../common/network.js';
+import { URI } from '../common/uri.js';
 
 export function asCssValueWithDefault(cssPropertyValue: string | undefined, dflt: string): string {
 	if (cssPropertyValue !== undefined) {
