3305293ba2a816441925e25561aec0e8a5829a87
Aiday Marlen Kyzy <amarlenkyzy@microsoft.com>
2024-10-03 11:12:17 +0200

    EditContext: Setting the edit context when current window id does not correspond to previous window id (#230368)
    setting the edit context within nativeeditcontext

--- src/vs/editor/browser/view.ts
+++ src/vs/editor/browser/view.ts
@@ -111,8 +111,6 @@ export class View extends ViewEventHandler {
 	private _shouldRecomputeGlyphMarginLanes: boolean = false;
 	private _renderAnimationFrame: IDisposable | null;
 
-	private _targetWindow: CodeWindow | undefined;
-
 	constructor(
 		commandDelegate: ICommandDelegate,
 		configuration: IEditorConfiguration,
@@ -458,14 +456,13 @@ export class View extends ViewEventHandler {
 			throw new BugIndicatingError();
 		}
 		if (this._renderAnimationFrame === null) {
-			const targetWindow = dom.getWindow(this.domNode?.domNode);
-			if (targetWindow !== this._targetWindow && this._editContext instanceof NativeEditContext) {
+			// TODO: workaround fix for https://github.com/microsoft/vscode/issues/229825
+			if (this._editContext instanceof NativeEditContext) {
 				this._editContext.setEditContextOnDomNode();
-				this._targetWindow = targetWindow;
 			}
 			const rendering = this._createCoordinatedRendering();
 			this._renderAnimationFrame = EditorRenderingCoordinator.INSTANCE.scheduleCoordinatedRendering({
-				window: targetWindow,
+				window: dom.getWindow(this.domNode?.domNode),
 				prepareRenderText: () => {
 					if (this._store.isDisposed) {
 						throw new BugIndicatingError();
