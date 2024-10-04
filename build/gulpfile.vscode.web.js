6cf3d00e74b31378e2db210075a2daa2149a4548
Benjamin Pasero <benjamin.pasero@microsoft.com>
2024-10-04 12:49:58 +0200

    esm - better content mapper to address sourcemaps

--- build/gulpfile.vscode.web.js
+++ build/gulpfile.vscode.web.js
@@ -86,76 +86,30 @@ const vscodeWebEntryPoints = [
 ].flat();
 
 /**
- * @param {object} product The parsed product.json file contents
- */
-const createVSCodeWebProductConfigurationPatcher = (product) => {
-	/**
-	 * @param content {string} The contents of the file
-	 * @param path {string} The absolute file path, always using `/`, even on Windows
-	 */
-	const result = (content, path) => {
-		// (1) Patch product configuration
-		if (path.endsWith('vs/platform/product/common/product.js')) {
-			const productConfiguration = JSON.stringify({
-				...product,
-				version,
-				commit,
-				date: readISODate('out-build')
-			});
-			return content.replace('/*BUILD->INSERT_PRODUCT_CONFIGURATION*/', () => productConfiguration.substr(1, productConfiguration.length - 2) /* without { and }*/);
-		}
-
-		return content;
-	};
-	return result;
-};
-
-/**
- * @param extensionsRoot {string} The location where extension will be read from
- */
-const createVSCodeWebBuiltinExtensionsPatcher = (extensionsRoot) => {
-	/**
-	 * @param content {string} The contents of the file
-	 * @param path {string} The absolute file path, always using `/`, even on Windows
-	 */
-	const result = (content, path) => {
-		// (2) Patch builtin extensions
-		if (path.endsWith('vs/workbench/services/extensionManagement/browser/builtinExtensionsScannerService.js')) {
-			const builtinExtensions = JSON.stringify(extensions.scanBuiltinExtensions(extensionsRoot));
-			return content.replace('/*BUILD->INSERT_BUILTIN_EXTENSIONS*/', () => builtinExtensions.substr(1, builtinExtensions.length - 2) /* without [ and ]*/);
-		}
-
-		return content;
-	};
-	return result;
-};
-
-/**
- * @param patchers {((content:string, path: string)=>string)[]}
- */
-const combineContentPatchers = (...patchers) => {
-	/**
-	 * @param content {string} The contents of the file
-	 * @param path {string} The absolute file path, always using `/`, even on Windows
-	 */
-	const result = (content, path) => {
-		for (const patcher of patchers) {
-			content = patcher(content, path);
-		}
-		return content;
-	};
-	return result;
-};
-
-/**
  * @param extensionsRoot {string} The location where extension will be read from
  * @param {object} product The parsed product.json file contents
  */
 const createVSCodeWebFileContentMapper = (extensionsRoot, product) => {
-	return combineContentPatchers(
-		createVSCodeWebProductConfigurationPatcher(product),
-		createVSCodeWebBuiltinExtensionsPatcher(extensionsRoot)
-	);
+	return path => {
+		if (path.endsWith('vs/platform/product/common/product.js')) {
+			return content => {
+				const productConfiguration = JSON.stringify({
+					...product,
+					version,
+					commit,
+					date: readISODate('out-build')
+				});
+				return content.replace('/*BUILD->INSERT_PRODUCT_CONFIGURATION*/', () => productConfiguration.substr(1, productConfiguration.length - 2) /* without { and }*/);
+			};
+		} else if (path.endsWith('vs/workbench/services/extensionManagement/browser/builtinExtensionsScannerService.js')) {
+			return content => {
+				const builtinExtensions = JSON.stringify(extensions.scanBuiltinExtensions(extensionsRoot));
+				return content.replace('/*BUILD->INSERT_BUILTIN_EXTENSIONS*/', () => builtinExtensions.substr(1, builtinExtensions.length - 2) /* without [ and ]*/);
+			};
+		}
+
+		return undefined;
+	};
 };
 exports.createVSCodeWebFileContentMapper = createVSCodeWebFileContentMapper;
 
