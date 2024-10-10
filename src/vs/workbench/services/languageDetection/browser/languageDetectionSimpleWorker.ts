5b8b74e23db5f5ba8a36eb3ffa0d8d672fc23ad3
Innei <tukon479@gmail.com>
2024-10-10 14:16:18 +0800

    chore: typo

--- src/vs/workbench/services/languageDetection/browser/languageDetectionSimpleWorker.ts
+++ src/vs/workbench/services/languageDetection/browser/languageDetectionSimpleWorker.ts
@@ -210,7 +210,7 @@ export class LanguageDetectionSimpleWorker implements ILanguageDetectionWorker {
 			case 'csv':
 			case 'toml':
 				// Other considerations for negativeConfidenceCorrection that
-				// aren't built in but suported by the model include:
+				// aren't built in but suportted by the model include:
 				// * Assembly, TeX - These languages didn't have clear language modes in the community
 				// * Markdown, Dockerfile - These languages are simple but they embed other languages
 				modelResult.confidence -= LanguageDetectionSimpleWorker.negativeConfidenceCorrection;
