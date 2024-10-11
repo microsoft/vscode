17e0cbd391c75893b6a14ea9cdb4202d96c76266
Johannes Rieken <johannes.rieken@gmail.com>
2024-10-11 14:34:41 +0200

    process and apply `textEditGroup` parts as they come (#231110)

--- src/vs/workbench/contrib/chat/browser/chatViewPane.ts
+++ src/vs/workbench/contrib/chat/browser/chatViewPane.ts
@@ -159,7 +159,17 @@ export class ChatViewPane extends ViewPane {
 				ChatWidget,
 				this.chatOptions.location,
 				{ viewId: this.id },
-				{ renderFollowups: this.chatOptions.location === ChatAgentLocation.Panel, supportsFileReferences: true, supportsAdditionalParticipants: this.chatOptions.location === ChatAgentLocation.Panel, rendererOptions: { renderCodeBlockPills: this.chatOptions.location === ChatAgentLocation.EditingSession } },
+				{
+					renderFollowups: this.chatOptions.location === ChatAgentLocation.Panel,
+					supportsFileReferences: true,
+					supportsAdditionalParticipants: this.chatOptions.location === ChatAgentLocation.Panel,
+					rendererOptions: {
+						renderCodeBlockPills: this.chatOptions.location === ChatAgentLocation.EditingSession,
+						renderTextEditsAsSummary: (uri) => {
+							return this.chatOptions.location === ChatAgentLocation.EditingSession;
+						},
+					}
+				},
 				{
 					listForeground: SIDE_BAR_FOREGROUND,
 					listBackground: locationBasedColors.background,
