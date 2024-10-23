3d32c122ed541ca8a6ba161dc995eea301a39e97
Johannes <johannes.rieken@gmail.com>
2024-10-23 11:01:51 +0200

    use `never` for `ChatParticipantToolToken` so that nothing can be assigned to it
    https://github.com/microsoft/vscode/issues/231937

--- src/vscode-dts/vscode.d.ts
+++ src/vscode-dts/vscode.d.ts
@@ -19903,7 +19903,7 @@ declare module 'vscode' {
 	/**
 	 * A token that can be passed to {@link lm.invokeTool} when invoking a tool inside the context of handling a chat request.
 	 */
-	export type ChatParticipantToolToken = unknown;
+	export type ChatParticipantToolToken = never;
 
 	/**
 	 * Options provided for tool invocation.
