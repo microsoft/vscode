c1b45edf6147908b3809245c8c47976fda1c024b
Rob Lourens <roblourens@gmail.com>
2024-10-28 11:03:20 -0700

    Remove backcompat workaround  (#232429)
    Remove backcompat workaround (#232302)
    
    From https://github.com/microsoft/vscode/pull/232023
    Fix #232301

--- src/vs/workbench/api/common/extHostTypes.ts
+++ src/vs/workbench/api/common/extHostTypes.ts
@@ -4663,14 +4663,8 @@ export class LanguageModelToolCallPart implements vscode.LanguageModelToolCallPa
 	parameters: any;
 
 	constructor(callId: string, name: string, input: any) {
-		// TODO TEMP- swapped the order of these two arguments, trying to preserve the behavior for a build or two
-		if (name.startsWith('call_')) {
-			this.name = callId;
-			this.callId = name;
-		} else {
-			this.callId = callId;
-			this.name = name;
-		}
+		this.callId = callId;
+		this.name = name;
 
 		this.input = input;
 		// TODO@API backwards compat, remove
