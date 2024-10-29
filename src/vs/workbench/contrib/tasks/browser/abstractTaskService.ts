ed620031a944f67bb0b7a491e649401496b20cf2
Megan Rogge <merogge@microsoft.com>
2024-10-29 13:20:21 -0500

    move up a line

--- src/vs/workbench/contrib/tasks/browser/abstractTaskService.ts
+++ src/vs/workbench/contrib/tasks/browser/abstractTaskService.ts
@@ -616,11 +616,11 @@ export abstract class AbstractTaskService extends Disposable implements ITaskSer
 	}
 
 	private async _activateTaskProviders(type: string | undefined): Promise<void> {
-		// We need to first wait for extensions to be registered because we might read
-		// the `TaskDefinitionRegistry` in case `type` is `undefined`
 		if (type && this._activatedTaskProviders.has(type)) {
 			return;
 		}
+		// We need to first wait for extensions to be registered because we might read
+		// the `TaskDefinitionRegistry` in case `type` is `undefined`
 		await this._extensionService.whenInstalledExtensionsRegistered();
 		this._log('Activating task providers ' + (type ?? 'all'));
 		const result = await raceTimeout(
