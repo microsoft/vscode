63e5a64a61d75b5f2f26806939ef9c853881d729
Rob Lourens <roblourens@gmail.com>
2024-10-29 16:18:00 -0700

    Fix #232519 (#232540)

--- src/vs/workbench/contrib/chat/browser/contrib/chatInputCompletions.ts
+++ src/vs/workbench/contrib/chat/browser/contrib/chatInputCompletions.ts
@@ -60,6 +60,11 @@ class SlashCommandCompletions extends Disposable {
 					return null;
 				}
 
+				if (!isEmptyUpToCompletionWord(model, range)) {
+					// No text allowed before the completion
+					return;
+				}
+
 				const parsedRequest = widget.parsedInput.parts;
 				const usedAgent = parsedRequest.find(p => p instanceof ChatRequestAgentPart);
 				if (usedAgent) {
@@ -79,7 +84,7 @@ class SlashCommandCompletions extends Disposable {
 							label: withSlash,
 							insertText: c.executeImmediately ? '' : `${withSlash} `,
 							detail: c.detail,
-							range: new Range(1, 1, 1, 1),
+							range,
 							sortText: c.sortText ?? 'a'.repeat(i + 1),
 							kind: CompletionItemKind.Text, // The icons are disabled here anyway,
 							command: c.executeImmediately ? { id: SubmitAction.ID, title: withSlash, arguments: [{ widget, inputValue: `${withSlash} ` }] } : undefined,
@@ -90,7 +95,7 @@ class SlashCommandCompletions extends Disposable {
 		}));
 		this._register(this.languageFeaturesService.completionProvider.register({ scheme: ChatInputPart.INPUT_SCHEME, hasAccessToAllModels: true }, {
 			_debugDisplayName: 'globalSlashCommandsAt',
-			triggerCharacters: ['@'],
+			triggerCharacters: [chatAgentLeader],
 			provideCompletionItems: async (model: ITextModel, position: Position, _context: CompletionContext, _token: CancellationToken) => {
 				const widget = this.chatWidgetService.getWidgetByInputUri(model.uri);
 				if (!widget || !widget.viewModel) {
@@ -100,6 +105,11 @@ class SlashCommandCompletions extends Disposable {
 				const range = computeCompletionRanges(model, position, /@\w*/g);
 				if (!range) {
 					return null;
+				}
+
+				if (!isEmptyUpToCompletionWord(model, range)) {
+					// No text allowed before the completion
+					return;
 				}
 
 				const slashCommands = this.chatSlashCommandService.getCommands(widget.location);
@@ -114,7 +124,7 @@ class SlashCommandCompletions extends Disposable {
 							label: withSlash,
 							insertText: c.executeImmediately ? '' : `${withSlash} `,
 							detail: c.detail,
-							range: new Range(1, 1, 1, 1),
+							range,
 							filterText: `${chatAgentLeader}${c.command}`,
 							sortText: c.sortText ?? 'z'.repeat(i + 1),
 							kind: CompletionItemKind.Text, // The icons are disabled here anyway,
@@ -205,6 +215,11 @@ class AgentCompletions extends Disposable {
 					return null;
 				}
 
+				if (!isEmptyUpToCompletionWord(model, range)) {
+					// No text allowed before the completion
+					return;
+				}
+
 				const agents = this.chatAgentService.getAgents()
 					.filter(a => a.locations.includes(widget.location));
 
@@ -231,7 +246,7 @@ class AgentCompletions extends Disposable {
 							detail,
 							filterText: `${chatAgentLeader}${agent.name}`,
 							insertText: `${agentLabel} `,
-							range: new Range(1, 1, 1, 1),
+							range,
 							kind: CompletionItemKind.Text,
 							sortText: `${chatAgentLeader}${agent.name}`,
 							command: { id: AssignSelectedAgentAction.ID, title: AssignSelectedAgentAction.ID, arguments: [{ agent, widget } satisfies AssignSelectedAgentActionArgs] },
@@ -251,7 +266,7 @@ class AgentCompletions extends Disposable {
 								filterText: getFilterText(agent, c.name),
 								commitCharacters: [' '],
 								insertText: label + ' ',
-								range: new Range(1, 1, 1, 1),
+								range,
 								kind: CompletionItemKind.Text, // The icons are disabled here anyway
 								sortText: `x${chatAgentLeader}${agent.name}${c.name}`,
 								command: { id: AssignSelectedAgentAction.ID, title: AssignSelectedAgentAction.ID, arguments: [{ agent, widget } satisfies AssignSelectedAgentActionArgs] },
@@ -286,6 +301,11 @@ class AgentCompletions extends Disposable {
 					return null;
 				}
 
+				if (!isEmptyUpToCompletionWord(model, range)) {
+					// No text allowed before the completion
+					return;
+				}
+
 				const agents = this.chatAgentService.getAgents()
 					.filter(a => a.locations.includes(widget.location));
 
@@ -300,7 +320,7 @@ class AgentCompletions extends Disposable {
 							commitCharacters: [' '],
 							insertText: `${agentLabel} ${withSlash} `,
 							detail: `(${agentLabel}) ${c.description ?? ''}`,
-							range: new Range(1, 1, 1, 1),
+							range,
 							kind: CompletionItemKind.Text, // The icons are disabled here anyway
 							sortText,
 							command: { id: AssignSelectedAgentAction.ID, title: AssignSelectedAgentAction.ID, arguments: [{ agent, widget } satisfies AssignSelectedAgentActionArgs] },
@@ -333,11 +353,21 @@ class AgentCompletions extends Disposable {
 					return;
 				}
 
+				const range = computeCompletionRanges(model, position, /(@|\/)\w*/g);
+				if (!range) {
+					return;
+				}
+
+				if (!isEmptyUpToCompletionWord(model, range)) {
+					// No text allowed before the completion
+					return;
+				}
+
 				const label = localize('installLabel', "Install Chat Extensions...");
 				const item: CompletionItem = {
 					label,
 					insertText: '',
-					range: new Range(1, 1, 1, 1),
+					range,
 					kind: CompletionItemKind.Text, // The icons are disabled here anyway
 					command: { id: 'workbench.extensions.search', title: '', arguments: ['@tag:chat-participant'] },
 					filterText: chatAgentLeader + label,
@@ -557,7 +587,13 @@ class BuiltinDynamicCompletions extends Disposable {
 
 Registry.as<IWorkbenchContributionsRegistry>(WorkbenchExtensions.Workbench).registerWorkbenchContribution(BuiltinDynamicCompletions, LifecyclePhase.Eventually);
 
-export function computeCompletionRanges(model: ITextModel, position: Position, reg: RegExp, onlyOnWordStart = false): { insert: Range; replace: Range; varWord: IWordAtPosition | null } | undefined {
+export interface IChatCompletionRangeResult {
+	insert: Range;
+	replace: Range;
+	varWord: IWordAtPosition | null;
+}
+
+export function computeCompletionRanges(model: ITextModel, position: Position, reg: RegExp, onlyOnWordStart = false): IChatCompletionRangeResult | undefined {
 	const varWord = getWordAtText(position.column, reg, model.getLineContent(position.lineNumber), 0);
 	if (!varWord && model.getWordUntilPosition(position).word) {
 		// inside a "normal" word
@@ -589,6 +625,11 @@ export function computeCompletionRanges(model: ITextModel, position: Position, r
 	}
 
 	return { insert, replace, varWord };
+}
+
+function isEmptyUpToCompletionWord(model: ITextModel, rangeResult: IChatCompletionRangeResult): boolean {
+	const startToCompletionWordStart = new Range(1, 1, rangeResult.replace.startLineNumber, rangeResult.replace.startColumn);
+	return !!model.getValueInRange(startToCompletionWordStart).match(/^\s*$/);
 }
 
 class VariableCompletions extends Disposable {
