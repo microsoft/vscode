c7fe0659a04f95d4b12e7dbae7b648dba97dd807
Rob Lourens <roblourens@gmail.com>
2024-10-30 15:25:12 -0700

    Add stub lmTools proposal to prevent old extensions from installing in 1.95 (#232670)

--- src/vs/platform/extensions/common/extensionsApiProposals.ts
+++ src/vs/platform/extensions/common/extensionsApiProposals.ts
@@ -235,6 +235,10 @@ const _allApiProposals = {
 	languageStatusText: {
 		proposal: 'https://raw.githubusercontent.com/microsoft/vscode/main/src/vscode-dts/vscode.proposed.languageStatusText.d.ts',
 	},
+	lmTools: {
+		proposal: 'https://raw.githubusercontent.com/microsoft/vscode/main/src/vscode-dts/vscode.proposed.lmTools.d.ts',
+		version: 15
+	},
 	mappedEditsProvider: {
 		proposal: 'https://raw.githubusercontent.com/microsoft/vscode/main/src/vscode-dts/vscode.proposed.mappedEditsProvider.d.ts',
 	},
