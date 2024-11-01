4af859d4fc3dfff768b2db5aa2ff4845a5fdf97a
amunger <aamunger@microsoft.com>
2024-11-01 14:55:22 -0700

    reset kernel affinity for notebooks in REPL

--- src/vs/workbench/contrib/notebook/browser/services/notebookKernelServiceImpl.ts
+++ src/vs/workbench/contrib/notebook/browser/services/notebookKernelServiceImpl.ts
@@ -308,27 +308,35 @@ export class NotebookKernelService extends Disposable implements INotebookKernel
 				this.preselectKernelForNotebook(kernel.kernel, notebook);
 			}
 		}
+
+		// clear affinity for for the resource path since it was used directly from the notebook instance for the REPL
+		if (notebook.uri.scheme === Schemas.untitled) {
+			for (const kernel in preferredKernels) {
+				this.updateKernelNotebookAffinity(kernel, notebook.uri, undefined);
+			}
+		}
 	}
 
-	updateKernelNotebookAffinity(kernel: INotebookKernel, notebook: URI, preference: number | undefined): void {
-		const info = this._kernels.get(kernel.id);
+	updateKernelNotebookAffinity(kernel: INotebookKernel | string, notebook: URI, preference: number | undefined): void {
+		const kernelId = typeof kernel === 'string' ? kernel : kernel.id;
+		const info = this._kernels.get(kernelId);
 		if (!info) {
-			throw new Error(`UNKNOWN kernel '${kernel.id}'`);
+			throw new Error(`UNKNOWN kernel '${kernelId}'`);
 		}
 		if (preference === undefined) {
 			info.notebookPriorities.delete(notebook);
 		} else {
 			info.notebookPriorities.set(notebook, preference);
 		}
-		this.setReplKernelAffinity(kernel, notebook, preference);
+		this.setReplKernelAffinity(kernelId, notebook, preference);
 		this._onDidChangeNotebookAffinity.fire();
 	}
 
-	setReplKernelAffinity(kernel: INotebookKernel, notebook: URI, preference: number | undefined): void {
+	setReplKernelAffinity(kernelId: string, notebook: URI, preference: number | undefined): void {
 		if (preference === 2) {
-			this._notebookService.getNotebookTextModel(notebook)?.preferredReplKernels.add(kernel.id);
+			this._notebookService.getNotebookTextModel(notebook)?.preferredReplKernels.add(kernelId);
 		} else {
-			this._notebookService.getNotebookTextModel(notebook)?.preferredReplKernels.delete(kernel.id);
+			this._notebookService.getNotebookTextModel(notebook)?.preferredReplKernels.delete(kernelId);
 		}
 	}
 
