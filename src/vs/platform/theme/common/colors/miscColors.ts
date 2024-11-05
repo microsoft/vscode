cdd03ef969205d031c1cc1114242b30787d94bec
Connor Peet <connor@peet.io>
2024-11-05 15:35:37 -0800

    testing: try out new 'badges' UI for test failures (#233137)
    * badge wip
    
    * badge wip
    
    * testing: try out new 'badges' UI for test failures
    
    I'm exploring some UI to make tests look better in already-busy
    scenarios, like Copilot edits. We've long had these big inline message
    decorations which get very noisy quickly, especially with word wrap on.
    I'm exploring these smaller 'badge' type indicators. These are shown in
    at most one place per test and their content length is very limited.
    Clicking on them opens the traditional test error peek. Because I feel
    like they are easier to see and more inviting to click, I turned off the
    default behavior of opening the error peek. Making any edits in the test
    case will dismiss the badge.
    
    Closes #233028

--- src/vs/platform/theme/common/colors/miscColors.ts
+++ src/vs/platform/theme/common/colors/miscColors.ts
@@ -30,6 +30,22 @@ export const badgeForeground = registerColor('badge.foreground',
 	{ dark: Color.white, light: '#333', hcDark: Color.white, hcLight: Color.white },
 	nls.localize('badgeForeground', "Badge foreground color. Badges are small information labels, e.g. for search results count."));
 
+export const activityWarningBadgeForeground = registerColor('activityWarningBadge.foreground',
+	{ dark: Color.black.lighten(0.2), light: Color.white, hcDark: null, hcLight: Color.black.lighten(0.2) },
+	nls.localize('activityWarningBadge.foreground', 'Foreground color of the warning activity badge'));
+
+export const activityWarningBadgeBackground = registerColor('activityWarningBadge.background',
+	{ dark: '#CCA700', light: '#BF8803', hcDark: null, hcLight: '#CCA700' },
+	nls.localize('activityWarningBadge.background', 'Background color of the warning activity badge'));
+
+export const activityErrorBadgeForeground = registerColor('activityErrorBadge.foreground',
+	{ dark: Color.black.lighten(0.2), light: Color.white, hcDark: null, hcLight: Color.black.lighten(0.2) },
+	nls.localize('activityErrorBadge.foreground', 'Foreground color of the error activity badge'));
+
+export const activityErrorBadgeBackground = registerColor('activityErrorBadge.background',
+	{ dark: '#F14C4C', light: '#E51400', hcDark: null, hcLight: '#F14C4C' },
+	nls.localize('activityErrorBadge.background', 'Background color of the error activity badge'));
+
 
 // ----- scrollbar
 
