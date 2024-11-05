f6b354000096b9ae87bfc82e7b5ad60c64d4a1cc
Daniel Imms <2193314+Tyriar@users.noreply.github.com>
2024-11-05 06:44:43 -0800

    Fix link in ligatures setting

--- src/vs/workbench/contrib/terminal/common/terminalConfiguration.ts
+++ src/vs/workbench/contrib/terminal/common/terminalConfiguration.ts
@@ -172,7 +172,7 @@ const terminalConfiguration: IConfigurationNode = {
 			type: 'string'
 		},
 		[TerminalSettingId.FontLigatures]: {
-			description: localize('terminal.integrated.fontLigatures', "Controls whether font ligatures are enabled in the terminal. Ligatures will only work if the configured {0} supports them.", `\`#${TerminalSettingId.FontFamily}#\``),
+			markdownDescription: localize('terminal.integrated.fontLigatures', "Controls whether font ligatures are enabled in the terminal. Ligatures will only work if the configured {0} supports them.", `\`#${TerminalSettingId.FontFamily}#\``),
 			type: 'boolean',
 			tags: ['preview'],
 			default: false
