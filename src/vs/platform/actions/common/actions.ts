a970a0d3cc96fc212beff3f795806d9f16f0e0e9
Johannes Rieken <johannes.rieken@gmail.com>
2024-11-05 18:27:09 +0100

    add/expose keybindings for chat overlay commands (#233113)

--- src/vs/platform/actions/common/actions.ts
+++ src/vs/platform/actions/common/actions.ts
@@ -226,6 +226,7 @@ export class MenuId {
 	static readonly ChatInput = new MenuId('ChatInput');
 	static readonly ChatInputSide = new MenuId('ChatInputSide');
 	static readonly ChatEditingWidgetToolbar = new MenuId('ChatEditingWidgetToolbar');
+	static readonly ChatEditingEditorContent = new MenuId('ChatEditingEditorContent');
 	static readonly ChatEditingWidgetModifiedFilesToolbar = new MenuId('ChatEditingWidgetModifiedFilesToolbar');
 	static readonly ChatInlineResourceAnchorContext = new MenuId('ChatInlineResourceAnchorContext');
 	static readonly ChatInlineSymbolAnchorContext = new MenuId('ChatInlineSymbolAnchorContext');
