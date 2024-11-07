1103b51d62168c26bb3920e3bdebe61f3a3da6cc
Megan Rogge <merogge@microsoft.com>
2024-11-07 14:23:54 -0600

    re-record mac, move onDidShow out of timeout so ctxt key gets set immediately

--- src/vs/workbench/contrib/terminalContrib/suggest/test/browser/recordings/macos_pwsh_getcontent_file.ts
+++ src/vs/workbench/contrib/terminalContrib/suggest/test/browser/recordings/macos_pwsh_getcontent_file.ts
@@ -6,7 +6,7 @@
 export const events = [
 	{
 		"type": "resize",
-		"cols": 72,
+		"cols": 111,
 		"rows": 29
 	},
 	{
@@ -51,7 +51,7 @@ export const events = [
 	},
 	{
 		"type": "output",
-		"data": "\u001b[?25l\u001b[1;36H\u001b[0m\u001b[93mg\u001b[0m\u001b[97;2;3mit stash\u001b[0m\u001b[39;49m\u001b[0m\u001b[1;37H\u001b[?12l\u001b[?25h\u001b[6n"
+		"data": "\u001b[?25l\u001b[1;36H\u001b[0m\u001b[93mg\u001b[0m\u001b[97;2;3mit show\u001b[0m\u001b[39;49m\u001b[0m\u001b[1;37H\u001b[?12l\u001b[?25h\u001b[6n"
 	},
 	{
 		"type": "input",
@@ -59,19 +59,19 @@ export const events = [
 	},
 	{
 		"type": "promptInputChange",
-		"data": "g|[it stash]"
+		"data": "g|[it show]"
 	},
 	{
 		"type": "sendText",
 		"data": "\u001b[24~e"
 	},
 	{
-		"type": "input",
-		"data": "e"
-	},
-	{
 		"type": "output",
 		"data": "\u001b[39;49m\u001b[39;49m\u001b]633;Completions;;0;1;[[\"./gulpfile.js\",3,\"/Users/meganrogge/Repos/vscode/gulpfile.js\"],[\"${$}\",9,\"$\"],[\"$?\",9,\"?\"],[\"$args\",9,\"args\"],[\"$commandLine\",9,\"commandLine\"],[\"$completionPrefix\",9,\"completionPrefix\"],[\"$ConfirmPreference\",9,\"ConfirmPreference\"],[\"$ContinuationPrompt\",9,\"ContinuationPrompt\"],[\"$cursorIndex\",9,\"cursorIndex\"],[\"$DebugPreference\",9,\"DebugPreference\"],[\"$EnabledExperimentalFeatures\",9,\"EnabledExperimentalFeatures\"],[\"$Error\",9,\"Error\"],[\"$ErrorActionPreference\",9,\"ErrorActionPreference\"],[\"$ErrorView\",9,\"ErrorView\"],[\"$ExecutionContext\",9,\"ExecutionContext\"],[\"$false\",9,\"false\"],[\"$FormatEnumerationLimit\",9,\"FormatEnumerationLimit\"],[\"$HOME\",9,\"HOME\"],[\"$Host\",9,\"Host\"],[\"$InformationPreference\",9,\"InformationPreference\"],[\"$input\",9,\"input\"],[\"$IsCoreCLR\",9,\"IsCoreCLR\"],[\"$IsLinux\",9,\"IsLinux\"],[\"$IsMacOS\",9,\"IsMacOS\"],[\"$isStable\",9,\"isStable\"],[\"$IsWindows\",9,\"IsWindows\"],[\"$isWindows10\",9,\"isWindows10\"],[\"$MaximumHistoryCount\",9,\"MaximumHistoryCount\"],[\"$MyInvocation\",9,\"MyInvocation\"],[\"$NestedPromptLevel\",9,\"NestedPromptLevel\"],[\"$Nonce\",9,\"Nonce\"],[\"$null\",9,\"null\"],[\"$osVersion\",9,\"osVersion\"],[\"$OutputEncoding\",9,\"OutputEncoding\"],[\"$PID\",9,\"PID\"],[\"$prefixCursorDelta\",9,\"prefixCursorDelta\"],[\"$PROFILE\",9,\"PROFILE\"],[\"$ProgressPreference\",9,\"ProgressPreference\"],[\"$PSBoundParameters\",9,\"PSBoundParameters\"],[\"$PSCommandPath\",9,\"PSCommandPath\"],[\"$PSCulture\",9,\"PSCulture\"],[\"$PSDefaultParameterValues\",9,\"PSDefaultParameterValues\"],[\"$PSEdition\",9,\"PSEdition\"],[\"$PSEmailServer\",9,\"PSEmailServer\"],[\"$PSHOME\",9,\"PSHOME\"],[\"$PSNativeCommandArgumentPassing\",9,\"PSNativeCommandArgumentPassing\"],[\"$PSNativeCommandUseErrorActionPreference\",9,\"PSNativeCommandUseErrorActionPreference\"],[\"$PSScriptRoot\",9,\"PSScriptRoot\"],[\"$PSSessionApplicationName\",9,\"PSSessionApplicationName\"],[\"$PSSessionConfigurationName\",9,\"PSSessionConfigurationName\"],[\"$PSSessionOption\",9,\"PSSessionOption\"],[\"$PSStyle\",9,\"PSStyle\"],[\"$PSUICulture\",9,\"PSUICulture\"],[\"$PSVersionTable\",9,\"PSVersionTable\"],[\"$PWD\",9,\"PWD\"],[\"$result\",9,\"result\"],[\"$ShellId\",9,\"ShellId\"],[\"$StackTrace\",9,\"StackTrace\"],[\"$true\",9,\"true\"],[\"$VerbosePreference\",9,\"VerbosePreference\"],[\"$WarningPreference\",9,\"WarningPreference\"],[\"$WhatIfPreference\",9,\"WhatIfPreference\"],[\"${^}\",9,\"^\"],[\"$__LastHistoryId\",9,\"__LastHistoryId\"],[\"$__VSCodeOriginalPrompt\",9,\"__VSCodeOriginalPrompt\"],[\"$__VSCodeOriginalPSConsoleHostReadLine\",9,\"__VSCodeOriginalPSConsoleHostReadLine\"],[\"$env:APPLICATIONINSIGHTS_CONFIGURATION_CONTENT\",9,\"[string]env:APPLICATIONINSIGHTS_CONFIGURATION_CONTENT\"],[\"$env:COLORTERM\",9,\"[string]env:COLORTERM\"],[\"$env:COMMAND_MODE\",9,\"[string]env:COMMAND_MODE\"],[\"$env:GIT_ASKPASS\",9,\"[string]env:GIT_ASKPASS\"],[\"$env:HOME\",9,\"[string]env:HOME\"],[\"$env:HOMEBREW_CELLAR\",9,\"[string]env:HOMEBREW_CELLAR\"],[\"$env:HOMEBREW_PREFIX\",9,\"[string]env:HOMEBREW_PREFIX\"],[\"$env:HOMEBREW_REPOSITORY\",9,\"[string]env:HOMEBREW_REPOSITORY\"],[\"$env:INFOPATH\",9,\"[string]env:INFOPATH\"],[\"$env:LANG\",9,\"[string]env:LANG\"],[\"$env:LOGNAME\",9,\"[string]env:LOGNAME\"],[\"$env:MallocNanoZone\",9,\"[string]env:MallocNanoZone\"],[\"$env:NODE_ENV\",9,\"[string]env:NODE_ENV\"],[\"$env:NODE_OPTIONS\",9,\"[string]env:NODE_OPTIONS\"],[\"$env:OLDPWD\",9,\"[string]env:OLDPWD\"],[\"$env:ORIGINAL_XDG_CURRENT_DESKTOP\",9,\"[string]env:ORIGINAL_XDG_CURRENT_DESKTOP\"],[\"$env:PATH\",9,\"[string]env:PATH\"],[\"$env:PSModulePath\",9,\"[string]env:PSModulePath\"],[\"$env:PWD\",9,\"[string]env:PWD\"],[\"$env:SHELL\",9,\"[string]env:SHELL\"],[\"$env:SHLVL\",9,\"[string]env:SHLVL\"],[\"$env:SSH_AUTH_SOCK\",9,\"[string]env:SSH_AUTH_SOCK\"],[\"$env:TERM\",9,\"[string]env:TERM\"],[\"$env:TERM_PROGRAM\",9,\"[string]env:TERM_PROGRAM\"],[\"$env:TERM_PROGRAM_VERSION\",9,\"[string]env:TERM_PROGRAM_VERSION\"],[\"$env:TMPDIR\",9,\"[string]env:TMPDIR\"],[\"$env:USER\",9,\"[string]env:USER\"],[\"$env:VSCODE_GIT_ASKPASS_EXTRA_ARGS\",9,\"[string]env:VSCODE_GIT_ASKPASS_EXTRA_ARGS\"],[\"$env:VSCODE_GIT_ASKPASS_MAIN\",9,\"[string]env:VSCODE_GIT_ASKPASS_MAIN\"],[\"$env:VSCODE_GIT_ASKPASS_NODE\",9,\"[string]env:VSCODE_GIT_ASKPASS_NODE\"],[\"$env:VSCODE_GIT_IPC_HANDLE\",9,\"[string]env:VSCODE_GIT_IPC_HANDLE\"],[\"$env:VSCODE_INJECTION\",9,\"[string]env:VSCODE_INJECTION\"],[\"$env:VSCODE_INSPECTOR_OPTIONS\",9,\"[string]env:VSCODE_INSPECTOR_OPTIONS\"],[\"$env:XPC_FLAGS\",9,\"[string]env:XPC_FLAGS\"],[\"$env:XPC_SERVICE_NAME\",9,\"[string]env:XPC_SERVICE_NAME\"],[\"$env:__CFBundleIdentifier\",9,\"[string]env:__CFBundleIdentifier\"],[\"$env:__CF_USER_TEXT_ENCODING\",9,\"[string]env:__CF_USER_TEXT_ENCODING\"],[\"$CurrentlyExecutingCommand\",9,\"CurrentlyExecutingCommand\"],[\"$foreach\",9,\"foreach\"],[\"$LASTEXITCODE\",9,\"LASTEXITCODE\"],[\"$LogCommandHealthEvent\",9,\"LogCommandHealthEvent\"],[\"$LogCommandLifecycleEvent\",9,\"LogCommandLifecycleEvent\"],[\"$LogEngineHealthEvent\",9,\"LogEngineHealthEvent\"],[\"$LogEngineLifecycleEvent\",9,\"LogEngineLifecycleEvent\"],[\"$LogProviderHealthEvent\",9,\"LogProviderHealthEvent\"],[\"$LogProviderLifecycleEvent\",9,\"LogProviderLifecycleEvent\"],[\"$LogSettingsEvent\",9,\"LogSettingsEvent\"],[\"$Matches\",9,\"Matches\"],[\"$OFS\",9,\"OFS\"],[\"$PSCmdlet\",9,\"PSCmdlet\"],[\"$PSDebugContext\",9,\"PSDebugContext\"],[\"$PSItem\",9,\"PSItem\"],[\"$PSLogUserData\",9,\"PSLogUserData\"],[\"$PSModuleAutoLoadingPreference\",9,\"PSModuleAutoLoadingPreference\"],[\"$PSSenderInfo\",9,\"PSSenderInfo\"],[\"$switch\",9,\"switch\"],[\"$this\",9,\"this\"],[\"$VerboseHelpErrors\",9,\"VerboseHelpErrors\"],[\"$_\",9,\"_\"],[\"$Alias:\",9,\"Drive that contains a view of the aliases stored in a session state\"],[\"$Env:\",9,\"Drive that contains a view of the environment variables for the process\"],[\"$Function:\",9,\"Drive that contains a view of the functions stored in a session state\"],[\"$Temp:\",9,\"Drive that maps to the temporary directory path for the current user\"],[\"$Variable:\",9,\"Drive that contains a view of those variables stored in a session state\"],[\"$Global:\",9,\"Global:\"],[\"$Local:\",9,\"Local:\"],[\"$Script:\",9,\"Script:\"],[\"$Private:\",9,\"Private:\"]]\u0007\u001b[39;49m\u001b[39;49m"
+	},
+	{
+		"type": "input",
+		"data": "e"
 	},
 	{
 		"type": "output",
@@ -83,7 +83,7 @@ export const events = [
 	},
 	{
 		"type": "output",
-		"data": "\u001b[?25l\u001b[1;36H\u001b[0m\u001b[93mge\u001b[0m\u001b[97;2;3mt-Al\u001b[0m\u001b[39;49m   \u001b[0m\u001b[1;38H\u001b[?12l\u001b[?25h\u001b[6n"
+		"data": "\u001b[?25l\u001b[1;36H\u001b[0m\u001b[93mge\u001b[0m\u001b[97;2;3mt-Al\u001b[0m\u001b[39;49m  \u001b[0m\u001b[1;38H\u001b[?12l\u001b[?25h\u001b[6n"
 	},
 	{
 		"type": "input",
@@ -195,7 +195,7 @@ export const events = [
 	},
 	{
 		"type": "output",
-		"data": "\u001b[?25l\u001b[1;36H\u001b[0m\u001b[93mG\u001b[0m\u001b[97;2;3mit stash\u001b[0m\u001b[39;49m\u001b[0m\u001b[1;37H\u001b[?12l\u001b[?25h\u001b[6n"
+		"data": "\u001b[?25l\u001b[1;36H\u001b[0m\u001b[93mG\u001b[0m\u001b[97;2;3mit show\u001b[0m\u001b[39;49m \u001b[0m\u001b[1;37H\u001b[?12l\u001b[?25h\u001b[6n"
 	},
 	{
 		"type": "input",
@@ -203,11 +203,11 @@ export const events = [
 	},
 	{
 		"type": "promptInputChange",
-		"data": "G|[it stash]"
+		"data": "G|[it show]"
 	},
 	{
 		"type": "output",
-		"data": "\u001b[?25l\u001b[1;36H\u001b[0m\u001b[93mGe\u001b[0m\u001b[97;2;3mt-Al\u001b[0m\u001b[39;49m   \u001b[0m\u001b[1;38H\u001b[?12l\u001b[?25h\u001b[6n"
+		"data": "\u001b[?25l\u001b[1;36H\u001b[0m\u001b[93mGe\u001b[0m\u001b[97;2;3mt-Al\u001b[0m\u001b[39;49m  \u001b[0m\u001b[1;38H\u001b[?12l\u001b[?25h\u001b[6n"
 	},
 	{
 		"type": "input",
