bbda1a6bd8818ce3f08976f88df8a6002aece3a2
amunger <aamunger@microsoft.com>
2024-11-07 15:28:54 -0800

    SR command to read last REPL execution

---
+++ src/vs/workbench/contrib/notebook/browser/viewModel/outputHelper.ts
@@ -0,0 +1,57 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { NotebookTextModel } from '../../common/model/notebookTextModel.js';
+import { ICellViewModel } from '../notebookBrowser.js';
+
+export function getOutputText(notebook: NotebookTextModel, viewCell: ICellViewModel): string {
+	let outputContent = '';
+	const decoder = new TextDecoder();
+	for (let i = 0; i < viewCell.outputsViewModels.length; i++) {
+		const outputViewModel = viewCell.outputsViewModels[i];
+		const outputTextModel = viewCell.model.outputs[i];
+		const [mimeTypes, pick] = outputViewModel.resolveMimeTypes(notebook, undefined);
+		const mimeType = mimeTypes[pick].mimeType;
+		let buffer = outputTextModel.outputs.find(output => output.mime === mimeType);
+
+		if (!buffer || mimeType.startsWith('image')) {
+			buffer = outputTextModel.outputs.find(output => !output.mime.startsWith('image'));
+		}
+
+		let text = `${mimeType}`; // default in case we can't get the text value for some reason.
+		if (buffer) {
+			const charLimit = 100_000;
+			text = decoder.decode(buffer.data.slice(0, charLimit).buffer);
+
+			if (buffer.data.byteLength > charLimit) {
+				text = text + '...(truncated)';
+			}
+
+			if (mimeType.endsWith('error')) {
+				text = text.replace(/\\u001b\[[0-9;]*m/gi, '').replaceAll('\\n', '\n');
+			}
+		}
+
+		const index = viewCell.outputsViewModels.length > 1
+			? `Cell output ${i + 1} of ${viewCell.outputsViewModels.length}\n`
+			: '';
+		outputContent = outputContent.concat(`${index}${text}\n`);
+	}
+	return outputContent.trim();
+}
+
+export const TEXT_BASED_MIMETYPES = [
+	'text/latex',
+	'text/html',
+	'application/vnd.code.notebook.error',
+	'application/vnd.code.notebook.stdout',
+	'application/x.notebook.stdout',
+	'application/x.notebook.stream',
+	'application/vnd.code.notebook.stderr',
+	'application/x.notebook.stderr',
+	'text/plain',
+	'text/markdown',
+	'application/json'
+];
--- src/vs/workbench/contrib/replNotebook/browser/replEditor.ts
+++ src/vs/workbench/contrib/replNotebook/browser/replEditor.ts
@@ -59,7 +59,6 @@ import { ReplInputHintContentWidget } from '../../interactive/browser/replInputH
 import { ServiceCollection } from '../../../../platform/instantiation/common/serviceCollection.js';
 import { ICodeEditor } from '../../../../editor/browser/editorBrowser.js';
 import { localize } from '../../../../nls.js';
-import { NotebookViewModel } from '../../notebook/browser/viewModel/notebookViewModelImpl.js';
 import { IAccessibilityService } from '../../../../platform/accessibility/common/accessibility.js';
 
 const INTERACTIVE_EDITOR_VIEW_STATE_PREFERENCE_KEY = 'InteractiveEditorViewState';
@@ -543,26 +542,10 @@ export class ReplEditor extends EditorPane implements IEditorPaneWithScrolling {
 			if (addedCells.length) {
 				const viewModel = notebookWidget.viewModel;
 				if (viewModel) {
-					this.handleAppend(notebookWidget, viewModel);
+					this._notebookWidgetService.updateReplContextKey(viewModel.notebookDocument.uri.toString());
 					break;
 				}
 			}
-		}
-	}
-
-	private handleAppend(notebookWidget: NotebookEditorWidget, viewModel: NotebookViewModel) {
-		this._notebookWidgetService.updateReplContextKey(viewModel.notebookDocument.uri.toString());
-		const navigateToCell = this._configurationService.getValue(ReplEditorSettings.autoFocusAppendedCell);
-		if ((this._accessibilityService.isScreenReaderOptimized() && navigateToCell !== 'never')
-			|| navigateToCell === 'always') {
-
-			setTimeout(() => {
-				const lastCellIndex = viewModel.length - 1;
-				if (lastCellIndex >= 0) {
-					const cell = viewModel.viewCells[lastCellIndex];
-					notebookWidget.focusNotebookCell(cell, 'container');
-				}
-			}, 0);
 		}
 	}
 
