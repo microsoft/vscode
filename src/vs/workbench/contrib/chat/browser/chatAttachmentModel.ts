37d40fce4c1555d5ab5d85bbac33efbcb86d8ba3
Rob Lourens <roblourens@gmail.com>
2024-11-15 23:32:11 -0800

    Add telemetry for chat attachments (#233973)

--- src/vs/workbench/contrib/chat/browser/chatAttachmentModel.ts
+++ src/vs/workbench/contrib/chat/browser/chatAttachmentModel.ts
@@ -41,7 +41,7 @@ export class ChatAttachmentModel extends Disposable {
 		this.addContext(this.asVariableEntry(uri, range));
 	}
 
-	asVariableEntry(uri: URI, range?: IRange) {
+	asVariableEntry(uri: URI, range?: IRange): IChatRequestVariableEntry {
 		return {
 			value: range ? { uri, range } : uri,
 			id: uri.toString() + (range?.toString() ?? ''),
