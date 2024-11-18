ffab3e639e72aab06955f4bf63444c3f8fa4d102
Johannes Rieken <johannes.rieken@gmail.com>
2024-11-18 17:00:58 +0100

    fix an issue with navigating changes (#234110)

--- src/vs/workbench/contrib/chat/browser/chatEditorActions.ts
+++ src/vs/workbench/contrib/chat/browser/chatEditorActions.ts
@@ -95,7 +95,7 @@ abstract class NavigateAction extends Action2 {
 		}
 
 		const entry = entries[newIdx];
-		const change = entry.diffInfo.get().changes.at(0);
+		const change = entry.diffInfo.get().changes.at(this.next ? 0 : -1);
 
 		return editorService.openEditor({
 			resource: entry.modifiedURI,
