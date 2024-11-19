b174c27d901a35b66cd7d744ce31eba3e38da2b2
Megan Rogge <merogge@microsoft.com>
2024-11-19 08:31:06 -0500

    fix #234184

--- src/vs/workbench/contrib/terminalContrib/chat/browser/terminalChatWidget.ts
+++ src/vs/workbench/contrib/terminalContrib/chat/browser/terminalChatWidget.ts
@@ -157,6 +157,10 @@ export class TerminalChatWidget extends Disposable {
 		this._register(this._focusTracker.onDidFocus(() => this._focusedContextKey.set(true)));
 		this._register(this._focusTracker.onDidBlur(() => this._focusedContextKey.set(false)));
 
+		this._requestActiveContextKey = TerminalChatContextKeys.requestActive.bindTo(this._contextKeyService);
+		this._responseContainsCodeBlockContextKey = TerminalChatContextKeys.responseContainsCodeBlock.bindTo(this._contextKeyService);
+		this._responseContainsMulitpleCodeBlocksContextKey = TerminalChatContextKeys.responseContainsMultipleCodeBlocks.bindTo(this._contextKeyService);
+
 		this._register(autorun(r => {
 			const isBusy = this._inlineChatWidget.requestInProgress.read(r);
 			this._container.classList.toggle('busy', isBusy);
@@ -179,10 +183,6 @@ export class TerminalChatWidget extends Disposable {
 		}));
 
 		this.hide();
-
-		this._requestActiveContextKey = TerminalChatContextKeys.requestActive.bindTo(this._contextKeyService);
-		this._responseContainsCodeBlockContextKey = TerminalChatContextKeys.responseContainsCodeBlock.bindTo(this._contextKeyService);
-		this._responseContainsMulitpleCodeBlocksContextKey = TerminalChatContextKeys.responseContainsMultipleCodeBlocks.bindTo(this._contextKeyService);
 	}
 
 	private _dimension?: Dimension;
