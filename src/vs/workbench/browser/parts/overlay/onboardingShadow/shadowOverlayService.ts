b7bf561711b701d39d3e877c56abbc972f97aea7
John Pan <john.pan@mail.mcgill.ca>
2024-11-18 19:24:35 -0800

    remove transparent bg for highlight (#124)

--- src/vs/workbench/browser/parts/overlay/onboardingShadow/shadowOverlayService.ts
+++ src/vs/workbench/browser/parts/overlay/onboardingShadow/shadowOverlayService.ts
@@ -1,54 +1,72 @@
-import { createDecorator } from 'vs/platform/instantiation/common/instantiation';
-import { registerSingleton, InstantiationType } from 'vs/platform/instantiation/common/extensions';
-import { Disposable, IDisposable } from 'vs/base/common/lifecycle';
-import { CommandsRegistry } from 'vs/platform/commands/common/commands';
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+import { createDecorator } from "vs/platform/instantiation/common/instantiation";
+import {
+	registerSingleton,
+	InstantiationType,
+} from "vs/platform/instantiation/common/extensions";
+import { Disposable, IDisposable } from "vs/base/common/lifecycle";
+import { CommandsRegistry } from "vs/platform/commands/common/commands";
 
-export const IShadowOverlayService = createDecorator<IShadowOverlayService>('shadowOverlayService');
+export const IShadowOverlayService = createDecorator<IShadowOverlayService>(
+	"shadowOverlayService",
+);
 
 export interface IShadowOverlayService extends IDisposable {
-    readonly _serviceBrand: undefined;
+	readonly _serviceBrand: undefined;
 
-    highlight(elements: string[]): void;
+	highlight(elements: string[]): void;
 	restoreStyles(elements: string[]): void;
 }
 
+export class ShadowOverlayService
+	extends Disposable
+	implements IShadowOverlayService
+{
+	declare readonly _serviceBrand: undefined;
+	private highlightedElements: Map<
+		string,
+		{
+			position: string;
+			zIndex: string;
+			isolation: string;
+			pointerEvents: string;
+			backgroundColor: string;
+			boxShadow: string;
+		}
+	> = new Map();
 
-export class ShadowOverlayService extends Disposable implements IShadowOverlayService {
-    declare readonly _serviceBrand: undefined;
-	private highlightedElements: Map<string, {
-        position: string;
-        zIndex: string;
-        isolation: string;
-        pointerEvents: string;
-        backgroundColor: string;
-        boxShadow: string;
-    }> = new Map();
-
-    constructor(
-    ) {
+	constructor() {
 		super();
 		this.registerCommands();
 	}
 
 	private registerCommands(): void {
-		CommandsRegistry.registerCommand('pearai.highlightElements', (accessor, ...args) => {
+		CommandsRegistry.registerCommand(
+			"pearai.highlightElements",
+			(accessor, ...args) => {
+				const selectors = args[0] as string[]; // array of CSS selectors
+				this.highlight(selectors);
+			},
+		);
 
-			const selectors = args[0] as string[];  // array of CSS selectors
-			this.highlight(selectors);
-		});
-
-		CommandsRegistry.registerCommand('pearai.removeHighlight', (accessor, ...args) => {
-			const selectors = args[0] as string[];  // array of CSS selectors
-			 // Convert selectors to elements
-			this.restoreStyles(selectors);
-		});
+		CommandsRegistry.registerCommand(
+			"pearai.removeHighlight",
+			(accessor, ...args) => {
+				const selectors = args[0] as string[]; // array of CSS selectors
+				// Convert selectors to elements
+				this.restoreStyles(selectors);
+			},
+		);
 	}
 
 	restoreStyles(selectors: string[]): void {
-		selectors.forEach(selector => {
+		selectors.forEach((selector) => {
 			const originalStyles = this.highlightedElements.get(selector);
 
-			const element = document.querySelector(selector) as HTMLElement
+			const element = document.querySelector(selector) as HTMLElement;
 
 			if (originalStyles) {
 				element.style.position = originalStyles.position;
@@ -65,30 +83,33 @@ export class ShadowOverlayService extends Disposable implements IShadowOverlaySe
 	}
 
 	highlight(selectors: string[]): void {
-        selectors.forEach(selector => {
-            if (selector) {
-				const element = document.querySelector(selector) as HTMLElement
+		selectors.forEach((selector) => {
+			if (selector) {
+				const element = document.querySelector(selector) as HTMLElement;
 
 				// save original styles
 				if (!this.highlightedElements.has(selector)) {
-                    this.highlightedElements.set(selector, {
-                        position: element.style.position,
-                        zIndex: element.style.zIndex,
-                        isolation: element.style.isolation,
-                        pointerEvents: element.style.pointerEvents,
-                        backgroundColor: element.style.backgroundColor,
-                        boxShadow: element.style.boxShadow,
-                    });
-                }
-                element.style.position = 'absolute';
-                element.style.zIndex = '3000';
-				element.style.isolation = 'isolate';
-				element.style.pointerEvents = 'auto';
-				element.style.backgroundColor = 'transparent';
-            	element.style.boxShadow = '0 0 0 9999px rgba(0, 0, 0, 0.6)';
-            }
-        });
-    }
+					this.highlightedElements.set(selector, {
+						position: element.style.position,
+						zIndex: element.style.zIndex,
+						isolation: element.style.isolation,
+						pointerEvents: element.style.pointerEvents,
+						backgroundColor: element.style.backgroundColor,
+						boxShadow: element.style.boxShadow,
+					});
+				}
+				element.style.position = "absolute";
+				element.style.zIndex = "3000";
+				element.style.isolation = "isolate";
+				element.style.pointerEvents = "auto";
+				element.style.boxShadow = "0 0 0 9999px rgba(0, 0, 0, 0.6)";
+			}
+		});
+	}
 }
 
-registerSingleton(IShadowOverlayService, ShadowOverlayService, InstantiationType.Eager,);
+registerSingleton(
+	IShadowOverlayService,
+	ShadowOverlayService,
+	InstantiationType.Eager,
+);
