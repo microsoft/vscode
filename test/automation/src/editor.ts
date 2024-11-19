46dcce7bf95701f1d1eee1c7e18f018457fa6714
Aiday Marlen Kyzy <amarlenkyzy@microsoft.com>
2024-11-19 13:38:56 +0100

    changing selector depending on app quality smoke test

--- test/automation/src/editor.ts
+++ test/automation/src/editor.ts
@@ -6,6 +6,7 @@
 import { References } from './peek';
 import { Commands } from './workbench';
 import { Code } from './code';
+import { Quality } from './application';
 
 const RENAME_BOX = '.monaco-editor .monaco-editor.rename-box';
 const RENAME_INPUT = `${RENAME_BOX} .rename-input`;
@@ -78,7 +79,7 @@ export class Editor {
 	async waitForEditorFocus(filename: string, lineNumber: number, selectorPrefix = ''): Promise<void> {
 		const editor = [selectorPrefix || '', EDITOR(filename)].join(' ');
 		const line = `${editor} .view-lines > .view-line:nth-child(${lineNumber})`;
-		const editContext = `${editor} .native-edit-context`;
+		const editContext = `${editor} ${this._editContextSelector()}`;
 
 		await this.code.waitAndClick(line, 1, 1);
 		await this.code.waitForActiveElement(editContext);
@@ -92,12 +93,16 @@ export class Editor {
 
 		await this.code.waitForElement(editor);
 
-		const editContext = `${editor} .native-edit-context`;
+		const editContext = `${editor} ${this._editContextSelector()}`;
 		await this.code.waitForActiveElement(editContext);
 
 		await this.code.waitForTypeInEditor(editContext, text);
 
 		await this.waitForEditorContents(filename, c => c.indexOf(text) > -1, selectorPrefix);
+	}
+
+	private _editContextSelector() {
+		return this.code.quality === Quality.Stable ? 'textarea' : '.native-edit-context';
 	}
 
 	async waitForEditorContents(filename: string, accept: (contents: string) => boolean, selectorPrefix = ''): Promise<any> {
