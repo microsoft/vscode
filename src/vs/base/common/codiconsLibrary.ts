72ea4caaeb78d99335b8969fb684be91536f197a
Michael Lively <milively@microsoft.com>
2024-11-21 12:36:35 -0800

    add run in section icon

--- src/vs/base/common/codiconsLibrary.ts
+++ src/vs/base/common/codiconsLibrary.ts
@@ -582,4 +582,5 @@ export const codiconsLibrary = {
 	goToEditingSession: register('go-to-editing-session', 0xec35),
 	editSession: register('edit-session', 0xec36),
 	codeReview: register('code-review', 0xec37),
+	runInSection: register('run-in-section', 0xec38),
 } as const;
