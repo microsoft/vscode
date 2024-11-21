959f83be7ee564714b95e482dee3300f8aa12acf
Benjamin Pasero <benjamin.pasero@microsoft.com>
2024-11-21 10:48:41 +0100

    chat - add collection url and adopt in welcome

--- src/vs/workbench/contrib/chat/browser/chatSetup.contribution.ts
+++ src/vs/workbench/contrib/chat/browser/chatSetup.contribution.ts
@@ -47,6 +47,7 @@ const defaultChat = {
 	chatWelcomeTitle: product.defaultChatAgent?.chatWelcomeTitle ?? '',
 	documentationUrl: product.defaultChatAgent?.documentationUrl ?? '',
 	privacyStatementUrl: product.defaultChatAgent?.privacyStatementUrl ?? '',
+	collectionDocumentationUrl: product.defaultChatAgent?.collectionDocumentationUrl ?? '',
 	providerId: product.defaultChatAgent?.providerId ?? '',
 	providerName: product.defaultChatAgent?.providerName ?? '',
 	providerScopes: product.defaultChatAgent?.providerScopes ?? [],
@@ -119,8 +120,8 @@ class ChatSetupContribution extends Disposable implements IWorkbenchContribution
 	}
 
 	private registerChatWelcome(): void {
-		const header = localize('setupPreamble1', "{0} is your AI pair programmer that helps you write code faster and smarter.", defaultChat.name);
-		const footer = localize('setupPreamble2', "By proceeding you agree to the [Privacy Statement]({0}).", defaultChat.privacyStatementUrl);
+		const header = localize('setupPreamble1', "{0} is your AI pair programmer.", defaultChat.name);
+		const footer = localize({ key: 'setupPreamble2', comment: ['{Locked="]({0})"}'] }, "{0} may use your code snippets for product improvements. Read our [privacy statement]({1}) and learn how to [opt out]({2}).", defaultChat.name, defaultChat.privacyStatementUrl, defaultChat.collectionDocumentationUrl);
 
 		// Setup: Triggered (signed-out)
 		Registry.as<IChatViewsWelcomeContributionRegistry>(ChatViewsWelcomeExtensions.ChatViewsWelcomeRegistry).register({
