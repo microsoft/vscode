b69589ce385eff52cdfedd7ab89ec8b1fb98ad16
Aiday Marlen Kyzy <amarlenkyzy@microsoft.com>
2024-11-21 11:58:33 +0100

    Smoke test failure - changing selectors depending on app quality (#234181)
    * changing selector depending on app quality smoke test
    
    * using correct selector
    
    * resetting to just insiders
    
    * focusing if not focused after event
    
    * removing dom node focus

--- test/automation/src/code.ts
+++ test/automation/src/code.ts
@@ -12,6 +12,7 @@ import { launch as launchPlaywrightBrowser } from './playwrightBrowser';
 import { PlaywrightDriver } from './playwrightDriver';
 import { launch as launchPlaywrightElectron } from './playwrightElectron';
 import { teardown } from './processes';
+import { Quality } from './application';
 
 export interface LaunchOptions {
 	codePath?: string;
@@ -28,6 +29,7 @@ export interface LaunchOptions {
 	readonly tracing?: boolean;
 	readonly headless?: boolean;
 	readonly browser?: 'chromium' | 'webkit' | 'firefox';
+	readonly quality: Quality;
 }
 
 interface ICodeInstance {
@@ -77,7 +79,7 @@ export async function launch(options: LaunchOptions): Promise<Code> {
 		const { serverProcess, driver } = await measureAndLog(() => launchPlaywrightBrowser(options), 'launch playwright (browser)', options.logger);
 		registerInstance(serverProcess, options.logger, 'server');
 
-		return new Code(driver, options.logger, serverProcess);
+		return new Code(driver, options.logger, serverProcess, options.quality);
 	}
 
 	// Electron smoke tests (playwright)
@@ -85,7 +87,7 @@ export async function launch(options: LaunchOptions): Promise<Code> {
 		const { electronProcess, driver } = await measureAndLog(() => launchPlaywrightElectron(options), 'launch playwright (electron)', options.logger);
 		registerInstance(electronProcess, options.logger, 'electron');
 
-		return new Code(driver, options.logger, electronProcess);
+		return new Code(driver, options.logger, electronProcess, options.quality);
 	}
 }
 
@@ -96,7 +98,8 @@ export class Code {
 	constructor(
 		driver: PlaywrightDriver,
 		readonly logger: Logger,
-		private readonly mainProcess: cp.ChildProcess
+		private readonly mainProcess: cp.ChildProcess,
+		readonly quality: Quality
 	) {
 		this.driver = new Proxy(driver, {
 			get(target, prop) {
