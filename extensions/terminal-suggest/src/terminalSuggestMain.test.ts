bfe7c02b02301c3148d1bcafe774246e4c562637
Megan Rogge <merogge@microsoft.com>
2024-11-22 18:37:56 -0500

    get tests in good format

--- extensions/terminal-suggest/src/terminalSuggestMain.test.ts
+++ extensions/terminal-suggest/src/terminalSuggestMain.test.ts
@@ -8,23 +8,39 @@ import 'mocha';
 import { availableSpecs, getCompletionItemsFromSpecs } from './terminalSuggestMain';
 
 suite('Terminal Suggest', () => {
-
+	const availableCommands = new Set(['cd', 'code', 'code-insiders', 'ls', 'pwd']);
 	suiteSetup(async function () {
 
 	});
 	suiteTeardown(async function () {
 	});
 
-	test(`No results when there are no available commands:`, function () {
-		const result = getCompletionItemsFromSpecs(availableSpecs, { 'commandLine': '', cursorPosition: 0 }, new Set(), '');
-		assert(result.items.length === 0);
-		assert(result.filesRequested === false);
-		assert(result.foldersRequested === false);
-	});
-	test(`Results when there are matching available commands and the prefix matches:`, function () {
-		const result = getCompletionItemsFromSpecs(availableSpecs, { 'commandLine': 'c', cursorPosition: 1 }, new Set(['code', 'code-insiders', 'cd']), 'c');
-		assert(result.items.length === 3);
-		assert(result.filesRequested === false);
-		assert(result.foldersRequested === false);
+	suite('prefix: c', () => {
+		createTestCase(`No available commands:`, 'c|', [], 'neither', availableSpecs, new Set());
+		createTestCase('Available commands', 'c|', ['cd', 'code', 'code-insiders'], 'neither', availableSpecs, availableCommands);
+		createTestCase('Available commands, prior command on the line', 'ls && c|', ['cd', 'code', 'code-insiders'], 'neither', availableSpecs, availableCommands);
 	});
 });
+
+function createTestCase(name: string, commandLineWithCursor: string, expectedSpecs: string[], resourcesRequested: 'files' | 'folders' | 'both' | 'neither', availableSpecs: Fig.Spec[], availableCommands: Set<string>): void {
+	const commandLine = commandLineWithCursor.split('|')[0];
+	const cursorPosition = commandLineWithCursor.indexOf('|');
+	const prefix = commandLine.slice(0, cursorPosition).split(' ').pop() || '';
+	const filesRequested = resourcesRequested === 'files' || resourcesRequested === 'both';
+	const foldersRequested = resourcesRequested === 'folders' || resourcesRequested === 'both';
+	test(name, function () {
+		const result = getCompletionItemsFromSpecs(availableSpecs, { commandLine, cursorPosition }, availableCommands, prefix);
+		assert(arraysEqual(result.items.map(i => i.label), expectedSpecs));
+		assert(result.filesRequested === filesRequested);
+		assert(result.foldersRequested === foldersRequested);
+	});
+}
+
+function arraysEqual(a: string[], b: string[]) {
+	if (a.length !== b.length) {
+		return false;
+	}
+	const sortedA = [...a].sort();
+	const sortedB = [...b].sort();
+	return sortedA.every((value, index) => value === sortedB[index]);
+}
