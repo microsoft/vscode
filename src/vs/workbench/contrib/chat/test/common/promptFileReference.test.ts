bf6c595fc17610365899df9c87dee4d1d5fce4cd
Oleg Solomko <legomushroom@gmail.com>
2024-11-21 19:48:37 -0800

    [prompt snippets]: improve unit tests for the `PromptFileReference`

--- src/vs/workbench/contrib/chat/test/common/promptFileReference.test.ts
+++ src/vs/workbench/contrib/chat/test/common/promptFileReference.test.ts
@@ -7,6 +7,7 @@ import assert from 'assert';
 import { URI } from '../../../../../base/common/uri.js';
 import { VSBuffer } from '../../../../../base/common/buffer.js';
 import { Schemas } from '../../../../../base/common/network.js';
+import { isWindows } from '../../../../../base/common/platform.js';
 import { Disposable } from '../../../../../base/common/lifecycle.js';
 import { NullLogService } from '../../../../../platform/log/common/log.js';
 import { IFileService } from '../../../../../platform/files/common/files.js';
@@ -103,6 +104,15 @@ class TestPromptFileReference extends Disposable {
 		const resolvedReferences = (await rootReference.resolve(true))
 			.flatten();
 
+		assert.strictEqual(
+			resolvedReferences.length,
+			this.expectedReferences.length,
+			[
+				`\nExpected(${this.expectedReferences.length}): [\n ${this.expectedReferences.join('\n ')}\n]`,
+				`Received(${resolvedReferences.length}): [\n ${resolvedReferences.join('\n ')}\n]`,
+			].join('\n')
+		);
+
 		for (let i = 0; i < this.expectedReferences.length; i++) {
 			const expectedReference = this.expectedReferences[i];
 			const resolvedReference = resolvedReferences[i];
@@ -134,15 +144,6 @@ class TestPromptFileReference extends Disposable {
 				].join(', '),
 			);
 		}
-
-		assert.strictEqual(
-			resolvedReferences.length,
-			this.expectedReferences.length,
-			[
-				`Expected to resolve ${this.expectedReferences.length} references`,
-				`got ${resolvedReferences.length}.`,
-			].join(', ')
-		);
 	}
 
 	/**
@@ -176,18 +177,24 @@ class TestPromptFileReference extends Disposable {
 	}
 }
 
-suite('ChatbotPromptReference', function () {
+suite('ChatbotPromptReference (Unix)', function () {
 	const testDisposables = ensureNoDisposablesAreLeakedInTestSuite();
 
 	test('resolves nested file references', async function () {
-		const rootFolder = URI.file('/resolves-nested-file-references');
+		if (isWindows) {
+			this.skip();
+		}
+
+		const rootFolderName = 'resolves-nested-file-references';
+		const rootFolder = `/${rootFolderName}`;
+		const rootUri = URI.file(rootFolder);
 
 		const test = testDisposables.add(new TestPromptFileReference(
 			/**
 			 * The file structure to be created on the disk for the test.
 			 */
 			{
-				name: 'resolves-nested-file-references',
+				name: rootFolderName,
 				children: [
 					{
 						name: 'file1.prompt.md',
@@ -202,7 +209,7 @@ suite('ChatbotPromptReference', function () {
 						children: [
 							{
 								name: 'file3.prompt.md',
-								contents: '\n\n\t- some seemingly random #file:/resolves-nested-file-references/folder1/some-other-folder/yetAnotherFolder🤭/another-file.prompt.md contents\n some more\t content',
+								contents: `\n\n\t- some seemingly random #file:${rootFolder}/folder1/some-other-folder/yetAnotherFolder🤭/another-file.prompt.md contents\n some more\t content`,
 							},
 							{
 								name: 'some-other-folder',
@@ -239,39 +246,39 @@ suite('ChatbotPromptReference', function () {
 			 */
 			[
 				testDisposables.add(new ExpectedReference(
-					URI.joinPath(rootFolder, './file2.prompt.md'),
+					URI.joinPath(rootUri, './file2.prompt.md'),
 					undefined,
 				)),
 				testDisposables.add(new ExpectedReference(
-					URI.joinPath(rootFolder, './folder1/file3.prompt.md'),
+					URI.joinPath(rootUri, './folder1/file3.prompt.md'),
 					undefined,
 				)),
 				testDisposables.add(new ExpectedReference(
-					URI.joinPath(rootFolder, './folder1/some-other-folder/yetAnotherFolder🤭/another-file.prompt.md'),
+					URI.joinPath(rootUri, './folder1/some-other-folder/yetAnotherFolder🤭/another-file.prompt.md'),
 					undefined,
 				)),
 				testDisposables.add(new ExpectedReference(
-					URI.joinPath(rootFolder, './folder1/some-other-folder/file.txt'),
+					URI.joinPath(rootUri, './folder1/some-other-folder/file.txt'),
 					new NotPromptSnippetFile(
-						URI.joinPath(rootFolder, './folder1/some-other-folder/file.txt'),
+						URI.joinPath(rootUri, './folder1/some-other-folder/file.txt'),
 						'Ughh oh!',
 					),
 				)),
 				testDisposables.add(new ExpectedReference(
-					URI.joinPath(rootFolder, './folder1/some-other-folder/file4.prompt.md'),
+					URI.joinPath(rootUri, './folder1/some-other-folder/file4.prompt.md'),
 					undefined,
 				)),
 				testDisposables.add(new ExpectedReference(
-					URI.joinPath(rootFolder, './folder1/some-other-folder/some-non-existing/file.prompt.md'),
+					URI.joinPath(rootUri, './folder1/some-other-folder/some-non-existing/file.prompt.md'),
 					new FileOpenFailed(
-						URI.joinPath(rootFolder, './folder1/some-other-folder/some-non-existing/file.prompt.md'),
+						URI.joinPath(rootUri, './folder1/some-other-folder/some-non-existing/file.prompt.md'),
 						'Some error message.',
 					),
 				)),
 				testDisposables.add(new ExpectedReference(
-					URI.joinPath(rootFolder, './folder1/some-other-folder/some-non-prompt-file.md'),
+					URI.joinPath(rootUri, './folder1/some-other-folder/some-non-prompt-file.md'),
 					new NotPromptSnippetFile(
-						URI.joinPath(rootFolder, './folder1/some-other-folder/some-non-prompt-file.md'),
+						URI.joinPath(rootUri, './folder1/some-other-folder/some-non-prompt-file.md'),
 						'Oh no!',
 					),
 				)),
@@ -282,14 +289,20 @@ suite('ChatbotPromptReference', function () {
 	});
 
 	test('does not fall into infinite reference recursion', async function () {
-		const rootFolder = URI.file('/infinite-recursion');
+		if (isWindows) {
+			this.skip();
+		}
+
+		const rootFolderName = 'infinite-recursion';
+		const rootFolder = `/${rootFolderName}`;
+		const rootUri = URI.file(rootFolder);
 
 		const test = testDisposables.add(new TestPromptFileReference(
 			/**
 			 * The file structure to be created on the disk for the test.
 			 */
 			{
-				name: 'infinite-recursion',
+				name: rootFolderName,
 				children: [
 					{
 						name: 'file1.md',
@@ -297,14 +310,14 @@ suite('ChatbotPromptReference', function () {
 					},
 					{
 						name: 'file2.prompt.md',
-						contents: '## Files\n\t- this file #file:folder1/file3.prompt.md \n\t- also this #file:./folder1/some-other-folder/file4.prompt.md\n\n#file:/infinite-recursion/folder1/some-other-folder/file5.prompt.md\t please!\n\t#file:./file1.md ',
+						contents: `## Files\n\t- this file #file:folder1/file3.prompt.md \n\t- also this #file:./folder1/some-other-folder/file4.prompt.md\n\n#file:${rootFolder}/folder1/some-other-folder/file5.prompt.md\t please!\n\t#file:./file1.md `,
 					},
 					{
 						name: 'folder1',
 						children: [
 							{
 								name: 'file3.prompt.md',
-								contents: '\n\n\t- some seemingly random #file:/infinite-recursion/folder1/some-other-folder/yetAnotherFolder🤭/another-file.prompt.md contents\n some more\t content',
+								contents: `\n\n\t- some seemingly random #file:${rootFolder}/folder1/some-other-folder/yetAnotherFolder🤭/another-file.prompt.md contents\n some more\t content`,
 							},
 							{
 								name: 'some-other-folder',
@@ -323,7 +336,7 @@ suite('ChatbotPromptReference', function () {
 											{
 												name: 'another-file.prompt.md',
 												// absolute path with recursion
-												contents: 'some test goes\t\nhere #file:/infinite-recursion/file2.prompt.md',
+												contents: `some test goes\t\nhere #file:${rootFolder}/file2.prompt.md`,
 											},
 											{
 												name: 'one_more_file_just_in_case.prompt.md',
@@ -342,15 +355,15 @@ suite('ChatbotPromptReference', function () {
 			 */
 			[
 				testDisposables.add(new ExpectedReference(
-					URI.joinPath(rootFolder, './file2.prompt.md'),
+					URI.joinPath(rootUri, './file2.prompt.md'),
 					undefined,
 				)),
 				testDisposables.add(new ExpectedReference(
-					URI.joinPath(rootFolder, './folder1/file3.prompt.md'),
+					URI.joinPath(rootUri, './folder1/file3.prompt.md'),
 					undefined,
 				)),
 				testDisposables.add(new ExpectedReference(
-					URI.joinPath(rootFolder, './folder1/some-other-folder/yetAnotherFolder🤭/another-file.prompt.md'),
+					URI.joinPath(rootUri, './folder1/some-other-folder/yetAnotherFolder🤭/another-file.prompt.md'),
 					undefined,
 				)),
 				/**
@@ -359,9 +372,9 @@ suite('ChatbotPromptReference', function () {
 				 * (the absolute reference case)
 				 */
 				testDisposables.add(new ExpectedReference(
-					URI.joinPath(rootFolder, './file2.prompt.md'),
+					URI.joinPath(rootUri, './file2.prompt.md'),
 					new RecursiveReference(
-						URI.joinPath(rootFolder, './file2.prompt.md'),
+						URI.joinPath(rootUri, './file2.prompt.md'),
 						[
 							'/infinite-recursion/file2.prompt.md',
 							'/infinite-recursion/folder1/file3.prompt.md',
@@ -371,18 +384,18 @@ suite('ChatbotPromptReference', function () {
 					),
 				)),
 				testDisposables.add(new ExpectedReference(
-					URI.joinPath(rootFolder, './folder1/some-other-folder/file4.prompt.md'),
+					URI.joinPath(rootUri, './folder1/some-other-folder/file4.prompt.md'),
 					undefined,
 				)),
 				testDisposables.add(new ExpectedReference(
-					URI.joinPath(rootFolder, './folder1/some-non-existing/file.prompt.md'),
+					URI.joinPath(rootUri, './folder1/some-non-existing/file.prompt.md'),
 					new FileOpenFailed(
-						URI.joinPath(rootFolder, './folder1/some-non-existing/file.prompt.md'),
+						URI.joinPath(rootUri, './folder1/some-non-existing/file.prompt.md'),
 						'Some error message.',
 					),
 				)),
 				testDisposables.add(new ExpectedReference(
-					URI.joinPath(rootFolder, './folder1/some-other-folder/file5.prompt.md'),
+					URI.joinPath(rootUri, './folder1/some-other-folder/file5.prompt.md'),
 					undefined,
 				)),
 				/**
@@ -391,9 +404,9 @@ suite('ChatbotPromptReference', function () {
 				 * (the relative reference case)
 				 */
 				testDisposables.add(new ExpectedReference(
-					URI.joinPath(rootFolder, './file2.prompt.md'),
+					URI.joinPath(rootUri, './file2.prompt.md'),
 					new RecursiveReference(
-						URI.joinPath(rootFolder, './file2.prompt.md'),
+						URI.joinPath(rootUri, './file2.prompt.md'),
 						[
 							'/infinite-recursion/file2.prompt.md',
 							'/infinite-recursion/folder1/some-other-folder/file5.prompt.md',
@@ -402,9 +415,261 @@ suite('ChatbotPromptReference', function () {
 					),
 				)),
 				testDisposables.add(new ExpectedReference(
-					URI.joinPath(rootFolder, './file1.md'),
+					URI.joinPath(rootUri, './file1.md'),
 					new NotPromptSnippetFile(
-						URI.joinPath(rootFolder, './file1.md'),
+						URI.joinPath(rootUri, './file1.md'),
+						'Uggh oh!',
+					),
+				)),
+			]
+		));
+
+		await test.run();
+	});
+});
+
+
+suite('ChatbotPromptReference (Windows)', function () {
+	const testDisposables = ensureNoDisposablesAreLeakedInTestSuite();
+
+	test('resolves nested file references', async function () {
+		if (!isWindows) {
+			this.skip();
+		}
+
+		const rootFolderName = 'resolves-nested-file-references';
+		const rootFolder = `D:\\${rootFolderName}`;
+		const rootUri = URI.file(rootFolder);
+
+		const test = testDisposables.add(new TestPromptFileReference(
+			/**
+			 * The file structure to be created on the disk for the test.
+			 */
+			{
+				name: rootFolderName,
+				children: [
+					{
+						name: 'file1.prompt.md',
+						contents: '## Some Header\nsome contents\n ',
+					},
+					{
+						name: 'file2.prompt.md',
+						contents: '## Files\n\t- this file #file:folder1\\file3.prompt.md \n\t- also this #file:.\\folder1\\some-other-folder\\file4.prompt.md please!\n ',
+					},
+					{
+						name: 'folder1',
+						children: [
+							{
+								name: 'file3.prompt.md',
+								contents: `\n\n\t- some seemingly random #file:${rootFolder}\\folder1\\some-other-folder\\yetAnotherFolder🤭\\another-file.prompt.md contents\n some more\t content`,
+							},
+							{
+								name: 'some-other-folder',
+								children: [
+									{
+										name: 'file4.prompt.md',
+										contents: 'this file has a non-existing #file:.\\some-non-existing\\file.prompt.md\t\treference\n\nand some non-prompt #file:.\\some-non-prompt-file.md',
+									},
+									{
+										name: 'file.txt',
+										contents: 'contents of a non-prompt-snippet file',
+									},
+									{
+										name: 'yetAnotherFolder🤭',
+										children: [
+											{
+												name: 'another-file.prompt.md',
+												contents: 'another-file.prompt.md contents\t  #file:..\\file.txt',
+											},
+											{
+												name: 'one_more_file_just_in_case.prompt.md',
+												contents: 'one_more_file_just_in_case.prompt.md contents',
+											},
+										],
+									},
+								],
+							},
+						],
+					},
+				],
+			},
+			/**
+			 * The expected references to be resolved.
+			 */
+			[
+				testDisposables.add(new ExpectedReference(
+					URI.joinPath(rootUri, '.\\file2.prompt.md'),
+					undefined,
+				)),
+				testDisposables.add(new ExpectedReference(
+					URI.joinPath(rootUri, '.\\folder1\\file3.prompt.md'),
+					undefined,
+				)),
+				testDisposables.add(new ExpectedReference(
+					URI.joinPath(rootUri, '.\\folder1\\some-other-folder\\yetAnotherFolder🤭\\another-file.prompt.md'),
+					undefined,
+				)),
+				testDisposables.add(new ExpectedReference(
+					URI.joinPath(rootUri, '.\\folder1\\some-other-folder\\file.txt'),
+					new NotPromptSnippetFile(
+						URI.joinPath(rootUri, '.\\folder1\\some-other-folder\\file.txt'),
+						'Ughh oh!',
+					),
+				)),
+				testDisposables.add(new ExpectedReference(
+					URI.joinPath(rootUri, '.\\folder1\\some-other-folder\\file4.prompt.md'),
+					undefined,
+				)),
+				testDisposables.add(new ExpectedReference(
+					URI.joinPath(rootUri, '.\\folder1\\some-other-folder\\some-non-existing\\file.prompt.md'),
+					new FileOpenFailed(
+						URI.joinPath(rootUri, '.\\folder1\\some-other-folder\\some-non-existing\\file.prompt.md'),
+						'Some error message.',
+					),
+				)),
+				testDisposables.add(new ExpectedReference(
+					URI.joinPath(rootUri, '.\\folder1\\some-other-folder\\some-non-prompt-file.md'),
+					new NotPromptSnippetFile(
+						URI.joinPath(rootUri, '.\\folder1\\some-other-folder\\some-non-prompt-file.md'),
+						'Oh no!',
+					),
+				)),
+			]
+		));
+
+		await test.run();
+	});
+
+	test('does not fall into infinite reference recursion', async function () {
+		if (!isWindows) {
+			this.skip();
+		}
+
+		const rootFolderName = 'infinite-recursion';
+		const rootFolder = `D:\\${rootFolderName}`;
+		const rootUri = URI.file(rootFolder);
+
+		const test = testDisposables.add(new TestPromptFileReference(
+			/**
+			 * The file structure to be created on the disk for the test.
+			 */
+			{
+				name: rootFolderName,
+				children: [
+					{
+						name: 'file1.md',
+						contents: '## Some Header\nsome contents\n ',
+					},
+					{
+						name: 'file2.prompt.md',
+						contents: `## Files\n\t- this file #file:folder1\\file3.prompt.md \n\t- also this #file:.\\folder1\\some-other-folder\\file4.prompt.md\n\n#file:${rootFolder}\\folder1\\some-other-folder\\file5.prompt.md\t please!\n\t#file:.\\file1.md `,
+					},
+					{
+						name: 'folder1',
+						children: [
+							{
+								name: 'file3.prompt.md',
+								contents: `\n\n\t- some seemingly random #file:${rootFolder}\\folder1\\some-other-folder\\yetAnotherFolder🤭\\another-file.prompt.md contents\n some more\t content`,
+							},
+							{
+								name: 'some-other-folder',
+								children: [
+									{
+										name: 'file4.prompt.md',
+										contents: 'this file has a non-existing #file:..\\some-non-existing\\file.prompt.md\t\treference',
+									},
+									{
+										name: 'file5.prompt.md',
+										contents: 'this file has a relative recursive #file:..\\..\\file2.prompt.md\nreference\n ',
+									},
+									{
+										name: 'yetAnotherFolder🤭',
+										children: [
+											{
+												name: 'another-file.prompt.md',
+												// absolute path with recursion
+												contents: `some test goes\t\nhere #file:${rootFolder}\\file2.prompt.md`,
+											},
+											{
+												name: 'one_more_file_just_in_case.prompt.md',
+												contents: 'one_more_file_just_in_case.prompt.md contents',
+											},
+										],
+									},
+								],
+							},
+						],
+					},
+				],
+			},
+			/**
+			 * The expected references to be resolved.
+			 */
+			[
+				testDisposables.add(new ExpectedReference(
+					URI.joinPath(rootUri, '.\\file2.prompt.md'),
+					undefined,
+				)),
+				testDisposables.add(new ExpectedReference(
+					URI.joinPath(rootUri, '.\\folder1\\file3.prompt.md'),
+					undefined,
+				)),
+				testDisposables.add(new ExpectedReference(
+					URI.joinPath(rootUri, '.\\folder1\\some-other-folder\\yetAnotherFolder🤭\\another-file.prompt.md'),
+					undefined,
+				)),
+				/**
+				 * This reference should be resolved as
+				 * a recursive reference error condition.
+				 * (the absolute reference case)
+				 */
+				testDisposables.add(new ExpectedReference(
+					URI.joinPath(rootUri, '.\\file2.prompt.md'),
+					new RecursiveReference(
+						URI.joinPath(rootUri, '.\\file2.prompt.md'),
+						[
+							'\\infinite-recursion\\file2.prompt.md',
+							'\\infinite-recursion\\folder1\\file3.prompt.md',
+							'\\infinite-recursion\\folder1\\some-other-folder\\yetAnotherFolder🤭\\another-file.prompt.md',
+							'\\infinite-recursion\\file2.prompt.md',
+						],
+					),
+				)),
+				testDisposables.add(new ExpectedReference(
+					URI.joinPath(rootUri, '.\\folder1\\some-other-folder\\file4.prompt.md'),
+					undefined,
+				)),
+				testDisposables.add(new ExpectedReference(
+					URI.joinPath(rootUri, '.\\folder1\\some-non-existing\\file.prompt.md'),
+					new FileOpenFailed(
+						URI.joinPath(rootUri, '.\\folder1\\some-non-existing\\file.prompt.md'),
+						'Some error message.',
+					),
+				)),
+				testDisposables.add(new ExpectedReference(
+					URI.joinPath(rootUri, '.\\folder1\\some-other-folder\\file5.prompt.md'),
+					undefined,
+				)),
+				/**
+				 * This reference should be resolved as
+				 * a recursive reference error condition.
+				 * (the relative reference case)
+				 */
+				testDisposables.add(new ExpectedReference(
+					URI.joinPath(rootUri, '.\\file2.prompt.md'),
+					new RecursiveReference(
+						URI.joinPath(rootUri, '.\\file2.prompt.md'),
+						[
+							'\\infinite-recursion\\file2.prompt.md',
+							'\\infinite-recursion\\folder1\\some-other-folder\\file5.prompt.md',
+							'\\infinite-recursion\\file2.prompt.md',
+						],
+					),
+				)),
+				testDisposables.add(new ExpectedReference(
+					URI.joinPath(rootUri, '.\\file1.md'),
+					new NotPromptSnippetFile(
+						URI.joinPath(rootUri, '.\\file1.md'),
 						'Uggh oh!',
 					),
 				)),
