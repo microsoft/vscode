d0210b24cfe3695f90ff1a4de0bce17fe3e08f7f
Henning Dieterichs <hdieterichs@microsoft.com>
2024-11-25 16:25:32 +0100

    Improves logging

--- src/vs/editor/contrib/inlineCompletions/browser/model/changeRecorder.ts
+++ src/vs/editor/contrib/inlineCompletions/browser/model/changeRecorder.ts
@@ -6,23 +6,28 @@
 import { Disposable } from '../../../../../base/common/lifecycle.js';
 import { autorunWithStore } from '../../../../../base/common/observable.js';
 import { IContextKeyService } from '../../../../../platform/contextkey/common/contextkey.js';
+import { IInstantiationService } from '../../../../../platform/instantiation/common/instantiation.js';
 import { ILogService } from '../../../../../platform/log/common/log.js';
 import { ICodeEditor } from '../../../../browser/editorBrowser.js';
 import { CodeEditorWidget } from '../../../../browser/widget/codeEditor/codeEditorWidget.js';
-import { formatRecordableLogEntry, IRecordableEditorLogEntry, observableContextKey } from './inlineCompletionsSource.js';
+import { formatRecordableLogEntry, IRecordableEditorLogEntry, observableContextKey, StructuredLogger } from './inlineCompletionsSource.js';
 
 export class TextModelChangeRecorder extends Disposable {
 	private readonly _recordingLoggingEnabled = observableContextKey('editor.inlineSuggest.logChangeReason', this._contextKeyService).recomputeInitiallyAndOnChange(this._store);
+	private readonly _structuredLogger = this._instantiationService.createInstance(StructuredLogger.cast<IRecordableEditorLogEntry & { source: string }>(),
+		'editor.inlineSuggest.logChangeReason.commandId'
+	);
 
 	constructor(
 		private readonly _editor: ICodeEditor,
 		@IContextKeyService private readonly _contextKeyService: IContextKeyService,
-		@ILogService private readonly _logService: ILogService
+		@ILogService private readonly _logService: ILogService,
+		@IInstantiationService private readonly _instantiationService: IInstantiationService,
 	) {
 		super();
 		this._register(autorunWithStore((reader, store) => {
 			if (!(this._editor instanceof CodeEditorWidget)) { return; }
-			if (!this._recordingLoggingEnabled.read(reader)) { return; }
+			if (!this._recordingLoggingEnabled.read(reader) && !this._structuredLogger.isEnabled.read(reader)) { return; }
 
 			const sources: string[] = [];
 
@@ -36,12 +41,18 @@ export class TextModelChangeRecorder extends Disposable {
 				const tm = this._editor.getModel();
 				if (!tm) { return; }
 				for (const source of sources) {
-					this._logService.info(formatRecordableLogEntry<IRecordableEditorLogEntry & { source: string }>('TextModel.setChangeReason', {
+					const data: IRecordableEditorLogEntry & { source: string } = {
+						sourceId: 'TextModel.setChangeReason',
+						source: source,
 						time: Date.now(),
 						modelUri: tm.uri.toString(),
 						modelVersion: tm.getVersionId(),
-						source: source,
-					}));
+					};
+
+					if (this._recordingLoggingEnabled.read(reader)) {
+						this._logService.info(formatRecordableLogEntry(data)); // TODO remove this
+					}
+					this._structuredLogger.log(data);
 				}
 				sources.length = 0;
 			}));
