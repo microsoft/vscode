88343a04294e3ff45a11c5e2de8106959d057f7e
Connor Peet <connor@peet.io>
2024-11-25 14:46:36 -0800

    testing: apply coverage comments for attributable coverage
    Refs #233709

--- src/vs/workbench/api/common/extHostTypes.ts
+++ src/vs/workbench/api/common/extHostTypes.ts
@@ -4262,7 +4262,7 @@ export class FileCoverage implements vscode.FileCoverage {
 		public statementCoverage: vscode.TestCoverageCount,
 		public branchCoverage?: vscode.TestCoverageCount,
 		public declarationCoverage?: vscode.TestCoverageCount,
-		public fromTests: vscode.TestItem[] = [],
+		public includesTests: vscode.TestItem[] = [],
 	) {
 	}
 }
