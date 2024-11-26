0c4df1f30bd6490af9ef5884102b97d92e5fe3f6
Porrasm86 <153502699+Porrasm86@users.noreply.github.com>
2024-11-25 23:19:44 -0700

    Delete resources directory

--- resources/server/bin/helpers/browser.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-setlocal
-set ROOT_DIR=%~dp0..\..
-start "Open Browser" /B "%ROOT_DIR%\node.exe" "%ROOT_DIR%\out\server-cli.js" "@@APPNAME@@" "@@VERSION@@" "@@COMMIT@@" "@@APPNAME@@.cmd" "--openExternal" "%*"
-endlocal
