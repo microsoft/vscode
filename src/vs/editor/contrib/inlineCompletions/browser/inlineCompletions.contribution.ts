1bdee4500fc32f4eb195e087501b8c9331447fb3
Henning Dieterichs <hdieterichs@microsoft.com>
2024-11-28 11:37:14 +0100

    Implements onlyShowWhenCloseToCursor (#234818)

--- src/vs/editor/contrib/inlineCompletions/browser/inlineCompletions.contribution.ts
+++ src/vs/editor/contrib/inlineCompletions/browser/inlineCompletions.contribution.ts
@@ -6,7 +6,7 @@
 import { AccessibleViewRegistry } from '../../../../platform/accessibility/browser/accessibleViewRegistry.js';
 import { registerAction2 } from '../../../../platform/actions/common/actions.js';
 import { wrapInHotClass1 } from '../../../../platform/observable/common/wrapInHotClass.js';
-import { EditorContributionInstantiation, registerEditorAction, registerEditorContribution } from '../../../browser/editorExtensions.js';
+import { EditorContributionInstantiation, registerEditorAction, registerEditorCommand, registerEditorContribution } from '../../../browser/editorExtensions.js';
 import { HoverParticipantRegistry } from '../../hover/browser/hoverTypes.js';
 import { AcceptInlineCompletion, AcceptNextLineOfInlineCompletion, AcceptNextWordOfInlineCompletion, DevExtractReproSample, HideInlineCompletion, JumpToNextInlineEdit, ShowNextInlineSuggestionAction, ShowPreviousInlineSuggestionAction, ToggleAlwaysShowInlineSuggestionToolbar, ExplicitTriggerInlineEditAction, TriggerInlineSuggestionAction, TriggerInlineEditAction } from './controller/commands.js';
 import { InlineCompletionsController } from './controller/inlineCompletionsController.js';
@@ -21,7 +21,7 @@ registerEditorContribution(InlineCompletionsController.ID, wrapInHotClass1(Inlin
 
 registerEditorAction(TriggerInlineSuggestionAction);
 registerEditorAction(ExplicitTriggerInlineEditAction);
-registerAction2(TriggerInlineEditAction);
+registerEditorCommand(new TriggerInlineEditAction());
 registerEditorAction(ShowNextInlineSuggestionAction);
 registerEditorAction(ShowPreviousInlineSuggestionAction);
 registerEditorAction(AcceptNextWordOfInlineCompletion);
