f408139c28c2c653d673e9a85ba250d6c6a588be
Megan Rogge <merogge@microsoft.com>
2024-12-02 14:51:55 -0500

    only show command/spec suggestions for first on the line (#235005)

--- extensions/terminal-suggest/src/terminalSuggestMain.ts
+++ extensions/terminal-suggest/src/terminalSuggestMain.ts
@@ -216,6 +216,7 @@ export function getCompletionItemsFromSpecs(specs: Fig.Spec[], terminalContext: 
 	let filesRequested = false;
 	let foldersRequested = false;
 	let specificSuggestionsProvided = false;
+	const firstCommand = getFirstCommand(terminalContext.commandLine);
 	for (const spec of specs) {
 		const specLabels = getLabel(spec);
 		if (!specLabels) {
@@ -229,8 +230,8 @@ export function getCompletionItemsFromSpecs(specs: Fig.Spec[], terminalContext: 
 			if (
 				// If the prompt is empty
 				!terminalContext.commandLine
-				// or the prefix matches the command and the prefix is not equal to the command
-				|| !!prefix && specLabel.startsWith(prefix)
+				// or the first command matches the command
+				|| !!firstCommand && specLabel.startsWith(firstCommand)
 			) {
 				// push it to the completion items
 				items.push(createCompletionItem(terminalContext.commandLine, terminalContext.cursorPosition, prefix, specLabel));
@@ -291,7 +292,7 @@ export function getCompletionItemsFromSpecs(specs: Fig.Spec[], terminalContext: 
 	if (!specificSuggestionsProvided && (filesRequested === foldersRequested)) {
 		// Include builitin/available commands in the results
 		for (const command of availableCommands) {
-			if ((!terminalContext.commandLine.trim() || !!prefix) && command.startsWith(prefix) && !items.find(item => item.label === command)) {
+			if ((!terminalContext.commandLine.trim() || firstCommand && command.startsWith(firstCommand)) && !items.find(item => item.label === command)) {
 				items.push(createCompletionItem(terminalContext.commandLine, terminalContext.cursorPosition, prefix, command));
 			}
 		}
@@ -370,3 +371,13 @@ function osIsWindows(): boolean {
 	return os.platform() === 'win32';
 }
 
+function getFirstCommand(commandLine: string): string | undefined {
+	const wordsOnLine = commandLine.split(' ');
+	let firstCommand: string | undefined = wordsOnLine[0];
+	if (wordsOnLine.length > 1) {
+		firstCommand = undefined;
+	} else if (wordsOnLine.length === 0) {
+		firstCommand = commandLine;
+	}
+	return firstCommand;
+}
