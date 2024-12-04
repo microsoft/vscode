f47a5ac827f586c305c8c5e35f6f7b595b95c7e0
Johannes <johannes.rieken@gmail.com>
2024-12-04 16:08:08 +0100

    Improves editor progress for edits
    * dim code that hasn't been revisited yet
    * chat overlay shows textual progress message
    * fix revealing of nearest edit
    
    https://github.com/microsoft/vscode-copilot/issues/10949

--- src/vs/workbench/contrib/chat/browser/chatEditing/chatEditingModifiedFileEntry.ts
+++ src/vs/workbench/contrib/chat/browser/chatEditing/chatEditingModifiedFileEntry.ts
@@ -11,6 +11,7 @@ import { themeColorFromId } from '../../../../../base/common/themables.js';
 import { URI } from '../../../../../base/common/uri.js';
 import { EditOperation, ISingleEditOperation } from '../../../../../editor/common/core/editOperation.js';
 import { OffsetEdit } from '../../../../../editor/common/core/offsetEdit.js';
+import { Range } from '../../../../../editor/common/core/range.js';
 import { IDocumentDiff, nullDocumentDiff } from '../../../../../editor/common/diff/documentDiffProvider.js';
 import { TextEdit } from '../../../../../editor/common/languages.js';
 import { ILanguageService } from '../../../../../editor/common/languages/language.js';
@@ -100,7 +101,7 @@ export class ChatEditingModifiedFileEntry extends Disposable implements IModifie
 	private readonly _editDecorationClear = this._register(new RunOnceScheduler(() => { this._editDecorations = this.doc.deltaDecorations(this._editDecorations, []); }, 500));
 	private _editDecorations: string[] = [];
 
-	private static readonly _editDecorationOptions = ModelDecorationOptions.register({
+	private static readonly _lastEditDecorationOptions = ModelDecorationOptions.register({
 		isWholeLine: true,
 		description: 'chat-editing',
 		className: 'rangeHighlight',
@@ -109,6 +110,12 @@ export class ChatEditingModifiedFileEntry extends Disposable implements IModifie
 			position: OverviewRulerLane.Full,
 			color: themeColorFromId(editorSelectionBackground)
 		},
+	});
+
+	private static readonly _pendingEditDecorationOptions = ModelDecorationOptions.register({
+		isWholeLine: true,
+		description: 'chat-pending-editing',
+		className: 'chat-editing-pending-edit',
 	});
 
 	get telemetryInfo(): IModifiedEntryTelemetryInfo {
@@ -282,13 +289,6 @@ export class ChatEditingModifiedFileEntry extends Disposable implements IModifie
 
 	acceptAgentEdits(textEdits: TextEdit[], isLastEdits: boolean): void {
 
-		// highlight edits
-		this._editDecorations = this.doc.deltaDecorations(this._editDecorations, textEdits.map(edit => {
-			return {
-				options: ChatEditingModifiedFileEntry._editDecorationOptions,
-				range: edit.range
-			} satisfies IModelDeltaDecoration;
-		}));
 
 		// push stack element for the first edit
 		if (this._isFirstEditAfterStartOrSnapshot) {
@@ -301,11 +301,31 @@ export class ChatEditingModifiedFileEntry extends Disposable implements IModifie
 		const ops = textEdits.map(TextEdit.asEditOperation);
 		const undoEdits = this._applyEdits(ops);
 
+		const maxLineNumber = undoEdits.reduce((max, op) => Math.max(max, op.range.startLineNumber), 0);
+
+		const newDecorations: IModelDeltaDecoration[] = [
+			// decorate pending edit (region)
+			{
+				options: ChatEditingModifiedFileEntry._pendingEditDecorationOptions,
+				range: new Range(maxLineNumber + 1, 1, Number.MAX_SAFE_INTEGER, Number.MAX_SAFE_INTEGER)
+			}
+		];
+
+		if (maxLineNumber > 0) {
+			// decorate last edit
+			newDecorations.push({
+				options: ChatEditingModifiedFileEntry._lastEditDecorationOptions,
+				range: new Range(maxLineNumber, 1, maxLineNumber, Number.MAX_SAFE_INTEGER)
+			});
+		}
+
+		this._editDecorations = this.doc.deltaDecorations(this._editDecorations, newDecorations);
+
+
 		transaction((tx) => {
 			if (!isLastEdits) {
 				this._stateObs.set(WorkingSetEntryState.Modified, tx);
 				this._isCurrentlyBeingModifiedObs.set(true, tx);
-				const maxLineNumber = undoEdits.reduce((max, op) => Math.max(max, op.range.startLineNumber), 0);
 				const lineCount = this.doc.getLineCount();
 				this._rewriteRatioObs.set(Math.min(1, maxLineNumber / lineCount), tx);
 				this._maxLineNumberObs.set(maxLineNumber, tx);
