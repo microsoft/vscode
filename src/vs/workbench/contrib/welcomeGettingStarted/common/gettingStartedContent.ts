138f619c86f1199955d53b4166bef66ef252935c
Logan Ramos <lramos15@gmail.com>
2024-12-10 15:22:47 -0700

    Remove quota wording  (#235775)
    * Remove quota from sku messaging
    
    * Update wording
    
    * Fix codicon

--- src/vs/workbench/contrib/welcomeGettingStarted/common/gettingStartedContent.ts
+++ src/vs/workbench/contrib/welcomeGettingStarted/common/gettingStartedContent.ts
@@ -211,7 +211,7 @@ export const startEntries: GettingStartedStartEntryContent = [
 const Button = (title: string, href: string) => `[${title}](${href})`;
 
 const CopilotStepTitle = localize('gettingStarted.copilotSetup.title', "Use AI features with Copilot for free");
-const CopilotDescription = localize({ key: 'gettingStarted.copilotSetup.description', comment: ['{Locked="["}', '{Locked="]({0})"}'] }, "Write code faster and smarter using [Copilot]({0}). With your GitHub account, get 2,000 code completions and 50 chat messages per month for free.", product.defaultChatAgent?.documentationUrl ?? '');
+const CopilotDescription = localize({ key: 'gettingStarted.copilotSetup.description', comment: ['{Locked="["}', '{Locked="]({0})"}'] }, "Write code faster and smarter using [Copilot]({0}) for free with your GitHub account.", product.defaultChatAgent?.documentationUrl ?? '');
 const CopilotTermsString = localize({ key: 'copilotTerms', comment: ['{Locked="["}', '{Locked="]({0})"}', '{Locked="]({1})"}'] }, "By continuing, you agree to Copilot [Terms]({0}) and [Privacy Policy]({1}).", product.defaultChatAgent?.termsStatementUrl ?? '', product.defaultChatAgent?.privacyStatementUrl ?? '');
 const CopilotSignedOutButton = Button(localize('setupCopilotButton.signIn', "Sign in to Use Copilot"), `command:workbench.action.chat.triggerSetup?${encodeURIComponent(JSON.stringify([true]))}`);
 const CopilotSignedInButton = Button(localize('setupCopilotButton.setup', "Set Up Copilot"), `command:workbench.action.chat.triggerSetup?${encodeURIComponent(JSON.stringify([true]))}`);
