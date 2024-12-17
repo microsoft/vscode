7cc0155fe48534e1df603343d9214f5f692a4e86
Ladislau Szomoru <3372902+lszomoru@users.noreply.github.com>
2024-12-17 09:55:52 +0100

    SCM - remove more debt from the quick diff

--- src/vs/workbench/contrib/scm/browser/quickDiffModel.ts
+++ src/vs/workbench/contrib/scm/browser/quickDiffModel.ts
@@ -351,15 +351,7 @@ export class QuickDiffModel extends Disposable {
 		}
 
 		const isSynchronized = this._model.textEditorModel ? shouldSynchronizeModel(this._model.textEditorModel) : undefined;
-		const quickDiffs = await this.quickDiffService.getQuickDiffs(uri, this._model.getLanguageId(), isSynchronized);
-
-		// TODO@lszomoru - find a long term solution for this
-		// When the QuickDiffModel is created for a diff editor, there is no
-		// need to compute the diff information for the `isSCM` quick diff
-		// provider as that information will be provided by the diff editor
-		return this.options.maxComputationTimeMs === undefined
-			? quickDiffs.filter(quickDiff => !quickDiff.isSCM)
-			: quickDiffs;
+		return this.quickDiffService.getQuickDiffs(uri, this._model.getLanguageId(), isSynchronized);
 	}
 
 	findNextClosestChange(lineNumber: number, inclusive = true, provider?: string): number {
