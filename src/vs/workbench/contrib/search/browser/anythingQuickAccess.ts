19c7c0190d58569f06e2d97f11573ea31360ba07
tcostew <143664112+tcostew@users.noreply.github.com>
2024-12-16 19:01:11 -0500

    Allow Github Copilot chat to appear in QuickAccess (#210805)

--- src/vs/workbench/contrib/search/browser/anythingQuickAccess.ts
+++ src/vs/workbench/contrib/search/browser/anythingQuickAccess.ts
@@ -485,8 +485,9 @@ export class AnythingQuickAccessProvider extends PickerQuickAccessProvider<IAnyt
 		for (const editor of this.historyService.getHistory()) {
 			const resource = editor.resource;
 			// allow untitled and terminal editors to go through
-			if (!resource || (!this.fileService.hasProvider(resource) && resource.scheme !== Schemas.untitled && resource.scheme !== Schemas.vscodeTerminal)) {
-				continue; // exclude editors without file resource if we are searching by pattern
+			// allow github copilot chat to go through
+			if (!resource) {
+				continue;
 			}
 
 			const editorHistoryPick = this.createAnythingPick(editor, configuration);
