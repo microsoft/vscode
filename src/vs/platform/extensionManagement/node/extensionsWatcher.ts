8b678300b641ec33250aed93be46d4d1ee83f3aa
Benjamin Pasero <benjamin.pasero@gmail.com>
2024-12-18 06:37:04 +0100

    Revert "debt: clean up obsolete file usage (#236379)"
    This reverts commit 625bae23758002b62954fd10b53d25409421d718.

--- src/vs/platform/extensionManagement/node/extensionsWatcher.ts
+++ src/vs/platform/extensionManagement/node/extensionsWatcher.ts
@@ -48,7 +48,7 @@ export class ExtensionsWatcher extends Disposable {
 		await this.extensionsScannerService.initializeDefaultProfileExtensions();
 		await this.onDidChangeProfiles(this.userDataProfilesService.profiles);
 		this.registerListeners();
-		await this.deleteExtensionsNotInProfiles();
+		await this.uninstallExtensionsNotInProfiles();
 	}
 
 	private registerListeners(): void {
@@ -102,7 +102,7 @@ export class ExtensionsWatcher extends Disposable {
 	}
 
 	private async onDidRemoveExtensions(e: DidRemoveProfileExtensionsEvent): Promise<void> {
-		const extensionsToDelete: IExtension[] = [];
+		const extensionsToUninstall: IExtension[] = [];
 		const promises: Promise<void>[] = [];
 		for (const extension of e.extensions) {
 			const key = this.getKey(extension.identifier, extension.version);
@@ -115,7 +115,7 @@ export class ExtensionsWatcher extends Disposable {
 					promises.push(this.extensionManagementService.scanInstalledExtensionAtLocation(extension.location)
 						.then(result => {
 							if (result) {
-								extensionsToDelete.push(result);
+								extensionsToUninstall.push(result);
 							} else {
 								this.logService.info('Extension not found at the location', extension.location.toString());
 							}
@@ -125,8 +125,8 @@ export class ExtensionsWatcher extends Disposable {
 		}
 		try {
 			await Promise.all(promises);
-			if (extensionsToDelete.length) {
-				await this.deleteExtensionsNotInProfiles(extensionsToDelete);
+			if (extensionsToUninstall.length) {
+				await this.uninstallExtensionsNotInProfiles(extensionsToUninstall);
 			}
 		} catch (error) {
 			this.logService.error(error);
@@ -180,13 +180,13 @@ export class ExtensionsWatcher extends Disposable {
 		}
 	}
 
-	private async deleteExtensionsNotInProfiles(toDelete?: IExtension[]): Promise<void> {
-		if (!toDelete) {
+	private async uninstallExtensionsNotInProfiles(toUninstall?: IExtension[]): Promise<void> {
+		if (!toUninstall) {
 			const installed = await this.extensionManagementService.scanAllUserInstalledExtensions();
-			toDelete = installed.filter(installedExtension => !this.allExtensions.has(this.getKey(installedExtension.identifier, installedExtension.manifest.version)));
+			toUninstall = installed.filter(installedExtension => !this.allExtensions.has(this.getKey(installedExtension.identifier, installedExtension.manifest.version)));
 		}
-		if (toDelete.length) {
-			await this.extensionManagementService.deleteExtensions(...toDelete);
+		if (toUninstall.length) {
+			await this.extensionManagementService.markAsUninstalled(...toUninstall);
 		}
 	}
 
