6c2faf1c5db3a438360b44a9f7b83bafac8efa0b
Benjamin Pasero <benjamin.pasero@microsoft.com>
2024-12-18 13:06:31 +0100

    chat - fix setup removal key (#236463)

--- src/vs/workbench/contrib/chat/browser/actions/chatActions.ts
+++ src/vs/workbench/contrib/chat/browser/actions/chatActions.ts
@@ -531,13 +531,7 @@ MenuRegistry.appendMenuItem(MenuId.CommandCenter, {
 	submenu: MenuId.ChatCommandCenter,
 	title: localize('title4', "Chat"),
 	icon: Codicon.copilot,
-	when: ContextKeyExpr.and(
-		ContextKeyExpr.has('config.chat.commandCenter.enabled'),
-		ContextKeyExpr.or(
-			ChatContextKeys.Setup.installed,
-			ChatContextKeys.panelParticipantRegistered
-		)
-	),
+	when: ContextKeyExpr.has('config.chat.commandCenter.enabled'),
 	order: 10001,
 });
 
@@ -547,13 +541,7 @@ registerAction2(class ToggleCopilotControl extends ToggleTitleBarConfigAction {
 			'chat.commandCenter.enabled',
 			localize('toggle.chatControl', 'Copilot Controls'),
 			localize('toggle.chatControlsDescription', "Toggle visibility of the Copilot Controls in title bar"), 4, false,
-			ContextKeyExpr.and(
-				ContextKeyExpr.has('config.window.commandCenter'),
-				ContextKeyExpr.or(
-					ChatContextKeys.Setup.installed,
-					ChatContextKeys.panelParticipantRegistered
-				)
-			)
+			ContextKeyExpr.has('config.window.commandCenter')
 		);
 	}
 });
