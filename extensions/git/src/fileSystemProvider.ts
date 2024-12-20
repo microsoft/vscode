b9084edd1cf2414e21de4f9e5b5acae240a22218
Ladislau Szomoru <3372902+lszomoru@users.noreply.github.com>
2024-12-20 10:08:22 +0100

    Git - file system provider should throw `FileNotFound` if the resource does not exist in git instead of returning an empty file (#236676)

--- extensions/git/src/fileSystemProvider.ts
+++ extensions/git/src/fileSystemProvider.ts
@@ -192,7 +192,8 @@ export class GitFileSystemProvider implements FileSystemProvider {
 		try {
 			return await repository.buffer(sanitizeRef(ref, path, repository), path);
 		} catch (err) {
-			return new Uint8Array(0);
+			// File does not exist in git (ex: git ignored)
+			throw FileSystemError.FileNotFound();
 		}
 	}
 
