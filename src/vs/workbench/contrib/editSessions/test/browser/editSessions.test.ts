d18b81ee50bf1b1d8c94c67c22adf3c12c93dd77
Joyce Er <joyce.er@microsoft.com>
2024-12-20 15:05:20 -0800

    test: adopt `ensureNoDisposablesAreLeakedInTestSuite`

--- src/vs/workbench/contrib/editSessions/test/browser/editSessions.test.ts
+++ src/vs/workbench/contrib/editSessions/test/browser/editSessions.test.ts
@@ -50,11 +50,13 @@ import { TestStorageService } from '../../../../test/common/workbenchTestService
 import { IUriIdentityService } from '../../../../../platform/uriIdentity/common/uriIdentity.js';
 import { UriIdentityService } from '../../../../../platform/uriIdentity/common/uriIdentityService.js';
 import { IWorkspaceIdentityService, WorkspaceIdentityService } from '../../../../services/workspaces/common/workspaceIdentityService.js';
+import { ensureNoDisposablesAreLeakedInTestSuite } from '../../../../../base/test/common/utils.js';
 
 const folderName = 'test-folder';
 const folderUri = URI.file(`/${folderName}`);
 
 suite('Edit session sync', () => {
+
 	let instantiationService: TestInstantiationService;
 	let editSessionsContribution: EditSessionsContribution;
 	let fileService: FileService;
@@ -63,6 +65,8 @@ suite('Edit session sync', () => {
 	const disposables = new DisposableStore();
 
 	suiteSetup(() => {
+		ensureNoDisposablesAreLeakedInTestSuite();
+
 		sandbox = sinon.createSandbox();
 
 		instantiationService = new TestInstantiationService();
