0c85e4e2fa7a3e75bf816b7465abf8b93124cc91
Ladislau Szomoru <3372902+lszomoru@users.noreply.github.com>
2024-12-20 16:18:47 +0100

    Git - cleanup `vscode-merge-base` git config key when deleteing a branch

--- extensions/git/src/repository.ts
+++ extensions/git/src/repository.ts
@@ -1082,15 +1082,15 @@ export class Repository implements Disposable {
 	}
 
 	getConfig(key: string): Promise<string> {
-		return this.run(Operation.Config(true), () => this.repository.config('local', key));
+		return this.run(Operation.Config(true), () => this.repository.config('get', 'local', key));
 	}
 
 	getGlobalConfig(key: string): Promise<string> {
-		return this.run(Operation.Config(true), () => this.repository.config('global', key));
+		return this.run(Operation.Config(true), () => this.repository.config('get', 'global', key));
 	}
 
 	setConfig(key: string, value: string): Promise<string> {
-		return this.run(Operation.Config(false), () => this.repository.config('local', key, value));
+		return this.run(Operation.Config(false), () => this.repository.config('set', 'local', key, value));
 	}
 
 	log(options?: LogOptions & { silent?: boolean }): Promise<Commit[]> {
@@ -1465,7 +1465,10 @@ export class Repository implements Disposable {
 	}
 
 	async deleteBranch(name: string, force?: boolean): Promise<void> {
-		await this.run(Operation.DeleteBranch, () => this.repository.deleteBranch(name, force));
+		return this.run(Operation.DeleteBranch, async () => {
+			await this.repository.deleteBranch(name, force);
+			await this.repository.config('unset', 'local', `branch.${name}.vscode-merge-base`);
+		});
 	}
 
 	async renameBranch(name: string): Promise<void> {
