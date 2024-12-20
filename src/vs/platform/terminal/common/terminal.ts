9abb61f10a9905511669465046fa26defd3d12c8
Megan Rogge <megan.rogge@microsoft.com>
2024-12-20 11:15:01 -0600

    Revert "Reland fix custom task shell doesn't work without manually passing in…"
    This reverts commit 330ab6c2928963dd83a7d90a271d9f2eb8db90d2.

--- src/vs/platform/terminal/common/terminal.ts
+++ src/vs/platform/terminal/common/terminal.ts
@@ -872,7 +872,6 @@ export interface ITerminalProfile {
 	overrideName?: boolean;
 	color?: string;
 	icon?: ThemeIcon | URI | { light: URI; dark: URI };
-	isAutomationShell?: boolean;
 }
 
 export interface ITerminalDimensionsOverride extends Readonly<ITerminalDimensions> {
