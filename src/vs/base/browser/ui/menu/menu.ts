03784fa9d055d88691cf34e6c2ce7d53f7c1fde3
g122622 <51020363+g122622@users.noreply.github.com>
2024-12-27 20:34:40 +0800

    Update menu.ts

--- src/vs/base/browser/ui/menu/menu.ts
+++ src/vs/base/browser/ui/menu/menu.ts
@@ -903,7 +903,7 @@ class SubmenuMenuActionViewItem extends BaseMenuActionViewItem {
 			const computedStyles = getWindow(this.parentData.parent.domNode).getComputedStyle(this.parentData.parent.domNode);
 			const paddingTop = parseFloat(computedStyles.paddingTop || '0') || 0;
 			// this.submenuContainer.style.top = `${this.element.offsetTop - this.parentData.parent.scrollOffset - paddingTop}px`;
-			this.submenuContainer.style.zIndex = '1';
+			// this.submenuContainer.style.zIndex = '1';
 			this.submenuContainer.style.position = 'fixed';
 			this.submenuContainer.style.top = '0';
 			this.submenuContainer.style.left = '0';
