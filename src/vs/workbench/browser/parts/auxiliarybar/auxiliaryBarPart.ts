37f2b7eed99e27eef014a044b40195d5fb95f156
Nang <nathanang2000@gmail.com>
2025-01-04 22:11:05 +0530

    Added min for the auxbar (#94)
    Co-authored-by: nang-dev <contactnang88@gmail.com>

--- src/vs/workbench/browser/parts/auxiliarybar/auxiliaryBarPart.ts
+++ src/vs/workbench/browser/parts/auxiliarybar/auxiliaryBarPart.ts
@@ -45,7 +45,7 @@ export class AuxiliaryBarPart extends AbstractPaneCompositePart {
 	static readonly viewContainersWorkspaceStateKey = 'workbench.auxiliarybar.viewContainersWorkspaceState';
 
 	// Use the side bar dimensions
-	override readonly minimumWidth: number = 170;
+	override readonly minimumWidth: number = 400;
 	override readonly maximumWidth: number = Number.POSITIVE_INFINITY;
 	override readonly minimumHeight: number = 0;
 	override readonly maximumHeight: number = Number.POSITIVE_INFINITY;
