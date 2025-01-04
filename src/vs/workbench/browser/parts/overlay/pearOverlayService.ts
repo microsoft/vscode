7af4df854d866d1270bdd3d083aae9d882ea7786
John Pan <john.pan@mail.mcgill.ca>
2025-01-04 22:08:23 +0530

    Handle overlay integration shortcuts (#84)

--- src/vs/workbench/browser/parts/overlay/pearOverlayService.ts
+++ src/vs/workbench/browser/parts/overlay/pearOverlayService.ts
@@ -10,6 +10,7 @@ import {
 } from "vs/platform/instantiation/common/instantiation";
 import { IEditorService } from "vs/workbench/services/editor/common/editorService";
 import { ITerminalService } from "vs/workbench/contrib/terminal/browser/terminal";
+import { CommandsRegistry } from "vs/platform/commands/common/commands";
 
 export const IPearOverlayService = createDecorator<IPearOverlayService>(
 	"pearaiOverlayService",
@@ -37,6 +38,11 @@ export interface IPearOverlayService extends IDisposable {
 	 * Toggles the visibility of the PearAI popup.
 	 */
 	toggle(): void;
+
+	/**
+	 * Returns true if the PearAI popup is visible.
+	 */
+	isVisible(): boolean;
 }
 
 export class PearOverlayService
@@ -52,11 +58,13 @@ export class PearOverlayService
 		private readonly instantiationService: IInstantiationService,
 		@IEditorService private readonly _editorService: IEditorService,
 		@ITerminalService private readonly _terminalService: ITerminalService,
+		// @ICommandService private readonly commandService: ICommandService,
 	) {
 		super();
 		this._pearOverlayPart =
 			this.instantiationService.createInstance(PearOverlayPart);
 		this.registerListeners();
+		this.registerCommands();
 	}
 
 	private registerListeners(): void {
@@ -71,6 +79,29 @@ export class PearOverlayService
 				this.hide();
 			}),
 		);
+	}
+
+	private registerCommands(): void {
+		// Register commands for external use e.g. in pearai submodule
+		CommandsRegistry.registerCommand('pearai.isOverlayVisible', (accessor) => {
+			const overlayService = accessor.get(IPearOverlayService);
+			return overlayService.isVisible();
+		});
+
+		CommandsRegistry.registerCommand('pearai.showOverlay', (accessor) => {
+			const overlayService = accessor.get(IPearOverlayService);
+			overlayService.show();
+		});
+
+		CommandsRegistry.registerCommand('pearai.hideOverlay', (accessor) => {
+			const overlayService = accessor.get(IPearOverlayService);
+			overlayService.hide();
+		});
+
+		CommandsRegistry.registerCommand('pearai.toggleOverlay', (accessor) => {
+			const overlayService = accessor.get(IPearOverlayService);
+			overlayService.toggle();
+		});
 	}
 
 	get pearOverlayPart(): PearOverlayPart {
@@ -93,6 +124,11 @@ export class PearOverlayService
 		super.dispose();
 		this._pearOverlayPart.dispose();
 	}
+
+	isVisible(): boolean {
+		return this._pearOverlayPart.isVisible();
+	}
+
 }
 
 registerSingleton(
