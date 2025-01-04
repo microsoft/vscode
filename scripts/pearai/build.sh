89708cd700d1c91b1effdddb8d6f5a41452d913b
Nang <nathanang2000@gmail.com>
2025-01-04 19:22:48 +0530

    Updated scripts (#89)
    * Updated scripts
    
    * Added build.sh

---
+++ scripts/pearai/build.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Function to execute a command and check its status
+execute() {
+	local cmd=$1
+	local failure_message=$2
+
+	$cmd
+	if [ $? -ne 0 ]; then
+		echo "Setup | $failure_message"
+		exit 1
+	fi
+}
+
+# Define the submodule directory
+submodule_dir="extensions/pearai-submodule"
+
+# Change to the submodule directory and install dependencies
+execute "cd $submodule_dir" "Failed to change directory to $submodule_dir"
+execute "./scripts/install-dependencies.sh" "Failed to install dependencies for the submodule"
+execute "./scripts/build-extension.sh" "Failed to build the extension"
+execute "cd ../../" "Failed to change back to the root directory"
+
+# Install dependencies using yarn
+execute "yarn" "Failed to install dependencies using yarn"
+
+# Success message
+echo "Dependencies Installed and Build Completed Successfully! ⭐"
