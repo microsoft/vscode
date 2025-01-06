6847f9e4ab98f881984708a11d7c715651b600f3
Ladislau Szomoru <3372902+lszomoru@users.noreply.github.com>
2025-01-06 22:00:52 +0100

    Git -  remove commands that are not used

--- extensions/git/src/timelineProvider.ts
+++ extensions/git/src/timelineProvider.ts
@@ -86,7 +86,7 @@ export class GitTimelineItem extends TimelineItem {
 		if (hash) {
 			this.tooltip.appendMarkdown(`---\n\n`);
 
-			this.tooltip.appendMarkdown(`[\`$(git-commit) ${getCommitShortHash(uri, hash)} \`](command:git.viewCommit2?${encodeURIComponent(JSON.stringify([uri, hash]))} "${l10n.t('View Commit')}")`);
+			this.tooltip.appendMarkdown(`[\`$(git-commit) ${getCommitShortHash(uri, hash)} \`](command:git.viewCommit?${encodeURIComponent(JSON.stringify([uri, hash]))} "${l10n.t('View Commit')}")`);
 			this.tooltip.appendMarkdown('&nbsp;');
 			this.tooltip.appendMarkdown(`[$(copy)](command:git.copyContentToClipboard?${encodeURIComponent(JSON.stringify(hash))} "${l10n.t('Copy Commit Hash')}")`);
 		}
