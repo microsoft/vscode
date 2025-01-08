bb93fdd965e0e044195b2fb6bbad252bb2525bd6
Anthony Kim <62267334+anthonykim1@users.noreply.github.com>
2025-01-07 21:57:15 -0800

    Update src/vscode-dts/vscode.proposed.terminalShellEnv.d.ts
    Co-authored-by: Daniel Imms <2193314+Tyriar@users.noreply.github.com>

--- src/vscode-dts/vscode.proposed.terminalShellEnv.d.ts
+++ src/vscode-dts/vscode.proposed.terminalShellEnv.d.ts
@@ -6,17 +6,13 @@
 declare module 'vscode' {
 	// @anthonykim1 @tyriar https://github.com/microsoft/vscode/issues/227467
 
-	// Expose the terminal's actual shell environment to extensions
 	export interface TerminalShellIntegration {
-		// The shell environment
-		// undefined means we don't know anything about the env
-		// NOTE: This is similar to command line in that it's verified with a nonce, however we just ignore when there's no nonce, so we don't need the trust flag like `TerminalShellExecutionCommandLine.isTrusted`
+		/**
+		 * The environment of the shell process. This is undefined if the shell integration script
+		 * does not send the environment.
+		 */
 		readonly env: { [key: string]: string | undefined } | undefined;
 	}
 
-	export namespace window {
-		// Fires when TerminalShellIntegration.env changes
-		// TODO: Should this just use `onDidChangeTerminalShellIntegration` like `TerminalShellIntegration.cwd` does?
-		export const onDidChangeTerminalShellIntegrationEnvironment: Event<TerminalShellIntegration>;
-	}
+	// TODO: Is it fine that this shares onDidChangeTerminalShellIntegration with cwd and the shellIntegration object itself?
 }
