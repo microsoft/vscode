2ae308fcd133a4aacb3cca808394f90439107fef
Ladislau Szomoru <3372902+lszomoru@users.noreply.github.com>
2025-01-10 13:55:34 +0100

    Git - fix timeline item hover rendering

--- extensions/git/src/timelineProvider.ts
+++ extensions/git/src/timelineProvider.ts
@@ -69,19 +69,20 @@ export class GitTimelineItem extends TimelineItem {
 		if (shortStat) {
 			this.tooltip.appendMarkdown(`---\n\n`);
 
+			const labels: string[] = [];
 			if (shortStat.insertions) {
-				this.tooltip.appendMarkdown(`<span style="color:var(--vscode-scmGraph-historyItemHoverAdditionsForeground);">${shortStat.insertions === 1 ?
+				labels.push(`<span style="color:var(--vscode-scmGraph-historyItemHoverAdditionsForeground);">${shortStat.insertions === 1 ?
 					l10n.t('{0} insertion{1}', shortStat.insertions, '(+)') :
 					l10n.t('{0} insertions{1}', shortStat.insertions, '(+)')}</span>`);
 			}
 
 			if (shortStat.deletions) {
-				this.tooltip.appendMarkdown(`,&nbsp;<span style="color:var(--vscode-scmGraph-historyItemHoverDeletionsForeground);">${shortStat.deletions === 1 ?
+				labels.push(`<span style="color:var(--vscode-scmGraph-historyItemHoverDeletionsForeground);">${shortStat.deletions === 1 ?
 					l10n.t('{0} deletion{1}', shortStat.deletions, '(-)') :
 					l10n.t('{0} deletions{1}', shortStat.deletions, '(-)')}</span>`);
 			}
 
-			this.tooltip.appendMarkdown(`\n\n`);
+			this.tooltip.appendMarkdown(`${labels.join(', ')}\n\n`);
 		}
 
 		if (hash) {
