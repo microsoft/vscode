2f4dee8b637e6e0df1acc8618181480251c226b8
Megan Rogge <merogge@microsoft.com>
2025-01-14 14:02:20 -0600

    Add comment

--- src/vs/workbench/services/suggest/browser/simpleCompletionModel.ts
+++ src/vs/workbench/services/suggest/browser/simpleCompletionModel.ts
@@ -207,6 +207,7 @@ export class SimpleCompletionModel {
 				}
 				// Then by file extension length ascending
 				score = a.fileExtLow.length - b.fileExtLow.length;
+				// Then by label alphabetically
 				score = a.completion.label.localeCompare(b.completion.label);
 			}
 			return score;
