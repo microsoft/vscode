71ac7457c767c43e7e0ee86a8f5e319a3e32bb8f
Benjamin Christopher Simmonds <44439583+benibenj@users.noreply.github.com>
2025-01-15 10:05:56 +0100

    Fix halo effect removal in inline completions (#237881)
    * fix halo effect removal
    
    * :lipstick:

--- src/vs/editor/contrib/inlineCompletions/browser/model/inlineCompletionsModel.ts
+++ src/vs/editor/contrib/inlineCompletions/browser/model/inlineCompletionsModel.ts
@@ -56,7 +56,8 @@ export class InlineCompletionsModel extends Disposable {
 	private readonly _editorObs = observableCodeEditor(this._editor);
 
 	private readonly _acceptCompletionDecorationTimer = this._register(new MutableDisposable());
-	private readonly _acceptCompletionDecoration: IModelDecorationOptions = {
+	private readonly _acceptCompletionDecorationCollection = this._editor.createDecorationsCollection();
+	private readonly _acceptCompletionDecorationOptions: IModelDecorationOptions = {
 		description: 'inline-completion-accepted',
 		className: 'inline-completion-accepted',
 		stickiness: TrackedRangeStickiness.NeverGrowsWhenTypingAtEdges
@@ -581,8 +582,6 @@ export class InlineCompletionsModel extends Disposable {
 			completion.source.addRef();
 		}
 
-		this._acceptCompletionDecorationTimer.clear();
-
 		editor.pushUndoStop();
 		if (completion.snippetInfo) {
 			editor.executeEdits(
@@ -605,8 +604,8 @@ export class InlineCompletionsModel extends Disposable {
 			editor.setSelections(selections, 'inlineCompletionAccept');
 
 			if (state.kind === 'inlineEdit') {
-				const acceptEditsDecorations = editor.createDecorationsCollection(modifiedRanges.map(r => ({ range: r, options: this._acceptCompletionDecoration })));
-				this._acceptCompletionDecorationTimer.value = disposableTimeout(() => acceptEditsDecorations.clear(), 2500);
+				this._acceptCompletionDecorationCollection.set(modifiedRanges.map(r => ({ range: r, options: this._acceptCompletionDecorationOptions })));
+				this._acceptCompletionDecorationTimer.value = disposableTimeout(() => this._acceptCompletionDecorationCollection.clear(), 2500);
 			}
 		}
 
