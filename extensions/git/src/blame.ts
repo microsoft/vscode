f067e39ebdb331ec59fff242283e8c8c72ffee92
Ladislau Szomoru <3372902+lszomoru@users.noreply.github.com>
2025-01-16 16:23:14 +0100

    Initial implementation

--- extensions/git/src/blame.ts
+++ extensions/git/src/blame.ts
@@ -12,7 +12,7 @@ import { BlameInformation, Commit } from './git';
 import { fromGitUri, isGitUri } from './uri';
 import { emojify, ensureEmojis } from './emoji';
 import { getWorkingTreeAndIndexDiffInformation, getWorkingTreeDiffInformation } from './staging';
-import { provideSourceControlHistoryItemHoverCommands, provideSourceControlHistoryItemMessageLinks } from './historyItemDetailsProvider';
+import { provideSourceControlHistoryItemAvatar, provideSourceControlHistoryItemHoverCommands, provideSourceControlHistoryItemMessageLinks } from './historyItemDetailsProvider';
 
 function lineRangesContainLine(changes: readonly TextEditorChange[], lineNumber: number): boolean {
 	return changes.some(c => c.modified.startLineNumber <= lineNumber && lineNumber < c.modified.endLineNumberExclusive);
@@ -205,6 +205,7 @@ export class GitBlameController {
 
 	async getBlameInformationHover(documentUri: Uri, blameInformation: BlameInformation, includeCommitDetails = false): Promise<MarkdownString> {
 		const remoteHoverCommands: Command[] = [];
+		let commitAvatar: string | undefined;
 		let commitInformation: Commit | undefined;
 		let commitMessageWithLinks: string | undefined;
 
@@ -216,6 +217,10 @@ export class GitBlameController {
 					commitInformation = await repository.getCommit(blameInformation.hash);
 				} catch { }
 			}
+
+			// Avatar
+			commitAvatar = await provideSourceControlHistoryItemAvatar(
+				this._model, repository, blameInformation.hash, blameInformation.authorName, blameInformation.authorEmail);
 
 			// Remote hover commands
 			const unpublishedCommits = await repository.getUnpublishedCommits();
@@ -234,16 +239,18 @@ export class GitBlameController {
 		markdownString.supportThemeIcons = true;
 
 		// Author, date
+		const hash = commitInformation?.hash ?? blameInformation.hash;
 		const authorName = commitInformation?.authorName ?? blameInformation.authorName;
 		const authorEmail = commitInformation?.authorEmail ?? blameInformation.authorEmail;
 		const authorDate = commitInformation?.authorDate ?? blameInformation.authorDate;
+		const avatar = commitAvatar ? `![${authorName}](${commitAvatar})` : '$(account)';
 
 		if (authorName) {
 			if (authorEmail) {
 				const emailTitle = l10n.t('Email');
-				markdownString.appendMarkdown(`$(account) [**${authorName}**](mailto:${authorEmail} "${emailTitle} ${authorName}")`);
+				markdownString.appendMarkdown(`${avatar} [**${authorName}**](mailto:${authorEmail} "${emailTitle} ${authorName}")`);
 			} else {
-				markdownString.appendMarkdown(`$(account) **${authorName}**`);
+				markdownString.appendMarkdown(`${avatar} **${authorName}**`);
 			}
 
 			if (authorDate) {
@@ -282,8 +289,6 @@ export class GitBlameController {
 		}
 
 		// Commands
-		const hash = commitInformation?.hash ?? blameInformation.hash;
-
 		markdownString.appendMarkdown(`[\`$(git-commit) ${getCommitShortHash(documentUri, hash)} \`](command:git.viewCommit?${encodeURIComponent(JSON.stringify([documentUri, hash]))} "${l10n.t('Open Commit')}")`);
 		markdownString.appendMarkdown('&nbsp;');
 		markdownString.appendMarkdown(`[$(copy)](command:git.copyContentToClipboard?${encodeURIComponent(JSON.stringify(hash))} "${l10n.t('Copy Commit Hash')}")`);
--- extensions/github/src/historyItemDetailsProvider.ts
+++ extensions/github/src/historyItemDetailsProvider.ts
@@ -4,12 +4,92 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { Command, l10n } from 'vscode';
-import { Repository, SourceControlHistoryItemDetailsProvider } from './typings/git';
-import { getRepositoryDefaultRemote, getRepositoryDefaultRemoteUrl } from './util';
+import { Commit, Repository as GitHubRepository, Maybe } from '@octokit/graphql-schema';
+import { API, Repository, SourceControlHistoryItemDetailsProvider } from './typings/git';
+import { DisposableStore, getRepositoryDefaultRemote, getRepositoryDefaultRemoteUrl, getRepositoryFromUrl, sequentialize } from './util';
+import { getOctokitGraphql } from './auth';
+
+const AVATAR_SIZE = 20;
 
 const ISSUE_EXPRESSION = /(([A-Za-z0-9_.\-]+)\/([A-Za-z0-9_.\-]+))?(#|GH-)([1-9][0-9]*)($|\b)/g;
 
+const ASSIGNABLE_USERS_QUERY = `
+	query assignableUsers($owner: String!, $repo: String!) {
+		repository(owner: $owner, name: $repo) {
+			assignableUsers(first: 100) {
+				nodes {
+					id
+					login
+					name
+					email
+					avatarUrl(size: ${AVATAR_SIZE})
+				}
+			}
+		}
+	}
+`;
+
+const COMMIT_AUTHOR_QUERY = `
+	query commitAuthor($owner: String!, $repo: String!, $commit: String!) {
+		repository(owner: $owner, name: $repo) {
+			object(expression: $commit) {
+				... on Commit {
+					author {
+						name
+						email
+						avatarUrl(size: ${AVATAR_SIZE})
+						user {
+							id
+							login
+						}
+					}
+				}
+			}
+		}
+	}
+`;
+
+interface GitHubUser {
+	readonly id: string;
+	readonly login: string;
+	readonly name?: Maybe<string>;
+	readonly email: string;
+	readonly avatarUrl: string;
+}
+
 export class GitHubSourceControlHistoryItemDetailsProvider implements SourceControlHistoryItemDetailsProvider {
+	private readonly _avatars = new Map<string, GitHubUser[]>();
+	private readonly _disposables = new DisposableStore();
+
+	constructor(private readonly _gitAPI: API) {
+		this._disposables.add(this._gitAPI.onDidCloseRepository(this._onDidCloseRepository));
+	}
+
+	async provideAvatar(repository: Repository, commit: string, authorName?: string, authorEmail?: string): Promise<string | undefined> {
+		const descriptor = getRepositoryDefaultRemote(repository);
+		if (!descriptor) {
+			return undefined;
+		}
+
+		// Get the first page of the assignable users
+		await this._loadAssignableUsers(descriptor);
+
+		const avatarUrl = this._avatars.get(this._getRepositoryKey(descriptor))?.find(
+			user => user.email === authorEmail || user.name === authorName)?.avatarUrl;
+
+		if (avatarUrl) {
+			return this._getAvatarUrl(avatarUrl, AVATAR_SIZE);
+		}
+
+		// Get the commit details
+		const commitAuthor = await this._getCommitAuthor(descriptor, commit);
+		if (commitAuthor) {
+			return this._getAvatarUrl(commitAuthor.avatarUrl, AVATAR_SIZE);
+		}
+
+		return undefined;
+	}
+
 	async provideHoverCommands(repository: Repository): Promise<Command[] | undefined> {
 		const url = getRepositoryDefaultRemoteUrl(repository);
 		if (!url) {
@@ -46,5 +126,96 @@ export class GitHubSourceControlHistoryItemDetailsProvider implements SourceCont
 
 				return `[${label}](https://github.com/${owner}/${repo}/issues/${number})`;
 			});
+	}
+
+	private _onDidCloseRepository(repository: Repository) {
+		for (const remote of repository.state.remotes) {
+			if (!remote.fetchUrl) {
+				continue;
+			}
+
+			const repository = getRepositoryFromUrl(remote.fetchUrl);
+			if (!repository) {
+				continue;
+			}
+
+			this._avatars.delete(this._getRepositoryKey(repository));
+		}
+	}
+
+	@sequentialize
+	private async _loadAssignableUsers(descriptor: { owner: string; repo: string }): Promise<void> {
+		if (this._avatars.has(this._getRepositoryKey(descriptor))) {
+			return;
+		}
+
+		try {
+			const graphql = await getOctokitGraphql();
+			const { repository } = await graphql<{ repository: GitHubRepository }>(ASSIGNABLE_USERS_QUERY, descriptor);
+
+			const users: GitHubUser[] = [];
+			for (const node of repository.assignableUsers.nodes ?? []) {
+				if (!node) {
+					continue;
+				}
+
+				users.push({
+					id: node.id,
+					login: node.login,
+					name: node.name,
+					email: node.email,
+					avatarUrl: node.avatarUrl,
+				} satisfies GitHubUser);
+			}
+
+			this._avatars.set(this._getRepositoryKey(descriptor), users);
+		} catch {
+			// TODO@lszomoru - log error
+		}
+	}
+
+	private async _getCommitAuthor(descriptor: { owner: string; repo: string }, commit: string): Promise<GitHubUser | undefined> {
+		try {
+			const graphql = await getOctokitGraphql();
+			const { repository } = await graphql<{ repository: GitHubRepository }>(COMMIT_AUTHOR_QUERY, { ...descriptor, commit });
+
+			const commitAuthor = (repository.object as Commit).author;
+			if (!commitAuthor?.user?.id || !commitAuthor.user?.login ||
+				!commitAuthor?.name || !commitAuthor?.email || !commitAuthor?.avatarUrl) {
+				return undefined;
+			}
+
+			const user = {
+				id: commitAuthor.user.id,
+				login: commitAuthor.user.login,
+				name: commitAuthor.name,
+				email: commitAuthor.email,
+				avatarUrl: commitAuthor.avatarUrl,
+			} satisfies GitHubUser;
+
+			// Save the user
+			const users = this._avatars.get(this._getRepositoryKey(descriptor));
+			if (users) {
+				users.push(user);
+			}
+
+			return user;
+		} catch {
+			// TODO@lszomoru - log error
+		}
+
+		return undefined;
+	}
+
+	private _getAvatarUrl(url: string, size: number): string {
+		return `${url}|height=${size},width=${size}`;
+	}
+
+	private _getRepositoryKey(descriptor: { owner: string; repo: string }): string {
+		return `${descriptor.owner}/${descriptor.repo}`;
+	}
+
+	dispose(): void {
+		this._disposables.dispose();
 	}
 }
