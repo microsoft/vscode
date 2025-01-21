b4d3e3124615b8f60d4fc80221a605b66a8063f8
Anthony Kim <anthonykim@microsoft.com>
2025-01-21 14:59:41 -0500

    edit terminalShellEnv.d.ts to allow env.value and env.isTrusted

--- src/vscode-dts/vscode.proposed.terminalShellEnv.d.ts
+++ src/vscode-dts/vscode.proposed.terminalShellEnv.d.ts
@@ -11,18 +11,23 @@ declare module 'vscode' {
 		 * The environment of the shell process. This is undefined if the shell integration script
 		 * does not send the environment.
 		 */
-		readonly env: { [key: string]: string | undefined } | undefined;
+		readonly env: {
+			/**
+			 * The dictionary of environment variables.
+			 */
+			value: { [key: string]: string | undefined } | undefined;
 
-		/**
-		 * Whether the environment came from a trusted source and is therefore safe to used its
-		 * values in a manner that could lead to execution of arbitrary code. If this value is
-		 * `false`, {@link env} should either not be used for something that could lead to arbitrary
-		 * code execution, or the user should be warned beforehand.
-		 *
-		 * This is `true` only when the environment was reported explicitly and it used a nonce for
-		 * verification.
-		 */
-		readonly isTrusted: boolean;
+			/**
+			 * Whether the environment came from a trusted source and is therefore safe to use its
+			 * values in a manner that could lead to execution of arbitrary code. If this value is
+			 * `false`, {@link value} should either not be used for something that could lead to arbitrary
+			 * code execution, or the user should be warned beforehand.
+			 *
+			 * This is `true` only when the environment was reported explicitly and it used a nonce for
+			 * verification.
+			 */
+			isTrusted: boolean;
+		};
 	}
 
 	// TODO: Is it fine that this shares onDidChangeTerminalShellIntegration with cwd and the shellIntegration object itself?
