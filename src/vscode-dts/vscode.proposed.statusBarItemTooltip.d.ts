cc3aa1b4c5b7a745e2afd14473d89945934423fb
Benjamin Pasero <benjamin.pasero@microsoft.com>
2025-01-21 08:22:17 +0100

    docs

--- src/vscode-dts/vscode.proposed.statusBarItemTooltip.d.ts
+++ src/vscode-dts/vscode.proposed.statusBarItemTooltip.d.ts
@@ -8,6 +8,12 @@ declare module 'vscode' {
 	// https://github.com/microsoft/vscode/issues/234339
 
 	export interface StatusBarItem {
+
+		/**
+		 * The tooltip text when you hover over this entry.
+		 *
+		 * Can optionally return the tooltip in a thenable if the computation is expensive.
+		 */
 		tooltip2: string | MarkdownString | undefined | ((token: CancellationToken) => ProviderResult<string | MarkdownString | undefined>);
 	}
 }
