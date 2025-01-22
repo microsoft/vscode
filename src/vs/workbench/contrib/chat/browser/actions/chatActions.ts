b611fd2a16caf1cd81d61eb55ee2eb3b46b4fd1c
Nang <nathanang2000@gmail.com>
2025-01-22 14:03:21 -0800

    Command center fixes (#165)
    * Added command center fix
    
    * fixed

--- src/vs/workbench/contrib/chat/browser/actions/chatActions.ts
+++ src/vs/workbench/contrib/chat/browser/actions/chatActions.ts
@@ -27,7 +27,6 @@ import { KeybindingWeight } from '../../../../../platform/keybinding/common/keyb
 import { IOpenerService } from '../../../../../platform/opener/common/opener.js';
 import product from '../../../../../platform/product/common/product.js';
 import { IQuickInputButton, IQuickInputService, IQuickPickItem, IQuickPickSeparator } from '../../../../../platform/quickinput/common/quickInput.js';
-import { ToggleTitleBarConfigAction } from '../../../../browser/parts/titlebar/titlebarActions.js';
 import { IWorkbenchContribution } from '../../../../common/contributions.js';
 import { IEditorGroupsService } from '../../../../services/editor/common/editorGroupsService.js';
 import { ACTIVE_GROUP, IEditorService } from '../../../../services/editor/common/editorService.js';
@@ -527,38 +526,6 @@ const defaultChat = {
 	managePlanUrl: product.defaultChatAgent?.managePlanUrl ?? '',
 };
 
-MenuRegistry.appendMenuItem(MenuId.CommandCenter, {
-	submenu: MenuId.ChatCommandCenter,
-	title: localize('title4', "Chat"),
-	icon: Codicon.copilot,
-	when: ContextKeyExpr.and(
-		ContextKeyExpr.has('config.chat.commandCenter.enabled'),
-		ContextKeyExpr.or(
-			ContextKeyExpr.has('config.chat.experimental.offerSetup'),
-			ChatContextKeys.Setup.installed,
-			ChatContextKeys.panelParticipantRegistered
-		)
-	),
-	order: 10001,
-});
-
-registerAction2(class ToggleCopilotControl extends ToggleTitleBarConfigAction {
-	constructor() {
-		super(
-			'chat.commandCenter.enabled',
-			localize('toggle.chatControl', 'Copilot Controls'),
-			localize('toggle.chatControlsDescription', "Toggle visibility of the Copilot Controls in title bar"), 4, false,
-			ContextKeyExpr.and(
-				ContextKeyExpr.has('config.window.commandCenter'),
-				ContextKeyExpr.or(
-					ChatContextKeys.Setup.installed,
-					ContextKeyExpr.has('config.chat.experimental.offerSetup'),
-					ChatContextKeys.panelParticipantRegistered
-				)
-			)
-		);
-	}
-});
 
 export class ChatCommandCenterRendering extends Disposable implements IWorkbenchContribution {
 
