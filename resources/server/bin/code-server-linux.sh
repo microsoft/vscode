b89e1cd84a521edf3b7aa0bca4ddc09ce705723c
deepak1556 <hop2deep@gmail.com>
2025-01-22 15:02:38 +0900

    chore: add logging

--- resources/server/bin/code-server-linux.sh
+++ resources/server/bin/code-server-linux.sh
@@ -10,8 +10,11 @@ esac
 ROOT="$(dirname "$(dirname "$(readlink -f "$0")")")"
 
 if [ -n "$VSCODE_SERVER_CUSTOM_GLIBC_LINKER" ] && [ -n "$VSCODE_SERVER_CUSTOM_GLIBC_PATH" ] && [ -n "$VSCODE_SERVER_PATCHELF_PATH" ]; then
+	echo "Patching glibc from $VSCODE_SERVER_CUSTOM_GLIBC_PATH with $VSCODE_SERVER_PATCHELF_PATH..."
 	"$VSCODE_SERVER_PATCHELF_PATH" --set-rpath "$VSCODE_SERVER_CUSTOM_GLIBC_PATH" "$ROOT/node"
+	echo "Patching linker from $VSCODE_SERVER_CUSTOM_GLIBC_LINKER with $VSCODE_SERVER_PATCHELF_PATH..."
 	"$VSCODE_SERVER_PATCHELF_PATH" --set-interpreter "$VSCODE_SERVER_CUSTOM_GLIBC_LINKER" "$ROOT/node"
+	echo "Patching complete."
 fi
 
 "$ROOT/node" ${INSPECT:-} "$ROOT/out/server-main.js" "$@"
