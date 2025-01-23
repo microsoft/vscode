ab172453fd2fe51a2bcf556c64542ee45675b3e8
Benjamin Christopher Simmonds <44439583+benibenj@users.noreply.github.com>
2025-01-23 18:47:17 +0100

    Support showing multi line changes as overlay (#238585)
    * multi line replacement
    
    * fix test?

--- src/vs/editor/contrib/inlineCompletions/browser/view/inlineEdits/wordReplacementView.ts
+++ src/vs/editor/contrib/inlineCompletions/browser/view/inlineEdits/wordReplacementView.ts
@@ -3,7 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { Disposable } from '../../../../../../base/common/lifecycle.js';
+import { Disposable, toDisposable } from '../../../../../../base/common/lifecycle.js';
 import { constObservable, derived, mapObservableArrayCached } from '../../../../../../base/common/observable.js';
 import { editorHoverStatusBarBackground } from '../../../../../../platform/theme/common/colorRegistry.js';
 import { registerColor, transparent } from '../../../../../../platform/theme/common/colorUtils.js';
@@ -20,6 +20,10 @@ import { LineTokens } from '../../../../../common/tokens/lineTokens.js';
 import { TokenArray } from '../../../../../common/tokens/tokenArray.js';
 import { mapOutFalsy, n, rectToProps } from './utils.js';
 import { localize } from '../../../../../../nls.js';
+import { Range } from '../../../../../common/core/range.js';
+import { LineRange } from '../../../../../common/core/lineRange.js';
+import { InlineDecoration, InlineDecorationType } from '../../../../../common/viewModel.js';
+import { IModelDecorationOptions, TrackedRangeStickiness } from '../../../../../common/model.js';
 export const transparentHoverBackground = registerColor(
 	'inlineEdit.wordReplacementView.background',
 	{
@@ -251,6 +255,287 @@ export class WordReplacementView extends Disposable {
 			allowEditorOverflow: false,
 		}));
 	}
+}
+
+export class LineReplacementView extends Disposable {
+
+	private readonly _originalBubblesDecorationCollection = this._editor.editor.createDecorationsCollection();
+	private readonly _originalBubblesDecorationOptions: IModelDecorationOptions = {
+		description: 'inlineCompletions-original-bubble',
+		className: 'inlineCompletions-original-bubble',
+		stickiness: TrackedRangeStickiness.NeverGrowsWhenTypingAtEdges
+	};
+
+	private readonly maxPrefixTrim = derived(this, reader => {
+		const maxPrefixTrim = Math.max(...this._replacements.flatMap(r => [r.originalRange, r.modifiedRange]).map(r => r.isSingleLine() ? r.startColumn - 1 : 0));
+		if (maxPrefixTrim === 0) {
+			return 0;
+		}
+
+		const textModel = this._editor.editor.getModel()!;
+
+		const getLineTrimColLength = (line: string) => {
+			let i = 0;
+			while (i < line.length && line[i] === ' ') { i++; }
+			return i;
+		};
+
+		// TODO: make sure this works for tabs
+		return Math.min(
+			maxPrefixTrim,
+			...this._originalRange.mapToLineArray(line => getLineTrimColLength(textModel.getLineContent(line))),
+			...this._modifiedLines.map(line => getLineTrimColLength(line))
+		);
+	});
+
+	private readonly _modifiedLineElements = derived(reader => {
+
+		const maxPrefixTrim = this.maxPrefixTrim.read(reader);
+
+		const lines = [];
+		let requiredWidth = 0;
+
+		const modifiedBubbles = rangesToBubbleRanges(this._replacements.map(r => r.modifiedRange)).map(r => new Range(r.startLineNumber, r.startColumn - maxPrefixTrim, r.endLineNumber, r.endColumn - maxPrefixTrim));
+
+		const textModel = this._editor.model.get()!;
+		const startLineNumber = this._modifiedRange.startLineNumber;
+		for (let i = 0; i < this._modifiedRange.length; i++) {
+			const line = document.createElement('div');
+			const lineNumber = startLineNumber + i;
+			const modLine = this._modifiedLines[i].replace(/\t\n/g, '').slice(maxPrefixTrim);
+
+			const t = textModel.tokenization.tokenizeLinesAt(lineNumber, [modLine])?.[0];
+			let tokens: LineTokens;
+			if (t) {
+				tokens = TokenArray.fromLineTokens(t).toLineTokens(modLine, this._languageService.languageIdCodec);
+			} else {
+				tokens = LineTokens.createEmpty(modLine, this._languageService.languageIdCodec);
+			}
+
+			const decorations = [];
+			for (const modified of modifiedBubbles.filter(b => b.startLineNumber === lineNumber)) {
+				decorations.push(new InlineDecoration(new Range(1, modified.startColumn, 1, modified.endColumn), 'inlineCompletions-modified-bubble', InlineDecorationType.Regular));
+				decorations.push(new InlineDecoration(new Range(1, modified.startColumn, 1, modified.startColumn + 1), 'start', InlineDecorationType.Regular));
+				decorations.push(new InlineDecoration(new Range(1, modified.endColumn - 1, 1, modified.endColumn), 'end', InlineDecorationType.Regular));
+			}
+
+			const result = renderLines(new LineSource([tokens]), RenderOptions.fromEditor(this._editor.editor).withSetWidth(false), decorations, line, true);
+
+			requiredWidth = Math.max(requiredWidth, result.minWidthInPx);
+
+			lines.push(line);
+		}
+
+		return { lines, requiredWidth: requiredWidth - 10 }; // TODO: Width is always too large, why?
+	});
+
+	private readonly _layout = derived(this, reader => {
+		const { requiredWidth } = this._modifiedLineElements.read(reader);
+
+		const lineHeight = this._editor.getOption(EditorOption.lineHeight).read(reader);
+		const contentLeft = this._editor.layoutInfoContentLeft.read(reader);
+		const scrollLeft = this._editor.scrollLeft.read(reader);
+		const scrollTop = this._editor.scrollTop.read(reader);
+		const editorLeftOffset = contentLeft - scrollLeft;
+		const w = this._editor.getOption(EditorOption.fontInfo).read(reader).typicalHalfwidthCharacterWidth;
+		const PADDING = 4;
+
+		const editorModel = this._editor.editor.getModel()!;
+		const maxPrefixTrim = this.maxPrefixTrim.read(reader);
+
+		// TODO, correctly count tabs
+		const originalLineContents: string[] = [];
+		this._originalRange.forEach(line => originalLineContents.push(editorModel.getLineContent(line)));
+		const maxOriginalLineLength = Math.max(...originalLineContents.map(l => l.length)) - maxPrefixTrim;
+		const maxLineWidth = Math.max(maxOriginalLineLength * w, requiredWidth);
+
+		const startLineNumber = this._originalRange.startLineNumber;
+		const endLineNumber = this._originalRange.endLineNumberExclusive - 1;
+		const topOfOriginalLines = this._editor.editor.getTopForLineNumber(startLineNumber) - scrollTop;
+		const bottomOfOriginalLines = this._editor.editor.getBottomForLineNumber(endLineNumber) - scrollTop;
+
+		if (bottomOfOriginalLines <= 0) {
+			return undefined;
+		}
+
+		const prefixTrimOffset = maxPrefixTrim * w;
+
+		// Box Widget positioning
+		const originalLine = Rect.fromLeftTopWidthHeight(
+			editorLeftOffset + prefixTrimOffset,
+			topOfOriginalLines,
+			maxLineWidth,
+			bottomOfOriginalLines - topOfOriginalLines + PADDING
+		);
+		const modifiedLine = Rect.fromLeftTopWidthHeight(
+			originalLine.left,
+			originalLine.bottom + PADDING,
+			originalLine.width,
+			this._modifiedRange.length * lineHeight
+		);
+		const background = Rect.hull([originalLine, modifiedLine]).withMargin(PADDING);
+
+		const lowerBackground = background.intersectVertical(new OffsetRange(originalLine.bottom, Number.MAX_SAFE_INTEGER));
+		const lowerText = new Rect(lowerBackground.left + PADDING, lowerBackground.top + PADDING, lowerBackground.right, lowerBackground.bottom);
+
+		return {
+			originalLine,
+			modifiedLine,
+			background,
+			lowerBackground,
+			lowerText,
+			padding: PADDING,
+			minContentWidthRequired: prefixTrimOffset + maxLineWidth + PADDING * 2,
+		};
+	});
+
+	private readonly _div = n.div({
+		class: 'line-replacement',
+	}, [
+		derived(reader => {
+			const layout = mapOutFalsy(this._layout).read(reader);
+			if (!layout) {
+				return [];
+			}
+
+			const layoutProps = layout.read(reader);
+			const scrollLeft = this._editor.scrollLeft.read(reader);
+			let contentLeft = this._editor.layoutInfoContentLeft.read(reader);
+			let contentWidth = this._editor.contentWidth.read(reader);
+			const contentHeight = this._editor.editor.getContentHeight();
+
+			if (scrollLeft === 0) {
+				contentLeft -= layoutProps.padding;
+				contentWidth += layoutProps.padding;
+			}
+
+			const lineHeight = this._editor.getOption(EditorOption.lineHeight).read(reader);
+			const modifiedLines = this._modifiedLineElements.read(reader).lines;
+			modifiedLines.forEach(l => {
+				l.style.width = `${layout.read(reader).lowerText.width}px`;
+				l.style.height = `${lineHeight}px`;
+				l.style.position = 'relative';
+			});
+
+			return [
+				n.div({
+					style: {
+						position: 'absolute',
+						top: 0,
+						left: contentLeft,
+						width: contentWidth,
+						height: contentHeight,
+						overflow: 'hidden',
+						pointerEvents: 'none',
+					}
+				}, [
+					n.div({ // overlay to make sure the code is not visible between original and modified lines
+						style: {
+							position: 'absolute',
+							top: layoutProps.lowerBackground.top - layoutProps.padding,
+							left: layoutProps.lowerBackground.left - contentLeft,
+							width: layoutProps.lowerBackground.width,
+							height: layoutProps.padding * 2,
+							background: 'var(--vscode-editor-background)',
+						},
+					}),
+					n.div({ // styling for the modified lines widget
+						style: {
+							position: 'absolute',
+							...rectToProps(reader => layout.read(reader).lowerBackground.moveLeft(contentLeft)),
+							borderRadius: '4px',
+							background: 'var(--vscode-editor-background)',
+							boxShadow: 'var(--vscode-scrollbar-shadow) 0 6px 6px -6px',
+							borderTop: '1px solid var(--vscode-editorHoverWidget-border)',
+							overflow: 'hidden',
+						},
+					}, [
+						n.div({ // adds background color to modified lines widget (may be transparent)
+							style: {
+								position: 'absolute',
+								top: 0,
+								left: 0,
+								width: '100%',
+								height: '100%',
+								background: 'var(--vscode-diffEditor-insertedLineBackground)',
+								opacity: '0.5',
+							},
+						})
+					]),
+					n.div({
+						style: {
+							position: 'absolute',
+							padding: '0px',
+							boxSizing: 'border-box',
+							...rectToProps(reader => layout.read(reader).lowerText.moveLeft(contentLeft)),
+							fontFamily: this._editor.getOption(EditorOption.fontFamily),
+							fontSize: this._editor.getOption(EditorOption.fontSize),
+							fontWeight: this._editor.getOption(EditorOption.fontWeight),
+							pointerEvents: 'none',
+						}
+					}, [...modifiedLines]),
+					n.div({
+						style: {
+							position: 'absolute',
+							...rectToProps(reader => layout.read(reader).background.moveLeft(contentLeft)),
+							borderRadius: '4px',
+
+							border: '1px solid var(--vscode-editorHoverWidget-border)',
+							//background: 'rgba(122, 122, 122, 0.12)', looks better
+							background: 'var(--vscode-inlineEdit-wordReplacementView-background)',
+							pointerEvents: 'none',
+							boxSizing: 'border-box',
+						}
+					}, []),
+				])
+			];
+		})
+	]).keepUpdated(this._store);
+
+	constructor(
+		private readonly _editor: ObservableCodeEditor,
+		private readonly _originalRange: LineRange,
+		private readonly _modifiedRange: LineRange,
+		private readonly _modifiedLines: string[],
+		private readonly _replacements: readonly Replacement[],
+		@ILanguageService private readonly _languageService: ILanguageService,
+	) {
+		super();
+
+		this._register(toDisposable(() => this._originalBubblesDecorationCollection.clear()));
+
+		const originalBubbles = rangesToBubbleRanges(this._replacements.map(r => r.originalRange));
+		this._originalBubblesDecorationCollection.set(originalBubbles.map(r => ({ range: r, options: this._originalBubblesDecorationOptions })));
+
+		this._register(this._editor.createOverlayWidget({
+			domNode: this._div.element,
+			minContentWidthInPx: derived(reader => { // TODO: is this helping?
+				return this._layout.read(reader)?.minContentWidthRequired ?? 0;
+			}),
+			position: constObservable({ preference: { top: 0, left: 0 } }),
+			allowEditorOverflow: false,
+		}));
+	}
+}
+
+function rangesToBubbleRanges(ranges: Range[]): Range[] {
+	const result: Range[] = [];
+	while (ranges.length) {
+		let range = ranges.shift()!;
+		if (range.startLineNumber !== range.endLineNumber) {
+			ranges.push(new Range(range.startLineNumber + 1, 1, range.endLineNumber, range.endColumn));
+			range = new Range(range.startLineNumber, range.startColumn, range.startLineNumber, Number.MAX_SAFE_INTEGER); // TODO: this is not correct
+		}
+
+		result.push(range);
+	}
+	return result;
+
+}
+
+export interface Replacement {
+	originalRange: Range;
+	modifiedRange: Range;
 }
 
 export class WordInsertView extends Disposable {
