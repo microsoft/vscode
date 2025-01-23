5c226b8a8237e20f7842181abd7a8dca728e2ff9
Megan Rogge <merogge@microsoft.com>
2025-01-23 16:58:06 -0600

    add comment

--- src/vs/workbench/contrib/terminalContrib/suggest/browser/terminalSuggestAddon.ts
+++ src/vs/workbench/contrib/terminalContrib/suggest/browser/terminalSuggestAddon.ts
@@ -401,6 +401,9 @@ export class SuggestAddon extends Disposable implements ITerminalAddon, ISuggest
 		const fontInfo: ISimpleSuggestWidgetFontInfo = {
 			fontFamily: font.fontFamily,
 			fontSize: font.fontSize,
+			// In the editor's world, lineHeight is the pixels between the baselines of two lines of text
+			// In the terminal's world, lineHeight is the multiplier of the font size
+			// 1.5 is needed so that it's taller than a 16px icon
 			lineHeight: Math.ceil(c.lineHeight * font.fontSize * 1.5),
 			fontWeight: c.fontWeight.toString(),
 			letterSpacing: font.letterSpacing
