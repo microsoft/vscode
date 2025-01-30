cdb4a0132a8f34d4f44470c073b200a519e9061f
Megan Rogge <merogge@microsoft.com>
2025-01-30 12:18:06 -0600

    rm condition

--- src/vs/workbench/services/suggest/browser/simpleCompletionModel.ts
+++ src/vs/workbench/services/suggest/browser/simpleCompletionModel.ts
@@ -212,26 +212,22 @@ export class SimpleCompletionModel {
 					return score;
 				}
 			}
-			if ((fileExtScore(a.fileExtLow) === 0 && fileExtScore(b.fileExtLow) === 0)) {
-				// Count depth of path (number of / or \ occurrences)
-				score = count(a.labelLowExcludeTrailingFileSep, '/') + count(a.labelLowExcludeTrailingFileSep, '\\') - count(b.labelLowExcludeTrailingFileSep, '/') - count(b.labelLowExcludeTrailingFileSep, '\\');
-				if (score !== 0) {
-					return score;
-				}
-
-				// Ensure shorter prefixes appear first
-				if (b.labelLowExcludeTrailingFileSep.startsWith(a.labelLowExcludeTrailingFileSep)) {
-					return -1; // `a` is a prefix of `b`, so `a` should come first
-				}
-				if (a.labelLowExcludeTrailingFileSep.startsWith(b.labelLowExcludeTrailingFileSep)) {
-					return 1; // `b` is a prefix of `a`, so `b` should come first
-				}
-
-				// Sort alphabetically
-				return a.labelLow.localeCompare(b.labelLow);
+			// Count depth of path (number of / or \ occurrences)
+			score = count(a.labelLowExcludeTrailingFileSep, '/') + count(a.labelLowExcludeTrailingFileSep, '\\') - count(b.labelLowExcludeTrailingFileSep, '/') - count(b.labelLowExcludeTrailingFileSep, '\\');
+			if (score !== 0) {
+				return score;
 			}
 
-			return score;
+			// Ensure shorter prefixes appear first
+			if (b.labelLowExcludeTrailingFileSep.startsWith(a.labelLowExcludeTrailingFileSep)) {
+				return -1; // `a` is a prefix of `b`, so `a` should come first
+			}
+			if (a.labelLowExcludeTrailingFileSep.startsWith(b.labelLowExcludeTrailingFileSep)) {
+				return 1; // `b` is a prefix of `a`, so `b` should come first
+			}
+
+			// Sort alphabetically
+			return a.labelLow.localeCompare(b.labelLow);
 		});
 		this._refilterKind = Refilter.Nothing;
 
