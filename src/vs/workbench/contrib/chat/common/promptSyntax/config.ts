bd43946a62716a3eb13fc3779d07ee5f6dae5fbc
Oleg Solomko <legomushroom@gmail.com>
2025-01-31 09:13:23 -0800

    [config]: fix localization ID compilation issue

--- src/vs/workbench/contrib/chat/common/promptSyntax/config.ts
+++ src/vs/workbench/contrib/chat/common/promptSyntax/config.ts
@@ -216,16 +216,14 @@ export namespace PromptFilesConfig {
 		return DEFAULT_LOCATION;
 	};
 
-	const localizationID = `${CONFIG_KEY}.config`;
-	const descriptionLocalizationID = `${localizationID}.description`;
 	const usageExample1 = nls.localize(
-		`${descriptionLocalizationID}.example1`,
+		`chat.promptFiles.config.description.example1`,
 		"Enable with the default location of the prompt files (`{0}`):\n{1}",
 		DEFAULT_LOCATION[0],
 		`\`\`\`json\n{\n  "${CONFIG_KEY}": true,\n}\n\`\`\``,
 	);
 	const usageExample2 = nls.localize(
-		`${descriptionLocalizationID}.example2`,
+		`chat.promptFiles.config.description.example2`,
 		"Specify custom location(s) of the prompt files:\n{0}",
 		`\`\`\`json\n{\n  "${CONFIG_KEY}": {\n    ".github/prompts": true,\n    "/Users/vscode/prompts": true,\n}\n\`\`\``,
 	);
@@ -234,7 +232,7 @@ export namespace PromptFilesConfig {
 	 * Configuration setting description to use in the settings UI.
 	 */
 	export const CONFIG_DESCRIPTION = nls.localize(
-		descriptionLocalizationID,
+		'chat.promptFiles.config.description',
 		"Enable support for attaching reusable prompt files (`*{0}`) for Chat, Edits, and Inline Chat sessions. [Learn More]({1}).\n\nSet to `true` or use the `{ \"/path/to/folder\": boolean }` notation to specify a different path (or a couple of them). Relative paths are resolved from the root folder(s) of your workspace, and the default value of `{2}` is used if no other paths provided.\n### Examples\n{3}\n{4}",
 		PROMPT_SNIPPET_FILE_EXTENSION,
 		DOCUMENTATION_URL,
@@ -247,7 +245,7 @@ export namespace PromptFilesConfig {
 	 * Configuration setting title to use in the settings UI.
 	 */
 	export const CONFIG_TITLE = nls.localize(
-		`${localizationID}.title`,
+		`chat.promptFiles.config.title`,
 		"Prompt Files",
 	);
 }
