c4fd81cbab479cc13c04530982d3584b793812e7
Oleg Solomko <legomushroom@gmail.com>
2025-02-04 18:55:11 -0800

    fix unit tests on `Windows` add more doc comments

--- src/vs/workbench/contrib/chat/test/common/promptSyntax/testUtils/expectedReference.ts
+++ src/vs/workbench/contrib/chat/test/common/promptSyntax/testUtils/expectedReference.ts
@@ -15,13 +15,43 @@ import { TErrorCondition } from '../../../../common/promptSyntax/parsers/basePro
  * Options for the {@link ExpectedReference} class.
  */
 interface IExpectedReferenceOptions {
+	/**
+	 * Final `URI` of the reference.
+	 */
 	readonly uri: URI;
+
+	/**
+	 * Full text of the reference as it appears in the source text.
+	 */
 	readonly text: string;
+
+	/**
+	 * The `path` part of the reference (e.g., the `/abs/path/to/file.md`
+	 * part of the `[](/abs/path/to/file.md)` reference).
+	 */
 	readonly path: string;
+
+	/**
+	 * Start line of the reference in the source text. Because links cannot
+	 * contain line breaks, the end line number is also equal to this value.
+	 */
 	readonly startLine: number;
+
+	/**
+	 * Start column of the full reference text as it appears in the source text.
+	 */
 	readonly startColumn: number;
+
+	/**
+	 * Start column number of the `path` part of the reference.
+	 */
 	readonly pathStartColumn: number;
-	readonly childrenOrError?: TErrorCondition | (ExpectedReference[]);
+
+	/**
+	 * Either an `error` that was generated during attempt to resolve this reference,
+	 * or a list of expected child references if the attempt was successful.
+	 */
+	readonly childrenOrError?: TErrorCondition | ExpectedReference[];
 }
 
 /**
