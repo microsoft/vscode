42d7d0b6c7b335feb2cfd3f5e9b09110f1efda83
Daniel Imms <2193314+Tyriar@users.noreply.github.com>
2025-02-06 01:54:32 -0800

    Add tests for shell-parser

---
+++ extensions/terminal-suggest/src/fig/shell-parser/test/command.test.ts
@@ -0,0 +1,33 @@
+import { deepStrictEqual } from 'node:assert';
+import { getCommand, Command } from "../src/command";
+
+suite("shell-parser getCommand", () => {
+	const aliases = {
+		woman: "man",
+		quote: "'q'",
+		g: "git",
+	};
+	const getTokenText = (command: Command | null) => command?.tokens.map((token) => token.text) ?? [];
+
+	test("works without matching aliases", () => {
+		deepStrictEqual(getTokenText(getCommand("git co ", {})), ["git", "co", ""]);
+		deepStrictEqual(getTokenText(getCommand("git co ", aliases)), ["git", "co", ""]);
+		deepStrictEqual(getTokenText(getCommand("woman ", {})), ["woman", ""]);
+		deepStrictEqual(getTokenText(getCommand("another string ", aliases)), [
+			"another",
+			"string",
+			"",
+		]);
+	});
+
+	test("works with regular aliases", () => {
+		// Don't change a single token.
+		deepStrictEqual(getTokenText(getCommand("woman", aliases)), ["woman"]);
+		// Change first token if length > 1.
+		deepStrictEqual(getTokenText(getCommand("woman ", aliases)), ["man", ""]);
+		// Don't change later tokens.
+		deepStrictEqual(getTokenText(getCommand("man woman ", aliases)), ["man", "woman", ""]);
+		// Handle quotes
+		deepStrictEqual(getTokenText(getCommand("quote ", aliases)), ["q", ""]);
+	});
+});
