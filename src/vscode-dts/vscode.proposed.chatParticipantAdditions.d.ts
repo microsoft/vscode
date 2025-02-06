b96caf191e8107e992c564b88e600da1e1d9597d
Rob Lourens <roblourens@gmail.com>
2025-02-05 17:34:23 -0800

    Move variable types that completions need into additions.d.ts (#239752)

--- src/vscode-dts/vscode.proposed.chatParticipantAdditions.d.ts
+++ src/vscode-dts/vscode.proposed.chatParticipantAdditions.d.ts
@@ -397,4 +397,30 @@ declare module 'vscode' {
 	export namespace lm {
 		export function fileIsIgnored(uri: Uri, token: CancellationToken): Thenable<boolean>;
 	}
+
+	export interface ChatVariableValue {
+		/**
+		 * The detail level of this chat variable value. If possible, variable resolvers should try to offer shorter values that will consume fewer tokens in an LLM prompt.
+		 */
+		level: ChatVariableLevel;
+
+		/**
+		 * The variable's value, which can be included in an LLM prompt as-is, or the chat participant may decide to read the value and do something else with it.
+		 */
+		value: string | Uri;
+
+		/**
+		 * A description of this value, which could be provided to the LLM as a hint.
+		 */
+		description?: string;
+	}
+
+	/**
+	 * The detail level of this chat variable value.
+	 */
+	export enum ChatVariableLevel {
+		Short = 1,
+		Medium = 2,
+		Full = 3
+	}
 }
