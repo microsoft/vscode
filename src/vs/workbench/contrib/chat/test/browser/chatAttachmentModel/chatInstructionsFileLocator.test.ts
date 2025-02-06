c327d9161d197ab4afef3a12e06ca2280c3fa95c
Oleg Solomko <legomushroom@gmail.com>
2025-02-05 16:28:09 -0800

    add unit tests with mocked filesystem structure

--- src/vs/workbench/contrib/chat/test/browser/chatAttachmentModel/chatInstructionsFileLocator.test.ts
+++ src/vs/workbench/contrib/chat/test/browser/chatAttachmentModel/chatInstructionsFileLocator.test.ts
@@ -4,18 +4,20 @@
  *--------------------------------------------------------------------------------------------*/
 
 import assert from 'assert';
+import { URI } from '../../../../../../base/common/uri.js';
 import { Schemas } from '../../../../../../base/common/network.js';
-import { mockObject, mockService } from '../../common/promptSyntax/testUtils/mock.js';
 import { PromptFilesConfig } from '../../../common/promptSyntax/config.js';
 import { IFileService } from '../../../../../../platform/files/common/files.js';
 import { FileService } from '../../../../../../platform/files/common/fileService.js';
+import { mockObject, mockService } from '../../common/promptSyntax/testUtils/mock.js';
 import { ILogService, NullLogService } from '../../../../../../platform/log/common/log.js';
+import { IMockFolder, MockFilesystem } from '../../common/promptSyntax/testUtils/mockFilesystem.js';
 import { ensureNoDisposablesAreLeakedInTestSuite } from '../../../../../../base/test/common/utils.js';
-import { IWorkspace, IWorkspaceContextService, IWorkspaceFolder } from '../../../../../../platform/workspace/common/workspace.js';
+import { ChatInstructionsFileLocator } from '../../../browser/chatAttachmentModel/chatInstructionsFileLocator.js';
 import { InMemoryFileSystemProvider } from '../../../../../../platform/files/common/inMemoryFilesystemProvider.js';
 import { TestInstantiationService } from '../../../../../../platform/instantiation/test/common/instantiationServiceMock.js';
 import { IConfigurationOverrides, IConfigurationService } from '../../../../../../platform/configuration/common/configuration.js';
-import { ChatInstructionsFileLocator } from '../../../browser/chatAttachmentModel/chatInstructionsFileLocator.js';
+import { IWorkspace, IWorkspaceContextService, IWorkspaceFolder } from '../../../../../../platform/workspace/common/workspace.js';
 
 /**
  * Mocked mocked instance of {@link IConfigurationService}.
@@ -62,14 +64,36 @@ suite('ChatInstructionsFileLocator', () => {
 		initService.stub(IFileService, fileService);
 	});
 
+
+	// const mockToWorkspaceFolder = (folder: IMockFolder): IWorkspaceFolder => {
+	// 	return mockObject<IWorkspaceFolder>({
+	// 		uri: fileSystemProvider.root.with({ path: folder.name }),
+	// 	});
+	// };
+
 	/**
 	 * Create a new instance of {@link ChatInstructionsFileLocator} with provided mocked
 	 * values for configuration and workspace services.
 	 */
-	const createLocator = (
+	const createPromptsLocator = async (
 		configValue: unknown,
 		workspaceFolders: IWorkspaceFolder[],
-	): ChatInstructionsFileLocator => {
+		filesystem: IMockFolder[],
+	): Promise<ChatInstructionsFileLocator> => {
+		const mockFilesystem = initService.createInstance(MockFilesystem, filesystem);
+		await mockFilesystem.mock();
+		// const workspaceFolders = filesystem.map((folder) => {
+		// 	return mockObject<IWorkspaceFolder>(folder);
+		// 	// TODO: @legomushroom - remove?
+		// 	// return {
+		// 	// 	...folder,
+		// 	// 	index,
+		// 	// 	toResource(relativePath: string) {
+		// 	// 		return extUri.resolvePath(folder.uri, relativePath);
+		// 	// 	},
+		// 	// };
+		// });
+
 		initService.stub(IConfigurationService, mockConfigService(configValue));
 		initService.stub(IWorkspaceContextService, mockWorkspaceService(workspaceFolders));
 
@@ -77,27 +101,100 @@ suite('ChatInstructionsFileLocator', () => {
 	};
 
 	suite('empty workspace', () => {
-		test('no config value', async () => {
-			const locator = createLocator(undefined, []);
+		const EMPTY_WORKSPACE: IWorkspaceFolder[] = [];
 
-			assert.deepStrictEqual(
-				await locator.listFiles([]),
-				[],
-				'No prompts must be found.',
-			);
+		suite('empty filesystem', () => {
+			test('no config value', async () => {
+				const locator = await createPromptsLocator(undefined, EMPTY_WORKSPACE, []);
+
+				assert.deepStrictEqual(
+					await locator.listFiles([]),
+					[],
+					'No prompts must be found.',
+				);
+			});
+
+			test('object config value', async () => {
+				const locator = await createPromptsLocator({
+					'/Users/legomushroom/repos/prompts/': true,
+					'/tmp/prompts/': false,
+				}, EMPTY_WORKSPACE, []);
+
+				assert.deepStrictEqual(
+					await locator.listFiles([]),
+					[],
+					'No prompts must be found.',
+				);
+			});
+
+			test('array config value', async () => {
+				const locator = await createPromptsLocator([
+					'relative/path/to/prompts/',
+					'/abs/path',
+				], EMPTY_WORKSPACE, []);
+
+				assert.deepStrictEqual(
+					await locator.listFiles([]),
+					[],
+					'No prompts must be found.',
+				);
+			});
 		});
 
-		test('object config value but no referenced folder', async () => {
-			const locator = createLocator({
-				"/Users/legomushroom/repos/prompts/": true,
-				"/tmp/prompts/": false,
-			}, []);
+		suite('non-empty filesystem', () => {
+			test('object config value', async () => {
+				const locator = await createPromptsLocator(
+					{
+						'/Users/legomushroom/repos/prompts': true,
+						'/tmp/prompts/': true,
+						'/absolut/path/prompts': false,
+						'.copilot/prompts': true,
+					},
+					EMPTY_WORKSPACE,
+					[
+						{
+							name: '/Users/legomushroom/repos/prompts',
+							children: [
+								{
+									name: 'test.prompt.md',
+									contents: 'Hello, World!',
+								},
+								{
+									name: 'refactor-tests.prompt.md',
+									contents: 'some file content goes here',
+								},
+							],
+						},
+						{
+							name: '/tmp/prompts',
+							children: [
+								{
+									name: 'translate.to-rust.prompt.md',
+									contents: 'some more random file contents',
+								},
+							],
+						},
+						{
+							name: '/absolut/path/prompts',
+							children: [
+								{
+									name: 'some-prompt-file.prompt.md',
+									contents: 'hey hey hey',
+								},
+							],
+						},
+					]);
 
-			assert.deepStrictEqual(
-				await locator.listFiles([]),
-				[],
-				'No prompts must be found.',
-			);
+				assert.deepStrictEqual(
+					await locator.listFiles([]),
+					[
+						URI.file('/Users/legomushroom/repos/prompts/test.prompt.md'),
+						URI.file('/Users/legomushroom/repos/prompts/refactor-tests.prompt.md'),
+						URI.file('/tmp/prompts/translate.to-rust.prompt.md'),
+					],
+					'Must find correct prompts.',
+				);
+			});
 		});
 	});
 });
