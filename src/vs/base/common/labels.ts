a2c1495bcad2be052190892ae10f7247e3703b86
Alex Ross <alros@microsoft.com>
2025-02-10 10:30:42 +0100

    Add Folder to Workspace button displaying as "&Add" Fixes #240105

--- src/vs/base/common/labels.ts
+++ src/vs/base/common/labels.ts
@@ -434,6 +434,10 @@ export function unmnemonicLabel(label: string): string {
 	return label.replace(/&/g, '&&');
 }
 
+export function removeMnemonic(label: string): string {
+	return label.replace(/&(?!\s)/g, '');
+}
+
 /**
  * Splits a recent label in name and parent path, supporting both '/' and '\' and workspace suffixes.
  * If the location is remote, the remote name is included in the name part.
