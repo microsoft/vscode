d17994f15c0365b159a277eb084044190d2cae37
Oleg Solomko <legomushroom@gmail.com>
2025-02-10 11:17:08 -0800

    register use prompt commands for every chat context

--- src/vs/workbench/contrib/chat/browser/promptSyntax/contributions/usePromptCommand.ts
+++ src/vs/workbench/contrib/chat/browser/promptSyntax/contributions/usePromptCommand.ts
@@ -3,43 +3,44 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-// import * as nls from '../../../../../../nls.js';
+import * as nls from '../../../../../../nls.js';
 import { URI } from '../../../../../../base/common/uri.js';
-// import { CHAT_CATEGORY } from '../../actions/chatActions.js';
-// import { Schemas } from '../../../../../../base/common/network.js';
-// import { ResourceContextKey } from '../../../../../common/contextkeys.js';
+import { CHAT_CATEGORY } from '../../actions/chatActions.js';
+import { ChatAgentLocation } from '../../../common/chatAgents.js';
+import { ChatContextKeys } from '../../../common/chatContextKeys.js';
 import { KeyMod, KeyCode } from '../../../../../../base/common/keyCodes.js';
-// import { PROMPT_FILE_EXTENSION } from '../../../common/promptSyntax/constants.js';
 import { IEditorService } from '../../../../../services/editor/common/editorService.js';
 import { ICommandService } from '../../../../../../platform/commands/common/commands.js';
 import { BasePromptParser } from '../../../common/promptSyntax/parsers/basePromptParser.js';
-// import { ContextKeyExpr } from '../../../../../../platform/contextkey/common/contextkey.js';
-// import { MenuId, MenuRegistry } from '../../../../../../platform/actions/common/actions.js';
+import { ContextKeyExpr } from '../../../../../../platform/contextkey/common/contextkey.js';
+import { appendToCommandPalette } from '../../../../files/browser/fileActions.contribution.js';
 import { ServicesAccessor } from '../../../../../../platform/instantiation/common/instantiation.js';
 import { IActiveCodeEditor, isCodeEditor } from '../../../../../../editor/browser/editorBrowser.js';
 import { IChatUsePromptActionOptions, USE_PROMPT_ACTION_ID } from '../../actions/chatContextActions.js';
 import { KeybindingsRegistry, KeybindingWeight } from '../../../../../../platform/keybinding/common/keybindingsRegistry.js';
-// import { appendEditorTitleContextMenuItem, appendToCommandPalette } from '../../../../files/browser/fileActions.contribution.js';
 
 /**
  * TODO: @legomushroom
+ *  - chat panel support
  *  - chat edits support
  *  - inline edits support
- *  - file editor context menu support
- *  - file tree context support
- *  - command palette support
+ *  - terminal support
+ *  - notebooks support
+ *  - make the shortcut configurable
  *  - what to do with the "current file" context when using a current file as a prompt?
  */
 
-export const USE_PROMPT_COMMAND_ID = 'usePrompt';
-// const USE_PROMPT_LABEL = nls.localize2(USE_PROMPT_COMMAND_ID, "Use Prompt");
+export const USE_PROMPT_COMMAND_ID = 'use-prompt';
+const USE_PROMPT_LABEL = nls.localize2(USE_PROMPT_COMMAND_ID, "Use Prompt");
+
+// import { Schemas } from '../../../../../../base/common/network.js';
+// import { ResourceContextKey } from '../../../../../common/contextkeys.js';
+// import { PROMPT_FILE_EXTENSION } from '../../../common/promptSyntax/constants.js';
+// import { ContextKeyExpr } from '../../../../../../platform/contextkey/common/contextkey.js';
 // const USE_PROMPT_WHEN_CONTEXT = ContextKeyExpr.or(
 // 	ResourceContextKey.Scheme.isEqualTo(Schemas.file),
 // 	ResourceContextKey.Extension.isEqualTo(PROMPT_FILE_EXTENSION),
 // );
-
-const USE_PROMPT_IN_EDITS_COMMAND_ID = 'usePromptInEdits';
-// const USE_PROMPT_IN_EDITS_LABEL = nls.localize2(USE_PROMPT_IN_EDITS_COMMAND_ID, "Use Prompt in Edits");
 
 /**
  * Gets active editor instance, if any.
@@ -93,70 +94,132 @@ const getActivePromptUri = (
  * Implementation of the "Use Prompt" command.
  */
 const usePromptCommand = async (
-	// location: ChatAgentLocation,
 	accessor: ServicesAccessor,
 	resource?: URI,
+	location?: ChatAgentLocation,
 ): Promise<void> => {
 	const commandService = accessor.get(ICommandService);
 
 	const options: IChatUsePromptActionOptions = {
-		// location,
 		resource: getActivePromptUri(resource, accessor),
+		location,
 	};
 
 	await commandService.executeCommand(USE_PROMPT_ACTION_ID, options);
 };
 
-// const usePromptCommandFactory = (
-// 	location: ChatAgentLocation,
-// ) => {
-// 	return async (
-// 		accessor: ServicesAccessor,
-// 		// TODO: @legomushroom - uncomment for the code editor context menu actions
-// 		// resource?: URI,
-// 	): Promise<void> => {
-// 		const resource = undefined;
-// 		return await usePromptCommand(location, accessor, resource);
-// 	};
-// };
+const usePromptCommandFactory = (
+	location: ChatAgentLocation,
+) => {
+	return async (
+		accessor: ServicesAccessor,
+		resource?: URI,
+	): Promise<void> => {
+		return await usePromptCommand(accessor, resource, location);
+	};
+};
 
 // Key bindings
 
-const BASE_KEYS = KeyMod.Alt | KeyMod.Shift;
+const USE_COMMAND_KEY_BINDINGS = KeyMod.Alt | KeyMod.Shift | KeyCode.KeyW;
 KeybindingsRegistry.registerCommandAndKeybindingRule({
 	id: USE_PROMPT_COMMAND_ID,
 	weight: KeybindingWeight.WorkbenchContrib,
-	primary: BASE_KEYS | KeyCode.KeyR,
+	primary: USE_COMMAND_KEY_BINDINGS,
 	handler: usePromptCommand,
-	// when: USE_PROMPT_WHEN_CONTEXT,
+	when: ContextKeyExpr.and(
+		ChatContextKeys.location.notEqualsTo(ChatAgentLocation.EditingSession),
+		ChatContextKeys.location.notEqualsTo(ChatAgentLocation.Editor),
+		ChatContextKeys.location.notEqualsTo(ChatAgentLocation.Panel),
+		ChatContextKeys.location.notEqualsTo(ChatAgentLocation.Terminal),
+		ChatContextKeys.location.notEqualsTo(ChatAgentLocation.Notebook),
+	),
 });
 
+const USE_PROMPT_IN_EDITS_COMMAND_ID = `${USE_PROMPT_COMMAND_ID}.edits`;
 KeybindingsRegistry.registerCommandAndKeybindingRule({
 	id: USE_PROMPT_IN_EDITS_COMMAND_ID,
 	weight: KeybindingWeight.WorkbenchContrib,
-	primary: BASE_KEYS | KeyCode.KeyE,
-	handler: usePromptCommand,
-	// when: USE_PROMPT_WHEN_CONTEXT,
+	primary: USE_COMMAND_KEY_BINDINGS,
+	handler: usePromptCommandFactory(ChatAgentLocation.EditingSession),
+	when: ContextKeyExpr.and(
+		ChatContextKeys.location.isEqualTo(ChatAgentLocation.EditingSession),
+	),
 });
 
-// // Command Palette
+const USE_PROMPT_IN_PANEL_COMMAND_ID = `${USE_PROMPT_COMMAND_ID}.panel`;
+KeybindingsRegistry.registerCommandAndKeybindingRule({
+	id: USE_PROMPT_IN_PANEL_COMMAND_ID,
+	weight: KeybindingWeight.WorkbenchContrib,
+	primary: USE_COMMAND_KEY_BINDINGS,
+	handler: usePromptCommandFactory(ChatAgentLocation.Panel),
+	when: ContextKeyExpr.and(
+		ChatContextKeys.location.isEqualTo(ChatAgentLocation.Panel),
+	),
+});
 
-// appendToCommandPalette({
-// 	id: USE_PROMPT_COMMAND_ID,
-// 	title: USE_PROMPT_LABEL,
-// 	category: CHAT_CATEGORY,
-// });
+// TODO: @legomushroom - remove for now?
+const USE_PROMPT_IN_TERMINAL_COMMAND_ID = `${USE_PROMPT_COMMAND_ID}.terminal`;
+KeybindingsRegistry.registerCommandAndKeybindingRule({
+	id: USE_PROMPT_IN_TERMINAL_COMMAND_ID,
+	weight: KeybindingWeight.WorkbenchContrib,
+	primary: USE_COMMAND_KEY_BINDINGS,
+	handler: usePromptCommandFactory(ChatAgentLocation.Terminal),
+	when: ContextKeyExpr.and(
+		ChatContextKeys.location.isEqualTo(ChatAgentLocation.Terminal),
+	),
+});
 
-// appendToCommandPalette({
-// 	id: USE_PROMPT_IN_EDITS_COMMAND_ID,
-// 	title: USE_PROMPT_IN_EDITS_LABEL,
-// 	category: CHAT_CATEGORY,
-// });
+const USE_PROMPT_IN_INLINE_EDITS_COMMAND_ID = `${USE_PROMPT_COMMAND_ID}.editor`;
+KeybindingsRegistry.registerCommandAndKeybindingRule({
+	id: USE_PROMPT_IN_INLINE_EDITS_COMMAND_ID,
+	weight: KeybindingWeight.WorkbenchContrib,
+	primary: USE_COMMAND_KEY_BINDINGS,
+	handler: usePromptCommandFactory(ChatAgentLocation.Editor),
+	when: ContextKeyExpr.and(
+		ChatContextKeys.location.isEqualTo(ChatAgentLocation.Editor),
+	),
+});
 
+// TODO: @legomushroom - remove for now?
+const USE_PROMPT_IN_NOTEBOOKS_COMMAND_ID = `${USE_PROMPT_COMMAND_ID}.editor`;
+KeybindingsRegistry.registerCommandAndKeybindingRule({
+	id: USE_PROMPT_IN_NOTEBOOKS_COMMAND_ID,
+	weight: KeybindingWeight.WorkbenchContrib,
+	primary: USE_COMMAND_KEY_BINDINGS,
+	handler: usePromptCommandFactory(ChatAgentLocation.Editor),
+	when: ContextKeyExpr.and(
+		ChatContextKeys.location.isEqualTo(ChatAgentLocation.Editor),
+	),
+});
+
+// Command Palette
+
+appendToCommandPalette({
+	id: USE_PROMPT_COMMAND_ID,
+	title: USE_PROMPT_LABEL,
+	category: CHAT_CATEGORY,
+});
+
+// From an active chat input
+
+
+// ContextKeyExpr.and(ChatContextKeys.location.isEqualTo(ChatAgentLocation.Panel)),
+
+
+// TODO: @legomushroom - do we need this?
 // // File editor context menu
 
-// appendEditorTitleContextMenuItem(USE_PROMPT_COMMAND_ID, USE_PROMPT_LABEL.value, USE_PROMPT_WHEN_CONTEXT, '2_files', false, 0);
+// appendEditorTitleContextMenuItem(
+// 	USE_PROMPT_COMMAND_ID,
+// 	USE_PROMPT_LABEL.value,
+// 	USE_PROMPT_WHEN_CONTEXT,
+// 	'2_files',
+// 	false,
+// 	0,
+// );
 
+// TODO: @legomushroom - do we need this?
 // // Menu registration - explorer
 
 // MenuRegistry.appendMenuItem(MenuId.ExplorerContext, {
