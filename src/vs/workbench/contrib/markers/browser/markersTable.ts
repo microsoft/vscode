5232f0e0cd449c44a748dfa4ac9054a5193758eb
Ladislau Szomoru <3372902+lszomoru@users.noreply.github.com>
2025-02-11 09:48:42 +0100

    Problems - fix the source column so that it displays the source instead of the owner

--- src/vs/workbench/contrib/markers/browser/markersTable.ts
+++ src/vs/workbench/contrib/markers/browser/markersTable.ts
@@ -230,21 +230,21 @@ class MarkerFileColumnRenderer implements ITableRenderer<MarkerTableItem, IMarke
 	}
 }
 
-class MarkerOwnerColumnRenderer implements ITableRenderer<MarkerTableItem, IMarkerHighlightedLabelColumnTemplateData> {
+class MarkerSourceColumnRenderer implements ITableRenderer<MarkerTableItem, IMarkerHighlightedLabelColumnTemplateData> {
 
-	static readonly TEMPLATE_ID = 'owner';
+	static readonly TEMPLATE_ID = 'source';
 
-	readonly templateId: string = MarkerOwnerColumnRenderer.TEMPLATE_ID;
+	readonly templateId: string = MarkerSourceColumnRenderer.TEMPLATE_ID;
 
 	renderTemplate(container: HTMLElement): IMarkerHighlightedLabelColumnTemplateData {
-		const columnElement = DOM.append(container, $('.owner'));
+		const columnElement = DOM.append(container, $('.source'));
 		const highlightedLabel = new HighlightedLabel(columnElement);
 		return { columnElement, highlightedLabel };
 	}
 
 	renderElement(element: MarkerTableItem, index: number, templateData: IMarkerHighlightedLabelColumnTemplateData, height: number | undefined): void {
-		templateData.columnElement.title = element.marker.owner;
-		templateData.highlightedLabel.set(element.marker.owner, element.ownerMatches);
+		templateData.columnElement.title = element.marker.source ?? '';
+		templateData.highlightedLabel.set(element.marker.source ?? '', element.sourceMatches);
 	}
 
 	disposeTemplate(templateData: IMarkerHighlightedLabelColumnTemplateData): void {
@@ -321,7 +321,7 @@ export class MarkersTable extends Disposable implements IProblemsWidget {
 					weight: 1,
 					minimumWidth: 100,
 					maximumWidth: 300,
-					templateId: MarkerOwnerColumnRenderer.TEMPLATE_ID,
+					templateId: MarkerSourceColumnRenderer.TEMPLATE_ID,
 					project(row: Marker): Marker { return row; }
 				}
 			],
@@ -330,7 +330,7 @@ export class MarkersTable extends Disposable implements IProblemsWidget {
 				this.instantiationService.createInstance(MarkerCodeColumnRenderer),
 				this.instantiationService.createInstance(MarkerMessageColumnRenderer),
 				this.instantiationService.createInstance(MarkerFileColumnRenderer),
-				this.instantiationService.createInstance(MarkerOwnerColumnRenderer),
+				this.instantiationService.createInstance(MarkerSourceColumnRenderer),
 			],
 			options
 		) as WorkbenchTable<MarkerTableItem>;
@@ -454,11 +454,10 @@ export class MarkersTable extends Disposable implements IProblemsWidget {
 					const codeMatches = marker.marker.code ? FilterOptions._filter(this.filterOptions.textFilter.text, typeof marker.marker.code === 'string' ? marker.marker.code : marker.marker.code.value) ?? undefined : undefined;
 					const messageMatches = FilterOptions._messageFilter(this.filterOptions.textFilter.text, marker.marker.message) ?? undefined;
 					const fileMatches = FilterOptions._messageFilter(this.filterOptions.textFilter.text, this.labelService.getUriLabel(marker.resource, { relative: true })) ?? undefined;
-					const ownerMatches = FilterOptions._messageFilter(this.filterOptions.textFilter.text, marker.marker.owner) ?? undefined;
 
-					const matched = sourceMatches || codeMatches || messageMatches || fileMatches || ownerMatches;
+					const matched = sourceMatches || codeMatches || messageMatches || fileMatches;
 					if ((matched && !this.filterOptions.textFilter.negate) || (!matched && this.filterOptions.textFilter.negate)) {
-						items.push(new MarkerTableItem(marker, sourceMatches, codeMatches, messageMatches, fileMatches, ownerMatches));
+						items.push(new MarkerTableItem(marker, sourceMatches, codeMatches, messageMatches, fileMatches));
 					}
 
 					continue;
