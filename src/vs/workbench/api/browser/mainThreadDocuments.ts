bd7d19e21a346219c046d334fe53160ef1e33a65
Benjamin Pasero <benjamin.pasero@microsoft.com>
2025-02-13 16:28:03 +0100

    .

--- src/vs/workbench/api/browser/mainThreadDocuments.ts
+++ src/vs/workbench/api/browser/mainThreadDocuments.ts
@@ -219,15 +219,13 @@ export class MainThreadDocuments extends Disposable implements MainThreadDocumen
 
 		if (options?.encoding) {
 			const target = uri.scheme === Schemas.untitled ? this._textFileService.untitled.get(inputUri) : this._textFileService.files.get(inputUri);
-			if (target) {
-				const result = await target.setEncoding(options.encoding, EncodingMode.Encode);
-				if (!result) {
-					return false;
-				}
+			const result = await target?.setEncoding(options.encoding, EncodingMode.Encode);
+			if (!result) {
+				return false;
+			}
 
-				if (uri.scheme !== Schemas.untitled) {
-					return result; // non untitled files get saved right away
-				}
+			if (uri.scheme !== Schemas.untitled) {
+				return result; // non untitled files get saved right away
 			}
 		}
 
