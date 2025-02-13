55b5490186bdf284a562affbde9833b9e434212e
Felipe <65371336+mrfelpa@users.noreply.github.com>
2025-02-13 10:22:35 -0300

    Implementation of improvements: Security HardeningandEnhanced Randomness
    - The cryptography implemented in the original code was not fully secure, so I implemented a replacement of rand::thread_rng() with rand::rngs::OsRng, which provides a higher quality and cryptographically secure source of entropy.
    
    - I also implemented the constant_time_eq crate and used its constant_time_eq, this helps prevent attempts to reverse engineer a valid signature by timing how long the comparison takes.

--- cli/src/tunnels/challenge.rs
+++ cli/src/tunnels/challenge.rs
@@ -3,40 +3,43 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+use rand::distributions::{Alphanumeric, DistString};
+use rand::rngs::OsRng;
+
 #[cfg(not(feature = "vsda"))]
 pub fn create_challenge() -> String {
-	use rand::distributions::{Alphanumeric, DistString};
-	Alphanumeric.sample_string(&mut rand::thread_rng(), 16)
+    Alphanumeric.sample_string(&mut OsRng, 16)
 }
 
 #[cfg(not(feature = "vsda"))]
 pub fn sign_challenge(challenge: &str) -> String {
-	use base64::{engine::general_purpose as b64, Engine as _};
-	use sha2::{Digest, Sha256};
-	let mut hash = Sha256::new();
-	hash.update(challenge.as_bytes());
-	let result = hash.finalize();
-	b64::URL_SAFE_NO_PAD.encode(result)
+    use base64::{engine::general_purpose as b64, Engine as _};
+    use sha2::{Digest, Sha256};
+
+    let mut hasher = Sha256::new();
+    hasher.update(challenge.as_bytes());
+    let hash = hasher.finalize();
+
+    b64::URL_SAFE_NO_PAD.encode(hash)
 }
 
 #[cfg(not(feature = "vsda"))]
 pub fn verify_challenge(challenge: &str, response: &str) -> bool {
-	sign_challenge(challenge) == response
+    constant_time_eq::constant_time_eq(sign_challenge(challenge).as_bytes(), response.as_bytes())
 }
 
 #[cfg(feature = "vsda")]
 pub fn create_challenge() -> String {
-	use rand::distributions::{Alphanumeric, DistString};
-	let str = Alphanumeric.sample_string(&mut rand::thread_rng(), 16);
-	vsda::create_new_message(&str)
+    let str = Alphanumeric.sample_string(&mut OsRng, 16);
+    vsda::create_new_message(&str)
 }
 
 #[cfg(feature = "vsda")]
 pub fn sign_challenge(challenge: &str) -> String {
-	vsda::sign(challenge)
+    vsda::sign(challenge)
 }
 
 #[cfg(feature = "vsda")]
 pub fn verify_challenge(challenge: &str, response: &str) -> bool {
-	vsda::validate(challenge, response)
+    vsda::validate(challenge, response)
 }
