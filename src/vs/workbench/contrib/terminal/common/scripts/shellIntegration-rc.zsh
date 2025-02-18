5f2333c0eb2a67fcb17bea942644cda1debe5153
Daniel Imms <2193314+Tyriar@users.noreply.github.com>
2025-02-18 07:23:44 -0800

    Autoload is-at-least

--- src/vs/workbench/contrib/terminal/common/scripts/shellIntegration-rc.zsh
+++ src/vs/workbench/contrib/terminal/common/scripts/shellIntegration-rc.zsh
@@ -2,7 +2,7 @@
 #   Copyright (c) Microsoft Corporation. All rights reserved.
 #   Licensed under the MIT License. See License.txt in the project root for license information.
 # ---------------------------------------------------------------------------------------------
-builtin autoload -Uz add-zsh-hook
+builtin autoload -Uz add-zsh-hook is-at-least
 
 # Prevent the script recursing when setting up
 if [ -n "$VSCODE_SHELL_INTEGRATION" ]; then
