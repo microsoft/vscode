08969339a695ea21ae798c1cd302e0d24669cc9e
meganrogge <merogge@microsoft.com>
2025-02-19 11:23:42 -0600

    fix #239995

--- src/vs/editor/common/standaloneStrings.ts
+++ src/vs/editor/common/standaloneStrings.ts
@@ -13,6 +13,7 @@ export namespace AccessibilityHelpNLS {
 	export const readonlyEditor = nls.localize("readonlyEditor", "You are in a read-only code editor.");
 	export const editableEditor = nls.localize("editableEditor", "You are in a code editor.");
 	export const activeEditorState = nls.localize("activeEditorState", "Get information about the active editor such as Modified, Problems, and more by setting activeEditorState as a part of the window.title setting.");
+	export const toolbar = nls.localize("toolbar", "Around the workbench, when the screen reader announces you've landed in a toolbar, use narrow keys to navigate between the toolbar's actions.");
 	export const changeConfigToOnMac = nls.localize("changeConfigToOnMac", "Configure the application to be optimized for usage with a Screen Reader (Command+E).");
 	export const changeConfigToOnWinLinux = nls.localize("changeConfigToOnWinLinux", "Configure the application to be optimized for usage with a Screen Reader (Control+E).");
 	export const auto_on = nls.localize("auto_on", "The application is configured to be optimized for usage with a Screen Reader.");
