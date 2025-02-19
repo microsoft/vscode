000eb9327c7688f9d0a6d87d7436cb1854efe8d1
Benjamin Pasero <benjamin.pasero@microsoft.com>
2025-02-19 16:47:54 +0100

    fix encoding difference when caching

--- src/vs/workbench/api/common/extHostDocuments.ts
+++ src/vs/workbench/api/common/extHostDocuments.ts
@@ -85,17 +85,31 @@ export class ExtHostDocuments implements ExtHostDocumentsShape {
 
 		let promise = this._documentLoader.get(uri.toString());
 		if (!promise) {
-			promise = this._proxy.$tryOpenDocument(uri, options).then(uriData => {
-				this._documentLoader.delete(uri.toString());
-				const canonicalUri = URI.revive(uriData);
-				return assertIsDefined(this._documentsAndEditors.getDocument(canonicalUri));
-			}, err => {
-				this._documentLoader.delete(uri.toString());
-				return Promise.reject(err);
-			});
-			this._documentLoader.set(uri.toString(), promise);
+			promise = this.doEnsureDocumentData(uri, options);
+		} else {
+			if (options?.encoding) {
+				promise = promise.then(data => {
+					if (data.document.encoding !== options.encoding) {
+						return this.doEnsureDocumentData(uri, options);
+					}
+					return data;
+				});
+			}
 		}
 
+		return promise;
+	}
+
+	private doEnsureDocumentData(uri: URI, options?: { encoding?: string }): Promise<ExtHostDocumentData> {
+		const promise = this._proxy.$tryOpenDocument(uri, options).then(uriData => {
+			this._documentLoader.delete(uri.toString());
+			const canonicalUri = URI.revive(uriData);
+			return assertIsDefined(this._documentsAndEditors.getDocument(canonicalUri));
+		}, err => {
+			this._documentLoader.delete(uri.toString());
+			return Promise.reject(err);
+		});
+		this._documentLoader.set(uri.toString(), promise);
 		return promise;
 	}
 
