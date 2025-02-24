e9145901e35b37d41acfe539019539ac2ecb3894
meganrogge <merogge@microsoft.com>
2025-02-23 23:37:03 -0600

    remove info log

--- extensions/terminal-suggest/src/fig/execute.ts
+++ extensions/terminal-suggest/src/fig/execute.ts
@@ -21,7 +21,6 @@ export const executeCommandTimeout = async (
 	const command = [input.command, ...input.args].join(' ');
 	try {
 		console.info(`About to run shell command '${command}'`);
-		const start = performance.now();
 		const result = await withTimeout(
 			Math.max(timeout, input.timeout ?? 0),
 			spawnHelper2(input.command, input.args, {
@@ -30,11 +29,6 @@ export const executeCommandTimeout = async (
 				timeout: input.timeout,
 			})
 		);
-		const end = performance.now();
-		console.info(`Result of shell command '${command}'`, {
-			result,
-			time: end - start,
-		});
 
 		const cleanStdout = cleanOutput(result.stdout);
 		const cleanStderr = cleanOutput(result.stderr);
