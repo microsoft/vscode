d7ca4447b04e544d0e1e5768705b538b41b9f75c
Cassidy Williams <1454517+cassidoo@users.noreply.github.com>
2025-02-26 10:03:19 -0600

    Fix #241903: Add override for GitHub in settingsLayout.ts (#241911)
    Add override for GitHub in settingsLayout.ts

--- src/vs/workbench/contrib/preferences/browser/settingsLayout.ts
+++ src/vs/workbench/contrib/preferences/browser/settingsLayout.ts
@@ -319,3 +319,4 @@ knownTermMappings.set('power shell', 'PowerShell');
 knownTermMappings.set('powershell', 'PowerShell');
 knownTermMappings.set('javascript', 'JavaScript');
 knownTermMappings.set('typescript', 'TypeScript');
+knownTermMappings.set('github', 'GitHub');
