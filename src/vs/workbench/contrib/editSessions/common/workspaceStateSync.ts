2dc89114d587ef19a248fd9dd84f73d35603f41c
Sandeep Somavarapu <sasomava@microsoft.com>
2025-03-04 15:40:12 +0100

    remove method

--- src/vs/workbench/contrib/editSessions/common/workspaceStateSync.ts
+++ src/vs/workbench/contrib/editSessions/common/workspaceStateSync.ts
@@ -48,7 +48,6 @@ class NullEnablementService implements IUserDataSyncEnablementService {
 	canToggleEnablement(): boolean { return true; }
 	setEnablement(_enabled: boolean): void { }
 	isResourceEnabled(_resource: SyncResource): boolean { return true; }
-	getResourceEnablement(_resource: SyncResource): boolean | undefined { return true; }
 	setResourceEnablement(_resource: SyncResource, _enabled: boolean): void { }
 	getResourceSyncStateVersion(_resource: SyncResource): string | undefined { return undefined; }
 
