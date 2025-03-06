4215782c25727720f62abcf88a3f1d0f6309003f
Daniel Imms <2193314+Tyriar@users.noreply.github.com>
2025-03-06 04:31:53 -0800

    Fix indentation mapping

--- extensions/terminal-suggest/scripts/update-specs.js
+++ extensions/terminal-suggest/scripts/update-specs.js
@@ -16,7 +16,7 @@ const replaceStrings = [
 		'import { filepaths } from \'../../helpers/filepaths\';'
 	],
 ];
-const indentSearch = [20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1].map(e => new RegExp('^' + ' '.repeat(e * 2), 'g'));
+const indentSearch = [20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1].map(e => new RegExp('^' + ' '.repeat(e * 2), 'gm'));
 const indentReplaceValue = [20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1].map(e => '\t'.repeat(e));
 
 const specSpecificReplaceStrings = new Map([
