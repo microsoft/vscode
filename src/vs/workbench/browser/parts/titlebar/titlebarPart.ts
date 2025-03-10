70e3ed71fb750bd0a61708548aee9cd171a7e561
Benjamin Pasero <benjamin.pasero@microsoft.com>
2025-03-10 09:27:40 +0100

    .

--- src/vs/workbench/browser/parts/titlebar/titlebarPart.ts
+++ src/vs/workbench/browser/parts/titlebar/titlebarPart.ts
@@ -484,7 +484,7 @@ export class BrowserTitlebarPart extends Part implements ITitlebarPart {
 				// for something, except for web where a custom menu being supported). not putting the
 				// container helps with allowing to move the window when clicking very close to the
 				// window control buttons.
-			} else if (this.configurationService.getValue('window.titleControlsStyle') !== 'hidden') {
+			} else if (this.configurationService.getValue('window.titleControlsStyle') === 'hidden') {
 				// Linux/Windows: controls are explicitly disabled
 			} else {
 				this.windowControlsContainer = append(primaryWindowControlsLocation === 'left' ? this.leftContent : this.rightContent, $('div.window-controls-container'));
