fb57e96676e0383e44197382d4e3d10204a63d8f
Ladislau Szomoru <3372902+lszomoru@users.noreply.github.com>
2025-03-10 12:14:59 +0000

    SCM - 💄 fix property init order in `workingSet.ts` (#243061)

--- build/lib/propertyInitOrderChecker.js
+++ build/lib/propertyInitOrderChecker.js
@@ -210,7 +210,6 @@ const ignored = new Set([
     'vs/platform/quickinput/browser/commandsQuickAccess.ts',
     'vs/workbench/contrib/quickaccess/browser/commandsQuickAccess.ts',
     'vs/workbench/contrib/multiDiffEditor/browser/scmMultiDiffSourceResolver.ts',
-    'vs/workbench/contrib/scm/browser/workingSet.ts',
     'vs/workbench/contrib/scm/browser/scmHistoryViewPane.ts',
     'vs/workbench/contrib/debug/browser/debugMemory.ts',
     'vs/workbench/contrib/markers/browser/markersViewActions.ts',
