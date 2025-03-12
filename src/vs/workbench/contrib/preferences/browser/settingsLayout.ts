543dec11f031f6a1f781a61a27c91260aedce5fd
Your Name <justchen@microsoft.com>
2025-03-11 18:08:37 -0700

    remove unused setting

--- src/vs/workbench/contrib/preferences/browser/settingsLayout.ts
+++ src/vs/workbench/contrib/preferences/browser/settingsLayout.ts
@@ -243,11 +243,6 @@ export const tocData: ITOCEntry<string> = {
 					label: localize('issueReporter', 'Issue Reporter'),
 					settings: ['issueReporter.*'],
 					hide: !isWeb
-				},
-				{
-					id: 'features/feedback',
-					label: localize('feedback', 'Feedback'),
-					settings: ['feedback.*'],
 				}
 			]
 		},
