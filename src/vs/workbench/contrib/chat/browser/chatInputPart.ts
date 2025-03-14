7a063eef941a39eaa24cb21e2db998172f501148
Johannes <johannes.rieken@gmail.com>
2025-03-14 14:01:58 +0100

    polish tool picker
    * reset selected tools for new chat
    * honor selected tools on subsequent pick

--- src/vs/workbench/contrib/chat/browser/chatInputPart.ts
+++ src/vs/workbench/contrib/chat/browser/chatInputPart.ts
@@ -565,6 +565,8 @@ export class ChatInputPart extends Disposable implements IHistoryNavigationWidge
 		} else if (this.location === ChatAgentLocation.EditingSession) {
 			this.setChatMode(ChatMode.Edit);
 		}
+
+		this.selectedToolsModel.reset();
 	}
 
 	logInputHistory(): void {
