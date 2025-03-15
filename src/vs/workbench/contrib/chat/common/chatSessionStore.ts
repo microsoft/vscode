fc44c08f319f92e952da86b0452d508c3bee0cdc
Rob Lourens <roblourens@gmail.com>
2025-03-15 14:54:00 -0700

    More ChatSesssionStore work

--- src/vs/workbench/contrib/chat/common/chatSessionStore.ts
+++ src/vs/workbench/contrib/chat/common/chatSessionStore.ts
@@ -5,23 +5,26 @@
 
 import { Sequencer } from '../../../../base/common/async.js';
 import { VSBuffer } from '../../../../base/common/buffer.js';
+import { toErrorMessage } from '../../../../base/common/errorMessage.js';
 import { MarkdownString } from '../../../../base/common/htmlContent.js';
 import { revive } from '../../../../base/common/marshalling.js';
 import { joinPath } from '../../../../base/common/resources.js';
 import { URI } from '../../../../base/common/uri.js';
 import { localize } from '../../../../nls.js';
 import { IEnvironmentService } from '../../../../platform/environment/common/environment.js';
-import { FileOperationResult, IFileService, toFileOperationResult } from '../../../../platform/files/common/files.js';
+import { IFileService, toFileOperationResult } from '../../../../platform/files/common/files.js';
 import { ILogService } from '../../../../platform/log/common/log.js';
+import { IStorageService, StorageScope, StorageTarget } from '../../../../platform/storage/common/storage.js';
 import { ITelemetryService } from '../../../../platform/telemetry/common/telemetry.js';
 import { IWorkspaceContextService } from '../../../../platform/workspace/common/workspace.js';
 import { ChatModel, ISerializableChatData, ISerializableChatDataIn, ISerializableChatsData, normalizeSerializableChatData } from './chatModel.js';
 
 const maxPersistedSessions = 25;
 
+const ChatIndexStorageKey = 'ChatSessionStore.index';
+
 export class ChatSessionStore {
 	private readonly storageRoot: URI;
-	private readonly indexPath: URI;
 
 	private readonly storeQueue = new Sequencer();
 
@@ -31,6 +34,7 @@ export class ChatSessionStore {
 		@ILogService private readonly logService: ILogService,
 		@IWorkspaceContextService private readonly workspaceContextService: IWorkspaceContextService,
 		@ITelemetryService private readonly telemetryService: ITelemetryService,
+		@IStorageService private readonly storageService: IStorageService,
 	) {
 		const workspace = this.workspaceContextService.getWorkspace();
 		const isEmptyWindow = !workspace.configuration && workspace.folders.length === 0;
@@ -38,24 +42,23 @@ export class ChatSessionStore {
 			'no-workspace' :
 			this.workspaceContextService.getWorkspace().id;
 		this.storageRoot = joinPath(this.environmentService.workspaceStorageHome, workspaceId, 'chatSessions');
-		this.indexPath = joinPath(this.storageRoot, 'index.json');
 	}
 
 	async storeSessions(sessions: ChatModel[]): Promise<void> {
 		await this.storeQueue.queue(async () => {
 			try {
-				const { index } = await this.getIndex();
-				await Promise.all(sessions.map(session => this.writeSession(session, index)));
-				await this.trimEntries(index);
-				await this.flushIndex(index);
+				await Promise.all(sessions.map(session => this.writeSession(session)));
+				await this.trimEntries();
+				await this.flushIndex();
 			} catch (e) {
 				this.reportError('storeSessions', 'Error storing chat sessions', e);
 			}
 		});
 	}
 
-	private async writeSession(session: ChatModel | ISerializableChatData, index: IChatSessionIndexData): Promise<void> {
+	private async writeSession(session: ChatModel | ISerializableChatData): Promise<void> {
 		try {
+			const index = this.getIndex();
 			const storageLocation = this.getStorageLocation(session.sessionId);
 			const content = JSON.stringify(session, undefined, 2);
 			await this.fileService.writeFile(storageLocation, VSBuffer.fromString(content));
@@ -67,15 +70,24 @@ export class ChatSessionStore {
 		}
 	}
 
-	private async flushIndex(index: IChatSessionIndexData): Promise<void> {
+	private async flushIndex(): Promise<void> {
+		const index = this.getIndex();
 		try {
-			await this.fileService.writeFile(this.indexPath, VSBuffer.fromString(JSON.stringify(index, undefined, 2)));
+			this.storageService.store(ChatIndexStorageKey, index, this.getIndexStorageScope(), StorageTarget.MACHINE);
 		} catch (e) {
+			// Only if JSON.stringify fails, AFAIK
 			this.reportError('indexWrite', 'Error writing index', e);
 		}
 	}
 
-	private async trimEntries(index: IChatSessionIndexData): Promise<void> {
+	private getIndexStorageScope(): StorageScope {
+		const workspace = this.workspaceContextService.getWorkspace();
+		const isEmptyWindow = !workspace.configuration && workspace.folders.length === 0;
+		return isEmptyWindow ? StorageScope.APPLICATION : StorageScope.WORKSPACE;
+	}
+
+	private async trimEntries(): Promise<void> {
+		const index = this.getIndex();
 		const entries = Object.entries(index.entries)
 			.sort((a, b) => b[1].lastMessageDate - a[1].lastMessageDate)
 			.map(([id]) => id);
@@ -90,10 +102,28 @@ export class ChatSessionStore {
 		}
 	}
 
-	private reportError(reasonForTelemetry: string, message: string, error: Error): void {
-		this.logService.error(`ChatSessionStore: ` + message, error);
+	private async internalDeleteSession(sessionId: string): Promise<void> {
+		const index = this.getIndex();
+		const storageLocation = this.getStorageLocation(sessionId);
+		try {
+			await this.fileService.del(storageLocation);
+		} catch (e) {
+			this.reportError('sessionDelete', 'Error deleting chat session', e);
+		} finally {
+			delete index.entries[sessionId];
+		}
+	}
 
-		const fileOperationReason = toFileOperationResult(error);
+	async deleteSession(sessionId: string): Promise<void> {
+		await this.storeQueue.queue(async () => {
+			await this.internalDeleteSession(sessionId);
+		});
+	}
+
+	private reportError(reasonForTelemetry: string, message: string, error?: Error): void {
+		this.logService.error(`ChatSessionStore: ` + message, toErrorMessage(error));
+
+		const fileOperationReason = error && toFileOperationResult(error);
 		type ChatSessionStoreErrorData = {
 			reason: string;
 			fileOperationReason: number;
@@ -108,124 +138,96 @@ export class ChatSessionStore {
 		};
 		this.telemetryService.publicLog2<ChatSessionStoreErrorData, ChatSessionStoreErrorClassification>('chatSessionStoreError', {
 			reason: reasonForTelemetry,
-			fileOperationReason: fileOperationReason
+			fileOperationReason: fileOperationReason ?? -1
 		});
 	}
 
-	private indexSequencer = new Sequencer();
 	private indexCache: IChatSessionIndexData | undefined;
-	private async getIndex(): Promise<{ index: IChatSessionIndexData; isFresh: boolean }> {
+	private getIndex(): IChatSessionIndexData {
 		if (this.indexCache) {
-			return { index: this.indexCache, isFresh: false };
+			return this.indexCache;
 		}
 
-		return await this.indexSequencer.queue(async () => {
-			if (this.indexCache) {
-				return { index: this.indexCache, isFresh: false };
-			}
+		const data = this.storageService.get(ChatIndexStorageKey, this.getIndexStorageScope(), undefined);
+		if (!data) {
+			this.indexCache = { version: 1, entries: {} };
+			return this.indexCache;
+		}
 
-			try {
-				const content = await this.fileService.readFile(this.indexPath);
-				const data = JSON.parse(content.value.toString());
-				if (isChatSessionIndex(data)) {
-					this.indexCache = data;
-				} else {
-					// TODO Don't throw away index when moving backwards in VS Code version. Could rebuild it from data. But this scenario is hard anyway
-					this.reportError('invalidIndex', 'Invalid index format', data);
-					this.indexCache = { version: 1, entries: {} };
-				}
-
-				return { index: this.indexCache, isFresh: false };
-			} catch (e) {
-				const isFresh = await this.handleIndexReadError(e);
+		try {
+			const index = JSON.parse(data) as unknown;
+			if (isChatSessionIndex(index)) {
+				// Success
+				this.indexCache = index;
+			} else {
+				this.reportError('invalidIndexFormat', `Invalid index format: ${data}`);
 				this.indexCache = { version: 1, entries: {} };
-				return { index: this.indexCache, isFresh };
 			}
-		});
-	}
 
-	private async handleIndexReadError(error: Error): Promise<boolean> {
-		if (toFileOperationResult(error) === FileOperationResult.FILE_NOT_FOUND) {
-			try {
-				const stat = await this.fileService.resolve(this.storageRoot);
-				if (stat.children?.length === 0) {
-					this.logService.info(`ChatSessionStore: Initializing chat index in ${this.storageRoot.fsPath}`);
-					return true;
-				} else {
-					this.reportError('indexMissing', `Chats exist in ${this.storageRoot.fsPath} but index is missing`, error);
-				}
-			} catch (e2) {
-				if (toFileOperationResult(e2) === FileOperationResult.FILE_NOT_FOUND) {
-					this.logService.info(`ChatSessionStore: Initializing chat index in ${this.storageRoot.fsPath}`);
-					return true;
-				} else {
-					this.reportError('statStorageRoot', `Chat index missing, could not stat ${this.storageRoot.fsPath}`, e2);
-				}
-			}
-		} else {
-			this.reportError('indexRead', 'Error reading index', error);
+			return this.indexCache;
+		} catch (e) {
+			// Only if JSON.parse fails
+			this.reportError('invalidIndexJSON', `Index corrupt: ${data}`, e);
+			this.indexCache = { version: 1, entries: {} };
+			return this.indexCache;
 		}
-
-		return false;
 	}
 
 	async getSessionIndex(initialData?: ISerializableChatsData): Promise<IChatSessionIndex> {
-		const indexResult = await this.getIndex();
-
-		const needsMigrationFromStorageService = indexResult.isFresh && !Object.keys(indexResult.index.entries).length && initialData;
+		const data = this.storageService.get(ChatIndexStorageKey, this.getIndexStorageScope(), undefined);
+		const needsMigrationFromStorageService = !data && initialData;
 		if (needsMigrationFromStorageService) {
 			await this.migrate(initialData);
 		}
 
-		return indexResult.index.entries;
+		return this.getIndex().entries;
 	}
 
 	private async migrate(initialData: ISerializableChatsData): Promise<void> {
-		const index: IChatSessionIndexData = {
-			version: 1,
-			entries: {}
-		};
+		const numSessions = Object.keys(initialData).length;
+		this.logService.info(`ChatSessionStore: Migrating ${numSessions} chat sessions from storage service to file system`);
 
 		await Promise.all(Object.values(initialData).map(async session => {
-			await this.writeSession(session, index);
+			await this.writeSession(session);
 		}));
 
-		this.indexCache = index;
-		await this.flushIndex(index);
+		await this.flushIndex();
 	}
 
 	public async readSession(sessionId: string): Promise<ISerializableChatData | undefined> {
-		let rawData: string;
-		const storageLocation = this.getStorageLocation(sessionId);
-		try {
-			rawData = (await this.fileService.readFile(storageLocation)).value.toString();
-		} catch (e) {
-			this.reportError('sessionReadFile', `Error reading chat session file ${sessionId}`, e);
-			return undefined;
-		}
-
-		try {
-			// Copied from ChatService.ts, cleanup
-			const session: ISerializableChatDataIn = revive(JSON.parse(rawData)); // Revive serialized URIs in session data
-			// Revive serialized markdown strings in response data
-			for (const request of session.requests) {
-				if (Array.isArray(request.response)) {
-					request.response = request.response.map((response) => {
-						if (typeof response === 'string') {
-							return new MarkdownString(response);
-						}
-						return response;
-					});
-				} else if (typeof request.response === 'string') {
-					request.response = [new MarkdownString(request.response)];
-				}
+		return await this.storeQueue.queue(async () => {
+			let rawData: string;
+			const storageLocation = this.getStorageLocation(sessionId);
+			try {
+				rawData = (await this.fileService.readFile(storageLocation)).value.toString();
+			} catch (e) {
+				this.reportError('sessionReadFile', `Error reading chat session file ${sessionId}`, e);
+				return undefined;
 			}
 
-			return normalizeSerializableChatData(session);
-		} catch (err) {
-			this.reportError('malformedSession', `Malformed session data in ${storageLocation.fsPath}: [${rawData.substring(0, 20)}${rawData.length > 20 ? '...' : ''}]`, err);
-			return undefined;
-		}
+			try {
+				// TODO Copied from ChatService.ts, cleanup
+				const session: ISerializableChatDataIn = revive(JSON.parse(rawData)); // Revive serialized URIs in session data
+				// Revive serialized markdown strings in response data
+				for (const request of session.requests) {
+					if (Array.isArray(request.response)) {
+						request.response = request.response.map((response) => {
+							if (typeof response === 'string') {
+								return new MarkdownString(response);
+							}
+							return response;
+						});
+					} else if (typeof request.response === 'string') {
+						request.response = [new MarkdownString(request.response)];
+					}
+				}
+
+				return normalizeSerializableChatData(session);
+			} catch (err) {
+				this.reportError('malformedSession', `Malformed session data in ${storageLocation.fsPath}: [${rawData.substring(0, 20)}${rawData.length > 20 ? '...' : ''}]`, err);
+				return undefined;
+			}
+		});
 	}
 
 	private getStorageLocation(chatSessionId: string): URI {
@@ -256,6 +258,8 @@ interface IChatSessionIndexData {
 	entries: IChatSessionIndex;
 }
 
+// TODO if we update the index version:
+// Don't throw away index when moving backwards in VS Code version. Try to recover it. But this scenario is hard.
 function isChatSessionIndex(data: unknown): data is IChatSessionIndexData {
 	if (typeof data !== 'object' || data === null) {
 		return false;
@@ -281,7 +285,7 @@ function isChatSessionIndex(data: unknown): data is IChatSessionIndexData {
 
 function getSessionMetadata(session: ChatModel | ISerializableChatData): IChatSessionEntryMetadata {
 	const title = session instanceof ChatModel ?
-		(session.title || localize('newChat', "New Chat")) :
+		(session.title ?? localize('newChat', "New Chat")) :
 		session.customTitle ?? ChatModel.getDefaultTitle(session.requests);
 	return {
 		sessionId: session.sessionId,
