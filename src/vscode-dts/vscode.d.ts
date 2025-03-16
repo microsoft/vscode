46c8c4fc15cef15340e978a6a37d029a55aaa49e
Megan Rogge <merogge@microsoft.com>
2025-03-15 22:11:04 -0400

    Update vscode.d.ts

--- src/vscode-dts/vscode.d.ts
+++ src/vscode-dts/vscode.d.ts
@@ -9221,7 +9221,7 @@ declare module 'vscode' {
 	export interface TaskProcessEndEvent {
 
 		/**
-		 * The task execution for which the process ended.
+		 * The task execution for which the process got started.
 		 */
 		readonly execution: TaskExecution;
 
