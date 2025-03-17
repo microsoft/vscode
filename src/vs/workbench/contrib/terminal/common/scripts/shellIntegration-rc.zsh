17ddb3755e949b8dfd0f8693792078fb5284e551
Anthony Kim <anthonykim@microsoft.com>
2025-03-17 12:27:31 -0700

    Attempt to properly escape:
    Co-authored-by: Daniel Imms <Tyriar@users.noreply.github.com>

--- src/vs/workbench/contrib/terminal/common/scripts/shellIntegration-rc.zsh
+++ src/vs/workbench/contrib/terminal/common/scripts/shellIntegration-rc.zsh
@@ -86,8 +86,18 @@ __vsc_escape_value() {
 		byte="${str:$i:1}"
 
 		# Escape backslashes, semi-colons and newlines
+
+		# REPRO: echo "$(echo -e "$(__vsc_escape_value "ESC\033 BEL\007")")"
+		# Encode them properly
+		# print byte value --> Shows We cant just be looking at one byte to capture things like \a \033, etc..
+		printf 'my byte value is %s \n' "$byte"
+
 		if [ "$byte" = "\\" ]; then
 			token="\\\\"
+		elif [ "$byte" = $'\e' ]; then
+			token="\\x1b"
+		elif [ "$byte" = $'\a' ]; then
+			token="\\x07"
 		elif [ "$byte" = ";" ]; then
 			token="\\x3b"
 		elif [ "$byte" = $'\n' ]; then
@@ -212,7 +222,11 @@ __vsc_update_env() {
 				# Associative array is empty, do not diff, just add
 				while IFS='=' read -r key value; do
 					vsc_aa_env["$key"]="$value"
+					# print key value pair
+					# builtin printf ' Iterating through single key, value: \n'
+					# builtin printf 'my key is %s and my escaped value is %s \n' "$key" "$(__vsc_escape_value "$value")"
 					builtin printf '\e]633;EnvSingleEntry;%s;%s;%s\a' "$key" "$(__vsc_escape_value "$value")" "$__vsc_nonce"
+					# builtin printf ' Done iterating through single key, value: \n\n '
 				done < <(env)
 			else
 				# Diff approach for associative array
@@ -229,7 +243,12 @@ __vsc_update_env() {
 				while IFS='=' read -r key value; do
 					__vsc_env_keys+=("$key")
 					__vsc_env_values+=("$value")
-					builtin printf '\e]633;EnvSingleEntry;%s;%s;%s\a' "$key" "$(__vsc_escape_value "$value")" "$__vsc_nonce"
+					if [[ "$key" == "PS1" ]]; then
+						builtin printf '\e]633;EnvSingleEntry;%s;%s;%s\a' "$key" "$value" "$__vsc_nonce"
+					else
+						builtin printf '\e]633;EnvSingleEntry;%s;%s;%s\a' "$key" "$(__vsc_escape_value "$value")" "$__vsc_nonce"
+					fi
+
 				done < <(env)
 			else
 				# Diff approach for non-associative arrays
@@ -284,6 +303,12 @@ __vsc_update_prompt() {
 	__vsc_prior_prompt="$PS1"
 	__vsc_prior_prompt2="$PS2"
 	__vsc_in_command_execution=""
+	# builtin print value of __vsc_prompt_start, ps1, __vsc_prompt_end
+	# builtin printf 'printing value of __vsc_prompt_start, ps1, __vsc_prompt_end'
+	# builtin printf 'my __vsc_prompt_start is %s \n' "$__vsc_prompt_start"
+	# builtin printf 'my PS1 is %s \n' "$PS1"
+	# builtin printf 'my __vsc_prompt_end %s \n' "$__vsc_prompt_end"
+
 	PS1="%{$(__vsc_prompt_start)%}$PS1%{$(__vsc_prompt_end)%}"
 	PS2="%{$(__vsc_continuation_start)%}$PS2%{$(__vsc_continuation_end)%}"
 	if [ -n "$RPROMPT" ]; then
