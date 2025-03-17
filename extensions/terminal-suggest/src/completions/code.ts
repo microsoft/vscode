8c7d93c3b992e58146738c7f2a49953cec91d526
meganrogge <merogge@microsoft.com>
2025-03-17 15:30:15 -0500

    fix merge conflict issue

--- extensions/terminal-suggest/src/completions/code.ts
+++ extensions/terminal-suggest/src/completions/code.ts
@@ -394,76 +394,6 @@ export const troubleshootingOptions = (cliName: string): Fig.Option[] => [
 	},
 ];
 
-
-export const commonCLIOptions: Fig.Option[] = [
-	{
-		name: '--cli-data-dir',
-		description: 'Directory where CLI metadata should be stored',
-		isRepeatable: true,
-		args: {
-			name: 'cli_data_dir',
-			isOptional: true,
-		},
-	},
-	{
-		name: '--log-to-file',
-		description: 'Log to a file in addition to stdout. Used when running as a service',
-		hidden: true,
-		isRepeatable: true,
-		args: {
-			name: 'log_to_file',
-			isOptional: true,
-			template: 'filepaths',
-		},
-	},
-	{
-		name: '--log',
-		description: 'Log level to use',
-		isRepeatable: true,
-		args: {
-			name: 'log',
-			isOptional: true,
-			suggestions: [
-				'trace',
-				'debug',
-				'info',
-				'warn',
-				'error',
-				'critical',
-				'off',
-			],
-		},
-	},
-	{
-		name: '--telemetry-level',
-		description: 'Sets the initial telemetry level',
-		hidden: true,
-		isRepeatable: true,
-		args: {
-			name: 'telemetry_level',
-			isOptional: true,
-			suggestions: [
-				'off',
-				'crash',
-				'error',
-				'all',
-			],
-		},
-	},
-	{
-		name: '--verbose',
-		description: 'Print verbose output (implies --wait)',
-	},
-	{
-		name: '--disable-telemetry',
-		description: 'Disable telemetry for the current command, even if it was previously accepted as part of the license prompt or specified in \'--telemetry-level\'',
-	},
-	{
-		name: ['-h', '--help'],
-		description: 'Print help',
-	},
-];
-
 export function createCodeGenerators(cliName: string): Fig.Generator {
 	return {
 		script: [cliName, '--list-extensions', '--show-versions'],
@@ -499,7 +429,6 @@ const codeCompletionSpec: Fig.Spec = {
 		...extensionManagementOptions('code'),
 		...troubleshootingOptions('code'),
 	],
-	subcommands: codeSubcommands,
 };
 
 export default codeCompletionSpec;
