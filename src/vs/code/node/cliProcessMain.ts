bd077e517b99f927d4ad60d1f47ff485d986f0ed
Josh Spicer <23246594+joshspicer@users.noreply.github.com>
2025-03-17 12:54:43 -0700

    Add registering macOS NativePolicyServer in cliProcessMain (#243799)
    Add registering macOS NativePolicyServer in cliProcessMain (https://github.com/microsoft/vscode/issues/148942)

--- src/vs/code/node/cliProcessMain.ts
+++ src/vs/code/node/cliProcessMain.ts
@@ -11,7 +11,7 @@ import { isSigPipeError, onUnexpectedError, setUnexpectedErrorHandler } from '..
 import { Disposable } from '../../base/common/lifecycle.js';
 import { Schemas } from '../../base/common/network.js';
 import { isAbsolute, join } from '../../base/common/path.js';
-import { isWindows } from '../../base/common/platform.js';
+import { isWindows, isMacintosh } from '../../base/common/platform.js';
 import { cwd } from '../../base/common/process.js';
 import { URI } from '../../base/common/uri.js';
 import { IConfigurationService } from '../../platform/configuration/common/configuration.js';
@@ -165,9 +165,16 @@ class CliMain extends Disposable {
 		fileService.registerProvider(Schemas.vscodeUserData, new FileUserDataProvider(Schemas.file, diskFileSystemProvider, Schemas.vscodeUserData, userDataProfilesService, uriIdentityService, logService));
 
 		// Policy
-		const policyService = isWindows && productService.win32RegValueName ? this._register(new NativePolicyService(logService, productService.win32RegValueName))
-			: environmentService.policyFile ? this._register(new FilePolicyService(environmentService.policyFile, fileService, logService))
-				: new NullPolicyService();
+		let policyService: IPolicyService | undefined;
+		if (isWindows && productService.win32RegValueName) {
+			policyService = this._register(new NativePolicyService(logService, productService.win32RegValueName));
+		} else if (isMacintosh && productService.darwinBundleIdentifier) {
+			policyService = this._register(new NativePolicyService(logService, productService.darwinBundleIdentifier));
+		} else if (environmentService.policyFile) {
+			policyService = this._register(new FilePolicyService(environmentService.policyFile, fileService, logService));
+		} else {
+			policyService = new NullPolicyService();
+		}
 		services.set(IPolicyService, policyService);
 
 		// Configuration
