1716e1f32ab03970bb77d685a644af6a318ec2e8
Adil BENAMEUR <adil.benameur@sncf.fr>
2025-03-19 22:21:31 +0100

    git: fix message box on git commit errors is too short

--- extensions/git/src/commands.ts
+++ extensions/git/src/commands.ts
@@ -4744,7 +4744,7 @@ export class CommandCenter {
 							.replace(/^> husky.*$/mi, '')
 							.split(/[\r\n]/)
 							.filter((line: string) => !!line)
-						[0];
+							.join("\n");
 
 						message = hint
 							? l10n.t('Git: {0}', hint)
