e7a67da465d142c8b2e2b52d6eca8426b3fd523e
Benjamin Pasero <benjamin.pasero@microsoft.com>
2025-03-23 20:28:05 +0100

    allow to sign in

--- src/vs/workbench/contrib/chat/browser/actions/chatActions.ts
+++ src/vs/workbench/contrib/chat/browser/actions/chatActions.ts
@@ -750,6 +750,8 @@ export class CopilotTitleBarMenuRendering extends Disposable implements IWorkben
 				primaryActionId = CHAT_SETUP_ACTION_ID;
 				primaryActionTitle = localize('triggerChatSetup', "Use AI Features with Copilot for free...");
 			} else if (chatExtensionInstalled && signedOut) {
+				primaryActionId = setupFromDialog ? CHAT_SETUP_ACTION_ID : TOGGLE_CHAT_ACTION_ID;
+				primaryActionTitle = localize('signInToChatSetup', "Sign in to use Copilot...");
 				primaryActionIcon = Codicon.copilotNotConnected;
 			} else if (chatExtensionInstalled && (chatQuotaExceeded || completionsQuotaExceeded)) {
 				primaryActionId = OPEN_CHAT_QUOTA_EXCEEDED_DIALOG;
