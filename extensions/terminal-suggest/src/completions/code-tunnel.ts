00676f834e1fb63ebb4c1ee3f6bcb310961fbace
meganrogge <merogge@microsoft.com>
2025-03-26 12:40:37 -0500

    rm unused

--- extensions/terminal-suggest/src/completions/code-tunnel.ts
+++ extensions/terminal-suggest/src/completions/code-tunnel.ts
@@ -2,7 +2,7 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-import code, { codeTunnelSubcommands, commonOptions, extensionManagementOptions, troubleshootingOptions, globalTunnelOptions, codeTunnelOptions, commonCLIOptions, tunnelHelpOptions } from './code';
+import code, { codeTunnelSubcommands, commonOptions, extensionManagementOptions, troubleshootingOptions, globalTunnelOptions, codeTunnelOptions, tunnelHelpOptions } from './code';
 
 export const extTunnelSubcommand = {
 	name: 'ext',
