105a5e2c21f94db591cd33ce70087503ff89ec7d
Benjamin Pasero <benjamin.pasero@microsoft.com>
2025-03-28 12:15:58 +0100

    Welcome view flicker when loading chat in agent mode (#244936) (#244962)

--- src/vs/workbench/contrib/chat/browser/chatSetup.ts
+++ src/vs/workbench/contrib/chat/browser/chatSetup.ts
@@ -109,12 +109,13 @@ class SetupChatAgentImplementation extends Disposable implements IChatAgentImple
 			let id: string;
 			let description = localize('chatDescription', "Ask Copilot");
 			let welcomeMessageContent: IChatWelcomeMessageContent | undefined;
+			const baseMessage = localize('chatMessage', "Copilot is powered by AI, so mistakes are possible. Review output carefully before use.");
 			switch (location) {
 				case ChatAgentLocation.Panel:
 					id = 'setup.chat';
 					welcomeMessageContent = {
 						title: description,
-						message: new MarkdownString(localize('chatMessage', "Copilot is powered by AI, so mistakes are possible. Review output carefully before use.")),
+						message: new MarkdownString(baseMessage),
 						icon: Codicon.copilotLarge
 					};
 					break;
@@ -124,12 +125,12 @@ class SetupChatAgentImplementation extends Disposable implements IChatAgentImple
 					welcomeMessageContent = isToolsAgent ?
 						{
 							title: localize('editsTitle', "Edit with Copilot"),
-							message: new MarkdownString(localize('agentMessage', "Ask Copilot to edit your files in agent mode. Copilot will automatically use multiple requests to pick files to edit, run terminal commands, and iterate on errors.")),
+							message: new MarkdownString(localize('agentMessage', "Ask Copilot to edit your files in [agent mode]({0}). Copilot will automatically use multiple requests to pick files to edit, run terminal commands, and iterate on errors.", 'https://aka.ms/vscode-copilot-agent') + `\n\n${baseMessage}`),
 							icon: Codicon.copilotLarge
 						} :
 						{
 							title: localize('editsTitle', "Edit with Copilot"),
-							message: new MarkdownString(localize('editsMessage', "Start your editing session by defining a set of files that you want to work with. Then ask Copilot for the changes you want to make.")),
+							message: new MarkdownString(localize('editsMessage', "Start your editing session by defining a set of files that you want to work with. Then ask Copilot for the changes you want to make.") + `\n\n${baseMessage}`),
 							icon: Codicon.copilotLarge
 						};
 					break;
