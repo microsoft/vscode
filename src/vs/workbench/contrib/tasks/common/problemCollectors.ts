d84890c95ab7cc483c27890440d899862bf6d9d5
meganrogge <merogge@microsoft.com>
2025-03-28 15:34:39 -0500

    fix #244625

--- src/vs/workbench/contrib/tasks/common/problemCollectors.ts
+++ src/vs/workbench/contrib/tasks/common/problemCollectors.ts
@@ -362,8 +362,6 @@ export class StartStopProblemCollector extends AbstractProblemCollector implemen
 	private currentOwner: string | undefined;
 	private currentResource: string | undefined;
 
-	private _hasStarted: boolean = false;
-
 	constructor(problemMatchers: ProblemMatcher[], markerService: IMarkerService, modelService: IModelService, _strategy: ProblemHandlingStrategy = ProblemHandlingStrategy.Clean, fileService?: IFileService) {
 		super(problemMatchers, markerService, modelService, fileService);
 		const ownerSet: { [key: string]: boolean } = Object.create(null);
@@ -372,13 +370,10 @@ export class StartStopProblemCollector extends AbstractProblemCollector implemen
 		this.owners.forEach((owner) => {
 			this.recordResourcesToClean(owner);
 		});
+		this._onDidStateChange.fire(IProblemCollectorEvent.create(ProblemCollectorEventKind.BackgroundProcessingBegins));
 	}
 
 	protected async processLineInternal(line: string): Promise<void> {
-		if (!this._hasStarted) {
-			this._hasStarted = true;
-			this._onDidStateChange.fire(IProblemCollectorEvent.create(ProblemCollectorEventKind.BackgroundProcessingBegins));
-		}
 		const markerMatch = this.tryFindMarker(line);
 		if (!markerMatch) {
 			return;
