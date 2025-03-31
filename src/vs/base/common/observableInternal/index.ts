0baa64b2317991f43e8a2b7743698ec0efe3db41
Henning Dieterichs <hdieterichs@microsoft.com>
2025-03-31 19:08:47 +0200

    Fixes CI

--- src/vs/base/common/observableInternal/index.ts
+++ src/vs/base/common/observableInternal/index.ts
@@ -6,7 +6,7 @@
 // This is a facade for the observable implementation. Only import from here!
 
 export { observableValueOpts } from './api.js';
-export { autorun, autorunDelta, autorunHandleChanges, autorunOpts, autorunWithStore, autorunWithStoreHandleChanges } from './autorun.js';
+export { autorun, autorunDelta, autorunHandleChanges, autorunOpts, autorunWithStore, autorunWithStoreHandleChanges, autorunIterableDelta } from './autorun.js';
 export { asyncTransaction, disposableObservableValue, globalTransaction, observableValue, subtransaction, transaction, TransactionImpl, type IChangeContext, type IChangeTracker, type IObservable, type IObservableWithChange, type IObserver, type IReader, type ISettable, type ISettableObservable, type ITransaction, } from './base.js';
 export { derived, derivedDisposable, derivedHandleChanges, derivedOpts, derivedWithSetter, derivedWithStore } from './derived.js';
 export { ObservableLazy, ObservableLazyPromise, ObservablePromise, PromiseResult, } from './promise.js';
