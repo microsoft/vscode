a0696498424c9a58a8ad6933e482e4ed7c0497b7
Oleg Solomko <legomushroom@gmail.com>
2025-03-31 07:40:09 -0700

    implement front matter parser

---
+++ src/vs/editor/common/codecs/markdownExtensionsCodec/tokens/markdownExtensionsToken.ts
@@ -0,0 +1,11 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { MarkdownToken } from '../../markdownCodec/tokens/markdownToken.js';
+
+/**
+ * TODO: @legomushroom
+ */
+export abstract class MarkdownExtensionsToken extends MarkdownToken { }
