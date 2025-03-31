209cf9ef2de132e129bf6554278a0b26c371160b
Connor Peet <connor@peet.io>
2025-03-31 15:11:22 -0700

    testing/debug: cleanup property initialisation order changes
    Refs #243049

--- src/vs/workbench/contrib/chat/browser/chatEditing/chatEditingSession.ts
+++ src/vs/workbench/contrib/chat/browser/chatEditing/chatEditingSession.ts
@@ -198,6 +198,7 @@ export class ChatEditingSession extends Disposable implements IChatEditingSessio
 		@IAccessibilitySignalService private readonly _accessibilitySignalService: IAccessibilitySignalService,
 	) {
 		super();
+		this._ignoreTrimWhitespaceObservable = observableConfigValue('diffEditor.ignoreTrimWhitespace', true, this._configurationService);
 	}
 
 	public async init(): Promise<void> {
@@ -271,7 +272,7 @@ export class ChatEditingSession extends Disposable implements IChatEditingSessio
 
 	private _diffsBetweenStops = new Map<string, IObservable<IEditSessionEntryDiff | undefined>>();
 
-	private readonly _ignoreTrimWhitespaceObservable = observableConfigValue('diffEditor.ignoreTrimWhitespace', true, this._configurationService);
+	private readonly _ignoreTrimWhitespaceObservable: IObservable<boolean>;
 
 	/**
 	 * Gets diff for text entries between stops.
