eac62a4ca423eaa27ed1f974a03d497de4b2e690
Johannes <johannes.rieken@gmail.com>
2025-04-01 10:24:45 +0200

    make layers check happy

--- src/vs/workbench/api/node/extHostExtensionService.ts
+++ src/vs/workbench/api/node/extHostExtensionService.ts
@@ -141,7 +141,7 @@ class NodeModuleESMInterceptor extends RequireInterceptor {
 
 		let apiModuleFactory: INodeModuleFactory | undefined;
 
-		port1.onmessage = (e) => {
+		port1.onmessage = (e: { data: Message }) => {
 
 			// Get the vscode-module factory - which is the same logic that's also used by
 			// the CommonJS require interceptor
