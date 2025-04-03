4ead0ca0d066dbe9d382a8ee5a2960a9cb65850d
Alex Ross <38270282+alexr00@users.noreply.github.com>
2025-04-03 18:08:55 +0200

    Feedback: universal selectors not rendering with syntax highlighting Fixes #244574

--- src/vs/editor/common/languages/highlights/css.scm
+++ src/vs/editor/common/languages/highlights/css.scm
@@ -13,6 +13,8 @@
   "*="
 ] @keyword.operator.css
 
+("+") @keyword.operator.combinator.css
+
 (comment) @comment.block.css
 
 ; Selectors
@@ -22,6 +24,8 @@
 (class_selector) @entity.other.attribute-name.class.css
 
 (tag_name) @entity.name.tag.css
+
+(universal_selector) @entity.name.tag.wildcard.css
 
 (pseudo_class_selector) @entity.other.attribute-name.pseudo-class.css
 
