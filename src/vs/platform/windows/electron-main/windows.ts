6de978ad2a91db7e9fd018f77147d8be4ed32ffd
Aiday Marlen Kyzy <amarlenkyzy@microsoft.com>
2025-04-04 14:27:47 +0200

    undoing change

--- src/vs/platform/windows/electron-main/windows.ts
+++ src/vs/platform/windows/electron-main/windows.ts
@@ -151,7 +151,10 @@ export function defaultBrowserWindowOptions(accessor: ServicesAccessor, windowSt
 			// Enable experimental css highlight api https://chromestatus.com/feature/5436441440026624
 			// Refs https://github.com/microsoft/vscode/issues/140098
 			enableBlinkFeatures: 'HighlightAPI',
-			sandbox: true
+			sandbox: true,
+			// TODO(deepak1556): Should be removed once migration is complete
+			// https://github.com/microsoft/vscode/issues/239228
+			enableDeprecatedPaste: true,
 		},
 		experimentalDarkMode: true
 	};
