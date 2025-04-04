61c6b4b98dfb5535af48f423c35f70f5bcf809e9
Ladislau Szomoru <3372902+lszomoru@users.noreply.github.com>
2025-04-04 15:10:43 +0200

    Add remaining steps

--- build/azure-pipelines/win32/codesign.js
+++ build/azure-pipelines/win32/codesign.js
@@ -7,16 +7,89 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const zx_1 = require("zx");
 async function main() {
     (0, zx_1.usePwsh)();
+    console.log(`
+###################################################################
+#                                                                 #
+#                         CODE SIGNING                            #
+#                                                                 #
+###################################################################
+`);
+    const arch = process.env['VSCODE_ARCH'];
     const esrpCliDLLPath = process.env['EsrpCliDllPath'];
     const codesigningFolderPath = process.env['CodeSigningFolderPath'];
-    // Codesign executables and shared libraries
+    console.log(`
+###################################################################
+#                                                                 #
+#          Codesign executables and shared libraries              #
+#                                                                 #
+###################################################################
+`);
     await (0, zx_1.$) `node build/azure-pipelines/common/sign ${esrpCliDLLPath} sign-windows ${codesigningFolderPath} '*.dll,*.exe,*.node'`.pipe(process.stdout);
-    // Codesign Powershell scripts
-    await (0, zx_1.$) `node build/azure-pipelines/common/sign ${esrpCliDLLPath} sign-windows-powershell-scripts ${codesigningFolderPath} '*.ps1'`.pipe(process.stdout);
-    // Codesign context menu appx package
-    if (process.env['VSCODE_QUALITY'] !== 'oss') {
+    console.log(`
+###################################################################
+#                                                                 #
+#                 Codesign Powershell scripts                     #
+#                                                                 #
+###################################################################
+`);
+    await (0, zx_1.$) `node build/azure-pipelines/common/sign ${esrpCliDLLPath} sign-windows-appx ${codesigningFolderPath} '*.ps1'`.pipe(process.stdout);
+    if (process.env['VSCODE_QUALITY'] !== 'insider') {
+        console.log(`
+###################################################################
+#                                                                 #
+#              Codesign context menu appx package                 #
+#                                                                 #
+###################################################################
+`);
         await (0, zx_1.$) `node build/azure-pipelines/common/sign ${esrpCliDLLPath} sign-windows-appx ${codesigningFolderPath} '*.appx'`.pipe(process.stdout);
     }
+    const packageJson = await (0, zx_1.$) `Get-Content -Raw -Path ..\VSCode-win32-$(VSCODE_ARCH)\resources\app\package.json`.json();
+    const version = packageJson.version;
+    // Client
+    console.log(`
+###################################################################
+#                                                                 #
+#                       Package client                            #
+#                                                                 #
+###################################################################
+`);
+    const clientArchivePath = `.build/win32-${arch}/VSCode-win32-${arch}-${version}.zip`;
+    await (0, zx_1.$) `New-Item -ItemType Directory -Path .build/win32-${arch} -Force`;
+    await (0, zx_1.$) `7z.exe a -tzip ${clientArchivePath} ..\VSCode-win32-${arch}\* "-xr!CodeSignSummary*.md"`.pipe(process.stdout);
+    await (0, zx_1.$) `7z.exe l ${clientArchivePath}`.pipe(process.stdout);
+    // Server
+    console.log(`
+###################################################################
+#                                                                 #
+#                       Package server                            #
+#                                                                 #
+###################################################################
+`);
+    const serverArchivePath = `.build/win32-${arch}/vscode-server-win32-${arch}.zip`;
+    await (0, zx_1.$) `New-Item -ItemType Directory -Path .build/win32-${arch} -Force`;
+    await (0, zx_1.$) `7z.exe a -tzip ${serverArchivePath} ..\vscode-server-win32-${arch}`.pipe(process.stdout);
+    await (0, zx_1.$) `7z.exe l ${serverArchivePath}`.pipe(process.stdout);
+    // Web
+    console.log(`
+###################################################################
+#                                                                 #
+#                     Package server (web)                        #
+#                                                                 #
+###################################################################
+`);
+    const webArchivePath = `.build/win32-${arch}/vscode-server-win32-${arch}-web.zip`;
+    await (0, zx_1.$) `New-Item -ItemType Directory -Path .build/win32-${arch} -Force`;
+    await (0, zx_1.$) `7z.exe a -tzip ${webArchivePath} ..\vscode-server-win32-${arch}-web`.pipe(process.stdout);
+    await (0, zx_1.$) `7z.exe l ${webArchivePath}`.pipe(process.stdout);
+    // Sign setup
+    console.log(`
+###################################################################
+#                                                                 #
+#             Codesign setup packages (system, user)              #
+#                                                                 #
+###################################################################
+`);
+    await (0, zx_1.$) `npm exec -- npm-run-all -lp "gulp vscode-win32-${arch}-system-setup -- --sign" "gulp vscode-win32-${arch}-user-setup -- --sign"`.pipe(process.stdout);
 }
 main();
 //# sourceMappingURL=codesign.js.map
--- build/azure-pipelines/win32/codesign.ts
+++ build/azure-pipelines/win32/codesign.ts
@@ -8,19 +8,101 @@ import { $, usePwsh } from 'zx';
 async function main() {
 	usePwsh();
 
+	console.log(`
+###################################################################
+#                                                                 #
+#                         CODE SIGNING                            #
+#                                                                 #
+###################################################################
+`);
+
+	const arch = process.env['VSCODE_ARCH'];
 	const esrpCliDLLPath = process.env['EsrpCliDllPath'];
 	const codesigningFolderPath = process.env['CodeSigningFolderPath'];
 
-	// Codesign executables and shared libraries
+	console.log(`
+###################################################################
+#                                                                 #
+#          Codesign executables and shared libraries              #
+#                                                                 #
+###################################################################
+`);
 	await $`node build/azure-pipelines/common/sign ${esrpCliDLLPath} sign-windows ${codesigningFolderPath} '*.dll,*.exe,*.node'`.pipe(process.stdout);
 
-	// Codesign Powershell scripts
-	await $`node build/azure-pipelines/common/sign ${esrpCliDLLPath} sign-windows-powershell-scripts ${codesigningFolderPath} '*.ps1'`.pipe(process.stdout);
+	console.log(`
+###################################################################
+#                                                                 #
+#                 Codesign Powershell scripts                     #
+#                                                                 #
+###################################################################
+`);
+	await $`node build/azure-pipelines/common/sign ${esrpCliDLLPath} sign-windows-appx ${codesigningFolderPath} '*.ps1'`.pipe(process.stdout);
 
-	// Codesign context menu appx package
-	if (process.env['VSCODE_QUALITY'] !== 'oss') {
+	if (process.env['VSCODE_QUALITY'] !== 'insider') {
+		console.log(`
+###################################################################
+#                                                                 #
+#              Codesign context menu appx package                 #
+#                                                                 #
+###################################################################
+`);
 		await $`node build/azure-pipelines/common/sign ${esrpCliDLLPath} sign-windows-appx ${codesigningFolderPath} '*.appx'`.pipe(process.stdout);
 	}
+
+	const packageJson = await $`Get-Content -Raw -Path ..\VSCode-win32-$(VSCODE_ARCH)\resources\app\package.json`.json();
+	const version = packageJson.version;
+
+	// Client
+	console.log(`
+###################################################################
+#                                                                 #
+#                       Package client                            #
+#                                                                 #
+###################################################################
+`);
+	const clientArchivePath = `.build/win32-${arch}/VSCode-win32-${arch}-${version}.zip`;
+
+	await $`New-Item -ItemType Directory -Path .build/win32-${arch} -Force`;
+	await $`7z.exe a -tzip ${clientArchivePath} ..\VSCode-win32-${arch}\* "-xr!CodeSignSummary*.md"`.pipe(process.stdout);
+	await $`7z.exe l ${clientArchivePath}`.pipe(process.stdout);
+
+	// Server
+	console.log(`
+###################################################################
+#                                                                 #
+#                       Package server                            #
+#                                                                 #
+###################################################################
+`);
+
+	const serverArchivePath = `.build/win32-${arch}/vscode-server-win32-${arch}.zip`;
+	await $`New-Item -ItemType Directory -Path .build/win32-${arch} -Force`;
+	await $`7z.exe a -tzip ${serverArchivePath} ..\vscode-server-win32-${arch}`.pipe(process.stdout);
+	await $`7z.exe l ${serverArchivePath}`.pipe(process.stdout);
+
+	// Web
+	console.log(`
+###################################################################
+#                                                                 #
+#                     Package server (web)                        #
+#                                                                 #
+###################################################################
+`);
+
+	const webArchivePath = `.build/win32-${arch}/vscode-server-win32-${arch}-web.zip`;
+	await $`New-Item -ItemType Directory -Path .build/win32-${arch} -Force`;
+	await $`7z.exe a -tzip ${webArchivePath} ..\vscode-server-win32-${arch}-web`.pipe(process.stdout);
+	await $`7z.exe l ${webArchivePath}`.pipe(process.stdout);
+
+	// Sign setup
+	console.log(`
+###################################################################
+#                                                                 #
+#             Codesign setup packages (system, user)              #
+#                                                                 #
+###################################################################
+`);
+	await $`npm exec -- npm-run-all -lp "gulp vscode-win32-${arch}-system-setup -- --sign" "gulp vscode-win32-${arch}-user-setup -- --sign"`.pipe(process.stdout);
 }
 
 main();
