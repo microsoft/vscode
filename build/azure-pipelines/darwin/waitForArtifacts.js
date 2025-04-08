0a466d46d9e3ffacf9a18ceeaabdbbb77342fd97
Ladislau Szomoru <3372902+lszomoru@users.noreply.github.com>
2025-04-08 10:40:27 +0200

    Improve logging

--- build/azure-pipelines/darwin/waitForArtifacts.js
+++ build/azure-pipelines/darwin/waitForArtifacts.js
@@ -6,24 +6,30 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const publish_1 = require("../common/publish");
 const retry_1 = require("../common/retry");
-const artifacts = [
-    'unsigned_vscode_client_darwin_x64_archive',
-    'unsigned_vscode_client_darwin_arm64_archive'
-];
 async function main() {
+    const artifacts = [
+        'unsigned_vscode_client_darwin_x64_archive',
+        'unsigned_vscode_client_darwin_arm64_archive'
+    ];
     // Wait for artifacts for 30 minutes
     for (let index = 0; index < 60; index++) {
-        const allArtifacts = await (0, retry_1.retry)(() => (0, publish_1.getPipelineArtifacts)());
-        console.log(`A total of ${allArtifacts.length} artifacts attached to the pipeline`);
-        const foundArtifacts = allArtifacts.filter(a => artifacts.includes(a.name));
-        console.log(`Found ${foundArtifacts.length} of ${artifacts.length} artifacts${foundArtifacts.length > 0 ? `: ${foundArtifacts.map(a => a.name).join(', ')}` : ''}`);
-        if (foundArtifacts.length === artifacts.length) {
-            console.log(`All required artifacts (${artifacts.join(', ')}) were found.`);
-            return;
+        try {
+            console.log(`Waiting for artifacts (${artifacts.join(', ')}) to be uploaded (${index + 1}/60)...`);
+            const allArtifacts = await (0, retry_1.retry)(() => (0, publish_1.getPipelineArtifacts)());
+            console.log(`  * Artifacts attached to the pipelines: ${allArtifacts.length > 0 ? allArtifacts.map(a => a.name).join(', ') : 'none'}`);
+            const foundArtifacts = allArtifacts.filter(a => artifacts.includes(a.name));
+            console.log(`  * Found ${foundArtifacts.length} of ${artifacts.length} artifacts${foundArtifacts.length > 0 ? `: ${foundArtifacts.map(a => a.name).join(', ')}` : ''}`);
+            if (foundArtifacts.length === artifacts.length) {
+                console.log(`  * All artifacts (${artifacts.join(', ')}) were found`);
+                return;
+            }
+        }
+        catch (err) {
+            console.error(`ERROR: Failed to get pipeline artifacts: ${err}`);
         }
         await new Promise(c => setTimeout(c, 30_000));
     }
-    throw new Error(`Required artifacts (${artifacts.join(', ')}) were not uploaded within 30 minutes.`);
+    throw new Error(`ERROR: Artifacts (${artifacts.join(', ')}) were not uploaded within 30 minutes.`);
 }
 if (require.main === module) {
     main().then(() => {
