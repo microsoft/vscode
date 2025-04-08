237f806fc28c010b473fa654981dfbae01fc8d2b
Ladislau Szomoru <3372902+lszomoru@users.noreply.github.com>
2025-04-08 15:14:10 +0200

    Background codesign

---
+++ build/azure-pipelines/darwin/codesign.js
@@ -0,0 +1,36 @@
+"use strict";
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+Object.defineProperty(exports, "__esModule", { value: true });
+const zx_1 = require("zx");
+const arch = process.env['VSCODE_ARCH'];
+const agentRootDirectory = process.env['AGENT_ROOTDIRECTORY'];
+const pipelineWorkspace = process.env['PIPELINE_WORKSPACE'];
+function printBanner(title) {
+    title = `${title} (${new Date().toISOString()})`;
+    console.log('\n\n');
+    console.log('#'.repeat(75));
+    console.log(`# ${title.padEnd(71)} #`);
+    console.log('#'.repeat(75));
+    console.log('\n\n');
+}
+function sign(esrpCliDLLPath, type, folder, glob) {
+    return (0, zx_1.$) `node build/azure-pipelines/common/sign ${esrpCliDLLPath} ${type} ${folder} '${glob}'`;
+}
+async function main() {
+    const esrpCliDLLPath = `${agentRootDirectory}/_tasks/EsrpCodeSigning_*/*/net6.0/esrpcli.dll`;
+    const folder = `${pipelineWorkspace}/unsigned_vscode_client_darwin_${arch}_archive`;
+    const glob = `VSCode-darwin-${arch}.zip`;
+    // Codesign
+    const codeSignTask = sign(esrpCliDLLPath, 'sign-darwin', folder, glob);
+    printBanner('Codesign');
+    await codeSignTask.pipe(process.stdout);
+    // Notarize
+    const notarizeTask = sign(esrpCliDLLPath, 'notarize-darwin', folder, glob);
+    printBanner('Notarize');
+    await notarizeTask.pipe(process.stdout);
+}
+main();
+//# sourceMappingURL=codesign.js.map
---
+++ build/azure-pipelines/darwin/codesign.ts
@@ -0,0 +1,42 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { $, ProcessPromise } from 'zx';
+
+const arch = process.env['VSCODE_ARCH'];
+const agentRootDirectory = process.env['AGENT_ROOTDIRECTORY'];
+const pipelineWorkspace = process.env['PIPELINE_WORKSPACE'];
+
+function printBanner(title: string) {
+	title = `${title} (${new Date().toISOString()})`;
+
+	console.log('\n\n');
+	console.log('#'.repeat(75));
+	console.log(`# ${title.padEnd(71)} #`);
+	console.log('#'.repeat(75));
+	console.log('\n\n');
+}
+
+function sign(esrpCliDLLPath: string, type: 'sign-darwin' | 'notarize-darwin', folder: string, glob: string): ProcessPromise {
+	return $`node build/azure-pipelines/common/sign ${esrpCliDLLPath} ${type} ${folder} '${glob}'`;
+}
+
+async function main() {
+	const esrpCliDLLPath = `${agentRootDirectory}/_tasks/EsrpCodeSigning_*/*/net6.0/esrpcli.dll`;
+	const folder = `${pipelineWorkspace}/unsigned_vscode_client_darwin_${arch}_archive`;
+	const glob = `VSCode-darwin-${arch}.zip`;
+
+	// Codesign
+	const codeSignTask = sign(esrpCliDLLPath, 'sign-darwin', folder, glob);
+	printBanner('Codesign');
+	await codeSignTask.pipe(process.stdout);
+
+	// Notarize
+	const notarizeTask = sign(esrpCliDLLPath, 'notarize-darwin', folder, glob);
+	printBanner('Notarize');
+	await notarizeTask.pipe(process.stdout);
+}
+
+main();
