48fe50a3adfbb0c5ab37e2e4aa68fea68128cbe0
Rob Lourens <roblourens@gmail.com>
2025-04-09 16:44:11 -0700

    Add comment

--- src/vs/workbench/contrib/chat/browser/chatInputPart.ts
+++ src/vs/workbench/contrib/chat/browser/chatInputPart.ts
@@ -566,6 +566,7 @@ export class ChatInputPart extends Disposable implements IHistoryNavigationWidge
 			this.setChatMode(state.inputState.chatMode);
 		}
 
+		// TODO@roblourens This is for an experiment which will be obsolete in a month or two and can then be removed.
 		if (modelIsEmpty) {
 			const storageKey = this.getDefaultModeExperimentStorageKey();
 			const hasSetDefaultMode = this.storageService.getBoolean(storageKey, StorageScope.WORKSPACE, false);
