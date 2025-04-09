a75f6e6f95d4b0c8938563b00f57140cb31f8f66
Ladislau Szomoru <3372902+lszomoru@users.noreply.github.com>
2025-04-09 13:03:35 +0200

    Add codesign script

---
+++ build/azure-pipelines/linux/codesign.js
@@ -0,0 +1,34 @@
+"use strict";
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+Object.defineProperty(exports, "__esModule", { value: true });
+const zx_1 = require("zx");
+const esrpCliDLLPath = process.env['EsrpCliDllPath'];
+function printBanner(title) {
+    title = `${title} (${new Date().toISOString()})`;
+    console.log('\n\n');
+    console.log('#'.repeat(75));
+    console.log(`# ${title.padEnd(71)} #`);
+    console.log('#'.repeat(75));
+    console.log('\n\n');
+}
+function sign(type, folder, glob) {
+    return (0, zx_1.$) `node build/azure-pipelines/common/sign ${esrpCliDLLPath} ${type} ${folder} '${glob}'`;
+}
+async function main() {
+    // Start the code sign processes in parallel
+    // 1. Codesign deb package
+    // 2. Codesign rpm package
+    const codesignTask1 = sign('sign-pgp', '.build/linux/deb', '*.deb');
+    const codesignTask2 = sign('sign-pgp', '.build/linux/rpm', '*.rpm');
+    // Codesign deb package
+    printBanner('Codesign deb package');
+    await codesignTask1.pipe(process.stdout);
+    // Codesign rpm package
+    printBanner('Codesign rpm package');
+    await codesignTask2.pipe(process.stdout);
+}
+main();
+//# sourceMappingURL=codesign.js.map
---
+++ build/azure-pipelines/linux/codesign.ts
@@ -0,0 +1,40 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { $, ProcessPromise } from 'zx';
+
+const esrpCliDLLPath = process.env['EsrpCliDllPath'];
+
+function printBanner(title: string) {
+	title = `${title} (${new Date().toISOString()})`;
+
+	console.log('\n\n');
+	console.log('#'.repeat(75));
+	console.log(`# ${title.padEnd(71)} #`);
+	console.log('#'.repeat(75));
+	console.log('\n\n');
+}
+
+function sign(type: 'sign-pgp', folder: string, glob: string): ProcessPromise {
+	return $`node build/azure-pipelines/common/sign ${esrpCliDLLPath} ${type} ${folder} '${glob}'`;
+}
+
+async function main() {
+	// Start the code sign processes in parallel
+	// 1. Codesign deb package
+	// 2. Codesign rpm package
+	const codesignTask1 = sign('sign-pgp', '.build/linux/deb', '*.deb');
+	const codesignTask2 = sign('sign-pgp', '.build/linux/rpm', '*.rpm');
+
+	// Codesign deb package
+	printBanner('Codesign deb package');
+	await codesignTask1.pipe(process.stdout);
+
+	// Codesign rpm package
+	printBanner('Codesign rpm package');
+	await codesignTask2.pipe(process.stdout);
+}
+
+main();
