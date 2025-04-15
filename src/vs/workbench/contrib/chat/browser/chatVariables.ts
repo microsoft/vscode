783318d37e365057c2ad1b7f942ac524d23618bf
Johannes Rieken <johannes.rieken@gmail.com>
2025-04-15 08:13:11 +0200

    fixes https://github.com/microsoft/vscode-copilot/issues/15955 (#246571)
    also remove `IChatVariableService#resolveVariables` and inline the sorting of references into the chat service

--- src/vs/workbench/contrib/chat/browser/chatVariables.ts
+++ src/vs/workbench/contrib/chat/browser/chatVariables.ts
@@ -3,9 +3,6 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { coalesce } from '../../../../base/common/arrays.js';
-import { IChatRequestVariableData, IChatRequestVariableEntry } from '../common/chatModel.js';
-import { ChatRequestToolPart, IParsedChatRequest } from '../common/chatParserTypes.js';
 import { IChatVariablesService, IDynamicVariable } from '../common/chatVariables.js';
 import { IChatWidgetService } from './chat.js';
 import { ChatDynamicVariableModel } from './contrib/chatDynamicVariables.js';
@@ -16,33 +13,6 @@ export class ChatVariablesService implements IChatVariablesService {
 	constructor(
 		@IChatWidgetService private readonly chatWidgetService: IChatWidgetService,
 	) { }
-
-	resolveVariables(prompt: IParsedChatRequest, attachedContextVariables: IChatRequestVariableEntry[] | undefined): IChatRequestVariableData {
-		let resolvedVariables: IChatRequestVariableEntry[] = [];
-
-		prompt.parts
-			.forEach((part, i) => {
-				if (part instanceof ChatRequestToolPart) {
-					resolvedVariables[i] = part.toVariableEntry();
-				}
-			});
-
-		// Make array not sparse
-		resolvedVariables = coalesce<IChatRequestVariableEntry>(resolvedVariables);
-
-		// "reverse", high index first so that replacement is simple
-		resolvedVariables.sort((a, b) => b.range!.start - a.range!.start);
-
-		if (attachedContextVariables) {
-			// attachments not in the prompt
-			resolvedVariables.push(...attachedContextVariables);
-		}
-
-
-		return {
-			variables: resolvedVariables,
-		};
-	}
 
 	getDynamicVariables(sessionId: string): ReadonlyArray<IDynamicVariable> {
 		// This is slightly wrong... the parser pulls dynamic references from the input widget, but there is no guarantee that message came from the input here.
