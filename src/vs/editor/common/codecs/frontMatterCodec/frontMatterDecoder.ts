11610a96e69ee8816dfd28906ba92279bfdb7920
Oleg Solomko <legomushroom@gmail.com>
2025-04-15 13:18:46 -0700

    fix hygiene issue

--- src/vs/editor/common/codecs/frontMatterCodec/frontMatterDecoder.ts
+++ src/vs/editor/common/codecs/frontMatterCodec/frontMatterDecoder.ts
@@ -5,7 +5,7 @@
 
 import { VALID_SPACE_TOKENS } from './constants.js';
 import { Word } from '../simpleCodec/tokens/index.js';
-import { TokenStream } from '../utils/tokensStream.js';
+import { TokenStream } from '../utils/tokenStream.js';
 import { VSBuffer } from '../../../../base/common/buffer.js';
 import { ReadableStream } from '../../../../base/common/stream.js';
 import { FrontMatterToken, FrontMatterRecord } from './tokens/index.js';
