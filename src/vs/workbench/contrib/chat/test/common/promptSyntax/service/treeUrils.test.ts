9fb2ddd2cb4ffb0e2643de173c931c439bdb298f
Oleg Solomko <legomushroom@gmail.com>
2025-04-24 15:21:37 -0700

    improve logic and unit tests of the map tree utility

--- src/vs/workbench/contrib/chat/test/common/promptSyntax/service/treeUrils.test.ts
+++ src/vs/workbench/contrib/chat/test/common/promptSyntax/service/treeUrils.test.ts
@@ -181,122 +181,276 @@ suite('tree utilities', () => {
 		});
 	});
 
-	test('• map', () => {
-		interface ITree {
-			id: string;
-			children?: ITree[];
-		}
-
-		const tree: ITree = {
-			id: '1',
-			children: [
-				{
-					id: '1.1',
-				},
-				{
-					id: '1.2',
-					children: [
-						{
-							id: '1.2.1',
-							children: [
-								{
-									id: '1.2.1.1',
-								},
-								{
-									id: '1.2.1.2',
-								},
-								{
-									id: '1.2.1.3',
-									children: [
-										{
-											id: '1.2.1.3.1',
-										},
-										{
-											id: '1.2.1.3.2',
-										},
-									],
-								}
-							],
-						},
-						{
-							id: '1.2.2',
-						},
-					]
-				},
-			],
-		};
-
-		const treeCopy = JSON.parse(JSON.stringify(tree));
-
-		const newRootNode = {
-			newId: '__1__',
-		};
-
-		const newNodeWithoutChildren = {
-			newId: '__1.2.1.3__',
-			children: undefined,
-		};
-
-		const newTree = map((node) => {
-			if (node.id === '1') {
-				return newRootNode;
+	suite('• map', () => {
+		test('• maps a tree', () => {
+			interface ITree {
+				id: string;
+				children?: ITree[];
 			}
 
-			if (node.id === '1.2.1.3') {
-				return newNodeWithoutChildren;
-			}
-
-			return {
-				newId: `__${node.id}__`,
+			const tree: ITree = {
+				id: '1',
+				children: [
+					{
+						id: '1.1',
+					},
+					{
+						id: '1.2',
+						children: [
+							{
+								id: '1.2.1',
+								children: [
+									{
+										id: '1.2.1.1',
+									},
+									{
+										id: '1.2.1.2',
+									},
+									{
+										id: '1.2.1.3',
+									}
+								],
+							},
+							{
+								id: '1.2.2',
+							},
+						]
+					},
+				],
 			};
-		}, tree);
 
-		assert.deepStrictEqual(newTree, {
-			newId: '__1__',
-			children: [
-				{
-					newId: '__1.1__',
-				},
-				{
-					newId: '__1.2__',
-					children: [
-						{
-							newId: '__1.2.1__',
-							children: [
-								{
-									newId: '__1.2.1.1__',
-								},
-								{
-									newId: '__1.2.1.2__',
-								},
-								{
-									newId: '__1.2.1.3__',
-								},
-							],
-						},
-						{
-							newId: '__1.2.2__',
-						},
-					]
-				},
-			],
+			const treeCopy = JSON.parse(JSON.stringify(tree));
+
+			const newRootNode = {
+				newId: '__1__',
+			};
+
+			const newChildNode = {
+				newId: '__1.2.1.3__',
+			};
+
+			const newTree = map((node) => {
+				if (node.id === '1') {
+					return newRootNode;
+				}
+
+				if (node.id === '1.2.1.3') {
+					return newChildNode;
+				}
+
+				return {
+					newId: `__${node.id}__`,
+				};
+			}, tree);
+
+			assert.deepStrictEqual(newTree, {
+				newId: '__1__',
+				children: [
+					{
+						newId: '__1.1__',
+					},
+					{
+						newId: '__1.2__',
+						children: [
+							{
+								newId: '__1.2.1__',
+								children: [
+									{
+										newId: '__1.2.1.1__',
+									},
+									{
+										newId: '__1.2.1.2__',
+									},
+									{
+										newId: '__1.2.1.3__',
+									},
+								],
+							},
+							{
+								newId: '__1.2.2__',
+							},
+						]
+					},
+				],
+			});
+
+			assert(
+				newRootNode === newTree,
+				'Map should not replace return node reference (root node).',
+			);
+
+			assert(
+				newChildNode === newTree.children![1].children![0].children![2],
+				'Map should not replace return node reference (child node).',
+			);
+
+			assert.deepStrictEqual(
+				treeCopy,
+				tree,
+				'forEach should not modify the tree',
+			);
 		});
 
-		assert(
-			newRootNode === newTree,
-			'Map should not replace return node reference (root node).',
-		);
+		test('• callback can control resulting children', () => {
+			interface ITree {
+				id: string;
+				children?: ITree[];
+			}
 
-		assert(
-			newNodeWithoutChildren === newTree.children![1].children![0].children![2],
-			'Map should not replace return node reference (node without children).',
-		);
+			const tree: ITree = {
+				id: '1',
+				children: [
+					{ id: '1.1' },
+					{
+						id: '1.2',
+						children: [
+							{
+								id: '1.2.1',
+								children: [
+									{ id: '1.2.1.1' },
+									{ id: '1.2.1.2' },
+									{
+										id: '1.2.1.3',
+										children: [
+											{
+												id: '1.2.1.3.1',
+											},
+											{
+												id: '1.2.1.3.2',
+											},
+										],
+									}
+								],
+							},
+							{
+								id: '1.2.2',
+								children: [
+									{ id: '1.2.2.1' },
+									{ id: '1.2.2.2' },
+									{ id: '1.2.2.3' },
+								],
+							},
+							{
+								id: '1.2.3',
+								children: [
+									{ id: '1.2.3.1' },
+									{ id: '1.2.3.2' },
+									{ id: '1.2.3.3' },
+									{ id: '1.2.3.4' },
+								],
+							},
+						]
+					},
+				],
+			};
 
-		assert.deepStrictEqual(
-			treeCopy,
-			tree,
-			'forEach should not modify the tree',
-		);
+			const treeCopy = JSON.parse(JSON.stringify(tree));
+
+			const newNodeWithoutChildren = {
+				newId: '__1.2.1.3__',
+				children: undefined,
+			};
+
+			const newTree = map((node, newChildren) => {
+				// validates that explicitly setting `children` to
+				// `undefined` will be preserved on the resulting new node
+				if (node.id === '1.2.1.3') {
+					return newNodeWithoutChildren;
+				}
+
+				// validates that setting `children` to a new array
+				// will be preserved on the resulting new node
+				if (node.id === '1.2.2') {
+					assert.deepStrictEqual(
+						newChildren,
+						[
+							{ newId: '__1.2.2.1__' },
+							{ newId: '__1.2.2.2__' },
+							{ newId: '__1.2.2.3__' },
+						],
+						`Node '${node.id}' must have correct new children.`,
+					);
+
+					return {
+						newId: `__${node.id}__`,
+						children: [newChildren[2]],
+					};
+				}
+
+				// validates that modifying `newChildren` directly
+				// will be preserved on the resulting new node
+				if (node.id === '1.2.3') {
+					assert.deepStrictEqual(
+						newChildren,
+						[
+							{ newId: '__1.2.3.1__' },
+							{ newId: '__1.2.3.2__' },
+							{ newId: '__1.2.3.3__' },
+							{ newId: '__1.2.3.4__' },
+						],
+						`Node '${node.id}' must have correct new children.`,
+					);
+
+					newChildren.length = 2;
+
+					return {
+						newId: `__${node.id}__`,
+					};
+				}
+
+				// convert to a new node in all other cases
+				return {
+					newId: `__${node.id}__`,
+				};
+			}, tree);
+
+			assert.deepStrictEqual(newTree, {
+				newId: '__1__',
+				children: [
+					{ newId: '__1.1__' },
+					{
+						newId: '__1.2__',
+						children: [
+							{
+								newId: '__1.2.1__',
+								children: [
+									{ newId: '__1.2.1.1__' },
+									{ newId: '__1.2.1.2__' },
+									{
+										newId: '__1.2.1.3__',
+										children: undefined,
+									},
+								],
+							},
+							{
+								newId: '__1.2.2__',
+								children: [
+									{ newId: '__1.2.2.3__' },
+								],
+							},
+							{
+								newId: '__1.2.3__',
+								children: [
+									{ newId: '__1.2.3.1__' },
+									{ newId: '__1.2.3.2__' },
+								],
+							},
+						]
+					},
+				],
+			});
+
+			assert(
+				newNodeWithoutChildren === newTree.children![1].children![0].children![2],
+				'Map should not replace return node reference (node without children).',
+			);
+
+			assert.deepStrictEqual(
+				treeCopy,
+				tree,
+				'forEach should not modify the tree',
+			);
+		});
 	});
 
 	test('• curry', () => {
@@ -316,6 +470,12 @@ suite('tree utilities', () => {
 				curriedFunction(secondArgument, thirdArgument),
 				originalFunction(firstArgument, secondArgument, thirdArgument),
 				'Curried and original functions must yield the same result.',
+			);
+
+			// a sanity check to ensure we don't compare ambiguous infinities
+			assert(
+				isFinite(originalFunction(firstArgument, secondArgument, thirdArgument)),
+				'Function results must be finite.',
 			);
 		}
 	});
