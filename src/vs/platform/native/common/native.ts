b8bf4f1612d3091bf78032da671793ae998c8b89
Justin Chen <54879025+justschen@users.noreply.github.com>
2025-04-27 18:58:23 -0700

    make overlay collapsible and some disposable cleanups (#247542)

--- src/vs/platform/native/common/native.ts
+++ src/vs/platform/native/common/native.ts
@@ -156,7 +156,7 @@ export interface ICommonNativeHostService {
 
 	// Screenshots
 	getScreenshot(rect?: IRectangle): Promise<VSBuffer | undefined>;
-	getElementData(offsetX: number, offsetY: number, token: CancellationToken): Promise<IElementData | undefined>;
+	getElementData(offsetX: number, offsetY: number, token: CancellationToken, cancellationId?: number): Promise<IElementData | undefined>;
 
 	// Process
 	getProcessId(): Promise<number | undefined>;
