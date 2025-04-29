b001b52284b359db3cb6f6703582fdee9baa4277
Oleg Solomko <legomushroom@gmail.com>
2025-04-29 12:03:28 -0700

    improve the logic of config migration contribution

--- src/vs/workbench/contrib/chat/common/promptSyntax/contributions/configMigration.ts
+++ src/vs/workbench/contrib/chat/common/promptSyntax/contributions/configMigration.ts
@@ -3,6 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+import { assert } from '../../../../../../base/common/assert.js';
 import { asBoolean } from '../../../../../../platform/prompts/common/config.js';
 import { IWorkbenchContribution } from '../../../../../common/contributions.js';
 import { IConfigurationService } from '../../../../../../platform/configuration/common/configuration.js';
@@ -14,17 +15,30 @@ export class ConfigMigration implements IWorkbenchContribution {
 	constructor(
 		@IConfigurationService configService: IConfigurationService,
 	) {
-		// TODO: @legomushroom - merge object with potentially existent `promptFilesLocations` config
 		const value = configService.getValue('chat.promptFiles');
-		// const value2 = read('promptFiles');
+		const locationsValue = configService.getValue('chat.promptFilesLocations');
 
+		// the setting split happened at the same time as the 'location'
+		// setting was added, hence if the 'location' setting is present,
+		// we don't need to do anything since user already has new settings
+		if ((locationsValue !== undefined) && (locationsValue !== null)) {
+			return;
+		}
+
+		// if setting is not set, nothing to do
 		if ((value === undefined) || (value === null)) {
 			return;
 		}
 
-		if (Array.isArray(value)) {
-			configService.updateValue('chat.promptFiles', true);
+		// if the setting value is a boolean, we don't need to do
+		// anything since it is already a valid configuration value
+		if ((typeof value === 'boolean') || (asBoolean(value) !== undefined)) {
+			return;
+		}
 
+		// in the old setting logic an array of strings was treated
+		// as a list of locations, so we need to migrate that
+		if (Array.isArray(value)) {
 			const locationsValue: Record<string, boolean> = {};
 			for (const filePath of value) {
 				if (typeof filePath !== 'string') {
@@ -38,22 +52,51 @@ export class ConfigMigration implements IWorkbenchContribution {
 				locationsValue[trimmedValue] = true;
 			}
 
+			configService.updateValue('chat.promptFiles', true);
 			configService.updateValue('chat.promptFilesLocations', locationsValue);
 			return;
 		}
 
+		// in the old setting logic an object was treated as a map
+		// of `location -> boolean`, so we need to migrate that
 		if (typeof value === 'object') {
+			// TODO: @legomushroom
+			assert(
+				value !== null,
+				'Object value must not be a null.',
+			);
+
+
+			const locationsValue: Record<string, boolean> = {};
+			for (const [location, enabled] of Object.entries(value)) {
+				// if the old location enabled value wasn't a boolean
+				// then ignore it as it is not a valid value
+				if ((typeof enabled !== 'boolean') || (asBoolean(enabled) === undefined)) {
+					continue;
+				}
+
+				const trimmedValue = location.trim();
+				if (!trimmedValue) {
+					continue;
+				}
+
+				locationsValue[trimmedValue] = enabled;
+			}
+
 			configService.updateValue('chat.promptFiles', true);
-			configService.updateValue('chat.promptFilesLocations', value);
+			configService.updateValue('chat.promptFilesLocations', locationsValue);
 
 			return;
 		}
 
+		// in the old setting logic a string was treated as a single
+		// location path, so we need to migrate that
 		if (typeof value === 'string') {
-			const booleanValue = asBoolean(value);
-			if (booleanValue !== undefined) {
-				return;
-			}
+			// TODO: @legomushroom
+			assert(
+				asBoolean(value) === undefined,
+				`String value must not be a boolean, got '${value}'.`,
+			);
 
 			configService.updateValue('chat.promptFiles', true);
 			configService.updateValue('chat.promptFilesLocations', { [value]: true });
