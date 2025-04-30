101f7245119fca29c75804f20424f6461f3b72f9
Oleg Solomko <legomushroom@gmail.com>
2025-04-30 12:11:50 -0700

    fix inline edit chat instruction attachments

--- src/vs/workbench/contrib/chat/browser/actions/promptActions/dialogs/askToSelectPrompt/promptFilePickers.ts
+++ src/vs/workbench/contrib/chat/browser/actions/promptActions/dialogs/askToSelectPrompt/promptFilePickers.ts
@@ -192,6 +192,7 @@ export class PromptFilePickers {
 						inNewChat: keyMods.ctrlCmd,
 						viewsService: this.viewsService,
 						commandService: this.commandService,
+						widget: options.widget,
 					},
 				);
 				lastActiveWidget = widget;
