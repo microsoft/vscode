6790b6bdc4dbb8e9a0a37b0ea8c7dcf899e00151
oganes <manasianoganes@gmail.com>
2025-05-04 20:19:50 +0200

    another test commit

--- src/vs/editor/common/diff/documentDiffProvider.ts
+++ src/vs/editor/common/diff/documentDiffProvider.ts
@@ -4,6 +4,7 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { CancellationToken } from '../../../base/common/cancellation.js';
+
 import { Event } from '../../../base/common/event.js';
 import { MovedText } from './linesDiffComputer.js';
 import { DetailedLineRangeMapping } from './rangeMapping.js';
