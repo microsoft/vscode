85b3e45d77c8cca82c581aecae2257ff96ba9da0
Connor Peet <copeet@microsoft.com>
2025-05-06 12:50:19 -0700

    chat: fix standalone chat window moves focus when tool call prompts occur (#248243)
    Fixes #248156

--- src/vs/workbench/contrib/chat/browser/chatContentParts/chatConfirmationContentPart.ts
+++ src/vs/workbench/contrib/chat/browser/chatContentParts/chatConfirmationContentPart.ts
@@ -39,7 +39,7 @@ export class ChatConfirmationContentPart extends Disposable implements IChatCont
 				{ label: localize('accept', "Accept"), data: confirmation.data },
 				{ label: localize('dismiss', "Dismiss"), data: confirmation.data, isSecondary: true },
 			];
-		const confirmationWidget = this._register(this.instantiationService.createInstance(ChatConfirmationWidget, confirmation.title, undefined, confirmation.message, buttons));
+		const confirmationWidget = this._register(this.instantiationService.createInstance(ChatConfirmationWidget, confirmation.title, undefined, confirmation.message, buttons, context.container));
 		confirmationWidget.setShowButtons(!confirmation.isUsed);
 
 		this._register(confirmationWidget.onDidChangeHeight(() => this._onDidChangeHeight.fire()));
