16e19c2a3f7b8132c255634d7c9f7faa1706cae5
Johannes <johannes.rieken@gmail.com>
2025-05-09 15:20:30 +0200

    add types and some eager init

--- src/vs/editor/contrib/inlayHints/browser/inlayHintsController.ts
+++ src/vs/editor/contrib/inlayHints/browser/inlayHintsController.ts
@@ -107,14 +107,14 @@ export class InlayHintsController implements IEditorContribution {
 		return editor.getContribution<InlayHintsController>(InlayHintsController.ID) ?? undefined;
 	}
 
-	private readonly _disposables;
-	private readonly _sessionDisposables;
+	private readonly _disposables = new DisposableStore();
+	private readonly _sessionDisposables = new DisposableStore();
+	private readonly _decorationsMetadata = new Map<string, InlayHintDecorationRenderInfo>();
 	private readonly _debounceInfo: IFeatureDebounceInformation;
-	private readonly _decorationsMetadata;
-	private readonly _ruleFactory;
+	private readonly _ruleFactory: DynamicCssRules;
 
 	private _cursorInfo?: { position: Position; notEarlierThan: number };
-	private _activeRenderMode;
+	private _activeRenderMode = RenderMode.Normal;
 	private _activeInlayHintPart?: ActiveInlayHintInfo;
 
 	constructor(
@@ -126,11 +126,7 @@ export class InlayHintsController implements IEditorContribution {
 		@INotificationService private readonly _notificationService: INotificationService,
 		@IInstantiationService private readonly _instaService: IInstantiationService,
 	) {
-		this._disposables = new DisposableStore();
-		this._sessionDisposables = new DisposableStore();
-		this._decorationsMetadata = new Map<string, InlayHintDecorationRenderInfo>();
 		this._ruleFactory = new DynamicCssRules(this._editor);
-		this._activeRenderMode = RenderMode.Normal;
 		this._debounceInfo = _featureDebounce.for(_languageFeaturesService.inlayHintsProvider, 'InlayHint', { min: 25 });
 		this._disposables.add(_languageFeaturesService.inlayHintsProvider.onDidChange(() => this._update()));
 		this._disposables.add(_editor.onDidChangeModel(() => this._update()));
