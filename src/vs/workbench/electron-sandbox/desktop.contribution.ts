75073bac6e409e930f3456634c2ce15b8fdf10d9
Connor Peet <connor@peet.io>
2025-05-19 15:18:37 -0700

    comments

--- src/vs/workbench/electron-sandbox/desktop.contribution.ts
+++ src/vs/workbench/electron-sandbox/desktop.contribution.ts
@@ -145,7 +145,6 @@ import { registerWorkbenchContribution2, WorkbenchPhase } from '../common/contri
 				'default': 10,
 				'minimum': 1,
 				'maximum': 120,
-				'included': !isWindows,
 				'scope': ConfigurationScope.APPLICATION,
 				'markdownDescription': localize('application.shellEnvironmentResolutionTimeout', "Controls the timeout in seconds before giving up resolving the shell environment when the application is not already launched from a terminal. See our [documentation](https://go.microsoft.com/fwlink/?linkid=2149667) for more information.")
 			}
