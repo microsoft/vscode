6a8a059a6a3afd5882b823fa8707d0dc0b9ab3fe
Simon Siefke <simon.siefke@gmail.com>
2025-05-25 17:39:14 +0200

    simplify code

--- extensions/sample-diagnostic-extension/sample-demo.rs
+++ extensions/sample-diagnostic-extension/sample-demo.rs
@@ -4,8 +4,8 @@
  *--------------------------------------------------------------------------------------------*/
 
 fn main() {
-let x = 5;
-println!("The value of x is: {}", x);
-x = 6;
-println!("The value of x is: {}", x);
+	let x = 5;
+	println!("The value of x is: {}", x);
+	x = 6;
+	println!("The value of x is: {}", x);
 }
--- extensions/sample-diagnostic-extension/src/extension.ts
+++ extensions/sample-diagnostic-extension/src/extension.ts
@@ -50,8 +50,8 @@ _This is italic text_
 				code: '',
 				message,
 				range: new vscode.Range(
-					new vscode.Position(7, 4),
-					new vscode.Position(7, 10)
+					new vscode.Position(8, 4),
+					new vscode.Position(8, 10)
 				),
 				severity: vscode.DiagnosticSeverity.Error,
 				source: '',
