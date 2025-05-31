ae34e2662696e3fe2bdc1e076a528a4d0749b10c
Ely Ronnen <elyronnen@gmail.com>
2025-05-31 08:50:40 +0200

    Debugger disassembly: reload disassembly around current instruction, instead of the middle of the page

--- src/vs/workbench/contrib/debug/browser/disassemblyView.ts
+++ src/vs/workbench/contrib/debug/browser/disassemblyView.ts
@@ -638,9 +638,19 @@ export class DisassemblyView extends EditorPane {
 		this.clear();
 		this._instructionBpList = this._debugService.getModel().getInstructionBreakpoints();
 		this.loadDisassembledInstructions(instructionReference, offset, -DisassemblyView.NUM_INSTRUCTIONS_TO_LOAD * 4, DisassemblyView.NUM_INSTRUCTIONS_TO_LOAD * 8).then(() => {
-			// on load, set the target instruction in the middle of the page.
+			// on load, set the target instruction as the current instructionReference.
 			if (this._disassembledInstructions!.length > 0) {
-				const targetIndex = Math.floor(this._disassembledInstructions!.length / 2);
+				let targetIndex: number | undefined = undefined;
+				const refBaseAddress = this._referenceToMemoryAddress.get(instructionReference);
+				if (refBaseAddress !== undefined) {
+					targetIndex = this._disassembledInstructions!.findNextIndex(0, false, entry => entry.address === refBaseAddress);
+				}
+
+				// If didn't find the instructonReference, set the target instruction in the middle of the page.
+				if (targetIndex === undefined) {
+					targetIndex = Math.floor(this._disassembledInstructions!.length / 2);
+				}
+
 				this._disassembledInstructions!.reveal(targetIndex, 0.5);
 
 				// Always focus the target address on reload, or arrow key navigation would look terrible
