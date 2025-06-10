8e71990ff1427da61b5c49e17d3e71778c256d74
Megan Rogge <merogge@microsoft.com>
2025-06-10 14:05:59 -0400

    Apply suggestion from @meganrogge

--- extensions/terminal-suggest/src/terminalSuggestMain.ts
+++ extensions/terminal-suggest/src/terminalSuggestMain.ts
@@ -143,7 +143,6 @@ export async function activate(context: vscode.ExtensionContext) {
 			}
 
 			if (terminal.shellIntegration?.cwd && (result.filesRequested || result.foldersRequested)) {
-				// vscode.env.remoteName
 
 				return new vscode.TerminalCompletionList(result.items, { filesRequested: result.filesRequested, foldersRequested: result.foldersRequested, fileExtensions: result.fileExtensions, cwd: result.cwd ?? terminal.shellIntegration.cwd, env: terminal.shellIntegration?.env?.value, });
 			}
