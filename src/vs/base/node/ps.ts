08f759cdc372061eaab80275264beeca8a30a19e
Hiroki Tagato <tagattie@gmail.com>
2025-06-20 15:42:27 +0900

    Adjust arguments to ps command
    The format '-o pid=,ppid=,pcpu=,pmem=,command=' is not supported by FreeBSD. So adjust it so that the arguments can be accepted by ps command on both Linux and FreeBSD.

--- src/vs/base/node/ps.ts
+++ src/vs/base/node/ps.ts
@@ -225,7 +225,7 @@ export function listProcesses(rootPid: number): Promise<ProcessItem> {
 					}
 				} else {
 					const ps = stdout.toString().trim();
-					const args = '-ax -o pid=,ppid=,pcpu=,pmem=,command=';
+					const args = '-ax -o pid= -o ppid= -o pcpu= -o pmem= -o command=';
 
 					// Set numeric locale to ensure '.' is used as the decimal separator
 					exec(`${ps} ${args}`, { maxBuffer: 1000 * 1024, env: { LC_NUMERIC: 'en_US.UTF-8' } }, (err, stdout, stderr) => {
