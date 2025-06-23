623ef0c92f466ac07edc26c187903bbdcbb29e35
Hiroki Tagato <tagattie@gmail.com>
2025-06-23 21:17:29 +0900

    Add one missing patch to support FreeBSD
    First I thought this change is unnecessary because there is not update service provided for FreeBSD. However, without this change code-oss does not start with error:
    Error: [invokeFunction] unknown service 'updateService'

--- src/vs/code/electron-main/app.ts
+++ src/vs/code/electron-main/app.ts
@@ -990,6 +990,7 @@ export class CodeApplication extends Disposable {
 				break;
 
 			case 'linux':
+			case 'freebsd':
 				if (isLinuxSnap) {
 					services.set(IUpdateService, new SyncDescriptor(SnapUpdateService, [process.env['SNAP'], process.env['SNAP_REVISION']]));
 				} else {
