d57aa3e9a5412891116ded85df35312384ec8f40
Benjamin Pasero <benjamin.pasero@microsoft.com>
2025-06-26 20:49:55 +0200

    .

--- src/vs/workbench/browser/parts/auxiliarybar/auxiliaryBarActions.ts
+++ src/vs/workbench/browser/parts/auxiliarybar/auxiliaryBarActions.ts
@@ -17,9 +17,10 @@ import { ServicesAccessor } from '../../../../platform/instantiation/common/inst
 import { KeybindingWeight } from '../../../../platform/keybinding/common/keybindingsRegistry.js';
 import { KeyCode, KeyMod } from '../../../../base/common/keyCodes.js';
 import { SwitchCompositeViewAction } from '../compositeBarActions.js';
-import { closeIcon } from '../panel/panelActions.js';
+import { closeIcon as panelCloseIcon } from '../panel/panelActions.js';
 
 const maximizeIcon = registerIcon('auxiliarybar-maximize', Codicon.screenFull, localize('maximizeIcon', 'Icon to maximize the secondary side bar.'));
+const closeIcon = registerIcon('auxiliarybar-close', panelCloseIcon, localize('closeIcon', 'Icon to close the secondary side bar.'));
 
 const auxiliaryBarRightIcon = registerIcon('auxiliarybar-right-layout-icon', Codicon.layoutSidebarRight, localize('toggleAuxiliaryIconRight', 'Icon to toggle the secondary side bar off in its right position.'));
 const auxiliaryBarRightOffIcon = registerIcon('auxiliarybar-right-off-layout-icon', Codicon.layoutSidebarRightOff, localize('toggleAuxiliaryIconRightOn', 'Icon to toggle the secondary side bar on in its right position.'));
