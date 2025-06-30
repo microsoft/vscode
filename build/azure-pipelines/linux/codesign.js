3720f6aca5ce24b5adceb4f3a0efae97fa67dd1c
Raymond Zhao <7199958+rzhao271@users.noreply.github.com>
2025-06-30 10:46:09 -0700

    Add built files

--- build/azure-pipelines/linux/codesign.js
+++ build/azure-pipelines/linux/codesign.js
@@ -13,12 +13,21 @@ async function main() {
     // 2. Codesign rpm package
     const codesignTask1 = (0, codesign_1.spawnCodesignProcess)(esrpCliDLLPath, 'sign-pgp', '.build/linux/deb', '*.deb');
     const codesignTask2 = (0, codesign_1.spawnCodesignProcess)(esrpCliDLLPath, 'sign-pgp', '.build/linux/rpm', '*.rpm');
+    // Additional codesign processes with the newer key
+    const codesignTask3 = (0, codesign_1.spawnCodesignProcess)(esrpCliDLLPath, 'sign-pgp-500207', '.build/linux/deb-500207', '*.deb');
+    const codesignTask4 = (0, codesign_1.spawnCodesignProcess)(esrpCliDLLPath, 'sign-pgp-500207', '.build/linux/rpm-500207', '*.rpm');
     // Codesign deb package
     (0, codesign_1.printBanner)('Codesign deb package');
     await (0, codesign_1.streamProcessOutputAndCheckResult)('Codesign deb package', codesignTask1);
     // Codesign rpm package
     (0, codesign_1.printBanner)('Codesign rpm package');
     await (0, codesign_1.streamProcessOutputAndCheckResult)('Codesign rpm package', codesignTask2);
+    // Codesign deb package with the newer key
+    (0, codesign_1.printBanner)('Codesign deb package (sign-pgp-500207)');
+    await (0, codesign_1.streamProcessOutputAndCheckResult)('Codesign deb package (sign-pgp-500207)', codesignTask3);
+    // Codesign rpm package with the newer key
+    (0, codesign_1.printBanner)('Codesign rpm package (sign-pgp-500207)');
+    await (0, codesign_1.streamProcessOutputAndCheckResult)('Codesign rpm package (sign-pgp-500207)', codesignTask4);
 }
 main().then(() => {
     process.exit(0);
