404594b5d67ab5eacafd88a7bff43274ec9e4b61
Benjamin Pasero <benjamin.pasero@microsoft.com>
2025-06-30 11:22:35 +0200

    chat - set `visibleInNewWorkspace` unless `stable`

--- src/vs/workbench/browser/workbench.contribution.ts
+++ src/vs/workbench/browser/workbench.contribution.ts
@@ -537,8 +537,7 @@ const registry = Registry.as<IConfigurationRegistry>(ConfigurationExtensions.Con
 			'workbench.secondarySideBar.defaultVisibility': {
 				'type': 'string',
 				'enum': ['hidden', 'visibleInWorkspace', 'visibleInNewWorkspace', 'visible'],
-				'default': 'hidden',
-				'tags': ['onExp'],
+				'default': product.quality === 'stable' ? 'hidden' : 'visibleInNewWorkspace',
 				'description': localize('secondarySideBarDefaultVisibility', "Controls the default visibility of the secondary side bar in workspaces or empty windows opened for the first time."),
 				'enumDescriptions': [
 					localize('workbench.secondarySideBar.defaultVisibility.hidden', "The secondary side bar is hidden by default."),
