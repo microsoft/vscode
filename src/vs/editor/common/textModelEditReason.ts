0a5fcd27f974f7005b9b76254552e68009952a77
Henning Dieterichs <hdieterichs@microsoft.com>
2025-06-30 15:50:51 +0200

    Avoid redaction of modelId.

--- src/vs/editor/common/textModelEditReason.ts
+++ src/vs/editor/common/textModelEditReason.ts
@@ -90,7 +90,7 @@ export const EditReasons = {
 	inlineChatApplyEdit(data: { modelId: string | undefined }) {
 		return createEditReason({
 			source: 'inlineChat.applyEdits',
-			$modelId: data.modelId,
+			$modelId: avoidRedaction(data.modelId),
 		} as const);
 	},
 
@@ -115,3 +115,11 @@ export const EditReasons = {
 
 type Values<T> = T[keyof T];
 type ITextModelEditReasonMetadata = Values<{ [TKey in keyof typeof EditReasons]: ReturnType<typeof EditReasons[TKey]>['metadataT'] }>;
+
+function avoidRedaction(str: string | undefined): string | undefined {
+	if (str === undefined) {
+		return undefined;
+	}
+	// To avoid false-positive file path redaction.
+	return str.replaceAll('/', '|');
+}
