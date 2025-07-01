5368bb157a4b5cacf7980260fb5fd6dc5e79d010
Bhavya U <bhavyau@microsoft.com>
2025-06-30 19:39:04 -0700

    Update chat widget welcome view and telemetry logging (#253121)
    * Update chat widget welcome view and telemetry logging
    
    - Localize the welcome title in the chat widget.
    - Update max width for chat inputs
    - Integrate telemetry logging for suggested prompt clicks in the chat view welcome part.
    
    * Rename telemetry event for suggested prompt clicks to improve clarity

--- src/vs/workbench/contrib/chat/browser/chatWidget.ts
+++ src/vs/workbench/contrib/chat/browser/chatWidget.ts
@@ -788,7 +788,7 @@ export class ChatWidget extends Disposable implements IChatWidget {
 	private getExpWelcomeViewContent(): IChatViewWelcomeContent {
 		const baseMessage = localize('chatMessage', "Copilot is powered by AI, so mistakes are possible. Review output carefully before use.");
 		const welcomeContent = {
-			title: 'Get Started with VS Code',
+			title: localize('expChatTitle', 'Get Started with VS Code'),
 			message: new MarkdownString(baseMessage),
 			icon: Codicon.copilotLarge,
 			suggestedPrompts: this.getExpSuggestedPrompts(),
@@ -1725,7 +1725,14 @@ export class ChatWidget extends Disposable implements IChatWidget {
 		if (this.viewModel?.editing) {
 			this.inlineInputPart?.layout(layoutHeight, width);
 		}
-		this.inputPart.layout(layoutHeight, width);
+
+		if (this.container.classList.contains('experimental-welcome-view')) {
+			this.inputPart.layout(layoutHeight, Math.min(width, 700));
+		}
+		else {
+			this.inputPart.layout(layoutHeight, width);
+		}
+
 		const inputHeight = this.inputPart.inputPartHeight;
 		const lastElementVisible = this.tree.scrollTop + this.tree.renderHeight >= this.tree.scrollHeight - 2;
 
