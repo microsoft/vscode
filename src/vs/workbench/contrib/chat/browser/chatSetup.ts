dab832e4c140551f4c7d644f770bfc7e7649088e
Benjamin Pasero <benjamin.pasero@microsoft.com>
2025-07-04 11:18:34 +0200

    chat - remove alternate fallback

--- src/vs/workbench/contrib/chat/browser/chatSetup.ts
+++ src/vs/workbench/contrib/chat/browser/chatSetup.ts
@@ -736,10 +736,6 @@ class ChatSetup {
 		if (this.context.state.entitlement === ChatEntitlement.Unknown) {
 			let alternateProvider: 'off' | 'monochrome' | 'colorful' | 'first' = 'off';
 			if (defaultChat.alternativeProviderId) {
-				if (this.configurationService.getValue('chat.setup.signInWithAlternateProvider')) {
-					alternateProvider = 'colorful'; // TODO@bpasero remove me soon
-				}
-
 				switch (variant) {
 					case 'alternate-first':
 						alternateProvider = 'first';
