67019ad99a80d4f79d97d05b1c4363e48e1103fd
Henning Dieterichs <hdieterichs@microsoft.com>
2025-07-07 13:04:45 +0200

    Fixes https://github.com/microsoft/vscode-internalbacklog/issues/5608

--- src/vs/workbench/contrib/editTelemetry/browser/editTelemetry.contribution.ts
+++ src/vs/workbench/contrib/editTelemetry/browser/editTelemetry.contribution.ts
@@ -7,7 +7,7 @@ import { Registry } from '../../../../platform/registry/common/platform.js';
 import { EditTelemetryService } from './editTelemetryService.js';
 import { Extensions as ConfigurationExtensions, IConfigurationRegistry } from '../../../../platform/configuration/common/configurationRegistry.js';
 import { localize } from '../../../../nls.js';
-import { EDIT_TELEMETRY_SETTING_ID, EDIT_TELEMETRY_SHOW_DECORATIONS, EDIT_TELEMETRY_SHOW_STATUS_BAR } from './settings.js';
+import { EDIT_TELEMETRY_DETAILS_SETTING_ID, EDIT_TELEMETRY_SETTING_ID, EDIT_TELEMETRY_SHOW_DECORATIONS, EDIT_TELEMETRY_SHOW_STATUS_BAR } from './settings.js';
 import { registerWorkbenchContribution2, WorkbenchPhase } from '../../../common/contributions.js';
 
 registerWorkbenchContribution2('EditTelemetryService', EditTelemetryService, WorkbenchPhase.AfterRestored);
@@ -23,6 +23,12 @@ configurationRegistry.registerConfiguration({
 			markdownDescription: localize('telemetry.editStats.enabled', "Controls whether to enable telemetry for edit statistics (only sends statistics if general telemetry is enabled)."),
 			type: 'boolean',
 			default: true,
+			tags: ['experimental'],
+		},
+		[EDIT_TELEMETRY_DETAILS_SETTING_ID]: {
+			markdownDescription: localize('telemetry.editStats.detailed.enabled', "Controls whether to enable telemetry for detailed edit statistics (only sends statistics if general telemetry is enabled)."),
+			type: 'boolean',
+			default: false,
 			tags: ['experimental'],
 		},
 		[EDIT_TELEMETRY_SHOW_STATUS_BAR]: {
