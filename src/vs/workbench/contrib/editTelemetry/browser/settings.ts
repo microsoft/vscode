c7241fce5d4e6e57f91033f15f7d3774781115ed
Henning Dieterichs <hdieterichs@microsoft.com>
2025-07-07 15:53:27 +0200

    Fixes https://github.com/microsoft/vscode-internalbacklog/issues/5608 (#254423)

--- src/vs/workbench/contrib/editTelemetry/browser/settings.ts
+++ src/vs/workbench/contrib/editTelemetry/browser/settings.ts
@@ -4,5 +4,6 @@
  *--------------------------------------------------------------------------------------------*/
 
 export const EDIT_TELEMETRY_SETTING_ID = 'telemetry.editStats.enabled';
+export const EDIT_TELEMETRY_DETAILS_SETTING_ID = 'telemetry.editStats.details.enabled';
 export const EDIT_TELEMETRY_SHOW_DECORATIONS = 'telemetry.editStats.showDecorations';
 export const EDIT_TELEMETRY_SHOW_STATUS_BAR = 'telemetry.editStats.showStatusBar';
