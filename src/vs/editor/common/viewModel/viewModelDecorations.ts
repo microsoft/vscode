572ad79ddf520684c00ff367695d8ae69a5146a2
Aiday Marlen Kyzy <amarlenkyzy@microsoft.com>
2025-07-08 11:15:36 +0200

    refactor: extracting inlineDecoration types into separate file

--- src/vs/editor/common/viewModel/viewModelDecorations.ts
+++ src/vs/editor/common/viewModel/viewModelDecorations.ts
@@ -9,9 +9,10 @@ import { Range } from '../core/range.js';
 import { IEditorConfiguration } from '../config/editorConfiguration.js';
 import { IModelDecoration, ITextModel, PositionAffinity } from '../model.js';
 import { IViewModelLines } from './viewModelLines.js';
-import { ICoordinatesConverter, InlineDecoration, InlineDecorationType, ViewModelDecoration } from '../viewModel.js';
+import { ICoordinatesConverter, ViewModelDecoration } from '../viewModel.js';
 import { filterFontDecorations, filterValidationDecorations } from '../config/editorOptions.js';
 import { StandardTokenType } from '../encodedTokenAttributes.js';
+import { InlineDecoration, InlineDecorationType } from './inlineDecorations.js';
 
 export interface IDecorationsViewportData {
 	/**
