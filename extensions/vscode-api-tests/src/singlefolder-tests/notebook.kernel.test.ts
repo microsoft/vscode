2ff34581ea1ee3c0677a41984308b395da6a7933
Don Jayamanne <don.jayamanne@outlook.com>
2025-07-10 21:57:40 +1000

    Skip flaky Notebook Kernel API tests (#255119)
    * Skip flaky Notebook Kernel API tests
    
    * updates

--- extensions/vscode-api-tests/src/singlefolder-tests/notebook.kernel.test.ts
+++ extensions/vscode-api-tests/src/singlefolder-tests/notebook.kernel.test.ts
@@ -123,7 +123,7 @@ const apiTestSerializer: vscode.NotebookSerializer = {
 	}
 };
 
-suite('Notebook Kernel API tests', function () {
+(vscode.env.uiKind === vscode.UIKind.Web ? suite.skip : suite)('Notebook Kernel API tests', function () {
 
 	const testDisposables: vscode.Disposable[] = [];
 	const suiteDisposables: vscode.Disposable[] = [];
