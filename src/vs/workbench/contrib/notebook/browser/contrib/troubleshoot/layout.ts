93d6588222e87cd467e9e7ad3f4fdf917fa04c67
amunger <2019016+amunger@users.noreply.github.com>
2025-07-11 14:49:28 -0700

    cleanup

--- src/vs/workbench/contrib/notebook/browser/contrib/troubleshoot/layout.ts
+++ src/vs/workbench/contrib/notebook/browser/contrib/troubleshoot/layout.ts
@@ -3,12 +3,12 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { Disposable, DisposableStore, dispose, IDisposable, toDisposable } from '../../../../../../base/common/lifecycle.js';
+import { Disposable, DisposableStore, dispose, toDisposable } from '../../../../../../base/common/lifecycle.js';
 import { localize2 } from '../../../../../../nls.js';
 import { Categories } from '../../../../../../platform/action/common/actionCommonCategories.js';
 import { Action2, registerAction2 } from '../../../../../../platform/actions/common/actions.js';
 import { ServicesAccessor } from '../../../../../../platform/instantiation/common/instantiation.js';
-import { getNotebookEditorFromEditorPane, ICellViewModel, ICommonCellViewModelLayoutChangeInfo, INotebookDeltaCellStatusBarItems, INotebookEditor, INotebookEditorContribution } from '../../notebookBrowser.js';
+import { getNotebookEditorFromEditorPane, ICellViewModel, INotebookDeltaCellStatusBarItems, INotebookEditor, INotebookEditorContribution } from '../../notebookBrowser.js';
 import { registerNotebookContribution } from '../../notebookEditorExtensions.js';
 import { NotebookEditorWidget } from '../../notebookEditorWidget.js';
 import { CellStatusbarAlignment, INotebookCellStatusBarItem } from '../../../common/notebookCommon.js';
@@ -23,7 +23,6 @@ export class TroubleshootController extends Disposable implements INotebookEdito
 	private _cellDisposables: DisposableStore[] = [];
 	private _enabled: boolean = false;
 	private _cellStatusItems: string[] = [];
-	private _cellOverlayIds: string[] = [];
 	private _notebookOverlayDomNode: HTMLElement | undefined;
 
 	constructor(private readonly _notebookEditor: INotebookEditor) {
@@ -43,9 +42,8 @@ export class TroubleshootController extends Disposable implements INotebookEdito
 
 	private _update() {
 		this._localStore.clear();
-		this._cellDisposables.forEach(listener => listener.dispose());
+		this._cellDisposables.forEach(d => d.dispose());
 		this._cellDisposables = [];
-		this._removeCellOverlays();
 		this._removeNotebookOverlay();
 
 		if (!this._notebookEditor.hasModel()) {
@@ -216,7 +214,6 @@ export class TroubleshootController extends Disposable implements INotebookEdito
 		});
 
 		if (overlayId) {
-			this._cellOverlayIds.push(overlayId);
 
 			// Update overlay when layout changes
 			const updateLayout = () => {
@@ -255,17 +252,6 @@ export class TroubleshootController extends Disposable implements INotebookEdito
 			}));
 		}
 
-	}
-
-	private _removeCellOverlays() {
-		if (this._cellOverlayIds.length > 0) {
-			this._notebookEditor.changeCellOverlays((accessor) => {
-				for (const id of this._cellOverlayIds) {
-					accessor.removeOverlay(id);
-				}
-			});
-			this._cellOverlayIds = [];
-		}
 	}
 
 	private _removeNotebookOverlay() {
@@ -345,7 +331,6 @@ export class TroubleshootController extends Disposable implements INotebookEdito
 
 	override dispose() {
 		dispose(this._cellDisposables);
-		this._removeCellOverlays();
 		this._removeNotebookOverlay();
 		this._localStore.clear();
 		super.dispose();
