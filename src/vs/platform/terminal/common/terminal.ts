1d2ff521de7889ccfcb1aa0be49204233b49b92e
jiacheng.li <jiacheng.li@bytedance.com>
2025-07-17 00:29:04 +0800

    chore: update interface name

--- src/vs/platform/terminal/common/terminal.ts
+++ src/vs/platform/terminal/common/terminal.ts
@@ -286,7 +286,7 @@ export interface IFixedTerminalDimensions {
 	rows?: number;
 }
 
-export interface IProcessCreationResult {
+export interface IProcessCreateResult {
 	injectedArgs: string[];
 }
 
@@ -331,7 +331,7 @@ export interface IPtyService {
 	 */
 	getLatency(): Promise<IPtyHostLatencyMeasurement[]>;
 
-	start(id: number): Promise<ITerminalLaunchError | IProcessCreationResult | undefined>;
+	start(id: number): Promise<ITerminalLaunchError | IProcessCreateResult | undefined>;
 	shutdown(id: number, immediate: boolean): Promise<void>;
 	input(id: number, data: string): Promise<void>;
 	sendSignal(id: number, signal: string): Promise<void>;
@@ -770,7 +770,7 @@ export interface ITerminalChildProcess {
 	 * @returns undefined when the process was successfully started, otherwise an object containing
 	 * information on what went wrong.
 	 */
-	start(): Promise<ITerminalLaunchError | IProcessCreationResult | undefined>;
+	start(): Promise<ITerminalLaunchError | IProcessCreateResult | undefined>;
 
 	/**
 	 * Detach the process from the UI and await reconnect.
