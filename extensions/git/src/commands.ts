6ed5fa94857b271aa9b0babc3306954fddbec322
Christy <t-nguyenchr@microsoft.com>
2025-07-21 11:17:50 -0700

    open worktree refinements

--- extensions/git/src/commands.ts
+++ extensions/git/src/commands.ts
@@ -3482,17 +3482,26 @@ export class CommandCenter {
 	}
 
 	@command('git.openWorktree', { repository: true })
-	async openWorktreeInCurrentWindow(repository: Repository): Promise<void> {
+	async openWorktreeInCurrentWindow(repository: Repository, ...args: SourceControl[]): Promise<void> {
+		// If multiple repositories are selected, no action is taken
+		if (args.length > 0) {
+			return;
+		}
+
 		const uri = Uri.file(repository.root);
 		await commands.executeCommand('vscode.openFolder', uri, { forceReuseWindow: true });
 	}
 
 	@command('git.openWorktreeInNewWindow', { repository: true })
-	async openWorktreeInNewWindow(repository: Repository): Promise<void> {
+	async openWorktreeInNewWindow(repository: Repository, ...args: SourceControl[]): Promise<void> {
+		// If multiple repositories are selected, no action is taken
+		if (args.length > 0) {
+			return;
+		}
+
 		const uri = Uri.file(repository.root);
 		await commands.executeCommand('vscode.openFolder', uri, { forceNewWindow: true });
 	}
-
 
 	@command('git.graph.deleteTag', { repository: true })
 	async deleteTag2(repository: Repository, historyItem?: SourceControlHistoryItem, historyItemRefId?: string): Promise<void> {
