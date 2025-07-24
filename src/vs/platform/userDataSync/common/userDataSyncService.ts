01dfef42f44458abc2cbd61dbe4785cd05614bf6
Sandeep Somavarapu <sasomava@microsoft.com>
2025-07-24 12:39:34 +0200

    #256423 fallback to get manifest in stable in case

--- src/vs/platform/userDataSync/common/userDataSyncService.ts
+++ src/vs/platform/userDataSync/common/userDataSyncService.ts
@@ -38,6 +38,7 @@ import {
 	IUserData,
 	isUserDataManifest,
 } from './userDataSync.js';
+import { IProductService } from '../../product/common/productService.js';
 
 type SyncErrorClassification = {
 	owner: 'sandy081';
@@ -48,6 +49,15 @@ type SyncErrorClassification = {
 	url?: { classification: 'SystemMetaData'; purpose: 'FeatureInsight'; comment: 'Settings Sync resource URL for which this error has occurred' };
 	resource?: { classification: 'SystemMetaData'; purpose: 'FeatureInsight'; comment: 'Settings Sync resource for which this error has occurred' };
 	executionId?: { classification: 'SystemMetaData'; purpose: 'FeatureInsight'; comment: 'Settings Sync execution id for which this error has occurred' };
+};
+
+type SyncErrorEvent = {
+	code: string;
+	service: string;
+	serverCode?: string;
+	url?: string;
+	resource?: string;
+	executionId?: string;
 };
 
 const LAST_SYNC_TIME_KEY = 'sync.lastSyncTime';
@@ -98,6 +108,7 @@ export class UserDataSyncService extends Disposable implements IUserDataSyncServ
 		@IUserDataProfilesService private readonly userDataProfilesService: IUserDataProfilesService,
 		@IUserDataSyncResourceProviderService private readonly userDataSyncResourceProviderService: IUserDataSyncResourceProviderService,
 		@IUserDataSyncLocalStoreService private readonly userDataSyncLocalStoreService: IUserDataSyncLocalStoreService,
+		@IProductService private readonly productService: IProductService,
 	) {
 		super();
 		this._status = userDataSyncStoreManagementService.userDataSyncStore ? SyncStatus.Idle : SyncStatus.Uninitialized;
@@ -157,13 +168,33 @@ export class UserDataSyncService extends Disposable implements IUserDataSyncServ
 		const startTime = new Date().getTime();
 		const executionId = generateUuid();
 		const syncHeaders = createSyncHeaders(executionId);
-		let latestUserData: IUserDataSyncLatestData | null;
+		let latestUserDataOrManifest: IUserDataSyncLatestData | IUserDataManifest | null;
 		try {
-			latestUserData = await this.userDataSyncStoreService.getLatestData(syncHeaders);
+			latestUserDataOrManifest = await this.userDataSyncStoreService.getLatestData(syncHeaders);
 		} catch (error) {
 			const userDataSyncError = UserDataSyncError.toUserDataSyncError(error);
-			reportUserDataSyncError(userDataSyncError, executionId, this.userDataSyncStoreManagementService, this.telemetryService);
-			throw userDataSyncError;
+			this.telemetryService.publicLog2<SyncErrorEvent, SyncErrorClassification>('sync.download.latest',
+				{
+					code: userDataSyncError.code,
+					serverCode: userDataSyncError instanceof UserDataSyncStoreError ? String(userDataSyncError.serverCode) : undefined,
+					url: userDataSyncError instanceof UserDataSyncStoreError ? userDataSyncError.url : undefined,
+					resource: userDataSyncError.resource,
+					executionId,
+					service: this.userDataSyncStoreManagementService.userDataSyncStore!.url.toString()
+				});
+
+			if (this.productService.quality !== 'stable') {
+				throw error;
+			}
+
+			// Fallback to manifest in stable
+			try {
+				latestUserDataOrManifest = await this.userDataSyncStoreService.manifest(null, syncHeaders);
+			} catch (error) {
+				const userDataSyncError = UserDataSyncError.toUserDataSyncError(error);
+				reportUserDataSyncError(userDataSyncError, executionId, this.userDataSyncStoreManagementService, this.telemetryService);
+				throw userDataSyncError;
+			}
 		}
 
 		/* Manual sync shall start on clean local state */
@@ -174,18 +205,18 @@ export class UserDataSyncService extends Disposable implements IUserDataSyncServ
 		return {
 			id: executionId,
 			async merge(): Promise<void> {
-				return that.sync(latestUserData, true, executionId, cancellableToken.token);
+				return that.sync(latestUserDataOrManifest, true, executionId, cancellableToken.token);
 			},
 			async apply(): Promise<void> {
 				try {
 					try {
-						await that.applyManualSync(latestUserData, executionId, cancellableToken.token);
+						await that.applyManualSync(latestUserDataOrManifest, executionId, cancellableToken.token);
 					} catch (error) {
 						if (UserDataSyncError.toUserDataSyncError(error).code === UserDataSyncErrorCode.MethodNotFound) {
 							that.logService.info('Client is making invalid requests. Cleaning up data...');
 							await that.cleanUpRemoteData();
 							that.logService.info('Applying manual sync again...');
-							await that.applyManualSync(latestUserData, executionId, cancellableToken.token);
+							await that.applyManualSync(latestUserDataOrManifest, executionId, cancellableToken.token);
 						} else {
 							throw error;
 						}
@@ -898,7 +929,7 @@ function canBailout(e: any): boolean {
 }
 
 function reportUserDataSyncError(userDataSyncError: UserDataSyncError, executionId: string, userDataSyncStoreManagementService: IUserDataSyncStoreManagementService, telemetryService: ITelemetryService): void {
-	telemetryService.publicLog2<{ code: string; service: string; serverCode?: string; url?: string; resource?: string; executionId?: string }, SyncErrorClassification>('sync/error',
+	telemetryService.publicLog2<SyncErrorEvent, SyncErrorClassification>('sync/error',
 		{
 			code: userDataSyncError.code,
 			serverCode: userDataSyncError instanceof UserDataSyncStoreError ? String(userDataSyncError.serverCode) : undefined,
