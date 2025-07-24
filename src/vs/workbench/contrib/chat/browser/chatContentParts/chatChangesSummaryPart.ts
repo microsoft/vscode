343d9fafbb58576db5c69332f8b9dc6eb9f1feda
Aiday Marlen Kyzy <amarlenkyzy@microsoft.com>
2025-07-24 15:00:31 +0200

    polishing

--- src/vs/workbench/contrib/chat/browser/chatContentParts/chatChangesSummaryPart.ts
+++ src/vs/workbench/contrib/chat/browser/chatContentParts/chatChangesSummaryPart.ts
@@ -28,7 +28,6 @@ import { autorun, derived, IObservableWithChange } from '../../../../../base/com
 import { MultiDiffEditorInput } from '../../../multiDiffEditor/browser/multiDiffEditorInput.js';
 import { MultiDiffEditorItem } from '../../../multiDiffEditor/browser/multiDiffSourceResolverService.js';
 import { IEditorGroupsService } from '../../../../services/editor/common/editorGroupsService.js';
-import { EditorActivation } from '../../../../../platform/editor/common/editor.js';
 import { Emitter } from '../../../../../base/common/event.js';
 
 export class ChatCheckpointFileChangesSummaryContentPart extends Disposable implements IChatContentPart {
@@ -113,21 +112,8 @@ export class ChatCheckpointFileChangesSummaryContentPart extends Disposable impl
 
 	private renderHeader(container: HTMLElement): IDisposable {
 		const viewListButtonContainer = container.appendChild($('.chat-file-changes-label'));
-		const viewListButton = this._register(new ButtonWithIcon(viewListButtonContainer, {
-			buttonBackground: undefined,
-			buttonBorder: undefined,
-			buttonForeground: undefined,
-			buttonHoverBackground: undefined,
-			buttonSecondaryBackground: undefined,
-			buttonSecondaryForeground: undefined,
-			buttonSecondaryHoverBackground: undefined,
-			buttonSeparator: undefined
-		}));
-		if (this.fileChanges.length === 1) {
-			viewListButton.label = `Changed 1 file`;
-		} else {
-			viewListButton.label = `Changed ${this.fileChanges.length} files`;
-		}
+		const viewListButton = new ButtonWithIcon(viewListButtonContainer, {});
+		viewListButton.label = this.fileChanges.length === 1 ? `Changed 1 file` : `Changed ${this.fileChanges.length} files`;
 
 		const setExpansionState = () => {
 			viewListButton.icon = this.isCollapsed ? Codicon.chevronRight : Codicon.chevronDown;
@@ -137,24 +123,23 @@ export class ChatCheckpointFileChangesSummaryContentPart extends Disposable impl
 		setExpansionState();
 
 		const disposables = new DisposableStore();
+		disposables.add(viewListButton);
 		disposables.add(viewListButton.onDidClick(() => {
 			this.isCollapsed = !this.isCollapsed;
 			setExpansionState();
 		}));
 		disposables.add(this.renderViewAllFileChangesButton(viewListButton.element));
-
 		return toDisposable(() => disposables.dispose());
 	}
 
 	private renderViewAllFileChangesButton(container: HTMLElement): IDisposable {
-
 		const button = container.appendChild($('.chat-view-changes-icon'));
 		button.classList.add(...ThemeIcon.asClassNameArray(Codicon.diffMultiple));
 
 		return dom.addDisposableListener(button, 'click', (e) => {
 			const resources: { originalUri: URI; modifiedUri?: URI }[] = [];
-			this.fileChanges.forEach(e => {
-				const diffEntry = this.fileChangesDiffsObservable.get().get(this.changeID(e));
+			for (const fileChange of this.fileChanges) {
+				const diffEntry = this.fileChangesDiffsObservable.get().get(this.changeID(fileChange));
 				if (diffEntry) {
 					resources.push({
 						originalUri: diffEntry.originalURI,
@@ -162,14 +147,14 @@ export class ChatCheckpointFileChangesSummaryContentPart extends Disposable impl
 					});
 				} else {
 					resources.push({
-						originalUri: e.reference
+						originalUri: fileChange.reference
 					});
 				}
-			});
-			const multiDiffSource = URI.parse(`multi-diff-editor:${new Date().getMilliseconds().toString() + Math.random().toString()}`);
+			}
+			const source = URI.parse(`multi-diff-editor:${new Date().getMilliseconds().toString() + Math.random().toString()}`);
 			const input = this.instantiationService.createInstance(
 				MultiDiffEditorInput,
-				multiDiffSource,
+				source,
 				'Checkpoint File Changes',
 				resources.map(resource => {
 					return new MultiDiffEditorItem(
@@ -180,23 +165,19 @@ export class ChatCheckpointFileChangesSummaryContentPart extends Disposable impl
 				}),
 				false
 			);
-			this.editorGroupsService.activeGroup.openEditor(input, { pinned: true, activation: EditorActivation.ACTIVATE });
+			this.editorGroupsService.activeGroup.openEditor(input);
 			dom.EventHelper.stop(e, true);
 		});
 	}
 
 	private renderFilesList(container: HTMLElement): IDisposable {
 		const store = new DisposableStore();
-		const listpool = store.add(this.instantiationService.createInstance(CollapsibleChangesSummaryListPool));
-		this.list = listpool.get();
-		const listElement = this.list.getHTMLElement();
+		this.list = store.add(this.instantiationService.createInstance(CollapsibleChangesSummaryListPool)).get();
 		const itemsShown = Math.min(this.fileChanges.length, this.MAX_ITEMS_SHOWN);
 		const height = itemsShown * this.ELEMENT_HEIGHT;
-		listElement.style.height = `${height}px`;
-		container.appendChild(listElement.parentElement!);
-
 		this.list.layout(height);
 		this.updateList(this.fileChanges, this.fileChangesDiffsObservable.get());
+		container.appendChild(this.list.getHTMLElement().parentElement!);
 
 		store.add(this.list.onDidOpen((item) => {
 			const element = item.element;
@@ -218,7 +199,6 @@ export class ChatCheckpointFileChangesSummaryContentPart extends Disposable impl
 		store.add(this.list.onContextMenu(e => {
 			dom.EventHelper.stop(e.browserEvent, true);
 		}));
-
 		return store;
 	}
 
@@ -263,44 +243,54 @@ export class ChatCheckpointFileChangesSummaryContentPart extends Disposable impl
 	}
 }
 
-export interface IChatFileChangesSummaryItem extends IChatFileChangesSummary {
+interface IChatFileChangesSummaryItem extends IChatFileChangesSummary {
 	additionalLabels?: { description: string; className: string }[];
 }
 
-export class CollapsibleChangesSummaryListPool extends Disposable {
+interface IChatFileChangesSummaryListWrapper extends IDisposable {
+	list: WorkbenchList<IChatFileChangesSummaryItem>;
+}
 
-	private _pool: ResourcePool<WorkbenchList<IChatFileChangesSummaryItem>>;
+class CollapsibleChangesSummaryListPool extends Disposable {
+
+	private _resourcePool: ResourcePool<IChatFileChangesSummaryListWrapper>;
 
 	constructor(
 		@IInstantiationService private readonly instantiationService: IInstantiationService,
 		@IThemeService private readonly themeService: IThemeService
 	) {
 		super();
-		this._pool = this._register(new ResourcePool(() => this.listFactory()));
+		this._resourcePool = this._register(new ResourcePool(() => this.listFactory()));
 	}
 
-	private listFactory(): WorkbenchList<IChatFileChangesSummaryItem> {
+	private listFactory(): IChatFileChangesSummaryListWrapper {
 		const container = $('.chat-summary-list');
-		this._register(createFileIconThemableTreeContainerScope(container, this.themeService));
-		const resourceLabels = this._register(this.instantiationService.createInstance(ResourceLabels, { onDidChangeVisibility: () => Disposable.None }));
-		const list = this._register(this.instantiationService.createInstance(
+		const store = new DisposableStore();
+		store.add(createFileIconThemableTreeContainerScope(container, this.themeService));
+		const resourceLabels = store.add(this.instantiationService.createInstance(ResourceLabels, { onDidChangeVisibility: () => Disposable.None }));
+		const list = store.add(this.instantiationService.createInstance(
 			WorkbenchList<IChatFileChangesSummaryItem>,
 			'ChatListRenderer',
 			container,
 			new CollapsibleChangesSummaryListDelegate(),
 			[this.instantiationService.createInstance(CollapsibleChangesSummaryListRenderer, resourceLabels)],
-			{}));
-		return list;
+			{}
+		));
+		return {
+			list: list,
+			dispose: () => {
+				store.dispose();
+			}
+		};
 	}
 
 	get(): WorkbenchList<IChatFileChangesSummaryItem> {
-		return this._pool.get();
+		return this._resourcePool.get().list;
 	}
 }
 
-interface ICollapsibleChangesSummaryListTemplate {
+interface ICollapsibleChangesSummaryListTemplate extends IDisposable {
 	readonly label: IResourceLabel;
-	readonly disposables: IDisposable;
 }
 
 class CollapsibleChangesSummaryListDelegate implements IListVirtualDelegate<IChatFileChangesSummaryItem> {
@@ -317,16 +307,15 @@ class CollapsibleChangesSummaryListDelegate implements IListVirtualDelegate<ICha
 class CollapsibleChangesSummaryListRenderer implements IListRenderer<IChatFileChangesSummaryItem, ICollapsibleChangesSummaryListTemplate> {
 
 	static TEMPLATE_ID = 'collapsibleChangesSummaryListRenderer';
+	static CHANGES_SUMMARY_CLASS_NAME = 'insertions-and-deletions';
 
 	readonly templateId: string = CollapsibleChangesSummaryListRenderer.TEMPLATE_ID;
-	readonly insertionAndDeletionsClassName = 'insertions-and-deletions';
 
 	constructor(private labels: ResourceLabels) { }
 
 	renderTemplate(container: HTMLElement): ICollapsibleChangesSummaryListTemplate {
-		const disposables = new DisposableStore();
-		const label = disposables.add(this.labels.create(container, { supportHighlights: true, supportIcons: true }));
-		return { disposables, label };
+		const label = this.labels.create(container, { supportHighlights: true, supportIcons: true });
+		return { label, dispose: () => label.dispose() };
 	}
 
 	renderElement(data: IChatFileChangesSummaryItem, index: number, templateData: ICollapsibleChangesSummaryListTemplate): void {
@@ -336,15 +325,18 @@ class CollapsibleChangesSummaryListRenderer implements IListRenderer<IChatFileCh
 			title: data.reference.path
 		});
 		const labelElement = label.element;
-		labelElement.querySelector(`.${this.insertionAndDeletionsClassName}`)?.remove();
-		const insertionsAndDeletions = labelElement.appendChild($(`.${this.insertionAndDeletionsClassName}`));
-		data.additionalLabels?.forEach(additionalLabel => {
-			const element = insertionsAndDeletions.appendChild($(`.${additionalLabel.className}`));
+		labelElement.querySelector(`.${CollapsibleChangesSummaryListRenderer.CHANGES_SUMMARY_CLASS_NAME}`)?.remove();
+		if (!data.additionalLabels) {
+			return;
+		}
+		const changesSummary = labelElement.appendChild($(`.${CollapsibleChangesSummaryListRenderer.CHANGES_SUMMARY_CLASS_NAME}`));
+		for (const additionalLabel of data.additionalLabels) {
+			const element = changesSummary.appendChild($(`.${additionalLabel.className}`));
 			element.textContent = additionalLabel.description;
-		});
+		}
 	}
 
 	disposeTemplate(templateData: ICollapsibleChangesSummaryListTemplate): void {
-		templateData.disposables.dispose();
+		templateData.dispose();
 	}
 }
