9299465fff45e5d8068040280d359d341ae89b73
Christy <t-nguyenchr@microsoft.com>
2025-07-28 11:29:56 -0700

    Error handle delete worktree within a worktree

--- extensions/git/src/commands.ts
+++ extensions/git/src/commands.ts
@@ -3589,7 +3589,7 @@ export class CommandCenter {
 		return;
 	}
 
-	@command('git.deleteWorktree', { repository: true })
+	@command('git.deleteWorktree', { repository: true, repositoryFilter: ['worktree'] })
 	async deleteWorktree(repository: Repository): Promise<void> {
 		if (!repository.dotGit.commonPath) {
 			return;
@@ -3597,6 +3597,7 @@ export class CommandCenter {
 
 		const mainRepository = this.model.getRepository(path.dirname(repository.dotGit.commonPath));
 		if (!mainRepository) {
+			await window.showErrorMessage(l10n.t('You cannot delete the worktree you are currently in. Please switch to the main repository first.'), { modal: true });
 			return;
 		}
 
