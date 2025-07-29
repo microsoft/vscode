b83f651a78fba030b8d9ee56ac094f9a756f7ae1
Daniel Imms <2193314+Tyriar@users.noreply.github.com>
2025-07-29 11:04:25 -0700

    hi there

--- src/vs/workbench/contrib/chat/browser/chatContentParts/toolInvocationParts/chatResultListSubPart.ts
+++ src/vs/workbench/contrib/chat/browser/chatContentParts/toolInvocationParts/chatResultListSubPart.ts
@@ -17,27 +17,22 @@ export class ChatResultListSubPart extends BaseChatToolInvocationSubPart {
 	public readonly domNode: HTMLElement;
 	public readonly codeblocks: IChatCodeBlockInfo[] = [];
 
-	constructor(
-		toolInvocation: IChatToolInvocation | IChatToolInvocationSerialized,
-		context: IChatContentPartRenderContext,
-		message: string | IMarkdownString,
-		toolDetails: Array<URI | Location>,
-		listPool: CollapsibleListPool,
-		@IInstantiationService instantiationService: IInstantiationService,
+	listPool: CollapsibleListPool,
+	@IInstantiationService instantiationService: IInstantiationService,
 	) {
-		super(toolInvocation);
+	super(toolInvocation);
 
-		const collapsibleListPart = this._register(instantiationService.createInstance(
-			ChatCollapsibleListContentPart,
-			toolDetails.map<IChatCollapsibleListItem>(detail => ({
-				kind: 'reference',
-				reference: detail,
-			})),
-			message,
-			context,
-			listPool,
-		));
-		this._register(collapsibleListPart.onDidChangeHeight(() => this._onDidChangeHeight.fire()));
-		this.domNode = collapsibleListPart.domNode;
-	}
+	const collapsibleListPart = this._register(instantiationService.createInstance(
+		ChatCollapsibleListContentPart,
+		toolDetails.map<IChatCollapsibleListItem>(detail => ({
+			kind: 'reference',
+			reference: detail,
+		})),
+		message,
+		context,
+		listPool,
+	));
+	this._register(collapsibleListPart.onDidChangeHeight(() => this._onDidChangeHeight.fire()));
+	this.domNode = collapsibleListPart.domNode;
+}
 }
