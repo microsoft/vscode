8965cb9f979b69f89b928aff9982ec0b7da6f7f6
Justin Chen <54879025+justschen@users.noreply.github.com>
2025-07-29 17:34:27 -0700

    checkpoint polish 💅🏻  (#258711)
    some checkpoint cleanup

--- src/vs/workbench/contrib/chat/browser/chatListRenderer.ts
+++ src/vs/workbench/contrib/chat/browser/chatListRenderer.ts
@@ -430,7 +430,7 @@ export class ChatListItemRenderer extends Disposable implements ITreeRenderer<Ch
 		const codiconRestoreContainer = dom.append(checkpointRestoreContainer, $('.codicon-container'));
 		dom.append(codiconRestoreContainer, $('span.codicon.codicon-bookmark'));
 		const label = dom.append(checkpointRestoreContainer, $('span.checkpoint-label-text'));
-		label.textContent = localize('checkpointRestore', 'Checkpoint restored');
+		label.textContent = localize('checkpointRestore', 'Checkpoint Restored');
 		const checkpointRestoreToolbar = templateDisposables.add(scopedInstantiationService.createInstance(MenuWorkbenchToolBar, checkpointRestoreContainer, MenuId.ChatMessageRestoreCheckpoint, {
 			actionViewItemProvider: (action, options) => {
 				if (action instanceof MenuItemAction) {
