15c0dabee11af1b0d633dc19fabc8370f036368b
Peng Lyu <penn.lv@gmail.com>
2025-07-30 16:54:40 -0700

    Fix #258586.

--- src/vs/workbench/contrib/chat/browser/chatWidget.ts
+++ src/vs/workbench/contrib/chat/browser/chatWidget.ts
@@ -1619,7 +1619,7 @@ export class ChatWidget extends Disposable implements IChatWidget {
 		this._lockedToCodingAgentContextKey.set(true);
 		this.renderWelcomeViewContentIfNeeded();
 		this.input.setChatMode(ChatModeKind.Ask);
-		this.renderer.updateOptions({ restorable: false, editable: false });
+		this.renderer.updateOptions({ restorable: false, editable: false, progressMessageAtBottomOfResponse: true });
 		this.tree.rerender();
 	}
 
@@ -1637,7 +1637,7 @@ export class ChatWidget extends Disposable implements IChatWidget {
 			this.viewModel.resetInputPlaceholder();
 		}
 		this.inputEditor.updateOptions({ placeholder: undefined });
-		this.renderer.updateOptions({ restorable: true, editable: true });
+		this.renderer.updateOptions({ restorable: true, editable: true, progressMessageAtBottomOfResponse: mode => mode !== ChatModeKind.Ask });
 		this.tree.rerender();
 	}
 
