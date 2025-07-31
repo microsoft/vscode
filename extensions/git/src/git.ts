6cba2da24590a95fdef6f4bd73f05f53aaec9d15
Ladislau Szomoru <3372902+lszomoru@users.noreply.github.com>
2025-07-31 12:39:58 +0200

    Git - detect worktrees when opening a worktree

--- extensions/git/src/git.ts
+++ extensions/git/src/git.ts
@@ -2786,14 +2786,9 @@ export class Repository {
 			return [];
 		}
 
-		if (this.kind !== 'repository') {
-			this.logger.info('[Git][getWorktreesFS] Either a submodule or a worktree, skipping worktree detection');
-			return [];
-		}
-
 		try {
 			// List all worktree folder names
-			const worktreesPath = path.join(this.repositoryRoot, '.git', 'worktrees');
+			const worktreesPath = path.join(this.dotGit.commonPath ?? this.dotGit.path, 'worktrees');
 			const dirents = await fs.readdir(worktreesPath, { withFileTypes: true });
 			const result: Worktree[] = [];
 
@@ -2812,7 +2807,7 @@ export class Repository {
 					result.push({
 						name: dirent.name,
 						// Remove '/.git' suffix
-						path: gitdirContent.replace(/\.git.*$/, ''),
+						path: gitdirContent.replace(/\/.git.*$/, ''),
 						// Remove 'ref: ' prefix
 						ref: headContent.replace(/^ref: /, ''),
 					});
