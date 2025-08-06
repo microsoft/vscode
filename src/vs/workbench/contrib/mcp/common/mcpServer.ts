d1c55b3013b4427b80e8b788d60bb10c8af678be
Joel Verhagen <jver@microsoft.com>
2025-08-06 18:26:56 -0400

    Add support for a help link

--- src/vs/workbench/contrib/mcp/common/mcpServer.ts
+++ src/vs/workbench/contrib/mcp/common/mcpServer.ts
@@ -579,6 +579,9 @@ export class McpServer extends Disposable implements IMcpServer {
 				case 'dnx':
 					docsLink = `https://aka.ms/vscode-mcp-install/dnx`;
 					break;
+				case 'dotnet':
+					docsLink = `https://aka.ms/vscode-mcp-install/dotnet`;
+					break;
 			}
 
 			const options: IPromptChoice[] = [{
