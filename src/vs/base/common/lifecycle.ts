a4b0f9354d27376623d58c8318a098fee4c63e8d
Benjamin Pasero <benjamin.pasero@microsoft.com>
2025-08-07 10:30:39 +0200

    debt - remove some unused code

--- src/vs/base/common/lifecycle.ts
+++ src/vs/base/common/lifecycle.ts
@@ -634,35 +634,6 @@ export class RefCountedDisposable {
 	}
 }
 
-/**
- * A safe disposable can be `unset` so that a leaked reference (listener)
- * can be cut-off.
- */
-export class SafeDisposable implements IDisposable {
-
-	dispose: () => void = () => { };
-	unset: () => void = () => { };
-	isset: () => boolean = () => false;
-
-	constructor() {
-		trackDisposable(this);
-	}
-
-	set(fn: Function) {
-		let callback: Function | undefined = fn;
-		this.unset = () => callback = undefined;
-		this.isset = () => callback !== undefined;
-		this.dispose = () => {
-			if (callback) {
-				callback();
-				callback = undefined;
-				markAsDisposed(this);
-			}
-		};
-		return this;
-	}
-}
-
 export interface IReference<T> extends IDisposable {
 	readonly object: T;
 }
