77f5b108a13ec92bab32aac6bec48d92a9a085e1
Tyler Leonhardt <tyleonha@microsoft.com>
2025-08-27 14:07:33 -0700

    use unsigned option

--- extensions/microsoft-authentication/src/common/config.ts
+++ extensions/microsoft-authentication/src/common/config.ts
@@ -5,14 +5,15 @@
 
 
 export interface IConfig {
-	// The macOS broker redirect URI which is dependent on the bundle identifier of the app.
-	// Other platforms do not require a redirect URI to be set.
+	// The macOS broker redirect URI which is dependent on the bundle identifier of the signed app.
+	// Other platforms do not require a redirect URI to be set. For unsigned apps, the unsigned
+	// format can be used.
 	// Example formats:
 	// msauth.com.msauth.unsignedapp://auth or msauth.<bundleId>://auth
 	macOSBrokerRedirectUri: string;
 }
 
 export const Config: IConfig = {
-	// TODO: This needs to be brought in via distro...
-	macOSBrokerRedirectUri: 'msauth.com.microsoft.VSCodeInsiders.helper://auth'
+	// This is replaced in the build with the correct bundle id for that distro.
+	macOSBrokerRedirectUri: 'msauth.com.msauth.unsignedapp://auth'
 };
