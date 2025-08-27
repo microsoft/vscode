9863cfe3eca7f7373b2eb3e93bbe81393dc9002f
Josh Spicer <23246594+joshspicer@users.noreply.github.com>
2025-08-27 11:38:12 -0700

    prefetch chatSessionContent (#263632)
    prefetch cahtSessionContent

--- src/vs/workbench/contrib/chat/browser/chatSessions.contribution.ts
+++ src/vs/workbench/contrib/chat/browser/chatSessions.contribution.ts
@@ -620,6 +620,14 @@ class CodingAgentChatImplementation extends Disposable implements IChatAgentImpl
 					pinned: true,
 					preferredTitle: chatSessionItem.label,
 				};
+
+				// Prefetch the chat session content to make the subsequent editor swap quick
+				await this.chatSessionService.provideChatSessionContent(
+					this.chatSession.type,
+					chatSessionItem.id,
+					token
+				);
+
 				const activeGroup = this.editorGroupService.activeGroup;
 				const currentEditor = activeGroup?.activeEditor;
 				if (currentEditor instanceof ChatEditorInput) {
@@ -641,8 +649,6 @@ class CodingAgentChatImplementation extends Disposable implements IChatAgentImpl
 						content: new MarkdownString(localize('continueInNewChat', 'Continue **{0}** in a new chat editor', chatSessionItem.label)),
 					}]);
 				}
-
-
 			} catch (error) {
 				// End up here if extension does not support 'provideNewChatSessionItem'
 				// TODO(jospicer): Fallback that should be removed/generalized when API stabilizes
