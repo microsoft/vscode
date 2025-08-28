b0de5aa83567102cb3def102cb29edbc5ff280ac
meganrogge <merogge@microsoft.com>
2025-08-28 09:06:37 -0400

    fix regression

--- src/vs/workbench/contrib/terminal/common/scripts/shellIntegration.ps1
+++ src/vs/workbench/contrib/terminal/common/scripts/shellIntegration.ps1
@@ -173,7 +173,8 @@ elseif ((Test-Path variable:global:GitPromptSettings) -and $Global:GitPromptSett
 if (-not (Get-Module -Name PSReadLine)) {
 	# pwsh version 7+ is required for accessibility support in PSReadLine
 	if ($PSVersionTable.PSVersion.Major -ge 7 -and $env:VSCODE_A11Y_MODE) {
-		$specialPsrlPath = Join-Path '..\psreadline\'
+		$scriptRoot = Split-Path -Parent $MyInvocation.MyCommand.Path
+		$specialPsrlPath = Join-Path $scriptRoot '..\psreadline\'
 		Import-Module $specialPsrlPath
 	}
 }
