265cf9d897f30cc823ec79e01cb748d464c95c6f
Osvaldo Ortega <48293249+osortega@users.noreply.github.com>
2025-08-28 16:33:18 -0700

    Fix double line in chat view description (#263909)

--- src/vs/workbench/contrib/chat/browser/chatSessions.ts
+++ src/vs/workbench/contrib/chat/browser/chatSessions.ts
@@ -843,7 +843,7 @@ class SessionsDelegate implements IListVirtualDelegate<ChatSessionItemWithProvid
 
 	getHeight(element: ChatSessionItemWithProvider): number {
 		// Return consistent height for all items (single-line layout)
-		if (element.description && this.configurationService.getValue(ChatConfiguration.ShowAgentSessionsViewDescription)) {
+		if (element.description && this.configurationService.getValue(ChatConfiguration.ShowAgentSessionsViewDescription) && element.provider.chatSessionType !== 'local') {
 			return SessionsDelegate.ITEM_HEIGHT_WITH_DESCRIPTION;
 		} else {
 			return SessionsDelegate.ITEM_HEIGHT;
@@ -1043,7 +1043,7 @@ class SessionsRenderer extends Disposable implements ITreeRenderer<IChatSessionI
 			this.applyIconColorStyle(iconTheme.id, iconTheme.color.id);
 		}
 
-		const renderDescriptionOnSecondRow = this.configurationService.getValue(ChatConfiguration.ShowAgentSessionsViewDescription);
+		const renderDescriptionOnSecondRow = this.configurationService.getValue(ChatConfiguration.ShowAgentSessionsViewDescription) && sessionWithProvider.provider.chatSessionType !== 'local';
 
 		if (renderDescriptionOnSecondRow && session.description) {
 			templateData.container.classList.toggle('multiline', true);
