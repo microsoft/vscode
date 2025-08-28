21cbca891de565d6c740a72e807e55b74a524c1a
ritesh006 <ritesh.kumar0793@gmail.com>
2025-08-28 11:36:31 +0530

    ts-codeLens: show implementations CodeLens for overridden/abstract class methods

--- extensions/typescript-language-features/src/languageFeatures/codeLens/implementationsCodeLens.ts
+++ extensions/typescript-language-features/src/languageFeatures/codeLens/implementationsCodeLens.ts
@@ -3,6 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+
 import * as vscode from 'vscode';
 import { DocumentSelector } from '../../configuration/documentSelector';
 import { LanguageDescription } from '../../configuration/languageDescription';
