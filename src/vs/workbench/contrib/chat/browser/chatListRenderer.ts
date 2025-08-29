8311ce54364fd42772a239892922c67da09519bf
Josh Spicer <23246594+joshspicer@users.noreply.github.com>
2025-08-28 18:55:56 -0700

    hide chat footer (https://github.com/microsoft/vscode/issues/263519) (#263933)
    * hide chat footer (https://github.com/microsoft/vscode/issues/263519)
    
    * add css

--- src/vs/workbench/contrib/chat/browser/chatListRenderer.ts
+++ src/vs/workbench/contrib/chat/browser/chatListRenderer.ts
@@ -419,6 +419,10 @@ export class ChatListItemRenderer extends Disposable implements ITreeRenderer<Ch
 		const elementDisposables = new DisposableStore();
 
 		const footerToolbarContainer = dom.append(rowContainer, $('.chat-footer-toolbar'));
+		if (this.rendererOptions.noFooter) {
+			footerToolbarContainer.classList.add('hidden');
+		}
+
 		const footerToolbar = templateDisposables.add(scopedInstantiationService.createInstance(MenuWorkbenchToolBar, footerToolbarContainer, MenuId.ChatMessageFooter, {
 			eventDebounceDelay: 0,
 			menuOptions: { shouldForwardArgs: true, renderShortTitle: true },
