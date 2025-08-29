3a98362c1446f5bb7df29bf76828f21f0f99f66d
Osvaldo Ortega <osortega@microsoft.com>
2025-08-29 16:31:13 -0700

    Chat sessions view experiment update

--- src/vs/workbench/contrib/chat/browser/chat.contribution.ts
+++ src/vs/workbench/contrib/chat/browser/chat.contribution.ts
@@ -431,7 +431,10 @@ configurationRegistry.registerConfiguration({
 			enum: ['disabled', 'view'],
 			description: nls.localize('chat.sessionsViewLocation.description', "Controls where to show the agent sessions menu."),
 			default: 'disabled',
-			tags: ['experimental', 'onExp'],
+			tags: ['experimental', 'onExP'],
+			experiment: {
+				mode: 'auto'
+			}
 		},
 		[mcpDiscoverySection]: {
 			type: 'object',
