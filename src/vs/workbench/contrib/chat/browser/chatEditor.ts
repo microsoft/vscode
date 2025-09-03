af47bdcdc5fb140b2b8bf883bfa7a2e1e39010aa
Josh Spicer <23246594+joshspicer@users.noreply.github.com>
2025-09-03 09:32:56 -0700

    refactor to remove other usages

--- src/vs/workbench/contrib/chat/browser/chatEditor.ts
+++ src/vs/workbench/contrib/chat/browser/chatEditor.ts
@@ -6,7 +6,6 @@
 import * as dom from '../../../../base/browser/dom.js';
 import { raceCancellationError } from '../../../../base/common/async.js';
 import { CancellationToken } from '../../../../base/common/cancellation.js';
-import { Schemas } from '../../../../base/common/network.js';
 import { IContextKeyService, IScopedContextKeyService } from '../../../../platform/contextkey/common/contextkey.js';
 import { IEditorOptions } from '../../../../platform/editor/common/editor.js';
 import { IInstantiationService } from '../../../../platform/instantiation/common/instantiation.js';
@@ -24,10 +23,10 @@ import { ChatContextKeys } from '../common/chatContextKeys.js';
 import { IChatModel, IExportableChatData, ISerializableChatData } from '../common/chatModel.js';
 import { CHAT_PROVIDER_ID } from '../common/chatParticipantContribTypes.js';
 import { IChatSessionsService } from '../common/chatSessionsService.js';
-import { ChatSessionUri } from '../common/chatUri.js';
 import { ChatAgentLocation, ChatModeKind } from '../common/constants.js';
 import { clearChatEditor } from './actions/chatClear.js';
 import { ChatEditorInput } from './chatEditorInput.js';
+import { getChatSessionType } from './chatSessions/common.js';
 import { ChatWidget, IChatViewState } from './chatWidget.js';
 
 export interface IChatEditorOptions extends IEditorOptions {
@@ -137,19 +136,14 @@ export class ChatEditor extends EditorPane {
 		}
 
 		let isContributedChatSession = false;
-		const chatSessionTypeFromQueryParam = new URLSearchParams(input.resource.query).get('chatSessionType');
-		if (chatSessionTypeFromQueryParam || input.resource.scheme === Schemas.vscodeChatSession) {
-			const chatSessionType = chatSessionTypeFromQueryParam ?? ChatSessionUri.parse(input.resource)?.chatSessionType;
-			if (chatSessionType) {
-				await raceCancellationError(this.chatSessionsService.canResolveContentProvider(chatSessionType), token);
-				const contributions = this.chatSessionsService.getAllChatSessionContributions();
-				const contribution = contributions.find(c => c.type === chatSessionType);
-				if (contribution) {
-					this.widget.lockToCodingAgent(contribution.name, contribution.displayName, contribution.type);
-					isContributedChatSession = true;
-				} else {
-					this.widget.unlockFromCodingAgent();
-				}
+		const chatSessionType = getChatSessionType(input);
+		if (chatSessionType !== 'local') {
+			await raceCancellationError(this.chatSessionsService.canResolveContentProvider(chatSessionType), token);
+			const contributions = this.chatSessionsService.getAllChatSessionContributions();
+			const contribution = contributions.find(c => c.type === chatSessionType);
+			if (contribution) {
+				this.widget.lockToCodingAgent(contribution.name, contribution.displayName, contribution.type);
+				isContributedChatSession = true;
 			} else {
 				this.widget.unlockFromCodingAgent();
 			}
