977d8762a69506822d22a57906850b62c4781744
Aiday Marlen Kyzy <amarlenkyzy@microsoft.com>
2025-09-04 15:30:25 +0200

    removing the padding on the xRightBoundary because it is causing the issue that when you click on the last character it jumps to the end of the line

--- src/vs/editor/browser/controller/mouseHandler.ts
+++ src/vs/editor/browser/controller/mouseHandler.ts
@@ -570,7 +570,6 @@ class MouseDownOperation extends Disposable {
 
 		const possibleLineNumber = viewLayout.getLineNumberAtVerticalOffset(viewLayout.getCurrentScrollTop() + e.relativePos.y);
 
-		const horizontalScrollPadding = 10;
 		const layoutInfo = this._context.configuration.options.get(EditorOption.layoutInfo);
 
 		const xLeftBoundary = layoutInfo.contentLeft;
@@ -584,7 +583,7 @@ class MouseDownOperation extends Disposable {
 				? layoutInfo.width - layoutInfo.verticalScrollbarWidth // Happens when minimap is hidden
 				: layoutInfo.minimap.minimapLeft
 		);
-		const xRightBoundary = contentRight - horizontalScrollPadding;
+		const xRightBoundary = contentRight;
 		if (e.relativePos.x >= xRightBoundary) {
 			const outsideDistance = e.relativePos.x - xRightBoundary;
 			return MouseTarget.createOutsideEditor(mouseColumn, new Position(possibleLineNumber, model.getLineMaxColumn(possibleLineNumber)), 'right', outsideDistance);
