6209e1f2a005984d8ea055f1c8e3c3a3452b9d1c
Benjamin Pasero <benjamin.pasero@microsoft.com>
2025-09-04 15:19:53 +0200

    files - update to latest `@parcel/watcher`

--- src/vs/platform/files/test/node/parcelWatcher.test.ts
+++ src/vs/platform/files/test/node/parcelWatcher.test.ts
@@ -65,7 +65,8 @@ export class TestParcelWatcher extends ParcelWatcher {
 // mocha but generally). as such they will run only on demand
 // whenever we update the watcher library.
 
-suite.skip('File Watcher (parcel)', function () {
+// eslint-disable-next-line local/code-ensure-no-disposables-leak-in-test
+suite('File Watcher (parcel)', function () {
 
 	this.timeout(10000);
 
