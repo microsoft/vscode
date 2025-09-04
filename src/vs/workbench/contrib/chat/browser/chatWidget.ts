1caef9ef1119ba347fe7e9c9b83caafcc5caaf6b
Martin Aeschlimann <martinae@microsoft.com>
2025-09-04 11:51:40 +0200

    prompt file parsing: handle broken links, unreadabe files (#265105)
    * ComputeAutomaticInstructions: handle broken links, unreadabe files
    
    * more

--- src/vs/workbench/contrib/chat/browser/chatWidget.ts
+++ src/vs/workbench/contrib/chat/browser/chatWidget.ts
@@ -2163,7 +2163,11 @@ export class ChatWidget extends Disposable implements IChatWidget {
 			// if not, check if the context contains a prompt file: This is the old workflow that we still support for legacy reasons
 			const uri = this._findPromptFileInContext(requestInput.attachedContext);
 			if (uri) {
-				parseResult = await this.promptsService.parse(uri, PromptsType.prompt, CancellationToken.None);
+				try {
+					parseResult = await this.promptsService.parse(uri, PromptsType.prompt, CancellationToken.None);
+				} catch (error) {
+					this.logService.error(`[_applyPromptFileIfSet] Failed to parse prompt file: ${uri}`, error);
+				}
 			}
 		}
 
