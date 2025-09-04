f3491d831a514de4cb9720add493a11ff2b15bca
Alex Dima <alexdima@microsoft.com>
2025-09-04 11:01:50 +0200

    Remove the assertion from the event

--- src/vs/workbench/api/common/extHost.api.impl.ts
+++ src/vs/workbench/api/common/extHost.api.impl.ts
@@ -668,7 +668,6 @@ export function createApiFactoryAndRegisterActors(accessor: ServicesAccessor): I
 				return extHostLanguageFeatures.inlineCompletionsUnificationState;
 			},
 			onDidChangeCompletionsUnificationState(listener, thisArg?, disposables?) {
-				checkProposedApiEnabled(extension, 'inlineCompletionsAdditions');
 				return _asExtensionEvent(extHostLanguageFeatures.onDidChangeInlineCompletionsUnificationState)(listener, thisArg, disposables);
 			},
 			registerDocumentLinkProvider(selector: vscode.DocumentSelector, provider: vscode.DocumentLinkProvider): vscode.Disposable {
