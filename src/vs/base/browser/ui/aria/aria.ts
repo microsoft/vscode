686e52c994e1835245e67b230266a3be524467c9
meganrogge <merogge@microsoft.com>
2025-09-09 15:16:32 -0400

    revert another change

--- src/vs/base/browser/ui/aria/aria.ts
+++ src/vs/base/browser/ui/aria/aria.ts
@@ -44,8 +44,8 @@ export function setARIAContainer(parent: HTMLElement) {
 /**
  * Given the provided message, will make sure that it is read as alert to screen readers.
  */
-export function alert(msg?: string): void {
-	if (!ariaContainer || !msg) {
+export function alert(msg: string): void {
+	if (!ariaContainer) {
 		return;
 	}
 
