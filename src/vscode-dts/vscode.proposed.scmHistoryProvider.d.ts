f3d03059592ff7b149e5d143ad6c1da12d1e30ad
Ladislau Szomoru <3372902+lszomoru@users.noreply.github.com>
2025-09-09 15:40:52 +0200

    SCM - add method to resolve a source control history item (#265844)

--- src/vscode-dts/vscode.proposed.scmHistoryProvider.d.ts
+++ src/vscode-dts/vscode.proposed.scmHistoryProvider.d.ts
@@ -30,6 +30,7 @@ declare module 'vscode' {
 		provideHistoryItems(options: SourceControlHistoryOptions, token: CancellationToken): ProviderResult<SourceControlHistoryItem[]>;
 		provideHistoryItemChanges(historyItemId: string, historyItemParentId: string | undefined, token: CancellationToken): ProviderResult<SourceControlHistoryItemChange[]>;
 
+		resolveHistoryItem(historyItemId: string, token: CancellationToken): ProviderResult<SourceControlHistoryItem>;
 		resolveHistoryItemChatContext(historyItemId: string, token: CancellationToken): ProviderResult<string>;
 		resolveHistoryItemRefsCommonAncestor(historyItemRefs: string[], token: CancellationToken): ProviderResult<string>;
 	}
