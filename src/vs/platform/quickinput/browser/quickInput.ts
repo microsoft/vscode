867ea4049c2bd567f88313512c666258d4363235
Tyler James Leonhardt <2644648+TylerLeonhardt@users.noreply.github.com>
2025-09-11 18:28:30 -0400

    Use mouse instead of element for Quick Pick hovers (#266276)
    Fixes https://github.com/microsoft/vscode/issues/263442
    
    Doesn't seem to regress https://github.com/microsoft/vscode/issues/206039

--- src/vs/platform/quickinput/browser/quickInput.ts
+++ src/vs/platform/quickinput/browser/quickInput.ts
@@ -1340,7 +1340,7 @@ export class QuickInputHoverDelegate extends WorkbenchHoverDelegate {
 		@IConfigurationService configurationService: IConfigurationService,
 		@IHoverService hoverService: IHoverService
 	) {
-		super('element', undefined, (options) => this.getOverrideOptions(options), configurationService, hoverService);
+		super('mouse', undefined, (options) => this.getOverrideOptions(options), configurationService, hoverService);
 	}
 
 	private getOverrideOptions(options: IHoverDelegateOptions): Partial<IHoverOptions> {
