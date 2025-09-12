f5c9a1dadf09aa433554a45cb09cbde0e82cb4ab
Megan Rogge <merogge@microsoft.com>
2025-09-12 09:46:26 -0400

    Update src/vs/workbench/contrib/chat/browser/actions/chatAccessibilityHelp.ts
    Co-authored-by: Daniel Imms <2193314+Tyriar@users.noreply.github.com>

--- src/vs/workbench/contrib/chat/browser/actions/chatAccessibilityHelp.ts
+++ src/vs/workbench/contrib/chat/browser/actions/chatAccessibilityHelp.ts
@@ -114,8 +114,8 @@ export function getAccessibilityHelpText(type: 'panelChat' | 'inlineChat' | 'qui
 		content.push(localize('chatEditing.acceptAllFiles', '- Keep All Edits{0}.', '<keybinding:chatEditing.acceptAllFiles>'));
 		content.push(localize('chatEditing.discardAllFiles', '- Undo All Edits{0}.', '<keybinding:chatEditing.discardAllFiles>'));
 		content.push(localize('chatEditing.openFileInDiff', '- Open File in Diff{0}.', '<keybinding:chatEditing.openFileInDiff>'));
-		content.push(ChatEditingShowChangesAction.LABEL, '<keybinding:chatEditing.viewChanges>');
-		content.push(ViewPreviousEditsAction.Label, '<keybinding:chatEditing.viewPreviousEdits>');
+		content.push(`- ${ChatEditingShowChangesAction.LABEL}<keybinding:chatEditing.viewChanges>`);
+		content.push(`- ${ViewPreviousEditsAction.Label}<keybinding:chatEditing.viewPreviousEdits>`);
 	}
 	else {
 		content.push(localize('inlineChat.overview', "Inline chat occurs within a code editor and takes into account the current selection. It is useful for making changes to the current editor. For example, fixing diagnostics, documenting or refactoring code. Keep in mind that AI generated code may be incorrect."));
