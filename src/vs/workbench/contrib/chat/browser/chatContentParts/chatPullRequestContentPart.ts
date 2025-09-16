8a260cb366fd299304cc329f6a1383f2975b625a
Alex Ross <38270282+alexr00@users.noreply.github.com>
2025-09-16 18:46:51 +0200

    Render PR description as markdown (#267072)

--- src/vs/workbench/contrib/chat/browser/chatContentParts/chatPullRequestContentPart.ts
+++ src/vs/workbench/contrib/chat/browser/chatContentParts/chatPullRequestContentPart.ts
@@ -16,6 +16,7 @@ import { ThemeIcon } from '../../../../../base/common/themables.js';
 import { localize } from '../../../../../nls.js';
 import { addDisposableListener } from '../../../../../base/browser/dom.js';
 import { IOpenerService } from '../../../../../platform/opener/common/opener.js';
+import { renderMarkdown } from '../../../../../base/browser/markdownRenderer.js';
 
 export class ChatPullRequestContentPart extends Disposable implements IChatContentPart {
 	public readonly domNode: HTMLElement;
@@ -41,7 +42,8 @@ export class ChatPullRequestContentPart extends Disposable implements IChatConte
 
 		const descriptionElement = dom.append(contentContainer, dom.$('.description'));
 		const descriptionWrapper = dom.append(descriptionElement, dom.$('.description-wrapper'));
-		descriptionWrapper.textContent = this.pullRequestContent.description;
+		const markdown = this._register(renderMarkdown({ value: this.pullRequestContent.description }));
+		dom.append(descriptionWrapper, markdown.element);
 
 		const seeMoreContainer = dom.append(descriptionElement, dom.$('.see-more'));
 		const seeMore: HTMLAnchorElement = dom.append(seeMoreContainer, dom.$('a'));
