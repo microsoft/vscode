0aa2f6590a0ec52412bbb91351412c29ed962509
Will Nickols <willnickols@college.harvard.edu>
2025-09-19 00:00:37 -0700

    Remove vue

--- extensions/erdos-database-client/src/vue/result/component/Contextmenu/constant.ts
+++
@@ -1,5 +0,0 @@
-export const SUBMENU_X_OFFSET = 3;
-export const SUBMENU_Y_OFFSET = -8;
-export const SUBMENU_OPEN_TREND_LEFT = "left";
-export const SUBMENU_OPEN_TREND_RIGHT = "right";
-export const COMPONENT_NAME = "contextmenu-submenu";
