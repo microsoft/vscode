787bbe18ddf3390fccfba978d7e29a861808b8dd
SteVen Batten <sbatten@microsoft.com>
2025-09-23 15:37:36 -0700

    don't show the view container title if it matches the view pane title

--- src/vs/workbench/browser/parts/views/viewPane.ts
+++ src/vs/workbench/browser/parts/views/viewPane.ts
@@ -613,7 +613,8 @@ export abstract class ViewPane extends Pane implements IView {
 		const viewDescriptor = this.viewDescriptorService.getViewDescriptorById(this.id);
 		const isDefault = this.viewDescriptorService.getDefaultContainerById(this.id) === viewContainer;
 
-		if (!isDefault && viewDescriptor?.containerTitle && model.title !== viewDescriptor.containerTitle) {
+		console.log(`Calculating title: isDefault(${isDefault}), containerTitle(${viewDescriptor?.containerTitle}), modelTitle(${model.title})`);
+		if (!isDefault && viewDescriptor?.containerTitle && model.title !== viewDescriptor.containerTitle && title !== viewDescriptor.containerTitle) {
 			return `${viewDescriptor.containerTitle}: ${title}`;
 		}
 
