444c2fbd74c2ba92cb288a8b34a736fc23717c64
Josh Spicer <23246594+joshspicer@users.noreply.github.com>
2025-09-24 13:12:35 -0700

    deprecated note

--- src/vscode-dts/vscode.proposed.chatSessionsProvider.d.ts
+++ src/vscode-dts/vscode.proposed.chatSessionsProvider.d.ts
@@ -42,6 +42,7 @@ declare module 'vscode' {
 		readonly onDidCommitChatSessionItem: Event<{ original: ChatSessionItem /** untitled */; modified: ChatSessionItem /** newly created */ }>;
 
 		/**
+		 * DEPRECATED: Will be removed!
 		 * Creates a new chat session.
 		 *
 		 * @param options Options for the new session including an optional initial prompt and history
