e73ff84a31c9f157a6ed5b5685ea815a7f9ae87f
meganrogge <merogge@microsoft.com>
2025-09-29 12:50:11 -0400

    fix #268836

--- src/vs/workbench/contrib/terminalContrib/chatAgentTools/browser/tools/monitoring/outputMonitor.ts
+++ src/vs/workbench/contrib/terminalContrib/chatAgentTools/browser/tools/monitoring/outputMonitor.ts
@@ -119,7 +119,6 @@ export class OutputMonitor extends Disposable implements IOutputMonitor {
 							continue;
 						} else {
 							this._promptPart?.hide();
-							this._promptPart?.dispose();
 							this._promptPart = undefined;
 							break;
 						}
@@ -154,7 +153,6 @@ export class OutputMonitor extends Disposable implements IOutputMonitor {
 				resources
 			};
 			this._promptPart?.hide();
-			this._promptPart?.dispose();
 			this._promptPart = undefined;
 			this._onDidFinishCommand.fire();
 		}
@@ -237,7 +235,7 @@ export class OutputMonitor extends Disposable implements IOutputMonitor {
 		]);
 
 		if (race.kind === 'decision') {
-			try { continuePollingPart?.hide(); continuePollingPart?.dispose?.(); } catch { /* noop */ }
+			try { await continuePollingPart?.hide(); } catch { /* noop */ }
 			continuePollingPart = undefined;
 
 			// User explicitly declined to keep waiting, so finish with the timed-out result
@@ -255,7 +253,7 @@ export class OutputMonitor extends Disposable implements IOutputMonitor {
 			const r = race.r;
 
 			if (r === OutputMonitorState.Idle || r === OutputMonitorState.Cancelled || r === OutputMonitorState.Timeout) {
-				try { continuePollingPart?.hide(); continuePollingPart?.dispose?.(); } catch { /* noop */ }
+				try { await continuePollingPart?.hide(); } catch { /* noop */ }
 				continuePollingPart = undefined;
 				continuePollingDecisionP = undefined;
 
@@ -516,10 +514,9 @@ export class OutputMonitor extends Disposable implements IOutputMonitor {
 		);
 
 		const inputPromise = new Promise<boolean>(resolve => {
-			const inputDataDisposable = this._register(execution.instance.onDidInputData((data) => {
+			const inputDataDisposable = this._register(execution.instance.onDidInputData(async (data) => {
 				if (!data || data === '\r' || data === '\n' || data === '\r\n') {
-					part.hide();
-					part.dispose();
+					await part.hide();
 					inputDataDisposable.dispose();
 					this._state = OutputMonitorState.PollingForIdle;
 					resolve(true);
@@ -562,9 +559,8 @@ export class OutputMonitor extends Disposable implements IOutputMonitor {
 			getMoreActions(suggestedOption, confirmationPrompt)
 		);
 		const inputPromise = new Promise<string | undefined>(resolve => {
-			inputDataDisposable = this._register(execution.instance.onDidInputData(() => {
-				part.hide();
-				part.dispose();
+			inputDataDisposable = this._register(execution.instance.onDidInputData(async () => {
+				await part.hide();
 				inputDataDisposable.dispose();
 				this._state = OutputMonitorState.PollingForIdle;
 				resolve(undefined);
@@ -611,8 +607,7 @@ export class OutputMonitor extends Disposable implements IOutputMonitor {
 				rejectLabel,
 				async (value: IAction | true) => {
 					thePart.state = 'accepted';
-					thePart.hide();
-					thePart.dispose();
+					await thePart.hide();
 					this._promptPart = undefined;
 					try {
 						const r = await (onAccept ? onAccept(value) : undefined);
@@ -623,8 +618,7 @@ export class OutputMonitor extends Disposable implements IOutputMonitor {
 				},
 				async () => {
 					thePart.state = 'rejected';
-					thePart.hide();
-					thePart.dispose();
+					await thePart.hide();
 					this._promptPart = undefined;
 					try {
 						const r = await (onReject ? onReject() : undefined);
@@ -637,6 +631,7 @@ export class OutputMonitor extends Disposable implements IOutputMonitor {
 				moreActions,
 				() => this._outputMonitorTelemetryCounters.inputToolManualShownCount++
 			));
+
 			chatModel.acceptResponseProgress(request, thePart);
 			this._promptPart = thePart;
 		});
