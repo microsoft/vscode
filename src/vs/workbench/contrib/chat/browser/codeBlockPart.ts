50dab5872c9f64e1a3074e917010e32a57df6268
BeniBenj <besimmonds@microsoft.com>
2025-09-30 12:33:34 +0200

    testing

--- src/vs/workbench/contrib/chat/browser/codeBlockPart.ts
+++ src/vs/workbench/contrib/chat/browser/codeBlockPart.ts
@@ -2,7 +2,7 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-
+// testing
 import './codeBlockPart.css';
 
 import * as dom from '../../../../base/browser/dom.js';
