352bd260a3436bdfdb9be1f552040d29912b30cd
Harald Kirschner <hkirschner@microsoft.com>
2025-09-30 16:48:37 -0700

    newFile as New Chat Editor based on feedback

--- src/vs/workbench/contrib/chat/browser/actions/chatActions.ts
+++ src/vs/workbench/contrib/chat/browser/actions/chatActions.ts
@@ -1056,7 +1056,7 @@ export function registerChatActions() {
 			super({
 				id: ACTION_ID_OPEN_CHAT,
 				title: localize2('interactiveSession.open', "New Chat Editor"),
-				icon: Codicon.plus,
+				icon: Codicon.newFile,
 				f1: true,
 				category: CHAT_CATEGORY,
 				precondition: ChatContextKeys.enabled,
