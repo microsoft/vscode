97b86c8bd9ee65842f5953c066920752a8f7b245
meganrogge <merogge@microsoft.com>
2025-10-01 12:09:32 -0400

    fixes #269091

--- src/vs/workbench/contrib/tasks/browser/abstractTaskService.ts
+++ src/vs/workbench/contrib/tasks/browser/abstractTaskService.ts
@@ -48,7 +48,7 @@ import { ITerminalGroupService, ITerminalService } from '../../terminal/browser/
 import { ITerminalProfileResolverService } from '../../terminal/common/terminal.js';
 
 import { ConfiguringTask, ContributedTask, CustomTask, ExecutionEngine, InMemoryTask, InstancePolicy, ITaskEvent, ITaskIdentifier, ITaskInactiveEvent, ITaskProcessEndedEvent, ITaskSet, JsonSchemaVersion, KeyedTaskIdentifier, RerunAllRunningTasksCommandId, RuntimeType, Task, TASK_RUNNING_STATE, TaskDefinition, TaskEventKind, TaskGroup, TaskRunSource, TaskSettingId, TaskSorter, TaskSourceKind, TasksSchemaProperties, USER_TASKS_GROUP_KEY } from '../common/tasks.js';
-import { ChatAgentLocation, ChatConfiguration, ChatModeKind } from '../../chat/common/constants.js';
+import { ChatAgentLocation, ChatModeKind } from '../../chat/common/constants.js';
 import { CustomExecutionSupportedContext, ICustomizationProperties, IProblemMatcherRunOptions, ITaskFilter, ITaskProvider, ITaskService, IWorkspaceFolderTaskResult, ProcessExecutionSupportedContext, ServerlessWebContext, ShellExecutionSupportedContext, TaskCommandsRegistered, TaskExecutionSupportedContext, TasksAvailableContext } from '../common/taskService.js';
 import { ITaskExecuteResult, ITaskResolver, ITaskSummary, ITaskSystem, ITaskSystemInfo, ITaskTerminateResponse, TaskError, TaskErrors, TaskExecuteKind, Triggers, VerifiedTask } from '../common/taskSystem.js';
 import { getTemplates as getTaskTemplates } from '../common/taskTemplates.js';
@@ -400,7 +400,10 @@ export abstract class AbstractTaskService extends Disposable implements ITaskSer
 				case TaskEventKind.ProcessEnded: {
 					const processEndedEvent = e as ITaskProcessEndedEvent;
 					const startTime = this._taskRunStartTimes.get(e.taskId);
-					const durationMs = processEndedEvent.durationMs ?? (startTime !== undefined ? Date.now() - startTime : undefined);
+					if (!startTime) {
+						break;
+					}
+					const durationMs = processEndedEvent.durationMs ?? (Date.now() - startTime);
 					if (durationMs !== undefined) {
 						this._handleLongRunningTaskCompletion(processEndedEvent, durationMs);
 					}
@@ -411,7 +414,10 @@ export abstract class AbstractTaskService extends Disposable implements ITaskSer
 				case TaskEventKind.Inactive: {
 					const processEndedEvent = e as ITaskInactiveEvent;
 					const startTime = this._taskRunStartTimes.get(e.taskId);
-					const durationMs = processEndedEvent.durationMs ?? (startTime !== undefined ? Date.now() - startTime : undefined);
+					if (!startTime) {
+						break;
+					}
+					const durationMs = processEndedEvent.durationMs ?? (Date.now() - startTime);
 					if (durationMs !== undefined) {
 						this._handleLongRunningTaskCompletion(processEndedEvent, durationMs);
 					}
@@ -502,10 +508,7 @@ export abstract class AbstractTaskService extends Disposable implements ITaskSer
 		}
 
 		const taskRunSource = this._taskRunSources.get(event.taskId);
-		const chatNotificationsEnabled = this._configurationService.getValue<boolean>(ChatConfiguration.NotifyWindowOnResponseReceived);
-
-		// If task was run by chat agent and chat response notifications are enabled, avoid duplicate notifications
-		if (taskRunSource === TaskRunSource.ChatAgent && chatNotificationsEnabled) {
+		if (taskRunSource === TaskRunSource.ChatAgent) {
 			return;
 		}
 
