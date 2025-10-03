70a7ef1bc9e3f65a91a87b5bfaabb0358799f71f
Benjamin Pasero <benjamin.pasero@gmail.com>
2025-10-03 17:23:14 +0200

    debt - start to reduce explicit `any` in my code (#269479)

--- src/vs/code/electron-browser/workbench/workbench.ts
+++ src/vs/code/electron-browser/workbench/workbench.ts
@@ -17,8 +17,7 @@
 	type IMainWindowSandboxGlobals = import('../../../base/parts/sandbox/electron-browser/globals.js').IMainWindowSandboxGlobals;
 	type IDesktopMain = import('../../../workbench/electron-browser/desktop.main.js').IDesktopMain;
 
-	// eslint-disable-next-line local/code-no-any-casts
-	const preloadGlobals: IMainWindowSandboxGlobals = (window as any).vscode; // defined by preload.ts
+	const preloadGlobals = (window as unknown as { vscode: IMainWindowSandboxGlobals }).vscode; // defined by preload.ts
 	const safeProcess = preloadGlobals.process;
 
 	//#region Splash Screen Helpers
@@ -127,8 +126,7 @@
 				titleDiv.style.left = '0';
 				titleDiv.style.top = '0';
 				titleDiv.style.backgroundColor = `${colorInfo.titleBarBackground}`;
-				// eslint-disable-next-line local/code-no-any-casts
-				(titleDiv.style as any)['-webkit-app-region'] = 'drag';
+				(titleDiv.style as CSSStyleDeclaration & { '-webkit-app-region': string })['-webkit-app-region'] = 'drag';
 				splash.appendChild(titleDiv);
 
 				if (colorInfo.titleBarBorder) {
