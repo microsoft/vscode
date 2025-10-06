a0e043c4f13a61213f725264a4d2e58a0d53accc
João Moreno <joaomoreno@users.noreply.github.com>
2025-10-06 15:37:31 +0200

    Update pfs.ts

--- src/vs/base/node/pfs.ts
+++ src/vs/base/node/pfs.ts
@@ -1,5 +1,5 @@
 /*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Copyright (c) Microsoft Corporation. All rights reserved. DO NOT MERGE
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
