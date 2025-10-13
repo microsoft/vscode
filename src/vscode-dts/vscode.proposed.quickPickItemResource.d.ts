2e45f1345a5ae5a6adb901ebf45dd0b33e6d6717
Dmitriy Vasyura <dmitriv@microsoft.com>
2025-10-13 16:18:28 -0700

    Add quickPickItemResource API proposal

---
+++ src/vscode-dts/vscode.proposed.quickPickItemResource.d.ts
@@ -0,0 +1,20 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+declare module 'vscode' {
+
+	// https://github.com/microsoft/vscode/issues/59826
+
+	export interface QuickPickItem {
+		/**
+		 * The {@link Uri} of the resource representing this item.
+		 *
+		 * Will be used to derive the {@link label}, when it is not provided (falsy or empty).
+		 * Will be used to derive the {@link description}, when it is not provided (falsy or empty).
+		 * Will be used to derive the icon from current file icon theme, when {@link iconPath} has {@link ThemeIcon.File} value.
+		 */
+		resourceUri?: Uri;
+	}
+}
