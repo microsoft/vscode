5b8859a889ccf0062fcbd41e9e39f1d2e4420aaa
meganrogge <merogge@microsoft.com>
2025-10-16 11:06:28 -0400

    rm unneeded file

--- src/vs/workbench/contrib/terminalContrib/browser/terminalChatAgentToolsExports.ts
+++
@@ -1,8 +0,0 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
-// Deprecated: This file has been superseded by ITerminalChatService (see terminal.ts and
-// TerminalChatService). It remains temporarily as an empty placeholder to avoid stale imports
-// during incremental development. It should be deleted once all references are removed.
-
