008bb8823254942b39e79ad0ff282633a8ad3690
Ben Villalobos <bevillal@microsoft.com>
2025-10-17 15:09:21 -0700

    simplify hoverCopyButton

--- src/vs/editor/contrib/hover/browser/hoverCopyButton.ts
+++ src/vs/editor/contrib/hover/browser/hoverCopyButton.ts
@@ -3,25 +3,20 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import * as dom from '../../../../base/browser/dom.js';
-import { KeyCode } from '../../../../base/common/keyCodes.js';
 import { Disposable } from '../../../../base/common/lifecycle.js';
 import { IClipboardService } from '../../../../platform/clipboard/common/clipboardService.js';
 import { IHoverService } from '../../../../platform/hover/browser/hover.js';
 import { getDefaultHoverDelegate } from '../../../../base/browser/ui/hover/hoverDelegateFactory.js';
 import { localize } from '../../../../nls.js';
-import { ThemeIcon } from '../../../../base/common/themables.js';
 import { Codicon } from '../../../../base/common/codicons.js';
-import { ClickAction, KeyDownAction } from '../../../../base/browser/ui/hover/hoverWidget.js';
-
-const $ = dom.$;
+import { SimpleButton } from '../../find/browser/findWidget.js';
 
 /**
  * A button that appears in hover parts to copy their content to the clipboard.
  */
 export class HoverCopyButton extends Disposable {
 
-	private readonly _button: HTMLElement;
+	private readonly _button: SimpleButton;
 
 	constructor(
 		private readonly _container: HTMLElement,
@@ -33,22 +28,15 @@ export class HoverCopyButton extends Disposable {
 
 		this._container.classList.add('hover-row-with-copy');
 
-		this._button = dom.append(this._container, $('div.hover-copy-button'));
-		this._button.setAttribute('role', 'button');
-		this._button.setAttribute('tabindex', '0');
-		this._button.setAttribute('aria-label', localize('hover.copy', "Copy"));
+		this._button = this._register(new SimpleButton({
+			label: localize('hover.copy', "Copy"),
+			icon: Codicon.copy,
+			hoverDelegate: getDefaultHoverDelegate('element'),
+			onTrigger: () => this._copyContent(),
+			className: 'hover-copy-button',
+		}, this._hoverService));
 
-		dom.append(this._button, $(ThemeIcon.asCSSSelector(Codicon.copy)));
-
-		this._register(new ClickAction(this._button, () => this._copyContent()));
-		this._register(new KeyDownAction(this._button, () => this._copyContent(), [KeyCode.Enter, KeyCode.Space]));
-
-		const hoverDelegate = getDefaultHoverDelegate('element');
-		this._register(this._hoverService.setupManagedHover(
-			hoverDelegate,
-			this._button,
-			localize('hover.copyTooltip', "Copy")
-		));
+		this._container.appendChild(this._button.domNode);
 	}
 
 	private async _copyContent(): Promise<void> {
