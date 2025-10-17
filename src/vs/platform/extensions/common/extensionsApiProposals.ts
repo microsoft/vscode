c8f6138825f8a39da31a0f3f7627ba677f77acb9
Connor Peet <copeet@microsoft.com>
2025-10-17 15:04:50 -0700

    tools: initial proposal for static mcp tool definitions (#271716)
    * tools: initial proposal for static mcp tool definitions
    
    * finish implementation
    
    * up distro

--- src/vs/platform/extensions/common/extensionsApiProposals.ts
+++ src/vs/platform/extensions/common/extensionsApiProposals.ts
@@ -274,6 +274,9 @@ const _allApiProposals = {
 	mappedEditsProvider: {
 		proposal: 'https://raw.githubusercontent.com/microsoft/vscode/main/src/vscode-dts/vscode.proposed.mappedEditsProvider.d.ts',
 	},
+	mcpToolDefinitions: {
+		proposal: 'https://raw.githubusercontent.com/microsoft/vscode/main/src/vscode-dts/vscode.proposed.mcpToolDefinitions.d.ts',
+	},
 	multiDocumentHighlightProvider: {
 		proposal: 'https://raw.githubusercontent.com/microsoft/vscode/main/src/vscode-dts/vscode.proposed.multiDocumentHighlightProvider.d.ts',
 	},
