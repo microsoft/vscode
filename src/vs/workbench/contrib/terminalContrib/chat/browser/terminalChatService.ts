54cadfe42406ca0bfb3ce4440ac631966f71ad95
Megan Rogge <merogge@microsoft.com>
2025-10-17 10:12:42 -0400

    follow-ups to hidden chat terminal work (#271940)
    follow-ups to #271400

--- src/vs/workbench/contrib/terminalContrib/chat/browser/terminalChatService.ts
+++ src/vs/workbench/contrib/terminalContrib/chat/browser/terminalChatService.ts
@@ -44,7 +44,7 @@ export class TerminalChatService extends Disposable implements ITerminalChatServ
 		this._register(this._lifecycleService.onBeforeShutdown(async e => {
 			// Show all hidden terminals before shutdown so they are restored
 			for (const [toolSessionId, instance] of this._terminalInstancesByToolSessionId) {
-				if (this.terminalIsHidden(toolSessionId) && (instance.capabilities.get(TerminalCapability.CommandDetection)?.promptInputModel.state === PromptInputState.Execute || instance.hasChildProcesses)) {
+				if (this.isBackgroundTerminal(toolSessionId) && (instance.capabilities.get(TerminalCapability.CommandDetection)?.promptInputModel.state === PromptInputState.Execute || instance.hasChildProcesses)) {
 					await this._terminalService.showBackgroundTerminal(instance, true, true);
 				}
 			}
@@ -83,7 +83,7 @@ export class TerminalChatService extends Disposable implements ITerminalChatServ
 		return this._terminalInstancesByToolSessionId.get(terminalToolSessionId);
 	}
 
-	terminalIsHidden(terminalToolSessionId: string | undefined): boolean {
+	isBackgroundTerminal(terminalToolSessionId: string | undefined): boolean {
 		if (!terminalToolSessionId) {
 			return false;
 		}
