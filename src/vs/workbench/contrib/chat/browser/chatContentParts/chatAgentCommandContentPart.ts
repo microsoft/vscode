69d1cbe07c62a20a37558085a42c2108df3fc0b9
Daniel Imms <2193314+Tyriar@users.noreply.github.com>
2025-10-20 03:20:05 -0700

    Adopt HoverStyle.Pointer in a few places
    Part of #243348

--- src/vs/workbench/contrib/chat/browser/chatContentParts/chatAgentCommandContentPart.ts
+++ src/vs/workbench/contrib/chat/browser/chatContentParts/chatAgentCommandContentPart.ts
@@ -15,6 +15,7 @@ import { Codicon } from '../../../../../base/common/codicons.js';
 import { localize } from '../../../../../nls.js';
 import { Button } from '../../../../../base/browser/ui/button/button.js';
 import { generateUuid } from '../../../../../base/common/uuid.js';
+import { HoverStyle } from '../../../../../base/browser/ui/hover/hover.js';
 
 
 export class ChatAgentCommandContentPart extends Disposable implements IChatContentPart {
@@ -36,14 +37,20 @@ export class ChatAgentCommandContentPart extends Disposable implements IChatCont
 		const commandSpan = document.createElement('span');
 		this.domNode.appendChild(commandSpan);
 		commandSpan.innerText = chatSubcommandLeader + cmd.name;
-		this._store.add(this._hoverService.setupDelayedHover(commandSpan, { content: cmd.description, appearance: { showPointer: true } }, { groupId }));
+		this._store.add(this._hoverService.setupDelayedHover(commandSpan, {
+			content: cmd.description,
+			style: HoverStyle.Pointer,
+		}, { groupId }));
 
 		const rerun = localize('rerun', "Rerun without {0}{1}", chatSubcommandLeader, cmd.name);
 		const btn = new Button(this.domNode, { ariaLabel: rerun });
 		btn.icon = Codicon.close;
 		this._store.add(btn.onDidClick(() => onClick()));
 		this._store.add(btn);
-		this._store.add(this._hoverService.setupDelayedHover(btn.element, { content: rerun, appearance: { showPointer: true } }, { groupId }));
+		this._store.add(this._hoverService.setupDelayedHover(btn.element, {
+			content: rerun,
+			style: HoverStyle.Pointer,
+		}, { groupId }));
 	}
 
 	hasSameContent(other: IChatRendererContent, followingContent: IChatRendererContent[], element: ChatTreeItem): boolean {
