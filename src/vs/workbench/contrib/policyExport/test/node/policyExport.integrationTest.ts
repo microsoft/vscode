7f8eebaaabc76fdb1aaf5ee20c04cc958da9d5b7
Paul Wang <paul_wang347@hotmail.com>
2025-10-20 15:39:02 -0700

    fix build

--- src/vs/workbench/contrib/policyExport/test/node/policyExport.integrationTest.ts
+++ src/vs/workbench/contrib/policyExport/test/node/policyExport.integrationTest.ts
@@ -9,7 +9,7 @@ import { promises as fs } from 'fs';
 import * as os from 'os';
 import { ensureNoDisposablesAreLeakedInTestSuite } from '../../../../../base/test/common/utils.js';
 import { isWindows } from '../../../../../base/common/platform.js';
-import { join } from '../../../../../base/common/path.js';
+import { dirname, join } from '../../../../../base/common/path.js';
 import { FileAccess } from '../../../../../base/common/network.js';
 import * as util from 'util';
 
@@ -22,7 +22,8 @@ suite('PolicyExport Integration Tests', () => {
 		// This test launches VS Code with --export-policy-data flag, so it takes longer
 		this.timeout(60000);
 
-		const rootPath = FileAccess.asFileUri('').fsPath.replace(/[\/\\]out[\/\\].*$/, '');
+		// Get the repository root (FileAccess.asFileUri('') points to the 'out' directory)
+		const rootPath = dirname(FileAccess.asFileUri('').fsPath);
 		const checkedInFile = join(rootPath, 'build/lib/policies/policyData.jsonc');
 		const tempFile = join(os.tmpdir(), `policyData-test-${Date.now()}.jsonc`);
 
