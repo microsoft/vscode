1f4897e138563fa7fe1dde61b723ff71719fc83d
Ladislau Szomoru <3372902+lszomoru@users.noreply.github.com>
2025-10-22 20:57:08 +0200

    SCM - more repositories view polish

--- src/vs/workbench/contrib/scm/browser/scmRepositoryRenderer.ts
+++ src/vs/workbench/contrib/scm/browser/scmRepositoryRenderer.ts
@@ -26,6 +26,10 @@ import { IKeybindingService } from '../../../../platform/keybinding/common/keybi
 import { ITelemetryService } from '../../../../platform/telemetry/common/telemetry.js';
 import { IconLabel } from '../../../../base/browser/ui/iconLabel/iconLabel.js';
 import { ThemeIcon } from '../../../../base/common/themables.js';
+import { IUriIdentityService } from '../../../../platform/uriIdentity/common/uriIdentity.js';
+import { shorten } from '../../../../base/common/labels.js';
+import { dirname } from '../../../../base/common/resources.js';
+import { ILabelService } from '../../../../platform/label/common/label.js';
 
 export class RepositoryActionRunner extends ActionRunner {
 	constructor(private readonly getSelectedRepositories: () => ISCMRepository[]) {
@@ -72,9 +76,11 @@ export class RepositoryRenderer implements ICompressibleTreeRenderer<ISCMReposit
 		@IContextKeyService private contextKeyService: IContextKeyService,
 		@IContextMenuService private contextMenuService: IContextMenuService,
 		@IKeybindingService private keybindingService: IKeybindingService,
+		@ILabelService private labelService: ILabelService,
 		@IMenuService private menuService: IMenuService,
 		@ISCMViewService private scmViewService: ISCMViewService,
-		@ITelemetryService private telemetryService: ITelemetryService
+		@ITelemetryService private telemetryService: ITelemetryService,
+		@IUriIdentityService private uriIdentityService: IUriIdentityService
 	) { }
 
 	renderTemplate(container: HTMLElement): RepositoryTemplate {
@@ -105,11 +111,39 @@ export class RepositoryRenderer implements ICompressibleTreeRenderer<ISCMReposit
 			templateData.icon.classList.add(...ThemeIcon.asClassNameArray(repository.provider.iconPath));
 		}
 
+		// Use the description to disambiguate repositories with the same name and have
+		// a `rootUri`. Use the `provider.rootUri` for disambiguation. If they have the
+		// same path, we will use the provider label to disambiguate.
+		let description: string | undefined = undefined;
 		if (repository.provider.rootUri) {
-			templateData.label.setLabel(repository.provider.name, repository.provider.label, { title: `${repository.provider.label}: ${repository.provider.rootUri.fsPath}` });
-		} else {
-			templateData.label.setLabel(repository.provider.name, undefined, { title: repository.provider.label });
+			const repositoriesWithRootUri = this.scmViewService.repositories
+				.filter(r => r.provider.rootUri !== undefined &&
+					this.uriIdentityService.extUri.isEqual(r.provider.rootUri, repository.provider.rootUri));
+
+			const repositoriesWithSameName = this.scmViewService.repositories
+				.filter(r => r.provider.rootUri !== undefined &&
+					r.provider.name === repository.provider.name);
+
+			if (repositoriesWithRootUri.length > 1) {
+				description = repository.provider.label;
+			} else if (repositoriesWithSameName.length > 1) {
+				const repositoryIndex = repositoriesWithSameName.findIndex(r => r === repository);
+				const shortDescription = shorten(repositoriesWithSameName
+					.map(r => this.labelService.getUriLabel(dirname(r.provider.rootUri!), { relative: true })));
+
+				description = shortDescription[repositoryIndex];
+			}
 		}
+
+		const title = repository.provider.rootUri
+			? `${repository.provider.label}: ${repository.provider.rootUri.fsPath}`
+			: repository.provider.label;
+
+		templateData.label.setLabel(repository.provider.name, description, { title });
+
+		// Label minimum width set to fit the three default actions (checkout, sync, more actions)
+		// 170px (activity bar) - 30px (indentation) - 18px (icon) - 3 x 24px (actions) - 12px (padding) - 1px (border)
+		templateData.label.element.style.minWidth = '37px';
 
 		let statusPrimaryActions: IAction[] = [];
 		let menuPrimaryActions: IAction[] = [];
