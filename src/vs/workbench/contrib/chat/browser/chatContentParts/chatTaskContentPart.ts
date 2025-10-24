eee499206f2c7c19b302c8fb40bf2839a63a15e8
Daniel Imms <2193314+Tyriar@users.noreply.github.com>
2025-10-24 05:29:34 -0700

    Remove uneeded todo

--- src/vs/workbench/contrib/chat/browser/chatContentParts/chatTaskContentPart.ts
+++ src/vs/workbench/contrib/chat/browser/chatContentParts/chatTaskContentPart.ts
@@ -41,7 +41,6 @@ export class ChatTaskContentPart extends Disposable implements IChatContentPart 
 				true;
 			this.isSettled = isSettled;
 			const showSpinner = !isSettled && !context.element.isComplete;
-			// TODO: Pass in confirmed reason
 			const progressPart = this._register(instantiationService.createInstance(ChatProgressContentPart, task, chatContentMarkdownRenderer, context, showSpinner, true, undefined, undefined));
 			this.domNode = progressPart.domNode;
 			this.onDidChangeHeight = Event.None;
