a9ba7ce2fd76eec40408d797ed4b9ac099db258a
Rob Lourens <roblourens@gmail.com>
2025-10-26 21:45:15 -0700

    CCR thank you

--- src/vs/workbench/contrib/debug/browser/callStackView.ts
+++ src/vs/workbench/contrib/debug/browser/callStackView.ts
@@ -941,11 +941,11 @@ function stoppedDescription(stoppedDetails: IRawStoppedDetails): string {
 }
 
 function isDebugModel(obj: unknown): obj is IDebugModel {
-	return typeof obj === 'object' && obj !== null && 'getSessions' in obj && typeof obj.getSessions === 'function';
+	return typeof obj && typeof (obj as IDebugModel).getSessions === 'function';
 }
 
 function isDebugSession(obj: unknown): obj is IDebugSession {
-	return typeof obj === 'object' && obj !== null && 'getAllThreads' in obj && typeof obj.getAllThreads === 'function';
+	return typeof obj && typeof (obj as IDebugSession).getAllThreads === 'function';
 }
 
 class CallStackDataSource implements IAsyncDataSource<IDebugModel, CallStackItem> {
