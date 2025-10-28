70060fd170445886ae60aa4670bc9276d51b07c7
Henning Dieterichs <hdieterichs@microsoft.com>
2025-10-28 11:27:05 +0100

    Uses reader.store instead of stateDisposables

--- src/vs/editor/contrib/inlineCompletions/browser/view/inlineEdits/components/gutterIndicatorView.ts
+++ src/vs/editor/contrib/inlineCompletions/browser/view/inlineEdits/components/gutterIndicatorView.ts
@@ -40,7 +40,6 @@ export class InlineEditsGutterIndicator extends Disposable {
 
 	private readonly _gutterIndicatorStyles;
 	private readonly _isHoveredOverInlineEditDebounced: IObservable<boolean>;
-	private _stateDisposables?: DisposableStore;
 
 	constructor(
 		private readonly _editorObs: ObservableCodeEditor,
@@ -93,14 +92,11 @@ export class InlineEditsGutterIndicator extends Disposable {
 
 		this._originalRangeObs = mapOutFalsy(this._originalRange);
 		this._state = derived(this, reader => {
-			this._stateDisposables?.dispose();
-			this._stateDisposables = this._register(new DisposableStore());
-
 			const range = this._originalRangeObs.read(reader);
 			if (!range) { return undefined; }
 			return {
 				range,
-				lineOffsetRange: this._editorObs.observeLineOffsetRange(range, this._stateDisposables),
+				lineOffsetRange: this._editorObs.observeLineOffsetRange(range, reader.store),
 			};
 		});
 		this._stickyScrollController = StickyScrollController.get(this._editorObs.editor);
