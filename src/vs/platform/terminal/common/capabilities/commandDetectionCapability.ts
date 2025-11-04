616df7a2ba6b82d236c9ddc28e6bca0cb978175e
meganrogge <merogge@microsoft.com>
2025-11-03 20:54:08 -0500

    pass in id

--- src/vs/platform/terminal/common/capabilities/commandDetectionCapability.ts
+++ src/vs/platform/terminal/common/capabilities/commandDetectionCapability.ts
@@ -403,7 +403,7 @@ export class CommandDetectionCapability extends Disposable implements ICommandDe
 			this._onCommandFinished.fire(newCommand);
 		}
 		// Create new command for next execution
-		this._currentCommand = new PartialTerminalCommand(this._terminal);
+		this._currentCommand = new PartialTerminalCommand(this._terminal, this._currentCommand.id);
 		this._handleCommandStartOptions = undefined;
 	}
 
