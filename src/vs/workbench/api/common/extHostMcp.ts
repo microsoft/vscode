94c60277973c13756d62360618531ddc890c592c
Tyler Leonhardt <tyleonha@microsoft.com>
2025-11-05 09:27:11 -0800

    Handle 403s as well for step up auth
    The spec calls this out but we didn't include it.
    Fixes https://github.com/microsoft/vscode/issues/269883

--- src/vs/workbench/api/common/extHostMcp.ts
+++ src/vs/workbench/api/common/extHostMcp.ts
@@ -367,8 +367,8 @@ export class McpHTTPHandle extends Disposable {
 		if (this._mode.value === HttpMode.Unknown &&
 			// We care about 4xx errors...
 			res.status >= 400 && res.status < 500
-			// ...except for 401 and 403, which are auth errors
-			&& res.status !== 401 && res.status !== 403
+			// ...except for auth errors
+			&& !isAuthStatusCode(res.status)
 		) {
 			this._log(LogLevel.Info, `${res.status} status sending message to ${this._launch.uri}, will attempt to fall back to legacy SSE`);
 			this._sseFallbackWithMessage(message);
@@ -746,8 +746,8 @@ export class McpHTTPHandle extends Disposable {
 	}
 
 	/**
-	 * Helper method to perform fetch with 401 authentication retry logic.
-	 * If the initial request returns 401 and we don't have auth metadata,
+	 * Helper method to perform fetch with authentication retry logic.
+	 * If the initial request returns an auth error and we don't have auth metadata,
 	 * it will populate the auth metadata and retry once.
 	 * If we already have auth metadata, check if the scopes changed and update them.
 	 */
@@ -755,7 +755,7 @@ export class McpHTTPHandle extends Disposable {
 		const doFetch = () => this._fetch(mcpUrl, init);
 
 		let res = await doFetch();
-		if (res.status === 401) {
+		if (isAuthStatusCode(res.status)) {
 			if (!this._authMetadata) {
 				await this._populateAuthMetadata(mcpUrl, res);
 				await this._addAuthHeader(headers);
@@ -765,7 +765,7 @@ export class McpHTTPHandle extends Disposable {
 					res = await doFetch();
 				}
 			} else {
-				// We have auth metadata, but got a 401. Check if the scopes changed.
+				// We have auth metadata, but got an auth error. Check if the scopes changed.
 				const { scopesChallenge } = this._parseWWWAuthenticateHeader(res);
 				if (!scopesMatch(scopesChallenge, this._authMetadata.scopes)) {
 					this._log(LogLevel.Debug, `Scopes changed from ${JSON.stringify(this._authMetadata.scopes)} to ${JSON.stringify(scopesChallenge)}, updating and retrying`);
@@ -779,8 +779,8 @@ export class McpHTTPHandle extends Disposable {
 				}
 			}
 		}
-		// If we have an Authorization header and still get a 401, we should retry with a new auth registration
-		if (headers['Authorization'] && res.status === 401) {
+		// If we have an Authorization header and still get an auth error, we should retry with a new auth registration
+		if (headers['Authorization'] && isAuthStatusCode(res.status)) {
 			await this._addAuthHeader(headers, true);
 			res = await doFetch();
 		}
@@ -873,4 +873,8 @@ function isJSON(str: string): boolean {
 	} catch (e) {
 		return false;
 	}
+}
+
+function isAuthStatusCode(status: number): boolean {
+	return status === 401 || status === 403;
 }
