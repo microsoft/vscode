c6a24316284ef29ba5aa6ab1e87347c50f619206
Matt Bierner <12821956+mjbvz@users.noreply.github.com>
2025-11-05 20:53:59 -0800

    Small tweak to logic
    We want to check the sessionResource here since it's more accurate

--- src/vs/workbench/contrib/chat/browser/actions/chatClear.ts
+++ src/vs/workbench/contrib/chat/browser/actions/chatClear.ts
@@ -3,12 +3,12 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+import { URI } from '../../../../../base/common/uri.js';
+import { generateUuid } from '../../../../../base/common/uuid.js';
 import { ServicesAccessor } from '../../../../../platform/instantiation/common/instantiation.js';
+import { IEditorService } from '../../../../services/editor/common/editorService.js';
 import { IChatEditorOptions } from '../chatEditor.js';
 import { ChatEditorInput } from '../chatEditorInput.js';
-import { IEditorService } from '../../../../services/editor/common/editorService.js';
-import { generateUuid } from '../../../../../base/common/uuid.js';
-import { URI } from '../../../../../base/common/uri.js';
 
 export async function clearChatEditor(accessor: ServicesAccessor, chatEditorInput?: ChatEditorInput): Promise<void> {
 	const editorService = accessor.get(IEditorService);
@@ -19,11 +19,10 @@ export async function clearChatEditor(accessor: ServicesAccessor, chatEditorInpu
 	}
 
 	if (chatEditorInput instanceof ChatEditorInput) {
-		const genericResource = ChatEditorInput.getNewEditorUri();
-		const resource = chatEditorInput.resource.scheme === genericResource.scheme ? genericResource : URI.from({
+		const resource = chatEditorInput.sessionResource ? URI.from({
 			scheme: chatEditorInput.resource.scheme,
 			path: `/untitled-${generateUuid()}`,
-		});
+		}) : ChatEditorInput.getNewEditorUri();
 
 		// A chat editor can only be open in one group
 		const identifier = editorService.findEditors(chatEditorInput.resource)[0];
