75c7c38a9dba74e0430c905c5289d721ce346545
Matt Bierner <12821956+mjbvz@users.noreply.github.com>
2025-11-10 13:52:43 -0800

    Simpler fix

--- .eslint-plugin-local/code-limited-top-functions.ts
+++ .eslint-plugin-local/code-limited-top-functions.ts
@@ -4,9 +4,9 @@
  *--------------------------------------------------------------------------------------------*/
 
 import * as eslint from 'eslint';
-import * as ESTree from 'estree';
 import { dirname, relative } from 'path';
 import minimatch from 'minimatch';
+import * as ESTree from 'estree';
 
 export = new class implements eslint.Rule.RuleModule {
 
@@ -44,14 +44,10 @@ export = new class implements eslint.Rule.RuleModule {
 		const restrictedFunctions = ruleArgs[matchingKey];
 
 		return {
-			FunctionDeclaration: (node: ESTree.Node) => {
-				const functionDeclaration = node as ESTree.FunctionDeclaration & { parent?: ESTree.Node };
-				if (!functionDeclaration.id) {
-					return;
-				}
-				const isTopLevel = functionDeclaration.parent?.type === 'Program';
-				const functionName = functionDeclaration.id.name;
-				if (isTopLevel && !restrictedFunctions.includes(functionName)) {
+			FunctionDeclaration: (node: ESTree.FunctionDeclaration & { parent?: ESTree.Node }) => {
+				const isTopLevel = node.parent?.type === 'Program';
+				const functionName = node.id.name;
+				if (isTopLevel && !restrictedFunctions.includes(node.id.name)) {
 					context.report({
 						node,
 						message: `Top-level function '${functionName}' is restricted in this file. Allowed functions are: ${restrictedFunctions.join(', ')}.`
@@ -60,13 +56,9 @@ export = new class implements eslint.Rule.RuleModule {
 			},
 			ExportNamedDeclaration(node: ESTree.ExportNamedDeclaration & { parent?: ESTree.Node }) {
 				if (node.declaration && node.declaration.type === 'FunctionDeclaration') {
-					const declaration = node.declaration as ESTree.FunctionDeclaration & { parent?: ESTree.Node };
-					if (!declaration.id) {
-						return;
-					}
-					const functionName = declaration.id.name;
+					const functionName = node.declaration.id.name;
 					const isTopLevel = node.parent?.type === 'Program';
-					if (isTopLevel && !restrictedFunctions.includes(functionName)) {
+					if (isTopLevel && !restrictedFunctions.includes(node.declaration.id.name)) {
 						context.report({
 							node,
 							message: `Top-level function '${functionName}' is restricted in this file. Allowed functions are: ${restrictedFunctions.join(', ')}.`
