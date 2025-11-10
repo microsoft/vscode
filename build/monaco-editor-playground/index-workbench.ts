b6b78218fe1ee7b5fb353914aa8d0ba95047683a
Henning Dieterichs <hdieterichs@microsoft.com>
2025-11-10 18:14:21 +0100

    adds monaco editor playground launch config

---
+++ build/monaco-editor-playground/index-workbench.ts
@@ -0,0 +1,10 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+WebWorkerDescriptor.useBundlerLocationRef();
+
+import { WebWorkerDescriptor } from '../../src/vs/base/browser/webWorkerFactory.js';
+import '../../src/vs/code/browser/workbench/workbench.ts';
+
