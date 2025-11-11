864c563693c1959a263af76c12857d4dfb847c7f
Dmitriy Vasyura <dmitriv@microsoft.com>
2025-11-11 12:09:56 -0800

    Move accent-insensitive filtering to common

--- src/vs/base/common/normalization.ts
+++ src/vs/base/common/normalization.ts
@@ -39,11 +39,32 @@ function normalize(str: string, form: string, normalizedCache: LRUCache<string, 
 	return res;
 }
 
-export const removeAccents: (str: string) => string = (function () {
-	// transform into NFD form and remove accents
-	// see: https://stackoverflow.com/questions/990904/remove-accents-diacritics-in-a-string-in-javascript/37511463#37511463
-	const regex = /[\u0300-\u036f]/g;
-	return function (str: string) {
-		return normalizeNFD(str).replace(regex, '');
+/**
+ * Attempts to normalize the string to Unicode base format (NFD -> remove accents -> lower case).
+ * When original string contains accent characters directly, only lower casing will be performed.
+ * This is done so as to keep the string length the same and not affect indices.
+*
+* @see https://stackoverflow.com/questions/990904/remove-accents-diacritics-in-a-string-in-javascript/37511463#37511463
+*/
+export const tryNormalizeToBase: (str: string) => string = function () {
+	const cache = new LRUCache<string, string>(10000); // bounded to 10000 elements
+	const accentsRegex = /[\u0300-\u036f]/g;
+	return function (str: string): string {
+		const cached = cache.get(str);
+		if (cached) {
+			return cached;
+		}
+
+		let result = str;
+		if (nonAsciiCharactersPattern.test(str)) {
+			const noAccents = str.normalize('NFD').replace(accentsRegex, '');
+			if (noAccents.length === str.length) {
+				result = noAccents;
+			}
+		}
+
+		result = result.toLowerCase();
+		cache.set(str, result);
+		return result;
 	};
-})();
+}();
