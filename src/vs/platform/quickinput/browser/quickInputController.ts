68679b4f826c1d81c6f4b6b88e235b99fd13690e
Dmitriy Vasyura <dmitriv@microsoft.com>
2025-11-11 17:58:38 -0800

    Undo previous change

--- src/vs/platform/quickinput/browser/quickInputController.ts
+++ src/vs/platform/quickinput/browser/quickInputController.ts
@@ -323,7 +323,7 @@ export class QuickInputController extends Disposable {
 			// to prevent screen reader re-announcements when users press Ctrl to silence speech.
 			// See: https://github.com/microsoft/vscode/issues/271032
 			const keyCode = e.keyCode;
-			if (!isModifierKey(keyCode)) {
+			if (keyCode !== KeyCode.Ctrl && keyCode !== KeyCode.Shift && keyCode !== KeyCode.Alt && keyCode !== KeyCode.Meta) {
 				inputBox.removeAttribute('aria-activedescendant');
 			}
 		}));
