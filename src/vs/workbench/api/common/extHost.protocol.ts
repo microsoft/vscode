362acc53da6043bfda5c85d81a7847ad95dbd5b1
Tyler Leonhardt <tyleonha@microsoft.com>
2025-11-17 15:24:56 -0800

    First cut of Mcp definitions specifying auth upfront
    In order to smooth out the process and skip some auth steps.

--- src/vs/workbench/api/common/extHost.protocol.ts
+++ src/vs/workbench/api/common/extHost.protocol.ts
@@ -3125,6 +3125,7 @@ export interface MainThreadMcpShape {
 	$upsertMcpCollection(collection: McpCollectionDefinition.FromExtHost, servers: McpServerDefinition.Serialized[]): void;
 	$deleteMcpCollection(collectionId: string): void;
 	$getTokenFromServerMetadata(id: number, authDetails: IMcpAuthenticationDetails, options?: IMcpAuthenticationOptions): Promise<string | undefined>;
+	$getTokenForProviderId(id: number, providerId: string, scopes: string[], options?: IMcpAuthenticationOptions): Promise<string | undefined>;
 }
 
 export interface MainThreadDataChannelsShape extends IDisposable {
