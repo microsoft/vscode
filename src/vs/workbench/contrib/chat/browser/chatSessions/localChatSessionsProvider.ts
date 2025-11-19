dc72558ec7a12e0d2784a37d83d3ce7a0f08db5c
Benjamin Pasero <benjamin.pasero@gmail.com>
2025-11-19 08:31:03 +0100

    debt - reduce `in` operator (#278280)

--- src/vs/workbench/contrib/chat/browser/chatSessions/localChatSessionsProvider.ts
+++ src/vs/workbench/contrib/chat/browser/chatSessions/localChatSessionsProvider.ts
@@ -16,7 +16,7 @@ import { IChatModel } from '../../common/chatModel.js';
 import { IChatService } from '../../common/chatService.js';
 import { ChatSessionStatus, IChatSessionItem, IChatSessionItemProvider, IChatSessionsService, localChatSessionType } from '../../common/chatSessionsService.js';
 import { ChatAgentLocation } from '../../common/constants.js';
-import { IChatWidget, IChatWidgetService } from '../chat.js';
+import { IChatWidget, IChatWidgetService, isIChatViewViewContext } from '../chat.js';
 import { ChatSessionItemWithProvider } from './common.js';
 
 export class LocalChatSessionsProvider extends Disposable implements IChatSessionItemProvider, IWorkbenchContribution {
@@ -59,8 +59,7 @@ export class LocalChatSessionsProvider extends Disposable implements IChatSessio
 		this._register(this.chatWidgetService.onDidAddWidget(widget => {
 			// Only fire for chat view instance
 			if (widget.location === ChatAgentLocation.Chat &&
-				typeof widget.viewContext === 'object' &&
-				'viewId' in widget.viewContext &&
+				isIChatViewViewContext(widget.viewContext) &&
 				widget.viewContext.viewId === LocalChatSessionsProvider.CHAT_WIDGET_VIEW_ID) {
 				this._onDidChange.fire();
 				this._registerWidgetModelListeners(widget);
@@ -69,7 +68,7 @@ export class LocalChatSessionsProvider extends Disposable implements IChatSessio
 
 		// Check for existing chat widgets and register listeners
 		const existingWidgets = this.chatWidgetService.getWidgetsByLocations(ChatAgentLocation.Chat)
-			.filter(widget => typeof widget.viewContext === 'object' && 'viewId' in widget.viewContext && widget.viewContext.viewId === LocalChatSessionsProvider.CHAT_WIDGET_VIEW_ID);
+			.filter(widget => isIChatViewViewContext(widget.viewContext) && widget.viewContext.viewId === LocalChatSessionsProvider.CHAT_WIDGET_VIEW_ID);
 
 		existingWidgets.forEach(widget => {
 			this._registerWidgetModelListeners(widget);
