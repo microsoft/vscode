a73588288a71f10c60b4b0c1975dd653787a5d16
Ladislau Szomoru <3372902+lszomoru@users.noreply.github.com>
2025-11-21 15:48:43 +0000

    SCM - refactor history item hover for the graph  (#278778)
    * SCM - refactor history item tooltip
    
    * Extract the hover code into a separate file
    
    * SCM - add references into the hover
    
    * Pull request feedback
    
    * Fix compilation errors

--- extensions/git/src/historyProvider.ts
+++ extensions/git/src/historyProvider.ts
@@ -4,29 +4,26 @@
  *--------------------------------------------------------------------------------------------*/
 
 
-import { CancellationToken, Disposable, Event, EventEmitter, FileDecoration, FileDecorationProvider, SourceControlHistoryItem, SourceControlHistoryItemChange, SourceControlHistoryOptions, SourceControlHistoryProvider, ThemeIcon, Uri, window, LogOutputChannel, SourceControlHistoryItemRef, l10n, SourceControlHistoryItemRefsChangeEvent, workspace, ConfigurationChangeEvent, MarkdownString, Command, commands } from 'vscode';
+import { CancellationToken, Disposable, Event, EventEmitter, FileDecoration, FileDecorationProvider, SourceControlHistoryItem, SourceControlHistoryItemChange, SourceControlHistoryOptions, SourceControlHistoryProvider, ThemeIcon, Uri, window, LogOutputChannel, SourceControlHistoryItemRef, l10n, SourceControlHistoryItemRefsChangeEvent, workspace, ConfigurationChangeEvent, Command, commands } from 'vscode';
 import { Repository, Resource } from './repository';
-import { IDisposable, deltaHistoryItemRefs, dispose, filterEvent, fromNow, getCommitShortHash, subject, truncate } from './util';
+import { IDisposable, deltaHistoryItemRefs, dispose, filterEvent, subject, truncate } from './util';
 import { toMultiFileDiffEditorUris } from './uri';
 import { AvatarQuery, AvatarQueryCommit, Branch, LogOptions, Ref, RefType } from './api/git';
 import { emojify, ensureEmojis } from './emoji';
-import { Commit, CommitShortStat } from './git';
+import { Commit } from './git';
 import { OperationKind, OperationResult } from './operation';
 import { ISourceControlHistoryItemDetailsProviderRegistry, provideSourceControlHistoryItemAvatar, provideSourceControlHistoryItemHoverCommands, provideSourceControlHistoryItemMessageLinks } from './historyItemDetailsProvider';
 import { throttle } from './decorators';
+import { getHistoryItemHover, getHoverCommitHashCommands, processHoverRemoteCommands } from './hover';
 
-type SourceControlHistoryItemRefWithRenderOptions = SourceControlHistoryItemRef & {
-	backgroundColor?: string;
-};
-
-function compareSourceControlHistoryItemRef(ref1: SourceControlHistoryItemRefWithRenderOptions, ref2: SourceControlHistoryItemRefWithRenderOptions): number {
-	const getOrder = (ref: SourceControlHistoryItemRefWithRenderOptions): number => {
+function compareSourceControlHistoryItemRef(ref1: SourceControlHistoryItemRef, ref2: SourceControlHistoryItemRef): number {
+	const getOrder = (ref: SourceControlHistoryItemRef): number => {
 		if (ref.id.startsWith('refs/heads/')) {
-			return ref.backgroundColor ? 1 : 5;
+			return 1;
 		} else if (ref.id.startsWith('refs/remotes/')) {
-			return ref.backgroundColor ? 2 : 15;
+			return 2;
 		} else if (ref.id.startsWith('refs/tags/')) {
-			return ref.backgroundColor ? 3 : 25;
+			return 3;
 		}
 
 		return 99;
@@ -308,11 +305,11 @@ export class GitHistoryProvider implements SourceControlHistoryProvider, FileDec
 				const references = this._resolveHistoryItemRefs(commit);
 
 				const commands: Command[][] = [
-					getHistoryItemHoverCommitHashCommands(Uri.file(this.repository.root), commit.hash),
-					processHistoryItemRemoteHoverCommands(remoteHoverCommands, commit.hash)
+					getHoverCommitHashCommands(Uri.file(this.repository.root), commit.hash),
+					processHoverRemoteCommands(remoteHoverCommands, commit.hash)
 				];
 
-				const tooltip = getHistoryItemHover(avatarUrl, commit.authorName, commit.authorEmail, commit.authorDate ?? commit.commitDate, messageWithLinks, commit.shortStat, references, commands);
+				const tooltip = getHistoryItemHover(avatarUrl, commit.authorName, commit.authorEmail, commit.authorDate ?? commit.commitDate, messageWithLinks, commit.shortStat, commands);
 
 				historyItems.push({
 					id: commit.hash,
@@ -489,8 +486,8 @@ export class GitHistoryProvider implements SourceControlHistoryProvider, FileDec
 		return this.historyItemDecorations.get(uri.toString());
 	}
 
-	private _resolveHistoryItemRefs(commit: Commit): SourceControlHistoryItemRefWithRenderOptions[] {
-		const references: SourceControlHistoryItemRefWithRenderOptions[] = [];
+	private _resolveHistoryItemRefs(commit: Commit): SourceControlHistoryItemRef[] {
+		const references: SourceControlHistoryItemRef[] = [];
 
 		for (const ref of commit.refNames) {
 			if (ref === 'refs/remotes/origin/HEAD') {
@@ -504,8 +501,7 @@ export class GitHistoryProvider implements SourceControlHistoryProvider, FileDec
 						name: ref.substring('HEAD -> refs/heads/'.length),
 						revision: commit.hash,
 						category: l10n.t('branches'),
-						icon: new ThemeIcon('target'),
-						backgroundColor: `--vscode-scmGraph-historyItemRefColor`
+						icon: new ThemeIcon('target')
 					});
 					break;
 				case ref.startsWith('refs/heads/'):
@@ -523,12 +519,7 @@ export class GitHistoryProvider implements SourceControlHistoryProvider, FileDec
 						name: ref.substring('refs/remotes/'.length),
 						revision: commit.hash,
 						category: l10n.t('remote branches'),
-						icon: new ThemeIcon('cloud'),
-						backgroundColor: ref === this.currentHistoryItemRemoteRef?.id
-							? `--vscode-scmGraph-historyItemRemoteRefColor`
-							: ref === this.currentHistoryItemBaseRef?.id
-								? `--vscode-scmGraph-historyItemBaseRefColor`
-								: undefined
+						icon: new ThemeIcon('cloud')
 					});
 					break;
 				case ref.startsWith('tag: refs/tags/'):
@@ -537,10 +528,7 @@ export class GitHistoryProvider implements SourceControlHistoryProvider, FileDec
 						name: ref.substring('tag: refs/tags/'.length),
 						revision: commit.hash,
 						category: l10n.t('tags'),
-						icon: new ThemeIcon('tag'),
-						backgroundColor: ref === this.currentHistoryItemRef?.id
-							? `--vscode-scmGraph-historyItemRefColor`
-							: undefined
+						icon: new ThemeIcon('tag')
 					});
 					break;
 			}
@@ -620,128 +608,4 @@ export class GitHistoryProvider implements SourceControlHistoryProvider, FileDec
 	dispose(): void {
 		dispose(this.disposables);
 	}
-}
-
-export const AVATAR_SIZE = 20;
-
-export function getHistoryItemHoverCommitHashCommands(documentUri: Uri, hash: string): Command[] {
-	return [{
-		title: `$(git-commit) ${getCommitShortHash(documentUri, hash)}`,
-		tooltip: l10n.t('Open Commit'),
-		command: 'git.viewCommit',
-		arguments: [documentUri, hash, documentUri]
-	}, {
-		title: `$(copy)`,
-		tooltip: l10n.t('Copy Commit Hash'),
-		command: 'git.copyContentToClipboard',
-		arguments: [hash]
-	}] satisfies Command[];
-}
-
-export function processHistoryItemRemoteHoverCommands(commands: Command[], hash: string): Command[] {
-	return commands.map(command => ({
-		...command,
-		arguments: [...command.arguments ?? [], hash]
-	} satisfies Command));
-}
-
-export function getHistoryItemHover(authorAvatar: string | undefined, authorName: string | undefined, authorEmail: string | undefined, authorDate: Date | number | undefined, message: string, shortStats: CommitShortStat | undefined, references: SourceControlHistoryItemRefWithRenderOptions[] | undefined, commands: Command[][] | undefined): MarkdownString {
-	const markdownString = new MarkdownString('', true);
-	markdownString.isTrusted = {
-		enabledCommands: commands?.flat().map(c => c.command) ?? []
-	};
-
-	// Author
-	if (authorName) {
-		// Avatar
-		if (authorAvatar) {
-			markdownString.appendMarkdown('![');
-			markdownString.appendText(authorName);
-			markdownString.appendMarkdown('](');
-			markdownString.appendText(authorAvatar);
-			markdownString.appendMarkdown(`|width=${AVATAR_SIZE},height=${AVATAR_SIZE})`);
-		} else {
-			markdownString.appendMarkdown('$(account)');
-		}
-
-		// Email
-		if (authorEmail) {
-			markdownString.appendMarkdown(' [**');
-			markdownString.appendText(authorName);
-			markdownString.appendMarkdown('**](mailto:');
-			markdownString.appendText(authorEmail);
-			markdownString.appendMarkdown(')');
-		} else {
-			markdownString.appendMarkdown(' **');
-			markdownString.appendText(authorName);
-			markdownString.appendMarkdown('**');
-		}
-
-		// Date
-		if (authorDate && !isNaN(new Date(authorDate).getTime())) {
-			const dateString = new Date(authorDate).toLocaleString(undefined, {
-				year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric'
-			});
-
-			markdownString.appendMarkdown(', $(history)');
-			markdownString.appendText(` ${fromNow(authorDate, true, true)} (${dateString})`);
-		}
-
-		markdownString.appendMarkdown('\n\n');
-	}
-
-	// Subject | Message (escape image syntax)
-	markdownString.appendMarkdown(`${emojify(message.replace(/!\[/g, '&#33;&#91;').replace(/\r\n|\r|\n/g, '\n\n'))}\n\n`);
-	markdownString.appendMarkdown(`---\n\n`);
-
-	// Short stats
-	if (shortStats) {
-		markdownString.appendMarkdown(`<span>${shortStats.files === 1 ?
-			l10n.t('{0} file changed', shortStats.files) :
-			l10n.t('{0} files changed', shortStats.files)}</span>`);
-
-		if (shortStats.insertions) {
-			markdownString.appendMarkdown(`,&nbsp;<span style="color:var(--vscode-scmGraph-historyItemHoverAdditionsForeground);">${shortStats.insertions === 1 ?
-				l10n.t('{0} insertion{1}', shortStats.insertions, '(+)') :
-				l10n.t('{0} insertions{1}', shortStats.insertions, '(+)')}</span>`);
-		}
-
-		if (shortStats.deletions) {
-			markdownString.appendMarkdown(`,&nbsp;<span style="color:var(--vscode-scmGraph-historyItemHoverDeletionsForeground);">${shortStats.deletions === 1 ?
-				l10n.t('{0} deletion{1}', shortStats.deletions, '(-)') :
-				l10n.t('{0} deletions{1}', shortStats.deletions, '(-)')}</span>`);
-		}
-
-		markdownString.appendMarkdown(`\n\n---\n\n`);
-	}
-
-	// References
-	if (references && references.length > 0) {
-		for (const reference of references) {
-			const labelIconId = reference.icon instanceof ThemeIcon ? reference.icon.id : '';
-			const backgroundColor = `var(${reference.backgroundColor ?? '--vscode-scmGraph-historyItemHoverDefaultLabelBackground'})`;
-			const color = reference.backgroundColor ? `var(--vscode-scmGraph-historyItemHoverLabelForeground)` : `var(--vscode-scmGraph-historyItemHoverDefaultLabelForeground)`;
-
-			markdownString.appendMarkdown(`<span style="color:${color};background-color:${backgroundColor};border-radius:10px;">&nbsp;$(${labelIconId})&nbsp;`);
-			markdownString.appendText(reference.name);
-			markdownString.appendMarkdown(`&nbsp;&nbsp;</span>`);
-		}
-
-		markdownString.appendMarkdown(`\n\n---\n\n`);
-	}
-
-	// Commands
-	if (commands && commands.length > 0) {
-		for (let index = 0; index < commands.length; index++) {
-			if (index !== 0) {
-				markdownString.appendMarkdown('&nbsp;&nbsp;|&nbsp;&nbsp;');
-			}
-
-			const commandsMarkdown = commands[index]
-				.map(command => `[${command.title}](command:${command.command}?${encodeURIComponent(JSON.stringify(command.arguments))} "${command.tooltip}")`);
-			markdownString.appendMarkdown(commandsMarkdown.join('&nbsp;'));
-		}
-	}
-
-	return markdownString;
 }
