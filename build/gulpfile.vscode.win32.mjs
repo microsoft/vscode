4c5bfb172a2d7fe6efe1d1010219f2141bd7f7ab
Robo <hop2deep@gmail.com>
2025-11-24 21:32:12 +0900

    feat: create versioned resources for windows setup (#263998)
    * feat: create versioned resources for windows setup
    
    * chore: use inno_updater to remove old installation
    
    * chore: remove old installation as part of setup
    
    * chore: update explorer-command
    
    * chore: prefer session-end
    
    * chore: uninst delete updating_version
    
    * chore: make session-ending write synchronous
    
    * chore: cleanup updateService.win32.ts
    
    * chore: invoke inno_updater gc path for non background update
    
    * chore: move session-end path to runtime
    
    * chore: use commit for updating_version
    
    * chore: fix invalid string
    
    * chore: set appUpdate path
    
    * chore: update inno_updater
    
    * chore: empty commit for testing
    
    * chore: some cleanups
    
    1) Check for session-ending flag in appx and tunnel callsites
    2) Move gc for background update to cleanup phase in updateservice
    3) Set update state to ready when there is a running inno_setup
    
    * chore: disallow same version update
    
    * chore: disallow application launch in the middle of update
    
    * chore: empty commit for testing
    
    * chore: bump inno_updater
    
    * chore: empty commit for testing
    
    * chore: move gc to update startup
    
    * chore: move feature behind insider only check
    
    * chore: bump inno_updater
    
    * chore: bump explorer-command
    
    * fix: build
    
    * fix: gc for background update in system setup
    
    * chore: create separate cli entrypoints for build
    
    * fix: check for setup mutex created by inno
    
    * chore: remove problematic updatingVersionPath deletion
    
    * chore: remove redundant update check
    
    * chore: bump inno_updater
    
    * chore: fix build
    
    * chore: bump inno updater

--- build/gulpfile.vscode.win32.mjs
+++ build/gulpfile.vscode.win32.mjs
@@ -8,6 +8,7 @@ import * as fs from 'fs';
 import assert from 'assert';
 import * as cp from 'child_process';
 import * as util from './lib/util.ts';
+import * as getVersionModule from './lib/getVersion.ts';
 import * as task from './lib/task.ts';
 import pkg from '../package.json' with { type: 'json' };
 import product from '../product.json' with { type: 'json' };
@@ -15,11 +16,12 @@ import vfs from 'vinyl-fs';
 import rcedit from 'rcedit';
 import { createRequire } from 'module';
 
+const { getVersion } = getVersionModule;
 const require = createRequire(import.meta.url);
 const repoPath = path.dirname(import.meta.dirname);
+const commit = getVersion(repoPath);
 const buildPath = (/** @type {string} */ arch) => path.join(path.dirname(repoPath), `VSCode-win32-${arch}`);
 const setupDir = (/** @type {string} */ arch, /** @type {string} */ target) => path.join(repoPath, '.build', `win32-${arch}`, `${target}-setup`);
-const issPath = path.join(import.meta.dirname, 'win32', 'code.iss');
 const innoSetupPath = path.join(path.dirname(path.dirname(require.resolve('innosetup'))), 'bin', 'ISCC.exe');
 const signWin32Path = path.join(repoPath, 'build', 'azure-pipelines', 'common', 'sign-win32.ts');
 
@@ -75,19 +77,26 @@ function buildWin32Setup(arch, target) {
 		const outputPath = setupDir(arch, target);
 		fs.mkdirSync(outputPath, { recursive: true });
 
-		const originalProductJsonPath = path.join(sourcePath, 'resources/app/product.json');
+		const quality = product.quality || 'dev';
+		let versionedResourcesFolder = '';
+		let issPath = path.join(import.meta.dirname, 'win32', 'code.iss');
+		if (quality && quality === 'insider') {
+			versionedResourcesFolder = commit.substring(0, 10);
+			issPath = path.join(import.meta.dirname, 'win32', 'code-insider.iss');
+		}
+		const originalProductJsonPath = path.join(sourcePath, versionedResourcesFolder, 'resources/app/product.json');
 		const productJsonPath = path.join(outputPath, 'product.json');
 		const productJson = JSON.parse(fs.readFileSync(originalProductJsonPath, 'utf8'));
 		productJson['target'] = target;
 		fs.writeFileSync(productJsonPath, JSON.stringify(productJson, undefined, '\t'));
 
-		const quality = product.quality || 'dev';
 		const definitions = {
 			NameLong: product.nameLong,
 			NameShort: product.nameShort,
 			DirName: product.win32DirName,
 			Version: pkg.version,
 			RawVersion: pkg.version.replace(/-\w+$/, ''),
+			Commit: commit,
 			NameVersion: product.win32NameVersion + (target === 'user' ? ' (User)' : ''),
 			ExeBasename: product.nameShort,
 			RegValueName: product.win32RegValueName,
@@ -108,10 +117,11 @@ function buildWin32Setup(arch, target) {
 			OutputDir: outputPath,
 			InstallTarget: target,
 			ProductJsonPath: productJsonPath,
+			VersionedResourcesFolder: versionedResourcesFolder,
 			Quality: quality
 		};
 
-		if (quality !== 'exploration') {
+		if (quality === 'stable' || quality === 'insider') {
 			definitions['AppxPackage'] = `${quality === 'stable' ? 'code' : 'code_insider'}_${arch}.appx`;
 			definitions['AppxPackageDll'] = `${quality === 'stable' ? 'code' : 'code_insider'}_explorer_command_${arch}.dll`;
 			definitions['AppxPackageName'] = `${product.win32AppUserModelId}`;
