d1f3f773d2afbee69aa8d17a8c0150fd7670865b
Tony Kim <tony@fixdapp.com>
2025-11-25 16:37:57 -0600

    test(extensions): add tests for minRating and minDownloads query parsing

--- src/vs/workbench/contrib/extensions/test/common/extensionQuery.test.ts
+++ src/vs/workbench/contrib/extensions/test/common/extensionQuery.test.ts
@@ -149,4 +149,83 @@ suite('Extension query', () => {
 		Query.suggestions('@category:blah', null).some(x => x === '@category:"extension packs" ');
 		Query.suggestions('@category:"extension packs"', null).every(x => x !== '@category:formatters ');
 	});
+
+	test('parse minRating', () => {
+		let query = Query.parse('@minRating:4');
+		assert.strictEqual(query.value, '');
+		assert.strictEqual(query.minRating, 4);
+
+		query = Query.parse('@minRating:4.5');
+		assert.strictEqual(query.minRating, 4.5);
+
+		query = Query.parse('python @minRating:3');
+		assert.strictEqual(query.value, 'python');
+		assert.strictEqual(query.minRating, 3);
+
+		query = Query.parse('@minRating:6');
+		assert.strictEqual(query.minRating, undefined);
+
+		query = Query.parse('@minRating:invalid');
+		assert.strictEqual(query.minRating, undefined);
+	});
+
+	test('parse minDownloads', () => {
+		let query = Query.parse('@minDownloads:1000');
+		assert.strictEqual(query.value, '');
+		assert.strictEqual(query.minDownloads, 1000);
+
+		query = Query.parse('@minDownloads:10K');
+		assert.strictEqual(query.minDownloads, 10000);
+
+		query = Query.parse('@minDownloads:10k');
+		assert.strictEqual(query.minDownloads, 10000);
+
+		query = Query.parse('@minDownloads:1M');
+		assert.strictEqual(query.minDownloads, 1000000);
+
+		query = Query.parse('react @minDownloads:100K');
+		assert.strictEqual(query.value, 'react');
+		assert.strictEqual(query.minDownloads, 100000);
+	});
+
+	test('parse combined filters', () => {
+		const query = Query.parse('python @minRating:4 @minDownloads:10K @sort:installs');
+		assert.strictEqual(query.value, 'python');
+		assert.strictEqual(query.minRating, 4);
+		assert.strictEqual(query.minDownloads, 10000);
+		assert.strictEqual(query.sortBy, 'installs');
+	});
+
+	test('toString with filters', () => {
+		let query = new Query('hello', '', 4, undefined);
+		assert.strictEqual(query.toString(), 'hello @minRating:4');
+
+		query = new Query('hello', '', undefined, 10000);
+		assert.strictEqual(query.toString(), 'hello @minDownloads:10000');
+
+		query = new Query('hello', 'installs', 4, 10000);
+		assert.strictEqual(query.toString(), 'hello @sort:installs @minRating:4 @minDownloads:10000');
+	});
+
+	test('equals with filters', () => {
+		const query1 = new Query('hello', '', 4, 1000);
+		let query2 = new Query('hello', '', 4, 1000);
+		assert(query1.equals(query2));
+
+		query2 = new Query('hello', '', 3, 1000);
+		assert(!query1.equals(query2));
+
+		query2 = new Query('hello', '', 4, 2000);
+		assert(!query1.equals(query2));
+	});
+
+	test('parseDownloadCount', () => {
+		assert.strictEqual(Query.parseDownloadCount('1000'), 1000);
+		assert.strictEqual(Query.parseDownloadCount('10K'), 10000);
+		assert.strictEqual(Query.parseDownloadCount('10k'), 10000);
+		assert.strictEqual(Query.parseDownloadCount('1M'), 1000000);
+		assert.strictEqual(Query.parseDownloadCount('1m'), 1000000);
+		assert.strictEqual(Query.parseDownloadCount('invalid'), undefined);
+		assert.strictEqual(Query.parseDownloadCount(''), undefined);
+	});
 });
