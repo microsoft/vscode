e65d19da82f4e810e9102650f56a12211feb49c9
Beace <beaceshimin@gmail.com>
2025-11-26 11:40:46 +0000

    fix: fix terminal webgl context memory leak

--- src/vs/workbench/contrib/terminal/browser/xterm/xtermTerminal.ts
+++ src/vs/workbench/contrib/terminal/browser/xterm/xtermTerminal.ts
@@ -966,6 +966,7 @@ export class XtermTerminal extends Disposable implements IXtermTerminal, IDetach
 	override dispose(): void {
 		this._anyTerminalFocusContextKey.reset();
 		this._anyFocusedTerminalHasSelection.reset();
+		this._disposeOfWebglRenderer();
 		this._onDidDispose.fire();
 		super.dispose();
 	}
