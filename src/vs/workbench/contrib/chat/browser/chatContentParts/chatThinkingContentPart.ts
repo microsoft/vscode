2cdc8a78493f3f33146d49b67a1247cc23f29ff5
justschen <justchen@microsoft.com>
2025-12-01 15:17:23 +0800

    single tool call, do not generate llm title

--- src/vs/workbench/contrib/chat/browser/chatContentParts/chatThinkingContentPart.ts
+++ src/vs/workbench/contrib/chat/browser/chatContentParts/chatThinkingContentPart.ts
@@ -54,6 +54,7 @@ export class ChatThinkingContentPart extends ChatCollapsibleContentPart implemen
 	private toolInvocationCount: number = 0;
 	private streamingCompleted: boolean = false;
 	private isActive: boolean = true;
+	private currentToolCallLabel: string | undefined;
 
 	constructor(
 		content: IChatThinkingPart,
@@ -297,6 +298,15 @@ export class ChatThinkingContentPart extends ChatCollapsibleContentPart implemen
 			return;
 		}
 
+		// case where we only have one dropdown in the thinking container
+		if (this.toolInvocationCount === 1 && this.extractedTitles.length === 1 && this.currentToolCallLabel) {
+			const title = this.currentToolCallLabel;
+			this.currentTitle = title;
+			this.content.generatedTitle = title;
+			this.setTitleWithWidgets(new MarkdownString(title), this.instantiationService, this.chatMarkdownAnchorService, this.chatContentMarkdownRenderer);
+			return;
+		}
+
 		// if exactly one actual extracted title and no tool invocations, use that as the final title.
 		if (this.extractedTitles.length === 1 && this.toolInvocationCount === 0) {
 			const title = this.extractedTitles[0];
@@ -396,6 +406,10 @@ export class ChatThinkingContentPart extends ChatCollapsibleContentPart implemen
 				toolCallLabel = message;
 			} else {
 				toolCallLabel = `Invoked \`${toolInvocationId}\``;
+			}
+
+			if (toolInvocation?.pastTenseMessage) {
+				this.currentToolCallLabel = typeof toolInvocation.pastTenseMessage === 'string' ? toolInvocation.pastTenseMessage : toolInvocation.pastTenseMessage.value;
 			}
 
 			// Add tool call to extracted titles for LLM title generation
