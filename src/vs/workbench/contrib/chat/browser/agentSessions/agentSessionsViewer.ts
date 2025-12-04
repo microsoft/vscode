ad9bc4c0ab0cc7ed11d27bbc47e32f334610d8be
Benjamin Pasero <benjamin.pasero@microsoft.com>
2025-12-04 17:20:02 +0100

    agent sessions - allow to show all sessions

--- src/vs/workbench/contrib/chat/browser/agentSessions/agentSessionsViewer.ts
+++ src/vs/workbench/contrib/chat/browser/agentSessions/agentSessionsViewer.ts
@@ -327,7 +327,7 @@ export interface IAgentSessionsFilter {
 	/**
 	 * Optional limit on the number of sessions to show.
 	 */
-	readonly limitResults?: number;
+	readonly limitResults?: () => number | undefined;
 
 	/**
 	 * A callback to notify the filter about the number of
@@ -358,9 +358,10 @@ export class AgentSessionsDataSource implements IAsyncDataSource<IAgentSessionsM
 		let filteredSessions = element.sessions.filter(session => !this.filter?.exclude?.(session));
 
 		// Apply limiter if configured (requires sorting)
-		if (this.filter?.limitResults !== undefined) {
+		const limitResultsCount = this.filter?.limitResults?.();
+		if (typeof limitResultsCount === 'number') {
 			filteredSessions.sort(this.sorter.compare.bind(this.sorter));
-			filteredSessions = filteredSessions.slice(0, this.filter.limitResults);
+			filteredSessions = filteredSessions.slice(0, limitResultsCount);
 		}
 
 		// Callback results count
