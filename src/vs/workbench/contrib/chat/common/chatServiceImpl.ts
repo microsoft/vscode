710e7ddcc4235712a4453d38dcb98eda56d2c716
Osvaldo Ortega <osortega@microsoft.com>
2025-12-09 15:54:18 -0800

    Delte extra complete call

--- src/vs/workbench/contrib/chat/common/chatServiceImpl.ts
+++ src/vs/workbench/contrib/chat/common/chatServiceImpl.ts
@@ -693,8 +693,6 @@ export class ChatService extends Disposable implements IChatService {
 					for (const part of message.parts) {
 						model.acceptResponseProgress(lastRequest, part);
 					}
-
-					lastRequest.response?.completeResponseIfNeeded();
 				}
 			}
 		}
