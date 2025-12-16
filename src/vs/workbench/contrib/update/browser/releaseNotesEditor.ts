d99c6e4349f49387d76d0fcb78666b3647366651
mrleemurray <mrleemurray@users.noreply.github.com>
2025-12-16 12:36:27 +0000

    Improve code styling for text preformat in release notes editor

--- src/vs/workbench/contrib/update/browser/releaseNotesEditor.ts
+++ src/vs/workbench/contrib/update/browser/releaseNotesEditor.ts
@@ -287,7 +287,10 @@ export class ReleaseNotesManager extends Disposable {
 					code:has(.codesetting) {
 						background-color: var(--vscode-textPreformat-background);
 						color: var(--vscode-textPreformat-foreground);
-					border: 1px solid var(--vscode-textPreformat-border);
+						border: 1px solid var(--vscode-textPreformat-border);
+						padding-left: 1px;
+						margin-right: 3px;
+						padding-right: 0px;
 					}
 					code:has(.codesetting):focus {
 						border: 1px solid var(--vscode-button-border, transparent);
