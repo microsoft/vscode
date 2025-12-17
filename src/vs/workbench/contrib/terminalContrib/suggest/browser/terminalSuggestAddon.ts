67f18f167f3b21708fbd2c35a7cce20670c59b12
meganrogge <merogge@microsoft.com>
2025-12-17 16:46:31 -0600

    fixes #258694

--- src/vs/workbench/contrib/terminalContrib/suggest/browser/terminalSuggestAddon.ts
+++ src/vs/workbench/contrib/terminalContrib/suggest/browser/terminalSuggestAddon.ts
@@ -257,6 +257,7 @@ export class SuggestAddon extends Disposable implements ITerminalAddon, ISuggest
 			this._lastUserDataTimestamp = Date.now();
 		}));
 		this._register(xterm.onScroll(() => this.hideSuggestWidget(true)));
+		this._register(xterm.onResize(() => this._relayoutOnResize()));
 	}
 
 	private async _handleCompletionProviders(terminal: Terminal | undefined, token: CancellationToken, explicitlyInvoked?: boolean): Promise<void> {
@@ -1041,6 +1042,18 @@ export class SuggestAddon extends Disposable implements ITerminalAddon, ISuggest
 		this._focusedItem = undefined;
 		this._suggestWidget?.hide();
 	}
+
+	private _relayoutOnResize(): void {
+		if (!this._terminalSuggestWidgetVisibleContextKey.get() || !this._terminal) {
+			return;
+		}
+		const cursorPosition = this._getCursorPosition(this._terminal);
+		if (!cursorPosition) {
+			this.hideSuggestWidget(true);
+			return;
+		}
+		this._suggestWidget?.relayout(cursorPosition);
+	}
 }
 
 class PersistedWidgetSize {
@@ -1080,4 +1093,3 @@ export function normalizePathSeparator(path: string, sep: string): string {
 	}
 	return path.replaceAll('/', '\\');
 }
-
