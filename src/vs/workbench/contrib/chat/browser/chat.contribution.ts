a8f871a3af2ceff97c87d82f8f2b0193c91640fa
Rob Lourens <roblourens@gmail.com>
2025-12-17 10:57:22 -0800

    Remove chatAgentMaxRequestsLimit experiment (#284120)
    Fix microsoft/vscode#284072

--- src/vs/workbench/contrib/chat/browser/chat.contribution.ts
+++ src/vs/workbench/contrib/chat/browser/chat.contribution.ts
@@ -946,10 +946,7 @@ class ChatAgentSettingContribution extends Disposable implements IWorkbenchContr
 			const treatmentId = this.entitlementService.entitlement === ChatEntitlement.Free ?
 				'chatAgentMaxRequestsFree' :
 				'chatAgentMaxRequestsPro';
-			Promise.all([
-				this.experimentService.getTreatment<number>(treatmentId),
-				this.experimentService.getTreatment<number>('chatAgentMaxRequestsLimit')
-			]).then(([value, maxLimit]) => {
+			this.experimentService.getTreatment<number>(treatmentId).then((value) => {
 				const defaultValue = value ?? (this.entitlementService.entitlement === ChatEntitlement.Free ? 25 : 25);
 				const node: IConfigurationNode = {
 					id: 'chatSidebar',
@@ -960,7 +957,6 @@ class ChatAgentSettingContribution extends Disposable implements IWorkbenchContr
 							type: 'number',
 							markdownDescription: nls.localize('chat.agent.maxRequests', "The maximum number of requests to allow per-turn when using an agent. When the limit is reached, will ask to confirm to continue."),
 							default: defaultValue,
-							maximum: maxLimit,
 						},
 					}
 				};
