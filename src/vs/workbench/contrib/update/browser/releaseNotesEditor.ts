df339b1d5b2418e3b89b21da6993136a5b6a7bee
mrleemurray <mrleemurray@users.noreply.github.com>
2025-12-17 09:44:58 +0000

    style: comment out border property in various CSS files for consistency

--- src/vs/workbench/contrib/update/browser/releaseNotesEditor.ts
+++ src/vs/workbench/contrib/update/browser/releaseNotesEditor.ts
@@ -287,7 +287,7 @@ export class ReleaseNotesManager extends Disposable {
 					code:has(.codesetting) {
 						background-color: var(--vscode-textPreformat-background);
 						color: var(--vscode-textPreformat-foreground);
-						border: 1px solid var(--vscode-textPreformat-border);
+						/* border: 1px solid var(--vscode-textPreformat-border); */
 						padding-left: 1px;
 						margin-right: 3px;
 						padding-right: 0px;
