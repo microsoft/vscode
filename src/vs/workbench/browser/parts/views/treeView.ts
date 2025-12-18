b24735517f031df1aaaa6ff78a0a6813e190115a
gjsjohnmurray <johnm@georgejames.com>
2025-12-18 13:04:55 +0000

    Limit where symbol-* codicons are colored

--- src/vs/workbench/browser/parts/views/treeView.ts
+++ src/vs/workbench/browser/parts/views/treeView.ts
@@ -1474,7 +1474,8 @@ class TreeRenderer extends Disposable implements ITreeRenderer<ITreeItem, FuzzyS
 					templateData.icon.style.color = this.themeService.getColorTheme().getColor(node.themeIcon.color.id)?.toString() ?? '';
 				}
 			}
-			templateData.icon.className = iconClass ? `custom-view-tree-node-item-icon ${iconClass}` : '';
+			// Allow symbol codicons to show their colors in custom tree views
+			templateData.icon.className = iconClass ? `custom-view-tree-node-item-icon codicon-colored ${iconClass}` : '';
 			templateData.icon.style.backgroundImage = '';
 		}
 
