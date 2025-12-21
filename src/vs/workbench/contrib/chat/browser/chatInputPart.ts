b5ef2e0fc00c1f4c36be41fe32f50a779bd34137
Justin Chen <54879025+justschen@users.noreply.github.com>
2025-12-21 19:52:26 +0800

    make selections pinnable as implicit context (#284657)
    * make selections pinnable as implicit context
    
    * extra edit not needed
    
    * reduce duplicated code

--- src/vs/workbench/contrib/chat/browser/chatInputPart.ts
+++ src/vs/workbench/contrib/chat/browser/chatInputPart.ts
@@ -38,6 +38,7 @@ import { CodeEditorWidget } from '../../../../editor/browser/widget/codeEditor/c
 import { EditorOptions, IEditorOptions } from '../../../../editor/common/config/editorOptions.js';
 import { IDimension } from '../../../../editor/common/core/2d/dimension.js';
 import { IPosition } from '../../../../editor/common/core/position.js';
+import { IRange, Range } from '../../../../editor/common/core/range.js';
 import { isLocation } from '../../../../editor/common/languages.js';
 import { ITextModel } from '../../../../editor/common/model.js';
 import { IModelService } from '../../../../editor/common/services/model.js';
@@ -1859,18 +1860,9 @@ export class ChatInputPart extends Disposable implements IHistoryNavigationWidge
 
 		if (isSuggestedEnabled && implicitValue) {
 			const targetUri: URI | undefined = this.implicitContext.uri;
-
-			const currentlyAttached = attachments.some(([, attachment]) => {
-				let uri: URI | undefined;
-				if (URI.isUri(attachment.value)) {
-					uri = attachment.value;
-				} else if (isStringVariableEntry(attachment)) {
-					uri = attachment.uri;
-				}
-				return uri && isEqual(uri, targetUri);
-			});
-
-			const shouldShowImplicit = !isLocation(implicitValue) ? !currentlyAttached : implicitValue.range;
+			const targetRange = isLocation(implicitValue) ? implicitValue.range : undefined;
+			const currentlyAttached = this.isAttachmentAlreadyAttached(targetUri, targetRange, attachments.map(([, a]) => a));
+			const shouldShowImplicit = !currentlyAttached;
 			if (shouldShowImplicit) {
 				const implicitPart = store.add(this.instantiationService.createInstance(ImplicitContextAttachmentWidget, () => this._widget, this.implicitContext, this._contextResourceLabels, this._attachmentModel));
 				container.appendChild(implicitPart.domNode);
@@ -1896,16 +1888,42 @@ export class ChatInputPart extends Disposable implements IHistoryNavigationWidge
 			return true;
 		}
 
-		// TODO @justschen: merge this with above showing implicit logic
 		const isUri = URI.isUri(implicit);
 		if (isUri || isLocation(implicit)) {
 			const targetUri = isUri ? implicit : implicit.uri;
-			const attachments = [...this._attachmentModel.attachments.entries()];
-			const currentlyAttached = attachments.some(([, a]) => URI.isUri(a.value) && isEqual(a.value, targetUri));
-			const shouldShowImplicit = isUri ? !currentlyAttached : implicit.range;
-			return !!shouldShowImplicit;
+			const targetRange = isLocation(implicit) ? implicit.range : undefined;
+			const attachments = [...this._attachmentModel.attachments.values()];
+			const currentlyAttached = this.isAttachmentAlreadyAttached(targetUri, targetRange, attachments);
+			return !currentlyAttached;
 		}
 		return false;
+	}
+
+	private isAttachmentAlreadyAttached(targetUri: URI | undefined, targetRange: IRange | undefined, attachments: IChatRequestVariableEntry[]): boolean {
+		return attachments.some((attachment) => {
+			let uri: URI | undefined;
+			let range: IRange | undefined;
+
+			if (URI.isUri(attachment.value)) {
+				uri = attachment.value;
+			} else if (isLocation(attachment.value)) {
+				uri = attachment.value.uri;
+				range = attachment.value.range;
+			} else if (isStringVariableEntry(attachment)) {
+				uri = attachment.uri;
+			}
+
+			if (!uri || !isEqual(uri, targetUri)) {
+				return false;
+			}
+
+			// check if the exact range is already attached
+			if (targetRange) {
+				return range && Range.equalsRange(range, targetRange);
+			}
+
+			return true;
+		});
 	}
 
 	private handleAttachmentDeletion(e: KeyboardEvent | unknown, index: number, attachment: IChatRequestVariableEntry) {
