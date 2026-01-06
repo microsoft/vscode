3c3b57dea8cf93ec8d87fcf2aa2996fcf6c977bc
Aiday Marlen Kyzy <amarlenkyzy@microsoft.com>
2026-01-06 10:54:06 +0100

    changing the api

--- src/vs/editor/common/languages.ts
+++ src/vs/editor/common/languages.ts
@@ -72,8 +72,8 @@ export interface IFontToken {
 	readonly startIndex: number;
 	readonly endIndex: number;
 	readonly fontFamily: string | null;
-	readonly fontSize: number | null;
-	readonly lineHeight: number | null;
+	readonly fontSizeMultiplier: number | null;
+	readonly lineHeightMultiplier: number | null;
 }
 
 /**
