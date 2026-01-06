e378241e4f7a3d4900067a7539637e35cddc3037
Anthony Kim <anthonykim@microsoft.com>
2026-01-06 14:54:24 -0800

    check for 1 or 127 for command not found

--- src/vs/workbench/contrib/tasks/browser/abstractTaskService.ts
+++ src/vs/workbench/contrib/tasks/browser/abstractTaskService.ts
@@ -2191,9 +2191,9 @@ export abstract class AbstractTaskService extends Disposable implements ITaskSer
 				await this.run(updatedTask);
 			} else {
 				const success = await this.run(task);
-				if (success?.exitCode === 1) {
+				if (success?.exitCode === 1 || success?.exitCode === 127) {
 					// Task no longer exists, show warning
-					this._notificationService.warn(nls.localize('TaskSystem.taskNoLongerExists', 'Task {0} no longer exists or has been really modified. Cannot restart.', task.configurationProperties.name));
+					this._notificationService.warn(nls.localize('TaskSystem.taskNoLongerExists', 'Task {0} no longer exists or has been modified. Cannot restart.', task.configurationProperties.name));
 				}
 			}
 		} catch {
