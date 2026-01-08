7dc75dee87a7ddb8ef85bd2c11405a640ee3295c
Daniel Imms <2193314+Tyriar@users.noreply.github.com>
2026-01-08 11:56:27 -0800

    Make enforceTimeoutModel restricted

--- src/vs/workbench/contrib/terminalContrib/chatAgentTools/common/terminalChatAgentToolsConfiguration.ts
+++ src/vs/workbench/contrib/terminalContrib/chatAgentTools/common/terminalChatAgentToolsConfiguration.ts
@@ -473,6 +473,7 @@ export const terminalChatAgentToolsConfiguration: IStringDictionary<IConfigurati
 		].join('\n'),
 	},
 	[TerminalChatAgentToolsSettingId.EnforceTimeoutFromModel]: {
+		restricted: true,
 		type: 'boolean',
 		default: false,
 		tags: ['experimental'],
