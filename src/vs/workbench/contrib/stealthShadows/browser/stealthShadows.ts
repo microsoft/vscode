db19df42a0e4817bcb999b96edd8c81a23a4075a
eli-w-king <elijahwilliamking@icloud.com>
2026-01-13 20:24:47 -0800

    added workbench.stealthShadows.enabled

---
+++ src/vs/workbench/contrib/stealthShadows/browser/stealthShadows.ts
@@ -0,0 +1,34 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { Disposable } from '../../../../base/common/lifecycle.js';
+import { IConfigurationService } from '../../../../platform/configuration/common/configuration.js';
+import { IWorkbenchContribution } from '../../../common/contributions.js';
+import { ILayoutService } from '../../../../platform/layout/browser/layoutService.js';
+
+export class StealthShadowsContribution extends Disposable implements IWorkbenchContribution {
+
+	static readonly ID = 'workbench.contrib.stealthShadows';
+
+	constructor(
+		@IConfigurationService private readonly configurationService: IConfigurationService,
+		@ILayoutService private readonly layoutService: ILayoutService
+	) {
+		super();
+
+		this.updateStealthShadows();
+
+		this._register(this.configurationService.onDidChangeConfiguration(e => {
+			if (e.affectsConfiguration('workbench.stealthShadows.enabled')) {
+				this.updateStealthShadows();
+			}
+		}));
+	}
+
+	private updateStealthShadows(): void {
+		const enabled = this.configurationService.getValue<boolean>('workbench.stealthShadows.enabled') ?? false;
+		this.layoutService.mainContainer.classList.toggle('stealth-shadows-enabled', enabled);
+	}
+}
