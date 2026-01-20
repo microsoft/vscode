56f4f934c8aec9cf0390ce3740524481c435afc7
meganrogge <merogge@microsoft.com>
2026-01-20 11:52:56 -0500

    rm description

--- src/vs/workbench/api/common/extHostLanguageModels.ts
+++ src/vs/workbench/api/common/extHostLanguageModels.ts
@@ -215,7 +215,6 @@ export class ExtHostLanguageModels implements ExtHostLanguageModelsShape {
 					family: m.family ?? '',
 					detail: m.detail,
 					tooltip: m.tooltip,
-					description: m.description,
 					version: m.version,
 					maxInputTokens: m.maxInputTokens,
 					maxOutputTokens: m.maxOutputTokens,
