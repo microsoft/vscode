ecfa144b812f084f8b4eb9c5d977db86fedf8006
Logan Ramos <loganramos@microsoft.com>
2026-01-21 11:50:11 -0500

    Cleanup old context widget

--- src/vs/workbench/contrib/chat/browser/widget/input/chatInputPart.ts
+++ src/vs/workbench/contrib/chat/browser/widget/input/chatInputPart.ts
@@ -119,7 +119,6 @@ import { DelegationSessionPickerActionItem } from './delegationSessionPickerActi
 import { IChatInputPickerOptions } from './chatInputPickerActionItem.js';
 import { SearchableOptionPickerActionItem } from '../../chatSessions/searchableOptionPickerActionItem.js';
 import { mixin } from '../../../../../../base/common/objects.js';
-import { ChatContextUsageWidget } from './chatContextUsageWidget.js';
 
 const $ = dom.$;
 
@@ -269,7 +268,6 @@ export class ChatInputPart extends Disposable implements IHistoryNavigationWidge
 	private chatInputTodoListWidgetContainer!: HTMLElement;
 	private chatInputWidgetsContainer!: HTMLElement;
 	private readonly _widgetController = this._register(new MutableDisposable<ChatInputPartWidgetController>());
-	private readonly _contextUsageWidget = this._register(new MutableDisposable<ChatContextUsageWidget>());
 
 	readonly height = observableValue<number>(this, 0);
 
@@ -1623,7 +1621,6 @@ export class ChatInputPart extends Disposable implements IHistoryNavigationWidge
 			this.updateAgentSessionTypeContextKey();
 			this.refreshChatSessionPickers();
 			this.tryUpdateWidgetController();
-			this._contextUsageWidget.value?.setModel(widget.viewModel?.model);
 		}));
 
 		let elements;
@@ -1688,15 +1685,6 @@ export class ChatInputPart extends Disposable implements IHistoryNavigationWidge
 		this.chatEditingSessionWidgetContainer = elements.chatEditingSessionWidgetContainer;
 		this.chatInputTodoListWidgetContainer = elements.chatInputTodoListWidgetContainer;
 		this.chatInputWidgetsContainer = elements.chatInputWidgetsContainer;
-
-		const isInline = isIChatResourceViewContext(widget.viewContext) && widget.viewContext.isInlineChat;
-		if (this.location !== ChatAgentLocation.EditorInline && !isInline) {
-			this._contextUsageWidget.value = this.instantiationService.createInstance(ChatContextUsageWidget);
-			elements.editorContainer.appendChild(this._contextUsageWidget.value.domNode);
-			if (this._widget?.viewModel) {
-				this._contextUsageWidget.value.setModel(this._widget.viewModel.model);
-			}
-		}
 
 		if (this.options.enableImplicitContext && !this._implicitContext) {
 			this._implicitContext = this._register(
