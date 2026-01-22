997d8b3f3f5f06689a4115d723afa8cb49b471c7
Aiday Marlen Kyzy <amarlenkyzy@microsoft.com>
2026-01-22 13:17:12 +0100

    removing code with unknown origin

--- src/vs/editor/common/viewLayout/lineHeights.ts
+++ src/vs/editor/common/viewLayout/lineHeights.ts
@@ -4,7 +4,6 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { binarySearch2 } from '../../../base/common/arrays.js';
-import { intersection } from '../../../base/common/collections.js';
 
 export class CustomLine {
 
@@ -243,45 +242,9 @@ export class LineHeightsManager {
 		} else {
 			startIndexOfInsertion = -(candidateStartIndexOfInsertion + 1);
 		}
-		const toReAdd: ICustomLineHeightData[] = [];
-		const decorationsImmediatelyAfter = new Set<string>();
-		for (let i = startIndexOfInsertion; i < this._orderedCustomLines.length; i++) {
-			if (this._orderedCustomLines[i].lineNumber === fromLineNumber) {
-				decorationsImmediatelyAfter.add(this._orderedCustomLines[i].decorationId);
-			}
-		}
-		const decorationsImmediatelyBefore = new Set<string>();
-		for (let i = startIndexOfInsertion - 1; i >= 0; i--) {
-			if (this._orderedCustomLines[i].lineNumber === fromLineNumber - 1) {
-				decorationsImmediatelyBefore.add(this._orderedCustomLines[i].decorationId);
-			}
-		}
-		const decorationsWithGaps = intersection(decorationsImmediatelyBefore, decorationsImmediatelyAfter);
 		for (let i = startIndexOfInsertion; i < this._orderedCustomLines.length; i++) {
 			this._orderedCustomLines[i].lineNumber += insertCount;
 			this._orderedCustomLines[i].prefixSum += this._defaultLineHeight * insertCount;
-		}
-
-		if (decorationsWithGaps.size > 0) {
-			for (const decorationId of decorationsWithGaps) {
-				const decoration = this._decorationIDToCustomLine.get(decorationId);
-				if (decoration) {
-					const startLineNumber = decoration.reduce((min, l) => Math.min(min, l.lineNumber), fromLineNumber); // min
-					const endLineNumber = decoration.reduce((max, l) => Math.max(max, l.lineNumber), fromLineNumber); // max
-					const lineHeight = decoration.reduce((max, l) => Math.max(max, l.specialHeight), 0);
-					toReAdd.push({
-						decorationId,
-						startLineNumber,
-						endLineNumber,
-						lineHeight
-					});
-				}
-			}
-
-			for (const dec of toReAdd) {
-				this.insertOrChangeCustomLineHeight(dec.decorationId, dec.startLineNumber, dec.endLineNumber, dec.lineHeight);
-			}
-			this.commit();
 		}
 	}
 
