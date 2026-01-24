74564df37a7c78790989c362fcffe3aca00bda08
Dmitriy Vasyura <dmitriv@microsoft.com>
2026-01-23 22:45:18 -0800

    Back to original version

--- test/sanity/scripts/run-docker.sh
+++ test/sanity/scripts/run-docker.sh
@@ -4,7 +4,7 @@ set -e
 CONTAINER=""
 ARCH="amd64"
 BASE_IMAGE=""
-CACHE_DIR=""
+CACHE_FROM=""
 ARGS=""
 
 while [ $# -gt 0 ]; do
@@ -12,7 +12,7 @@ while [ $# -gt 0 ]; do
 		--container) CONTAINER="$2"; shift 2 ;;
 		--arch) ARCH="$2"; shift 2 ;;
 		--base-image) BASE_IMAGE="$2"; shift 2 ;;
-		--cache-dir) CACHE_DIR="$2"; shift 2 ;;
+		--cache-from) CACHE_FROM="$2"; shift 2 ;;
 		*) ARGS="$ARGS $1"; shift ;;
 	esac
 done
@@ -25,25 +25,11 @@ fi
 SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
 ROOT_DIR=$(cd "$SCRIPT_DIR/.." && pwd)
 
-# Create a buildx builder with docker-container driver if cache is enabled
-# (required for --cache-to support)
-if [ -n "$CACHE_DIR" ]; then
-	BUILDER_NAME="vscode-sanity"
-	if ! docker buildx inspect "$BUILDER_NAME" > /dev/null 2>&1; then
-		echo "Creating buildx builder: $BUILDER_NAME"
-		docker buildx create --name "$BUILDER_NAME" --driver docker-container --use
-	else
-		docker buildx use "$BUILDER_NAME"
-	fi
-fi
-
 echo "Building container image: $CONTAINER"
 docker buildx build \
 	--platform "linux/$ARCH" \
 	${BASE_IMAGE:+--build-arg "BASE_IMAGE=$BASE_IMAGE"} \
-	${CACHE_DIR:+--cache-from "type=local,src=$CACHE_DIR"} \
-	${CACHE_DIR:+--cache-to "type=local,dest=$CACHE_DIR,mode=max"} \
-	--load \
+	${CACHE_FROM:+--cache-from "type=docker,ref=$CACHE_FROM"} \
 	--tag "$CONTAINER" \
 	--file "$ROOT_DIR/containers/$CONTAINER.dockerfile" \
 	"$ROOT_DIR/containers"
