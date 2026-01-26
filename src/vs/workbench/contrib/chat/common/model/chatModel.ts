14ce95527ea56260ef3d01177c39a58608d1988f
Dileep Yavanmandha <dileepy@microsoft.com>
2026-01-26 11:38:31 -0800

    remove sandboxwrapped property

--- src/vs/workbench/contrib/chat/common/model/chatModel.ts
+++ src/vs/workbench/contrib/chat/common/model/chatModel.ts
@@ -546,7 +546,7 @@ class AbstractResponse implements IResponse {
 			if (toolInvocation.toolSpecificData.kind === 'terminal') {
 				message = 'Ran terminal command';
 				const terminalData = migrateLegacyTerminalToolSpecificData(toolInvocation.toolSpecificData);
-				input = terminalData.commandLine.sandboxWrapped ?? terminalData.commandLine.userEdited ?? terminalData.commandLine.toolEdited ?? terminalData.commandLine.original;
+				input = terminalData.commandLine.userEdited ?? terminalData.commandLine.toolEdited ?? terminalData.commandLine.original;
 			}
 		}
 
