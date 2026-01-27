1cab63855cb7d36a311fc7d0cd8599fe0a86b96c
Dmitriy Vasyura <dmitriv@microsoft.com>
2026-01-26 21:40:15 -0800

    Ensure WSL is installed and ready in Windows test script

--- test/sanity/scripts/run-win32.cmd
+++ test/sanity/scripts/run-win32.cmd
@@ -1,6 +1,12 @@
 @echo off
 setlocal
 
+echo Ensuring WSL with Ubuntu is installed
+wsl --install -d Ubuntu --no-launch
+
+echo Starting WSL
+wsl -d Ubuntu echo WSL is ready
+
 set PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD=1
 set PLAYWRIGHT_CHROMIUM_EXECUTABLE_PATH=C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe
 
