13c699615185499d6bb05bceddc87a64bf5d79c7
meganrogge <merogge@microsoft.com>
2026-02-10 12:11:23 -0600

    z -> h

--- src/vs/workbench/contrib/chat/browser/actions/chatActions.ts
+++ src/vs/workbench/contrib/chat/browser/actions/chatActions.ts
@@ -793,7 +793,7 @@ export function registerChatActions() {
 				precondition: ChatContextKeys.inChatSession,
 				keybinding: [{
 					weight: KeybindingWeight.WorkbenchContrib,
-					primary: KeyMod.CtrlCmd | KeyMod.Shift | KeyCode.KeyQ,
+					primary: KeyMod.CtrlCmd | KeyMod.Shift | KeyCode.KeyH,
 					when: ChatContextKeys.inChatSession,
 				}]
 			});
