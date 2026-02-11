dcba11921bea506ff3efa5efb630299d3920174e
Logan Ramos <loganramos@microsoft.com>
2026-02-11 13:42:12 -0500

    Remove some legacy fallbacks

--- src/vs/workbench/contrib/chat/browser/widget/input/chatInputPart.ts
+++ src/vs/workbench/contrib/chat/browser/widget/input/chatInputPart.ts
@@ -665,7 +665,7 @@ export class ChatInputPart extends Disposable implements IHistoryNavigationWidge
 
 	private initSelectedModel() {
 		const persistedSelection = this.storageService.get(this.getSelectedModelStorageKey(), StorageScope.APPLICATION);
-		const persistedAsDefault = this.storageService.getBoolean(this.getSelectedModelIsDefaultStorageKey(), StorageScope.APPLICATION, persistedSelection === 'copilot/gpt-4.1');
+		const persistedAsDefault = this.storageService.getBoolean(this.getSelectedModelIsDefaultStorageKey(), StorageScope.APPLICATION, true);
 
 		if (persistedSelection) {
 			const model = this.getModels().find(m => m.identifier === persistedSelection);
