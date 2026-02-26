1099ad9e3c8f5dac084aea8cc41f227f7f8efbc9
Sandeep Somavarapu <sasomava@microsoft.com>
2026-02-26 16:09:04 +0100

    fix compilation

--- src/vs/editor/standalone/browser/standaloneServices.ts
+++ src/vs/editor/standalone/browser/standaloneServices.ts
@@ -245,6 +245,7 @@ class StandaloneEnvironmentService implements IEnvironmentService {
 	readonly disableTelemetry: boolean = false;
 	readonly serviceMachineIdResource: URI = URI.from({ scheme: 'monaco', authority: 'serviceMachineIdResource' });
 	readonly policyFile?: URI | undefined = undefined;
+	readonly agentSessionsWorkspace: URI = URI.from({ scheme: 'monaco', authority: 'agentSessionsWorkspace' });
 }
 
 class StandaloneDialogService implements IDialogService {
