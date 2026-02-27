cf1ca1b4fd4aeac6455fdaa7662117b0569c832f
Sandeep Somavarapu <sasomava@microsoft.com>
2026-02-27 01:44:48 +0100

    add collapse all action

--- src/vs/sessions/contrib/files/browser/files.contribution.ts
+++ src/vs/sessions/contrib/files/browser/files.contribution.ts
@@ -5,7 +5,10 @@
 
 import { Codicon } from '../../../../base/common/codicons.js';
 import { localize2 } from '../../../../nls.js';
+import { Action2, MenuId, registerAction2 } from '../../../../platform/actions/common/actions.js';
+import { ContextKeyExpr } from '../../../../platform/contextkey/common/contextkey.js';
 import { SyncDescriptor } from '../../../../platform/instantiation/common/descriptors.js';
+import { ServicesAccessor } from '../../../../platform/instantiation/common/instantiation.js';
 import { Registry } from '../../../../platform/registry/common/platform.js';
 import { registerIcon } from '../../../../platform/theme/common/iconRegistry.js';
 import { IWorkbenchContribution, registerWorkbenchContribution2, WorkbenchPhase } from '../../../../workbench/common/contributions.js';
@@ -13,8 +16,10 @@ import { IViewContainersRegistry, IViewsRegistry, ViewContainerLocation, Extensi
 import { WorkspaceFolderCountContext } from '../../../../workbench/common/contextkeys.js';
 import { ExplorerView } from '../../../../workbench/contrib/files/browser/views/explorerView.js';
 import { ViewPaneContainer } from '../../../../workbench/browser/parts/views/viewPaneContainer.js';
+import { IViewsService } from '../../../../workbench/services/views/common/viewsService.js';
 
 const SESSIONS_FILES_CONTAINER_ID = 'workbench.sessions.auxiliaryBar.filesContainer';
+const SESSIONS_FILES_VIEW_ID = 'sessions.files.explorer';
 
 const filesViewIcon = registerIcon('sessions-files-view-icon', Codicon.files, localize2('sessionsFilesViewIcon', 'View icon of the files view in the sessions window.').value);
 
@@ -40,7 +45,7 @@ class RegisterFilesViewContribution implements IWorkbenchContribution {
 
 		// Re-register the explorer view inside the new Files container
 		viewsRegistry.registerViews([{
-			id: 'sessions.files.explorer',
+			id: SESSIONS_FILES_VIEW_ID,
 			name: localize2('files', "Files"),
 			containerIcon: filesViewIcon,
 			ctorDescriptor: new SyncDescriptor(ExplorerView),
@@ -53,3 +58,26 @@ class RegisterFilesViewContribution implements IWorkbenchContribution {
 }
 
 registerWorkbenchContribution2(RegisterFilesViewContribution.ID, RegisterFilesViewContribution, WorkbenchPhase.AfterRestored);
+
+registerAction2(class extends Action2 {
+	constructor() {
+		super({
+			id: 'sessions.files.action.collapseExplorerFolders',
+			title: localize2('collapseExplorerFolders', "Collapse Folders in Explorer"),
+			icon: Codicon.collapseAll,
+			menu: {
+				id: MenuId.ViewTitle,
+				group: 'navigation',
+				when: ContextKeyExpr.equals('view', SESSIONS_FILES_VIEW_ID),
+			},
+		});
+	}
+
+	run(accessor: ServicesAccessor) {
+		const viewsService = accessor.get(IViewsService);
+		const view = viewsService.getViewWithId(SESSIONS_FILES_VIEW_ID);
+		if (view !== null) {
+			(view as ExplorerView).collapseAll();
+		}
+	}
+});
