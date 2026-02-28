e194a6180385da41a7b1e409414c92719bdf0692
Dmitriy Vasyura <dmitriv@microsoft.com>
2026-02-28 14:40:38 -0800

    Fix build break

--- build/gulpfile.vscode.linux.ts
+++ build/gulpfile.vscode.linux.ts
@@ -268,9 +268,8 @@ function prepareSnapPackage(arch: string) {
 		const snapcraft = gulp.src('resources/linux/snap/snapcraft.yaml', { base: '.' })
 			.pipe(replace('@@NAME@@', product.applicationName))
 			.pipe(replace('@@VERSION@@', commit!.substr(0, 8)))
-			// Possible platform values https://snapcraft.io/docs/architectures
+			// Possible values https://snapcraft.io/docs/architectures
 			.pipe(replace('@@ARCHITECTURE@@', arch === 'x64' ? 'amd64' : arch))
-			.pipe(replace('@@BUILD_ARCHITECTURE@@', arch === 'x64' ? 'amd64' : arch))
 			.pipe(replace('@@DEB_ARCHITECTURE@@', debMultiarch))
 			.pipe(rename('snap/snapcraft.yaml'));
 
