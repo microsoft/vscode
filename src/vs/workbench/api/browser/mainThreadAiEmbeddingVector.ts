e618e1c2d5da860a503650e8263568d38e9ded1b
Cohen <cohen.velazquez@gmail.com>
2026-03-09 13:44:31 -0700

    aiEmbeddingVector: avoid throwing from main-thread RPC registration

--- src/vs/workbench/api/browser/mainThreadAiEmbeddingVector.ts
+++ src/vs/workbench/api/browser/mainThreadAiEmbeddingVector.ts
@@ -8,7 +8,6 @@ import { Disposable, DisposableMap } from '../../../base/common/lifecycle.js';
 import { ExtHostAiEmbeddingVectorShape, ExtHostContext, MainContext, MainThreadAiEmbeddingVectorShape } from '../common/extHost.protocol.js';
 import { IAiEmbeddingVectorProvider, IAiEmbeddingVectorService } from '../../services/aiEmbeddingVector/common/aiEmbeddingVectorService.js';
 import { IExtHostContext, extHostNamedCustomer } from '../../services/extensions/common/extHostCustomers.js';
-import { isFalsyOrWhitespace } from '../../../base/common/strings.js';
 
 @extHostNamedCustomer(MainContext.MainThreadAiEmbeddingVector)
 export class MainThreadAiEmbeddingVector extends Disposable implements MainThreadAiEmbeddingVectorShape {
@@ -24,10 +23,6 @@ export class MainThreadAiEmbeddingVector extends Disposable implements MainThrea
 	}
 
 	$registerAiEmbeddingVectorProvider(model: string, handle: number): void {
-		if (isFalsyOrWhitespace(model)) {
-			throw new Error('Embedding vector model must be a non-empty string.');
-		}
-
 		const provider: IAiEmbeddingVectorProvider = {
 			provideAiEmbeddingVector: (strings: string[], token: CancellationToken) => {
 				return this._proxy.$provideAiEmbeddingVector(
