eecaae1355eacad111bd6285a78b579d36f95e60
Sandeep Somavarapu <sasomava@microsoft.com>
2026-03-10 12:36:34 +0100

    fix: update experimentMode assignment logic in configuration properties

--- src/vs/workbench/api/common/configurationExtensionPoint.ts
+++ src/vs/workbench/api/common/configurationExtensionPoint.ts
@@ -294,7 +294,7 @@ configurationExtPoint.setHandler((extensions, { added, removed }) => {
 				if (extensionConfigurationPolicy?.[key]) {
 					propertyConfiguration.policy = extensionConfigurationPolicy?.[key];
 				}
-				if (propertyConfiguration.tags?.some(tag => tag.toLowerCase() === 'onexp')) {
+				if (!propertyConfiguration.experimentMode) {
 					propertyConfiguration.experimentMode = 'startup';
 				}
 				seenProperties.add(key);
