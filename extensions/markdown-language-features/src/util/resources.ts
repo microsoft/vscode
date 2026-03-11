909a1067252f1adaa49f5ecf97cda63c641f361d
Matt Bierner <12821956+mjbvz@users.noreply.github.com>
2026-03-10 19:26:48 -0700

    Use case insentive file uri compare in md extension
    For #265277

--- extensions/markdown-language-features/src/util/resources.ts
+++ extensions/markdown-language-features/src/util/resources.ts
@@ -11,3 +11,10 @@ export interface WebviewResourceProvider {
 	readonly cspSource: string;
 }
 
+export function areUrisEqual(uri1: vscode.Uri, uri2: vscode.Uri): boolean {
+	if (uri1.scheme === 'file' && uri2.scheme === 'file') {
+		return uri1.fsPath.toLowerCase() === uri2.fsPath.toLowerCase();
+	}
+
+	return uri1.fsPath === uri2.fsPath;
+}
