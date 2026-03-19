432c44b42908bead72e5324b55596995f4222f0e
Paul <paul_wang347@hotmail.com>
2026-03-18 17:02:30 -0700

    Revert "Add chat perf markers (#302491)"
    This reverts commit 58f7811b3d8ab728dcfd116beb78e39dc54cb9ec.

--- src/vs/workbench/contrib/chat/browser/widget/chatWidget.ts
+++ src/vs/workbench/contrib/chat/browser/widget/chatWidget.ts
@@ -17,7 +17,6 @@ import { Emitter, Event } from '../../../../../base/common/event.js';
 import { hash } from '../../../../../base/common/hash.js';
 import { IMarkdownString, MarkdownString } from '../../../../../base/common/htmlContent.js';
 import { Iterable } from '../../../../../base/common/iterator.js';
-import { mark } from '../../../../../base/common/performance.js';
 import { Disposable, DisposableStore, IDisposable, MutableDisposable, thenIfNotDisposed } from '../../../../../base/common/lifecycle.js';
 import { ResourceSet } from '../../../../../base/common/map.js';
 import { Schemas } from '../../../../../base/common/network.js';
@@ -2192,10 +2191,7 @@ export class ChatWidget extends Disposable implements IChatWidget {
 	}
 
 	async acceptInput(query?: string, options?: IChatAcceptInputOptions): Promise<IChatResponseModel | undefined> {
-		mark('code/chat/willAcceptInput');
-		const result = await this._acceptInput(query ? { query } : undefined, options);
-		mark('code/chat/didAcceptInput');
-		return result;
+		return this._acceptInput(query ? { query } : undefined, options);
 	}
 
 	async rerunLastRequest(): Promise<void> {
