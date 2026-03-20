bb061435ac57bca58f548c18dbdad44e9e3fd6cd
Martin Aeschlimann <martinae@microsoft.com>
2026-03-20 08:09:36 +0100

    fix

--- src/vs/workbench/test/browser/componentFixtures/aiCustomizationListWidget.fixture.ts
+++ src/vs/workbench/test/browser/componentFixtures/aiCustomizationListWidget.fixture.ts
@@ -4,7 +4,7 @@
  *--------------------------------------------------------------------------------------------*/
 
 import { Event } from '../../../../base/common/event.js';
-import { ResourceMap, ResourceSet } from '../../../../base/common/map.js';
+import { ResourceSet } from '../../../../base/common/map.js';
 import { observableValue } from '../../../../base/common/observable.js';
 import { URI } from '../../../../base/common/uri.js';
 import { mock } from '../../../../base/test/common/mock.js';
@@ -45,14 +45,6 @@ interface IFixtureInstructionFile {
 }
 
 function createMockPromptsService(instructionFiles: IFixtureInstructionFile[], agentInstructionFiles: IResolvedAgentFile[] = []): IPromptsService {
-	// Build a map from URI to applyTo for parseNew
-	const applyToMap = new ResourceMap<string | undefined>();
-	const descriptionMap = new ResourceMap<string | undefined>();
-	for (const file of instructionFiles) {
-		applyToMap.set(file.uri, file.applyTo);
-		descriptionMap.set(file.uri, file.description);
-	}
-
 	return new class extends mock<IPromptsService>() {
 		override readonly onDidChangeCustomAgents = Event.None;
 		override readonly onDidChangeSlashCommands = Event.None;
@@ -60,13 +52,7 @@ function createMockPromptsService(instructionFiles: IFixtureInstructionFile[], a
 		override getDisabledPromptFiles(): ResourceSet { return new ResourceSet(); }
 		override async listPromptFiles(type: PromptsType) {
 			if (type === PromptsType.instructions) {
-				return instructionFiles.map(f => ({
-					uri: f.uri,
-					storage: f.storage as PromptsStorage.local,
-					type: f.type,
-					name: f.name,
-					description: f.description,
-				}));
+				return instructionFiles.map(f => f.promptPath);
 			}
 			return [];
 		}
