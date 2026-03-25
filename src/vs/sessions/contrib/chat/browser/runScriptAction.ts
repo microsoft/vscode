4a5abce4e45dc1889d90669794a69015c4bb24d1
Hawk Ticehurst <hticehurst@microsoft.com>
2026-03-25 17:09:47 -0400

    Revert "feat: add refresh functionality for dropdown menu in RunScriptActionViewItem"
    This reverts commit a7b7a0d935570abe88b1e7d9d328cbfe4f6f7c0e.

--- src/vs/sessions/contrib/chat/browser/runScriptAction.ts
+++ src/vs/sessions/contrib/chat/browser/runScriptAction.ts
@@ -526,7 +526,6 @@ class RunScriptActionViewItem extends BaseActionViewItem {
 			contextKeyService,
 			telemetryService,
 		));
-
 	}
 
 	override render(container: HTMLElement): void {
@@ -624,7 +623,6 @@ class RunScriptActionViewItem extends BaseActionViewItem {
 					enabled: !!repo?.uri,
 					run: async () => {
 						this._sessionsConfigService.setPinnedTaskLabel(repo?.uri, isPinned ? undefined : task.label);
-						this._refreshDropdownMenuIfVisible();
 					}
 				},
 				{
@@ -635,7 +633,6 @@ class RunScriptActionViewItem extends BaseActionViewItem {
 					enabled: true,
 					run: async () => {
 						await this._showCustomCommandInput(session, { task, target: entry.target }, 'configure');
-						this._refreshDropdownMenuIfVisible();
 					}
 				},
 				{
@@ -646,7 +643,6 @@ class RunScriptActionViewItem extends BaseActionViewItem {
 					enabled: true,
 					run: async () => {
 						await this._sessionsConfigService.removeTask(task.label, session, entry.target);
-						this._refreshDropdownMenuIfVisible();
 					}
 				}
 			];
@@ -719,15 +715,6 @@ class RunScriptActionViewItem extends BaseActionViewItem {
 		});
 
 		return actions;
-	}
-
-	private _refreshDropdownMenuIfVisible(): void {
-		if (!this._actionWidgetService.isVisible) {
-			return;
-		}
-
-		this._actionWidgetService.hide();
-		this._dropdown.show();
 	}
 }
 
