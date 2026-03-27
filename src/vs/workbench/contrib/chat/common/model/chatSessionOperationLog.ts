b65b7be0b606b6c447d986899cab08a79aaf85fd
justschen <justchen@microsoft.com>
2026-03-27 16:58:48 -0700

    make sure thinking is serialized dynamically'

--- src/vs/workbench/contrib/chat/common/model/chatSessionOperationLog.ts
+++ src/vs/workbench/contrib/chat/common/model/chatSessionOperationLog.ts
@@ -61,6 +61,7 @@ const responsePartSchema = Adapt.v<IChatProgressResponseContent, SerializedChatR
 				case 'textEditGroup':
 				case 'multiDiffData':
 				case 'mcpServersStarting':
+				case 'thinking':
 					return objectsEqual(a, b);
 
 				// Static types that won't change after being pushed can use strict equality.
@@ -76,7 +77,6 @@ const responsePartSchema = Adapt.v<IChatProgressResponseContent, SerializedChatR
 				case 'progressMessage':
 				case 'pullRequest':
 				case 'questionCarousel':
-				case 'thinking':
 				case 'undoStop':
 				case 'warning':
 				case 'treeData':
