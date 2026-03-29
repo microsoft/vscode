34d78ab544cd9afe6014d9e0839829a2b7415910
Benjamin Pasero <benjamin.pasero@microsoft.com>
2026-03-29 21:14:29 +0200

    .

--- src/vs/platform/editor/common/editor.ts
+++ src/vs/platform/editor/common/editor.ts
@@ -336,11 +336,6 @@ export interface IModalEditorPartOptions {
 	readonly maximized?: boolean;
 
 	/**
-	 * Minimum width of the modal editor part in pixels.
-	 */
-	readonly minWidth?: number;
-
-	/**
 	 * Size of the modal editor part unless it is maximized.
 	 */
 	readonly size?: { readonly width: number; readonly height: number };
