a601a86f065823bb0180f8e5915c89acdd1bcabb
Hawk Ticehurst <hticehurst@microsoft.com>
2026-03-30 15:44:33 -0400

    fix: simplify storage options handling by enabling/disabling based on target lock state

--- src/vs/sessions/contrib/chat/browser/runScriptCustomTaskWidget.ts
+++ src/vs/sessions/contrib/chat/browser/runScriptCustomTaskWidget.ts
@@ -142,13 +142,7 @@ export class RunScriptCustomTaskWidget extends Disposable {
 		}));
 		this._storageOptions.domNode.setAttribute('aria-label', localize('storageAriaLabel', "Task storage target"));
 		this._storageOptions.domNode.classList.toggle('run-script-action-radio-disabled', this._targetLocked);
-		if (this._targetLocked) {
-			for (const child of Array.from(this._storageOptions.domNode.children)) {
-				if (dom.isHTMLElement(child)) {
-					child.tabIndex = -1;
-				}
-			}
-		}
+		this._storageOptions.setEnabled(!this._targetLocked);
 		storageSection.appendChild(this._storageOptions.domNode);
 
 		const buttonRow = dom.append(this.domNode, dom.$('.run-script-action-buttons'));
