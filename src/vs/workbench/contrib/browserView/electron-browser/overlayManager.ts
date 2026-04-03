1394fbde3cd56a7b722814a55667d4330395ce1f
Kyle Cutler <67761731+kycutler@users.noreply.github.com>
2026-04-03 10:42:20 -0700

    Fix reasoning effort menu being hidden behind browser (#307670)

--- src/vs/workbench/contrib/browserView/electron-browser/overlayManager.ts
+++ src/vs/workbench/contrib/browserView/electron-browser/overlayManager.ts
@@ -20,6 +20,7 @@ export enum BrowserOverlayType {
 
 const OVERLAY_DEFINITIONS: ReadonlyArray<{ className: string; type: BrowserOverlayType }> = [
 	{ className: 'monaco-menu-container', type: BrowserOverlayType.Menu },
+	{ className: 'action-list-submenu-panel', type: BrowserOverlayType.Menu },
 	{ className: 'quick-input-widget', type: BrowserOverlayType.QuickInput },
 	{ className: 'monaco-hover', type: BrowserOverlayType.Hover },
 	{ className: 'editor-widget', type: BrowserOverlayType.Hover },
