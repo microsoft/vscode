68cb95b653fbf437374e0e23b97b316667003241
heliole <154069426+heliole@users.noreply.github.com>
2026-05-07 02:41:34 +0000

    fix: restore word wrap after diff editor inline to split-view transition

--- src/vs/editor/browser/widget/diffEditor/components/diffEditorEditors.ts
+++ src/vs/editor/browser/widget/diffEditor/components/diffEditorEditors.ts
@@ -148,6 +148,7 @@ export class DiffEditorEditors extends Disposable {
 		} else {
 			result.unicodeHighlight = this._options.editorOptions.get().unicodeHighlight || {};
 			result.wordWrapOverride1 = this._options.diffWordWrap.get();
+			result.wordWrapOverride2 = 'inherit';
 		}
 		result.glyphMargin = this._options.renderSideBySide.get();
 
@@ -168,6 +169,7 @@ export class DiffEditorEditors extends Disposable {
 		}
 		result.ariaLabel = this._updateAriaLabel(result.ariaLabel);
 		result.wordWrapOverride1 = this._options.diffWordWrap.get();
+		result.wordWrapOverride2 = 'inherit';
 		result.revealHorizontalRightPadding = EditorOptions.revealHorizontalRightPadding.defaultValue + OverviewRulerFeature.ENTIRE_DIFF_OVERVIEW_WIDTH;
 		result.scrollbar!.verticalHasArrows = false;
 		result.extraEditorClassName = 'modified-in-monaco-diff-editor';
