ed10e68313a719bc2a9681dc305ba1070fde0bfc
Vishwanath Barve <barvevishwanath9@gmail.com>
2026-05-16 00:20:45 +0530

    Update code-amd-node-module.ts

--- .eslint-plugin-local/code-amd-node-module.ts
+++ .eslint-plugin-local/code-amd-node-module.ts
@@ -7,22 +7,20 @@ import * as eslint from 'eslint';
 import type * as ESTree from 'estree';
 import { readFileSync } from 'fs';
 import { join } from 'path';
-
-
 export default new class ApiProviderNaming implements eslint.Rule.RuleModule {
-
 	readonly meta: eslint.Rule.RuleMetaData = {
 		messages: {
 			amdX: 'Use `import type` for import declarations, use `amdX#importAMDNodeModule` for import expressions'
 		},
 		schema: false,
 	};
-
 	create(context: eslint.Rule.RuleContext): eslint.Rule.RuleListener {
 
 		const modules = new Set<string>();
 
-		try {
+		
+
+	try {
 			const packageJson = JSON.parse(readFileSync(join(import.meta.dirname, '../package.json'), 'utf-8'));
 			const { dependencies, optionalDependencies } = packageJson;
 			const all = Object.keys(dependencies).concat(Object.keys(optionalDependencies));
