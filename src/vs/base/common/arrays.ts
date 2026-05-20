f7a7a1a8d71ed8f0042e511b248a2ee3d513c62d
Mohit Yadav <ymohit799057@gmail.com>
2026-05-21 00:56:12 +0530

    Update arrays.ts

--- src/vs/base/common/arrays.ts
+++ src/vs/base/common/arrays.ts
@@ -445,14 +445,11 @@ export function commonPrefixLength<T>(one: ReadonlyArray<T>, other: ReadonlyArra
 export function range(to: number): number[];
 export function range(from: number, to: number): number[];
 export function range(arg: number, to?: number): number[] {
-	let from: number;
+	let from = typeof to === 'number' ? arg : 0;
 
-	if (typeof to === 'number') {
-		from = arg;
-	} else {
-		from = 0;
+	if (typeof to !== 'number') {
 		to = arg;
-	}
+	} 
 
 	const result: number[] = [];
 
