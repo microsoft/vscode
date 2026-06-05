bfb1048bd3dd23509147b674a2800c501b602e8f
thernstig <30827238+thernstig@users.noreply.github.com>
2026-06-05 15:06:18 +0200

    Update forwarded ports toggle label for clarity

--- src/vs/workbench/contrib/remote/browser/messages.ts
+++ src/vs/workbench/contrib/remote/browser/messages.ts
@@ -6,5 +6,8 @@
 import * as nls from '../../../../nls.js';
 
 export class Messages {
-	public static FORWARDED_PORTS_TOGGLE_LABEL: string = nls.localize('remote.togglePortsView', "Toggle Ports View");
+	public static FORWARDED_PORTS_TOGGLE_LABEL: string = nls.localize(
+		"remote.togglePortsView",
+		"Toggle Forwarded Ports View",
+	);
 }
