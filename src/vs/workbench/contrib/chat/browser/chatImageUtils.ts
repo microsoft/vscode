b9711513839a7f6d3c9d340926bbfb634360bbc5
josh-qm-mishra <josh@quarkmemory.com>
2026-06-11 23:25:46 -0700

    Update chatImageUtils.ts

--- src/vs/workbench/contrib/chat/browser/chatImageUtils.ts
+++ src/vs/workbench/contrib/chat/browser/chatImageUtils.ts
@@ -3,6 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+
 import { raceTimeout } from '../../../../base/common/async.js';
 import { decodeBase64, VSBuffer } from '../../../../base/common/buffer.js';
 import { LRUCache } from '../../../../base/common/map.js';
