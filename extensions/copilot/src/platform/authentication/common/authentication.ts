d31d1ac721fd73254744b4415533628f51159ebd
Logan Ramos <loganramos@microsoft.com>
2026-06-12 17:09:47 -0400

    More cleanup

--- extensions/copilot/src/platform/authentication/common/authentication.ts
+++ extensions/copilot/src/platform/authentication/common/authentication.ts
@@ -311,8 +311,12 @@ export abstract class BaseAuthenticationService extends Disposable implements IA
 	}
 
 	resetCopilotToken(httpError?: number): void {
+		const hadToken = !!this._tokenStore.copilotToken;
 		this._tokenStore.copilotToken = undefined;
 		this._tokenManager.resetCopilotToken(httpError);
+		if (hadToken) {
+			this.fireCopilotTokenChange('resetCopilotToken');
+		}
 	}
 
 	//#endregion
