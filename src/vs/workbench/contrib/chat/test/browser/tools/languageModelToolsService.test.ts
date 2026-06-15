d099916458f503a55da619f6aaef9132258fa778
Martin Aeschlimann <martinae@microsoft.com>
2026-06-15 16:56:28 +0200

    fix test

--- src/vs/workbench/contrib/chat/test/browser/tools/languageModelToolsService.test.ts
+++ src/vs/workbench/contrib/chat/test/browser/tools/languageModelToolsService.test.ts
@@ -27,7 +27,7 @@ import { IChatToolRiskAssessmentService, IToolRiskAssessment, ToolRiskLevel, Too
 import { ChatModel, IChatModel } from '../../../common/model/chatModel.js';
 import { IChatService, IChatProgress, IChatInfoMessage, IChatToolInputInvocationData, IChatToolInvocation, ToolConfirmKind } from '../../../common/chatService/chatService.js';
 import { ChatConfiguration, ChatPermissionLevel } from '../../../common/constants.js';
-import { SpecedToolAliases, isToolResultInputOutputDetails, IToolData, IToolImpl, IToolInvocation, IToolSet, ToolDataSource, ToolSet, IToolResultTextPart } from '../../../common/tools/languageModelToolsService.js';
+import { SpecedToolAliases, isToolResultInputOutputDetails, IToolData, IToolImpl, IToolInvocation, ToolDataSource, ToolSet, IToolResultTextPart } from '../../../common/tools/languageModelToolsService.js';
 import { MockChatService } from '../../common/chatService/mockChatService.js';
 import { ChatToolInvocation } from '../../../common/model/chatProgressTypes/chatToolInvocation.js';
 import { LocalChatSessionUri } from '../../../common/model/chatUri.js';
@@ -1109,14 +1109,10 @@ suite('LanguageModelToolsService', () => {
 		store.add(toolSet.addTool(toolSetTool1));
 		store.add(toolSet.addTool(toolSetTool2));
 
-		const selection = new Map<IToolData | IToolSet, boolean>([
-			[toolSet, true],
-			[toolSetTool1, true],
-			[toolSetTool2, false],
-		]);
+		const selection = service.toToolAndToolSetEnablementMap(['refToolSet', 'toolSetTool1Ref'], undefined);
 
 		const fullReferenceNames = service.toFullReferenceNames(selection);
-		assert.deepStrictEqual(fullReferenceNames, [service.getFullReferenceName(toolSetTool1)]);
+		assert.deepStrictEqual(fullReferenceNames, [service.getFullReferenceName(toolSet)]);
 	});
 
 	test('toToolAndToolSetEnablementMap with non-existent tool names', () => {
