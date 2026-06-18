c54d7f8c42c9cc28c8221add6efdeaf94d914acc
Alex Dima <alexdima@microsoft.com>
2026-06-18 23:25:44 +0200

    Disable `process.report`

---
+++ extensions/copilot/src/extension/extension/vscode-node/disableProcessReport.ts
@@ -0,0 +1,10 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+Object.defineProperty(process, 'report', {
+	configurable: true,
+	writable: true,
+	value: undefined
+});
