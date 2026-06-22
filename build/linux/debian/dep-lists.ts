7b4223784bc23c01350d22afb4cac6ef2518f38d
Anthony Kim <anthonykim@microsoft.com>
2026-06-22 16:23:36 -0700

    Update amd64 deb deps for @github/copilot-linux-x64/copilot

--- build/linux/debian/dep-lists.ts
+++ build/linux/debian/dep-lists.ts
@@ -47,7 +47,7 @@ export const referenceGeneratedDepsByArch = {
 		'libnss3 (>= 2:3.30)',
 		'libnss3 (>= 3.26)',
 		'libpango-1.0-0 (>= 1.14.0)',
-		'libstdc++6 (>= 6)',
+		'libstdc++6 (>= 7)',
 		'libudev1 (>= 183)',
 		'libx11-6',
 		'libx11-6 (>= 2:1.4.99.1)',
