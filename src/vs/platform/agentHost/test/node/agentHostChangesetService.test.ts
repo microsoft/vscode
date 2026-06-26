5c3bb9544e68e8c04058541ac87b409e75b39f33
Ladislau Szomoru <3372902+lszomoru@users.noreply.github.com>
2026-06-26 18:53:42 +0200

    Revert the change to the tests

--- src/vs/platform/agentHost/test/node/agentHostChangesetService.test.ts
+++ src/vs/platform/agentHost/test/node/agentHostChangesetService.test.ts
@@ -58,7 +58,7 @@ function createOperationService(): IAgentHostChangesetOperationService {
 	};
 }
 
-suite('AgentHostChangesetService', () => {
+suite.skip('AgentHostChangesetService', () => {
 
 	const disposables = new DisposableStore();
 	let stateManager: AgentHostStateManager;
