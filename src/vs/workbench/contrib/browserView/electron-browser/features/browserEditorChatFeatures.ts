15ddfcd749de4b59034d344bb35cf30e22885461
Joaquín Ruales <1588988+jruales@users.noreply.github.com>
2026-06-26 18:00:29 -0700

    Unused import

--- src/vs/workbench/contrib/browserView/electron-browser/features/browserEditorChatFeatures.ts
+++ src/vs/workbench/contrib/browserView/electron-browser/features/browserEditorChatFeatures.ts
@@ -36,7 +36,6 @@ import { BrowserEditor, BrowserEditorContribution, BrowserWidgetLocation, IBrows
 import { IConfigurationRegistry, Extensions as ConfigurationExtensions } from '../../../../../platform/configuration/common/configurationRegistry.js';
 import { Registry } from '../../../../../platform/registry/common/platform.js';
 import { PolicyCategory } from '../../../../../base/common/policy.js';
-import product from '../../../../../platform/product/common/product.js';
 import { AgentHostEnabledSettingId } from '../../../../../platform/agentHost/common/agentService.js';
 import { workbenchConfigurationNodeBase } from '../../../../common/configuration.js';
 import { safeSetInnerHtml } from '../../../../../base/browser/domSanitize.js';
