microsoft/vscode

commit cce0642bfb890ca622a922cdb202bf834a3f71ba

If the users selects a language, let's have it actually choose the language they selected. (Yaml vs yaml) (#288153)
* If the users selects a language, let's have it actually choose the language they selected. (Yaml vs yaml)

* polish

---------

Co-authored-by: Benjamin Pasero <benjamin.pasero@gmail.com>

src/vs/workbench/browser/parts/editor/editorStatus.ts
@@ -1198,6 +1198,7 @@ export class ChangeLanguageAction extends Action2 {
 				}
 
 				return {
+					id: languageId,
 					label: languageName,
 					meta: extensions,
 					iconClasses: getIconClassesForLanguageId(languageId),
@@ -1280,8 +1281,7 @@ export class ChangeLanguageAction extends Action2 {
 						}
 					}
 				} else {
-					const languageId = languageService.getLanguageIdByLanguageName(pick.label);
-					languageSelection = languageService.createById(languageId);
+					languageSelection = languageService.createById(pick.id);
 
 					if (resource) {
 						// fire and forget to not slow things down

commit 77543032fab9353986454f46b10b04cedfa95563

Broken languageMode arg when changed to language id picking
When added languageMode as an arg in aaa5cb462e8e365d0c123378fd89284f1f58ba12 it was treated as the language label. It got broken after cce0642bfb890ca622a922cdb202bf834a3f71ba to prevent ambiguity in cases like "Yaml" and "yaml". To fix the argument and to be consistent with that last commit, languageMode should be treated as languageId

src/vs/workbench/browser/parts/editor/editorStatus.ts
@@ -1218,7 +1218,7 @@ export class ChangeLanguageAction extends Action2 {
 			picks.unshift(autoDetectLanguage);
 		}
 
-		const pick = typeof languageMode === 'string' ? { label: languageMode } : await quickInputService.pick(picks, { placeHolder: localize('pickLanguage', "Select Language Mode"), matchOnDescription: true });
+		const pick = typeof languageMode === 'string' ? languageService.createById(languageMode) : await quickInputService.pick(picks, { placeHolder: localize('pickLanguage', "Select Language Mode"), matchOnDescription: true });
 		if (!pick) {
 			return;
 		}