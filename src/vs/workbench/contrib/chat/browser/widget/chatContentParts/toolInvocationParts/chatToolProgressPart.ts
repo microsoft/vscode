098619aae79c26e04357a172263e27506be9477a
Justin Chen <54879025+justschen@users.noreply.github.com>
2026-08-06 20:57:07 -0700

    fix streaming tool calls (#329519)
    * fix streaming tool calls
    
    * address comments

--- src/vs/workbench/contrib/chat/browser/widget/chatContentParts/toolInvocationParts/chatToolProgressPart.ts
+++ src/vs/workbench/contrib/chat/browser/widget/chatContentParts/toolInvocationParts/chatToolProgressPart.ts
@@ -6,11 +6,9 @@
 import * as dom from '../../../../../../../base/browser/dom.js';
 import { renderAsPlaintext } from '../../../../../../../base/browser/markdownRenderer.js';
 import { status } from '../../../../../../../base/browser/ui/aria/aria.js';
-import { Codicon } from '../../../../../../../base/common/codicons.js';
 import { IMarkdownString, MarkdownString } from '../../../../../../../base/common/htmlContent.js';
 import { stripIcons } from '../../../../../../../base/common/iconLabels.js';
 import { autorun } from '../../../../../../../base/common/observable.js';
-import { ThemeIcon } from '../../../../../../../base/common/themables.js';
 import { IMarkdownRenderer } from '../../../../../../../platform/markdown/browser/markdownRenderer.js';
 import { IConfigurationService } from '../../../../../../../platform/configuration/common/configuration.js';
 import { IInstantiationService } from '../../../../../../../platform/instantiation/common/instantiation.js';
@@ -110,11 +108,6 @@ export class ChatToolProgressSubPart extends BaseChatToolInvocationSubPart {
 
 		const shouldShimmer = shouldShimmerForTool(this.toolInvocation, content);
 		return this.instantiationService.createInstance(ChatProgressContentPart, progressMessage, this.renderer, this.context, shouldShimmer ? true : undefined, true, this.getProgressIcon(), this.toolInvocation, shouldShimmer);
-	}
-
-	private getProgressIcon(): ThemeIcon {
-		const icon = this.getIcon();
-		return ThemeIcon.isEqual(icon, ThemeIcon.modify(Codicon.loading, 'spin')) ? Codicon.check : icon;
 	}
 
 	private getAnnouncementKey(kind: 'progress' | 'complete'): string {
