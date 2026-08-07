2e77db1b632f498675649e01734a8649e7e9d2e9
Aiday Marlen Kyzy <amarlenkyzy@microsoft.com>
2026-08-07 16:50:08 +0200

    adding command which returns internal copilot settings

---
+++ extensions/copilot/src/extension/configuration/common/internalConfigurationInformation.ts
@@ -0,0 +1,33 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { Config, ConfigType, ExperimentBasedConfig, ExperimentBasedConfigType } from '../../../platform/configuration/common/configurationService';
+
+export interface IInternalConfigurationPropertyInformation {
+	defaultValue: unknown;
+	configType: 'simple' | 'experimentBased';
+	experimentName?: string;
+	oldId?: string;
+}
+
+export interface IInternalConfigurationInformation {
+	[id: string]: IInternalConfigurationPropertyInformation;
+}
+
+export function buildInternalConfigurationInformation(configs: Iterable<Config<any> | ExperimentBasedConfig<ExperimentBasedConfigType>>): IInternalConfigurationInformation {
+	const configurationInformation: IInternalConfigurationInformation = {};
+	for (const config of configs) {
+		if (!config.options?.valueIgnoredForExternals) {
+			continue;
+		}
+		configurationInformation[config.fullyQualifiedId] = {
+			defaultValue: config.defaultValue,
+			configType: config.configType === ConfigType.Simple ? 'simple' : 'experimentBased',
+			experimentName: config.configType === ConfigType.ExperimentBased ? config.experimentName : undefined,
+			oldId: config.fullyQualifiedOldId,
+		};
+	}
+	return configurationInformation;
+}
