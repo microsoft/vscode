c2d1b13fdc4a77628e5f3bb70173351c8f2fbad1
Paul Wang <Paul.Wang@microsoft.com>
2026-08-07 10:44:35 -0700

    Fix tests (#90)

--- src/vs/workbench/services/environment/browser/environmentService.ts
+++ src/vs/workbench/services/environment/browser/environmentService.ts
@@ -306,8 +306,8 @@ export class BrowserWorkbenchEnvironmentService implements IBrowserWorkbenchEnvi
 			extensionDevelopmentKind: undefined
 		};
 
-		// Extension host development options from the payload are only valid in development builds.
-		if (this.payload && !this.isBuilt) {
+		// Extension host development options from the payload are only valid in development or smoke test builds.
+		if (this.payload && (!this.isBuilt || this.enableSmokeTestDriver)) {
 			for (const [key, value] of this.payload) {
 				switch (key) {
 					case 'extensionDevelopmentPath':
