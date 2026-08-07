0dcbac460f96d93eddfd99b2355a5b76c86a3d87
SteVen Batten <sbatten@microsoft.com>
2026-08-06 20:01:45 -0700

    Fix stationary mouse menu selection issues (#329507)
    Agent Host changes for sbatten/agents/fix-stationary-mouse-menu-selection

--- src/vs/base/test/browser/ui/menu/menu.test.ts
+++ src/vs/base/test/browser/ui/menu/menu.test.ts
@@ -4,13 +4,82 @@
  *--------------------------------------------------------------------------------------------*/
 
 import assert from 'assert';
-import { $, append, getWindow } from '../../../../browser/dom.js';
-import { getMenuWidgetCSS, unthemedMenuStyles } from '../../../../browser/ui/menu/menu.js';
+import sinon from 'sinon';
+import { $, append, EventType, getWindow } from '../../../../browser/dom.js';
+import { getMenuWidgetCSS, Menu, unthemedMenuStyles } from '../../../../browser/ui/menu/menu.js';
+import { Action, SubmenuAction } from '../../../../common/actions.js';
 import { toDisposable } from '../../../../common/lifecycle.js';
 import { ensureNoDisposablesAreLeakedInTestSuite } from '../../../common/utils.js';
 
 suite('Menu', () => {
 	const disposables = ensureNoDisposablesAreLeakedInTestSuite();
+
+	teardown(() => {
+		sinon.restore();
+	});
+
+	// A menu positioned under a resting pointer receives `mouseover` without any
+	// `mousemove`, so hover must react to `mousemove` to leave keyboard focus alone.
+	test('stationary mouse does not change focus (#110594, #148158)', () => {
+		const host = append(document.body, $('div'));
+		disposables.add(toDisposable(() => host.remove()));
+		const menu = disposables.add(new Menu(host, [
+			disposables.add(new Action('first', 'First')),
+			disposables.add(new Action('second', 'Second'))
+		], {}, unthemedMenuStyles));
+		const actionItems = Array.from(host.querySelectorAll<HTMLElement>('.action-item'));
+		const getFocusedActions = () => actionItems.map((_, index) => menu.isFocused(index));
+
+		menu.focus(true);
+		const focusStates = [getFocusedActions()];
+
+		actionItems[1].dispatchEvent(new MouseEvent(EventType.MOUSE_OVER, { bubbles: true }));
+		focusStates.push(getFocusedActions());
+
+		actionItems[1].dispatchEvent(new MouseEvent(EventType.MOUSE_MOVE, { bubbles: true }));
+		focusStates.push(getFocusedActions());
+
+		actionItems[1].dispatchEvent(new MouseEvent(EventType.MOUSE_MOVE, { bubbles: true }));
+		focusStates.push(getFocusedActions());
+
+		actionItems[0].dispatchEvent(new MouseEvent(EventType.MOUSE_MOVE, { bubbles: true }));
+		focusStates.push(getFocusedActions());
+
+		assert.deepStrictEqual(focusStates, [
+			[true, false],
+			[true, false],
+			[false, true],
+			[false, true],
+			[true, false]
+		]);
+	});
+
+	test('stationary mouse does not open submenu (#110594, #148158)', () => {
+		const clock = sinon.useFakeTimers();
+		const host = append(document.body, $('div'));
+		disposables.add(toDisposable(() => host.remove()));
+		const submenu = new SubmenuAction('submenu', 'Submenu', [
+			disposables.add(new Action('child', 'Child'))
+		]);
+		disposables.add(new Menu(host, [submenu], {}, unthemedMenuStyles));
+		const submenuAction = host.querySelector<HTMLElement>('.action-item')!;
+		const submenuItem = submenuAction.querySelector<HTMLElement>('.action-menu-item')!;
+
+		submenuAction.dispatchEvent(new MouseEvent(EventType.MOUSE_OVER, { bubbles: true }));
+		clock.tick(250);
+		const expandedAfterMouseOver = submenuItem.getAttribute('aria-expanded');
+
+		submenuAction.dispatchEvent(new MouseEvent(EventType.MOUSE_MOVE, { bubbles: true }));
+		clock.tick(250);
+
+		assert.deepStrictEqual({
+			expandedAfterMouseOver,
+			expandedAfterMouseMove: submenuItem.getAttribute('aria-expanded')
+		}, {
+			expandedAfterMouseOver: 'false',
+			expandedAfterMouseMove: 'true'
+		});
+	});
 
 	test('high contrast selection outline does not apply to nested submenu items (#327543)', () => {
 		const host = append(document.body, $('div'));
