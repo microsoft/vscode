84308cbd773b897befeac11e8673bc423705836b
Henning Dieterichs <hdieterichs@microsoft.com>
2026-08-14 20:46:22 +0200

    Allows settings to be set by experiments

--- src/vs/workbench/contrib/chat/browser/chat.shared.contribution.ts
+++ src/vs/workbench/contrib/chat/browser/chat.shared.contribution.ts
@@ -522,6 +522,7 @@ configurationRegistry.registerConfiguration({
 			description: nls.localize('chat.experimental.richLinks.enabled', "Controls whether supported links in chat are rendered as rich links with live metadata. Enabling this may make authenticated requests to services such as GitHub."),
 			default: false,
 			tags: ['experimental'],
+			experiment: { mode: 'auto' },
 		},
 		[ChatConfiguration.IncrementalRenderingStyle]: {
 			type: 'string',
