6fa7bf7ef20267d186404f3bf7e18cb515aa8a60
Vaggelis <baggelis100@gmail.com>
2026-08-25 18:39:27 -0400

    markdown preview: collapse slugify test assertions into sequence snapshots
    Same assertions, one deepStrictEqual per scenario instead of several
    strictEqual calls, matching the repo preference for snapshot-style
    assertions.
    
    Test Plan:
      node_modules/.bin/tsc -p extensions/markdown-language-features/tsconfig.json --noEmit
      exit 0

--- extensions/markdown-language-features/src/test/slugify.test.ts
+++ extensions/markdown-language-features/src/test/slugify.test.ts
@@ -3,7 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { strictEqual } from 'assert';
+import { deepStrictEqual } from 'assert';
 import 'mocha';
 import { githubSlugifier } from '../slugify';
 
@@ -11,24 +11,23 @@ suite('githubSlugifier', () => {
 	suite('createBuilder', () => {
 		test('Duplicate heading whose generated id is taken gets the next free suffix', () => {
 			const builder = githubSlugifier.createBuilder();
-			strictEqual(builder.add('Foo').value, 'foo');
-			strictEqual(builder.add('Foo-1').value, 'foo-1');
-			strictEqual(builder.add('Foo').value, 'foo-2');
+			deepStrictEqual(
+				['Foo', 'Foo-1', 'Foo'].map(heading => builder.add(heading).value),
+				['foo', 'foo-1', 'foo-2']);
 		});
 
 		test('Heading whose slug collides with a generated id is suffixed further', () => {
 			const builder = githubSlugifier.createBuilder();
-			strictEqual(builder.add('Foo').value, 'foo');
-			strictEqual(builder.add('Foo').value, 'foo-1');
-			strictEqual(builder.add('Foo 1').value, 'foo-1-1');
-			strictEqual(builder.add('Foo').value, 'foo-2');
+			deepStrictEqual(
+				['Foo', 'Foo', 'Foo 1', 'Foo'].map(heading => builder.add(heading).value),
+				['foo', 'foo-1', 'foo-1-1', 'foo-2']);
 		});
 
 		test('Repeated headings keep their existing ids', () => {
 			const builder = githubSlugifier.createBuilder();
-			strictEqual(builder.add('Foo').value, 'foo');
-			strictEqual(builder.add('Foo').value, 'foo-1');
-			strictEqual(builder.add('Foo').value, 'foo-2');
+			deepStrictEqual(
+				['Foo', 'Foo', 'Foo'].map(heading => builder.add(heading).value),
+				['foo', 'foo-1', 'foo-2']);
 		});
 	});
 });
