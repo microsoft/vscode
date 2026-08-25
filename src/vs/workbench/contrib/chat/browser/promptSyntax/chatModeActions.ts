f70a7eb423487cddb78af9a7efdd29fff177c3aa
Martin Aeschlimann <martinae@microsoft.com>
2026-08-25 12:57:02 +0200

    update

--- src/vs/workbench/contrib/chat/browser/promptSyntax/chatModeActions.ts
+++ src/vs/workbench/contrib/chat/browser/promptSyntax/chatModeActions.ts
@@ -41,7 +41,7 @@ function createPickerConfigureAgentsActionConfig(disabled: boolean) {
 	return config;
 }
 
-export class PickerConfigAgentAction extends ConfigAgentActionImpl { constructor() { super(createPickerConfigureAgentsActionConfig(false)); } }
+class PickerConfigAgentAction extends ConfigAgentActionImpl { constructor() { super(createPickerConfigureAgentsActionConfig(false)); } }
 class PickerConfigAgentActionDisabled extends ConfigAgentActionImpl { constructor() { super(createPickerConfigureAgentsActionConfig(true)); } }
 
 /**
