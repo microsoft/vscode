4b0e5e92a4019f2b040157c6b06e716739190b05
Joaquín Ruales <1588988+jruales@users.noreply.github.com>
2026-08-27 19:08:57 -0700

    Use fuzzy matching as before

--- test/automation/src/quickaccess.ts
+++ test/automation/src/quickaccess.ts
@@ -7,7 +7,6 @@ import { Editors } from './editors';
 import { Code } from './code';
 import { QuickInput } from './quickinput';
 import { basename, isAbsolute } from 'path';
-import { Quality } from './application';
 
 enum QuickAccessKind {
 	Files = 1,
@@ -33,12 +32,7 @@ export class QuickAccess {
 
 		// make sure the file quick access is not "polluted"
 		// with entries from the editor history when opening
-		if (this.code.quality === Quality.Stable) {
-			// Stable does not expose Quick Pick item IDs to current automation.
-			await this.runCommand('Clear Editor History without Confirmation', { match: 'exactLabel' });
-		} else {
-			await this.runCommand('workbench.action.clearEditorHistoryWithoutConfirm');
-		}
+		await this.runCommand('workbench.action.clearEditorHistoryWithoutConfirm', { match: 'fuzzy' });
 
 		const PollingStrategy = {
 			Stop: true,
