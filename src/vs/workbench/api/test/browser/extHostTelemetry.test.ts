3335aa6b5e3fb44932f26c5363a936937c6ce87a
Simon Siefke <simon.siefke@gmail.com>
2026-09-02 14:55:34 +0000

    fix: memory leak in extHostTelemetry

--- src/vs/workbench/api/test/browser/extHostTelemetry.test.ts
+++ src/vs/workbench/api/test/browser/extHostTelemetry.test.ts
@@ -192,6 +192,20 @@ suite('ExtHostTelemetry', function () {
 
 	});
 
+	test('Disposed telemetry loggers are removed from extension bookkeeping', function () {
+		const extensionTelemetry = createExtHostTelemetry();
+		const functionSpy: TelemetryLoggerSpy = { dataArr: [], exceptionArr: [], flushCalled: false };
+		const firstLogger = createLogger(functionSpy, extensionTelemetry);
+		const secondLogger = createLogger(functionSpy, extensionTelemetry);
+		const telemetryLoggers = Reflect.get(extensionTelemetry, '_telemetryLoggers') as Map<string, ExtHostTelemetryLogger[]>;
+
+		assert.strictEqual(telemetryLoggers.get(mockExtensionIdentifier.identifier.value)?.length, 2);
+		firstLogger.dispose();
+		assert.strictEqual(telemetryLoggers.get(mockExtensionIdentifier.identifier.value)?.length, 1);
+		secondLogger.dispose();
+		assert.strictEqual(telemetryLoggers.has(mockExtensionIdentifier.identifier.value), false);
+	});
+
 	test('Simple log event to TelemetryLogger with options', function () {
 		const functionSpy: TelemetryLoggerSpy = { dataArr: [], exceptionArr: [], flushCalled: false };
 
