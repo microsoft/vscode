c0335fc5fccf3a8ba11dbd81c2c25eb2a04490db
Paul Wang <8560030+pwang347@users.noreply.github.com>
2026-09-03 13:00:15 -0700

    agentHost: fix Codex connector test fixtures

--- src/vs/platform/agentHost/test/node/codex/codexSessionTitleSpans.test.ts
+++ src/vs/platform/agentHost/test/node/codex/codexSessionTitleSpans.test.ts
@@ -30,8 +30,10 @@ import { ICodexProxyService } from '../../../node/codex/codexProxyService.js';
 import { ICopilotApiService } from '../../../node/shared/copilotApiService.js';
 import { createNoopCustomizationEnablementService } from '../testCustomizationEnablementService.js';
 import { IAgentHostGitHubEndpointService } from '../../../node/agentHostGitHubEndpointService.js';
+import { IAgentHostMcpConnectorsService } from '../../../node/agentHostMcpConnectorsService.js';
 import { IAgentHostProxyResolver } from '../../../node/agentHostProxyResolver.js';
 import { createTestGitHubEndpointService } from '../testGitHubEndpointService.js';
+import { createTestMcpConnectorsService } from '../testMcpConnectorsService.js';
 import { createTestAgentHostProxyResolver } from '../agentServiceTestUtils.js';
 
 /**
@@ -81,6 +83,7 @@ function createTestContext(disposables: Pick<DisposableStore, 'add'>): { stateMa
 	instantiationService.stub(IAgentHostOTelService, otelService);
 	instantiationService.stub(IAgentHostSessionTitleSignal, disposables.add(new AgentHostSessionTitleSignal(stateManager)));
 	instantiationService.stub(IAgentHostGitHubEndpointService, createTestGitHubEndpointService());
+	instantiationService.stub(IAgentHostMcpConnectorsService, createTestMcpConnectorsService());
 	instantiationService.stub(IAgentHostProxyResolver, createTestAgentHostProxyResolver());
 	instantiationService.stub(IProductService, { _serviceBrand: undefined, version: '1.0.0-test' } as IProductService);
 	instantiationService.stub(INativeEnvironmentService, { userHome: URI.file('/tmp') });
