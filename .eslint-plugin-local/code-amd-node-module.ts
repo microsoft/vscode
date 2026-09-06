5c87e7bf69051eb41bf2a1a71a3d0d32fc0fe130
Deniz Güney Yıldırım <dnzgny99@gmail.com>
2026-09-06 18:45:19 +0300

    Optimize package.json loading for AmdModuleImportCheck
    Moved package.json reading outside of the rule module to reduce disk I/O and JSON parsing on each file scan.

--- .eslint-plugin-local/code-amd-node-module.ts
+++ .eslint-plugin-local/code-amd-node-module.ts
@@ -8,6 +8,20 @@ import type * as ESTree from 'estree';
 import { readFileSync } from 'fs';
 import { join } from 'path';
 
+// `package.json` okumasını ve modül listesinin oluşturulmasını kural modülünün dışına (module scope)
+// alarak her dosya taramasında gereksiz disk I/O ve JSON.parse işlemlerinin önüne geçilmiş olundu.
+const modules = new Set<string>();
+
+try {
+	const packageJson = JSON.parse(readFileSync(join(import.meta.dirname, '../package.json'), 'utf-8'));
+	const { dependencies = {}, optionalDependencies = {} } = packageJson;
+	const all = Object.keys(dependencies).concat(Object.keys(optionalDependencies));
+	for (const key of all) {
+		modules.add(key);
+	}
+} catch (e) {
+	console.error('Failed to load package.json for AmdModuleImportCheck rule:', e);
+}
 
 export default new class ApiProviderNaming implements eslint.Rule.RuleModule {
 
@@ -19,22 +33,6 @@ export default new class ApiProviderNaming implements eslint.Rule.RuleModule {
 	};
 
 	create(context: eslint.Rule.RuleContext): eslint.Rule.RuleListener {
-
-		const modules = new Set<string>();
-
-		try {
-			const packageJson = JSON.parse(readFileSync(join(import.meta.dirname, '../package.json'), 'utf-8'));
-			const { dependencies, optionalDependencies } = packageJson;
-			const all = Object.keys(dependencies).concat(Object.keys(optionalDependencies));
-			for (const key of all) {
-				modules.add(key);
-			}
-
-		} catch (e) {
-			console.error(e);
-			throw e;
-		}
-
 
 		const checkImport = (node: ESTree.Literal & { parent?: ESTree.Node & { importKind?: string } }) => {
 
