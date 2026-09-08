1e6adf87da425f6263280c2831dc1c07420bbcde
Alex Ross <38270282+alexr00@users.noreply.github.com>
2026-09-08 15:09:36 +0200

    Feedback: move constant to a safer file

--- src/vs/sessions/contrib/extensions/browser/extensions.contribution.ts
+++ src/vs/sessions/contrib/extensions/browser/extensions.contribution.ts
@@ -5,8 +5,8 @@
 
 import { localize } from '../../../../nls.js';
 import { ConfigurationScope, Extensions as ConfigurationExtensions, IConfigurationRegistry } from '../../../../platform/configuration/common/configurationRegistry.js';
+import { EXTENSIONS_ENABLE_AGENTS_WINDOW_CAPABILITY } from '../../../../platform/extensions/common/extensions.js';
 import { Registry } from '../../../../platform/registry/common/platform.js';
-import { EXTENSIONS_ENABLE_AGENTS_WINDOW_CAPABILITY } from '../../../../workbench/services/extensions/common/extensionManifestPropertiesService.js';
 
 Registry.as<IConfigurationRegistry>(ConfigurationExtensions.Configuration).registerConfiguration({
 	id: 'extensions',
