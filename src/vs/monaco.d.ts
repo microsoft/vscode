460f597f1bee7967a2b8aca80fc7eb7e7bee1b8b
Aiday Marlen Kyzy <amarlenkyzy@microsoft.com>
2026-09-09 12:59:35 +0200

    polishing

--- src/vs/monaco.d.ts
+++ src/vs/monaco.d.ts
@@ -4012,8 +4012,6 @@ declare namespace monaco.editor {
 		inlineCompletionsAccessibilityVerbose?: boolean;
 	}
 
-	export type DiffEditorViewMode = 'inline' | 'sideBySide' | 'automatic';
-
 	export interface IDiffEditorBaseOptions {
 		/**
 		 * Allow the user to resize the diff editor split view.
