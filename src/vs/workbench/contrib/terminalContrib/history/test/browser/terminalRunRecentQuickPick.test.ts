346c56552906bd5100c370f9964e1129f2973ee5
Anthony Kim <anthonykim@microsoft.com>
2026-09-12 19:49:02 -0700

    terminal: filter untrusted recent commands

--- src/vs/workbench/contrib/terminalContrib/history/test/browser/terminalRunRecentQuickPick.test.ts
+++ src/vs/workbench/contrib/terminalContrib/history/test/browser/terminalRunRecentQuickPick.test.ts
@@ -6,7 +6,7 @@
 import assert from 'assert';
 import { ensureNoDisposablesAreLeakedInTestSuite } from '../../../../../../base/test/common/utils.js';
 import { ITerminalInstance } from '../../../../terminal/browser/terminal.js';
-import { runRecentSelection } from '../../browser/terminalRunRecentQuickPick.js';
+import { isSafeTerminalHistoryText, runRecentSelection } from '../../browser/terminalRunRecentQuickPick.js';
 
 suite('TerminalRunRecentQuickPick', () => {
 	ensureNoDisposablesAreLeakedInTestSuite();
@@ -25,5 +25,17 @@ suite('TerminalRunRecentQuickPick', () => {
 			didRun: false,
 			calls: { focused: 1, commands: [] },
 		});
+	});
+
+	test('rejects terminal control characters', () => {
+		const rejected = [0x00, 0x08, 0x09, 0x0A, 0x0D, 0x1B, 0x1F, 0x7F, 0x80, 0x85, 0x9B, 0x9F];
+		assert.deepStrictEqual(
+			rejected.map(code => isSafeTerminalHistoryText(`echo${String.fromCharCode(code)}value`)),
+			rejected.map(() => false)
+		);
+		assert.deepStrictEqual(
+			['echo value', `echo${String.fromCharCode(0x7E)}value`, `echo${String.fromCharCode(0xA0)}value`].map(isSafeTerminalHistoryText),
+			[true, true, true]
+		);
 	});
 });
