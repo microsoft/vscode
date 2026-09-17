98ea30730e127268a30b43af0367df1b3d471ccc
Deniz Güney Yıldırım <dnzgny99@gmail.com>
2026-09-18 01:14:26 +0300

    Refactor Extension class by removing status bar item
    Removed status bar item initialization and related code from the Extension class. Updated manual refresh command to directly trigger the NpmUpToDateFeature method.

--- .vscode/extensions/vscode-extras/src/extension.ts
+++ .vscode/extensions/vscode-extras/src/extension.ts
@@ -9,28 +9,23 @@ import { NpmUpToDateFeature } from './npmUpToDateFeature';
 export class Extension extends vscode.Disposable {
 	private readonly _output: vscode.LogOutputChannel;
 	private _npmFeature: NpmUpToDateFeature | undefined;
-	private readonly _statusBarItem: vscode.StatusBarItem;
 
-	constructor(context: vscode.ExtensionContext) {
+	constructor(_context: vscode.ExtensionContext) {
 		const disposables: vscode.Disposable[] = [];
 		super(() => disposables.forEach(d => d.dispose()));
 
 		this._output = vscode.window.createOutputChannel('VS Code Extras', { log: true });
 		disposables.push(this._output);
 
-		// Status Bar Item oluşturuluyor (Sağ taraf, öncelik 100)
-		this._statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
-		this._statusBarItem.command = 'vscode-extras.refreshNpmStatus';
-		this._statusBarItem.text = '$(sync~spin) NPM: Initializing';
-		this._statusBarItem.tooltip = 'Click to check NPM packages status manually';
-		disposables.push(this._statusBarItem);
-
-		// Manuel yenileme komutu kaydediliyor
+		// Manuel yenileme komutu doğrudan NpmUpToDateFeature üzerindeki metod tetikleyicisine bağlanıyor
 		disposables.push(
 			vscode.commands.registerCommand('vscode-extras.refreshNpmStatus', () => {
 				this._output.info('Manual NPM status refresh triggered via Status Bar.');
-				vscode.window.showInformationMessage('NPM Up-to-Date check triggered!');
-				// İleride buraya _npmFeature üzerinden bir refresh metod tetiklemesi eklenebilir
+				if (this._npmFeature) {
+					this._npmFeature.refresh();
+				} else {
+					vscode.window.showWarningMessage('NPM Up-to-Date feature is currently disabled.');
+				}
 			})
 		);
 
@@ -49,14 +44,11 @@ export class Extension extends vscode.Disposable {
 		const enabled = vscode.workspace.getConfiguration('vscode-extras').get<boolean>('npmUpToDateFeature.enabled', true);
 		if (enabled && !this._npmFeature) {
 			this._npmFeature = new NpmUpToDateFeature(this._output);
-			this._statusBarItem.text = '$(check) NPM: Active';
-			this._statusBarItem.show();
-			this._output.info('NpmUpToDateFeature enabled and Status Bar activated.');
+			this._output.info('NpmUpToDateFeature enabled.');
 		} else if (!enabled && this._npmFeature) {
 			this._npmFeature.dispose();
 			this._npmFeature = undefined;
-			this._statusBarItem.hide();
-			this._output.info('NpmUpToDateFeature disabled and Status Bar hidden.');
+			this._output.info('NpmUpToDateFeature disabled.');
 		}
 	}
 }
