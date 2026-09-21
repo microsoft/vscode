3b01eb9d2bddbc910a9f04bbaef8329113e80d88
SquarePots <46488165+squarepots@users.noreply.github.com>
2026-09-21 22:40:05 +0800

    Remove expired default account cache migration

--- src/vs/workbench/services/accounts/test/browser/defaultAccount.test.ts
+++ src/vs/workbench/services/accounts/test/browser/defaultAccount.test.ts
@@ -21,7 +21,7 @@ import { COPILOT_FORCE_REMOTE_SETTINGS_REFRESH_KEY, IFileManagedSettingsService,
 import { IManagedSettingsFreshness, ManagedSettingsFreshnessFailure, ManagedSettingsFreshnessState } from '../../../../../platform/policy/common/managedSettingsFreshness.js';
 import { IProductService } from '../../../../../platform/product/common/productService.js';
 import { IRequestService } from '../../../../../platform/request/common/request.js';
-import { InMemoryStorageService, IStorageService } from '../../../../../platform/storage/common/storage.js';
+import { InMemoryStorageService, IStorageService, StorageScope, StorageTarget } from '../../../../../platform/storage/common/storage.js';
 import { ITelemetryService } from '../../../../../platform/telemetry/common/telemetry.js';
 import { NullTelemetryService } from '../../../../../platform/telemetry/common/telemetryUtils.js';
 import { AuthenticationSession, AuthenticationSessionsChangeEvent, IAuthenticationExtensionsService, IAuthenticationService } from '../../../authentication/common/authentication.js';
@@ -42,6 +42,17 @@ suite('DefaultAccountProvider', () => {
 		account: { id: accountId, label: 'octocat' },
 		scopes: ['user:email'],
 	}];
+
+	test('reads wrapped cached account data and ignores retired flat cache data', async () => {
+		const provider = await createProvider(new TestRequestService(async () => jsonResponse({})));
+		const accountPolicyData = createCachedPolicy(false);
+
+		provider['storageService'].store('defaultAccount.cachedPolicyData', JSON.stringify({ accountPolicyData }), StorageScope.APPLICATION, StorageTarget.MACHINE);
+		assert.deepStrictEqual(provider['getCachedAccountData'](), { accountPolicyData, copilotTokenInfo: undefined });
+
+		provider['storageService'].store('defaultAccount.cachedPolicyData', JSON.stringify(accountPolicyData), StorageScope.APPLICATION, StorageTarget.MACHINE);
+		assert.strictEqual(provider['getCachedAccountData'](), null);
+	});
 
 	test('cached settings perform one startup compatibility fetch', async () => {
 		const requestService = new TestRequestService(async () => jsonResponse({
