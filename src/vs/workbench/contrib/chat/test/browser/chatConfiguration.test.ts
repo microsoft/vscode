0120f727fbef462ef7fb5aee2cd3acbe6bec7814
Paul <8560030+pwang347@users.noreply.github.com>
2026-09-24 17:15:32 -0700

    chat: Gate Discover separately and default the GitHub Feed on (#337810)

--- src/vs/workbench/contrib/chat/test/browser/chatConfiguration.test.ts
+++ src/vs/workbench/contrib/chat/test/browser/chatConfiguration.test.ts
@@ -9,7 +9,9 @@ import { Extensions as ConfigurationExtensions, IConfigurationRegistry } from '.
 import { Registry } from '../../../../../platform/registry/common/platform.js';
 import { ConfigurationMigration, Extensions as WorkbenchConfigurationExtensions, IConfigurationMigrationRegistry } from '../../../../common/configuration.js';
 import { ChatConfiguration } from '../../common/constants.js';
+import { CustomizationMarketplaceConfiguration } from '../../../../../platform/customizationMarketplace/common/customizationMarketplaceSources.js';
 import { chatProgressConfigurationProperties } from '../../browser/chatProgressConfiguration.js';
+import { customizationMarketplaceConfigurationProperties } from '../../browser/aiCustomization/customizationMarketplaceConfiguration.js';
 import '../../browser/agentSessionsConfiguration.js';
 
 const configurationProperties = Registry.as<IConfigurationRegistry>(ConfigurationExtensions.Configuration).getConfigurationProperties();
@@ -30,6 +32,16 @@ suite('Chat configuration', () => {
 
 	test('registers Agents Window settings in the shared workbench contribution', () => {
 		assert.deepStrictEqual(registeredAgentSessionsSettings, [true, true, true]);
+	});
+
+	test('Marketplace visibility is default-off while the GitHub Feed is default-on', () => {
+		assert.deepStrictEqual({
+			marketplace: customizationMarketplaceConfigurationProperties[CustomizationMarketplaceConfiguration.MarketplaceEnabled].default,
+			publicFeed: customizationMarketplaceConfigurationProperties[CustomizationMarketplaceConfiguration.AgentFinderPublicFeedEnabled].default,
+		}, {
+			marketplace: false,
+			publicFeed: true,
+		});
 	});
 
 	test('defaults persistent progress to Draw as an experimental setting', () => {
